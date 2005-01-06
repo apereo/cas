b3740a304457c3d7894d57f8b48e733c7787258e
William G. Thompson <wgthom@gmail.com>
2005-01-06 03:24:12 +0000

    first pass at unified service facade

---
+++ src/org/jasig/cas/Service.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2004 The JA-SIG Collaborative. All rights reserved. See license distributed with this file and available online at
+ * http://www.uportal.org/license.html
+ */
+package org.jasig.cas;
+
+import java.net.URL;
+
+/**
+ * A Service providing service to an authenticated principal.
+ * Typically a web application or web service.
+ * 
+ * @author William G. Thompson, Jr.
+ *
+ */
+public interface Service {
+    
+    URL getUrl();
+
+}
