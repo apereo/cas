21f0842a7ea57f882f0e23bac228ffdeab91fed8
Scott Battaglia <scott.battaglia@gmail.com>
2005-01-19 20:38:05 +0000

    combine controllers for CAS 1.0 and CAS 2.0

--- src/org/jasig/cas/web/ServiceValidateController.java
+++ src/org/jasig/cas/web/ServiceValidateController.java
@@ -4,6 +4,7 @@
  */
 package org.jasig.cas.web;
 
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -18,13 +19,15 @@ import org.apache.commons.logging.LogFactory;
 import org.jasig.cas.CentralAuthenticationService;
 import org.jasig.cas.authentication.Assertion;
 import org.jasig.cas.authentication.AuthenticationSpecification;
+import org.jasig.cas.authentication.Cas10ProtocolAuthenticationSpecification;
 import org.jasig.cas.authentication.Cas20ProtocolAuthenticationSpecification;
 import org.jasig.cas.authentication.SimpleService;
+import org.jasig.cas.authentication.principal.Principal;
 import org.jasig.cas.ticket.TicketException;
 import org.jasig.cas.web.support.ViewNames;
 import org.jasig.cas.web.support.WebConstants;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.util.StringUtils;
+import org.springframework.web.bind.BindUtils;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
@@ -39,13 +42,26 @@ public class ServiceValidateController extends AbstractController implements Ini
     protected final Log log = LogFactory.getLog(getClass());
 
     private CentralAuthenticationService centralAuthenticationService;
-
+    
+    private boolean cas10protocol = false;
+    
+    private Class authenticationSpecificationClass;
+    
     /**
      * @see org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
      */
     public void afterPropertiesSet() throws Exception {
         if (this.centralAuthenticationService == null) {
             throw new IllegalStateException("centralAuthenticationService cannot be null on " + this.getClass().getName());
+        }
+        
+        if (cas10protocol) {
+        	this.authenticationSpecificationClass = Cas10ProtocolAuthenticationSpecification.class;
+        }
+        
+        if (this.authenticationSpecificationClass == null) {
+        	this.authenticationSpecificationClass = Cas20ProtocolAuthenticationSpecification.class;
+        	log.info("No authentication specification class set.  Defaulting to " + this.authenticationSpecificationClass.getClass().getName());
         }
     }
     /**
@@ -55,10 +71,11 @@ public class ServiceValidateController extends AbstractController implements Ini
     protected ModelAndView handleRequestInternal(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
         final String serviceTicketId = request.getParameter(WebConstants.TICKET);
         final String service = request.getParameter(WebConstants.SERVICE);
-        final String renew = request.getParameter("renew");
         final Map model = new HashMap();
-        final AuthenticationSpecification authenticationSpecification = new Cas20ProtocolAuthenticationSpecification(StringUtils.hasText(renew));
+        final AuthenticationSpecification authenticationSpecification = this.getCommandClass();
         final Assertion assertion;
+        
+        BindUtils.bind(request, authenticationSpecification, "authenticationSpecification");
 
         try {
             assertion = centralAuthenticationService.validateServiceTicket(serviceTicketId, new SimpleService(service));
@@ -82,10 +99,24 @@ public class ServiceValidateController extends AbstractController implements Ini
                 
                 model.put(WebConstants.PROXIES, Collections.unmodifiableList(proxies));
             }
-            
+            if (this.cas10protocol) {
+        		final PrintWriter out = response.getWriter();
+                log.info("Successfully retrieved ServiceTicket for ticket id [" + serviceTicketId + "] and service [" + service + "]");
+                out.print("yes\n" + ((Principal) assertion.getChainedPrincipals().get(0)).getId() + "\n");
+                return null;
+            }
+
             return new ModelAndView(ViewNames.CONST_SERVICE_SUCCESS, model);
         }
         catch (TicketException te) {
+        	if (this.cas10protocol) {
+        		final PrintWriter out = response.getWriter();
+                log.info("Unable to retrieve ServiceTicket for ticket id [" + serviceTicketId + "] and service [" + service + "]");
+                out.print("no\n\n");
+                out.flush();
+                return null;
+        	}
+
             model.put(WebConstants.CODE, te.getCode());
             model.put(WebConstants.DESC, te.getDescription());
 
@@ -104,6 +135,15 @@ public class ServiceValidateController extends AbstractController implements Ini
         // TODO implement
     }
 
+    private AuthenticationSpecification getCommandClass() {
+    	try {
+    	return (AuthenticationSpecification) this.authenticationSpecificationClass.newInstance();
+    	} catch (Exception e) {
+    		throw new IllegalStateException("Unable to instanciate instance of " + this.authenticationSpecificationClass.getClass().getName());
+    	}
+    }
+    
+    
     /**
      * @param centralAuthenticationService The centralAuthenticationService to set.
      */
