0840aa9ab9a167c224144be926386901838f9b02
Scott Battaglia <scott.battaglia@gmail.com>
2005-01-25 17:54:53 +0000

    copyright

--- src/org/jasig/cas/web/bind/CredentialsBinder.java
+++ src/org/jasig/cas/web/bind/CredentialsBinder.java
@@ -1,3 +1,7 @@
+/* Copyright 2004 The JA-SIG Collaborative.  All rights reserved.
+ * See license distributed with this file and
+ * available online at http://www.uportal.org/license.html
+ */
 package org.jasig.cas.web.bind;
 
 import javax.servlet.http.HttpServletRequest;
@@ -5,6 +9,10 @@ import org.jasig.cas.authentication.principal.Credentials;
 
 
 /**
+ * Interface for a class that can bind items stored in the request to a particular
+ * credentials implementation.  This allows for binding beyond the basic
+ * JavaBean/Request parameter binding that is handled by Spring automatically.
+ * 
  * @author Scott Battaglia
  * @version $Id$
  *
