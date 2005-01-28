dc4f148fa7e9964cbaf9a7fe9d83aa482d4e0cff
Scott Battaglia <scott.battaglia@gmail.com>
2005-01-28 21:22:41 +0000

    copyright message

--- test/org/jasig/cas/ticket/registry/AbstractTicketRegistryTestCase.java
+++ test/org/jasig/cas/ticket/registry/AbstractTicketRegistryTestCase.java
@@ -1,3 +1,7 @@
+/* Copyright 2004 The JA-SIG Collaborative.  All rights reserved.
+ * See license distributed with this file and
+ * available online at http://www.uportal.org/license.html
+ */
 package org.jasig.cas.ticket.registry;
 
 import java.util.ArrayList;
