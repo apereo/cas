1551f00b41d027adb183dc63c064acdae4553dcb
Andrew Petro <apetro@unicon.net>
2005-02-02 22:39:34 +0000

    additional comments.

--- src/org/jasig/cas/authentication/Assertion.java
+++ src/org/jasig/cas/authentication/Assertion.java
@@ -7,8 +7,8 @@ package org.jasig.cas.authentication;
 import java.util.List;
 
 /**
- * Interface for returning the results of a validation attempt
- * on a ticket.
+ * Interface for returning the results of a successful validation of
+ * a ticket.
  * 
  * @author Scott Battaglia
  * @version $Id$
@@ -16,8 +16,24 @@ import java.util.List;
  */
 public interface Assertion {
 	
+    /**
+     * Get a List of Principals which represent the owners of the GrantingTickets
+     * which granted the ticket that was validated.
+     * The first Principal of this list is the Principal which originally authenticated 
+     * to CAS to obtain the first Granting Ticket.  Subsequent Principals are those
+     * associated with GrantingTickets that were granted from that original granting
+     * ticket.  The last Principal in this List is that associated with the GrantingTicket
+     * that was the immediate grantor of the ticket that was validated.
+     * The List returned by this method will contain at least one Principal.
+     * @return a List of Principals
+     */
     List getChainedPrincipals();
     
+    /**
+     * True if the validated ticket was granted in the same transaction as that in
+     * which its grantor GrantingTicket was originally issued.
+     * @return true if validated ticket was granted simultaneous with its grantor's issuance
+     */
     boolean isFromNewLogin();
 
 }
