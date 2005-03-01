99ef7fb279043fa455ec290c9890971b1b841823
Scott Battaglia <scott.battaglia@gmail.com>
2005-03-01 21:01:50 +0000

    start of service

---
+++ core/src/main/java/org/jasig/cas/services/ServiceRegistryManager.java
@@ -0,0 +1,27 @@
+/* Copyright 2004 The JA-SIG Collaborative.  All rights reserved.
+ * See license distributed with this file and
+ * available online at http://www.uportal.org/license.html
+ */
+package org.jasig.cas.services;
+
+
+/**
+ * @author Scott Battaglia
+ * @version $Id$
+ *
+ */
+public interface ServiceRegistryManager {
+    /**
+     * Method to manually add one service to the list.
+     * 
+     * @param service The service to add.
+     */
+    void addService(AuthenticatedService service);
+    
+    /**
+     * Method to delete a service from the list.
+     * 
+     * @param serviceId The service to delete
+     */
+    void deleteService(String serviceId);
+}
