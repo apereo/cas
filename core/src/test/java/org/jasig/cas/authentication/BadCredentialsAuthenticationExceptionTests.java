0204878d6175954fc2ef6aac9d9892f0e0e7f117
Scott Battaglia <scott.battaglia@gmail.com>
2005-03-14 14:35:04 +0000

    more test cases

---
+++ core/src/test/java/org/jasig/cas/authentication/BadCredentialsAuthenticationExceptionTests.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2005 The JA-SIG Collaborative.  All rights reserved.
+ * See license distributed with this file and
+ * available online at http://www.uportal.org/license.html
+ */
+package org.jasig.cas.authentication;
+
+import junit.framework.TestCase;
+
+/**
+ * 
+ * @author Scott Battaglia
+ * @version $Revision$ $Date$
+ * @since 3.0
+ *
+ */
+public class BadCredentialsAuthenticationExceptionTests extends TestCase {
+	public void testGetCode() {
+		AuthenticationException e = new BadCredentialsAuthenticationException();
+		assertEquals("error.authentication.credentials.bad", e.getCode());
+	}
+}
