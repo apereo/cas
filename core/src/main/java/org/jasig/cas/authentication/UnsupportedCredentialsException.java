972f1d42e4ff09fdb421b156aaec5e199be4bc83
Scott Battaglia <scott.battaglia@gmail.com>
2005-03-18 05:37:46 +0000

    made authentication exception abstract

--- core/src/main/java/org/jasig/cas/authentication/UnsupportedCredentialsException.java
+++ core/src/main/java/org/jasig/cas/authentication/UnsupportedCredentialsException.java
@@ -27,28 +27,6 @@ public class UnsupportedCredentialsException extends AuthenticationException {
         super();
     }
 
-    /**
-     * @param arg0
-     */
-    public UnsupportedCredentialsException(String arg0) {
-        super(arg0);
-    }
-
-    /**
-     * @param arg0
-     * @param arg1
-     */
-    public UnsupportedCredentialsException(String arg0, Throwable arg1) {
-        super(arg0, arg1);
-    }
-
-    /**
-     * @param arg0
-     */
-    public UnsupportedCredentialsException(Throwable arg0) {
-        super(arg0);
-    }
-
     public String getCode() {
         return CODE;
     }
