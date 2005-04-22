78c2f4729090428e7a4e7662265d3df21573bab0
Howard Gilbert <Howard.Gilbert@yale.edu>
2005-04-22 15:56:20 +0000

    Add or update Javadoc

--- core/src/main/java/org/jasig/cas/authentication/ImmutableAuthentication.java
+++ core/src/main/java/org/jasig/cas/authentication/ImmutableAuthentication.java
@@ -16,9 +16,14 @@ import org.apache.commons.lang.builder.ToStringBuilder;
 import org.jasig.cas.authentication.principal.Principal;
 
 /**
- * Default implementation of Authentication.
+ * The implementation of Authentation returned by AuthenticationManagerImpl.
  * 
- * <p> Immutable (no setters, all contents must be specified at construction)</p>
+ * <p>All property values must be set in the constructor and then may not
+ * be changed.
+ * Serializable since it is tied to the TGT and may be persisted.
+ * </p>
+ * 
+ * 
  * 
  * @author Dmitriy Kopylenko
  * @author Scott Battaglia
@@ -30,7 +35,7 @@ public final class ImmutableAuthentication implements Authentication {
     /** UID for serializing. */
     private static final long serialVersionUID = 3906647483978365235L;
 
-    /** The principal this authentication object is valid for. */
+    /** A Principal object returned by the CredentialsToPrincipalResolver */
     private final Principal principal; // TODO refactor to be an immutable
 
     // proxy?
@@ -38,9 +43,15 @@ public final class ImmutableAuthentication implements Authentication {
     /** The date/time this authentication object became valid. */
     private final Date authenticatedDate;
 
-    /** An arbitrary object to hold additional attributes. */
+    /** Associated authentication attributes. Initially empty but may
+     * be filled in by an AuthenticationAttributesPopulator */
     private final Map attributes;
 
+	/**
+	 * Only constructor, must provide values for properties.
+	 * @param principal Principal representing user
+	 * @param attributes Authentication attributes map.
+	 */
     protected ImmutableAuthentication(final Principal principal,
         final Map attributes) {
         this.principal = principal;
--- core/src/main/java/org/jasig/cas/authentication/principal/Credentials.java
+++ core/src/main/java/org/jasig/cas/authentication/principal/Credentials.java
@@ -10,6 +10,15 @@ import java.io.Serializable;
 /**
  * Marker interface for credentials required to authenticate a principal.
  * 
+ * <p>The Credentials is an opaque object passed from the Web layer
+ * through the CAS and AuthenticationManager layer to the various 
+ * Handler and Resolver objects. You add this marker to any object 
+ * you want to pass along this way. Credentials can contain a 
+ * userid and password, or a Certificate, or an IP address, or
+ * a cookie value. Some credentials require validation, while others
+ * (such as container based or Filter based validation) are 
+ * inherently trustworthy.
+ * 
  * @author William G. Thompson, Jr.
  * @version $Revision$ $Date$
  * @since 3.0
