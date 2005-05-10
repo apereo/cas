43ef1063f67a897844acef5b968907b2bf6b4d18
Scott Battaglia <scott.battaglia@gmail.com>
2005-05-10 19:55:09 +0000

    tests for web flow

---
+++ core/src/test/java/org/jasig/cas/web/flow/LogonFormActionTests.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2005 The JA-SIG Collaborative. All rights reserved. See license
+ * distributed with this file and available online at
+ * http://www.uportal.org/license.html
+ */
+package org.jasig.cas.web.flow;
+
+import junit.framework.TestCase;
+
+
+public class LogonFormActionTests extends TestCase {
+
+    public void testGetLoginToken() {
+        
+    }
+    
+    public void testSubmitNoLoginToken() {
+        
+    }
+    
+    public void testSubmitBadCredentials() {
+        
+    }
+    
+    public void testSubmitProperCredentialsWithService() {
+        
+    }
+    
+    public void testSubmitProperCredentialsWithNoService() {
+        
+    }
+    
+}
