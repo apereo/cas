24abe5f4f4b1a0bdca6bdd1c2d01514fdb803a4a
Scott Battaglia <scott.battaglia@gmail.com>
2005-05-12 01:50:55 +0000

    CAS-158
    removed tests related to extending Credentials marked as final

--- adaptors/cas/src/test/java/org/jasig/cas/adaptors/cas/LegacyCasCredentialsBinderTest.java
+++ adaptors/cas/src/test/java/org/jasig/cas/adaptors/cas/LegacyCasCredentialsBinderTest.java
@@ -5,7 +5,10 @@
  */
 package org.jasig.cas.adaptors.cas;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.jasig.cas.authentication.principal.Credentials;
+import org.jasig.cas.web.bind.CredentialsBinder;
 import org.springframework.mock.web.MockHttpServletRequest;
 
 import junit.framework.TestCase;
@@ -16,6 +19,8 @@ import junit.framework.TestCase;
  */
 public class LegacyCasCredentialsBinderTest extends TestCase {
 
+    private CredentialsBinder credentialsBinder = new LegacyCasCredentialsBinder();
+
     protected void setUp() throws Exception {
         super.setUp();
     }
@@ -25,77 +30,33 @@ public class LegacyCasCredentialsBinderTest extends TestCase {
     }
 
     /**
-     * Test that binding succeeds for a subclass of LegacyCasCredentials..
-     */
-    public void testBindCasCredentialsSubclass() {
-        LegacyCasCredentialsBinder binder = new LegacyCasCredentialsBinder();
-        MockHttpServletRequest mockRequest = new MockHttpServletRequest();
-        LegacyCasCredentialsSubclass legacyCasCredentials 
-            = new LegacyCasCredentialsSubclass();
-        binder.bind(mockRequest, legacyCasCredentials);
-        
-        assertSame(mockRequest, legacyCasCredentials.getServletRequest());
-    }
-    
-    /**
-     * Test that binding succeeds for a subclass of LegacyCasTrustedCredentials.
-     */
-    public void testBindTrustedCasCredentialsSubclass() {
-        LegacyCasCredentialsBinder binder = new LegacyCasCredentialsBinder();
-        MockHttpServletRequest mockRequest = new MockHttpServletRequest();
-        LegacyCasTrustedCredentialsSubclass legacyCasTrustedCredentials 
-            = new LegacyCasTrustedCredentialsSubclass();
-        
-        binder.bind(mockRequest, legacyCasTrustedCredentials);
-        
-        assertSame(mockRequest, legacyCasTrustedCredentials.getServletRequest());
-    }
-    
-    /**
      * Tests that we support LegacyCasCredentials and LegacyCasTrustedCredentials 
      * and that we do not support some adhoc Credentials that are not and
      * do not subclass these credentials.
      */
     public void testSupports() {
-        LegacyCasCredentialsBinder binder = new LegacyCasCredentialsBinder();
-        assertTrue(binder.supports(LegacyCasCredentials.class));
-        assertTrue(binder.supports(LegacyCasTrustedCredentials.class));
-        assertFalse(binder.supports(AdHocUnsupportedCredentials.class));
+        assertTrue(this.credentialsBinder.supports(LegacyCasCredentials.class));
+        assertTrue(this.credentialsBinder
+            .supports(LegacyCasTrustedCredentials.class));
+        assertFalse(this.credentialsBinder
+            .supports(AdHocUnsupportedCredentials.class));
     }
 
-    /**
-     * Test that binding succeeds for a subclass of LegacyCasCredentials
-     * and for a subclass of LegacyCasTrustedCredentials.
-     */
-    public void testSupportsSubclasses() {
-        
-        LegacyCasCredentialsBinder binder = new LegacyCasCredentialsBinder();
-        assertTrue(binder.supports(LegacyCasCredentialsSubclass.class));
-        assertTrue(binder.supports(LegacyCasTrustedCredentialsSubclass.class));
-        
+    public void testBindMethod() {
+        HttpServletRequest request = new MockHttpServletRequest();
+        LegacyCasCredentials credentials = new LegacyCasCredentials();
+
+        this.credentialsBinder.bind(request, credentials);
+
+        assertEquals(request, credentials.getServletRequest());
     }
 
-    /**
-     * A dummy subclass of LegacyCasCredentials that we use to test that 
-     * LegacyCasCredentialsBinder binds subclasses of LegacyCasCredentials.
-     */
-    private class LegacyCasCredentialsSubclass extends LegacyCasCredentials {
-        // just being a subclass is sufficient for our testing needs.
-    }
-    
-    /**
-     * A dummy subclass of LegacyCasTrustedCredentials that we use to test that 
-     * LegacyCasCredentialsBinder binds subclasses of LegacyCasTrustedCredentials.
-     */
-    private class LegacyCasTrustedCredentialsSubclass extends LegacyCasTrustedCredentials {
-        // just being a subclass is sufficient for our testing needs.
-    }
-    
     /**
      * We test that we do not support these adhoc non-legacy do-nothing credentials.
      */
     private class AdHocUnsupportedCredentials implements Credentials {
+
+        private static final long serialVersionUID = 3257285812100936752L;
         // does nothing
     }
 }
-
