8d27da1b802d0a3d518f2c51b2f4cf2aa84a0c29
Scott Battaglia <scott.battaglia@gmail.com>
2005-05-24 18:57:19 +0000

    updates to Spring WebFlow Preview 3 and code reviewing

--- core/src/test/java/org/jasig/cas/web/flow/LogonFormActionTests.java
+++ core/src/test/java/org/jasig/cas/web/flow/LogonFormActionTests.java
@@ -5,8 +5,6 @@
  */
 package org.jasig.cas.web.flow;
 
-import java.util.HashMap;
-
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
@@ -61,34 +59,14 @@ public class LogonFormActionTests extends TestCase {
         this.centralAuthenticationService.setTicketGrantingTicketExpirationPolicy(new NeverExpiresExpirationPolicy());
         this.centralAuthenticationService.setUniqueTicketIdGenerator(new DefaultUniqueTicketIdGenerator());
         this.logonFormAction.setCentralAuthenticationService(this.centralAuthenticationService);
-        this.logonFormAction.setLoginTokens(new HashMap());
         this.logonFormAction.afterPropertiesSet();
     }
-
-    public void testGetLoginToken() throws Exception {
-        MockRequestContext context = new MockRequestContext();
-        context.setSourceEvent(new ServletEvent(new MockHttpServletRequest(), new MockHttpServletResponse()));
-        
-        assertEquals("success", this.logonFormAction.setupReferenceData(context).getId());
-        assertNotNull(ContextUtils.getAttribute(context, "loginToken"));
-    }
-    
-    public void testSubmitNoLoginToken() throws Exception {
-        MockRequestContext context = new MockRequestContext();
-        context.setSourceEvent(new ServletEvent(new MockHttpServletRequest(), new MockHttpServletResponse()));
-        
-        assertEquals("error", this.logonFormAction.submit(context).getId());
-    }
-    
+ 
     public void testSubmitBadCredentials() throws Exception {
         MockRequestContext context = new MockRequestContext();
         MockHttpServletRequest request = new MockHttpServletRequest();
         context.setSourceEvent(new ServletEvent(request, new MockHttpServletResponse()));
         
-        this.logonFormAction.setupReferenceData(context);
-        final String loginToken = (String) ContextUtils.getAttribute(context, "loginToken");
-        
-        request.addParameter("lt", loginToken);
         final UsernamePasswordCredentials credentials = new UsernamePasswordCredentials();
         credentials.setUsername("test");
         credentials.setPassword("test2");
@@ -104,10 +82,6 @@ public class LogonFormActionTests extends TestCase {
         MockHttpServletRequest request = new MockHttpServletRequest();
         context.setSourceEvent(new ServletEvent(request, new MockHttpServletResponse()));
         
-        this.logonFormAction.setupReferenceData(context);
-        final String loginToken = (String) ContextUtils.getAttribute(context, "loginToken");
-        
-        request.addParameter("lt", loginToken);
         request.addParameter("service", "test");
         final UsernamePasswordCredentials credentials = new UsernamePasswordCredentials();
         credentials.setUsername("test");
@@ -124,10 +98,6 @@ public class LogonFormActionTests extends TestCase {
         MockHttpServletRequest request = new MockHttpServletRequest();
         context.setSourceEvent(new ServletEvent(request, new MockHttpServletResponse()));
         
-        this.logonFormAction.setupReferenceData(context);
-        final String loginToken = (String) ContextUtils.getAttribute(context, "loginToken");
-        
-        request.addParameter("lt", loginToken);
         final UsernamePasswordCredentials credentials = new UsernamePasswordCredentials();
         credentials.setUsername("test");
         credentials.setPassword("test");
@@ -143,10 +113,6 @@ public class LogonFormActionTests extends TestCase {
         MockHttpServletRequest request = new MockHttpServletRequest();
         context.setSourceEvent(new ServletEvent(request, new MockHttpServletResponse()));
         
-        this.logonFormAction.setupReferenceData(context);
-        final String loginToken = (String) ContextUtils.getAttribute(context, "loginToken");
-        
-        request.addParameter("lt", loginToken);
         request.addParameter("warn", "on");
         final UsernamePasswordCredentials credentials = new UsernamePasswordCredentials();
         credentials.setUsername("test");
@@ -166,11 +132,7 @@ public class LogonFormActionTests extends TestCase {
         MockHttpServletRequest request = new MockHttpServletRequest();
         context.setSourceEvent(new ServletEvent(request, new MockHttpServletResponse()));
         
-        this.logonFormAction.setupReferenceData(context);
-        final String loginToken = (String) ContextUtils.getAttribute(context, "loginToken");
-        
         request.addParameter("service", "true");
-        request.addParameter("lt", loginToken);
         request.addParameter("renew", "true");
         final UsernamePasswordCredentials credentials = new UsernamePasswordCredentials();
         credentials.setUsername("test");
@@ -185,9 +147,9 @@ public class LogonFormActionTests extends TestCase {
         assertEquals("success", this.logonFormAction.submit(context).getId());
     }
     
-    public void testAfterPropertiesSetNoLoginToken() {
+    public void testAfterPropertiesSetCas() {
         try {
-            this.logonFormAction.setLoginTokens(null);
+            this.logonFormAction.setCentralAuthenticationService(null);
             this.logonFormAction.afterPropertiesSet();
             fail("Exception expected.");
         } catch (Exception e) {
