928565c92cc34f77da2c50e51b3366d757575116
Scott Battaglia <scott.battaglia@gmail.com>
2005-05-26 19:06:11 +0000

    java doc updates

--- core/src/main/java/org/jasig/cas/authentication/handler/BlockedCredentialsAuthenticationException.java
+++ core/src/main/java/org/jasig/cas/authentication/handler/BlockedCredentialsAuthenticationException.java
@@ -19,24 +19,47 @@ public class BlockedCredentialsAuthenticationException extends
     /** Unique ID for serialization. */
     private static final long serialVersionUID = 3544669598642420017L;
 
-    /** The code description of this exception. */
+    /** The default code for this exception used for message resolving. */
     private static final String CODE = "error.authentication.credentials.blocked";
 
+    /**
+     * Default constructor that does not allow the chaining of exceptions and
+     * uses the default code as the error code for this exception.
+     */
     public BlockedCredentialsAuthenticationException() {
         super(CODE);
     }
 
+    /**
+     * Constructor that allows for the chaining of exceptions. Defaults to the
+     * default code provided for this exception.
+     * 
+     * @param throwable the chained exception.
+     */
     public BlockedCredentialsAuthenticationException(final Throwable throwable) {
         super(CODE, throwable);
     }
 
+    /**
+     * Constructor that allows for providing a custom error code for this class.
+     * Error codes are often used to resolve exceptions into messages. Providing
+     * a custom error code allows the use of a different message.
+     * 
+     * @param code the custom code to use with this exception.
+     */
     public BlockedCredentialsAuthenticationException(final String code) {
         super(code);
     }
 
+    /**
+     * Constructor that allows for chaining of exceptions and a custom error
+     * code.
+     * 
+     * @param code the custom error code to use in message resolving.
+     * @param throwable the chained exception.
+     */
     public BlockedCredentialsAuthenticationException(final String code,
         final Throwable throwable) {
         super(code, throwable);
     }
-
 }
--- core/src/main/java/org/jasig/cas/authentication/handler/PlainTextPasswordEncoder.java
+++ core/src/main/java/org/jasig/cas/authentication/handler/PlainTextPasswordEncoder.java
@@ -6,8 +6,8 @@
 package org.jasig.cas.authentication.handler;
 
 /**
- * Default password translator for the case where no password translator is
- * needed. Translation results in the same password that was passed in.
+ * Default password encoder for the case where no password encoder is needed.
+ * Encoding results in the same password that was passed in.
  * 
  * @author Scott Battaglia
  * @version $Revision$ $Date$
