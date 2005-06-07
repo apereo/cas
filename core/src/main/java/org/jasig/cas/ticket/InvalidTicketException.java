4157dd5ab3fc4fee9189ca9f5c2fb93e46e01ba7
Drew Mazurek <dmazurek@unicon.net>
2005-06-07 04:19:09 +0000

    Renamed TicketNotFoundException to InvalidTicketException and fixed a place in CentralAuthenticationServiceImpl where the wrong exception was thrown (CAS-196)

--- core/src/main/java/org/jasig/cas/ticket/InvalidTicketException.java
+++ core/src/main/java/org/jasig/cas/ticket/InvalidTicketException.java
@@ -6,13 +6,13 @@
 package org.jasig.cas.ticket;
 
 /**
- * TicketException to alert that a Ticket was not found.
+ * TicketException to alert that a Ticket was not found or that it is expired.
  * 
  * @author Scott Battaglia
  * @version $Revison$ $Date$
  * @since 3.0
  */
-public class TicketNotFoundException extends TicketException {
+public class InvalidTicketException extends TicketException {
 
     /** The Unique Serializable ID. */
     private static final long serialVersionUID = 3256723974594508849L;
@@ -21,19 +21,19 @@ public class TicketNotFoundException extends TicketException {
     private static final String CODE = "INVALID_TICKET";
 
     /**
-     * Constructs a TicketNotFoundException with the default exception code.
+     * Constructs a InvalidTicketException with the default exception code.
      */
-    public TicketNotFoundException() {
+    public InvalidTicketException() {
         super(CODE);
     }
 
     /**
-     * Constructs a TicketNotFoundException with the default exception code and
+     * Constructs a InvalidTicketException with the default exception code and
      * the original exception that was thrown.
      * 
      * @param throwable the chained exception
      */
-    public TicketNotFoundException(final Throwable throwable) {
+    public InvalidTicketException(final Throwable throwable) {
         super(CODE, throwable);
     }
 }
