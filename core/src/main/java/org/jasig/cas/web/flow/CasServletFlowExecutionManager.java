d93c91ffc4ab9be3b86d32091edec871553a5e6e
Scott Battaglia <scott.battaglia@gmail.com>
2005-06-07 15:41:19 +0000

    javadoc comment

--- core/src/main/java/org/jasig/cas/web/flow/CasServletFlowExecutionManager.java
+++ core/src/main/java/org/jasig/cas/web/flow/CasServletFlowExecutionManager.java
@@ -18,10 +18,16 @@ import org.springframework.web.flow.execution.servlet.ServletFlowExecutionManage
  */
 public class CasServletFlowExecutionManager extends ServletFlowExecutionManager {
 
+    /**
+     * The string constant representing what CAS identifies as the Flow Id.
+     * Matches the Login Token id from CAS2 to eliminate the need to duplicate
+     * logic.
+     */
     private static final String LOGIN_TOKEN = "lt";
 
     /**
-     * This method is overridden to match our login token id from the CAS 2 domain.
+     * This method is overridden to match our login token id from the CAS 2
+     * domain.
      */
     protected final String getFlowExecutionIdParameterName() {
         return LOGIN_TOKEN;
