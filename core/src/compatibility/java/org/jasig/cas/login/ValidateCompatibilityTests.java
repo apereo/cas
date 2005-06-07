167f79cfd8cf55cd73c7c1fd996f058a3f234fce
Andrew Petro <apetro@unicon.net>
2005-06-07 22:38:23 +0000

    CAS-203 enhanced CAS 1 validation response for failed validation to test for exact match to spec.

--- core/src/compatibility/java/org/jasig/cas/login/ValidateCompatibilityTests.java
+++ core/src/compatibility/java/org/jasig/cas/login/ValidateCompatibilityTests.java
@@ -5,7 +5,9 @@
  */
 package org.jasig.cas.login;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
@@ -29,12 +31,40 @@ public class ValidateCompatibilityTests extends AbstractCompatibilityTests {
     public void testNoParameters() {
         beginAt("/validate");
         assertTextPresent("no");
+        
+        // here we test that the response was exactly that specified 
+        // in section 2.4.2 of the CAS spec
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(byteArrayOutputStream);
+        
+        dumpResponse(printStream);
+        
+        String validateOutput = byteArrayOutputStream.toString();
+        
+        String exactExpectedResponse = "no\n\n";
+        
+        assertEquals(exactExpectedResponse, validateOutput);
+
     }
     
     public void testBadServiceTicket() throws UnsupportedEncodingException {
         final String service = "https://localhost:8443/contacts-cas/j_acegi_cas_security_check";
         beginAt("/validate?service=" + URLEncoder.encode(service, "UTF-8") + "&ticket=test");
         assertTextPresent("no");
+        
+        // here we test that the response was exactly that specified 
+        // in section 2.4.2 of the CAS spec
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(byteArrayOutputStream);
+        
+        dumpResponse(printStream);
+        
+        String validateOutput = byteArrayOutputStream.toString();
+        
+        String exactExpectedResponse = "no\n\n";
+        
+        assertEquals(exactExpectedResponse, validateOutput);
+
     }
     
     public void testProperCredentialsAndServiceTicket() throws UnsupportedEncodingException {
