b25d4090468cb394dff62b4ffc0b7ffae645eac5
Andrew Petro <apetro@unicon.net>
2005-06-10 11:18:53 +0000

    Extracted out service ticket extraction as helper class / method to make this available for use in other compat tests.

--- core/src/compatibility/java/org/jasig/cas/login/ValidateCompatibilityTests.java
+++ core/src/compatibility/java/org/jasig/cas/login/ValidateCompatibilityTests.java
@@ -85,64 +85,7 @@ public class ValidateCompatibilityTests extends AbstractCompatibilityTests {
         HttpUnitDialog htDialog = getDialog();
         String response = htDialog.getResponse().getText();
         
-        // the serviceTicket, which we'll find in one of two ways
-        String serviceTicket = null;
-        
-        // now we need to extract the service ticket.
-        
-        // in a baseline CAS 2.x distribution return to the service is accomplished by 
-        // JavaScript redirect
-        // 
-        // CAS 3 accomplishes this differently such that our client has already
-        // followed the redirect to the service, so we'll find the service ticket
-        // on the response URL.
-        
-        String queryString = htDialog.getResponse().getURL().getQuery();
-        
-        int ticketIndex = queryString.indexOf("ticket=");
-        
-        if (ticketIndex == -1) {
-
-        	// the ticket wasn't in the response URL.
-            // we're testing for CAS 2.x style JavaScript for redirection, as 
-        	// recommended in appendix B of the CAS 2 protocol specification
-        	
-        	// parse the ticket out of the JavaScript
-            
-            int declarationStartsAt = response.indexOf("window.location.href");
-            // cut off the front of the response up to the beginning of the service URL
-            String responseAfterWindowLocHref = response.substring(declarationStartsAt + "window.location.href12".length());
-            
-            // The URL might be single or double quoted
-            final int endDoubleQuoteIndex = responseAfterWindowLocHref.indexOf("\"");
-            final int endSingleQuoteIndex = responseAfterWindowLocHref.indexOf("\'");
-            
-            // we will set this variable to be the index of the first ' or " character
-            int endQuoteIndex = 0;
-            if (endDoubleQuoteIndex == -1 && endSingleQuoteIndex == -1) {
-            	fail("Failed parsing a service ticket from the response:" + response);
-            } else if ( (endDoubleQuoteIndex > -1) && 
-            		(endDoubleQuoteIndex < endSingleQuoteIndex || endSingleQuoteIndex == -1)) {
-            	endQuoteIndex = endDoubleQuoteIndex;
-            } else {
-            	endQuoteIndex = endSingleQuoteIndex;
-            }
-            
-            int ticketEqualsIndex = responseAfterWindowLocHref.indexOf("ticket=");
-            
-            serviceTicket = responseAfterWindowLocHref.substring(ticketEqualsIndex + "ticket=".length(), endQuoteIndex);
-
-        	
-        } else {
-        	// service ticket was found on query String, parse it from there
-        	
-        	// TODO Is this type of redirection compatible?  
-        	// Does it address all the issues that CAS2 JavaScript redirection
-        	// was intended to address?
-        	
-        	serviceTicket = queryString.substring(ticketIndex + "ticket=".length(), queryString.length());
-        	
-        }
+        String serviceTicket = LoginHelper.serviceTicketFromResponse(htDialog.getResponse());
         
         beginAt("/validate?service=" + URLEncoder.encode(service, "UTF-8") + "&ticket=" + serviceTicket);
         assertTextPresent("yes");
