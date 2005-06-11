7ea088881036754a371c3992f2e1ad8e623b658e
Andrew Petro <apetro@unicon.net>
2005-06-11 05:19:36 +0000

    added properties for a second username, passsword pair.

--- core/src/compatibility/java/org/jasig/cas/login/AbstractLoginCompatibilityTests.java
+++ core/src/compatibility/java/org/jasig/cas/login/AbstractLoginCompatibilityTests.java
@@ -11,6 +11,18 @@ import java.io.IOException;
 public abstract class AbstractLoginCompatibilityTests extends AbstractCompatibilityTests {
     public static final String FORM_USERNAME = "username";
     public static final String FORM_PASSWORD = "password";
+    
+    /**
+     * The name of the compatibility test configuration property the value of which
+     * should be an alternate username to test logging into the CAS server.
+     */
+    public static final String ALTERNATE_USERNAME_PROPERTY = "credentials.alternate.username";
+    
+    /**
+     * The name of the compatibility test configuration property the value of which should be 
+     * the correct password to go with that alternate username.
+     */
+    public static final String ALTERNATE_PASSWORD_PROPERTY = "credentials.alternate.password";
 
     public AbstractLoginCompatibilityTests() throws IOException {
         super();
@@ -19,6 +31,15 @@ public abstract class AbstractLoginCompatibilityTests extends AbstractCompatibil
     public AbstractLoginCompatibilityTests(String name) throws IOException {
         super(name);
     }
+    
+    protected String getAlternateUsername(){
+    	return getProperties().getProperty(ALTERNATE_USERNAME_PROPERTY);
+    }
+    
+    protected String getAlternatePassword() {
+    	return getProperties().getProperty(ALTERNATE_PASSWORD_PROPERTY);
+    }
+	
 
     
 }
