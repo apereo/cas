8637fcbede71c9ec5ecc5ae5cf8719451744a224
Andrew Petro <apetro@unicon.net>
2005-06-16 21:44:00 +0000

    CAS-235 JavaDoc and deprecate CAS 2 AuthHandler.

--- adaptors/cas/src/main/java/edu/yale/its/tp/cas/auth/AuthHandler.java
+++ adaptors/cas/src/main/java/edu/yale/its/tp/cas/auth/AuthHandler.java
@@ -1,6 +1,16 @@
 package edu.yale.its.tp.cas.auth;
 
-/** Marker interface for authentication handlers. */
+/** 
+ * Marker interface for CAS 2 authentication handlers. 
+ * 
+ * Deprecated in the context of CAS 3 as CAS 3 provides new authentication APIs.
+ * However, CAS 2 AuthHandler implementations continue to work (via adaptor) in
+ * CAS 3.
+ *
+ * @since CAS 2.0
+ * @version $Revision$ $Date$
+ * @deprecated Recommendation is to use the CAS 3 APIs.  See type comments. 
+ */
 public interface AuthHandler {
     // no methods as this is a marker interface
 }
