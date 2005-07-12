b6ea6d39a45889f601172080a8ad58580ab3dea8
Scott Battaglia <scott.battaglia@gmail.com>
2005-07-12 12:21:29 +0000

    CAS-233
    updated javadoc

--- adaptors/generic/src/main/java/org/jasig/cas/adaptors/generic/RejectUsersAuthenticationHandler.java
+++ adaptors/generic/src/main/java/org/jasig/cas/adaptors/generic/RejectUsersAuthenticationHandler.java
@@ -11,7 +11,25 @@ import org.jasig.cas.authentication.handler.support.AbstractUsernamePasswordAuth
 import org.jasig.cas.authentication.principal.UsernamePasswordCredentials;
 
 /**
- * Class to provide a list of users to automatically reject.
+ * AuthenticationHandler which fails to authenticate a user purporting to be one
+ * of the blocked usernames, and blindly authenticates all other users.
+ * <p>
+ * Note that the current implementation of AuthenticationManagerImpl tries all
+ * AuthenitcationHandlers supporting a particular Credentials until it finds one
+ * that succeeds. Since this implementation succeeds on all but those it is
+ * configured to reject, adding it to the default AuthenticationManagerImpl has
+ * the consequence that all users not explicitly rejected will be available for
+ * authentication using any password (regardless of other handlers). Also note
+ * that since this implementation does not throw any AuthenticationExceptions,
+ * but only returns false for rejected users, rejecting a user using this
+ * AuthenticationHandler will not prevent another AuthenticationHandler from
+ * authenticating those credentials.
+ * <p>
+ * This is, under the current implementation of AuthenticationManagerImpl,
+ * adding an instance of RejectUsersAuthenticationHandler to the
+ * authenticationManager's List of AuthenticationHandlers will have no effect
+ * for rejected users and will have the effect of allowing all other users to
+ * authenticate regardless of presented password.
  * 
  * @author Scott Battaglia
  * @version $Revision$ $Date$
@@ -37,7 +55,9 @@ public final class RejectUsersAuthenticationHandler extends
     }
 
     /**
-     * @param users The users to set.
+     * Set the Collection of usernames which we will fail to authenticate.
+     * 
+     * @param users The Collection of usernames we should not authenticate.
      */
     public void setUsers(final Collection users) {
         this.users = users;
