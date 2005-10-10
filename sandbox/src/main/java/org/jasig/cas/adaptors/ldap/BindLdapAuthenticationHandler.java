9138c9dd0268bd0cff81466d39a073ee82c0d8f1
Scott Battaglia <scott.battaglia@gmail.com>
2005-10-10 19:08:12 +0000

    updates to clean up bind handler based on Seth Ladd's comments

--- sandbox/src/main/java/org/jasig/cas/adaptors/ldap/BindLdapAuthenticationHandler.java
+++ sandbox/src/main/java/org/jasig/cas/adaptors/ldap/BindLdapAuthenticationHandler.java
@@ -18,7 +18,6 @@ import org.jasig.cas.adaptors.ldap.util.LdapUtils;
 import org.jasig.cas.authentication.handler.AuthenticationHandler;
 import org.jasig.cas.authentication.principal.Credentials;
 import org.jasig.cas.authentication.principal.UsernamePasswordCredentials;
-import org.springframework.ldap.core.AttributeHelper;
 import org.springframework.ldap.core.SearchResultCallbackHandler;
 import org.springframework.ldap.core.support.LdapDaoSupport;
 
@@ -29,22 +28,20 @@ import org.springframework.ldap.core.support.LdapDaoSupport;
  * @version $Revision$ $Date$
  * @since 3.0
  */
-public class BindLdapAuthenticationHandler extends
-    LdapDaoSupport implements AuthenticationHandler {
-
-    private static final String[] RETURN_VALUES = new String[] {"cn"};
+public class BindLdapAuthenticationHandler extends LdapDaoSupport implements
+    AuthenticationHandler {
 
     private static final int DEFAULT_MAX_NUMBER_OF_RESULTS = 1000;
 
     private static final int DEFAULT_TIMEOUT = 1000;
 
+    private static final int[] VALID_SCOPE_VALUES = new int[] {
+        SearchControls.OBJECT_SCOPE, SearchControls.ONELEVEL_SCOPE,
+        SearchControls.SUBTREE_SCOPE};
+
     private String searchBase;
 
-    private boolean scopeOneLevel;
-
-    private boolean scopeObject;
-
-    private boolean scopeSubtree;
+    private int scope = SearchControls.SUBTREE_SCOPE;
 
     private String filter;
 
@@ -52,23 +49,21 @@ public class BindLdapAuthenticationHandler extends
 
     private int timeout = DEFAULT_TIMEOUT;
 
-    private int scopeValue;
-
     private boolean allowMultipleAccounts;
 
     public boolean authenticate(final Credentials request) {
         final UsernamePasswordCredentials uRequest = (UsernamePasswordCredentials) request;
 
-        List values = (List) this.getLdapTemplate().search(this.searchBase,
+        final List values = (List) this.getLdapTemplate().search(
+            this.searchBase,
             LdapUtils.getFilterWithValues(this.filter, uRequest.getUsername()),
             this.getSearchControls(), new SearchResultCallbackHandler(){
 
-                private List cns = new ArrayList();
+                private final List cns = new ArrayList();
 
-                public void processSearchResult(SearchResult searchResult)
+                public void processSearchResult(final SearchResult searchResult)
                     throws NamingException {
-                    this.cns.add(AttributeHelper.getAttributeAsString(
-                        searchResult, "cn"));
+                    this.cns.add(searchResult.getName());
                 }
 
                 public Object getResult() {
@@ -76,21 +71,24 @@ public class BindLdapAuthenticationHandler extends
                 }
             });
 
-        if (values == null || values.isEmpty())
+        if (values == null || values.isEmpty()
+            || (values.size() > 1 && !this.allowMultipleAccounts)) {
             return false;
+        }
 
-        if (values.size() > 1 && !this.allowMultipleAccounts)
-            return false;
+        for (final Iterator iter = values.iterator(); iter.hasNext();) {
+            final String dn = (String) iter.next();
 
-        for (Iterator iter = values.iterator(); iter.hasNext();) {
-            String dn = (String) iter.next();
+            DirContext test = null;
+            try {
+                test = this.getContextSource().getDirContext(
+                    dn + "," + this.searchBase, uRequest.getPassword());
 
-            DirContext test = this.getContextSource().getDirContext(
-                dn + "," + this.searchBase, uRequest.getPassword());
-
-            if (test != null) {
+                if (test != null) {
+                    return true;
+                }
+            } finally {
                 org.springframework.ldap.support.LdapUtils.closeContext(test);
-                return true;
             }
         }
 
@@ -98,32 +96,23 @@ public class BindLdapAuthenticationHandler extends
     }
 
     protected SearchControls getSearchControls() {
-        SearchControls constraints = new SearchControls(this.scopeValue,
-            this.maxNumberResults, this.timeout, RETURN_VALUES, false, false);
-        constraints.setSearchScope(SearchControls.SUBTREE_SCOPE);
+        final SearchControls constraints = new SearchControls();
+        constraints.setSearchScope(this.scope);
+        constraints.setReturningAttributes(new String[0]);
+        constraints.setTimeLimit(this.timeout);
+        constraints.setCountLimit(this.maxNumberResults);
 
         return constraints;
     }
 
     protected void initDao() throws Exception {
-        if (!this.scopeOneLevel && !this.scopeObject && !this.scopeSubtree)
-            throw new IllegalStateException(
-                "Either scopeOneLevel, scopeObject or scopeSubtree must be set to true on "
-                    + this.getClass().getName());
+        for (int i = 0; i < VALID_SCOPE_VALUES.length; i++) {
+            if (this.scope == VALID_SCOPE_VALUES[i]) {
+                return;
+            }
+        }
 
-        if ((this.scopeOneLevel && this.scopeObject)
-            || (this.scopeOneLevel && this.scopeSubtree)
-            || (this.scopeObject && this.scopeSubtree))
-            throw new IllegalStateException(
-                "You can only set one property to tree on scopeOneLevel, scopeObject, and scopeSubtree for "
-                    + this.getClass().getName());
-
-        if (this.scopeOneLevel)
-            this.scopeValue = SearchControls.ONELEVEL_SCOPE;
-        else if (this.scopeObject)
-            this.scopeValue = SearchControls.OBJECT_SCOPE;
-        else
-            this.scopeValue = SearchControls.SUBTREE_SCOPE;
+        throw new IllegalStateException("You must set a scope.");
     }
 
     public boolean supports(Credentials credentials) {
@@ -131,59 +120,42 @@ public class BindLdapAuthenticationHandler extends
             && credentials.getClass().equals(UsernamePasswordCredentials.class);
     }
 
+    public void setScope(final int scope) {
+        this.scope = scope;
+    }
+
     /**
      * @param allowMultipleAccounts The allowMultipleAccounts to set.
      */
-    public void setAllowMultipleAccounts(boolean allowMultipleAccounts) {
+    public void setAllowMultipleAccounts(final boolean allowMultipleAccounts) {
         this.allowMultipleAccounts = allowMultipleAccounts;
     }
 
     /**
      * @param maxNumberResults The maxNumberResults to set.
      */
-    public void setMaxNumberResults(int maxNumberResults) {
+    public void setMaxNumberResults(final int maxNumberResults) {
         this.maxNumberResults = maxNumberResults;
-    }
-
-    /**
-     * @param scopeObject The scopeObject to set.
-     */
-    public void setScopeObject(boolean scopeObject) {
-        this.scopeObject = scopeObject;
-    }
-
-    /**
-     * @param scopeOneLevel The scopeOneLevel to set.
-     */
-    public void setScopeOneLevel(boolean scopeOneLevel) {
-        this.scopeOneLevel = scopeOneLevel;
-    }
-
-    /**
-     * @param scopeSubtree The scopeSubtree to set.
-     */
-    public void setScopeSubtree(boolean scopeSubtree) {
-        this.scopeSubtree = scopeSubtree;
     }
 
     /**
      * @param searchBase The searchBase to set.
      */
-    public void setSearchBase(String searchBase) {
+    public void setSearchBase(final String searchBase) {
         this.searchBase = searchBase;
     }
 
     /**
      * @param timeout The timeout to set.
      */
-    public void setTimeout(int timeout) {
+    public void setTimeout(final int timeout) {
         this.timeout = timeout;
     }
 
     /**
      * @param filter The filter to set.
      */
-    public void setFilter(String filter) {
+    public void setFilter(final String filter) {
         this.filter = filter;
     }
 }
