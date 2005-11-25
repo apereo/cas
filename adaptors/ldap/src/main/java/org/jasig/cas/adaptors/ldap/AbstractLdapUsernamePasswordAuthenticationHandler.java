3f25a4c3e48148afaa65f2dc54dc1b10f14f656c
Scott Battaglia <scott.battaglia@gmail.com>
2005-11-25 05:33:51 +0000

    moved filter to abstract ldap class

--- adaptors/ldap/src/main/java/org/jasig/cas/adaptors/ldap/AbstractLdapUsernamePasswordAuthenticationHandler.java
+++ adaptors/ldap/src/main/java/org/jasig/cas/adaptors/ldap/AbstractLdapUsernamePasswordAuthenticationHandler.java
@@ -16,12 +16,15 @@ import org.springframework.util.Assert;
  * @author Scott Battaglia
  * @version $Revision$ $Date$
  * @since 3.0.3
- *
  */
 public abstract class AbstractLdapUsernamePasswordAuthenticationHandler extends
     AbstractUsernamePasswordAuthenticationHandler {
 
+    /** LdapTemplate to execute ldap queries. */
     private LdapTemplate ldapTemplate;
+
+    /** The filter path to the uid of the user. */
+    private String filter;
 
     /**
      * Method to set the datasource and generate a JdbcTemplate.
@@ -45,9 +48,21 @@ public abstract class AbstractLdapUsernamePasswordAuthenticationHandler extends
         return this.ldapTemplate.getContextSource();
     }
 
+    protected final String getFilter() {
+        return this.filter;
+    }
+
     protected final void afterPropertiesSetInternal() throws Exception {
         Assert.notNull(this.ldapTemplate);
+        Assert.notNull(this.filter);
         initDao();
+    }
+
+    /**
+     * @param filter The filter to set.
+     */
+    public void setFilter(final String filter) {
+        this.filter = filter;
     }
 
     /**
