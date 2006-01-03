45e330b88b7fed9b75f87706d9e677555e57c4ab
Scott Battaglia <scott.battaglia@gmail.com>
2006-01-03 03:35:33 +0000

    CAS-291
    allow id generator to append suffix to generated id.

--- core/src/main/java/org/jasig/cas/util/DefaultUniqueTicketIdGenerator.java
+++ core/src/main/java/org/jasig/cas/util/DefaultUniqueTicketIdGenerator.java
@@ -26,14 +26,60 @@ public final class DefaultUniqueTicketIdGenerator implements
     /** The RandomStringGenerator to generate the secure random part of the id. */
     private final RandomStringGenerator randomStringGenerator;
 
+    /**
+     * Optional suffix to ensure uniqueness across JVMs by specifying unique
+     * values.
+     */
+    private String suffix;
+
+    /**
+     * Creates an instance of DefaultUniqueTicketIdGenerator with default values
+     * including a {@link DefaultLongNumericGenerator} with a starting value of
+     * 1.
+     */
     public DefaultUniqueTicketIdGenerator() {
         this.numericGenerator = new DefaultLongNumericGenerator(1);
         this.randomStringGenerator = new DefaultRandomStringGenerator();
     }
 
+    /**
+     * Creates an instance of DefaultUniqueTicketIdGenerator with a specified
+     * maximum length for the random portion.
+     * 
+     * @param maxLength the maximum length of the random string used to generate
+     * the id.
+     */
     public DefaultUniqueTicketIdGenerator(final int maxLength) {
         this.numericGenerator = new DefaultLongNumericGenerator(1);
         this.randomStringGenerator = new DefaultRandomStringGenerator(maxLength);
+    }
+
+    /**
+     * Creates an instance of DefaultUniqueTicketIdGenerator with default values
+     * including a {@link DefaultLongNumericGenerator} with a starting value of
+     * 1.
+     * 
+     * @param suffix the value to append at the end of the unique id to ensure
+     * uniqueness across JVMs.
+     */
+    public DefaultUniqueTicketIdGenerator(final String suffix) {
+        this();
+        this.suffix = suffix;
+    }
+
+    /**
+     * Creates an instance of DefaultUniqueTicketIdGenerator with a specified
+     * maximum length for the random portion.
+     * 
+     * @param maxLength the maximum length of the random string used to generate
+     * the id.
+     * @param suffix the value to append at the end of the unique id to ensure
+     * uniqueness across JVMs.
+     */
+    public DefaultUniqueTicketIdGenerator(final int maxLength,
+        final String suffix) {
+        this(maxLength);
+        this.suffix = suffix;
     }
 
     public String getNewTicketId(final String prefix) {
@@ -43,7 +89,12 @@ public final class DefaultUniqueTicketIdGenerator implements
         buffer.append(this.numericGenerator.getNextNumberAsString());
         buffer.append("-");
         buffer.append(this.randomStringGenerator.getNewString());
+
+        if (this.suffix != null) {
+            buffer.append("-");
+            buffer.append(this.suffix);
+        }
+
         return buffer.toString();
     }
-
 }
