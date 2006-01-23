b9bcb85a0af72ed1a89505486d41ae9e5a3eb2f7
Scott Battaglia <scott.battaglia@gmail.com>
2006-01-23 05:32:43 +0000

    CAS-297
    support for client certificates in PrincipalResolvers

---
+++ core/src/main/java/org/jasig/cas/authentication/principal/X509CertificateCredentialsToSerialNumberPrincipalResolver.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2005 The JA-SIG Collaborative. All rights reserved. See license
+ * distributed with this file and available online at
+ * http://www.uportal.org/license.html
+ */
+package org.jasig.cas.authentication.principal;
+
+import java.security.cert.X509Certificate;
+
+/**
+ * 
+ * @author Scott Battaglia
+ * @version $Revision$ $Date$
+ * @since 3.0.4
+ *
+ */
+public final class X509CertificateCredentialsToSerialNumberPrincipalResolver extends
+    AbstractX509CertificateCredentialsToPrincipalResolver {
+
+    protected Principal resolvePrincipalInternal(final X509Certificate certificate) {
+        return new SimplePrincipal(certificate.getSerialNumber().toString());
+    }
+}
