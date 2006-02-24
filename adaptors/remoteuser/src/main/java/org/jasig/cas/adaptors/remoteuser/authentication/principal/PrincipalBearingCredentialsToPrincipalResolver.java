ba704dbe6786c3a9a9001541ac3d068866e72aea
Scott Battaglia <scott.battaglia@gmail.com>
2006-02-24 23:35:21 +0000

    refactored RemoteUser support to use AbstractNonInteractiveAction as well as  not allowing a null PrincipalBearingCredentials to occur.  Also moved Credentials to remote user adaptor as that seems like the only use case for it.

--- adaptors/remoteuser/src/main/java/org/jasig/cas/adaptors/remoteuser/authentication/principal/PrincipalBearingCredentialsToPrincipalResolver.java
+++ adaptors/remoteuser/src/main/java/org/jasig/cas/adaptors/remoteuser/authentication/principal/PrincipalBearingCredentialsToPrincipalResolver.java
@@ -3,7 +3,11 @@
  * distributed with this file and available online at
  * http://www.uportal.org/license.html
  */
-package org.jasig.cas.authentication.principal;
+package org.jasig.cas.adaptors.remoteuser.authentication.principal;
+
+import org.jasig.cas.authentication.principal.Credentials;
+import org.jasig.cas.authentication.principal.CredentialsToPrincipalResolver;
+import org.jasig.cas.authentication.principal.Principal;
 
 
 /**
@@ -11,18 +15,18 @@ package org.jasig.cas.authentication.principal;
  * It is very simple to resolve PrincipalBearingCredentials to a Principal
  * since the credentials already bear the ready-to-go Principal.
  * 
- * @since 3.0.5
+ * @author Andrew Petro
  * @version $Revision$ $Date$
+ * @since 3.0.5
  */
 public final class PrincipalBearingCredentialsToPrincipalResolver 
     implements CredentialsToPrincipalResolver {
 
-    public Principal resolvePrincipal(Credentials credentials) {
+    public Principal resolvePrincipal(final Credentials credentials) {
         return ((PrincipalBearingCredentials) credentials).getPrincipal();
     }
 
-    public boolean supports(Credentials credentials) {
-        return credentials instanceof PrincipalBearingCredentials;
+    public boolean supports(final Credentials credentials) {
+        return credentials.getClass().equals(PrincipalBearingCredentials.class);
     }
-
 }
