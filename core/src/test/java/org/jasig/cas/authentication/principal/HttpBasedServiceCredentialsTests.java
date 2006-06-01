fd8e892a33ff77a644facd81c7a05ebe438abd45
Scott Battaglia <scott.battaglia@gmail.com>
2006-06-01 19:33:38 +0000

    more test cases

--- core/src/test/java/org/jasig/cas/authentication/principal/HttpBasedServiceCredentialsTests.java
+++ core/src/test/java/org/jasig/cas/authentication/principal/HttpBasedServiceCredentialsTests.java
@@ -5,6 +5,8 @@
  */
 package org.jasig.cas.authentication.principal;
 
+import java.net.URL;
+
 import org.jasig.cas.TestUtils;
 
 import junit.framework.TestCase;
@@ -19,5 +21,27 @@ public final class HttpBasedServiceCredentialsTests extends TestCase {
     public void testProperUrl() {
         assertEquals(TestUtils.CONST_GOOD_URL, TestUtils
             .getHttpBasedServiceCredentials().getCallbackUrl().toExternalForm());
+    }
+    
+    public void testEqualsWithNull() throws Exception {
+        final HttpBasedServiceCredentials c = new HttpBasedServiceCredentials(new URL("http://www.cnn.com"));
+        
+        assertFalse(c.equals(null));
+    }
+    
+    public void testEqualsWithFalse() throws Exception {
+        final HttpBasedServiceCredentials c = new HttpBasedServiceCredentials(new URL("http://www.cnn.com"));
+        final HttpBasedServiceCredentials c2 = new HttpBasedServiceCredentials(new URL("http://www.msn.com"));
+        
+        assertFalse(c.equals(c2));
+        assertFalse(c.equals(new Object()));
+    }
+    
+    public void testEqualsWithTrue() throws Exception {
+        final HttpBasedServiceCredentials c = new HttpBasedServiceCredentials(new URL("http://www.cnn.com"));
+        final HttpBasedServiceCredentials c2 = new HttpBasedServiceCredentials(new URL("http://www.cnn.com"));
+        
+        assertTrue(c.equals(c2));
+        assertTrue(c2.equals(c));
     }
 }
