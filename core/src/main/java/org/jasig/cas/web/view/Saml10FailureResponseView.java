08ddb57fb3b95ebb6be7ea97129d3c1d0991cd9c
Scott Battaglia <scott.battaglia@gmail.com>
2006-08-25 03:08:17 +0000

    CAS-314
    added SAML views (no attribute support yet)

---
+++ core/src/main/java/org/jasig/cas/web/view/Saml10FailureResponseView.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2005 The JA-SIG Collaborative. All rights reserved. See license
+ * distributed with this file and available online at
+ * http://www.uportal.org/license.html
+ */
+package org.jasig.cas.web.view;
+
+import java.util.Date;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jasig.cas.web.CasArgumentExtractor;
+import org.opensaml.SAMLException;
+import org.opensaml.SAMLResponse;
+
+/**
+ * Represents a failed attempt at validating a ticket, responding via a SAML
+ * assertion.
+ * 
+ * @author Scott Battaglia
+ * @version $Revision$ $Date$
+ * @since 3.1
+ */
+public class Saml10FailureResponseView extends AbstractCasView {
+
+    private CasArgumentExtractor casArgumentExtractor;
+
+    protected void renderMergedOutputModel(final Map model,
+        final HttpServletRequest request, final HttpServletResponse response)
+        throws Exception {
+
+        final String errorDescription = getDescriptionForError(model);
+
+        final SAMLResponse samlResponse = new SAMLResponse();
+        samlResponse.setRecipient(this.casArgumentExtractor.extractServiceFrom(
+            request).getId());
+        samlResponse.setIssueInstant(new Date());
+        samlResponse.setInResponseTo(this.casArgumentExtractor
+            .extractTicketFrom(request));
+
+        final SAMLException samlException = new SAMLException(errorDescription);
+
+        samlResponse.setStatus(samlException);
+
+        response.getWriter().print(samlResponse.toString());
+    }
+
+    public void setCasArgumentExtractor(
+        final CasArgumentExtractor casArgumentExtractor) {
+        this.casArgumentExtractor = casArgumentExtractor;
+    }
+}
