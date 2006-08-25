014448536b54bd2c9e1312da35133d11596649ad
Scott Battaglia <scott.battaglia@gmail.com>
2006-08-25 19:15:09 +0000

    updated javadocs

--- core/src/main/java/org/jasig/cas/web/view/Saml10SuccessResponseView.java
+++ core/src/main/java/org/jasig/cas/web/view/Saml10SuccessResponseView.java
@@ -31,100 +31,117 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.util.Assert;
 
 /**
+ * Implementation of a view to return a SAML response and assertion, based on
+ * the SAML 1.1 specification.
+ * <p>
+ * If an AttributePrincipal is supplied, then the assertion will include the
+ * attributes from it (assuming a String key/value pair). The only
+ * Authentication attribute it will look at is the authMethod (if supplied).
+ * <p>
+ * Note that this class will currently not handle proxy authentication.
  * 
  * @author Scott Battaglia
  * @version $Revision$ $Date$
  * @since 3.1
- * 
- * This class makes the following assumptions right now:
- * 1.  It does not handle additional Authentication attributes beyond type.
- * 2.  It assumes you will use an AttributePrincipal.
- * 3.  It assumes all attributes are String key/value pairs.
- *
  */
-public class Saml10SuccessResponseView extends AbstractCasView implements InitializingBean {
-    
+public class Saml10SuccessResponseView extends AbstractCasView implements
+    InitializingBean {
+
     private static final String NAMESPACE = "http://www.ja-sig.org/cas/";
-    
+
     private String issuer;
-    
+
     private long issueLength = 30000;
-    
+
     private CasArgumentExtractor casArgumentExtractor;
 
-    protected void renderMergedOutputModel(final Map model, final HttpServletRequest request,
-        final HttpServletResponse response) throws Exception {
-        
+    protected void renderMergedOutputModel(final Map model,
+        final HttpServletRequest request, final HttpServletResponse response)
+        throws Exception {
+
         final Assertion assertion = getAssertionFrom(model);
-        final Authentication authentication = assertion.getChainedAuthentications()[0];
+        final Authentication authentication = assertion
+            .getChainedAuthentications()[0];
         final Date currentDate = new Date();
-        final String authenticationMethod = (String) authentication.getAttributes().get("samlAuthenticationStatement::authMethod");
-        final Service service = this.casArgumentExtractor.extractServiceFrom(request);
-        
+        final String authenticationMethod = (String) authentication
+            .getAttributes().get("samlAuthenticationStatement::authMethod");
+        final Service service = this.casArgumentExtractor
+            .extractServiceFrom(request);
+
         final SAMLResponse samlResponse = new SAMLResponse();
         samlResponse.setRecipient(service.getId());
         samlResponse.setIssueInstant(currentDate);
-        samlResponse.setInResponseTo(this.casArgumentExtractor.extractTicketFrom(request));
-        
+        samlResponse.setInResponseTo(this.casArgumentExtractor
+            .extractTicketFrom(request));
+
         final SAMLAssertion samlAssertion = new SAMLAssertion();
         samlAssertion.setIssueInstant(currentDate);
         samlAssertion.setIssuer(this.issuer);
         samlAssertion.setNotBefore(currentDate);
-        samlAssertion.setNotOnOrAfter(new Date(currentDate.getTime() + this.issueLength));
-        
+        samlAssertion.setNotOnOrAfter(new Date(currentDate.getTime()
+            + this.issueLength));
+
         final SAMLAudienceRestrictionCondition samlAudienceRestrictionCondition = new SAMLAudienceRestrictionCondition();
         samlAudienceRestrictionCondition.addAudience(service.getId());
-        
+
         final SAMLAuthenticationStatement samlAuthenticationStatement = new SAMLAuthenticationStatement();
-        samlAuthenticationStatement.setAuthInstant(authentication.getAuthenticatedDate());
-        samlAuthenticationStatement.setAuthMethod(authenticationMethod != null ? authenticationMethod : SAMLAuthenticationStatement.AuthenticationMethod_Unspecified);
-        
+        samlAuthenticationStatement.setAuthInstant(authentication
+            .getAuthenticatedDate());
+        samlAuthenticationStatement.setAuthMethod(authenticationMethod != null
+            ? authenticationMethod
+            : SAMLAuthenticationStatement.AuthenticationMethod_Unspecified);
+
         final SAMLSubject samlSubject = new SAMLSubject();
         samlSubject.addConfirmationMethod(SAMLSubject.CONF_SENDER_VOUCHES);
-        
+
         if (authentication.getPrincipal() instanceof AttributePrincipal) {
-            final AttributePrincipal attributePrincipal = (AttributePrincipal) authentication.getPrincipal();
+            final AttributePrincipal attributePrincipal = (AttributePrincipal) authentication
+                .getPrincipal();
             final SAMLAttributeStatement attributeStatement = new SAMLAttributeStatement();
-            
+
             attributeStatement.setSubject(samlSubject);
             samlAssertion.addStatement(attributeStatement);
-            
-            for (final Iterator iter = attributePrincipal.getAttributes().keySet().iterator(); iter.hasNext();) {
+
+            for (final Iterator iter = attributePrincipal.getAttributes()
+                .keySet().iterator(); iter.hasNext();) {
 
                 final Object key = iter.next();
-                final Object value = attributePrincipal.getAttributes().get(key);
-                
+                final Object value = attributePrincipal.getAttributes()
+                    .get(key);
+
                 final SAMLAttribute attribute = new SAMLAttribute();
                 attribute.setName((String) key);
                 final Collection c = new ArrayList();
                 c.add(value);
                 attribute.setValues(c);
                 attribute.setNamespace(NAMESPACE);
-                
+
                 attributeStatement.addAttribute(attribute);
             }
         }
-        
+
         final SAMLNameIdentifier samlNameIdentifier = new SAMLNameIdentifier();
         samlNameIdentifier.setName(authentication.getPrincipal().getId());
-        
+
         samlSubject.setNameIdentifier(samlNameIdentifier);
         samlAuthenticationStatement.setSubject(samlSubject);
         samlAssertion.addStatement(samlAuthenticationStatement);
         samlAssertion.addCondition(samlAudienceRestrictionCondition);
         samlResponse.addAssertion(samlAssertion);
-        
+
         final String xmlResponse = samlResponse.toString();
-        
+
         response.getWriter().print(xmlResponse);
     }
-    
-    public void setCasArgumentExtractor(final CasArgumentExtractor casArgumentExtractor) {
+
+    public void setCasArgumentExtractor(
+        final CasArgumentExtractor casArgumentExtractor) {
         this.casArgumentExtractor = casArgumentExtractor;
     }
 
     public void afterPropertiesSet() throws Exception {
-        Assert.notNull(this.casArgumentExtractor, "casArgumentExtractor cannot be null.");
+        Assert.notNull(this.casArgumentExtractor,
+            "casArgumentExtractor cannot be null.");
         Assert.notNull(this.issuer, "issuer cannot be null.");
     }
 
@@ -132,11 +149,8 @@ public class Saml10SuccessResponseView extends AbstractCasView implements Initia
         this.issueLength = issueLength;
     }
 
-    
     public void setIssuer(final String issuer) {
         this.issuer = issuer;
     }
-    
-    
 
 }
