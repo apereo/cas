e2db3ab9abb5f9b3d76027c6177e2e578df05860
Scott Battaglia <scott.battaglia@gmail.com>
2006-08-30 02:32:04 +0000

    cleaned up maven template file

--- cas-server-core/src/test/java/org/jasig/cas/authentication/handler/support/JaasAuthenticationHandlerTests.java
+++ cas-server-core/src/test/java/org/jasig/cas/authentication/handler/support/JaasAuthenticationHandlerTests.java
@@ -19,7 +19,7 @@ public class JaasAuthenticationHandlerTests extends TestCase {
 
     protected void setUp() throws Exception {
         String pathPrefix = System.getProperty("user.dir");
-        pathPrefix = pathPrefix.indexOf("/doc") == -1 ? pathPrefix : pathPrefix.substring(0, pathPrefix.indexOf("/doc")); 
+        pathPrefix = pathPrefix.indexOf("/cas-server-core") == -1 ? pathPrefix + "/cas-server-core" : pathPrefix; 
         log.info("PATH PREFIX: " + pathPrefix);
         
         final String pathToConfig = pathPrefix + "/src/test/resources/org/jasig/cas/authentication/handler/support/jaas.conf";
