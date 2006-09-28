659644c587a11c4c3655741e312025ee42055064
Jan Van der Velpen <velpi@industria.be>
2006-09-28 08:05:54 +0000

    bug fix http://www.ja-sig.org/issues/browse/CAS-409?page=all

--- cas-server-support-x509/src/test/java/org/jasig/cas/adaptors/x509/authentication/principal/X509CertificateCredentialsToDistinguishedNamePrincipalResolverTests.java
+++ cas-server-support-x509/src/test/java/org/jasig/cas/adaptors/x509/authentication/principal/X509CertificateCredentialsToDistinguishedNamePrincipalResolverTests.java
@@ -8,21 +8,20 @@ package org.jasig.cas.adaptors.x509.authentication.principal;
 import java.security.cert.X509Certificate;
 
 import org.jasig.cas.adaptors.x509.authentication.principal.X509CertificateCredentials;
-import org.jasig.cas.adaptors.x509.authentication.principal.X509CertificateCredentialsToSerialNumberPrincipalResolver;
+import org.jasig.cas.adaptors.x509.authentication.principal.X509CertificateCredentialsToDistinguishedNamePrincipalResolver;
+import org.jasig.cas.authentication.principal.UsernamePasswordCredentials;
 
-
-public class X509CertificateCredentialsToSerialNumberPrincipalResolverTests
+public class X509CertificateCredentialsToDistinguishedNamePrincipalResolverTests
     extends AbstractX509CertificateTests {
 
-    private X509CertificateCredentialsToSerialNumberPrincipalResolver resolver = new X509CertificateCredentialsToSerialNumberPrincipalResolver();
+    private X509CertificateCredentialsToDistinguishedNamePrincipalResolver resolver = new X509CertificateCredentialsToDistinguishedNamePrincipalResolver();
     
     public void testResolvePrincipalInternal() {
         final X509CertificateCredentials c = new X509CertificateCredentials(new X509Certificate[] {VALID_CERTIFICATE});
-        c.setCertificate(VALID_CERTIFICATE);
-        
-        assertEquals(VALID_CERTIFICATE.getSerialNumber().toString(), this.resolver.resolvePrincipal(c).getId());
+        c.setCertificate(VALID_CERTIFICATE);        
+        assertEquals(VALID_CERTIFICATE.getSubjectDN().getName(), this.resolver.resolvePrincipal(c).getId());
     }
-
+    
     public void testSupport() {
         final X509CertificateCredentials c = new X509CertificateCredentials(new X509Certificate[] {VALID_CERTIFICATE});
         assertTrue(this.resolver.supports(c));
@@ -31,5 +30,5 @@ public class X509CertificateCredentialsToSerialNumberPrincipalResolverTests
     public void testSupportFalse() {
         assertFalse(this.resolver.supports(new UsernamePasswordCredentials()));
     }
-
+    
 }
