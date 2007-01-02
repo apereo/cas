5571179b68d8bfecdb0b4702b906ce9dfcf6ce5a
Scott Battaglia <scott.battaglia@gmail.com>
2007-01-02 19:30:10 +0000

    CAS-436
    created abstract distributed ticket registry

--- cas-server-integration-jboss/src/main/java/org/jasig/cas/ticket/registry/JBossCacheTicketRegistry.java
+++ cas-server-integration-jboss/src/main/java/org/jasig/cas/ticket/registry/JBossCacheTicketRegistry.java
@@ -12,12 +12,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
-import org.jasig.cas.authentication.Authentication;
-import org.jasig.cas.authentication.principal.Service;
-import org.jasig.cas.ticket.ExpirationPolicy;
-import org.jasig.cas.ticket.ServiceTicket;
 import org.jasig.cas.ticket.Ticket;
-import org.jasig.cas.ticket.TicketGrantingTicket;
 import org.jboss.cache.CacheException;
 import org.jboss.cache.Node;
 import org.jboss.cache.TreeCache;
@@ -32,7 +27,7 @@ import org.springframework.util.Assert;
  * @since 3.1
  *
  */
-public final class JBossCacheTicketRegistry extends AbstractTicketRegistry implements InitializingBean {
+public final class JBossCacheTicketRegistry extends AbstractDistributedTicketRegistry implements InitializingBean {
     
     /** Indicator of what tree branch to put tickets in. */
     private static final String FQN_TICKET = "ticket";
@@ -40,6 +35,16 @@ public final class JBossCacheTicketRegistry extends AbstractTicketRegistry imple
     /** Instance of JBoss TreeCache. */
     private TreeCache cache;
     
+    
+    
+    protected void updateTicket(Ticket ticket) {
+        try {
+            this.cache.put(FQN_TICKET, ticket.getId(), ticket);
+        } catch (final CacheException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public void addTicket(final Ticket ticket) {
         try {
             if (log.isDebugEnabled()){
@@ -108,156 +113,5 @@ public final class JBossCacheTicketRegistry extends AbstractTicketRegistry imple
 
     public void setCache(final TreeCache cache) {
         this.cache = cache;
-    }
-    
-    private Ticket getProxiedTicketInstance(final Ticket ticket) {
-        if (ticket == null) {
-            return null;
-        }
-        if (ticket instanceof ServiceTicket) {
-            return new ProxiedServiceTicket((ServiceTicket) ticket, this.cache);
-        }
-        
-        return new ProxiedTicketGrantingTicket((TicketGrantingTicket) ticket, this.cache);
-    }
-    
-    private final class ProxiedServiceTicket implements ServiceTicket {
-
-        /** Unique id for serialization */
-        private static final long serialVersionUID = -8212338455270445676L;
-
-        private final ServiceTicket serviceTicket;
-
-        private final TreeCache cache;
-
-        protected ProxiedServiceTicket(final ServiceTicket serviceTicket,
-            final TreeCache cache) {
-            this.serviceTicket = serviceTicket;
-            this.cache = cache;
-        }
-
-        public Service getService() {
-            return this.serviceTicket.getService();
-        }
-
-        public TicketGrantingTicket grantTicketGrantingTicket(final String id,
-            final Authentication authentication,
-            final ExpirationPolicy expirationPolicy) {
-            return this.serviceTicket.grantTicketGrantingTicket(id,
-                authentication, expirationPolicy);
-        }
-
-        public boolean isFromNewLogin() {
-            return this.serviceTicket.isFromNewLogin();
-        }
-
-        public long getCreationTime() {
-            return this.serviceTicket.getCreationTime();
-        }
-
-        public TicketGrantingTicket getGrantingTicket() {
-            return this.serviceTicket.getGrantingTicket();
-        }
-
-        public String getId() {
-            return this.serviceTicket.getId();
-        }
-
-        public boolean isValidFor(final Service service) {
-            final boolean result = this.serviceTicket.isValidFor(service);
-            try {
-                this.cache.put(FQN_TICKET, this.serviceTicket.getId(), this.serviceTicket);
-            } catch (final CacheException e) {
-                throw new RuntimeException(e);
-            }
-            return result;
-        }
-
-        public boolean isExpired() {
-            return this.serviceTicket.isExpired();
-        }
-
-        public boolean equals(final Object obj) {
-            return this.serviceTicket.equals(obj);
-        }
-
-        public String toString() {
-            return this.serviceTicket.toString();
-        }
-    }
-
-    private final class ProxiedTicketGrantingTicket implements
-        TicketGrantingTicket {
-
-        /** Unique Id for Serializaion */
-        private static final long serialVersionUID = -4361481214176025025L;
-
-        private final TicketGrantingTicket ticket;
-
-        private final TreeCache cache;
-
-        protected ProxiedTicketGrantingTicket(
-            final TicketGrantingTicket ticket, final TreeCache cache) {
-            this.ticket = ticket;
-            this.cache = cache;
-        }
-
-        public long getCreationTime() {
-            return this.ticket.getCreationTime();
-        }
-
-        public TicketGrantingTicket getGrantingTicket() {
-            return this.ticket.getGrantingTicket();
-        }
-
-        public String getId() {
-            return this.ticket.getId();
-        }
-
-        public boolean isExpired() {
-            return this.ticket.isExpired();
-        }
-
-        public void expire() {
-            this.ticket.expire();
-            try {
-                this.cache.put(FQN_TICKET, this.ticket.getId(), this.ticket);
-            } catch (final CacheException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        public Authentication getAuthentication() {
-            return this.ticket.getAuthentication();
-        }
-
-        public List getChainedAuthentications() {
-            return this.ticket.getChainedAuthentications();
-        }
-
-        public ServiceTicket grantServiceTicket(final String id,
-            final Service service, final ExpirationPolicy expirationPolicy,
-            final boolean credentialsProvided) {
-            final ServiceTicket serviceTicket = this.ticket.grantServiceTicket(
-                id, service, expirationPolicy, credentialsProvided);
-            try {
-                this.cache.put(FQN_TICKET, this.ticket.getId(), this.ticket);
-            } catch (final CacheException e) {
-                throw new RuntimeException(e);
-            }
-            return serviceTicket;
-        }
-
-        public boolean isRoot() {
-            return this.ticket.isRoot();
-        }
-
-        public boolean equals(final Object obj) {
-            return this.ticket.equals(obj);
-        }
-
-        public String toString() {
-            return this.ticket.toString();
-        }
     }
 }
