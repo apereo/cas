2973caba51cc540ccaa0648ade1483ceb5cb0032
Scott Battaglia <scott.battaglia@gmail.com>
2007-03-01 05:23:13 +0000

    work in progress on getting Add Service to work

--- cas-server-core/src/main/java/org/jasig/cas/services/web/AddRegisteredServiceSimpleFormController.java
+++ cas-server-core/src/main/java/org/jasig/cas/services/web/AddRegisteredServiceSimpleFormController.java
@@ -11,11 +11,14 @@ import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.jasig.cas.services.RegisteredService;
 import org.jasig.cas.services.ServiceRegistryManager;
 import org.springframework.util.Assert;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.ServletRequestDataBinder;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.SimpleFormController;
 
 
@@ -28,16 +31,20 @@ public class AddRegisteredServiceSimpleFormController extends
         Assert.notNull(serviceRegistryManager);
         this.serviceRegistryManager = serviceRegistryManager;
     }
-
-    protected void doSubmitAction(final Object command) throws Exception {
+    
+    protected ModelAndView onSubmit(final HttpServletRequest request, final HttpServletResponse response, final Object command, final BindException errors) throws Exception {
         final RegisteredService service = (RegisteredService) command;
-        
         this.serviceRegistryManager.addService(service);
+        
+        final Map<String, Object> model = new HashMap<String, Object>();
+        model.put("successMessage", "The service has been successfully saved in the database.");
+        
+        return showForm(request, response, errors, model);
     }
 
     protected void initBinder(final HttpServletRequest request,
         final ServletRequestDataBinder binder) throws Exception {
-        binder.setRequiredFields(new String[] {"description", "id", "name", "allowedToProxy", "allowedToSeeAllAttributes", "enabled", "ssoEnabled"});
+        binder.setRequiredFields(new String[] {"description", "id", "name", "allowedToProxy", "enabled", "ssoEnabled"});
     }
 
     protected Map referenceData(final HttpServletRequest request) throws Exception {
@@ -45,6 +52,7 @@ public class AddRegisteredServiceSimpleFormController extends
         
         final List<String> attributes = new ArrayList<String>();
         
+        // TODO HACK
         attributes.add("test");
         attributes.add("test1");
         attributes.add("test2");
@@ -53,6 +61,4 @@ public class AddRegisteredServiceSimpleFormController extends
         model.put("availableAttributes", attributes);
         return model;
     }
-    
-    
 }
