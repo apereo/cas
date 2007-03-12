52a0068644f5a7032be0d13737be577b2acf43f2
Scott Battaglia <scott.battaglia@gmail.com>
2007-03-12 03:34:04 +0000

    fixed "Web 2.0ish" stuff

--- cas-server-webapp/src/main/webapp/js/common_rosters.js
+++ cas-server-webapp/src/main/webapp/js/common_rosters.js
@@ -99,30 +99,6 @@ function getRef(el)
 	else return null;
 }
 
-/* see if following can be used in row highlighting
-document.onclick = function(e)
-{
-	var target = e ? e.target : window.event.srcElement;
-
-	while (target && !/^(a|body)$/i.test(target.nodeName))           while (target && !/^(tr)$/i.test(target.nodeName))
-	{
-		target = target.parentNode;
-	}
-
-	if (target && target.getAttribute('rel') && target.rel == 'external')           if (target && target.getAttribute('class') && target.className == 'external')
-	{
-		var external = window.open(target.href);
-		return external.closed;
-	}
-}
-
-Using a single, document-wide event handler is the most efficient approach 
--- it's much better than iterating through all the links and binding a handler 
-to each one individually. We can find out which element was actually clicked by 
-referencing the event target property.
-
-*/
-
 // Find all tables with highlight class and on highlight table mouseover trigger highlightRows function
 function initHighlightTables()
 {
@@ -193,89 +169,16 @@ function fade(elId, sr, sg, sb, er, eg, eb, step, current, speed){
 
 function fadeIn(){
 	if(!W3C_DOM)return;
-	// printfire("- START fadeIn()");
 	if(getRef('msg')) fade('msg', 51,204,0, 221,255,170, 30,1,20);
 	if(getRef('status')) fade('status', 187,0,0, 255,238,221, 30,1,20);
-	if(getRef('added')) fade('added', 255,255,51, 255,255,255, 30, 1, 70)
-	// printfire("- END fadeIn()");
+	
+	var arrayElements = getElementsByAttribute("tr", "class", "added");
+	
+	if (arrayElements.length > 0) {
+    	fade(arrayElements[0].id, 255,255,51, 255,255,255, 30, 1, 70)
+	}
 }
 addLoadEvent(fadeIn);
-
-// 0,8,9,16,17,18,37,38,39,40,46 8 - BACKSPACE; 9 - TAB; 13 - ENTER; 16 - SHIFT; 46 - DELETE;
-function autoTab(e) 
-{
-	var keycode;
-	
-	if (typeof e == "undefined")
-	{
-		e = window.event; // IE
-		keycode = e.keyCode;
-	}
-	else if (e)
-	{
-		keycode = e.which;
-	}
-	
-	// if(window.event){var keycode = window.event.keyCode;}
-	// else if (e){var keycode = e.which;}
-
-	// change the maxlength of the unit field based on the first character input
-	if((this.id == "unit" || this.id == "section") && this.value.length == 1)
-	{
-		// if letter then maxlength is 3
-		if(keycode > 64 && keycode < 91)
-		{
-			this.maxLength = 3;
-		}
-		// if number then maxlength is 2
-		else if((keycode > 48 && keycode < 58) || (keycode > 95 && keycode < 106))
-		{
-			this.maxLength = 2;
-		}
-	}
-
-	if(this.value.length == this.maxLength && keycode != 8 && keycode != 9 && keycode != 16 && keycode !=17 && keycode != 18 && keycode != 46)
-	{
-		var focusEl = findNextElement(this.getAttribute("tabindex"),this.form.id);
-		focusEl.focus();
-	}
-}
-
-function findNextElement(tabIndx,formId) 
-{
-	var aEls = document.forms[formId].elements;
-
-	for(var i = 0; i < aEls.length; i++) {
-		var el = aEls[i];
-		if(el.getAttribute("tabindex") == (parseInt(tabIndx)+1)) {
-			return el;
-		}
-	}
-	return aEls[0];
-}
-
-function initAutoTab(){
-	if(!W3C_DOM)return;
-	
-	// printfire("- START initAutoTab()");
-	
-	if(!document.getElementById("roster-view") && document.getElementsByTagName("input").length > 0 && getElementsByAttribute("table","class","search").length > 0)
-	{
-		// printfire("--- there are inputs and search tables ");
-
-		var aAutoTabEls = new Array();
-		aAutoTabEls = getElementsByAttribute("input","class","autoTab");
-		
-
-		for(var i = 0; i < aAutoTabEls.length; i++)
-		{
-			// printfire("--- assign onkeyup to field");
-			aAutoTabEls[i].onkeyup = autoTab;
-		}
-	}
-	// printfire("- END initAutoTab()");
-}
-addLoadEvent(initAutoTab);
 
 function swapButtonsForConfirm(rowId) {
 	swapConfirmAndButtons(rowId,"none","block","highlightBottom");
