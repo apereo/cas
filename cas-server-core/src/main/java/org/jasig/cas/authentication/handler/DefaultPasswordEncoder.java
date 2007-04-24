e14ecd9ec1d3ee999a18ce7e3a5bf236eeb84ee4
Scott Battaglia <scott.battaglia@gmail.com>
2007-04-24 13:01:12 +0000

    added NotNull annotation

--- cas-server-core/src/main/java/org/jasig/cas/authentication/handler/DefaultPasswordEncoder.java
+++ cas-server-core/src/main/java/org/jasig/cas/authentication/handler/DefaultPasswordEncoder.java
@@ -8,7 +8,7 @@ package org.jasig.cas.authentication.handler;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-import org.springframework.util.Assert;
+import org.jasig.cas.util.annotation.NotNull;
 
 /**
  * Implementation of PasswordEncoder using message digest. Can accept any
@@ -25,10 +25,10 @@ public final class DefaultPasswordEncoder implements PasswordEncoder {
     private static final char[] HEX_DIGITS = {'0', '1', '2', '3', '4', '5',
         '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
+    @NotNull
     private final String encodingAlgorithm;
 
     public DefaultPasswordEncoder(final String encodingAlgorithm) {
-        Assert.notNull(encodingAlgorithm, "encodingAlgorithm cannot be null.");
         this.encodingAlgorithm = encodingAlgorithm;
     }
 
