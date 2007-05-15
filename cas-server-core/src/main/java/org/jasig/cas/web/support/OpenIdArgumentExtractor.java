f86a8f37014e0c5724018aff7b8e071b3fc1e570
Scott Battaglia <scott.battaglia@gmail.com>
2007-05-15 19:53:38 +0000

    CAS-503
    started to add OpenId support via OpenIdService and argument extractor.  Need to do validation.

---
+++ cas-server-core/src/main/java/org/jasig/cas/web/support/OpenIdArgumentExtractor.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2005 The JA-SIG Collaborative. All rights reserved. See license
+ * distributed with this file and available online at
+ * http://www.uportal.org/license.html
+ */
+package org.jasig.cas.web.support;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.jasig.cas.authentication.principal.OpenIdService;
+import org.jasig.cas.authentication.principal.WebApplicationService;
+
+/**
+ * Constructs an OpenId Service.
+ * 
+ * @author Scott Battaglia
+ * @version $Revision: 1.1 $ $Date: 2005/08/19 18:27:17 $
+ * @since 3.1
+ *
+ */
+public class OpenIdArgumentExtractor extends AbstractArgumentExtractor {
+
+    public WebApplicationService extractService(final HttpServletRequest request) {
+        return OpenIdService.createServiceFrom(request);
+    }
+}
