f8a9d926ee5a0c99df120f4c6bf8ee825d4414c2
Scott Battaglia <scott.battaglia@gmail.com>
2007-07-18 19:21:43 +0000

    CAS-529 added support for PersonDirectory attribute resolution

---
+++ cas-server-core/src/main/java/org/jasig/cas/authentication/principal/AbstractPersonDirectoryCredentialsToPrincipalResolver.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2007 The JA-SIG Collaborative. All rights reserved. See license
+ * distributed with this file and available online at
+ * http://www.uportal.org/license.html
+ */
+package org.jasig.cas.authentication.principal;
+
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jasig.cas.util.annotation.NotNull;
+import org.jasig.services.persondir.IPersonAttributeDao;
+import org.jasig.services.persondir.support.StubPersonAttributeDao;
+
+/**
+ * 
+ * @author Scott Battaglia
+ * @version $Revision: 1.1 $ $Date: 2005/08/19 18:27:17 $
+ * @since 3.1
+ *
+ */
+public abstract class AbstractPersonDirectoryCredentialsToPrincipalResolver
+    implements CredentialsToPrincipalResolver {
+
+    /** Log instance. */
+    protected final Log log = LogFactory.getLog(this.getClass());
+    
+    /** Repository of principal attributes to be retrieved */
+    @NotNull
+    private IPersonAttributeDao attributeRepository = new StubPersonAttributeDao();
+
+    public final Principal resolvePrincipal(final Credentials credentials) {
+        if (log.isDebugEnabled()) {
+            log.debug("Attempting to resolve a principal...");
+        }
+
+        final String principalId = extractPrincipalId(credentials);
+        
+        if (principalId == null) {
+            return null;
+        }
+        
+        if (log.isDebugEnabled()) {
+            log.debug("Creating SimplePrincipal for ["
+                + principalId + "]");
+        }
+        
+        final Map attributes = this.attributeRepository.getUserAttributes(principalId);
+        return new SimplePrincipal(principalId, attributes);
+    }
+    
+    /**
+     * Extracts the id of the user from the provided credentials.
+     * 
+     * @param credentials the credentials provided by the user.
+     * @return the username, or null if it could not be resolved.
+     */
+    protected abstract String extractPrincipalId(Credentials credentials);
+    
+    public final void setAttributeRepository(final IPersonAttributeDao attributeRepository) {
+        this.attributeRepository = attributeRepository;
+    }
+}
