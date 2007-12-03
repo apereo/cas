5d7d682799601c0585fdba4e113ee50e18ba4891
Scott Battaglia <scott.battaglia@gmail.com>
2007-12-03 15:15:02 +0000

    CAS-600
    made the check case-insensitive

--- cas-server-core/src/main/java/org/jasig/cas/services/RegisteredService.java
+++ cas-server-core/src/main/java/org/jasig/cas/services/RegisteredService.java
@@ -88,6 +88,7 @@ public interface RegisteredService extends Cloneable, Serializable {
 
     /**
      * Returns whether the service matches the registered service.
+     * <p>Note, as of 3.1.2, matches are case insensitive.
      * 
      * @param service the service to match.
      * @return true if they match, false otherwise.
