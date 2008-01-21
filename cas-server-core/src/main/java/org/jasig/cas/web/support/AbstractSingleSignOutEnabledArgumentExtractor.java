577cb578ccd0f68dca5093ea2649284e56c313ee
Scott Battaglia <scott.battaglia@gmail.com>
2008-01-21 04:18:59 +0000

    NOJIRA
    updated javadocs

--- cas-server-core/src/main/java/org/jasig/cas/web/support/AbstractSingleSignOutEnabledArgumentExtractor.java
+++ cas-server-core/src/main/java/org/jasig/cas/web/support/AbstractSingleSignOutEnabledArgumentExtractor.java
@@ -8,6 +8,7 @@ package org.jasig.cas.web.support;
 import org.jasig.cas.util.HttpClient;
 
 /**
+ * Abstract class for handling the enabling and disabling of Single Sign Out features.
  * 
  * @author scottbattaglia
  * @version $Revision: 1.1 $ $Date: 2005/08/19 18:27:17 $
@@ -17,8 +18,10 @@ import org.jasig.cas.util.HttpClient;
 public abstract class AbstractSingleSignOutEnabledArgumentExtractor implements
     ArgumentExtractor {
 
+    /** Whether single sign out is disabled or not. */
     private boolean disableSingleSignOut = false;
     
+    /** Default instance of HttpClient. */
     private HttpClient httpClient = new HttpClient();
     
     public void setHttpClient(final HttpClient httpClient) {
