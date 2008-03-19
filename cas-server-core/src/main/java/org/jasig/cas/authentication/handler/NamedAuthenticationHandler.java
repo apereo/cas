4f4a97e72a6fc37aacfbeb0c7cf1af5e38fcb086
Scott Battaglia <scott.battaglia@gmail.com>
2008-03-19 12:39:26 +0000

    CAS-632
    add a authentication method type to the Authentication interface.

---
+++ cas-server-core/src/main/java/org/jasig/cas/authentication/handler/NamedAuthenticationHandler.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2007 The JA-SIG Collaborative. All rights reserved. See license
+ * distributed with this file and available online at
+ * http://www.uportal.org/license.html
+ */
+package org.jasig.cas.authentication.handler;
+
+/**
+ * Offers AuthenticationHandlers a way to identify themselves by a 
+ * user-configured name.
+ * 
+ * @author Scott Battaglia
+ * @version $Revision: 1.1 $ $Date: 2005/08/19 18:27:17 $
+ * @since 3.2.1
+ *
+ */
+public interface NamedAuthenticationHandler extends AuthenticationHandler {
+    
+    String getName();
+}
