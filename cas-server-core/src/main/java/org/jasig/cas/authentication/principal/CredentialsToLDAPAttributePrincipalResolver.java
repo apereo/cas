fc3eee02c2aa2165a23b86688efc8e3b3755eba5
Scott Battaglia <scott.battaglia@gmail.com>
2008-06-13 17:53:12 +0000

    CAS-658
    added support for the remove lookup of IP addresses

--- cas-server-core/src/main/java/org/jasig/cas/authentication/principal/CredentialsToLDAPAttributePrincipalResolver.java
+++ cas-server-core/src/main/java/org/jasig/cas/authentication/principal/CredentialsToLDAPAttributePrincipalResolver.java
@@ -5,18 +5,13 @@
  */
 package org.jasig.cas.authentication.principal;
 
-import org.inspektr.common.ioc.annotation.IsIn;
 import org.inspektr.common.ioc.annotation.NotNull;
 import org.jasig.cas.util.LdapUtils;
 import org.springframework.ldap.core.AttributesMapper;
-import org.springframework.ldap.core.LdapTemplate;
-import org.springframework.ldap.core.support.LdapContextSource;
 
 import javax.naming.NamingException;
 import javax.naming.directory.Attribute;
 import javax.naming.directory.Attributes;
-import javax.naming.directory.SearchControls;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -26,13 +21,7 @@ import java.util.List;
  * @version $Revision:$ $Date:$
  * @since 3.1
  */
-public final class CredentialsToLDAPAttributePrincipalResolver extends AbstractPersonDirectoryCredentialsToPrincipalResolver {
-
-    /** The default maximum number of results to return. */
-    private static final int DEFAULT_MAX_NUMBER_OF_RESULTS = 2;
-
-    /** The default timeout. */
-    private static final int DEFAULT_TIMEOUT = 1000;
+public final class CredentialsToLDAPAttributePrincipalResolver extends AbstractLdapPersonDirectoryCredentialsToPrincipalResolver {
 
     /**
      * The CredentialsToPrincipalResolver that resolves the principal from the
@@ -40,45 +29,6 @@ public final class CredentialsToLDAPAttributePrincipalResolver extends AbstractP
      */
     @NotNull
     private CredentialsToPrincipalResolver credentialsToPrincipalResolver;
-
-    /** LdapTemplate to execute ldap queries. */
-    @NotNull
-    private LdapTemplate ldapTemplate;
-
-    /** The filter path to the lookup value of the user. */
-    @NotNull
-    private String filter;
-
-    /** The attribute that contains the value that should become the principal */
-    @NotNull
-    private String[] attributeIds;
-
-    /** The search base to find the user under. */
-    @NotNull
-    private String searchBase;
-
-    /** The scope. */
-    @IsIn({SearchControls.OBJECT_SCOPE, SearchControls.ONELEVEL_SCOPE,
-        SearchControls.SUBTREE_SCOPE})
-    private int scope = SearchControls.SUBTREE_SCOPE;
-
-    /** The amount of time to wait. */
-    private int timeout = DEFAULT_TIMEOUT;
-
-    private SearchControls getSearchControls() {
-        final SearchControls constraints = new SearchControls();
-        if (log.isDebugEnabled()) {
-            log.debug("returning searchcontrols: scope=" + this.scope
-                + "; search base=" + this.searchBase
-                + "; attributes=" + Arrays.toString(this.attributeIds)
-                + "; timeout=" + this.timeout);
-        }
-        constraints.setSearchScope(this.scope);
-        constraints.setReturningAttributes(this.attributeIds);
-        constraints.setTimeLimit(this.timeout);
-        constraints.setCountLimit(DEFAULT_MAX_NUMBER_OF_RESULTS);
-        return constraints;
-    }
     
     protected String extractPrincipalId(final Credentials credentials) {
         final Principal principal = this.credentialsToPrincipalResolver
@@ -107,7 +57,7 @@ public final class CredentialsToLDAPAttributePrincipalResolver extends AbstractP
     }
 
     private String resolveFromLDAP(final String lookupAttributeValue) {
-        final String searchFilter = LdapUtils.getFilterWithValues(this.filter,
+        final String searchFilter = LdapUtils.getFilterWithValues(getFilter(),
             lookupAttributeValue);
 
         if (log.isDebugEnabled()) {
@@ -116,9 +66,9 @@ public final class CredentialsToLDAPAttributePrincipalResolver extends AbstractP
         
         try {
             // searching the directory
-            final String idAttribute = this.attributeIds[0];
-            final List principalList = this.ldapTemplate.search(
-                this.searchBase, searchFilter, getSearchControls(),
+            final String idAttribute = getAttributeIds()[0];
+            final List principalList = getLdapTemplate().search(
+                getSearchBase(), searchFilter, getSearchControls(),
                 
                 new AttributesMapper() {
                     public Object mapFromAttributes(final Attributes attrs)
@@ -166,49 +116,5 @@ public final class CredentialsToLDAPAttributePrincipalResolver extends AbstractP
     public final void setCredentialsToPrincipalResolver(
         CredentialsToPrincipalResolver credentialsToPrincipalResolver) {
         this.credentialsToPrincipalResolver = credentialsToPrincipalResolver;
-    }
-
-    /**
-     * Method to set the datasource and generate a LDAPTemplate.
-     * 
-     * @param contextSource the datasource to use.
-     */
-    public final void setContextSource(final LdapContextSource contextSource) {
-        this.ldapTemplate = new LdapTemplate(contextSource);
-    }
-
-    /**
-     * @param filter The LDAP filter to set.
-     */
-    public void setFilter(final String filter) {
-        this.filter = filter;
-    }
-
-    /**
-     * @param principalAttributeName The principalAttributeName to set.
-     */
-    public final void setPrincipalAttributeName(final String principalAttributeName) {
-        this.attributeIds = new String[] {principalAttributeName};
-    }
-
-    /**
-     * @param scope The scope to set.
-     */
-    public final void setScope(final int scope) {
-        this.scope = scope;
-    }
-
-    /**
-     * @param searchBase The searchBase to set.
-     */
-    public final void setSearchBase(final String searchBase) {
-        this.searchBase = searchBase;
-    }
-
-    /**
-     * @param timeout The timeout to set.
-     */
-    public final void setTimeout(final int timeout) {
-        this.timeout = timeout;
     }
 }
---
+++ cas-server-support-ldap/src/main/java/org/jasig/cas/adaptors/ldap/remote/RemoteAddressAuthenticationHandler.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2007 The JA-SIG Collaborative. All rights reserved. See license
+ * distributed with this file and available online at
+ * http://www.uportal.org/license.html
+ */
+package org.jasig.cas.adaptors.ldap.remote;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.inspektr.common.ioc.annotation.NotNull;
+import org.jasig.cas.authentication.handler.AuthenticationException;
+import org.jasig.cas.authentication.handler.AuthenticationHandler;
+import org.jasig.cas.authentication.principal.Credentials;
+
+/**
+ * Checks if the remote address is in the range of allowed addresses.
+ * 
+ * @author David Harrison
+ * @author Scott Battaglia
+ * @version $Revision: 1.1 $ $Date: 2005/08/19 18:27:17 $
+ * @since 3.2.1
+ *
+ */
+public final class RemoteAddressAuthenticationHandler implements
+    AuthenticationHandler {
+    
+    private final Log log = LogFactory.getLog(getClass());
+    
+    /** The network netmask */
+    @NotNull
+    private InetAddress inetNetmask = null;
+    
+    /** The network base address */
+    @NotNull
+    private InetAddress inetNetwork = null;
+
+    public boolean authenticate(final Credentials credentials)
+        throws AuthenticationException {
+        final RemoteAddressCredentials c = (RemoteAddressCredentials) credentials;
+        try {
+            final InetAddress inetAddress = InetAddress.getByName(c.getRemoteAddress().trim());
+            return containsAddress(this.inetNetwork, this.inetNetmask, inetAddress);
+        } catch (final UnknownHostException e) {
+            return false;
+        }  
+    }
+
+    public boolean supports(final Credentials credentials) {
+        return credentials.getClass().equals(RemoteAddressCredentials.class);
+    }
+    
+    /**
+     * The following code is from the Apache Software Foundations's Lenya project
+     * See InetAddressUtil.java 
+     * Distributed under the Apache 2.0 software license
+     */
+       
+    /**
+     * Checks if a subnet contains a specific IP address.
+     * 
+     * @param network The network address.
+     * @param netmask The subnet mask.
+     * @param ip The IP address to check.
+     * @return A boolean value.
+     */
+    private boolean containsAddress(final InetAddress network, final InetAddress netmask, final InetAddress ip) {
+        if(log.isDebugEnabled()) {
+            log.debug("Checking IP address: " + ip + " in " + network + " / " + netmask);
+        }
+        
+        byte[] networkBytes = network.getAddress();
+        byte[] netmaskBytes = netmask.getAddress();
+        byte[] ipBytes = ip.getAddress();
+        
+        /* check IPv4/v6-compatibility or parameters: */
+        if(networkBytes.length != netmaskBytes.length
+            || netmaskBytes.length != ipBytes.length)
+        {
+            if(log.isDebugEnabled()) {
+                log.debug("Network address " + network + ", subnet mask "
+                     + netmask + " and/or host address " + ip
+                     + " have different sizes! (return false ...)");
+            }
+            return false;
+        }
+        
+        /* Check if the masked network and ip addresses match: */
+        for(int i=0; i<netmaskBytes.length; i++) {
+            int mask = netmaskBytes[i] & 0xff;
+            if((networkBytes[i] & mask) != (ipBytes[i] & mask)) {
+                if(log.isDebugEnabled()) {
+                    log.debug(ip + " is not in " + network + " / " + netmask);
+                }
+                return false;
+            }
+        }
+        if(log.isDebugEnabled()) {
+            log.debug(ip + " is in " + network + " / " + netmask);
+        }
+        return true;
+    }
+    
+    /**
+     * @param ipAddressRange the IP address range that should be allowed trusted logins     * 
+     */
+    public void setIpNetworkRange(final String ipAddressRange) {
+            
+        if(ipAddressRange != null) {
+        
+            final String[] splitAddress = ipAddressRange.split( "/" );
+
+            if (splitAddress.length == 2 ) {
+                // A valid ip address/netmask was supplied parse values
+                final String network = splitAddress[0].trim();
+                final String netmask = splitAddress[1].trim();
+        
+                try {
+                    this.inetNetwork = InetAddress.getByName(network);
+                    if(log.isDebugEnabled()) {
+                        log.debug("InetAddress network: " + this.inetNetwork.toString());
+                    }                    
+                } catch (final UnknownHostException e ) {
+                    log.error("The network address was not valid: " + e.getMessage());
+                }
+                
+                try {
+                    this.inetNetmask = InetAddress.getByName(netmask);
+                    if(log.isDebugEnabled()) {
+                        log.debug("InetAddress netmask: " + this.inetNetmask.toString());
+                    }                    
+                } catch (final UnknownHostException e ) {
+                    log.error("The network netmask was not valid: " + e.getMessage());
+                }
+            }
+        }
+    }
+}
