19cff5ee1a9ba0a6f893d95745f0881be35b093c
Scott Battaglia <scott.battaglia@gmail.com>
2008-07-14 15:55:21 +0000

    CAS-680
    added initial memcache support

---
+++ cas-server-integration-memcached/src/main/java/org/jasig/cas/ticket/registry/MemCacheTicketRegistry.java
@@ -0,0 +1,112 @@
+package org.jasig.cas.ticket.registry;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Future;
+
+import net.spy.memcached.MemcachedClient;
+
+import org.inspektr.common.ioc.annotation.GreaterThan;
+import org.jasig.cas.ticket.ServiceTicket;
+import org.jasig.cas.ticket.Ticket;
+import org.jasig.cas.ticket.TicketGrantingTicket;
+import org.jasig.cas.ticket.registry.AbstractDistributedTicketRegistry;
+import org.springframework.beans.factory.DisposableBean;
+
+/**
+ * Memcache (or Repcache) backed ticket registry.
+ * 
+ * @author Scott Battaglia
+ * @version $Revision: 1.1 $ $Date: 2005/08/19 18:27:17 $
+ * @since 3.3
+ *
+ */
+public final class MemCacheTicketRegistry extends AbstractDistributedTicketRegistry implements DisposableBean {
+	
+	private final MemcachedClient client;
+	
+	@GreaterThan(0)
+	private final int tgtTimeout;
+	
+	@GreaterThan(0)
+	private final int stTimeout;
+	
+	/**
+	 * Host names should be given in a list of the format: &lt;hostname&gt;:&lt;port&gt;
+	 * 
+	 * @param hostnames
+	 * @param ticketGrantingTicketTimeOut
+	 * @param serviceTicketTimeOut
+	 */
+	public MemCacheTicketRegistry(final String[] hostnames, final int ticketGrantingTicketTimeOut, final int serviceTicketTimeOut) {
+		this.tgtTimeout = ticketGrantingTicketTimeOut;
+		this.stTimeout = serviceTicketTimeOut;
+		final List<InetSocketAddress> addresses = new ArrayList<InetSocketAddress>();
+		
+		for (final String hostname : hostnames) {
+			String[] hostPort = hostname.split(":");
+			addresses.add(new InetSocketAddress(hostPort[0], Integer.parseInt(hostPort[1])));
+		}
+		
+		try {
+			this.client = new MemcachedClient(addresses);
+		} catch (final IOException e) {
+			throw new IllegalStateException(e);
+		}
+	}
+
+	protected void updateTicket(final Ticket ticket) {
+		if (ticket instanceof TicketGrantingTicket) {
+			client.replace(ticket.getId(), this.tgtTimeout, ticket);
+		}
+		
+		if (ticket instanceof ServiceTicket) {
+			client.replace(ticket.getId(), this.stTimeout, ticket);
+		}
+	}
+
+	public void addTicket(final Ticket ticket) {
+		if (ticket instanceof TicketGrantingTicket) {
+			client.add(ticket.getId(), this.tgtTimeout, ticket);
+		}
+		
+		if (ticket instanceof ServiceTicket) {
+			client.add(ticket.getId(), this.stTimeout, ticket);
+		}
+	}
+
+	public boolean deleteTicket(final String ticketId) {
+		Future<Boolean> f= client.delete(ticketId);
+		try {
+			return f.get().booleanValue();
+		} catch (final Exception e) {
+			log.error(e,e);
+			return false;
+		}
+	}
+
+	public Ticket getTicket(final String ticketId) {
+		final Ticket t = (Ticket) client.get(ticketId);
+		if (t == null) {
+			return null;
+		}
+		
+		return getProxiedTicketInstance(t);
+	}
+
+	/**
+	 * This operation is not supported.
+	 * 
+	 * @throws UnsupportedOperationException if you try and call this operation.
+	 */
+	public Collection<Ticket> getTickets() {
+		throw new UnsupportedOperationException("GetTickets not supported.");
+	}
+
+	public void destroy() throws Exception {
+		this.client.shutdown();
+	}
+}
