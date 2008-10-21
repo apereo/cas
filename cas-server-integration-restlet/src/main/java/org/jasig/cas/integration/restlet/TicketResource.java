5492fd65413a81e24398d65260b4631b979c1f5b
Scott Battaglia <scott.battaglia@gmail.com>
2008-10-21 14:28:35 +0000

    CAS-724
    changed back to 201 and added HTML

--- cas-server-integration-restlet/src/main/java/org/jasig/cas/integration/restlet/TicketResource.java
+++ cas-server-integration-restlet/src/main/java/org/jasig/cas/integration/restlet/TicketResource.java
@@ -16,6 +16,7 @@ import org.jasig.cas.authentication.principal.Credentials;
 import org.jasig.cas.authentication.principal.UsernamePasswordCredentials;
 import org.jasig.cas.ticket.TicketException;
 import org.restlet.data.Form;
+import org.restlet.data.MediaType;
 import org.restlet.data.Reference;
 import org.restlet.data.Request;
 import org.restlet.data.Status;
@@ -60,13 +61,25 @@ public class TicketResource extends Resource {
         final Credentials c = obtainCredentials();
         try {
             final String ticketGrantingTicketId = this.centralAuthenticationService.createTicketGrantingTicket(c);
-            getResponse().setStatus(Status.SUCCESS_NO_CONTENT);
+            getResponse().setStatus(determineStatus());
             final Reference ticket_ref = getRequest().getResourceRef().addSegment(ticketGrantingTicketId);
-            getResponse().setLocationRef(ticket_ref); 
+            getResponse().setLocationRef(ticket_ref);
+            getResponse().setEntity("<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\"><html><head><title>" + getResponse().getStatus().getCode() + " " + getResponse().getStatus().getDescription() + "</title></head><body><h1>Created</h1><p>The object has been created and can be found <a href=\"http://blah.com/cas/v1/tickets/TGT-54353485093458340\">here</a>.</p></body></html>", MediaType.TEXT_HTML);            
         } catch (final TicketException e) {
             log.error(e,e);
             getResponse().setStatus(Status.CLIENT_ERROR_BAD_REQUEST, e.getMessage());
         }
+    }
+    
+    /**
+     * Template method for determining which status to return on a successful ticket creation. 
+     * This method exists for compatibility reasons with bad clients (i.e. Flash) that can't
+     * process 201 with a Location header.
+     * 
+     * @return the status to return.
+     */
+    protected Status determineStatus() {
+        return Status.SUCCESS_CREATED;
     }
     
     protected Credentials obtainCredentials() {
