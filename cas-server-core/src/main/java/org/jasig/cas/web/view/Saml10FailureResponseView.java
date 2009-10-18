239d5654ff5ec9266792b8b83d689b89bb49edb2
Scott Battaglia <scott.battaglia@gmail.com>
2009-10-18 03:58:59 +0000

    CAS-808
    supply the more descriptive error message

--- cas-server-core/src/main/java/org/jasig/cas/web/view/Saml10FailureResponseView.java
+++ cas-server-core/src/main/java/org/jasig/cas/web/view/Saml10FailureResponseView.java
@@ -39,8 +39,9 @@ public class Saml10FailureResponseView extends AbstractCasView {
         final HttpServletRequest request, final HttpServletResponse response)
         throws Exception {
         final WebApplicationService service = this.samlArgumentExtractor.extractService(request);
+        final String errorMessage = (String) model.get("description");
 
-        final SAMLResponse samlResponse = new SAMLResponse(service.getArtifactId(), service.getId(), new ArrayList<Object>(), new SAMLException("Success"));
+        final SAMLResponse samlResponse = new SAMLResponse(service.getArtifactId(), service.getId(), new ArrayList<Object>(), new SAMLException(errorMessage));
         samlResponse.setIssueInstant(new Date());
 
         response.setContentType("text/xml; charset=" + this.encoding);
