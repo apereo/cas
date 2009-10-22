6e3e86b03a06fc49a12f83b3f98fd4e5df3703df
Scott Battaglia <scott.battaglia@gmail.com>
2009-10-22 03:54:03 +0000

    CAS-130
    started work on admin summary views.

--- cas-server-core/src/main/java/org/jasig/cas/web/support/AbstractThrottledSubmissionHandlerInterceptorAdapter.java
+++ cas-server-core/src/main/java/org/jasig/cas/web/support/AbstractThrottledSubmissionHandlerInterceptorAdapter.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2007 The JA-SIG Collaborative. All rights reserved. See license
+ * distributed with this file and available online at
+ * http://www.ja-sig.org/products/cas/overview/license/
+ */
 package org.jasig.cas.web.support;
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
