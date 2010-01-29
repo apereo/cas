64de0a1f42edd26ca6cc8169537c0d77b4451e1e
Scott Battaglia <scott.battaglia@gmail.com>
2010-01-29 04:22:50 +0000

    CAS-817
    allow you to configure which SAML compatibility it is via setter (saml2compliant)

--- cas-server-core/src/main/java/org/jasig/cas/util/SamlCompliantUniqueTicketIdGenerator.java
+++ cas-server-core/src/main/java/org/jasig/cas/util/SamlCompliantUniqueTicketIdGenerator.java
@@ -6,6 +6,7 @@
 package org.jasig.cas.util;
 
 import org.opensaml.artifact.SAMLArtifact;
+import org.opensaml.artifact.SAMLArtifactType0001;
 import org.opensaml.artifact.SAMLArtifactType0002;
 import org.opensaml.artifact.URI;
 
@@ -14,21 +15,23 @@ import javax.validation.constraints.NotNull;
 /**
  * Unique Ticket Id Generator compliant with the SAML 1.1 specification for
  * artifacts. This should also be compliant with the SAML 2 specification.
+ * <p>
+ * Default to SAML 1.1 Compliance.
  * 
- * @author Scott
+ * @author Scott Battaglia
  * @version $Revision$ $Date$
  * @since 3.0
  */
-public final class SamlCompliantUniqueTicketIdGenerator implements
-    UniqueTicketIdGenerator {
+public final class SamlCompliantUniqueTicketIdGenerator implements UniqueTicketIdGenerator {
 
     /** SAML defines the source id as the server name. */
     @NotNull
     private final String sourceLocation;
 
+    private boolean saml2compliant;
+
     /** Random generator to construct the AssertionHandle. */
-    private final RandomStringGenerator randomStringGenerator = new DefaultRandomStringGenerator(
-        20);
+    private final RandomStringGenerator randomStringGenerator = new DefaultRandomStringGenerator(20);
 
     public SamlCompliantUniqueTicketIdGenerator(final String sourceId) {
         this.sourceLocation = sourceId;
@@ -38,10 +41,14 @@ public final class SamlCompliantUniqueTicketIdGenerator implements
      * We ignore prefixes for SAML compliance.
      */
     public String getNewTicketId(final String prefix) {
-        final SAMLArtifact samlArtifact = new SAMLArtifactType0002(
-            this.randomStringGenerator.getNewStringAsBytes(), new URI(
-                this.sourceLocation));
+        if (saml2compliant) {
+            return new SAMLArtifactType0002(this.randomStringGenerator.getNewStringAsBytes(), new URI(this.sourceLocation)).encode();
+        } else {
+            return new SAMLArtifactType0001(this.randomStringGenerator.getNewStringAsBytes(), new URI(this.sourceLocation).toBytes()).encode();
+        }
+    }
 
-        return samlArtifact.encode();
+    public void setSaml2compliant(final boolean saml2compliant) {
+        this.saml2compliant = saml2compliant;
     }
 }
