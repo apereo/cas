d26b5ce1d095fd5528e46fc5f0f17a8504cfbf58
Scott Battaglia <scott.battaglia@gmail.com>
2010-09-25 17:56:32 +0000

    CAS-817
    fixed SAML1.1 compliance and added test cases to make sure they work.

--- cas-server-core/src/main/java/org/jasig/cas/util/SamlCompliantUniqueTicketIdGenerator.java
+++ cas-server-core/src/main/java/org/jasig/cas/util/SamlCompliantUniqueTicketIdGenerator.java
@@ -1,8 +1,22 @@
-/*
- * Copyright 2007 The JA-SIG Collaborative. All rights reserved. See license
- * distributed with this file and available online at
- * http://www.ja-sig.org/products/cas/overview/license/
+/**
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
+
 package org.jasig.cas.util;
 
 import org.opensaml.artifact.SAMLArtifactType0001;
@@ -10,18 +24,22 @@ import org.opensaml.artifact.SAMLArtifactType0002;
 import org.opensaml.artifact.URI;
 
 import javax.validation.constraints.NotNull;
+import java.security.MessageDigest;
 
 /**
  * Unique Ticket Id Generator compliant with the SAML 1.1 specification for
  * artifacts. This should also be compliant with the SAML 2 specification.
  * <p>
  * Default to SAML 1.1 Compliance.
- * 
+ *
  * @author Scott Battaglia
  * @version $Revision$ $Date$
  * @since 3.0
  */
 public final class SamlCompliantUniqueTicketIdGenerator implements UniqueTicketIdGenerator {
+
+    /** SAML defines the source id as the server name. */
+    private final byte[] sourceIdDigest;
 
     /** SAML defines the source id as the server name. */
     @NotNull
@@ -34,6 +52,13 @@ public final class SamlCompliantUniqueTicketIdGenerator implements UniqueTicketI
 
     public SamlCompliantUniqueTicketIdGenerator(final String sourceId) {
         this.sourceLocation = sourceId;
+        try {
+            final MessageDigest messageDigest = MessageDigest.getInstance("SHA");
+            messageDigest.update(sourceId.getBytes("8859_1"));
+            this.sourceIdDigest = messageDigest.digest();
+        } catch (final Exception e) {
+            throw new IllegalStateException("Exception generating digest which should not happen...EVER");
+        }
     }
 
     /**
@@ -43,7 +68,7 @@ public final class SamlCompliantUniqueTicketIdGenerator implements UniqueTicketI
         if (saml2compliant) {
             return new SAMLArtifactType0002(this.randomStringGenerator.getNewStringAsBytes(), new URI(this.sourceLocation)).encode();
         } else {
-            return new SAMLArtifactType0001(this.randomStringGenerator.getNewStringAsBytes(), new URI(this.sourceLocation).toBytes()).encode();
+            return new SAMLArtifactType0001(this.sourceIdDigest, this.randomStringGenerator.getNewStringAsBytes()).encode();
         }
     }
 
