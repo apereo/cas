8cb3dba51a294953b1433b36d1adcb4cc5cb92e6
Scott Battaglia <scott.battaglia@gmail.com>
2010-09-26 03:50:56 +0000

    CAS-869
    changed over to using jQuery to remove a lot of our custom code.  Could probably remove more.

--- cas-server-webapp/src/main/webapp/WEB-INF/view/jsp/default/ui/includes/top.jsp
+++ cas-server-webapp/src/main/webapp/WEB-INF/view/jsp/default/ui/includes/top.jsp
@@ -22,12 +22,14 @@
            <c:otherwise>
                 <spring:theme code="standard.custom.css.file" var="customCssFile" />
                 <link type="text/css" rel="stylesheet" href="<c:url value="${customCssFile}" />" />
-                <script type="text/javascript" src="<c:url value="/js/common_rosters.js" />"></script>
            </c:otherwise>
         </c:choose>
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.5/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="<c:url value="/js/cas.js" />"></script>
 	    <link rel="icon" href="<c:url value="/favicon.ico" />" type="image/x-icon" />
 	</head>
-	<body id="cas" onload="init();" class="fl-theme-iphone">
+	<body id="cas" class="fl-theme-iphone">
     <div class="flc-screenNavigator-view-container">
         <div class="fl-screenNavigator-view">
             <div id="header" class="flc-screenNavigator-navbar fl-navbar fl-table">
---
+++ cas-server-webapp/src/main/webapp/js/cas.js
@@ -0,0 +1,50 @@
+// $Id$
+var editInnerHTML = "";
+var deleteInnerHTML = "";
+var currentRow = null;
+
+function swapButtonsForConfirm(rowId, serviceId) {
+
+    resetOldValue();
+    var editCell = $("#edit"+rowId);
+    var deleteCell = $("#delete"+rowId);
+
+    var row = $("#row" + rowId);
+    row.removeClass("over");
+    row.addClass("highlightBottom");
+
+    editInnerHTML = editCell.html();
+    deleteInnerHTML = deleteCell.html();
+    currentRow = rowId;
+    
+    editCell.html("Really?");
+    deleteCell.html("<a id=\"yes\" href=\"deleteRegisteredService.html?id=" + serviceId + "\">Yes</a> <a id=\"no\" href=\"#\" onclick=\"resetOldValue();return false;\">No</a>");
+}
+
+function resetOldValue() {
+    if (currentRow != null) {
+        var curRow = $("#row"+currentRow);
+        curRow.removeClass("over");
+        curRow.removeClass("highlightBottom");
+        var editCell = $("#edit"+currentRow);
+        var deleteCell = $("#delete"+currentRow);
+
+        editCell.html(editInnerHTML);
+        deleteCell.html(deleteInnerHTML);
+       
+        editInnerHTML = null;
+        deleteInnerHTML = null;
+        currentRow = null;
+    }
+}
+
+$(document).ready(function(){
+    //focus username field
+    $("input:visible:enabled:first").focus();
+    //flash error box
+    $('#status').animate({ backgroundColor: 'rgb(187,0,0)' }, 30).animate({ backgroundColor: 'rgb(255,238,221)' }, 500);
+
+    //flash success box
+    $('#msg').animate({ backgroundColor: 'rgb(51,204,0)' }, 30).animate({ backgroundColor: 'rgb(221,255,170)' }, 500);
+});
+
