a520041d3155bb81a521db09edab2d63188ad09c
Marvin S. Addison <marvin.addison@gmail.com>
2011-01-26 19:05:16 +0000

    CAS-936 Add sample deployerConfigContext.xml that demonstrates how to wire up X.509 handler and related beans.

---
+++ cas-server-support-x509/src/test/java/org/jasig/cas/adaptors/x509/authentication/handler/support/WiringTests.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2007 The JA-SIG Collaborative. All rights reserved. See license
+ * distributed with this file and available online at
+ * http://www.uportal.org/license.html
+ */
+package org.jasig.cas.adaptors.x509.authentication.handler.support;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+
+/**
+ * Verifies Spring IOC wiring for X.509 beans.
+ *
+ * @author Marvin S. Addison
+ * @version $Revision$
+ * @since 3.4.7
+ *
+ */
+public class WiringTests {
+    @Test
+    public void testWiring() {
+        final ApplicationContext context = new ClassPathXmlApplicationContext("deployerConfigContext.xml");
+        Assert.assertTrue(context.getBeanDefinitionCount() > 0);
+    }
+}
