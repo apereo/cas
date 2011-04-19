e982201ad9e8e12720ce2971d86020f7accf559d
Marvin S. Addison <marvin.addison@gmail.com>
2011-04-19 14:42:34 +0000

    CAS-958 Use random stateful UUID instead of encryption to provide for randomness in LT.  Avoidance of symmetric encryption was driven by concerns about key management for deployers.

--- cas-server-3.4.2/cas-server-webapp/src/main/java/org/jasig/cas/web/flow/CasFlowExecutionKeyFactory.java
+++ cas-server-3.4.2/cas-server-webapp/src/main/java/org/jasig/cas/web/flow/CasFlowExecutionKeyFactory.java
@@ -15,118 +15,51 @@ import org.springframework.webflow.execution.repository.impl.DefaultFlowExecutio
 import org.springframework.webflow.execution.repository.snapshot.FlowExecutionSnapshotFactory;
 import org.springframework.webflow.execution.repository.support.CompositeFlowExecutionKey;
 
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.DESKeySpec;
-import javax.crypto.spec.IvParameterSpec;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
-import java.nio.charset.Charset;
-import java.security.InvalidKeyException;
-import java.security.Key;
-import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.security.spec.InvalidKeySpecException;
 import java.util.UUID;
 
 /**
- * Extension to the default Spring Web Flow {@link DefaultFlowExecutionRepository}.  We override a number of protected methods to
- * provide our own key which includes a random part. Due to the structure of the super class, we've also had to copy a
- * number of private methods to the sub class in order to have it actually work.  <strong>This is not a good idea.</strong>
+ * Extension to the default Spring Web Flow {@link DefaultFlowExecutionRepository}
+ * that supports {@link CasFlowExecutionKey}, which contains a random component
+ * that is suitable for use as the LT parameter of the CAS protocol.  The random
+ * UUID component of the flow key is stored as a conversation attribute to
+ * support validation when the flow is resumed.
  *
  * @author Scott Battaglia
+ * @author Marvin S. Addison
  * @version $Revision$ $Date$
  * @since 3.4.7
  */
 public final class CasFlowExecutionKeyFactory extends DefaultFlowExecutionRepository {
 
-    public static final String DEFAULT_ENCRYPTION_ALGORITHM = "AES";
-
-    public static final String DEFAULT_CIPHER_ALGORITHM = "AES/CBC/PKCS5Padding";
+    private static final String UUID_KEY = "CAS_UUID";
+    
+    private final SecureRandom secureRandom = new SecureRandom();
 
     private boolean defaultBehavior = false;
 
     @NotNull
     private final ConversationManager conversationManager;
 
-    @NotNull
-    private final Key key;
-
-    @NotNull
-    private final String cipherAlgorithm;
-
-    private final byte[] initialVector = getRandomSalt(16);
-
-    private final IvParameterSpec ivs = new IvParameterSpec(this.initialVector);
-
-    public CasFlowExecutionKeyFactory(final ConversationManager conversationManager, final FlowExecutionSnapshotFactory snapshotFactory) throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException {
-        this(conversationManager, snapshotFactory, DEFAULT_CIPHER_ALGORITHM, KeyGenerator.getInstance(DEFAULT_ENCRYPTION_ALGORITHM).generateKey());
-    }
-
-    public CasFlowExecutionKeyFactory(final ConversationManager conversationManager, final FlowExecutionSnapshotFactory snapshotFactory, final String cipherAlgorithm, final Key secretKey) {
+    public CasFlowExecutionKeyFactory(final ConversationManager conversationManager, final FlowExecutionSnapshotFactory snapshotFactory) {
         super(conversationManager, snapshotFactory);
         this.conversationManager = conversationManager;
-        this.key = secretKey;
-        this.cipherAlgorithm = cipherAlgorithm;
     }
 
-    private static byte[] getRandomSalt(final int size) {
-        final SecureRandom secureRandom = new SecureRandom();
-        final byte[] bytes = new byte[size];
-
-        secureRandom.nextBytes(bytes);
-        return bytes;
-    }
-
-    protected String decrypt(final String value) {
-        if (value == null) {
-            return null;
-        }
-
-        try {
-            final Cipher cipher = Cipher.getInstance(this.cipherAlgorithm);
-            cipher.init(Cipher.DECRYPT_MODE, this.key, this.ivs);
-            return new String(cipher.doFinal(hexStringToByteArray(value)));
-        } catch (final Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    protected String encrypt(final String value) {
-        if (value == null) {
-            return null;
-        }
-
-        try {
-            final Cipher cipher = Cipher.getInstance(this.cipherAlgorithm);
-            cipher.init(Cipher.ENCRYPT_MODE, this.key, this.ivs);
-            return byteArrayToHexString(cipher.doFinal(value.getBytes()));
-        } catch (final Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-public static String byteArrayToHexString(byte[] b){
-     StringBuffer sb = new StringBuffer(b.length * 2);
-     for (int i = 0; i < b.length; i++){
-       int v = b[i] & 0xff;
-       if (v < 16) {
-         sb.append('0');
-       }
-       sb.append(Integer.toHexString(v));
-     }
-     return sb.toString().toUpperCase();
-  }
-
-    protected static byte[] hexStringToByteArray(final String s) {
-        byte[] b = new byte[s.length() / 2];
-        for (int i = 0; i < b.length; i++){
-          int index = i * 2;
-          int v = Integer.parseInt(s.substring(index, index + 2), 16);
-          b[i] = (byte)v;
-        }
-        return b;
+    /**
+     * Set to true to enable the default Spring Webflow execution key format.
+     * <p>
+     * <strong>NOTE:</strong>
+     * Setting to true is technically a violation of the CAS protocol since the
+     * LT is required to have randomness that the default SWF key format lacks.
+     *
+     * @param defaultBehavior True to enable default behavior, false otherwise.
+     * Default is false.
+     */
+    public void setDefaultBehavior(final boolean defaultBehavior) {
+        this.defaultBehavior = defaultBehavior;
     }
 
     public FlowExecutionKey getKey(final FlowExecution execution) {
@@ -134,26 +67,19 @@ public static String byteArrayToHexString(byte[] b){
             return super.getKey(execution);
         }
 
-		final CasFlowExecutionKey key = (CasFlowExecutionKey) execution.getKey();
+		CasFlowExecutionKey key = (CasFlowExecutionKey) execution.getKey();
 		if (key == null) {
-			final Conversation conversation = beginConversation(execution);
+	        final Conversation conversation = this.conversationManager.beginConversation(
+	            createConversationParameters(execution));
 			final ConversationId executionId = conversation.getId();
-            final Serializable nextSnapshotId = nextSnapshotId(executionId);
-            final String unencryptedVersion = UUID.randomUUID().toString() + CasFlowExecutionKey.KEY_SEPARATOR + "e" + executionId + "s" + nextSnapshotId;
-            final String encryptedVersion = encrypt(unencryptedVersion);
-			return new CasFlowExecutionKey(executionId, nextSnapshotId, encryptedVersion);
+            key = newKey(conversation, executionId);
 		} else {
 			if (getAlwaysGenerateNewNextKey()) {
                 final Serializable executionId = key.getExecutionId();
-                final Serializable snapshotId = nextSnapshotId(key.getExecutionId());
-                final String unencryptedVersion = UUID.randomUUID().toString() + CasFlowExecutionKey.KEY_SEPARATOR + "e" + executionId + "s" + snapshotId;
-                final String encryptedVersion = encrypt(unencryptedVersion);
-
-				return new CasFlowExecutionKey(executionId, snapshotId, encryptedVersion);
-			} else {
-				return execution.getKey();
+                key = newKey(getConversation(executionId), executionId);
 			}
 		}
+		return key;
     }
 
 	public FlowExecutionKey parseFlowExecutionKey(final String encodedKey) throws FlowExecutionRepositoryException {
@@ -161,15 +87,20 @@ public static String byteArrayToHexString(byte[] b){
             return super.parseFlowExecutionKey(encodedKey);
         }
 
-
 		if (!StringUtils.hasText(encodedKey)) {
-			throw new BadlyFormattedFlowExecutionKeyException(encodedKey, "The string-encoded flow execution key is required");
+			throw new IllegalStateException("The string-encoded flow execution key is required");
 		}
-        final String unencryptedVersion = decrypt(encodedKey);
-		String[] keyParts = CasFlowExecutionKey.keyParts(unencryptedVersion);
-		Serializable executionId = parseExecutionId(keyParts[0], encodedKey);
-		Serializable snapshotId = parseSnapshotId(keyParts[1], encodedKey);
-        return new CasFlowExecutionKey(executionId, snapshotId, encodedKey);
+		final String[] keyParts = CasFlowExecutionKey.keyParts(encodedKey);
+		final UUID uuid;
+		try {
+		    uuid = UUID.fromString(keyParts[0]);
+		} catch (Exception e) {
+			throw new BadlyFormattedFlowExecutionKeyException(encodedKey, CasFlowExecutionKey.FORMAT);
+		}
+		Serializable executionId = parseExecutionId(keyParts[1], encodedKey);
+		Serializable snapshotId = parseSnapshotId(keyParts[2], encodedKey);
+		validateUUID(uuid, executionId);
+		return new CasFlowExecutionKey(executionId, snapshotId, uuid);
 	}
 
 	private ConversationId parseExecutionId(final String encodedId, final String encodedKey) throws BadlyFormattedFlowExecutionKeyException {
@@ -187,19 +118,33 @@ public static String byteArrayToHexString(byte[] b){
 			throw new BadlyFormattedFlowExecutionKeyException(encodedKey, CompositeFlowExecutionKey.getFormat(), e);
 		}
 	}
-
-    /**
-     * Copied from super-class since its marked as private.
-     * @param execution
-     * @return
-     */
-    	private Conversation beginConversation(FlowExecution execution) {
-		ConversationParameters parameters = createConversationParameters(execution);
-		Conversation conversation = this.conversationManager.beginConversation(parameters);
-		return conversation;
+	
+	private CasFlowExecutionKey newKey(final Conversation conversation, final Serializable executionId) {
+	    // Include time component in UUID to prevent successive dupes
+	    // Use ~24h of millis so we have more bytes for random data
+	    final long hi_word = (long) this.secureRandom.nextInt() << 32;
+	    final long lo_word = System.currentTimeMillis() & 0xFFFFFFFF;
+	    final UUID uuid = new UUID(hi_word | lo_word, this.secureRandom.nextLong());
+	    conversation.lock();
+	    try {
+	        conversation.putAttribute(UUID_KEY, uuid);
+	    } finally {
+	        conversation.unlock();
+	    }
+	    return new CasFlowExecutionKey(executionId, nextSnapshotId(executionId), uuid);
+	}
+	
+	private void validateUUID(final UUID givenUUID, final Serializable executionId) {
+	    final Conversation conversation = getConversation(executionId);
+	    conversation.lock();
+	    try {
+	        final UUID savedUUID = (UUID) conversation.getAttribute(UUID_KEY);
+	        if (!givenUUID.equals(savedUUID)) {
+	            throw new IllegalStateException("UUID component of flow execution key not recognized.");
+	        }
+	    } finally {
+	        conversation.unlock();
+	    }
 	}
 
-    public void setDefaultBehavior(final boolean defaultBehavior) {
-        this.defaultBehavior = defaultBehavior;
-    }
 }
