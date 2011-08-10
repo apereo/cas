fa5b325b5be9206d140b3557814f38c1bfc7723c
William G. Thompson <wgthom@gmail.com>
2011-08-10 20:47:27 +0000

    CAS-1027
    new expiration policy incorporating both idle and hard timeout, and throttling.
    
    throttling needs to be looked.  A collision of recently-used-ticket/cool-down and the registry cleaner could result in an otherwise valid ticket being deleted.

---
+++ cas-server-3.4.2/cas-server-core/src/test/java/org/jasig/cas/ticket/support/TicketGrantingTicketExpirationPolicyTests.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2007 The JA-SIG Collaborative. All rights reserved. See license
+ * distributed with this file and available online at
+ * http://www.ja-sig.org/products/cas/overview/license/
+ */
+package org.jasig.cas.ticket.support;
+
+import junit.framework.TestCase;
+import org.jasig.cas.TestUtils;
+import org.jasig.cas.ticket.TicketGrantingTicket;
+import org.jasig.cas.ticket.TicketGrantingTicketImpl;
+
+/**
+ * @author William G. Thompson, Jr.
+ * @version $Revision$ $Date$
+ */
+public class TicketGrantingTicketExpirationPolicyTests extends TestCase {
+
+    private static final long HARD_TIMEOUT = 10000; // 10s
+
+    private static final long SLIDING_TIMEOUT = 2000; // 2s
+
+    private static final long COOL_DOWN = 500; // 1/2s
+
+    private TicketGrantingTicketExpirationPolicy expirationPolicy;
+
+    private TicketGrantingTicket ticketGrantingTicket;
+
+    protected void setUp() throws Exception {
+        expirationPolicy = new TicketGrantingTicketExpirationPolicy();
+        expirationPolicy.setMaxTimeToLiveInMilliSeconds(HARD_TIMEOUT);
+        expirationPolicy.setTimeToKillInMilliSeconds(SLIDING_TIMEOUT);
+        expirationPolicy.setTimeInBetweenUsesInMilliSeconds(COOL_DOWN);
+        ticketGrantingTicket = new TicketGrantingTicketImpl("test", TestUtils.getAuthentication(), expirationPolicy);
+        super.setUp();
+    }
+
+     public void testTgtIsExpiredByHardTimeOut() {
+         try {
+             // keep tgt alive via sliding window until within SLIDING_TIME / 2 of the HARD_TIMEOUT
+             while (System.currentTimeMillis() - ticketGrantingTicket.getCreationTime() < (HARD_TIMEOUT - SLIDING_TIMEOUT / 2)) {
+                 ticketGrantingTicket.grantServiceTicket("test", TestUtils.getService(), expirationPolicy, false);
+                 Thread.sleep(SLIDING_TIMEOUT - 100);
+                 assertFalse(this.ticketGrantingTicket.isExpired());
+             }
+
+             // final sliding window extension past the HARD_TIMEOUT
+             ticketGrantingTicket.grantServiceTicket("test", TestUtils.getService(), expirationPolicy, false);
+             Thread.sleep(SLIDING_TIMEOUT / 2 + 100);
+             assertTrue(ticketGrantingTicket.isExpired());
+
+         } catch (InterruptedException e) {
+             fail(e.getMessage());
+         }
+     }
+
+    public void testTgtIsExpiredBySlidingWindow() {
+        try {
+            ticketGrantingTicket.grantServiceTicket("test", TestUtils.getService(), expirationPolicy, false);
+            Thread.sleep(SLIDING_TIMEOUT - 100);
+            assertFalse(ticketGrantingTicket.isExpired());
+
+            ticketGrantingTicket.grantServiceTicket("test", TestUtils.getService(), expirationPolicy, false);
+            Thread.sleep(SLIDING_TIMEOUT - 100);
+            assertFalse(ticketGrantingTicket.isExpired());
+
+            ticketGrantingTicket.grantServiceTicket("test", TestUtils.getService(), expirationPolicy, false);
+            Thread.sleep(SLIDING_TIMEOUT + 100);
+            assertTrue(ticketGrantingTicket.isExpired());
+
+        } catch (InterruptedException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    // XXX problem with CoolDownTime...if RegistryCleaner calls isExpired within CoolDown period it will remove a valid ticket!?!?
+    public void testTgtUsedBeforeCoolDownTime() {
+        try {
+            assertFalse(ticketGrantingTicket.isExpired());
+            ticketGrantingTicket.grantServiceTicket("test", TestUtils.getService(), expirationPolicy, false);
+            assertTrue(ticketGrantingTicket.isExpired());
+            Thread.sleep(COOL_DOWN + 100);
+            assertFalse(ticketGrantingTicket.isExpired());
+        } catch (InterruptedException e) {
+            fail(e.getMessage());
+        }
+    }
+}
