94c48bc4c68163f7e2902681591ff99909483fa6
William G. Thompson <wgthom@gmail.com>
2011-08-11 15:33:52 +0000

    CAS-1027
    improved property names and debug messaging.  added more comments about the throttling issue.
    throttling needs to be looked.  A collision of recently-used-ticket/cool-down and the registry cleaner could result in an otherwise valid ticket being deleted.

--- cas-server-3.4.2/cas-server-core/src/main/java/org/jasig/cas/ticket/support/TicketGrantingTicketExpirationPolicy.java
+++ cas-server-3.4.2/cas-server-core/src/main/java/org/jasig/cas/ticket/support/TicketGrantingTicketExpirationPolicy.java
@@ -31,7 +31,7 @@ public final class TicketGrantingTicketExpirationPolicy implements ExpirationPol
     private long timeToKillInMilliSeconds;
 
     /** Time between which a ticket must wait to be used again. */
-    private long timeInBetweenUsesInMilliSeconds;
+    private long minTimeInBetweenUsesInMilliSeconds;
 
     public void setMaxTimeToLiveInMilliSeconds(final long maxTimeToLiveInMilliSeconds){
         this.maxTimeToLiveInMilliSeconds = maxTimeToLiveInMilliSeconds;
@@ -41,8 +41,8 @@ public final class TicketGrantingTicketExpirationPolicy implements ExpirationPol
         this.timeToKillInMilliSeconds = timeToKillInMilliSeconds;
     }
 
-    public void setTimeInBetweenUsesInMilliSeconds(final long timeInBetweenUsesInMilliSeconds) {
-        this.timeInBetweenUsesInMilliSeconds = timeInBetweenUsesInMilliSeconds;
+    public void setMinTimeInBetweenUsesInMilliSeconds(final long minTimeInBetweenUsesInMilliSeconds) {
+        this.minTimeInBetweenUsesInMilliSeconds = minTimeInBetweenUsesInMilliSeconds;
     }
 
     public boolean isExpired(final TicketState ticketState) {
@@ -51,7 +51,7 @@ public final class TicketGrantingTicketExpirationPolicy implements ExpirationPol
         if (ticketState.getCountOfUses() == 0) {
             if ((System.currentTimeMillis() - ticketState.getCreationTime() < timeToKillInMilliSeconds)) {
                 if (log.isDebugEnabled()) {
-                    log.debug("Ticket is not expired due to a uses count of zero and the time being within timeToKillInMilliseconds");
+                    log.debug("Ticket is not expired due to a uses count of zero and the time since creation being within timeToKillInMilliseconds");
                 }
                 return false;
             }
@@ -60,7 +60,7 @@ public final class TicketGrantingTicketExpirationPolicy implements ExpirationPol
         // Ticket has been used, check maxTimeToLive (hard window)
         if ((System.currentTimeMillis() - ticketState.getCreationTime() >= maxTimeToLiveInMilliSeconds)) {
             if (log.isDebugEnabled()) {
-                log.debug("Ticket is expired due to the time being greater than the maxTimeToLiveInMilliSeconds");
+                log.debug("Ticket is expired due to the time since creation being greater than the maxTimeToLiveInMilliSeconds");
             }
             return true;
         }
@@ -68,14 +68,14 @@ public final class TicketGrantingTicketExpirationPolicy implements ExpirationPol
         // Ticket is within hard window, check timeToKill (sliding window)
         if ((System.currentTimeMillis() - ticketState.getLastTimeUsed() >= timeToKillInMilliSeconds)) {
             if (log.isDebugEnabled()) {
-                log.debug("Ticket is expired due to the time being greater than the timeToKillInMilliseconds");
+                log.debug("Ticket is expired due to the time since last use being greater than the timeToKillInMilliseconds");
             }
             return true;
         }
 
         // Ticket is within timeouts, check cool down period.
-        if ((System.currentTimeMillis() - ticketState.getLastTimeUsed() <= timeInBetweenUsesInMilliSeconds)) {
-            log.warn("Ticket is expired due to the time being less than the waiting period.");
+        if ((System.currentTimeMillis() - ticketState.getLastTimeUsed() <= minTimeInBetweenUsesInMilliSeconds)) {
+            log.warn("Ticket is expired (throttled, really) due to the time since last use being less than minTimeInBetweenUsesInMilliSeconds.");
             return true;
         }
 
