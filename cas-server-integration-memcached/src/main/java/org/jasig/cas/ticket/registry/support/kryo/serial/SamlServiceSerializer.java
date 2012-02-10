bb6c4a48e81fd3ea9e1cede32e12ba1dcb341649
Marvin S. Addison <marvin.addison@gmail.com>
2012-02-10 13:02:22 -0500

    CAS-1116 Add KryoTranscoder component that uses Kryo serialization framework to marshal objects to bytes for storage in memcached. Add type-specific Kryo serializers for efficiency and proper handling of unmodifiable collections that are commonly used in CAS ticket types. The Kryo FieldSerializer class that handles complex types by default proved incapable of dealing with unmodiable collections in testing.

---
+++ cas-server-integration-memcached/src/main/java/org/jasig/cas/ticket/registry/support/kryo/serial/SamlServiceSerializer.java
@@ -0,0 +1,66 @@
+/*
+  $Id: $
+
+  Copyright (C) 2012 Virginia Tech.
+  All rights reserved.
+
+  SEE LICENSE FOR MORE INFORMATION
+
+  Author:  Middleware Services
+  Email:   middleware@vt.edu
+  Version: $Revision: $
+  Updated: $Date: $
+*/
+package org.jasig.cas.ticket.registry.support.kryo.serial;
+
+import com.esotericsoftware.kryo.Kryo;
+import org.jasig.cas.authentication.principal.SamlService;
+import org.jasig.cas.ticket.registry.support.kryo.FieldHelper;
+import org.jasig.cas.util.HttpClient;
+
+import java.lang.reflect.Constructor;
+import java.nio.ByteBuffer;
+
+/**
+ * Description of SamlServiceSerializer.
+ *
+ * @author Middleware Services
+ * @version $Revision: $
+ */
+public class SamlServiceSerializer extends AbstractWebApplicationServiceSerializer<SamlService> {
+    
+    private static final Constructor CONSTRUCTOR;
+    
+    static {
+        try {
+            CONSTRUCTOR = SamlService.class.getDeclaredConstructor(
+                    String.class, String.class, String.class, HttpClient.class, String.class);
+            CONSTRUCTOR.setAccessible(true);
+        } catch (NoSuchMethodException e) {
+            throw new IllegalStateException("Expected constructor signature not found.", e);
+        }
+    }
+
+    public SamlServiceSerializer(final Kryo kryo, final FieldHelper helper) {
+        super(kryo, helper);
+    }
+
+    public void write(final ByteBuffer buffer, final SamlService service) {
+        super.write(buffer, service);
+        kryo.writeObjectData(buffer, service.getRequestID());
+    }
+
+    protected SamlService createService(
+            final ByteBuffer buffer,
+            final String id,
+            final String originalUrl,
+            final String artifactId) {
+
+        final String requestId = kryo.readObjectData(buffer, String.class);
+        try {
+            return (SamlService) CONSTRUCTOR.newInstance(id, originalUrl, artifactId, new HttpClient(), requestId);
+        } catch (Exception e) {
+            throw new IllegalStateException("Error creating SamlService", e);
+        }
+    }
+}
