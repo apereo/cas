0a43606b2b8916ca42d6815cd8154c1985afe834
LELEU Jerome <leleuj@gmail.com>
2012-03-15 22:19:03 +0100

    CAS-1041 : attributes should be attached to principal and not to authentication

--- cas-server-support-oauth/src/main/java/org/jasig/cas/support/oauth/authentication/OAuthAuthenticationMetaDataPopulator.java
+++ cas-server-support-oauth/src/main/java/org/jasig/cas/support/oauth/authentication/OAuthAuthenticationMetaDataPopulator.java
@@ -17,7 +17,10 @@ package org.jasig.cas.support.oauth.authentication;
 
 import org.jasig.cas.authentication.Authentication;
 import org.jasig.cas.authentication.AuthenticationMetaDataPopulator;
+import org.jasig.cas.authentication.MutableAuthentication;
 import org.jasig.cas.authentication.principal.Credentials;
+import org.jasig.cas.authentication.principal.Principal;
+import org.jasig.cas.authentication.principal.SimplePrincipal;
 import org.jasig.cas.support.oauth.authentication.principal.OAuthCredentials;
 
 /**
@@ -32,7 +35,13 @@ public final class OAuthAuthenticationMetaDataPopulator implements Authenticatio
     public Authentication populateAttributes(Authentication authentication, Credentials credentials) {
         if (credentials instanceof OAuthCredentials) {
             OAuthCredentials oauthCredentials = (OAuthCredentials) credentials;
-            authentication.getAttributes().putAll(oauthCredentials.getUserAttributes());
+            final Principal simplePrincipal = new SimplePrincipal(authentication.getPrincipal().getId(),
+                                                                  oauthCredentials.getUserAttributes());
+            final MutableAuthentication mutableAuthentication = new MutableAuthentication(simplePrincipal,
+                                                                                          authentication
+                                                                                              .getAuthenticatedDate());
+            mutableAuthentication.getAttributes().putAll(authentication.getAttributes());
+            return mutableAuthentication;
         }
         return authentication;
     }
