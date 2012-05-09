874852cb3c83754f9d0ec6844c74bdf81184e788
Misagh Moayyed <mmoayyed@unicon.net>
2012-05-08 20:49:49 -0700

    Feature-LPPE: Removed all editor noise, and moved the config to the unused folder and prepared a sample properties file for the list of settings.
    Added additional comments inside the login flow and the deployerConfigContext.xml file for how to enable LPPE.

--- cas-server-core/src/main/java/org/jasig/cas/authentication/handler/AuthenticationException.java
+++ cas-server-core/src/main/java/org/jasig/cas/authentication/handler/AuthenticationException.java
@@ -22,7 +22,7 @@ package org.jasig.cas.authentication.handler;
  * The most generic type of authentication exception that one can catch if not
  * sure what specific implementation will be thrown. Top of the tree of all
  * other AuthenticationExceptions.
- * 
+ *
  * @author Scott Battaglia
  * @version $Revision$ $Date$
  * @since 3.0
@@ -35,11 +35,14 @@ public abstract class AuthenticationException extends Exception {
     /** The code to return for resolving to a message description. */
     private String code;
 
+    /** The error type that provides additional info about the nature of the exception cause **/
+    private String type = "error";
+
     /**
      * Constructor that takes a code description of the error. These codes
      * normally have a corresponding entries in the messages file for the
      * internationalization of error messages.
-     * 
+     *
      * @param code The short unique identifier for this error.
      */
     public AuthenticationException(final String code) {
@@ -47,10 +50,39 @@ public abstract class AuthenticationException extends Exception {
     }
 
     /**
+     * Constructor that takes a <code>code</code> description of the error along with the exception
+     * <code>msg</code> generally for logging purposes. These codes normally have a corresponding
+     * entries in the messages file for the internationalization of error messages.
+     *
+     * @param code The short unique identifier for this error.
+     * @param msg The error message associated with this exception for additional logging purposes.
+     */
+    public AuthenticationException(final String code, final String msg) {
+        super(msg);
+        this.code = code;
+    }
+
+    /**
+     * Constructor that takes a <code>code</code> description of the error along with the exception
+     * <code>msg</code> generally for logging purposes and the <code>type</code> of the error that originally caused the exception.
+     * These codes normally have a corresponding entries in the messages file for the internationalization of error messages.
+     *
+     * @param code The short unique identifier for this error.
+     * @param msg The error message associated with this exception for additional logging purposes.
+     * @param type The type of the error message that caused the exception to be thrown. By default,
+     * all errors are considered of <code>error</code>.
+     */
+    public AuthenticationException(final String code, final String msg, final String type) {
+        super(msg);
+        this.code = code;
+        this.type = type;
+    }
+
+    /**
      * Constructor that takes a code description of the error and the chained
      * exception. These codes normally have a corresponding entries in the
      * messages file for the internationalization of error messages.
-     * 
+     *
      * @param code The short unique identifier for this error.
      * @param throwable The chained exception for this AuthenticationException
      */
@@ -60,15 +92,29 @@ public abstract class AuthenticationException extends Exception {
     }
 
     /**
+     * Method to return the error type of this exception
+     *
+     * @return the String identifier for the cause of this error.
+     */
+    public final String getType() {
+        return this.type;
+    }
+
+    /**
      * Method to return the unique identifier for this error type.
-     * 
+     *
      * @return the String identifier for this error type.
      */
     public final String getCode() {
         return this.code;
     }
 
+    @Override
     public final String toString() {
-        return getCode();
+        String msg = getCode();
+        if (getMessage() != null && getMessage().trim().length() > 0)
+            msg = ":" + getMessage();
+        return msg;
     }
+
 }
