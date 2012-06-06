cec6145a7a4775aa2e1b1c8e32f7f01f84793912
Misagh Moayyed <mmoayyed@unicon.net>
2012-06-05 22:10:14 -0700

    JIRA: https://issues.jasig.org/browse/CAS-1133
    When an authentication exception occurs, such as one where the user's account is locked or its password has expired, the LPPE module does not return the error type back to the login flow. This causes the generic error message of "bad credentials" to be always displayed.
    
    Also, moved sample templates for the contextSource, dirContextValidator and pooledContextSource into the lppe-config and out of the main config file.

--- cas-server-core/src/main/java/org/jasig/cas/authentication/LinkedAuthenticationHandlerAndCredentialsToPrincipalResolverAuthenticationManager.java
+++ cas-server-core/src/main/java/org/jasig/cas/authentication/LinkedAuthenticationHandlerAndCredentialsToPrincipalResolverAuthenticationManager.java
@@ -51,6 +51,8 @@ public class LinkedAuthenticationHandlerAndCredentialsToPrincipalResolverAuthent
     protected Pair<AuthenticationHandler, Principal> authenticateAndObtainPrincipal(final Credentials credentials) throws AuthenticationException {
         boolean foundOneThatWorks = false;
         String handlerName;
+        AuthenticationException authException = BadCredentialsAuthenticationException.ERROR; 
+        
         for (final AuthenticationHandler authenticationHandler : this.linkedHandlers.keySet()) {
             if (!authenticationHandler.supports(credentials)) {
                 continue;
@@ -59,11 +61,15 @@ public class LinkedAuthenticationHandlerAndCredentialsToPrincipalResolverAuthent
             foundOneThatWorks = true;
             boolean authenticated = false;
             handlerName = authenticationHandler.getClass().getName();
+                        
             try {
                 authenticated = authenticationHandler.authenticate(credentials);
+            } catch (AuthenticationException e) {
+                authException = e;
+                logAuthenticationHandlerError(handlerName, credentials, e);
             } catch (Exception e) {
-                log.error("{} threw error authenticating {}", new Object[] {handlerName, credentials, e});
-            }
+                logAuthenticationHandlerError(handlerName, credentials, e);
+            } 
 
             if (authenticated) {
                 log.info("{} successfully authenticated {}", handlerName, credentials);
@@ -74,9 +80,21 @@ public class LinkedAuthenticationHandlerAndCredentialsToPrincipalResolverAuthent
         }
 
         if (foundOneThatWorks) {
-            throw BadCredentialsAuthenticationException.ERROR;
+            throw authException;
         }
 
         throw UnsupportedCredentialsException.ERROR;
+    }
+    
+    /**
+     * Logs the exception occurred as an error.
+     * 
+     * @param handlerName The class name of the authentication handler.
+     * @param credentials Client credentials subject to authentication. 
+     * @param e The exception that has occurred during authentication attempt.
+     */
+    private void logAuthenticationHandlerError(final String handlerName, final Credentials credentials, final Exception e) {
+        if (this.log.isErrorEnabled())
+            this.log.error("{} threw error authenticating {}", new Object[] {handlerName, credentials, e});
     }
 }
