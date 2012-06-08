41d81b6cd5a29506f727c5cd4bc3bd8417b34da9
Marvin S. Addison <marvin.addison@gmail.com>
2012-06-08 13:46:07 -0400

    CAS-1135
    Don't assume the value returned from COUNT aggregate query is an integer.
    Add verifying test case for JpaTicketRegistry.

--- cas-server-core/src/test/java/org/jasig/cas/monitor/SessionMonitorTests.java
+++ cas-server-core/src/test/java/org/jasig/cas/monitor/SessionMonitorTests.java
@@ -18,13 +18,28 @@
  */
 package org.jasig.cas.monitor;
 
+import org.jasig.cas.authentication.ImmutableAuthentication;
+import org.jasig.cas.authentication.principal.SimplePrincipal;
 import org.jasig.cas.mock.MockService;
-import org.jasig.cas.mock.MockTicketGrantingTicket;
+import org.jasig.cas.ticket.ExpirationPolicy;
+import org.jasig.cas.ticket.TicketGrantingTicketImpl;
 import org.jasig.cas.ticket.registry.DefaultTicketRegistry;
+import org.jasig.cas.ticket.registry.JpaTicketRegistry;
+import org.jasig.cas.ticket.registry.TicketRegistry;
+import org.jasig.cas.ticket.support.HardTimeoutExpirationPolicy;
+import org.jasig.cas.util.DefaultUniqueTicketIdGenerator;
+import org.jasig.cas.util.UniqueTicketIdGenerator;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for {@link SessionMonitor} class.
@@ -32,21 +47,30 @@ import static org.junit.Assert.*;
  * @author Marvin S. Addison
  * @version $Revision: $
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:/jpaTestApplicationContext.xml"})
+@Transactional
 public class SessionMonitorTests {
 
-    private DefaultTicketRegistry registry;
+    private static final ExpirationPolicy TEST_EXP_POLICY = new HardTimeoutExpirationPolicy(10000);
+    private static final UniqueTicketIdGenerator GENERATOR = new DefaultUniqueTicketIdGenerator();
+
+    @Autowired
+    private JpaTicketRegistry jpaRegistry;
+
+    private DefaultTicketRegistry defaultRegistry;
     private SessionMonitor monitor;
 
     @Before
     public void setUp() {
-        registry = new DefaultTicketRegistry();
+        defaultRegistry = new DefaultTicketRegistry();
         monitor = new SessionMonitor();
-        monitor.setTicketRegistry(registry);
+        monitor.setTicketRegistry(defaultRegistry);
     }
 
     @Test
     public void testObserveOk() throws Exception {
-        addTicketsToRegistry(5, 10);
+        addTicketsToRegistry(defaultRegistry, 5, 10);
         final SessionStatus status = monitor.observe();
         assertEquals(5, status.getSessionCount());
         assertEquals(10, status.getServiceTicketCount());
@@ -55,7 +79,7 @@ public class SessionMonitorTests {
 
     @Test
     public void testObserveWarnSessionsExceeded() throws Exception {
-        addTicketsToRegistry(10, 1);
+        addTicketsToRegistry(defaultRegistry, 10, 1);
         monitor.setSessionCountWarnThreshold(5);
         final SessionStatus status = monitor.observe();
         assertEquals(StatusCode.WARN, status.getCode());
@@ -64,22 +88,41 @@ public class SessionMonitorTests {
 
     @Test
     public void testObserveWarnServiceTicketsExceeded() throws Exception {
-        addTicketsToRegistry(1, 10);
+        addTicketsToRegistry(defaultRegistry, 1, 10);
         monitor.setServiceTicketCountWarnThreshold(5);
         final SessionStatus status = monitor.observe();
         assertEquals(StatusCode.WARN, status.getCode());
         assertTrue(status.getDescription().contains("Service ticket count"));
     }
 
-    private void addTicketsToRegistry(final int tgtCount, final int stCount) {
-        MockTicketGrantingTicket ticket = null;
+    @Test
+    @Rollback(false)
+    public void testObserveOkJpaTicketRegistry() throws Exception {
+        addTicketsToRegistry(jpaRegistry, 5, 5);
+        assertEquals(10, jpaRegistry.getTickets().size());
+        monitor.setTicketRegistry(jpaRegistry);
+        final SessionStatus status = monitor.observe();
+        assertEquals(5, status.getSessionCount());
+        assertEquals(5, status.getServiceTicketCount());
+        assertEquals(StatusCode.OK, status.getCode());
+    }
+
+    private void addTicketsToRegistry(final TicketRegistry registry, final int tgtCount, final int stCount) {
+        TicketGrantingTicketImpl ticket = null;
         for (int i = 0; i < tgtCount; i++) {
-            ticket = new MockTicketGrantingTicket("junit");
+            ticket = new TicketGrantingTicketImpl(
+                    GENERATOR.getNewTicketId("TGT"),
+                    new ImmutableAuthentication(new SimplePrincipal("grover")),
+                    TEST_EXP_POLICY);
             registry.addTicket(ticket);
         }
         String id;
         for (int i = 0; i < stCount; i++) {
-            registry.addTicket(ticket.grantServiceTicket(new MockService("junit")));
+            registry.addTicket(ticket.grantServiceTicket(
+                    GENERATOR.getNewTicketId("ST"),
+                    new MockService("junit"),
+                    TEST_EXP_POLICY,
+                    false));
         }
     }
 }
