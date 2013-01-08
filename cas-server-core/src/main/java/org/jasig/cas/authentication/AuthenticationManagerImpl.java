apereo/cas

commit baf4ff7f7d9f0c60341be70aad49b5895667da20

CAS-1123 Leverage @Profiled for configuration of authentication stats.
AbstractAuthenticationManger#authenticate is now instrumented with tag AUTHENTICATE to track authentication statistics in a configurable manner as with other Perf4j metrics.
Improve and simplify logging of authentication managers.

cas-server-core/src/main/java/org/jasig/cas/authentication/AuthenticationManagerImpl.java
@@ -19,6 +19,8 @@
 package org.jasig.cas.authentication;
 
 import java.util.List;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import org.jasig.cas.authentication.handler.AuthenticationException;
 import org.jasig.cas.authentication.handler.AuthenticationHandler;
@@ -27,10 +29,6 @@ import org.jasig.cas.authentication.handler.UnsupportedCredentialsException;
 import org.jasig.cas.authentication.principal.Credentials;
 import org.jasig.cas.authentication.principal.CredentialsToPrincipalResolver;
 import org.jasig.cas.authentication.principal.Principal;
-import org.perf4j.LoggingStopWatch;
-
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 /**
  * <p>
@@ -85,37 +83,22 @@ public final class AuthenticationManagerImpl extends AbstractAuthenticationManag
         boolean foundSupported = false;
         boolean authenticated = false;
         AuthenticationHandler authenticatedClass = null;
-        
+        String handlerName;
         for (final AuthenticationHandler authenticationHandler : this.authenticationHandlers) {
             if (authenticationHandler.supports(credentials)) {
                 foundSupported = true;
-
-                boolean auth = false;
-                final LoggingStopWatch stopWatch = new LoggingStopWatch(authenticationHandler.getClass().getSimpleName());
-
+                handlerName = authenticationHandler.getClass().getName();
                 try {
-                    auth = authenticationHandler.authenticate(credentials);
-                } finally {
-                    stopWatch.stop();
-                }
-
-                if (!auth) {
-                    if (log.isInfoEnabled()) {
-                        log.info("AuthenticationHandler: "
-                                + authenticationHandler.getClass().getName()
-                                + " failed to authenticate the user which provided the following credentials: "
-                                + credentials.toString());
+                    if (!authenticationHandler.authenticate(credentials)) {
+                        log.info("{} failed to authenticate {}", handlerName, credentials);
+                    } else {
+                        log.info("{} successfully authenticated {}", handlerName, credentials);
+                        authenticatedClass = authenticationHandler;
+                        authenticated = true;
+                        break;
                     }
-                } else {
-                    if (log.isInfoEnabled()) {
-                        log.info("AuthenticationHandler: "
-                                + authenticationHandler.getClass().getName()
-                                + " successfully authenticated the user which provided the following credentials: "
-                                + credentials.toString());
-                    }
-                    authenticatedClass = authenticationHandler;
-                    authenticated = true;
-                    break;
+                } catch (Exception e) {
+                    log.error("{} threw error authenticating {}", new Object[] {handlerName, credentials, e});
                 }
             }
         }

cas-server-core/src/main/java/org/jasig/cas/authentication/DirectMappingAuthenticationManagerImpl.java
@@ -19,6 +19,8 @@
 package org.jasig.cas.authentication;
 
 import java.util.Map;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import org.jasig.cas.authentication.handler.AuthenticationException;
 import org.jasig.cas.authentication.handler.AuthenticationHandler;
@@ -26,12 +28,7 @@ import org.jasig.cas.authentication.handler.BadCredentialsAuthenticationExceptio
 import org.jasig.cas.authentication.principal.Credentials;
 import org.jasig.cas.authentication.principal.CredentialsToPrincipalResolver;
 import org.jasig.cas.authentication.principal.Principal;
-import org.perf4j.LoggingStopWatch;
-import org.perf4j.StopWatch;
 import org.springframework.util.Assert;
-
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 /**
  * Authentication Manager that provides a direct mapping between credentials
@@ -58,18 +55,19 @@ public final class DirectMappingAuthenticationManagerImpl extends AbstractAuthen
 
         Assert.notNull(d, "no mapping found for: " + credentialsClass.getName());
 
+        final String handlerName = d.getAuthenticationHandler().getClass().getSimpleName();
         boolean authenticated = false;
-        final LoggingStopWatch stopWatch = new LoggingStopWatch(d.getAuthenticationHandler().getClass().getSimpleName());
-
         try {
             authenticated = d.getAuthenticationHandler().authenticate(credentials);
-        } finally {
-            stopWatch.stop();
+        } catch (Exception e) {
+            log.error("{} threw error authenticating {}", new Object[] {handlerName, credentials, e});
         }
 
         if (!authenticated) {
+            log.info("{} failed to authenticate {}", handlerName, credentials);
             throw new BadCredentialsAuthenticationException();
         }
+        log.info("{} successfully authenticated {}", handlerName, credentials);
 
         final Principal p = d.getCredentialsToPrincipalResolver().resolvePrincipal(credentials);
 

cas-server-core/src/main/java/org/jasig/cas/authentication/LinkedAuthenticationHandlerAndCredentialsToPrincipalResolverAuthenticationManager.java
@@ -18,6 +18,10 @@
  */
 package org.jasig.cas.authentication;
 
+import java.util.Map;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import org.jasig.cas.authentication.handler.AuthenticationException;
 import org.jasig.cas.authentication.handler.AuthenticationHandler;
 import org.jasig.cas.authentication.handler.BadCredentialsAuthenticationException;
@@ -25,11 +29,6 @@ import org.jasig.cas.authentication.handler.UnsupportedCredentialsException;
 import org.jasig.cas.authentication.principal.Credentials;
 import org.jasig.cas.authentication.principal.CredentialsToPrincipalResolver;
 import org.jasig.cas.authentication.principal.Principal;
-import org.perf4j.LoggingStopWatch;
-
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import java.util.Map;
 
 /**
  * Ensures that all authentication handlers are tried, but if one is tried, the associated CredentialsToPrincipalResolver is used.
@@ -51,6 +50,7 @@ public class LinkedAuthenticationHandlerAndCredentialsToPrincipalResolverAuthent
     @Override
     protected Pair<AuthenticationHandler, Principal> authenticateAndObtainPrincipal(final Credentials credentials) throws AuthenticationException {
         boolean foundOneThatWorks = false;
+        String handlerName;
         for (final AuthenticationHandler authenticationHandler : this.linkedHandlers.keySet()) {
             if (!authenticationHandler.supports(credentials)) {
                 continue;
@@ -58,18 +58,19 @@ public class LinkedAuthenticationHandlerAndCredentialsToPrincipalResolverAuthent
 
             foundOneThatWorks = true;
             boolean authenticated = false;
-            final LoggingStopWatch stopWatch = new LoggingStopWatch(authenticationHandler.getClass().getSimpleName());
-
-
+            handlerName = authenticationHandler.getClass().getName();
             try {
                 authenticated = authenticationHandler.authenticate(credentials);
-            } finally {
-                stopWatch.stop();
+            } catch (Exception e) {
+                log.error("{} threw error authenticating {}", new Object[] {handlerName, credentials, e});
             }
+
             if (authenticated) {
+                log.info("{} successfully authenticated {}", handlerName, credentials);
                 final Principal p = this.linkedHandlers.get(authenticationHandler).resolvePrincipal(credentials);
                 return new Pair<AuthenticationHandler,Principal>(authenticationHandler, p);
             }
+            log.info("{} failed to authenticate {}", handlerName, credentials);
         }
 
         if (foundOneThatWorks) {

cas-server-core/src/main/java/org/jasig/cas/authentication/AbstractAuthenticationManager.java
@@ -18,18 +18,19 @@
  */
 package org.jasig.cas.authentication;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.validation.constraints.NotNull;
+
 import com.github.inspektr.audit.annotation.Audit;
 import org.jasig.cas.authentication.handler.AuthenticationException;
 import org.jasig.cas.authentication.handler.AuthenticationHandler;
 import org.jasig.cas.authentication.handler.NamedAuthenticationHandler;
 import org.jasig.cas.authentication.principal.Credentials;
 import org.jasig.cas.authentication.principal.Principal;
+import org.perf4j.aop.Profiled;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Scott Battaglia
@@ -37,6 +38,10 @@ import java.util.List;
  * @since 3.3.5
  */
 public abstract class AbstractAuthenticationManager implements AuthenticationManager {
+
+    /** Describes a credential that is not resolved to a principal. */
+    protected static Pair<AuthenticationHandler, Principal> VOID_PRINCIPAL =
+            new Pair<AuthenticationHandler, Principal>(null, null);
 
     /** Log instance for logging events, errors, warnings, etc. */
     protected final Logger log = LoggerFactory.getLogger(AuthenticationManagerImpl.class);
@@ -49,6 +54,7 @@ public abstract class AbstractAuthenticationManager implements AuthenticationMan
         action="AUTHENTICATION",
         actionResolverName="AUTHENTICATION_RESOLVER",
         resourceResolverName="AUTHENTICATION_RESOURCE_RESOLVER")
+    @Profiled(tag = "AUTHENTICATE", logFailuresSeparately = false)
     public final Authentication authenticate(final Credentials credentials) throws AuthenticationException {
 
         final Pair<AuthenticationHandler, Principal> pair = authenticateAndObtainPrincipal(credentials);
@@ -94,7 +100,7 @@ public abstract class AbstractAuthenticationManager implements AuthenticationMan
     protected abstract Pair<AuthenticationHandler,Principal> authenticateAndObtainPrincipal(Credentials credentials) throws AuthenticationException;
 
 
-    protected class Pair<A,B> {
+    protected static class Pair<A,B> {
 
         private final A first;
 

cas-server-webapp/src/main/webapp/WEB-INF/classes/log4j.xml
@@ -61,17 +61,17 @@
         </layout>
     </appender>
 
-       <appender name="graphExecutionTimes" class="org.perf4j.log4j.GraphingStatisticsAppender">
-           <!-- Possible GraphTypes are Mean, Min, Max, StdDev, Count and TPS -->
-           <param name="GraphType" value="Mean"/>
-           <!-- The tags of the timed execution blocks to graph are specified here -->
-           <param name="TagNamesToGraph" value="DESTROY_TICKET_GRANTING_TICKET,GRANT_SERVICE_TICKET,GRANT_PROXY_GRANTING_TICKET,VALIDATE_SERVICE_TICKET,CREATE_TICKET_GRANTING_TICKET" />
-       </appender>
+    <appender name="graphExecutionTimes" class="org.perf4j.log4j.GraphingStatisticsAppender">
+        <!-- Possible GraphTypes are Mean, Min, Max, StdDev, Count and TPS -->
+        <param name="GraphType" value="Mean"/>
+        <!-- The tags of the timed execution blocks to graph are specified here -->
+        <param name="TagNamesToGraph" value="DESTROY_TICKET_GRANTING_TICKET,GRANT_SERVICE_TICKET,GRANT_PROXY_GRANTING_TICKET,VALIDATE_SERVICE_TICKET,CREATE_TICKET_GRANTING_TICKET,AUTHENTICATE" />
+    </appender>
 
-       <appender name="graphExecutionTPS" class="org.perf4j.log4j.GraphingStatisticsAppender">
-           <param name="GraphType" value="TPS" />
-           <param name="TagNamesToGraph" value="DESTROY_TICKET_GRANTING_TICKET,GRANT_SERVICE_TICKET,GRANT_PROXY_GRANTING_TICKET,VALIDATE_SERVICE_TICKET,CREATE_TICKET_GRANTING_TICKET" />
-       </appender>
+    <appender name="graphExecutionTPS" class="org.perf4j.log4j.GraphingStatisticsAppender">
+        <param name="GraphType" value="TPS" />
+        <param name="TagNamesToGraph" value="DESTROY_TICKET_GRANTING_TICKET,GRANT_SERVICE_TICKET,GRANT_PROXY_GRANTING_TICKET,VALIDATE_SERVICE_TICKET,CREATE_TICKET_GRANTING_TICKET,AUTHENTICATE" />
+    </appender>
 
     <!-- Loggers -->
     <!--

commit 5ae953fe06dfcc899e439024c1f31d07471cfe0d

CAS-1244 Restore error handling behavior.
Commit baf4ff7f7d9f0c60341be70aad49b5895667da20 broke the documented behavior of AuthenticationManagerImpl and the undocumented behavior of the other AuthetnicationManager implementations. The former behavior, to throw the first exception of any kind produced by an AuthenticationHandler, has been restored.

cas-server-core/src/main/java/org/jasig/cas/authentication/AuthenticationManagerImpl.java
@@ -85,8 +85,6 @@ public final class AuthenticationManagerImpl extends AbstractAuthenticationManag
         AuthenticationHandler authenticatedClass = null;
         String handlerName;
         
-        AuthenticationException unAuthSupportedHandlerException = BadCredentialsAuthenticationException.ERROR; 
-     
         for (final AuthenticationHandler authenticationHandler : this.authenticationHandlers) {
             if (authenticationHandler.supports(credentials)) {
                 foundSupported = true;
@@ -101,19 +99,19 @@ public final class AuthenticationManagerImpl extends AbstractAuthenticationManag
                         break;
                     }
                 } catch (AuthenticationException e) {
-                    unAuthSupportedHandlerException = e;
                     logAuthenticationHandlerError(handlerName, credentials, e);
-                } catch (Exception e) {
+                    throw e;
+                } catch (RuntimeException e) {
                     logAuthenticationHandlerError(handlerName, credentials, e);
+                    throw e;
                 }
             }
         }
 
         if (!authenticated) {
             if (foundSupported) {
-                throw unAuthSupportedHandlerException;
+                throw BadCredentialsAuthenticationException.ERROR;
             }
-
             throw UnsupportedCredentialsException.ERROR;
         }
 

cas-server-core/src/main/java/org/jasig/cas/authentication/DirectMappingAuthenticationManagerImpl.java
@@ -57,21 +57,20 @@ public final class DirectMappingAuthenticationManagerImpl extends AbstractAuthen
 
         final String handlerName = d.getAuthenticationHandler().getClass().getSimpleName();
         boolean authenticated = false;
-        
-        AuthenticationException authException = BadCredentialsAuthenticationException.ERROR; 
-        
+
         try {
             authenticated = d.getAuthenticationHandler().authenticate(credentials);
         } catch (AuthenticationException e) {
-            authException = e;
             logAuthenticationHandlerError(handlerName, credentials, e);
-        } catch (Exception e) {
+            throw e;
+        } catch (RuntimeException e) {
             logAuthenticationHandlerError(handlerName, credentials, e);
-        } 
+            throw e;
+        }
 
         if (!authenticated) {
             log.info("{} failed to authenticate {}", handlerName, credentials);
-            throw authException;
+            throw BadCredentialsAuthenticationException.ERROR;
         }
         log.info("{} successfully authenticated {}", handlerName, credentials);
 

cas-server-core/src/main/java/org/jasig/cas/authentication/LinkedAuthenticationHandlerAndCredentialsToPrincipalResolverAuthenticationManager.java
@@ -51,8 +51,7 @@ public class LinkedAuthenticationHandlerAndCredentialsToPrincipalResolverAuthent
     protected Pair<AuthenticationHandler, Principal> authenticateAndObtainPrincipal(final Credentials credentials) throws AuthenticationException {
         boolean foundOneThatWorks = false;
         String handlerName;
-        AuthenticationException authException = BadCredentialsAuthenticationException.ERROR; 
-        
+
         for (final AuthenticationHandler authenticationHandler : this.linkedHandlers.keySet()) {
             if (!authenticationHandler.supports(credentials)) {
                 continue;
@@ -65,11 +64,12 @@ public class LinkedAuthenticationHandlerAndCredentialsToPrincipalResolverAuthent
             try {
                 authenticated = authenticationHandler.authenticate(credentials);
             } catch (AuthenticationException e) {
-                authException = e;
                 logAuthenticationHandlerError(handlerName, credentials, e);
-            } catch (Exception e) {
+                throw e;
+            } catch (RuntimeException e) {
                 logAuthenticationHandlerError(handlerName, credentials, e);
-            } 
+                throw e;
+            }
 
             if (authenticated) {
                 log.info("{} successfully authenticated {}", handlerName, credentials);
@@ -80,7 +80,7 @@ public class LinkedAuthenticationHandlerAndCredentialsToPrincipalResolverAuthent
         }
 
         if (foundOneThatWorks) {
-            throw authException;
+            throw BadCredentialsAuthenticationException.ERROR;
         }
 
         throw UnsupportedCredentialsException.ERROR;