8042a7a7aa3bb603950e540ef3875d443a37b92a
Misagh Moayyed <mm1844@gmail.com>
2013-01-23 09:41:40 -0700

    CAS-1251: Move the JsonExceptionView resolver to the management webapp
    Addressed issues raised by Scott. Fixed the import location, used a direct "return null" instead of a stale variable and removed the message key constant.

--- cas-management-webapp/src/main/java/org/jasig/cas/web/view/AjaxAwareJsonExceptionResolver.java
+++ cas-management-webapp/src/main/java/org/jasig/cas/web/view/AjaxAwareJsonExceptionResolver.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.json.exception.JsonExceptionResolver;
 /**
 * Implementation of JsonExceptionResolver that only triages the exception occurred
 * for json requests.
@@ -13,8 +14,6 @@ import org.springframework.web.servlet.ModelAndView;
 * @author Misagh Moayyed
 * @since 4.0.0
 */
-import org.springframework.web.servlet.view.json.exception.JsonExceptionResolver;
-
 public class AjaxAwareJsonExceptionResolver extends JsonExceptionResolver {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
@@ -31,14 +30,13 @@ public class AjaxAwareJsonExceptionResolver extends JsonExceptionResolver {
     }
 
     @Override
-    public ModelAndView resolveException(final HttpServletRequest request, final HttpServletResponse response, final Object handler, final Exception ex) {
-        final ModelAndView mv = null;        
+    public ModelAndView resolveException(final HttpServletRequest request, final HttpServletResponse response, final Object handler, final Exception ex) {      
         final String contentType = request.getHeader(this.ajaxRequestHeaderName);
         if (contentType.equals(this.ajaxRequestHeaderValue)) {
             log.debug("Handling exception {} for ajax request indicated by header {}", ex.getClass().getName(), this.ajaxRequestHeaderName);
             return super.resolveException(request, response, handler, ex);
         }
-        return mv;
+        return null;
     }
 
 }
