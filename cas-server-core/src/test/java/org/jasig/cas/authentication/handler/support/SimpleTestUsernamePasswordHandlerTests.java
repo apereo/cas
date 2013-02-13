79efa5d7c78d4d3a37b94c4a3638f23e1dffbbfd
Marvin S. Addison <marvin.addison@gmail.com>
2013-02-13 16:28:16 -0500

    CAS-1238 Integrate new authentication API error handling into webflow.
    Define AuthenticationExceptionHandler webflow action to decouple error
    handling from the mechanics of authentication (AuthenticationViaFormAction)
    to allow for pluggable strategies for error handling such as supporting
    additional classes of authentication errors, extending existing LPPE
    workflows, and developing new workflows for authentication failures.
    
    SimpleTestUsernamePasswordAuthenticationHandler was extended to throw
    well-known exception classes when pre-defined user names attempt to
    authenticate. This new facility supports high-level testing.

--- cas-server-core/src/test/java/org/jasig/cas/authentication/handler/support/SimpleTestUsernamePasswordHandlerTests.java
+++ cas-server-core/src/test/java/org/jasig/cas/authentication/handler/support/SimpleTestUsernamePasswordHandlerTests.java
@@ -18,13 +18,16 @@
  */
 package org.jasig.cas.authentication.handler.support;
 
-import org.jasig.cas.TestUtils;
-import org.jasig.cas.authentication.handler.AuthenticationException;
-import org.jasig.cas.authentication.handler.PlainTextPasswordEncoder;
-import org.jasig.cas.authentication.handler.support.SimpleTestUsernamePasswordAuthenticationHandler;
-import org.jasig.cas.authentication.principal.UsernamePasswordCredentials;
+import javax.security.auth.login.FailedLoginException;
 
-import junit.framework.TestCase;
+import org.jasig.cas.TestUtils;
+import org.jasig.cas.authentication.HandlerResult;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Test of the simple username/password handler
@@ -33,69 +36,41 @@ import junit.framework.TestCase;
  * @version $Revision$ $Date$
  * @since 3.0
  */
-public final class SimpleTestUsernamePasswordHandlerTests extends TestCase {
+public final class SimpleTestUsernamePasswordHandlerTests {
 
     private SimpleTestUsernamePasswordAuthenticationHandler authenticationHandler;
 
-    protected void setUp() throws Exception {
+    @Before
+    public void setUp() throws Exception {
         this.authenticationHandler = new SimpleTestUsernamePasswordAuthenticationHandler();
-        this.authenticationHandler
-            .setPasswordEncoder(new PlainTextPasswordEncoder());
     }
 
+    @Test
     public void testSupportsProperUserCredentials() {
         assertTrue(this.authenticationHandler.supports(TestUtils
             .getCredentialsWithSameUsernameAndPassword()));
     }
 
+    @Test
     public void testDoesntSupportBadUserCredentials() {
         assertFalse(this.authenticationHandler.supports(TestUtils
             .getHttpBasedServiceCredentials()));
     }
 
-    public void testValidUsernamePassword() throws AuthenticationException {
-        assertTrue(this.authenticationHandler.authenticate(TestUtils
-            .getCredentialsWithSameUsernameAndPassword()));
+    @Test
+    public void testValidUsernamePassword() throws Exception {
+        final HandlerResult result = this.authenticationHandler.authenticate(
+                TestUtils.getCredentialsWithSameUsernameAndPassword());
+        assertEquals(this.authenticationHandler.getName(), result.getHandlerName());
     }
 
-    public void testInvalidUsernamePassword() {
-        try {
-            assertFalse(this.authenticationHandler.authenticate(TestUtils
-                .getCredentialsWithDifferentUsernameAndPassword()));
-        } catch (AuthenticationException ae) {
-            // this is okay
-        }
+    @Test(expected = FailedLoginException.class)
+    public void testInvalidUsernamePassword() throws Exception {
+        this.authenticationHandler.authenticate(TestUtils.getCredentialsWithDifferentUsernameAndPassword());
     }
 
-    public void testNullUsernamePassword() {
-        try {
-            assertFalse(this.authenticationHandler.authenticate(TestUtils
-                .getCredentialsWithSameUsernameAndPassword(null)));
-        } catch (AuthenticationException ae) {
-            // this is okay
-        }
-    }
-    
-    public void testAlternateClass() {
-        this.authenticationHandler.setClassToSupport(UsernamePasswordCredentials.class);
-        assertTrue(this.authenticationHandler.supports(new UsernamePasswordCredentials()));
-    }
-    
-    public void testAlternateClassWithSubclassSupport() {
-        this.authenticationHandler.setClassToSupport(UsernamePasswordCredentials.class);
-        this.authenticationHandler.setSupportSubClasses(true);
-        assertTrue(this.authenticationHandler.supports(new ExtendedCredentials()));
-    }
-    
-    public void testAlternateClassWithNoSubclassSupport() {
-        this.authenticationHandler.setClassToSupport(UsernamePasswordCredentials.class);
-        this.authenticationHandler.setSupportSubClasses(false);
-        assertFalse(this.authenticationHandler.supports(new ExtendedCredentials()));
-    }
-    
-    protected class ExtendedCredentials extends UsernamePasswordCredentials {
-
-        private static final long serialVersionUID = 406992293105518363L;
-        // nothing to see here
+    @Test(expected = FailedLoginException.class)
+    public void testNullUsernamePassword() throws Exception {
+        this.authenticationHandler.authenticate(TestUtils.getCredentialsWithSameUsernameAndPassword(null));
     }
 }
