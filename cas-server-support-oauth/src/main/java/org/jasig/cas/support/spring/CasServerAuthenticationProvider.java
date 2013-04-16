f0ad1b512b5aca38bab325b73a0c48848ffa8ce9
Joe <joemccall@intrepid-networks.com>
2013-04-16 12:23:46 -0400

    Initial Version of new Authentication Provider
    This authentication provider is used along side of spring security. Once
    this is polished and tested we will have CAS manage the identities and
    spring security secure those for those who have ROLE_CAS_USER (in
    progress).

---
+++ cas-server-support-oauth/src/main/java/org/jasig/cas/support/spring/CasServerAuthenticationProvider.java
@@ -0,0 +1,69 @@
+package org.jasig.cas.support.spring;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.validation.constraints.NotNull;
+
+import org.jasig.cas.authentication.AuthenticationManager;
+import org.jasig.cas.authentication.principal.UsernamePasswordCredentials;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+public class CasServerAuthenticationProvider implements AuthenticationProvider {
+    
+    @NotNull
+    final Logger log = LoggerFactory.getLogger(CasServerAuthenticationProvider.class);
+    
+    @NotNull
+    private AuthenticationManager casAuthenticationManager;
+
+    public CasServerAuthenticationProvider(AuthenticationManager casAuthenticationManager) {
+        super();
+        this.casAuthenticationManager = casAuthenticationManager;
+    }
+
+    @Override
+    public Authentication authenticate(final Authentication authentication) throws AuthenticationException {
+        if (!supports(authentication.getClass())) {
+            return null;
+        }
+        
+        if ((authentication.getCredentials() == null) || "".equals(authentication.getCredentials())) {
+            throw new BadCredentialsException("TODO: no credentials");
+        }
+        
+        UsernamePasswordCredentials casCredentials = new UsernamePasswordCredentials();
+        casCredentials.setUsername((String) authentication.getPrincipal());
+        casCredentials.setPassword((String) authentication.getCredentials());
+        
+        
+        org.jasig.cas.authentication.Authentication result;
+        
+        try {
+            result = casAuthenticationManager.authenticate(casCredentials);
+        } catch (org.jasig.cas.authentication.handler.AuthenticationException e) {
+            e.printStackTrace();
+            throw new BadCredentialsException("TODO: cas authentication exception");
+        }
+        
+        Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_CAS_USER"));
+        
+        // TODO 
+        return new UsernamePasswordAuthenticationToken(result.getPrincipal(), authentication.getCredentials(), authorities);
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+
+}
