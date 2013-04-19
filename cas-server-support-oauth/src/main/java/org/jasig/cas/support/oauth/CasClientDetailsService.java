a5bf33ca0155f9db604722128220179872e3befa
Joe <joemccall@intrepid-networks.com>
2013-04-19 18:07:36 -0400

    Moved grant types into its own definition class Added CasTokenServices, even though we may not need them. Pending further word from cas devel on this.

--- cas-server-support-oauth/src/main/java/org/jasig/cas/support/oauth/CasClientDetailsService.java
+++ cas-server-support-oauth/src/main/java/org/jasig/cas/support/oauth/CasClientDetailsService.java
@@ -7,6 +7,8 @@ import javax.validation.constraints.NotNull;
 
 import org.jasig.cas.services.RegisteredService;
 import org.jasig.cas.services.ServicesManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.oauth2.provider.BaseClientDetails;
 import org.springframework.security.oauth2.provider.ClientDetails;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
@@ -14,9 +16,11 @@ import org.springframework.security.oauth2.provider.ClientRegistrationException;
 
 public class CasClientDetailsService implements ClientDetailsService {
     
+    private static final Logger log = LoggerFactory.getLogger(CasClientDetailsService.class);
+    
     @NotNull
     private ServicesManager servicesManager;
-
+    
     public CasClientDetailsService(ServicesManager servicesManager) {
         super();
         this.servicesManager = servicesManager;
@@ -25,11 +29,17 @@ public class CasClientDetailsService implements ClientDetailsService {
     @Override
     public ClientDetails loadClientByClientId(String clientId) throws ClientRegistrationException {
         
+        log.debug("Called loadClientByClientId with argument {}", clientId);
+        
         BaseClientDetails details = new BaseClientDetails();
         
+        // By default, only use secure grant types for every client
         Collection<String> authorizedGrantTypes = new ArrayList<String>();
-        authorizedGrantTypes.add("authorization_code");
+        authorizedGrantTypes.add(GrantType.AUTHORIZATION_CODE);
+        authorizedGrantTypes.add(GrantType.PASSWORD);
+        authorizedGrantTypes.add(GrantType.REFRESH_TOKEN);
         
+        // Set the client id and secret based on the service name and service description respectively
         for (RegisteredService service: servicesManager.getAllServices()) {
             if (clientId.equals(service.getName())) {
                 details.setClientId(clientId);
