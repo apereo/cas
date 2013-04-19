73f4c94fdc7d9e51da21f68d9eaa450da8c35bc9
Joe <joemccall@intrepid-networks.com>
2013-04-19 18:07:36 -0400

    Moved grant types into its own definition class Added CasTokenServices, even though we may not need them. Pending further word from cas devel on this.

---
+++ cas-server-support-oauth/src/main/java/org/jasig/cas/support/oauth/GrantType.java
@@ -0,0 +1,8 @@
+package org.jasig.cas.support.oauth;
+
+public final class GrantType {
+    public static final String AUTHORIZATION_CODE = "authorization_code";
+    public static final String PASSWORD = "password";
+    public static final String CLIENT_CREDENTIALS = "client_credentials";
+    public static final String REFRESH_TOKEN = "refresh_token";
+}
