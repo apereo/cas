a63a81be662094454b1325d3b6929088cbc2dc47
Misagh Moayyed <mmoayyed@unicon.net>
2013-05-02 06:46:19 -0700

    NOJIRA-VARNAMES: Var name fixes in the ldap module

--- cas-server-support-ldap/src/main/java/org/jasig/cas/authentication/PasswordPolicyEnforcer.java
+++ cas-server-support-ldap/src/main/java/org/jasig/cas/authentication/PasswordPolicyEnforcer.java
@@ -28,8 +28,9 @@ public interface PasswordPolicyEnforcer {
     /**
      * @param userId The unique ID of the user
      * @return Number of days to the expiration date, or -1 if checks pass.
+     * @throws LdapPasswordPolicyEnforcementException
      */
-    public long getNumberOfDaysToPasswordExpirationDate(final String userId)
+    long getNumberOfDaysToPasswordExpirationDate(String userId)
             throws LdapPasswordPolicyEnforcementException;
 
 }
