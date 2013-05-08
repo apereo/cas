01e5b24ff79cf53e8f05dcb37b1751e9c4cda928
Misagh Moayyed <mm1844@gmail.com>
2013-05-08 13:18:12 -0700

    Core changes

--- cas-server-core/src/main/java/org/jasig/cas/ticket/registry/AbstractTicketRegistry.java
+++ cas-server-core/src/main/java/org/jasig/cas/ticket/registry/AbstractTicketRegistry.java
@@ -34,7 +34,7 @@ import org.springframework.util.Assert;
 public abstract class AbstractTicketRegistry implements TicketRegistry, TicketRegistryState {
 
     /** The Commons Logging log instance. */
-    protected final Logger log = LoggerFactory.getLogger(getClass());
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
 
     /**
      * @throws IllegalArgumentException if class is null.
@@ -60,13 +60,13 @@ public abstract class AbstractTicketRegistry implements TicketRegistry, TicketRe
     }
 
     public int sessionCount() {
-      log.debug("sessionCount() operation is not implemented by the ticket registry instance {}. Returning unknown as {}",
+      logger.debug("sessionCount() operation is not implemented by the ticket registry instance {}. Returning unknown as {}",
                 this.getClass().getName(), Integer.MIN_VALUE);
       return Integer.MIN_VALUE;
     }
 
     public int serviceTicketCount() {
-      log.debug("serviceTicketCount() operation is not implemented by the ticket registry instance {}. Returning unknown as {}",
+      logger.debug("serviceTicketCount() operation is not implemented by the ticket registry instance {}. Returning unknown as {}",
                 this.getClass().getName(), Integer.MIN_VALUE);
       return Integer.MIN_VALUE;
     }
