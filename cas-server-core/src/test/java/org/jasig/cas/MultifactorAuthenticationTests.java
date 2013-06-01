6bf060d9869498dcb3312ba0cd5a4b37c28474ca
Marvin S. Addison <marvin.addison@gmail.com>
2013-06-01 08:48:51 -0400

    CAS-1238 Ensure authentication in ticket assertion satisfies sec policy.
    The "primaryAssertion" field in the assertion must contain the
    authentication event that satisfies security policy in order to support the
    use case where the ticket validation response contains information to
    indicate security policy was satisfied.
    
    SuccessfulHandlerMetaDataPopulator provides a simple means to support
    sending back handler names in the ticket validation response which could be
    the basis of a simple policy check on the client side.

--- cas-server-core/src/test/java/org/jasig/cas/MultifactorAuthenticationTests.java
+++ cas-server-core/src/test/java/org/jasig/cas/MultifactorAuthenticationTests.java
@@ -19,10 +19,12 @@
 package org.jasig.cas;
 
 import org.jasig.cas.authentication.OneTimePasswordCredentials;
+import org.jasig.cas.authentication.SuccessfulHandlerMetaDataPopulator;
 import org.jasig.cas.authentication.principal.Service;
 import org.jasig.cas.authentication.principal.SimpleWebApplicationServiceImpl;
 import org.jasig.cas.authentication.principal.UsernamePasswordCredentials;
 import org.jasig.cas.ticket.UnsatisfiedAuthenticationPolicyException;
+import org.jasig.cas.validation.Assertion;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -92,12 +94,20 @@ public class MultifactorAuthenticationTests {
         // Note the original credential used to start SSO session does not satisfy security policy
         final String tgt = cas.createTicketGrantingTicket(newUserPassCredentials("alice", "alice"));
         assertNotNull(tgt);
+        final Service service = newService("https://example.com/high/");
         final String st = cas.grantServiceTicket(
                 tgt,
-                newService("https://example.com/high/"),
+                service,
                 newUserPassCredentials("alice", "alice"),
                 new OneTimePasswordCredentials("alice", "31415"));
         assertNotNull(st);
+        // Confirm the authentication in the assertion is the one that satisfies security policy
+        final Assertion assertion = cas.validateServiceTicket(st, service);
+        assertEquals(2, assertion.getPrimaryAuthentication().getSuccesses().size());
+        assertTrue(assertion.getPrimaryAuthentication().getSuccesses().containsKey("passwordHandler"));
+        assertTrue(assertion.getPrimaryAuthentication().getSuccesses().containsKey("oneTimePasswordHandler"));
+        assertTrue(assertion.getPrimaryAuthentication().getAttributes().containsKey(
+                SuccessfulHandlerMetaDataPopulator.SUCCESSFUL_AUTHENTICATION_HANDLERS));
     }
 
     private static UsernamePasswordCredentials newUserPassCredentials(final String user, final String pass) {
