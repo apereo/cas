cc620f7f28191d299e912be750c6878d08e3dbae
Stefan Paetow <stefan.paetow@diamond.ac.uk>
2013-09-17 15:50:01 +0100

    Fix glitch in the innerProtocol specified for EAP_TTLS_MD5.

--- cas-server-support-radius/src/main/java/org/jasig/cas/adaptors/radius/RadiusProtocol.java
+++ cas-server-support-radius/src/main/java/org/jasig/cas/adaptors/radius/RadiusProtocol.java
@@ -30,7 +30,7 @@ public enum RadiusProtocol {
     EAP_MSCHAPv2("eap-mschapv2"),
     EAP_TLS("eap-tls"),
     EAP_TTLS_PAP("eap-ttls:innerProtocol=pap"),
-    EAP_TTLS_MD5("eap-ttls:innerProtocol=md5"),
+    EAP_TTLS_MD5("eap-ttls:innerProtocol=eap-md5"),
     EAP_TTLS_EAP_MSCHAPv2("eap-ttls:innerProtocol=eap-mschapv2"),
     MSCHAPv1("mschapv1"),
     MSCHAPv2("mschapv2"),
