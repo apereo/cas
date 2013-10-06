59329a117d38c416da64f4380da516d2269dad61
Art O Cathain <art.home@gmail.com>
2013-10-06 09:56:00 +0100

    speed up unit tests by reducing timeouts

--- cas-server-core/src/test/java/org/jasig/cas/ticket/support/TicketGrantingTicketExpirationPolicyTests.java
+++ cas-server-core/src/test/java/org/jasig/cas/ticket/support/TicketGrantingTicketExpirationPolicyTests.java
@@ -32,13 +32,11 @@ import org.junit.Test;
  */
 public class TicketGrantingTicketExpirationPolicyTests {
 
-    private static final long HARD_TIMEOUT = 10000L; // 10s
+    private static final long HARD_TIMEOUT = 100L;
 
-    private static final long SLIDING_TIMEOUT = 2000L; // 2s
+    private static final long SLIDING_TIMEOUT = 20L; 
 
-    private static final long HARD_TIMEOUT_SECONDS = 10L;
-
-    private static final long SLIDING_TIMEOUT_SECONDS = 2L;
+    private static final long TIMEOUT_BUFFER = 5L; // needs to be long enough for timeouts to be triggered
 
     private TicketGrantingTicketExpirationPolicy expirationPolicy;
 
@@ -47,51 +45,43 @@ public class TicketGrantingTicketExpirationPolicyTests {
     @Before
     public void setUp() throws Exception {
         this.expirationPolicy = new TicketGrantingTicketExpirationPolicy();
-        this.expirationPolicy.setMaxTimeToLiveInSeconds(HARD_TIMEOUT_SECONDS);
-        this.expirationPolicy.setTimeToKillInSeconds(SLIDING_TIMEOUT_SECONDS);
+        this.expirationPolicy.setMaxTimeToLiveInMilliSeconds(HARD_TIMEOUT);
+        this.expirationPolicy.setTimeToKillInMilliSeconds(SLIDING_TIMEOUT);
         this.ticketGrantingTicket = new TicketGrantingTicketImpl("test", TestUtils.getAuthentication(),
                 this.expirationPolicy);
     }
 
     @Test
     public void testTgtIsExpiredByHardTimeOut() throws InterruptedException {
-     try {
          // keep tgt alive via sliding window until within SLIDING_TIME / 2 of the HARD_TIMEOUT
          while (System.currentTimeMillis() - ticketGrantingTicket.getCreationTime()
                  < (HARD_TIMEOUT - SLIDING_TIMEOUT / 2)) {
              ticketGrantingTicket.grantServiceTicket("test", TestUtils.getService(), expirationPolicy, false);
-             Thread.sleep(SLIDING_TIMEOUT - 100);
+             Thread.sleep(SLIDING_TIMEOUT - TIMEOUT_BUFFER);
              assertFalse(this.ticketGrantingTicket.isExpired());
          }
 
          // final sliding window extension past the HARD_TIMEOUT
          ticketGrantingTicket.grantServiceTicket("test", TestUtils.getService(), expirationPolicy, false);
-         Thread.sleep(SLIDING_TIMEOUT / 2 + 100);
+         Thread.sleep(SLIDING_TIMEOUT / 2 + TIMEOUT_BUFFER);
          assertTrue(ticketGrantingTicket.isExpired());
 
-     } catch (final InterruptedException e) {
-         throw e;
-     }
     }
 
     @Test
     public void testTgtIsExpiredBySlidingWindow() throws InterruptedException {
-        try {
-            ticketGrantingTicket.grantServiceTicket("test", TestUtils.getService(), expirationPolicy, false);
-            Thread.sleep(SLIDING_TIMEOUT - 100);
-            assertFalse(ticketGrantingTicket.isExpired());
+        ticketGrantingTicket.grantServiceTicket("test", TestUtils.getService(), expirationPolicy, false);
+        Thread.sleep(SLIDING_TIMEOUT - TIMEOUT_BUFFER);
+        assertFalse(ticketGrantingTicket.isExpired());
 
-            ticketGrantingTicket.grantServiceTicket("test", TestUtils.getService(), expirationPolicy, false);
-            Thread.sleep(SLIDING_TIMEOUT - 100);
-            assertFalse(ticketGrantingTicket.isExpired());
+        ticketGrantingTicket.grantServiceTicket("test", TestUtils.getService(), expirationPolicy, false);
+        Thread.sleep(SLIDING_TIMEOUT - TIMEOUT_BUFFER);
+        assertFalse(ticketGrantingTicket.isExpired());
 
-            ticketGrantingTicket.grantServiceTicket("test", TestUtils.getService(), expirationPolicy, false);
-            Thread.sleep(SLIDING_TIMEOUT + 100);
-            assertTrue(ticketGrantingTicket.isExpired());
+        ticketGrantingTicket.grantServiceTicket("test", TestUtils.getService(), expirationPolicy, false);
+        Thread.sleep(SLIDING_TIMEOUT + TIMEOUT_BUFFER);
+        assertTrue(ticketGrantingTicket.isExpired());
 
-        } catch (final InterruptedException e) {
-            throw e;
-        }
     }
 
 }
