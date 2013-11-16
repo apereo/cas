7cd2ac1f7f462b00b72d0838de0fbdb095be4b8d
Misagh Moayyed <mmoayyed@unicon.net>
2013-11-15 21:47:09 -0700

    CAS-1374: Moved the changes into a ISOStandardDateFormatUtils

---
+++ cas-server-core/src/main/java/org/jasig/cas/util/ISOStandardDateFormatUtils.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License.  You may obtain a
+ * copy of the License at the following location:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jasig.cas.util;
+
+import java.util.Date;
+
+import org.apache.commons.lang.time.FastDateFormat;
+
+/**
+ * A fast date format based on the ISO-8601 standard.
+ * @author Misagh Moayyed
+ */
+public final class ISOStandardDateFormatUtils extends FastDateFormat {
+
+    private static final long serialVersionUID = 9196017562782775535L;
+
+    private static ISOStandardDateFormatUtils INSTANCE = null;
+    
+    private ISOStandardDateFormatUtils() {
+        super("yyyy-MM-dd'T'HH:mm:ss'Z'", null, null);
+        init();
+    }
+
+    public static ISOStandardDateFormatUtils getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new ISOStandardDateFormatUtils();
+        }
+        return INSTANCE;
+    }
+    
+    public String getCurrentDateAndTime() {
+        return getInstance().format(new Date());
+    }
+}
