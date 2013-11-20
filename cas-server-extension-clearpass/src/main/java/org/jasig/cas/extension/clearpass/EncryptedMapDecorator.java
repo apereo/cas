8c4c641066e377d0afd297920332cd233ba34339
rdev5 <rdev5@borjawebs.com>
2013-11-20 16:28:55 -0700

    Update EncryptedMapDecorator.java

--- cas-server-extension-clearpass/src/main/java/org/jasig/cas/extension/clearpass/EncryptedMapDecorator.java
+++ cas-server-extension-clearpass/src/main/java/org/jasig/cas/extension/clearpass/EncryptedMapDecorator.java
@@ -28,6 +28,7 @@ import java.security.spec.KeySpec;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
+import java.util.Arrays;
 import java.util.concurrent.ConcurrentHashMap;
 
 import javax.crypto.Cipher;
