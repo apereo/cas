0d5c76ac02d5e1b702625a059f0658472173836a
Misagh Moayyed <mmoayyed@unicon.net>
2013-12-05 04:23:14 -0700

    JAVADOC: Added missing javadocs.

--- cas-server-core/src/main/java/org/jasig/cas/remoting/server/RemoteCentralAuthenticationService.java
+++ cas-server-core/src/main/java/org/jasig/cas/remoting/server/RemoteCentralAuthenticationService.java
@@ -135,6 +135,11 @@ public final class RemoteCentralAuthenticationService implements CentralAuthenti
         return this.centralAuthenticationService.delegateTicketGrantingTicket(serviceTicketId, credentials);
     }
 
+    /**
+     * Check for errors by asking the validator to review each credential.
+     *
+     * @param credentials the credentials
+     */
     private void checkForErrors(final Credential... credentials) {
         if (credentials == null) {
             return;
