8fd6042b872e4441b03f0ef023175589d6c4a903
Misagh Moayyed <mmoayyed@unicon.net>
2013-12-07 02:22:16 -0700

    JAVADOC: Added missing javadocs.

--- cas-server-support-legacy/src/main/java/org/jasig/cas/authentication/CredentialsAdapter.java
+++ cas-server-support-legacy/src/main/java/org/jasig/cas/authentication/CredentialsAdapter.java
@@ -27,9 +27,11 @@ import org.jasig.cas.authentication.principal.Credentials;
  * @since 4.0
  */
 public interface CredentialsAdapter {
+    
     /**
      * Converts a CAS 4.0 credential to a CAS 3.0 credential.
      *
+     * @param credential the credential
      * @return CAS 3.0 credential.
      */
     Credentials convert(Credential credential);
