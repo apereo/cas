bfc498d1e19673ce6c5504ba682e11960a429450
Misagh Moayyed <mm1844@gmail.com>
2013-12-09 03:25:19 -0700

    CAS-1296: Added logic for a registered service to invoke the policy

--- cas-server-core/src/test/java/org/jasig/cas/services/JpaServiceRegistryDaoImplTests.java
+++ cas-server-core/src/test/java/org/jasig/cas/services/JpaServiceRegistryDaoImplTests.java
@@ -18,8 +18,6 @@
  */
 package org.jasig.cas.services;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
@@ -56,24 +54,6 @@ public class JpaServiceRegistryDaoImplTests extends AbstractTransactionalJUnit4S
     }
 
     @Test
-    public void testSaveMethodWithNonExistentServiceAndAttributes() {
-        final RegisteredServiceImpl r = new RegisteredServiceImpl();
-        r.setName("test");
-        r.setServiceId("testId");
-        r.setTheme("theme");
-        r.setDescription("description");
-        r.setAllowedAttributes(Arrays.asList("Test"));
-
-        final RegisteredService r2 = this.dao.save(r);
-        final RegisteredService r3 = this.dao.findServiceById(r2.getId());
-
-        assertEquals(r, r2);
-        assertEquals(r2, r3);
-
-        assertTrue(r.getAllowedAttributes().contains("Test"));
-    }
-
-    @Test
     public void testSaveMethodWithExistingServiceNoAttribute() {
         final RegisteredServiceImpl r = new RegisteredServiceImpl();
         r.setName("test");
@@ -98,36 +78,4 @@ public class JpaServiceRegistryDaoImplTests extends AbstractTransactionalJUnit4S
         assertEquals(r.getTheme(), r3.getTheme());
     }
 
-    @Test
-    public void testSaveMethodWithExistingServiceAddAttribute() {
-        final RegisteredServiceImpl r = new RegisteredServiceImpl();
-        r.setName("test");
-        r.setServiceId("testId");
-        r.setTheme("theme");
-        r.setDescription("description");
-
-        this.dao.save(r);
-
-        final List<RegisteredService> services = this.dao.load();
-
-        final RegisteredService r2 = services.get(0);
-
-        r.setId(r2.getId());
-        r.setTheme("mytheme");
-        r.setAllowedAttributes(Arrays.asList("Test"));
-
-        this.dao.save(r);
-
-        final RegisteredService r3 = this.dao.findServiceById(r.getId());
-
-        assertEquals(r, r2);
-        assertEquals(r.getTheme(), r3.getTheme());
-
-        assertTrue(r3.getAllowedAttributes().contains("Test"));
-
-        r.setAllowedAttributes(new ArrayList<String>());
-        this.dao.save(r);
-        final RegisteredService r4 = this.dao.findServiceById(r.getId());
-        assertTrue(r4.getAllowedAttributes().isEmpty());
-    }
 }
