774bd4b140c9b0ec321aa15a9389498c1472fc3a
Misagh Moayyed <mmoayyed@unicon.net>
2013-12-24 09:03:42 -0700

    CAS-1406: Fixed test cases.

--- cas-server-core/src/test/java/org/jasig/cas/logout/SamlCompliantLogoutMessageBuilderTests.java
+++ cas-server-core/src/test/java/org/jasig/cas/logout/SamlCompliantLogoutMessageBuilderTests.java
@@ -1,22 +1,43 @@
 package org.jasig.cas.logout;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 import org.jasig.cas.authentication.principal.SingleLogoutService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
 import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
 
 @RunWith(JUnit4.class)
 public class SamlCompliantLogoutMessageBuilderTests {
-    
+
     private final LogoutMessageBuilder builder = new SamlCompliantLogoutMessageBuilder();
+
     @Test
-    public void testMessageBuilding() {
-        
+    public void testMessageBuilding() throws Exception {
+
         final SingleLogoutService service = mock(SingleLogoutService.class);
         final LogoutRequest request = new LogoutRequest("TICKET-ID", service);
+
+        final String msg = builder.build(request);
+
+        final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        final DocumentBuilder builder = factory.newDocumentBuilder();
+
+        final InputStream is = new ByteArrayInputStream(msg.getBytes());
+        final Document document = builder.parse(is);
         
-        builder.build(request);
+        final NodeList list = document.getDocumentElement().getElementsByTagName("samlp:SessionIndex");
+        assertEquals(list.getLength(), 1);
+        
+        assertEquals(list.item(0).getTextContent(), request.getTicketId());
     }
 }
