eee0bc99864cc494c94b05c6b3fa677f6c533571
Misagh Moayyed <mm1844@gmail.com>
2013-12-25 14:31:33 -0700

    CAS-1406: Fixed test cases.

--- cas-server-webapp-support/src/test/java/org/jasig/cas/web/flow/FrontChannelLogoutActionTests.java
+++ cas-server-webapp-support/src/test/java/org/jasig/cas/web/flow/FrontChannelLogoutActionTests.java
@@ -36,6 +36,7 @@ import org.jasig.cas.logout.LogoutManager;
 import org.jasig.cas.logout.LogoutManagerImpl;
 import org.jasig.cas.logout.LogoutRequest;
 import org.jasig.cas.logout.LogoutRequestStatus;
+import org.jasig.cas.logout.SamlCompliantLogoutMessageBuilder;
 import org.jasig.cas.services.ServicesManager;
 import org.jasig.cas.util.SimpleHttpClient;
 import org.jasig.cas.web.support.WebUtils;
@@ -70,7 +71,8 @@ public class FrontChannelLogoutActionTests {
 
     @Before
     public void onSetUp() throws Exception {
-        final LogoutManager logoutManager = new LogoutManagerImpl(mock(ServicesManager.class), new SimpleHttpClient());
+        final LogoutManager logoutManager = new LogoutManagerImpl(mock(ServicesManager.class),
+                new SimpleHttpClient(), new SamlCompliantLogoutMessageBuilder());
         this.frontChannelLogoutAction = new FrontChannelLogoutAction(logoutManager);
 
         this.request = new MockHttpServletRequest();
@@ -112,7 +114,6 @@ public class FrontChannelLogoutActionTests {
         assertEquals(FrontChannelLogoutAction.FINISH_EVENT, event.getId());
     }
 
-    @SuppressWarnings("unchecked")
     @Test
     public void testLogoutOneLogoutRequestNotAttempted() throws Exception {
         final String FAKE_URL = "http://url";
