3322874cd3346f8c03e6a531a30f54dd1db49712
Misagh Moayyed <mmoayyed@unicon.net>
2013-12-27 04:31:20 -0700

    CAS-1406: Renamed the class and others to be and point to LogoutMessageCreator

--- cas-server-core/src/test/java/org/jasig/cas/logout/LogoutManagerImplTests.java
+++ cas-server-core/src/test/java/org/jasig/cas/logout/LogoutManagerImplTests.java
@@ -59,7 +59,7 @@ public class LogoutManagerImplTests {
     @Before
     public void setUp() {
         final ServicesManager servicesManager = mock(ServicesManager.class);
-        this.logoutManager = new LogoutManagerImpl(servicesManager, new SimpleHttpClient(), new SamlCompliantLogoutMessageBuilder());
+        this.logoutManager = new LogoutManagerImpl(servicesManager, new SimpleHttpClient(), new SamlCompliantLogoutMessageCreator());
         this.tgt = mock(TicketGrantingTicket.class);
         this.services = new HashMap<String, Service>();
         this.simpleWebApplicationServiceImpl = new SimpleWebApplicationServiceImpl(URL);
