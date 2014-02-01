bde9349ad24fd4f5397fb118019db3c45a3cffdb
Misagh Moayyed <mmoayyed@unicon.net>
2014-02-01 02:07:19 -0700

    reorganized pages to match by version

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,22 @@
   $(function(){
+  	  var currentVersion = "current";
+  	  var href = location.href;
+      var index = href.indexOf("/_site/");
+
+      if (index == -1) {
+  	  	var uri = new URI(document.location);
+  	  	currentVersion = uri.segment(0);
+  	  } else {
+        href = href.substring(index + 7);
+        index = href.indexOf("/");
+        
+        currentVersion = href.substring(0, index);
+      }
+
       $('a').each(function() {
           var href = this.href;
           if (href.indexOf("$version") != -1) {
-            href = href.replace("$version", "current");
+            href = href.replace("$version", currentVersion);
             $(this).attr('href', href);
           }
       });
