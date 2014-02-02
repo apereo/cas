21b66455ddcc60dd815062c0232293af267a4c08
Misagh Moayyed <mmoayyed@unicon.net>
2014-02-02 13:11:48 -0700

    fixed sidebar links

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,25 +1,3 @@
   $(function(){
-  	  var currentVersion = "current";
-  	  var href = location.href;
-      var index = href.indexOf("/_site/");
-
-      if (index == -1) {
-  	  	var uri = new URI(document.location);
-  	  	currentVersion = uri.segment(1);
-  	  } else {
-        href = href.substring(index + 7);
-        index = href.indexOf("/");
-        
-        currentVersion = href.substring(0, index);
-      }
-
-      document.title = document.title + " - Version " + currentVersion;
-      
-      $('a').each(function() {
-          var href = this.href;
-          if (href.indexOf("$version") != -1) {
-            href = href.replace("$version", currentVersion);
-            $(this).attr('href', href);
-          }
-      });
+  	  
   });
