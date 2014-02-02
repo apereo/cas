982a1dc12a5160333108111a578812c49bce37b2
Misagh Moayyed <mmoayyed@unicon.net>
2014-02-02 13:37:26 -0700

    testing dynamic sidebars in child pages

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,23 @@
-  $(function(){
+ 
+$(function(){
+  	  var currentVersion = "current";
+  	  var href = location.href;
+      var index = href.indexOf("/_site/");
+
+      if (index == -1) {
+  	  	var uri = new URI(document.location);
+  	  	currentVersion = uri.segment(1);
+  	  } else {
+        href = href.substring(index + 7);
+        index = href.indexOf("/");
+        
+        currentVersion = href.substring(0, index);
+      }
+
+      document.title = document.title + " - Version " + currentVersion;
+      
+      $("#sidebartoc").load(currentVersion + "/sidebar.html");
+});
+
   	  
-  });
+
