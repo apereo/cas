91d40ef1b7b1cc9a6b76ea8ce6356f6340e7ce36
Misagh Moayyed <mm1844@gmail.com>
2014-05-24 02:45:41 -0700

    CAS-1296: fixed compile issues; added more test cases

--- cas-server-core/src/test/java/org/jasig/cas/services/AttributeFilteringPolicyTests.java
+++ cas-server-core/src/test/java/org/jasig/cas/services/AttributeFilteringPolicyTests.java
@@ -18,8 +18,7 @@
  */
 package org.jasig.cas.services;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -27,7 +26,9 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.lang.SerializationUtils;
 import org.jasig.cas.authentication.principal.Principal;
+import org.jasig.cas.services.support.RegisteredServiceRegexAttributeFilter;
 import org.junit.Test;
 
 /**
@@ -56,6 +57,11 @@ public class AttributeFilteringPolicyTests {
         final Map<String, Object> attr = policy.getAttributes(p);
         assertEquals(attr.size(), 1);
         assertTrue(attr.containsKey("newAttr1"));
+        
+        final byte[] data = SerializationUtils.serialize(policy);
+        final ReturnMappedAttributeFilteringPolicy p2 = (ReturnMappedAttributeFilteringPolicy) SerializationUtils.deserialize(data);
+        assertNotNull(p2);
+        assertEquals(p2.getAllowedAttributes(), policy.getAllowedAttributes());
     }
     
     @Test
@@ -76,6 +82,37 @@ public class AttributeFilteringPolicyTests {
         assertEquals(attr.size(), 2);
         assertTrue(attr.containsKey("attr1"));
         assertTrue(attr.containsKey("attr3"));
+        
+        final byte[] data = SerializationUtils.serialize(policy);
+        final ReturnAllowedAttributeFilteringPolicy p2 = (ReturnAllowedAttributeFilteringPolicy) SerializationUtils.deserialize(data);
+        assertNotNull(p2);
+        assertEquals(p2.getAllowedAttributes(), policy.getAllowedAttributes());
+    }
+    
+    @Test
+    public void testServiceAttributeFilterAllowedAttributesWithARegexFilter() {
+        final ReturnAllowedAttributeFilteringPolicy policy = new ReturnAllowedAttributeFilteringPolicy();
+        policy.setAllowedAttributes(Arrays.asList("attr1", "attr3", "another"));
+        policy.setAttributeFilter(new RegisteredServiceRegexAttributeFilter("v3"));
+        final Principal p = mock(Principal.class);
+        
+        final Map<String, Object> map = new HashMap<String, Object>();
+        map.put("attr1", "value1");
+        map.put("attr2", "value2");
+        map.put("attr3", Arrays.asList("v3", "v4"));
+        
+        when(p.getAttributes()).thenReturn(map);
+        when(p.getId()).thenReturn("principalId");
+        
+        final Map<String, Object> attr = policy.getAttributes(p);
+        assertEquals(attr.size(), 1);
+        assertTrue(attr.containsKey("attr3"));
+        
+        final byte[] data = SerializationUtils.serialize(policy);
+        final ReturnAllowedAttributeFilteringPolicy p2 = (ReturnAllowedAttributeFilteringPolicy) SerializationUtils.deserialize(data);
+        assertNotNull(p2);
+        assertEquals(p2.getAllowedAttributes(), policy.getAllowedAttributes());
+        assertEquals(p2.getAttributeFilter(), policy.getAttributeFilter());
     }
     
     @Test
@@ -93,5 +130,9 @@ public class AttributeFilteringPolicyTests {
         
         final Map<String, Object> attr = policy.getAttributes(p);
         assertEquals(attr.size(), map.size());
+        
+        final byte[] data = SerializationUtils.serialize(policy);
+        final ReturnAllAttributeFilteringPolicy p2 = (ReturnAllAttributeFilteringPolicy) SerializationUtils.deserialize(data);
+        assertNotNull(p2);
     }
 }
