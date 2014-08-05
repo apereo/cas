8ba6ed1b7cf9623f1ae5cebabe66e47a052fd8b5
Misagh Moayyed <mmoayyed@unicon.net>
2014-08-05 05:15:52 -0700

    Allow for time skewing of the issue instant when validating saml calls

--- cas-server-support-saml/src/main/java/org/jasig/cas/support/saml/web/view/AbstractSaml10ResponseView.java
+++ cas-server-support-saml/src/main/java/org/jasig/cas/support/saml/web/view/AbstractSaml10ResponseView.java
@@ -19,15 +19,6 @@
 
 package org.jasig.cas.support.saml.web.view;
 
-import java.lang.reflect.Field;
-import java.security.NoSuchAlgorithmException;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.constraints.NotNull;
-import javax.xml.namespace.QName;
-
 import org.jasig.cas.authentication.principal.WebApplicationService;
 import org.jasig.cas.support.saml.authentication.principal.SamlService;
 import org.jasig.cas.support.saml.util.CasHTTPSOAP11Encoder;
@@ -48,6 +39,14 @@ import org.opensaml.saml1.core.StatusCode;
 import org.opensaml.saml1.core.StatusMessage;
 import org.opensaml.ws.transport.http.HttpServletResponseAdapter;
 import org.opensaml.xml.ConfigurationException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.constraints.NotNull;
+import javax.xml.namespace.QName;
+import java.lang.reflect.Field;
+import java.security.NoSuchAlgorithmException;
+import java.util.Map;
 
 /**
  * Base class for all views that render SAML1 SOAP messages directly to the HTTP response stream.
@@ -70,6 +69,8 @@ public abstract class AbstractSaml10ResponseView extends AbstractCasView {
     @NotNull
     private String encoding = DEFAULT_ENCODING;
 
+    private int skewAllowance = 0;
+
     /**
      * Sets the character encoding in the HTTP response.
      *
@@ -77,6 +78,19 @@ public abstract class AbstractSaml10ResponseView extends AbstractCasView {
      */
     public void setEncoding(final String encoding) {
         this.encoding = encoding;
+    }
+
+    /**
+    * Sets the allowance for time skew between CAS and the client server.  Default 0s.
+    * This value will be subtracted from the current time when setting the SAML
+    * NotBeforeDate attribute, thereby allowing for the CAS server to be ahead of the
+    * client by as much as the value defined here.
+    *
+    * @param skewAllowance Number of seconds to allow for variance.
+    */
+    public void setSkewAllowance(final int skewAllowance) {
+        logger.debug("Using {} seconds as skew allowance.", skewAllowance);
+        this.skewAllowance = skewAllowance;
     }
 
     static {
@@ -112,7 +126,7 @@ public abstract class AbstractSaml10ResponseView extends AbstractCasView {
         try {
             final Response samlResponse = newSamlObject(Response.class);
             samlResponse.setID(generateId());
-            samlResponse.setIssueInstant(new DateTime());
+            samlResponse.setIssueInstant(DateTime.now().minusSeconds(skewAllowance));
             samlResponse.setVersion(SAMLVersion.VERSION_11);
             samlResponse.setRecipient(serviceId);
             if (service instanceof SamlService) {
