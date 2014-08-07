dcf27b036a2f6a866b1e9525e07648c1730ee518
Misagh Moayyed <mm1844@gmail.com>
2014-08-07 03:26:27 -0700

    updated ctor with more deprecation details

--- cas-server-core/src/main/java/org/jasig/cas/services/DefaultServicesManagerImpl.java
+++ cas-server-core/src/main/java/org/jasig/cas/services/DefaultServicesManagerImpl.java
@@ -66,7 +66,9 @@ public final class DefaultServicesManagerImpl implements ReloadableServicesManag
      * Constructs an instance of the {@link DefaultServicesManagerImpl} where the default RegisteredService
      * can include a set of default attributes to use if no services are defined in the registry.
      * @deprecated As of 4.1. Use {@link org.jasig.cas.services.DefaultServicesManagerImpl(ServiceRegistryDao)}
-     * instead. The <code>defaultAttributes</code> parameter is no longer used.
+     * instead. The <code>defaultAttributes</code> parameter is no longer used. Attributes are configured
+     * per service definition in the services registry. See {@link RegisteredService#getAttributeReleasePolicy()}
+     * for more details.
      *
      * @param serviceRegistryDao the Service Registry Dao.
      * @param defaultAttributes the list of default attributes to use.
