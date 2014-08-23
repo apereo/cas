63f537d6f1c73441e7b63afedf2b78e688ba1201
Misagh Moayyed <mm1844@gmail.com>
2014-08-23 11:54:52 -0700

    non-caching principal attributes based on factories

---
+++ cas-server-core/src/main/java/org/jasig/cas/authentication/principal/UncachedAttributesPrincipal.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License.  You may obtain a
+ * copy of the License at the following location:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.jasig.cas.authentication.principal;
+
+import org.jasig.cas.util.PrincipalUtils;
+import org.jasig.services.persondir.IPersonAttributeDao;
+
+import javax.validation.constraints.NotNull;
+import java.util.Map;
+
+/**
+ * A {@link org.jasig.cas.authentication.principal.Principal} whose attributes are not cached.
+ * Every time attributes are requested, the requested is submitted to the underlying
+ * attribute repository to get a fresh copy.
+ * @author Misagh Moayyed
+ * @since 4.1
+ */
+public final class UncachedAttributesPrincipal extends SimplePrincipal {
+
+    private final IPersonAttributeDao attributeRepository;
+
+    /**
+     * Instantiates a new Uncached attributes principal.
+     *
+     * @param id the id
+     * @param attributes the attributes
+     * @param attributeRepository the attribute repository
+     */
+    public UncachedAttributesPrincipal(@NotNull final String id, @NotNull final Map<String, Object> attributes,
+                                       @NotNull final IPersonAttributeDao attributeRepository) {
+        super(id, attributes);
+        this.attributeRepository = attributeRepository;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return PrincipalUtils.convertPersonAttributesToPrincipalAttributes(getId(), this.attributeRepository);
+    }
+}
