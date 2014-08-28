4e3586edefc2dbbc680ac483509680b1715d60d0
Misagh Moayyed <mm1844@gmail.com>
2014-08-27 23:57:35 -0700

    fixed compile issues by the merge

--- cas-server-core/src/main/java/org/jasig/cas/services/PrincipalAttributeRegisteredServiceUsernameProvider.java
+++ cas-server-core/src/main/java/org/jasig/cas/services/PrincipalAttributeRegisteredServiceUsernameProvider.java
@@ -18,8 +18,8 @@
  */
 package org.jasig.cas.services;
 
-import org.apache.commons.lang.builder.ToStringBuilder;
-import org.apache.commons.lang.builder.ToStringStyle;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import org.jasig.cas.authentication.principal.Principal;
 import org.jasig.cas.authentication.principal.Service;
 import org.slf4j.Logger;
