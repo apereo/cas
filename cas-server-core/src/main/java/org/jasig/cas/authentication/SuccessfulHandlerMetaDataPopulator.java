12c2094baf4f8d2db0561e924aa55a9f61ca2af3
Alaa Mohsen <alaa.mohsen@mubasher.info>
2014-09-28 13:06:45 +0200

    fix Issue #709

--- cas-server-core/src/main/java/org/jasig/cas/authentication/SuccessfulHandlerMetaDataPopulator.java
+++ cas-server-core/src/main/java/org/jasig/cas/authentication/SuccessfulHandlerMetaDataPopulator.java
@@ -18,6 +18,8 @@
  */
 package org.jasig.cas.authentication;
 
+import java.util.HashSet;
+
 /**
  * Sets an authentication attribute containing the collection of authentication handlers (by name) that successfully
  * authenticated credential. The attribute name is given by {@link #SUCCESSFUL_AUTHENTICATION_HANDLERS}.
@@ -25,6 +27,7 @@ package org.jasig.cas.authentication;
  * response to support level of assurance and MFA use cases.
  *
  * @author Marvin S. Addison
+ * @author Alaa Nassef
  * @since 4.0
  */
 public class SuccessfulHandlerMetaDataPopulator implements AuthenticationMetaDataPopulator {
@@ -33,6 +36,6 @@ public class SuccessfulHandlerMetaDataPopulator implements AuthenticationMetaDat
 
     @Override
     public void populateAttributes(final AuthenticationBuilder builder, final Credential credential) {
-        builder.addAttribute(SUCCESSFUL_AUTHENTICATION_HANDLERS, builder.getSuccesses().keySet());
+        builder.addAttribute(SUCCESSFUL_AUTHENTICATION_HANDLERS, new HashSet(builder.getSuccesses().keySet()));
     }
 }
