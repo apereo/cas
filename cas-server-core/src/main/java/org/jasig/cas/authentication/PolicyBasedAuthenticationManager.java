5394f370461f12e8e2bd441ac79474db3d785944
Misagh Moayyed <mm1844@gmail.com>
2014-10-11 00:31:04 -0700

    NullPrincipal is turned into a singleton

--- cas-server-core/src/main/java/org/jasig/cas/authentication/PolicyBasedAuthenticationManager.java
+++ cas-server-core/src/main/java/org/jasig/cas/authentication/PolicyBasedAuthenticationManager.java
@@ -18,22 +18,22 @@
  */
 package org.jasig.cas.authentication;
 
+import com.github.inspektr.audit.annotation.Audit;
+import org.jasig.cas.authentication.principal.NullPrincipal;
+import org.jasig.cas.authentication.principal.Principal;
+import org.jasig.cas.authentication.principal.PrincipalResolver;
+import org.perf4j.aop.Profiled;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.Assert;
+
+import javax.validation.constraints.NotNull;
 import java.security.GeneralSecurityException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import javax.validation.constraints.NotNull;
-
-import com.github.inspektr.audit.annotation.Audit;
-import org.jasig.cas.authentication.principal.NullPrincipal;
-import org.jasig.cas.authentication.principal.PrincipalResolver;
-import org.jasig.cas.authentication.principal.Principal;
-import org.perf4j.aop.Profiled;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.util.Assert;
 
 /**
  * Provides an authenticaiton manager that is inherently aware of multiple credentials and supports pluggable
@@ -70,9 +70,6 @@ import org.springframework.util.Assert;
  * @since 4.0
  */
 public class PolicyBasedAuthenticationManager implements AuthenticationManager {
-
-    /** Default principal implementation that allows us to create {@link Authentication}s (principal cannot be null). */
-    private static final Principal NULL_PRINCIPAL = new NullPrincipal();
 
     /** Log instance for logging events, errors, warnings, etc. */
     protected final Logger logger = LoggerFactory.getLogger(getClass());
@@ -195,7 +192,7 @@ public class PolicyBasedAuthenticationManager implements AuthenticationManager {
     protected AuthenticationBuilder authenticateInternal(final Credential... credentials)
             throws AuthenticationException {
 
-        final AuthenticationBuilder builder = new AuthenticationBuilder(NULL_PRINCIPAL);
+        final AuthenticationBuilder builder = new AuthenticationBuilder(NullPrincipal.getInstance());
         for (final Credential c : credentials) {
             builder.addCredential(new BasicCredentialMetaData(c));
         }
