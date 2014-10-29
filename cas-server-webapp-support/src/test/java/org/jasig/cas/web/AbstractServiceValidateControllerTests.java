968fef70cc98c3ee819b694d123d78e7345264aa
Misagh Moayyed <mm1844@gmail.com>
2014-10-29 01:59:06 -0700

    fixed compile issue

--- cas-server-webapp-support/src/test/java/org/jasig/cas/web/AbstractServiceValidateControllerTests.java
+++ cas-server-webapp-support/src/test/java/org/jasig/cas/web/AbstractServiceValidateControllerTests.java
@@ -290,13 +290,13 @@ public abstract class AbstractServiceValidateControllerTests extends AbstractCen
     }
 
     protected final ModelAndView getModelAndViewUponServiceValidationWithSecurePgtUrl() throws Exception {
-        final String tId = getCentralAuthenticationService()
+        final TicketGrantingTicket tId = getCentralAuthenticationService()
                 .createTicketGrantingTicket(TestUtils.getCredentialsWithSameUsernameAndPassword());
-        final String sId = getCentralAuthenticationService().grantServiceTicket(tId, TestUtils.getService());
+        final ServiceTicket sId = getCentralAuthenticationService().grantServiceTicket(tId.getId(), TestUtils.getService());
 
         final MockHttpServletRequest request = new MockHttpServletRequest();
         request.addParameter("service", TestUtils.getService().getId());
-        request.addParameter("ticket", sId);
+        request.addParameter("ticket", sId.getId());
         request.addParameter("pgtUrl", "https://www.github.com");
 
 
