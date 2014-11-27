1e31125d1b8bed6657f67a6721d9914363479c8c
Misagh Moayyed <mm1844@gmail.com>
2014-11-26 19:35:31 -0700

    added javadoc comments

--- cas-server-support-ldap/src/test/java/org/jasig/cas/adaptors/ldap/AbstractLdapTests.java
+++ cas-server-support-ldap/src/test/java/org/jasig/cas/adaptors/ldap/AbstractLdapTests.java
@@ -25,7 +25,6 @@ import org.junit.BeforeClass;
 import org.ldaptive.LdapEntry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.core.io.ClassPathResource;
