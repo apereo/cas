06cb3cd2d7eed1c7b5850771b26bce0315de0e6b
Misagh Moayyed <mmoayyed@unicon.net>
2015-01-06 11:15:30 -0700

    Using default ticketid generator if one is not specified.

--- cas-server-core/src/main/java/org/jasig/cas/util/UniqueTicketIdGenerator.java
+++ cas-server-core/src/main/java/org/jasig/cas/util/UniqueTicketIdGenerator.java
@@ -22,14 +22,13 @@ package org.jasig.cas.util;
  * Interface that enables for pluggable unique ticket Ids strategies.
  *
  * @author Scott Battaglia
-
  * @since 3.0.0
- * <p>
- * This is a published and supported CAS Server 3 API.
- * </p>
  */
 public interface UniqueTicketIdGenerator {
-
+    /**
+     * Default instance for the ticket id generator.
+     */
+    UniqueTicketIdGenerator DEFAULT = new DefaultUniqueTicketIdGenerator();
     /**
      * Return a new unique ticket id beginning with the prefix.
      *
