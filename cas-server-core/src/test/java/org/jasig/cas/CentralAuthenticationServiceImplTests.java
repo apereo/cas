0a7325cb71093ab5e9c7da188c85d87f049f3f14
Misagh Moayyed <mm1844@gmail.com>
2015-01-06 11:19:42 -0700

    added check to unit test

--- cas-server-core/src/test/java/org/jasig/cas/CentralAuthenticationServiceImplTests.java
+++ cas-server-core/src/test/java/org/jasig/cas/CentralAuthenticationServiceImplTests.java
@@ -102,6 +102,7 @@ public class CentralAuthenticationServiceImplTests extends AbstractCentralAuthen
                         TestUtils.getCredentialsWithSameUsernameAndPassword());
         final ServiceTicket serviceTicketId = getCentralAuthenticationService()
                 .grantServiceTicket(ticketId.getId(), mockService);
+        assertNotNull(serviceTicketId);
     }
 
     @Test
