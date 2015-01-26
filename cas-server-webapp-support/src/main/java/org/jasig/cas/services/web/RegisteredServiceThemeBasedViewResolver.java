aad9e292a68ba596e16b875021fc1f070d2eaffc
Misagh Moayyed <mmoayyed@unicon.net>
2015-01-26 22:31:23 +0330

    used "access" rather than "authz". changed interface method to reflect behavior.

--- cas-server-webapp-support/src/main/java/org/jasig/cas/services/web/RegisteredServiceThemeBasedViewResolver.java
+++ cas-server-webapp-support/src/main/java/org/jasig/cas/services/web/RegisteredServiceThemeBasedViewResolver.java
@@ -95,7 +95,7 @@ public final class RegisteredServiceThemeBasedViewResolver extends InternalResou
         final RegisteredService registeredService = this.servicesManager.findServiceBy(service);
 
         final String themeId = service != null && registeredService != null
-                && registeredService.getAuthorizationStrategy().isServiceAuthorized()
+                && registeredService.getAuthorizationStrategy().isServiceAccessAllowed()
                 && StringUtils.hasText(registeredService.getTheme()) ? registeredService.getTheme() : defaultThemeId;
 
         final String themePrefix = String.format("%s/%s/ui/", pathPrefix, themeId);
