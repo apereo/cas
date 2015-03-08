5ebb368f84ba60d607f2dc806ac2f085d7ab8a46
Misagh Moayyed <mm1844@gmail.com>
2015-03-08 15:51:41 +0330

    allowed extension possibility for alternate implies

--- cas-server-core/src/main/java/org/jasig/cas/util/services/RegisteredServicePublicKeyCipherExecutor.java
+++ cas-server-core/src/main/java/org/jasig/cas/util/services/RegisteredServicePublicKeyCipherExecutor.java
@@ -34,7 +34,7 @@ import java.security.PublicKey;
  * @author Misagh Moayyed
  * @since 4.1
  */
-public final class RegisteredServicePublicKeyCipherExecutor implements CipherExecutor {
+public class RegisteredServicePublicKeyCipherExecutor implements CipherExecutor {
     private static final String UTF8_ENCODING = "UTF-8";
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RegisteredServicePublicKeyCipherExecutor.class);
@@ -56,12 +56,12 @@ public final class RegisteredServicePublicKeyCipherExecutor implements CipherExe
      * @param data the data
      * @return the encoded piece of data in base64
      */
-    public String encode(final String data) {
+    public final String encode(final String data) {
         try {
-            final Cipher cipher = initializeCipherBasedOnServicePublicKey();
-            if (cipher != null) {
-                final byte[] cipherData = cipher.doFinal(data.getBytes(UTF8_ENCODING));
-                return CompressionUtils.encodeBase64(cipherData);
+            final PublicKey publicKey = createRegisteredServicePublicKey();
+            final byte[] result = encodeInternal(data, publicKey);
+            if (result != null) {
+                return CompressionUtils.encodeBase64(result);
             }
         } catch (final Exception e) {
             throw new RuntimeException(e);
@@ -70,25 +70,58 @@ public final class RegisteredServicePublicKeyCipherExecutor implements CipherExe
     }
 
     /**
+     * Encode internally, meant to be called by extensions.
+     * Default behavior will encode the data based on the
+     * registered service public's alg using {@link javax.crypto.Cipher}.
+     *
+     * @param data the data
+     * @param publicKey the public key
+     * @return the byte [ ]
+     */
+    protected byte[] encodeInternal(final String data, final PublicKey publicKey) {
+        try {
+            final Cipher cipher = initializeCipherBasedOnServicePublicKey(publicKey);
+            if (cipher != null) {
+                return cipher.doFinal(data.getBytes(UTF8_ENCODING));
+            }
+        } catch (final Exception e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    /**
+     * Create registered service public key defined.
+     *
+     * @return the public key
+     * @throws Exception the exception, if key cant be created
+     */
+    private PublicKey createRegisteredServicePublicKey() throws Exception {
+        if (this.registeredService.getPublicKey() == null) {
+            LOGGER.debug("No public key is defined for service [{}]. No encoding will take place.",
+                    this.registeredService);
+            return null;
+        }
+        final PublicKey publicKey = this.registeredService.getPublicKey().createInstance();
+        if (publicKey == null) {
+            LOGGER.debug("No public key instance created for service [{}]. No encoding will take place.",
+                    this.registeredService);
+            return null;
+        }
+        return publicKey;
+    }
+
+    /**
      * Initialize cipher based on service public key.
      *
+     * @param publicKey the public key
      * @return the false if no public key is found
      * or if cipher cannot be initialized, etc.
      */
-    private Cipher initializeCipherBasedOnServicePublicKey() {
+    private Cipher initializeCipherBasedOnServicePublicKey(final PublicKey publicKey) {
         try {
-            if (this.registeredService.getPublicKey() == null) {
-                LOGGER.debug("No public key is defined for service [{}]. No encoding will take place.",
-                        this.registeredService);
-                return null;
-            }
-            final PublicKey publicKey = this.registeredService.getPublicKey().createInstance();
-            if (publicKey == null) {
-                LOGGER.debug("No public key instance created for service [{}]. No encoding will take place.",
-                        this.registeredService);
-                return null;
-            }
-            LOGGER.debug("Using public key [{}] to initialize the cipher", this.registeredService.getPublicKey());
+            LOGGER.debug("Using public key [{}] to initialize the cipher",
+                    this.registeredService.getPublicKey());
 
             final Cipher cipher = Cipher.getInstance(publicKey.getAlgorithm());
             cipher.init(Cipher.ENCRYPT_MODE, publicKey);
