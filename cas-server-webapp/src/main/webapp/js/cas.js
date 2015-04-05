49992e63fb5e6527b6b9627dc67a67ec7fa4f815
Misagh Moayyed <mm1844@gmail.com>
2015-04-05 03:40:20 -0700

    used a jquery plugin for cookie ops

--- cas-server-webapp/src/main/webapp/js/cas.js
+++ cas-server-webapp/src/main/webapp/js/cas.js
@@ -19,6 +19,7 @@
 
 var scripts = [ "https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js",
     "https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js",
     "https://rawgithub.com/cowboy/javascript-debug/master/ba-debug.min.js"];
 
 head.ready(document, function() {
@@ -26,11 +27,22 @@ head.ready(document, function() {
 });
 
 
+function areCookiesEnabled() {
+    $.cookie('cookiesEnabled', 'true');
+    var value = $.cookie('cookiesEnabled');
+    if (value != undefined) {
+        $.removeCookie('cookiesEnabled');
+        return true;
+    }
+    return false;
+}
+
 function resourceLoadedSuccessfully() {
     $(document).ready(function() {
         if ($(":focus").length === 0){
             $("input:visible:enabled:first").focus();
         }
+
 
         if (areCookiesEnabled()) {
            $('#cookiesDisabled').hide();
@@ -38,7 +50,7 @@ function resourceLoadedSuccessfully() {
            $('#cookiesDisabled').show();   
            $('#cookiesDisabled').animate({ backgroundColor: 'rgb(187,0,0)' }, 30).animate({ backgroundColor: 'rgb(255,238,221)' }, 500);
         }
-
+            
         //flash error box
         $('#msg.errors').animate({ backgroundColor: 'rgb(187,0,0)' }, 30).animate({ backgroundColor: 'rgb(255,238,221)' }, 500);
 
@@ -64,39 +76,3 @@ function resourceLoadedSuccessfully() {
     });
 
 };
-
-function createCookie(name, value, days) {
-    var expires;
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toGMTString();
-    }
-    else expires = "";
-    document.cookie = name + "=" + value + expires + "; path=/";
-}
-
-function readCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-}
-
-function eraseCookie(name) {
-    createCookie(name, "", -1);
-}
-
-function areCookiesEnabled() {
-    var r = false;
-    createCookie("cookiesEnabled", "true", 1);
-    if (readCookie("cookiesEnabled") != null) {
-        r = true;
-        eraseCookie("cookiesEnabled");
-    }
-    return r;
-}
