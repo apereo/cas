6391eaffd89592a2e7d63b82ce9866c2ae8e6d4e
Misagh Moayyed <mmoayyed@unicon.net>
2015-05-12 00:25:04 -0700

    applied patch for pooling and non-pooling fetchers

--- cas-server-support-x509/src/main/java/org/jasig/cas/adaptors/x509/authentication/handler/support/ldap/LdaptiveResourceCRLFetcher.java
+++ cas-server-support-x509/src/main/java/org/jasig/cas/adaptors/x509/authentication/handler/support/ldap/LdaptiveResourceCRLFetcher.java
@@ -19,53 +19,46 @@
 
 package org.jasig.cas.adaptors.x509.authentication.handler.support.ldap;
 
-import org.jasig.cas.adaptors.x509.authentication.handler.support.ResourceCRLFetcher;
-import org.jasig.cas.util.CompressionUtils;
-import org.ldaptive.LdapAttribute;
-import org.ldaptive.LdapEntry;
-import org.ldaptive.Response;
-import org.ldaptive.ResultCode;
-import org.ldaptive.SearchFilter;
-import org.ldaptive.SearchResult;
-import org.ldaptive.concurrent.AbstractAggregateSearchExecutor;
-import org.ldaptive.pool.AbstractConnectionPool;
-import org.ldaptive.pool.PooledConnectionFactory;
-import org.ldaptive.provider.ConnectionFactoryMetadata;
-import org.ldaptive.provider.ConnectionStrategy;
-import org.ldaptive.provider.ProviderConfig;
-import org.springframework.core.io.ByteArrayResource;
-
-import javax.annotation.PreDestroy;
-import javax.naming.AuthenticationException;
-import javax.naming.OperationNotSupportedException;
-import javax.validation.constraints.NotNull;
 import java.security.cert.CertificateException;
 import java.security.cert.X509CRL;
-import java.util.Collection;
+import javax.validation.constraints.NotNull;
+import org.jasig.cas.adaptors.x509.authentication.handler.support.ResourceCRLFetcher;
+import org.jasig.cas.util.CompressionUtils;
+import org.ldaptive.ConnectionConfig;
+import org.ldaptive.ConnectionFactory;
+import org.ldaptive.DefaultConnectionFactory;
+import org.ldaptive.LdapAttribute;
+import org.ldaptive.LdapEntry;
+import org.ldaptive.LdapException;
+import org.ldaptive.Response;
+import org.ldaptive.ResultCode;
+import org.ldaptive.SearchExecutor;
+import org.ldaptive.SearchResult;
+import org.springframework.core.io.ByteArrayResource;
 
 /**
  * Fetches a CRL from an LDAP instance.
- * @author Misagh Moayyed
+ * @author Daniel Fisher
  * @since 4.1
  */
 public class LdaptiveResourceCRLFetcher extends ResourceCRLFetcher {
 
     /** Search exec that looks for the attribute. */
-    protected final AbstractAggregateSearchExecutor searchExecutor;
+    protected final SearchExecutor searchExecutor;
 
-    /** The connection factory to prep for connections. **/
-    protected final PooledConnectionFactory connectionFactory;
+    /** The connection config to prep for connections. **/
+    protected final ConnectionConfig connectionConfig;
 
     /**
      * Instantiates a new Ldap resource cRL fetcher.
 
-     * @param connectionFactory the connection factory
+     * @param connectionConfig the connection configuration
      * @param searchExecutor the search executor
      */
     public LdaptiveResourceCRLFetcher(
-            @NotNull final PooledConnectionFactory connectionFactory,
-            @NotNull final AbstractAggregateSearchExecutor searchExecutor) {
-        this.connectionFactory = connectionFactory;
+            @NotNull final ConnectionConfig connectionConfig,
+            @NotNull final SearchExecutor searchExecutor) {
+        this.connectionConfig = connectionConfig;
         this.searchExecutor = searchExecutor;
     }
 
@@ -78,57 +71,37 @@ public class LdaptiveResourceCRLFetcher extends ResourceCRLFetcher {
     }
 
     /**
-     * Close connection pull and shut down the executor.
-     */
-    @PreDestroy
-    public void destroy() {
-        logger.debug("Shutting down connection pool...");
-        final AbstractConnectionPool pool = getConnectionPool();
-        pool.close();
-        logger.debug("Shutting down search execution pool...");
-        this.searchExecutor.shutdown();
-    }
-
-    /**
      * Downloads a CRL from given LDAP url.
      *
      * @param r the resource that is the ldap url.
      * @return the x 509 cRL
      * @throws Exception if connection to ldap fails, or attribute to get the revocation list is unavailable
      */
-    @SuppressWarnings("unchecked")
     protected X509CRL fetchCRLFromLdap(final Object r) throws Exception {
         try {
             final String ldapURL = r.toString();
             logger.debug("Fetching CRL from ldap {}", ldapURL);
 
-            prepareConnectionFactory(ldapURL);
-            final Collection<Response<SearchResult>> results =
-                    this.searchExecutor.search(
-                            new PooledConnectionFactory[] {this.connectionFactory},
-                            new SearchFilter[] {this.searchExecutor.getSearchFilter()},
-                            this.searchExecutor.getBinaryAttributes());
+            final Response<SearchResult> result = performLdapSearch(ldapURL);
+            if (result.getResultCode() == ResultCode.SUCCESS) {
+                final LdapEntry entry = result.getResult().getEntry();
+                final LdapAttribute attribute = entry.getAttribute();
 
-            for (final Response<SearchResult> result : results) {
-                if (result.getResultCode() == ResultCode.SUCCESS) {
-                    final LdapEntry entry = result.getResult().getEntry();
-                    final LdapAttribute attribute = entry.getAttribute();
-
-                    logger.debug("Located entry [{}]. Retrieving first attribute [{}]",
-                            entry, attribute);
-                    return fetchX509CRLFromAttribute(attribute);
-                } else {
-                    logger.debug("Failed to execute the search [{}]", result);
-                }
+                logger.debug("Located entry [{}]. Retrieving first attribute [{}]",
+                        entry, attribute);
+                return fetchX509CRLFromAttribute(attribute);
+            } else {
+                logger.debug("Failed to execute the search [{}]", result);
             }
 
             throw new CertificateException("Failed to establish a connection ldap and search.");
 
-        } catch (final AuthenticationException | OperationNotSupportedException e) {
+        } catch (final LdapException e) {
             logger.error(e.getMessage(), e);
             throw new CertificateException(e);
         }
     }
+
 
     /**
      * Gets x509 cRL from attribute. Retrieves the binary attribute value,
@@ -140,7 +113,7 @@ public class LdaptiveResourceCRLFetcher extends ResourceCRLFetcher {
      */
     protected X509CRL fetchX509CRLFromAttribute(final LdapAttribute aval) throws Exception {
         if (aval != null) {
-            final byte[] val = (byte[]) aval.getBinaryValue();
+            final byte[] val = aval.getBinaryValue();
             if (val == null || val.length == 0) {
                 throw new CertificateException("Empty attribute. Can not download CRL from ldap");
             }
@@ -155,33 +128,26 @@ public class LdaptiveResourceCRLFetcher extends ResourceCRLFetcher {
     }
 
     /**
-     * Prepare connection factory.
+     * Executes an LDAP search against the supplied URL.
      *
-     * @param ldapURL the ldap uRL
+     * @param ldapURL to search
+     * @return search result
+     * @throws LdapException if an error occurs performing the search
      */
-    private void prepareConnectionFactory(final String ldapURL) {
-
-        final AbstractConnectionPool pool = getConnectionPool();
-        final ProviderConfig providerConfig = pool.getConnectionFactory().getProvider().getProviderConfig();
-        try {
-            providerConfig.checkImmutable();
-            providerConfig.setConnectionStrategy(
-                new ConnectionStrategy() {
-                    @Override
-                    public String[] getLdapUrls(final ConnectionFactoryMetadata metadata) {
-                        return new String[]{String.format(metadata.getLdapUrl(), ldapURL)};
-                    }
-                }
-            );
-            logger.debug("Connection strategy to [{}] configured. Initializing pool...", ldapURL);
-            pool.initialize();
-            logger.debug("Initialized connection pool for [{}]", ldapURL);
-        } catch (final IllegalStateException e){
-            logger.trace("Connection pool failed to initialize, or is already initialized", e);
-        }
+    protected Response<SearchResult> performLdapSearch(final String ldapURL) throws LdapException {
+        final ConnectionFactory connectionFactory = prepareConnectionFactory(ldapURL);
+        return this.searchExecutor.search(connectionFactory);
     }
 
-    private AbstractConnectionPool getConnectionPool() {
-        return (AbstractConnectionPool) this.connectionFactory.getConnectionPool();
+    /**
+     * Prepare a new LDAP connection.
+     *
+     * @param ldapURL the ldap uRL
+     * @return connection factory
+     */
+    protected ConnectionFactory prepareConnectionFactory(final String ldapURL) {
+        final ConnectionConfig cc = ConnectionConfig.newConnectionConfig(this.connectionConfig);
+        cc.setLdapUrl(ldapURL);
+        return new DefaultConnectionFactory(cc);
     }
 }
