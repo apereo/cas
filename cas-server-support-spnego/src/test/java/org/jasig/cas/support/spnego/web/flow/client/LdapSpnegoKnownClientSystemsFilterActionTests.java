c05c24dd17c075b88efe1e98b6a8895bd663cdf6
Misagh Moayyed <mm1844@gmail.com>
2015-05-12 07:34:12 -0700

    added ldap unit tests

--- cas-server-support-spnego/src/test/java/org/jasig/cas/support/spnego/web/flow/client/LdapSpnegoKnownClientSystemsFilterActionTests.java
+++ cas-server-support-spnego/src/test/java/org/jasig/cas/support/spnego/web/flow/client/LdapSpnegoKnownClientSystemsFilterActionTests.java
@@ -20,10 +20,26 @@
 package org.jasig.cas.support.spnego.web.flow.client;
 
 import org.jasig.cas.adaptors.ldap.AbstractLdapTests;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.ldaptive.ConnectionFactory;
+import org.ldaptive.SearchRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.webflow.action.EventFactorySupport;
+import org.springframework.webflow.context.servlet.ServletExternalContext;
+import org.springframework.webflow.execution.Event;
+import org.springframework.webflow.test.MockRequestContext;
+
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Test cases for {@link LdapSpnegoKnownClientSystemsFilterAction}.
@@ -34,8 +50,35 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration({"/ldap-context.xml"})
 public class LdapSpnegoKnownClientSystemsFilterActionTests extends AbstractLdapTests {
 
+    @Autowired
+    @Qualifier("provisioningConnectionFactory")
+    private ConnectionFactory connectionFactory;
+
+    @Autowired
+    private SearchRequest searchRequest;
+
+    @BeforeClass
+    public static void bootstrap() throws Exception {
+        initDirectoryServer();
+    }
+
     @Test
     public void ensureLdapAttributeShouldDoSpnego() {
+        Collection c = getEntries();
+        System.out.println(c.size());
 
+        final LdapSpnegoKnownClientSystemsFilterAction action =
+                new LdapSpnegoKnownClientSystemsFilterAction(this.connectionFactory,
+                this.searchRequest, "mail");
+        final MockRequestContext ctx = new MockRequestContext();
+        final MockHttpServletRequest req = new MockHttpServletRequest();
+        req.setRemoteAddr("127.0.0.1");
+        final ServletExternalContext extCtx = new ServletExternalContext(
+                new MockServletContext(), req,
+                new MockHttpServletResponse());
+        ctx.setExternalContext(extCtx);
+
+        final Event ev = action.doExecute(ctx);
+        assertEquals(ev.getId(), new EventFactorySupport().yes(this).getId());
     }
 }
