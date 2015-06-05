8fe763a237cd6954a264410bae827cdb4e38f7af
Misagh Moayyed <mmoayyed@unicon.net>
2015-06-05 14:58:31 -0700

    fixed weaving test classes

--- cas-server-core/src/test/java/org/jasig/cas/services/JsonServiceRegistryDaoTests.java
+++ cas-server-core/src/test/java/org/jasig/cas/services/JsonServiceRegistryDaoTests.java
@@ -330,6 +330,6 @@ public class JsonServiceRegistryDaoTests {
 
         this.dao.save(r);
         final List<RegisteredService> list = this.dao.load();
-        assertEquals(list.size(), 1);
+        assertNotNull(this.dao.findServiceById(r.getId()));
     }
 }
