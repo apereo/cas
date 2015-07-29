721f178bf85c5652192ad4a6ff0e0cebcfcf0572
Misagh Moayyed <mmoayyed@unicon.net>
2015-07-29 00:03:13 -0700

    refactored

--- cas-server-core/src/main/java/org/jasig/cas/util/JCEBasedCipherExecutor.java
+++ cas-server-core/src/main/java/org/jasig/cas/util/JCEBasedCipherExecutor.java
@@ -19,6 +19,12 @@
 
 package org.jasig.cas.util;
 
+import org.jose4j.jws.AlgorithmIdentifiers;
+import org.jose4j.jws.JsonWebSignature;
+import org.jose4j.keys.AesKey;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
@@ -34,6 +40,8 @@ import javax.validation.constraints.NotNull;
 public class JCEBasedCipherExecutor implements CipherExecutor<byte[], byte[]> {
     private static final String UTF8_ENCODING = "UTF-8";
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     /** Cipher algorithm. Default is <code>AES/CBC/NoPadding</code>. */
     private String cipherAlgorithm = "AES/CBC/PKCS5Padding";
 
@@ -42,16 +50,21 @@ public class JCEBasedCipherExecutor implements CipherExecutor<byte[], byte[]> {
 
     private final String encryptionSecretKey;
     private final String encryptionSeed;
+    private final AesKey signingKey;
 
     /**
      * Instantiates a new cryptic ticket cipher executor.
      *
      * @param encryptionSecretKey the encryption secret key
      * @param encryptionSeed the encryption seed
+     * @param signingSecretKey the signing key
      */
-    public JCEBasedCipherExecutor(final String encryptionSecretKey, final String encryptionSeed) {
+    public JCEBasedCipherExecutor(final String encryptionSecretKey,
+                                  final String encryptionSeed,
+                                  final String signingSecretKey) {
         this.encryptionSecretKey = encryptionSecretKey;
         this.encryptionSeed = encryptionSeed;
+        this.signingKey = new AesKey(signingSecretKey.getBytes());
     }
 
     public void setCipherAlgorithm(final String cipherAlgorithm) {
@@ -63,7 +76,7 @@ public class JCEBasedCipherExecutor implements CipherExecutor<byte[], byte[]> {
     }
 
     @Override
-    public byte[] encode(@NotNull final byte[] value) {
+    public byte[] encode(final byte[] value) {
         try {
             final Cipher cipher = Cipher.getInstance(this.cipherAlgorithm);
             final SecretKeySpec key = new SecretKeySpec(this.encryptionSecretKey.getBytes(),
@@ -71,23 +84,69 @@ public class JCEBasedCipherExecutor implements CipherExecutor<byte[], byte[]> {
             cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(
                     this.encryptionSeed.getBytes()));
             final byte[] result = cipher.doFinal(value);
-            return result;
+            return sign(result);
         } catch (final Exception e) {
             throw new RuntimeException(e);
         }
     }
 
     @Override
-    public byte[] decode(@NotNull final byte[] value) {
+    public byte[] decode(final byte[] value) {
         try {
+            final byte[] verifiedValue = verifySignature(value);
             final Cipher cipher = Cipher.getInstance(this.cipherAlgorithm);
             final SecretKeySpec key = new SecretKeySpec(this.encryptionSecretKey.getBytes(UTF8_ENCODING),
                     this.secretKeyAlgorithm);
             cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(
                     this.encryptionSeed.getBytes(UTF8_ENCODING)));
 
-            final byte[] result = cipher.doFinal(value);
+            final byte[] result = cipher.doFinal(verifiedValue);
             return result;
+        } catch (final Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Sign the array by first turning it into a base64 encoded string.
+     *
+     * @param value the value
+     * @return the byte [ ]
+     */
+    private byte[] sign(final byte[] value) {
+        try {
+            final String base64 = CompressionUtils.encodeBase64(value);
+            final JsonWebSignature jws = new JsonWebSignature();
+            jws.setPayload(base64);
+            jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.HMAC_SHA512);
+            jws.setKey(this.signingKey);
+            return jws.getCompactSerialization().getBytes();
+        } catch (final Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Verify signature.
+     *
+     * @param value the value
+     * @return the value associated with the signature, which may have to
+     * be decoded, or null.
+     */
+    private byte[] verifySignature(@NotNull final byte[] value) {
+        try {
+            final String asString = new String(value);
+            final JsonWebSignature jws = new JsonWebSignature();
+            jws.setCompactSerialization(asString);
+            jws.setKey(this.signingKey);
+
+            final boolean verified = jws.verifySignature();
+            if (verified) {
+                final String payload = jws.getPayload();
+                logger.debug("Successfully decoded value. Result in Base64-encoding is [{}]", payload);
+                return CompressionUtils.decodeBase64(payload);
+            }
+            return null;
         } catch (final Exception e) {
             throw new RuntimeException(e);
         }
