52923060ce8a54b38a8bc7b027cbe4ca6a7f4912
Misagh Moayyed <mmoayyed@unicon.net>
2015-08-04 04:21:50 -0700

    fixed typos

--- cas-server-integration-mongo/src/main/java/org/jasig/cas/services/convert/Converters.java
+++ cas-server-integration-mongo/src/main/java/org/jasig/cas/services/convert/Converters.java
@@ -26,7 +26,6 @@ import org.jasig.services.persondir.IPersonAttributes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 
 import javax.cache.Cache;
 import javax.cache.CacheManager;
@@ -42,9 +41,20 @@ import java.security.cert.CertPath;
  * @since 4.1
  */
 public final class Converters {
+    /**
+     * Instantiates a new Converters.
+     */
     private Converters() {}
 
+    /**
+     * The type Null converter.
+     * @param <I>  the type parameter
+     * @param <O>  the type parameter
+     */
     public static class NullConverter<I, O> implements Converter<I, O> {
+        /**
+         * The Logger.
+         */
         protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 
         @Override
@@ -53,50 +63,101 @@ public final class Converters {
         }
     }
 
+    /**
+     * The type Logger converter.
+     */
     public static class LoggerConverter extends NullConverter<Logger, DBObject> {
     }
 
+    /**
+     * The type Class converter.
+     * @since 4.1
+     */
     public static class ClassConverter extends NullConverter<Class, DBObject> {
     }
 
-    public static class CommonsLogConveter extends NullConverter<Log, DBObject> {
+    /**
+     * The type Commons log converter.
+     * @since 4.1
+     */
+    public static class CommonsLogConverter extends NullConverter<Log, DBObject> {
     }
 
-    public static class PersonAttributesConveter extends NullConverter<IPersonAttributes, DBObject> {
+    /**
+     * The type Person attributes converter.
+     * @since 4.1
+     */
+    public static class PersonAttributesConverter extends NullConverter<IPersonAttributes, DBObject> {
     }
 
+    /**
+     * The type Mutable configuration converter.
+     * @since 4.1
+     */
     public static class MutableConfigurationConverter
             extends NullConverter<MutableConfiguration, DBObject> {
     }
 
+    /**
+     * The type Cache loader converter.
+     * @since 4.1
+     */
     public static class CacheLoaderConverter
             extends NullConverter<CacheLoader, DBObject> {
     }
 
+    /**
+     * The type Cache writer converter.
+     * @since 4.1
+     */
     public static class CacheWriterConverter
             extends NullConverter<CacheWriter, DBObject> {
     }
 
+    /**
+     * The type Runnable converter.
+     * @since 4.1
+     */
     public static class RunnableConverter
             extends NullConverter<Runnable, DBObject> {
     }
 
+    /**
+     * The type Reference queue converter.
+     * @since 4.1
+     */
     public static class ReferenceQueueConverter
             extends NullConverter<ReferenceQueue, DBObject> {
     }
 
+    /**
+     * The type Thread local converter.
+     * @since 4.1
+     */
     public static class ThreadLocalConverter
             extends NullConverter<ThreadLocal, DBObject> {
     }
 
+    /**
+     * The type Cert path converter.
+     * @since 4.1
+     */
     public static class CertPathConverter
             extends NullConverter<CertPath, DBObject> {
     }
 
+    /**
+     * The type Cache manager converter.
+     * @since 4.1
+     */
     public static class CacheManagerConverter
             extends NullConverter<CacheManager, DBObject> {
     }
 
+    /**
+     * The type Cache converter.
+     * @since 4.1
+     */
     public static class CacheConverter
             extends NullConverter<Cache, DBObject> {
     }
