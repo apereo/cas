3586db7d1818b9ef8c172aa19660d1bc23040544
Misagh Moayyed <mmoayyed@unicon.net>
2015-08-08 03:02:05 -0700

    wsfed module added

---
+++ cas-server-support-wsfederation/src/main/java/org/jasig/cas/support/wsfederation/WsFederationHelper.java
@@ -0,0 +1,222 @@
+package org.jasig.cas.support.wsfederation;
+
+import net.shibboleth.utilities.java.support.resolver.CriteriaSet;
+import org.jasig.cas.support.saml.OpenSamlConfigBean;
+import org.jasig.cas.support.wsfederation.authentication.principal.WsFederationCredential;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.opensaml.core.criterion.EntityIdCriterion;
+import org.opensaml.core.xml.io.Unmarshaller;
+import org.opensaml.core.xml.io.UnmarshallerFactory;
+import org.opensaml.core.xml.schema.XSAny;
+import org.opensaml.saml.common.xml.SAMLConstants;
+import org.opensaml.saml.criterion.EntityRoleCriterion;
+import org.opensaml.saml.criterion.ProtocolCriterion;
+import org.opensaml.saml.saml1.core.Assertion;
+import org.opensaml.saml.saml1.core.Attribute;
+import org.opensaml.saml.saml1.core.Conditions;
+import org.opensaml.saml.saml2.metadata.IDPSSODescriptor;
+import org.opensaml.saml.security.impl.SAMLSignatureProfileValidator;
+import org.opensaml.security.SecurityException;
+import org.opensaml.security.credential.Credential;
+import org.opensaml.security.credential.CredentialResolver;
+import org.opensaml.security.credential.UsageType;
+import org.opensaml.security.credential.impl.StaticCredentialResolver;
+import org.opensaml.security.criteria.UsageCriterion;
+import org.opensaml.security.x509.BasicX509Credential;
+import org.opensaml.soap.wsfed.RequestSecurityTokenResponse;
+import org.opensaml.soap.wsfed.RequestedSecurityToken;
+import org.opensaml.xmlsec.keyinfo.KeyInfoCredentialResolver;
+import org.opensaml.xmlsec.keyinfo.impl.StaticKeyInfoCredentialResolver;
+import org.opensaml.xmlsec.signature.support.SignatureException;
+import org.opensaml.xmlsec.signature.support.SignaturePrevalidator;
+import org.opensaml.xmlsec.signature.support.SignatureTrustEngine;
+import org.opensaml.xmlsec.signature.support.impl.ExplicitKeySignatureTrustEngine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.validation.constraints.NotNull;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Helper class that does the heavy lifting with the openSaml library.
+ *
+ * @author John Gasper
+ * @since 4.2.0
+ */
+@Component
+public final class WsFederationHelper {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WsFederationHelper.class);
+
+    @Autowired
+    @NotNull
+    private OpenSamlConfigBean configBean;
+
+    /**
+     * private constructor.
+     */
+    public WsFederationHelper() {
+    }
+
+    /**
+     * createCredentialFromToken converts a SAML 1.1 assertion to a WSFederationCredential.
+     *
+     * @param assertion the provided assertion
+     * @return an equivalent credential.
+     */
+    public WsFederationCredential createCredentialFromToken(final Assertion assertion) {
+        final DateTime retrievedOn = new DateTime().withZone(DateTimeZone.UTC);
+        LOGGER.debug("createCredentialFromToken: retrieved on {}", retrievedOn);
+
+        final WsFederationCredential credential = new WsFederationCredential();
+        credential.setRetrievedOn(retrievedOn);
+        credential.setId(assertion.getID());
+        credential.setIssuer(assertion.getIssuer());
+        credential.setIssuedOn(assertion.getIssueInstant());
+
+        final Conditions conditions = assertion.getConditions();
+        if (conditions != null) {
+            credential.setNotBefore(conditions.getNotBefore());
+            credential.setNotOnOrAfter(conditions.getNotOnOrAfter());
+            credential.setAudience(conditions.getAudienceRestrictionConditions().get(0).getAudiences().get(0).getUri());
+        }
+
+        if (!assertion.getAuthenticationStatements().isEmpty()) {
+            credential.setAuthenticationMethod(assertion.getAuthenticationStatements().get(0).getAuthenticationMethod());
+        }
+
+        //retrieve an attributes from the assertion
+        final HashMap<String, Object> attributes = new HashMap<String, Object>();
+        for (final Attribute item : assertion.getAttributeStatements().get(0).getAttributes()) {
+            LOGGER.debug("createCredentialFromToken: processed attribute: {}", item.getAttributeName());
+
+            if (item.getAttributeValues().size() == 1) {
+                attributes.put(item.getAttributeName(), ((XSAny) item.getAttributeValues().get(0)).getTextContent());
+            } else {
+                final List<String> itemList = new ArrayList<String>();
+                for (int i = 0; i < item.getAttributeValues().size(); i++) {
+                    itemList.add(((XSAny) item.getAttributeValues().get(i)).getTextContent());
+                }
+
+                if (!itemList.isEmpty()) {
+                    attributes.put(item.getAttributeName(), itemList);
+                }
+            }
+        }
+        credential.setAttributes(attributes);
+        LOGGER.debug("createCredentialFromToken: {}", credential);
+        return credential;
+    }
+
+
+
+    /**
+     * parseTokenFromString converts a raw wresult and extracts it into an assertion.
+     *
+     * @param wresult the raw token returned by the IdP
+     * @return an assertion
+     */
+    public Assertion parseTokenFromString(final String wresult) {
+        try (final InputStream in = new ByteArrayInputStream(wresult.getBytes("UTF-8"))) {
+
+            final Document document = configBean.getParserPool().parse(in);
+            final Element metadataRoot = document.getDocumentElement();
+            final UnmarshallerFactory unmarshallerFactory = configBean.getUnmarshallerFactory();
+            final Unmarshaller unmarshaller = unmarshallerFactory.getUnmarshaller(metadataRoot);
+            if (unmarshaller == null) {
+                throw new IllegalArgumentException("Unmarshaller for the metadata root element cannot be determined");
+            }
+
+            final RequestSecurityTokenResponse rsToken = (RequestSecurityTokenResponse) unmarshaller.unmarshall(metadataRoot);
+
+            //Get our SAML token
+            final List<RequestedSecurityToken> rst = rsToken.getRequestedSecurityToken();
+            final Assertion assertion = (Assertion) rst.get(0).getSecurityTokens().get(0);
+
+            if (assertion == null) {
+                LOGGER.debug("parseTokenFromString: assertion null");
+            } else {
+                LOGGER.debug("parseTokenFromString: {}", assertion);
+            }
+            return assertion;
+        } catch (final Exception ex) {
+            LOGGER.warn(ex.getMessage());
+            return null;
+        }
+    }
+
+    /**
+     * validateSignature checks to see if the signature on an assertion is valid.
+     *
+     * @param assertion a provided assertion
+     * @param wsFederationConfiguration WS-Fed configuration provided.
+     * @return true if the assertion's signature is valid, otherwise false
+     */
+    public boolean validateSignature(final Assertion assertion,
+                                     final WsFederationConfiguration wsFederationConfiguration) {
+
+        if (assertion == null) {
+            LOGGER.warn("No assertion was provided to validate signatures");
+            return false;
+        }
+
+        boolean valid = false;
+        if (assertion.getSignature() != null) {
+            final SignaturePrevalidator validator = new SAMLSignatureProfileValidator();
+            try {
+                validator.validate(assertion.getSignature());
+
+                final CriteriaSet criteriaSet = new CriteriaSet();
+                criteriaSet.add(new UsageCriterion(UsageType.SIGNING));
+                criteriaSet.add(new EntityRoleCriterion(IDPSSODescriptor.DEFAULT_ELEMENT_NAME));
+                criteriaSet.add(new ProtocolCriterion(SAMLConstants.SAML20P_NS));
+                criteriaSet.add(new EntityIdCriterion(wsFederationConfiguration.getIdentityProviderIdentifier()));
+
+                try {
+                    final SignatureTrustEngine engine = buildSignatureTrustEngine(wsFederationConfiguration);
+                    valid = engine.validate(assertion.getSignature(), criteriaSet);
+                } catch (final SecurityException e) {
+                    LOGGER.warn(e.getMessage(), e);
+                } finally {
+                    if (!valid) {
+                        LOGGER.warn("validateSignature: Signature doesn't match any signing credential.");
+                    }
+                }
+
+            } catch (final SignatureException e) {
+                LOGGER.warn("Failed to validate assertion signature", e);
+            }
+        }
+        return valid;
+    }
+
+    /**
+     * Build signature trust engine.
+     *
+     * @param wsFederationConfiguration the ws federation configuration
+     * @return the signature trust engine
+     */
+    private SignatureTrustEngine buildSignatureTrustEngine(final WsFederationConfiguration wsFederationConfiguration) {
+        try {
+            final CredentialResolver resolver = new
+                    StaticCredentialResolver(wsFederationConfiguration.getSigningCertificates());
+            final KeyInfoCredentialResolver keyResolver =
+                    new StaticKeyInfoCredentialResolver(wsFederationConfiguration.getSigningCertificates());
+
+            return new ExplicitKeySignatureTrustEngine(resolver, keyResolver);
+        } catch (final Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ cas-server-support-wsfederation/src/main/java/org/jasig/cas/support/wsfederation/authentication/principal/WsFederationCredential.java
@@ -0,0 +1,157 @@
+package org.jasig.cas.support.wsfederation.authentication.principal;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.jasig.cas.authentication.Credential;
+import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+/**
+ * This class represents the basic elements of the WsFederation token.
+ *
+ * @author John Gasper
+ * @since 4.2.0
+ */
+public final class WsFederationCredential implements Credential {
+    private final Logger logger = LoggerFactory.getLogger(WsFederationCredential.class);
+
+    private String audience;
+    private String authenticationMethod;
+    private String id;
+    private String issuer;
+    private DateTime issuedOn;
+    private DateTime notBefore;
+    private DateTime notOnOrAfter;
+    private DateTime retrievedOn;
+    private Map<String, Object> attributes;
+
+    public String getAuthenticationMethod() {
+        return this.authenticationMethod;
+    }
+
+    public void setAuthenticationMethod(final String authenticationMethod) {
+        this.authenticationMethod = authenticationMethod;
+    }
+
+    public String getAudience() {
+        return this.audience;
+    }
+
+    public void setAudience(final String audience) {
+        this.audience = audience;
+    }
+
+    public Map<String, Object> getAttributes() {
+        return this.attributes;
+    }
+
+    public void setAttributes(final Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(final String id) {
+        this.id = id;
+    }
+
+    public DateTime getIssuedOn() {
+        return this.issuedOn;
+    }
+
+    public void setIssuedOn(final DateTime issuedOn) {
+        this.issuedOn = issuedOn;
+    }
+
+    public String getIssuer() {
+        return this.issuer;
+    }
+
+    public void setIssuer(final String issuer) {
+        this.issuer = issuer;
+    }
+
+    public DateTime getNotBefore() {
+        return this.notBefore;
+    }
+
+    public void setNotBefore(final DateTime notBefore) {
+        this.notBefore = notBefore;
+    }
+
+    public DateTime getNotOnOrAfter() {
+        return this.notOnOrAfter;
+    }
+
+    public void setNotOnOrAfter(final DateTime notOnOrAfter) {
+        this.notOnOrAfter = notOnOrAfter;
+    }
+
+    public DateTime getRetrievedOn() {
+        return this.retrievedOn;
+    }
+
+    public void setRetrievedOn(final DateTime retrievedOn) {
+        this.retrievedOn = retrievedOn;
+    }
+
+    /**
+     * toString produces a human readable representation of the WsFederationCredential.
+     *
+     * @return a human readable representation of the WsFederationCredential
+     */
+    @Override
+    public String toString() {
+
+        return new ToStringBuilder(this, ToStringStyle.NO_CLASS_NAME_STYLE)
+                .append("ID", this.id)
+                .append("Issuer", this.issuer)
+                .append("Audience", this.audience)
+                .append("Authentication Method", this.authenticationMethod)
+                .append("Issued On", this.issuedOn)
+                .append("Valid After", this.notBefore)
+                .append("Valid Before", this.notOnOrAfter)
+                .append("Attributes", this.attributes)
+                .build();
+
+    }
+
+    /**
+     * isValid validates the credential.
+     *
+     * @param expectedAudience the audience that the token was issued to (CAS Server)
+     * @param expectedIssuer   the issuer of the token (the IdP)
+     * @param timeDrift        the amount of acceptable time drift
+     * @return true if the credentials are valid, otherwise false
+     */
+    public boolean isValid(final String expectedAudience, final String expectedIssuer, final int timeDrift) {
+        if (!this.getAudience().equalsIgnoreCase(expectedAudience)) {
+            logger.warn(".isValid: audience is invalid: {}", this.getAudience());
+            return false;
+        }
+
+        if (!this.getIssuer().equalsIgnoreCase(expectedIssuer)) {
+            logger.warn(".isValid: issuer is invalid: {}", this.getIssuer());
+            return false;
+        }
+
+        if (this.getIssuedOn().isBefore(this.getRetrievedOn().minusMillis(timeDrift))
+                || this.getIssuedOn().isAfter(this.getRetrievedOn().plusMillis(timeDrift))) {
+            logger.warn(".isValid: Ticket outside of drift.");
+            return false;
+        }
+
+        if (this.getRetrievedOn().isAfter(this.getNotOnOrAfter())) {
+            logger.warn(".isValid: ticket is too late.");
+            return false;
+        }
+
+        logger.debug(".isValid: credential is valid.");
+        return true;
+    }
+}
