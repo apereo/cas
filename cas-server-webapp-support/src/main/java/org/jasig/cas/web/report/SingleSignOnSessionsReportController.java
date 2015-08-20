e1f5cd5363a5f3f627f943f71787c48364c563e3
Misagh Moayyed <mmoayyed@unicon.net>
2015-08-20 02:27:19 -0700

    plugin updates

--- cas-server-webapp-support/src/main/java/org/jasig/cas/web/report/SingleSignOnSessionsReportController.java
+++ cas-server-webapp-support/src/main/java/org/jasig/cas/web/report/SingleSignOnSessionsReportController.java
@@ -19,10 +19,10 @@
 
 package org.jasig.cas.web.report;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import org.apache.commons.collections4.Predicate;
 import org.jasig.cas.CentralAuthenticationService;
+import org.jasig.cas.authentication.Authentication;
+import org.jasig.cas.authentication.principal.Principal;
 import org.jasig.cas.ticket.Ticket;
 import org.jasig.cas.ticket.TicketGrantingTicket;
 import org.slf4j.Logger;
@@ -30,15 +30,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -54,23 +56,53 @@ public final class SingleSignOnSessionsReportController {
 
     private static final String VIEW_SSO_SESSIONS = "monitoring/viewSsoSessions";
 
+    private enum SsoSessionReportOptions {
+        ALL("all"),
+        PROXIED("all"),
+        DIRECT("direct");
+
+        private final String type;
+
+        /**
+         * Instantiates a new Sso session report options.
+         *
+         * @param type the type
+         */
+        SsoSessionReportOptions(final String type) {
+            this.type = type;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        @Override
+        public String toString() {
+            return this.type;
+        }
+    }
     /**
      * The enum Sso session attribute keys.
      */
     private enum SsoSessionAttributeKeys {
         AUTHENTICATED_PRINCIPAL("authenticated_principal"),
+        PRINCIPAL_ATTRIBUTES("principal_attributes"),
         AUTHENTICATION_DATE("authentication_date"),
         TICKET_GRANTING_TICKET("ticket_granting_ticket"),
+        AUTHENTICATION_ATTRIBUTES("authentication_attributes"),
+        PROXIED_BY("proxied_by"),
+        AUTHENTICATED_SERVICES("authenticated_services"),
+        IS_PROXIED("is_proxied"),
         NUMBER_OF_USES("number_of_uses");
 
-        private String attributeKey;
+        private final String attributeKey;
 
         /**
          * Instantiates a new Sso session attribute keys.
          *
          * @param attributeKey the attribute key
          */
-        private SsoSessionAttributeKeys(final String attributeKey) {
+        SsoSessionAttributeKeys(final String attributeKey) {
             this.attributeKey = attributeKey;
         }
 
@@ -82,11 +114,8 @@ public final class SingleSignOnSessionsReportController {
 
     private static final String ROOT_REPORT_ACTIVE_SESSIONS_KEY = "activeSsoSessions";
 
-    private static final String ROOT_REPORT_NA_KEY = "notAvailable";
-
     private static final Logger LOGGER = LoggerFactory.getLogger(SingleSignOnSessionsReportController.class);
 
-    private final ObjectMapper jsonMapper = new ObjectMapper();
 
     @Autowired
     private CentralAuthenticationService centralAuthenticationService;
@@ -97,29 +126,47 @@ public final class SingleSignOnSessionsReportController {
     /**
      * Instantiates a new Single sign on sessions report resource.
      */
-    public SingleSignOnSessionsReportController() {
-        this.jsonMapper.disable(SerializationFeature.INDENT_OUTPUT);
-        this.jsonMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-    }
+    public SingleSignOnSessionsReportController() {}
 
     /**
      * Gets sso sessions.
      *
+     * @param option the option
      * @return the sso sessions
      */
-    private Collection<Map<String, Object>> getSsoSessions() {
-        final List<Map<String, Object>> activeSessions = new ArrayList<Map<String, Object>>();
+    private Collection<Map<String, Object>> getActiveSsoSessions(final SsoSessionReportOptions option) {
+        final Collection<Map<String, Object>> activeSessions = new ArrayList<>();
 
-        for(final Ticket ticket : getNonExpiredTicketGrantingTickets()) {
+        for (final Ticket ticket : getNonExpiredTicketGrantingTickets()) {
             final TicketGrantingTicket tgt = (TicketGrantingTicket) ticket;
 
+            if (option == SsoSessionReportOptions.DIRECT && tgt.getProxiedBy() != null) {
+                continue;
+            }
+
+            final Authentication authentication = tgt.getAuthentication();
+            final Principal principal = authentication.getPrincipal();
+
             final Map<String, Object> sso = new HashMap<>(SsoSessionAttributeKeys.values().length);
-            sso.put(SsoSessionAttributeKeys.AUTHENTICATED_PRINCIPAL.toString(), tgt.getAuthentication().getPrincipal().getId());
-            sso.put(SsoSessionAttributeKeys.AUTHENTICATION_DATE.toString(), tgt.getAuthentication().getAuthenticationDate());
+            sso.put(SsoSessionAttributeKeys.AUTHENTICATED_PRINCIPAL.toString(), principal.getId());
+            sso.put(SsoSessionAttributeKeys.AUTHENTICATION_DATE.toString(), authentication.getAuthenticationDate());
             sso.put(SsoSessionAttributeKeys.NUMBER_OF_USES.toString(), tgt.getCountOfUses());
             if (this.includeTicketGrantingTicketId) {
                 sso.put(SsoSessionAttributeKeys.TICKET_GRANTING_TICKET.toString(), tgt.getId());
             }
+            sso.put(SsoSessionAttributeKeys.PRINCIPAL_ATTRIBUTES.toString(), principal.getAttributes());
+            sso.put(SsoSessionAttributeKeys.AUTHENTICATION_ATTRIBUTES.toString(), authentication.getAttributes());
+
+            if (option != SsoSessionReportOptions.DIRECT) {
+                if (tgt.getProxiedBy() != null) {
+                    sso.put(SsoSessionAttributeKeys.IS_PROXIED.toString(), true);
+                    sso.put(SsoSessionAttributeKeys.PROXIED_BY.toString(), tgt.getProxiedBy().getId());
+                } else {
+                    sso.put(SsoSessionAttributeKeys.IS_PROXIED.toString(), false);
+                }
+            }
+
+            sso.put(SsoSessionAttributeKeys.AUTHENTICATED_SERVICES.toString(), tgt.getServices());
 
             activeSessions.add(Collections.unmodifiableMap(sso));
         }
@@ -144,6 +191,21 @@ public final class SingleSignOnSessionsReportController {
     }
 
     /**
+     * Endpoint for getting SSO Sessions in JSON format
+     *
+     * @param type the type
+     * @return the sso sessions
+     */
+    @RequestMapping(value = "/getSsoSessions", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, Object> getSsoSessions(@RequestParam(defaultValue = "ALL") final String type) {
+        final Map<String, Object> sessionsMap = new HashMap<>(1);
+        final SsoSessionReportOptions option = SsoSessionReportOptions.valueOf(type);
+        sessionsMap.put(ROOT_REPORT_ACTIVE_SESSIONS_KEY, getActiveSsoSessions(option));
+        return sessionsMap;
+    }
+
+    /**
      * Show sso sessions.
      *
      * @return the model and view where json data will be rendered
@@ -151,11 +213,7 @@ public final class SingleSignOnSessionsReportController {
      */
     @RequestMapping(method = RequestMethod.GET)
     public ModelAndView showSsoSessions() throws Exception {
-        final Map<String, Object> sessionsMap = new HashMap<String, Object>(1);
-        sessionsMap.put(ROOT_REPORT_ACTIVE_SESSIONS_KEY, getSsoSessions());
-        final String jsonRepresentation = this.jsonMapper.writeValueAsString(sessionsMap);
         final ModelAndView modelAndView = new ModelAndView(VIEW_SSO_SESSIONS);
-        modelAndView.addObject(ROOT_REPORT_ACTIVE_SESSIONS_KEY, jsonRepresentation);
         return modelAndView;
     }
 }
