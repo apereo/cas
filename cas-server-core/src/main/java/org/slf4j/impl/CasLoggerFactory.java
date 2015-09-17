2e2511986b17f96b2d33dc491ef371df3e32742a
Misagh Moayyed <mm1844@gmail.com>
2015-09-17 06:43:26 -0700

    updated views

--- cas-server-core/src/main/java/org/slf4j/impl/CasLoggerFactory.java
+++ cas-server-core/src/main/java/org/slf4j/impl/CasLoggerFactory.java
@@ -26,7 +26,6 @@ import org.reflections.util.ClasspathHelper;
 import org.reflections.util.ConfigurationBuilder;
 import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.slf4j.helpers.NOPLogger;
 import org.slf4j.helpers.Util;
 
