50b5ba0b7420f82e3fc1fc0a107b78473449b121
Misagh Moayyed <mmoayyed@unicon.net>
2015-09-19 10:43:02 -0700

    added support for shiro authN

--- cas-server-support-generic/src/main/java/org/jasig/cas/adaptors/generic/ShiroAuthenticationHandler.java
+++ cas-server-support-generic/src/main/java/org/jasig/cas/adaptors/generic/ShiroAuthenticationHandler.java
@@ -19,6 +19,7 @@
 
 package org.jasig.cas.adaptors.generic;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.DisabledAccountException;
@@ -28,7 +29,10 @@ import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.LockedAccountException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
 import org.jasig.cas.authentication.AccountDisabledException;
 import org.jasig.cas.authentication.Credential;
 import org.jasig.cas.authentication.HandlerResult;
@@ -36,12 +40,15 @@ import org.jasig.cas.authentication.PreventedException;
 import org.jasig.cas.authentication.RememberMeUsernamePasswordCredential;
 import org.jasig.cas.authentication.UsernamePasswordCredential;
 import org.jasig.cas.authentication.handler.support.AbstractUsernamePasswordAuthenticationHandler;
+import org.springframework.core.io.Resource;
 
 import javax.security.auth.login.AccountLockedException;
 import javax.security.auth.login.AccountNotFoundException;
 import javax.security.auth.login.CredentialExpiredException;
 import javax.security.auth.login.FailedLoginException;
 import java.security.GeneralSecurityException;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * An authentication handler that routes requests to Apache Shiro.
@@ -50,6 +57,37 @@ import java.security.GeneralSecurityException;
  * @since 4.2
  */
 public class ShiroAuthenticationHandler extends AbstractUsernamePasswordAuthenticationHandler {
+
+    private Set<String> requiredRoles = new HashSet<>();
+    private Set<String> requiredPermissions = new HashSet<>();
+
+    public void setRequiredRoles(final Set<String> requiredRoles) {
+        this.requiredRoles = requiredRoles;
+    }
+
+    public void setRequiredPermissions(final Set<String> requiredPermissions) {
+        this.requiredPermissions = requiredPermissions;
+    }
+
+    /**
+     * Sets shiro configuration to the path of the resource
+     * that points to the <code>shiro.ini</code> file.
+     *
+     * @param resource the resource
+     */
+    public void setShiroConfiguration(final Resource resource) {
+        try {
+            final String location = resource.getURI().toString();
+            logger.debug("Loading Shiro configuration from {}", location);
+
+            final Factory<SecurityManager> factory = new IniSecurityManagerFactory(location);
+            final SecurityManager securityManager = factory.getInstance();
+            SecurityUtils.setSecurityManager(securityManager);
+        } catch (final Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
     protected final HandlerResult authenticateUsernamePasswordInternal(final UsernamePasswordCredential transformedCredential)
             throws GeneralSecurityException, PreventedException {
@@ -62,6 +100,9 @@ public class ShiroAuthenticationHandler extends AbstractUsernamePasswordAuthenti
 
             final Subject currentUser = getCurrentExecutingSubject();
             currentUser.login(token);
+
+            checkSubjectRolesAndPermissions(currentUser);
+
             return createAuthenticatedSubjectResult(credential, currentUser);
         } catch (final UnknownAccountException uae) {
             throw new AccountNotFoundException(uae.getMessage());
@@ -77,6 +118,26 @@ public class ShiroAuthenticationHandler extends AbstractUsernamePasswordAuthenti
             throw new AccountDisabledException(eae.getMessage());
         } catch (final AuthenticationException ae){
             throw new FailedLoginException(ae.getMessage());
+        }
+    }
+
+    /**
+     * Check subject roles and permissions.
+     *
+     * @param currentUser the current user
+     * @throws FailedLoginException the failed login exception in case roles or permissions are absent
+     */
+    protected void checkSubjectRolesAndPermissions(final Subject currentUser) throws FailedLoginException {
+        for (final String role : this.requiredRoles) {
+            if (!currentUser.hasRole(role)) {
+                throw new FailedLoginException("Required role " + role + " does not exist");
+            }
+        }
+
+        for (final String perm : this.requiredPermissions) {
+            if (!currentUser.isPermitted(perm)) {
+                throw new FailedLoginException("Required permission " + perm + " does not exist");
+            }
         }
     }
 
