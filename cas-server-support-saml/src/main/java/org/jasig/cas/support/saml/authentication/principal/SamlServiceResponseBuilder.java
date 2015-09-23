12ee2f13f53de97623573d191311d07da6a29cbe
Misagh Moayyed <mm1844@gmail.com>
2015-09-23 07:50:24 -0700

    Refactored service responses into individual components

---
+++ cas-server-support-saml/src/main/java/org/jasig/cas/support/saml/authentication/principal/SamlServiceResponseBuilder.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to Apereo under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Apereo licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License.  You may obtain a
+ * copy of the License at the following location:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.jasig.cas.support.saml.authentication.principal;
+
+import org.jasig.cas.authentication.principal.AbstractServiceResponseBuilder;
+import org.jasig.cas.authentication.principal.Response;
+import org.jasig.cas.authentication.principal.WebApplicationService;
+import org.jasig.cas.support.saml.SamlProtocolConstants;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Builds responses to SAML service requests.
+ * @author Misagh Moayyed
+ * @since 4.2
+ */
+public class SamlServiceResponseBuilder extends AbstractServiceResponseBuilder {
+
+    @Override
+    public Response build(final WebApplicationService service, final String ticketId) {
+        final Map<String, String> parameters = new HashMap<>();
+        parameters.put(SamlProtocolConstants.CONST_PARAM_ARTIFACT, ticketId);
+        return buildRedirect(service, parameters);
+    }
+}
+
