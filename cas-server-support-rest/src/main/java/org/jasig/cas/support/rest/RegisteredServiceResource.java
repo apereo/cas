67ba03e6cf41a33e04cb8c162b22ed2f9483ebf5
Misagh Moayyed <mmoayyed@unicon.net>
2015-10-01 05:06:14 -0700

    cas services rest api

--- cas-server-support-rest/src/main/java/org/jasig/cas/support/rest/RegisteredServiceResource.java
+++ cas-server-support-rest/src/main/java/org/jasig/cas/support/rest/RegisteredServiceResource.java
@@ -19,20 +19,15 @@
 
 package org.jasig.cas.support.rest;
 
-import com.google.common.collect.Collections2;
-import com.google.common.collect.Sets;
+import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.jasig.cas.CasProtocolConstants;
 import org.jasig.cas.CentralAuthenticationService;
-import org.jasig.cas.authentication.principal.Principal;
-import org.jasig.cas.authentication.principal.WebApplicationServiceFactory;
 import org.jasig.cas.services.DefaultRegisteredServiceAccessStrategy;
 import org.jasig.cas.services.RegexRegisteredService;
+import org.jasig.cas.services.RegisteredService;
 import org.jasig.cas.services.ServicesManager;
 import org.jasig.cas.ticket.InvalidTicketException;
-import org.jasig.cas.ticket.ServiceTicket;
 import org.jasig.cas.ticket.TicketGrantingTicket;
-import org.jasig.cas.ticket.registry.TicketRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -48,6 +43,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -73,22 +69,22 @@ public class RegisteredServiceResource {
     @Qualifier("centralAuthenticationService")
     private CentralAuthenticationService centralAuthenticationService;
 
-    @Value("${cas.rest.services.attributename:")
+    @Value("${cas.rest.services.attributename:}")
     private String attributeName;
 
-    @Value("${cas.rest.services.attributevalue:")
+    @Value("${cas.rest.services.attributevalue:}")
     private String attributeValue;
 
     /**
-     * Create new service ticket.
+     * Create new service.
      *
      * @param requestBody service application/x-www-form-urlencoded value
      * @param tgtId ticket granting ticket id URI path param
      * @return {@link ResponseEntity} representing RESTful response
      */
-    @RequestMapping(value = "/v1/services/{tgtId:.+}", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public final ResponseEntity<String> createServiceTicket(@RequestBody final MultiValueMap<String, String> requestBody,
-                                                            @PathVariable("tgtId") final String tgtId) {
+    @RequestMapping(value = "/services/add/{tgtId:.+}", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public final ResponseEntity<String> createService(@RequestBody final MultiValueMap<String, String> requestBody,
+                                                      @PathVariable("tgtId") final String tgtId) {
         try {
 
             if (StringUtils.isBlank(this.attributeName) || StringUtils.isBlank(this.attributeValue)) {
@@ -97,42 +93,26 @@ public class RegisteredServiceResource {
 
             final TicketGrantingTicket ticket =
                 this.centralAuthenticationService.getTicket(tgtId, TicketGrantingTicket.class);
+            if (ticket == null || ticket.isExpired()) {
+                throw new InvalidTicketException("Ticket-granting ticket " + tgtId + " is not found");
+            }
             final Map<String, Object> attributes = ticket.getAuthentication().getPrincipal().getAttributes();
             final Map<String, Set<String>> requiredAttrs = new HashMap<>();
             requiredAttrs.put(this.attributeName, Collections.singleton(this.attributeValue));
 
             if (attributes.containsKey(this.attributeName)) {
-                final Set requiredValues = new HashSet(requiredAttrs.values());
-                final Set presentValues = new HashSet(attributes.values());
+                final Collection<String> attributeValuesToCompare = new HashSet<>();
+                final Object value = attributes.get(this.attributeName);
+                if (value instanceof Collection) {
+                    attributeValuesToCompare.addAll((Collection<String>) value);
+                } else {
+                    attributeValuesToCompare.add(value.toString());
+                }
 
-                final Sets.SetView<String> difference = Sets.intersection(requiredValues, presentValues);
-                final Set<String> copy = difference.immutableCopy();
-
-                if (!copy.isEmpty()) {
-                    final String serviceId = requestBody.getFirst("serviceId");
-                    final String serviceName = requestBody.getFirst("name");
-                    final String description = requestBody.getFirst("description");
-                    final String evaluationOrder = requestBody.getFirst("evaluationOrder");
-
-                    if (StringUtils.isBlank(serviceId) || StringUtils.isBlank(serviceName)
-                        || StringUtils.isBlank(description)) {
-                        throw new IllegalArgumentException("Service name/description/id is missing");
-                    }
-
-                    final RegexRegisteredService service = new RegexRegisteredService();
-                    service.setServiceId(serviceId);
-                    service.setDescription(description);
-                    service.setName(serviceName);
-                    service.setAccessStrategy(new DefaultRegisteredServiceAccessStrategy(false, false));
-
-                    if (StringUtils.isBlank(evaluationOrder)) {
-                        service.setEvaluationOrder(Integer.MAX_VALUE);
-                    } else {
-                        service.setEvaluationOrder(Integer.parseInt(evaluationOrder));
-                    }
-
-                    this.servicesManager.save(service);
-                    return new ResponseEntity<>(String.valueOf(service.getId()), HttpStatus.OK);
+                if (attributeValuesToCompare.contains(this.attributeValue)) {
+                    final RegexRegisteredService service = parseRequestToExtractService(requestBody);
+                    final RegisteredService savedService = this.servicesManager.save(service);
+                    return new ResponseEntity<>(String.valueOf(savedService.getId()), HttpStatus.OK);
                 }
             }
             throw new IllegalArgumentException("Request is not authorized");
@@ -145,4 +125,54 @@ public class RegisteredServiceResource {
         }
     }
 
+    private RegexRegisteredService parseRequestToExtractService(@RequestBody final MultiValueMap<String, String> requestBody) {
+        final String serviceId = requestBody.getFirst("serviceId");
+        final String serviceName = requestBody.getFirst("name");
+        final String description = requestBody.getFirst("description");
+        final String evaluationOrder = requestBody.getFirst("evaluationOrder");
+        final String enabled = requestBody.getFirst("enabled");
+        final String ssoEnabled = requestBody.getFirst("ssoEnabled");
+
+        if (StringUtils.isBlank(serviceId) || StringUtils.isBlank(serviceName)
+            || StringUtils.isBlank(description)) {
+            throw new IllegalArgumentException("Service name/description/id is missing");
+        }
+
+
+        final RegexRegisteredService service = new RegexRegisteredService();
+        service.setServiceId(serviceId);
+        service.setDescription(description);
+        service.setName(serviceName);
+
+        if (StringUtils.isBlank(evaluationOrder)) {
+            service.setEvaluationOrder(Integer.MAX_VALUE);
+        } else {
+            service.setEvaluationOrder(Integer.parseInt(evaluationOrder));
+        }
+
+        boolean enabledService = false;
+        boolean ssoEnabledService = false;
+        if (!StringUtils.isBlank(enabled)) {
+            enabledService = BooleanUtils.toBoolean(enabled);
+        }
+        if (!StringUtils.isBlank(ssoEnabled)) {
+            ssoEnabledService = BooleanUtils.toBoolean(ssoEnabled);
+        }
+
+        service.setAccessStrategy(
+            new DefaultRegisteredServiceAccessStrategy(enabledService, ssoEnabledService));
+        return service;
+    }
+
+    public void setAttributeName(final String attributeName) {
+        this.attributeName = attributeName;
+    }
+
+    public void setAttributeValue(final String attributeValue) {
+        this.attributeValue = attributeValue;
+    }
+
+    public void setCentralAuthenticationService(final CentralAuthenticationService centralAuthenticationService) {
+        this.centralAuthenticationService = centralAuthenticationService;
+    }
 }
