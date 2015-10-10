4c47bcabc380ab3cf5f58c14e9fa98a5732f7db5
Misagh Moayyed <mm1844@gmail.com>
2015-10-10 11:38:38 -0700

    added duo module

--- cas-server-support-duo/src/main/java/org/jasig/cas/adaptors/duo/DuoAuthenticationHandler.java
+++ cas-server-support-duo/src/main/java/org/jasig/cas/adaptors/duo/DuoAuthenticationHandler.java
@@ -17,22 +17,43 @@
  * under the License.
  */
 
-package org.jasig.cas.adaptors.duo
+package org.jasig.cas.adaptors.duo;
 
 
+import org.jasig.cas.MessageDescriptor;
 import org.jasig.cas.authentication.Credential;
 import org.jasig.cas.authentication.HandlerResult;
 import org.jasig.cas.authentication.PreventedException;
 import org.jasig.cas.authentication.handler.support.AbstractPreAndPostProcessingAuthenticationHandler;
+import org.jasig.cas.authentication.principal.Principal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+import javax.security.auth.login.FailedLoginException;
 import java.security.GeneralSecurityException;
+import java.util.ArrayList;
 
-public final class DuoAuthenticationHandler implements AbstractPreAndPostProcessingAuthenticationHandler {
+/**
+ * Authenticate CAS credentials against Duo Security.
+ * @author Misagh Moayyed
+ * @author Dmitriy Kopylenko
+ * @since 4.2
+ */
+@Component("duoAuthenticationHandler")
+public final class DuoAuthenticationHandler extends AbstractPreAndPostProcessingAuthenticationHandler {
 
-    private final DuoAuthenticationService duoAuthenticationService
+    private final DuoAuthenticationService duoAuthenticationService;
 
-    public DuoAuthenticationHandler(final DuoAuthenticationService duoAuthenticationService) {
-        this.duoAuthenticationService = duoAuthenticationService
+    /**
+     * Creates the duo authentication handler.
+     * @param duoAuthenticationService the duo authentication service
+     */
+    @Autowired
+
+    public DuoAuthenticationHandler(@Qualifier("duoAuthenticationService")
+                                        final DuoAuthenticationService duoAuthenticationService) {
+        this.duoAuthenticationService = duoAuthenticationService;
     }
 
 
@@ -48,24 +69,33 @@ public final class DuoAuthenticationHandler implements AbstractPreAndPostProcess
      */
     @Override
     protected HandlerResult doAuthentication(final Credential credential) throws GeneralSecurityException, PreventedException {
-        final DuoCredential duoCredential = (DuoCredential) credential;
 
-        final duoVerifyResponse = this.duoAuthenticationService.authenticate(duoCredential.signedDuoResponse);
-        logger.debug("Response from Duo verify: [{}]", duoVerifyResponse);
-        final String primaryCredentialsUsername = duoCredential.username;
+        try {
+            final DuoCredential duoCredential = (DuoCredential) credential;
 
-        final isGoodAuthentication = duoVerifyResponse == primaryCredentialsUsername
-        if (isGoodAuthentication) {
-            logger.info("Successful Duo authentication for [{}]", primaryCredentialsUsername)
-            return true
+            final String duoVerifyResponse = this.duoAuthenticationService.authenticate(duoCredential.getSignedDuoResponse());
+            logger.debug("Response from Duo verify: [{}]", duoVerifyResponse);
+            final String primaryCredentialsUsername = duoCredential.getUsername();
+
+            final boolean isGoodAuthentication= duoVerifyResponse.equals(primaryCredentialsUsername);
+
+            if (isGoodAuthentication) {
+                logger.info("Successful Duo authentication for [{}]", primaryCredentialsUsername);
+
+                final Principal principal = this.principalFactory.createPrincipal(primaryCredentialsUsername);
+                return createHandlerResult(credential, principal, new ArrayList<MessageDescriptor>());
+            }
+            throw new FailedLoginException("Duo authentication error for username "
+                    + primaryCredentialsUsername + " and Duo response: " + duoVerifyResponse);
+
+        } catch (final Exception e) {
+            logger.error(e.getMessage(), e);
+            throw new FailedLoginException(e.getMessage());
         }
-        logger.error("Duo authentication error! Login username: [{}], Duo response: [{}]",
-                primaryCredentialsUsername ? : "null", duoVerifyResponse);
-
     }
 
     @Override
     public boolean supports(final Credential credential) {
-        return DuoCredential.isAssignableFrom(credential.getClass());
+        return DuoCredential.class.isAssignableFrom(credential.getClass());
     }
 }
