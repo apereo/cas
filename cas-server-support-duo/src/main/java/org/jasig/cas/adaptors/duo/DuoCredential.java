b4a418f1beb19a085f3547873b390388e4ce5133
Misagh Moayyed <mm1844@gmail.com>
2015-10-10 11:15:37 -0700

    added duo module

---
+++ cas-server-support-duo/src/main/java/org/jasig/cas/adaptors/duo/DuoCredential.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to Apereo under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Apereo licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License.  You may obtain a
+ * copy of the License at the following location:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.jasig.cas.adaptors.duo;
+
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.jasig.cas.authentication.Credential;
+
+/**
+ * Represents the duo credential.
+ * @author Misagh Moayyed
+ * @since 4.2
+ */
+public final class DuoCredential implements Credential {
+
+    private String username;
+    private String signedDuoResponse;
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append("username", this.username).toString();
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (!(obj instanceof DuoCredential)) {
+            return false;
+        }
+        if (obj == this) {
+            return true;
+        }
+        final DuoCredential other = (DuoCredential) obj;
+        final EqualsBuilder builder = new EqualsBuilder();
+        builder.append(this.username, other.username);
+        return builder.isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        final HashCodeBuilder builder = new HashCodeBuilder(97, 31);
+        builder.append(this.username);
+        return builder.toHashCode();
+    }
+
+    @Override
+    public String getId() {
+        return this.username;
+    }
+
+    public String getSignedDuoResponse() {
+        return signedDuoResponse;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
