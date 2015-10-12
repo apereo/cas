c7e852826b288c86ad939e789b5da66d4c44777f
Misagh Moayyed <mmoayyed@unicon.net>
2015-10-12 17:57:42 -0600

    fixed test cases

--- cas-server-integration-memcached/src/test/java/org/jasig/cas/ticket/registry/MemCacheTicketRegistryTests.java
+++ cas-server-integration-memcached/src/test/java/org/jasig/cas/ticket/registry/MemCacheTicketRegistryTests.java
@@ -156,11 +156,11 @@ public class MemCacheTicketRegistryTests {
         final Service service = TestUtils.getService("TGT_DELETE_TEST");
 
         final ServiceTicket st1 = tgt.grantServiceTicket(
-                "ST1", service, new NeverExpiresExpirationPolicy(), true);
+                "ST1", service, new NeverExpiresExpirationPolicy(), true, false);
         final ServiceTicket st2 = tgt.grantServiceTicket(
-                "ST2", service, new NeverExpiresExpirationPolicy(), true);
+                "ST2", service, new NeverExpiresExpirationPolicy(), true, false);
         final ServiceTicket st3 = tgt.grantServiceTicket(
-                "ST3", service, new NeverExpiresExpirationPolicy(), true);
+                "ST3", service, new NeverExpiresExpirationPolicy(), true, false);
 
         this.registry.addTicket(st1);
         this.registry.addTicket(st2);
