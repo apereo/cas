7c20af6a02b55245b11bf378cc6373bd94988e4a
Misagh Moayyed <mmoayyed@unicon.net>
2015-10-14 19:34:43 -0600

    Use TryResource syntax for LDAP connections

--- cas-server-support-ldap/src/main/java/org/jasig/cas/adaptors/ldap/services/LdapServiceRegistryDao.java
+++ cas-server-support-ldap/src/main/java/org/jasig/cas/adaptors/ldap/services/LdapServiceRegistryDao.java
@@ -20,7 +20,6 @@ package org.jasig.cas.adaptors.ldap.services;
 
 import org.jasig.cas.services.RegisteredService;
 import org.jasig.cas.services.ServiceRegistryDao;
-import org.jasig.cas.util.LdapUtils;
 import org.ldaptive.AddOperation;
 import org.ldaptive.AddRequest;
 import org.ldaptive.AttributeModification;
@@ -100,23 +99,23 @@ public final class LdapServiceRegistryDao implements ServiceRegistryDao {
 
     @Override
     public RegisteredService save(final RegisteredService rs) {
-        if (rs.getId() != RegisteredService.INITIAL_IDENTIFIER_VALUE) {
-            return update(rs);
-        }
 
-        Connection connection = null;
-        try {
-            connection = getConnection();
-            final AddOperation operation = new AddOperation(connection);
+        if (this.ldapServiceMapper != null && this.searchRequest != null) {
+            if (rs.getId() != RegisteredService.INITIAL_IDENTIFIER_VALUE) {
+                return update(rs);
+            }
 
-            final LdapEntry entry = this.ldapServiceMapper.mapFromRegisteredService(this.searchRequest.getBaseDn(), rs);
-            operation.execute(new AddRequest(entry.getDn(), entry.getAttributes()));
-        } catch (final LdapException e) {
-            logger.error(e.getMessage(), e);
-        } finally {
-            LdapUtils.closeConnection(connection);
+            try (final Connection connection = getConnection()) {
+                final AddOperation operation = new AddOperation(connection);
+
+                final LdapEntry entry = this.ldapServiceMapper.mapFromRegisteredService(this.searchRequest.getBaseDn(), rs);
+                operation.execute(new AddRequest(entry.getDn(), entry.getAttributes()));
+            } catch (final LdapException e) {
+                logger.error(e.getMessage(), e);
+            }
+            return rs;
         }
-        return rs;
+        return null;
     }
 
     /**
@@ -126,17 +125,20 @@ public final class LdapServiceRegistryDao implements ServiceRegistryDao {
      * @return the registered service
      */
     private RegisteredService update(final RegisteredService rs) {
-        Connection searchConnection = null;
-        try {
-            searchConnection = getConnection();
+
+        if (ldapServiceMapper == null || searchRequest == null) {
+            return null;
+        }
+
+        try (final Connection searchConnection = getConnection()) {
+
             final Response<SearchResult> response = searchForServiceById(searchConnection, rs.getId());
             if (hasResults(response)) {
                 final String currentDn = response.getResult().getEntry().getDn();
 
-                Connection modifyConnection = null;
-                try {
-                    modifyConnection = getConnection();
-                    final ModifyOperation operation = new ModifyOperation(searchConnection);
+
+                try (final Connection modifyConnection = getConnection()) {
+                    final ModifyOperation operation = new ModifyOperation(modifyConnection);
 
                     final List<AttributeModification> mods = new ArrayList<>();
 
@@ -148,26 +150,20 @@ public final class LdapServiceRegistryDao implements ServiceRegistryDao {
                     }
                     final ModifyRequest request = new ModifyRequest(currentDn, mods.toArray(new AttributeModification[]{}));
                     operation.execute(request);
-                }catch (final LdapException e) {
+                } catch (final LdapException e) {
                     logger.error(e.getMessage(), e);
-                } finally {
-                    LdapUtils.closeConnection(modifyConnection);
                 }
             }
         } catch (final LdapException e) {
             logger.error(e.getMessage(), e);
-        } finally {
-            LdapUtils.closeConnection(searchConnection);
         }
         return rs;
     }
 
     @Override
     public boolean delete(final RegisteredService registeredService) {
-        Connection connection = null;
-        try {
-            connection = getConnection();
 
+        try (final Connection connection = getConnection()) {
             final Response<SearchResult> response = searchForServiceById(connection, registeredService.getId());
             if (hasResults(response)) {
                 final LdapEntry entry = response.getResult().getEntry();
@@ -178,19 +174,18 @@ public final class LdapServiceRegistryDao implements ServiceRegistryDao {
             }
         } catch (final LdapException e) {
             logger.error(e.getMessage(), e);
-        } finally {
-            LdapUtils.closeConnection(connection);
         }
-
         return false;
     }
 
     @Override
     public List<RegisteredService> load() {
-        Connection connection = null;
         final List<RegisteredService> list = new LinkedList<>();
-        try {
-            connection = getConnection();
+        if (ldapServiceMapper == null) {
+            return list;
+        }
+
+        try (final Connection connection = getConnection()) {
             final Response<SearchResult> response =
                     executeSearchOperation(connection, new SearchFilter(this.loadFilter));
             if (hasResults(response)) {
@@ -201,28 +196,24 @@ public final class LdapServiceRegistryDao implements ServiceRegistryDao {
             }
         } catch (final LdapException e) {
             logger.error(e.getMessage(), e);
-        } finally {
-            LdapUtils.closeConnection(connection);
         }
         return list;
     }
 
     @Override
     public RegisteredService findServiceById(final long id) {
-        Connection connection = null;
-        try {
-            connection = getConnection();
+        if (ldapServiceMapper == null) {
+            return null;
+        }
 
+        try (final Connection connection = getConnection()) {
             final Response<SearchResult> response = searchForServiceById(connection, id);
             if (hasResults(response)) {
                 return this.ldapServiceMapper.mapToRegisteredService(response.getResult().getEntry());
             }
         } catch (final LdapException e) {
             logger.error(e.getMessage(), e);
-        } finally {
-            LdapUtils.closeConnection(connection);
         }
-
         return null;
     }
 
