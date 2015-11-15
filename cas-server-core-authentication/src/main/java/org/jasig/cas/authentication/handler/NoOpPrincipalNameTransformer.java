fe7abdecb055eb984db55c3380b702aac8bb52b8
Misagh Moayyed <mm1844@gmail.com>
2015-11-15 14:53:09 -0700

    auto config of authn handlers

--- cas-server-core-authentication/src/main/java/org/jasig/cas/authentication/handler/NoOpPrincipalNameTransformer.java
+++ cas-server-core-authentication/src/main/java/org/jasig/cas/authentication/handler/NoOpPrincipalNameTransformer.java
@@ -1,5 +1,7 @@
 package org.jasig.cas.authentication.handler;
 
+import org.springframework.stereotype.Component;
+
 /**
  * Simple implementation that actually does NO transformation.
  *
@@ -7,6 +9,7 @@ package org.jasig.cas.authentication.handler;
 
  * @since 3.3.6
  */
+@Component("noOpPrincipalNameTransformer")
 public final class NoOpPrincipalNameTransformer implements PrincipalNameTransformer {
 
     @Override
