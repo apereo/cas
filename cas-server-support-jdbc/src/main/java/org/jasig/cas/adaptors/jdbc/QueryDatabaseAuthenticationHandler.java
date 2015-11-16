d59e56d1d6f371ef47a6637edebd1e1d89be5b7c
Misagh Moayyed <mm1844@gmail.com>
2015-11-16 11:23:48 -0700

    auto config of authn handlers

--- cas-server-support-jdbc/src/main/java/org/jasig/cas/adaptors/jdbc/QueryDatabaseAuthenticationHandler.java
+++ cas-server-support-jdbc/src/main/java/org/jasig/cas/adaptors/jdbc/QueryDatabaseAuthenticationHandler.java
@@ -1,13 +1,19 @@
 package org.jasig.cas.adaptors.jdbc;
 
+import org.apache.commons.lang3.StringUtils;
 import org.jasig.cas.authentication.HandlerResult;
 import org.jasig.cas.authentication.PreventedException;
 import org.jasig.cas.authentication.UsernamePasswordCredential;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.stereotype.Component;
 
 import javax.security.auth.login.AccountNotFoundException;
 import javax.security.auth.login.FailedLoginException;
+import javax.sql.DataSource;
 import javax.validation.constraints.NotNull;
 import java.security.GeneralSecurityException;
 
@@ -23,17 +29,19 @@ import java.security.GeneralSecurityException;
  *
  * @since 3.0.0
  */
+@Component("queryDatabaseAuthenticationHandler")
 public class QueryDatabaseAuthenticationHandler extends AbstractJdbcUsernamePasswordAuthenticationHandler {
 
     @NotNull
     private String sql;
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     protected final HandlerResult authenticateUsernamePasswordInternal(final UsernamePasswordCredential credential)
             throws GeneralSecurityException, PreventedException {
+
+        if (StringUtils.isBlank(this.sql) || getJdbcTemplate() == null) {
+            throw new GeneralSecurityException("Authentication handler is not configured correctly");
+        }
 
         final String username = credential.getUsername();
         final String encryptedPassword = this.getPasswordEncoder().encode(credential.getPassword());
@@ -57,7 +65,14 @@ public class QueryDatabaseAuthenticationHandler extends AbstractJdbcUsernamePass
     /**
      * @param sql The sql to set.
      */
-    public void setSql(final String sql) {
+    @Autowired
+    public void setSql(@Value("${cas.jdbc.authn.query.sql:}") final String sql) {
         this.sql = sql;
+    }
+
+    @Override
+    @Autowired(required = false)
+    public void setDataSource(@Qualifier("queryDatabaseDataSource") final DataSource dataSource) {
+        super.setDataSource(dataSource);
     }
 }
