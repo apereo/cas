f7a2d6769bd9386ab9d8069e94808401d4f42043
Misagh Moayyed <mm1844@gmail.com>
2015-11-16 11:23:48 -0700

    auto config of authn handlers

--- cas-server-support-jdbc/src/main/java/org/jasig/cas/adaptors/jdbc/BindModeSearchDatabaseAuthenticationHandler.java
+++ cas-server-support-jdbc/src/main/java/org/jasig/cas/adaptors/jdbc/BindModeSearchDatabaseAuthenticationHandler.java
@@ -1,11 +1,16 @@
 package org.jasig.cas.adaptors.jdbc;
 
+import org.apache.commons.lang3.StringUtils;
 import org.jasig.cas.authentication.HandlerResult;
 import org.jasig.cas.authentication.PreventedException;
 import org.jasig.cas.authentication.UsernamePasswordCredential;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.stereotype.Component;
 
 import javax.security.auth.login.FailedLoginException;
+import javax.sql.DataSource;
 import java.security.GeneralSecurityException;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -22,22 +27,37 @@ import java.sql.SQLException;
  *
  * @since 3.0.0
  */
+@Component("bindModeSearchDatabaseAuthenticationHandler")
 public class BindModeSearchDatabaseAuthenticationHandler extends AbstractJdbcUsernamePasswordAuthenticationHandler {
 
     @Override
     protected final HandlerResult authenticateUsernamePasswordInternal(final UsernamePasswordCredential credential)
             throws GeneralSecurityException, PreventedException {
 
+        if (getDataSource() == null) {
+            throw new GeneralSecurityException("Authentication handler is not configured correctly");
+        }
+
+        Connection connection = null;
         try {
             final String username = credential.getUsername();
             final String password = getPasswordEncoder().encode(credential.getPassword());
-            final Connection c = this.getDataSource().getConnection(username, password);
-            DataSourceUtils.releaseConnection(c, this.getDataSource());
+            connection = this.getDataSource().getConnection(username, password);
             return createHandlerResult(credential, this.principalFactory.createPrincipal(username), null);
         } catch (final SQLException e) {
             throw new FailedLoginException(e.getMessage());
         } catch (final Exception e) {
             throw new PreventedException("Unexpected SQL connection error", e);
+        } finally {
+            if (connection != null) {
+                DataSourceUtils.releaseConnection(connection, this.getDataSource());
+            }
         }
+    }
+
+    @Autowired(required = false)
+    @Override
+    public void setDataSource(@Qualifier("bindSearchDatabaseDataSource") final DataSource dataSource) {
+        super.setDataSource(dataSource);
     }
 }
