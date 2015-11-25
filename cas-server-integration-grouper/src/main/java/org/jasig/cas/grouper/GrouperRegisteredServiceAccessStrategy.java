d4b87429de58f86eebc77e9f845bb093218e965b
Misagh Moayyed <mm1844@gmail.com>
2015-11-25 11:22:11 -0700

    added grouper access strategy

---
+++ cas-server-integration-grouper/src/main/java/org/jasig/cas/grouper/GrouperRegisteredServiceAccessStrategy.java
@@ -0,0 +1,10 @@
+package org.jasig.cas.grouper;
+
+/**
+ * The {@link GrouperRegisteredServiceAccessStrategy} is responsible for...
+ *
+ * @author Misagh Moayyed
+ * @since 4.2
+ */
+public class GrouperRegisteredServiceAccessStrategy extends DefaultRegisteredServiceAccess {
+}
