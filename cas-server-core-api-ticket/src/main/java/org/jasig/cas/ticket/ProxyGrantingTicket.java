800e84a70fc55e83173a54be3c54178db869d8bf
Misagh Moayyed <mmoayyed@unicon.net>
2015-11-26 13:41:00 -0700

    introduce the PT api

--- cas-server-core-api-ticket/src/main/java/org/jasig/cas/ticket/ProxyGrantingTicket.java
+++ cas-server-core-api-ticket/src/main/java/org/jasig/cas/ticket/ProxyGrantingTicket.java
@@ -1,5 +1,7 @@
 package org.jasig.cas.ticket;
 
+import org.jasig.cas.authentication.principal.Service;
+
 /**
  * Interface for a proxy granting ticket. A proxy-granting ticket is an opaque string that is
  * used by a service to obtain proxy tickets for obtaining access to a back-end service on behalf of a client.
@@ -15,5 +17,20 @@ public interface ProxyGrantingTicket extends TicketGrantingTicket {
 
     /** The prefix to use when generating an id for a Proxy Granting Ticket IOU. */
     String PROXY_GRANTING_TICKET_IOU_PREFIX = "PGTIOU";
+
+    /**
+     * Grant a proxy ticket for a specific service.
+     *
+     * @param id The unique identifier for this ticket.
+     * @param service The service for which we are granting a ticket
+     * @param expirationPolicy the expiration policy.
+     * @param onlyTrackMostRecentSession track the most recent session by keeping the latest service ticket
+     * @return the service ticket granted to a specific service for the
+     * principal of the TicketGrantingTicket
+     */
+    ProxyTicket grantProxyTicket(String id, Service service,
+                                 ExpirationPolicy expirationPolicy,
+                                 boolean onlyTrackMostRecentSession);
+
 }
 
