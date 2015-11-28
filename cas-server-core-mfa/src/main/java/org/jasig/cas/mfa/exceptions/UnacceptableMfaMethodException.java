2465c6ce33461b13012f6772addfb25af4219489
Misagh Moayyed <mmoayyed@unicon.net>
2015-11-28 15:59:51 -0700

    Added translators for authn methods

--- cas-server-core-mfa/src/main/java/org/jasig/cas/mfa/exceptions/UnacceptableMfaMethodException.java
+++ cas-server-core-mfa/src/main/java/org/jasig/cas/mfa/exceptions/UnacceptableMfaMethodException.java
@@ -5,17 +5,28 @@ package org.jasig.cas.mfa.exceptions;
  * when the request authentication method cannot be accepted/provided
  * by this CAS server.
  * @author Misagh Moayyed
+ * @since 4.2
  */
-public class UnacceptableMfaMethodException extends AbstractMfaTicketValidationException {
-    private static final long serialVersionUID = -8544747236126342213L;
+public class UnacceptableMfaMethodException extends RuntimeException {
+    private static final long serialVersionUID = 7880539766094343828L;
+
+    private final String authenticationMethod;
+    private final String code = "unacceptable.authentication.method";
 
     /**
-     * Constructor to spin up the exception instance.
-     * @param code  error code
-     * @param msg error message
-     * @param authnMethod authentication method associated with the error
+     * Initialize the exception object.
+     * @param msg the error message describing this exception
+     * @param authnMethod the authentication method requested
      */
-    public UnacceptableMfaMethodException(final String code, final String msg, final String authnMethod) {
-        super(code, msg, authnMethod);
+    public UnacceptableMfaMethodException(final String msg, final String authnMethod) {
+        this.authenticationMethod = authnMethod;
+    }
+
+    public final String getAuthenticationMethod() {
+        return this.authenticationMethod;
+    }
+
+    public final String getCode() {
+        return this.code;
     }
 }
