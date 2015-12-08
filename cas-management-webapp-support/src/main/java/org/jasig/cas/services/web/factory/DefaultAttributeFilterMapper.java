89981c3b9d3926996071a9977678888a4238a208
Daniel Frett <daniel.frett@ccci.org>
2015-12-08 17:20:51 -0500

    move ServiceData => RegisteredService logic to extensible factory model

--- cas-management-webapp-support/src/main/java/org/jasig/cas/services/web/factory/DefaultAttributeFilterMapper.java
+++ cas-management-webapp-support/src/main/java/org/jasig/cas/services/web/factory/DefaultAttributeFilterMapper.java
@@ -1,5 +1,6 @@
 package org.jasig.cas.services.web.factory;
 
+import org.apache.commons.lang3.StringUtils;
 import org.jasig.cas.services.RegisteredServiceAttributeFilter;
 import org.jasig.cas.services.support.RegisteredServiceRegexAttributeFilter;
 import org.jasig.cas.services.web.beans.RegisteredServiceEditBean.ServiceData;
@@ -17,5 +18,15 @@ public class DefaultAttributeFilterMapper implements AttributeFilterMapper {
             final RegisteredServiceRegexAttributeFilter regex = (RegisteredServiceRegexAttributeFilter) filter;
             bean.getAttrRelease().setAttrFilter(regex.getPattern().pattern());
         }
+    }
+
+    @Override
+    public RegisteredServiceAttributeFilter toAttributeFilter(final ServiceData data) {
+        final String filter = data.getAttrRelease().getAttrFilter();
+        if (StringUtils.isNotBlank(filter)) {
+            return new RegisteredServiceRegexAttributeFilter(filter);
+        }
+
+        return null;
     }
 }
