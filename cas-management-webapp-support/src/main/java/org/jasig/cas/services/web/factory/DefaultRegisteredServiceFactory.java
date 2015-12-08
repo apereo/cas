8e0e4d7d9ebb5880ff0c0e4168a2cdad0d6e4e9e
Daniel Frett <daniel.frett@ccci.org>
2015-12-08 15:17:06 -0500

    move base RegisteredService => ServiceData logic to overridable mapper

--- cas-management-webapp-support/src/main/java/org/jasig/cas/services/web/factory/DefaultRegisteredServiceFactory.java
+++ cas-management-webapp-support/src/main/java/org/jasig/cas/services/web/factory/DefaultRegisteredServiceFactory.java
@@ -1,21 +1,12 @@
 package org.jasig.cas.services.web.factory;
 
-import org.jasig.cas.services.LogoutType;
 import org.jasig.cas.services.RegisteredService;
-import org.jasig.cas.services.RegisteredServicePublicKey;
 import org.jasig.cas.services.web.beans.RegisteredServiceEditBean.ServiceData;
-import org.jasig.cas.services.web.beans.RegisteredServiceLogoutTypeEditBean;
-import org.jasig.cas.services.web.beans.RegisteredServiceOAuthTypeEditBean;
-import org.jasig.cas.services.web.beans.RegisteredServicePublicKeyEditBean;
-import org.jasig.cas.services.web.beans.RegisteredServiceTypeEditBean;
-import org.jasig.cas.support.oauth.services.OAuthRegisteredCallbackAuthorizeService;
-import org.jasig.cas.support.oauth.services.OAuthRegisteredService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.validation.constraints.NotNull;
-import java.net.URL;
 
 /**
  * Default implmentation of {@link RegisteredServiceFactory}.
@@ -45,6 +36,10 @@ public final class DefaultRegisteredServiceFactory implements RegisteredServiceF
     @NotNull
     @Autowired(required = false)
     private ProxyPolicyMapper proxyPolicyMapper = new DefaultProxyPolicyMapper();
+
+    @NotNull
+    @Autowired(required = false)
+    private RegisteredServiceMapper registeredServiceMapper = new DefaultRegisteredServiceMapper();
 
     @NotNull
     @Autowired(required = false)
@@ -94,59 +89,12 @@ public final class DefaultRegisteredServiceFactory implements RegisteredServiceF
     public ServiceData createServiceData(final RegisteredService svc) {
         final ServiceData bean = new ServiceData();
 
-        bean.setAssignedId(svc.getId());
-        bean.setServiceId(svc.getServiceId());
-        bean.setName(svc.getName());
-        bean.setDescription(svc.getDescription());
-        if (svc.getLogo() != null) {
-            bean.setLogoUrl(svc.getLogo().toExternalForm());
-        }
-        bean.setRequiredHandlers(svc.getRequiredHandlers());
-
+        registeredServiceMapper.mapRegisteredService(svc, bean);
         accessStrategyMapper.mapAccessStrategy(svc.getAccessStrategy(), bean);
-
-        if (svc instanceof OAuthRegisteredCallbackAuthorizeService) {
-            bean.setType(RegisteredServiceTypeEditBean.OAUTH_CALLBACK_AUTHZ.toString());
-        }
-
-        if (svc instanceof OAuthRegisteredService) {
-            bean.setType(RegisteredServiceTypeEditBean.OAUTH.toString());
-            final OAuthRegisteredService oauth = (OAuthRegisteredService) svc;
-            final RegisteredServiceOAuthTypeEditBean oauthBean = bean.getOauth();
-            oauthBean.setBypass(oauth.isBypassApprovalPrompt());
-            oauthBean.setClientId(oauth.getClientId());
-            oauthBean.setClientSecret(oauth.getClientSecret());
-        }
-
-        bean.setTheme(svc.getTheme());
-        bean.setEvalOrder(svc.getEvaluationOrder());
-        final LogoutType logoutType = svc.getLogoutType();
-        switch (logoutType) {
-            case BACK_CHANNEL:
-                bean.setLogoutType(RegisteredServiceLogoutTypeEditBean.BACK.toString());
-                break;
-            case FRONT_CHANNEL:
-                bean.setLogoutType(RegisteredServiceLogoutTypeEditBean.FRONT.toString());
-                break;
-            default:
-                bean.setLogoutType(RegisteredServiceLogoutTypeEditBean.NONE.toString());
-                break;
-        }
-        final URL url = svc.getLogoutUrl();
-        if (url != null) {
-            bean.setLogoutUrl(url.toExternalForm());
-        }
-
         usernameAttributeProviderMapper.mapUsernameAttributeProvider(svc.getUsernameAttributeProvider(), bean);
-
-        final RegisteredServicePublicKey key = svc.getPublicKey();
-        final RegisteredServicePublicKeyEditBean pBean = bean.getPublicKey();
-        if (key != null) {
-            pBean.setAlgorithm(key.getAlgorithm());
-            pBean.setLocation(key.getLocation());
-        }
         proxyPolicyMapper.mapProxyPolicy(svc.getProxyPolicy(), bean);
         attributeReleasePolicyMapper.mapAttributeReleasePolicy(svc.getAttributeReleasePolicy(), bean);
+
         return bean;
     }
 }
