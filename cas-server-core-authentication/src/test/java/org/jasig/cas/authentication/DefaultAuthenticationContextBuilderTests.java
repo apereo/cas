f9db29d5b7d8b16841c4232b28a9b367f1673a24
Misagh Moayyed <mm1844@gmail.com>
2015-12-10 03:10:56 -0700

    fixed prop resolution for jetty

--- cas-server-core-authentication/src/test/java/org/jasig/cas/authentication/DefaultAuthenticationContextBuilderTests.java
+++ cas-server-core-authentication/src/test/java/org/jasig/cas/authentication/DefaultAuthenticationContextBuilderTests.java
@@ -44,14 +44,14 @@ public class DefaultAuthenticationContextBuilderTests {
     }
 
     @Test
-    public void checkBuilderWithManyAuthN() throws Exception {
+    public void checkBuilderWithManyAuthNAsDuplicate() throws Exception {
         final Authentication authN1 = manager.authenticate(TestUtils.getCredentialsWithSameUsernameAndPassword());
         builder.collect(authN1);
 
         final Authentication authN2 = manager.authenticate(TestUtils.getCredentialsWithSameUsernameAndPassword());
         builder.collect(authN2);
 
-        assertEquals(builder.size(), 2);
+        assertEquals(builder.size(), 1);
 
         final AuthenticationContext ctx = builder.build();
         assertNotNull(ctx);
