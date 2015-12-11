288b90733a75eca10862774bbf1c9bb976e38756
Misagh Moayyed <mm1844@gmail.com>
2015-12-11 00:10:26 -0700

    removed unused imports

--- cas-server-support-mongo/src/test/java/org/jasig/cas/authentication/MongoAuthenticationHandlerTests.java
+++ cas-server-support-mongo/src/test/java/org/jasig/cas/authentication/MongoAuthenticationHandlerTests.java
@@ -4,11 +4,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.test.annotation.IfProfileValue;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Test cases for {@link MongoAuthenticationHandler}.
