f5d31136d26f943b1581a2b384178f024ac16bf1
Misagh Moayyed <mm1844@gmail.com>
2015-12-12 09:14:53 -0700

    Merge branch 'master' into jwt-authN

--- cas-server-support-token/src/main/java/org/jasig/cas/authentication/handler/support/TokenCredential.java
+++ cas-server-support-token/src/main/java/org/jasig/cas/authentication/handler/support/TokenCredential.java
@@ -1,13 +1,10 @@
 package org.jasig.cas.authentication.handler.support;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.jasig.cas.authentication.BasicIdentifiableCredential;
-import org.jasig.cas.authentication.token.Token;
-import org.jasig.cas.authentication.token.TokenAttributes;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.jasig.cas.authentication.principal.Service;
 
 import javax.validation.constraints.NotNull;
-import java.util.Map;
 
 /**
  * This is {@link TokenCredential} that represents the user credentials in form of an encrypted token.
@@ -15,65 +12,34 @@ import java.util.Map;
  * @author Misagh Moayyed
  * @since 4.2.0
  */
-public class TokenCredential extends BasicIdentifiableCredential {
+public final class TokenCredential extends BasicIdentifiableCredential {
+    /** Token parameter passed to the request. */
+    public static final String TOKEN_PARAMETER = "token";
+
     private static final long serialVersionUID = 2749515041385101770L;
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(TokenCredential.class);
-
     @NotNull
-    private Token token;
-
-    @NotNull
-    private String username;
-
-    @NotNull
-    private String service;
-
-    private Map<String, Object> userAttributes;
+    private final Service service;
 
     /**
      * Instantiates a new Token credential.
-     *
-     * @param username the username
      * @param tokenId    the token
      * @param service  the service
      */
-    public TokenCredential(final String username, final String tokenId, final String service) {
+    public TokenCredential(final String tokenId, final Service service) {
         super(tokenId);
-        this.token = new Token(tokenId);
         this.service = service;
-        this.username = username;
     }
 
-    public final void setToken(final Token token) {
-        this.token = token;
-    }
-
-    public final Token getToken() {
-        return this.token;
-    }
-
-    public final String getService() {
+    public Service getService() {
         return this.service;
     }
 
-    public void setService(final String service) {
-        this.service = service;
-    }
-
-    public final void setUsername(final String username) {
-        this.username = username;
-    }
-
-    public final String getUsername() {
-        return this.username;
-    }
-
-    public final Map<String, Object> getUserAttributes() {
-        return this.userAttributes;
-    }
-
-    public void setUserAttributes(final TokenAttributes userProfile) {
-        this.userAttributes = userProfile;
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .appendSuper(super.toString())
+                .append("service", service)
+                .toString();
     }
 }
