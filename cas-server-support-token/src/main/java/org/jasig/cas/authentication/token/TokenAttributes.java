1492dd0de7c58f9fe7fb17dc8b9f8965e64b14e3
Misagh Moayyed <mmoayyed@unicon.net>
2015-12-11 23:02:53 -0700

    initial pass at jwt

---
+++ cas-server-support-token/src/main/java/org/jasig/cas/authentication/token/TokenAttributes.java
@@ -0,0 +1,13 @@
+package org.jasig.cas.authentication.token;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * This is {@link TokenAttributes}.
+ *
+ * @author Misagh Moayyed
+ * @since 4.2.0
+ */
+public class TokenAttributes extends ConcurrentHashMap<String, Object> {
+
+}
