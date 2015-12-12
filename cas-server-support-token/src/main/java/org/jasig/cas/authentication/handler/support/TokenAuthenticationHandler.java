b421b0245a2b460b154ced5e04fcfe1797b333fb
Misagh Moayyed <mm1844@gmail.com>
2015-12-12 09:14:53 -0700

    Merge branch 'master' into jwt-authN

--- cas-server-support-token/src/main/java/org/jasig/cas/authentication/handler/support/TokenAuthenticationHandler.java
+++ cas-server-support-token/src/main/java/org/jasig/cas/authentication/handler/support/TokenAuthenticationHandler.java
@@ -1,22 +1,19 @@
 package org.jasig.cas.authentication.handler.support;
 
+import org.apache.commons.lang3.StringUtils;
 import org.jasig.cas.authentication.Credential;
 import org.jasig.cas.authentication.handler.PrincipalNameTransformer;
-import org.jasig.cas.authentication.token.Token;
-import org.jasig.cas.authentication.token.TokenKey;
-import org.jasig.cas.authentication.token.TokenKeystore;
 import org.jasig.cas.integration.pac4j.authentication.handler.support.TokenWrapperAuthenticationHandler;
+import org.jasig.cas.services.RegisteredService;
+import org.jasig.cas.services.RegisteredServiceProperty;
+import org.jasig.cas.services.ServicesManager;
+import org.jasig.cas.services.UnauthorizedServiceException;
 import org.pac4j.jwt.credentials.authenticator.JwtAuthenticator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import javax.security.auth.login.CredentialNotFoundException;
 import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * This is {@link TokenAuthenticationHandler} that authenticates instances of {@link TokenCredential}.
@@ -26,74 +23,34 @@ import java.util.Map;
  * @author Misagh Moayyed
  * @since 4.2.0
  */
+@Component("tokenAuthenticationHandler")
 public class TokenAuthenticationHandler extends TokenWrapperAuthenticationHandler {
     @NotNull
     @Autowired
-    @Qualifier("tokenAuthenticationKeystore")
-    private TokenKeystore keystore;
-
-    /**
-     * A list of required attributes that will be passed along to the
-     * {@link org.jasig.cas.authentication.token.TokenAttributes} instance.
-     **/
-    @NotNull
-    @Autowired(required=false)
-    @Qualifier("requiredTokenAuthenticationAttributes")
-    private List requiredTokenAttributes = new ArrayList();
-
-    /**
-     * A map of attribute names to {@link org.jasig.cas.authentication.token.TokenAttributes}
-     * properties that will be passed along.
-     **/
-    @NotNull
-    @Autowired(required=false)
-    @Qualifier("tokenAuthenticationAttributesMap")
-    private Map tokenAttributesMap = new HashMap();
-
-    /**
-     * Maximum amount of time (before or after current time) that the 'generated' parameter
-     * in the supplied token can differ from the server.
-     **/
-    private int maxDrift;
-
-    /**
-     * Initialize.
-     */
-    @PostConstruct
-    public void initialize() {
-        final JwtAuthenticator tokenAuthenticator = new JwtAuthenticator();
-        setAuthenticator(tokenAuthenticator);
-    }
+    @Qualifier("servicesManager")
+    private ServicesManager servicesManager;
 
     @Override
     protected boolean preAuthenticate(final Credential credential) {
         final TokenCredential tokenCredential = (TokenCredential) credential;
-        final TokenKey apiKey = this.keystore.get(tokenCredential.getService());
-        if (apiKey == null) {
-            logger.warn("API key not found in the token keystore for [{}]", tokenCredential.getService());
-            throw new RuntimeException(new CredentialNotFoundException("error.authentication.credentials.bad.token.apikey"));
+        final RegisteredService service = this.servicesManager.findServiceBy(tokenCredential.getService());
+        if (service == null || !service.getAccessStrategy().isServiceAccessAllowed()) {
+            throw new UnauthorizedServiceException(UnauthorizedServiceException.CODE_UNAUTHZ_SERVICE);
         }
 
-        // Configure the credential's token so that it can be decrypted.
-        final Token token = tokenCredential.getToken();
-        token.setKey(apiKey);
-        token.setRequiredTokenAttributes(this.requiredTokenAttributes);
-        token.setTokenAttributesMap(this.tokenAttributesMap);
-        tokenCredential.setToken(token);
+        if (service.getProperties().containsKey(TokenCredential.TOKEN_PARAMETER)) {
+            final RegisteredServiceProperty prop = service.getProperties().get(TokenCredential.TOKEN_PARAMETER);
+            final String tokenSecret = prop.getValue();
 
-        try {
-            tokenCredential.setUserAttributes(token.getAttributes());
-        } catch (final Exception e) {
-            logger.warn("Could not decrypt token", e);
-            throw new RuntimeException(new CredentialNotFoundException("error.authentication.credentials.bad.token.key"));
+            if (StringUtils.isNotBlank(tokenSecret)) {
+                final JwtAuthenticator tokenAuthenticator = new JwtAuthenticator(tokenSecret);
+                setAuthenticator(tokenAuthenticator);
+                return super.preAuthenticate(credential);
+            }
         }
-
-        if (!token.getAttributes().isValid()) {
-            logger.warn("Invalid token attributes detected.");
-            throw new RuntimeException(new CredentialNotFoundException("error.authentication.credentials.missing.required.attributes"));
-        }
-
-        return super.preAuthenticate(credential);
+        logger.warn("No token secret is defined for service [{}]. Ensure [{}] property is defined for service",
+                service.getServiceId(), TokenCredential.TOKEN_PARAMETER);
+        return false;
     }
 
     @Autowired(required=false)
@@ -105,19 +62,4 @@ public class TokenAuthenticationHandler extends TokenWrapperAuthenticationHandle
         }
     }
 
-    public TokenKeystore getKeystore() {
-        return keystore;
-    }
-
-    public List getRequiredTokenAttributes() {
-        return requiredTokenAttributes;
-    }
-
-    public Map getTokenAttributesMap() {
-        return tokenAttributesMap;
-    }
-
-    public int getMaxDrift() {
-        return maxDrift;
-    }
 }
