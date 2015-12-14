aaf0e50ed7e35967fe4f3bc3805ff9b9e35445c3
Misagh Moayyed <mm1844@gmail.com>
2015-12-14 08:54:00 -0700

    updated tests

--- cas-server-core-api-authentication/src/main/java/org/jasig/cas/authentication/AuthenticationContextBuilder.java
+++ cas-server-core-api-authentication/src/main/java/org/jasig/cas/authentication/AuthenticationContextBuilder.java
@@ -1,12 +1,17 @@
 package org.jasig.cas.authentication;
 
+import java.io.Serializable;
+
 /**
- * This is {@link AuthenticationContextBuilder}.
+ * This is {@link AuthenticationContextBuilder}. It attempts to collect authentication objects
+ * and will put the result into an authentication context. Implementations MUST BE thread-safe.
+ * Instances of this class should never be declared as a field. Rather they should always be passed
+ * around to methods that need them.
  *
  * @author Misagh Moayyed
  * @since 4.2.0
  */
-public interface AuthenticationContextBuilder {
+public interface AuthenticationContextBuilder extends Serializable {
 
     /**
      * Collect authentication context builder.
