5f6509ae611a712e247dea6a4603e71fdc7a30ea
Misagh Moayyed <mmoayyed@unicon.net>
2015-12-16 23:58:16 -0700

    fixed test cases

--- cas-server-core-authentication/src/main/java/org/jasig/cas/authentication/principal/DefaultPrincipalFactory.java
+++ cas-server-core-authentication/src/main/java/org/jasig/cas/authentication/principal/DefaultPrincipalFactory.java
@@ -1,5 +1,6 @@
 package org.jasig.cas.authentication.principal;
 
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
@@ -24,4 +25,23 @@ public final class DefaultPrincipalFactory implements PrincipalFactory {
         return new SimplePrincipal(id, attributes);
     }
 
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj == this) {
+            return true;
+        }
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(13, 33).toHashCode();
+    }
 }
