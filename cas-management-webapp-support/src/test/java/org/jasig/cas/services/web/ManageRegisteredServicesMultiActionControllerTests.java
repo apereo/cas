62cbff56d9b88e9b0d65eb124e205f274cb10577
Daniel Frett <daniel.frett@ccci.org>
2015-12-18 15:56:42 -0500

    clarify documentation for customComponents in the ViewBean
    also, extend tests to excercise the customComponent functionality

--- cas-management-webapp-support/src/test/java/org/jasig/cas/services/web/ManageRegisteredServicesMultiActionControllerTests.java
+++ cas-management-webapp-support/src/test/java/org/jasig/cas/services/web/ManageRegisteredServicesMultiActionControllerTests.java
@@ -1,9 +1,15 @@
 package org.jasig.cas.services.web;
 
+import com.google.common.collect.ImmutableMap;
 import org.jasig.cas.services.DefaultServicesManagerImpl;
 import org.jasig.cas.services.InMemoryServiceRegistryDaoImpl;
+import org.jasig.cas.services.RegisteredService;
 import org.jasig.cas.services.RegisteredServiceImpl;
+import org.jasig.cas.services.web.beans.RegisteredServiceEditBean.ServiceData;
+import org.jasig.cas.services.web.beans.RegisteredServiceViewBean;
 import org.jasig.cas.services.web.factory.DefaultRegisteredServiceFactory;
+import org.jasig.cas.services.web.factory.DefaultRegisteredServiceMapper;
+import org.jasig.cas.services.web.factory.RegisteredServiceMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -84,6 +90,7 @@ public class ManageRegisteredServicesMultiActionControllerTests {
         final RegisteredServiceImpl r = new RegisteredServiceImpl();
         r.setId(1200);
         r.setName("name");
+        r.setDescription("uniqueDescription");
         r.setServiceId("test");
         r.setEvaluationOrder(2);
 
@@ -96,6 +103,56 @@ public class ManageRegisteredServicesMultiActionControllerTests {
         assertTrue(mv.getModel().containsKey("status"));
 
         this.controller.getServices(response);
-        assertTrue(response.getContentAsString().contains("services"));
+        final String content = response.getContentAsString();
+        assertTrue(content.contains("services"));
+        assertTrue(content.contains("uniqueDescription"));
+    }
+
+    @Test
+    public void verifyCustomComponents() throws Exception {
+        // override the RegisteredServiceMapper
+        this.registeredServiceFactory.setRegisteredServiceMapper(new CustomRegisteredServiceMapper());
+
+        final RegisteredServiceImpl r = new RegisteredServiceImpl();
+        r.setId(1200);
+        r.setName("name");
+        r.setDescription("uniqueDescription");
+        r.setServiceId("test");
+        r.setEvaluationOrder(2);
+
+        this.servicesManager.save(r);
+
+        final MockHttpServletResponse response = new MockHttpServletResponse();
+        final ModelAndView mv = this.controller.manage(response);
+
+        assertTrue(mv.getModel().containsKey("defaultServiceUrl"));
+        assertTrue(mv.getModel().containsKey("status"));
+
+        this.controller.getServices(response);
+        final String content = response.getContentAsString();
+        assertTrue(content.contains("services"));
+        assertTrue(content.contains("uniqueDescription"));
+        assertTrue(content.contains("customComponent1"));
+        assertTrue(content.contains("key2"));
+    }
+
+    private static class CustomRegisteredServiceMapper implements RegisteredServiceMapper {
+        private final RegisteredServiceMapper base = new DefaultRegisteredServiceMapper();
+
+        @Override
+        public void mapRegisteredService(final RegisteredService svc, final ServiceData bean) {
+            base.mapRegisteredService(svc, bean);
+        }
+
+        @Override
+        public void mapRegisteredService(final RegisteredService svc, final RegisteredServiceViewBean bean) {
+            base.mapRegisteredService(svc, bean);
+            bean.setCustomComponent("customComponent1", ImmutableMap.of("key1", "string", "key2", 100));
+        }
+
+        @Override
+        public RegisteredService toRegisteredService(final ServiceData data) {
+            return base.toRegisteredService(data);
+        }
     }
 }
