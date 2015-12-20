d51b171bb5b8545784cb4ee9abc75997cb36c31f
Misagh Moayyed <mm1844@gmail.com>
2015-12-20 04:00:02 -0700

    API updates

--- cas-server-core-api-authentication/src/main/java/org/jasig/cas/authentication/AuthenticationTransaction.java
+++ cas-server-core-api-authentication/src/main/java/org/jasig/cas/authentication/AuthenticationTransaction.java
@@ -53,6 +53,9 @@ public final class AuthenticationTransaction implements Serializable {
                     it.remove();
                 }
             }
+            if (set.isEmpty()) {
+                throw new RuntimeException("Credential specified for authentication were all null and removed from the collection");
+            }
             return set;
         }
         return Collections.emptySet();
