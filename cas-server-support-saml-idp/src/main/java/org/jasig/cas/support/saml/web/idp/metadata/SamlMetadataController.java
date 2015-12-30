45f899184d4e4ab31b9f7beeed4eed06b481fde5
Misagh Moayyed <mm1844@gmail.com>
2015-12-30 07:08:50 -0700

    cleaned up metadata gen components

--- cas-server-support-saml-idp/src/main/java/org/jasig/cas/support/saml/web/idp/metadata/SamlMetadataController.java
+++ cas-server-support-saml-idp/src/main/java/org/jasig/cas/support/saml/web/idp/metadata/SamlMetadataController.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
