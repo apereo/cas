ccc328180e9f93fa46d529a676431795576d9136
Misagh Moayyed <mm1844@gmail.com>
2015-12-31 04:09:37 -0700

    removed unused imports

--- cas-server-webapp-support/src/main/java/org/jasig/cas/web/view/Cas30ResponseView.java
+++ cas-server-webapp-support/src/main/java/org/jasig/cas/web/view/Cas30ResponseView.java
@@ -18,17 +18,13 @@
  */
 package org.jasig.cas.web.view;
 
-import org.apache.commons.lang3.StringUtils;
 import org.jasig.cas.CasProtocolConstants;
 import org.jasig.cas.authentication.principal.Service;
 import org.jasig.cas.services.RegisteredService;
-import org.jasig.cas.services.ServicesManager;
-import org.jasig.cas.authentication.support.CasAttributeEncoder;
 import org.springframework.web.servlet.view.AbstractUrlBasedView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.validation.constraints.NotNull;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
