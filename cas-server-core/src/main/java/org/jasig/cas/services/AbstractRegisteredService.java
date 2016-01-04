9322c7355f1af5212a1cae7190bb4d4ac742f515
Misagh Moayyed <mm1844@gmail.com>
2016-01-04 09:08:38 -0700

    backport of service properties for 4.1

--- cas-server-core/src/main/java/org/jasig/cas/services/AbstractRegisteredService.java
+++ cas-server-core/src/main/java/org/jasig/cas/services/AbstractRegisteredService.java
@@ -27,21 +27,27 @@ import org.apache.commons.lang3.builder.ToStringStyle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorColumn;
 import javax.persistence.DiscriminatorType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
+import javax.persistence.JoinTable;
 import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 import javax.persistence.PostLoad;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -134,6 +140,11 @@ public abstract class AbstractRegisteredService implements RegisteredService, Co
     @Column(name = "public_key", nullable = true, length = Integer.MAX_VALUE)
     private RegisteredServicePublicKey publicKey;
 
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name="RegisteredServiceImpl_Properties")
+    private Map<String, DefaultRegisteredServiceProperty> properties = new HashMap<>();
+
     @Override
     public long getId() {
         return this.id;
@@ -199,6 +210,9 @@ public abstract class AbstractRegisteredService implements RegisteredService, Co
         if (this.attributeReleasePolicy == null) {
             this.attributeReleasePolicy = new ReturnAllowedAttributeReleasePolicy();
         }
+        if (this.properties == null) {
+            this.properties = new HashMap<>();
+        }
     }
 
     @Override
@@ -233,6 +247,7 @@ public abstract class AbstractRegisteredService implements RegisteredService, Co
                 .append(this.publicKey, that.publicKey)
                 .append(this.logoutUrl, that.logoutUrl)
                 .append(this.requiredHandlers, that.requiredHandlers)
+                .append(this.properties, that.properties)
                 .isEquals();
     }
 
@@ -253,6 +268,7 @@ public abstract class AbstractRegisteredService implements RegisteredService, Co
                 .append(this.publicKey)
                 .append(this.logoutUrl)
                 .append(this.requiredHandlers)
+                .append(this.properties)
                 .toHashCode();
     }
 
@@ -359,7 +375,7 @@ public abstract class AbstractRegisteredService implements RegisteredService, Co
         this.setLogoutUrl(source.getLogoutUrl());
         this.setPublicKey(source.getPublicKey());
         this.setRequiredHandlers(source.getRequiredHandlers());
-
+        this.setProperties(source.getProperties());
     }
 
     /**
@@ -396,6 +412,7 @@ public abstract class AbstractRegisteredService implements RegisteredService, Co
         toStringBuilder.append("logo", this.logo);
         toStringBuilder.append("logoutUrl", this.logoutUrl);
         toStringBuilder.append("requiredHandlers", this.requiredHandlers);
+        toStringBuilder.append("properties", this.properties);
 
         return toStringBuilder.toString();
     }
@@ -460,5 +477,14 @@ public abstract class AbstractRegisteredService implements RegisteredService, Co
 
     public void setPublicKey(@NotNull final RegisteredServicePublicKey publicKey) {
         this.publicKey = publicKey;
+    }
+
+    @Override
+    public Map<String, RegisteredServiceProperty> getProperties() {
+        return (Map) this.properties;
+    }
+
+    public void setProperties(final Map<String, RegisteredServiceProperty> properties) {
+        this.properties = (Map) properties;
     }
 }
