5561cf13aa472a3cef61952aeee7c98b34701497
Misagh Moayyed <mmoayyed@unicon.net>
2016-01-16 07:48:23 -0700

    Provide support for a groovy shell based on the CAS context

---
+++ cas-server-webapp/src/main/resources/scripts/CasVersion.groovy
@@ -0,0 +1,12 @@
+package scripts
+
+def class CasVersion {
+    def static run(def ctx) {
+        def output = "CAS version: " + org.jasig.cas.CasVersion.getVersion()
+
+        def ticketRegistry = ctx.getBean("ticketRegistry");
+        output += "\nTicket registry instance: " + ticketRegistry.getClass().getSimpleName()
+
+        return output
+    }
+}
