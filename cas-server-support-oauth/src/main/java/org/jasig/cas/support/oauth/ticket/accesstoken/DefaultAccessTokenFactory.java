401191a3e7cb104b41733b8c3b60f22a41208f40
Jérôme LELEU <jerome.leleu@sfr.com>
2016-01-19 12:04:04 +0100

    fix checkstyle and tests

--- cas-server-support-oauth/src/main/java/org/jasig/cas/support/oauth/ticket/accesstoken/DefaultAccessTokenFactory.java
+++ cas-server-support-oauth/src/main/java/org/jasig/cas/support/oauth/ticket/accesstoken/DefaultAccessTokenFactory.java
@@ -2,7 +2,12 @@ package org.jasig.cas.support.oauth.ticket.accesstoken;
 
 import org.jasig.cas.authentication.Authentication;
 import org.jasig.cas.authentication.principal.Service;
-import org.jasig.cas.ticket.*;
+import org.jasig.cas.ticket.AccessToken;
+import org.jasig.cas.ticket.AccessTokenImpl;
+import org.jasig.cas.ticket.ExpirationPolicy;
+import org.jasig.cas.ticket.Ticket;
+import org.jasig.cas.ticket.TicketFactory;
+import org.jasig.cas.ticket.UniqueTicketIdGenerator;
 import org.jasig.cas.util.DefaultUniqueTicketIdGenerator;
 import org.pac4j.core.util.CommonHelper;
 import org.slf4j.Logger;
