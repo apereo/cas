b94347b73841b0a0b85fed645fb86b17589c2bca
Jérôme LELEU <leleuj@gmail.com>
2016-01-26 16:55:17 +0100

    fix checkstyle

--- cas-server-core-tickets/src/main/java/org/jasig/cas/ticket/registry/AbstractTicketRegistry.java
+++ cas-server-core-tickets/src/main/java/org/jasig/cas/ticket/registry/AbstractTicketRegistry.java
@@ -21,7 +21,11 @@ import org.springframework.util.Assert;
 import javax.annotation.Nullable;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Scott Battaglia
