de3918ef03ef79fe56fbc970a23136ea976e6570
Misagh Moayyed <mmoayyed@unicon.net>
2016-01-27 09:36:27 -0700

    fixed checkstyle issues

--- cas-management-webapp/src/main/java/org/jasig/cas/services/web/beans/RegisteredServiceEditBean.java
+++ cas-management-webapp/src/main/java/org/jasig/cas/services/web/beans/RegisteredServiceEditBean.java
@@ -68,8 +68,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
-import java.util.regex.Pattern;
-import java.util.regex.PatternSyntaxException;
 
 import static org.slf4j.LoggerFactory.*;
 
