1b3035c56bf01d650b0cd0bd6b6abde74aabe1ad
Jérôme LELEU <leleuj@gmail.com>
2016-02-05 16:03:24 +0100

    Support implicit grant type for OAuth

---
+++ cas-server-support-oauth/src/main/java/org/jasig/cas/support/oauth/web/OAuthResponseType.java
@@ -0,0 +1,11 @@
+package org.jasig.cas.support.oauth.web;
+
+/**
+ * The OAuth response types.
+ *
+ * @author Jerome Leleu
+ * @since 4.3.0
+ */
+public enum OAuthResponseType {
+    CODE, TOKEN
+}
