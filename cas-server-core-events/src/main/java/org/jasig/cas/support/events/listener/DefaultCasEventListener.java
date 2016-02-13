7974539d14a8d29a9f327b65581903859cb8e6b6
Misagh Moayyed <mmoayyed@unicon.net>
2016-02-13 04:14:37 -0700

    added mongodb event listener

--- cas-server-core-events/src/main/java/org/jasig/cas/support/events/listener/DefaultCasEventListener.java
+++ cas-server-core-events/src/main/java/org/jasig/cas/support/events/listener/DefaultCasEventListener.java
@@ -1,26 +1,52 @@
 package org.jasig.cas.support.events.listener;
 
 import org.jasig.cas.support.events.CasTicketGrantingTicketCreatedEvent;
-import org.jasig.cas.support.events.dao.CasEventsRepository;
+import org.jasig.cas.support.events.dao.CasEventDTO;
+import org.jasig.cas.support.events.dao.CasEventRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionalEventListener;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * This is {@link DefaultCasEventListener} that attempts to consume CAS events
  * upon various authentication events. Event data is persisted into a repository
- * via {@link CasEventsRepository}.
+ * via {@link CasEventRepository}.
  *
  * @author Misagh Moayyed
  * @since 4.3.0
  */
-@Component("mongoDbCasEventListener")
+@Component("defaultCasEventListener")
 public class DefaultCasEventListener {
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Autowired(required=false)
-    @Qualifier("casEventsRepository")
-    private CasEventsRepository casEventsRepository;
+    @Autowired(required = false)
+    @Qualifier("casEventRepository")
+    private CasEventRepository casEventRepository;
+
+    /**
+     * Gets the http request based on the
+     * {@link org.springframework.web.context.request.RequestContextHolder}.
+     *
+     * @return the request or null
+     */
+    protected final HttpServletRequest getRequest() {
+        try {
+            final ServletRequestAttributes attrs = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+            if (attrs != null) {
+                return attrs.getRequest();
+            }
+        } catch (final Exception e) {
+            logger.trace("Unable to obtain the http request", e);
+        }
+        return null;
+    }
 
     /**
      * Handle TGT creation event.
@@ -29,8 +55,24 @@ public class DefaultCasEventListener {
      */
     @TransactionalEventListener
     public void handleCasTicketGrantingTicketCreatedEvent(final CasTicketGrantingTicketCreatedEvent event) {
-        if (casEventsRepository != null) {
-            casEventsRepository.save(event);
+        if (casEventRepository != null) {
+
+            final CasEventDTO dto = new CasEventDTO();
+            dto.setType(event.getClass().getCanonicalName());
+            dto.putTimestamp(event.getTimestamp());
+            dto.putCreationTime(event.getTicketGrantingTicket().getCreationTime());
+            dto.putId(event.getTicketGrantingTicket().getId());
+
+            final HttpServletRequest request = getRequest();
+            if (request != null) {
+                String ipAddress = request.getHeader("X-FORWARDED-FOR");
+                if (ipAddress == null) {
+                    ipAddress = request.getRemoteAddr();
+                }
+                dto.putLocation(ipAddress);
+                dto.putAgent(request.getHeader("user-agent"));
+            }
+            casEventRepository.save(dto);
         }
     }
 }
