dbddff23c37139455d7ebbeb30b75bc87a31bfdb
Misagh Moayyed <mm1844@gmail.com>
2016-02-20 13:38:56 -0700

    added audit trail

---
+++ cas-server-webapp-config/src/main/java/org/jasig/cas/config/CasAuditTrailConfiguration.java
@@ -0,0 +1,71 @@
+package org.jasig.cas.config;
+
+import com.google.common.collect.ImmutableList;
+import org.jasig.inspektr.audit.AuditTrailManagementAspect;
+import org.jasig.inspektr.audit.AuditTrailManager;
+import org.jasig.inspektr.audit.spi.support.DefaultAuditActionResolver;
+import org.jasig.inspektr.audit.spi.support.ReturnValueAsStringResourceResolver;
+import org.jasig.inspektr.common.spi.PrincipalResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+import java.util.Map;
+
+/**
+ * This is {@link CasAuditTrailConfiguration} that configures relevant audit trail beans.
+ *
+ * @author Misagh Moayyed
+ * @since 4.3.0
+ */
+@Configuration("casAuditTrailConfiguration")
+public class CasAuditTrailConfiguration {
+    @Value("${cas.audit.appcode:CAS}")
+    private String appCode;
+
+    @Autowired
+    @Qualifier("auditablePrincipalResolver")
+    private PrincipalResolver principalResolver;
+
+    @Autowired
+    @Qualifier("auditTrailManager")
+    private AuditTrailManager auditTrailManager;
+
+    @Resource(name = "auditResourceResolverMap")
+    private Map auditResourceResolverMap;
+
+    @Resource(name = "auditActionResolverMap")
+    private Map auditActionResolverMap;
+
+    @Bean(name = "auditTrailManagementAspect")
+    public AuditTrailManagementAspect auditTrailManagementAspect() {
+        return new AuditTrailManagementAspect(this.appCode,
+                this.principalResolver, ImmutableList.of(this.auditTrailManager), auditActionResolverMap,
+                auditResourceResolverMap);
+
+    }
+
+    @Bean(name = "authenticationActionResolver")
+    public DefaultAuditActionResolver authenticationActionResolver() {
+        return new DefaultAuditActionResolver("_SUCCESS", "_FAILED");
+    }
+
+    @Bean(name = "ticketCreationActionResolver")
+    public DefaultAuditActionResolver ticketCreationActionResolver() {
+        return new DefaultAuditActionResolver("_CREATED", "_NOT_CREATED");
+    }
+
+    @Bean(name = "ticketValidationActionResolver")
+    public DefaultAuditActionResolver ticketValidationActionResolver() {
+        return new DefaultAuditActionResolver("D", "_FAILED");
+    }
+
+    @Bean(name = "returnValueResourceResolver")
+    public ReturnValueAsStringResourceResolver returnValueResourceResolver() {
+        return new ReturnValueAsStringResourceResolver();
+    }
+}
+
---
+++ cas-server-webapp-config/src/main/java/org/jasig/cas/config/CasWebflowContextConfiguration.java
@@ -0,0 +1,69 @@
+package org.jasig.cas.config;
+
+import com.google.common.collect.ImmutableList;
+import org.jasig.cas.web.flow.LogoutConversionService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.binding.convert.ConversionService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.ResourceBundleViewResolver;
+import org.springframework.webflow.expression.spel.WebFlowSpringELExpressionParser;
+import org.springframework.webflow.mvc.builder.MvcViewFactoryCreator;
+
+/**
+ * This is {@link CasWebflowContextConfiguration} that attempts to create Spring-managed beans
+ * backed by external configuration.
+ *
+ * @author Misagh Moayyed
+ * @since 4.3.0
+ */
+@Configuration("casWebflowContextConfiguration")
+public class CasWebflowContextConfiguration {
+
+    private static final int VIEW_RESOLVER_ORDER = 10000;
+
+    @Value("${cas.themeResolver.pathprefix:/WEB-INF/view/jsp}/default/ui/}")
+    private String resolverPathPrefix;
+
+    @Bean(name = "expressionParser")
+    public WebFlowSpringELExpressionParser expressionParser() {
+        final WebFlowSpringELExpressionParser parser = new WebFlowSpringELExpressionParser(
+                new SpelExpressionParser(),
+                logoutConversionService());
+        return parser;
+    }
+
+    @Bean(name = "logoutConversionService")
+    public ConversionService logoutConversionService() {
+        return new LogoutConversionService();
+    }
+
+    @Bean(name = "internalViewResolver")
+    public InternalResourceViewResolver internalViewResolver() {
+        final InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix(this.resolverPathPrefix);
+        resolver.setSuffix(".jsp");
+        resolver.setOrder(VIEW_RESOLVER_ORDER);
+        return resolver;
+    }
+
+    @Bean(name = "viewResolver")
+    public ResourceBundleViewResolver viewResolver() {
+        final ResourceBundleViewResolver resolver = new ResourceBundleViewResolver();
+        resolver.setOrder(0);
+        resolver.setBasename("cas_views");
+        return resolver;
+    }
+
+    @Bean(name = "viewFactoryCreator")
+    public MvcViewFactoryCreator viewFactoryCreator() {
+        final MvcViewFactoryCreator resolver = new MvcViewFactoryCreator();
+        resolver.setViewResolvers(ImmutableList.of(viewResolver(), internalViewResolver()));
+        return resolver;
+    }
+}
+
