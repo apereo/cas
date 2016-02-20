3863c388b11fdef29b444a3e606f6588ca3d85a6
Misagh Moayyed <mmoayyed@unicon.net>
2016-02-20 05:37:32 -0700

    added sec context

---
+++ cas-server-webapp-config/src/main/java/org/jasig/cas/config/CasSecurityContextConfiguration.java
@@ -0,0 +1,28 @@
+package org.jasig.cas.config;
+
+import org.pac4j.core.config.Config;
+import org.pac4j.http.client.direct.IpClient;
+import org.pac4j.http.credentials.authenticator.IpRegexpAuthenticator;
+import org.pac4j.springframework.web.RequiresAuthenticationInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * This is {@link CasSecurityContextConfiguration} that attempts to create Spring-managed beans
+ * backed by external configuration.
+ *
+ * @author Misagh Moayyed
+ * @since 4.3.0
+ */
+@Configuration("casSecurityContextConfiguration")
+public class CasSecurityContextConfiguration {
+
+    @Value("${cas.securityContext.adminpages.ip:127\\.0\\.\\.1}")
+    private String regexPattern;
+
+    @Bean(name = "requiresAuthenticationInterceptor")
+    public RequiresAuthenticationInterceptor requiresAuthenticationInterceptor() {
+        return new RequiresAuthenticationInterceptor(new Config(new IpClient(new IpRegexpAuthenticator(this.regexPattern))), "IpClient");
+    }
+}
