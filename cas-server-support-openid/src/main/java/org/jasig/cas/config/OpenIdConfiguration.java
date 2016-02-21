5b38cb2df85b3b323ca8646214acaa8bde704be6
Misagh Moayyed <mm1844@gmail.com>
2016-02-21 18:25:53 +0330

    Updated javadoc

--- cas-server-support-openid/src/main/java/org/jasig/cas/config/OpenIdConfiguration.java
+++ cas-server-support-openid/src/main/java/org/jasig/cas/config/OpenIdConfiguration.java
@@ -20,46 +20,88 @@ import java.util.Arrays;
  */
 @Configuration("openidConfiguration")
 public class OpenIdConfiguration {
-    
+
+    /**
+     * The Smart open id association controller.
+     */
     @Autowired
     @Qualifier("smartOpenIdAssociationController")
     private AbstractDelegateController smartOpenIdAssociationController;
-        
+
+    /**
+     * The Open id validate controller.
+     */
     @Autowired
     @Qualifier("openIdValidateController")
     private AbstractDelegateController openIdValidateController;
-    
+
+    /**
+     * The Endpoint.
+     */
     @Value("${server.prefix}/login")
     private String endpoint;
-    
+
+    /**
+     * The Enforce rp id.
+     */
     @Value("${cas.openid.enforce.rpid:false}")
     private boolean enforceRpId;
 
+    /**
+     * Cas open id service failure view jstl view.
+     *
+     * @return the jstl view
+     */
     @Bean(name="casOpenIdServiceFailureView")
     public JstlView casOpenIdServiceFailureView() {
         return new JstlView("/WEB-INF/view/jsp/protocol/openid/casOpenIdServiceFailureView.jsp");
     }
-    
+
+    /**
+     * Cas open id service success view jstl view.
+     *
+     * @return the jstl view
+     */
     @Bean(name="casOpenIdServiceSuccessView")
     public JstlView casOpenIdServiceSuccessView() {
         return new JstlView("/WEB-INF/view/jsp/protocol/openid/casOpenIdServiceSuccessView.jsp");
     }
-    
+
+    /**
+     * Cas open id association failure view jstl view.
+     *
+     * @return the jstl view
+     */
     @Bean(name="casOpenIdAssociationFailureView")
     public JstlView casOpenIdAssociationFailureView() {
         return new JstlView("/WEB-INF/view/jsp/protocol/openid/casOpenIdAssociationFailureView.jsp");
     }
-    
+
+    /**
+     * Cas open id association success view jstl view.
+     *
+     * @return the jstl view
+     */
     @Bean(name="casOpenIdAssociationSuccessView")
     public JstlView casOpenIdAssociationSuccessView() {
         return new JstlView("/WEB-INF/view/jsp/protocol/openid/casOpenIdAssociationSuccessView.jsp");
     }
-    
+
+    /**
+     * Open id provider view jstl view.
+     *
+     * @return the jstl view
+     */
     @Bean(name="openIdProviderView")
     public JstlView openIdProviderView() {
         return new JstlView("/WEB-INF/view/jsp/protocol/openid/user.jsp");
     }
-    
+
+    /**
+     * Openid delegating controller delegating controller.
+     *
+     * @return the delegating controller
+     */
     @Bean(name="openidDelegatingController")
     public DelegatingController openidDelegatingController() {
         final DelegatingController controller = new DelegatingController();
@@ -68,6 +110,11 @@ public class OpenIdConfiguration {
         return controller;
     }
 
+    /**
+     * Server manager server manager.
+     *
+     * @return the server manager
+     */
     @Bean(name="serverManager")
     public ServerManager serverManager() {
         final ServerManager manager = new ServerManager();
