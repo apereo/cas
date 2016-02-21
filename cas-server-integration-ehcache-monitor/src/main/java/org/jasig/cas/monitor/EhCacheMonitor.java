f0b2501ce331bcb374c7be7f7ca42bb0dd8e35cb
Misagh Moayyed <mm1844@gmail.com>
2016-02-21 22:21:09 +0330

    Fixed test cases

--- cas-server-integration-ehcache-monitor/src/main/java/org/jasig/cas/monitor/EhCacheMonitor.java
+++ cas-server-integration-ehcache-monitor/src/main/java/org/jasig/cas/monitor/EhCacheMonitor.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Monitors a {@link Cache} instance.
@@ -19,11 +21,17 @@ import javax.annotation.Nullable;
 @Component("ehcacheMonitor")
 public class EhCacheMonitor extends AbstractCacheMonitor {
 
+    /**
+     * The Ticket granting tickets cache.
+     */
     @Nullable
     @Autowired(required = false)
     @Qualifier("ticketGrantingTicketsCache")
     private Cache ticketGrantingTicketsCache;
 
+    /**
+     * The Service tickets cache.
+     */
     @Nullable
     @Autowired(required = false)
     @Qualifier("serviceTicketsCache")
@@ -36,11 +44,35 @@ public class EhCacheMonitor extends AbstractCacheMonitor {
     }
 
 
+    /**
+     * Instantiates a new Eh cache monitor.
+     *
+     * @param ticketGrantingTicketsCache the ticket granting tickets cache
+     */
+    public EhCacheMonitor(final Cache ticketGrantingTicketsCache) {
+        this.ticketGrantingTicketsCache = ticketGrantingTicketsCache;
+    }
+
+    /**
+     * Instantiates a new Eh cache monitor.
+     *
+     * @param ticketGrantingTicketsCache the ticket granting tickets cache
+     * @param serviceTicketsCache        the service tickets cache
+     */
+    public EhCacheMonitor(final Cache ticketGrantingTicketsCache, final Cache serviceTicketsCache) {
+        this.ticketGrantingTicketsCache = ticketGrantingTicketsCache;
+        this.serviceTicketsCache = serviceTicketsCache;
+    }
+    
     @Override
     protected CacheStatistics[] getStatistics() {
-        return new EhCacheStatistics[]{
-                new EhCacheStatistics(ticketGrantingTicketsCache),
-                new EhCacheStatistics(serviceTicketsCache)
-        };
+        final List<CacheStatistics> list = new ArrayList<>();
+        if (this.ticketGrantingTicketsCache != null) {
+            list.add(new EhCacheStatistics(this.ticketGrantingTicketsCache));
+        }
+        if (this.serviceTicketsCache != null) {
+            list.add(new EhCacheStatistics(this.serviceTicketsCache));
+        }
+        return list.toArray(new CacheStatistics[] {});
     }
 }
