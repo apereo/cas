ecf73ff38fc0d933a58bdcfc2eada70ff1e0683d
Misagh Moayyed <mmoayyed@unicon.net>
2016-03-05 13:04:49 -0700

    working on context initializers

--- cas-server-core-tickets/src/main/java/org/jasig/cas/util/HostNameBasedUniqueTicketIdGenerator.java
+++ cas-server-core-tickets/src/main/java/org/jasig/cas/util/HostNameBasedUniqueTicketIdGenerator.java
@@ -8,15 +8,15 @@ import java.net.UnknownHostException;
  * the suffix based on the underlying host name.
  *
  * <p>In order to assist with multi-node deployments, in scenarios where CAS configuration
- * and specially {@code cas.properties} file is externalized, it would be ideal to simply just have one set
- * of configuration files for all nodes, such that there would for instance be one {@code cas.properties} file
+ * and specially {@code application.properties} file is externalized, it would be ideal to simply just have one set
+ * of configuration files for all nodes, such that there would for instance be one {@code application.properties} file
  * for all nodes. This would remove the need to copy/sync config files over across nodes, again in a
  * situation where they are externalized.
- * <p>The drawback is that in keeping only one {@code cas.properties} file, we'd lose the ability
+ * <p>The drawback is that in keeping only one {@code application.properties} file, we'd lose the ability
  * to define unique {@code host.name} property values for each node as the suffix, which would assist with troubleshooting
  * and diagnostics. To provide a remedy, this ticket generator is able to retrieve the {@code host.name} value directly from
  * the actual node name, rather than relying on the configuration, only if one isn't specified in
- * the {@code cas.properties} file. </p>
+ * the {@code application.properties} file. </p>
  * @author Misagh Moayyed
  * @since 4.1.0
  */
