dccd531cae34a3b2a7a55c8e0a83e113116e720e
adias <antonio.dias@scytl.com>
2016-03-07 17:44:51 +0100

    CAS-1550 first try at implementing direct OP signature verification. The OpenIdValidateController now handles the 'check_authentication' request (signature validation).  Changed ResponseBuilder to not replace the association handle with the ST.

--- cas-server-support-openid/src/main/java/org/jasig/cas/support/openid/web/mvc/OpenIdValidateController.java
+++ cas-server-support-openid/src/main/java/org/jasig/cas/support/openid/web/mvc/OpenIdValidateController.java
@@ -1,15 +1,25 @@
 package org.jasig.cas.support.openid.web.mvc;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
+import org.jasig.cas.support.openid.OpenIdProtocolConstants;
 import org.jasig.cas.ticket.proxy.ProxyHandler;
 import org.jasig.cas.web.AbstractServiceValidateController;
+import org.openid4java.message.ParameterList;
+import org.openid4java.message.VerifyResponse;
+import org.openid4java.server.ServerManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * An Openid controller that delegates to its own views on service validates.
@@ -19,10 +29,42 @@ import javax.servlet.http.HttpServletResponse;
 @Component("openIdValidateController")
 public class OpenIdValidateController extends AbstractServiceValidateController {
 
+    private final Logger logger = LoggerFactory.getLogger(OpenIdValidateController.class);
+
+    /** The view to redirect to on a successful validation. */
+    private String successView;
+
+    /** The view to redirect to on a unsuccessful validation. */
+    private String failureView;
+
+    @Autowired
+    @Qualifier("serverManager")
+    private ServerManager serverManager;
+
     @Override
     protected ModelAndView handleRequestInternal(final HttpServletRequest request, final HttpServletResponse response)
         throws Exception {
-        return super.handleRequestInternal(request, response);
+        final String openIdMode = request.getParameter(OpenIdProtocolConstants.OPENID_MODE);
+        if (StringUtils.equals(openIdMode, OpenIdProtocolConstants.CHECK_AUTHENTICATION)) {
+
+            final VerifyResponse message = (VerifyResponse)
+                serverManager.verify(new ParameterList(request.getParameterMap()));
+
+            final Map<String, String> parameters = new HashMap<>();
+            parameters.putAll(message.getParameterMap());
+
+            if(message.isSignatureVerified()) {
+                logger.debug("Signature verification request successful.");
+                return new ModelAndView(successView, "parameters", parameters);
+            } else {
+                logger.debug("Signature verification request unsuccessful.");
+                return new ModelAndView(failureView, "parameters", parameters);
+            }
+        } else {
+            // we should probably fail here(?),
+            // since we only deal OpenId signature verification
+            return super.handleRequestInternal(request, response);
+        }
     }
 
     @Override
@@ -36,17 +78,30 @@ public class OpenIdValidateController extends AbstractServiceValidateController 
     @Autowired
     public void setFailureView(@Value("casOpenIdServiceFailureView") final String failureView) {
         super.setFailureView(failureView);
+        this.failureView = failureView;
     }
 
     @Override
     @Autowired
     public void setSuccessView(@Value("casOpenIdServiceSuccessView") final String successView) {
         super.setSuccessView(successView);
+        this.successView = successView;
     }
 
     @Override
     @Autowired
     public void setProxyHandler(@Qualifier("proxy20Handler") final ProxyHandler proxyHandler) {
         super.setProxyHandler(proxyHandler);
+    }
+
+    @Override
+    public boolean canHandle(final HttpServletRequest request, final HttpServletResponse response) {
+        final String openIdMode = request.getParameter(OpenIdProtocolConstants.OPENID_MODE);
+        if (StringUtils.equals(openIdMode, OpenIdProtocolConstants.CHECK_AUTHENTICATION)) {
+            logger.info("Handling request. openid.mode : {}", openIdMode);
+            return true;
+        }
+        logger.info("Cannot handle request. openid.mode : {}", openIdMode);
+        return false;
     }
 }
