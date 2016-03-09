e61e3eaeccc8601b73245a80ed6236cd769f5229
Daniel Frett <daniel.frett@ccci.org>
2016-03-09 01:27:29 -0500

    correct DefaultMultifactorTriggerSelectionStrategy to differentiate between bean name and provider id
    RegisteredServices currently are the only place that an MFA provider is configured by bean name instead of provider id. This should probably change, but for now this commit updates the Default trigger selection strategy to mimic how the webflow makes decisions

--- cas-server-core-api-services/src/main/java/org/jasig/cas/authentication/MultifactorTriggerSelectionStrategy.java
+++ cas-server-core-api-services/src/main/java/org/jasig/cas/authentication/MultifactorTriggerSelectionStrategy.java
@@ -1,11 +1,12 @@
 package org.jasig.cas.authentication;
 
 import org.jasig.cas.authentication.principal.Principal;
+import org.jasig.cas.services.MultifactorAuthenticationProvider;
 import org.jasig.cas.services.RegisteredService;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 
 /**
  * This interface is responsible for deciding which Multifactor provider to use based on the request, service, and
@@ -19,12 +20,12 @@ public interface MultifactorTriggerSelectionStrategy {
      * Resolve the multifactor authentication provider id for the specified HttpServletRequest, RegisteredService and
      * Principal.
      *
-     * @param availableProviders a Set of available MFA providers
+     * @param availableProviders a Map of available MFA providers loaded in the spring context
      * @param request            The original request to check for MFA requirements
      * @param service            The service to check for MFA requirements
      * @param principal          The principal to check for MFA requirements
-     * @return the id of the MFA provider required for authentication
+     * @return the provider id of the MFA provider required for authentication
      */
-    Optional<String> resolve(Set<String> availableProviders, HttpServletRequest request, RegisteredService service,
-                             Principal principal);
+    Optional<String> resolve(Map<String, MultifactorAuthenticationProvider> availableProviders, HttpServletRequest request,
+                             RegisteredService service, Principal principal);
 }
