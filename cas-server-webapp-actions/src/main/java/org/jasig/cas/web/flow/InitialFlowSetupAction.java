eeaf6bf8dd10f16eceff42fff4928c77e51ccfce
Misagh Moayyed <mm1844@gmail.com>
2016-03-18 08:42:30 -0700

    Merge branch 'master' into Mfa
    # Conflicts:
    #	cas-server-webapp-actions/src/main/java/org/jasig/cas/web/flow/InitialFlowSetupAction.java

--- cas-server-webapp-actions/src/main/java/org/jasig/cas/web/flow/InitialFlowSetupAction.java
+++ cas-server-webapp-actions/src/main/java/org/jasig/cas/web/flow/InitialFlowSetupAction.java
@@ -4,7 +4,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.jasig.cas.authentication.principal.Service;
 import org.jasig.cas.services.RegisteredService;
 import org.jasig.cas.services.RegisteredServiceAccessStrategy;
-import org.jasig.cas.services.RegisteredServiceAccessStrategySupport;
 import org.jasig.cas.services.ServicesManager;
 import org.jasig.cas.services.UnauthorizedServiceException;
 import org.jasig.cas.web.support.ArgumentExtractor;
@@ -45,69 +44,29 @@ public final class InitialFlowSetupAction extends AbstractAction {
 
     /** The services manager with access to the registry. **/
     @NotNull
-    @Autowired
-    @Qualifier("servicesManager")
     private ServicesManager servicesManager;
 
     /** CookieGenerator for the Warnings. */
     @NotNull
-    @Autowired
-    @Qualifier("warnCookieGenerator")
     private CookieRetrievingCookieGenerator warnCookieGenerator;
 
     /** CookieGenerator for the TicketGrantingTickets. */
     @NotNull
-    @Autowired
-    @Qualifier("ticketGrantingTicketCookieGenerator")
     private CookieRetrievingCookieGenerator ticketGrantingTicketCookieGenerator;
 
     /** Extractors for finding the service. */
     @NotNull
     @Size(min=1)
-    @Resource(name="argumentExtractors")
     private List<ArgumentExtractor> argumentExtractors;
 
+
     /** If no authentication request from a service is present, halt and warn the user. */
-    @Value("${create.sso.missing.service:true}")
     private boolean enableFlowOnAbsentServiceRequest = true;
-
-
-
-    /**
-     * Instantiates a new Initial flow setup action.
-     */
-    public InitialFlowSetupAction() {}
 
     @Override
     protected Event doExecute(final RequestContext context) throws Exception {
-        processCookieGenerators(context);
-        processService(context);
-
-        return result(CasWebflowConstants.TRANSITION_ID_SUCCESS);
-    }
-
-    private void processService(final RequestContext context) {
-        final Service service = WebUtils.getService(this.argumentExtractors, context);
-        if (service != null) {
-            logger.debug("Placing service in context scope: [{}]", service.getId());
-
-            final RegisteredService registeredService = this.servicesManager.findServiceBy(service);
-            RegisteredServiceAccessStrategySupport.ensureServiceAccessIsAllowed(service, registeredService);
-            logger.debug("Placing registered service [{}] with id [{}] in context scope",
-                    registeredService.getServiceId(),
-                    registeredService.getId());
-            WebUtils.putRegisteredService(context, registeredService);
-        } else if (!this.enableFlowOnAbsentServiceRequest) {
-            logger.warn("No service authentication request is available at [{}]. CAS is configured to disable the flow.",
-                    WebUtils.getHttpServletRequest(context).getRequestURL());
-            throw new NoSuchFlowExecutionException(context.getFlowExecutionContext().getKey(),
-                    new UnauthorizedServiceException("screen.service.required.message", "Service is required"));
-        }
-        WebUtils.putService(context, service);
-    }
-
-    private void processCookieGenerators(final RequestContext context) {
         final HttpServletRequest request = WebUtils.getHttpServletRequest(context);
+
         final String contextPath = context.getExternalContext().getContextPath();
         final String cookiePath = StringUtils.isNotBlank(contextPath) ? contextPath + '/' : "/";
 
@@ -128,8 +87,22 @@ public final class InitialFlowSetupAction extends AbstractAction {
 
         WebUtils.putTicketGrantingTicketInScopes(context,
                 this.ticketGrantingTicketCookieGenerator.retrieveCookieValue(request));
+
         WebUtils.putWarningCookie(context,
                 Boolean.valueOf(this.warnCookieGenerator.retrieveCookieValue(request)));
+
+        final Service service = WebUtils.getService(this.argumentExtractors, context);
+
+
+        if (service != null) {
+            logger.debug("Placing service in context scope: [{}]", service.getId());
+
+            final RegisteredService registeredService = this.servicesManager.findServiceBy(service);
+            if (registeredService != null && registeredService.getAccessStrategy().isServiceAccessAllowed()) {
+                logger.debug("Placing registered service [{}] with id [{}] in context scope",
+                        registeredService.getServiceId(),
+                        registeredService.getId());
+                WebUtils.putRegisteredService(context, registeredService);
 
                 final RegisteredServiceAccessStrategy accessStrategy = registeredService.getAccessStrategy();
                 if (accessStrategy.getUnauthorizedRedirectUrl() != null) {
@@ -138,18 +111,31 @@ public final class InitialFlowSetupAction extends AbstractAction {
                             registeredService.getServiceId());
                     WebUtils.putUnauthorizedRedirectUrl(context, accessStrategy.getUnauthorizedRedirectUrl());
                 }
+            }
+        } else if (!this.enableFlowOnAbsentServiceRequest) {
+            logger.warn("No service authentication request is available at [{}]. CAS is configured to disable the flow.",
+                    WebUtils.getHttpServletRequest(context).getRequestURL());
+            throw new NoSuchFlowExecutionException(context.getFlowExecutionContext().getKey(),
+                    new UnauthorizedServiceException("screen.service.required.message", "Service is required"));
+        }
+        WebUtils.putService(context, service);
+        return result("success");
     }
 
     @Autowired
-    public void setTicketGrantingTicketCookieGenerator(final CookieRetrievingCookieGenerator ticketGrantingTicketCookieGenerator) {
+    public void setTicketGrantingTicketCookieGenerator(
+            @Qualifier("ticketGrantingTicketCookieGenerator")
+            final CookieRetrievingCookieGenerator ticketGrantingTicketCookieGenerator) {
         this.ticketGrantingTicketCookieGenerator = ticketGrantingTicketCookieGenerator;
     }
 
     @Autowired
-    public void setWarnCookieGenerator(final CookieRetrievingCookieGenerator warnCookieGenerator) {
+    public void setWarnCookieGenerator(@Qualifier("warnCookieGenerator")
+                                       final CookieRetrievingCookieGenerator warnCookieGenerator) {
         this.warnCookieGenerator = warnCookieGenerator;
     }
 
+    @Resource(name="argumentExtractors")
     public void setArgumentExtractors(final List<ArgumentExtractor> argumentExtractors) {
         this.argumentExtractors = argumentExtractors;
     }
@@ -161,7 +147,8 @@ public final class InitialFlowSetupAction extends AbstractAction {
      * Since 4.1
      * @param servicesManager the services manager
      */
-    public void setServicesManager(final ServicesManager servicesManager) {
+    @Autowired
+    public void setServicesManager(@Qualifier("servicesManager") final ServicesManager servicesManager) {
         this.servicesManager = servicesManager;
     }
 
@@ -171,7 +158,9 @@ public final class InitialFlowSetupAction extends AbstractAction {
      *
      * @param enableFlowOnAbsentServiceRequest the enable flow on absent service request
      */
-    public void setEnableFlowOnAbsentServiceRequest(final boolean enableFlowOnAbsentServiceRequest) {
+    @Autowired
+    public void setEnableFlowOnAbsentServiceRequest(@Value("${create.sso.missing.service:true}")
+                                                    final boolean enableFlowOnAbsentServiceRequest) {
         this.enableFlowOnAbsentServiceRequest = enableFlowOnAbsentServiceRequest;
     }
 }
