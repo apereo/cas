f1279dfa304ab19c486ad5911bdf6a8090c1f093
Misagh Moayyed <mm1844@gmail.com>
2016-04-19 08:10:59 -0700

    Merge pull request #1688 from Unicon/hz-unsup-ops
    Let hazelcast handle session and service ticket counts
    # Conflicts:
    #	cas-server-core-tickets/src/main/java/org/jasig/cas/util/DefaultUniqueTicketIdGenerator.java
    #	cas-server-documentation/installation/Configuring-SSO-Session-Cookie.md
    #	cas-server-documentation/installation/Configuring-Service-Access-Strategy.md
    #	cas-server-documentation/installation/LDAP-Authentication.md
    #	cas-server-documentation/installation/Webflow-Customization.md
    #	cas-server-documentation/protocol/CAS-Protocol.md
    #	gradle.properties

--- cas-server-core-tickets/src/test/java/org/jasig/cas/util/HostNameBasedUniqueTicketIdGeneratorTests.java
+++ cas-server-core-tickets/src/test/java/org/jasig/cas/util/HostNameBasedUniqueTicketIdGeneratorTests.java
@@ -13,7 +13,7 @@ public class HostNameBasedUniqueTicketIdGeneratorTests {
 
     @Test
     public void verifyUniqueGenerationOfTicketIds() throws Exception {
-        final HostNameBasedUniqueTicketIdGenerator generator = new HostNameBasedUniqueTicketIdGenerator(10);
+        final HostNameBasedUniqueTicketIdGenerator generator = new HostNameBasedUniqueTicketIdGenerator(10, "");
         final String id1 = generator.getNewTicketId("TEST");
         final String id2 = generator.getNewTicketId("TEST");
         assertNotSame(id1, id2);
