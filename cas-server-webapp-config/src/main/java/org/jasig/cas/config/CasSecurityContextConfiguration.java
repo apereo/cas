df773bc1266bfd94249360b00de8930ebd87f0c6
Misagh Moayyed <mmoayyed@unicon.net>
2016-04-22 14:56:10 -0700

    fixing test cases

--- cas-server-webapp-config/src/main/java/org/jasig/cas/config/CasSecurityContextConfiguration.java
+++ cas-server-webapp-config/src/main/java/org/jasig/cas/config/CasSecurityContextConfiguration.java
@@ -1,17 +1,35 @@
 package org.jasig.cas.config;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpStatus;
+import org.pac4j.cas.client.CasClient;
+import org.pac4j.core.authorization.RequireAnyRoleAuthorizer;
+import org.pac4j.core.authorization.generator.SpringSecurityPropertiesAuthorizationGenerator;
+import org.pac4j.core.client.Client;
+import org.pac4j.core.client.IndirectClient;
 import org.pac4j.core.config.Config;
+import org.pac4j.core.context.WebContext;
+import org.pac4j.core.profile.UserProfile;
 import org.pac4j.http.client.direct.IpClient;
 import org.pac4j.http.credentials.authenticator.IpRegexpAuthenticator;
 import org.pac4j.springframework.web.RequiresAuthenticationInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.endpoint.mvc.EndpointHandlerMappingCustomizer;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import org.springframework.web.servlet.mvc.WebContentInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Properties;
 
 /**
  * This is {@link CasSecurityContextConfiguration} that attempts to create Spring-managed beans
@@ -22,9 +40,22 @@ import org.springframework.web.servlet.mvc.WebContentInterceptor;
  */
 @Configuration("casSecurityContextConfiguration")
 public class CasSecurityContextConfiguration extends WebMvcConfigurerAdapter {
-    
-    @Value("${cas.securityContext.adminpages.ip:127\\.0\\.0\\.1}")
+    private static final Logger LOGGER = LoggerFactory.getLogger(CasSecurityContextConfiguration.class);
+
+    @Value("${cas.securityContext.adminpages.ip:}")
     private String regexPattern;
+
+    @Value("${cas.securityContext.adminpages.adminRoles:}")
+    private String roles;
+
+    @Value("${cas.securityContext.adminpages.loginUrl:}")
+    private String loginUrl;
+
+    @Value("${cas.securityContext.adminpages.service:}")
+    private String callbackUrl;
+
+    @Value("${cas.securityContext.adminpages.users:}")
+    private Resource userPropertiesFile;
 
     /**
      * Web content interceptor web content interceptor.
@@ -39,7 +70,7 @@ public class CasSecurityContextConfiguration extends WebMvcConfigurerAdapter {
         interceptor.setAlwaysUseFullPath(true);
         return interceptor;
     }
-    
+
     /**
      * Requires authentication interceptor requires authentication interceptor.
      *
@@ -47,8 +78,49 @@ public class CasSecurityContextConfiguration extends WebMvcConfigurerAdapter {
      */
     @RefreshScope
     @Bean(name = "requiresAuthenticationStatusStatsInterceptor")
-    public RequiresAuthenticationInterceptor requiresAuthenticationInterceptor() {
-        return new RequiresAuthenticationInterceptor(new Config(new IpClient(new IpRegexpAuthenticator(this.regexPattern))), "IpClient");
+    public HandlerInterceptorAdapter requiresAuthenticationInterceptor() {
+
+        if (StringUtils.isNotBlank(this.regexPattern)) {
+            LOGGER.debug("Securing sensitive endpoints via IP pattern {}", this.regexPattern);
+            return new RequiresAuthenticationInterceptor(
+                    new Config(new IpClient(new IpRegexpAuthenticator(this.regexPattern))), "IpClient");
+        }
+
+        try {
+            if (StringUtils.isNotBlank(this.loginUrl) && StringUtils.isNotBlank(this.callbackUrl)
+                && StringUtils.isNotBlank(this.roles)) {
+                final IndirectClient client = new CasClient(this.loginUrl);
+                final Properties properties = new Properties();
+                properties.load(this.userPropertiesFile.getInputStream());
+                client.setAuthorizationGenerator(new SpringSecurityPropertiesAuthorizationGenerator(properties));
+
+                final Config cfg = new Config(this.callbackUrl, client);
+                cfg.setAuthorizer(new RequireAnyRoleAuthorizer(org.springframework.util.StringUtils.commaDelimitedListToSet(this.roles)));
+
+                final RequiresAuthenticationInterceptor interceptor = new RequiresAuthenticationInterceptor(cfg, "CasClient",
+                        "securityHeaders,csrfToken,RequireAnyRoleAuthorizer") {
+                    @Override
+                    protected void forbidden(final WebContext context, final List<Client> currentClients, final UserProfile profile) {
+                        context.setResponseStatus(HttpStatus.SC_FORBIDDEN);
+                    }
+                };
+                return interceptor;
+            }
+        } catch (final Exception e) {
+            LOGGER.warn(e.getMessage(), e);
+        }
+
+        return new HandlerInterceptorAdapter() {
+            @Override
+            public boolean preHandle(final HttpServletRequest request,
+                                     final HttpServletResponse response,
+                                     final Object handler) throws Exception {
+
+                LOGGER.info("Access denied to {}", request.getRequestURI());
+                response.setStatus(HttpStatus.SC_FORBIDDEN);
+                return false;
+            }
+        };
     }
 
     @Override
@@ -65,11 +137,10 @@ public class CasSecurityContextConfiguration extends WebMvcConfigurerAdapter {
      * @return the endpoint handler mapping customizer
      */
     @RefreshScope
-    @Bean(name="mappingCustomizer")
+    @Bean(name = "mappingCustomizer")
     public EndpointHandlerMappingCustomizer mappingCustomizer() {
-        return mapping -> mapping.setInterceptors(new Object[] {requiresAuthenticationInterceptor()});
+        return mapping -> mapping.setInterceptors(new Object[]{requiresAuthenticationInterceptor()});
     }
-    
-    
+
 
 }
