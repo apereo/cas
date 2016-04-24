1a544fb54f6f96b2439e234f5a445d8387314915
Daniel Frett <daniel.frett@gmail.com>
2016-04-24 00:50:50 -0400

    JPA fixes (#1710)
    * use the ticket registry jpa properties for the jpa-ticket-registry
    
    * fix an NPE in the JPA Locking Strategy
    
    * add pointcuts for ticketRegistrySupport object
    
    * remove duplicate property

--- cas-server-support-jpa-ticket-registry/src/main/java/org/jasig/cas/ticket/registry/support/JpaLockingStrategy.java
+++ cas-server-support-jpa-ticket-registry/src/main/java/org/jasig/cas/ticket/registry/support/JpaLockingStrategy.java
@@ -102,12 +102,12 @@ public class JpaLockingStrategy implements LockingStrategy {
 
         boolean result = false;
         if (lock != null) {
-            final ZonedDateTime expDate = ZonedDateTime.from(lock.getExpirationDate());
+            final ZonedDateTime expDate = lock.getExpirationDate();
             if (lock.getUniqueId() == null) {
                 // No one currently possesses lock
                 logger.debug("{} trying to acquire {} lock.", uniqueId, applicationId);
                 result = acquire(entityManager, lock);
-            } else if (ZonedDateTime.now(ZoneOffset.UTC).isAfter(expDate)) {
+            } else if (expDate == null || ZonedDateTime.now(ZoneOffset.UTC).isAfter(expDate)) {
                 // Acquire expired lock regardless of who formerly owned it
                 logger.debug("{} trying to acquire expired {} lock.", uniqueId, applicationId);
                 result = acquire(entityManager, lock);
