c87c283a2966c1deaf4ef7abd35945fae5dd6ef7
Misagh Moayyed <mmoayyed@unicon.net>
2016-04-24 00:01:56 -0700

    fixing test cases

---
+++ cas-server-support-oidc/src/main/java/org/jasig/cas/OidcConstants.java
@@ -0,0 +1,18 @@
+package org.jasig.cas;
+
+
+/**
+ * This is {@link OidcConstants}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.0.0
+ */
+public interface OidcConstants {
+    String JWKS_URL = "jwks";
+    
+    String REGISTRATION_URL = "register";
+    
+    String INTROSPECTION_URL = "introspect";
+    
+    String REVOCATION_URL = "revocation";
+}
