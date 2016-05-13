93d45d3eea3b1b73695018364b6a98e723eeb94c
Jj <jsjohnson@unicon.net>
2016-05-12 22:46:37 -0500

    node rework WIP

--- cas-server-webapp/gulpfile.js
+++ cas-server-webapp/gulpfile.js
@@ -3,27 +3,45 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass');
 
-var config = {
-    sassPath: './src/main/resources/static/sass',
-    npmPath: './node_modules',
-    cssPath: './src/main/resources/static/css'
-}
+var argv = require('yargs')
+    .option('sassPath', {
+        alias: 's',
+        describe: "where's your sass?",
+        default: './src/main/resources/static/sass'
+    })
+    .option('npmPath', {
+        alias: 'n',
+        describe: "where's your npm?",
+        default: './node_modules'
+    })
+    .option('cssPath', {
+        alias: 'c',
+        describe: "where do you want your css?",
+        default: './src/main/resources/static/css'
+    })
+    .help('help')
+    .argv;
 
 gulp.task('default', ['sass']);
 
 gulp.task('sass', function () {
-    return gulp.src(config.sassPath + '/**/*.scss')
+    process.stdout.write("running with these values:\n");
+    process.stdout.write("\tsassPath: " + argv.sassPath + "\n");
+    process.stdout.write("\tnpmPath: " + argv.npmPath + "\n");
+    process.stdout.write("\tcssPath: " + argv.cssPath + "\n");
+    process.stdout.write("\there: " + argv.here + "\n");
+    return gulp.src(argv.sassPath + '/**/*.scss')
         .pipe(sass({
             //outputStyle: 'compressed',
             includePaths: [
-                config.sassPath,
-                config.npmPath + '/bootstrap-sass/assets/stylesheets'
+                argv.sassPath,
+                argv.npmPath + '/bootstrap-sass/assets/stylesheets'
             ]
         }).on('error', sass.logError))
-        .pipe(gulp.dest(config.cssPath));
+        .pipe(gulp.dest(argv.cssPath));
 
 });
 
 gulp.task('sass:watch', function () {
-    gulp.watch(config.sassPath + '/**/*.scss', ['sass']);
+    gulp.watch(argv.sassPath + '/**/*.scss', ['sass']);
 });
