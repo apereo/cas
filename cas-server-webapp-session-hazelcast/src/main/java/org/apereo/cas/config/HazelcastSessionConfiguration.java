c66ed1fcb9b9c2a7c18d5ce99ae8bf73d356e670
Misagh Moayyed <mmoayyed@unicon.net>
2016-05-17 21:39:17 -0700

    fixed cs errors with javadocs

--- cas-server-webapp-session-hazelcast/src/main/java/org/apereo/cas/config/HazelcastSessionConfiguration.java
+++ cas-server-webapp-session-hazelcast/src/main/java/org/apereo/cas/config/HazelcastSessionConfiguration.java
@@ -5,7 +5,6 @@ import com.hazelcast.config.XmlConfigBuilder;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
