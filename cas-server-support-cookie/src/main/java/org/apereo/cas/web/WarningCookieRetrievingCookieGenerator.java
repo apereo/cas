9396c12487913c39e13124ea21033b569546c179
Dmitriy Kopylenko <dima767@gmail.com>
2016-06-03 12:47:50 -0400

    Type-safe config model for cookie. WIP...

--- cas-server-support-cookie/src/main/java/org/apereo/cas/web/WarningCookieRetrievingCookieGenerator.java
+++ cas-server-support-cookie/src/main/java/org/apereo/cas/web/WarningCookieRetrievingCookieGenerator.java
@@ -10,32 +10,4 @@ import org.springframework.beans.factory.annotation.Value;
  * @since 4.2
  */
 public class WarningCookieRetrievingCookieGenerator extends CookieRetrievingCookieGenerator {
-
-    @Override
-    @Autowired
-    public void setCookieName(@Value("${warn.cookie.name:CASPRIVACY}")
-                                  final String cookieName) {
-        super.setCookieName(cookieName);
-    }
-
-    @Override
-    @Autowired
-    public void setCookiePath(@Value("${warn.cookie.path:}")
-                                  final String cookiePath) {
-        super.setCookiePath(cookiePath);
-    }
-
-    @Override
-    @Autowired
-    public void setCookieMaxAge(@Value("${warn.cookie.maxAge:-1}")
-                                    final Integer cookieMaxAge) {
-        super.setCookieMaxAge(cookieMaxAge);
-    }
-
-    @Override
-    @Autowired
-    public void setCookieSecure(@Value("${warn.cookie.secure:true}")
-                                    final boolean cookieSecure) {
-        super.setCookieSecure(cookieSecure);
-    }
 }
