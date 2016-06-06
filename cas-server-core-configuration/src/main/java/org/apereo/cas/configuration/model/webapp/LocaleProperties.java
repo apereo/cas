ac3078686bff4ac0a24052c91628962490cdc129
Dmitriy Kopylenko <dima767@gmail.com>
2016-06-06 09:11:37 -0400

    Type-safe config model for locale

---
+++ cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/webapp/LocaleProperties.java
@@ -0,0 +1,33 @@
+package org.apereo.cas.configuration.model.webapp;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Configuration properties class for locale.
+ *
+ * @author Dmitriy Kopylenko
+ * @since 5.0.0
+ */
+@ConfigurationProperties(prefix = "locale", ignoreUnknownFields = false)
+public class LocaleProperties {
+
+    private String paramName = "locale";
+
+    private String defaultValue = "en";
+
+    public String getParamName() {
+        return paramName;
+    }
+
+    public void setParamName(final String paramName) {
+        this.paramName = paramName;
+    }
+
+    public String getDefaultValue() {
+        return defaultValue;
+    }
+
+    public void setDefaultValue(final String defaultValue) {
+        this.defaultValue = defaultValue;
+    }
+}
