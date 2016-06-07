40964751bdf034eb885a89dd75083631cd6b397c
Dmitriy Kopylenko <dima767@gmail.com>
2016-06-07 08:27:34 -0400

    Type-safe config model for events. WIP...

--- cas-server-support-events-jpa/src/main/java/org/apereo/cas/config/JpaEventsConfiguration.java
+++ cas-server-support-events-jpa/src/main/java/org/apereo/cas/config/JpaEventsConfiguration.java
@@ -1,8 +1,8 @@
 package org.apereo.cas.config;
 
-import com.zaxxer.hikari.HikariDataSource;
 import org.apereo.cas.configuration.model.core.EventsProperties;
 import org.apereo.cas.configuration.model.support.jpa.DatabaseProperties;
+import org.apereo.cas.configuration.model.support.jpa.JpaConfigDataHolder;
 import org.apereo.cas.support.events.dao.CasEventRepository;
 import org.apereo.cas.support.events.jpa.JpaCasEventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +17,8 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
-import java.util.Properties;
+
+import static org.apereo.cas.configuration.support.Beans.*;
 
 /**
  * This is {@link JpaEventsConfiguration}, defines certain beans via configuration
@@ -45,10 +46,7 @@ public class JpaEventsConfiguration {
     @RefreshScope
     @Bean
     public HibernateJpaVendorAdapter jpaEventVendorAdapter() {
-        final HibernateJpaVendorAdapter jpaEventVendorAdapter = new HibernateJpaVendorAdapter();
-        jpaEventVendorAdapter.setGenerateDdl(this.generateDdl);
-        jpaEventVendorAdapter.setShowSql(this.showSql);
-        return jpaEventVendorAdapter;
+        return newHibernateJpaVendorAdapter(this.databaseProperties);
     }
 
 
@@ -60,24 +58,7 @@ public class JpaEventsConfiguration {
     @RefreshScope
     @Bean
     public DataSource dataSourceEvent() {
-        try {
-            final HikariDataSource bean = new HikariDataSource();
-            bean.setDriverClassName(this.driverClass);
-            bean.setJdbcUrl(this.jdbcUrl);
-            bean.setUsername(this.user);
-            bean.setPassword(this.password);
-
-            bean.setMaximumPoolSize(this.maxPoolSize);
-            bean.setMinimumIdle(this.maxIdleTimeExcessConnections);
-
-            bean.setLoginTimeout(this.checkoutTimeout);
-            bean.setValidationTimeout(this.checkoutTimeout);
-
-            return bean;
-        }
-        catch (final Exception e) {
-            throw new RuntimeException(e);
-        }
+        return newHickariDataSource(this.eventsProperties.getJpa().getDatabase());
     }
 
     /**
@@ -98,21 +79,16 @@ public class JpaEventsConfiguration {
     @RefreshScope
     @Bean
     public LocalContainerEntityManagerFactoryBean eventsEntityManagerFactory() {
-        final LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
+        final LocalContainerEntityManagerFactoryBean bean =
+                newEntityManagerFactoryBean(
+                        new JpaConfigDataHolder(
+                                jpaEventVendorAdapter(),
+                                "jpaEventRegistryContext",
+                                jpaEventPackagesToScan(),
+                                dataSourceEvent()),
+                        this.eventsProperties.getJpa().getDatabase());
 
-        bean.setJpaVendorAdapter(jpaEventVendorAdapter());
-        bean.setPersistenceUnitName("jpaEventRegistryContext");
-        bean.setPackagesToScan(jpaEventPackagesToScan());
-        bean.setDataSource(dataSourceEvent());
-
-        final Properties properties = new Properties();
-        properties.put("hibernate.dialect", this.hibernateDialect);
-        properties.put("hibernate.hbm2ddl.auto", this.hibernateHbm2DdlAuto);
-        properties.put("hibernate.jdbc.batch_size", this.hibernateBatchSize);
-        properties.put("hibernate.enable_lazy_load_no_trans", Boolean.TRUE);
-        bean.setJpaProperties(properties);
-        //TODO
-        bean.getJpaPropertyMap();
+        bean.getJpaPropertyMap().put("hibernate.enable_lazy_load_no_trans", Boolean.TRUE);
         return bean;
     }
 
