156fca3b4fcecaa2691bb24ac7decd8ab5fb7448
Charles Heidemann <charles.heidemann@gmail.com>
2016-06-09 16:17:31 -0300

    Fix for issue #1829  (#1830)
    * SAML Response is invalid message #1827
    
    * Use $skewAllowance for add to SubjectConformationData/NotOnOrAfter #1829

--- cas-server-support-saml-idp/src/main/java/org/apereo/cas/support/saml/web/idp/profile/builders/SamlProfileSamlSubjectBuilder.java
+++ cas-server-support-saml-idp/src/main/java/org/apereo/cas/support/saml/web/idp/profile/builders/SamlProfileSamlSubjectBuilder.java
@@ -3,7 +3,6 @@ package org.apereo.cas.support.saml.web.idp.profile.builders;
 import org.apereo.cas.support.saml.services.SamlRegisteredService;
 import org.apereo.cas.support.saml.services.idp.metadata.SamlRegisteredServiceServiceProviderMetadataFacade;
 import org.apereo.cas.support.saml.util.AbstractSaml20ObjectBuilder;
-import org.apereo.cas.util.DateTimeUtils;
 import org.jasig.cas.client.validation.Assertion;
 import org.apereo.cas.support.saml.SamlException;
 import org.opensaml.saml.saml2.core.AuthnRequest;
@@ -11,8 +10,12 @@ import org.opensaml.saml.saml2.core.NameID;
 import org.opensaml.saml.saml2.core.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
+
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -32,6 +35,9 @@ public class SamlProfileSamlSubjectBuilder extends AbstractSaml20ObjectBuilder i
     @Qualifier("samlProfileSamlNameIdBuilder")
     private SamlProfileSamlNameIdBuilder ssoPostProfileSamlNameIdBuilder;
 
+    @Value("${cas.samlidp.response.skewAllowance:0}")
+    private int skewAllowance;
+    
     @Override
     public Subject build(final AuthnRequest authnRequest, final HttpServletRequest request, final HttpServletResponse response,
                                final Assertion assertion, final SamlRegisteredService service,
@@ -46,9 +52,10 @@ public class SamlProfileSamlSubjectBuilder extends AbstractSaml20ObjectBuilder i
                                  final SamlRegisteredService service,
                                  final SamlRegisteredServiceServiceProviderMetadataFacade adaptor) throws SamlException {
         final NameID nameID = this.ssoPostProfileSamlNameIdBuilder.build(authnRequest, request, response, assertion, service, adaptor);
+        final ZonedDateTime validFromDate = ZonedDateTime.ofInstant(assertion.getValidFromDate().toInstant(), ZoneOffset.UTC);
         final Subject subject = newSubject(nameID.getFormat(), nameID.getValue(),
                 authnRequest.getAssertionConsumerServiceURL(),
-                DateTimeUtils.zonedDateTimeOf(assertion.getValidFromDate()),
+                validFromDate.plusSeconds(this.skewAllowance),
                 authnRequest.getID());
         subject.setNameID(nameID);
         return subject;
