0545653979d72c935cdb0e49e2834ac9ed316e70
Misagh Moayyed <mmoayyed@unicon.net>
2016-07-01 23:24:03 -0700

    Updated config

--- cas-server-core-util/src/main/java/org/apereo/cas/util/AsciiArtUtils.java
+++ cas-server-core-util/src/main/java/org/apereo/cas/util/AsciiArtUtils.java
@@ -14,15 +14,8 @@ import java.io.PrintStream;
  * @since 5.0.0
  */
 public final class AsciiArtUtils {
-    private static final String ANSI_BLUE = "\u001B[34m";
     private static final String ANSI_RESET = "\u001B[0m";
-    private static final String ANSI_BLACK = "\u001B[30m";
-    private static final String ANSI_RED = "\u001B[31m";
-    private static final String ANSI_GREEN = "\u001B[32m";
-    private static final String ANSI_YELLOW = "\u001B[33m";
-    private static final String ANSI_PURPLE = "\u001B[35m";
     private static final String ANSI_CYAN = "\u001B[36m";
-    private static final String ANSI_WHITE = "\u001B[37m";
 
     private AsciiArtUtils() {
     }
@@ -46,7 +39,7 @@ public final class AsciiArtUtils {
      */
     public static void printAsciiArt(final Logger out, final String asciiArt, final String additional) {
         try {
-            out.warn(ANSI_RED);
+            out.warn(ANSI_CYAN);
             out.warn("\n\n" + FigletFont.convertOneLine(asciiArt) + additional);
             out.warn(ANSI_RESET);
         } catch (final Exception e) {
