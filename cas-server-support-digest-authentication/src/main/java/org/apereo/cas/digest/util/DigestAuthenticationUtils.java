a50c5117b8b0b70a24cb9d6a6e86c36822974e0e
Misagh Moayyed <mmoayyed@unicon.net>
2016-07-11 09:39:22 -0700

    Updated docs

--- cas-server-support-digest-authentication/src/main/java/org/apereo/cas/digest/util/DigestAuthenticationUtils.java
+++ cas-server-support-digest-authentication/src/main/java/org/apereo/cas/digest/util/DigestAuthenticationUtils.java
@@ -18,6 +18,11 @@ public final class DigestAuthenticationUtils {
     private DigestAuthenticationUtils() {
     }
 
+    /**
+     * Create nonce string.
+     *
+     * @return the nonce
+     */
     public static String createNonce() {
         final String fmtDate = ZonedDateTime.now().toString();
         final SecureRandom rand = new SecureRandom();
@@ -25,14 +30,34 @@ public final class DigestAuthenticationUtils {
         return DigestUtils.md5Hex(fmtDate + randomInt.toString());
     }
 
+    /**
+     * Create cnonce string.
+     *
+     * @return the cnonce
+     */
     public static String createCnonce() {
         return DigestScheme.createCnonce();
     }
 
+    /**
+     * Create opaque.
+     *
+     * @param domain the domain
+     * @param nonce  the nonce
+     * @return the opaque
+     */
     public static String createOpaque(final String domain, final String nonce) {
         return DigestUtils.md5Hex(domain + nonce);
     }
 
+    /**
+     * Create authenticate header, containing the realm, nonce, opaque, etc.
+     *
+     * @param realm      the realm
+     * @param authMethod the auth method
+     * @param nonce      the nonce
+     * @return the header string
+     */
     public static String createAuthenticateHeader(final String realm, final String authMethod, final String nonce) {
         String header = "";
         header += "Digest realm=\"" + realm + "\",";
