dbcf745652054cab303986b60ef992919f834534
Robert Kornmesser <rkorn@gfz-potsdam.de>
2016-07-15 10:53:54 +0200

    Fix codacy indentation level issues

--- cas-server-support-ldap/src/main/java/org/apereo/cas/authentication/PooledCompareCustomAttributeAuthenticationHandler.java
+++ cas-server-support-ldap/src/main/java/org/apereo/cas/authentication/PooledCompareCustomAttributeAuthenticationHandler.java
@@ -15,56 +15,45 @@ import org.ldaptive.auth.AuthenticationHandlerResponse;
 import org.ldaptive.auth.PooledCompareAuthenticationHandler;
 import org.ldaptive.pool.PooledConnectionFactory;
 
-public class PooledCompareCustomAttributeAuthenticationHandler extends
-		PooledCompareAuthenticationHandler {
+public class PooledCompareCustomAttributeAuthenticationHandler extends PooledCompareAuthenticationHandler {
 
-	protected static final String DEFAULT_PASSWORD_ATTRIBUTE_NAME = "userPassword";
+    protected static final String DEFAULT_PASSWORD_ATTRIBUTE_NAME = "userPassword";
 
-	private String passwordAttributeName = DEFAULT_PASSWORD_ATTRIBUTE_NAME;
-	
-	public PooledCompareCustomAttributeAuthenticationHandler(PooledConnectionFactory cf) {
-		super(cf);
+    private String passwordAttributeName = DEFAULT_PASSWORD_ATTRIBUTE_NAME;
+
+    public PooledCompareCustomAttributeAuthenticationHandler(PooledConnectionFactory cf) {
+	super(cf);
+    }
+
+    public String getPasswordAttributeName() {
+	return passwordAttributeName;
+    }
+
+    public void setPasswordAttributeName(String passwordAttributeName) {
+	this.passwordAttributeName = passwordAttributeName;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected AuthenticationHandlerResponse authenticateInternal(final Connection c,
+	    final AuthenticationCriteria criteria) throws LdapException {
+	byte[] hash;
+	try {
+	    final MessageDigest md = MessageDigest.getInstance(getPasswordScheme());
+	    md.update(criteria.getCredential().getBytes());
+	    hash = md.digest();
+	} catch (NoSuchAlgorithmException e) {
+	    throw new LdapException(e);
 	}
 
-	public String getPasswordAttributeName() {
-		return passwordAttributeName;
-	}
+	final LdapAttribute la = new LdapAttribute(getPasswordAttributeName(),
+		String.format("{%s}%s", getPasswordScheme(), LdapUtils.base64Encode(hash)).getBytes());
+	final CompareOperation compare = new CompareOperation(c);
+	final CompareRequest request = new CompareRequest(criteria.getDn(), la);
+	request.setControls(getAuthenticationControls());
 
-	public void setPasswordAttributeName(String passwordAttributeName) {
-		this.passwordAttributeName = passwordAttributeName;
-	}
-
-	/** {@inheritDoc} */
-	@Override
-	  protected AuthenticationHandlerResponse authenticateInternal(
-	    final Connection c,
-	    final AuthenticationCriteria criteria)
-	    throws LdapException
-	  {
-	    byte[] hash;
-	    try {
-	      final MessageDigest md = MessageDigest.getInstance(getPasswordScheme());
-	      md.update(criteria.getCredential().getBytes());
-	      hash = md.digest();
-	    } catch (NoSuchAlgorithmException e) {
-	      throw new LdapException(e);
-	    }
-
-	    final LdapAttribute la = new LdapAttribute(
-	    		getPasswordAttributeName(),
-	      String.format("{%s}%s", getPasswordScheme(), LdapUtils.base64Encode(hash)).getBytes());
-	    final CompareOperation compare = new CompareOperation(c);
-	    final CompareRequest request = new CompareRequest(criteria.getDn(), la);
-	    request.setControls(getAuthenticationControls());
-
-	    final Response<Boolean> compareResponse = compare.execute(request);
-	    return
-	      new AuthenticationHandlerResponse(
-	        compareResponse.getResult(),
-	        compareResponse.getResultCode(),
-	        c,
-	        compareResponse.getMessage(),
-	        compareResponse.getControls(),
-	        compareResponse.getMessageId());
-	  }
+	final Response<Boolean> compareResponse = compare.execute(request);
+	return new AuthenticationHandlerResponse(compareResponse.getResult(), compareResponse.getResultCode(), c,
+		compareResponse.getMessage(), compareResponse.getControls(), compareResponse.getMessageId());
+    }
 }
