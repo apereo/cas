8eb7008a40abfbbd20ac95a8d941e2058d486d8e
Misagh Moayyed <mm1844@gmail.com>
2016-07-25 02:40:23 -0700

    Updated docs

--- cas-server-support-reports/src/main/java/org/apereo/cas/web/report/DashboardController.java
+++ cas-server-support-reports/src/main/java/org/apereo/cas/web/report/DashboardController.java
@@ -1,11 +1,16 @@
 package org.apereo.cas.web.report;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.bus.BusProperties;
+import org.springframework.cloud.config.server.config.ConfigServerProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * This is {@link DashboardController}.
@@ -15,7 +20,13 @@ import javax.servlet.http.HttpServletResponse;
  */
 @Controller("dashboardController")
 public class DashboardController {
-
+    
+    @Autowired
+    private BusProperties busProperties;
+    
+    @Autowired
+    private ConfigServerProperties configServerProperties;
+    
     /**
      * Handle request internal model and view.
      *
@@ -25,10 +36,12 @@ public class DashboardController {
      * @throws Exception the exception
      */
     @RequestMapping("/status/dashboard")
-    protected ModelAndView handleRequestInternal(
-            final HttpServletRequest request, final HttpServletResponse response)
+    protected ModelAndView handleRequestInternal(final HttpServletRequest request, final HttpServletResponse response)
             throws Exception {
 
-        return new ModelAndView("monitoring/viewDashboard");
+        final Map<String, String> model = new HashMap<>();
+        model.put("refreshEndpoint", busProperties.isEnabled() ? "/cas" + configServerProperties.getPrefix() 
+                + "/cas/bus/refresh" : "/cas/status/refresh");
+        return new ModelAndView("monitoring/viewDashboard", model);
     }
 }
