80e30685e10b86ece7984feac22bc01562ee48d4
Misagh Moayyed <mmoayyed@unicon.net>
2016-08-01 02:35:58 -0700

    Fixed check style issues

--- cas-server-support-jpa-service-registry/src/test/java/org/apereo/cas/services/JpaServiceRegistryDaoImplTests.java
+++ cas-server-support-jpa-service-registry/src/test/java/org/apereo/cas/services/JpaServiceRegistryDaoImplTests.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.ConfigFileApplicationContextInitializer;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
