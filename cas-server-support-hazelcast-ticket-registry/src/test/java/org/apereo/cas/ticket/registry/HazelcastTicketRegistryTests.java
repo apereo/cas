3cf0a2d98eb4a53de34b8a67b25ec7c565c1f079
Misagh Moayyed <mmoayyed@unicon.net>
2016-08-03 08:28:32 -0700

    Added hz properties for multicast config

--- cas-server-support-hazelcast-ticket-registry/src/test/java/org/apereo/cas/ticket/registry/HazelcastTicketRegistryTests.java
+++ cas-server-support-hazelcast-ticket-registry/src/test/java/org/apereo/cas/ticket/registry/HazelcastTicketRegistryTests.java
@@ -13,9 +13,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Collection;
 
@@ -27,9 +28,9 @@ import static org.junit.Assert.*;
  * @author Dmitriy Kopylenko
  * @since 4.1.0
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(locations = {"classpath:HazelcastTicketRegistryTests-context.xml"},
-        classes = {RefreshAutoConfiguration.class})
+@RunWith(SpringRunner.class)
+@ContextConfiguration(locations = {"classpath:HazelcastTicketRegistryTests-context.xml"})
+@SpringBootTest(classes = {RefreshAutoConfiguration.class})
 public class HazelcastTicketRegistryTests {
     @Autowired
     @Qualifier("hzTicketRegistry1")
