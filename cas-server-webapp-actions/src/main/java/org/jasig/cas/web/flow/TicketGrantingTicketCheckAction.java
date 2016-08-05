3f50dfc6fc38391868cff9068c48743587fe5f1e
danielshim <daniel.shim@ziontech.com>
2016-08-05 15:47:20 +0900

    Added codes to logout when a user change a tenant(sub domain) after logging in another tenant

--- cas-server-webapp-actions/src/main/java/org/jasig/cas/web/flow/TicketGrantingTicketCheckAction.java
+++ cas-server-webapp-actions/src/main/java/org/jasig/cas/web/flow/TicketGrantingTicketCheckAction.java
@@ -1,8 +1,10 @@
 package org.jasig.cas.web.flow;
 
 import org.jasig.cas.CentralAuthenticationService;
+import org.jasig.cas.authentication.principal.Service;
 import org.jasig.cas.ticket.AbstractTicketException;
 import org.jasig.cas.ticket.Ticket;
+import org.jasig.cas.ticket.TicketGrantingTicket;
 import org.jasig.cas.web.support.WebUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -12,6 +14,10 @@ import org.springframework.webflow.action.AbstractAction;
 import org.springframework.webflow.execution.Event;
 import org.springframework.webflow.execution.RequestContext;
 
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.constraints.NotNull;
 
 /**
@@ -80,7 +86,17 @@ public class TicketGrantingTicketCheckAction extends AbstractAction {
         String eventId = INVALID;
         try {
             final Ticket ticket = this.centralAuthenticationService.getTicket(tgtId, Ticket.class);
-            if (ticket != null && !ticket.isExpired()) {
+            
+            final TicketGrantingTicket ticketGrantingTicket = (TicketGrantingTicket) ticket;
+            final HttpServletRequest request = WebUtils.getHttpServletRequest(requestContext);
+            
+            final Map<String, Service> services = ticketGrantingTicket.getServices();
+            final Entry<String, Service> service = services.entrySet().iterator().next();
+            final String serviceId = service.getValue().getId();
+            final String tenantNameOfService = AuthUtils.extractTenantID(serviceId);
+            final String tenantNameOfRequest = AuthUtils.extractTenantID(request);
+            
+            if (ticket != null && !ticket.isExpired() && tenantNameOfService.equals(tenantNameOfRequest)) {
                 eventId = VALID;
             }
         } catch (final AbstractTicketException e) {
