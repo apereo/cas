4a4a85c816a66d454f7f8556c5b79c549108a2ec
Misagh Moayyed <mmoayyed@unicon.net>
2016-08-08 12:38:37 -0700

    Ported memcache encryption bug fix over to master

--- cas-server-support-memcached-ticket-registry/src/main/java/org/apereo/cas/ticket/registry/MemCacheTicketRegistry.java
+++ cas-server-support-memcached-ticket-registry/src/main/java/org/apereo/cas/ticket/registry/MemCacheTicketRegistry.java
@@ -47,7 +47,7 @@ public class MemCacheTicketRegistry extends AbstractTicketRegistry {
         final Ticket ticket = encodeTicket(ticketToUpdate);
         logger.debug("Updating ticket {}", ticket);
         try {
-            if (!this.client.replace(ticket.getId(), getTimeout(ticket), ticket).get()) {
+            if (!this.client.replace(ticket.getId(), getTimeout(ticketToUpdate), ticket).get()) {
                 logger.error("Failed to update {}", ticket);
             }
         } catch (final InterruptedException e) {
@@ -68,7 +68,7 @@ public class MemCacheTicketRegistry extends AbstractTicketRegistry {
         final Ticket ticket = encodeTicket(ticketToAdd);
         logger.debug("Adding ticket {}", ticket);
         try {
-            if (!this.client.add(ticket.getId(), getTimeout(ticket), ticket).get()) {
+            if (!this.client.add(ticket.getId(), getTimeout(ticketToAdd), ticket).get()) {
                 logger.error("Failed to add {}", ticket);
             }
         } catch (final InterruptedException e) {
