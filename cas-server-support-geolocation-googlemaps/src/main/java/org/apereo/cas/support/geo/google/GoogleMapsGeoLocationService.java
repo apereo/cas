9e4adaeec92836c00b8cc0e59ecd00aed12e3779
Misagh Moayyed <mmoayyed@unicon.net>
2016-08-09 04:48:25 -0700

    Working on googlemaps geo profiling

--- cas-server-support-geolocation-googlemaps/src/main/java/org/apereo/cas/support/geo/google/GoogleMapsGeoLocationService.java
+++ cas-server-support-geolocation-googlemaps/src/main/java/org/apereo/cas/support/geo/google/GoogleMapsGeoLocationService.java
@@ -1,9 +1,23 @@
 package org.apereo.cas.support.geo.google;
 
+import com.google.maps.GaeRequestHandler;
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+import com.google.maps.model.GeocodingResult;
+import com.google.maps.model.LatLng;
+import io.userinfo.client.UserInfo;
+import io.userinfo.client.model.Info;
+import org.apache.commons.lang3.StringUtils;
+import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.support.geo.GeoLocation;
 import org.apereo.cas.support.geo.GeoLocationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.net.InetAddress;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 /**
  * This is {@link GoogleMapsGeoLocationService}.
@@ -12,20 +26,54 @@ import java.net.InetAddress;
  * @since 5.0.0
  */
 public class GoogleMapsGeoLocationService implements GeoLocationService {
-    private String apiKey;
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+    
+    @Autowired
+    private CasConfigurationProperties casProperties;
 
-    public GoogleMapsGeoLocationService(final String apiKey) {
-        this.apiKey = apiKey;
+    private final GeoApiContext context;
+    
+    public GoogleMapsGeoLocationService() {
+        
+        if (casProperties.getGoogleMaps().isGoogleAppsEngine()) {
+            context = new GeoApiContext(new GaeRequestHandler());
+        } else {
+            context = new GeoApiContext();
+        }
+        if (StringUtils.isNotBlank(casProperties.getGoogleMaps().getClientId())
+                && StringUtils.isNotBlank(casProperties.getGoogleMaps().getClientSecret())) {
+
+            context.setEnterpriseCredentials(casProperties.getGoogleMaps().getClientId(),
+                    casProperties.getGoogleMaps().getClientSecret());
+        }
+        context.setApiKey(casProperties.getGoogleMaps().getApiKey());
+        context.setConnectTimeout(casProperties.getGoogleMaps().getConnectTimeout(), TimeUnit.MILLISECONDS);
     }
 
     @Override
     public GeoLocation locate(final InetAddress address) {
-        return null;
+        return locate(address.getHostAddress());
     }
 
     @Override
     public GeoLocation locate(final String address) {
+        final Info info = UserInfo.getInfo(address);
+        return locate(info.getPosition().getLatitude(), info.getPosition().getLongitude());
+    }
+
+    @Override
+    public GeoLocation locate(final double latitude, final double longitude) {
+        final LatLng latlng = new LatLng(latitude, longitude);
+        try {
+            final GeocodingResult[] results = GeocodingApi.reverseGeocode(this.context, latlng).await();
+            if (results != null && results.length > 0) {
+                final GeocodingResult res = Arrays.stream(results).findFirst().get();
+                
+                return new GeoLocation();
+            }
+        } catch (final Exception e) {
+            logger.error(e.getMessage(), e);
+        }
         return null;
     }
-    
 }
