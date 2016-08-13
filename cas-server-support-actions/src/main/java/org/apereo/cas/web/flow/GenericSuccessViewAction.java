f3961be186306432571f4dc6fa07a582cdcf10dc
Misagh Moayyed <mm1844@gmail.com>
2016-08-13 09:49:43 -0700

    added default redirect url when service is not specified.

--- cas-server-support-actions/src/main/java/org/apereo/cas/web/flow/GenericSuccessViewAction.java
+++ cas-server-support-actions/src/main/java/org/apereo/cas/web/flow/GenericSuccessViewAction.java
@@ -1,8 +1,14 @@
 package org.apereo.cas.web.flow;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apereo.cas.CentralAuthenticationService;
 import org.apereo.cas.authentication.principal.NullPrincipal;
 import org.apereo.cas.authentication.principal.Principal;
+import org.apereo.cas.authentication.principal.Service;
+import org.apereo.cas.authentication.principal.ServiceFactory;
+import org.apereo.cas.services.RegisteredService;
+import org.apereo.cas.services.RegisteredServiceAccessStrategyUtils;
+import org.apereo.cas.services.ServicesManager;
 import org.apereo.cas.ticket.InvalidTicketException;
 import org.apereo.cas.ticket.TicketGrantingTicket;
 import org.apereo.cas.web.support.WebUtils;
@@ -14,33 +20,54 @@ import org.springframework.webflow.execution.RequestContext;
 
 /**
  * Action that should execute prior to rendering the generic-success login view.
+ *
  * @author Misagh Moayyed
  * @since 4.1.0
  */
 public class GenericSuccessViewAction extends AbstractAction {
-    /** Log instance for logging events, info, warnings, errors, etc. */
-    private transient Logger logger = LoggerFactory.getLogger(this.getClass());
-    
-    private CentralAuthenticationService centralAuthenticationService;
 
-    public GenericSuccessViewAction() {
-    }
+    /**
+     * Log instance for logging events, info, warnings, errors, etc.
+     */
+    private transient Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private CentralAuthenticationService centralAuthenticationService;
+    private ServicesManager servicesManager;
+    private ServiceFactory serviceFactory;
+
+    private String redirectUrl;
 
     /**
      * Instantiates a new Generic success view action.
      *
      * @param centralAuthenticationService the central authentication service
      */
-    public GenericSuccessViewAction(final CentralAuthenticationService centralAuthenticationService) {
+    public GenericSuccessViewAction(final CentralAuthenticationService centralAuthenticationService,
+                                    final ServicesManager servicesManager,
+                                    final ServiceFactory serviceFactory) {
         this.centralAuthenticationService = centralAuthenticationService;
+        this.servicesManager = servicesManager;
+        this.serviceFactory = serviceFactory;
+    }
+    
+    public void setRedirectUrl(final String redirectUrl) {
+        this.redirectUrl = redirectUrl;
     }
 
     @Override
     protected Event doExecute(final RequestContext requestContext) throws Exception {
-        final String tgt = WebUtils.getTicketGrantingTicketId(requestContext);
-        return success(getAuthenticationPrincipal(tgt));
+        if (StringUtils.isNotBlank(this.redirectUrl)) {
+            final Service service = this.serviceFactory.createService(this.redirectUrl);
+            final RegisteredService registeredService = this.servicesManager.findServiceBy(service);
+            RegisteredServiceAccessStrategyUtils.ensureServiceAccessIsAllowed(service, registeredService);
+            requestContext.getExternalContext().requestExternalRedirect(service.getId());
+        } else {
+            final String tgt = WebUtils.getTicketGrantingTicketId(requestContext);
+            WebUtils.putPrincipal(requestContext, getAuthenticationPrincipal(tgt));
+        }
+        return success();
     }
-    
+
     /**
      * Gets authentication principal.
      *
@@ -53,7 +80,7 @@ public class GenericSuccessViewAction extends AbstractAction {
             final TicketGrantingTicket ticketGrantingTicket =
                     this.centralAuthenticationService.getTicket(ticketGrantingTicketId, TicketGrantingTicket.class);
             return ticketGrantingTicket.getAuthentication().getPrincipal();
-        } catch (final InvalidTicketException e){
+        } catch (final InvalidTicketException e) {
             logger.warn(e.getMessage());
         }
         logger.debug("In the absence of valid TGT, the authentication principal cannot be determined. Returning {}",
