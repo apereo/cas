271ede0ab35de8a72657442660aaaa5728d08ee5
Misagh Moayyed <mm1844@gmail.com>
2016-08-16 00:48:20 -0700

    fixed check style issues

--- cas-server-support-saml-sp-integrations/src/main/java/org/apereo/cas/util/SamlSPUtils.java
+++ cas-server-support-saml-sp-integrations/src/main/java/org/apereo/cas/util/SamlSPUtils.java
@@ -18,8 +18,6 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static org.bouncycastle.asn1.x500.style.RFC4519Style.*;
-
 /**
  * This is {@link SamlSPUtils}.
  *
