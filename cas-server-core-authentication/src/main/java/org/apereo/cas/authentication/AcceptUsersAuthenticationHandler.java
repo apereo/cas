98d843fc9fe0f663b6ee452f3dd23a8f38e9d166
Misagh Moayyed <mm1844@gmail.com>
2016-08-17 07:40:19 -0700

    removed simulated failure from authn

--- cas-server-core-authentication/src/main/java/org/apereo/cas/authentication/AcceptUsersAuthenticationHandler.java
+++ cas-server-core-authentication/src/main/java/org/apereo/cas/authentication/AcceptUsersAuthenticationHandler.java
@@ -51,14 +51,7 @@ public class AcceptUsersAuthenticationHandler extends AbstractUsernamePasswordAu
     @Override
     protected HandlerResult authenticateUsernamePasswordInternal(final UsernamePasswordCredential credential)
             throws GeneralSecurityException, PreventedException {
-
-        if (1==1) {
-            throw new AccountPasswordMustChangeException("change the password");
-        }
         
-        if (this.users == null || this.users.isEmpty()) {
-            throw new FailedLoginException("No user can be accepted because none is defined");
-        }
         final String username = credential.getUsername();
         final String cachedPassword = this.users.get(username);
 
