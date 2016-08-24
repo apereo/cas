d8d3a8512e73e8a658cd073d5d3338b717703d46
serv <liuyuhua69@gmail.com>
2016-08-24 23:10:23 +0800

    fixed Some checks

--- cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/redis/RedisTicketRegistryProperties.java
+++ cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/redis/RedisTicketRegistryProperties.java
@@ -7,143 +7,143 @@ package org.apereo.cas.configuration.model.support.redis;
  */
 public class RedisTicketRegistryProperties {
 
-	/**
-	 * Database index used by the connection factory.
-	 */
-	private int database = 0;
+    /**
+     * Database index used by the connection factory.
+     */
+    private int database = 0;
 
-	/**
-	 * Redis server host.
-	 */
-	private String host = "localhost";
+    /**
+     * Redis server host.
+     */
+    private String host = "localhost";
 
-	/**
-	 * Login password of the redis server.
-	 */
-	private String password;
+    /**
+     * Login password of the redis server.
+     */
+    private String password;
 
-	/**
-	 * Redis server port.
-	 */
-	private int port = 6379;
+    /**
+     * Redis server port.
+     */
+    private int port = 6379;
 
-	/**
-	 * Connection timeout in milliseconds.
-	 */
-	private int timeout;
+    /**
+     * Connection timeout in milliseconds.
+     */
+    private int timeout;
 
-	private Pool pool;
+    private Pool pool;
 
-	public int getDatabase() {
-		return this.database;
-	}
+    public int getDatabase() {
+        return this.database;
+    }
 
-	public void setDatabase(int database) {
-		this.database = database;
-	}
+    public void setDatabase(int database) {
+        this.database = database;
+    }
 
-	public String getHost() {
-		return this.host;
-	}
+    public String getHost() {
+        return this.host;
+    }
 
-	public void setHost(String host) {
-		this.host = host;
-	}
+    public void setHost(String host) {
+        this.host = host;
+    }
 
-	public String getPassword() {
-		return this.password;
-	}
+    public String getPassword() {
+        return this.password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public int getPort() {
-		return this.port;
-	}
+    public int getPort() {
+        return this.port;
+    }
 
-	public void setPort(int port) {
-		this.port = port;
-	}
+    public void setPort(int port) {
+        this.port = port;
+    }
 
-	public void setTimeout(int timeout) {
-		this.timeout = timeout;
-	}
+    public void setTimeout(int timeout) {
+        this.timeout = timeout;
+    }
 
-	public int getTimeout() {
-		return this.timeout;
-	}
+    public int getTimeout() {
+        return this.timeout;
+    }
 
-	public Pool getPool() {
-		return this.pool;
-	}
+    public Pool getPool() {
+        return this.pool;
+    }
 
-	public void setPool(Pool pool) {
-		this.pool = pool;
-	}
+    public void setPool(Pool pool) {
+        this.pool = pool;
+    }
 
-	/**
-	 * Pool properties.
-	 */
-	public static class Pool {
+    /**
+     * Pool properties.
+     */
+    public static class Pool {
 
-		/**
-		 * Max number of "idle" connections in the pool. Use a negative value to indicate
-		 * an unlimited number of idle connections.
-		 */
-		private int maxIdle = 8;
+        /**
+         * Max number of "idle" connections in the pool. Use a negative value to indicate
+         * an unlimited number of idle connections.
+         */
+        private int maxIdle = 8;
 
-		/**
-		 * Target for the minimum number of idle connections to maintain in the pool. This
-		 * setting only has an effect if it is positive.
-		 */
-		private int minIdle = 0;
+        /**
+         * Target for the minimum number of idle connections to maintain in the pool. This
+         * setting only has an effect if it is positive.
+         */
+        private int minIdle = 0;
 
-		/**
-		 * Max number of connections that can be allocated by the pool at a given time.
-		 * Use a negative value for no limit.
-		 */
-		private int maxActive = 8;
+        /**
+         * Max number of connections that can be allocated by the pool at a given time.
+         * Use a negative value for no limit.
+         */
+        private int maxActive = 8;
 
-		/**
-		 * Maximum amount of time (in milliseconds) a connection allocation should block
-		 * before throwing an exception when the pool is exhausted. Use a negative value
-		 * to block indefinitely.
-		 */
-		private int maxWait = -1;
+        /**
+         * Maximum amount of time (in milliseconds) a connection allocation should block
+         * before throwing an exception when the pool is exhausted. Use a negative value
+         * to block indefinitely.
+         */
+        private int maxWait = -1;
 
-		public int getMaxIdle() {
-			return this.maxIdle;
-		}
+        public int getMaxIdle() {
+            return this.maxIdle;
+        }
 
-		public void setMaxIdle(int maxIdle) {
-			this.maxIdle = maxIdle;
-		}
+        public void setMaxIdle(int maxIdle) {
+            this.maxIdle = maxIdle;
+        }
 
-		public int getMinIdle() {
-			return this.minIdle;
-		}
+        public int getMinIdle() {
+            return this.minIdle;
+        }
 
-		public void setMinIdle(int minIdle) {
-			this.minIdle = minIdle;
-		}
+        public void setMinIdle(int minIdle) {
+            this.minIdle = minIdle;
+        }
 
-		public int getMaxActive() {
-			return this.maxActive;
-		}
+        public int getMaxActive() {
+            return this.maxActive;
+        }
 
-		public void setMaxActive(int maxActive) {
-			this.maxActive = maxActive;
-		}
+        public void setMaxActive(int maxActive) {
+            this.maxActive = maxActive;
+        }
 
-		public int getMaxWait() {
-			return this.maxWait;
-		}
+        public int getMaxWait() {
+            return this.maxWait;
+        }
 
-		public void setMaxWait(int maxWait) {
-			this.maxWait = maxWait;
-		}
+        public void setMaxWait(int maxWait) {
+            this.maxWait = maxWait;
+        }
 
-	}
+    }
 
 }
