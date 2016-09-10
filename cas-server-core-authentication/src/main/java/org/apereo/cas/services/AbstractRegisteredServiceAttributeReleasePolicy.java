fa1de5f57f503b1f0a005bb94525d6cd5ffc28a1
Misagh Moayyed <mmoayyed@unicon.net>
2016-09-10 14:57:22 +0430

    Default principal attributes for release

--- cas-server-core-authentication/src/main/java/org/apereo/cas/services/AbstractRegisteredServiceAttributeReleasePolicy.java
+++ cas-server-core-authentication/src/main/java/org/apereo/cas/services/AbstractRegisteredServiceAttributeReleasePolicy.java
@@ -1,39 +1,57 @@
 package org.apereo.cas.services;
 
+import com.google.common.collect.Maps;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apereo.cas.authentication.principal.DefaultPrincipalAttributesRepository;
 import org.apereo.cas.authentication.principal.Principal;
 import org.apereo.cas.authentication.principal.PrincipalAttributesRepository;
+import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.util.ApplicationContextProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
 
 import java.util.Map;
+import java.util.Set;
+
+import static com.google.common.collect.Maps.*;
 
 /**
  * Abstract release policy for attributes, provides common shared settings such as loggers and attribute filter config.
  * Subclasses are to provide the behavior for attribute retrieval.
+ *
  * @author Misagh Moayyed
  * @since 4.1.0
  */
 public abstract class AbstractRegisteredServiceAttributeReleasePolicy implements RegisteredServiceAttributeReleasePolicy {
-    
+
     private static final long serialVersionUID = 5325460875620586503L;
 
-    /** The logger. */
+    /**
+     * The logger.
+     */
     private static final Logger LOGGER = LoggerFactory.getLogger(AbstractRegisteredServiceAttributeReleasePolicy.class);
 
-    /** The attribute filter. */
+    /**
+     * The attribute filter.
+     */
     private RegisteredServiceAttributeFilter registeredServiceAttributeFilter;
 
-    /** Attribute repository that refreshes attributes for a principal. **/
+    /**
+     * Attribute repository that refreshes attributes for a principal.
+     **/
     private PrincipalAttributesRepository principalAttributesRepository = new DefaultPrincipalAttributesRepository();
 
-    /** Authorize the release of credential for this service. Default is false. **/
+    /**
+     * Authorize the release of credential for this service. Default is false.
+     **/
     private boolean authorizedToReleaseCredentialPassword;
 
-    /** Authorize the release of PGT for this service. Default is false. **/
+    /**
+     * Authorize the release of PGT for this service. Default is false.
+     **/
     private boolean authorizedToReleaseProxyGrantingTicket;
 
     @Override
@@ -78,16 +96,66 @@ public abstract class AbstractRegisteredServiceAttributeReleasePolicy implements
 
     @Override
     public Map<String, Object> getAttributes(final Principal p) {
+        LOGGER.debug("Locating principal attributes for {}", p.getId());
         final Map<String, Object> principalAttributes = this.principalAttributesRepository == null
                 ? p.getAttributes() : this.principalAttributesRepository.getAttributes(p);
-        final Map<String, Object> attributesToRelease = getAttributesInternal(principalAttributes);
-        
+        LOGGER.debug("Found principal attributes {} for {}", principalAttributes, p.getId());
+
+        LOGGER.debug("Calling attribute policy {} to process attributes for {}", getClass().getSimpleName(), p.getId());
+        final Map<String, Object> policyAttributes = getAttributesInternal(principalAttributes);
+        LOGGER.debug("Attribute policy {} allows release of {} for {}", getClass().getSimpleName(), policyAttributes, p.getId());
+
+        LOGGER.debug("Checking default attribute policy attributes");
+        final Map<String, Object> defaultAttributes = getReleasedByDefaultAttributes(p, principalAttributes);
+        LOGGER.debug("Default attributes found to be released are {}", defaultAttributes);
+
+        LOGGER.debug("Attempting to merge policy attributes and default attributes");
+        final Map<String, Object> attributesToRelease = newTreeMap(String.CASE_INSENSITIVE_ORDER);
+
+        LOGGER.debug("Adding default attributes first to the released set of attributes");
+        attributesToRelease.putAll(defaultAttributes);
+
+        LOGGER.debug("Adding policy attributes to the released set of attributes");
+        attributesToRelease.putAll(policyAttributes);
+
         if (this.registeredServiceAttributeFilter != null) {
+            LOGGER.debug("Invoking attribute filter on the final set of attributes");
             return this.registeredServiceAttributeFilter.filter(attributesToRelease);
         }
+
+        LOGGER.debug("Final collection of attributes allowed are: {}", attributesToRelease);
         return attributesToRelease;
     }
-    
+
+    /**
+     * Determines a default bundle of attributes that may be released to all services
+     * without the explicit mapping for each service.
+     *
+     * @param p          the principal
+     * @param attributes the attributes
+     * @return the released by default attributes
+     */
+    protected Map<String, Object> getReleasedByDefaultAttributes(final Principal p, final Map<String, Object> attributes) {
+        final ApplicationContext ctx = ApplicationContextProvider.getApplicationContext();
+        if (ctx != null) {
+            LOGGER.debug("Located application context. Retrieving default attributes for release, if any");
+            final CasConfigurationProperties props = ctx.getAutowireCapableBeanFactory().getBean(CasConfigurationProperties.class);
+            final Set<String> defaultAttrs = props.getAuthn().getAttributeRepository().getDefaultAttributesToRelease();
+            LOGGER.debug("Default attributes for release are: {}", defaultAttrs);
+
+            final Map<String, Object> defaultAttributesToRelease = Maps.newTreeMap(String.CASE_INSENSITIVE_ORDER);
+            defaultAttrs.stream().forEach(key -> {
+                if (attributes.containsKey(key)) {
+                    LOGGER.debug("Found and added default attribute for release: {}", key);
+                    defaultAttributesToRelease.put(key, attributes.get(key));
+                }
+            });
+            return defaultAttributesToRelease;
+        }
+
+        return newTreeMap();
+    }
+
     /**
      * Gets the attributes internally from the implementation.
      *
