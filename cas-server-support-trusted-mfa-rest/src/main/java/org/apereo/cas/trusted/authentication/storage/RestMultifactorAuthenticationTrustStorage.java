c8457e5fd4b0c50630409bcf58b4038f300b2e98
Misagh Moayyed <mm1844@gmail.com>
2016-09-17 13:27:02 +0430

    Updated docs

--- cas-server-support-trusted-mfa-rest/src/main/java/org/apereo/cas/trusted/authentication/storage/RestMultifactorAuthenticationTrustStorage.java
+++ cas-server-support-trusted-mfa-rest/src/main/java/org/apereo/cas/trusted/authentication/storage/RestMultifactorAuthenticationTrustStorage.java
@@ -2,14 +2,11 @@ package org.apereo.cas.trusted.authentication.storage;
 
 import com.google.common.collect.Sets;
 import org.apereo.cas.trusted.authentication.api.MultifactorAuthenticationTrustRecord;
-import org.apereo.cas.util.cipher.NoOpCipherExecutor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * This is {@link RestMultifactorAuthenticationTrustStorage}.
@@ -28,35 +25,24 @@ public class RestMultifactorAuthenticationTrustStorage extends BaseMultifactorAu
     @Override
     public Set<MultifactorAuthenticationTrustRecord> get(final String principal) {
         final String url = (!this.endpoint.endsWith("/") ? this.endpoint.concat("/") : this.endpoint).concat(principal);
-
         final RestTemplate restTemplate = new RestTemplate();
-        
         final ResponseEntity<MultifactorAuthenticationTrustRecord[]> responseEntity = 
                 restTemplate.getForEntity(url, MultifactorAuthenticationTrustRecord[].class);
         if (responseEntity.getStatusCode() == HttpStatus.OK) {
-            final Object[] results = responseEntity.getBody();
-            return Arrays.stream(results)
-                    .map(e -> MultifactorAuthenticationTrustRecord.class.cast(e))
-                    .collect(Collectors.toSet());
+            final MultifactorAuthenticationTrustRecord[] results = responseEntity.getBody();
+            return Sets.newHashSet(results);
         }
+        
         return Sets.newHashSet();
     }
 
     @Override
     protected MultifactorAuthenticationTrustRecord setInternal(final MultifactorAuthenticationTrustRecord record) {
         final RestTemplate restTemplate = new RestTemplate();
-        final Integer status = restTemplate.postForObject(this.endpoint, record, Integer.class);
-        if (status == HttpStatus.OK.value()) {
+        final ResponseEntity<Object> response = restTemplate.postForEntity(this.endpoint, record, Object.class);
+        if (response != null && response.getStatusCode() == HttpStatus.OK) {
             return record;
         }
         return null;
-    }
-
-    public static void main(final String[] args) {
-        RestMultifactorAuthenticationTrustStorage r = new RestMultifactorAuthenticationTrustStorage(
-                "http://demo5926981.mockable.io/"
-        );
-        r.setCipherExecutor(new NoOpCipherExecutor());
-        r.get("casuser");
     }
 }
