80e0bc15241540a765403f03a890ba6a51f8ee39
Misagh Moayyed <mmoayyed@unicon.net>
2016-09-20 13:58:45 +0430

    Update push-javadoc-to-gh-pages.sh

--- travis/push-javadoc-to-gh-pages.sh
+++ travis/push-javadoc-to-gh-pages.sh
@@ -50,12 +50,18 @@ if [[ "$invokeJavadoc" == true || "$invokeDoc" == true ]]; then
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
   git config --global pack.threads "24"
+  
   echo -e "Cloning the repository to push documentation...\n"
   git clone --quiet https://${GH_TOKEN}@github.com/apereo/cas gh-pages > /dev/null
+  
   cd gh-pages
   
+  echo -e "Configuring tracking branches for repository:\n"
+  for branch in `git branch -a | grep remotes | grep -v HEAD | grep -v master`; do
+     git branch --track ${branch##*/} $branch
+  done
+
   echo -e "Switching to gh-pages branch\n"
-  
   git checkout gh-pages
   git status
 
@@ -113,8 +119,8 @@ if [[ "$invokeJavadoc" == true || "$invokeDoc" == true ]]; then
   git count-objects -vH
   
   echo -e "Pushing upstream to origin/gh-pages...\n"
-  git push -fq origin gh-pages > /dev/null
+  git push -fq origin --all > /dev/null
 
-  echo -e "Successfully published documentation to [gh-pages] branch.\n"
+  echo -e "Successfully published documentation.\n"
 
 fi
