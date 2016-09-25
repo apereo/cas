3b19ba5a712071fe77f867d05e122f51b5867a74
Misagh Moayyed <mmoayyed@unicon.net>
2016-09-25 11:25:37 +0330

    Updated tests

--- support/cas-server-support-stormpath/src/test/java/org/apereo/cas/authentication/StormpathAuthenticationHandlerTests.java
+++ support/cas-server-support-stormpath/src/test/java/org/apereo/cas/authentication/StormpathAuthenticationHandlerTests.java
@@ -10,8 +10,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.ConfigFileApplicationContextInitializer;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
 import org.springframework.mock.web.MockHttpServletRequest;
