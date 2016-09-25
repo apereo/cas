62dab47196780f9a330921e6dbd48b624bc2b0c2
Misagh Moayyed <mm1844@gmail.com>
2016-09-25 10:59:02 +0330

    Updated tests

--- support/cas-server-support-wsfederation/src/test/java/org/apereo/cas/support/wsfederation/AbstractWsFederationTests.java
+++ support/cas-server-support-wsfederation/src/test/java/org/apereo/cas/support/wsfederation/AbstractWsFederationTests.java
@@ -1,13 +1,30 @@
 package org.apereo.cas.support.wsfederation;
 
 import org.apereo.cas.config.CasCoreAuthenticationConfiguration;
+import org.apereo.cas.config.CasCoreConfiguration;
 import org.apereo.cas.config.CasCoreServicesConfiguration;
+import org.apereo.cas.config.CasCoreTicketsConfiguration;
 import org.apereo.cas.config.CasCoreUtilConfiguration;
+import org.apereo.cas.config.CasCoreWebConfiguration;
+import org.apereo.cas.config.CasPersonDirectoryAttributeRepositoryConfiguration;
+import org.apereo.cas.config.CoreSamlConfiguration;
+import org.apereo.cas.config.SamlConfiguration;
+import org.apereo.cas.logout.config.CasCoreLogoutConfiguration;
 import org.apereo.cas.support.saml.AbstractOpenSamlTests;
 import org.apereo.cas.support.wsfederation.config.WsFederationAuthenticationConfiguration;
+import org.apereo.cas.validation.config.CasCoreValidationConfiguration;
+import org.apereo.cas.web.config.CasCookieConfiguration;
+import org.apereo.cas.web.config.CasProtocolViewsConfiguration;
+import org.apereo.cas.web.config.CasValidationConfiguration;
+import org.apereo.cas.web.flow.config.CasCoreWebflowConfiguration;
 import org.junit.runner.RunWith;
+import org.springframework.aop.framework.AopConfigException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
@@ -17,9 +34,27 @@ import org.springframework.test.context.junit4.SpringRunner;
  * @since 4.2
  */
 @RunWith(SpringRunner.class)
-@SpringApplicationConfiguration(locations = {"classpath:/applicationContext.xml"},
-        classes = {WsFederationAuthenticationConfiguration.class, CasCoreAuthenticationConfiguration.class,
-                CasCoreServicesConfiguration.class, CasCoreUtilConfiguration.class})
+@SpringBootTest(
+        classes = {WsFederationAuthenticationConfiguration.class, 
+                CasCoreAuthenticationConfiguration.class,
+                CasCoreServicesConfiguration.class,
+                CoreSamlConfiguration.class,
+                CasCoreWebConfiguration.class,
+                CasCoreWebflowConfiguration.class,
+                RefreshAutoConfiguration.class,
+                AopAutoConfiguration.class,
+                CasCookieConfiguration.class,
+                CasCoreAuthenticationConfiguration.class,
+                CasCoreTicketsConfiguration.class,
+                CasCoreLogoutConfiguration.class,
+                CasValidationConfiguration.class,
+                CasProtocolViewsConfiguration.class,
+                CasCoreValidationConfiguration.class,
+                CasCoreConfiguration.class,
+                SamlConfiguration.class,
+                CasPersonDirectoryAttributeRepositoryConfiguration.class,
+                CasCoreUtilConfiguration.class})
+@ContextConfiguration(locations = {"classpath:/applicationContext.xml"})
 public class AbstractWsFederationTests extends AbstractOpenSamlTests {
 
     @Autowired
