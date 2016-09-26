0e01e6eb5ccd99f2798dc3798df06e9a68a4f257
Misagh Moayyed <mm1844@gmail.com>
2016-09-26 23:24:24 +0330

    Trusted Devices UI

--- webapp/cas-server-webapp/src/main/resources/static/js/trustedDevices.js
+++ webapp/cas-server-webapp/src/main/resources/static/js/trustedDevices.js
@@ -16,15 +16,30 @@ var trustedDevices = (function () {
 
     var trustedDevicesTable = function (jsonData) {
         var t = $('#trustedDevicesTable').DataTable({
+            "order": [[2, "desc"]],
+            retrieve: true,
+            columnDefs: [
+
+                {"width": "20%", "targets": 0},
+                {"width": "10%", "targets": 1},
+                {"width": "60%", "targets": 2},
+                {"width": "10%", "targets": 3},
+                {"width": "30%", "targets": 4}
+            ]
         });
         for (var i = 0; i < jsonData.length; i++) {
+
             var rec = jsonData[i];
             t.row.add([
-                
+                rec.name,
+                rec.principal,
+                new Date(rec.date),
+                rec.geography,
+                "<button class='btn btn-sm btn-danger' disabled type='button' value='ALL'>Revoke</button>"
             ]).draw(false);
         }
     };
-    
+
     // initialization *******
     (function init() {
         getData();
