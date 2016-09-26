b7dbdb8d20cecf1b1e7f6bded7d1582a82cef99e
Misagh Moayyed <mm1844@gmail.com>
2016-09-26 23:24:24 +0330

    Trusted Devices UI

--- support/cas-server-support-reports/src/main/java/org/apereo/cas/web/report/TrustedDevicesController.java
+++ support/cas-server-support-reports/src/main/java/org/apereo/cas/web/report/TrustedDevicesController.java
@@ -1,14 +1,21 @@
 package org.apereo.cas.web.report;
 
+import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.trusted.authentication.api.MultifactorAuthenticationTrustRecord;
 import org.apereo.cas.trusted.authentication.api.MultifactorAuthenticationTrustStorage;
+import org.apereo.cas.util.DateTimeUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDate;
+import java.util.Set;
 
 /**
  * This is {@link TrustedDevicesController}.
@@ -21,6 +28,9 @@ import javax.servlet.http.HttpServletResponse;
 @ConditionalOnClass(value = MultifactorAuthenticationTrustStorage.class)
 public class TrustedDevicesController {
 
+    @Autowired
+    private CasConfigurationProperties casProperties;
+    
     private final MultifactorAuthenticationTrustStorage mfaTrustEngine;
 
     public TrustedDevicesController(final MultifactorAuthenticationTrustStorage mfaTrustEngine) {
@@ -40,4 +50,24 @@ public class TrustedDevicesController {
             throws Exception {
         return new ModelAndView("monitoring/viewTrustedDevices");
     }
+
+    /**
+     * Gets records.
+     *
+     * @param request  the request
+     * @param response the response
+     * @return the records
+     * @throws Exception the exception
+     */
+    @RequestMapping(value = "/getRecords", method = RequestMethod.GET)
+    @ResponseBody
+    public Set<MultifactorAuthenticationTrustRecord> getRecords(final HttpServletRequest request,
+                                                                      final HttpServletResponse response)
+            throws Exception {
+        final LocalDate onOrAfter = LocalDate.now().minus(casProperties.getAuthn().getMfa().getTrusted().getExpiration(),
+                DateTimeUtils.toChronoUnit(casProperties.getAuthn().getMfa().getTrusted().getTimeUnit()));
+        this.mfaTrustEngine.expire(onOrAfter);
+        return this.mfaTrustEngine.get(onOrAfter);
+    }
+
 }
