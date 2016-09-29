d6195012d0b1de3548ae146cab3d0403c63cbc43
Misagh Moayyed <mm1844@gmail.com>
2016-09-29 12:12:00 +0330

    Fixed validation response view to ignore whitespace

--- support/cas-server-support-themes/src/main/java/org/apereo/cas/services/web/config/CasThemesConfiguration.java
+++ support/cas-server-support-themes/src/main/java/org/apereo/cas/services/web/config/CasThemesConfiguration.java
@@ -1,5 +1,7 @@
 package org.apereo.cas.services.web.config;
 
+import com.google.common.collect.Sets;
+import org.apache.velocity.runtime.directive.Macro;
 import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.services.ServicesManager;
 import org.apereo.cas.services.web.RegisteredServiceThemeBasedViewResolver;
@@ -12,11 +14,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ThemeResolver;
 import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.dialect.IPostProcessorDialect;
+import org.thymeleaf.engine.AbstractTemplateHandler;
+import org.thymeleaf.model.ICloseElementTag;
+import org.thymeleaf.model.IOpenElementTag;
+import org.thymeleaf.model.IText;
+import org.thymeleaf.postprocessor.IPostProcessor;
+import org.thymeleaf.postprocessor.PostProcessor;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * This is {@link CasThemesConfiguration}.
@@ -69,6 +80,24 @@ public class CasThemesConfiguration {
         r.setStaticVariables(this.thymeleafViewResolver.getStaticVariables());
 
         final SpringTemplateEngine engine = SpringTemplateEngine.class.cast(this.thymeleafViewResolver.getTemplateEngine());
+        engine.addDialect(new IPostProcessorDialect() {
+            @Override
+            public int getDialectPostProcessorPrecedence() {
+                return Integer.MAX_VALUE;
+            }
+
+            @Override
+            public Set<IPostProcessor> getPostProcessors() {
+                return Sets.newHashSet(new PostProcessor(TemplateMode.parse(thymeleafProperties.getMode()),
+                        CasThymeleafOutputTemplateHandler.class, Integer.MAX_VALUE));
+            }
+
+            @Override
+            public String getName() {
+                return CasThymeleafOutputTemplateHandler.class.getSimpleName();
+            }
+        });
+
         r.setTemplateEngine(engine);
         r.setViewNames(this.thymeleafViewResolver.getViewNames());
         r.setServicesManager(this.servicesManager);
@@ -88,4 +117,34 @@ public class CasThemesConfiguration {
         return resolver;
     }
 
+    public static class CasThymeleafOutputTemplateHandler extends AbstractTemplateHandler {
+        private boolean writeWhitespace;
+        
+        public CasThymeleafOutputTemplateHandler() {
+        }
+
+        @Override
+        public void handleText(final IText text) {
+            final String contentString = text.getText();
+            if (!contentString.isEmpty() && contentString.trim().isEmpty()) {
+                if (!writeWhitespace) {
+                    return;
+                }
+                writeWhitespace = false;
+            }
+            super.handleText(text);
+        }
+
+        @Override
+        public void handleCloseElement(final ICloseElementTag tag) {
+            super.handleCloseElement(tag);
+            writeWhitespace = true;
+        }
+
+        @Override
+        public void handleOpenElement(final IOpenElementTag openElementTag) {
+            super.handleOpenElement(openElementTag);
+            writeWhitespace = true;
+        }
+    }
 }
