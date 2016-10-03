61406ae71090f4fd10774a104f000a77ab01d914
Misagh Moayyed <mm1844@gmail.com>
2016-10-03 12:47:19 +0330

    Working on electro-fence

--- support/cas-server-support-electrofence/src/main/java/org/apereo/cas/web/flow/RiskAwareAuthenticationWebflowEventResolver.java
+++ support/cas-server-support-electrofence/src/main/java/org/apereo/cas/web/flow/RiskAwareAuthenticationWebflowEventResolver.java
@@ -2,12 +2,15 @@ package org.apereo.cas.web.flow;
 
 import org.apereo.cas.api.AuthenticationRiskEngine;
 import org.apereo.cas.authentication.Authentication;
+import org.apereo.cas.authentication.AuthenticationException;
+import org.apereo.cas.services.MultifactorAuthenticationProvider;
 import org.apereo.cas.services.RegisteredService;
 import org.apereo.cas.web.flow.resolver.impl.AbstractCasWebflowEventResolver;
 import org.apereo.cas.web.support.WebUtils;
 import org.springframework.webflow.execution.Event;
 import org.springframework.webflow.execution.RequestContext;
 
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -31,6 +34,13 @@ public class RiskAwareAuthenticationWebflowEventResolver extends AbstractCasWebf
         if (service == null || authentication == null) {
             logger.debug("No service or authentication is available to determine event for principal");
             return null;
+        }
+
+        final Map<String, MultifactorAuthenticationProvider> providerMap =
+                WebUtils.getAllMultifactorAuthenticationProviders(this.applicationContext);
+        if (providerMap == null || providerMap.isEmpty()) {
+            logger.warn("No multifactor authentication providers are available in the application context");
+            throw new AuthenticationException();
         }
         
         return null;
