95725e25deeb7c25617d044dd625cd21b0642f30
Misagh Moayyed <mm1844@gmail.com>
2016-10-04 14:49:37 +0330

    Handle rest endpoints with full pathWorking on electro-fence

---
+++ support/cas-server-support-electrofence/src/main/java/org/apereo/cas/impl/plans/BlockAuthenticationContingencyPlan.java
@@ -0,0 +1,10 @@
+package org.apereo.cas.impl.plans;
+
+/**
+ * This is {@link BlockAuthenticationContingencyPlan}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.1.0
+ */
+public class BlockAuthenticationContingencyPlan extends BaseAuthenticationRiskContingencyPlan {
+}
