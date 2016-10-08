3000ac029f47658fd7db8e861801fb69931a4bc4
Misagh Moayyed <mmoayyed@unicon.net>
2016-10-08 14:44:27 +0330

    Added tests

--- support/cas-server-support-electrofence/src/test/java/org/apereo/cas/impl/calcs/IpAddressAuthenticationRequestRiskCalculatorTests.java
+++ support/cas-server-support-electrofence/src/test/java/org/apereo/cas/impl/calcs/IpAddressAuthenticationRequestRiskCalculatorTests.java
@@ -1,11 +1,17 @@
 package org.apereo.cas.impl.calcs;
 
 import org.apereo.cas.config.ElectroFenceConfiguration;
+import org.apereo.cas.impl.MockTicketGrantingTicketCreatedEventProducer;
 import org.apereo.cas.support.events.config.CasCoreEventsConfiguration;
+import org.apereo.cas.support.events.dao.CasEventRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -19,7 +25,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(classes = {RefreshAutoConfiguration.class, 
         ElectroFenceConfiguration.class, CasCoreEventsConfiguration.class})
 @TestPropertySource(locations = {"classpath:/ip-calc.properties"})
+@DirtiesContext
 public class IpAddressAuthenticationRequestRiskCalculatorTests {
+    
+    @Autowired
+    @Qualifier("casEventRepository")
+    private CasEventRepository casEventRepository;
+    
+    @Before
+    public void prepTest() {
+        MockTicketGrantingTicketCreatedEventProducer.createEvents(this.casEventRepository); 
+    }
     
     @Test
     public void verifyTest() {}
