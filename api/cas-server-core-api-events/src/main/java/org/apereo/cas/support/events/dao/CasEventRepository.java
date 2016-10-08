78581e7b4921cd089c1d554b82b2f1dc4f8392d4
Misagh Moayyed <mm1844@gmail.com>
2016-10-08 15:10:04 +0330

    Working on risk-based authn.

--- api/cas-server-core-api-events/src/main/java/org/apereo/cas/support/events/dao/CasEventRepository.java
+++ api/cas-server-core-api-events/src/main/java/org/apereo/cas/support/events/dao/CasEventRepository.java
@@ -1,5 +1,6 @@
 package org.apereo.cas.support.events.dao;
 
+import java.time.ZonedDateTime;
 import java.util.Collection;
 
 /**
@@ -24,6 +25,14 @@ public interface CasEventRepository {
     Collection<CasEvent> load();
 
     /**
+     * Load collection of events created after the given date.
+     *
+     * @param dateTime the date time
+     * @return the collection
+     */
+    Collection<CasEvent> load(ZonedDateTime dateTime);
+
+    /**
      * Gets events of type for principal.
      *
      * @param type      the type
@@ -31,6 +40,16 @@ public interface CasEventRepository {
      * @return the events of type
      */
     Collection<CasEvent> getEventsOfTypeForPrincipal(String type, String principal);
+
+    /**
+     * Gets events of type for principal after date.
+     *
+     * @param type      the type
+     * @param principal the principal
+     * @param dateTime  the date time
+     * @return the events of type
+     */
+    Collection<CasEvent> getEventsOfTypeForPrincipal(String type, String principal, ZonedDateTime dateTime);
 
     /**
      * Gets events of type.
@@ -41,10 +60,28 @@ public interface CasEventRepository {
     Collection<CasEvent> getEventsOfType(String type);
 
     /**
+     * Gets events of type after date.
+     *
+     * @param type     the type
+     * @param dateTime the date time
+     * @return the events of type
+     */
+    Collection<CasEvent> getEventsOfType(String type, ZonedDateTime dateTime);
+
+    /**
      * Gets events for principal.
      *
      * @param id the id
      * @return the events for principal
      */
     Collection<CasEvent> getEventsForPrincipal(String id);
+
+    /**
+     * Gets events for principal after date.
+     *
+     * @param id       the id
+     * @param dateTime the date time
+     * @return the events for principal
+     */
+    Collection<CasEvent> getEventsForPrincipal(String id, ZonedDateTime dateTime);
 }
--- core/cas-server-core-events/src/main/java/org/apereo/cas/support/events/dao/AbstractCasEventRepository.java
+++ core/cas-server-core-events/src/main/java/org/apereo/cas/support/events/dao/AbstractCasEventRepository.java
@@ -3,6 +3,7 @@ package org.apereo.cas.support.events.dao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.ZonedDateTime;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
@@ -26,6 +27,42 @@ public abstract class AbstractCasEventRepository implements CasEventRepository {
         return getEventsForPrincipal(principal)
                 .stream()
                 .filter(event -> event.getType().equals(type))
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public Collection<CasEvent> getEventsForPrincipal(final String id) {
+        return load().stream().filter(e -> e.getPrincipalId().equalsIgnoreCase(id)).collect(Collectors.toSet());
+    }
+
+    @Override
+    public Collection<CasEvent> load(final ZonedDateTime dateTime) {
+        return load().stream()
+                .filter(e -> e.getCreationTime().isEqual(dateTime) || e.getCreationTime().isAfter(dateTime))
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public Collection<CasEvent> getEventsOfTypeForPrincipal(final String type, final String principal, final ZonedDateTime dateTime) {
+        return getEventsOfTypeForPrincipal(type, principal)
+                .stream()
+                .filter(e -> e.getCreationTime().isEqual(dateTime) || e.getCreationTime().isAfter(dateTime))
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public Collection<CasEvent> getEventsOfType(final String type, final ZonedDateTime dateTime) {
+        return getEventsOfType(type)
+                .stream()
+                .filter(e -> e.getCreationTime().isEqual(dateTime) || e.getCreationTime().isAfter(dateTime))
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public Collection<CasEvent> getEventsForPrincipal(final String id, final ZonedDateTime dateTime) {
+        return getEventsForPrincipal(id)
+                .stream()
+                .filter(e -> e.getCreationTime().isEqual(dateTime) || e.getCreationTime().isAfter(dateTime))
                 .collect(Collectors.toSet());
     }
 }
