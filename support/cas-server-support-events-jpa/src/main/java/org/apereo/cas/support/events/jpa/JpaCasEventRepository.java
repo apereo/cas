d8dcc7fe982cad5295d82d534fb3b0409b7a3685
Misagh Moayyed <mm1844@gmail.com>
2016-10-08 15:10:04 +0330

    Working on risk-based authn.

--- support/cas-server-support-events-jpa/src/main/java/org/apereo/cas/support/events/jpa/JpaCasEventRepository.java
+++ support/cas-server-support-events-jpa/src/main/java/org/apereo/cas/support/events/jpa/JpaCasEventRepository.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.time.ZonedDateTime;
 import java.util.Collection;
 
 /**
@@ -18,7 +19,9 @@ import java.util.Collection;
 @EnableTransactionManagement(proxyTargetClass = true)
 @Transactional(readOnly = false, transactionManager = "transactionManagerEvents")
 public class JpaCasEventRepository extends AbstractCasEventRepository {
-    
+
+    private static final String SELECT_QUERY = "SELECT r from CasEvent r ";
+
     @PersistenceContext(unitName = "eventsEntityManagerFactory")
     private EntityManager entityManager;
 
@@ -26,7 +29,7 @@ public class JpaCasEventRepository extends AbstractCasEventRepository {
     public String toString() {
         return getClass().getSimpleName();
     }
-    
+
     @Override
     public void save(final CasEvent event) {
         this.entityManager.merge(event);
@@ -34,12 +37,47 @@ public class JpaCasEventRepository extends AbstractCasEventRepository {
 
     @Override
     public Collection<CasEvent> load() {
-        return this.entityManager.createQuery("SELECT r FROM CasEvent r", CasEvent.class).getResultList();
+        return this.entityManager.createQuery(SELECT_QUERY.trim(), CasEvent.class).getResultList();
     }
 
     @Override
     public Collection<CasEvent> getEventsForPrincipal(final String id) {
-        return this.entityManager.createQuery("select r from CasEvent r where r.principalId = :principalId",
+        return this.entityManager.createQuery(SELECT_QUERY.concat("where r.principalId = :principalId"),
                 CasEvent.class).setParameter("principalId", id).getResultList();
+    }
+
+    @Override
+    public Collection<CasEvent> load(final ZonedDateTime dateTime) {
+        return this.entityManager.createQuery(SELECT_QUERY.concat("where r.creationTime >= :creationTime"),
+                CasEvent.class).setParameter("creationTime", dateTime.toString()).getResultList();
+    }
+
+    @Override
+    public Collection<CasEvent> getEventsOfTypeForPrincipal(final String type, final String principal, final ZonedDateTime dateTime) {
+        return this.entityManager.createQuery(
+                SELECT_QUERY.concat("where r.type = :type and r.creationTime >= :creationTime and r.principalId = :principalId"),
+                CasEvent.class)
+                .setParameter("type", type)
+                .setParameter("principalId", principal)
+                .setParameter("creationTime", dateTime.toString())
+                .getResultList();
+    }
+
+    @Override
+    public Collection<CasEvent> getEventsOfType(final String type, final ZonedDateTime dateTime) {
+        return this.entityManager.createQuery(
+                SELECT_QUERY.concat("where r.type = :type and r.creationTime >= :creationTime"), CasEvent.class)
+                .setParameter("type", type)
+                .setParameter("creationTime", dateTime.toString())
+                .getResultList();
+    }
+
+    @Override
+    public Collection<CasEvent> getEventsForPrincipal(final String id, final ZonedDateTime dateTime) {
+        return this.entityManager.createQuery(
+                SELECT_QUERY.concat("where r.principalId = :principalId and r.creationTime >= :creationTime"), CasEvent.class)
+                .setParameter("principalId", id)
+                .setParameter("creationTime", dateTime.toString())
+                .getResultList();
     }
 }
