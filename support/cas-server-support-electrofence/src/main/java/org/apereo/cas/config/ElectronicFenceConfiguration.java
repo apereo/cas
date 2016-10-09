468bc0521ff6e362dbcb923317084dc1fe2aeb1e
Misagh Moayyed <mmoayyed@unicon.net>
2016-10-10 00:58:49 +0330

    Working on events auto-wiring

--- support/cas-server-support-electrofence/src/main/java/org/apereo/cas/config/ElectronicFenceConfiguration.java
+++ support/cas-server-support-electrofence/src/main/java/org/apereo/cas/config/ElectronicFenceConfiguration.java
@@ -1,17 +1,23 @@
 package org.apereo.cas.config;
 
 import com.google.common.collect.Sets;
+import org.apache.commons.lang3.StringUtils;
 import org.apereo.cas.api.AuthenticationRequestRiskCalculator;
 import org.apereo.cas.api.AuthenticationRiskContingencyPlan;
 import org.apereo.cas.api.AuthenticationRiskEvaluator;
 import org.apereo.cas.api.AuthenticationRiskMitigator;
+import org.apereo.cas.api.AuthenticationRiskNotifier;
 import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.configuration.model.core.authentication.RiskBasedAuthenticationProperties;
 import org.apereo.cas.impl.calcs.DateTimeAuthenticationRequestRiskCalculator;
 import org.apereo.cas.impl.calcs.GeoLocationAuthenticationRequestRiskCalculator;
 import org.apereo.cas.impl.calcs.IpAddressAuthenticationRequestRiskCalculator;
 import org.apereo.cas.impl.calcs.UserAgentAuthenticationRequestRiskCalculator;
 import org.apereo.cas.impl.engine.DefaultAuthenticationRiskEvaluator;
 import org.apereo.cas.impl.engine.DefaultAuthenticationRiskMitigator;
+import org.apereo.cas.impl.notify.AuthenticationRiskEmailNotifier;
+import org.apereo.cas.impl.notify.AuthenticationRiskTwilioSmsNotifier;
+import org.apereo.cas.impl.plans.BaseAuthenticationRiskContingencyPlan;
 import org.apereo.cas.impl.plans.BlockAuthenticationContingencyPlan;
 import org.apereo.cas.impl.plans.MultifactorAuthenticationContingencyPlan;
 import org.apereo.cas.support.events.dao.CasEventRepository;
@@ -47,6 +53,20 @@ public class ElectronicFenceConfiguration {
     @Autowired
     private CasConfigurationProperties casProperties;
 
+    @ConditionalOnMissingBean(name = "authenticationRiskEmailNotifier")
+    @Bean
+    @RefreshScope
+    public AuthenticationRiskNotifier authenticationRiskEmailNotifier() {
+        return new AuthenticationRiskEmailNotifier();
+    }
+
+    @ConditionalOnMissingBean(name = "authenticationRiskSmsNotifier")
+    @Bean
+    @RefreshScope
+    public AuthenticationRiskNotifier authenticationRiskSmsNotifier() {
+        return new AuthenticationRiskTwilioSmsNotifier();
+    }
+
     @ConditionalOnMissingBean(name = "riskAwareAuthenticationWebflowEventResolver")
     @Bean
     @RefreshScope
@@ -58,14 +78,18 @@ public class ElectronicFenceConfiguration {
     @Bean
     @RefreshScope
     public AuthenticationRiskContingencyPlan blockAuthenticationContingencyPlan() {
-        return new BlockAuthenticationContingencyPlan();
+        final BlockAuthenticationContingencyPlan b = new BlockAuthenticationContingencyPlan();
+        configureContingencyPlan(b);
+        return b;
     }
 
     @ConditionalOnMissingBean(name = "multifactorAuthenticationContingencyPlan")
     @Bean
     @RefreshScope
     public AuthenticationRiskContingencyPlan multifactorAuthenticationContingencyPlan() {
-        return new MultifactorAuthenticationContingencyPlan();
+        final MultifactorAuthenticationContingencyPlan b = new MultifactorAuthenticationContingencyPlan();
+        configureContingencyPlan(b);
+        return b;
     }
 
     @ConditionalOnMissingBean(name = "authenticationRiskMitigator")
@@ -110,26 +134,46 @@ public class ElectronicFenceConfiguration {
     @Bean
     @RefreshScope
     public AuthenticationRiskEvaluator authenticationRiskEvaluator() {
-        final Set<AuthenticationRequestRiskCalculator> calcs = Sets.newHashSet();
+        final Set<AuthenticationRequestRiskCalculator> calculators = Sets.newHashSet();
 
         if (casProperties.getAuthn().getAdaptive().getRisk().getIp().isEnabled()) {
-            calcs.add(ipAddressAuthenticationRequestRiskCalculator());
+            calculators.add(ipAddressAuthenticationRequestRiskCalculator());
         }
         if (casProperties.getAuthn().getAdaptive().getRisk().getAgent().isEnabled()) {
-            calcs.add(userAgentAuthenticationRequestRiskCalculator());
+            calculators.add(userAgentAuthenticationRequestRiskCalculator());
         }
         if (casProperties.getAuthn().getAdaptive().getRisk().getDateTime().isEnabled()) {
-            calcs.add(dateTimeAuthenticationRequestRiskCalculator());
+            calculators.add(dateTimeAuthenticationRequestRiskCalculator());
         }
         if (casProperties.getAuthn().getAdaptive().getRisk().getGeoLocation().isEnabled()) {
-            calcs.add(geoLocationAuthenticationRequestRiskCalculator());
+            calculators.add(geoLocationAuthenticationRequestRiskCalculator());
         }
 
-        if (calcs.isEmpty()) {
+        if (calculators.isEmpty()) {
             LOGGER.warn("No risk calculators are define to examine authentication requests");
         }
 
-        return new DefaultAuthenticationRiskEvaluator(calcs);
+        return new DefaultAuthenticationRiskEvaluator(calculators);
     }
 
+    private void configureContingencyPlan(final BaseAuthenticationRiskContingencyPlan b) {
+        final RiskBasedAuthenticationProperties.Response.Mail mail =
+                casProperties.getAuthn().getAdaptive().getRisk().getResponse().getMail();
+
+        final RiskBasedAuthenticationProperties.Response.Sms sms =
+                casProperties.getAuthn().getAdaptive().getRisk().getResponse().getSms();
+
+        if (StringUtils.isNotBlank(mail.getText())
+                && StringUtils.isNotBlank(mail.getFrom())
+                && StringUtils.isNotBlank(mail.getSubject())) {
+            b.getNotifiers().add(authenticationRiskEmailNotifier());
+        }
+
+        if (StringUtils.isNotBlank(sms.getText())
+                && StringUtils.isNotBlank(sms.getFrom())
+                && StringUtils.isNotBlank(sms.getTwilio().getToken())
+                && StringUtils.isNotBlank(sms.getTwilio().getAccountId())) {
+            b.getNotifiers().add(authenticationRiskSmsNotifier());
+        }
+    }
 }
