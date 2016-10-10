800f75382a46eb380f3c725e38ac14db00212477
Misagh Moayyed <mm1844@gmail.com>
2016-10-10 12:23:14 +0330

    Added view for blocked authentication

--- support/cas-server-support-electrofence/src/main/java/org/apereo/cas/impl/plans/BlockAuthenticationContingencyPlan.java
+++ support/cas-server-support-electrofence/src/main/java/org/apereo/cas/impl/plans/BlockAuthenticationContingencyPlan.java
@@ -15,10 +15,13 @@ import javax.servlet.http.HttpServletRequest;
  * @since 5.1.0
  */
 public class BlockAuthenticationContingencyPlan extends BaseAuthenticationRiskContingencyPlan {
+    /** Block authentication event. */
+    public static final String EVENT_ID_BLOCK_AUTHN = "blockedAuthentication";
+    
     @Override
     protected AuthenticationRiskContingencyResponse executeInternal(final Authentication authentication, final RegisteredService service, 
                                                                     final AuthenticationRiskScore score, final HttpServletRequest request) {
         
-        return new AuthenticationRiskContingencyResponse(new Event(this, "blockedAuthentication"));
+        return new AuthenticationRiskContingencyResponse(new Event(this, EVENT_ID_BLOCK_AUTHN));
     }
 }
