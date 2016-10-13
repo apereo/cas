bd56c224b221f32a1f38d2e8ef8d66d17ba0ce43
Misagh Moayyed <mm1844@gmail.com>
2016-10-13 23:13:29 +0330

    Fixed check style issues

--- core/cas-server-core-authentication/src/main/java/org/apereo/cas/config/CasPersonDirectoryAttributeRepositoryConfiguration.java
+++ core/cas-server-core-authentication/src/main/java/org/apereo/cas/config/CasPersonDirectoryAttributeRepositoryConfiguration.java
@@ -34,7 +34,6 @@ import org.springframework.core.io.Resource;
 import javax.naming.directory.SearchControls;
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
