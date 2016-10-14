b73a7c5285844e938c925ede84661e90f4519aaa
Misagh Moayyed <mmoayyed@unicon.net>
2016-10-14 13:17:55 +0330

    Updated docs

--- api/cas-server-core-api-authentication/src/main/java/org/apereo/cas/authentication/RootCasException.java
+++ api/cas-server-core-api-authentication/src/main/java/org/apereo/cas/authentication/RootCasException.java
@@ -16,7 +16,7 @@ public abstract class RootCasException extends RuntimeException {
     /**
      * The code description of the exception.
      */
-    private final String code;
+    private String code;
 
     /**
      * Constructor that takes a {@code code} description of the error along with the exception
@@ -69,5 +69,14 @@ public abstract class RootCasException extends RuntimeException {
     @Override
     public String toString() {
         return this.getCode();
+    }
+
+    /**
+     * Sets code.
+     *
+     * @param code the code
+     */
+    protected void setCode(final String code) {
+        this.code = code;
     }
 }
