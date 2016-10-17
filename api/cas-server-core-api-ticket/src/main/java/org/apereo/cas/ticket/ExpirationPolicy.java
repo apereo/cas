f4a5cd305f54a940870e6684a8681c96d4409b08
Misagh Moayyed <mm1844@gmail.com>
2016-10-17 18:57:22 +0330

    Updated docs

--- api/cas-server-core-api-ticket/src/main/java/org/apereo/cas/ticket/ExpirationPolicy.java
+++ api/cas-server-core-api-ticket/src/main/java/org/apereo/cas/ticket/ExpirationPolicy.java
@@ -24,16 +24,18 @@ public interface ExpirationPolicy extends Serializable {
     /**
      * Describes the time duration where this policy should consider the item alive.
      * Once this time passes, the item is considered expired and dead.
-     * @return alive time in seconds. A zero value indicates the time duration
-     * is not supported or is inactive.
+     * @return A zero value indicates the time duration
+     * is not supported or is inactive. Unit of measure is defined
+     * by the implementation.
      */
     Long getTimeToLive();
 
     /**
      * Describes the idle time duration for the item.
      *
-     * @return idle time in seconds. A zero value indicates the time duration
-     * is not supported or is inactive.
+     * @return idle time. A zero value indicates the time duration
+     * is not supported or is inactive. Unit of measure is defined
+     * by the implementation.
      */
     Long getTimeToIdle();
 }
