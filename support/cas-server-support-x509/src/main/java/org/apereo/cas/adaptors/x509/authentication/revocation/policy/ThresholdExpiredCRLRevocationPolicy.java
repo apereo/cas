c07f0779743dd11a9a563b12f71b5529d8898ccf
Misagh Moayyed <mm1844@gmail.com>
2016-10-23 15:51:43 +0330

    Bug fixes on x509

--- support/cas-server-support-x509/src/main/java/org/apereo/cas/adaptors/x509/authentication/revocation/policy/ThresholdExpiredCRLRevocationPolicy.java
+++ support/cas-server-support-x509/src/main/java/org/apereo/cas/adaptors/x509/authentication/revocation/policy/ThresholdExpiredCRLRevocationPolicy.java
@@ -1,7 +1,6 @@
 package org.apereo.cas.adaptors.x509.authentication.revocation.policy;
 
 import org.apereo.cas.adaptors.x509.authentication.ExpiredCRLException;
-import org.apereo.cas.adaptors.x509.authentication.revocation.policy.RevocationPolicy;
 import org.apereo.cas.adaptors.x509.util.CertUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
