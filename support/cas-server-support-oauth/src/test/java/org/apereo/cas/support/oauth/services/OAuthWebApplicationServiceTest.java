e38b8d25834627f935a69ace306a2501b1780fe6
David Rodriguez <david.rodriguez@williamhill.com>
2016-10-24 10:40:23 +0200

    Make sure we can serialize and read back OAuthWebApplicationService

---
+++ support/cas-server-support-oauth/src/test/java/org/apereo/cas/support/oauth/services/OAuthWebApplicationServiceTest.java
@@ -0,0 +1,33 @@
+package org.apereo.cas.support.oauth.services;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apereo.cas.services.AbstractRegisteredService;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class OAuthWebApplicationServiceTest {
+
+    private static final File JSON_FILE = new File("oAuthWebApplicationService.json");
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Test
+    public void verifySerializeACompletePrincipalToJson() throws IOException {
+        final AbstractRegisteredService service = new OAuthRegisteredService();
+        service.setName("checkCloning");
+        service.setServiceId("testId");
+        service.setTheme("theme");
+        service.setDescription("description");
+        final OAuthWebApplicationService serviceWritten = new OAuthWebApplicationService(service);
+
+        mapper.writeValue(JSON_FILE, serviceWritten);
+
+        final OAuthWebApplicationService serviceRead = mapper.readValue(JSON_FILE, OAuthWebApplicationService.class);
+
+        assertEquals(serviceWritten, serviceRead);
+    }
+}
