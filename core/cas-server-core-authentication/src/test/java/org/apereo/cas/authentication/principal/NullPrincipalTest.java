030e3e233884685f25290f196b05aaffb9eefc32
David Rodriguez <david.rodriguez@williamhill.com>
2016-10-24 10:14:56 +0200

    Make sure we can serialize and read back NullPrincipal

---
+++ core/cas-server-core-authentication/src/test/java/org/apereo/cas/authentication/principal/NullPrincipalTest.java
@@ -0,0 +1,27 @@
+package org.apereo.cas.authentication.principal;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class NullPrincipalTest {
+
+    private static final File JSON_FILE = new File("nullPrincipal.json");
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Test
+    public void verifySerializeANullPrincipalToJson() throws IOException {
+        final NullPrincipal serviceWritten = NullPrincipal.getInstance();
+
+        mapper.writeValue(JSON_FILE, serviceWritten);
+
+        final NullPrincipal serviceRead = mapper.readValue(JSON_FILE, NullPrincipal.class);
+
+        assertEquals(serviceWritten, serviceRead);
+    }
+}
