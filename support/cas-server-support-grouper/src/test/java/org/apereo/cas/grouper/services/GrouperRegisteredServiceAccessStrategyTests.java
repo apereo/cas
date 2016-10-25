95bb587239dba65f49e388ba306e781cf7010537
David Rodriguez <david.rodriguez@williamhill.com>
2016-10-25 11:15:59 +0200

    Add JSON serialization test for GrouperRegisteredServiceAccessStrategy

--- support/cas-server-support-grouper/src/test/java/org/apereo/cas/grouper/services/GrouperRegisteredServiceAccessStrategyTests.java
+++ support/cas-server-support-grouper/src/test/java/org/apereo/cas/grouper/services/GrouperRegisteredServiceAccessStrategyTests.java
@@ -1,15 +1,21 @@
 package org.apereo.cas.grouper.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apereo.cas.services.RegisteredServiceAccessStrategy;
 import org.apereo.cas.services.TestUtils;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * The {@link GrouperRegisteredServiceAccessStrategyTests} provides
@@ -19,7 +25,10 @@ import java.util.Set;
  * @since 4.2
  */
 public class GrouperRegisteredServiceAccessStrategyTests {
+
     protected transient Logger logger = LoggerFactory.getLogger(this.getClass());
+    private static final File JSON_FILE = new File("grouperRegisteredServiceAccessStrategy.json");
+    private static final ObjectMapper mapper = new ObjectMapper();
 
     @Test
     public void checkGrouperAttributes() {
@@ -33,5 +42,20 @@ public class GrouperRegisteredServiceAccessStrategyTests {
         } else {
             logger.info("{} is not configured. Skipping tests", resource.getFilename());
         }
+    }
+
+    @Test
+    public void verifySerializeAGrouperRegisteredServiceAccessStrategyToJson() throws IOException {
+        final GrouperRegisteredServiceAccessStrategy strategyWritten = new GrouperRegisteredServiceAccessStrategy();
+        final Map<String, Set<String>> requiredAttributes = new HashMap<>();
+        requiredAttributes.put("memberOf", Collections.singleton("admin"));
+        strategyWritten.setRequiredAttributes(requiredAttributes);
+        strategyWritten.doPrincipalAttributesAllowServiceAccess("banderson", (Map) TestUtils.getTestAttributes());
+
+        mapper.writeValue(JSON_FILE, strategyWritten);
+
+        final RegisteredServiceAccessStrategy credentialRead = mapper.readValue(JSON_FILE, GrouperRegisteredServiceAccessStrategy.class);
+
+        assertEquals(strategyWritten, credentialRead);
     }
 }
