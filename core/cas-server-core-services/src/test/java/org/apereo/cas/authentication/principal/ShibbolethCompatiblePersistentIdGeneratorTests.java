aa585e0cf615c18e54eb5ab06c0fd280671288fb
David Rodriguez <david.rodriguez@williamhill.com>
2016-10-25 10:13:23 +0200

    Add JSON serialization support for PersistentIdGenerator implementations

--- core/cas-server-core-services/src/test/java/org/apereo/cas/authentication/principal/ShibbolethCompatiblePersistentIdGeneratorTests.java
+++ core/cas-server-core-services/src/test/java/org/apereo/cas/authentication/principal/ShibbolethCompatiblePersistentIdGeneratorTests.java
@@ -1,17 +1,25 @@
 package org.apereo.cas.authentication.principal;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apereo.cas.services.TestUtils;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import java.io.File;
+import java.io.IOException;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * @author Scott Battaglia
  * @since 3.1
  */
 public class ShibbolethCompatiblePersistentIdGeneratorTests {
+
+    private static final File JSON_FILE = new File("shibbolethCompatiblePersistentIdGenerator.json");
+    private static final ObjectMapper mapper = new ObjectMapper();
 
     @Test
     public void verifyGenerator() {
@@ -25,4 +33,15 @@ public class ShibbolethCompatiblePersistentIdGeneratorTests {
         assertNotNull(value);
     }
 
+    @Test
+    public void verifySerializeAShibbolethCompatiblePersistentIdGeneratorToJson() throws IOException {
+        final ShibbolethCompatiblePersistentIdGenerator generatorWritten =
+                new ShibbolethCompatiblePersistentIdGenerator("scottssalt");
+
+        mapper.writeValue(JSON_FILE, generatorWritten);
+
+        final PersistentIdGenerator credentialRead = mapper.readValue(JSON_FILE, ShibbolethCompatiblePersistentIdGenerator.class);
+
+        assertEquals(generatorWritten, credentialRead);
+    }
 }
