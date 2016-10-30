edd844c4943310b0c1095f4ee122c53bc61ceed5
Misagh Moayyed <mm1844@gmail.com>
2016-10-30 16:24:34 -0700

    Fix issue with ticket timeouts for ehcache and infinispan

--- support/cas-server-support-infinispan-ticket-registry/src/main/java/org/apereo/cas/ticket/registry/InfinispanTicketRegistry.java
+++ support/cas-server-support-infinispan-ticket-registry/src/main/java/org/apereo/cas/ticket/registry/InfinispanTicketRegistry.java
@@ -8,7 +8,8 @@ import java.util.Collection;
 import java.util.concurrent.TimeUnit;
 
 /**
- * This is {@link InfinispanTicketRegistry}. Infinispan is a distributed in-memory key/value data store with optional schema.
+ * This is {@link InfinispanTicketRegistry}. Infinispan is a distributed in-memory
+ * key/value data store with optional schema.
  * It offers advanced functionality such as transactions, events, querying and distributed processing.
  * See <a href="http://infinispan.org/features/">http://infinispan.org/features/</a> for more info.
  *
@@ -16,7 +17,7 @@ import java.util.concurrent.TimeUnit;
  * @since 4.2.0
  */
 public class InfinispanTicketRegistry extends AbstractTicketRegistry {
-    
+
     private Cache cache;
 
     /**
@@ -32,7 +33,7 @@ public class InfinispanTicketRegistry extends AbstractTicketRegistry {
     public void init() {
         logger.info("Setting up Infinispan Ticket Registry...");
     }
-    
+
     @Override
     public void updateTicket(final Ticket ticket) {
         this.cache.put(ticket.getId(), ticket);
@@ -41,11 +42,19 @@ public class InfinispanTicketRegistry extends AbstractTicketRegistry {
     @Override
     public void addTicket(final Ticket ticketToAdd) {
         final Ticket ticket = encodeTicket(ticketToAdd);
-        this.cache.put(ticket.getId(), ticket, 
+
+        final long idleTime = ticket.getExpirationPolicy().getTimeToIdle() <= 0
+                ? ticket.getExpirationPolicy().getTimeToLive()
+                : ticket.getExpirationPolicy().getTimeToIdle();
+
+        logger.debug("Adding ticket {} to cache store to live {} seconds and stay idle for {} seconds",
+                ticket.getId(), ticket.getExpirationPolicy().getTimeToLive(), idleTime);
+
+        this.cache.put(ticket.getId(), ticket,
                 ticket.getExpirationPolicy().getTimeToLive(), TimeUnit.SECONDS,
-                ticket.getExpirationPolicy().getTimeToIdle(), TimeUnit.SECONDS);
+                idleTime, TimeUnit.SECONDS);
     }
-    
+
     @Override
     public Ticket getTicket(final String ticketId) {
         final String encTicketId = encodeTicketId(ticketId);
@@ -63,14 +72,13 @@ public class InfinispanTicketRegistry extends AbstractTicketRegistry {
     }
 
     /**
-     *
      * Retrieve all tickets from the registry.
-     *
+     * <p>
      * Note! Usage of this method can be computational and I/O intensive and should not be used for other than
      * debugging.
      *
      * @return collection of tickets currently stored in the registry. Tickets
-     *         might or might not be valid i.e. expired.
+     * might or might not be valid i.e. expired.
      */
     @Override
     public Collection<Ticket> getTickets() {
