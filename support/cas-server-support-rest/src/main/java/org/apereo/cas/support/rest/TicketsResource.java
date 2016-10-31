76427a9d709c7edabd62c589dc32854dd761ce23
David Rodriguez <david.rodriguez@williamhill.com>
2016-10-31 09:08:51 +0100

    Do all the concatenations we can on start up. Add an initial capacity to the StringBuilder

--- support/cas-server-support-rest/src/main/java/org/apereo/cas/support/rest/TicketsResource.java
+++ support/cas-server-support-rest/src/main/java/org/apereo/cas/support/rest/TicketsResource.java
@@ -47,9 +47,12 @@ import java.util.stream.Collectors;
 public class TicketsResource {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TicketsResource.class);
+    private static final String CLOSE_TITLE_AND_OPEN_FORM = "</title></head><body><h1>TGT Created</h1><form action=\"";
     private static final String DOCTYPE_AND_TITLE = "<!DOCTYPE HTML PUBLIC \\\"-//IETF//DTD HTML 2.0//EN\\\"><html><head><title>";
-    private static final String CLOSE_TITLE_AND_FORM = "</title></head><body><h1>TGT Created</h1><form action=\"";
+    private static final String TGT_CREATED_TITLE_CONTENT = HttpStatus.CREATED.toString() + ' ' + HttpStatus.CREATED.getReasonPhrase();
+    private static final String DOCTYPE_AND_OPENING_FORM = DOCTYPE_AND_TITLE + TGT_CREATED_TITLE_CONTENT + CLOSE_TITLE_AND_OPEN_FORM;
     private static final String REST_OF_THE_FORM_AND_CLOSING_TAGS = "\" method=\"POST\">Service:<input type=\"text\" name=\"service\" value=\"\"><br><input type=\"submit\" value=\"Submit\"></form></body></html>";
+    private static final int SUCCESSFUL_TGT_CREATED_INITIAL_LENGTH = DOCTYPE_AND_OPENING_FORM.length() + REST_OF_THE_FORM_AND_CLOSING_TAGS.length();
 
     private CentralAuthenticationService centralAuthenticationService;
 
@@ -96,9 +99,10 @@ public class TicketsResource {
             final HttpHeaders headers = new HttpHeaders();
             headers.setLocation(ticketReference);
             headers.setContentType(MediaType.TEXT_HTML);
-            String response = new StringBuilder(DOCTYPE_AND_TITLE)
-                    .append(HttpStatus.CREATED).append(' ').append(HttpStatus.CREATED.getReasonPhrase())
-                    .append(CLOSE_TITLE_AND_FORM).append(ticketReference.toString())
+            String tgtUrl = ticketReference.toString();
+            String response = new StringBuilder(SUCCESSFUL_TGT_CREATED_INITIAL_LENGTH + tgtUrl.length())
+                    .append(DOCTYPE_AND_OPENING_FORM)
+                    .append(tgtUrl)
                     .append(REST_OF_THE_FORM_AND_CLOSING_TAGS)
                     .toString();
             return new ResponseEntity<>(response, headers, HttpStatus.CREATED);
