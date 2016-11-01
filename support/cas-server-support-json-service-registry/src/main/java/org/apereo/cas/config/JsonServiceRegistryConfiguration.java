9af81b91f87d9d1d7f7e3d19dd5fe7db621c23e2
Misagh Moayyed <mm1844@gmail.com>
2016-11-01 12:27:20 -0700

    added warnings for classpath-based service registries

--- support/cas-server-support-json-service-registry/src/main/java/org/apereo/cas/config/JsonServiceRegistryConfiguration.java
+++ support/cas-server-support-json-service-registry/src/main/java/org/apereo/cas/config/JsonServiceRegistryConfiguration.java
@@ -2,13 +2,17 @@ package org.apereo.cas.config;
 
 import com.google.common.base.Throwables;
 import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.configuration.model.core.services.ServiceRegistryProperties;
 import org.apereo.cas.services.JsonServiceRegistryDao;
 import org.apereo.cas.services.ServiceRegistryDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 /**
  * This is {@link JsonServiceRegistryConfiguration}.
@@ -20,6 +24,8 @@ import org.springframework.context.annotation.Configuration;
 @EnableConfigurationProperties(CasConfigurationProperties.class)
 public class JsonServiceRegistryConfiguration {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(JsonServiceRegistryConfiguration.class);
+
     @Autowired
     private ApplicationEventPublisher eventPublisher;
 
@@ -28,13 +34,21 @@ public class JsonServiceRegistryConfiguration {
 
     @Bean(name = {"jsonServiceRegistryDao", "serviceRegistryDao"})
     public ServiceRegistryDao jsonServiceRegistryDao() {
+        final ServiceRegistryProperties registry = casProperties.getServiceRegistry();
+        if (registry.getConfig().getLocation() == null) {
+            LOGGER.warn("The location of service definitions is undefined for the service registry");
+            throw new IllegalArgumentException("Service configuration directory for registry must be defined");
+        }
+
+
         try {
-            final JsonServiceRegistryDao dao =
-                    new JsonServiceRegistryDao(
-                            casProperties.getServiceRegistry().getConfig().getLocation(),
-                            casProperties.getServiceRegistry().isWatcherEnabled(),
-                            eventPublisher);
-            return dao;
+            if (registry.getConfig().getLocation() instanceof ClassPathResource) {
+                LOGGER.warn("The location of service definitions {} is on the classpath. It is recommended that the location of service definitions "
+                        + "be externalized to allow for easily making changes, removing the need for re-deployments and better "
+                        + "sharing of the configuration.", registry.getConfig().getLocation());
+            }
+
+            return new JsonServiceRegistryDao(registry.getConfig().getLocation(), registry.isWatcherEnabled(), eventPublisher);
         } catch (final Throwable e) {
             throw Throwables.propagate(e);
         }
