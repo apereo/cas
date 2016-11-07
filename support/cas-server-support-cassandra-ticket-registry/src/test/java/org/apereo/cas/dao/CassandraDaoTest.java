48b9f7dbeda52dcb45fa5ccf84734dad4ef65202
David Rodriguez <david.rodriguez@williamhill.com>
2016-11-07 16:17:58 +0100

    WIP cassandra-unit running (I think) but test not passing

--- support/cas-server-support-cassandra-ticket-registry/src/test/java/org/apereo/cas/dao/CassandraDaoTest.java
+++ support/cas-server-support-cassandra-ticket-registry/src/test/java/org/apereo/cas/dao/CassandraDaoTest.java
@@ -4,6 +4,7 @@ import org.apereo.cas.authentication.Authentication;
 import org.apereo.cas.authentication.DefaultAuthentication;
 import org.apereo.cas.authentication.HandlerResult;
 import org.apereo.cas.authentication.principal.NullPrincipal;
+import org.apereo.cas.serializer.JacksonJSONSerializer;
 import org.apereo.cas.ticket.TicketGrantingTicketImpl;
 import org.apereo.cas.ticket.support.TimeoutExpirationPolicy;
 import org.cassandraunit.CassandraCQLUnit;
@@ -21,12 +22,11 @@ import static org.junit.Assert.assertEquals;
 public class CassandraDaoTest {
 
     @Rule
-    public CassandraCQLUnit cassandraUnit = new CassandraCQLUnit(new ClassPathCQLDataSet("schema.cql"));
+    public CassandraCQLUnit cassandraUnit = new CassandraCQLUnit(new ClassPathCQLDataSet("schema.cql"), "cassandra.yaml", 120_000L);
 
     @Test
     public void shouldHaveLoadAnExtendDataSet() throws Exception {
-
-        CassandraDao dao = new CassandraDao("localhost", 24, "", "", 100, null);
+        CassandraDao dao = new CassandraDao("localhost", 24, "", "", 100, new JacksonJSONSerializer());
 
         Map<String, HandlerResult> successes = new HashMap<>();
         successes.put("something", null);
@@ -34,6 +34,7 @@ public class CassandraDaoTest {
         TicketGrantingTicketImpl tgt = new TicketGrantingTicketImpl("id", defaultAuthentication, new TimeoutExpirationPolicy(3000));
 
         dao.addTicketGrantingTicket(tgt);
+        cassandraUnit.session.execute("select * from cas.ticketgrantingticket;").all().forEach(row -> System.out.println("ID = " + row.getString("id")));
 
         assertEquals(tgt, dao.getTicketGrantingTicket("id"));
     }
