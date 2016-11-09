f6be8f3c4b457fae498ec078e3bf570926a02858
Misagh Moayyed <mmoayyed@unicon.net>
2016-11-08 17:04:08 -0700

    Fixed javadoc issues

--- support/cas-server-support-mongo-service-registry/src/main/java/org/apereo/cas/services/MongoServiceRegistryDao.java
+++ support/cas-server-support-mongo-service-registry/src/main/java/org/apereo/cas/services/MongoServiceRegistryDao.java
@@ -15,9 +15,8 @@ import java.util.regex.Pattern;
  * <p>Implementation of {@code ServiceRegistryDao} that uses a MongoDb repository as the backend
  * persistence mechanism. The repository is configured by the Spring application context. </p>
  * <p>The class will automatically create a default collection to use with services. The name
- * of the collection may be specified through {@link #setCollectionName(String)}.
- * It also presents the ability to drop an existing collection and start afresh
- * through the use of {@link #setDropCollection(boolean)}.</p>
+ * of the collection may be specified.
+ * It also presents the ability to drop an existing collection and start afresh.
  *
  * @author Misagh Moayyed
  * @since 4.1
