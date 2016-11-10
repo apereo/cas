137bde602443d471a6a4f9e2ed27dfe623630405
Misagh Moayyed <mm1844@gmail.com>
2016-11-09 18:48:52 -0700

    Cleaned up support for durations in time units

--- core/cas-server-core-util/src/main/java/org/apereo/cas/config/CasCoreUtilConfiguration.java
+++ core/cas-server-core-util/src/main/java/org/apereo/cas/config/CasCoreUtilConfiguration.java
@@ -1,9 +1,18 @@
 package org.apereo.cas.config;
 
 import org.apereo.cas.util.ApplicationContextProvider;
-import org.springframework.context.ApplicationContextAware;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.config.EmbeddedValueResolver;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.ConversionFailedException;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.scheduling.annotation.ScheduledAnnotationBeanPostProcessor;
+
+import javax.annotation.PostConstruct;
+import java.time.Duration;
 
 /**
  * This is {@link CasCoreUtilConfiguration}.
@@ -13,10 +22,34 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration("casCoreUtilConfiguration")
 public class CasCoreUtilConfiguration {
-    
+    private static final Logger LOGGER = LoggerFactory.getLogger(CasCoreUtilConfiguration.class);
+
     @Bean
-    public ApplicationContextAware applicationContextProvider() {
+    public ApplicationContextProvider applicationContextProvider() {
         return new ApplicationContextProvider();
     }
-    
+
+    @PostConstruct
+    public void init() {
+        final ConfigurableApplicationContext applicationContext = applicationContextProvider().getConfigurableApplicationContext();
+        final ScheduledAnnotationBeanPostProcessor p = applicationContext.getBean(ScheduledAnnotationBeanPostProcessor.class);
+        p.setEmbeddedValueResolver(new EmbeddedValueResolver(applicationContext.getBeanFactory()) {
+            @Override
+            public String resolveStringValue(final String strVal) {
+                final String value = super.resolveStringValue(strVal);
+                try {
+                    final ConversionService service = applicationContext.getEnvironment().getConversionService();
+                    final Duration dur = service.convert(value, Duration.class);
+                    if (dur != null) {
+                        return String.valueOf(dur.toMillis());
+                    }
+                    return value;
+                } catch (final ConversionFailedException e) {
+                    LOGGER.trace(e.getMessage());
+                    return value;
+                }
+            }
+        });
+    }
+
 }
