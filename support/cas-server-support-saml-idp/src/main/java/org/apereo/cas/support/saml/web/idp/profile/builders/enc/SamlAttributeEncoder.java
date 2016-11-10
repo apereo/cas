43d10707f2c6f335dc911a95292a4ebe54f0a27e
Misagh Moayyed <mmoayyed@unicon.net>
2016-11-09 21:22:27 -0700

    Code cleanup; removed pair class and replaced it with commons

--- support/cas-server-support-saml-idp/src/main/java/org/apereo/cas/support/saml/web/idp/profile/builders/enc/SamlAttributeEncoder.java
+++ support/cas-server-support-saml-idp/src/main/java/org/apereo/cas/support/saml/web/idp/profile/builders/enc/SamlAttributeEncoder.java
@@ -1,6 +1,7 @@
 package org.apereo.cas.support.saml.web.idp.profile.builders.enc;
 
 import com.google.common.collect.Maps;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apereo.cas.support.saml.services.SamlRegisteredService;
 import org.apereo.cas.support.saml.services.idp.metadata.SamlRegisteredServiceServiceProviderMetadataFacade;
 import org.opensaml.saml.saml2.core.AuthnRequest;
@@ -19,7 +20,7 @@ import java.util.stream.Collectors;
  */
 public class SamlAttributeEncoder {
     private static final Logger LOGGER = LoggerFactory.getLogger(SamlAttributeEncoder.class);
-    
+
     /**
      * Encode and transform attributes.
      *
@@ -29,27 +30,27 @@ public class SamlAttributeEncoder {
      * @param adaptor      the service provider facade
      * @return the map
      */
-    public Map<String, Object> encode(final AuthnRequest authnRequest, final Map<String, Object> attributes, 
-                                      final SamlRegisteredService service, 
+    public Map<String, Object> encode(final AuthnRequest authnRequest, final Map<String, Object> attributes,
+                                      final SamlRegisteredService service,
                                       final SamlRegisteredServiceServiceProviderMetadataFacade adaptor) {
         final Map<String, Object> finalAttributes = Maps.newHashMap(attributes);
-        
+
         transformUniformResourceNames(finalAttributes);
-        
+
         return finalAttributes;
     }
-    
+
     private static void transformUniformResourceNames(final Map<String, Object> attributes) {
         final Set<Pair<String, Object>> attrs = attributes.keySet().stream()
                 .filter(s -> s.toLowerCase().startsWith("urn_"))
-                .map(s -> new Pair<>(s.replace('_', ':'), attributes.get(s)))
+                .map(s -> Pair.of(s.replace('_', ':'), attributes.get(s)))
                 .collect(Collectors.toSet());
         if (!attrs.isEmpty()) {
             LOGGER.debug("Found {} URN attribute(s) that will be transformed.");
             attributes.entrySet().removeIf(s -> s.getKey().startsWith("urn_"));
             attrs.forEach(p -> {
-                LOGGER.debug("Transformed attribute name to be {}", p.getFirst());
-                attributes.put(p.getFirst(), p.getSecond());
+                LOGGER.debug("Transformed attribute name to be {}", p.getKey());
+                attributes.put(p.getKey(), p.getValue());
             });
         }
     }
