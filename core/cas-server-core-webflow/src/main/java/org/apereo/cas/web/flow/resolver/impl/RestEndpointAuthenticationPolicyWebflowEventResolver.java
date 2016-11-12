7d334c1a323a7147f63f127ec810fad15299701d
Misagh Moayyed <mm1844@gmail.com>
2016-11-11 19:07:52 -0700

    Refactor duo config for authN

--- core/cas-server-core-webflow/src/main/java/org/apereo/cas/web/flow/resolver/impl/RestEndpointAuthenticationPolicyWebflowEventResolver.java
+++ core/cas-server-core-webflow/src/main/java/org/apereo/cas/web/flow/resolver/impl/RestEndpointAuthenticationPolicyWebflowEventResolver.java
@@ -8,7 +8,6 @@ import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.services.MultifactorAuthenticationProvider;
 import org.apereo.cas.services.RegisteredService;
 import org.apereo.cas.web.flow.authentication.BaseMultifactorAuthenticationWebflowEventResolver;
-import org.apereo.cas.web.flow.authn.MultifactorAuthenticationWebflowEventResolver;
 import org.apereo.cas.web.support.WebUtils;
 import org.apereo.inspektr.audit.annotation.Audit;
 import org.springframework.beans.factory.annotation.Autowired;
