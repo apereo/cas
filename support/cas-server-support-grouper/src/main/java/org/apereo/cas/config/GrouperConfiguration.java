f0c6a4475f401aee850ca0e6793cfc9c02ffc7df
Misagh Moayyed <mm1844@gmail.com>
2016-11-12 12:07:29 -0700

    Refactor duo config for authN

--- support/cas-server-support-grouper/src/main/java/org/apereo/cas/config/GrouperConfiguration.java
+++ support/cas-server-support-grouper/src/main/java/org/apereo/cas/config/GrouperConfiguration.java
@@ -7,7 +7,7 @@ import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.services.MultifactorAuthenticationProviderSelector;
 import org.apereo.cas.services.ServicesManager;
 import org.apereo.cas.ticket.registry.TicketRegistrySupport;
-import org.apereo.cas.web.flow.GrouperMultifactorAuthenticationWebflowEventResolver;
+import org.apereo.cas.web.flow.GrouperMultifactorAuthenticationProviderResolver;
 import org.apereo.cas.web.flow.resolver.CasDelegatingWebflowEventResolver;
 import org.apereo.cas.web.flow.resolver.CasWebflowEventResolver;
 import org.apereo.cas.web.flow.resolver.impl.AbstractCasWebflowEventResolver;
@@ -73,7 +73,7 @@ public class GrouperConfiguration {
 
         final AbstractCasWebflowEventResolver r;
         if (StringUtils.isNotBlank(casProperties.getAuthn().getMfa().getGrouperGroupField())) {
-            r = new GrouperMultifactorAuthenticationWebflowEventResolver();
+            r = new GrouperMultifactorAuthenticationProviderResolver();
             LOGGER.debug("Activating MFA event resolver based on Grouper groups...");
         } else {
             r = new NoOpCasWebflowEventResolver();
