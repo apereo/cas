f4b055ffbf770e22659306959105bf7900b2f837
Misagh Moayyed <mmoayyed@unicon.net>
2016-11-14 16:43:07 -0700

    disabled embedded container when using external tomcat

--- webapp/cas-server-webapp-init/src/main/java/org/apereo/cas/web/CasWebApplicationServletInitializer.java
+++ webapp/cas-server-webapp-init/src/main/java/org/apereo/cas/web/CasWebApplicationServletInitializer.java
@@ -1,5 +1,7 @@
 package org.apereo.cas.web;
 
+import com.google.common.collect.ImmutableMap;
+import org.apereo.cas.config.CasEmbeddedContainerConfiguration;
 import org.apereo.cas.util.CasBanner;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
@@ -11,9 +13,12 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
  * @since 5.0.0
  */
 public class CasWebApplicationServletInitializer extends SpringBootServletInitializer {
-    
+
     @Override
     protected SpringApplicationBuilder configure(final SpringApplicationBuilder builder) {
-        return builder.sources(CasWebApplication.class).banner(new CasBanner());
+        return builder
+                .sources(CasWebApplication.class)
+                .properties(ImmutableMap.of(CasEmbeddedContainerConfiguration.EMBEDDED_CONTAINER_CONFIG_ACTIVE, Boolean.FALSE))
+                .banner(new CasBanner());
     }
 }
