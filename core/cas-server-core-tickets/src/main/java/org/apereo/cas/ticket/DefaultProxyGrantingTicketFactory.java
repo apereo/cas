028bb91a0f9b7810be655c84cca3f9fc7fff107f
Misagh Moayyed <mmoayyed@unicon.net>
2016-11-19 14:14:51 -0700

    initial pass at jwt

--- core/cas-server-core-tickets/src/main/java/org/apereo/cas/ticket/DefaultProxyGrantingTicketFactory.java
+++ core/cas-server-core-tickets/src/main/java/org/apereo/cas/ticket/DefaultProxyGrantingTicketFactory.java
@@ -1,8 +1,11 @@
 package org.apereo.cas.ticket;
 
+import org.apereo.cas.CipherExecutor;
 import org.apereo.cas.authentication.Authentication;
 import org.apereo.cas.ticket.proxy.ProxyGrantingTicket;
 import org.apereo.cas.ticket.proxy.ProxyGrantingTicketFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * The {@link DefaultTicketGrantingTicketFactory} is responsible
@@ -12,6 +15,7 @@ import org.apereo.cas.ticket.proxy.ProxyGrantingTicketFactory;
  * @since 4.2
  */
 public class DefaultProxyGrantingTicketFactory implements ProxyGrantingTicketFactory {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DefaultProxyGrantingTicketFactory.class);
 
     /**
      * Used to generate ids for {@link TicketGrantingTicket}s
@@ -19,15 +23,22 @@ public class DefaultProxyGrantingTicketFactory implements ProxyGrantingTicketFac
      */
     protected UniqueTicketIdGenerator ticketGrantingTicketUniqueTicketIdGenerator;
 
-    /** Expiration policy for ticket granting tickets. */
+    /**
+     * Expiration policy for ticket granting tickets.
+     */
     protected ExpirationPolicy ticketGrantingTicketExpirationPolicy;
+
+    private CipherExecutor<String, String> cipherExecutor;
 
     @Override
     public <T extends ProxyGrantingTicket> T create(final ServiceTicket serviceTicket,
-                                                    final Authentication authentication)
-                                                    throws AbstractTicketException {
-        final String pgtId = this.ticketGrantingTicketUniqueTicketIdGenerator.getNewTicketId(
-                ProxyGrantingTicket.PROXY_GRANTING_TICKET_PREFIX);
+                                                    final Authentication authentication) throws AbstractTicketException {
+        String pgtId = this.ticketGrantingTicketUniqueTicketIdGenerator.getNewTicketId(ProxyGrantingTicket.PROXY_GRANTING_TICKET_PREFIX);
+        if (this.cipherExecutor != null) {
+            LOGGER.debug("Attempting to encode proxy-granting ticket {}", pgtId);
+            pgtId = this.cipherExecutor.encode(pgtId);
+            LOGGER.debug("Encoded proxy-granting ticket id {}", pgtId);
+        }
         final ProxyGrantingTicket proxyGrantingTicket = serviceTicket.grantProxyGrantingTicket(pgtId,
                 authentication, this.ticketGrantingTicketExpirationPolicy);
         return (T) proxyGrantingTicket;
@@ -44,5 +55,9 @@ public class DefaultProxyGrantingTicketFactory implements ProxyGrantingTicketFac
 
     public void setTicketGrantingTicketExpirationPolicy(final ExpirationPolicy ticketGrantingTicketExpirationPolicy) {
         this.ticketGrantingTicketExpirationPolicy = ticketGrantingTicketExpirationPolicy;
+    }
+
+    public void setCipherExecutor(final CipherExecutor<String, String> cipherExecutor) {
+        this.cipherExecutor = cipherExecutor;
     }
 }
