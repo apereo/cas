94bb41bdd94a58fd4f667227593e4ee1420be864
Misagh Moayyed <mm1844@gmail.com>
2016-11-19 14:14:51 -0700

    initial pass at jwt

--- core/cas-server-core-tickets/src/main/java/org/apereo/cas/ticket/DefaultTicketGrantingTicketFactory.java
+++ core/cas-server-core-tickets/src/main/java/org/apereo/cas/ticket/DefaultTicketGrantingTicketFactory.java
@@ -1,6 +1,9 @@
 package org.apereo.cas.ticket;
 
+import org.apereo.cas.CipherExecutor;
 import org.apereo.cas.authentication.Authentication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * The {@link DefaultTicketGrantingTicketFactory} is responsible
@@ -10,23 +13,32 @@ import org.apereo.cas.authentication.Authentication;
  * @since 4.2
  */
 public class DefaultTicketGrantingTicketFactory implements TicketGrantingTicketFactory {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DefaultTicketGrantingTicketFactory.class);
 
     /**
      * UniqueTicketIdGenerator to generate ids for {@link TicketGrantingTicket}s
      * created.
      */
     protected UniqueTicketIdGenerator ticketGrantingTicketUniqueTicketIdGenerator;
-
+    
     /**
      * Expiration policy for ticket granting tickets.
      */
     protected ExpirationPolicy ticketGrantingTicketExpirationPolicy;
 
+    private CipherExecutor<String, String> cipherExecutor;
+
     @Override
     public <T extends TicketGrantingTicket> T create(final Authentication authentication) {
+        String tgtId = this.ticketGrantingTicketUniqueTicketIdGenerator.getNewTicketId(TicketGrantingTicket.PREFIX);
+        if (this.cipherExecutor != null) {
+            LOGGER.debug("Attempting to encode ticket-granting ticket {}", tgtId);
+            tgtId = this.cipherExecutor.encode(tgtId);
+            LOGGER.debug("Encoded ticket-granting ticket id {}", tgtId);
+        }
+
         final TicketGrantingTicket ticketGrantingTicket = new TicketGrantingTicketImpl(
-                this.ticketGrantingTicketUniqueTicketIdGenerator.getNewTicketId(TicketGrantingTicket.PREFIX),
-                authentication, this.ticketGrantingTicketExpirationPolicy);
+                tgtId, authentication, this.ticketGrantingTicketExpirationPolicy);
         return (T) ticketGrantingTicket;
     }
 
@@ -41,5 +53,9 @@ public class DefaultTicketGrantingTicketFactory implements TicketGrantingTicketF
 
     public void setTicketGrantingTicketExpirationPolicy(final ExpirationPolicy ticketGrantingTicketExpirationPolicy) {
         this.ticketGrantingTicketExpirationPolicy = ticketGrantingTicketExpirationPolicy;
+    }
+
+    public void setCipherExecutor(final CipherExecutor<String, String> cipherExecutor) {
+        this.cipherExecutor = cipherExecutor;
     }
 }
