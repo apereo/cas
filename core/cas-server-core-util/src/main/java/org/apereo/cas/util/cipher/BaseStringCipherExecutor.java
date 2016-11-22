c93f5761d186fb4def7f2fad10159a57f013265c
Misagh Moayyed <mm1844@gmail.com>
2016-11-22 11:58:08 -0700

    Fix issue with pac4j logout

--- core/cas-server-core-util/src/main/java/org/apereo/cas/util/cipher/BaseStringCipherExecutor.java
+++ core/cas-server-core-util/src/main/java/org/apereo/cas/util/cipher/BaseStringCipherExecutor.java
@@ -141,6 +141,7 @@ public abstract class BaseStringCipherExecutor extends AbstractCipherExecutor<St
         try {
             final JsonWebEncryption jwe = new JsonWebEncryption();
             jwe.setPayload(value);
+            jwe.enableDefaultCompression();
             jwe.setAlgorithmHeaderValue(KeyManagementAlgorithmIdentifiers.DIRECT);
             jwe.setEncryptionMethodHeaderParameter(this.contentEncryptionAlgorithmIdentifier);
             jwe.setKey(this.secretKeyEncryptionKey);
