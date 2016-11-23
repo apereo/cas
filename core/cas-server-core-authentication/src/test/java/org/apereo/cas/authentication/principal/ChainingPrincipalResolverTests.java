08b54d17d126783a849dc22b7833b46eecc440a7
Misagh Moayyed <mm1844@gmail.com>
2016-11-22 17:10:35 -0700

    updated docs

--- core/cas-server-core-authentication/src/test/java/org/apereo/cas/authentication/principal/ChainingPrincipalResolverTests.java
+++ core/cas-server-core-authentication/src/test/java/org/apereo/cas/authentication/principal/ChainingPrincipalResolverTests.java
@@ -46,19 +46,14 @@ public class ChainingPrincipalResolverTests {
         when(resolver1.resolve(eq(credential), any(Principal.class))).thenReturn(principalFactory.createPrincipal("output"));
 
         final PrincipalResolver resolver2 = mock(PrincipalResolver.class);
-        when(resolver2.supports(any(Credential.class))).thenReturn(false);
-        when(resolver2.resolve(argThat(new ArgumentMatcher<Credential>() {
-            @Override
-            public boolean matches(final Object o) {
-                return "output".equals(((Credential) o).getId());
-            }
-        }), any(Principal.class)))
+        when(resolver2.supports(any(Credential.class))).thenReturn(true);
+        when(resolver2.resolve(any(Credential.class), any(Principal.class)))
                 .thenReturn(principalFactory.createPrincipal("final", Collections.<String, Object>singletonMap("mail", "final@example.com")));
 
         final ChainingPrincipalResolver resolver = new ChainingPrincipalResolver();
         resolver.setChain(Lists.newArrayList(resolver1, resolver2));
         final Principal principal = resolver.resolve(credential, any(Principal.class));
-        assertEquals("final", principal.getId());
+        assertEquals("output", principal.getId());
         assertEquals("final@example.com", principal.getAttributes().get("mail"));
     }
 
