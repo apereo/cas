044c84eda95beb08103ac403121dc49fe56d9ac9
Misagh Moayyed <mm1844@gmail.com>
2016-11-28 11:54:49 -0700

    Let Jackson objectmapper find and register modules

--- support/cas-server-support-duo/src/main/java/org/apereo/cas/adaptors/duo/authn/BaseDuoAuthenticationService.java
+++ support/cas-server-support-duo/src/main/java/org/apereo/cas/adaptors/duo/authn/BaseDuoAuthenticationService.java
@@ -60,6 +60,7 @@ public abstract class BaseDuoAuthenticationService implements DuoAuthenticationS
                 final String response = URLDecoder.decode(msg.getMessage(), StandardCharsets.UTF_8.name());
                 logger.debug("Received Duo ping response {}", response);
                 final ObjectMapper mapper = new ObjectMapper();
+                mapper.findAndRegisterModules();
                 final JsonNode result = mapper.readTree(response);
                 if (result.has(RESULT_KEY_RESPONSE) && result.has(RESULT_KEY_STAT)
                         && result.get(RESULT_KEY_RESPONSE).asText().equalsIgnoreCase("pong")
@@ -126,6 +127,7 @@ public abstract class BaseDuoAuthenticationService implements DuoAuthenticationS
                 final String jsonResponse = URLDecoder.decode(msg.getMessage(), StandardCharsets.UTF_8.name());
                 logger.debug("Received Duo admin response {}", jsonResponse);
                 final ObjectMapper mapper = new ObjectMapper();
+                mapper.findAndRegisterModules();
                 final JsonNode result = mapper.readTree(jsonResponse);
                 if (result.has(RESULT_KEY_RESPONSE) && result.has(RESULT_KEY_STAT)
                         && result.get(RESULT_KEY_STAT).asText().equalsIgnoreCase("OK")) {
@@ -164,6 +166,7 @@ public abstract class BaseDuoAuthenticationService implements DuoAuthenticationS
                 final String jsonResponse = URLDecoder.decode(msg.getMessage(), StandardCharsets.UTF_8.name());
                 logger.debug("Received Duo admin response {}", jsonResponse);
                 final ObjectMapper mapper = new ObjectMapper();
+                mapper.findAndRegisterModules();
                 final JsonNode result = mapper.readTree(jsonResponse);
                 if (result.has(RESULT_KEY_RESPONSE) && result.has(RESULT_KEY_STAT)
                         && result.get(RESULT_KEY_STAT).asText().equalsIgnoreCase("OK")) {
