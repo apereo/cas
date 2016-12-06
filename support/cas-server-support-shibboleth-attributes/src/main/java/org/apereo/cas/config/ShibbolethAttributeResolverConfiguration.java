ec622cd0a8b1272e66d843baf85af569758790e8
Misagh Moayyed <mm1844@gmail.com>
2016-12-05 22:12:08 -0700

    updated tests

--- support/cas-server-support-shibboleth-attributes/src/main/java/org/apereo/cas/config/ShibbolethAttributeResolverConfiguration.java
+++ support/cas-server-support-shibboleth-attributes/src/main/java/org/apereo/cas/config/ShibbolethAttributeResolverConfiguration.java
@@ -1,17 +1,20 @@
 package org.apereo.cas.config;
 
-import com.google.common.base.Throwables;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import net.shibboleth.ext.spring.util.SpringSupport;
 import net.shibboleth.idp.attribute.resolver.AttributeDefinition;
-import net.shibboleth.idp.attribute.resolver.AttributeResolver;
 import net.shibboleth.idp.attribute.resolver.DataConnector;
 import net.shibboleth.idp.attribute.resolver.impl.AttributeResolverImpl;
 import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.configuration.support.Beans;
 import org.apereo.cas.persondir.support.ShibbolethPersonAttributeDao;
 import org.apereo.services.persondir.IPersonAttributeDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.BeanFactoryUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
@@ -21,6 +24,7 @@ import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.EnumerablePropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -36,6 +40,8 @@ import java.util.Properties;
 @EnableConfigurationProperties(CasConfigurationProperties.class)
 @EnableScheduling
 public class ShibbolethAttributeResolverConfiguration {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ShibbolethAttributeResolverConfiguration.class);
 
     @Autowired
     private ApplicationContext applicationContext;
@@ -60,6 +66,8 @@ public class ShibbolethAttributeResolverConfiguration {
             final Properties p = new Properties();
             p.putAll(result);
             cfg.setProperties(p);
+
+            registerBeanIntoApplicationContext(cfg, "shibboleth.PropertySourcesPlaceholderConfigurer");
             final ApplicationContext tempApplicationContext = SpringSupport.newContext(
                     getClass().getName(),
                     casProperties.getShibAttributeResolver().getResources(),
@@ -69,16 +77,32 @@ public class ShibbolethAttributeResolverConfiguration {
                     this.applicationContext
             );
 
+            final Collection<DataConnector> connectors =
+                    Sets.newHashSet(BeanFactoryUtils.beansOfTypeIncludingAncestors(tempApplicationContext, DataConnector.class).values());
             final AttributeResolverImpl impl = new AttributeResolverImpl();
             impl.setId(getClass().getSimpleName());
+            impl.setApplicationContext(tempApplicationContext);
             impl.setAttributeDefinitions(BeanFactoryUtils.beansOfTypeIncludingAncestors(tempApplicationContext, AttributeDefinition.class).values());
-            impl.setDataConnectors(BeanFactoryUtils.beansOfTypeIncludingAncestors(tempApplicationContext, DataConnector.class).values());
+            impl.setDataConnectors(connectors);
             if (!impl.isInitialized()) {
                 impl.initialize();
             }
             return new ShibbolethPersonAttributeDao(impl);
         } catch (final Exception e) {
-            throw Throwables.propagate(e);
+            LOGGER.warn(e.getMessage(), e);
         }
+        return Beans.newStubAttributeRepository(casProperties.getAuthn().getAttributeRepository());
+    }
+
+    /**
+     * Register bean into application context.
+     *
+     * @param beanInstance the bean instance
+     * @param name         the name
+     */
+    protected void registerBeanIntoApplicationContext(final Object beanInstance, final String name) {
+        final Object initBean = this.applicationContext.getAutowireCapableBeanFactory().initializeBean(beanInstance, name);
+        final ConfigurableListableBeanFactory factory = (ConfigurableListableBeanFactory) applicationContext.getAutowireCapableBeanFactory();
+        factory.registerSingleton(name, initBean);
     }
 }
