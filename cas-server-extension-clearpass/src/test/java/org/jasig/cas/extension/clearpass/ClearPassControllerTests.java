a5ea8b9545781c922a7e924172e238fb5494f68c
Misagh Moayyed <mmoayyed@unicon.net>
2016-12-05 22:08:52 -0700

    updated tests

--- cas-server-extension-clearpass/src/test/java/org/jasig/cas/extension/clearpass/ClearPassControllerTests.java
+++ cas-server-extension-clearpass/src/test/java/org/jasig/cas/extension/clearpass/ClearPassControllerTests.java
@@ -39,7 +39,7 @@ public class ClearPassControllerTests {
         this.manager.shutdown();
     }
 
-    @Test
+    @Test                                                                                 w
     public void verifyClearPassWithNoUsername() throws Exception {
         final ClearPassController controller = new ClearPassController(this.map);
         controller.setTicketGrantingTicketCookieGenerator(new TGCCookieRetrievingCookieGenerator(new NoOpCookieValueManager()));
