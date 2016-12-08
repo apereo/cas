603fade78c34f6bfd45158fc7337a0a23a7fb317
Misagh Moayyed <mmoayyed@unicon.net>
2016-12-08 10:21:22 -0700

    duo refactoring for bypass based on duo authN api

--- support/cas-server-support-oidc/src/main/java/org/apereo/cas/web/controllers/OidcDynamicClientRegistrationEndpointController.java
+++ support/cas-server-support-oidc/src/main/java/org/apereo/cas/web/controllers/OidcDynamicClientRegistrationEndpointController.java
@@ -54,10 +54,11 @@ public class OidcDynamicClientRegistrationEndpointController extends BaseOAuthWr
      * @return the model and view
      * @throws Exception the exception
      */
-    @PostMapping(value = '/' + OidcConstants.BASE_OIDC_URL + '/' + OidcConstants.REGISTRATION_URL, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = '/' + OidcConstants.BASE_OIDC_URL + '/' + OidcConstants.REGISTRATION_URL,
+            consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<OidcClientRegistrationResponse> handleRequestInternal(@RequestBody final String jsonInput,
-                                                                            final HttpServletRequest request,
-                                                                            final HttpServletResponse response) throws Exception {
+                                                                                final HttpServletRequest request,
+                                                                                final HttpServletResponse response) throws Exception {
         try {
             final OidcClientRegistrationRequest registrationRequest = this.clientRegistrationRequestSerializer.from(jsonInput);
             logger.debug("Received client registration request {}", registrationRequest);
