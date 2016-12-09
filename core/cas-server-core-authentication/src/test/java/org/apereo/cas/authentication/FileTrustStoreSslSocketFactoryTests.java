866d7243ed6cea4d219945e8a5f0fe29c725088b
David Rodriguez <david.rodriguez@williamhill.com>
2016-12-09 12:26:09 +0100

    Reworking exceptions in FileTrustStoreSslSocketFactoryTests.java

--- core/cas-server-core-authentication/src/test/java/org/apereo/cas/authentication/FileTrustStoreSslSocketFactoryTests.java
+++ core/cas-server-core-authentication/src/test/java/org/apereo/cas/authentication/FileTrustStoreSslSocketFactoryTests.java
@@ -3,7 +3,9 @@ package org.apereo.cas.authentication;
 import org.apereo.cas.util.http.HttpClient;
 import org.apereo.cas.util.http.SimpleHttpClientFactoryBean;
 import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 
@@ -17,12 +19,15 @@ import static org.junit.Assert.*;
  */
 public class FileTrustStoreSslSocketFactoryTests {
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    private static final ClassPathResource RESOURCE = new ClassPathResource("truststore.jks");
+
     @Test
     public void verifyTrustStoreLoadingSuccessfullyWithCertAvailable() throws Exception {
-        final ClassPathResource resource = new ClassPathResource("truststore.jks");
-        final FileTrustStoreSslSocketFactory factory = new FileTrustStoreSslSocketFactory(resource, "changeit");
         final SimpleHttpClientFactoryBean clientFactory = new SimpleHttpClientFactoryBean();
-        clientFactory.setSslSocketFactory(factory);
+        clientFactory.setSslSocketFactory(sslFactory());
         final HttpClient client = clientFactory.getObject();
         assertTrue(client.isValidEndPoint("https://self-signed.badssl.com"));
     }
@@ -30,42 +35,44 @@ public class FileTrustStoreSslSocketFactoryTests {
     @Ignore
     @Test
     public void verifyTrustStoreLoadingSuccessfullyWithCertAvailable2() throws Exception {
-        final ClassPathResource resource = new ClassPathResource("truststore.jks");
-        final FileTrustStoreSslSocketFactory factory = new FileTrustStoreSslSocketFactory(resource, "changeit");
         final SimpleHttpClientFactoryBean clientFactory = new SimpleHttpClientFactoryBean();
-        clientFactory.setSslSocketFactory(factory);
+        clientFactory.setSslSocketFactory(sslFactory());
         final HttpClient client = clientFactory.getObject();
         assertTrue(client.isValidEndPoint("https://test.scaldingspoon.org/idp/shibboleth"));
     }
 
-
-    @Test(expected = RuntimeException.class)
+    @Test
      public void verifyTrustStoreNotFound() throws Exception {
+        this.thrown.expect(RuntimeException.class);
+        this.thrown.expectMessage("java.io.FileNotFoundException: test.jks (No such file or directory)");
+
         new FileTrustStoreSslSocketFactory(new FileSystemResource("test.jks"), "changeit");
     }
 
-    @Test(expected = RuntimeException.class)
+    @Test
     public void verifyTrustStoreBadPassword() throws Exception {
-        final ClassPathResource resource = new ClassPathResource("truststore.jks");
-        new FileTrustStoreSslSocketFactory(resource, "invalid");
+        this.thrown.expect(RuntimeException.class);
+        this.thrown.expectMessage("java.io.IOException: Keystore was tampered with, or password was incorrect");
+
+        new FileTrustStoreSslSocketFactory(RESOURCE, "invalid");
     }
 
     @Test
     public void verifyTrustStoreLoadingSuccessfullyForValidEndpointWithNoCert() throws Exception {
-        final ClassPathResource resource = new ClassPathResource("truststore.jks");
-        final FileTrustStoreSslSocketFactory factory = new FileTrustStoreSslSocketFactory(resource, "changeit");
         final SimpleHttpClientFactoryBean clientFactory = new SimpleHttpClientFactoryBean();
-        clientFactory.setSslSocketFactory(factory);
+        clientFactory.setSslSocketFactory(sslFactory());
         final HttpClient client = clientFactory.getObject();
         assertTrue(client.isValidEndPoint("https://www.google.com"));
     }
     @Test
     public void verifyTrustStoreLoadingSuccessfullyWihInsecureEndpoint() throws Exception {
-        final ClassPathResource resource = new ClassPathResource("truststore.jks");
-        final FileTrustStoreSslSocketFactory factory = new FileTrustStoreSslSocketFactory(resource, "changeit");
         final SimpleHttpClientFactoryBean clientFactory = new SimpleHttpClientFactoryBean();
-        clientFactory.setSslSocketFactory(factory);
+        clientFactory.setSslSocketFactory(sslFactory());
         final HttpClient client = clientFactory.getObject();
         assertTrue(client.isValidEndPoint("http://wikipedia.org"));
+    }
+
+    private static FileTrustStoreSslSocketFactory sslFactory() {
+        return new FileTrustStoreSslSocketFactory(RESOURCE, "changeit");
     }
 }
