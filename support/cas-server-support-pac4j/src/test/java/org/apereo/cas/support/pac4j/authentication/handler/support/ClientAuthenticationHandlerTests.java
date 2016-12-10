648e3adcfb7f65559b48e51943368f1d0ff49f35
David Rodriguez <davidrg131092@gmail.com>
2016-12-10 07:53:09 +0100

    Make checkstyle happy

--- support/cas-server-support-pac4j/src/test/java/org/apereo/cas/support/pac4j/authentication/handler/support/ClientAuthenticationHandlerTests.java
+++ support/cas-server-support-pac4j/src/test/java/org/apereo/cas/support/pac4j/authentication/handler/support/ClientAuthenticationHandlerTests.java
@@ -36,11 +36,11 @@ import static org.mockito.Mockito.*;
 @SpringBootTest(classes = {RefreshAutoConfiguration.class})
 public class ClientAuthenticationHandlerTests {
 
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-
     private static final String CALLBACK_URL = "http://localhost:8080/callback";
     private static final String ID = "123456789";
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     private MockFacebookClient fbClient;
     private ClientAuthenticationHandler handler;
