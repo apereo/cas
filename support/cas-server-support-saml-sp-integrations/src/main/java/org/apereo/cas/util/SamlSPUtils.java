c9f2d93d0d057cc6ada13d37ca5b1e63ccb9ff4d
Misagh Moayyed <mm1844@gmail.com>
2016-12-12 09:59:19 -0700

    updated

--- support/cas-server-support-saml-sp-integrations/src/main/java/org/apereo/cas/util/SamlSPUtils.java
+++ support/cas-server-support-saml-sp-integrations/src/main/java/org/apereo/cas/util/SamlSPUtils.java
@@ -18,7 +18,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
--- support/cas-server-support-x509/src/main/java/org/apereo/cas/adaptors/x509/util/X509CertificateCredentialJsonDeserializer.java
+++ support/cas-server-support-x509/src/main/java/org/apereo/cas/adaptors/x509/util/X509CertificateCredentialJsonDeserializer.java
@@ -13,7 +13,6 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
