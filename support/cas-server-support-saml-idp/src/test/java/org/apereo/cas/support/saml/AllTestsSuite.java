6858126fce53a6a6b6cf4f492ddb98c7b231cb01
Misagh Moayyed <mm1844@gmail.com>
2016-12-12 17:06:52 -0700

    Initial pass at ECP endpoint

--- support/cas-server-support-saml-idp/src/test/java/org/apereo/cas/support/saml/AllTestsSuite.java
+++ support/cas-server-support-saml-idp/src/test/java/org/apereo/cas/support/saml/AllTestsSuite.java
@@ -5,12 +5,13 @@ import org.junit.runners.Suite;
 
 /**
  * Test suite to run all SAML tests.
+ *
  * @author Misagh Moayyed
  * @since 4.2.0
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-
+        SamlRegisteredServiceTests.class, SamlIdPConfigurationTests.class
 })
 public class AllTestsSuite {
 }
