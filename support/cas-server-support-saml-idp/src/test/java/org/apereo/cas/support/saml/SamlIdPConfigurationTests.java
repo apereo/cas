edeedb4b77fe388decd7516eda77db7a85ce9aff
Misagh Moayyed <mm1844@gmail.com>
2016-12-13 08:55:12 -0700

    added test cases for ECP

--- support/cas-server-support-saml-idp/src/test/java/org/apereo/cas/support/saml/SamlIdPConfigurationTests.java
+++ support/cas-server-support-saml-idp/src/test/java/org/apereo/cas/support/saml/SamlIdPConfigurationTests.java
@@ -1,7 +1,7 @@
 package org.apereo.cas.support.saml;
 
 import net.shibboleth.utilities.java.support.xml.BasicParserPool;
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apereo.cas.config.CasCoreAuthenticationConfiguration;
 import org.apereo.cas.config.CasCoreConfiguration;
 import org.apereo.cas.config.CasCoreServicesConfiguration;
@@ -17,14 +17,21 @@ import org.apereo.cas.web.config.CasCookieConfiguration;
 import org.apereo.cas.web.flow.config.CasCoreWebflowConfiguration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.opensaml.core.xml.util.XMLObjectSupport;
+import org.opensaml.saml.common.binding.BindingDescriptor;
+import org.opensaml.saml.common.binding.impl.SAMLSOAPDecoderBodyHandler;
+import org.opensaml.saml.saml2.binding.decoding.impl.HTTPSOAP11Decoder;
+import org.opensaml.soap.messaging.context.SOAP11Context;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
 
 /**
  * This is {@link SamlIdPConfigurationTests}.
@@ -57,8 +64,21 @@ public class SamlIdPConfigurationTests {
 
     @Test
     public void verifyEcp() throws Exception {
-        XMLObjectSupport.unmarshallFromInputStream(parserPool,
-                new ClassPathResource("sample-ecp-request.xml").getInputStream());
+        final MockHttpServletRequest mockRequest =
+                new MockHttpServletRequest("POST", "https://sso.example.org");
+        mockRequest.setContentType(MediaType.TEXT_XML_VALUE);
+        mockRequest.setContent(IOUtils.toByteArray(
+                new ClassPathResource("sample-ecp-request.xml").getInputStream()));
 
+        final HTTPSOAP11Decoder decoder = new HTTPSOAP11Decoder();
+        decoder.setParserPool(parserPool);
+        decoder.setHttpServletRequest(mockRequest);
+        decoder.setBindingDescriptor(new BindingDescriptor());
+        decoder.setBodyHandler(new SAMLSOAPDecoderBodyHandler());
+        decoder.initialize();
+        decoder.decode();
+        final SOAP11Context context = decoder.getMessageContext().getSubcontext(SOAP11Context.class);
+        assertNotNull(context);
+        assertNotNull(context.getEnvelope());
     }
 }
