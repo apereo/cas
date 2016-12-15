263a3a63c461defc884299ed058d703f69673ea7
Misagh Moayyed <mm1844@gmail.com>
2016-12-15 12:10:55 -0700

    Working on metadata download threading

--- core/cas-server-core-events/src/main/java/org/apereo/cas/support/events/listener/DefaultCasEventListener.java
+++ core/cas-server-core-events/src/main/java/org/apereo/cas/support/events/listener/DefaultCasEventListener.java
@@ -1,6 +1,7 @@
 package org.apereo.cas.support.events.listener;
 
 import org.apereo.cas.authentication.adaptive.geo.GeoLocationRequest;
+import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.support.events.CasRiskyAuthenticationDetectedEvent;
 import org.apereo.cas.support.events.CasTicketGrantingTicketCreatedEvent;
 import org.apereo.cas.support.events.dao.CasEvent;
@@ -10,7 +11,16 @@ import org.apereo.cas.util.serialization.TicketIdSanitizationUtils;
 import org.apereo.cas.web.support.WebUtils;
 import org.apereo.inspektr.common.web.ClientInfo;
 import org.apereo.inspektr.common.web.ClientInfoHolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBindingPostProcessor;
+import org.springframework.cloud.bus.event.RefreshRemoteApplicationEvent;
+import org.springframework.cloud.context.environment.EnvironmentChangeEvent;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.event.EventListener;
+
+import java.util.Map;
 
 /**
  * This is {@link DefaultCasEventListener} that attempts to consume CAS events
@@ -21,11 +31,55 @@ import org.springframework.context.event.EventListener;
  * @since 5.0.0
  */
 public class DefaultCasEventListener {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DefaultCasEventListener.class);
+
+    @Autowired
+    private ConfigurationPropertiesBindingPostProcessor binder;
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     private final CasEventRepository casEventRepository;
 
     public DefaultCasEventListener(final CasEventRepository casEventRepository) {
         this.casEventRepository = casEventRepository;
+    }
+
+    /**
+     * Handle refresh event when issued to this CAS server locally.
+     *
+     * @param event the event
+     */
+    @EventListener
+    public void handleRefreshEvent(final EnvironmentChangeEvent event) {
+        LOGGER.debug("Received event {}", event);
+        rebindCasConfigurationProperties();
+    }
+
+    /**
+     * Handle refresh event when issued by the cloud bus.
+     *
+     * @param event the event
+     */
+    @EventListener
+    public void handleRefreshEvent(final RefreshRemoteApplicationEvent event) {
+        LOGGER.debug("Received event {}", event);
+        rebindCasConfigurationProperties();
+    }
+
+    /**
+     * Rebind cas configuration properties.
+     */
+    public void rebindCasConfigurationProperties() {
+        final Map<String, CasConfigurationProperties> map =
+                this.applicationContext.getBeansOfType(CasConfigurationProperties.class);
+        final String name = map.keySet().iterator().next();
+        LOGGER.debug("Reloading CAS configuration via {}", name);
+        final Object e = this.applicationContext.getBean(name);
+        this.binder.postProcessBeforeInitialization(e, name);
+        final Object bean = this.applicationContext.getAutowireCapableBeanFactory().initializeBean(e, name);
+        this.applicationContext.getAutowireCapableBeanFactory().autowireBean(bean);
+        LOGGER.info("Reloaded CAS configuration {}", name);
     }
 
     /**
