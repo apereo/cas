03881bbdfa512366ad8a0a159a535d74610a1eb3
Misagh Moayyed <mm1844@gmail.com>
2016-12-19 17:27:56 -0700

    Removed unused imports

--- core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/saml/idp/SamlIdPProperties.java
+++ core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/saml/idp/SamlIdPProperties.java
@@ -6,7 +6,6 @@ import org.springframework.core.io.Resource;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 /**
--- support/cas-server-support-saml-idp/src/main/java/org/apereo/cas/support/saml/web/idp/profile/SSOPostProfileHandlerController.java
+++ support/cas-server-support-saml-idp/src/main/java/org/apereo/cas/support/saml/web/idp/profile/SSOPostProfileHandlerController.java
@@ -23,7 +23,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
 import java.util.Set;
 
 /**
