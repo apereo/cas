4cde82d63cab341626303aa2b8c4b018566be11c
Misagh Moayyed <mm1844@gmail.com>
2016-12-19 17:27:56 -0700

    Removed unused imports

--- support/cas-server-support-saml-idp/src/main/java/org/apereo/cas/support/saml/web/idp/profile/SLOPostProfileHandlerController.java
+++ support/cas-server-support-saml-idp/src/main/java/org/apereo/cas/support/saml/web/idp/profile/SLOPostProfileHandlerController.java
@@ -27,7 +27,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
 import java.util.Set;
 
 /**
