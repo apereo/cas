f94c78f10ae1176c8c77206ad63ac55a627d8afb
Misagh Moayyed <mm1844@gmail.com>
2017-01-03 13:25:02 +0330

    MongoDb ticket registry

--- core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/mongo/ticketregistry/MongoTicketRegistryProperties.java
+++ core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/mongo/ticketregistry/MongoTicketRegistryProperties.java
@@ -1,10 +1,12 @@
 package org.apereo.cas.configuration.model.support.mongo.ticketregistry;
 
+import org.apereo.cas.configuration.model.support.mongo.AbstractMongoInstanceProperties;
+
 /**
  * This is {@link MongoTicketRegistryProperties}.
  *
  * @author Misagh Moayyed
  * @since 5.1.0
  */
-public class MongoTicketRegistryProperties {
+public class MongoTicketRegistryProperties extends AbstractMongoInstanceProperties {
 }
