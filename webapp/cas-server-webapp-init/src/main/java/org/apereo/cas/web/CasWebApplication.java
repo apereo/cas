e96801cd2d45caf4f2f47523a737e8b70c211d4c
Misagh Moayyed <mm1844@gmail.com>
2017-01-04 21:44:54 +0330

    Clean up the logging config to not use @Value

--- webapp/cas-server-webapp-init/src/main/java/org/apereo/cas/web/CasWebApplication.java
+++ webapp/cas-server-webapp-init/src/main/java/org/apereo/cas/web/CasWebApplication.java
@@ -6,11 +6,15 @@ import org.apereo.cas.util.CasBanner;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.actuate.autoconfigure.MetricsDropwizardAutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.cassandra.CassandraAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.groovy.template.GroovyTemplateAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.jersey.JerseyAutoConfiguration;
 import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.autoconfigure.velocity.VelocityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -39,6 +43,10 @@ import java.util.Collections;
                 GroovyTemplateAutoConfiguration.class,
                 JmxAutoConfiguration.class,
                 DataSourceAutoConfiguration.class,
+                RedisAutoConfiguration.class,
+                MongoAutoConfiguration.class,
+                MongoDataAutoConfiguration.class,
+                CassandraAutoConfiguration.class,
                 DataSourceTransactionManagerAutoConfiguration.class,
                 MetricsDropwizardAutoConfiguration.class,
                 VelocityAutoConfiguration.class})
