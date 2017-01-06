ff6cf6b0ad86d68c7f198fe836b4fc54de333ef6
Misagh Moayyed <mmoayyed@unicon.net>
2017-01-07 01:16:57 +0330

    Added conditionals into principal factory instances; turns ldap/jdbc authn handlers into beans

--- support/cas-server-support-token/src/main/java/org/apereo/cas/token/config/TokenAuthenticationConfiguration.java
+++ support/cas-server-support-token/src/main/java/org/apereo/cas/token/config/TokenAuthenticationConfiguration.java
@@ -21,6 +21,7 @@ import org.apereo.cas.web.flow.resolver.CasDelegatingWebflowEventResolver;
 import org.apereo.cas.web.flow.resolver.CasWebflowEventResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -77,6 +78,7 @@ public class TokenAuthenticationConfiguration {
                 grantingTicketExpirationPolicy);
     }
 
+    @ConditionalOnMissingBean(name = "tokenPrincipalFactory")
     @Bean
     public PrincipalFactory tokenPrincipalFactory() {
         return new DefaultPrincipalFactory();
