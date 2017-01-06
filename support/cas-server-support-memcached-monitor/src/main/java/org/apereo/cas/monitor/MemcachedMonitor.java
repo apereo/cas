a3036a800638594ed31da28b4e2cba9d68f53a2f
David Rodriguez <davidrg131092@gmail.com>
2017-01-06 11:16:05 +0100

    Remove setters from memcached monitor

--- support/cas-server-support-memcached-monitor/src/main/java/org/apereo/cas/monitor/MemcachedMonitor.java
+++ support/cas-server-support-memcached-monitor/src/main/java/org/apereo/cas/monitor/MemcachedMonitor.java
@@ -16,13 +16,7 @@ import java.util.List;
  */
 public class MemcachedMonitor extends AbstractCacheMonitor {
 
-    private MemcachedClientIF memcachedClient;
-
-    /**
-     * Instantiates a new Memcached monitor.
-     */
-    public MemcachedMonitor() {
-    }
+    private final MemcachedClientIF memcachedClient;
 
     /**
      * Creates a new monitor that observes the given memcached client.
@@ -32,7 +26,6 @@ public class MemcachedMonitor extends AbstractCacheMonitor {
     public MemcachedMonitor(final MemcachedClientIF client) {
         this.memcachedClient = client;
     }
-
 
     /**
      * Supersede the default cache status algorithm by considering unavailable memcached nodes above cache statistics.
@@ -84,9 +77,5 @@ public class MemcachedMonitor extends AbstractCacheMonitor {
             });
         }
         return statsList.toArray(new CacheStatistics[statsList.size()]);
-    }
-
-    public void setMemcachedClient(final MemcachedClientIF memcachedClient) {
-        this.memcachedClient = memcachedClient;
     }
 }
