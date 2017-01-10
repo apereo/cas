ee1e1369affd10ceb1703ccf1c67abc4bf5cc935
Misagh Moayyed <mm1844@gmail.com>
2017-01-10 14:05:34 +0330

    Fix issue with NPE when generating stats with hazelcast

--- support/cas-server-support-hazelcast-monitor/src/main/java/org/apereo/cas/monitor/HazelcastMonitor.java
+++ support/cas-server-support-hazelcast-monitor/src/main/java/org/apereo/cas/monitor/HazelcastMonitor.java
@@ -4,6 +4,7 @@ import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
 import com.hazelcast.monitor.LocalMapStats;
+import org.apereo.cas.configuration.model.support.hazelcast.HazelcastProperties;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,12 +20,14 @@ public class HazelcastMonitor extends AbstractCacheMonitor {
     @Override
     protected CacheStatistics[] getStatistics() {
         final List<CacheStatistics> statsList = new ArrayList<>();
-
-        final HazelcastInstance instance = Hazelcast.getHazelcastInstanceByName(
-                casProperties.getTicket().getRegistry().getHazelcast().getCluster().getInstanceName());
-        final IMap map = instance.getMap(casProperties.getTicket().getRegistry().getHazelcast().getMapName());
+        final HazelcastProperties hz = casProperties.getTicket().getRegistry().getHazelcast();
+        logger.debug("Locating hazelcast instance {}...", hz.getCluster().getInstanceName());
+        final HazelcastInstance instance = Hazelcast.getHazelcastInstanceByName(hz.getCluster().getInstanceName());
+        logger.debug("Locating hazelcast map {} from instance {}...", hz.getMapName(), hz.getCluster().getInstanceName());
+        final IMap map = instance.getMap(hz.getMapName());
+        logger.debug("Starting to collect hazelcast statistics...");
         statsList.add(new HazelcastStatistics(map));
-
+        
         return statsList.toArray(new CacheStatistics[statsList.size()]);
     }
 
