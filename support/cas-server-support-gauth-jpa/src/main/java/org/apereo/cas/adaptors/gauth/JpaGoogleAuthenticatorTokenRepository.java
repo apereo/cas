64683846632a332c8ef2764c8e10eeccd52eb75a
Misagh Moayyed <mm1844@gmail.com>
2017-01-11 12:53:04 +0330

    GAuth token repository for JPA

--- support/cas-server-support-gauth-jpa/src/main/java/org/apereo/cas/adaptors/gauth/JpaGoogleAuthenticatorTokenRepository.java
+++ support/cas-server-support-gauth-jpa/src/main/java/org/apereo/cas/adaptors/gauth/JpaGoogleAuthenticatorTokenRepository.java
@@ -8,7 +8,9 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import java.time.LocalDateTime;
 
 /**
  * This is {@link JpaGoogleAuthenticatorTokenRepository}.
@@ -23,19 +25,34 @@ public class JpaGoogleAuthenticatorTokenRepository extends BaseGoogleAuthenticat
 
     @PersistenceContext(unitName = "googleAuthenticatorEntityManagerFactory")
     private EntityManager entityManager;
-    
+
     @Override
     public void clean() {
-        
+        final int count = this.entityManager.createQuery("DELETE FROM " + GoogleAuthenticatorToken.class.getSimpleName()
+                + " r where r.issuedDateTime<= :now", GoogleAuthenticatorToken.class)
+                .setParameter("now", LocalDateTime.now())
+                .executeUpdate();
+        logger.debug("Deleted {} expired previously used otp record(s)", count);
     }
 
     @Override
     public void store(final GoogleAuthenticatorToken token) {
-
+        this.entityManager.merge(token);
     }
 
     @Override
     public boolean exists(final String uid, final Integer otp) {
+        try {
+            final GoogleAuthenticatorToken r =
+                    this.entityManager.createQuery("SELECT r FROM " + GoogleAuthenticatorToken.class.getSimpleName()
+                            + " r where r.userId = :userId and r.token = :token", GoogleAuthenticatorToken.class)
+                            .setParameter("userId", uid)
+                            .setParameter("token", otp)
+                            .getSingleResult();
+            return r != null;
+        } catch (final NoResultException e) {
+            LOGGER.debug("No record could be found for google authenticator id {}", uid);
+        }
         return false;
     }
 }
