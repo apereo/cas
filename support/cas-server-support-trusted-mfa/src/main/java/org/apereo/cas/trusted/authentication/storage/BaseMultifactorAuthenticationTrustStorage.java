be0aa434663486d42f0327e918d2e0e4e568fded
Misagh Moayyed <mm1844@gmail.com>
2017-01-17 15:48:14 +0330

    Clean up redundant annotations

--- support/cas-server-support-trusted-mfa/src/main/java/org/apereo/cas/trusted/authentication/storage/BaseMultifactorAuthenticationTrustStorage.java
+++ support/cas-server-support-trusted-mfa/src/main/java/org/apereo/cas/trusted/authentication/storage/BaseMultifactorAuthenticationTrustStorage.java
@@ -22,7 +22,7 @@ import java.util.Set;
  * @since 5.0.0
  */
 @EnableTransactionManagement(proxyTargetClass = true)
-@Transactional(readOnly = false, transactionManager = "transactionManagerMfaAuthnTrust")
+@Transactional(transactionManager = "transactionManagerMfaAuthnTrust")
 public abstract class BaseMultifactorAuthenticationTrustStorage implements MultifactorAuthenticationTrustStorage {
     protected final transient Logger logger = LoggerFactory.getLogger(this.getClass());
     
