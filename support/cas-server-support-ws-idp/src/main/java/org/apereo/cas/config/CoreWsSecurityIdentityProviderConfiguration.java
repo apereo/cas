d47fcb53b7309e242f508146fa9d25a9b486dd2e
Misagh Moayyed <mm1844@gmail.com>
2017-01-19 12:30:44 +0330

    Added WS IDP module

--- support/cas-server-support-ws-idp/src/main/java/org/apereo/cas/config/CoreWsSecurityIdentityProviderConfiguration.java
+++ support/cas-server-support-ws-idp/src/main/java/org/apereo/cas/config/CoreWsSecurityIdentityProviderConfiguration.java
@@ -1,10 +1,15 @@
 package org.apereo.cas.config;
 
+import org.apache.cxf.transport.servlet.CXFServlet;
 import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+
+import java.util.Collections;
 
 /**
  * This is {@link CoreWsSecurityIdentityProviderConfiguration}.
@@ -19,6 +24,21 @@ public class CoreWsSecurityIdentityProviderConfiguration {
 
     @Autowired
     private CasConfigurationProperties casProperties;
-
+    
+    @Bean
+    public ServletRegistrationBean wsIdpMetadataServlet() {
+        final ServletRegistrationBean bean = new ServletRegistrationBean();
+        bean.setEnabled(true);
+        bean.setName("cxfServlet");
+        /**
+         * Fediz dependencies are only available inside the fediz-idp war file
+         * Need to see if including the idp as a war dependency allow us to use
+         * sources buried in the war. war is published to central.
+         */
+        bean.setServlet(new MetadataServlet());
+        bean.setUrlMappings(Collections.singleton("/ws/idp/*"));
+        bean.setAsyncSupported(true);
+        return bean;
+    }
 
 }
