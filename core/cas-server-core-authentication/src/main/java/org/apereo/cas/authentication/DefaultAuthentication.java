947cadc14da27d8ff931d7cd752da162fb30d438
Misagh Moayyed <mm1844@gmail.com>
2017-01-28 14:12:24 +0330

    Fixed check style issues

--- core/cas-server-core-authentication/src/main/java/org/apereo/cas/authentication/DefaultAuthentication.java
+++ core/cas-server-core-authentication/src/main/java/org/apereo/cas/authentication/DefaultAuthentication.java
@@ -1,6 +1,5 @@
 package org.apereo.cas.authentication;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
