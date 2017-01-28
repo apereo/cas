c797749d505d75ddd10d9939506ccf5372f560ec
Misagh Moayyed <mm1844@gmail.com>
2017-01-28 13:16:24 +0330

    Fix issue with bcrypt encoder NPE

--- core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/support/Beans.java
+++ core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/support/Beans.java
@@ -207,6 +207,14 @@ public class Beans {
             case STANDARD:
                 return new StandardPasswordEncoder(properties.getSecret());
             default:
+                LOGGER.debug("Creating BCRYPT password encoder given the strength [{}] and secret in the configuration",
+                        properties.getStrength());
+                if (StringUtils.isBlank(properties.getSecret())) {
+                    LOGGER.debug("Creating BCRYPT encoder without secret");
+                    return new BCryptPasswordEncoder(properties.getStrength());
+                }
+
+                LOGGER.debug("Creating BCRYPT encoder with secret");
                 return new BCryptPasswordEncoder(properties.getStrength(),
                         new SecureRandom(properties.getSecret().getBytes(StandardCharsets.UTF_8)));
         }
