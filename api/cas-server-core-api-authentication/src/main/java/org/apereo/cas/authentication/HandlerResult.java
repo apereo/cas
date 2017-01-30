71502ddf358cb869cad3e9ada2c5c02493d21c17
Misagh Moayyed <mm1844@gmail.com>
2017-01-30 21:41:07 +0100

    clean up tests

--- api/cas-server-core-api-authentication/src/main/java/org/apereo/cas/authentication/HandlerResult.java
+++ api/cas-server-core-api-authentication/src/main/java/org/apereo/cas/authentication/HandlerResult.java
@@ -12,7 +12,7 @@ import java.util.List;
  * @author Misagh Moayyed
  * @since 4.1.0
  */
-@JsonTypeInfo(use=JsonTypeInfo.Id.CLASS, include= JsonTypeInfo.As.PROPERTY)
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY)
 public interface HandlerResult extends Serializable {
 
     /**
