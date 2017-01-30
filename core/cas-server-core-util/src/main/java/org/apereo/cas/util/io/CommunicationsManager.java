686537f805c907f87272410a8d69f2aec101c207
Misagh Moayyed <mmoayyed@unicon.net>
2017-01-30 19:45:35 +0100

    Fix bug in detection of mail sender

--- core/cas-server-core-util/src/main/java/org/apereo/cas/util/io/CommunicationsManager.java
+++ core/cas-server-core-util/src/main/java/org/apereo/cas/util/io/CommunicationsManager.java
@@ -95,7 +95,7 @@ public class CommunicationsManager {
                          final String subject, final String to,
                          final String cc, final String bcc) {
         try {
-            if (isMailSenderDefined() || StringUtils.isBlank(text) || StringUtils.isBlank(from)
+            if (!isMailSenderDefined() || StringUtils.isBlank(text) || StringUtils.isBlank(from)
                     || StringUtils.isBlank(subject) || StringUtils.isBlank(to)) {
                 LOGGER.warn("Could not send email to [{}] because either no address/subject/text is found or email settings are not configured.", to);
                 return false;
