d731bf942e9a8bb053e42d704825ebd9217a9a98
Misagh Moayyed <mm1844@gmail.com>
2017-01-30 21:41:07 +0100

    clean up tests

--- support/cas-server-support-actions/src/test/java/org/apereo/cas/web/flow/AllTestsSuite.java
+++ support/cas-server-support-actions/src/test/java/org/apereo/cas/web/flow/AllTestsSuite.java
@@ -1,17 +1,5 @@
-package org.apereo.cas;
+package org.apereo.cas.web.flow;
 
-import org.apereo.cas.web.flow.AuthenticationViaFormActionTests;
-import org.apereo.cas.web.flow.CasDefaultFlowUrlHandlerTests;
-import org.apereo.cas.web.flow.FrontChannelLogoutActionTests;
-import org.apereo.cas.web.flow.GenerateServiceTicketActionTests;
-import org.apereo.cas.web.flow.GenericSuccessViewActionTests;
-import org.apereo.cas.web.flow.InitialFlowSetupActionCookieTests;
-import org.apereo.cas.web.flow.InitialFlowSetupActionSsoTests;
-import org.apereo.cas.web.flow.InitialFlowSetupActionTests;
-import org.apereo.cas.web.flow.LogoutActionTests;
-import org.apereo.cas.web.flow.SendTicketGrantingTicketActionTests;
-import org.apereo.cas.web.flow.ServiceAuthorizationCheckTests;
-import org.apereo.cas.web.flow.TicketGrantingTicketCheckActionTests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
