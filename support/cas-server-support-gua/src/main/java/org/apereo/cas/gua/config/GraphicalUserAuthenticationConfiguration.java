b8541f2370167f94fd98f69598363bcfc4d466c1
Misagh Moayyed <mm1844@gmail.com>
2017-02-02 23:43:51 +0100

    Resumed work on GUA

--- support/cas-server-support-gua/src/main/java/org/apereo/cas/gua/config/GraphicalUserAuthenticationConfiguration.java
+++ support/cas-server-support-gua/src/main/java/org/apereo/cas/gua/config/GraphicalUserAuthenticationConfiguration.java
@@ -1,11 +1,20 @@
 package org.apereo.cas.gua.config;
 
 import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.web.flow.CasWebflowConfigurer;
+import org.apereo.cas.web.flow.GraphicalUserAuthenticationWebflowConfigurer;
+import org.apereo.cas.web.flow.PrepareForGraphicalAuthenticationAction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.webflow.definition.registry.FlowDefinitionRegistry;
+import org.springframework.webflow.engine.builder.support.FlowBuilderServices;
+import org.springframework.webflow.execution.Action;
 
 /**
  * This is {@link GraphicalUserAuthenticationConfiguration}.
@@ -21,5 +30,22 @@ public class GraphicalUserAuthenticationConfiguration {
     @Autowired
     private CasConfigurationProperties casProperties;
 
-    
+    @Autowired
+    @Qualifier("loginFlowRegistry")
+    private FlowDefinitionRegistry loginFlowDefinitionRegistry;
+
+    @Autowired
+    private FlowBuilderServices flowBuilderServices;
+
+    @ConditionalOnMissingBean(name = "graphicalUserAuthenticationWebflowConfigurer")
+    @Bean
+    public CasWebflowConfigurer graphicalUserAuthenticationWebflowConfigurer() {
+        return new GraphicalUserAuthenticationWebflowConfigurer(flowBuilderServices,
+                loginFlowDefinitionRegistry, casProperties.getAuthn().getGua());
+    }
+
+    @Bean
+    public Action prepareForGraphicalAuthenticationAction() {
+        return new PrepareForGraphicalAuthenticationAction(casProperties.getAuthn().getGua());
+    }
 }
