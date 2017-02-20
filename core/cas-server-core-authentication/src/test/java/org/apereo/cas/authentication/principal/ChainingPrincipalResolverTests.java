b02d052cb8fee6adfec4cabf7255bdeb3479fce0
Misagh Moayyed <mm1844@gmail.com>
2017-02-20 16:02:16 +0330

    Updated docs

--- core/cas-server-core-authentication/src/test/java/org/apereo/cas/authentication/principal/ChainingPrincipalResolverTests.java
+++ core/cas-server-core-authentication/src/test/java/org/apereo/cas/authentication/principal/ChainingPrincipalResolverTests.java
@@ -2,6 +2,7 @@ package org.apereo.cas.authentication.principal;
 
 import org.apereo.cas.authentication.AuthenticationHandler;
 import org.apereo.cas.authentication.Credential;
+import org.apereo.cas.authentication.handler.support.SimpleTestUsernamePasswordAuthenticationHandler;
 import org.apereo.cas.authentication.principal.resolvers.ChainingPrincipalResolver;
 import org.junit.Test;
 
@@ -49,12 +50,14 @@ public class ChainingPrincipalResolverTests {
 
         final PrincipalResolver resolver2 = mock(PrincipalResolver.class);
         when(resolver2.supports(any(Credential.class))).thenReturn(true);
-        when(resolver2.resolve(any(Credential.class), any(Principal.class), any(AuthenticationHandler.class)))
+        when(resolver2.resolve(any(Credential.class), any(Principal.class),
+                any(AuthenticationHandler.class)))
                 .thenReturn(principalFactory.createPrincipal("output", Collections.<String, Object>singletonMap("mail", "final@example.com")));
 
         final ChainingPrincipalResolver resolver = new ChainingPrincipalResolver();
         resolver.setChain(Arrays.asList(resolver1, resolver2));
-        final Principal principal = resolver.resolve(credential, any(Principal.class), any(AuthenticationHandler.class));
+        final Principal principal = resolver.resolve(credential, any(Principal.class), 
+                new SimpleTestUsernamePasswordAuthenticationHandler());
         assertEquals("output", principal.getId());
         assertEquals("final@example.com", principal.getAttributes().get("mail"));
     }
