4dfa135e601eff0a713b4a655d4db7b035042250
Misagh Moayyed <mm1844@gmail.com>
2017-02-24 20:16:24 +0330

    Javadoc fixes

--- support/cas-server-support-saml-idp/src/main/java/org/apereo/cas/support/saml/web/idp/profile/IdPInitiatedProfileHandlerController.java
+++ support/cas-server-support-saml-idp/src/main/java/org/apereo/cas/support/saml/web/idp/profile/IdPInitiatedProfileHandlerController.java
@@ -68,6 +68,7 @@ public class IdPInitiatedProfileHandlerController extends AbstractSamlProfileHan
      * @param logoutUrl                                    the logout url
      * @param forceSignedLogoutRequests                    the force signed logout requests
      * @param singleLogoutCallbacksDisabled                the single logout callbacks disabled
+     * @param samlObjectSignatureValidator                 the saml object signature validator
      */
     public IdPInitiatedProfileHandlerController(final BaseSamlObjectSigner samlObjectSigner,
                                                 final ParserPool parserPool,
