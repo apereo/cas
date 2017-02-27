59c2f30dd9bee6fc99b2d5706b7c44517b00f425
Misagh Moayyed <mm1844@gmail.com>
2017-02-27 19:17:40 +0330

    Clean up JPA based on ticket metadata so it no longer references oauth modules

--- core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/jpa/ticketregistry/JpaTicketRegistryProperties.java
+++ core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/jpa/ticketregistry/JpaTicketRegistryProperties.java
@@ -5,20 +5,23 @@ import org.apereo.cas.configuration.model.support.jpa.AbstractJpaProperties;
 import org.apereo.cas.configuration.support.Beans;
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
 
+import javax.persistence.LockModeType;
+
 /**
- * Configuration properties class for ticketreg.database.
+ * Common properties for jpa ticket reg.
  *
  * @author Dmitriy Kopylenko
  * @since 5.0.0
  */
-
 public class JpaTicketRegistryProperties extends AbstractJpaProperties {
 
-    /** Default lock timeout is 1 hour. */
+    /**
+     * Default lock timeout is 1 hour.
+     */
     public static final String DEFAULT_LOCK_TIMEOUT = "PT1H";
-    
-    private boolean jpaLockingTgtEnabled = true;
-    
+
+    private LockModeType ticketLockType = LockModeType.NONE;
+
     private String jpaLockingTimeout = DEFAULT_LOCK_TIMEOUT;
 
     @NestedConfigurationProperty
@@ -27,21 +30,13 @@ public class JpaTicketRegistryProperties extends AbstractJpaProperties {
     public JpaTicketRegistryProperties() {
         super.setUrl("jdbc:hsqldb:mem:cas-ticket-registry");
     }
-    
+
     public CryptographyProperties getCrypto() {
         return crypto;
     }
 
     public void setCrypto(final CryptographyProperties crypto) {
         this.crypto = crypto;
-    }
-    
-    public boolean isJpaLockingTgtEnabled() {
-        return jpaLockingTgtEnabled;
-    }
-
-    public void setJpaLockingTgtEnabled(final boolean jpaLockingTgtEnabled) {
-        this.jpaLockingTgtEnabled = jpaLockingTgtEnabled;
     }
 
     public long getJpaLockingTimeout() {
@@ -50,5 +45,13 @@ public class JpaTicketRegistryProperties extends AbstractJpaProperties {
 
     public void setJpaLockingTimeout(final String jpaLockingTimeout) {
         this.jpaLockingTimeout = jpaLockingTimeout;
+    }
+
+    public LockModeType getTicketLockType() {
+        return ticketLockType;
+    }
+
+    public void setTicketLockType(final LockModeType ticketLockType) {
+        this.ticketLockType = ticketLockType;
     }
 }
