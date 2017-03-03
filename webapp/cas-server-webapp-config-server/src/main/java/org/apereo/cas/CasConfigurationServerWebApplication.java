f47b019e2d84957e95150c24ba7773ed82386391
Misagh Moayyed <mm1844@gmail.com>
2017-03-03 13:40:09 +0330

    Externalize config server

--- webapp/cas-server-webapp-config-server/src/main/java/org/apereo/cas/CasConfigurationServerWebApplication.java
+++ webapp/cas-server-webapp-config-server/src/main/java/org/apereo/cas/CasConfigurationServerWebApplication.java
@@ -1,11 +1,10 @@
-package org.apereo.cas.web;
+package org.apereo.cas;
 
-import org.apereo.cas.CasEmbeddedContainerUtils;
-import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.springframework.boot.Banner;
-import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.MetricsDropwizardAutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CassandraAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
@@ -18,24 +17,17 @@ import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 import java.util.Map;
 
 /**
- * This is {@link CasWebApplication}.
+ * This is {@link CasConfigurationServerWebApplication}.
  *
  * @author Misagh Moayyed
- * @since 5.0.0
+ * @since 5.1.0
  */
-@ImportResource(locations = {
-        "classpath:/deployerConfigContext.groovy",
-        "classpath:/deployerConfigContext.xml"})
-@SpringBootConfiguration
+@SpringBootApplication
 @EnableAutoConfiguration(
         exclude = {HibernateJpaAutoConfiguration.class,
                 JerseyAutoConfiguration.class,
@@ -49,28 +41,19 @@ import java.util.Map;
                 DataSourceTransactionManagerAutoConfiguration.class,
                 MetricsDropwizardAutoConfiguration.class,
                 RedisRepositoriesAutoConfiguration.class})
-@EnableAsync
-@EnableConfigurationProperties(CasConfigurationProperties.class)
-@EnableTransactionManagement(proxyTargetClass = true)
-@EnableScheduling
-public class CasWebApplication {
-    /**
-     * Instantiates a new Cas web application.
-     */
-    protected CasWebApplication() {
+@EnableConfigServer
+public class CasConfigurationServerWebApplication {
+    protected CasConfigurationServerWebApplication() {
     }
 
     /**
-     * Main entry point of the CAS web application.
+     * Main.
      *
      * @param args the args
      */
     public static void main(final String[] args) {
-        final Map<String, Object> properties = CasEmbeddedContainerUtils.getRuntimeProperties(Boolean.TRUE);
-        final Banner banner = CasEmbeddedContainerUtils.getCasBannerInstance();
-        new SpringApplicationBuilder(CasWebApplication.class)
-                .banner(banner)
-                .properties(properties)
+        new SpringApplicationBuilder(CasConfigurationServerWebApplication.class)
+                .banner(new CasConfigurationServerBanner())
                 .logStartupInfo(true)
                 .run(args);
     }
