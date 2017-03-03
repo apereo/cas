69c5cbce9f9781c533684eb0ddc2b4c04e33662d
Misagh Moayyed <mm1844@gmail.com>
2017-03-03 17:25:47 +0330

    Externalize config server

---
+++ core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/config/CasCoreBootstrapStandaloneConfiguration.java
@@ -0,0 +1,74 @@
+package org.apereo.cas.configuration.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOCase;
+import org.apache.commons.io.filefilter.NameFileFilter;
+import org.apache.commons.io.filefilter.RegexFileFilter;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.jooq.lambda.Unchecked;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.bootstrap.config.PropertySourceLocator;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.env.PropertySource;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * This is {@link CasCoreBootstrapStandaloneConfiguration}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.1.0
+ */
+@Profile("standalone")
+@ConditionalOnProperty(value = "spring.cloud.config.enabled", havingValue = "false")
+@Configuration("casStandaloneBootstrapConfiguration")
+@EnableConfigurationProperties(CasConfigurationProperties.class)
+public class CasCoreBootstrapStandaloneConfiguration implements PropertySourceLocator {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CasCoreBootstrapStandaloneConfiguration.class);
+    private static final String PROPERTY_NAME_CAS_STANDALONE_CONFIG = "cas.standalone.config";
+
+    @Override
+    public PropertySource<?> locate(final Environment environment) {
+        final Properties props = new Properties();
+        
+        LOGGER.debug("Locating CAS standalone configuration directory under setting [{}]", PROPERTY_NAME_CAS_STANDALONE_CONFIG);
+        final File config = environment.getProperty(PROPERTY_NAME_CAS_STANDALONE_CONFIG, File.class);
+        
+        if (config.isDirectory()) {
+            final Collection<File> configFiles = FileUtils.listFiles(config, 
+                    new RegexFileFilter("(cas|application)\\.(yml|properties)", IOCase.INSENSITIVE), TrueFileFilter.INSTANCE);
+
+            LOGGER.debug("Configuration files found at [{}] are [{}]", config, configFiles);
+            final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+            configFiles.forEach(Unchecked.consumer(f -> {
+                LOGGER.debug("Loading configuration file [{}]", f);
+                if (f.getName().toLowerCase().endsWith("yml")) {
+                    final Map pp = mapper.readValue(f, Map.class);
+                    props.putAll(pp);
+                } else {
+                    final Properties pp = new Properties();
+                    pp.load(new FileReader(f));
+                    props.putAll(pp);
+                }
+            }));
+        } else {
+            LOGGER.warn("Configuration directory [{}] is not a directory and no settings are loaded by CAS", config);
+        }
+
+        LOGGER.info("Found and loaded [{}] setting(s) at [{}]", props.size(), config);
+        return new PropertiesPropertySource("standaloneCasConfigService", props);
+    }
+}
