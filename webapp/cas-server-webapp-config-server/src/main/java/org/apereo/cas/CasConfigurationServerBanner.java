ef70d36505722c5d26d25933b2dd4362a5fa124e
Misagh Moayyed <mm1844@gmail.com>
2017-03-03 16:05:31 +0330

    Dependency clean up

--- webapp/cas-server-webapp-config-server/src/main/java/org/apereo/cas/CasConfigurationServerBanner.java
+++ webapp/cas-server-webapp-config-server/src/main/java/org/apereo/cas/CasConfigurationServerBanner.java
@@ -1,5 +1,7 @@
 package org.apereo.cas;
 
+import org.apereo.cas.util.spring.boot.AbstractCasBanner;
+
 /**
  * This is {@link CasConfigurationServerBanner}.
  *
@@ -7,4 +9,8 @@ package org.apereo.cas;
  * @since 5.1.0
  */
 public class CasConfigurationServerBanner extends AbstractCasBanner {
+    @Override
+    protected String getTitle() {
+        return "CAS Config Server";
+    }
 }
