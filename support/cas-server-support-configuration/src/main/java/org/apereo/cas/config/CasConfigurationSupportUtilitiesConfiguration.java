5658a1f67e98add3296f4fb2682d0ccfddc5da6b
Misagh Moayyed <mm1844@gmail.com>
2017-03-03 17:52:24 +0330

    Externalize config server

--- support/cas-server-support-configuration/src/main/java/org/apereo/cas/config/CasConfigurationSupportUtilitiesConfiguration.java
+++ support/cas-server-support-configuration/src/main/java/org/apereo/cas/config/CasConfigurationSupportUtilitiesConfiguration.java
@@ -3,25 +3,19 @@ package org.apereo.cas.config;
 import com.google.common.base.Throwables;
 import org.apereo.cas.config.monitor.ConfigurationDirectoryPathWatchService;
 import org.apereo.cas.configuration.CasConfigurationProperties;
-import org.apereo.cas.util.ResourceUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.config.server.config.EnvironmentRepositoryConfiguration;
-import org.springframework.cloud.config.server.environment.NativeEnvironmentRepository;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.ResourceLoader;
 
 import javax.annotation.PostConstruct;
-import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.List;
+import java.io.File;
 
 /**
  * This is {@link CasConfigurationSupportUtilitiesConfiguration}.
@@ -41,8 +35,8 @@ public class CasConfigurationSupportUtilitiesConfiguration {
      * The watch configuration.
      */
     @Configuration("casCoreConfigurationWatchConfiguration")
-    @Profile("native")
-    @AutoConfigureAfter(value = {EnvironmentRepositoryConfiguration.class, EnvironmentRepositoryConfiguration.class})
+    @Profile("standalone")
+    @ConditionalOnProperty(value = "spring.cloud.config.enabled", havingValue = "false")
     public class CasCoreConfigurationWatchConfiguration {
 
         @Autowired
@@ -52,8 +46,7 @@ public class CasConfigurationSupportUtilitiesConfiguration {
         private ApplicationEventPublisher eventPublisher;
 
         @Autowired
-        @Qualifier("environmentRepository")
-        private NativeEnvironmentRepository environmentRepository;
+        private Environment environment;
 
         @PostConstruct
         public void init() {
@@ -62,19 +55,13 @@ public class CasConfigurationSupportUtilitiesConfiguration {
 
         public void runNativeConfigurationDirectoryPathWatchService() {
             try {
-                final List<String> locs = Arrays.asList(environmentRepository.getSearchLocations());
-                if (locs.isEmpty()) {
-                    throw new BeanCreationException("No search locations are defined for the native configuration profile");
-                }
-                final String loc = locs.get(0);
-                if (casProperties.getEvents().isTrackConfigurationModifications()
-                        && ResourceUtils.doesResourceExist(loc, this.resourceLoader)) {
-                    LOGGER.debug("Starting to watch configuration directory [{}]", loc);
-                    final Path directory = resourceLoader.getResource(loc).getFile().toPath();
-                    final Thread th = new Thread(new ConfigurationDirectoryPathWatchService(directory, eventPublisher));
+                final File config = CasConfigurationProperties.getStandaloneProfileConfigurationDirectory(environment);
+                if (casProperties.getEvents().isTrackConfigurationModifications() && config.exists()) {
+                    LOGGER.debug("Starting to watch configuration directory [{}]", config);
+                    final Thread th = new Thread(new ConfigurationDirectoryPathWatchService(config.toPath(), eventPublisher));
                     th.start();
                 } else {
-                    LOGGER.info("CAS is configured to NOT watch configuration directory [{}]. Changes require manual reloads/restarts.", loc);
+                    LOGGER.info("CAS is configured to NOT watch configuration directory [{}]. Changes require manual reloads/restarts.", config);
                 }
             } catch (final Exception e) {
                 throw Throwables.propagate(e);
