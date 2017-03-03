e0b4dcd543b3cbc9d7428f9ee1e7aabfa1eeedd7
Misagh Moayyed <mm1844@gmail.com>
2017-03-04 00:19:06 +0330

    Working on config management in dashboard

--- core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/CasConfigurationProperties.java
+++ core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/CasConfigurationProperties.java
@@ -1,5 +1,14 @@
 package org.apereo.cas.configuration;
 
+import com.google.common.base.Throwables;
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.FileBasedConfiguration;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.PropertiesConfigurationLayout;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.configuration2.convert.DefaultListDelimiterHandler;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apereo.cas.configuration.model.core.CasServerProperties;
 import org.apereo.cas.configuration.model.core.HostProperties;
 import org.apereo.cas.configuration.model.core.audit.AuditProperties;
@@ -46,6 +55,8 @@ import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.core.env.Environment;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 
 /**
  * This is {@link CasConfigurationProperties}.
@@ -515,5 +526,27 @@ public class CasConfigurationProperties {
      */
     public static File getStandaloneProfileConfigurationDirectory(final Environment environment) {
         return environment.getProperty("cas.standalone.config", File.class, new File("/etc/cas/config"));
+    }
+
+    /**
+     * Save property for standalone profile.
+     *
+     * @param pair        the pair
+     * @param environment the environment
+     */
+    public static void savePropertyForStandaloneProfile(final Pair<String, String> pair, final Environment environment) {
+        try {
+            final File file = getStandaloneProfileConfigurationDirectory(environment);
+            final Parameters params = new Parameters();
+            final FileBasedConfigurationBuilder<FileBasedConfiguration> builder =
+                    new FileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration.class)
+                            .configure(params.properties().setFile(new File(file, "cas.properties")));
+
+            final Configuration config = builder.getConfiguration();
+            config.setProperty(pair.getKey(), pair.getValue());
+            builder.save();
+        } catch (final Exception e) {
+            throw Throwables.propagate(e);
+        }
     }
 }
