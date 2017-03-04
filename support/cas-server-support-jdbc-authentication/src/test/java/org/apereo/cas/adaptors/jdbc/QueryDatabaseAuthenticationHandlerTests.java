7af79b4365dbb9257a8f671c4015dd72050a63ec
David Rodriguez <davidrg131092@gmail.com>
2017-03-04 18:43:12 +0100

    Fix checkstyle

--- support/cas-server-support-jdbc-authentication/src/test/java/org/apereo/cas/adaptors/jdbc/QueryDatabaseAuthenticationHandlerTests.java
+++ support/cas-server-support-jdbc-authentication/src/test/java/org/apereo/cas/adaptors/jdbc/QueryDatabaseAuthenticationHandlerTests.java
@@ -158,8 +158,8 @@ public class QueryDatabaseAuthenticationHandlerTests {
 
     @Test
     public void verifyFindUserAndExpired() throws Exception {
-        final QueryDatabaseAuthenticationHandler q = new QueryDatabaseAuthenticationHandler("", null, null, null, this.dataSource, SQL, PASSWORD_FIELD, "expired",
-                null, Collections.emptyMap());
+        final QueryDatabaseAuthenticationHandler q = new QueryDatabaseAuthenticationHandler("", null, null, null, this.dataSource, SQL, PASSWORD_FIELD,
+                "expired", null, Collections.emptyMap());
         this.thrown.expect(AccountPasswordMustChangeException.class);
         q.authenticate(CoreAuthenticationTestUtils.getCredentialsWithDifferentUsernameAndPassword("user20", "psw20"));
         fail("Shouldn't get here");
