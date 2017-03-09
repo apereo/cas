c9e33d14a0c69c586b2515fd9279aea2dc3fb18a
Misagh Moayyed <mm1844@gmail.com>
2017-03-09 13:50:27 +0330

    Clean up logout and metadata resolution

--- support/cas-server-support-saml-idp/src/main/java/org/apereo/cas/support/saml/web/flow/SamlIdPMetadataUIAction.java
+++ support/cas-server-support-saml-idp/src/main/java/org/apereo/cas/support/saml/web/flow/SamlIdPMetadataUIAction.java
@@ -4,6 +4,7 @@ import org.apereo.cas.authentication.principal.Service;
 import org.apereo.cas.services.RegisteredService;
 import org.apereo.cas.services.RegisteredServiceAccessStrategyUtils;
 import org.apereo.cas.services.ServicesManager;
+import org.apereo.cas.services.UnauthorizedServiceException;
 import org.apereo.cas.support.saml.mdui.MetadataUIUtils;
 import org.apereo.cas.support.saml.mdui.SamlMetadataUIInfo;
 import org.apereo.cas.support.saml.services.SamlRegisteredService;
@@ -14,6 +15,8 @@ import org.apereo.cas.web.support.WebUtils;
 import org.springframework.webflow.action.AbstractAction;
 import org.springframework.webflow.execution.Event;
 import org.springframework.webflow.execution.RequestContext;
+
+import java.util.Optional;
 
 /**
  * This is {@link SamlIdPMetadataUIAction}.
@@ -43,15 +46,18 @@ public class SamlIdPMetadataUIAction extends AbstractAction {
             service = serviceSelectionStrategy.resolveServiceFrom(service);
             final RegisteredService registeredService = this.servicesManager.findServiceBy(service);
             RegisteredServiceAccessStrategyUtils.ensureServiceAccessIsAllowed(service, registeredService);
-
-
+            
             if (registeredService instanceof SamlRegisteredService) {
-
                 final SamlRegisteredService samlService = SamlRegisteredService.class.cast(registeredService);
-                final SamlRegisteredServiceServiceProviderMetadataFacade facade =
+                final Optional<SamlRegisteredServiceServiceProviderMetadataFacade> adaptor =
                         SamlRegisteredServiceServiceProviderMetadataFacade.get(resolver, samlService, service.getId());
 
-                final SamlMetadataUIInfo mdui = MetadataUIUtils.locateMetadataUserInterfaceForEntityId(facade.getEntityDescriptor(),
+                if (!adaptor.isPresent()) {
+                    throw new UnauthorizedServiceException(UnauthorizedServiceException.CODE_UNAUTHZ_SERVICE, 
+                            "Cannot find metadata linked to " + service.getId());
+                }
+                
+                final SamlMetadataUIInfo mdui = MetadataUIUtils.locateMetadataUserInterfaceForEntityId(adaptor.get().getEntityDescriptor(),
                         service.getId(), registeredService);
                 WebUtils.putServiceUserInterfaceMetadata(requestContext, mdui);
             }
--- support/cas-server-support-saml-idp/src/main/java/org/apereo/cas/support/saml/web/idp/profile/slo/AbstractSamlSLOProfileHandlerController.java
+++ support/cas-server-support-saml-idp/src/main/java/org/apereo/cas/support/saml/web/idp/profile/slo/AbstractSamlSLOProfileHandlerController.java
@@ -1,5 +1,4 @@
-package org.apereo.cas.support.saml.web.idp.profile;
-
+package org.apereo.cas.support.saml.web.idp.profile.slo;
 
 import net.shibboleth.utilities.java.support.xml.ParserPool;
 import org.apache.commons.lang3.tuple.Pair;
@@ -8,42 +7,39 @@ import org.apereo.cas.authentication.principal.ServiceFactory;
 import org.apereo.cas.authentication.principal.WebApplicationService;
 import org.apereo.cas.services.ServicesManager;
 import org.apereo.cas.support.saml.OpenSamlConfigBean;
-import org.apereo.cas.support.saml.SamlIdPConstants;
 import org.apereo.cas.support.saml.SamlIdPUtils;
 import org.apereo.cas.support.saml.SamlUtils;
 import org.apereo.cas.support.saml.services.idp.metadata.cache.SamlRegisteredServiceCachingMetadataResolver;
+import org.apereo.cas.support.saml.web.idp.profile.AbstractSamlProfileHandlerController;
 import org.apereo.cas.support.saml.web.idp.profile.builders.SamlProfileObjectBuilder;
 import org.apereo.cas.support.saml.web.idp.profile.builders.enc.BaseSamlObjectSigner;
 import org.apereo.cas.support.saml.web.idp.profile.builders.enc.SamlObjectSignatureValidator;
 import org.opensaml.messaging.context.MessageContext;
 import org.opensaml.messaging.decoder.servlet.BaseHttpServletRequestXMLMessageDecoder;
 import org.opensaml.saml.common.SAMLException;
+import org.opensaml.saml.common.SAMLObject;
 import org.opensaml.saml.common.SignableSAMLObject;
 import org.opensaml.saml.common.binding.SAMLBindingSupport;
 import org.opensaml.saml.metadata.resolver.MetadataResolver;
-import org.opensaml.saml.saml2.binding.decoding.impl.HTTPPostDecoder;
 import org.opensaml.saml.saml2.core.LogoutRequest;
-import org.opensaml.saml.saml2.core.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Set;
 
 /**
- * This is {@link SLOPostProfileHandlerController}, responsible for
- * handling requests for SAML2 SLO.
+ * This is {@link AbstractSamlSLOProfileHandlerController}.
  *
  * @author Misagh Moayyed
- * @since 5.0.0
+ * @since 5.1.0
  */
-public class SLOPostProfileHandlerController extends AbstractSamlProfileHandlerController {
-    private static final Logger LOGGER = LoggerFactory.getLogger(SLOPostProfileHandlerController.class);
-
+public abstract class AbstractSamlSLOProfileHandlerController extends AbstractSamlProfileHandlerController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AbstractSamlSLOProfileHandlerController.class);
+    
     /**
-     * Instantiates a new slo saml profile handler controller.
+     * Instantiates a new Abstract saml profile handler controller.
      *
      * @param samlObjectSigner                             the saml object signer
      * @param parserPool                                   the parser pool
@@ -63,53 +59,23 @@ public class SLOPostProfileHandlerController extends AbstractSamlProfileHandlerC
      * @param singleLogoutCallbacksDisabled                the single logout callbacks disabled
      * @param samlObjectSignatureValidator                 the saml object signature validator
      */
-    public SLOPostProfileHandlerController(final BaseSamlObjectSigner samlObjectSigner,
-                                           final ParserPool parserPool,
-                                           final AuthenticationSystemSupport authenticationSystemSupport,
-                                           final ServicesManager servicesManager,
-                                           final ServiceFactory<WebApplicationService> webApplicationServiceFactory,
-                                           final SamlRegisteredServiceCachingMetadataResolver samlRegisteredServiceCachingMetadataResolver,
-                                           final OpenSamlConfigBean configBean,
-                                           final SamlProfileObjectBuilder<Response> responseBuilder,
-                                           final Set<String> authenticationContextClassMappings,
-                                           final String serverPrefix,
-                                           final String serverName,
-                                           final String authenticationContextRequestParameter,
-                                           final String loginUrl,
-                                           final String logoutUrl,
-                                           final boolean forceSignedLogoutRequests,
-                                           final boolean singleLogoutCallbacksDisabled,
-                                           final SamlObjectSignatureValidator samlObjectSignatureValidator) {
-        super(samlObjectSigner,
-                parserPool,
-                authenticationSystemSupport,
-                servicesManager,
-                webApplicationServiceFactory,
-                samlRegisteredServiceCachingMetadataResolver,
-                configBean,
-                responseBuilder,
-                authenticationContextClassMappings,
-                serverPrefix,
-                serverName,
-                authenticationContextRequestParameter,
-                loginUrl,
-                logoutUrl,
-                forceSignedLogoutRequests,
-                singleLogoutCallbacksDisabled,
-                samlObjectSignatureValidator);
-    }
-
-    /**
-     * Handle SLO POST profile request.
-     *
-     * @param response the response
-     * @param request  the request
-     * @throws Exception the exception
-     */
-    @PostMapping(path = SamlIdPConstants.ENDPOINT_SAML2_SLO_PROFILE_POST)
-    protected void handleSaml2ProfileSLOPostRequest(final HttpServletResponse response,
-                                                    final HttpServletRequest request) throws Exception {
-        handleSloPostProfileRequest(response, request, new HTTPPostDecoder());
+    public AbstractSamlSLOProfileHandlerController(final BaseSamlObjectSigner samlObjectSigner, final ParserPool parserPool,
+                                                   final AuthenticationSystemSupport authenticationSystemSupport,
+                                                   final ServicesManager servicesManager,
+                                                   final ServiceFactory<WebApplicationService> webApplicationServiceFactory,
+                                                   final SamlRegisteredServiceCachingMetadataResolver samlRegisteredServiceCachingMetadataResolver,
+                                                   final OpenSamlConfigBean configBean,
+                                                   final SamlProfileObjectBuilder<? extends SAMLObject> responseBuilder,
+                                                   final Set<String> authenticationContextClassMappings,
+                                                   final String serverPrefix, final String serverName,
+                                                   final String authenticationContextRequestParameter, final String loginUrl,
+                                                   final String logoutUrl,
+                                                   final boolean forceSignedLogoutRequests, final boolean singleLogoutCallbacksDisabled,
+                                                   final SamlObjectSignatureValidator samlObjectSignatureValidator) {
+        super(samlObjectSigner, parserPool, authenticationSystemSupport, servicesManager, webApplicationServiceFactory,
+                samlRegisteredServiceCachingMetadataResolver, configBean, responseBuilder, authenticationContextClassMappings,
+                serverPrefix, serverName, authenticationContextRequestParameter, loginUrl, logoutUrl, forceSignedLogoutRequests,
+                singleLogoutCallbacksDisabled, samlObjectSignatureValidator);
     }
 
     /**
@@ -120,7 +86,7 @@ public class SLOPostProfileHandlerController extends AbstractSamlProfileHandlerC
      * @param decoder  the decoder
      * @throws Exception the exception
      */
-    protected void handleSloPostProfileRequest(final HttpServletResponse response,
+    protected void handleSloProfileRequest(final HttpServletResponse response,
                                                final HttpServletRequest request,
                                                final BaseHttpServletRequestXMLMessageDecoder decoder) throws Exception {
         if (singleLogoutCallbacksDisabled) {
@@ -134,7 +100,9 @@ public class SLOPostProfileHandlerController extends AbstractSamlProfileHandlerC
 
         if (this.forceSignedLogoutRequests && !SAMLBindingSupport.isMessageSigned(ctx)) {
             throw new SAMLException("Logout request is not signed but should be.");
-        } else if (SAMLBindingSupport.isMessageSigned(ctx)) {
+        }
+
+        if (SAMLBindingSupport.isMessageSigned(ctx)) {
             final MetadataResolver resolver = SamlIdPUtils.getMetadataResolverForAllSamlServices(this.servicesManager,
                     SamlIdPUtils.getIssuerFromSamlRequest(logoutRequest),
                     this.samlRegisteredServiceCachingMetadataResolver);
