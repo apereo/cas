e944b78f7a26940425ea5f708a6b47b5061e7b9a
Misagh Moayyed <mmoayyed@unicon.net>
2017-03-10 21:55:59 +0330

    Fix check style issues

--- core/cas-server-core-authentication/src/main/java/org/apereo/cas/config/CasCoreHttpConfiguration.java
+++ core/cas-server-core-authentication/src/main/java/org/apereo/cas/config/CasCoreHttpConfiguration.java
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
