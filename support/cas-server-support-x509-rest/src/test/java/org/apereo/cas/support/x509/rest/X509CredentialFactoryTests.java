dc1cdaeb0a8178dfeaec92a70aa48dba797d74b3
Dmytro <fedongm@gmail.com>
2017-03-13 10:37:34 +0100

    Add x509 support over REST

---
+++ support/cas-server-support-x509-rest/src/test/java/org/apereo/cas/support/x509/rest/X509CredentialFactoryTests.java
@@ -0,0 +1,28 @@
+package org.apereo.cas.support.x509.rest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * Unit tests for {@link X509CredentialFactory}.
+ *
+ * @author Dmytro Fedonin
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class X509CredentialFactoryTests {
+
+    @InjectMocks
+    private X509CredentialFactory factory;
+
+    @Test
+    public void createX509Credential() {
+
+    }
+
+    @Test
+    public void createDefaultCredential() {
+
+    }
+}
