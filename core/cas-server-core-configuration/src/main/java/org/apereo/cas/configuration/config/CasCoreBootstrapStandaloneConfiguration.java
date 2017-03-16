25b4a29ee5fd706505fb442d5f279f57a4a0a5fc
Misagh Moayyed <mmoayyed@unicon.net>
2017-03-17 00:42:04 +0330

    Clean up docs. Attempt to load YML properties correctly

--- core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/config/CasCoreBootstrapStandaloneConfiguration.java
+++ core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/config/CasCoreBootstrapStandaloneConfiguration.java
@@ -1,7 +1,5 @@
 package org.apereo.cas.configuration.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOCase;
 import org.apache.commons.io.filefilter.RegexFileFilter;
@@ -10,6 +8,9 @@ import org.apereo.cas.configuration.CasConfigurationPropertiesEnvironmentManager
 import org.jooq.lambda.Unchecked;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.YamlProcessor;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.bootstrap.config.PropertySourceLocator;
 import org.springframework.context.annotation.Bean;
@@ -18,6 +19,9 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 import java.io.File;
 import java.io.FileReader;
@@ -42,6 +46,9 @@ import java.util.stream.Collectors;
 public class CasCoreBootstrapStandaloneConfiguration implements PropertySourceLocator {
     private static final Logger LOGGER = LoggerFactory.getLogger(CasCoreBootstrapStandaloneConfiguration.class);
 
+    @Autowired
+    private ResourceLoader resourceLoader;
+
     @Bean
     public CasConfigurationPropertiesEnvironmentManager configurationPropertiesEnvironmentManager() {
         return new CasConfigurationPropertiesEnvironmentManager();
@@ -50,44 +57,71 @@ public class CasCoreBootstrapStandaloneConfiguration implements PropertySourceLo
     @Override
     public PropertySource<?> locate(final Environment environment) {
         final Properties props = new Properties();
+        loadEmbeddedYamlOverriddenProperties(props);
 
         final File config = configurationPropertiesEnvironmentManager().getStandaloneProfileConfigurationDirectory();
         LOGGER.debug("Located CAS standalone configuration directory at [{}]", config);
-
         if (config.isDirectory() && config.exists()) {
-
-            final List<String> profiles = new ArrayList<>();
-            profiles.add(configurationPropertiesEnvironmentManager().getApplicationName());
-            profiles.addAll(Arrays.stream(environment.getActiveProfiles()).collect(Collectors.toList()));
-            final String propertyNames = profiles.stream().collect(Collectors.joining("|"));
-            final String regex = String.format("(%s|application)\\.(yml|properties)", propertyNames);
-            LOGGER.debug("Looking for configuration files at [{}] that match the pattern [{}]", config, regex);
-            
-            final Collection<File> configFiles = FileUtils.listFiles(config, new RegexFileFilter(regex, IOCase.INSENSITIVE), TrueFileFilter.INSTANCE)
-                    .stream()
-                    .sorted(Comparator.comparing(File::getName))
-                    .collect(Collectors.toList());
-            
-            LOGGER.info("Configuration files found at [{}] are [{}]", config, configFiles);
-            
-            final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-            configFiles.forEach(Unchecked.consumer(f -> {
-                LOGGER.debug("Loading configuration file [{}]", f);
-                if (f.getName().toLowerCase().endsWith("yml")) {
-                    final Map pp = mapper.readValue(f, Map.class);
-                    LOGGER.debug("Found settings [{}] in YAML file [{}]", pp.keySet(), f);
-                    props.putAll(pp);
-                } else {
-                    final Properties pp = new Properties();
-                    pp.load(new FileReader(f));
-                    LOGGER.debug("Found settings [{}] in file [{}]", pp.keySet(), f);
-                    props.putAll(pp);
-                }
-            }));
+            loadSettingsFromConfigurationSources(environment, props, config);
         } else {
             LOGGER.warn("Configuration directory [{}] is not a directory or cannot be found at the specific path", config);
         }
-        LOGGER.info("Found and loaded [{}] setting(s) from [{}]", props.size(), config);
+
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("Located setting(s) [{}] from [{}]", props.keySet(), config);
+        } else {
+            LOGGER.info("Found and loaded [{}] setting(s) from [{}]", props.size(), config);
+        }
         return new PropertiesPropertySource("standaloneCasConfigService", props);
+    }
+
+    private void loadSettingsFromConfigurationSources(final Environment environment, final Properties props, final File config) {
+        final List<String> profiles = new ArrayList<>();
+        profiles.add(configurationPropertiesEnvironmentManager().getApplicationName());
+        profiles.addAll(Arrays.stream(environment.getActiveProfiles()).collect(Collectors.toList()));
+        final String propertyNames = profiles.stream().collect(Collectors.joining("|"));
+        final String regex = String.format("(%s|application)\\.(yml|properties)", propertyNames);
+        LOGGER.debug("Looking for configuration files at [{}] that match the pattern [{}]", config, regex);
+
+        final Collection<File> configFiles = FileUtils.listFiles(config, new RegexFileFilter(regex, IOCase.INSENSITIVE), TrueFileFilter.INSTANCE)
+                .stream()
+                .sorted(Comparator.comparing(File::getName))
+                .collect(Collectors.toList());
+        LOGGER.info("Configuration files found at [{}] are [{}]", config, configFiles);
+        configFiles.forEach(Unchecked.consumer(f -> {
+            LOGGER.debug("Loading configuration file [{}]", f);
+            if (f.getName().toLowerCase().endsWith("yml")) {
+                final Map pp = loadYamlProperties(new FileSystemResource(f));
+                LOGGER.debug("Found settings [{}] in YAML file [{}]", pp.keySet(), f);
+                props.putAll(pp);
+            } else {
+                final Properties pp = new Properties();
+                pp.load(new FileReader(f));
+                LOGGER.debug("Found settings [{}] in file [{}]", pp.keySet(), f);
+                props.putAll(pp);
+            }
+        }));
+    }
+
+    private Map loadYamlProperties(final Resource... resource) {
+        final YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
+        factory.setResolutionMethod(YamlProcessor.ResolutionMethod.OVERRIDE);
+        factory.setResources(resource);
+        factory.setSingleton(true);
+        factory.afterPropertiesSet();
+        return factory.getObject();
+    }
+
+    private void loadEmbeddedYamlOverriddenProperties(final Properties props) {
+        final Resource resource = resourceLoader.getResource("classpath:/application.yml");
+        if (resource != null && resource.exists()) {
+            final Map pp = loadYamlProperties(resource);
+            if (pp.isEmpty()) {
+                LOGGER.debug("No properties were located inside [{}]", resource);
+            } else {
+                LOGGER.debug("Found settings [{}] in YAML file [{}]", pp.keySet(), resource);
+                props.putAll(pp);
+            }
+        }
     }
 }
