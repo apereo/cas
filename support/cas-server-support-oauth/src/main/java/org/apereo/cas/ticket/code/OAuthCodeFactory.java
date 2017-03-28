a7fe0e67db08c578f756f3ce282f0226eec8f3cc
Misagh Moayyed <mm1844@gmail.com>
2017-03-29 00:36:39 +0430

    Clean up oauth config. Clean up formatting

--- support/cas-server-support-oauth/src/main/java/org/apereo/cas/ticket/code/OAuthCodeFactory.java
+++ support/cas-server-support-oauth/src/main/java/org/apereo/cas/ticket/code/OAuthCodeFactory.java
@@ -3,6 +3,7 @@ package org.apereo.cas.ticket.code;
 import org.apereo.cas.authentication.Authentication;
 import org.apereo.cas.authentication.principal.Service;
 import org.apereo.cas.ticket.TicketFactory;
+import org.apereo.cas.ticket.TicketGrantingTicket;
 
 /**
  * Factory to create OAuth codes.
@@ -15,9 +16,10 @@ public interface OAuthCodeFactory extends TicketFactory {
     /**
      * Create an OAuth code.
      *
-     * @param service the service
-     * @param authentication the authentication
+     * @param service              the service
+     * @param authentication       the authentication
+     * @param ticketGrantingTicket the ticket granting ticket
      * @return the OAuth code
      */
-    OAuthCode create(Service service, Authentication authentication);
+    OAuthCode create(Service service, Authentication authentication, TicketGrantingTicket ticketGrantingTicket);
 }
