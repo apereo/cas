d452bc3c0cadf8bb8f713aec9af9f6a8053182f1
Misagh Moayyed <mm1844@gmail.com>
2017-04-05 15:37:34 +0430

    Initial pass at surrogate authn

---
+++ support/cas-server-support-surrogate-authentication/src/main/java/org/apereo/cas/authentication/SurrogateUsernamePasswordCredential.java
@@ -0,0 +1,33 @@
+package org.apereo.cas.authentication;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+/**
+ * This is {@link SurrogateUsernamePasswordCredential},
+ * able to substitute a target username on behalf of the given credentials.
+ *
+ * @author Misagh Moayyed
+ * @since 5.1.0
+ */
+public class SurrogateUsernamePasswordCredential extends RememberMeUsernamePasswordCredential {
+
+    private static final long serialVersionUID = 8760695298971444249L;
+
+    private String surrogateUsername;
+
+    public String getSurrogateUsername() {
+        return surrogateUsername;
+    }
+
+    public void setSurrogateUsername(final String surrogateUsername) {
+        this.surrogateUsername = surrogateUsername;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .appendSuper(super.toString())
+                .append("surrogateUsername", surrogateUsername)
+                .toString();
+    }
+}
