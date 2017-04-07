1d7d035e9cb33cfef8aae8e413ac21ff25e848c5
Misagh Moayyed <mm1844@gmail.com>
2017-04-07 17:46:22 +0430

    Fix issue with authentication success warnings in MFA flows

--- support/cas-server-support-u2f/src/main/java/org/apereo/cas/adaptors/u2f/web/flow/U2FMultifactorWebflowConfigurer.java
+++ support/cas-server-support-u2f/src/main/java/org/apereo/cas/adaptors/u2f/web/flow/U2FMultifactorWebflowConfigurer.java
@@ -1,6 +1,6 @@
 package org.apereo.cas.adaptors.u2f.web.flow;
 
-import org.apereo.cas.web.flow.AbstractCasWebflowConfigurer;
+import org.apereo.cas.web.flow.AbstractCasMultifactorWebflowConfigurer;
 import org.springframework.webflow.definition.registry.FlowDefinitionRegistry;
 import org.springframework.webflow.engine.builder.support.FlowBuilderServices;
 
@@ -10,7 +10,7 @@ import org.springframework.webflow.engine.builder.support.FlowBuilderServices;
  * @author Misagh Moayyed
  * @since 5.1.0
  */
-public class U2FMultifactorWebflowConfigurer extends AbstractCasWebflowConfigurer {
+public class U2FMultifactorWebflowConfigurer extends AbstractCasMultifactorWebflowConfigurer {
 
     /**
      * Webflow event id.
