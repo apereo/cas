f263b5f3d97984999c79dfcd89cac4417b3876fd
Misagh Moayyed <mm1844@gmail.com>
2017-04-12 02:08:46 +0430

    Add a “ready” ascii-art

--- core/cas-server-core-util/src/main/java/org/apereo/cas/util/AsciiArtUtils.java
+++ core/cas-server-core-util/src/main/java/org/apereo/cas/util/AsciiArtUtils.java
@@ -56,11 +56,12 @@ public final class AsciiArtUtils {
      */
     public static void printAsciiArt(final PrintStream out, final String asciiArt, final String additional) {
         try {
-            out.println();
-            out.println(ANSI_CYAN);
-            out.println(FigletFont.convertOneLine(asciiArt));
+            out.print(ANSI_CYAN);
             if (StringUtils.isNotBlank(additional)) {
+                out.println(FigletFont.convertOneLine(asciiArt));
                 out.println(additional);
+            } else {
+                out.print(FigletFont.convertOneLine(asciiArt));
             }
             out.println(ANSI_RESET);
         } catch (final Exception e) {
