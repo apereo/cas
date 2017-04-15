401737a25888d31499ab925ed433d8678dcb0562
Misagh Moayyed <mmoayyed@unicon.net>
2017-04-15 14:32:31 +0430

    Allow usernames to be encrypted using service public keys

--- core/cas-server-core-services/src/main/java/org/apereo/cas/services/BaseRegisteredServiceUsernameAttributeProvider.java
+++ core/cas-server-core-services/src/main/java/org/apereo/cas/services/BaseRegisteredServiceUsernameAttributeProvider.java
@@ -1,10 +1,15 @@
 package org.apereo.cas.services;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apereo.cas.authentication.principal.Principal;
 import org.apereo.cas.authentication.principal.Service;
+import org.apereo.cas.util.spring.ApplicationContextProvider;
 import org.apereo.services.persondir.util.CaseCanonicalizationMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
 
 import javax.persistence.PostLoad;
 import java.util.Locale;
@@ -17,6 +22,7 @@ import java.util.Locale;
  */
 public abstract class BaseRegisteredServiceUsernameAttributeProvider implements RegisteredServiceUsernameAttributeProvider {
     private static final long serialVersionUID = -8381275200333399951L;
+    private static final Logger LOGGER = LoggerFactory.getLogger(BaseRegisteredServiceUsernameAttributeProvider.class);
 
     private String canonicalizationMode = CaseCanonicalizationMode.NONE.name();
     private boolean encryptUsername;
@@ -33,13 +39,36 @@ public abstract class BaseRegisteredServiceUsernameAttributeProvider implements 
     public final String resolveUsername(final Principal principal, final Service service, final RegisteredService registeredService) {
         final String username = resolveUsernameInternal(principal, service);
         if (canonicalizationMode == null) {
-            return username;
+            canonicalizationMode = CaseCanonicalizationMode.NONE.name();
         }
+
         final String uid = CaseCanonicalizationMode.valueOf(canonicalizationMode).canonicalize(username.trim(), Locale.getDefault());
-        if (this.encryptUsername && registeredService.getPublicKey()) {
-            return encryptResolvedUsername(principal, service, registeredService);
+        LOGGER.debug("Resolved username for [{}] is [{}]", service.getId(), uid);
+
+        if (!this.encryptUsername) {
+            return uid;
         }
-        return uid;
+        final String encryptedId = encryptResolvedUsername(principal, service, registeredService, uid);
+        if (StringUtils.isBlank(encryptedId)) {
+            throw new IllegalArgumentException("Could not encrypt username " + uid + " for service " + service);
+        }
+        return encryptedId;
+    }
+
+    /**
+     * Encrypt resolved username.
+     *
+     * @param principal         the principal
+     * @param service           the service
+     * @param registeredService the registered service
+     * @param username          the username
+     * @return the encrypted username or null
+     */
+    protected String encryptResolvedUsername(final Principal principal, final Service service,
+                                             final RegisteredService registeredService, final String username) {
+        final ApplicationContext applicationContext = ApplicationContextProvider.getApplicationContext();
+        final RegisteredServiceCipherExecutor cipher = applicationContext.getBean("registeredServiceCipherExecutor", RegisteredServiceCipherExecutor.class);
+        return cipher.encode(username, registeredService);
     }
 
     /**
