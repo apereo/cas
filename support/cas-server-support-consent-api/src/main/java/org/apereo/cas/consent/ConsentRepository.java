7d145cae88239a3bbe39d06be4f5146d9c85cc89
Misagh Moayyed <mmoayyed@unicon.net>
2017-04-22 07:38:06 -0700

    Initial pass at consent APIs

---
+++ support/cas-server-support-consent-api/src/main/java/org/apereo/cas/consent/ConsentRepository.java
@@ -0,0 +1,12 @@
+package org.apereo.cas.consent;
+
+import java.io.Serializable;
+
+/**
+ * This is {@link ConsentRepository}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.1.0
+ */
+public interface ConsentRepository extends Serializable {
+}
