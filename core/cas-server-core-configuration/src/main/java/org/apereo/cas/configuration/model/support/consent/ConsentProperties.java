5216e2d6b742b82228aad8226c940944ffc64776
Misagh Moayyed <mm1844@gmail.com>
2017-04-22 11:45:39 -0700

    Cntd work on consent repositories

--- core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/consent/ConsentProperties.java
+++ core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/consent/ConsentProperties.java
@@ -1,5 +1,7 @@
 package org.apereo.cas.configuration.model.support.consent;
 
+import org.apereo.cas.configuration.model.support.jpa.AbstractJpaProperties;
+
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -11,6 +13,16 @@ import java.util.concurrent.TimeUnit;
 public class ConsentProperties {
     private int reminder = 14;
     private TimeUnit reminderTimeUnit = TimeUnit.DAYS;
+
+    private Jpa jpa = new Jpa();
+
+    public Jpa getJpa() {
+        return jpa;
+    }
+
+    public void setJpa(final Jpa jpa) {
+        this.jpa = jpa;
+    }
 
     public int getReminder() {
         return reminder;
@@ -26,5 +38,8 @@ public class ConsentProperties {
 
     public void setReminderTimeUnit(final TimeUnit reminderTimeUnit) {
         this.reminderTimeUnit = reminderTimeUnit;
+    }
+
+    public static class Jpa extends AbstractJpaProperties {
     }
 }
