25648b5ec0bea6d405359f59f86419828d8cf32f
Misagh Moayyed <mm1844@gmail.com>
2017-04-22 07:38:06 -0700

    Initial pass at consent APIs

---
+++ support/cas-server-support-consent-core/src/main/java/org/apereo/cas/consent/DefaultConsentEngine.java
@@ -0,0 +1,24 @@
+package org.apereo.cas.consent;
+
+import org.apereo.cas.authentication.Authentication;
+import org.apereo.cas.authentication.principal.Service;
+import org.apereo.cas.services.RegisteredService;
+
+import java.util.Map;
+
+/**
+ * This is {@link DefaultConsentEngine}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.1.0
+ */
+public class DefaultConsentEngine implements ConsentEngine {
+    private static final long serialVersionUID = -617809298856160625L;
+
+    @Override
+    public Map<String, Object> getConsentableAttributes(final Authentication authentication,
+                                                        final Service service,
+                                                        final RegisteredService registeredService) {
+        return null;
+    }
+}
