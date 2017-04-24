907ff9846e930031b40b4aebd2a62ca22d7d87d3
Misagh Moayyed <mm1844@gmail.com>
2017-04-24 10:15:41 -0700

    doc updates. added gartner as SP

--- support/cas-server-support-x509-webflow/src/main/java/org/apereo/cas/web/flow/X509WebflowConfigurer.java
+++ support/cas-server-support-x509-webflow/src/main/java/org/apereo/cas/web/flow/X509WebflowConfigurer.java
@@ -1,5 +1,6 @@
 package org.apereo.cas.web.flow;
 
+import org.apereo.cas.configuration.model.core.authentication.PersonDirPrincipalResolverProperties;
 import org.springframework.webflow.definition.registry.FlowDefinitionRegistry;
 import org.springframework.webflow.engine.ActionState;
 import org.springframework.webflow.engine.Flow;
@@ -8,24 +9,25 @@ import org.springframework.webflow.engine.builder.support.FlowBuilderServices;
 /**
  * The {@link X509WebflowConfigurer} is responsible for
  * adjusting the CAS webflow context for x509 integration.
- * 
- * Creates a flow that starts by trying to construct credentials using an X509 
+ * <p>
+ * Creates a flow that starts by trying to construct credentials using an X509
  * certificate found as request attribute with key javax.servlet.request.X509Certificate
- * {@link X509CertificateCredentialsNonInteractiveAction}
+ * {@link X509CertificateCredentialsNonInteractiveAction}.
  * If the check of the certificate is valid, flow goes to sendTicketGrantingTicket.
- * On error or authenticationFailure, the user is sent to the login page. 
- * The authenticationFailure outcome can happen when CAS got a valid certificate but 
- * couldn't find entry for the certificate in an attribute repository and 
+ * On error or authenticationFailure, the user is sent to the login page.
+ * The authenticationFailure outcome can happen when CAS got a valid certificate but
+ * couldn't find entry for the certificate in an attribute repository and
  * falling back to principal from the certificate is turned off via:
- * cas.authn.x509.principal.returnNull=true
- * 
+ * <code>cas.authn.x509.principal.returnNull=true</code> provided by
+ * {@link PersonDirPrincipalResolverProperties#isReturnNull()}.
+ * <p>
  * Credentials are cleared out at the end of the action in case the user
  * is sent to the login page where the X509 credentials object will cause
- * errors (e.g. no username property) 
- * 
- * The X509 action is added to the main login flow by overriding the success
- * outcome of the initializeLoginForm action.
- * 
+ * errors (e.g. no username property)
+ * <p>
+ * The X509 action is added to the main login flow by overriding the @link CasWebflowConstants#TRANSITION_ID_SUCCESS}
+ * outcome of the {@link CasWebflowConstants#STATE_ID_INIT_LOGIN_FORM} action.
+ *
  * @author Misagh Moayyed
  * @since 4.2
  */
