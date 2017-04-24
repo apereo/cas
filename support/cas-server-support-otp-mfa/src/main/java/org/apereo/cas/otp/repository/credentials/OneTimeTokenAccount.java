22eea37b4915b81ed39a560c45e1abfb2f69187d
Misagh Moayyed <mmoayyed@unicon.net>
2017-04-24 09:07:56 -0700

    ensure inheritance rules are correctly setup for OTPs

--- support/cas-server-support-otp-mfa/src/main/java/org/apereo/cas/otp/repository/credentials/OneTimeTokenAccount.java
+++ support/cas-server-support-otp-mfa/src/main/java/org/apereo/cas/otp/repository/credentials/OneTimeTokenAccount.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.apache.commons.lang3.builder.CompareToBuilder;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
@@ -12,6 +15,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -24,18 +29,19 @@ import java.util.List;
  * @since 5.0.0
  */
 @Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
 public class OneTimeTokenAccount implements Serializable, Comparable<OneTimeTokenAccount> {
-    
+
     private static final long serialVersionUID = -8289105320642735252L;
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id = Integer.MAX_VALUE;
 
     @Column(length = 255, updatable = true, insertable = true, nullable = false)
     private String secretKey;
-    
+
     @Column(length = 255, updatable = true, insertable = true, nullable = false)
     private int validationCode;
 
@@ -49,11 +55,11 @@ public class OneTimeTokenAccount implements Serializable, Comparable<OneTimeToke
 
     public OneTimeTokenAccount() {
     }
-    
+
     /**
      * Instantiates a new Google authenticator account.
      *
-     * @param username         the user id
+     * @param username       the user id
      * @param secretKey      the secret key
      * @param validationCode the validation code
      * @param scratchCodes   the scratch codes
@@ -113,5 +119,44 @@ public class OneTimeTokenAccount implements Serializable, Comparable<OneTimeToke
                 .append(this.secretKey, o.getSecretKey())
                 .append(this.username, o.getUsername())
                 .build();
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj == this) {
+            return true;
+        }
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
+        final OneTimeTokenAccount rhs = (OneTimeTokenAccount) obj;
+        return new EqualsBuilder()
+                .append(this.secretKey, rhs.secretKey)
+                .append(this.validationCode, rhs.validationCode)
+                .append(this.scratchCodes, rhs.scratchCodes)
+                .append(this.username, rhs.username)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder()
+                .append(secretKey)
+                .append(validationCode)
+                .append(scratchCodes)
+                .append(username)
+                .toHashCode();
+    }
+
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("id", id)
+                .append("username", username)
+                .toString();
     }
 }
