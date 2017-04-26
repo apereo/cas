4e29a41fd7477d6efc343274acf59e1f405fa1e7
Jeff Sittler <mindblender@gmail.com>
2017-04-26 10:01:05 -0700

    Adding WS Federation to the ServiceType dropdown Added the WS Federation claim choices for the claim dropdown

--- webapp-mgmt/cas-management-webapp/src/main/resources/static/js/app/app-angular.js
+++ webapp-mgmt/cas-management-webapp/src/main/resources/static/js/app/app-angular.js
@@ -649,7 +649,8 @@ if (array.length == 3) {
                     {name: 'OAuth Client', value: 'oauth'},
                     {name: 'OAuth Callback Authorize', value: 'oauth_callback_authz'},
                     {name: 'SAML Client', value: 'saml'},
-                    {name: 'OIDC Client', value: 'oidc'}
+                    {name: 'OIDC Client', value: 'oidc'},
+                    {name: 'WS Federation', value: 'wsfed'}
                 ],
                 oidcScopes: [
                     {name: 'Profile', value: 'profile'},
@@ -707,6 +708,30 @@ if (array.length == 3) {
                   {name: 'NONE', value: 'NONE'},
                   {name: 'UPPER', value: 'UPPER'},
                   {name: 'LOWER', value: 'LOWER'}
+                ],
+                wsfedClaimList: [
+                  {name: "EMAIL_ADDRESS_2005", value: "EMAIL_ADDRESS_2006"},
+                  {name: "EMAIL_ADDRESS", value: "EMAIL_ADDRESS"},
+                  {name: "GIVEN_NAME", value: "GIVEN_NAME"},
+                  {name: "NAME", value: "NAME"},
+                  {name: "USER_PRINCIPAL_NAME_2005", value: "USER_PRINCIPAL_NAME_2006"},
+                  {name: "USER_PRINCIPAL_NAME", value: "USER_PRINCIPAL_NAME"},
+                  {name: "COMMON_NAME", value: "COMMON_NAME"},
+                  {name: "GROUP", value: "GROUP"},
+                  {name: "MS_ROLE", value: "MS_ROLE"},
+                  {name: "ROLE", value: "ROLE"},
+                  {name: "SURNAME", value: "SURNAME"},
+                  {name: "PRIVATE_ID", value: "PRIVATE_ID"},
+                  {name: "NAME_IDENTIFIER", value: "NAME_IDENTIFIER"},
+                  {name: "AUTHENTICATION_METHOD", value: "AUTHENTICATION_METHOD"},
+                  {name: "DENY_ONLY_GROUP_SID", value: "DENY_ONLY_GROUP_SID"},
+                  {name: "DENY_ONLY_PRIMARY_SID", value: "DENY_ONLY_PRIMARY_SID"},
+                  {name: "DENY_ONLY_PRIMARY_GROUP_SID", value: "DENY_ONLY_PRIMARY_GROUP_SID"},
+                  {name: "GROUP_SID", value: "GROUP_SID"},
+                  {name: "PRIMARY_GROUP_SID", value: "PRIMARY_GROUP_SID"},
+                  {name: "PRIMARY_SID", value: "PRIMARY_SID"},
+                  {name: "WINDOWS_ACCOUNT_NAME", value: "WINDOWS_ACCOUNT_NAME"},
+                  {name: "PUID", value: "PUID"}
                 ]
             };
 
