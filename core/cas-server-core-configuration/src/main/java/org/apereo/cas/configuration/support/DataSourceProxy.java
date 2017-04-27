2e2b6485693e2af252cdc924b6a6dd2ae3a8a2e6
Misagh Moayyed <mm1844@gmail.com>
2017-04-27 06:40:34 -0700

    Minor formatting fixes

--- core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/support/DataSourceProxy.java
+++ core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/support/DataSourceProxy.java
@@ -10,12 +10,13 @@ import org.springframework.jdbc.datasource.AbstractDataSource;
 /**
  * This purpose of this class is to fix a class loading issue that occurs
  * in some application servers when using a datasource/pool from the
- * container ClassLoader. By having this class at the app level,
- * proxying that occurs (e.g. RefreshScope annotation) doesn't encounter
+ * container @{@link ClassLoader}. By having this class at the app level,
+ * proxying that occurs (e.g. {@link org.springframework.cloud.context.config.annotation.RefreshScope} annotation)
+ * doesn't encounter
  * problems if another proxying library somewhere else uses the container
  * class loader and encounters other proxying classes in the application. 
  *
- * Extends AbstractDataSource  but it could probably
+ * Extends {@link AbstractDataSource} but it could probably
  * just delegate all methods to the wrapped datasource if anything
  * that AbstractDataSource is doing causes a problem.
  *
@@ -24,7 +25,7 @@ import org.springframework.jdbc.datasource.AbstractDataSource;
  */
 public class DataSourceProxy extends AbstractDataSource {
 
-    private DataSource dataSource;
+    private final DataSource dataSource;
     
     public DataSourceProxy(final DataSource dataSource) {
         super();
