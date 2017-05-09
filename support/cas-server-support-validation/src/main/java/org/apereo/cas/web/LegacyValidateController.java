f336d754ac678b19f7b1beabe2dc137f2d8e96a9
Misagh Moayyed <mm1844@gmail.com>
2017-05-09 11:23:27 -0700

    Fix validation endpoint mapping

--- support/cas-server-support-validation/src/main/java/org/apereo/cas/web/LegacyValidateController.java
+++ support/cas-server-support-validation/src/main/java/org/apereo/cas/web/LegacyValidateController.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletResponse;
  * @author Misagh Moayyed
  * @since 4.2
  */
-@Controller("legacyValidateController")
 public class LegacyValidateController extends AbstractServiceValidateController {
 
     /**
