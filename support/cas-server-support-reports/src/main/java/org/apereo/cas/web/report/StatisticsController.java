2d3e60ea5dfbbc2cdc7fb268563f533c2c45a0bf
Misagh Moayyed <mm1844@gmail.com>
2017-05-09 11:13:24 -0700

    working on authN audit summary

--- support/cas-server-support-reports/src/main/java/org/apereo/cas/web/report/StatisticsController.java
+++ support/cas-server-support-reports/src/main/java/org/apereo/cas/web/report/StatisticsController.java
@@ -9,22 +9,35 @@ import org.apereo.cas.audit.spi.DelegatingAuditTrailManager;
 import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.ticket.ServiceTicket;
 import org.apereo.cas.ticket.Ticket;
+import org.apereo.cas.util.DateTimeUtils;
 import org.apereo.inspektr.audit.AuditActionContext;
+import org.apereo.inspektr.audit.AuditPointRuntimeInfo;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.ServletContextAware;
+import org.springframework.web.context.request.async.WebAsyncTask;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.time.Duration;
+import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
 
 /**
  * @author Scott Battaglia
@@ -43,7 +56,7 @@ public class StatisticsController extends BaseCasMvcEndpoint implements ServletC
     private final HealthCheckRegistry healthCheckRegistry;
     private final CasConfigurationProperties casProperties;
 
-    public StatisticsController(final DelegatingAuditTrailManager auditTrailManager, 
+    public StatisticsController(final DelegatingAuditTrailManager auditTrailManager,
                                 final CentralAuthenticationService centralAuthenticationService,
                                 final MetricRegistry metricsRegistry,
                                 final HealthCheckRegistry healthCheckRegistry,
@@ -106,9 +119,115 @@ public class StatisticsController extends BaseCasMvcEndpoint implements ServletC
      */
     @GetMapping(value = "/getAuthnAudit")
     @ResponseBody
-    public Set<AuditActionContext> getAuthnAudit(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+    public Set<AuditActionContext> getAuthnAudit(final HttpServletRequest request,
+                                                 final HttpServletResponse response) throws Exception {
         ensureEndpointAccessIsAuthorized(request, response);
         return this.auditTrailManager.get();
+    }
+
+    /**
+     * Gets authn audit summary.
+     *
+     * @param request  the request
+     * @param response the response
+     * @param start    the start
+     * @param range    the range
+     * @param scale    the scale
+     * @return the authn audit
+     * @throws Exception the exception
+     */
+    @GetMapping(value = "/getAuthnAudit/summary")
+    @ResponseBody
+    public WebAsyncTask<Collection<List<AuthenticationAuditSummary>>> getAuthnAuditSummary(final HttpServletRequest request,
+                                                                                           final HttpServletResponse response,
+                                                                                           @RequestParam final long start,
+                                                                                           @RequestParam final String range,
+                                                                                           @RequestParam final String scale) throws Exception {
+        ensureEndpointAccessIsAuthorized(request, response);
+
+        final Callable<Collection<List<AuthenticationAuditSummary>>> asyncTask = () -> {
+            //final Set<AuditActionContext> audits = getAuthnAudit(request, response);
+
+            final Set<AuditActionContext> audits = new HashSet<>();
+            audits.add(new AuditActionContext("user", "CAS", "AUTHENTICATION_SUCCESS",
+                    "CAS", new Date(2017, 1, 14), null, null, (AuditPointRuntimeInfo) () -> "Dude"));
+            audits.add(new AuditActionContext("user", "CAS", "AUTHENTICATION_SUCCESS",
+                    "CAS", new Date(2017, 1, 18), null, null, (AuditPointRuntimeInfo) () -> "Dude"));
+            audits.add(new AuditActionContext("user", "CAS", "AUTHENTICATION_SUCCESS",
+                    "CAS", new Date(2017, 1, 28), null, null, (AuditPointRuntimeInfo) () -> "Dude"));
+            audits.add(new AuditActionContext("user", "CAS", "AUTHENTICATION_SUCCESS",
+                    "CAS", new Date(2017, 1, 17), null, null, (AuditPointRuntimeInfo) () -> "Dude"));
+            audits.add(new AuditActionContext("user", "CAS", "AUTHENTICATION_SUCCESS",
+                    "CAS", new Date(2017, 1, 9), null, null, (AuditPointRuntimeInfo) () -> "Dude"));
+            audits.add(new AuditActionContext("user", "CAS", "AUTHENTICATION_SUCCESS",
+                    "CAS", new Date(2017, 1, 15), null, null, (AuditPointRuntimeInfo) () -> "Dude"));
+            audits.add(new AuditActionContext("user", "CAS", "AUTHENTICATION_SUCCESS",
+                    "CAS", new Date(2017, 1, 30), null, null, (AuditPointRuntimeInfo) () -> "Dude"));
+
+
+            final LocalDateTime endDate = LocalDateTime.now().plus(Duration.parse(range));
+            final LocalDateTime startDate = DateTimeUtils.localDateTimeOf(start);
+            final Set<AuditActionContext> authnEvents = audits.stream()
+                    .filter(a -> {
+                        final LocalDateTime actionTime = DateTimeUtils.localDateTimeOf(a.getWhenActionWasPerformed());
+                        return (actionTime.isEqual(startDate) || actionTime.isAfter(startDate))
+                                && (actionTime.isEqual(endDate) || actionTime.isBefore(endDate))
+                                && a.getActionPerformed().matches("AUTHENTICATION_".concat("SUCCESS|FAILED"));
+                    })
+                    .sorted(Comparator.comparing(o -> o.getWhenActionWasPerformed()))
+                    .collect(Collectors.toSet());
+
+            final Duration steps = Duration.parse(scale);
+            final Map<Integer, String> buckets = new LinkedHashMap<>();
+
+            LocalDateTime dt = startDate;
+            Integer index = 0;
+            while (dt != null) {
+                buckets.put(index++, dt.toString());
+                dt = dt.plus(steps);
+                if (dt.isAfter(endDate)) {
+                    dt = null;
+                }
+            }
+
+            final Map<LocalDateTime, List<AuthenticationAuditSummary>> summary = new LinkedHashMap<>();
+            authnEvents.forEach(event -> {
+                for (int i = 0; i < buckets.keySet().size(); i++) {
+                    final LocalDateTime actionTime = DateTimeUtils.localDateTimeOf(event.getWhenActionWasPerformed());
+                    final LocalDateTime bucketDateTime = DateTimeUtils.localDateTimeOf(buckets.get(i));
+                    if (actionTime.isEqual(bucketDateTime) || actionTime.isAfter(bucketDateTime)) {
+                        for (int j = i + 1; j < buckets.keySet().size(); j++) {
+                            final LocalDateTime nextBucketDateTime = DateTimeUtils.localDateTimeOf(buckets.get(j));
+                            if (actionTime.isBefore(nextBucketDateTime)) {
+                                if (summary.containsKey(bucketDateTime)) {
+                                    final List<AuthenticationAuditSummary> values = summary.get(bucketDateTime);
+                                    values.add(new AuthenticationAuditSummary(actionTime.toString()));
+                                } else {
+                                    final List<AuthenticationAuditSummary> values = new ArrayList<>();
+                                    values.add(new AuthenticationAuditSummary(actionTime.toString()));
+                                    summary.put(bucketDateTime, values);
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+            final Collection<List<AuthenticationAuditSummary>> values = summary.values();
+            return values;
+        };
+        return new WebAsyncTask<>(casProperties.getHttpClient().getAsyncTimeout(), asyncTask);
+    }
+
+    private static class AuthenticationAuditSummary {
+        private final String time;
+
+        public AuthenticationAuditSummary(final String time) {
+            this.time = time;
+        }
+
+        public String getTime() {
+            return time;
+        }
     }
 
     /**
