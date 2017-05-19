53811145f753669fb34a406f6bdd9e7cdc593a87
David Rodriguez <david.rodriguez@williamhill.com>
2017-05-19 14:42:58 +0200

    Rename package

--- support/cas-server-support-cassandra-ticket-registry/src/test/java/org/apereo/cas/ticket/registry/CassandraTicketRegistryTests.java
+++ support/cas-server-support-cassandra-ticket-registry/src/test/java/org/apereo/cas/ticket/registry/CassandraTicketRegistryTests.java
@@ -1,4 +1,4 @@
-package org.apereo.cas.dao;
+package org.apereo.cas.ticket.registry;
 
 import org.apereo.cas.serializer.JacksonJsonSerializer;
 import org.apereo.cas.ticket.TicketGrantingTicketImpl;
