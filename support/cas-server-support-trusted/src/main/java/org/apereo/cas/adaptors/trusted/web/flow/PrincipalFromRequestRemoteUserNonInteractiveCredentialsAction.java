d2c26a7e5fb20e828eb2db6f49fc7353924b5fb0
Misagh Moayyed <mm1844@gmail.com>
2017-05-27 18:25:16 -0700

    clean up trusted authN hierarchy

--- support/cas-server-support-trusted/src/main/java/org/apereo/cas/adaptors/trusted/web/flow/PrincipalFromRequestRemoteUserNonInteractiveCredentialsAction.java
+++ support/cas-server-support-trusted/src/main/java/org/apereo/cas/adaptors/trusted/web/flow/PrincipalFromRequestRemoteUserNonInteractiveCredentialsAction.java
@@ -1,17 +1,11 @@
 package org.apereo.cas.adaptors.trusted.web.flow;
 
 import org.apereo.cas.adaptors.trusted.authentication.principal.PrincipalBearingCredential;
-import org.apereo.cas.authentication.Credential;
 import org.apereo.cas.authentication.adaptive.AdaptiveAuthenticationPolicy;
 import org.apereo.cas.authentication.principal.PrincipalFactory;
 import org.apereo.cas.web.flow.AbstractNonInteractiveCredentialsAction;
 import org.apereo.cas.web.flow.resolver.CasDelegatingWebflowEventResolver;
 import org.apereo.cas.web.flow.resolver.CasWebflowEventResolver;
-import org.apereo.cas.web.support.WebUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.util.StringUtils;
-import org.springframework.webflow.execution.RequestContext;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -25,28 +19,17 @@ import javax.servlet.http.HttpServletRequest;
  * @author Scott Battaglia
  * @since 3.0.0
  */
-public class PrincipalFromRequestRemoteUserNonInteractiveCredentialsAction extends AbstractNonInteractiveCredentialsAction {
-    private static final Logger LOGGER = LoggerFactory.getLogger(PrincipalFromRequestRemoteUserNonInteractiveCredentialsAction.class);
-    
-    private final PrincipalFactory principalFactory;
-
-    public PrincipalFromRequestRemoteUserNonInteractiveCredentialsAction(
-            final CasDelegatingWebflowEventResolver initialAuthenticationAttemptWebflowEventResolver,
-            final CasWebflowEventResolver serviceTicketRequestWebflowEventResolver, final AdaptiveAuthenticationPolicy adaptiveAuthenticationPolicy,
-            final PrincipalFactory principalFactory) {
-        super(initialAuthenticationAttemptWebflowEventResolver, serviceTicketRequestWebflowEventResolver, adaptiveAuthenticationPolicy);
-        this.principalFactory = principalFactory;
+public class PrincipalFromRequestRemoteUserNonInteractiveCredentialsAction extends BasePrincipalFromNonInteractiveCredentialsAction {
+    public PrincipalFromRequestRemoteUserNonInteractiveCredentialsAction(final CasDelegatingWebflowEventResolver initialWebflowEventResolver,
+                                                                         final CasWebflowEventResolver serviceTicketRequestWebflowEventResolver,
+                                                                         final AdaptiveAuthenticationPolicy adaptiveAuthenticationPolicy,
+                                                                         final PrincipalFactory principalFactory) {
+        super(initialWebflowEventResolver, serviceTicketRequestWebflowEventResolver,
+                adaptiveAuthenticationPolicy, principalFactory);
     }
 
     @Override
-    protected Credential constructCredentialsFromRequest(final RequestContext context) {
-        final HttpServletRequest request = WebUtils.getHttpServletRequest(context);
-        final String remoteUser = request.getRemoteUser();
-
-        if (StringUtils.hasText(remoteUser)) {
-            LOGGER.debug("Remote User [{}] found in HttpServletRequest", remoteUser);
-            return new PrincipalBearingCredential(this.principalFactory.createPrincipal(remoteUser));
-        }
-        return null;
+    protected String getRemotePrincipalId(final HttpServletRequest request) {
+        return request.getRemoteUser();
     }
 }
