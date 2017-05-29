d70e3aa1a2f3926aa0e9923767b63f60499a1056
Misagh Moayyed <mm1844@gmail.com>
2017-05-29 11:45:54 -0700

    cassandra authN support

---
+++ support/cas-server-support-cassandra-authentication/src/main/java/org/apereo/cas/authentication/CassandraRepository.java
@@ -0,0 +1,20 @@
+package org.apereo.cas.authentication;
+
+import java.util.Map;
+
+/**
+ * This is {@link CassandraRepository}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.2.0
+ */
+public interface CassandraRepository {
+
+    /**
+     * Gets user.
+     *
+     * @param uid the uid
+     * @return the user
+     */
+    Map<String, Object> getUser(String uid);
+}
