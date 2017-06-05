c16881dc2b93c8d89c02d15e53c6a5a767d8c116
yudhi.k.surtan <yudhi.k.surtan@gdn-commerce.com>
2017-06-05 19:32:25 +0700

    fix indentation

--- core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/fortress/FortressProperties.java
+++ core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/fortress/FortressProperties.java
@@ -2,22 +2,23 @@ package org.apereo.cas.configuration.model.support.fortress;
 
 public class FortressProperties {
 
-  private String rbacContextId = "HOME";
-  private String name = "fortressHandler";
-  
-  public String getName() {
-    return name;
-  }
-  public void setName(String name) {
-    this.name = name;
-  }
+    private String rbacContextId = "HOME";
+    private String name = "fortressHandler";
 
-  public String getRbacContextId() {
-    return rbacContextId;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public void setRbacContextId(String rbacContextId) {
-    this.rbacContextId = rbacContextId;
-  }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRbacContextId() {
+        return rbacContextId;
+    }
+
+    public void setRbacContextId(String rbacContextId) {
+        this.rbacContextId = rbacContextId;
+    }
 
 }
--- support/cas-server-support-fortress/src/main/java/org/apereo/cas/adaptors/fortress/config/FortressAuthenticationConfiguration.java
+++ support/cas-server-support-fortress/src/main/java/org/apereo/cas/adaptors/fortress/config/FortressAuthenticationConfiguration.java
@@ -1,8 +1,5 @@
 package org.apereo.cas.adaptors.fortress.config;
 
-import java.util.Collection;
-import java.util.HashSet;
-
 import org.apache.directory.fortress.core.AccessMgr;
 import org.apache.directory.fortress.core.rest.AccessMgrRestImpl;
 import org.apereo.cas.adaptors.fortress.FortressAuthenticationHandler;
@@ -23,51 +20,53 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Collection;
+import java.util.HashSet;
+
 @Configuration("fortressAuthenticationConfiguration")
 @EnableConfigurationProperties(CasConfigurationProperties.class)
 public class FortressAuthenticationConfiguration {
-  private static final Logger LOGGER =
-      LoggerFactory.getLogger(FortressAuthenticationConfiguration.class);
 
-  @Autowired
-  private CasConfigurationProperties casProperties;
+    @Autowired
+    private CasConfigurationProperties casProperties;
 
-  @Autowired
-  @Qualifier("servicesManager")
-  private ServicesManager servicesManager;
+    @Autowired
+    @Qualifier("servicesManager")
+    private ServicesManager servicesManager;
 
-  @ConditionalOnMissingBean(name = "fortressPrincipalFactory")
-  @Bean
-  @RefreshScope
-  public PrincipalFactory fortressPrincipalFactory() {
-    return new DefaultPrincipalFactory();
-  }
-
-  public AccessMgr fortressAccessManager() {
-    AccessMgrRestImpl accessMgrRestImpl = new AccessMgrRestImpl();
-    accessMgrRestImpl.setContextId(casProperties.getFortressProperties().getRbacContextId());
-    return accessMgrRestImpl;
-  }
-
-  @ConditionalOnMissingBean(name = "fortressAuthenticationHandler")
-  @Bean
-  @RefreshScope
-  public Collection<AuthenticationHandler> fortressAuthenticationHandler() {
-    final Collection<AuthenticationHandler> handlers = new HashSet<>();
-    handlers.add(new FortressAuthenticationHandler(casProperties.getFortressProperties().getName(),
-        servicesManager, fortressPrincipalFactory(), null));
-    return handlers;
-  }
-  
-  @Configuration("fortressAuthenticationEventExecutionPlanConfiguration")
-  @EnableConfigurationProperties(CasConfigurationProperties.class)
-  public class FortressAuthenticationEventExecutionPlanConfiguration implements AuthenticationEventExecutionPlanConfigurer {
-    @Override
-    public void configureAuthenticationExecutionPlan(AuthenticationEventExecutionPlan plan) {
-      fortressAuthenticationHandler().forEach(handler -> {
-        plan.registerAuthenticationHandler(handler);
-      });
+    @ConditionalOnMissingBean(name = "fortressPrincipalFactory")
+    @Bean
+    @RefreshScope
+    public PrincipalFactory fortressPrincipalFactory() {
+        return new DefaultPrincipalFactory();
     }
-  }
+
+    public AccessMgr fortressAccessManager() {
+        AccessMgrRestImpl accessMgrRestImpl = new AccessMgrRestImpl();
+        accessMgrRestImpl.setContextId(casProperties.getFortressProperties().getRbacContextId());
+        return accessMgrRestImpl;
+    }
+
+    @ConditionalOnMissingBean(name = "fortressAuthenticationHandler")
+    @Bean
+    @RefreshScope
+    public Collection<AuthenticationHandler> fortressAuthenticationHandler() {
+        final Collection<AuthenticationHandler> handlers = new HashSet<>();
+        handlers.add(new FortressAuthenticationHandler(casProperties.getFortressProperties().getName(), servicesManager,
+                fortressPrincipalFactory(), null));
+        return handlers;
+    }
+
+    @Configuration("fortressAuthenticationEventExecutionPlanConfiguration")
+    @EnableConfigurationProperties(CasConfigurationProperties.class)
+    public class FortressAuthenticationEventExecutionPlanConfiguration
+            implements AuthenticationEventExecutionPlanConfigurer {
+        @Override
+        public void configureAuthenticationExecutionPlan(final AuthenticationEventExecutionPlan plan) {
+            fortressAuthenticationHandler().forEach(handler -> {
+                plan.registerAuthenticationHandler(handler);
+            });
+        }
+    }
 
 }
