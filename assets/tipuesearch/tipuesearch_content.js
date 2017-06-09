f38096625076d7748c34004d3f00555f23149ab0
Misagh Moayyed <mmoayyed@unicon.net>
2017-06-09 14:47:01 -0400

    Update tipuesearch_content.js

--- assets/tipuesearch/tipuesearch_content.js
+++ assets/tipuesearch/tipuesearch_content.js
@@ -84,7 +84,7 @@ var tipuesearch = {"pages": [
     "title": {{ document.title | smartify | strip_html | normalize_whitespace | jsonify }},
     "text": {{ document.content | strip_html | normalize_whitespace | jsonify }},
     "tags": {{ taxonomies | join: " " | normalize_whitespace | jsonify }},
-    "url": '/cas' + {{ document.url | relative_url | jsonify }}
+    "url": {{ document.url | relative_url | jsonify }}
   }{% unless forloop.last %},{% endunless %}
 {% endfor %}
 ]};
