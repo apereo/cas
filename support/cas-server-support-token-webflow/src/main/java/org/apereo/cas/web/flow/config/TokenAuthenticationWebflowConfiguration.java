b163f59b2376a4cceda0640be91bbc895fac6f3e
Misagh Moayyed <mmoayyed@unicon.net>
2017-06-11 13:58:18 -0400

    break down token functionality into 3 distinct modules

--- support/cas-server-support-token-webflow/src/main/java/org/apereo/cas/web/flow/config/TokenAuthenticationWebflowConfiguration.java
+++ support/cas-server-support-token-webflow/src/main/java/org/apereo/cas/web/flow/config/TokenAuthenticationWebflowConfiguration.java
@@ -1,8 +1,13 @@
 package org.apereo.cas.web.flow.config;
 
+import org.apereo.cas.authentication.adaptive.AdaptiveAuthenticationPolicy;
 import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.services.ServicesManager;
 import org.apereo.cas.web.flow.CasWebflowConfigurer;
+import org.apereo.cas.web.flow.TokenAuthenticationAction;
 import org.apereo.cas.web.flow.TokenWebflowConfigurer;
+import org.apereo.cas.web.flow.resolver.CasDelegatingWebflowEventResolver;
+import org.apereo.cas.web.flow.resolver.CasWebflowEventResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -11,6 +16,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.webflow.definition.registry.FlowDefinitionRegistry;
 import org.springframework.webflow.engine.builder.support.FlowBuilderServices;
+import org.springframework.webflow.execution.Action;
 
 /**
  * This is {@link TokenAuthenticationWebflowConfiguration}.
@@ -29,10 +35,33 @@ public class TokenAuthenticationWebflowConfiguration {
     @Autowired
     private FlowBuilderServices flowBuilderServices;
 
+    @Autowired
+    @Qualifier("adaptiveAuthenticationPolicy")
+    private AdaptiveAuthenticationPolicy adaptiveAuthenticationPolicy;
+
+    @Autowired
+    @Qualifier("serviceTicketRequestWebflowEventResolver")
+    private CasWebflowEventResolver serviceTicketRequestWebflowEventResolver;
+
+    @Autowired
+    @Qualifier("initialAuthenticationAttemptWebflowEventResolver")
+    private CasDelegatingWebflowEventResolver initialAuthenticationAttemptWebflowEventResolver;
+
+    @Autowired
+    @Qualifier("servicesManager")
+    private ServicesManager servicesManager;
+
     @ConditionalOnMissingBean(name = "tokenWebflowConfigurer")
     @Bean
     public CasWebflowConfigurer tokenWebflowConfigurer() {
         return new TokenWebflowConfigurer(flowBuilderServices, loginFlowDefinitionRegistry);
     }
 
+
+    @Bean
+    public Action tokenAuthenticationAction() {
+        return new TokenAuthenticationAction(initialAuthenticationAttemptWebflowEventResolver,
+                serviceTicketRequestWebflowEventResolver,
+                adaptiveAuthenticationPolicy, servicesManager);
+    }
 }
