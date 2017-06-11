b7ee75cc56e68d6d97aaeaa526bd7a23607b8c28
Misagh Moayyed <mm1844@gmail.com>
2017-06-10 22:42:07 -0400

    Build support for Swivel flow and UI

---
+++ support/cas-server-support-swivel/src/main/java/org/apereo/cas/adaptors/swivel/web/flow/SwivelAuthenticationWebflowAction.java
@@ -0,0 +1,26 @@
+package org.apereo.cas.adaptors.swivel.web.flow;
+
+import org.apereo.cas.web.flow.resolver.CasWebflowEventResolver;
+import org.springframework.webflow.action.AbstractAction;
+import org.springframework.webflow.execution.Event;
+import org.springframework.webflow.execution.RequestContext;
+
+/**
+ * This is {@link SwivelAuthenticationWebflowAction}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.2.0
+ */
+public class SwivelAuthenticationWebflowAction extends AbstractAction {
+
+    private final CasWebflowEventResolver casWebflowEventResolver;
+
+    public SwivelAuthenticationWebflowAction(final CasWebflowEventResolver casWebflowEventResolver) {
+        this.casWebflowEventResolver = casWebflowEventResolver;
+    }
+
+    @Override
+    protected Event doExecute(final RequestContext requestContext) throws Exception {
+        return this.casWebflowEventResolver.resolveSingle(requestContext);
+    }
+}
