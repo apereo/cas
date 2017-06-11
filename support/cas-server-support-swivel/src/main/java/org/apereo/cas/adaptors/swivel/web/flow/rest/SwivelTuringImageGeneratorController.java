79c8cc74b5931bc97d1c8a9f68ffb7e0df2764bb
Misagh Moayyed <mm1844@gmail.com>
2017-06-11 11:19:46 -0400

    clean up swivel mfa flow

--- support/cas-server-support-swivel/src/main/java/org/apereo/cas/adaptors/swivel/web/flow/rest/SwivelTuringImageGeneratorController.java
+++ support/cas-server-support-swivel/src/main/java/org/apereo/cas/adaptors/swivel/web/flow/rest/SwivelTuringImageGeneratorController.java
@@ -39,14 +39,18 @@ public class SwivelTuringImageGeneratorController {
     public void generate(final HttpServletResponse response, final HttpServletRequest request) throws Exception {
         response.setContentType("image/png");
         final String principal = request.getParameter("principal");
-        if (StringUtils.isNotBlank(principal)) {
-            generateImage(response.getOutputStream(), principal);
+        if (StringUtils.isBlank(principal)) {
+            throw new IllegalArgumentException("No principal is specified in the turing image request");
         }
+        generateImage(response.getOutputStream(), principal);
     }
 
     private void generateImage(final OutputStream stream, final String principal) {
         try {
             final String params = String.format("?username=%s&random=%s", principal, RandomUtils.nextLong(1, Long.MAX_VALUE));
+            if (StringUtils.isBlank(swivel.getSwivelTuringImageUrl())) {
+                throw new IllegalArgumentException("Swivel turing image url cannot be blank and must be specified");
+            }
             final URL url = new URL(swivel.getSwivelTuringImageUrl().concat(params));
             final BufferedImage image = ImageIO.read(url);
             ImageIO.write(image, "png", stream);
