65a75ffdd17ca8ab0464f8bfc2a0c83b7b018942
Misagh Moayyed <mm1844@gmail.com>
2017-06-10 22:19:41 -0400

    Build support for Swivel based on community contrib

--- support/cas-server-support-swivel/src/main/java/org/apereo/cas/config/SwivelConfiguration.java
+++ support/cas-server-support-swivel/src/main/java/org/apereo/cas/config/SwivelConfiguration.java
@@ -1,19 +1,28 @@
 package org.apereo.cas.config;
 
 import org.apereo.cas.CentralAuthenticationService;
+import org.apereo.cas.adaptors.swivel.web.flow.SwivelMultifactorTrustWebflowConfigurer;
+import org.apereo.cas.adaptors.swivel.web.flow.SwivelMultifactorWebflowConfigurer;
 import org.apereo.cas.authentication.AuthenticationServiceSelectionPlan;
 import org.apereo.cas.authentication.AuthenticationSystemSupport;
 import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.services.MultifactorAuthenticationProviderSelector;
 import org.apereo.cas.services.ServicesManager;
 import org.apereo.cas.ticket.registry.TicketRegistrySupport;
+import org.apereo.cas.trusted.authentication.api.MultifactorAuthenticationTrustStorage;
+import org.apereo.cas.web.flow.CasWebflowConfigurer;
 import org.apereo.cas.web.flow.authentication.RankedMultifactorAuthenticationProviderSelector;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.util.CookieGenerator;
+import org.springframework.webflow.config.FlowDefinitionRegistryBuilder;
 import org.springframework.webflow.definition.registry.FlowDefinitionRegistry;
 import org.springframework.webflow.engine.builder.support.FlowBuilderServices;
 
@@ -69,5 +78,34 @@ public class SwivelConfiguration {
     @Qualifier("warnCookieGenerator")
     private CookieGenerator warnCookieGenerator;
 
+    @Bean
+    public FlowDefinitionRegistry swivelAuthenticatorFlowRegistry() {
+        final FlowDefinitionRegistryBuilder builder = new FlowDefinitionRegistryBuilder(this.applicationContext, this.flowBuilderServices);
+        builder.setBasePath("classpath*:/webflow");
+        builder.addFlowLocationPattern("/mfa-swivel/*-webflow.xml");
+        return builder.build();
+    }
 
+    @ConditionalOnMissingBean(name = "swivelMultifactorWebflowConfigurer")
+    @Bean
+    public CasWebflowConfigurer swivelMultifactorWebflowConfigurer() {
+        return new SwivelMultifactorWebflowConfigurer(flowBuilderServices, loginFlowDefinitionRegistry,
+                swivelAuthenticatorFlowRegistry());
+    }
+
+    /**
+     * The swivel multifactor trust configuration.
+     */
+    @ConditionalOnClass(value = MultifactorAuthenticationTrustStorage.class)
+    @ConditionalOnProperty(prefix = "cas.authn.mfa.swivel", name = "trustedDeviceEnabled", havingValue = "true", matchIfMissing = true)
+    @Configuration("swivelMultifactorTrustConfiguration")
+    public class SwivelMultifactorTrustConfiguration {
+
+        @ConditionalOnMissingBean(name = "swivelMultifactorTrustWebflowConfigurer")
+        @Bean
+        public CasWebflowConfigurer swivelMultifactorTrustWebflowConfigurer() {
+            return new SwivelMultifactorTrustWebflowConfigurer(flowBuilderServices, loginFlowDefinitionRegistry,
+                    casProperties.getAuthn().getMfa().getTrusted().isDeviceRegistrationEnabled(), loginFlowDefinitionRegistry);
+        }
+    }
 }
