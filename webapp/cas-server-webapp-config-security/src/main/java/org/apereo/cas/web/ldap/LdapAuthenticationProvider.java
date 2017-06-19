076ca8ca23996bb0b06f6c0c3739944b8e0491d4
David Rodriguez <davidrg131092@gmail.com>
2017-06-19 10:32:16 +0200

    Remove unused import

--- webapp/cas-server-webapp-config-security/src/main/java/org/apereo/cas/web/ldap/LdapAuthenticationProvider.java
+++ webapp/cas-server-webapp-config-security/src/main/java/org/apereo/cas/web/ldap/LdapAuthenticationProvider.java
@@ -23,7 +23,6 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.stream.Collectors;
