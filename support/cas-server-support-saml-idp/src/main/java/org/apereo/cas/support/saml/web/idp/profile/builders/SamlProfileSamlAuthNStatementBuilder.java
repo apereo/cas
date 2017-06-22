c3bc83a40c94974d7c482b04e7a983faec0b7985
Misagh Moayyed <mm1844@gmail.com>
2017-06-22 12:25:15 -0700

    clean up javadoc issues

--- support/cas-server-support-saml-idp/src/main/java/org/apereo/cas/support/saml/web/idp/profile/builders/SamlProfileSamlAuthNStatementBuilder.java
+++ support/cas-server-support-saml-idp/src/main/java/org/apereo/cas/support/saml/web/idp/profile/builders/SamlProfileSamlAuthNStatementBuilder.java
@@ -1,21 +1,24 @@
 package org.apereo.cas.support.saml.web.idp.profile.builders;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.support.saml.OpenSamlConfigBean;
 import org.apereo.cas.support.saml.SamlException;
-import org.apereo.cas.support.saml.SamlIdPUtils;
 import org.apereo.cas.support.saml.services.SamlRegisteredService;
 import org.apereo.cas.support.saml.services.idp.metadata.SamlRegisteredServiceServiceProviderMetadataFacade;
 import org.apereo.cas.support.saml.util.AbstractSaml20ObjectBuilder;
 import org.apereo.cas.util.DateTimeUtils;
+import org.apereo.cas.util.InetAddressUtils;
 import org.jasig.cas.client.validation.Assertion;
 import org.opensaml.saml.saml2.core.AuthnRequest;
 import org.opensaml.saml.saml2.core.AuthnStatement;
 import org.opensaml.saml.saml2.core.SubjectLocality;
+import org.opensaml.saml.saml2.metadata.AssertionConsumerService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.net.InetAddress;
 import java.security.SecureRandom;
 import java.time.ZonedDateTime;
 
@@ -41,14 +44,14 @@ public class SamlProfileSamlAuthNStatementBuilder extends AbstractSaml20ObjectBu
     }
 
     @Override
-    public AuthnStatement build(final AuthnRequest authnRequest, 
+    public AuthnStatement build(final AuthnRequest authnRequest,
                                 final HttpServletRequest request,
                                 final HttpServletResponse response,
-                                final Assertion assertion, 
+                                final Assertion assertion,
                                 final SamlRegisteredService service,
                                 final SamlRegisteredServiceServiceProviderMetadataFacade adaptor,
                                 final String binding) throws SamlException {
-        return buildAuthnStatement(assertion, authnRequest, adaptor, service);
+        return buildAuthnStatement(assertion, authnRequest, adaptor, service, binding);
     }
 
     /**
@@ -58,12 +61,13 @@ public class SamlProfileSamlAuthNStatementBuilder extends AbstractSaml20ObjectBu
      * @param authnRequest the authn request
      * @param adaptor      the adaptor
      * @param service      the service
+     * @param binding      the binding
      * @return constructed authentication statement
      * @throws SamlException the saml exception
      */
     private AuthnStatement buildAuthnStatement(final Assertion assertion, final AuthnRequest authnRequest,
                                                final SamlRegisteredServiceServiceProviderMetadataFacade adaptor,
-                                               final SamlRegisteredService service) throws SamlException {
+                                               final SamlRegisteredService service, final String binding) throws SamlException {
 
         final String authenticationMethod = this.authnContextClassRefBuilder.build(assertion, authnRequest, adaptor, service);
         final String id = '_' + String.valueOf(Math.abs(new SecureRandom().nextLong()));
@@ -73,7 +77,7 @@ public class SamlProfileSamlAuthNStatementBuilder extends AbstractSaml20ObjectBu
             statement.setSessionNotOnOrAfter(
                     DateTimeUtils.dateTimeOf(dt.plusSeconds(casProperties.getAuthn().getSamlIdp().getResponse().getSkewAllowance())));
         }
-        statement.setSubjectLocality(buildSubjectLocality(assertion, authnRequest, adaptor));
+        statement.setSubjectLocality(buildSubjectLocality(assertion, authnRequest, adaptor, binding));
         return statement;
     }
 
@@ -83,14 +87,22 @@ public class SamlProfileSamlAuthNStatementBuilder extends AbstractSaml20ObjectBu
      * @param assertion    the assertion
      * @param authnRequest the authn request
      * @param adaptor      the adaptor
+     * @param binding      the binding
      * @return the subject locality
      * @throws SamlException the saml exception
      */
     protected SubjectLocality buildSubjectLocality(final Assertion assertion, final AuthnRequest authnRequest,
-                                                   final SamlRegisteredServiceServiceProviderMetadataFacade adaptor)
+                                                   final SamlRegisteredServiceServiceProviderMetadataFacade adaptor,
+                                                   final String binding)
             throws SamlException {
         final SubjectLocality subjectLocality = newSamlObject(SubjectLocality.class);
-        subjectLocality.setAddress(SamlIdPUtils.getIssuerFromSamlRequest(authnRequest));
+        final AssertionConsumerService acs = adaptor.getAssertionConsumerService(binding);
+        if (acs != null && StringUtils.isNotBlank(acs.getLocation())) {
+            final String ip = InetAddressUtils.getByName(acs.getLocation());
+            if (StringUtils.isNotBlank(ip)) {
+                subjectLocality.setAddress(ip);
+            }
+        }
         return subjectLocality;
     }
 }
