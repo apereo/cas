dbf12036d0fe2c15e5d41d1e491d4c4b82d618f5
Misagh Moayyed <mmoayyed@unicon.net>
2017-06-26 16:03:52 -0700

    [skip tests]

---
+++ travis/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$TRAVIS_COMMIT_MESSAGE" == "[skip tests]" ]
+then
+  echo -e "Travis CI build indicates that tests should be skipped.\n"
+else
+  sudo ./gradlew checkstyleTest test --stacktrace --parallel --build-cache
+fi
+
+
