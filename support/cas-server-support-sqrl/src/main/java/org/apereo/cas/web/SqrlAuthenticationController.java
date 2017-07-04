db4fe0caf57d00713667fb54c590ac223ed39977
Misagh Moayyed <misagh.moayyed@gmail.com>
2017-07-03 18:26:41 -0700

    [skip tests]

--- support/cas-server-support-sqrl/src/main/java/org/apereo/cas/web/SqrlAuthenticationController.java
+++ support/cas-server-support-sqrl/src/main/java/org/apereo/cas/web/SqrlAuthenticationController.java
@@ -1,15 +1,11 @@
 package org.apereo.cas.web;
 
-import org.jsqrl.model.SqrlAuthResponse;
-import org.jsqrl.model.SqrlClientRequest;
 import org.jsqrl.server.JSqrlServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -33,17 +29,36 @@ public class SqrlAuthenticationController {
     /**
      * Sqrl response entity.
      *
-     * @param request     the request
-     * @param nut         the nut
+     * @param httpRequest the http request
+     * @return the response entity
+     */
+    @PostMapping(path = "/sqrl/authn")
+    public ResponseEntity<String> sqrl(final HttpServletRequest httpRequest) {
+        /*
+        final SqrlAuthResponse sqrlAuthResponse = server.handleClientRequest(request, nut, httpRequest.getRemoteAddr());
+        LOGGER.info("SQRL authentication response [{}] with nut [{}]", sqrlAuthResponse, nut);
+        return new ResponseEntity(sqrlAuthResponse.toEncodedString(), HttpStatus.OK);
+        */
+
+        LOGGER.info("SQRL authentication response");
+        return new ResponseEntity("", HttpStatus.OK);
+    }
+
+    /**
+     * Sqrl authn response entity.
+     *
      * @param httpRequest the http request
      * @return the response entity
      */
     @PostMapping(path = "/sqrl")
-    public ResponseEntity sqrl(@ModelAttribute final SqrlClientRequest request,
-                               @RequestParam("nut") final String nut,
-                               final HttpServletRequest httpRequest) {
+    public ResponseEntity<String> sqrlAuthn(final HttpServletRequest httpRequest) {
+        /*
         final SqrlAuthResponse sqrlAuthResponse = server.handleClientRequest(request, nut, httpRequest.getRemoteAddr());
         LOGGER.info("SQRL authentication response [{}] with nut [{}]", sqrlAuthResponse, nut);
         return new ResponseEntity(sqrlAuthResponse.toEncodedString(), HttpStatus.OK);
+        */
+
+        LOGGER.info("SQRL authentication response 2");
+        return new ResponseEntity("", HttpStatus.OK);
     }
 }
