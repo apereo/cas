58a034f6050cbfd3abef2ae3a93d435433bed578
Misagh Moayyed <misagh.moayyed@gmail.com>
2017-07-05 14:55:42 -0700

    OIDC: scopes/claim release should cross check against supported scopes/claims

--- support/cas-server-support-oidc-core/src/main/java/org/apereo/cas/oidc/claims/OidcEmailScopeAttributeReleasePolicy.java
+++ support/cas-server-support-oidc-core/src/main/java/org/apereo/cas/oidc/claims/OidcEmailScopeAttributeReleasePolicy.java
@@ -3,8 +3,9 @@ package org.apereo.cas.oidc.claims;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.apereo.cas.oidc.OidcConstants;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * This is {@link OidcEmailScopeAttributeReleasePolicy}.
@@ -15,7 +16,7 @@ import java.util.List;
 public class OidcEmailScopeAttributeReleasePolicy extends BaseOidcScopeAttributeReleasePolicy {
     private static final long serialVersionUID = 1532960981124784595L;
 
-    private List<String> allowedAttributes = Arrays.asList("email", "email_verified");
+    private List<String> allowedAttributes = Stream.of("email", "email_verified").collect(Collectors.toList());
 
     public OidcEmailScopeAttributeReleasePolicy() {
         super(OidcConstants.EMAIL);
