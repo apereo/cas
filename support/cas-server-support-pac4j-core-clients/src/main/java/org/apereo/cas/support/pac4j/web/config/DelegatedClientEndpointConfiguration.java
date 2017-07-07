bbcaddaee070731ce227c18a3d1c63b67e1783c5
Ghenadii Batalski <ghenadii.batalski@gmail.com>
2017-07-07 15:06:34 +0200

    support delegation clients redirecting via /delegatedAuthn context path
    See https://github.com/apereo/cas/issues/2711 for details

---
+++ support/cas-server-support-pac4j-core-clients/src/main/java/org/apereo/cas/support/pac4j/web/config/DelegatedClientEndpointConfiguration.java
@@ -0,0 +1,27 @@
+package org.apereo.cas.support.pac4j.web.config;
+
+import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.support.pac4j.web.controllers.DelegatedClientEndpointController;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+
+/**
+ * Endpoint configuration
+ *
+ * @author Ghenadii Batalski
+ * @since 5.2.0
+ */
+@Configuration("delegatedClientEndpointConfiguration")
+@EnableConfigurationProperties(CasConfigurationProperties.class)
+public class DelegatedClientEndpointConfiguration {
+
+    @RefreshScope
+    @Bean
+    @Lazy
+    public DelegatedClientEndpointController creDelegatedClientEndpointController(){
+         return new DelegatedClientEndpointController();
+    }
+}
