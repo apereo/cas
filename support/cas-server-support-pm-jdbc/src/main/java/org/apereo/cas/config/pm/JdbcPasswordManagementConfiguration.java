907d8c5b4aa27ccb03d988c3b4daf8230b22dd86
Misagh Moayyed <misagh.moayyed@gmail.com>
2017-07-06 21:47:54 -0700

    clean up PM functionality per module

---
+++ support/cas-server-support-pm-jdbc/src/main/java/org/apereo/cas/config/pm/JdbcPasswordManagementConfiguration.java
@@ -0,0 +1,44 @@
+package org.apereo.cas.config.pm;
+
+import org.apereo.cas.CipherExecutor;
+import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.configuration.model.support.pm.PasswordManagementProperties;
+import org.apereo.cas.configuration.support.Beans;
+import org.apereo.cas.pm.PasswordManagementService;
+import org.apereo.cas.pm.jdbc.JdbcPasswordManagementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.Serializable;
+
+/**
+ * This is {@link JdbcPasswordManagementConfiguration}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.2.0
+ */
+@Configuration("jdbcPasswordManagementConfiguration")
+@EnableConfigurationProperties(CasConfigurationProperties.class)
+public class JdbcPasswordManagementConfiguration {
+    @Autowired
+    private CasConfigurationProperties casProperties;
+
+    @Autowired
+    @Qualifier("passwordManagementCipherExecutor")
+    private CipherExecutor<Serializable, String> passwordManagementCipherExecutor;
+    
+    @RefreshScope
+    @Bean
+    public PasswordManagementService passwordChangeService() {
+        final PasswordManagementProperties pm = casProperties.getAuthn().getPm();
+        return new JdbcPasswordManagementService(passwordManagementCipherExecutor,
+                casProperties.getServer().getPrefix(),
+                casProperties.getAuthn().getPm(),
+                Beans.newDataSource(casProperties.getAuthn().getPm().getJdbc()));
+    }
+}
--- support/cas-server-support-pm/src/main/java/org/apereo/cas/pm/web/flow/actions/VerifySecurityQuestionsAction.java
+++ support/cas-server-support-pm/src/main/java/org/apereo/cas/pm/web/flow/actions/VerifySecurityQuestionsAction.java
@@ -1,4 +1,4 @@
-package org.apereo.cas.pm.web.flow;
+package org.apereo.cas.pm.web.flow.actions;
 
 import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.configuration.model.support.pm.PasswordManagementProperties;
