ba74f60d375f2ef6bdce7c3af2e0d5698a627be0
Misagh Moayyed <misagh.moayyed@gmail.com>
2017-07-09 22:29:36 -0700

    Working on rest consent support and tests

--- support/cas-server-support-consent-rest/src/main/java/org/apereo/cas/consent/RestConsentRepository.java
+++ support/cas-server-support-consent-rest/src/main/java/org/apereo/cas/consent/RestConsentRepository.java
@@ -5,6 +5,15 @@ import org.apereo.cas.authentication.principal.Service;
 import org.apereo.cas.services.RegisteredService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 /**
  * This is {@link RestConsentRepository}.
@@ -14,8 +23,15 @@ import org.slf4j.LoggerFactory;
  */
 public class RestConsentRepository implements ConsentRepository {
     private static final long serialVersionUID = 6583408862493270206L;
-
     private static final Logger LOGGER = LoggerFactory.getLogger(RestConsentRepository.class);
+
+    private final RestTemplate restTemplate;
+    private final String endpoint;
+
+    public RestConsentRepository(final RestTemplate restTemplate, final String endpoint) {
+        this.restTemplate = restTemplate;
+        this.endpoint = endpoint;
+    }
 
     @Override
     public String toString() {
@@ -27,16 +43,30 @@ public class RestConsentRepository implements ConsentRepository {
                                                final RegisteredService registeredService,
                                                final Authentication authentication) {
         try {
-            return null;
+            final HttpHeaders headers = new HttpHeaders();
+            headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+            headers.put("service", Arrays.asList(service.getId()));
+            headers.put("principal", Arrays.asList(authentication.getPrincipal().getId()));
+
+            final HttpEntity<String> entity = new HttpEntity<>(headers);
+            final ResponseEntity<ConsentDecision> result = restTemplate.exchange(this.endpoint, HttpMethod.GET, entity, ConsentDecision.class);
+            if (result.getStatusCodeValue() == HttpStatus.OK.value()) {
+                return result.getBody();
+            }
         } catch (final Exception e) {
-            return null;
+            LOGGER.error(e.getMessage(), e);
         }
+        return null;
     }
 
     @Override
     public boolean storeConsentDecision(final ConsentDecision decision) {
         try {
-            return true;
+            final HttpHeaders headers = new HttpHeaders();
+            headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+            final HttpEntity<ConsentDecision> entity = new HttpEntity<>(decision, headers);
+            final ResponseEntity<ConsentDecision> result = restTemplate.exchange(this.endpoint, HttpMethod.POST, entity, ConsentDecision.class);
+            return result.getStatusCodeValue() == HttpStatus.OK.value();
         } catch (final Exception e) {
             LOGGER.error(e.getMessage(), e);
         }
