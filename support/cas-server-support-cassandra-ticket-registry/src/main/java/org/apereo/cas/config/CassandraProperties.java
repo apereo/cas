3f2fd12b6702daa3600c5d1039b6f5d780e5c43a
David Rodriguez <david.rodriguez@williamhill.com>
2017-07-11 08:43:57 +0200

    Delete table names

--- support/cas-server-support-cassandra-ticket-registry/src/main/java/org/apereo/cas/config/CassandraProperties.java
+++ support/cas-server-support-cassandra-ticket-registry/src/main/java/org/apereo/cas/config/CassandraProperties.java
@@ -14,10 +14,6 @@ public class CassandraProperties {
     private String contactPoints;
     private String username;
     private String password;
-    private String tgtTable;
-    private String stTable;
-    private String expiryTable;
-    private String lastRunTable;
 
     public String getContactPoints() {
         return contactPoints;
@@ -41,38 +37,6 @@ public class CassandraProperties {
 
     public void setPassword(final String password) {
         this.password = password;
-    }
-
-    public String getTgtTable() {
-        return tgtTable;
-    }
-
-    public void setTgtTable(final String tgtTable) {
-        this.tgtTable = tgtTable;
-    }
-
-    public String getStTable() {
-        return stTable;
-    }
-
-    public void setStTable(final String stTable) {
-        this.stTable = stTable;
-    }
-
-    public String getExpiryTable() {
-        return expiryTable;
-    }
-
-    public void setExpiryTable(final String expiryTable) {
-        this.expiryTable = expiryTable;
-    }
-
-    public String getLastRunTable() {
-        return lastRunTable;
-    }
-
-    public void setLastRunTable(final String lastRunTable) {
-        this.lastRunTable = lastRunTable;
     }
 }
 
