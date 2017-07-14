c3d1560fa0491a42fa1cdc3ab82fa7f24f402bc8
Misagh Moayyed <misagh.moayyed@gmail.com>
2017-07-14 10:25:46 -0700

    Clean up scripting utilities via groovy. add support for dyna account status checking

---
+++ support/cas-server-support-pm-webflow/src/main/java/org/apereo/cas/pm/web/flow/actions/acct/BaseAccountStateCheckAction.java
@@ -0,0 +1,18 @@
+package org.apereo.cas.pm.web.flow.actions.acct;
+
+import org.springframework.webflow.action.AbstractAction;
+import org.springframework.webflow.execution.Event;
+import org.springframework.webflow.execution.RequestContext;
+
+/**
+ * This is {@link BaseAccountStateCheckAction}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.2.0
+ */
+public class BaseAccountStateCheckAction extends AbstractAction {
+    @Override
+    protected Event doExecute(final RequestContext requestContext) throws Exception {
+        return null;
+    }
+}
