acf182625b5ca4eeaf75ff88a77c704277d9271a
Misagh Moayyed <misagh.moayyed@gmail.com>
2017-07-17 13:56:52 -0700

    baseline modules for having CAS services stream over a queue or cache

--- support/cas-server-support-service-registry-stream/src/main/java/org/apereo/cas/config/CasServicesStreamingConfiguration.java
+++ support/cas-server-support-service-registry-stream/src/main/java/org/apereo/cas/config/CasServicesStreamingConfiguration.java
@@ -1,7 +1,12 @@
 package org.apereo.cas.config;
 
 import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.services.CasServicesRegistryStreamingEventListener;
+import org.apereo.cas.services.publisher.CasRegisteredServiceNoOpStreamPublisher;
+import org.apereo.cas.services.publisher.CasRegisteredServiceStreamPublisher;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -13,4 +18,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration("casServicesStreamingConfiguration")
 @EnableConfigurationProperties(CasConfigurationProperties.class)
 public class CasServicesStreamingConfiguration {
+
+    @Bean
+    public CasServicesRegistryStreamingEventListener casServicesRegistryStreamingEventListener() {
+        return new CasServicesRegistryStreamingEventListener(casRegisteredServiceStreamPublisher());
+    }
+
+    @ConditionalOnMissingBean(name = "casRegisteredServiceStreamPublisher")
+    @Bean
+    public CasRegisteredServiceStreamPublisher casRegisteredServiceStreamPublisher() {
+        return new CasRegisteredServiceNoOpStreamPublisher();
+    }
 }
