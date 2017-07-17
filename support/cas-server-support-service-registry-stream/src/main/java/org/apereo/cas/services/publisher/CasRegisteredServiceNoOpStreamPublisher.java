843c75e39f7aaba49a10cf1f11c14f84d6571b62
Misagh Moayyed <misagh.moayyed@gmail.com>
2017-07-17 13:56:52 -0700

    baseline modules for having CAS services stream over a queue or cache

---
+++ support/cas-server-support-service-registry-stream/src/main/java/org/apereo/cas/services/publisher/CasRegisteredServiceNoOpStreamPublisher.java
@@ -0,0 +1,16 @@
+package org.apereo.cas.services.publisher;
+
+import org.apereo.cas.services.RegisteredService;
+import org.springframework.context.ApplicationEvent;
+
+/**
+ * This is {@link CasRegisteredServiceNoOpStreamPublisher}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.2.0
+ */
+public class CasRegisteredServiceNoOpStreamPublisher implements CasRegisteredServiceStreamPublisher {
+    @Override
+    public void publish(final RegisteredService service, final ApplicationEvent event) {
+    }
+}
