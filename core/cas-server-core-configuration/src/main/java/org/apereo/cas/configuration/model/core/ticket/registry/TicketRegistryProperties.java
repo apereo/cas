4b13357258639bb4fd209a2072631d0a0fb7fecd
Misagh Moayyed <mmoayyed@unicon.net>
2017-07-18 13:01:32 -0700

    doc updates

--- core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/core/ticket/registry/TicketRegistryProperties.java
+++ core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/core/ticket/registry/TicketRegistryProperties.java
@@ -22,37 +22,73 @@ import org.springframework.boot.context.properties.NestedConfigurationProperty;
  */
 public class TicketRegistryProperties {
 
+    /**
+     * DyanmoDb registry settings.
+     */
     @NestedConfigurationProperty
     private DynamoDbTicketRegistryProperties dynamoDb = new DynamoDbTicketRegistryProperties();
-    
+
+    /**
+     * Infinispan registry settings.
+     */
     @NestedConfigurationProperty
     private InfinispanProperties infinispan = new InfinispanProperties();
 
+    /**
+     * Couchbase registry settings.
+     */
     @NestedConfigurationProperty
     private CouchbaseTicketRegistryProperties couchbase = new CouchbaseTicketRegistryProperties();
 
+    /**
+     * MongoDb registry settings.
+     */
     @NestedConfigurationProperty
     private MongoTicketRegistryProperties mongo = new MongoTicketRegistryProperties();
 
+    /**
+     * Ehcache registry settings.
+     */
     @NestedConfigurationProperty
     private EhcacheProperties ehcache = new EhcacheProperties();
 
+    /**
+     * Hazelcast registry settings.
+     */
     @NestedConfigurationProperty
     private HazelcastProperties hazelcast = new HazelcastProperties();
 
+    /**
+     * Apache Ignite registry settings.
+     */
     @NestedConfigurationProperty
     private IgniteProperties ignite = new IgniteProperties();
 
+    /**
+     * JPA registry settings.
+     */
     @NestedConfigurationProperty
     private JpaTicketRegistryProperties jpa = new JpaTicketRegistryProperties();
 
+    /**
+     * Memcached registry settings.
+     */
     @NestedConfigurationProperty
     private MemcachedTicketRegistryProperties memcached = new MemcachedTicketRegistryProperties();
 
+    /**
+     * Redis registry settings.
+     */
     @NestedConfigurationProperty
     private RedisTicketRegistryProperties redis = new RedisTicketRegistryProperties();
 
+    /**
+     * Settings relevant for the default in-memory ticket registry.
+     */
     private InMemory inMemory = new InMemory();
+    /**
+     * Ticket registry cleaner settings.
+     */
     private Cleaner cleaner = new Cleaner();
 
     public MongoTicketRegistryProperties getMongo() {
@@ -146,16 +182,31 @@ public class TicketRegistryProperties {
     public DynamoDbTicketRegistryProperties getDynamoDb() {
         return dynamoDb;
     }
-
+                                        
     public void setDynamoDb(final DynamoDbTicketRegistryProperties dynamoDb) {
         this.dynamoDb = dynamoDb;
     }
 
     public static class InMemory {
+        /**
+         * The initial capacity of the underlying memory store.
+         * The implementation performs internal sizing to accommodate this many elements.
+         */
         private int initialCapacity = 1000;
+        /**
+         *  The load factor threshold, used to control resizing.
+         *  Resizing may be performed when the average number of elements per bin exceeds this threshold.
+         */
         private int loadFactor = 1;
+        /**
+         * The estimated number of concurrently updating threads.
+         * The implementation performs internal sizing to try to accommodate this many threads.
+         */
         private int concurrency = 20;
 
+        /**
+         * Crypto settings for the registry.
+         */
         @NestedConfigurationProperty
         private CryptographyProperties crypto = new CryptographyProperties();
 
@@ -193,8 +244,17 @@ public class TicketRegistryProperties {
     }
 
     public static class Cleaner {
+        /**
+         * Whether the ticket registry cleaner should be enabled.
+         */
         private boolean enabled = true;
+        /**
+         * Initial delay before the cleaner background job is scheduled to run.
+         */
         private String startDelay = "PT10S";
+        /**
+         * The periodic internal at which the cleaner will wake up to resume.
+         */
         private String repeatInterval = "PT1M";
 
         private String appId = "cas-ticket-registry-cleaner";
