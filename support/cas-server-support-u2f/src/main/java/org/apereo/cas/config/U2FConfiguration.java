08b4a56a2b9733185693fb70ae08ad48c9360fe1
Misagh Moayyed <mm1844@gmail.com>
2017-07-19 09:25:55 -0700

    doc updates

--- support/cas-server-support-u2f/src/main/java/org/apereo/cas/config/U2FConfiguration.java
+++ support/cas-server-support-u2f/src/main/java/org/apereo/cas/config/U2FConfiguration.java
@@ -19,7 +19,6 @@ import org.apereo.cas.authentication.AuthenticationServiceSelectionPlan;
 import org.apereo.cas.authentication.AuthenticationSystemSupport;
 import org.apereo.cas.authentication.PseudoPlatformTransactionManager;
 import org.apereo.cas.configuration.CasConfigurationProperties;
-import org.apereo.cas.configuration.model.support.mfa.MultifactorAuthenticationProperties;
 import org.apereo.cas.configuration.model.support.mfa.U2FProperties;
 import org.apereo.cas.services.MultifactorAuthenticationProviderSelector;
 import org.apereo.cas.services.ServicesManager;
