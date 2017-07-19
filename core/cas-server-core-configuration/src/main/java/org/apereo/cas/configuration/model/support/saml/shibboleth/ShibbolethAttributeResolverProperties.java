220c36484f55c6d400ba01e940526472618331e8
Misagh Moayyed <mmoayyed@unicon.net>
2017-07-19 14:18:38 -0700

    doc updates

--- core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/saml/shibboleth/ShibbolethAttributeResolverProperties.java
+++ core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/saml/shibboleth/ShibbolethAttributeResolverProperties.java
@@ -13,7 +13,12 @@ import java.util.List;
  * @since 5.0.0
  */
 public class ShibbolethAttributeResolverProperties {
-    
+
+    /**
+     * List of Shibboleth's attribute resolver XMLM resources to parse and load.
+     * Each resource can either be found on the file system, as a classpath entry
+     * or via a URL if needed.
+     */
     private List<Resource> resources = Arrays.asList(new ClassPathResource("attribute-resolver.xml"));
 
     public List<Resource> getResources() {
