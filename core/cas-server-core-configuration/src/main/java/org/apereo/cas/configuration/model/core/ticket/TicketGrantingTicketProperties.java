df8bc93ebac5a69d94db014b00c7e2fa5b1109a9
Dmitriy Kopylenko <dima767@gmail.com>
2017-07-20 11:24:29 -0400

    Configuration model documentation. wip...

--- core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/core/ticket/TicketGrantingTicketProperties.java
+++ core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/core/ticket/TicketGrantingTicketProperties.java
@@ -6,16 +6,46 @@ package org.apereo.cas.configuration.model.core.ticket;
  * @author Misagh Moayyed
  * @since 5.0.0
  */
-
 public class TicketGrantingTicketProperties {
+
+    /**
+     * Maximum length of TGTs.
+     */
     private int maxLength = 50;
+
+    /**
+     * Maximum time in seconds TGTs would be live in CAS server.
+     */
     private int maxTimeToLiveInSeconds = 28_800;
+
+    /**
+     * Time in seconds after which TGTs would be destroyed after a period of inactivity.
+     */
     private int timeToKillInSeconds = 7_200;
+
+    /**
+     * Flag to control whether to track most recent SSO sessions.
+     */
     private boolean onlyTrackMostRecentSession = true;
 
+    /**
+     * Hard timeout for TGTs.
+     */
     private HardTimeout hardTimeout = new HardTimeout();
+
+    /**
+     * Throttled timeout for TGTs.
+     */
     private ThrottledTimeout throttledTimeout = new ThrottledTimeout();
+
+    /**
+     * Timeout for TGTs.
+     */
     private Timeout timeout = new Timeout();
+
+    /**
+     * Remember me for TGTs.
+     */
     private RememberMe rememberMe = new RememberMe();
 
     public RememberMe getRememberMe() {
@@ -95,6 +125,10 @@ public class TicketGrantingTicketProperties {
     }
 
     public static class Timeout {
+
+        /**
+         * Maximum time in seconds. for TGTs to be live in CAS server.
+         */
         private int maxTimeToLiveInSeconds;
 
         public int getMaxTimeToLiveInSeconds() {
@@ -128,7 +162,15 @@ public class TicketGrantingTicketProperties {
     }
     
     public static class RememberMe {
+
+        /**
+         * Flag to indicate whether remember-me facility is enabled.
+         */
         private boolean enabled;
+
+        /**
+         * Time in seconds after which remember-me enabled SSO session will be destroyed.
+         */
         private long timeToKillInSeconds = 1_209_600;
 
         public boolean isEnabled() {
