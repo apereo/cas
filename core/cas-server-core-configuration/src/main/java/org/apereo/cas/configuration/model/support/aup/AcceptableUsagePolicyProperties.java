28336c84825916441683410c7eead0e9c5e900f2
Misagh Moayyed <mm1844@gmail.com>
2017-07-20 16:30:51 -0700

    doc updates

--- core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/aup/AcceptableUsagePolicyProperties.java
+++ core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/aup/AcceptableUsagePolicyProperties.java
@@ -10,8 +10,15 @@ import org.apereo.cas.configuration.model.support.ldap.AbstractLdapProperties;
  */
 
 public class AcceptableUsagePolicyProperties {
+    /**
+     * Control AUP via LDAP.
+     */
     private Ldap ldap = new Ldap();
 
+    /**
+     * AUP attribute to choose in order to determine whether policy
+     * has been accepted or not.
+     */
     private String aupAttributeName = "aupAccepted";
 
     public String getAupAttributeName() {
@@ -32,7 +39,14 @@ public class AcceptableUsagePolicyProperties {
 
     public static class Ldap extends AbstractLdapProperties {
         private static final long serialVersionUID = -7991011278378393382L;
+        /**
+         * Base DN to start the search for user accounts.
+         */
         private String baseDn;
+        /**
+         * Search filter to use.
+         * Syntax is <code>cn={user}</code> or <code>cn={0}</code>
+         */
         private String userFilter;
 
         public String getBaseDn() {
--- core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/ldap/AbstractLdapAuthenticationProperties.java
+++ core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/ldap/AbstractLdapAuthenticationProperties.java
@@ -75,15 +75,62 @@ public abstract class AbstractLdapAuthenticationProperties extends AbstractLdapP
         ANONYMOUS
     }
 
+    /**
+     * The authentication type.
+     * <ul>
+     * <li>Active Directory - Users authenticate with sAMAccountName. </li>
+     * <li>Authenticated Search - Manager bind/search</li>
+     * <li>Anonymous Search</li>
+     * <li>Direct Bind: Direct Bind - Compute user DN from format string and perform simple bind.
+     * This is relevant when no search is required to compute the DN needed for a bind operation.
+     * Use cases for this type are:
+     * 1) All users are under a single branch in the directory, <code>e.g. ou=Users,dc=example,dc=org.</code>
+     * 2) The username provided on the CAS login form is part of the DN, e.g.
+     * <code>uid=%s,ou=Users,dc=example,dc=org</code>.</li>
+     * </ul>
+     */
     private AuthenticationTypes type;
+
+    /**
+     * If principalAttributePassword is empty then a user simple bind is done to validate credentials
+     * otherwise the given attribute is compared with the given principalAttributePassword
+     * using the SHA encrypted value of it.
+     * <p>
+     * For the anonymous authentication type,
+     * if principalAttributePassword is empty then a user simple bind is done to validate credentials
+     * otherwise the given attribute is compared with the given principalAttributePassword
+     * using the SHA encrypted value of it.
+     * </p>
+     */
     private String principalAttributePassword;
+    /**
+     * Specify the dn format accepted by the AD authenticator, etc.
+     * Example format might be <code>uid=%s,ou=people,dc=example,dc=org</code>.
+     */
     private String dnFormat;
+    /**
+     * Whether specific search entry resolvers need to be set
+     * on the authenticator, or the default should be used.
+     */
     private boolean enhanceWithEntryResolver = true;
 
+    /**
+     * Whether subtree searching is allowed.
+     */
     private boolean subtreeSearch = true;
+    /**
+     * Base DN to use.
+     */
     private String baseDn;
+    /**
+     * User filter to use for searching.
+     * Syntax is <code>cn={user}</code> or <code>cn={0}</code>.
+     */
     private String userFilter;
 
+    /**
+     * Search entry to define on the authenticator.
+     */
     private List<SearchEntryHandlers> searchEntryHandlers = new ArrayList<>();
 
     public List<SearchEntryHandlers> getSearchEntryHandlers() {
@@ -101,7 +148,6 @@ public abstract class AbstractLdapAuthenticationProperties extends AbstractLdapP
     public void setType(final AuthenticationTypes type) {
         this.type = type;
     }
-
 
     public boolean isSubtreeSearch() {
         return subtreeSearch;
