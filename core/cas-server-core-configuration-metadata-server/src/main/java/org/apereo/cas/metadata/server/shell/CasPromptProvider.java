79defcf9071f4237ee478e36526fdea4980cf1b1
Misagh Moayyed <mm1844@gmail.com>
2017-07-28 18:35:34 +0430

    clean up checkstyle issues

--- core/cas-server-core-configuration-metadata-server/src/main/java/org/apereo/cas/metadata/server/shell/CasPromptProvider.java
+++ core/cas-server-core-configuration-metadata-server/src/main/java/org/apereo/cas/metadata/server/shell/CasPromptProvider.java
@@ -3,7 +3,6 @@ package org.apereo.cas.metadata.server.shell;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.shell.plugin.support.DefaultPromptProvider;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 /**
