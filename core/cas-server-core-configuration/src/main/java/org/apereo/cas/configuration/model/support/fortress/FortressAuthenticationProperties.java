a1ccd811f75830cbeabf22e490e9f165293e7102
Misagh Moayyed <mmoayyed@unicon.net>
2017-07-30 11:50:49 +0430

    clean up fortress integration along with docs

--- core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/fortress/FortressAuthenticationProperties.java
+++ core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/fortress/FortressAuthenticationProperties.java
@@ -1,14 +1,21 @@
 package org.apereo.cas.configuration.model.support.fortress;
 
+import java.io.Serializable;
+
 /**
  * This is {@link FortressAuthenticationProperties}.
  *
  * @author Yudhi Karunia Surtan
- * @since 5.0.0
+ * @since 5.2.0
  */
+public class FortressAuthenticationProperties implements Serializable {
 
-public class FortressAuthenticationProperties {
-
+    private static final long serialVersionUID = 9068259944327425315L;
+    /**
+     * Use this setting to set the tenant id onto function call into Fortress which allows segregation of data by customer.
+     * The context is used for multi-tenancy to isolate data sets within a particular sub-tree within DIT.
+     * Setting contextId into this object will render this class' implementer thread unsafe.
+     */
     private String rbaccontext = "HOME";
 
     public String getRbaccontext() {
