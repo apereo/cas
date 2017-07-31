ba2aa9d7d96f9c13fb17ad53a0b727cbfe71a251
Misagh Moayyed <mm1844@gmail.com>
2017-08-01 01:30:09 +0430

    Allow service definitions to be streamed over a CAS cluster via hazelcast

--- support/cas-server-support-service-registry-stream-hazelcast/src/main/java/org/apereo/cas/services/publisher/CasRegisteredServiceHazelcastStreamPublisher.java
+++ support/cas-server-support-service-registry-stream-hazelcast/src/main/java/org/apereo/cas/services/publisher/CasRegisteredServiceHazelcastStreamPublisher.java
@@ -3,12 +3,9 @@ package org.apereo.cas.services.publisher;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
 import org.apereo.cas.services.RegisteredService;
-import org.apereo.cas.support.events.service.BaseCasRegisteredServiceEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationEvent;
-
-import java.time.LocalDateTime;
 
 /**
  * This is {@link CasRegisteredServiceHazelcastStreamPublisher}.
@@ -16,30 +13,22 @@ import java.time.LocalDateTime;
  * @author Misagh Moayyed
  * @since 5.2.0
  */
-public class CasRegisteredServiceHazelcastStreamPublisher implements CasRegisteredServiceStreamPublisher {
+public class CasRegisteredServiceHazelcastStreamPublisher extends BaseCasRegisteredServiceStreamPublisher {
     private static final Logger LOGGER = LoggerFactory.getLogger(CasRegisteredServiceHazelcastStreamPublisher.class);
     
     private final HazelcastInstance instance;
     private final String mapName;
-    private final PublisherIdentifier publisherId;
 
     public CasRegisteredServiceHazelcastStreamPublisher(final HazelcastInstance instance,
                                                         final PublisherIdentifier publisherId) {
+        super(publisherId);
         this.instance = instance;
-        mapName = instance.getConfig().getMapConfigs().keySet().iterator().next();
-        this.publisherId = publisherId;
+        this.mapName = instance.getConfig().getMapConfigs().keySet().iterator().next();
     }
 
     @Override
-    public void publish(final RegisteredService service, final ApplicationEvent event) {
-        if (!BaseCasRegisteredServiceEvent.class.isAssignableFrom(event.getClass())) {
-            return;   
-        }
-        
+    protected void publishInternal(final RegisteredService service, final ApplicationEvent event) {
         final IMap<String, RegisteredServicesQueuedEvent> inst = instance.getMap(mapName);
-        LOGGER.debug("Publishing service definition [{}] into Hazelcast queue [{}] for event [{}] with publisher [{}]", 
-               service.getName(), mapName, event.getClass().getSimpleName(), this.publisherId);
-        inst.set(service.getName(), new RegisteredServicesQueuedEvent(LocalDateTime.now().toString(), event, service, this.publisherId));
+        inst.set(service.getName(), getEventToPublish(service, event));
     }
-
 }
