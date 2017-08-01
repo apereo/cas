eec8167e6fead301353762b9331563f7195e902f
Misagh Moayyed <mm1844@gmail.com>
2017-08-01 15:58:40 +0430

    Allow successful/failed authn attempts into the stats page as a chart (#2817)
    * Explaining how to use Unicode property values
    
    * Update Configuring-Monitoring-Administration.md
    
    * [skip ci]
    
    * Update Configuring-Monitoring-Administration.md
    
    * Update circle.yml
    
    * added graph
    
    * adding additional components
    
    * removed moment
    
    * webpack config changes to try and improve bundle size.  Also implemented compression on the embedded servers
    
    * adding webpack to the build process
    
    * Removed the dasboard bundle reference since it isn't completed yet.
    
    * Removed react container
    
    * code cleanup
    
    * changed element id from authn-graph to statistis-app
    
    * changed ref id from authn-graph to statistis-app
    
    * changed component name
    
    * removed Dashboard.js and DashboardCircle.js

---
+++ webapp/resources/static/js/StatisticsApp.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
+import AuthnAttemptsGraph from './components/authnaudit/AuthnAttemptsGraph'
+const {string} = React.PropTypes
+
+class StatisticsApp extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      start: '',
+      graphData: []
+    }
+  }
+
+  componentDidMount () {
+    let d = new Date()
+    const startTime = d.setHours(d.getHours() - 2)
+    axios.get(`/cas/status/stats/getAuthnAudit/summary?start=${startTime}&range=${this.props.range}&scale=${this.props.scale}`)
+    .then(res => {
+      res.data.forEach(function (value) {
+        let newDate = new Date(value.time)
+        value.time = (newDate.getHours() - (newDate.getHours() >= 12 ? 12 : 0)) + ':' + newDate.getMinutes()
+      })
+      const graphData = res.data
+      this.setState({graphData})
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <h3>Authentication Attempts</h3>
+        <AuthnAttemptsGraph graphData={this.state.graphData} />
+      </div>
+    )
+  }
+}
+
+StatisticsApp.propTypes = {
+  scale: string,
+  range: string
+}
+
+ReactDOM.render(
+  <StatisticsApp range='PT03H' scale='PT05M' />,
+  document.getElementById('statistics-app')
+)
