10232a1d9c624d5c747eb12f05c72f004d511e8c
Misagh Moayyed <mmoayyed@unicon.net>
2017-08-01 13:41:45 +0430

    replace guava caching with that of caffein so as to allow per-item eviction policies and gain better performance.

--- core/cas-server-core-authentication/src/main/java/org/apereo/cas/authentication/principal/cache/CachingPrincipalAttributesRepository.java
+++ core/cas-server-core-authentication/src/main/java/org/apereo/cas/authentication/principal/cache/CachingPrincipalAttributesRepository.java
@@ -1,13 +1,14 @@
 package org.apereo.cas.authentication.principal.cache;
 
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.CacheLoader;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.apereo.cas.authentication.principal.Principal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.concurrent.TimeUnit;
@@ -15,6 +16,7 @@ import java.util.concurrent.TimeUnit;
 /**
  * Wrapper around an attribute repository where attributes cached for a configurable period
  * based on google guava's caching library.
+ *
  * @author Misagh Moayyed
  * @since 4.2
  */
@@ -24,10 +26,9 @@ public class CachingPrincipalAttributesRepository extends AbstractPrincipalAttri
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CachingPrincipalAttributesRepository.class);
 
-    
+
     private final transient Cache<String, Map<String, Object>> cache;
-    private final transient PrincipalAttributesCacheLoader cacheLoader =
-            new PrincipalAttributesCacheLoader();
+    private final transient PrincipalAttributesCacheLoader cacheLoader = new PrincipalAttributesCacheLoader();
 
     private long maxCacheSize = DEFAULT_MAXIMUM_CACHE_SIZE;
 
@@ -36,14 +37,15 @@ public class CachingPrincipalAttributesRepository extends AbstractPrincipalAttri
      */
     private CachingPrincipalAttributesRepository() {
         super();
-        this.cache = CacheBuilder.newBuilder().maximumSize(this.maxCacheSize)
+        this.cache = Caffeine.newBuilder().maximumSize(this.maxCacheSize)
                 .expireAfterWrite(getExpiration(), TimeUnit.valueOf(getTimeUnit())).build(this.cacheLoader);
     }
 
     /**
      * Instantiates a new caching attributes principal factory.
      * Sets the default cache size to {@link #DEFAULT_MAXIMUM_CACHE_SIZE}.
-     * @param timeUnit the time unit
+     *
+     * @param timeUnit       the time unit
      * @param expiryDuration the expiry duration
      */
     public CachingPrincipalAttributesRepository(final String timeUnit, final long expiryDuration) {
@@ -52,8 +54,9 @@ public class CachingPrincipalAttributesRepository extends AbstractPrincipalAttri
 
     /**
      * Instantiates a new caching attributes principal factory.
-     * @param maxCacheSize the max cache size
-     * @param timeUnit the time unit
+     *
+     * @param maxCacheSize   the max cache size
+     * @param timeUnit       the time unit
      * @param expiryDuration the expiry duration
      */
     public CachingPrincipalAttributesRepository(final long maxCacheSize,
@@ -62,7 +65,7 @@ public class CachingPrincipalAttributesRepository extends AbstractPrincipalAttri
         super(expiryDuration, timeUnit);
         this.maxCacheSize = maxCacheSize;
 
-        this.cache = CacheBuilder.newBuilder().maximumSize(maxCacheSize)
+        this.cache = Caffeine.newBuilder().maximumSize(maxCacheSize)
                 .expireAfterWrite(getExpiration(), TimeUnit.valueOf(getTimeUnit())).build(this.cacheLoader);
     }
 
@@ -75,14 +78,14 @@ public class CachingPrincipalAttributesRepository extends AbstractPrincipalAttri
     @Override
     protected Map<String, Object> getPrincipalAttributes(final Principal p) {
         try {
-            return this.cache.get(p.getId(), () -> {
+            return this.cache.get(p.getId(), s -> {
                 LOGGER.debug("No cached attributes could be found for [{}]", p.getId());
                 return new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
             });
         } catch (final Exception e) {
             LOGGER.error(e.getMessage(), e);
         }
-        return null;
+        return new HashMap<>();
     }
 
     @Override
@@ -90,7 +93,7 @@ public class CachingPrincipalAttributesRepository extends AbstractPrincipalAttri
         this.cache.cleanUp();
     }
 
-    private static class PrincipalAttributesCacheLoader extends CacheLoader<String, Map<String, Object>> {
+    private static class PrincipalAttributesCacheLoader implements CacheLoader<String, Map<String, Object>> {
         @Override
         public Map<String, Object> load(final String key) throws Exception {
             return new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
