27e6c1538c2e60e4e1d8aa875ccfe58f8851f23c
Misagh Moayyed <mm1844@gmail.com>
2017-08-05 20:34:14 +0430

    Fix test cases

--- core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/ldap/AbstractLdapProperties.java
+++ core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/ldap/AbstractLdapProperties.java
@@ -241,7 +241,7 @@ public abstract class AbstractLdapProperties implements Serializable {
     /**
      * The SASL mechanism.
      */
-    private String saslMechanism = "GSSAPI";
+    private String saslMechanism;
     /**
      * SASL authorization id.
      */
