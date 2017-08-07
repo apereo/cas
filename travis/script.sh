fb2ee9f7e520e3c8de2b606b5238d6dccbb53d5f
Misagh Moayyed <mmoayyed@unicon.net>
2017-08-07 22:16:51 +0430

    Update script.sh

--- travis/script.sh
+++ travis/script.sh
@@ -4,5 +4,5 @@ if [ "$TRAVIS_COMMIT_MESSAGE" == "[skip tests]" ]
 then
   echo -e "Travis CI build indicates that tests should be skipped.\n"
 else
-  sudo ./gradlew checkstyleTest test --stacktrace --parallel --build-cache -DshowStandardStreams=true
+  sudo ./gradlew checkstyleTest test --stacktrace --parallel --build-cache
 fi
