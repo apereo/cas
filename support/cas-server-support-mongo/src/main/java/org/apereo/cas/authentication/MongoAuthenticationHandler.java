44a4ec729a619888024dd83100dcda309434ca7e
Misagh Moayyed <mm1844@gmail.com>
2017-08-09 01:36:05 +0430

    clean up mongo config for authn

--- support/cas-server-support-mongo/src/main/java/org/apereo/cas/authentication/MongoAuthenticationHandler.java
+++ support/cas-server-support-mongo/src/main/java/org/apereo/cas/authentication/MongoAuthenticationHandler.java
@@ -1,17 +1,8 @@
 package org.apereo.cas.authentication;
 
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
-import org.apache.commons.codec.binary.StringUtils;
 import org.apereo.cas.authentication.principal.PrincipalFactory;
 import org.apereo.cas.integration.pac4j.authentication.handler.support.UsernamePasswordWrapperAuthenticationHandler;
 import org.apereo.cas.services.ServicesManager;
-import org.pac4j.core.credentials.UsernamePasswordCredentials;
-import org.pac4j.core.credentials.authenticator.Authenticator;
-import org.pac4j.core.credentials.password.PasswordEncoder;
-import org.pac4j.mongo.credentials.authenticator.MongoAuthenticator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * An authentication handler to verify credentials against a MongoDb instance.
@@ -20,54 +11,7 @@ import org.slf4j.LoggerFactory;
  */
 public class MongoAuthenticationHandler extends UsernamePasswordWrapperAuthenticationHandler {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(MongoAuthenticationHandler.class);
-    
-    private final String collectionName;
-    private final String mongoHostUri;
-    private final String attributes;
-    private final String usernameAttribute;
-    private final String passwordAttribute;
-    
-    private PasswordEncoder mongoPasswordEncoder = new NoOpPasswordEncoder();
-
-    public MongoAuthenticationHandler(final String name, final ServicesManager servicesManager, final PrincipalFactory principalFactory,
-                                      final String collectionName, final String mongoHostUri, final String attributes, final String usernameAttribute,
-                                      final String passwordAttribute, final PasswordEncoder mongoPasswordEncoder) {
+    public MongoAuthenticationHandler(final String name, final ServicesManager servicesManager, final PrincipalFactory principalFactory) {
         super(name, servicesManager, principalFactory, null);
-        this.collectionName = collectionName;
-        this.mongoHostUri = mongoHostUri;
-        this.attributes = attributes;
-        this.usernameAttribute = usernameAttribute;
-        this.passwordAttribute = passwordAttribute;
-        this.mongoPasswordEncoder = mongoPasswordEncoder;
-    }
-
-    @Override
-    protected Authenticator<UsernamePasswordCredentials> getAuthenticator(final Credential credential) {
-        final MongoClientURI uri = new MongoClientURI(this.mongoHostUri);
-        final MongoClient client = new MongoClient(uri);
-        LOGGER.info("Connected to MongoDb instance @ [{}] using database [{}]",
-                uri.getHosts(), uri.getDatabase());
-
-        final MongoAuthenticator mongoAuthenticator = new MongoAuthenticator(client, this.attributes);
-        mongoAuthenticator.setUsersCollection(this.collectionName);
-        mongoAuthenticator.setUsersDatabase(uri.getDatabase());
-        mongoAuthenticator.setUsernameAttribute(this.usernameAttribute);
-        mongoAuthenticator.setPasswordAttribute(this.passwordAttribute);
-        mongoAuthenticator.setPasswordEncoder(this.mongoPasswordEncoder);
-        return mongoAuthenticator;
-    }
-    
-    private static class NoOpPasswordEncoder implements PasswordEncoder {
-        @Override
-        public String encode(final String s) {
-            LOGGER.debug("No password encoding shall take place by CAS");
-            return s;
-        }
-
-        @Override
-        public boolean matches(final String s, final String s1) {
-            return StringUtils.equals(s, s1);
-        }
     }
 }
