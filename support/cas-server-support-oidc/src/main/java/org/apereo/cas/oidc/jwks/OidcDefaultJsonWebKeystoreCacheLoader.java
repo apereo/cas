8d1ccd53a15145465eac80052c5eefb70724230c
Misagh Moayyed <mmoayyed@unicon.net>
2017-08-10 23:43:09 +0430

    switch out to caffein caching. add a json resource for trusted dev mfa.

--- support/cas-server-support-oidc/src/main/java/org/apereo/cas/oidc/jwks/OidcDefaultJsonWebKeystoreCacheLoader.java
+++ support/cas-server-support-oidc/src/main/java/org/apereo/cas/oidc/jwks/OidcDefaultJsonWebKeystoreCacheLoader.java
@@ -1,6 +1,6 @@
 package org.apereo.cas.oidc.jwks;
 
-import com.google.common.cache.CacheLoader;
+import com.github.benmanes.caffeine.cache.CacheLoader;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.jose4j.jwk.JsonWebKeySet;
@@ -19,7 +19,7 @@ import java.util.Optional;
  * @author Misagh Moayyed
  * @since 5.1.0
  */
-public class OidcDefaultJsonWebKeystoreCacheLoader extends CacheLoader<String, Optional<RsaJsonWebKey>> {
+public class OidcDefaultJsonWebKeystoreCacheLoader implements CacheLoader<String, Optional<RsaJsonWebKey>> {
     private static final Logger LOGGER = LoggerFactory.getLogger(OidcDefaultJsonWebKeystoreCacheLoader.class);
 
     private final Resource jwksFile;
