8cadbc2d2693212daddc8bbba63159108f8211bf
Travis Schmidt <tsschmidt@ucdavis.edu>
2017-08-10 11:11:49 -0700

    Mapping for RegisterdServiceView

--- webapp-mgmt/cas-management-webapp-support/src/main/java/org/apereo/cas/mgmt/services/web/factory/DefaultRegisteredServiceFactory.java
+++ webapp-mgmt/cas-management-webapp-support/src/main/java/org/apereo/cas/mgmt/services/web/factory/DefaultRegisteredServiceFactory.java
@@ -1,14 +1,23 @@
 package org.apereo.cas.mgmt.services.web.factory;
 
+import org.apereo.cas.mgmt.services.web.beans.AbstractRegisteredServiceAttributeReleasePolicyStrategyBean;
 import org.apereo.cas.mgmt.services.web.beans.FormData;
+import org.apereo.cas.mgmt.services.web.beans.RegisteredServiceAttributeReleasePolicyStrategyViewBean;
+import org.apereo.cas.mgmt.services.web.beans.RegisteredServiceAttributeReleasePolicyViewBean;
+import org.apereo.cas.mgmt.services.web.beans.RegisteredServiceProxyPolicyBean;
 import org.apereo.cas.mgmt.services.web.beans.RegisteredServiceViewBean;
-import org.apereo.cas.services.AbstractRegisteredService;
+import org.apereo.cas.services.AbstractRegisteredServiceAttributeReleasePolicy;
+import org.apereo.cas.services.DenyAllAttributeReleasePolicy;
+import org.apereo.cas.services.GroovyScriptAttributeReleasePolicy;
+import org.apereo.cas.services.RefuseRegisteredServiceProxyPolicy;
+import org.apereo.cas.services.RegexMatchingRegisteredServiceProxyPolicy;
 import org.apereo.cas.services.RegisteredService;
-import org.apereo.cas.services.RegisteredServiceAccessStrategy;
 import org.apereo.cas.services.RegisteredServiceAttributeReleasePolicy;
-import org.apereo.cas.services.RegisteredServiceMultifactorPolicy;
 import org.apereo.cas.services.RegisteredServiceProxyPolicy;
-import org.apereo.cas.services.RegisteredServiceUsernameAttributeProvider;
+import org.apereo.cas.services.ReturnAllAttributeReleasePolicy;
+import org.apereo.cas.services.ReturnAllowedAttributeReleasePolicy;
+import org.apereo.cas.services.ReturnMappedAttributeReleasePolicy;
+import org.apereo.cas.services.ScriptedRegisteredServiceAttributeReleasePolicy;
 
 import java.util.List;
 
@@ -42,10 +51,59 @@ public class DefaultRegisteredServiceFactory implements RegisteredServiceFactory
         bean.setServiceId(svc.getServiceId());
         bean.setName(svc.getName());
         bean.setDescription(svc.getDescription());
-        //bean.setLogoUrl(svc.getLogo().toString());
-        //bean.setProxyPolicy(svc.getProxyPolicy());
-        //bean.setAttrRelease(svc.getAttributeReleasePolicy());
+        if (svc.getLogo() != null) {
+            bean.setLogoUrl(svc.getLogo().toString());
+        }
+        mapAttributeReleasePolicy(svc.getAttributeReleasePolicy(),bean);
+        mapProxyPolicy(svc.getProxyPolicy(),bean);
 
         return bean;
+    }
+
+    private void mapAttributeReleasePolicy(final RegisteredServiceAttributeReleasePolicy policy, final RegisteredServiceViewBean bean) {
+        if (policy instanceof AbstractRegisteredServiceAttributeReleasePolicy) {
+            final AbstractRegisteredServiceAttributeReleasePolicy attrPolicy = (AbstractRegisteredServiceAttributeReleasePolicy) policy;
+
+            final RegisteredServiceAttributeReleasePolicyViewBean attrPolicyBean = bean.getAttrRelease();
+            attrPolicyBean.setReleasePassword(attrPolicy.isAuthorizedToReleaseCredentialPassword());
+            attrPolicyBean.setReleaseTicket(attrPolicy.isAuthorizedToReleaseProxyGrantingTicket());
+            attrPolicyBean.setExcludeDefault(attrPolicy.isExcludeDefaultAttributes());
+
+            if (attrPolicy instanceof ScriptedRegisteredServiceAttributeReleasePolicy) {
+                attrPolicyBean.setAttrPolicy(AbstractRegisteredServiceAttributeReleasePolicyStrategyBean.Types.SCRIPT.toString());
+            } else if (attrPolicy instanceof GroovyScriptAttributeReleasePolicy) {
+                attrPolicyBean.setAttrPolicy(AbstractRegisteredServiceAttributeReleasePolicyStrategyBean.Types.GROOVY.toString());
+            } else if (attrPolicy instanceof ReturnAllAttributeReleasePolicy) {
+                attrPolicyBean.setAttrPolicy(RegisteredServiceAttributeReleasePolicyStrategyViewBean.Types.ALL.toString());
+            } else if (attrPolicy instanceof ReturnAllowedAttributeReleasePolicy) {
+                final ReturnAllowedAttributeReleasePolicy attrPolicyAllowed = (ReturnAllowedAttributeReleasePolicy) attrPolicy;
+                if (attrPolicyAllowed.getAllowedAttributes().isEmpty()) {
+                    attrPolicyBean.setAttrPolicy(RegisteredServiceAttributeReleasePolicyStrategyViewBean.Types.NONE.toString());
+                } else {
+                    attrPolicyBean.setAttrPolicy(RegisteredServiceAttributeReleasePolicyStrategyViewBean.Types.ALLOWED.toString());
+                }
+            } else if (attrPolicy instanceof ReturnMappedAttributeReleasePolicy) {
+                final ReturnMappedAttributeReleasePolicy attrPolicyAllowed = (ReturnMappedAttributeReleasePolicy) attrPolicy;
+                if (attrPolicyAllowed.getAllowedAttributes().isEmpty()) {
+                    attrPolicyBean.setAttrPolicy(RegisteredServiceAttributeReleasePolicyStrategyViewBean.Types.NONE.toString());
+                } else {
+                    attrPolicyBean.setAttrPolicy(RegisteredServiceAttributeReleasePolicyStrategyViewBean.Types.MAPPED.toString());
+                }
+            } else if (attrPolicy instanceof DenyAllAttributeReleasePolicy) {
+                attrPolicyBean.setAttrPolicy(RegisteredServiceAttributeReleasePolicyStrategyViewBean.Types.DENY.toString());
+            }
+        }
+    }
+
+    private void mapProxyPolicy(final RegisteredServiceProxyPolicy policy, final RegisteredServiceViewBean bean) {
+        final RegisteredServiceProxyPolicyBean proxyPolicyBean = bean.getProxyPolicy();
+
+        if (policy instanceof RefuseRegisteredServiceProxyPolicy) {
+            proxyPolicyBean.setType(RegisteredServiceProxyPolicyBean.Types.REFUSE);
+        } else if (policy instanceof RegexMatchingRegisteredServiceProxyPolicy) {
+            final RegexMatchingRegisteredServiceProxyPolicy option = (RegexMatchingRegisteredServiceProxyPolicy) policy;
+            proxyPolicyBean.setType(RegisteredServiceProxyPolicyBean.Types.REGEX);
+            proxyPolicyBean.setValue(option.getPattern().toString());
+        }
     }
 }
--- webapp-mgmt/cas-management-webapp/src/domain/service-view-bean.ts
+++ webapp-mgmt/cas-management-webapp/src/domain/service-view-bean.ts
@@ -9,11 +9,23 @@ export class ServiceViewBean {
   name: String;
   description: String;
   logoUrl: String;
-  proxyPolicy: any;
-  attrRelease: any;
+  proxyPolicy: RegisteredServiceProxyPolicyBean;
+  attrRelease: RegisteredServiceAttributeReleasePolicyBean;
 }
 
 export class FormData {
   availableAttributes: String[] = [];
   customComponent: Map<String,Map<String,any>>;
+}
+
+export class RegisteredServiceProxyPolicyBean {
+  type: String;
+  value: String;
+}
+
+export class RegisteredServiceAttributeReleasePolicyBean {
+  releasePassword: boolean;
+  releaseTicket: boolean;
+  excludeDefault: boolean;
+  attrPolicy: String;
 }
