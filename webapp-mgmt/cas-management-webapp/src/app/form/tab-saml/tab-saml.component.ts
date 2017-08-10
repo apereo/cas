839f886caceace164f3f3f4f2a2f8ddef2dc8b16
Travis Schmidt <tsschmidt@ucdavis.edu>
2017-08-10 10:27:22 -0700

    Moved Service Type to Basics screen Refacored OAuth,OIDC,SAML and WS Fed into their own tab

---
+++ webapp-mgmt/cas-management-webapp/src/app/form/tab-saml/tab-saml.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tab-saml',
+  templateUrl: './tab-saml.component.html',
+  styleUrls: ['./tab-saml.component.css']
+})
+export class TabSamlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
