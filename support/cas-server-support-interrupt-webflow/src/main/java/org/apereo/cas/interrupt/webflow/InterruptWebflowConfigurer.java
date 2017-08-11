dd611a405cb9fcb6a5cff4953df40c3591a1264a
Misagh Moayyed <mm1844@gmail.com>
2017-08-11 20:04:35 +0430

    complete impls of inquirers

--- support/cas-server-support-interrupt-webflow/src/main/java/org/apereo/cas/interrupt/webflow/InterruptWebflowConfigurer.java
+++ support/cas-server-support-interrupt-webflow/src/main/java/org/apereo/cas/interrupt/webflow/InterruptWebflowConfigurer.java
@@ -1,8 +1,12 @@
 package org.apereo.cas.interrupt.webflow;
 
 import org.apereo.cas.web.flow.AbstractCasWebflowConfigurer;
+import org.apereo.cas.web.flow.CasWebflowConstants;
 import org.springframework.webflow.definition.registry.FlowDefinitionRegistry;
+import org.springframework.webflow.engine.ActionState;
 import org.springframework.webflow.engine.Flow;
+import org.springframework.webflow.engine.Transition;
+import org.springframework.webflow.engine.ViewState;
 import org.springframework.webflow.engine.builder.support.FlowBuilderServices;
 
 /**
@@ -12,8 +16,12 @@ import org.springframework.webflow.engine.builder.support.FlowBuilderServices;
  * @since 5.2.0
  */
 public class InterruptWebflowConfigurer extends AbstractCasWebflowConfigurer {
+
+    private static final String INTERRUPT_VIEW_ID = "interruptView";
+    private static final String STATE_ID_INQUIRE_INTERRUPT_CHECK = "inquireInterruptCheck";
+    
     public InterruptWebflowConfigurer(final FlowBuilderServices flowBuilderServices,
-                                    final FlowDefinitionRegistry flowDefinitionRegistry) {
+                                      final FlowDefinitionRegistry flowDefinitionRegistry) {
         super(flowBuilderServices, flowDefinitionRegistry);
     }
 
@@ -22,8 +30,24 @@ public class InterruptWebflowConfigurer extends AbstractCasWebflowConfigurer {
         final Flow flow = getLoginFlow();
 
         if (flow != null) {
-           
+            createInquireActionState(flow);
+            createAcceptableUsagePolicyView(flow);
         }
     }
 
+    private void createAcceptableUsagePolicyView(final Flow flow) {
+        final ViewState viewState = createViewState(flow, INTERRUPT_VIEW_ID, "casInterruptView");
+        viewState.getEntryActionList().add(createEvaluateAction("prepareInterruptViewAction"));
+    }
+    
+    private void createInquireActionState(final Flow flow) {
+        final ActionState actionState = createActionState(flow, STATE_ID_INQUIRE_INTERRUPT_CHECK, createEvaluateAction("inquireInterruptAction"));
+        
+        final Transition noInterruptTransition = createTransition(CasWebflowConstants.TRANSITION_ID_NO,
+                CasWebflowConstants.STATE_ID_SEND_TICKET_GRANTING_TICKET);
+        actionState.getTransitionSet().add(noInterruptTransition);
+        
+        final Transition yesInterruptTransition = createTransition(CasWebflowConstants.TRANSITION_ID_YES, INTERRUPT_VIEW_ID);
+        actionState.getTransitionSet().add(yesInterruptTransition);
+    }
 }
