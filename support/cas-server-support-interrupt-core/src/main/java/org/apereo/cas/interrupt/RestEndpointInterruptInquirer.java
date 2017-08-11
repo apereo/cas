2f0adecbad69309953c2d50f4e6af4113eb7f29e
Misagh Moayyed <mm1844@gmail.com>
2017-08-11 19:23:16 +0430

    Initial interrupt functionality and baseline modules

--- support/cas-server-support-interrupt-core/src/main/java/org/apereo/cas/interrupt/RestEndpointInterruptInquirer.java
+++ support/cas-server-support-interrupt-core/src/main/java/org/apereo/cas/interrupt/RestEndpointInterruptInquirer.java
@@ -2,6 +2,7 @@ package org.apereo.cas.interrupt;
 
 import org.apereo.cas.authentication.Authentication;
 import org.apereo.cas.authentication.principal.Service;
+import org.apereo.cas.configuration.model.support.interrupt.InterruptProperties;
 import org.apereo.cas.services.RegisteredService;
 
 /**
@@ -11,6 +12,12 @@ import org.apereo.cas.services.RegisteredService;
  * @since 5.2.0
  */
 public class RestEndpointInterruptInquirer implements InterruptInquirer {
+    private final InterruptProperties.Rest restProperties;
+
+    public RestEndpointInterruptInquirer(final InterruptProperties.Rest restProperties) {
+        this.restProperties = restProperties;
+    }
+
     @Override
     public InterruptResponse inquire(final Authentication authentication, final RegisteredService registeredService, final Service service) {
         return null;
