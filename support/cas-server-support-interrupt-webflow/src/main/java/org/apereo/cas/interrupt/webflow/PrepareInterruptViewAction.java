3e90ff6296bdd3029bf8084c60b0b57c661e5e31
Misagh Moayyed <mmoayyed@unicon.net>
2017-08-11 20:04:35 +0430

    complete impls of inquirers

---
+++ support/cas-server-support-interrupt-webflow/src/main/java/org/apereo/cas/interrupt/webflow/PrepareInterruptViewAction.java
@@ -0,0 +1,18 @@
+package org.apereo.cas.interrupt.webflow;
+
+import org.springframework.webflow.action.AbstractAction;
+import org.springframework.webflow.execution.Event;
+import org.springframework.webflow.execution.RequestContext;
+
+/**
+ * This is {@link PrepareInterruptViewAction}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.2.0
+ */
+public class PrepareInterruptViewAction extends AbstractAction {
+    @Override
+    protected Event doExecute(final RequestContext requestContext) throws Exception {
+        return null;
+    }
+}
