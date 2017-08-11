8f7830297c019775fd32be9f2a197419cd9dc395
Jeff Sittler <mindblender@gmail.com>
2017-08-10 20:30:59 -0700

    Webpack refactor & update dependicies (#2838)
    * Renamed AuthnAttemptsGraph.js to AuthnAttemptsGraph.jsx and StatisticsApp.js to StatisticsApp.jsx
    
    * Code spacing cleanup
    
    * Updated packages, code cleanup
    
    * Refactored the webpack.config.js and code cleanup

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -1,13 +1,10 @@
 const path = require('path')
-// const webpack = require('webpack');
 
 const config = {
   context: __dirname,
-  entry:
-    {
-      statistics: './resources/static/js/StatisticsApp'
-    },
-  devtool: 'eval',
+  entry: {
+    statistics: './resources/static/js/StatisticsApp'
+  },
   output: {
     path: path.join(__dirname, '/resources/static/js'),
     filename: '[name]-bundle.js'
@@ -17,67 +14,33 @@ const config = {
     historyApiFallback: true
   },
   resolve: {
-    extensions: ['.js', '.json'],
-    alias: {
-      react: 'preact-compat',
-      'react-dom': 'preact-compat'
+    extensions: ['.js', '.jsx', '.json'],
+    "alias": {
+      "react": "preact-compat",
+      "react-dom": "preact-compat"
     }
   },
   stats: {
-    colors: true,
-    reasons: true,
-    chunks: true
+    colors: true
   },
-  // plugins: [
-  //   // Ignore all locale files of moment.js
-  //   new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-  //   new BundleAnalyzerPlugin()
-  // ],
   module: {
     rules: [
       {
         enforce: 'pre',
-        test: /\.js$/,
+        test: /\.jsx?$/,
         loader: 'eslint-loader',
         exclude: /node_modules/
-      },
-      {
-        test: /\.json$/,
-        loader: 'json-loader'
       },
       {
         include: [
           path.resolve(__dirname, './resources/static/js'),
           path.resolve(__dirname, './node_modules/preact-compat/src')
         ],
-        test: /\.js$/,
+        test: /\.jsx?$/,
         loader: 'babel-loader'
-        // include: [
-        //   path.resolve('js'),
-        // ]
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              url: false
-            }
-          }
-        ]
       }
     ]
   }
 }
 
-if (process.env.NODE_ENV === 'production') {
-  config.entry = {
-    statistics: './resources/static/js/StatisticsApp'
-  }
-  config.devtool = false;
-  config.plugins = []
-
-}
 module.exports = config
