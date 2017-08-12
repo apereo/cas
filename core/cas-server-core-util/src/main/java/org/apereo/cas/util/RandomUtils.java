cff736ef4efeb1e31f8df084086572ccf2589748
Misagh Moayyed <mmoayyed@unicon.net>
2017-08-12 21:07:46 +0430

    rename good to native to reveal intention more or less

--- core/cas-server-core-util/src/main/java/org/apereo/cas/util/RandomUtils.java
+++ core/cas-server-core-util/src/main/java/org/apereo/cas/util/RandomUtils.java
@@ -1,5 +1,8 @@
 package org.apereo.cas.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
@@ -12,6 +15,8 @@ import java.security.SecureRandom;
  * @since 5.2.0
  */
 public final class RandomUtils {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RandomUtils.class);
+    
     private RandomUtils() {
     }
 
@@ -30,13 +35,14 @@ public final class RandomUtils {
 
     /**
      * Get strong enough SecureRandom instance and wrap the checked exception.
-     *
+     * TODO Try {@code NativePRNGNonBlocking} and failover to default SHA1PRNG until Java 9.
      * @return the strong instance
      */
-    public static SecureRandom getInstanceGood() {
+    public static SecureRandom getInstanceNative() {
         try {
             return SecureRandom.getInstance("NativePRNGNonBlocking");
         } catch (final NoSuchAlgorithmException e) {
+            LOGGER.trace(e.getMessage(), e);
             return new SecureRandom();
         }
     }
