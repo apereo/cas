359da820423a3b58d919f50da808ca5798ef32b3
Misagh Moayyed <mm1844@gmail.com>
2017-08-15 15:33:56 +0430

    enable travis build matrix

--- travis/script.sh
+++ travis/script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ "$TRAVIS_COMMIT_MESSAGE" == "[skip tests]" ]
-then
+if [ "$TRAVIS_COMMIT_MESSAGE" == "[skip tests]" ] || [ "$PUBLISH_SNAPSHOTS" == "false" ]; then
   echo -e "Travis CI build indicates that tests should be skipped.\n"
+  echo -e "Commit message is $TRAVIS_COMMIT_MESSAGE and snapshot publish status is $PUBLISH_SNAPSHOTS\n"
 else
   sudo ./gradlew checkstyleTest test --stacktrace --parallel --build-cache
 fi
