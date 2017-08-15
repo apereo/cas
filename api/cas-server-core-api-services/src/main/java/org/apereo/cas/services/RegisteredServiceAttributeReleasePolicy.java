21301722956cc842db620137b7745ae0cad53015
Misagh Moayyed <mm1844@gmail.com>
2017-08-15 12:20:49 +0430

    design a consent policy to include/exclude attributes

--- api/cas-server-core-api-services/src/main/java/org/apereo/cas/services/RegisteredServiceAttributeReleasePolicy.java
+++ api/cas-server-core-api-services/src/main/java/org/apereo/cas/services/RegisteredServiceAttributeReleasePolicy.java
@@ -53,4 +53,17 @@ public interface RegisteredServiceAttributeReleasePolicy extends Serializable {
      * @return the attributes
      */
     Map<String, Object> getAttributes(Principal p, Service selectedService, RegisteredService service);
+
+    /**
+     * Gets the attributes that qualify for consent.
+     *
+     * @param p               the principal that contains the resolved attributes
+     * @param selectedService the selected service
+     * @param service         the service
+     * @return the attributes
+     */
+    default Map<String, Object> getConsentableAttributes(final Principal p, final Service selectedService, 
+                                                         final RegisteredService service) {
+        return getAttributes(p, selectedService, service);    
+    }
 }
