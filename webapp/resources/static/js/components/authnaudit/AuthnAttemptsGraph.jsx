2abd5fa30f3907d6d4464ebe6502ec7581396804
Jeff Sittler <mindblender@gmail.com>
2017-08-16 11:30:42 -0700

    added airbnb style and ran autofix

--- webapp/resources/static/js/components/authnaudit/AuthnAttemptsGraph.jsx
+++ webapp/resources/static/js/components/authnaudit/AuthnAttemptsGraph.jsx
@@ -1,53 +1,55 @@
-import React from 'react'
-import { VictoryChart, VictoryVoronoiContainer, VictoryLine } from 'victory'
-const { array } = React.PropTypes
+import React from 'react';
+import { VictoryChart, VictoryVoronoiContainer, VictoryLine } from 'victory';
+
+const { array } = React.PropTypes;
 
 const AuthnAttemptsGraph = React.createClass({
   propTypes: {
-    graphData: array
+    graphData: array,
   },
 
-  render: function () {
+  render() {
     if (this.props.graphData.length < 2) {
       return (
         <div>
           <h4>No data found</h4>
         </div>
-      )
+      );
     }
 
     return (
       <div>
         <VictoryChart
-          width={950} height={300}
+          width={950}
+          height={300}
           padding={50}
           containerComponent={<VictoryVoronoiContainer />}
-          domainPadding={{y: 10}}
+          domainPadding={{ y: 10 }}
         >
           <VictoryLine
             data={this.props.graphData}
-            x='time'
-            y='failures'
+            x="time"
+            y="failures"
             style={{
-              data: {stroke: 'tomato', opacity: 1.0},
-              labels: {fontSize: 12},
-              parent: {border: '1px solid #ccc'}
+              data: { stroke: 'tomato', opacity: 1.0 },
+              labels: { fontSize: 12 },
+              parent: { border: '1px solid #ccc' },
             }}
           />
           <VictoryLine
             data={this.props.graphData}
-            x='time'
-            y='successes'
+            x="time"
+            y="successes"
             style={{
-              data: {stroke: 'darkgreen', opacity: 1.0},
-              labels: {fontSize: 12},
-              parent: {border: '1px solid #ccc'}
+              data: { stroke: 'darkgreen', opacity: 1.0 },
+              labels: { fontSize: 12 },
+              parent: { border: '1px solid #ccc' },
             }}
           />
         </VictoryChart>
       </div>
-    )
-  }
-})
+    );
+  },
+});
 
-export default AuthnAttemptsGraph
+export default AuthnAttemptsGraph;
