08f3d5f89ecc7e9e3df6192423034f158781a05f
Misagh Moayyed <mm1844@gmail.com>
2017-08-17 01:32:19 +0430

    Fix compile issues with controllers using ctor params

--- support/cas-server-support-saml/src/main/java/org/apereo/cas/support/saml/web/SamlValidateController.java
+++ support/cas-server-support-saml/src/main/java/org/apereo/cas/support/saml/web/SamlValidateController.java
@@ -1,9 +1,18 @@
 package org.apereo.cas.support.saml.web;
 
+import org.apereo.cas.CentralAuthenticationService;
+import org.apereo.cas.authentication.AuthenticationContextValidator;
+import org.apereo.cas.authentication.AuthenticationSystemSupport;
+import org.apereo.cas.authentication.MultifactorTriggerSelectionStrategy;
+import org.apereo.cas.services.ServicesManager;
 import org.apereo.cas.support.saml.SamlProtocolConstants;
+import org.apereo.cas.ticket.proxy.ProxyHandler;
+import org.apereo.cas.validation.ValidationSpecification;
 import org.apereo.cas.web.AbstractServiceValidateController;
+import org.apereo.cas.web.support.ArgumentExtractor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,6 +26,21 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class SamlValidateController extends AbstractServiceValidateController {
 
+    public SamlValidateController(final ValidationSpecification validationSpecification, 
+                                  final AuthenticationSystemSupport authenticationSystemSupport, 
+                                  final ServicesManager servicesManager, 
+                                  final CentralAuthenticationService centralAuthenticationService, 
+                                  final ProxyHandler proxyHandler, 
+                                  final ArgumentExtractor argumentExtractor, 
+                                  final MultifactorTriggerSelectionStrategy multifactorTriggerSelectionStrategy, 
+                                  final AuthenticationContextValidator authenticationContextValidator, 
+                                  final View jsonView, final View successView, final View failureView, 
+                                  final String authnContextAttribute) {
+        super(validationSpecification, authenticationSystemSupport, servicesManager, centralAuthenticationService, 
+                proxyHandler, argumentExtractor, multifactorTriggerSelectionStrategy, authenticationContextValidator, 
+                jsonView, successView, failureView, authnContextAttribute);
+    }
+
     /**
      * Handle model and view.
      *
@@ -27,7 +51,7 @@ public class SamlValidateController extends AbstractServiceValidateController {
      */
     @PostMapping(path = SamlProtocolConstants.ENDPOINT_SAML_VALIDATE)
     @Override
-    protected ModelAndView handleRequestInternal(final HttpServletRequest request, final HttpServletResponse response)
+    public ModelAndView handleRequestInternal(final HttpServletRequest request, final HttpServletResponse response)
             throws Exception {
         return super.handleRequestInternal(request, response);
     }
