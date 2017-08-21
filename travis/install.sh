814f4f0eb6e896bfc31e7290fbd586c77f2761af
Misagh Moayyed <mm1844@gmail.com>
2017-08-21 10:43:48 +0430

    Update install.sh

--- travis/install.sh
+++ travis/install.sh
@@ -2,8 +2,8 @@
 
 if [ "$PUBLISH_SNAPSHOTS" == "false" ]; then
   echo -e "Generating javadocs will be skipped since Travis CI build indicates that we are not publishing snapshots.\n"
-  sudo ./gradlew clean checkstyleMain bootRepackage --parallel -x test -x javadoc --stacktrace --build-cache
+  sudo ./gradlew checkstyleMain bootRepackage --parallel -x test -x javadoc --stacktrace --build-cache
 else
-  echo -e "Travis CI build indicates that tests should should run.\n"
+  echo -e "Travis CI build indicates that tests should not should run since we are publishing snapshots.\n"
   sudo ./gradlew bootRepackage --parallel -x test --stacktrace --build-cache -x check
 fi
