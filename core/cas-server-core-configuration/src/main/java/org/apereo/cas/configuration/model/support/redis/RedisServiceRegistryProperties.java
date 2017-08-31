c84f38976e488232fe71fc7bce90a1850d949d5f
Misagh Moayyed <mm1844@gmail.com>
2017-08-31 12:04:20 +0430

    Working on redis service registry

---
+++ core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/redis/RedisServiceRegistryProperties.java
@@ -0,0 +1,11 @@
+package org.apereo.cas.configuration.model.support.redis;
+
+/**
+ * Configuration properties for Redis.
+ *
+ * @author serv
+ * @since 5.2.0
+ */
+public class RedisServiceRegistryProperties extends BaseRedisProperties {
+    private static final long serialVersionUID = -9012996050439638782L;
+}
