8266ab74444face039e4620de325538c30ca93c9
Misagh Moayyed <mm1844@gmail.com>
2017-09-14 01:31:29 -0700

    Management Webapp: domain services manager and regex (#2928)
    * Fix for domain pattern validation
    
    * Changes to the DomainServicesManager to validate domains better
    Changes to Management App to handle either DefaultServicesManager or DomainServicesManager
    
    * Added Paginator to Services Screen

--- webapp-mgmt/cas-management-webapp/src/app/services/services.component.ts
+++ webapp-mgmt/cas-management-webapp/src/app/services/services.component.ts
@@ -1,24 +1,31 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild} from '@angular/core';
 import {ServiceViewBean} from "../../domain/service-view-bean";
 import {Messages} from "../messages";
 import {ActivatedRoute, Router} from "@angular/router";
 import {ServiceViewService} from "./service.service";
 import {Location} from "@angular/common";
-import {MdDialog, MdSnackBar} from "@angular/material";
+import {MdDialog, MdPaginator, MdSnackBar, PageEvent} from "@angular/material";
 import {DeleteComponent} from "../delete/delete.component";
+import {BehaviorSubject} from "rxjs/BehaviorSubject";
+import {DataSource} from "@angular/cdk/collections";
+import {Observable} from "rxjs/Observable";
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
   styleUrls: ['./services.component.css']
 })
-export class ServicesComponent implements OnInit {
+export class ServicesComponent implements OnInit,AfterViewInit {
 
   dataTable: ServiceViewBean[];
   detailRow: String;
   deleteItem: ServiceViewBean;
   domain: String;
   selectedItem: ServiceViewBean;
+  servicesDatabase = new ServicesDatabase();
+
+  @ViewChild("paginator")
+  paginator: MdPaginator;
 
   constructor(public messages: Messages,
               private route: ActivatedRoute,
@@ -33,14 +40,27 @@ export class ServicesComponent implements OnInit {
   ngOnInit() {
     this.route.data
       .subscribe((data: { resp: ServiceViewBean[]}) => {
-        this.dataTable = data.resp;
-        if (!this.dataTable) {
+        if (!data.resp) {
           this.snackBar.open(this.messages.management_services_status_listfail,'dismiss',{
             duration: 5000
           });
         }
+        this.servicesDatabase.load(data.resp);
       });
     this.route.params.subscribe((params) => this.domain = params['domain']);
+  }
+
+  ngAfterViewInit() {
+    const displayDataChanges = [
+      this.servicesDatabase.dataChange,
+      this.paginator.page,
+    ];
+
+    Observable.merge(...displayDataChanges).map(() => {
+      const data = this.servicesDatabase.data.slice();
+      const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
+      return data.splice(startIndex, this.paginator.pageSize);
+    }).subscribe((d) => setTimeout(() => this.dataTable = d,0));
   }
 
   serviceEdit(selectedItem: String) {
@@ -91,14 +111,12 @@ export class ServicesComponent implements OnInit {
   }
 
   refresh() {
-    setTimeout(() => {
-      this.getServices();
-    },500);
+    this.getServices();
   }
 
   getServices() {
     this.service.getServices(this.domain)
-      .then(resp => this.dataTable = resp)
+      .then(resp => this.servicesDatabase.load(resp))
       .catch((e: any) => this.snackBar.open(this.messages.management_services_status_listfail,'Dismiss', {
         duration: 5000
       }));
@@ -109,27 +127,44 @@ export class ServicesComponent implements OnInit {
   }
 
   moveUp(a: ServiceViewBean) {
-    let index: number = this.dataTable.indexOf(a);
+    let index: number = this.servicesDatabase.data.indexOf(a);
     if(index > 0) {
-      let b: ServiceViewBean = this.dataTable[index-1];
+      let b: ServiceViewBean = this.servicesDatabase.data[index-1];
       a.evalOrder = index-1;
       b.evalOrder = index;
-      this.dataTable[index] = b;
-      this.dataTable[index -1] = a;
       this.service.updateOrder(a,b).then(resp => this.refresh());
     }
   }
 
   moveDown(a: ServiceViewBean) {
-    let index: number = this.dataTable.indexOf(a);
-    if(index < this.dataTable.length -1) {
-      let b: ServiceViewBean = this.dataTable[index+1];
+    let index: number = this.servicesDatabase.data.indexOf(a);
+    if(index < this.servicesDatabase.data.length -1) {
+      let b: ServiceViewBean = this.servicesDatabase.data[index+1];
       a.evalOrder = index+1;
       b.evalOrder = index;
-      this.dataTable[index] = b;
-      this.dataTable[index+1] = a;
       this.service.updateOrder(a,b).then(resp => this.refresh());
     }
   }
 
+}
+
+export class ServicesDatabase {
+  dataChange: BehaviorSubject<ServiceViewBean[]> = new BehaviorSubject<ServiceViewBean[]>([]);
+  get data(): ServiceViewBean[] { return this.dataChange.value; }
+
+  constructor() {
+  }
+
+  load(services: ServiceViewBean[]) {
+    this.dataChange.next([]);
+    for(let service of services) {
+      this.addService(service);
+    }
+  }
+
+  addService(service: ServiceViewBean) {
+    const copiedData = this.data.slice();
+    copiedData.push(service);
+    this.dataChange.next(copiedData);
+  }
 }
