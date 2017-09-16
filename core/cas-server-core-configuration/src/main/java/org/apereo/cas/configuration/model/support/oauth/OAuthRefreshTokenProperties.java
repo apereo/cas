89d838345cc905519933152c2b9b3e88c691a81a
Misagh Moayyed <mmoayyed@unicon.net>
2017-09-16 14:39:14 +0430

    refactor oauth props

---
+++ core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/oauth/OAuthRefreshTokenProperties.java
@@ -0,0 +1,27 @@
+package org.apereo.cas.configuration.model.support.oauth;
+
+import org.apereo.cas.configuration.support.Beans;
+
+import java.io.Serializable;
+
+/**
+ * This is {@link OAuthRefreshTokenProperties}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.2.0
+ */
+public class OAuthRefreshTokenProperties implements Serializable {
+    private static final long serialVersionUID = -8328568272835831702L;
+    /**
+     * Hard timeout beyond which the refresh token is considered expired.
+     */
+    private String timeToKillInSeconds = "P14D";
+
+    public long getTimeToKillInSeconds() {
+        return Beans.newDuration(timeToKillInSeconds).getSeconds();
+    }
+
+    public void setTimeToKillInSeconds(final String timeToKillInSeconds) {
+        this.timeToKillInSeconds = timeToKillInSeconds;
+    }
+}
