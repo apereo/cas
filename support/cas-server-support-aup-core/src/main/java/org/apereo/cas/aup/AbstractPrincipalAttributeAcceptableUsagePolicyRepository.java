8e609acbb0b4eaa9efd38d41f7712ce391e948f9
Misagh Moayyed <mm1844@gmail.com>
2017-09-16 19:30:15 +0430

    update javadocs

--- support/cas-server-support-aup-core/src/main/java/org/apereo/cas/aup/AbstractPrincipalAttributeAcceptableUsagePolicyRepository.java
+++ support/cas-server-support-aup-core/src/main/java/org/apereo/cas/aup/AbstractPrincipalAttributeAcceptableUsagePolicyRepository.java
@@ -4,12 +4,14 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.apereo.cas.authentication.Credential;
 import org.apereo.cas.authentication.principal.Principal;
 import org.apereo.cas.ticket.registry.TicketRegistrySupport;
+import org.apereo.cas.util.CollectionUtils;
 import org.apereo.cas.web.support.WebUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.webflow.execution.RequestContext;
 
 import java.util.Map;
+import java.util.Set;
 
 /**
  * This is {@link AbstractPrincipalAttributeAcceptableUsagePolicyRepository}.
@@ -42,18 +44,32 @@ public abstract class AbstractPrincipalAttributeAcceptableUsagePolicyRepository 
     @Override
     public Pair<Boolean, Principal> verify(final RequestContext requestContext, final Credential credential) {
         final Principal principal = WebUtils.getPrincipalFromRequestContext(requestContext, this.ticketRegistrySupport);
-        final Map<String, Object> attributes = principal.getAttributes();
-        LOGGER.debug("Principal attributes found for [{}] are [{}]", principal.getId(), attributes);
 
-        if (attributes != null && attributes.containsKey(this.aupAttributeName)) {
-            final Object value = attributes.get(this.aupAttributeName);
-            LOGGER.debug("Evaluating attribute value [{}] found for [{}]", value, this.aupAttributeName);
-            if (value.toString().equalsIgnoreCase(Boolean.TRUE.toString())) {
-                return Pair.of(Boolean.TRUE, principal);
-            }
+        if (isUsagePolicyAcceptedBy(principal)) {
+            LOGGER.debug("Usage policy has been accepted by [{}]", principal.getId());
+            return Pair.of(Boolean.TRUE, principal);
         }
 
         LOGGER.warn("Usage policy has not been accepted by [{}]", principal.getId());
         return Pair.of(Boolean.FALSE, principal);
+    }
+
+    /**
+     * Is usage policy accepted by user?
+     * Looks into the attributes collected by the principal to find {@link #aupAttributeName}.
+     * If the attribute contains {@code true}, then the policy is determined as accepted.
+     * @param principal the principal
+     * @return true if accepted, false otherwise.
+     */
+    protected boolean isUsagePolicyAcceptedBy(final Principal principal) {
+        final Map<String, Object> attributes = principal.getAttributes();
+        LOGGER.debug("Principal attributes found for [{}] are [{}]", principal.getId(), attributes);
+
+        if (attributes != null && attributes.containsKey(this.aupAttributeName)) {
+            final Set value = CollectionUtils.toCollection(attributes.get(this.aupAttributeName));
+            LOGGER.debug("Evaluating attribute value [{}] found for [{}]", value, this.aupAttributeName);
+            return value.stream().anyMatch(v -> v.toString().equalsIgnoreCase(Boolean.TRUE.toString()));
+        }
+        return false;
     }
 }
