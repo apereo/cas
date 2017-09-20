fa8e37a2383eb573da755f914723c453269b497f
Misagh Moayyed <mm1844@gmail.com>
2017-09-20 13:09:46 +0430

    fixed linting issues

--- webapp/resources/static/js/cas.js
+++ webapp/resources/static/js/cas.js
@@ -14,18 +14,18 @@ function requestGeoPosition() {
 
 function logGeoLocationError(error) {
     switch (error.code) {
-        case error.PERMISSION_DENIED:
-            // console.log('User denied the request for GeoLocation.');
-            break;
-        case error.POSITION_UNAVAILABLE:
-            // console.log('Location information is unavailable.');
-            break;
-        case error.TIMEOUT:
-            // console.log('The request to get user location timed out.');
-            break;
-        default:
-            // console.log('An unknown error occurred.');
-            break;
+    case error.PERMISSION_DENIED:
+        // console.log('User denied the request for GeoLocation.');
+        break;
+    case error.POSITION_UNAVAILABLE:
+        // console.log('Location information is unavailable.');
+        break;
+    case error.TIMEOUT:
+        // console.log('The request to get user location timed out.');
+        break;
+    default:
+        // console.log('An unknown error occurred.');
+        break;
     }
 }
 
@@ -59,7 +59,7 @@ function animateCasMessageBoxes() {
 }
 
 function disableEmptyInputFormSubmission() {
-    var fields = $('#fm1 input[name="username"],[name="password"]')
+    var fields = $('#fm1 input[name="username"],[name="password"]');
 
     if (fields.length == 2) {
         fields.on('input', function (event) {
--- webapp/resources/static/js/viewConfigMetadata.js
+++ webapp/resources/static/js/viewConfigMetadata.js
@@ -9,7 +9,7 @@ $('.ui.search').search({
         onResponse: function (casResp) {
             var response = {
                 results: {}
-            }
+            };
 
             for (var key in casResp) {
                 if (casResp.hasOwnProperty(key)) {
@@ -39,55 +39,55 @@ $('.ui.search').search({
     minCharacters: 3,
     templates: {
         message: function (response, type) {
-            var html = "";
-            $( "#accordion" ).empty();
+            var html = '';
+            $( '#accordion' ).empty();
 
-            if (type === "empty") {
-                html += "<h3><i class='fa fa-search' />&nbsp;<strong>No Results</strong></h3>"
-                html += "<div class='alert alert-warning'>";
-                html += "<i class='fa fa-exclamation-circle'/>&nbsp;"
-                html += "No search results could be found based on the provided query."
-                html += "</div>";
+            if (type === 'empty') {
+                html += '<h3><i class=\'fa fa-search\' />&nbsp;<strong>No Results</strong></h3>';
+                html += '<div class=\'alert alert-warning\'>';
+                html += '<i class=\'fa fa-exclamation-circle\'/>&nbsp;';
+                html += 'No search results could be found based on the provided query.';
+                html += '</div>';
             }
             for (var group in response.results) {
 
-                html += "<h2><i class='fa fa-users' />&nbsp;<strong>Group: </strong>" + group + "</h2>"
+                html += '<h2><i class=\'fa fa-users\' />&nbsp;<strong>Group: </strong>' + group + '</h2>';
                 var props = response.results[group].results;
 
-                html += "<div>";
+                html += '<div>';
                 for (var i = 0; i < props.length; i++) {
-                    html += "<p>";
-                    var prop = props[i]
-                    html += "<i class='fa fa-cogs'/>&nbsp;"
-                    html += "Property: <code>" + prop.title + "=" + prop.defaultValue + "</code><br/>"
+                    html += '<p>';
+                    var prop = props[i];
+                    html += '<i class=\'fa fa-cogs\'/>&nbsp;';
+                    html += 'Property: <code>' + prop.title + '=' + prop.defaultValue + '</code><br/>';
                     if (prop.deprecated) {
-                        html += "<p/><div class='alert alert-warning'>"
-                        html += "<i class='fa fa-exclamation-circle'/>&nbsp;"
-                        html += "This property is deprecated and will be removed in future CAS versions."
-                        html += "</div>"
+                        html += '<p/><div class=\'alert alert-warning\'>';
+                        html += '<i class=\'fa fa-exclamation-circle\'/>&nbsp;';
+                        html += 'This property is deprecated and will be removed in future CAS versions.';
+                        html += '</div>';
                     }
                     if (prop.requiredProperty) {
-                        html += "<p/><div class='alert alert-success'>"
-                        html += "<i class='fa fa-check-square'/>&nbsp;"
-                        html += "This presence of this property is required."
-                        html += "</div>"
+                        html += '<p/><div class=\'alert alert-success\'>';
+                        html += '<i class=\'fa fa-check-square\'/>&nbsp;';
+                        html += 'This presence of this property is required.';
+                        html += '</div>';
                     }
 
-                    html += "<i class='fa fa-codepen' />&nbsp;Type: <code>" + prop.type + "</code><br/>"
+                    html += '<i class=\'fa fa-codepen\' />&nbsp;Type: <code>' + prop.type + '</code><br/>';
                     if (prop.description != null) {
-                        html += "<p>" + prop.description + "</p>"
+                        html += '<p>' + prop.description + '</p>';
                     }
-                    html += "</p><br/>";
+                    html += '</p><br/>';
                 }
-                html += "</div>";
+                html += '</div>';
             }
 
-            $( "#accordion" ).html(html);
-            $( "#accordion" ).accordion({
-                heightStyle: "content"
+            $( '#accordion' ).html(html);
+            $( '#accordion' ).accordion({
+                heightStyle: 'content'
             });
-            $( "#accordion" ).accordion("refresh");
-            return "";
+            $( '#accordion' ).accordion('refresh');
+            return '';
         }
     }
-})
+});
