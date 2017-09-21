fa89143786675aba3ae0018ed7e92510c742fdbe
Misagh Moayyed <mm1844@gmail.com>
2017-09-21 22:05:25 +0430

    ensure mongo/redis ticket registries handle crypto ops

--- core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/redis/RedisTicketRegistryProperties.java
+++ core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/redis/RedisTicketRegistryProperties.java
@@ -1,5 +1,8 @@
 package org.apereo.cas.configuration.model.support.redis;
 
+import org.apereo.cas.configuration.model.core.util.EncryptionRandomizedSigningJwtCryptographyProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+
 /**
  * Configuration properties for Redis.
  *
@@ -8,5 +11,23 @@ package org.apereo.cas.configuration.model.support.redis;
  */
 public class RedisTicketRegistryProperties extends BaseRedisProperties {
     private static final long serialVersionUID = -2600996050439638782L;
-    
+
+    /**
+     * Crypto settings for the registry.
+     */
+    @NestedConfigurationProperty
+    private EncryptionRandomizedSigningJwtCryptographyProperties crypto = new EncryptionRandomizedSigningJwtCryptographyProperties();
+
+    public RedisTicketRegistryProperties() {
+        this.crypto.setEnabled(false);
+    }
+
+    public EncryptionRandomizedSigningJwtCryptographyProperties getCrypto() {
+        return crypto;
+    }
+
+    public void setCrypto(final EncryptionRandomizedSigningJwtCryptographyProperties crypto) {
+        this.crypto = crypto;
+    }
+
 }
