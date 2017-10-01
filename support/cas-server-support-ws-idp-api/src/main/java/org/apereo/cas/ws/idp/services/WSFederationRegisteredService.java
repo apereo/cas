10731a1913b2db7fb3e97ac3c4e5fe475c54f3e3
Misagh Moayyed <mmoayyed@unicon.net>
2017-10-01 11:36:12 +0330

    clean up dependencies

--- support/cas-server-support-ws-idp-api/src/main/java/org/apereo/cas/ws/idp/services/WSFederationRegisteredService.java
+++ support/cas-server-support-ws-idp-api/src/main/java/org/apereo/cas/ws/idp/services/WSFederationRegisteredService.java
@@ -6,6 +6,7 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apereo.cas.services.AbstractRegisteredService;
 import org.apereo.cas.services.RegexRegisteredService;
 import org.apereo.cas.ws.idp.WSFederationConstants;
+import org.javers.core.metamodel.annotation.TypeName;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
@@ -18,6 +19,7 @@ import javax.persistence.Entity;
  */
 @Entity
 @DiscriminatorValue("wsfed")
+@TypeName("WSFederationRegisteredService")
 public class WSFederationRegisteredService extends RegexRegisteredService {
     private static final long serialVersionUID = -3700571300568534062L;
 
