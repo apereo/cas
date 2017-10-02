eb6e72d8211631d0ff58fbe9a814703496d66811
Misagh Moayyed <mm1844@gmail.com>
2017-10-02 10:48:04 +0330

    Working on path watcher capability in mgmt app

--- core/cas-server-core-util/src/main/java/org/apereo/cas/util/io/FileWatcherService.java
+++ core/cas-server-core-util/src/main/java/org/apereo/cas/util/io/FileWatcherService.java
@@ -1,5 +1,8 @@
 package org.apereo.cas.util.io;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.util.function.Consumer;
 
@@ -10,9 +13,12 @@ import java.util.function.Consumer;
  * @since 5.2.0
  */
 public class FileWatcherService extends PathWatcherService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileWatcherService.class);
+    
     public FileWatcherService(final File watchableFile, final Consumer<File> onModify) {
         super(watchableFile.getParentFile(), file -> {
             if (file.getPath().equals(watchableFile.getPath())) {
+                LOGGER.debug("Detected change in file [{}] and calling change consumer to handle event", file);
                 onModify.accept(file);
             }
         });
--- webapp-mgmt/cas-management-webapp-support/src/main/java/org/apereo/cas/mgmt/config/CasManagementAuthenticationConfiguration.java
+++ webapp-mgmt/cas-management-webapp-support/src/main/java/org/apereo/cas/mgmt/config/CasManagementAuthenticationConfiguration.java
@@ -1,16 +1,11 @@
 package org.apereo.cas.mgmt.config;
 
 import org.apereo.cas.configuration.CasConfigurationProperties;
-import org.apereo.cas.configuration.model.webapp.mgmt.ManagementWebappProperties;
 import org.apereo.cas.mgmt.CasManagementUtils;
-import org.apereo.cas.mgmt.authz.CasRoleBasedAuthorizer;
-import org.apereo.cas.util.ResourceUtils;
 import org.pac4j.cas.client.direct.DirectCasClient;
 import org.pac4j.cas.config.CasConfiguration;
 import org.pac4j.core.authorization.authorizer.Authorizer;
 import org.pac4j.core.authorization.generator.AuthorizationGenerator;
-import org.pac4j.core.authorization.generator.FromAttributesAuthorizationGenerator;
-import org.pac4j.core.authorization.generator.SpringSecurityPropertiesAuthorizationGenerator;
 import org.pac4j.core.client.Client;
 import org.pac4j.core.client.direct.AnonymousClient;
 import org.pac4j.core.config.Config;
@@ -19,6 +14,7 @@ import org.pac4j.http.credentials.authenticator.IpRegexpAuthenticator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -29,7 +25,6 @@ import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 
 /**
  * This is {@link CasManagementAuthenticationConfiguration}.
@@ -48,13 +43,18 @@ public class CasManagementAuthenticationConfiguration {
     @Autowired
     private CasConfigurationProperties casProperties;
     
-    @ConditionalOnMissingBean(name = "requireAnyRoleAuthorizer")
-    @Bean
-    @RefreshScope
-    public Authorizer requireAnyRoleAuthorizer() {
-        return new CasRoleBasedAuthorizer(casProperties.getMgmt().getAdminRoles());
-    }
+    @Autowired
+    @Qualifier("requireAnyRoleAuthorizer")
+    private Authorizer requireAnyRoleAuthorizer;
+    
+    @Autowired
+    @Qualifier("authorizationGenerator")
+    private AuthorizationGenerator authorizationGenerator;
 
+    @Autowired
+    @Qualifier("staticAdminRolesAuthorizationGenerator")
+    private AuthorizationGenerator staticAdminRolesAuthorizationGenerator;
+    
     @ConditionalOnMissingBean(name = "authenticationClients")
     @Bean
     @RefreshScope
@@ -65,7 +65,7 @@ public class CasManagementAuthenticationConfiguration {
             LOGGER.debug("Configuring an authentication strategy based on CAS running at [{}]", casProperties.getServer().getName());
             final CasConfiguration cfg = new CasConfiguration(casProperties.getServer().getLoginUrl());
             final DirectCasClient client = new DirectCasClient(cfg);
-            client.setAuthorizationGenerator(authorizationGenerator());
+            client.setAuthorizationGenerator(authorizationGenerator);
             client.setName("CasClient");
             clients.add(client);
         } else {
@@ -76,7 +76,7 @@ public class CasManagementAuthenticationConfiguration {
             LOGGER.info("Configuring an authentication strategy based on authorized IP addresses matching [{}]", casProperties.getMgmt().getAuthzIpRegex());
             final IpClient ipClient = new IpClient(new IpRegexpAuthenticator(casProperties.getMgmt().getAuthzIpRegex()));
             ipClient.setName("IpClient");
-            ipClient.setAuthorizationGenerator(getStaticAdminRolesAuthorizationGenerator());
+            ipClient.setAuthorizationGenerator(staticAdminRolesAuthorizationGenerator);
             clients.add(ipClient);
         } else {
             LOGGER.debug("Skipping IP address authentication strategy configuration; no pattern is defined");
@@ -86,60 +86,18 @@ public class CasManagementAuthenticationConfiguration {
             LOGGER.warn("No authentication strategy is defined, CAS will establish an anonymous authentication mode whereby access is immediately granted. "
                     + "This may NOT be relevant for production purposes. Consider configuring alternative authentication strategies for maximum security.");
             final AnonymousClient anon = AnonymousClient.INSTANCE;
-            anon.setAuthorizationGenerator(getStaticAdminRolesAuthorizationGenerator());
+            anon.setAuthorizationGenerator(staticAdminRolesAuthorizationGenerator);
             clients.add(anon);
         }
         return clients;
     }
-
     
-
     @ConditionalOnMissingBean(name = "casManagementSecurityConfiguration")
     @Bean
     @RefreshScope
     public Config casManagementSecurityConfiguration() {
         final Config cfg = new Config(CasManagementUtils.getDefaultCallbackUrl(casProperties, serverProperties), authenticationClients());
-        cfg.setAuthorizer(requireAnyRoleAuthorizer());
+        cfg.setAuthorizer(requireAnyRoleAuthorizer);
         return cfg;
-    }
-
-    @RefreshScope
-    @Bean
-    public Properties userProperties() {
-        try {
-            final Properties p = new Properties();
-
-            final ManagementWebappProperties mgmt = casProperties.getMgmt();
-            if (ResourceUtils.doesResourceExist(mgmt.getUserPropertiesFile())) {
-                p.load(mgmt.getUserPropertiesFile().getInputStream());
-            } else {
-                LOGGER.warn("Could not locate [{}]", mgmt.getUserPropertiesFile());
-            }
-
-            return p;
-        } catch (final Exception e) {
-            throw new RuntimeException(e.getMessage(), e);
-        }
-    }
-
-    @ConditionalOnMissingBean(name = "authorizationGenerator")
-    @Bean
-    @RefreshScope
-    public AuthorizationGenerator authorizationGenerator() {
-        final List<String> authzAttributes = casProperties.getMgmt().getAuthzAttributes();
-        if (!authzAttributes.isEmpty()) {
-            if (authzAttributes.stream().anyMatch(a -> a.equals("*"))) {
-                return getStaticAdminRolesAuthorizationGenerator();
-            }
-            return new FromAttributesAuthorizationGenerator(authzAttributes.toArray(new String[]{}), new String[]{});
-        }
-        return new SpringSecurityPropertiesAuthorizationGenerator(userProperties());
-    }
-
-    private AuthorizationGenerator getStaticAdminRolesAuthorizationGenerator() {
-        return (context, profile) -> {
-            profile.addRoles(casProperties.getMgmt().getAdminRoles());
-            return profile;
-        };
     }
 }
