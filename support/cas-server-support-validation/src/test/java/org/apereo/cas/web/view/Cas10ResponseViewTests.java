36f69f5ee76b2fdbbd8525c9c32cf08130dd6366
Misagh Moayyed <mm1844@gmail.com>
2017-10-04 15:08:33 +0330

    fixed test cases

--- support/cas-server-support-validation/src/test/java/org/apereo/cas/web/view/Cas10ResponseViewTests.java
+++ support/cas-server-support-validation/src/test/java/org/apereo/cas/web/view/Cas10ResponseViewTests.java
@@ -27,13 +27,13 @@ public class Cas10ResponseViewTests {
     private Map<String, Object> model;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         this.model = new HashMap<>();
         final List<Authentication> list = new ArrayList<>();
         list.add(CoreAuthenticationTestUtils.getAuthentication("someothername"));
         this.model.put("assertion", new DefaultAssertionBuilder(
                 CoreAuthenticationTestUtils.getAuthentication()).with(list).with(
-                CoreAuthenticationTestUtils.getService("TestService")).with(true));
+                CoreAuthenticationTestUtils.getService("TestService")).with(true).build());
     }
 
     @Test
