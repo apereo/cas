304352d00cd0e07d395c7974dbb836e745ba8942
Misagh Moayyed <mm1844@gmail.com>
2017-10-05 12:29:57 -0700

    Removed Service Details functionality (#2979)

--- webapp-mgmt/cas-management-webapp/src/app/services/service.service.ts
+++ webapp-mgmt/cas-management-webapp/src/app/services/service.service.ts
@@ -2,7 +2,7 @@
  * Created by tschmidt on 2/13/17.
  */
 import {Injectable} from "@angular/core";
-import {ServiceDetails, ServiceItem} from "../../domain/service-view-bean";
+import {ServiceItem} from "../../domain/service-view-bean";
 import {Service} from "../service";
 import {Http} from "@angular/http";
 
@@ -17,16 +17,12 @@ export class ServiceViewService extends Service {
     return this.get<ServiceItem[]>("getServices?domain=" + domain);
   }
 
-  getDetails(id: String): Promise<ServiceDetails> {
-    return this.get<ServiceDetails>("serviceDetails?id=" + id);
-  }
-
   delete(id: number): Promise<String> {
     return this.get<String>("deleteRegisteredService?id=" + id);
   }
 
   updateOrder(a: ServiceItem, b: ServiceItem): Promise<String> {
-    return this.post<String>("updateOrder",[a,b]);
+    return this.post<String>("updateOrder",[a, b]);
   }
 
 }
