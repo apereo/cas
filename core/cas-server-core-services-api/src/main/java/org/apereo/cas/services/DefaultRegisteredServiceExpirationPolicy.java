c6dc6a5e79ef0f60e6c82a9f7f32388ff070df4f
Misagh Moayyed <mm1844@gmail.com>
2017-10-07 01:46:36 +0330

    add missing dep for tests

--- core/cas-server-core-services-api/src/main/java/org/apereo/cas/services/DefaultRegisteredServiceExpirationPolicy.java
+++ core/cas-server-core-services-api/src/main/java/org/apereo/cas/services/DefaultRegisteredServiceExpirationPolicy.java
@@ -6,6 +6,7 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 /**
  * This is {@link DefaultRegisteredServiceExpirationPolicy}.
@@ -34,6 +35,11 @@ public class DefaultRegisteredServiceExpirationPolicy implements RegisteredServi
 
     public DefaultRegisteredServiceExpirationPolicy(final boolean deleteWhenExpired,
                                                     final LocalDate expirationDate) {
+        this(deleteWhenExpired, false, expirationDate.toString());
+    }
+
+    public DefaultRegisteredServiceExpirationPolicy(final boolean deleteWhenExpired,
+                                                    final LocalDateTime expirationDate) {
         this(deleteWhenExpired, false, expirationDate.toString());
     }
 
