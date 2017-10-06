36585e5a3e3f49e10889bc27f5eb673fbe6eb862
Misagh Moayyed <mm1844@gmail.com>
2017-10-06 21:29:27 +0330

    Fix test cases

--- core/cas-server-core-services/src/test/java/org/apereo/cas/services/DefaultServicesManagerTests.java
+++ core/cas-server-core-services/src/test/java/org/apereo/cas/services/DefaultServicesManagerTests.java
@@ -4,5 +4,8 @@ package org.apereo.cas.services;
  * @author battags
  * @since 3.0.0
  */
-public class DefaultServicesManagerTests extends BaseServicesManagerTests {
+public class DefaultServicesManagerTests extends AbstractServicesManagerTests {
+    public DefaultServicesManagerTests() {
+        super();
+    }
 }
