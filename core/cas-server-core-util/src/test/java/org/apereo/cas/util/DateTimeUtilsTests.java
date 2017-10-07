85e70e1abddc2b05d7362312ce01149c4d274e73
Misagh Moayyed <mm1844@gmail.com>
2017-10-07 23:09:19 +0330

    updated

--- core/cas-server-core-util/src/test/java/org/apereo/cas/util/DateTimeUtilsTests.java
+++ core/cas-server-core-util/src/test/java/org/apereo/cas/util/DateTimeUtilsTests.java
@@ -16,27 +16,27 @@ import static org.junit.Assert.*;
 public class DateTimeUtilsTests {
 
     @Test
-    public void testParsingDateAsLocalDateTime() {
+    public void verifyParsingDateAsLocalDateTime() {
         assertNotNull(DateTimeUtils.localDateTimeOf(LocalDateTime.now().toString()));
     }
 
     @Test
-    public void testParsingDateAsLocalDate() {
+    public void verifyParsingDateAsLocalDate() {
         assertNotNull(DateTimeUtils.localDateTimeOf(LocalDate.now().toString()));
     }
 
     @Test
-    public void testParsingDateAsLocalDateString1() {
+    public void verifyParsingDateAsLocalDateString1() {
         assertNotNull(DateTimeUtils.localDateTimeOf("2017-10-15"));
     }
 
     @Test
-    public void testParsingDateAsLocalDateString2() {
+    public void verifyParsingDateAsLocalDateString2() {
         assertNotNull(DateTimeUtils.localDateTimeOf("09/19/2017"));
     }
 
     @Test
-    public void testParsingDateAsLocalDateString3() {
+    public void verifyParsingDateAsLocalDateString3() {
         assertNotNull(DateTimeUtils.localDateTimeOf("09/19/2017 4:30 pm"));
     }
 }
