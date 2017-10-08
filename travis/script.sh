06833081eca3fcd95826d07ab76f713b69d96358
Misagh Moayyed <mm1844@gmail.com>
2017-10-08 11:21:25 +0330

    Fix test cases

--- travis/script.sh
+++ travis/script.sh
@@ -1,10 +1,1 @@
 #!/bin/bash
-
-if [ "$TRAVIS_COMMIT_MESSAGE" == "[skip tests]" ]; then
-  echo -e "Travis CI build indicates that tests should be skipped because the commit message says so. Commit message is [$TRAVIS_COMMIT_MESSAGE].\n"
-elif [ "$PUBLISH_SNAPSHOTS" == "true" ]; then
-  echo -e "Travis CI build indicates that tests should be skipped because we are publishing snapshots. Tests should auto-run as part of Travis build matrix configuration.\n"
-else
-  echo -e "Travis CI build indicates that tests along with coveralls test coverage should run.\n"
-  sudo ./gradlew checkstyleTest test coveralls --stacktrace --parallel --build-cache --max-workers=8
-fi
