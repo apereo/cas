f6cb071138a707f6bb47d5c2cd0719a52ba4dc49
Misagh Moayyed <mm1844@gmail.com>
2017-10-08 11:44:06 +0330

    quiet build

--- travis/install.sh
+++ travis/install.sh
@@ -2,11 +2,16 @@
 
 gradle="sudo ./gradlew"
 gradleOptions="--stacktrace --parallel --build-cache --max-workers=8 --configure-on-demand --no-daemon"
-gradleBuild="checkstyleMain bootRepackage javadoc install"
+gradleBuild="checkstyleMain bootRepackage install"
 gradleTest=""
 
+if [ "$PUBLISH_SNAPSHOTS" == "false" ]; then
+    echo -e "The build will aggregate javadocs from all modules into one JAR file.\n"
+    gradleBuild="$gradleBuild aggregateJavadocsIntoJar"
+fi
+
 if [ "$PUBLISH_SNAPSHOTS" == "false" ] && [ "$TRAVIS_COMMIT_MESSAGE" != "[skip tests]" ]; then
-    echo -e "Travis CI build indicates that tests along with coveralls test coverage should run.\n"
+    echo -e "The build indicates that tests along with coveralls test coverage should run.\n"
     gradleTest="checkstyleTest test coveralls"
 fi
 
