24e0678a8c91d88fa8ecab59158a1ca01e381855
Misagh Moayyed <mm1844@gmail.com>
2017-10-08 13:19:01 +0330

    travisci build cleanup

--- travis/install.sh
+++ travis/install.sh
@@ -4,27 +4,27 @@ branchName="master"
 
 gradle="sudo ./gradlew"
 gradleOptions="--stacktrace --parallel --build-cache --max-workers=8 --configure-on-demand --no-daemon"
-gradleBuild="checkstyleMain bootRepackage install"
-gradleTest=""
+gradleBuild="bootRepackage install"
 gradleUpload=""
 
 if [ "$PUBLISH_SNAPSHOTS" == "false" ]; then
     echo -e "The build will aggregate javadocs from all modules into one JAR file.\n"
-    gradleBuild="$gradleBuild aggregateJavadocsIntoJar"
+    gradleBuild="$gradleBuild checkstyleMain aggregateJavadocsIntoJar"
+else
+    gradleBuild="$gradleBuild -x check aggregateJavadocsIntoJar"
+    if [ "$TRAVIS_COMMIT_MESSAGE" != "[skip tests]" ]; then
+        echo -e "The build indicates that tests along with coveralls test coverage should run.\n"
+        gradleBuild="$gradleBuild checkstyleTest test coveralls"
+    fi
 fi
 
-if [ "$PUBLISH_SNAPSHOTS" == "false" ] && [ "$TRAVIS_COMMIT_MESSAGE" != "[skip tests]" ]; then
-    echo -e "The build indicates that tests along with coveralls test coverage should run.\n"
-    gradleTest="checkstyleTest test coveralls"
-fi
-
-tasks="$gradle $gradleOptions $gradleBuild $gradleTest"
+tasks="$gradle $gradleOptions $gradleBuild"
 echo $tasks
 eval $tasks
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "$branchName" ] && [ "$PUBLISH_SNAPSHOTS" == "true" ]; then
-	echo -e "Th build will deploy SNAPSHOT artifacts to Sonatype under Travis job ${TRAVIS_JOB_NUMBER}"
-	gradleUpload="uploadArchives -x test -x check -x javadoc -DpublishSnapshots=true -DsonatypeUsername=${SONATYPE_USER} -DsonatypePassword=${SONATYPE_PWD}"
+	echo -e "The build will deploy SNAPSHOT artifacts to Sonatype under Travis job ${TRAVIS_JOB_NUMBER}"
+	gradleUpload="uploadArchives -x test -x check -x javadoc --offline -DpublishSnapshots=true -DsonatypeUsername=${SONATYPE_USER} -DsonatypePassword=${SONATYPE_PWD}"
 	upload="$gradle $gradleOptions $gradleUpload"
 	echo upload
     eval $upload
