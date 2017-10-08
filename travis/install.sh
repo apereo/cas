458d7ccec6ef4b3938441b831281f892d0f7232c
Misagh Moayyed <mm1844@gmail.com>
2017-10-08 11:21:25 +0330

    Fix test cases

--- travis/install.sh
+++ travis/install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-if [ "$PUBLISH_SNAPSHOTS" == "false" ]; then
-  echo -e "Generating javadocs will be skipped since Travis CI build indicates that we are not publishing snapshots.\n"
-  sudo ./gradlew checkstyleMain bootRepackage --parallel -x test -x javadoc --stacktrace --build-cache --max-workers=8 --configure-on-demand
-else
-  echo -e "Travis CI build indicates that tests should not run since we are publishing snapshots.\n"
-  sudo ./gradlew javadoc bootRepackage --parallel -x test --stacktrace --build-cache -x check --max-workers=8 --configure-on-demand
+gradle="sudo ./gradlew"
+gradleOptions="--stacktrace --parallel --build-cache --max-workers=8 --configure-on-demand --no-daemon"
+gradleBuild="checkstyleMain bootRepackage javadoc install"
+gradleTest=""
+
+if [ "$PUBLISH_SNAPSHOTS" == "false" ] && [ "$TRAVIS_COMMIT_MESSAGE" != "[skip tests]" ]; then
+    echo -e "Travis CI build indicates that tests along with coveralls test coverage should run.\n"
+    gradleTest="checkstyleTest test coveralls"
 fi
+
+tasks="$gradle $gradleOptions $gradleBuild $gradleTest"
+echo $tasks
+eval $tasks
