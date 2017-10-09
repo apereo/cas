da00ae8cdc7c85a2d14e90a02b7e3a6f99caaa2f
Misagh Moayyed <mm1844@gmail.com>
2017-10-09 11:55:43 +0330

    travisci build cleanup

--- travis/install.sh
+++ travis/install.sh
@@ -12,24 +12,31 @@ if [ "$PUBLISH_SNAPSHOTS" == "false" ]; then
     gradleBuild="$gradleBuild checkstyleMain aggregateJavadocsIntoJar"
 else
     gradleBuild="$gradleBuild -x check aggregateJavadocsIntoJar"
-    if [ "$TRAVIS_COMMIT_MESSAGE" != "[skip tests]" ]; then
+    if [ "$TRAVIS_COMMIT_MESSAGE" == "[skip tests]" ]; then
+        echo -e "The build indicates that tests should be skipped.\n"
+        gradleBuild="$gradleBuild -x test"
+    else
         echo -e "The build indicates that tests along with coveralls test coverage should run.\n"
         gradleBuild="$gradleBuild checkstyleTest test coveralls"
-    else
-        gradleBuild="$gradleBuild -x test"
     fi
 fi
 
 tasks="$gradle $gradleOptions $gradleBuild"
 echo $tasks
 eval $tasks
-echo "Gradle build finished at `date`"
+retVal=$?
+echo -e "Gradle build finished at `date` with exit code $retVal\n"
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "$branchName" ] && [ "$PUBLISH_SNAPSHOTS" == "true" ]; then
-	echo -e "The build will deploy SNAPSHOT artifacts to Sonatype under Travis job ${TRAVIS_JOB_NUMBER}"
-	gradleUpload="uploadArchives -x test -x check -x javadoc --offline -DpublishSnapshots=true -DsonatypeUsername=${SONATYPE_USER} -DsonatypePassword=${SONATYPE_PWD}"
-	upload="$gradle $gradleOptions $gradleUpload"
-	echo $upload
-    eval $upload
-    echo "Deploying snapshots finished at `date`"
+if [ ! $? -eq 0 ]; then
+    echo -e "Gradle build finished with an error exit code\n"
+    exit $retVal
+else
+    if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "$branchName" ] && [ "$PUBLISH_SNAPSHOTS" == "true" ]; then
+        echo -e "The build will deploy SNAPSHOT artifacts to Sonatype under Travis job ${TRAVIS_JOB_NUMBER}"
+        gradleUpload="uploadArchives -x test -x check -x javadoc --offline -DpublishSnapshots=true -DsonatypeUsername=${SONATYPE_USER} -DsonatypePassword=${SONATYPE_PWD}"
+        upload="$gradle $gradleOptions $gradleUpload"
+        echo $upload
+        eval $upload
+        echo -e "Deploying snapshots to sonatype finished at `date` \n"
+    fi
 fi
