d32831651538df83f3475ddc517e95fa2bff31c1
Misagh Moayyed <mm1844@gmail.com>
2017-10-09 12:18:22 +0330

    travisci build cleanup

--- travis/install.sh
+++ travis/install.sh
@@ -9,15 +9,16 @@ gradleBuild="bootRepackage"
 if [ "$PUBLISH_SNAPSHOTS" == "false" ]; then
     echo -e "The build will aggregate javadocs from all modules into one JAR file.\n"
     gradleBuild="$gradleBuild checkstyleMain aggregateJavadocsIntoJar"
-else
-    gradleBuild="$gradleBuild -x check aggregateJavadocsIntoJar"
     if [[ "${TRAVIS_COMMIT_MESSAGE}" == *"[skip tests]"* ]]; then
-        echo -e "The build indicates that tests should be skipped.\n"
+        echo -e "The build commit message indicates that tests should be skipped.\n"
         gradleBuild="$gradleBuild -x test"
     else
         echo -e "The build indicates that tests along with coveralls test coverage should run.\n"
         gradleBuild="$gradleBuild checkstyleTest test coveralls"
     fi
+else
+    echo -e "The build indicates that tests should be skipped since we are publishing snapshots.\n"
+    gradleBuild="$gradleBuild -x check -x test aggregateJavadocsIntoJar"
 fi
 
 tasks="$gradle $gradleOptions $gradleBuild"
@@ -32,9 +33,9 @@ if [ ! $? -eq 0 ]; then
 else
     if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "$branchName" ] && [ "$PUBLISH_SNAPSHOTS" == "true" ]; then
         if [[ "${TRAVIS_COMMIT_MESSAGE}" == *"[skip snapshots]"* ]]; then
-             echo -e "The build will skip deploying SNAPSHOT artifacts to Sonatype under Travis job ${TRAVIS_JOB_NUMBER}"
+             echo -e "The build will skip deploying snapshot artifacts to Sonatype under Travis job ${TRAVIS_JOB_NUMBER}"
         else
-            echo -e "The build will deploy SNAPSHOT artifacts to Sonatype under Travis job ${TRAVIS_JOB_NUMBER}"
+            echo -e "The build will deploy snapshot artifacts to Sonatype under Travis job ${TRAVIS_JOB_NUMBER}"
             gradleUpload="uploadArchives -x test -x check -x javadoc --offline -DpublishSnapshots=true -DsonatypeUsername=${SONATYPE_USER} -DsonatypePassword=${SONATYPE_PWD}"
             upload="$gradle $gradleOptions $gradleUpload"
             echo $upload
