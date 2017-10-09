388d8c05e3955951e666faa03a35e29e95eb57df
Misagh Moayyed <mm1844@gmail.com>
2017-10-09 12:11:04 +0330

    travisci build cleanup

--- travis/script.sh
+++ travis/script.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+# This file serves as a placeholder for Travis CI's script phase.
+# It intentionally does nothing in order to override Travis CI's built-in commands for this phase.
+# All relevant checks and commands execute as part of the install phase, indicated by the install.sh script.
