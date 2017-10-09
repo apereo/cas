4f309157eff263bf99fba6f6285f73d0ba522d29
michalmmx <32496841+michalmmx@users.noreply.github.com>
2017-10-09 14:15:46 +0200

    Update RestAuthenticationHandler.java
    changed CredentialExpiredException to AccountPasswordMustChangeException

--- support/cas-server-support-rest-authentication/src/main/java/org/apereo/cas/adaptors/rest/RestAuthenticationHandler.java
+++ support/cas-server-support-rest-authentication/src/main/java/org/apereo/cas/adaptors/rest/RestAuthenticationHandler.java
@@ -2,6 +2,7 @@ package org.apereo.cas.adaptors.rest;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apereo.cas.authentication.exceptions.AccountDisabledException;
+import org.apereo.cas.authentication.exceptions.AccountPasswordMustChangeException;
 import org.apereo.cas.authentication.HandlerResult;
 import org.apereo.cas.authentication.PreventedException;
 import org.apereo.cas.authentication.UsernamePasswordCredential;
@@ -16,7 +17,6 @@ import javax.security.auth.login.AccountLockedException;
 import javax.security.auth.login.AccountNotFoundException;
 import javax.security.auth.login.FailedLoginException;
 import javax.security.auth.login.CredentialExpiredException;
-import java.security.GeneralSecurityException;
 import java.util.ArrayList;
 
 /**
@@ -66,7 +66,7 @@ public class RestAuthenticationHandler extends AbstractUsernamePasswordAuthentic
                 throw new AccountLockedException("Could not authenticate locked account for " + c.getUsername());
             }
             if (e.getStatusCode() == HttpStatus.PRECONDITION_REQUIRED) {
-                throw new CredentialExpiredException("Account password must change for " + c.getUsername());
+                throw new AccountPasswordMustChangeException("Account password must change for " + c.getUsername());
             }
             if (e.getStatusCode() == HttpStatus.PRECONDITION_FAILED) {
                 throw new AccountExpiredException("Could not authenticate expired account for " + c.getUsername());
