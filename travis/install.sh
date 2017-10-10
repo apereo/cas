00afa6814326c0afabed2dac473acd96d102871a
Misagh Moayyed <mm1844@gmail.com>
2017-10-11 03:15:28 +0330

    working on snapshots

--- travis/install.sh
+++ travis/install.sh
@@ -7,16 +7,12 @@ fi
 
 branchName="master"
 gradle="sudo ./gradlew"
-
-gradleBuildOptions="--stacktrace --parallel --build-cache"
+gradleOptions="--stacktrace --parallel --build-cache --configure-on-demand --no-daemon"
 gradleBuild="clean bootRepackage install"
-
-gradleUploadOptions="--parallel"
-gradleUpload="aggregateJavadocsIntoJar uploadArchives --parallel -DpublishSnapshots=true -DsonatypeUsername=${SONATYPE_USER} -DsonatypePassword=${SONATYPE_PWD}"
 
 if [ "$PUBLISH_SNAPSHOTS" == "false" ]; then
     echo -e "The build will aggregate javadocs from all modules into one JAR file.\n"
-    gradleBuild="$gradleBuild checkstyleMain"
+    gradleBuild="$gradleBuild checkstyleMain aggregateJavadocsIntoJar"
     if [[ "${TRAVIS_COMMIT_MESSAGE}" == *"[skip tests]"* ]]; then
         echo -e "The build commit message indicates that tests should be skipped.\n"
         gradleBuild="$gradleBuild -x test"
@@ -29,23 +25,22 @@ if [ "$PUBLISH_SNAPSHOTS" == "false" ]; then
     fi
 else
     echo -e "The build indicates that tests should be skipped since we are publishing snapshots.\n"
-    gradleBuild="$gradleBuild -x check -x test"
+    gradleBuild="$gradleBuild -x check -x test aggregateJavadocsIntoJar"
 fi
 
-tasks="$gradle $gradleBuild $gradleBuildOptions"
+tasks="$gradle $gradleOptions $gradleBuild"
 echo $tasks
 eval $tasks
 retVal=$?
-echo "***********************************************************"
-echo -e "Gradle build finished at `date` with exit code $retVal"
-echo "***********************************************************"
+echo -e "Gradle build finished at `date` with exit code $retVal\n"
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ $retVal == 0 ] && [ "$TRAVIS_BRANCH" == "$branchName" ] && [ "$PUBLISH_SNAPSHOTS" == "true" ]; then
     if [[ "${TRAVIS_COMMIT_MESSAGE}" == *"[skip snapshots]"* ]]; then
          echo -e "The build will skip deploying snapshot artifacts to Sonatype under Travis job ${TRAVIS_JOB_NUMBER}"
     else
         echo -e "The build will deploy snapshot artifacts to Sonatype under Travis job ${TRAVIS_JOB_NUMBER}"
-        upload="$gradle $gradleUpload $gradleUploadOptions"
+        gradleUpload="uploadArchives -x test -x check -x javadoc -DpublishSnapshots=true -DsonatypeUsername=${SONATYPE_USER} -DsonatypePassword=${SONATYPE_PWD}"
+        upload="$gradle $gradleOptions $gradleUpload"
         echo $upload
         eval $upload
         retVal=$?
