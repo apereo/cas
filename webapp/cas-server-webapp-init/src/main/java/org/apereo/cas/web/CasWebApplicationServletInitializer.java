72179246406b973349dc3fc97c3d42962e8fda1d
Misagh Moayyed <mmoayyed@unicon.net>
2017-10-11 22:06:58 +0330

    Remove workaround for servlet initializer logging

--- webapp/cas-server-webapp-init/src/main/java/org/apereo/cas/web/CasWebApplicationServletInitializer.java
+++ webapp/cas-server-webapp-init/src/main/java/org/apereo/cas/web/CasWebApplicationServletInitializer.java
@@ -1,15 +1,9 @@
 package org.apereo.cas.web;
 
 import org.apereo.cas.CasEmbeddedContainerUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.WebApplicationContext;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletContextEvent;
 import java.util.Map;
 
 /**
@@ -19,8 +13,6 @@ import java.util.Map;
  * @since 5.0.0
  */
 public class CasWebApplicationServletInitializer extends SpringBootServletInitializer {
-    private static final Logger LOGGER = LoggerFactory.getLogger(CasWebApplicationServletInitializer.class);
-    
     @Override
     protected SpringApplicationBuilder configure(final SpringApplicationBuilder builder) {
         final Map<String, Object> properties = CasEmbeddedContainerUtils.getRuntimeProperties(Boolean.FALSE);
@@ -28,25 +20,5 @@ public class CasWebApplicationServletInitializer extends SpringBootServletInitia
                 .sources(CasWebApplication.class)
                 .properties(properties)
                 .banner(CasEmbeddedContainerUtils.getCasBannerInstance());
-    }
-
-    /**
-     * {@inheritDoc}
-     * Overrides the parent method to remove a pre-mature logging operation via (<code>LogFactory#getLog(getClass());</code>)
-     * which forces log4j to locate the configuration file from its known locations (i.e. classpath)
-     * before Spring Boot has had a chance to consult properties, etc.
-     * @param servletContext the context
-     */
-    @Override
-    public void onStartup(final ServletContext servletContext) {
-        final WebApplicationContext rootAppContext = this.createRootApplicationContext(servletContext);
-        if (rootAppContext != null) {
-            servletContext.addListener(new ContextLoaderListener(rootAppContext) {
-                public void contextInitialized(final ServletContextEvent event) {
-                }
-            });
-        } else {
-            LOGGER.debug("No ContextLoaderListener registered, as createRootApplicationContext() did not return an application context");
-        }
     }
 }
