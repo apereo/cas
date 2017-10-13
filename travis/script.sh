04cf72cdf683682172f76240a65fcdad5777391e
Misagh Moayyed <mm1844@gmail.com>
2017-10-13 23:43:10 +0330

    remove/increase timeout

--- travis/script.sh
+++ travis/script.sh
@@ -2,7 +2,7 @@
 
 branchName="master"
 gradle="sudo ./gradlew"
-gradleUpload="uploadArchives -x test -x check"
+gradleUpload="uploadArchives -x test -x check -x build"
 gradleUploadOptions="-DpublishSnapshots=true -DsonatypeUsername=${SONATYPE_USER} -DsonatypePassword=${SONATYPE_PWD}"
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "$branchName" ] && [ "$PUBLISH_SNAPSHOTS" == "true" ]; then
