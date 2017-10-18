d47312c7bab9b5beea0d4f5bf8394e6f6f4d6333
philliprower <phillip.rower@presidiohealth.com>
2017-10-18 13:57:52 -0700

    Update for codacy.

--- core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/oauth/OAuthProperties.java
+++ core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/oauth/OAuthProperties.java
@@ -51,7 +51,7 @@ public class OAuthProperties {
         return throttler;
     }
 
-    public void setThrottler(String throttler) {
+    public void setThrottler(final String throttler) {
         this.throttler = throttler;
     }
 
