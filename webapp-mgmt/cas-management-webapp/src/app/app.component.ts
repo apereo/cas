273daad1836a36270067466d99d2becdab4d333f
Misagh Moayyed <mm1844@gmail.com>
2017-10-21 22:55:10 -0700

    Mgmt: fix issue with the angular router crashing on homepage refreshes (#3020)
    * Closes Issue #35 - Page refresh causes app to be unavailable
    
    * Added response back to fix tests
    
    * Setup app-controls, UI improvements, made more responsive
    
    * Fixed Form save to use the controls component
    
    * Refactored datasource functionality
    
    * fix Checkstyle errors

--- webapp-mgmt/cas-management-webapp/src/app/app.component.ts
+++ webapp-mgmt/cas-management-webapp/src/app/app.component.ts
@@ -1,7 +1,5 @@
-import {Component, ElementRef, OnInit} from '@angular/core';
-import {Router} from "@angular/router";
+import {Component, OnInit} from '@angular/core';
 import {Messages} from "./messages";
-import {element} from "protractor";
 
 @Component({
   selector: 'app-root',
@@ -10,20 +8,11 @@ import {element} from "protractor";
 
 export class AppComponent implements OnInit {
 
-  type: String;
-
-  constructor(private router: Router,
-              public messages: Messages,
-              public elRef: ElementRef) {
-    this.type = this.elRef.nativeElement.getAttribute('type');
+  constructor(public messages: Messages) {
   }
 
   ngOnInit() {
-    if (this.type === "DOMAIN") {
-      this.router.navigate(['/domains']);
-    } else {
-      this.router.navigate(['services','default']);
-    }
+
   }
 
 }
