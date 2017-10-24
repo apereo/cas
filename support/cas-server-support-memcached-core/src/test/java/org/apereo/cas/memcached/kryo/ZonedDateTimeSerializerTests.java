e644caf42b1712ad32c7566489078c5ff336b1d7
Misagh Moayyed <mm1844@gmail.com>
2017-10-24 22:48:40 +0330

    troubleshooting memcache's zoneddatetime serializer

--- support/cas-server-support-memcached-core/src/test/java/org/apereo/cas/memcached/kryo/ZonedDateTimeSerializerTests.java
+++ support/cas-server-support-memcached-core/src/test/java/org/apereo/cas/memcached/kryo/ZonedDateTimeSerializerTests.java
@@ -1,7 +1,6 @@
 package org.apereo.cas.memcached.kryo;
 
 import com.esotericsoftware.kryo.io.ByteBufferOutput;
-import org.apereo.cas.memcached.kryo.serial.ZonedDateTimeSerializer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -17,15 +16,14 @@ import java.time.ZonedDateTime;
  */
 @RunWith(JUnit4.class)
 public class ZonedDateTimeSerializerTests {
-    
+
     @Test
     public void verifyTranscoderWorks() {
         final CasKryoPool pool = new CasKryoPool();
         try (CloseableKryo kryo = pool.borrow()) {
-            final ZonedDateTimeSerializer transcoder = new ZonedDateTimeSerializer();
             final ByteBufferOutput output = new ByteBufferOutput(2048);
-            transcoder.write(kryo, output, ZonedDateTime.now(ZoneOffset.UTC));
+            kryo.writeObject(output, ZonedDateTime.now(ZoneOffset.UTC));
+            kryo.writeObject(output, ZonedDateTime.now());
         }
-        
     }
 }
