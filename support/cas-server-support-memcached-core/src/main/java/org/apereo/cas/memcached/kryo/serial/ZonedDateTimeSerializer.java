fc19a2a5e5e77dfbdc97c6f4a6d9e30787743e3f
Misagh Moayyed <mm1844@gmail.com>
2017-10-25 21:41:18 +0330

    fix minor issue with kryo parsing zoned date time

--- support/cas-server-support-memcached-core/src/main/java/org/apereo/cas/memcached/kryo/serial/ZonedDateTimeSerializer.java
+++ support/cas-server-support-memcached-core/src/main/java/org/apereo/cas/memcached/kryo/serial/ZonedDateTimeSerializer.java
@@ -4,6 +4,7 @@ import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.Serializer;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
+import org.apache.commons.lang3.StringUtils;
 import org.apereo.cas.util.DateTimeUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,14 +18,14 @@ import java.time.ZonedDateTime;
  */
 public class ZonedDateTimeSerializer extends Serializer<ZonedDateTime> {
     private static final Logger LOGGER = LoggerFactory.getLogger(ZonedDateTimeSerializer.class);
-    
+
     @Override
     public void write(final Kryo kryo, final Output output, final ZonedDateTime dateTime) {
         LOGGER.trace("Writing date/time [{}] to memcached", dateTime);
         final long epochMilli = dateTime.toInstant().toEpochMilli();
         LOGGER.trace("Writing date/time epoch milliseconds [{}] to memcached", epochMilli);
         kryo.writeObject(output, epochMilli);
-        
+
         final String id = dateTime.getZone().getId();
         LOGGER.trace("Writing date/time zone id [{}] to memcached", id);
         kryo.writeObject(output, id);
@@ -33,8 +34,9 @@ public class ZonedDateTimeSerializer extends Serializer<ZonedDateTime> {
     @Override
     public ZonedDateTime read(final Kryo kryo, final Input input, final Class<ZonedDateTime> type) {
         final long time = kryo.readObject(input, Long.class);
-        final String zoneId = input.readString();
+        final String zoneId = StringUtils.removeAll(input.readString().trim(), "\\p{C}");
         try {
+            LOGGER.trace("Reading zoned date time instance with time [{}] and zone id [{}]", time, zoneId);
             final ZoneId zone = ZoneId.of(zoneId);
             return DateTimeUtils.zonedDateTimeOf(time, zone);
         } catch (final Exception e) {
