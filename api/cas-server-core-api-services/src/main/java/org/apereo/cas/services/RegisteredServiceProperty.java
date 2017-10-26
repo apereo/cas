8e28bc8ea326c35178d6e8f04259e47f2cb04901
Misagh Moayyed <mmoayyed@unicon.net>
2017-10-26 19:17:07 +0330

    Encapsulate CAS properties in one single spot

--- api/cas-server-core-api-services/src/main/java/org/apereo/cas/services/RegisteredServiceProperty.java
+++ api/cas-server-core-api-services/src/main/java/org/apereo/cas/services/RegisteredServiceProperty.java
@@ -1,8 +1,11 @@
 package org.apereo.cas.services;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import org.apache.commons.lang3.BooleanUtils;
 
 import java.io.Serializable;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 /**
@@ -16,6 +19,89 @@ import java.util.Set;
 public interface RegisteredServiceProperty extends Serializable {
 
     /**
+     * Collection of supported properties that control various functionality in CAS.
+     */
+    enum RegisteredServiceProperties {
+
+        /**
+         * using when delegating authentication to ADFS to indicate the relying party identifier.
+         */
+        WSFED_RELYING_PARTY_ID("wsfed.relyingPartyIdentifier"),
+        /**
+         * Produce a JWT as a response.
+         **/
+        TOKEN_AS_RESOPONSE("jwtAsResponse"),
+
+        /**
+         * Jwt signing secret defined for a given service.
+         **/
+        TOKEN_SECRET_SIGNING("jwtSigningSecret"),
+
+        /**
+         * Jwt signing secret alg defined for a given service.
+         **/
+        TOKEN_SECRET_SIGNING_ALG("jwtSigningSecretAlg"),
+
+        /**
+         * Jwt encryption secret defined for a given service.
+         **/
+        TOKEN_SECRET_ENCRYPTION("jwtEncryptionSecret"),
+
+        /**
+         * Jwt encryption secret alg defined for a given service.
+         **/
+        TOKEN_SECRET_ENCRYPTION_ALG("jwtEncryptionSecretAlg"),
+
+        /**
+         * Jwt encryption secret method defined for a given service.
+         **/
+        TOKEN_SECRET_ENCRYPTION_METHOD("jwtEncryptionSecretMethod"),
+
+        /**
+         * Secrets are Base64 encoded.
+         **/
+        TOKEN_SECRETS_ARE_BASE64_ENCODED("jwtSecretsAreBase64Encoded");
+
+        private final String propertyName;
+
+        RegisteredServiceProperties(final String name) {
+            this.propertyName = name;
+        }
+
+        public String getPropertyName() {
+            return propertyName;
+        }
+
+        public RegisteredServiceProperty getPropertyValue(final RegisteredService service) {
+            if (isAssignedTo(service)) {
+                final Optional<Map.Entry<String, RegisteredServiceProperty>> property = service.getProperties()
+                        .entrySet()
+                        .stream()
+                        .filter(entry -> entry.getKey().equalsIgnoreCase(getPropertyName()) && BooleanUtils.toBoolean(entry.getValue().getValue()))
+                        .distinct()
+                        .findFirst();
+                if (property.isPresent()) {
+                    return property.get().getValue();
+                }
+            }
+            return null;
+        }
+
+        /**
+         * Check to see if the property is assigned to this service and is defined with a value.
+         *
+         * @param service registered service
+         * @return true/false
+         */
+        public boolean isAssignedTo(final RegisteredService service) {
+            return service.getProperties()
+                    .entrySet()
+                    .stream()
+                    .anyMatch(entry -> entry.getKey().equalsIgnoreCase(getPropertyName()) && BooleanUtils.toBoolean(entry.getValue().getValue()));
+        }
+    }
+
+    /**
      * Gets values.
      *
      * @return the values
@@ -24,7 +110,6 @@ public interface RegisteredServiceProperty extends Serializable {
 
     /**
      * Gets the first single value.
-     *
      *
      * @return the value, or null if the collection is empty.
      */
