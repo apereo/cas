50cd874efe977792987cf7d3791e527c90247c2c
Misagh Moayyed <mmoayyed@unicon.net>
2017-10-27 19:42:38 +0330

    remove misc module

--- support/cas-server-support-saml-idp-core/src/main/java/org/apereo/cas/support/saml/services/SamlRegisteredService.java
+++ support/cas-server-support-saml-idp-core/src/main/java/org/apereo/cas/support/saml/services/SamlRegisteredService.java
@@ -6,7 +6,6 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apereo.cas.services.AbstractRegisteredService;
 import org.apereo.cas.services.RegexRegisteredService;
 import org.apereo.cas.services.RegisteredService;
-import org.javers.core.metamodel.annotation.TypeName;
 
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
@@ -25,7 +24,6 @@ import java.util.TreeMap;
  */
 @Entity
 @DiscriminatorValue("saml")
-@TypeName("SamlRegisteredService")
 public class SamlRegisteredService extends RegexRegisteredService {
     private static final long serialVersionUID = 1218757374062931021L;
 
