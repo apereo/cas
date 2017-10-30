54d4580b5c60bb7989273d658ef96b44c81323da
Misagh Moayyed <mm1844@gmail.com>
2017-10-30 22:36:09 +0330

    Working on discovery profile

--- support/cas-server-support-discovery-profile/src/main/java/org/apereo/cas/config/CasDiscoveryProfileConfiguration.java
+++ support/cas-server-support-discovery-profile/src/main/java/org/apereo/cas/config/CasDiscoveryProfileConfiguration.java
@@ -2,7 +2,9 @@ package org.apereo.cas.config;
 
 import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.discovery.CasServerProfileRegistrar;
+import org.apereo.cas.services.ServicesManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +21,10 @@ import org.springframework.context.annotation.Configuration;
 public class CasDiscoveryProfileConfiguration {
 
     @Autowired
+    @Qualifier("servicesManager")
+    private ServicesManager servicesManager;
+    
+    @Autowired
     private CasConfigurationProperties casProperties;
 
     @Autowired
@@ -26,6 +32,6 @@ public class CasDiscoveryProfileConfiguration {
     
     @Bean
     public CasServerProfileRegistrar casServerProfileRegistrar() {
-        return new CasServerProfileRegistrar();
+        return new CasServerProfileRegistrar(this.servicesManager);
     }
 }
