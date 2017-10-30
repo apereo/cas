cc4c9a66a65d3b997c42e381f34bf483e42e8a6c
Misagh Moayyed <mm1844@gmail.com>
2017-10-30 15:01:20 +0330

    checkstyle fixes

--- support/cas-server-support-mongo-core/src/main/java/org/apereo/cas/mongo/MongoDbConnectionFactory.java
+++ support/cas-server-support-mongo-core/src/main/java/org/apereo/cas/mongo/MongoDbConnectionFactory.java
@@ -175,7 +175,7 @@ public class MongoDbConnectionFactory {
 
     private Set<Class<?>> scanForEntities(final String basePackage) {
         if (!StringUtils.isBlank(basePackage)) {
-            return new HashSet<>();
+            return new HashSet<>(0);
         }
 
         final Set<Class<?>> initialEntitySet = new HashSet<>();
