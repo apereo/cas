8a52b698f344da5176fd76fdc85c072b1d53779b
Misagh Moayyed <mmoayyed@unicon.net>
2017-10-31 09:57:50 -0700

    Management webapp: UI bug fixes (#3035)
    * Closes Issue #65 - Remove WS Fed Checkbox
    
    * Closes Issue #66 - OIDC Client options
    
    * Closes Issue #69 - New Service Breaks Expiration tab
    
    * Closes Issue #67 - Make Description and Realm optional
    
    * Fix Expiration
    
    * Added Footer
    
    * Closes Issue #68 - Move OIDC Scopes to Attribute Release tab
    
    * Removed back button
    Refactored Footer to separate component

---
+++ webapp-mgmt/cas-management-webapp/src/app/form/tab-oidc/tab-oidc.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+import {TabBaseComponent} from '../tab-base';
+
+@Component({
+  selector: 'app-tab-oidc',
+  templateUrl: './tab-oidc.component.html',
+  styleUrls: ['./tab-oidc.component.css']
+})
+export class TabOIDCComponent extends TabBaseComponent {}
