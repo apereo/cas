f9cc9d001a4cf2caa9b9b09cd22924ec68e76750
Misagh Moayyed <mm1844@gmail.com>
2017-10-31 19:38:07 +0330

    more verbose output for scrutinizer

--- ci/install.sh
+++ ci/install.sh
@@ -5,7 +5,7 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$PUBLISH_SNAPSHOTS" == "true" ]; 
     exit 0
 fi
 
-gradle="sudo ./gradlew"
+gradle="sudo ./gradlew $@"
 
 gradleBuildOptions="--stacktrace --parallel"
 gradleBuild="assemble"
