06d5f9ca90b6dae530f012849afde2a0318cc677
Misagh Moayyed <mm1844@gmail.com>
2017-11-02 15:14:52 +0330

    doc updates

--- core/cas-server-core-webflow/src/main/java/org/apereo/cas/web/flow/actions/AbstractAuthenticationAction.java
+++ core/cas-server-core-webflow/src/main/java/org/apereo/cas/web/flow/actions/AbstractAuthenticationAction.java
@@ -39,8 +39,8 @@ public abstract class AbstractAuthenticationAction extends AbstractAction {
 
     @Override
     protected Event doExecute(final RequestContext requestContext) {
-        final String agent = WebUtils.getHttpServletRequestUserAgent();
-        final GeoLocationRequest geoLocation = WebUtils.getHttpServletRequestGeoLocation();
+        final String agent = WebUtils.getHttpServletRequestUserAgentFromRequestContext();
+        final GeoLocationRequest geoLocation = WebUtils.getHttpServletRequestGeoLocationFromRequestContext();
 
         if (!adaptiveAuthenticationPolicy.apply(agent, geoLocation)) {
             final String msg = "Adaptive authentication policy does not allow this request for " + agent + " and " + geoLocation;
