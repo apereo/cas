e7250c5b93b5635ebfc9d62b9c926903366b2443
Misagh Moayyed <mmoayyed@unicon.net>
2017-11-03 01:52:45 +0330

    fix signing/encryption of ciphers in optional mode

--- support/cas-server-support-token-core/src/main/java/org/apereo/cas/config/TokenCoreConfiguration.java
+++ support/cas-server-support-token-core/src/main/java/org/apereo/cas/config/TokenCoreConfiguration.java
@@ -1,5 +1,6 @@
 package org.apereo.cas.config;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apereo.cas.CipherExecutor;
 import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.configuration.model.core.util.EncryptionOptionalSigningJwtCryptographyProperties;
@@ -7,7 +8,10 @@ import org.apereo.cas.ticket.ExpirationPolicy;
 import org.apereo.cas.token.JWTTokenTicketBuilder;
 import org.apereo.cas.token.TokenTicketBuilder;
 import org.apereo.cas.token.cipher.TokenTicketCipherExecutor;
+import org.apereo.cas.util.cipher.NoOpCipherExecutor;
 import org.jasig.cas.client.validation.AbstractUrlBasedTicketValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
@@ -28,7 +32,8 @@ import org.springframework.core.Ordered;
 @EnableConfigurationProperties(CasConfigurationProperties.class)
 @AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE)
 public class TokenCoreConfiguration {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(TokenCoreConfiguration.class);
+    
     @Autowired
     private CasConfigurationProperties casProperties;
 
@@ -45,9 +50,22 @@ public class TokenCoreConfiguration {
     @ConditionalOnMissingBean(name = "tokenCipherExecutor")
     public CipherExecutor tokenCipherExecutor() {
         final EncryptionOptionalSigningJwtCryptographyProperties crypto = casProperties.getAuthn().getToken().getCrypto();
-        return new TokenTicketCipherExecutor(crypto.getEncryption().getKey(),
-                crypto.getSigning().getKey(),
-                crypto.getAlg(), crypto.isEncryptionEnabled());
+        boolean enabled = crypto.isEnabled();
+        if (!enabled && (StringUtils.isNotBlank(crypto.getEncryption().getKey())) && StringUtils.isNotBlank(crypto.getSigning().getKey())) {
+            LOGGER.warn("Token encryption/signing is not enabled explicitly in the configuration, yet signing/encryption keys "
+                    + "are defined for operations. CAS will proceed to enable the token encryption/signing functionality.");
+            enabled = true;
+        }
+        
+        if (enabled) {
+            return new TokenTicketCipherExecutor(crypto.getEncryption().getKey(),
+                    crypto.getSigning().getKey(),
+                    crypto.getAlg(), crypto.isEncryptionEnabled());
+        }
+        LOGGER.info("Token cookie encryption/signing is turned off. This "
+                + "MAY NOT be safe in a production environment. Consider using other choices to handle encryption, "
+                + "signing and verification of generated tokens.");
+        return NoOpCipherExecutor.getInstance();
     }
 
     @RefreshScope
