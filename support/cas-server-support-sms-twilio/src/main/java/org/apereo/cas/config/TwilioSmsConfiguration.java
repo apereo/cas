d55aacec33ee7eb6ab108e8d4e37093507482ede
Misagh Moayyed <mm1844@gmail.com>
2017-11-02 11:41:53 +0330

    fix typos

--- support/cas-server-support-sms-twilio/src/main/java/org/apereo/cas/config/TwilioSmsConfiguration.java
+++ support/cas-server-support-sms-twilio/src/main/java/org/apereo/cas/config/TwilioSmsConfiguration.java
@@ -1,7 +1,7 @@
 package org.apereo.cas.config;
 
 import org.apereo.cas.configuration.CasConfigurationProperties;
-import org.apereo.cas.support.sms.TwillioSmsSender;
+import org.apereo.cas.support.sms.TwilioSmsSender;
 import org.apereo.cas.util.io.SmsSender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -10,23 +10,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.util.Assert;
 
 /**
- * This is {@link TwillioSmsConfiguration}.
+ * This is {@link TwilioSmsConfiguration}.
  *
  * @author Misagh Moayyed
  * @since 5.1.0
  */
-@Configuration("twillioSmsConfiguration")
+@Configuration("twilioSmsConfiguration")
 @EnableConfigurationProperties(CasConfigurationProperties.class)
-public class TwillioSmsConfiguration {
+public class TwilioSmsConfiguration {
     @Autowired
     private CasConfigurationProperties casProperties;
     
     @Bean
     public SmsSender smsSender() {
-        Assert.notNull(casProperties.getTwillio().getAccountId(), "Twillio account id cannot be blank");
-        Assert.notNull(casProperties.getTwillio().getToken(), "Twillio token cannot be blank");
-        
-        return new TwillioSmsSender(casProperties.getTwillio().getAccountId(),
-                casProperties.getTwillio().getToken());
+        Assert.notNull(casProperties.getTwilio().getAccountId(), "Twilio account id cannot be blank");
+        Assert.notNull(casProperties.getTwilio().getToken(), "Twilio token cannot be blank");
+        return new TwilioSmsSender(casProperties.getTwilio().getAccountId(), casProperties.getTwilio().getToken());
     }
 }
