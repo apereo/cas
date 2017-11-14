ea1556891290609966bd8c1f3052b3a7c1d22380
Travis Schmidt <tsschmidt@ucdavis.edu>
2017-11-13 16:06:04 -0800

    Refactored form tables to use RowDataSource

--- webapp-mgmt/cas-management-webapp/src/app/form/rejectedattributes/rejectedattributes.component.ts
+++ webapp-mgmt/cas-management-webapp/src/app/form/rejectedattributes/rejectedattributes.component.ts
@@ -1,14 +1,10 @@
 import {Component, OnInit, Input} from '@angular/core';
 import {Messages} from '../../messages';
-import {AbstractRegisteredService} from '../../../domain/registered-service';
 import {Data} from '../data';
-import {DataSource} from '@angular/cdk/table';
-import {BehaviorSubject} from 'rxjs/BehaviorSubject';
-import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/startWith';
 import 'rxjs/add/observable/merge';
 import 'rxjs/add/operator/map';
-import {Util} from '../../util/util';
+import {Row, RowDataSource} from '../row';
 
 @Component({
   selector: 'app-rejectedattributes',
@@ -18,8 +14,7 @@ import {Util} from '../../util/util';
 export class RejectedattributesComponent implements OnInit {
 
   displayedColumns = ['source', 'mapped', 'delete'];
-  attributeDatabase = new AttributeDatabase();
-  dataSource: AttributeDataSource | null;
+  dataSource: RowDataSource;
 
   @Input()
   attributes: Map<String, String[]>;
@@ -29,14 +24,15 @@ export class RejectedattributesComponent implements OnInit {
   }
 
   ngOnInit() {
+    const rows = [];
     for (const p of Array.from(Object.keys(this.attributes))) {
-      this.attributeDatabase.addRow(new Row(p));
+      rows.push(new Row(p));
     }
-    this.dataSource = new AttributeDataSource(this.attributeDatabase);
+    this.dataSource = new RowDataSource(rows);
   }
 
   addRow() {
-    this.attributeDatabase.addRow(new Row(''));
+    this.dataSource.addRow();
   }
 
   doChange(row: Row, val: string) {
@@ -46,47 +42,7 @@ export class RejectedattributesComponent implements OnInit {
   }
 
   delete(row: Row) {
-   delete this.attributes[row.key as string];
-   this.attributeDatabase.removeRow(row);
+    delete this.attributes[row.key as string];
+    this.dataSource.removeRow(row);
   }
-}
-
-export class Row {
-  key: String;
-
-  constructor(source: String) {
-    this.key = source;
-  }
-}
-
-export class AttributeDatabase {
-  dataChange: BehaviorSubject<Row[]> = new BehaviorSubject<Row[]>([]);
-  get data(): Row[] { return this.dataChange.value; }
-
-  constructor() {
-  }
-
-  addRow(row: Row) {
-    const copiedData = this.data.slice();
-    copiedData.push(row);
-    this.dataChange.next(copiedData);
-  }
-
-  removeRow(row: Row) {
-    const copiedData = this.data.slice();
-    copiedData.splice(copiedData.indexOf(row), 1);
-    this.dataChange.next(copiedData);
-  }
-}
-
-export class AttributeDataSource extends DataSource<any> {
-  constructor(private _attributeDatabase: AttributeDatabase) {
-    super();
-  }
-
-  connect(): Observable<Row[]> {
-    return this._attributeDatabase.dataChange;
-  }
-
-  disconnect() {}
 }
