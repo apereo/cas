5d62103c4ba069bc12f31a71428b2d41508ecb05
Misagh Moayyed <mm1844@gmail.com>
2017-11-20 15:54:47 -0700

    removing circular deps; wip.

--- core/cas-server-core-services/src/test/java/org/apereo/cas/services/PrincipalAttributeRegisteredServiceUsernameProviderTests.java
+++ core/cas-server-core-services/src/test/java/org/apereo/cas/services/PrincipalAttributeRegisteredServiceUsernameProviderTests.java
@@ -6,14 +6,13 @@ import com.google.common.collect.Multimap;
 import org.apache.commons.io.FileUtils;
 import org.apereo.cas.authentication.CoreAuthenticationTestUtils;
 import org.apereo.cas.authentication.principal.Principal;
-import org.apereo.cas.config.CasCoreServicesConfiguration;
-import org.apereo.cas.config.CasCoreUtilConfiguration;
-import org.apereo.cas.config.CasPersonDirectoryConfiguration;
 import org.apereo.cas.util.CollectionUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.File;
@@ -28,20 +27,16 @@ import static org.mockito.Mockito.*;
  * @author Misagh Moayyed
  * @since 4.1.0
  */
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = {RefreshAutoConfiguration.class, 
-        CasCoreUtilConfiguration.class,
-        CasPersonDirectoryConfiguration.class,
-        CasCoreServicesConfiguration.class})
+@RunWith(JUnit4.class)
 public class PrincipalAttributeRegisteredServiceUsernameProviderTests {
-    
+
     private static final File JSON_FILE = new File(FileUtils.getTempDirectoryPath(), "principalAttributeRegisteredServiceUsernameProvider.json");
     private static final ObjectMapper MAPPER = new ObjectMapper().findAndRegisterModules();
-    
+
     @Test
     public void verifyUsernameByPrincipalAttributeWithMapping() {
         final PrincipalAttributeRegisteredServiceUsernameProvider provider =
-                new PrincipalAttributeRegisteredServiceUsernameProvider("email");
+            new PrincipalAttributeRegisteredServiceUsernameProvider("email");
 
         final Multimap<String, String> allowedAttributes = ArrayListMultimap.create();
         final String mappedAttribute = "urn:oid:0.9.2342.19200300.100.1.3";
@@ -49,21 +44,21 @@ public class PrincipalAttributeRegisteredServiceUsernameProviderTests {
         final ReturnMappedAttributeReleasePolicy policy = new ReturnMappedAttributeReleasePolicy(CollectionUtils.wrap(allowedAttributes));
         final RegisteredService registeredService = CoreAuthenticationTestUtils.getRegisteredService();
         when(registeredService.getAttributeReleasePolicy()).thenReturn(policy);
-        
+
         final Map<String, Object> principalAttributes = new HashMap<>();
         principalAttributes.put("email", "user@example.org");
         final Principal p = mock(Principal.class);
         when(p.getId()).thenReturn("person");
         when(p.getAttributes()).thenReturn(principalAttributes);
-        final String id = provider.resolveUsername(p, 
-                RegisteredServiceTestUtils.getService("verifyUsernameByPrincipalAttributeWithMapping"), registeredService);
+        final String id = provider.resolveUsername(p,
+            RegisteredServiceTestUtils.getService("verifyUsernameByPrincipalAttributeWithMapping"), registeredService);
         assertEquals(id, "user@example.org");
     }
 
     @Test
     public void verifyUsernameByPrincipalAttributeAsCollection() {
         final PrincipalAttributeRegisteredServiceUsernameProvider provider =
-                new PrincipalAttributeRegisteredServiceUsernameProvider("cn");
+            new PrincipalAttributeRegisteredServiceUsernameProvider("cn");
 
         final Map<String, Object> attrs = new HashMap<>();
         attrs.put("userid", CollectionUtils.wrap("u1"));
@@ -74,14 +69,14 @@ public class PrincipalAttributeRegisteredServiceUsernameProviderTests {
         when(p.getAttributes()).thenReturn(attrs);
 
         final String id = provider.resolveUsername(p, RegisteredServiceTestUtils.getService("usernameAttributeProviderService"),
-                RegisteredServiceTestUtils.getRegisteredService("usernameAttributeProviderService"));
+            RegisteredServiceTestUtils.getRegisteredService("usernameAttributeProviderService"));
         assertEquals(id, "TheName");
     }
-    
+
     @Test
     public void verifyUsernameByPrincipalAttribute() {
         final PrincipalAttributeRegisteredServiceUsernameProvider provider =
-                new PrincipalAttributeRegisteredServiceUsernameProvider("cn");
+            new PrincipalAttributeRegisteredServiceUsernameProvider("cn");
 
         final Map<String, Object> attrs = new HashMap<>();
         attrs.put("userid", "u1");
@@ -92,14 +87,14 @@ public class PrincipalAttributeRegisteredServiceUsernameProviderTests {
         when(p.getAttributes()).thenReturn(attrs);
 
         final String id = provider.resolveUsername(p, RegisteredServiceTestUtils.getService("usernameAttributeProviderService"),
-                RegisteredServiceTestUtils.getRegisteredService("usernameAttributeProviderService"));
+            RegisteredServiceTestUtils.getRegisteredService("usernameAttributeProviderService"));
         assertEquals(id, "TheName");
     }
 
     @Test
     public void verifyUsernameByPrincipalAttributeNotFound() {
         final PrincipalAttributeRegisteredServiceUsernameProvider provider =
-                new PrincipalAttributeRegisteredServiceUsernameProvider("cn");
+            new PrincipalAttributeRegisteredServiceUsernameProvider("cn");
 
         final Map<String, Object> attrs = new HashMap<>();
         attrs.put("userid", "u1");
@@ -109,23 +104,23 @@ public class PrincipalAttributeRegisteredServiceUsernameProviderTests {
         when(p.getAttributes()).thenReturn(attrs);
 
         final String id = provider.resolveUsername(p, RegisteredServiceTestUtils.getService("usernameAttributeProviderService"),
-                RegisteredServiceTestUtils.getRegisteredService("usernameAttributeProviderService"));
+            RegisteredServiceTestUtils.getRegisteredService("usernameAttributeProviderService"));
         assertEquals(id, p.getId());
     }
 
     @Test
     public void verifyEquality() {
         final PrincipalAttributeRegisteredServiceUsernameProvider provider =
-                new PrincipalAttributeRegisteredServiceUsernameProvider("cn");
+            new PrincipalAttributeRegisteredServiceUsernameProvider("cn");
         final PrincipalAttributeRegisteredServiceUsernameProvider provider2 =
-                new PrincipalAttributeRegisteredServiceUsernameProvider("cn");
+            new PrincipalAttributeRegisteredServiceUsernameProvider("cn");
         assertEquals(provider, provider2);
     }
 
     @Test
     public void verifySerializeAPrincipalAttributeRegisteredServiceUsernameProviderToJson() throws IOException {
         final PrincipalAttributeRegisteredServiceUsernameProvider providerWritten =
-                new PrincipalAttributeRegisteredServiceUsernameProvider("cn");
+            new PrincipalAttributeRegisteredServiceUsernameProvider("cn");
         MAPPER.writeValue(JSON_FILE, providerWritten);
         final RegisteredServiceUsernameAttributeProvider providerRead = MAPPER.readValue(JSON_FILE, PrincipalAttributeRegisteredServiceUsernameProvider.class);
         assertEquals(providerWritten, providerRead);
