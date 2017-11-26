d569aa0f7b3d87baf267b0ee35a0e896488876a5
Misagh Moayyed <mm1844@gmail.com>
2017-11-26 14:08:31 -0700

    build support for request headers as mfa triggers

--- core/cas-server-core-webflow-api/src/main/java/org/apereo/cas/web/flow/resolver/impl/mfa/request/BaseRequestMultifactorAuthenticationPolicyEventResolver.java
+++ core/cas-server-core-webflow-api/src/main/java/org/apereo/cas/web/flow/resolver/impl/mfa/request/BaseRequestMultifactorAuthenticationPolicyEventResolver.java
@@ -1,6 +1,5 @@
-package org.apereo.cas.web.flow.resolver.impl.mfa;
+package org.apereo.cas.web.flow.resolver.impl.mfa.request;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apereo.cas.CentralAuthenticationService;
 import org.apereo.cas.authentication.Authentication;
 import org.apereo.cas.authentication.AuthenticationException;
@@ -24,33 +23,31 @@ import org.springframework.webflow.execution.Event;
 import org.springframework.webflow.execution.RequestContext;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
 /**
- * This is {@link RequestParameterMultifactorAuthenticationPolicyEventResolver}
+ * This is {@link BaseRequestMultifactorAuthenticationPolicyEventResolver}
  * that attempts to resolve the next event based on the authentication providers of this service.
  *
  * @author Misagh Moayyed
  * @since 5.0.0
  */
-public class RequestParameterMultifactorAuthenticationPolicyEventResolver extends BaseMultifactorAuthenticationProviderEventResolver {
-    private static final Logger LOGGER = LoggerFactory.getLogger(RequestParameterMultifactorAuthenticationPolicyEventResolver.class);
+public abstract class BaseRequestMultifactorAuthenticationPolicyEventResolver extends BaseMultifactorAuthenticationProviderEventResolver {
+    private static final Logger LOGGER = LoggerFactory.getLogger(BaseRequestMultifactorAuthenticationPolicyEventResolver.class);
 
-    private final String mfaRequestParameter;
-
-    public RequestParameterMultifactorAuthenticationPolicyEventResolver(final AuthenticationSystemSupport authenticationSystemSupport,
-                                                                        final CentralAuthenticationService centralAuthenticationService,
-                                                                        final ServicesManager servicesManager,
-                                                                        final TicketRegistrySupport ticketRegistrySupport,
-                                                                        final CookieGenerator warnCookieGenerator,
-                                                                        final AuthenticationServiceSelectionPlan authenticationStrategies,
-                                                                        final MultifactorAuthenticationProviderSelector selector,
-                                                                        final CasConfigurationProperties casProperties) {
+    public BaseRequestMultifactorAuthenticationPolicyEventResolver(final AuthenticationSystemSupport authenticationSystemSupport,
+                                                                   final CentralAuthenticationService centralAuthenticationService,
+                                                                   final ServicesManager servicesManager,
+                                                                   final TicketRegistrySupport ticketRegistrySupport,
+                                                                   final CookieGenerator warnCookieGenerator,
+                                                                   final AuthenticationServiceSelectionPlan authenticationStrategies,
+                                                                   final MultifactorAuthenticationProviderSelector selector,
+                                                                   final CasConfigurationProperties casProperties) {
         super(authenticationSystemSupport, centralAuthenticationService, servicesManager,
-                ticketRegistrySupport, warnCookieGenerator, authenticationStrategies, selector);
-        mfaRequestParameter = casProperties.getAuthn().getMfa().getRequestParameter();
+            ticketRegistrySupport, warnCookieGenerator, authenticationStrategies, selector);
     }
 
     @Override
@@ -62,47 +59,50 @@ public class RequestParameterMultifactorAuthenticationPolicyEventResolver extend
             LOGGER.debug("No service or authentication is available to determine event for principal");
             return null;
         }
-        if (StringUtils.isBlank(mfaRequestParameter)) {
-            LOGGER.debug("No request parameter is defined to trigger multifactor authentication.");
-            return null;
-        }
-        
+
         final HttpServletRequest request = WebUtils.getHttpServletRequestFromExternalWebflowContext(context);
-        final String[] values = request.getParameterValues(mfaRequestParameter);
-        if (values != null && values.length > 0) {
-            LOGGER.debug("Received request parameter [{}] as [{}]", mfaRequestParameter, values);
+        final List<String> values = resolveEventFromHttpRequest(request);
+        if (values != null && !values.isEmpty()) {
+            LOGGER.debug("Received request as [{}]", values);
 
             final Map<String, MultifactorAuthenticationProvider> providerMap =
-                    MultifactorAuthenticationUtils.getAvailableMultifactorAuthenticationProviders(this.applicationContext);
+                MultifactorAuthenticationUtils.getAvailableMultifactorAuthenticationProviders(this.applicationContext);
             if (providerMap == null || providerMap.isEmpty()) {
-                LOGGER.error("No multifactor authentication providers are available in the application context to satisfy [{}]", (Object[]) values);
+                LOGGER.error("No multifactor authentication providers are available in the application context to satisfy [{}]", values);
                 throw new AuthenticationException();
             }
 
-            final Optional<MultifactorAuthenticationProvider> providerFound = resolveProvider(providerMap, values[0]);
+            final Optional<MultifactorAuthenticationProvider> providerFound = resolveProvider(providerMap, values.get(0));
             if (providerFound.isPresent()) {
                 final MultifactorAuthenticationProvider provider = providerFound.get();
                 if (provider.isAvailable(service)) {
                     LOGGER.debug("Attempting to build an event based on the authentication provider [{}] and service [{}]", provider, service.getName());
                     final Event event = validateEventIdForMatchingTransitionInContext(provider.getId(), context,
-                            buildEventAttributeMap(authentication.getPrincipal(), service, provider));
+                        buildEventAttributeMap(authentication.getPrincipal(), service, provider));
                     return CollectionUtils.wrapSet(event);
                 }
                 LOGGER.warn("Located multifactor provider [{}], yet the provider cannot be reached or verified", providerFound.get());
                 return null;
             }
-            LOGGER.warn("No multifactor provider could be found for request parameter [{}]", (Object[]) values);
+            LOGGER.warn("No multifactor provider could be found for request parameter [{}]", values);
             throw new AuthenticationException();
         }
-        LOGGER.debug("No value could be found for request parameter [{}]", mfaRequestParameter);
         return null;
     }
 
     @Audit(action = "AUTHENTICATION_EVENT",
-            actionResolverName = "AUTHENTICATION_EVENT_ACTION_RESOLVER",
-            resourceResolverName = "AUTHENTICATION_EVENT_RESOURCE_RESOLVER")
+        actionResolverName = "AUTHENTICATION_EVENT_ACTION_RESOLVER",
+        resourceResolverName = "AUTHENTICATION_EVENT_RESOURCE_RESOLVER")
     @Override
     public Event resolveSingle(final RequestContext context) {
         return super.resolveSingle(context);
     }
+
+    /**
+     * Resolve event from http request.
+     *
+     * @param request the request
+     * @return the list
+     */
+    protected abstract List<String> resolveEventFromHttpRequest(HttpServletRequest request);
 }
