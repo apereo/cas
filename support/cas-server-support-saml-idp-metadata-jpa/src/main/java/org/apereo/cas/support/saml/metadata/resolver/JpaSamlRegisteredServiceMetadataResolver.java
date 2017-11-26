39b7a36f1fcef710009473733485916c02243743
Misagh Moayyed <mmoayyed@unicon.net>
2017-11-26 11:19:10 -0700

    clean up saml metadata resolvers; add support for jpa

--- support/cas-server-support-saml-idp-metadata-jpa/src/main/java/org/apereo/cas/support/saml/metadata/resolver/JpaSamlRegisteredServiceMetadataResolver.java
+++ support/cas-server-support-saml-idp-metadata-jpa/src/main/java/org/apereo/cas/support/saml/metadata/resolver/JpaSamlRegisteredServiceMetadataResolver.java
@@ -3,7 +3,6 @@ package org.apereo.cas.support.saml.metadata.resolver;
 import org.apereo.cas.configuration.model.support.saml.idp.SamlIdPProperties;
 import org.apereo.cas.support.saml.OpenSamlConfigBean;
 import org.apereo.cas.support.saml.services.SamlRegisteredService;
-import org.apereo.cas.support.saml.services.idp.metadata.SamlMetadataDocument;
 import org.apereo.cas.support.saml.services.idp.metadata.cache.resolver.BaseSamlRegisteredServiceMetadataResolver;
 import org.opensaml.saml.metadata.resolver.MetadataResolver;
 import org.slf4j.Logger;
@@ -11,9 +10,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 import java.util.Collection;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 /**
  * This is {@link MongoDbSamlRegisteredServiceMetadataResolver}.
@@ -21,28 +17,18 @@ import java.util.stream.Collectors;
  * @author Misagh Moayyed
  * @since 5.2.0
  */
-public class MongoDbSamlRegisteredServiceMetadataResolver extends BaseSamlRegisteredServiceMetadataResolver {
-    private static final Logger LOGGER = LoggerFactory.getLogger(MongoDbSamlRegisteredServiceMetadataResolver.class);
+public class JpaSamlRegisteredServiceMetadataResolver extends BaseSamlRegisteredServiceMetadataResolver {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JpaSamlRegisteredServiceMetadataResolver.class);
 
-    private final MongoTemplate mongoTemplate;
-    private final String collectionName;
-
-    public MongoDbSamlRegisteredServiceMetadataResolver(final SamlIdPProperties samlIdPProperties,
-                                                        final OpenSamlConfigBean configBean, final MongoTemplate mongoTemplate) {
+    public JpaSamlRegisteredServiceMetadataResolver(final SamlIdPProperties samlIdPProperties,
+                                                    final OpenSamlConfigBean configBean, final MongoTemplate mongoTemplate) {
         super(samlIdPProperties, configBean);
-        this.collectionName = samlIdPProperties.getMetadata().getMongo().getCollection();
-        this.mongoTemplate = mongoTemplate;
     }
 
     @Override
     public Collection<MetadataResolver> resolve(final SamlRegisteredService service) {
         try {
-            final List<SamlMetadataDocument> documents = mongoTemplate.findAll(SamlMetadataDocument.class, this.collectionName);
-            return documents
-                .stream()
-                .map(doc -> buildMetadataResolverFrom(service, doc))
-                .filter(Objects::nonNull)
-                .collect(Collectors.toList());
+           
         } catch (final Exception e) {
             LOGGER.error(e.getMessage(), e);
         }
