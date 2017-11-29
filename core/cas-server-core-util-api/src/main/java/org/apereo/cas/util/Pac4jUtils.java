8dbf9785db5a51e15ab47191e5a4a47379b07fe1
Jarda Kacer <jaroslav@kacer.biz>
2017-11-29 17:55:57 +0100

    SAML Single Logout Enhancements
    Adapted to the latest changes in master: new modules, PAC4J utils moved
    to another module too.

--- core/cas-server-core-util-api/src/main/java/org/apereo/cas/util/Pac4jUtils.java
+++ core/cas-server-core-util-api/src/main/java/org/apereo/cas/util/Pac4jUtils.java
@@ -3,6 +3,7 @@ package org.apereo.cas.util;
 import org.apereo.inspektr.common.spi.PrincipalResolver;
 import org.pac4j.core.context.J2EContext;
 import org.pac4j.core.context.WebContext;
+import org.pac4j.core.profile.CommonProfile;
 import org.pac4j.core.profile.ProfileManager;
 import org.pac4j.core.profile.UserProfile;
 
@@ -82,4 +83,21 @@ public final class Pac4jUtils {
         return getPac4jJ2EContext(HttpRequestUtils.getHttpServletRequestFromRequestAttributes(), 
                 HttpRequestUtils.getHttpServletResponseFromRequestAttributes());
     }
+
+    /**
+     * Finds the current client name from the context, using the PAC4J Profile Manager. It is assumed that the context has previously been
+     * populated with the profile.
+     * 
+     * @param webContext
+     *            A web context (request + response).
+     * 
+     * @return The currently used client's name or {@code null} if there is no active profile.
+     */
+    public static String findCurrentClientName(final WebContext webContext) {
+        @SuppressWarnings("unchecked")
+        final ProfileManager<? extends CommonProfile> pm = getPac4jProfileManager(webContext);
+        final Optional<? extends CommonProfile> profile = pm.get(true);
+        return profile.map(CommonProfile::getClientName).orElse(null);
+    }
+
 }
