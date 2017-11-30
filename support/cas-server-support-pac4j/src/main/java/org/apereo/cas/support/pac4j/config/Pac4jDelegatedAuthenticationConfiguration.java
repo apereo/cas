90a326ee332ce868cacf5e09f09295c3b622e3fc
Misagh Moayyed <mmoayyed@unicon.net>
2017-11-30 11:59:04 -0700

    working on delegated access strategies

--- support/cas-server-support-pac4j/src/main/java/org/apereo/cas/support/pac4j/config/Pac4jDelegatedAuthenticationConfiguration.java
+++ support/cas-server-support-pac4j/src/main/java/org/apereo/cas/support/pac4j/config/Pac4jDelegatedAuthenticationConfiguration.java
@@ -5,6 +5,10 @@ import org.apereo.cas.authentication.AuthenticationSystemSupport;
 import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.services.ServicesManager;
 import org.apereo.cas.support.pac4j.web.flow.DelegatedClientAuthenticationAction;
+import org.apereo.cas.validation.Pac4jServiceTicketValidationAuthorizer;
+import org.apereo.cas.validation.ServiceTicketValidationAuthorizer;
+import org.apereo.cas.validation.ServiceTicketValidationAuthorizerConfigurer;
+import org.apereo.cas.validation.ServiceTicketValidationAuthorizersExecutionPlan;
 import org.pac4j.core.client.Clients;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -23,7 +27,7 @@ import org.springframework.webflow.execution.Action;
  */
 @Configuration("pac4jDelegatedAuthenticationConfiguration")
 @EnableConfigurationProperties(CasConfigurationProperties.class)
-public class Pac4jDelegatedAuthenticationConfiguration {
+public class Pac4jDelegatedAuthenticationConfiguration implements ServiceTicketValidationAuthorizerConfigurer {
 
     @Autowired
     @Qualifier("servicesManager")
@@ -52,5 +56,15 @@ public class Pac4jDelegatedAuthenticationConfiguration {
             casProperties.getLocale().getParamName(),
             casProperties.getAuthn().getPac4j().isAutoRedirect(),
             servicesManager);
+    }
+
+    @Bean
+    public ServiceTicketValidationAuthorizer pac4jServiceTicketValidationAuthorizer() {
+        return new Pac4jServiceTicketValidationAuthorizer(this.servicesManager);
+    }
+
+    @Override
+    public void configureAuthorizersExecutionPlan(final ServiceTicketValidationAuthorizersExecutionPlan plan) {
+        plan.registerAuthorizer(pac4jServiceTicketValidationAuthorizer());
     }
 }
