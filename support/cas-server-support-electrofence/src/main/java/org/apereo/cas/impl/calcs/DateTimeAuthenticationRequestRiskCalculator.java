246401d788e8222d2b1e99882a77f8060c58e0c8
Misagh Moayyed <mmoayyed@unicon.net>
2017-12-03 19:29:01 -0700

    fix unit tests

--- support/cas-server-support-electrofence/src/main/java/org/apereo/cas/impl/calcs/DateTimeAuthenticationRequestRiskCalculator.java
+++ support/cas-server-support-electrofence/src/main/java/org/apereo/cas/impl/calcs/DateTimeAuthenticationRequestRiskCalculator.java
@@ -2,15 +2,17 @@ package org.apereo.cas.impl.calcs;
 
 import org.apereo.cas.authentication.Authentication;
 import org.apereo.cas.services.RegisteredService;
-import org.apereo.cas.support.events.dao.CasEvent;
 import org.apereo.cas.support.events.CasEventRepository;
+import org.apereo.cas.support.events.dao.CasEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import java.math.BigDecimal;
-import java.time.ZonedDateTime;
+import java.time.Instant;
 import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.chrono.ChronoZonedDateTime;
 import java.util.Collection;
 
 /**
@@ -34,16 +36,19 @@ public class DateTimeAuthenticationRequestRiskCalculator extends BaseAuthenticat
                                         final RegisteredService service, final Collection<CasEvent> events) {
         final ZonedDateTime timestamp = ZonedDateTime.now(ZoneOffset.UTC);
         LOGGER.debug("Filtering authentication events for timestamp [{}]", timestamp);
-        long count = 0;
+        
+        final int hoursFromNow = timestamp.plusHours(windowInHours).getHour();
+        final int hoursBeforeNow = timestamp.minusHours(windowInHours).getHour();
 
-        if (timestamp.getHour() <= timestamp.plusHours(windowInHours).getHour()
-                && timestamp.getHour() >= timestamp.minusHours(windowInHours).getHour()){
-            count = events.stream().filter(e->e.getCreationTime().getHour() <= timestamp.plusHours(windowInHours).getHour()
-                && e.getCreationTime().getHour() >= timestamp.minusHours(windowInHours).getHour()).count();
-        } else {
-            count = events.stream().filter(e -> e.getCreationTime().getHour() <= timestamp.plusHours(windowInHours).getHour()
-                || e.getCreationTime().getHour() >= timestamp.minusHours(windowInHours).getHour()).count();
-        }
+        final long count = events
+            .stream()
+            .map(time -> {
+                final Instant instant = ChronoZonedDateTime.from(time.getCreationTime()).toInstant();
+                final ZonedDateTime zdt = ZonedDateTime.ofInstant(instant, ZoneOffset.UTC);
+                return zdt.getHour();
+            })
+            .filter(hour -> hour <= hoursFromNow && hour >= hoursBeforeNow)
+            .count();
 
         LOGGER.debug("Total authentication events found for [{}] in a [{}]h window: [{}]", timestamp, windowInHours, count);
         if (count == events.size()) {
