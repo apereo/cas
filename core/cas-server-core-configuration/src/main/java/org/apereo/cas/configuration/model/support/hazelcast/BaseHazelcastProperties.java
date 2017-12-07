f6dd30e3ebc2745c991203cbacb4861cbc1299f8
Misagh Moayyed <mmoayyed@unicon.net>
2017-12-07 16:44:55 -0700

    add support for hazelcast aws discovery

--- core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/hazelcast/BaseHazelcastProperties.java
+++ core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/model/support/hazelcast/BaseHazelcastProperties.java
@@ -1,5 +1,6 @@
 package org.apereo.cas.configuration.model.support.hazelcast;
 
+import org.apereo.cas.configuration.support.RequiresModule;
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.core.io.Resource;
 
@@ -11,12 +12,53 @@ import java.io.Serializable;
  * @author Misagh Moayyed
  * @since 5.2.0
  */
+@RequiresModule(name = "cas-server-support-hazelcast-core")
 public class BaseHazelcastProperties implements Serializable {
     /**
      * Logging type property name.
      */
     public static final String LOGGING_TYPE_PROP = "hazelcast.logging.type";
+    /**
+     * Enable discovery.
+     */
+    public static final String HAZELCAST_DISCOVERY_ENABLED = "hazelcast.discovery.enabled";
 
+    /**
+     * AWS discovery access key property.
+     */
+    public static final String AWS_DISCOVERY_ACCESS_KEY = "access-key";
+    /**
+     * AWS discovery secret key property.
+     */
+    public static final String AWS_DISCOVERY_SECRET_KEY = "secret-key";
+    /**
+     * AWS discovery IAM role property.
+     */
+    public static final String AWS_DISCOVERY_IAM_ROLE = "iam-role";
+    /**
+     * AWS discovery  region property.
+     */
+    public static final String AWS_DISCOVERY_REGION = "region";
+    /**
+     * AWS discovery host header property.
+     */
+    public static final String AWS_DISCOVERY_HOST_HEADER = "host-header";
+    /**
+     * AWS discovery security group name property.
+     */
+    public static final String AWS_DISCOVERY_SECURITY_GROUP_NAME = "security-group-name";
+    /**
+     * AWS discovery  tag key property.
+     */
+    public static final String AWS_DISCOVERY_TAG_KEY = "tag-key";
+    /**
+     * AWS discovery tag value property.
+     */
+    public static final String AWS_DISCOVERY_TAG_VALUE = "tag-value";
+    /**
+     * AWS discovery HZ port property.
+     */
+    public static final String AWS_DISCOVERY_PORT = "hz-port";
     /**
      * Max num of seconds for heartbeat property name.
      */
@@ -25,6 +67,7 @@ public class BaseHazelcastProperties implements Serializable {
      * Ipv4 protocol stack.
      */
     public static final String IPV4_STACK_PROP = "hazelcast.prefer.ipv4.stack";
+    
     private static final long serialVersionUID = 4204884717547468480L;
 
     /**
