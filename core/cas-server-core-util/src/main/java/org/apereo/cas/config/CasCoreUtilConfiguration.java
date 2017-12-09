f1e3a7989a45988da3204d3bd03579257b89c1b0
Misagh Moayyed <mm1844@gmail.com>
2017-12-09 15:00:26 -0700

    Build support for consul

--- core/cas-server-core-util/src/main/java/org/apereo/cas/config/CasCoreUtilConfiguration.java
+++ core/cas-server-core-util/src/main/java/org/apereo/cas/config/CasCoreUtilConfiguration.java
@@ -5,11 +5,11 @@ import org.apereo.cas.util.SchedulingUtils;
 import org.apereo.cas.util.io.CommunicationsManager;
 import org.apereo.cas.util.spring.ApplicationContextProvider;
 import org.apereo.cas.util.spring.Converters;
-import org.apereo.cas.util.spring.CustomBeanValidationPostProcessor;
 import org.apereo.cas.util.spring.SpringAwareMessageMessageInterpolator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -24,6 +24,7 @@ import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.util.StringValueResolver;
+import org.springframework.validation.beanvalidation.BeanValidationPostProcessor;
 
 import javax.annotation.PostConstruct;
 import javax.validation.MessageInterpolator;
@@ -71,10 +72,11 @@ public class CasCoreUtilConfiguration {
     }
 
     @Bean
-    public CustomBeanValidationPostProcessor beanValidationPostProcessor() {
-        return new CustomBeanValidationPostProcessor();
+    @ConditionalOnMissingBean(name = "casBeanValidationPostProcessor")
+    public BeanValidationPostProcessor casBeanValidationPostProcessor() {
+        return new BeanValidationPostProcessor();
     }
-    
+
     @PostConstruct
     public void init() {
         final ConfigurableApplicationContext ctx = applicationContextProvider().getConfigurableApplicationContext();
