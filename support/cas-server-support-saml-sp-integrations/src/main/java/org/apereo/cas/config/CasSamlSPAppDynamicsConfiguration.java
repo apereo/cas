6ec3dd61cf451d09ad98177ef8db13cd8200054a
Misagh Moayyed <mmoayyed@unicon.net>
2017-12-09 16:09:57 -0700

    add support for app dynamics

---
+++ support/cas-server-support-saml-sp-integrations/src/main/java/org/apereo/cas/config/CasSamlSPAppDynamicsConfiguration.java
@@ -0,0 +1,22 @@
+package org.apereo.cas.config;
+
+import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.configuration.model.support.saml.sps.AbstractSamlSPProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * This is {@link CasSamlSPAppDynamicsConfiguration}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.0.0
+ */
+@Configuration("casSamlSPAppDynamicsConfiguration")
+@EnableConfigurationProperties(CasConfigurationProperties.class)
+public class CasSamlSPAppDynamicsConfiguration extends BaseCasSamlSPConfiguration {
+
+    @Override
+    protected AbstractSamlSPProperties getServiceProvider() {
+        return casProperties.getSamlSp().getAppDynamics();
+    }
+}
