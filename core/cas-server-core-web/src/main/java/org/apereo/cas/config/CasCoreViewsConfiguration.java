5e149f4d489ef953cfb01f8dcb33e26a7916d967
Misagh Moayyed <mm1844@gmail.com>
2017-12-16 13:18:08 -0700

    externalize view files

--- core/cas-server-core-web/src/main/java/org/apereo/cas/config/CasCoreViewsConfiguration.java
+++ core/cas-server-core-web/src/main/java/org/apereo/cas/config/CasCoreViewsConfiguration.java
@@ -10,14 +10,9 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.ResourceUtils;
-import org.springframework.web.context.ServletContextAware;
 import org.thymeleaf.templateresolver.AbstractConfigurableTemplateResolver;
 import org.thymeleaf.templateresolver.AbstractTemplateResolver;
 import org.thymeleaf.templateresolver.FileTemplateResolver;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
-import org.thymeleaf.templateresolver.UrlTemplateResolver;
-
-import javax.servlet.ServletContext;
 
 /**
  * This is {@link CasCoreViewsConfiguration}.
@@ -27,9 +22,7 @@ import javax.servlet.ServletContext;
  */
 @Configuration("casCoreWebViewsConfiguration")
 @EnableConfigurationProperties(CasConfigurationProperties.class)
-public class CasCoreViewsConfiguration implements ServletContextAware {
-
-    private ServletContext servletContext;
+public class CasCoreViewsConfiguration {
 
     @Autowired
     private CasConfigurationProperties casProperties;
@@ -49,14 +42,6 @@ public class CasCoreViewsConfiguration implements ServletContextAware {
             chain.addResolver(file);
         }));
 
-        final ServletContextTemplateResolver servlet = new ServletContextTemplateResolver(this.servletContext);
-        configureTemplateViewResolver(servlet);
-        chain.addResolver(servlet);
-
-        final UrlTemplateResolver url = new UrlTemplateResolver();
-        configureTemplateViewResolver(url);
-        chain.addResolver(url);
-
         return chain;
     }
 
@@ -68,10 +53,5 @@ public class CasCoreViewsConfiguration implements ServletContextAware {
         resolver.setOrder(0);
         resolver.setSuffix(".html");
         resolver.setTemplateMode(thymeleafProperties.getMode());
-    }
-
-    @Override
-    public void setServletContext(final ServletContext servletContext) {
-        this.servletContext = servletContext;
     }
 }
