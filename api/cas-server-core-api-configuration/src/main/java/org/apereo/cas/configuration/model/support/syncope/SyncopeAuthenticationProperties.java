dd99091dd3c89ca25888e126542dc71abf3895b3
Misagh Moayyed <mm1844@gmail.com>
2017-12-31 13:27:00 +0330

    doc updates

--- api/cas-server-core-api-configuration/src/main/java/org/apereo/cas/configuration/model/support/syncope/SyncopeAuthenticationProperties.java
+++ api/cas-server-core-api-configuration/src/main/java/org/apereo/cas/configuration/model/support/syncope/SyncopeAuthenticationProperties.java
@@ -1,5 +1,11 @@
 package org.apereo.cas.configuration.model.support.syncope;
 
+import org.apereo.cas.configuration.model.core.authentication.PasswordEncoderProperties;
+import org.apereo.cas.configuration.model.core.authentication.PrincipalTransformationProperties;
+import org.apereo.cas.configuration.support.RequiredProperty;
+import org.apereo.cas.configuration.support.RequiresModule;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+
 import java.io.Serializable;
 
 /**
@@ -8,6 +14,7 @@ import java.io.Serializable;
  * @author Misagh Moayyed
  * @since 5.3.0
  */
+@RequiresModule(name = "cas-server-support-syncope")
 public class SyncopeAuthenticationProperties implements Serializable {
     private static final long serialVersionUID = -2446926316502297496L;
 
@@ -24,7 +31,31 @@ public class SyncopeAuthenticationProperties implements Serializable {
     /**
      * Syncope instance URL primary used for REST.
      */
+    @RequiredProperty
     private String url;
+
+    /**
+     * Password encoder settings for the authentication handler.
+     */
+    @NestedConfigurationProperty
+    private PasswordEncoderProperties passwordEncoder = new PasswordEncoderProperties();
+
+    /**
+     * A number of authentication handlers are allowed to determine whether they can operate on the provided credential
+     * and as such lend themselves to be tried and tested during the authentication handler selection phase.
+     * The credential criteria may be one of the following options:<ul>
+     * <li>1) A regular expression pattern that is tested against the credential identifier.</li>
+     * <li>2) A fully qualified class name of your own design that implements {@code Predicate<Credential>}.</li>
+     * <li>3) Path to an external Groovy script that implements the same interface.</li>
+     * </ul>
+     */
+    private String credentialCriteria;
+
+    /**
+     * This is principal transformation properties.
+     */
+    @NestedConfigurationProperty
+    private PrincipalTransformationProperties principalTransformation = new PrincipalTransformationProperties();
 
     public String getUrl() {
         return url;
@@ -48,5 +79,29 @@ public class SyncopeAuthenticationProperties implements Serializable {
 
     public void setDomain(final String domain) {
         this.domain = domain;
+    }
+
+    public PasswordEncoderProperties getPasswordEncoder() {
+        return passwordEncoder;
+    }
+
+    public void setPasswordEncoder(final PasswordEncoderProperties passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public String getCredentialCriteria() {
+        return credentialCriteria;
+    }
+
+    public void setCredentialCriteria(final String credentialCriteria) {
+        this.credentialCriteria = credentialCriteria;
+    }
+
+    public PrincipalTransformationProperties getPrincipalTransformation() {
+        return principalTransformation;
+    }
+
+    public void setPrincipalTransformation(final PrincipalTransformationProperties principalTransformation) {
+        this.principalTransformation = principalTransformation;
     }
 }
