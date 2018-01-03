10aab9b4388d993ae6f4d29f059d5b7c5555f208
Misagh Moayyed <mm1844@gmail.com>
2018-01-03 17:45:49 +0330

    amazon as a saml SP

---
+++ support/cas-server-support-saml-sp-integrations/src/main/java/org/apereo/cas/config/CasSamlSPAmazonConfiguration.java
@@ -0,0 +1,22 @@
+package org.apereo.cas.config;
+
+import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.configuration.model.support.saml.sps.AbstractSamlSPProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * This is {@link CasSamlSPAmazonConfiguration}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.3.0
+ */
+@Configuration("casSamlSPAmazonConfiguration")
+@EnableConfigurationProperties(CasConfigurationProperties.class)
+public class CasSamlSPAmazonConfiguration extends BaseCasSamlSPConfiguration {
+
+    @Override
+    protected AbstractSamlSPProperties getServiceProvider() {
+        return casProperties.getSamlSp().getAmazon();
+    }
+}
