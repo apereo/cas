a030d81b5cfa322403673ae538cd5ab13825269e
Dmitriy Kopylenko <dima767@gmail.com>
2018-01-04 01:19:40 -0500

    Refactor un-necessary regex re-calc (#3119)

--- support/cas-server-support-saml-idp/src/main/java/org/apereo/cas/support/saml/services/SamlIdPEntityIdAuthenticationServiceSelectionStrategy.java
+++ support/cas-server-support-saml-idp/src/main/java/org/apereo/cas/support/saml/services/SamlIdPEntityIdAuthenticationServiceSelectionStrategy.java
@@ -24,12 +24,12 @@ public class SamlIdPEntityIdAuthenticationServiceSelectionStrategy implements Au
 
     private final int order = Ordered.HIGHEST_PRECEDENCE;
     private final ServiceFactory webApplicationServiceFactory;
-    private final String casServerPrefix;
+    private final String casServiceUrlPattern;
 
     public SamlIdPEntityIdAuthenticationServiceSelectionStrategy(final ServiceFactory webApplicationServiceFactory,
                                                                  final String casServerPrefix) {
         this.webApplicationServiceFactory = webApplicationServiceFactory;
-        this.casServerPrefix = casServerPrefix;
+        this.casServiceUrlPattern = "^".concat(casServerPrefix).concat(".*");
     }
 
     @Override
@@ -41,8 +41,7 @@ public class SamlIdPEntityIdAuthenticationServiceSelectionStrategy implements Au
 
     @Override
     public boolean supports(final Service service) {
-        final String casPattern = "^".concat(casServerPrefix).concat(".*");
-        return service != null && service.getId().matches(casPattern)
+        return service != null && service.getId().matches(this.casServiceUrlPattern)
                 && getEntityIdAsParameter(service).isPresent();
     }
 
