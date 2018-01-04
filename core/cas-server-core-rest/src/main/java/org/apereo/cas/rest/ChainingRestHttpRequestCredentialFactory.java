dae97d6c0e601c82c17ab376e99d12485ad2ac80
Misagh Moayyed <mm1844@gmail.com>
2018-01-04 10:46:31 +0330

    improve rest api for services

--- core/cas-server-core-rest/src/main/java/org/apereo/cas/rest/ChainingRestHttpRequestCredentialFactory.java
+++ core/cas-server-core-rest/src/main/java/org/apereo/cas/rest/ChainingRestHttpRequestCredentialFactory.java
@@ -5,6 +5,7 @@ import org.springframework.core.OrderComparator;
 import org.springframework.util.MultiValueMap;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -39,7 +40,7 @@ public class ChainingRestHttpRequestCredentialFactory implements RestHttpRequest
     }
 
     @Override
-    public List<Credential> fromRequestBody(final MultiValueMap<String, String> requestBody) {
+    public List<Credential> fromRequestBody(final Map<String, String> requestBody) {
         OrderComparator.sort(this.chain);
         return this.chain
             .stream()
