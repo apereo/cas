f1635127e3430fbf2ac076d8302bf432c16cceb3
Misagh Moayyed <mm1844@gmail.com>
2018-01-05 20:50:59 +0330

    add extra checks into ldap svc registry to prevent NPEs

--- support/cas-server-support-ldap-service-registry/src/main/java/org/apereo/cas/adaptors/ldap/services/LdapServiceRegistryDao.java
+++ support/cas-server-support-ldap-service-registry/src/main/java/org/apereo/cas/adaptors/ldap/services/LdapServiceRegistryDao.java
@@ -2,7 +2,6 @@ package org.apereo.cas.adaptors.ldap.services;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apereo.cas.configuration.model.support.ldap.serviceregistry.LdapServiceRegistryProperties;
-
 import org.apereo.cas.services.AbstractServiceRegistryDao;
 import org.apereo.cas.services.RegisteredService;
 import org.apereo.cas.support.events.service.CasRegisteredServiceLoadedEvent;
@@ -19,6 +18,7 @@ import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Implementation of the ServiceRegistryDao interface which stores the services in a LDAP Directory.
@@ -60,7 +60,7 @@ public class LdapServiceRegistryDao extends AbstractServiceRegistryDao {
             if (rs.getId() != RegisteredService.INITIAL_IDENTIFIER_VALUE) {
                 return update(rs);
             }
-            
+
             final LdapEntry entry = this.ldapServiceMapper.mapFromRegisteredService(this.baseDn, rs);
             LdapUtils.executeAddOperation(this.connectionFactory, entry);
         } catch (final Exception e) {
@@ -141,12 +141,13 @@ public class LdapServiceRegistryDao extends AbstractServiceRegistryDao {
             final Response<SearchResult> response = getSearchResultResponse();
             if (LdapUtils.containsResultEntry(response)) {
                 response.getResult().getEntries()
-                        .stream()
-                        .map(this.ldapServiceMapper::mapToRegisteredService)
-                        .forEach(s -> {
-                            publishEvent(new CasRegisteredServiceLoadedEvent(this, s));
-                            list.add(s);
-                        });
+                    .stream()
+                    .map(this.ldapServiceMapper::mapToRegisteredService)
+                    .filter(Objects::nonNull)
+                    .forEach(s -> {
+                        publishEvent(new CasRegisteredServiceLoadedEvent(this, s));
+                        list.add(s);
+                    });
             }
         } catch (final LdapException e) {
             LOGGER.error(e.getMessage(), e);
@@ -156,7 +157,7 @@ public class LdapServiceRegistryDao extends AbstractServiceRegistryDao {
 
     private Response<SearchResult> getSearchResultResponse() throws LdapException {
         return LdapUtils.executeSearchOperation(this.connectionFactory,
-                this.baseDn, LdapUtils.newLdaptiveSearchFilter(this.loadFilter));
+            this.baseDn, LdapUtils.newLdaptiveSearchFilter(this.loadFilter));
     }
 
     @Override
@@ -186,8 +187,8 @@ public class LdapServiceRegistryDao extends AbstractServiceRegistryDao {
      */
     private Response<SearchResult> searchForServiceById(final Long id) throws LdapException {
         final SearchFilter filter = LdapUtils.newLdaptiveSearchFilter(this.searchFilter,
-                LdapUtils.LDAP_SEARCH_FILTER_DEFAULT_PARAM_NAME,
-                CollectionUtils.wrap(id.toString()));
+            LdapUtils.LDAP_SEARCH_FILTER_DEFAULT_PARAM_NAME,
+            CollectionUtils.wrap(id.toString()));
         return LdapUtils.executeSearchOperation(this.connectionFactory, this.baseDn, filter);
     }
 
