55456924f494124233740d3c4c1b3a4d37265bad
3cdota <515386814@qq.com>
2018-01-07 11:16:34 +0000

    Update TerminateSessionAction.java

--- support/cas-server-support-actions/src/main/java/org/apereo/cas/web/flow/TerminateSessionAction.java
+++ support/cas-server-support-actions/src/main/java/org/apereo/cas/web/flow/TerminateSessionAction.java
@@ -105,7 +105,7 @@ public class TerminateSessionAction extends AbstractAction {
 
         final HttpSession session = request.getSession();
         if (session != null) {
-            session.invalidate();            
+             session.invalidate();            
         	String requestedUrl=request.getSession().getAttribute("pac4jRequestedUrl").toString();        	
             session.invalidate();
             // copy pac4jRequestedUrl in  new session 
