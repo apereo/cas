86608193f7926c68c64ed02cc748a9402f5e654d
Misagh Moayyed <mm1844@gmail.com>
2018-01-08 20:40:46 +0330

    add audit support based on REST

---
+++ api/cas-server-core-api-configuration/src/main/java/org/apereo/cas/configuration/model/core/audit/AuditRestProperties.java
@@ -0,0 +1,13 @@
+package org.apereo.cas.configuration.model.core.audit;
+
+import org.apereo.cas.configuration.support.RestEndpointProperties;
+
+/**
+ * This is {@link AuditRestProperties}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.3.0
+ */
+public class AuditRestProperties extends RestEndpointProperties {
+    private static final long serialVersionUID = 3893437775090452831L;
+}
