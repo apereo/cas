5c282ef546ea26c5a31f6c0ae85ab6830a1f1b8d
Misagh Moayyed <mm1844@gmail.com>
2018-01-12 19:19:23 +0330

    allow dynamodb service registry table name to be externalized

--- api/cas-server-core-api-configuration/src/main/java/org/apereo/cas/configuration/model/support/dynamodb/DynamoDbServiceRegistryProperties.java
+++ api/cas-server-core-api-configuration/src/main/java/org/apereo/cas/configuration/model/support/dynamodb/DynamoDbServiceRegistryProperties.java
@@ -11,4 +11,17 @@ import org.apereo.cas.configuration.support.RequiresModule;
 @RequiresModule(name = "cas-server-support-dynamodb-service-registry")
 public class DynamoDbServiceRegistryProperties extends AbstractDynamoDbProperties {
     private static final long serialVersionUID = 809653348774854955L;
+
+    /**
+     * The table name used and created by CAS to hold service definitions in DynamoDb.
+     */
+    private String tableName = "DynamoDbCasServices";
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(final String tableName) {
+        this.tableName = tableName;
+    }
 }
