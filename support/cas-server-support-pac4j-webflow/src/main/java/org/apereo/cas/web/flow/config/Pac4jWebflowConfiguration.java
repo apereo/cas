7fee1b26958ed0d41f24fbbf3a7dc966360f7999
francis <cifren@msn.com>
2018-01-15 14:18:37 +0100

    Add 'unauthorizedRedirectUrl' usable for p4j module

--- support/cas-server-support-pac4j-webflow/src/main/java/org/apereo/cas/web/flow/config/Pac4jWebflowConfiguration.java
+++ support/cas-server-support-pac4j-webflow/src/main/java/org/apereo/cas/web/flow/config/Pac4jWebflowConfiguration.java
@@ -1,14 +1,19 @@
 package org.apereo.cas.web.flow.config;
 
+import org.apereo.cas.authentication.AuthenticationServiceSelectionPlan;
 import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.services.ServicesManager;
+import org.apereo.cas.util.CollectionUtils;
 import org.apereo.cas.web.flow.CasWebflowConfigurer;
 import org.apereo.cas.web.flow.Pac4jErrorViewResolver;
 import org.apereo.cas.web.flow.Pac4jWebflowConfigurer;
+import org.apereo.cas.web.support.ArgumentExtractor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.web.ErrorViewResolver;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +21,7 @@ import org.springframework.context.annotation.DependsOn;
 import org.springframework.webflow.definition.registry.FlowDefinitionRegistry;
 import org.springframework.webflow.engine.builder.support.FlowBuilderServices;
 import org.springframework.webflow.execution.Action;
+import org.apereo.cas.web.flow.initialFlowSetupP4jAction;
 
 /**
  * This is {@link Pac4jWebflowConfiguration}.
@@ -39,7 +45,7 @@ public class Pac4jWebflowConfiguration {
 
     @Autowired
     private CasConfigurationProperties casProperties;
-    
+
     @Autowired
     @Qualifier("saml2ClientLogoutAction")
     private Action saml2ClientLogoutAction;
@@ -47,7 +53,15 @@ public class Pac4jWebflowConfiguration {
     @Autowired
     @Qualifier("logoutFlowRegistry")
     private FlowDefinitionRegistry logoutFlowDefinitionRegistry;
-    
+
+    @Autowired
+    @Qualifier("authenticationServiceSelectionPlan")
+    private AuthenticationServiceSelectionPlan authenticationRequestServiceSelectionStrategies;
+
+    @Autowired
+    @Qualifier("servicesManager")
+    private ServicesManager servicesManager;
+
     @ConditionalOnMissingBean(name = "pac4jWebflowConfigurer")
     @Bean
     @DependsOn("defaultWebflowConfigurer")
@@ -61,5 +75,18 @@ public class Pac4jWebflowConfiguration {
     @Bean
     public ErrorViewResolver pac4jErrorViewResolver() {
         return new Pac4jErrorViewResolver();
+    }
+
+    @RefreshScope
+    @Bean
+    @Autowired
+    @ConditionalOnMissingBean(name = "initialFlowSetupP4jAction")
+    public Action initialFlowSetupP4jAction(@Qualifier("argumentExtractor") final ArgumentExtractor argumentExtractor) {
+        return new initialFlowSetupP4jAction(
+            CollectionUtils.wrap(argumentExtractor),
+            servicesManager,
+            authenticationRequestServiceSelectionStrategies,
+            casProperties
+        );
     }
 }
