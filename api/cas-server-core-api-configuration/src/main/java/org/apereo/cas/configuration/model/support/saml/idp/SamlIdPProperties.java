c63ce4de7f3138d72ceeb98edbb99f080204d60b
Misagh Moayyed <mm1844@gmail.com>
2018-01-16 16:24:31 +0330

    Fix checkstyle error

--- api/cas-server-core-api-configuration/src/main/java/org/apereo/cas/configuration/model/support/saml/idp/SamlIdPProperties.java
+++ api/cas-server-core-api-configuration/src/main/java/org/apereo/cas/configuration/model/support/saml/idp/SamlIdPProperties.java
@@ -1,16 +1,15 @@
 package org.apereo.cas.configuration.model.support.saml.idp;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.apereo.cas.configuration.model.support.saml.idp.metadata.SamlIdPMetadataProperties;
 import org.apereo.cas.configuration.support.RequiredProperty;
 import org.apereo.cas.configuration.support.RequiresModule;
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
-import lombok.Getter;
-import lombok.Setter;
+import java.io.Serializable;
+import java.util.List;
 
 /**
  * This is {@link SamlIdPProperties}.
