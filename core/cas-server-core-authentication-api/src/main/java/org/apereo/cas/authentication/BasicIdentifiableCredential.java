275f6125f2af4e17ff164fe3f208a794a1f197dd
Misagh Moayyed <mmoayyed@unicon.net>
2018-01-18 20:03:02 +0330

    Clean up hashcode/equals

--- core/cas-server-core-authentication-api/src/main/java/org/apereo/cas/authentication/BasicIdentifiableCredential.java
+++ core/cas-server-core-authentication-api/src/main/java/org/apereo/cas/authentication/BasicIdentifiableCredential.java
@@ -1,9 +1,11 @@
 package org.apereo.cas.authentication;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 import java.io.Serializable;
+
 import lombok.ToString;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,38 +22,11 @@ import lombok.Setter;
 @ToString
 @Getter
 @Setter
+@AllArgsConstructor
+@EqualsAndHashCode
 public class BasicIdentifiableCredential implements Credential, Serializable {
 
     private static final long serialVersionUID = -700605020472810939L;
 
     private String id;
-
-    /**
-     * Instantiates a new Basic identifiable credential.
-     *
-     * @param id the id
-     */
-    public BasicIdentifiableCredential(final String id) {
-        this.id = id;
-    }
-
-    @Override
-    public boolean equals(final Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (obj == this) {
-            return true;
-        }
-        if (obj.getClass() != getClass()) {
-            return false;
-        }
-        final BasicIdentifiableCredential rhs = (BasicIdentifiableCredential) obj;
-        return new EqualsBuilder().append(this.id, rhs.id).isEquals();
-    }
-
-    @Override
-    public int hashCode() {
-        return new HashCodeBuilder().append(this.id).toHashCode();
-    }
 }
