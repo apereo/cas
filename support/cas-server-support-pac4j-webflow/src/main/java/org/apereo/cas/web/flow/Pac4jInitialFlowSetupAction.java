622d48d0de33a28d9f6e525e8567a02961160cd7
francis <cifren@msn.com>
2018-01-18 10:32:29 +0100

    take the method off of private in InitialFlowSetupAction

--- support/cas-server-support-pac4j-webflow/src/main/java/org/apereo/cas/web/flow/Pac4jInitialFlowSetupAction.java
+++ support/cas-server-support-pac4j-webflow/src/main/java/org/apereo/cas/web/flow/Pac4jInitialFlowSetupAction.java
@@ -31,13 +31,6 @@ public class Pac4jInitialFlowSetupAction extends InitialFlowSetupAction {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Pac4jInitialFlowSetupAction.class);
 
-    private final CasConfigurationProperties casProperties;
-    private final ServicesManager servicesManager;
-    private final AuthenticationServiceSelectionPlan authenticationRequestServiceSelectionStrategies;
-    private final CookieRetrievingCookieGenerator warnCookieGenerator;
-    private final CookieRetrievingCookieGenerator ticketGrantingTicketCookieGenerator;
-    private final List<ArgumentExtractor> argumentExtractors;
-
     public Pac4jInitialFlowSetupAction(final List<ArgumentExtractor> argumentExtractors,
                                   final ServicesManager servicesManager,
                                   final AuthenticationServiceSelectionPlan authenticationRequestServiceSelectionPlan,
@@ -46,48 +39,11 @@ public class Pac4jInitialFlowSetupAction extends InitialFlowSetupAction {
                                   final CasConfigurationProperties casProperties) {
 
         super(argumentExtractors, servicesManager, authenticationRequestServiceSelectionPlan, ticketGrantingTicketCookieGenerator, warnCookieGenerator, casProperties);
-
-        this.argumentExtractors = argumentExtractors;
-        this.servicesManager = servicesManager;
-        this.authenticationRequestServiceSelectionStrategies = authenticationRequestServiceSelectionPlan;
-        this.ticketGrantingTicketCookieGenerator = ticketGrantingTicketCookieGenerator;
-        this.warnCookieGenerator = warnCookieGenerator;
-        this.casProperties = casProperties;
     }
 
     @Override
     protected Event doExecute(final RequestContext context) {
         configureWebflowContextForService(context);
         return success();
-    }
-
-    private void configureWebflowContextForService(final RequestContext context) {
-        final Service service = WebUtils.getService(this.argumentExtractors, context);
-        if (service != null) {
-            LOGGER.debug("Placing service in context scope: [{}]", service.getId());
-
-            final Service selectedService = authenticationRequestServiceSelectionStrategies.resolveService(service);
-            final RegisteredService registeredService = this.servicesManager.findServiceBy(selectedService);
-            if (registeredService != null && registeredService.getAccessStrategy().isServiceAccessAllowed()) {
-                LOGGER.debug("Placing registered service [{}] with id [{}] in context scope",
-                        registeredService.getServiceId(),
-                        registeredService.getId());
-                WebUtils.putRegisteredService(context, registeredService);
-
-                final RegisteredServiceAccessStrategy accessStrategy = registeredService.getAccessStrategy();
-                if (accessStrategy.getUnauthorizedRedirectUrl() != null) {
-                    LOGGER.debug("Placing registered service's unauthorized redirect url [{}] with id [{}] in context scope",
-                            accessStrategy.getUnauthorizedRedirectUrl(),
-                            registeredService.getServiceId());
-                    WebUtils.putUnauthorizedRedirectUrl(context, accessStrategy.getUnauthorizedRedirectUrl());
-                }
-            }
-        } else if (!casProperties.getSso().isMissingService()) {
-            LOGGER.warn("No service authentication request is available at [{}]. CAS is configured to disable the flow.",
-                    WebUtils.getHttpServletRequestFromExternalWebflowContext(context).getRequestURL());
-            throw new NoSuchFlowExecutionException(context.getFlowExecutionContext().getKey(),
-                    new UnauthorizedServiceException("screen.service.required.message", "Service is required"));
-        }
-        WebUtils.putService(context, service);
     }
 }
