80ada22ef0827b005386daf6e7e2cadd18976bb5
Misagh Moayyed <mm1844@gmail.com>
2018-01-18 16:12:39 +0330

    build sneaky throws

--- support/cas-server-support-ldap-service-registry/src/test/java/org/apereo/cas/adaptors/ldap/services/LdapServiceRegistryDaoTests.java
+++ support/cas-server-support-ldap-service-registry/src/test/java/org/apereo/cas/adaptors/ldap/services/LdapServiceRegistryDaoTests.java
@@ -37,8 +37,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static org.junit.Assert.*;
 
