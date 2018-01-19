4186d6b37cc0943e8764451d875fc355a52eebdc
Misagh Moayyed <mm1844@gmail.com>
2018-01-19 12:37:03 +0330

    Fixing test cases

--- core/cas-server-core-services/src/test/java/org/apereo/cas/authentication/HttpBasedServiceCredentialTests.java
+++ core/cas-server-core-services/src/test/java/org/apereo/cas/authentication/HttpBasedServiceCredentialTests.java
@@ -27,20 +27,20 @@ public class HttpBasedServiceCredentialTests {
     @Test
     public void verifyProperUrl() {
         assertEquals(CoreAuthenticationTestUtils.CONST_GOOD_URL, 
-                CoreAuthenticationTestUtils.getHttpBasedServiceCredentials().getCallbackUrl().toExternalForm());
+                CoreAuthenticationTestUtils.getHttpBasedServiceCredentials().getCallbackUrl());
     }
 
     @Test
     public void verifyEqualsWithNull() throws Exception {
-        final HttpBasedServiceCredential c = new HttpBasedServiceCredential(new URL(CNN_URL), CoreAuthenticationTestUtils.getRegisteredService(SOME_APP_URL));
+        final HttpBasedServiceCredential c = new HttpBasedServiceCredential(CNN_URL, CoreAuthenticationTestUtils.getRegisteredService(SOME_APP_URL));
         assertNotEquals(c, null);
     }
 
     @Test
     public void verifyEqualsWithFalse() throws Exception {
-        final HttpBasedServiceCredential c = new HttpBasedServiceCredential(new URL(CNN_URL),
+        final HttpBasedServiceCredential c = new HttpBasedServiceCredential(CNN_URL,
                 CoreAuthenticationTestUtils.getRegisteredService(SOME_APP_URL));
-        final HttpBasedServiceCredential c2 = new HttpBasedServiceCredential(new URL("http://www.msn.com"),
+        final HttpBasedServiceCredential c2 = new HttpBasedServiceCredential("http://www.msn.com",
                 CoreAuthenticationTestUtils.getRegisteredService(SOME_APP_URL));
 
         assertFalse(c.equals(c2));
@@ -49,8 +49,8 @@ public class HttpBasedServiceCredentialTests {
 
     @Test
     public void verifyEqualsWithTrue() throws Exception {
-        final HttpBasedServiceCredential c = new HttpBasedServiceCredential(new URL(CNN_URL), RegisteredServiceTestUtils.getRegisteredService(SOME_APP_URL));
-        final HttpBasedServiceCredential c2 = new HttpBasedServiceCredential(new URL(CNN_URL), RegisteredServiceTestUtils.getRegisteredService(SOME_APP_URL));
+        final HttpBasedServiceCredential c = new HttpBasedServiceCredential(CNN_URL, RegisteredServiceTestUtils.getRegisteredService(SOME_APP_URL));
+        final HttpBasedServiceCredential c2 = new HttpBasedServiceCredential(CNN_URL, RegisteredServiceTestUtils.getRegisteredService(SOME_APP_URL));
 
         assertTrue(c.equals(c2));
         assertTrue(c2.equals(c));
@@ -59,7 +59,7 @@ public class HttpBasedServiceCredentialTests {
     @Test
     public void verifySerializeAnHttpBasedServiceCredentialToJson() throws IOException {
         final HttpBasedServiceCredential credentialMetaDataWritten = 
-                new HttpBasedServiceCredential(new URL(CNN_URL),
+                new HttpBasedServiceCredential(CNN_URL,
                 RegisteredServiceTestUtils.getRegisteredService(SOME_APP_URL));
 
         MAPPER.writeValue(JSON_FILE, credentialMetaDataWritten);
