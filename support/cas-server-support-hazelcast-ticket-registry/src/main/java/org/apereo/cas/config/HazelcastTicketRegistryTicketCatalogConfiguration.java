816a09be71076d9572b91588bae87234fcd93e3b
Misagh Moayyed <mm1844@gmail.com>
2018-01-22 17:06:49 +0330

    Update HazelcastTicketRegistryTicketCatalogConfiguration.java

--- support/cas-server-support-hazelcast-ticket-registry/src/main/java/org/apereo/cas/config/HazelcastTicketRegistryTicketCatalogConfiguration.java
+++ support/cas-server-support-hazelcast-ticket-registry/src/main/java/org/apereo/cas/config/HazelcastTicketRegistryTicketCatalogConfiguration.java
@@ -34,7 +34,7 @@ public class HazelcastTicketRegistryTicketCatalogConfiguration extends CasCoreTi
     protected void buildAndRegisterProxyTicketDefinition(final TicketCatalog plan, final TicketDefinition metadata) {
         metadata.getProperties().setStorageName("proxyTicketsCache");
         metadata.getProperties().setStorageTimeout(casProperties.getTicket().getPt().getTimeToKillInSeconds());
-        super.buildAndRegisterServiceTicketDefinition(plan, metadata);
+        super.buildAndRegisterProxyTicketDefinition(plan, metadata);
     }
 
     @Override
