d2ba9e120972ed807b9b905ce8d33d828c5593ca
Misagh Moayyed <mm1844@gmail.com>
2018-01-24 16:28:41 +0330

    ensure backup metadata is cleaned up for service matches

--- support/cas-server-support-saml-idp/src/main/java/org/apereo/cas/support/saml/services/idp/metadata/cache/resolver/UrlResourceMetadataResolver.java
+++ support/cas-server-support-saml-idp/src/main/java/org/apereo/cas/support/saml/services/idp/metadata/cache/resolver/UrlResourceMetadataResolver.java
@@ -1,7 +1,15 @@
 package org.apereo.cas.support.saml.services.idp.metadata.cache.resolver;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOCase;
+import org.apache.commons.io.filefilter.AndFileFilter;
+import org.apache.commons.io.filefilter.CanReadFileFilter;
+import org.apache.commons.io.filefilter.CanWriteFileFilter;
+import org.apache.commons.io.filefilter.PrefixFileFilter;
+import org.apache.commons.io.filefilter.SuffixFileFilter;
+import org.apache.commons.io.filefilter.TrueFileFilter;
 import org.apereo.cas.configuration.model.support.saml.idp.SamlIdPProperties;
 import org.apereo.cas.configuration.model.support.saml.idp.metadata.SamlIdPMetadataProperties;
 import org.apereo.cas.services.RegisteredService;
@@ -29,21 +37,31 @@ import java.util.UUID;
  */
 @Slf4j
 public class UrlResourceMetadataResolver extends BaseSamlRegisteredServiceMetadataResolver {
-
-
     private final HttpClient httpClient;
 
+    private File metadataBackupDirectory;
+
+    @SneakyThrows
     public UrlResourceMetadataResolver(final SamlIdPProperties samlIdPProperties,
                                        final OpenSamlConfigBean configBean,
                                        final HttpClient httpClient) {
         super(samlIdPProperties, configBean);
+
         this.httpClient = httpClient;
+        final SamlIdPMetadataProperties md = samlIdPProperties.getMetadata();
+        this.metadataBackupDirectory = new File(md.getLocation().getFile(), "metadata-backups");
+        try {
+            FileUtils.forceMkdir(this.metadataBackupDirectory);
+        } catch (final Exception e) {
+            LOGGER.error("Unable to create metadata backup directory [{}] to store downloaded metadata. "
+                + "This is likely due to a permission issue", this.metadataBackupDirectory);
+            LOGGER.debug(e.getMessage(), e);
+        }
     }
 
     @Override
     public Collection<MetadataResolver> resolve(final SamlRegisteredService service) {
         try {
-
             final String metadataLocation = service.getMetadataLocation();
             LOGGER.info("Loading SAML metadata from [{}]", metadataLocation);
             final AbstractResource metadataResource = ResourceUtils.getResourceFrom(metadataLocation);
@@ -52,6 +70,8 @@ public class UrlResourceMetadataResolver extends BaseSamlRegisteredServiceMetada
             final String canonicalPath = backupFile.getCanonicalPath();
             LOGGER.debug("Metadata backup file will be at [{}]", canonicalPath);
             FileUtils.forceMkdirParent(backupFile);
+
+            cleanUpExpiredBackupMetadataFilesFor(metadataResource, service);
 
             final FileBackedHTTPMetadataResolver metadataProvider = new FileBackedHTTPMetadataResolver(
                 this.httpClient.getWrappedHttpClient(), metadataResource.getURL().toExternalForm(),
@@ -64,27 +84,50 @@ public class UrlResourceMetadataResolver extends BaseSamlRegisteredServiceMetada
         return new ArrayList<>(0);
     }
 
+    private void cleanUpExpiredBackupMetadataFilesFor(final AbstractResource metadataResource, final SamlRegisteredService service) {
+        final String prefix = getBackupMetadataFilenamePrefix(metadataResource, service);
+        final Collection<File> backups = FileUtils.listFiles(this.metadataBackupDirectory,
+            new AndFileFilter(CollectionUtils.wrapList(new PrefixFileFilter(prefix, IOCase.INSENSITIVE),
+                new SuffixFileFilter(".xml", IOCase.INSENSITIVE),
+                CanWriteFileFilter.CAN_WRITE, CanReadFileFilter.CAN_READ)), TrueFileFilter.INSTANCE);
+        backups.forEach(file -> {
+            try {
+                FileUtils.forceDelete(file);
+            } catch (final Exception e) {
+                LOGGER.warn("Unable to delete metadata backup file [{}]", file);
+                LOGGER.debug(e.getMessage(), e);
+            }
+        });
+    }
+
     private File getMetadataBackupFile(final AbstractResource metadataResource,
                                        final RegisteredService service) throws IOException {
-        final SamlIdPMetadataProperties md = samlIdPProperties.getMetadata();
-        final File backupDirectory = new File(md.getLocation().getFile(), "metadata-backups");
-        LOGGER.debug("Metadata backup directory is at [{}]", backupDirectory.getCanonicalPath());
 
-        final String metadataFileName = service.getName().concat("-").concat(UUID.randomUUID().toString()).concat(metadataResource.getFilename());
-        final File backupFile = new File(backupDirectory, metadataFileName);
+        LOGGER.debug("Metadata backup directory is at [{}]", this.metadataBackupDirectory.getCanonicalPath());
+
+        final String metadataFileName = getBackupMetadataFilenamePrefix(metadataResource, service)
+            .concat(getBackupMetadataFilenameSuffix(metadataResource, service));
+
+        final File backupFile = new File(this.metadataBackupDirectory, metadataFileName);
         if (backupFile.exists()) {
             LOGGER.warn("Metadata file designated for service [{}] already exists at path [{}].", service.getName(), backupFile.getCanonicalPath());
         } else {
             LOGGER.debug("Metadata to fetch for service [{}] will be placed at [{}]", service.getName(), backupFile.getCanonicalPath());
         }
-        try {
-            FileUtils.forceMkdir(backupDirectory);
-        } catch (final Exception e) {
-            LOGGER.error("Unable to create metadata backup directory [{}] to store downloaded metadata from [{}]. "
-                + "This is likely due to a permission issue", backupDirectory, metadataResource);
-            LOGGER.debug(e.getMessage(), e);
-        }
         return backupFile;
+    }
+
+    private String getBackupMetadataFilenameSuffix(final AbstractResource metadataResource, final RegisteredService service) {
+        return UUID.randomUUID().toString() + ".xml";
+    }
+
+    private String getBackupMetadataFilenamePrefix(final AbstractResource metadataResource, final RegisteredService service) {
+        return service.getName()
+            .concat("-")
+            .concat(String.valueOf(service.getId()))
+            .concat("-")
+            .concat(metadataResource.getFilename())
+            .concat("-");
     }
 
     @Override
