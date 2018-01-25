8f7bde925abe9aa12c971491909a9c5fcd5f696d
Misagh Moayyed <mm1844@gmail.com>
2018-01-25 13:22:51 +0330

    add auditing support to saml2 for responses

--- api/cas-server-core-api-audit/src/main/java/org/apereo/cas/audit/AuditTrailRecordResolutionPlan.java
+++ api/cas-server-core-api-audit/src/main/java/org/apereo/cas/audit/AuditTrailRecordResolutionPlan.java
@@ -13,15 +13,47 @@ import java.util.Map;
  */
 public interface AuditTrailRecordResolutionPlan {
 
+    /**
+     * Register audit resource resolver.
+     *
+     * @param key      the key
+     * @param resolver the resolver
+     */
     void registerAuditResourceResolver(String key, AuditResourceResolver resolver);
 
+    /**
+     * Register audit action resolver.
+     *
+     * @param key      the key
+     * @param resolver the resolver
+     */
     void registerAuditActionResolver(String key, AuditActionResolver resolver);
 
+    /**
+     * Register audit action resolvers.
+     *
+     * @param resolvers the resolvers
+     */
     void registerAuditActionResolvers(Map<String, AuditActionResolver> resolvers);
 
+    /**
+     * Register audit resource resolvers.
+     *
+     * @param resolvers the resolvers
+     */
     void registerAuditResourceResolvers(Map<String, AuditResourceResolver> resolvers);
-    
+
+    /**
+     * Gets audit resource resolvers.
+     *
+     * @return the audit resource resolvers
+     */
     Map<String, AuditResourceResolver> getAuditResourceResolvers();
 
+    /**
+     * Gets audit action resolvers.
+     *
+     * @return the audit action resolvers
+     */
     Map<String, AuditActionResolver> getAuditActionResolvers();
 }
