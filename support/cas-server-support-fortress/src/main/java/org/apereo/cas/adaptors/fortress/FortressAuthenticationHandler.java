099cb641f008564bc6f13d401970649d5c39b4cb
Daniel Frett <frett@users.noreply.github.com>
2018-02-07 14:01:37 -0500

    fix an NPE in the GoogleAuthenticatorAuthenticationHandler (#3195)
    * fix an NPE in the GoogleAuthenticatorAuthenticationHandler
    
    * fix additional locations createHandlerResult is called with null warnings

--- support/cas-server-support-fortress/src/main/java/org/apereo/cas/adaptors/fortress/FortressAuthenticationHandler.java
+++ support/cas-server-support-fortress/src/main/java/org/apereo/cas/adaptors/fortress/FortressAuthenticationHandler.java
@@ -70,7 +70,7 @@ public class FortressAuthenticationHandler extends AbstractUsernamePasswordAuthe
                 LOGGER.debug("Fortress session result: [{}]", fortressXmlSession);
                 final Map<String, Object> attributes = new HashMap<>();
                 attributes.put(FORTRESS_SESSION_KEY, fortressXmlSession);
-                return createHandlerResult(c, principalFactory.createPrincipal(username, attributes), null);
+                return createHandlerResult(c, principalFactory.createPrincipal(username, attributes));
             } else {
                 LOGGER.warn("Could not establish a fortress session or session cannot authenticate");
             }
