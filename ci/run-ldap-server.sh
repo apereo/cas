1a2f54ecfd0e67420e995a355eb1b910a9e09b7c
Misagh Moayyed <mm1844@gmail.com>
2018-02-08 14:02:20 +0330

    add dockerized ldap

--- ci/run-ldap-server.sh
+++ ci/run-ldap-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone --depth 1 git@github.com:jtgasper3/docker-images.git
+git clone --depth 1 https://github.com/jtgasper3/docker-images.git
 cp ./ci/ldap/users.ldif docker-images/openldap
 docker build --tag="apereo/openldap" .
 docker run -d -p 10389:389 --name="ldap-server" apereo/openldap
