6a3fa883ac8139df1c08877c856a3da3001e46d6
Misagh Moayyed <mm1844@gmail.com>
2018-02-08 14:55:20 +0330

    add dockerized ldap

--- ci/run-ldap-server.sh
+++ ci/run-ldap-server.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-git clone --depth 1 https://github.com/jtgasper3/docker-images.git
-
 echo "Copying LDIF files to prepare LDAP server..."
-cat ./support/cas-server-support-ldap/src/test/resources/ldif/ldap-base.ldif >>  docker-images/389-ds/users.ldap
+cat ./support/cas-server-support-ldap/src/test/resources/ldif/ldap-base.ldif >>  ./ci/ldap/users.ldif
 
 echo "Building LDAP docker image..."
-docker build --tag="apereocastests/ldap"  ./docker-images/389-ds
+docker build --tag="apereocastests/ldap"  ./ci/ldap
 docker images
 
 echo "Running LDAP docker image"
