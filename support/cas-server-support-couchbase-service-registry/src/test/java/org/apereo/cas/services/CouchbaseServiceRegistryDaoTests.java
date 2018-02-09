d776eb52424e6dbcb3acf97a58a5a84aaf3bf548
Misagh Moayyed <mm1844@gmail.com>
2018-02-09 22:37:41 +0330

    fix javadoc issue; continue with tests

--- support/cas-server-support-couchbase-service-registry/src/test/java/org/apereo/cas/services/CouchbaseServiceRegistryDaoTests.java
+++ support/cas-server-support-couchbase-service-registry/src/test/java/org/apereo/cas/services/CouchbaseServiceRegistryDaoTests.java
@@ -3,11 +3,8 @@ package org.apereo.cas.services;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.message.BasicNameValuePair;
 import org.apereo.cas.config.CouchbaseServiceRegistryConfiguration;
-import org.apereo.cas.util.HttpUtils;
+import org.apereo.cas.couchbase.core.CouchbaseClientFactory;
 import org.apereo.cas.util.junit.ConditionalIgnore;
 import org.apereo.cas.util.junit.ConditionalSpringRunner;
 import org.apereo.cas.util.junit.RunningContinuousIntegrationCondition;
@@ -53,20 +50,13 @@ public class CouchbaseServiceRegistryDaoTests {
     public static void setup() {
         cleanup();
 
-        final List postParameters = new ArrayList<NameValuePair>();
-        postParameters.add(new BasicNameValuePair("authType", "none"));
-        postParameters.add(new BasicNameValuePair("name", "default"));
-        postParameters.add(new BasicNameValuePair("bucketType", "couchbase"));
-        postParameters.add(new BasicNameValuePair("proxyPort", "11216"));
-        postParameters.add(new BasicNameValuePair("ramQuotaMB", "120"));
-        final UrlEncodedFormEntity entity = new UrlEncodedFormEntity(postParameters, "UTF-8");
-        final HttpResponse response = HttpUtils.executePost("http://localhost:8091/pools/default/buckets", entity);
+        final HttpResponse response = CouchbaseClientFactory.createDefaultBucket();
         assertEquals(202, response.getStatusLine().getStatusCode());
     }
 
     @AfterClass
     public static void cleanup() {
-        HttpUtils.execute("http://localhost:8091/pools/default/buckets/default", "DELETE");
+        CouchbaseClientFactory.removeDefaultBucket();
     }
 
     @Before
