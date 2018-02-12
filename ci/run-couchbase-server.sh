749a185a8e5c846582a1f2c0ef69582f7593301a
Misagh Moayyed <mm1844@gmail.com>
2018-02-12 18:18:24 +0330

    cntd with tests

--- ci/run-couchbase-server.sh
+++ ci/run-couchbase-server.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
-while sleep 9m; do echo -e '\n=====[ Gradle build is still running ]====='; done &
+if [ "$PUBLISH_SNAPSHOTS" == "false" ]; then
+    while sleep 9m; do echo -e '\n=====[ Gradle build is still running ]====='; done &
 
-echo "Pulling Couchbase docker image..."
-docker pull couchbase/server:4.6.4
+    echo "Pulling Couchbase docker image..."
+    docker pull couchbase/server:4.6.4
 
-echo "Running Couchbase docker image..."
-docker run -d --name couchbase -p 8091-8094:8091-8094 -p 11210:11210 couchbase/server:4.6.4
+    echo "Running Couchbase docker image..."
+    docker run -d --name couchbase -p 8091-8094:8091-8094 -p 11210:11210 couchbase/server:4.6.4
 
-docker ps | grep "couchbase"
-retVal=$?
-if [ $retVal == 0 ]; then
-    echo "Couchbase docker image is running."
-else
-    echo "Couchbase docker image failed to start."
-    exit $retVal
+    docker ps | grep "couchbase"
+    retVal=$?
+    if [ $retVal == 0 ]; then
+        echo "Couchbase docker image is running."
+    else
+        echo "Couchbase docker image failed to start."
+        exit $retVal
+    fi
+
+    echo "Waiting for Couchbase server to come online..."
+    until $(curl --output /dev/null --silent --head --fail http://localhost:8091); do
+        printf '.'
+        sleep 1
+    done
 fi
-
-echo "Waiting for Couchbase server to come online..."
-until $(curl --output /dev/null --silent --head --fail http://localhost:8091); do
-    printf '.'
-    sleep 1
-done
 
 echo -e "\nCreating default Couchbase bucket..."
 curl -X POST -d 'name=default' -d 'bucketType=couchbase' -d 'ramQuotaMB=120' -d 'authType=none' -d 'proxyPort=11216' http://localhost:8091/pools/default/buckets
