dcb1b207c78cf6abedc8fb5d1aa665dcf5e9af40
Misagh Moayyed <mm1844@gmail.com>
2018-02-12 14:40:58 +0330

    clean up tests

--- ci/run-radius-server.sh
+++ ci/run-radius-server.sh
@@ -6,7 +6,8 @@ echo "Pulling FreeRadius docker image..."
 docker pull tpdock/freeradius:2.2.9
 
 echo "Running FreeRadius docker image..."
-docker run -itd --name radius-server -e RADIUS_LISTEN_IP=* -e RADIUS_CLIENTS=secret@127.0.0.1 -p 1812:1812/udp -p 1813:1813/udp tpdock/freeradius:2.2.9
+docker exec -it --name-="radius-server" tpdock/freeradius:2.2.9 mysql -ufreeradius -pfreeradius -hmysql -e "source /etc/freeradius/sql/mysql/schema.sql" tpdock/freeradius:2.2.9
+docker exec -it radius-server mysql -ufreeradius -pfreeradius -hmysql -e "insert into radcheck (username, attribute, op, value) values ('casuser', 'Mellon', ':=', 'casuser');" freeradius
 
 docker ps | grep "radius-server"
 retVal=$?
