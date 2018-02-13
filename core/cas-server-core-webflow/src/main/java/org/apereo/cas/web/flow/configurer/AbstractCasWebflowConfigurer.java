ffb12c67fb0e99f0fa25a1b29b3f095a30d6097d
Misagh Moayyed <mmoayyed@unicon.net>
2018-02-13 23:09:54 +0330

    checkstyle fixes

--- core/cas-server-core-webflow/src/main/java/org/apereo/cas/web/flow/configurer/AbstractCasWebflowConfigurer.java
+++ core/cas-server-core-webflow/src/main/java/org/apereo/cas/web/flow/configurer/AbstractCasWebflowConfigurer.java
@@ -67,8 +67,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 /**
  * The {@link AbstractCasWebflowConfigurer} is responsible for
