4c8aa1d552a23574f381a782e9fc7849ee32f860
Misagh Moayyed <mm1844@gmail.com>
2018-02-14 20:17:08 +0330

    Cleaning up formatting

--- support/cas-server-support-u2f/src/test/java/org/apereo/cas/adaptors/u2f/storage/AbstractU2FDeviceRepositoryTests.java
+++ support/cas-server-support-u2f/src/test/java/org/apereo/cas/adaptors/u2f/storage/AbstractU2FDeviceRepositoryTests.java
@@ -1,7 +1,6 @@
 package org.apereo.cas.adaptors.u2f.storage;
 
 import com.yubico.u2f.data.DeviceRegistration;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apereo.cas.util.crypto.CertUtils;
 import org.junit.Test;
