afe71062bd34116c24626f7f401afd93201b1d11
Misagh Moayyed <mm1844@gmail.com>
2018-02-14 22:41:59 +0330

    Fix tests

--- api/cas-server-core-api-audit/src/main/java/org/apereo/cas/audit/AuditableExecutionResult.java
+++ api/cas-server-core-api-audit/src/main/java/org/apereo/cas/audit/AuditableExecutionResult.java
@@ -5,8 +5,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.apereo.cas.authentication.Authentication;
+import org.apereo.cas.authentication.AuthenticationResult;
 import org.apereo.cas.authentication.principal.Service;
 import org.apereo.cas.services.RegisteredService;
+import org.apereo.cas.ticket.ServiceTicket;
+import org.apereo.cas.ticket.TicketGrantingTicket;
 
 import java.util.Map;
 import java.util.TreeMap;
@@ -24,8 +27,11 @@ import java.util.TreeMap;
 public class AuditableExecutionResult {
     private RegisteredService registeredService;
     private Service service;
+    private ServiceTicket serviceTicket;
     private Authentication authentication;
     private RuntimeException exception;
+    private TicketGrantingTicket ticketGrantingTicket;
+    private AuthenticationResult authenticationResult;
     private Map<String, Object> properties = new TreeMap<>();
 
     public boolean isExecutionFailure() {
@@ -71,5 +77,38 @@ public class AuditableExecutionResult {
     public static AuditableExecutionResult of(final Authentication authentication,
                                               final Service service, final RegisteredService registeredService) {
         return of(null, authentication, service, registeredService);
+    }
+
+    /**
+     * Of auditable execution result.
+     *
+     * @param serviceTicket        the service ticket
+     * @param authenticationResult the authentication result
+     * @param registeredService    the registered service
+     * @return the auditable execution result
+     */
+    public static AuditableExecutionResult of(final ServiceTicket serviceTicket, final AuthenticationResult authenticationResult,
+                                              final RegisteredService registeredService) {
+        final AuditableExecutionResult result = new AuditableExecutionResult();
+        result.setServiceTicket(serviceTicket);
+        result.setAuthenticationResult(authenticationResult);
+        result.setRegisteredService(registeredService);
+        return result;
+    }
+
+    /**
+     * Of auditable execution result.
+     *
+     * @param service              the service
+     * @param registeredService    the registered service
+     * @param ticketGrantingTicket the ticket granting ticket
+     * @return the auditable execution result
+     */
+    public static AuditableExecutionResult of(final Service service, final RegisteredService registeredService, final TicketGrantingTicket ticketGrantingTicket) {
+        final AuditableExecutionResult result = new AuditableExecutionResult();
+        result.setTicketGrantingTicket(ticketGrantingTicket);
+        result.setRegisteredService(registeredService);
+        result.setService(service);
+        return result;
     }
 }
