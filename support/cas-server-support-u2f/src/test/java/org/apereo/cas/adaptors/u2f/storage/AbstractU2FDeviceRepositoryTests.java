ca4854e0d5b05c8cdca5fa27ed1c9935a82091d7
Misagh Moayyed <mmoayyed@unicon.net>
2018-02-14 20:13:52 +0330

    Javadoc fixes

--- support/cas-server-support-u2f/src/test/java/org/apereo/cas/adaptors/u2f/storage/AbstractU2FDeviceRepositoryTests.java
+++ support/cas-server-support-u2f/src/test/java/org/apereo/cas/adaptors/u2f/storage/AbstractU2FDeviceRepositoryTests.java
@@ -22,15 +22,18 @@ import static org.junit.Assert.*;
 public abstract class AbstractU2FDeviceRepositoryTests {
 
     @Test
-    @SneakyThrows
     public void verifyDeviceSaved() {
-        final X509Certificate cert = CertUtils.readCertificate(new ClassPathResource("cert.crt"));
-        final DeviceRegistration r1 = new DeviceRegistration("keyhandle11", "publickey1", cert, 1);
-        final DeviceRegistration r2 = new DeviceRegistration("keyhandle22", "publickey1", cert, 2);
-        getDeviceRepository().registerDevice("casuser", r1);
-        getDeviceRepository().registerDevice("casuser", r2);
-        final Collection<DeviceRegistration> devs = getDeviceRepository().getRegisteredDevices("casuser");
-        assertEquals(2, devs.size());
+        try {
+            final X509Certificate cert = CertUtils.readCertificate(new ClassPathResource("cert.crt"));
+            final DeviceRegistration r1 = new DeviceRegistration("keyhandle11", "publickey1", cert, 1);
+            final DeviceRegistration r2 = new DeviceRegistration("keyhandle22", "publickey1", cert, 2);
+            getDeviceRepository().registerDevice("casuser", r1);
+            getDeviceRepository().registerDevice("casuser", r2);
+            final Collection<DeviceRegistration> devs = getDeviceRepository().getRegisteredDevices("casuser");
+            assertEquals(2, devs.size());
+        } catch (final Exception e) {
+            throw new AssertionError(e.getMessage(), e);
+        }
     }
 
     protected abstract U2FDeviceRepository getDeviceRepository();
