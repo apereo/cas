c0555cbf0e33fd8ab9cf4ab15f60f4b1b1a8c77f
Misagh Moayyed <mm1844@gmail.com>
2018-02-16 04:39:37 -0700

    checkstyle fixes

--- support/cas-server-support-surrogate-api/src/main/java/org/apereo/cas/services/GroovySurrogateRegisteredServiceAccessStrategy.java
+++ support/cas-server-support-surrogate-api/src/main/java/org/apereo/cas/services/GroovySurrogateRegisteredServiceAccessStrategy.java
@@ -20,7 +20,7 @@ import java.util.Map;
 @Getter
 @Setter
 @EqualsAndHashCode(callSuper = true)
-public class GroovySurrogateRegisteredServiceAccessStrategy extends BaseSurrogateextendsRegisteredServiceAccessStrategy {
+public class GroovySurrogateRegisteredServiceAccessStrategy extends BaseSurrogateRegisteredServiceAccessStrategy {
     private static final long serialVersionUID = -3998531629984937388L;
 
     private String groovyScript;
