5da408ea4e72efd83f03a71ef1d07083e036f895
Misagh Moayyed <mm1844@gmail.com>
2018-02-19 14:09:45 +0330

    working on java 9

--- webapp/cas-server-webapp-undertow/src/main/java/org/apereo/cas/config/CasEmbeddedContainerUndertowConfiguration.java
+++ webapp/cas-server-webapp-undertow/src/main/java/org/apereo/cas/config/CasEmbeddedContainerUndertowConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
@@ -21,7 +21,7 @@ import org.springframework.core.Ordered;
 @Configuration("casEmbeddedContainerUndertowConfiguration")
 @EnableConfigurationProperties(CasConfigurationProperties.class)
 @ConditionalOnProperty(name = CasEmbeddedContainerUtils.EMBEDDED_CONTAINER_CONFIG_ACTIVE, havingValue = "true")
-@AutoConfigureBefore(EmbeddedServletContainerAutoConfiguration.class)
+@AutoConfigureBefore(ServletWebServerFactoryAutoConfiguration.class)
 @AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE)
 @Slf4j
 public class CasEmbeddedContainerUndertowConfiguration {
