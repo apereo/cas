4a62a56598812e3e8c60f659cd698f053501b069
Misagh Moayyed <mm1844@gmail.com>
2018-02-21 01:31:32 +0330

    fix checkstyle

--- core/cas-server-core-authentication-api/src/main/java/org/apereo/cas/authentication/BasicIdentifiableCredential.java
+++ core/cas-server-core-authentication-api/src/main/java/org/apereo/cas/authentication/BasicIdentifiableCredential.java
@@ -4,8 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.Serializable;
-
 import lombok.ToString;
 import lombok.Getter;
 import lombok.Setter;
