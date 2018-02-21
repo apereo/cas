75d6266a8028d26c9484b4717a12d9cf8cc385c3
Misagh Moayyed <mm1844@gmail.com>
2018-02-21 15:14:29 +0330

    working on protocol attribute rendering

---
+++ api/cas-server-core-api-validation/src/main/java/org/apereo/cas/validation/CasProtocolAttributesRenderer.java
@@ -0,0 +1,24 @@
+package org.apereo.cas.validation;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * This is {@link CasProtocolAttributesRenderer} that decides how cas protocol attributes
+ * should be rendered in the final validation response. Implementations are free to choice the proper format
+ * in the way that multi-valued attributes or prefixes are defined.
+ *
+ * @author Misagh Moayyed
+ * @since 5.3.0
+ */
+@FunctionalInterface
+public interface CasProtocolAttributesRenderer {
+
+    /**
+     * Render attributes.
+     *
+     * @param attributes the attributes, expected to be finalized and encoded.
+     * @return the collection
+     */
+    Collection<String> render(Map<String, Object> attributes);
+}
