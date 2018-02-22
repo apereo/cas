c246d1c21c1ee9b30e525f59ab5a39c65ca6707b
Misagh Moayyed <mmoayyed@unicon.net>
2018-02-22 20:03:21 +0330

    break down build into a better matrix

--- ci/install.sh
+++ ci/install.sh
@@ -1,38 +1,29 @@
 #!/bin/bash
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$PUBLISH_SNAPSHOTS" == "true" ]; then
-    echo -e "Skipping build since this is a pull request and we are not publishing snapshots.\n"
-    exit 0
-fi
-
 gradle="sudo ./gradlew $@"
 gradleBuild="assemble"
 gradleBuildOptions="--stacktrace --build-cache --configure-on-demand -DskipNestedConfigMetadataGen=true --parallel "
 
-if [ "$PUBLISH_SNAPSHOTS" == "false" ]; then
-    echo -e "The build will aggregate javadocs from all modules into one JAR file.\n"
-    gradleBuild="$gradleBuild checkstyleMain"
-    
-    if [[ "${TRAVIS_COMMIT_MESSAGE}" == *"[skip tests]"* ]]; then
-        echo -e "The build commit message indicates that tests should be skipped.\n"
-        gradleBuild="$gradleBuild -x test"
-    else
-        echo -e "The build indicates that tests along with coveralls test coverage should run.\n"
-        gradleBuild="$gradleBuild checkstyleTest test coveralls"
-        if [[ "${TRAVIS_COMMIT_MESSAGE}" == *"[show streams]"* ]]; then
-            gradleBuild="$gradleBuild -DshowStandardStreams=true"
-        fi
-    fi
-else
-    echo -e "The build is publishing snapshots; Skipping tests and checks...\n"
-    gradleBuild="$gradleBuild -x test -x check -x javadoc  "
+if [ "$MATRIX_JOB_TYPE" == "STYLE" ]; then
+     gradleBuild="$gradleBuild checkstyleMain checkstyleTest -x test -x javadoc "
+elif [ "$MATRIX_JOB_TYPE" == "JAVADOC" ]; then
+     gradleBuild="$gradleBuild -x test -x check -DskipNpmLint=true "
+elif [ "$MATRIX_JOB_TYPE" == "TEST" ]; then
+    gradleBuild="$gradleBuild test coveralls -x javadoc -x check -DskipNpmLint=true "
+elif [ "$MATRIX_JOB_TYPE" == "SNAPSHOT" ]; then
+    echo -e "The build is publishing snapshots; Skipping tests and checks...\n "
+    gradleBuild="$gradleBuild -x test -x check -x javadoc -DskipNpmLint=true "
+fi
+
+if [[ "${TRAVIS_COMMIT_MESSAGE}" == *"[show streams]"* ]]; then
+    gradleBuild="$gradleBuild -DshowStandardStreams=true"
 fi
 
 tasks="$gradle $gradleBuildOptions $gradleBuild"
 echo $tasks
 
 waitRetVal=-1
-if [ "$PUBLISH_SNAPSHOTS" == "false" ]; then
+if [ "$MATRIX_JOB_TYPE" == "TEST" ]; then
     waitloop="while sleep 9m; do echo -e '\n=====[ Gradle build is still running ]====='; done &"
     eval $waitloop
     waitRetVal=$?
