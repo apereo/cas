57cb397f194e9c4aa55cebe609a17ed4ce63e6e1
Misagh Moayyed <mm1844@gmail.com>
2018-02-22 23:06:20 +0330

    contd with build matrix breakup

---
+++ core/cas-server-core-authentication-api/src/test/java/org/apereo/cas/authentication/AuthenticationCredentialTypeMetaDataPopulatorTests.java
@@ -0,0 +1,28 @@
+package org.apereo.cas.authentication;
+
+import org.apereo.cas.authentication.metadata.AuthenticationCredentialTypeMetaDataPopulator;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * This is {@link AuthenticationCredentialTypeMetaDataPopulatorTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.3.0
+ */
+public class AuthenticationCredentialTypeMetaDataPopulatorTests {
+    private AuthenticationCredentialTypeMetaDataPopulator populator =
+        new AuthenticationCredentialTypeMetaDataPopulator();
+
+    @Test
+    public void verifyPopulator() {
+        final UsernamePasswordCredential credentials = new UsernamePasswordCredential();
+        final AuthenticationBuilder builder = CoreAuthenticationTestUtils.getAuthenticationBuilder();
+        this.populator.populateAttributes(builder, AuthenticationTransaction.wrap(credentials));
+        final Authentication auth = builder.build();
+        assertEquals(
+            credentials.getClass().getSimpleName(),
+            auth.getAttributes().get(Credential.CREDENTIAL_TYPE_ATTRIBUTE));
+    }
+}
