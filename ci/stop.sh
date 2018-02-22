803d277af1d4cffc2ee5d8742e39b86ca0634a64
Misagh Moayyed <mm1844@gmail.com>
2018-02-23 00:29:27 +0330

    Finalize matrix

--- ci/stop.sh
+++ ci/stop.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker kill $(docker ps -q)
-docker rm $(docker ps -a -q)
-docker rmi $(docker images -q)
+if [ "$MATRIX_JOB_TYPE" == "TEST" ]; then
+    docker kill $(docker ps -q)
+    docker rm $(docker ps -a -q)
+    docker rmi $(docker images -q)
+fi
 
