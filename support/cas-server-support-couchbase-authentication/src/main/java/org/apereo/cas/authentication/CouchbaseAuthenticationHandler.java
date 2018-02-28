568c276c12973bb009dc1e25c62872a4e036ad36
Misagh Moayyed <mm1844@gmail.com>
2018-03-01 00:45:32 +0330

    couchbase authn tests

--- support/cas-server-support-couchbase-authentication/src/main/java/org/apereo/cas/authentication/CouchbaseAuthenticationHandler.java
+++ support/cas-server-support-couchbase-authentication/src/main/java/org/apereo/cas/authentication/CouchbaseAuthenticationHandler.java
@@ -9,7 +9,9 @@ import com.couchbase.client.java.query.SimpleN1qlQuery;
 import com.couchbase.client.java.query.Statement;
 import com.couchbase.client.java.query.dsl.Expression;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apereo.cas.authentication.handler.support.AbstractUsernamePasswordAuthenticationHandler;
+import org.apereo.cas.authentication.principal.Principal;
 import org.apereo.cas.authentication.principal.PrincipalFactory;
 import org.apereo.cas.configuration.model.support.couchbase.authentication.CouchbaseAuthenticationProperties;
 import org.apereo.cas.configuration.support.Beans;
@@ -20,8 +22,9 @@ import javax.security.auth.login.AccountNotFoundException;
 import javax.security.auth.login.FailedLoginException;
 import java.security.GeneralSecurityException;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 /**
  * This is {@link CouchbaseAuthenticationHandler}.
@@ -31,8 +34,6 @@ import java.util.concurrent.TimeUnit;
  */
 @Slf4j
 public class CouchbaseAuthenticationHandler extends AbstractUsernamePasswordAuthenticationHandler {
-
-
     private final CouchbaseAuthenticationProperties couchbaseProperties;
     private final CouchbaseClientFactory couchbase;
 
@@ -51,39 +52,56 @@ public class CouchbaseAuthenticationHandler extends AbstractUsernamePasswordAuth
     protected AuthenticationHandlerExecutionResult authenticateUsernamePasswordInternal(final UsernamePasswordCredential transformedCredential,
                                                                                         final String originalPassword) throws GeneralSecurityException {
         final Statement statement = Select.select("*")
-                .from(Expression.i(couchbaseProperties.getBucket()))
-                .where(Expression.x(couchbaseProperties.getUsernameAttribute())
-                        .eq('\'' + transformedCredential.getUsername() + '\''));
+            .from(Expression.i(couchbaseProperties.getBucket()))
+            .where(Expression.x(couchbaseProperties.getUsernameAttribute())
+                .eq('\'' + transformedCredential.getUsername() + '\''));
+
+        LOGGER.debug("Running query [{}] on bucket [{}]", statement.toString(), couchbase.getBucket().name());
 
         final SimpleN1qlQuery query = N1qlQuery.simple(statement);
-        final N1qlQueryResult result = couchbase.getBucket()
-            .query(query, Beans.newDuration(couchbaseProperties.getTimeout()).toMillis(), TimeUnit.MILLISECONDS);
-        if (result.finalSuccess()) {
-            if (result.allRows().size() > 1) {
-                throw new FailedLoginException("More then one row found for user " + transformedCredential.getId());
-            }
-            final N1qlQueryRow row = result.allRows().get(0);
-            if (row.value().containsKey(couchbase.getBucket().name())) {
-                final JsonObject value = (JsonObject) row.value().get(couchbase.getBucket().name());
-                if (!value.containsKey(couchbaseProperties.getUsernameAttribute())) {
-                    throw new FailedLoginException("No user attribute found for " + transformedCredential.getId());
-                }
-                if (!value.containsKey(couchbaseProperties.getPasswordAttribute())) {
-                    throw new FailedLoginException("No password attribute found for " + transformedCredential.getId());
-                }
-
-                if (!value.get(couchbaseProperties.getPasswordAttribute()).equals(transformedCredential.getPassword())) {
-                    LOGGER.warn("Account password on record for [{}] does not match the given/encoded password",
-                            transformedCredential.getId());
-                    throw new FailedLoginException();
-                }
-                return createHandlerResult(transformedCredential,
-                        this.principalFactory.createPrincipal(transformedCredential.getId(),
-                                new HashMap<>()), new ArrayList<>());
-
-            }
+        final long timeout = Beans.newDuration(couchbaseProperties.getTimeout()).toMillis();
+        final N1qlQueryResult result = couchbase.getBucket().query(query, timeout, TimeUnit.MILLISECONDS);
+        if (!result.finalSuccess()) {
+            LOGGER.error("Couchbase authentication failed with [{}]", result.errors().stream().map(JsonObject::toString).collect(Collectors.joining(",")));
+            throw new AccountNotFoundException("Could not locate account for user " + transformedCredential.getUsername());
         }
-        LOGGER.debug("Couchbase authentication failed with [{}]", result);
-        throw new AccountNotFoundException("Could not locate account for user " + transformedCredential.getUsername());
+
+        if (result.allRows().isEmpty()) {
+            LOGGER.error("Couchbase query did not return any results/rows.");
+            throw new AccountNotFoundException("Could not locate account for user " + transformedCredential.getUsername());
+        }
+
+        if (result.allRows().size() > 1) {
+            throw new FailedLoginException("More then one row found for user " + transformedCredential.getId());
+        }
+
+        final N1qlQueryRow row = result.allRows().get(0);
+        if (!row.value().containsKey(couchbase.getBucket().name())) {
+            throw new AccountNotFoundException("Couchbase query row does not contain this bucket [{}]" + couchbase.getBucket().name());
+        }
+
+        final JsonObject value = (JsonObject) row.value().get(couchbase.getBucket().name());
+        if (!value.containsKey(couchbaseProperties.getUsernameAttribute())) {
+            throw new FailedLoginException("No user attribute found for " + transformedCredential.getId());
+        }
+        if (!value.containsKey(couchbaseProperties.getPasswordAttribute())) {
+            throw new FailedLoginException("No password attribute found for " + transformedCredential.getId());
+        }
+
+        if (!value.get(couchbaseProperties.getPasswordAttribute()).equals(transformedCredential.getPassword())) {
+            LOGGER.warn("Account password on record for [{}] does not match the given/encoded password", transformedCredential.getId());
+            throw new FailedLoginException();
+        }
+
+        final Map<String, Object> attributes =
+            value.getNames().stream()
+                .filter(name -> !name.equals(couchbaseProperties.getPasswordAttribute())
+                    && !name.equals(couchbaseProperties.getUsernameAttribute()))
+                .map(name -> Pair.of(name, value.get(name)))
+                .collect(Collectors.toMap(Pair::getKey, Pair::getValue));
+
+        final Principal principal = this.principalFactory.createPrincipal(transformedCredential.getId(), attributes);
+        return createHandlerResult(transformedCredential, principal, new ArrayList<>());
+
     }
 }
---
+++ support/cas-server-support-couchbase-authentication/src/test/java/org/apereo/cas/authentication/CouchbaseAuthenticationHandlerTests.java
@@ -0,0 +1,86 @@
+package org.apereo.cas.authentication;
+
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.document.JsonDocument;
+import com.couchbase.client.java.document.json.JsonObject;
+import com.couchbase.client.java.query.util.IndexInfo;
+import lombok.SneakyThrows;
+import org.apereo.cas.config.CasAuthenticationEventExecutionPlanTestConfiguration;
+import org.apereo.cas.config.CasCoreAuthenticationPrincipalConfiguration;
+import org.apereo.cas.config.CasCoreAuthenticationServiceSelectionStrategyConfiguration;
+import org.apereo.cas.config.CasCoreConfiguration;
+import org.apereo.cas.config.CasCoreHttpConfiguration;
+import org.apereo.cas.config.CasCoreServicesConfiguration;
+import org.apereo.cas.config.CasCoreTicketCatalogConfiguration;
+import org.apereo.cas.config.CasCoreTicketIdGeneratorsConfiguration;
+import org.apereo.cas.config.CasCoreTicketsConfiguration;
+import org.apereo.cas.config.CasCoreUtilConfiguration;
+import org.apereo.cas.config.CasCoreWebConfiguration;
+import org.apereo.cas.config.CasDefaultServiceTicketIdGeneratorsConfiguration;
+import org.apereo.cas.config.CasPersonDirectoryTestConfiguration;
+import org.apereo.cas.config.CasRegisteredServicesTestConfiguration;
+import org.apereo.cas.config.CouchbaseAuthenticationConfiguration;
+import org.apereo.cas.config.support.CasWebApplicationServiceFactoryConfiguration;
+import org.apereo.cas.couchbase.core.CouchbaseClientFactory;
+import org.apereo.cas.logout.config.CasCoreLogoutConfiguration;
+import org.apereo.cas.util.junit.ConditionalIgnore;
+import org.apereo.cas.util.junit.ConditionalSpringRunner;
+import org.apereo.cas.util.junit.RunningContinuousIntegrationCondition;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * This is {@link CouchbaseAuthenticationHandlerTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.3.0
+ */
+@RunWith(ConditionalSpringRunner.class)
+@ConditionalIgnore(condition = RunningContinuousIntegrationCondition.class)
+@SpringBootTest(classes = {
+    RefreshAutoConfiguration.class,
+    CouchbaseAuthenticationConfiguration.class,
+    CasCoreConfiguration.class,
+    CasCoreTicketsConfiguration.class,
+    CasCoreLogoutConfiguration.class,
+    CasCoreServicesConfiguration.class,
+    CasCoreTicketIdGeneratorsConfiguration.class,
+    CasCoreTicketCatalogConfiguration.class,
+    CasCoreAuthenticationServiceSelectionStrategyConfiguration.class,
+    CasCoreHttpConfiguration.class,
+    CasCoreWebConfiguration.class,
+    CasPersonDirectoryTestConfiguration.class,
+    CasCoreUtilConfiguration.class,
+    CasRegisteredServicesTestConfiguration.class,
+    CasWebApplicationServiceFactoryConfiguration.class,
+    CasAuthenticationEventExecutionPlanTestConfiguration.class,
+    CasDefaultServiceTicketIdGeneratorsConfiguration.class,
+    CasCoreAuthenticationPrincipalConfiguration.class
+})
+public class CouchbaseAuthenticationHandlerTests {
+    @Autowired
+    @Qualifier("couchbaseAuthenticationHandler")
+    private AuthenticationHandler couchbaseAuthenticationHandler;
+    
+    @Test
+    @SneakyThrows
+    public void verifyAccount() {
+        final UsernamePasswordCredential c = CoreAuthenticationTestUtils.getCredentialsWithDifferentUsernameAndPassword("casuser", "Mellon");
+        final AuthenticationHandlerExecutionResult result = couchbaseAuthenticationHandler.authenticate(c);
+        assertNotNull(result);
+        assertEquals("casuser", result.getPrincipal().getId());
+        assertEquals(2, result.getPrincipal().getAttributes().size());
+        assertTrue(result.getPrincipal().getAttributes().containsKey("firstname"));
+        assertTrue(result.getPrincipal().getAttributes().containsKey("lastname"));
+    }
+}
