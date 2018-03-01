ef57affb956b771801b10067d70a327426d6ba47
Misagh Moayyed <mm1844@gmail.com>
2018-03-01 22:11:39 +0330

    working on couchbase attribute store

---
+++ support/cas-server-support-person-directory/src/main/java/org/apereo/cas/persondir/PersonDirectoryAttributeRepositoryPlanConfigurer.java
@@ -0,0 +1,17 @@
+package org.apereo.cas.persondir;
+
+/**
+ * This is {@link PersonDirectoryAttributeRepositoryPlanConfigurer}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.3.0
+ */
+public interface PersonDirectoryAttributeRepositoryPlanConfigurer {
+    /**
+     * Configure attribute repository plan.
+     *
+     * @param plan the plan
+     */
+    default void configureAttributeRepositoryPlan(PersonDirectoryAttributeRepositoryPlan plan) {
+    }
+}
