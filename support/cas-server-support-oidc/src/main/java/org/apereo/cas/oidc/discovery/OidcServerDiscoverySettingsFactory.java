4e3e0ab00da912de90a5ac8f2c7c807d48708c52
Misagh Moayyed <mm1844@gmail.com>
2018-03-02 21:30:02 +0330

    Fix OIDC protocol for discovery options

--- support/cas-server-support-oidc/src/main/java/org/apereo/cas/oidc/discovery/OidcServerDiscoverySettingsFactory.java
+++ support/cas-server-support-oidc/src/main/java/org/apereo/cas/oidc/discovery/OidcServerDiscoverySettingsFactory.java
@@ -27,14 +27,14 @@ public class OidcServerDiscoverySettingsFactory implements FactoryBean<OidcServe
     public OidcServerDiscoverySettings getObject() {
         final OidcProperties oidc = casProperties.getAuthn().getOidc();
         final OidcServerDiscoverySettings discoveryProperties =
-                new OidcServerDiscoverySettings(casProperties, oidc.getIssuer());
+            new OidcServerDiscoverySettings(casProperties, oidc.getIssuer());
 
         discoveryProperties.setClaimsSupported(oidc.getClaims());
         discoveryProperties.setScopesSupported(oidc.getScopes());
         discoveryProperties.setResponseTypesSupported(
-                CollectionUtils.wrapList(OAuth20ResponseTypes.CODE.getType(),
-                        OAuth20ResponseTypes.TOKEN.getType(),
-                        OAuth20ResponseTypes.IDTOKEN_TOKEN.getType()));
+            CollectionUtils.wrapList(OAuth20ResponseTypes.CODE.getType(),
+                OAuth20ResponseTypes.TOKEN.getType(),
+                OAuth20ResponseTypes.IDTOKEN_TOKEN.getType()));
 
         discoveryProperties.setSubjectTypesSupported(oidc.getSubjectTypes());
         discoveryProperties.setClaimTypesSupported(CollectionUtils.wrap("normal"));
@@ -44,9 +44,10 @@ public class OidcServerDiscoverySettingsFactory implements FactoryBean<OidcServe
         discoveryProperties.setIntrospectionSupportedAuthenticationMethods(authnMethods);
 
         discoveryProperties.setGrantTypesSupported(
-                CollectionUtils.wrapList(OAuth20GrantTypes.AUTHORIZATION_CODE.getType(),
-                        OAuth20GrantTypes.PASSWORD.getType(),
-                        OAuth20GrantTypes.REFRESH_TOKEN.getType()));
+            CollectionUtils.wrapList(OAuth20GrantTypes.AUTHORIZATION_CODE.getType(),
+                OAuth20GrantTypes.PASSWORD.getType(),
+                OAuth20GrantTypes.CLIENT_CREDENTIALS.getType(),
+                OAuth20GrantTypes.REFRESH_TOKEN.getType()));
 
         discoveryProperties.setIdTokenSigningAlgValuesSupported(CollectionUtils.wrapList("none", "RS256"));
         return discoveryProperties;
