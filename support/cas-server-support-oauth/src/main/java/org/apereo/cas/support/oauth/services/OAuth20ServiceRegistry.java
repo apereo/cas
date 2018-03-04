74ca8d0ac83a453648dbfb522acf0302f9fce813
Misagh Moayyed <mmoayyed@unicon.net>
2018-03-04 18:59:47 +0300

    working on immutable svc registries

--- support/cas-server-support-oauth/src/main/java/org/apereo/cas/support/oauth/services/OAuth20ServiceRegistry.java
+++ support/cas-server-support-oauth/src/main/java/org/apereo/cas/support/oauth/services/OAuth20ServiceRegistry.java
@@ -1,4 +1,4 @@
-package org.apereo.cas.support.saml.services;
+package org.apereo.cas.support.oauth.services;
 
 import org.apereo.cas.services.ImmutableInMemoryServiceRegistry;
 import org.apereo.cas.services.RegisteredService;
@@ -8,17 +8,17 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * This is {@link SamlIdPServiceRegistry}.
+ * This is {@link OAuth20ServiceRegistry}.
  *
  * @author Misagh Moayyed
  * @since 5.3.0
  */
-public class SamlIdPServiceRegistry extends ImmutableInMemoryServiceRegistry {
-    public SamlIdPServiceRegistry(final List<RegisteredService> services) {
+public class OAuth20ServiceRegistry extends ImmutableInMemoryServiceRegistry {
+    public OAuth20ServiceRegistry(final List<RegisteredService> services) {
         super(services);
     }
 
-    public SamlIdPServiceRegistry(final RegisteredService... services) {
+    public OAuth20ServiceRegistry(final RegisteredService... services) {
         this(Arrays.stream(services).collect(Collectors.toList()));
     }
 
