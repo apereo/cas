a1a7354d3161694f953d5dab39affd750c2988e8
Misagh Moayyed <mm1844@gmail.com>
2018-03-09 22:45:12 +0100

    backport patch from master

--- support/cas-server-support-trusted-webflow/src/main/java/org/apereo/cas/web/flow/TrustedAuthenticationWebflowConfigurer.java
+++ support/cas-server-support-trusted-webflow/src/main/java/org/apereo/cas/web/flow/TrustedAuthenticationWebflowConfigurer.java
@@ -3,6 +3,7 @@ package org.apereo.cas.web.flow;
 import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.web.flow.configurer.AbstractCasWebflowConfigurer;
 import org.springframework.context.ApplicationContext;
+import org.springframework.webflow.action.EvaluateAction;
 import org.springframework.webflow.definition.registry.FlowDefinitionRegistry;
 import org.springframework.webflow.engine.ActionState;
 import org.springframework.webflow.engine.Flow;
@@ -17,7 +18,7 @@ import org.springframework.webflow.engine.builder.support.FlowBuilderServices;
  */
 public class TrustedAuthenticationWebflowConfigurer extends AbstractCasWebflowConfigurer {
 
-    public TrustedAuthenticationWebflowConfigurer(final FlowBuilderServices flowBuilderServices, 
+    public TrustedAuthenticationWebflowConfigurer(final FlowBuilderServices flowBuilderServices,
                                                   final FlowDefinitionRegistry loginFlowDefinitionRegistry,
                                                   final ApplicationContext applicationContext,
                                                   final CasConfigurationProperties casProperties) {
@@ -28,12 +29,14 @@ public class TrustedAuthenticationWebflowConfigurer extends AbstractCasWebflowCo
     protected void doInitialize() {
         final Flow flow = getLoginFlow();
         if (flow != null) {
-            final ActionState actionState = createActionState(flow, "remoteAuthenticate",
-                    createEvaluateAction("remoteUserAuthenticationAction"));
-            actionState.getTransitionSet().add(createTransition(CasWebflowConstants.TRANSITION_ID_SUCCESS,
-                    CasWebflowConstants.TRANSITION_ID_SEND_TICKET_GRANTING_TICKET));
-            actionState.getTransitionSet().add(createTransition(CasWebflowConstants.TRANSITION_ID_ERROR,
-                    getStartState(flow).getId()));
+            final EvaluateAction action = createEvaluateAction("remoteUserAuthenticationAction");
+            final ActionState actionState = createActionState(flow, "remoteAuthenticate", action);
+            createTransitionForState(actionState, CasWebflowConstants.TRANSITION_ID_SUCCESS, CasWebflowConstants.STATE_ID_SEND_TICKET_GRANTING_TICKET);
+            final String currentStartState = getStartState(flow).getId();
+            createTransitionForState(actionState, CasWebflowConstants.TRANSITION_ID_ERROR, currentStartState);
+            createTransitionForState(actionState, CasWebflowConstants.TRANSITION_ID_AUTHENTICATION_FAILURE, CasWebflowConstants.STATE_ID_HANDLE_AUTHN_FAILURE);
+            actionState.getExitActionList().add(createEvaluateAction("clearWebflowCredentialsAction"));
+            registerMultifactorProvidersStateTransitionsIntoWebflow(actionState);
             setStartState(flow, actionState);
         }
     }
