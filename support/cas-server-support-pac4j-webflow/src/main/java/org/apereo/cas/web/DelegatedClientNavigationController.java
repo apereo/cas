d1f61b55eef9361ba28840ec7bb167b59b1a514a
Misagh Moayyed <mm1844@gmail.com>
2018-03-11 11:51:22 +0300

    Working on tests

--- support/cas-server-support-pac4j-webflow/src/main/java/org/apereo/cas/web/DelegatedClientNavigationController.java
+++ support/cas-server-support-pac4j-webflow/src/main/java/org/apereo/cas/web/DelegatedClientNavigationController.java
@@ -6,6 +6,7 @@ import org.apereo.cas.services.UnauthorizedServiceException;
 import org.apereo.cas.ticket.Ticket;
 import org.apereo.cas.util.Pac4jUtils;
 import org.apereo.cas.web.pac4j.DelegatedSessionCookieManager;
+import org.apereo.cas.web.view.DynamicHtmlView;
 import org.jasig.cas.client.util.URIBuilder;
 import org.pac4j.core.client.Clients;
 import org.pac4j.core.client.IndirectClient;
@@ -16,9 +17,7 @@ import org.pac4j.core.exception.HttpAction;
 import org.pac4j.core.profile.CommonProfile;
 import org.pac4j.core.redirect.RedirectAction;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.View;
@@ -26,7 +25,6 @@ import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
 
 /**
  * This is {@link DelegatedClientNavigationController}.
@@ -79,26 +77,11 @@ public class DelegatedClientNavigationController {
             return result;
         } catch (final HttpAction e) {
             if (e.getCode() == HttpStatus.UNAUTHORIZED.value()) {
-                LOGGER.debug("Authentication request was denied from the provider [{}]", clientName);
+                LOGGER.debug("Authentication request was denied from the provider [{}]", clientName, e);
             } else {
                 LOGGER.warn(e.getMessage(), e);
             }
             throw new UnauthorizedServiceException(e.getMessage(), e);
-        }
-    }
-
-
-    @RequiredArgsConstructor
-    private static class DynamicHtmlView implements View {
-        private final String html;
-
-        public String getContentType() {
-            return MediaType.TEXT_HTML_VALUE;
-        }
-
-        public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-            response.setContentType(this.getContentType());
-            FileCopyUtils.copy(this.html, response.getWriter());
         }
     }
 }
