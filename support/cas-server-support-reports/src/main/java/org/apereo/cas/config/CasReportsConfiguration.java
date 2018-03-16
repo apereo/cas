1e434b414a5987ff5732c14ca3ab252411675306
Misagh Moayyed <mm1844@gmail.com>
2018-03-16 20:22:15 +0330

    working on endpoints

--- support/cas-server-support-reports/src/main/java/org/apereo/cas/config/CasReportsConfiguration.java
+++ support/cas-server-support-reports/src/main/java/org/apereo/cas/config/CasReportsConfiguration.java
@@ -1,13 +1,18 @@
 package org.apereo.cas.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apereo.cas.CentralAuthenticationService;
 import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.discovery.CasServerProfileRegistrar;
+import org.apereo.cas.reports.CasStatisticsReportEndpoint;
+import org.apereo.cas.reports.ConfigurationDiscoveryProfileReportEndpoint;
 import org.apereo.cas.reports.RegisteredServicesReportEndpoint;
 import org.apereo.cas.reports.SpringWebflowReportEndpoint;
 import org.apereo.cas.services.ServicesManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.actuate.autoconfigure.endpoint.condition.ConditionalOnEnabledEndpoint;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,12 +30,16 @@ import org.springframework.context.annotation.Configuration;
 public class CasReportsConfiguration {
 
     @Autowired
+    @Qualifier("centralAuthenticationService")
+    private CentralAuthenticationService centralAuthenticationService;
+
+    @Autowired
     private CasConfigurationProperties casProperties;
 
     @Autowired
     @Qualifier("servicesManager")
     private ServicesManager servicesManager;
-    
+
     @Bean
     @ConditionalOnEnabledEndpoint
     public SpringWebflowReportEndpoint springWebflowEndpoint() {
@@ -41,6 +50,27 @@ public class CasReportsConfiguration {
     @ConditionalOnEnabledEndpoint
     public RegisteredServicesReportEndpoint registeredServicesReportEndpoint() {
         return new RegisteredServicesReportEndpoint(servicesManager);
+    }
+
+    /**
+     * The Cas server discovery profile configuration.
+     */
+    @ConditionalOnClass(value = CasServerProfileRegistrar.class)
+    @Configuration("casServerDiscoveryProfileConfiguration")
+    public static class CasServerDiscoveryProfileConfiguration {
+        @Autowired
+        @Bean
+        @ConditionalOnEnabledEndpoint
+        public ConfigurationDiscoveryProfileReportEndpoint configurationDiscoveryProfileReportEndpoint(@Qualifier("casServerProfileRegistrar")
+                                                                                                       final CasServerProfileRegistrar casServerProfileRegistrar) {
+            return new ConfigurationDiscoveryProfileReportEndpoint(casServerProfileRegistrar);
+        }
+    }
+
+    @Bean
+    @ConditionalOnEnabledEndpoint
+    public CasStatisticsReportEndpoint statisticsReportEndpoint() {
+        return new CasStatisticsReportEndpoint(centralAuthenticationService, casProperties.getHost().getName());
     }
 
 }
