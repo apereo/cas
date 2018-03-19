803d84c5a8d492661574facdfc8eb24a4df2e934
Misagh Moayyed <mm1844@gmail.com>
2018-03-19 12:49:03 +0330

    clean up consent review and endpoint

---
+++ support/cas-server-support-consent-core/src/main/java/org/apereo/cas/consent/AttributeConsentReportEndpoint.java
@@ -0,0 +1,75 @@
+package org.apereo.cas.consent;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.actuate.endpoint.annotation.DeleteOperation;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.annotation.Selector;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+/**
+ * This is {@link AttributeConsentReportEndpoint}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.3.0
+ */
+@Slf4j
+@Endpoint(id = "attribute-consent", enableByDefault = false)
+@RequiredArgsConstructor
+public class AttributeConsentReportEndpoint {
+    private final ConsentRepository consentRepository;
+    private final ConsentEngine consentEngine;
+
+    /**
+     * Consent decisions collection.
+     *
+     * @param principal the principal
+     * @return the collection
+     */
+    @ReadOperation
+    public Collection<Map<String, Object>> consentDecisions(@Selector final String principal) {
+        final Collection<Map<String, Object>> result = new HashSet<>();
+        LOGGER.debug("Fetching consent decisions for principal [{}]", principal);
+        final Collection<ConsentDecision> consentDecisions = this.consentRepository.findConsentDecisions(principal);
+        LOGGER.debug("Resolved consent decisions for principal [{}]: {}", principal, consentDecisions);
+
+        consentDecisions.stream().forEach(d -> {
+            final Map<String, Object> map = new HashMap<>();
+            map.put("decision", d);
+            map.put("attributes", this.consentEngine.resolveConsentableAttributesFrom(d));
+            result.add(map);
+        });
+        return result;
+    }
+
+    /**
+     * Revoke consent boolean.
+     *
+     * @param principal  the principal
+     * @param decisionId the decision id
+     * @return the boolean
+     */
+    @DeleteOperation
+    public boolean revokeConsent(@Selector final long decisionId) {
+        LOGGER.debug("Deleting consent decision with id [{}]", decisionId);
+        return this.consentRepository.deleteConsentDecision(decisionId);
+    }
+
+    /**
+     * Revoke consents.
+     *
+     * @param principal the principal
+     * @return the boolean
+     */
+    @DeleteOperation
+    public boolean revokeConsents(@Selector final String principal) {
+        LOGGER.debug("Deleting consent decisions for principal [{}].", principal);
+        return this.consentRepository.deleteConsentDecisions(principal);
+    }
+
+}
