2b667d1560aa704651747dafda835b1cfe8f77c0
Misagh Moayyed <mm1844@gmail.com>
2018-03-27 13:04:15 +0430

    Remove wait loops from CI build

--- ci/run-dynamodb-server.sh
+++ ci/run-dynamodb-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$MATRIX_JOB_TYPE" == "TEST" ]; then
-    while sleep 9m; do echo -e '\n=====[ Gradle build is still running ]====='; done &
+    # while sleep 9m; do echo -e '\n=====[ Gradle build is still running ]====='; done &
 
     echo "Running DynamoDb docker image..."
     docker run -d -p 8000:8000 --name "dynamodb-server" ryanratcliff/dynamodb
