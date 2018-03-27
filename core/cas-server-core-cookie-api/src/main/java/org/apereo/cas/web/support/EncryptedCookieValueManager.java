e6f9fc532c18062ba1f86b301f61ec57495d912c
Daniel Frett <frett@users.noreply.github.com>
2018-03-27 13:57:15 -0400

    Add support for using a cookie to track MFA trusted devices (#3250)
    * add a flag to indicate if this is a new trusted device
    
    This will be used by the cookie trusted device strategy to determine if we need to create a cookie for the device
    
    * implement a DefaultDeviceFingerprintStrategy that will combine multiple components together into a single fingerprint.
    
    * split current geography based device fingerprint into separate client ip and user agent components
    
    * add configuration properties for mfa trusted device fingerprint components
    
    * separate cookie encoding logic from DefaultCasCookieValueManager
    
    This will allow us to reuse the encode/decode logic within a Cookie based trusted device fingerprint component
    
    * move trusted device fingerprint configuration to separate configuration class
    
    * create a CipherExecutor for the trusted device fingerprint cookie
    
    * tweaking auto-wiring
    
    * finish creation of the device fingerprint cookie component
    
    * add documentation about configuring the trusted device fingerprint
    
    * fix checkstyle warnings
    
    * fix a javadoc error
    
    * fix checkstyle errors for new CookieValueManager
    
    * fix a couple annotations
    
    Also use setters to make it more clear what the default properties actually are
    
    * add log messages about disabled device fingerprint components
    
    * remove Nonnull annotation
    
    * fix typo
    
    * directly utilize mfaTrustEngine bean
    
    * move cookie generator configuration to common properties
    
    * rename extractor components
    
    * enable encryption of cookies for device fingerprints

---
+++ core/cas-server-core-cookie-api/src/main/java/org/apereo/cas/web/support/EncryptedCookieValueManager.java
@@ -0,0 +1,66 @@
+package org.apereo.cas.web.support;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.apereo.cas.CipherExecutor;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.io.Serializable;
+
+/**
+ * Provides basic encryption/decryption support for cookie values.
+ *
+ * @author Daniel Frett
+ * @since 5.3.0
+ */
+@Slf4j
+@AllArgsConstructor
+public class EncryptedCookieValueManager implements CookieValueManager {
+    /**
+     * The cipher exec that is responsible for encryption and signing of the cookie.
+     */
+    private final CipherExecutor<Serializable, Serializable> cipherExecutor;
+
+    @Override
+    public final String buildCookieValue(final String givenCookieValue, final HttpServletRequest request) {
+        final String res = buildCompoundCookieValue(givenCookieValue, request);
+        LOGGER.debug("Encoding cookie value [{}]", res);
+        return cipherExecutor.encode(res).toString();
+    }
+
+    @Override
+    public final String obtainCookieValue(final Cookie cookie, final HttpServletRequest request) {
+        final String cookieValue = cipherExecutor.decode(cookie.getValue()).toString();
+        LOGGER.debug("Decoded cookie value is [{}]", cookieValue);
+        if (StringUtils.isBlank(cookieValue)) {
+            LOGGER.debug("Retrieved decoded cookie value is blank. Failed to decode cookie [{}]", cookie.getName());
+            return null;
+        }
+
+        return obtainValueFromCompoundCookie(cookieValue, request);
+    }
+
+    /**
+     * Build the compound cookie value.
+     *
+     * @param cookieValue the raw cookie value that is being stored
+     * @param request the current web request
+     * @return a compound cookie value that may contain additional data beyond the raw cookieValue
+     */
+    protected String buildCompoundCookieValue(final String cookieValue, final HttpServletRequest request) {
+        return cookieValue;
+    }
+
+    /**
+     * Obtain the cookie value from the compound cookie value.
+     *
+     * @param compoundValue The compound cookie value
+     * @param request the current web request
+     * @return the original cookie value that was stored in the provided compound value.
+     */
+    protected String obtainValueFromCompoundCookie(final String compoundValue, final HttpServletRequest request) {
+        return compoundValue;
+    }
+}
