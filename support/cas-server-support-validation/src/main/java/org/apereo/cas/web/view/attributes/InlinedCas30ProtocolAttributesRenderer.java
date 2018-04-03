33f04df67b6f433cfeaff0b7a781f8074248ebfa
Misagh Moayyed <mm1844@gmail.com>
2018-04-03 13:44:09 +0430

    clean up views; add inlined renderer

---
+++ support/cas-server-support-validation/src/main/java/org/apereo/cas/web/view/attributes/InlinedCas30ProtocolAttributesRenderer.java
@@ -0,0 +1,21 @@
+package org.apereo.cas.web.view.attributes;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * This is {@link InlinedCas30ProtocolAttributesRenderer}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.3.0
+ */
+@Slf4j
+public class InlinedCas30ProtocolAttributesRenderer extends DefaultCas30ProtocolAttributesRenderer {
+
+    @Override
+    protected String buildSingleAttributeDefinitionLine(final String attributeName, final Object value) {
+        final String encodedValue = encodeAttributeValue(value);
+        return new StringBuilder()
+            .append("<cas:attribute name=\"".concat(attributeName).concat("\" value=\"".concat(encodedValue)).concat("\"></cas:attribute>"))
+            .toString();
+    }
+}
