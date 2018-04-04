533a63988e3440f8e8ad8fc29b900f4c9a3558f3
--replace-all <PestytheRat@gmail.com>
2018-04-04 11:54:06 -0600

    Adding prevent table creation for a couple more locations for dynamoDb

--- core/cas-server-core-configuration-cloud-dynamodb/src/main/java/org/apereo/cas/config/DynamoDbCloudConfigBootstrapConfiguration.java
+++ core/cas-server-core-configuration-cloud-dynamodb/src/main/java/org/apereo/cas/config/DynamoDbCloudConfigBootstrapConfiguration.java
@@ -67,7 +67,11 @@ public class DynamoDbCloudConfigBootstrapConfiguration implements PropertySource
     @Override
     public PropertySource<?> locate(final Environment environment) {
         final AmazonDynamoDB amazonDynamoDBClient = getAmazonDynamoDbClient(environment);
-        createSettingsTable(amazonDynamoDBClient, false);
+
+        final boolean preventTableCreationOnStartup = getSetting(environment, "preventTableCreationOnStartup");
+        if(!preventTableCreationOnStartup) {
+            createSettingsTable(amazonDynamoDBClient, false);
+        }
         final ScanRequest scan = new ScanRequest(TABLE_NAME);
         LOGGER.debug("Scanning table with request [{}]", scan);
         final ScanResult result = amazonDynamoDBClient.scan(scan);
