5bde66b6132ba138828f5b206258c8891ecd2e47
Misagh Moayyed <mmoayyed@unicon.net>
2018-04-09 20:34:37 +0430

    Add tests for surrogate authn

--- api/cas-server-core-api-authentication/src/test/java/org/apereo/cas/authentication/AuthenticationTransactionTests.java
+++ api/cas-server-core-api-authentication/src/test/java/org/apereo/cas/authentication/AuthenticationTransactionTests.java
@@ -1,8 +1,8 @@
 package org.apereo.cas.authentication;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 /**
  * @since 5.3.0
@@ -10,7 +10,7 @@ import org.junit.Test;
 public class AuthenticationTransactionTests {
     @Test
     public void verifyHasCredentialOfTypeSingle() {
-        final AuthenticationTransaction transaction = AuthenticationTransaction.of(new TestCredentialType1());
+        final AuthenticationTransaction transaction = DefaultAuthenticationTransaction.of(new TestCredentialType1());
         assertTrue(transaction.hasCredentialOfType(BaseTestCredential.class));
         assertTrue(transaction.hasCredentialOfType(TestCredentialType1.class));
         assertFalse(transaction.hasCredentialOfType(TestCredentialType2.class));
@@ -18,20 +18,22 @@ public class AuthenticationTransactionTests {
 
     @Test
     public void verifyHasCredentialOfTypeMultiple() {
-        final AuthenticationTransaction transaction = AuthenticationTransaction
-                .of(new TestCredentialType2(), new TestCredentialType1());
+        final AuthenticationTransaction transaction = DefaultAuthenticationTransaction.of(new TestCredentialType2(), new TestCredentialType1());
         assertTrue(transaction.hasCredentialOfType(BaseTestCredential.class));
         assertTrue(transaction.hasCredentialOfType(TestCredentialType1.class));
         assertTrue(transaction.hasCredentialOfType(TestCredentialType2.class));
     }
 
-    private abstract static class BaseTestCredential implements Credential {}
+    private abstract static class BaseTestCredential implements Credential {
+    }
+
     private static class TestCredentialType1 extends BaseTestCredential {
         @Override
         public String getId() {
             return null;
         }
     }
+
     private static class TestCredentialType2 implements Credential {
         @Override
         public String getId() {
