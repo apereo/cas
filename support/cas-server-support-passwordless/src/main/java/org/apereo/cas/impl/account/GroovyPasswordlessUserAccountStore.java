2d59940269cfce881d3ce6a27fa7b48f8832e670
Misagh Moayyed <mmoayyed@unicon.net>
2018-04-10 19:04:01 +0430

    Working on passwordless authn

--- support/cas-server-support-passwordless/src/main/java/org/apereo/cas/impl/account/GroovyPasswordlessUserAccountStore.java
+++ support/cas-server-support-passwordless/src/main/java/org/apereo/cas/impl/account/GroovyPasswordlessUserAccountStore.java
@@ -1,7 +1,11 @@
 package org.apereo.cas.impl.account;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apereo.cas.api.PasswordlessUserAccount;
 import org.apereo.cas.api.PasswordlessUserAccountStore;
+import org.apereo.cas.util.ScriptingUtils;
+import org.springframework.core.io.Resource;
 
 import java.util.Optional;
 
@@ -11,9 +15,15 @@ import java.util.Optional;
  * @author Misagh Moayyed
  * @since 5.3.0
  */
+@RequiredArgsConstructor
+@Slf4j
 public class GroovyPasswordlessUserAccountStore implements PasswordlessUserAccountStore {
+    private final transient Resource groovyResource;
+
     @Override
     public Optional<PasswordlessUserAccount> findUser(final String username) {
-        return null;
+        final PasswordlessUserAccount account = ScriptingUtils.executeGroovyScript(groovyResource,
+            new Object[]{username, LOGGER}, PasswordlessUserAccount.class);
+        return Optional.ofNullable(account);
     }
 }
