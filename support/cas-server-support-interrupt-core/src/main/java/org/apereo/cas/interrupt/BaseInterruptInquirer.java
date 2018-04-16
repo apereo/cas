5a805681c47561cd2fed5396b1d38177b2c04233
Misagh Moayyed <mm1844@gmail.com>
2018-04-17 01:10:29 +0430

    updated

--- support/cas-server-support-interrupt-core/src/main/java/org/apereo/cas/interrupt/BaseInterruptInquirer.java
+++ support/cas-server-support-interrupt-core/src/main/java/org/apereo/cas/interrupt/BaseInterruptInquirer.java
@@ -1,6 +1,9 @@
 package org.apereo.cas.interrupt;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apereo.cas.authentication.Authentication;
+import org.apereo.cas.authentication.principal.Service;
+import org.apereo.cas.services.RegisteredService;
 
 /**
  * This is {@link BaseInterruptInquirer}.
@@ -10,4 +13,18 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public abstract class BaseInterruptInquirer implements InterruptInquirer {
+    @Override
+    public final InterruptResponse inquire(final Authentication authentication, final RegisteredService registeredService, final Service service) {
+        return inquireInternal(authentication, registeredService, service);
+    }
+
+    /**
+     * Inquire internal interrupt response.
+     *
+     * @param authentication    the authentication
+     * @param registeredService the registered service
+     * @param service           the service
+     * @return the interrupt response
+     */
+    protected abstract InterruptResponse inquireInternal(Authentication authentication, RegisteredService registeredService, Service service);
 }
