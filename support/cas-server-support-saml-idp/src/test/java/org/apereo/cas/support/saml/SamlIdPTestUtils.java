9dd27414e941387553c2eb671c18e90a29a44f17
Misagh Moayyed <mm1844@gmail.com>
2018-04-17 15:17:22 +0430

    add saml test cases

---
+++ support/cas-server-support-saml-idp/src/test/java/org/apereo/cas/support/saml/SamlIdPTestUtils.java
@@ -0,0 +1,35 @@
+package org.apereo.cas.support.saml;
+
+import lombok.experimental.UtilityClass;
+import org.apereo.cas.support.saml.services.SamlRegisteredService;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+/**
+ * This is {@link SamlIdPTestUtils}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.3.0
+ */
+@UtilityClass
+public class SamlIdPTestUtils {
+
+    /**
+     * Gets saml registered service.
+     *
+     * @return the saml registered service
+     */
+    public static SamlRegisteredService getSamlRegisteredService() {
+        final SamlRegisteredService registeredService = new SamlRegisteredService();
+        registeredService.setId(100);
+        registeredService.setName("SAML");
+        registeredService.setServiceId("https://sp.testshib.org/shibboleth-sp");
+        registeredService.setMetadataLocation("http://www.testshib.org/metadata/testshib-providers.xml");
+
+        final MockHttpServletRequest request = new MockHttpServletRequest();
+        request.addParameter(SamlProtocolConstants.PARAMETER_ENTITY_ID, registeredService.getServiceId());
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+        return registeredService;
+    }
+}
