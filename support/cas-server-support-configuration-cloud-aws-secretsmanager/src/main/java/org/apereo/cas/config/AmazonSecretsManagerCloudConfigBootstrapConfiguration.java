ec2b27b5a423af72a16eb5c6feab04aa826dd3f7
Misagh Moayyed <mmoayyed@unicon.net>
2018-04-23 15:48:41 +0430

    fetching CAS settings from amz s3

--- support/cas-server-support-configuration-cloud-aws-secretsmanager/src/main/java/org/apereo/cas/config/AmazonSecretsManagerCloudConfigBootstrapConfiguration.java
+++ support/cas-server-support-configuration-cloud-aws-secretsmanager/src/main/java/org/apereo/cas/config/AmazonSecretsManagerCloudConfigBootstrapConfiguration.java
@@ -1,9 +1,5 @@
 package org.apereo.cas.config;
 
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.regions.Region;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.secretsmanager.AWSSecretsManager;
 import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
 import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest;
@@ -13,8 +9,7 @@ import com.amazonaws.services.secretsmanager.model.ListSecretsResult;
 import com.amazonaws.services.secretsmanager.model.SecretListEntry;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
-import org.apereo.cas.aws.ChainingAWSCredentialsProvider;
+import org.apereo.cas.aws.AWSClientBuilder;
 import org.springframework.cloud.bootstrap.config.PropertySourceLocator;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -34,13 +29,15 @@ import java.util.Properties;
 @Slf4j
 @Getter
 public class AmazonSecretsManagerCloudConfigBootstrapConfiguration implements PropertySourceLocator {
+    private static final String CAS_CONFIGURATION_PREFIX = "cas.spring.cloud.aws.secretsManager";
 
     @Override
     public PropertySource<?> locate(final Environment environment) {
         final Properties props = new Properties();
-        final AWSSecretsManager secretsManager = getAmazonSecretsManagerClient(environment);
-
         try {
+            final AWSClientBuilder builder = new AWSClientBuilder(CAS_CONFIGURATION_PREFIX, environment);
+            final AWSSecretsManager secretsManager = builder.build(AWSSecretsManagerClientBuilder.standard(), AWSSecretsManager.class);
+
             final ListSecretsRequest listRequest = new ListSecretsRequest();
             final ListSecretsResult listResults = secretsManager.listSecrets(listRequest);
             final List<SecretListEntry> secretList = listResults.getSecretList();
@@ -63,34 +60,5 @@ public class AmazonSecretsManagerCloudConfigBootstrapConfiguration implements Pr
         }
         LOGGER.debug("Located [{}] secret(s)", props.size());
         return new PropertiesPropertySource(getClass().getSimpleName(), props);
-    }
-
-    private static String getSetting(final Environment environment, final String key) {
-        return environment.getProperty("cas.spring.cloud.aws.secretsManager." + key);
-    }
-
-    private static AWSSecretsManager getAmazonSecretsManagerClient(final Environment environment) {
-        final String key = getSetting(environment, "credentialAccessKey");
-        final String secret = getSetting(environment, "credentialSecretKey");
-        final AWSCredentialsProvider credentials = ChainingAWSCredentialsProvider.getInstance(key, secret);
-        String region = getSetting(environment, "region");
-        final Region currentRegion = Regions.getCurrentRegion();
-        if (currentRegion != null && StringUtils.isBlank(region)) {
-            region = currentRegion.getName();
-        }
-        String regionOverride = getSetting(environment, "regionOverride");
-        if (currentRegion != null && StringUtils.isNotBlank(regionOverride)) {
-            regionOverride = currentRegion.getName();
-        }
-        final String endpoint = getSetting(environment, "endpoint");
-        final AWSSecretsManagerClientBuilder builder = AWSSecretsManagerClientBuilder.standard()
-            .withCredentials(credentials)
-            .withRegion(region);
-
-        
-        if (StringUtils.isNotBlank(endpoint) && StringUtils.isNotBlank(regionOverride)) {
-            builder.withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpoint, regionOverride));
-        }
-        return builder.build();
     }
 }
