3db87412111d5e9ba3c7e44e75a376142a75250c
Misagh Moayyed <mm1844@gmail.com>
2018-04-23 21:09:20 +0430

    improv perf of apache ignite tests

--- support/cas-server-support-ignite-ticket-registry/src/main/java/org/apereo/cas/config/IgniteTicketRegistryConfiguration.java
+++ support/cas-server-support-ignite-ticket-registry/src/main/java/org/apereo/cas/config/IgniteTicketRegistryConfiguration.java
@@ -8,6 +8,7 @@ import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
 import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
+import org.apache.ignite.ssl.SslContextFactory;
 import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.configuration.model.support.ignite.IgniteProperties;
 import org.apereo.cas.configuration.support.Beans;
@@ -24,11 +25,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StringUtils;
 
+import javax.cache.expiry.CreatedExpiryPolicy;
+import javax.cache.expiry.Duration;
 import java.util.Collection;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
-import javax.cache.expiry.CreatedExpiryPolicy;
-import javax.cache.expiry.Duration;
 
 /**
  * This is {@link IgniteTicketRegistryConfiguration}.
@@ -50,6 +51,7 @@ public class IgniteTicketRegistryConfiguration {
      * @param ticketCatalog the ticket catalog
      * @return the ignite configuration
      */
+    @Autowired
     @RefreshScope
     @Bean
     public IgniteConfiguration igniteConfiguration(@Qualifier("ticketCatalog") final TicketCatalog ticketCatalog) {
@@ -79,35 +81,81 @@ public class IgniteTicketRegistryConfiguration {
         config.setCacheConfiguration(cacheConfigurations.toArray(new CacheConfiguration[]{}));
         config.setClientMode(ignite.isClientMode());
 
+        final SslContextFactory factory = buildSecureTransportForIgniteConfiguration();
+        if (factory != null) {
+            config.setSslContextFactory(factory);
+        }
+
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("igniteConfiguration.cacheConfiguration=[{}]", (Object[]) config.getCacheConfiguration());
+            LOGGER.debug("igniteConfiguration.getDiscoverySpi=[{}]", config.getDiscoverySpi());
+            LOGGER.debug("igniteConfiguration.getSslContextFactory=[{}]", config.getSslContextFactory());
+        }
+
         return config;
+    }
+    
+    @Autowired
+    @Bean
+    @RefreshScope
+    public TicketRegistry ticketRegistry(@Qualifier("ticketCatalog") final TicketCatalog ticketCatalog) {
+        final IgniteProperties igniteProperties = casProperties.getTicket().getRegistry().getIgnite();
+        final IgniteConfiguration igniteConfiguration = igniteConfiguration(ticketCatalog);
+        final IgniteTicketRegistry r = new IgniteTicketRegistry(ticketCatalog, igniteConfiguration, igniteProperties);
+        r.setCipherExecutor(CoreTicketUtils.newTicketRegistryCipherExecutor(igniteProperties.getCrypto(), "ignite"));
+        r.initialize();
+        return r;
     }
 
     private static Collection<CacheConfiguration> buildIgniteTicketCaches(final IgniteProperties ignite,
                                                                           final TicketCatalog ticketCatalog) {
         final Collection<TicketDefinition> definitions = ticketCatalog.findAll();
         return definitions
-                .stream()
-                .map(t -> {
-                    final CacheConfiguration ticketsCache = new CacheConfiguration();
-                    ticketsCache.setName(t.getProperties().getStorageName());
-                    ticketsCache.setCacheMode(CacheMode.valueOf(ignite.getTicketsCache().getCacheMode()));
-                    ticketsCache.setAtomicityMode(CacheAtomicityMode.valueOf(ignite.getTicketsCache().getAtomicityMode()));
-                    final CacheWriteSynchronizationMode writeSync =
-                            CacheWriteSynchronizationMode.valueOf(ignite.getTicketsCache().getWriteSynchronizationMode());
-                    ticketsCache.setWriteSynchronizationMode(writeSync);
-                    final Duration duration = new Duration(TimeUnit.SECONDS, t.getProperties().getStorageTimeout());
-                    ticketsCache.setExpiryPolicyFactory(CreatedExpiryPolicy.factoryOf(duration));
-                    return ticketsCache;
-                })
-                .collect(Collectors.toSet());
+            .stream()
+            .map(t -> {
+                final CacheConfiguration ticketsCache = new CacheConfiguration();
+                ticketsCache.setName(t.getProperties().getStorageName());
+                ticketsCache.setCacheMode(CacheMode.valueOf(ignite.getTicketsCache().getCacheMode()));
+                ticketsCache.setAtomicityMode(CacheAtomicityMode.valueOf(ignite.getTicketsCache().getAtomicityMode()));
+                final CacheWriteSynchronizationMode writeSync =
+                    CacheWriteSynchronizationMode.valueOf(ignite.getTicketsCache().getWriteSynchronizationMode());
+                ticketsCache.setWriteSynchronizationMode(writeSync);
+                final Duration duration = new Duration(TimeUnit.SECONDS, t.getProperties().getStorageTimeout());
+                ticketsCache.setExpiryPolicyFactory(CreatedExpiryPolicy.factoryOf(duration));
+                return ticketsCache;
+            })
+            .collect(Collectors.toSet());
     }
 
-    @Bean
-    @RefreshScope
-    public TicketRegistry ticketRegistry(@Qualifier("ticketCatalog") final TicketCatalog ticketCatalog) {
-        final IgniteProperties igniteProperties = casProperties.getTicket().getRegistry().getIgnite();
-        final IgniteTicketRegistry r = new IgniteTicketRegistry(ticketCatalog, igniteConfiguration(ticketCatalog), igniteProperties);
-        r.setCipherExecutor(CoreTicketUtils.newTicketRegistryCipherExecutor(igniteProperties.getCrypto(), "ignite"));
-        return r;
+    private SslContextFactory buildSecureTransportForIgniteConfiguration() {
+        final IgniteProperties properties = casProperties.getTicket().getRegistry().getIgnite();
+        final String nullKey = "NULL";
+        if (StringUtils.hasText(properties.getKeyStoreFilePath()) && StringUtils.hasText(properties.getKeyStorePassword())
+            && StringUtils.hasText(properties.getTrustStoreFilePath()) && StringUtils.hasText(properties.getTrustStorePassword())) {
+            final SslContextFactory sslContextFactory = new SslContextFactory();
+            sslContextFactory.setKeyStoreFilePath(properties.getKeyStoreFilePath());
+            sslContextFactory.setKeyStorePassword(properties.getKeyStorePassword().toCharArray());
+            if (nullKey.equals(properties.getTrustStoreFilePath()) && nullKey.equals(properties.getTrustStorePassword())) {
+                sslContextFactory.setTrustManagers(SslContextFactory.getDisabledTrustManager());
+            } else {
+                sslContextFactory.setTrustStoreFilePath(properties.getTrustStoreFilePath());
+                sslContextFactory.setTrustStorePassword(properties.getKeyStorePassword().toCharArray());
+            }
+            if (org.apache.commons.lang3.StringUtils.isNotBlank(properties.getKeyAlgorithm())) {
+                sslContextFactory.setKeyAlgorithm(properties.getKeyAlgorithm());
+            }
+            if (org.apache.commons.lang3.StringUtils.isNotBlank(properties.getProtocol())) {
+                sslContextFactory.setProtocol(properties.getProtocol());
+            }
+            if (org.apache.commons.lang3.StringUtils.isNotBlank(properties.getTrustStoreType())) {
+                sslContextFactory.setTrustStoreType(properties.getTrustStoreType());
+            }
+            if (org.apache.commons.lang3.StringUtils.isNotBlank(properties.getKeyStoreType())) {
+                sslContextFactory.setKeyStoreType(properties.getKeyStoreType());
+            }
+            return sslContextFactory;
+        }
+        return null;
     }
+
 }
