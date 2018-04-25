b8244441132f9ad4cc11309911b622247dcbeca9
Misagh Moayyed <mm1844@gmail.com>
2018-04-25 15:46:34 +0430

    remove logger objects from props

--- api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/support/aws/BaseAmazonWebServicesProperties.java
+++ api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/support/aws/BaseAmazonWebServicesProperties.java
@@ -2,7 +2,6 @@ package org.apereo.cas.configuration.model.support.aws;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.extern.slf4j.Slf4j;
 import org.apereo.cas.configuration.support.RequiredProperty;
 import org.springframework.core.io.Resource;
 
@@ -14,7 +13,7 @@ import java.io.Serializable;
  * @author Misagh Moayyed
  * @since 5.3.0
  */
-@Slf4j
+
 @Getter
 @Setter
 public abstract class BaseAmazonWebServicesProperties implements Serializable {
--- api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/support/hazelcast/HazelcastTicketRegistryProperties.java
+++ api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/support/hazelcast/HazelcastTicketRegistryProperties.java
@@ -1,6 +1,5 @@
 package org.apereo.cas.configuration.model.support.hazelcast;
 
-import lombok.extern.slf4j.Slf4j;
 import org.apereo.cas.configuration.model.core.util.EncryptionRandomizedSigningJwtCryptographyProperties;
 import org.apereo.cas.configuration.support.RequiresModule;
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
@@ -14,7 +13,7 @@ import lombok.Setter;
  * @since 4.2.0
  */
 @RequiresModule(name = "cas-server-support-hazelcast-ticket-registry")
-@Slf4j
+
 @Getter
 @Setter
 public class HazelcastTicketRegistryProperties extends BaseHazelcastProperties {
