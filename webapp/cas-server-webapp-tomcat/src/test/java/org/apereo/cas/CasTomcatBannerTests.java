60c8bc307fbc1b3020edeb08140693a3db0a40ad
Misagh Moayyed <mm1844@gmail.com>
2018-04-26 01:44:55 +0430

    added tests

---
+++ webapp/cas-server-webapp-tomcat/src/test/java/org/apereo/cas/CasTomcatBannerTests.java
@@ -0,0 +1,45 @@
+package org.apereo.cas;
+
+import org.apache.commons.io.output.WriterOutputStream;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.PrintStream;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.*;
+
+/**
+ * This is {@link CasTomcatBannerTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.3.0
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {
+    RefreshAutoConfiguration.class,
+    AopAutoConfiguration.class
+})
+public class CasTomcatBannerTests {
+    @Autowired
+    private Environment environment;
+
+    @Test
+    public void verifyAction() {
+        final CasTomcatBanner banner = new CasTomcatBanner();
+        final StringWriter writer = new StringWriter();
+        final WriterOutputStream out = new WriterOutputStream(writer, StandardCharsets.UTF_8);
+        try (PrintStream stream = new PrintStream(out)) {
+            banner.printBanner(environment, CasTomcatBanner.class, stream);
+        }
+        final String output = writer.toString();
+        assertNotNull(output);
+    }
+}
