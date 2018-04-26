2ebd58c645b7b5372df12e7c0b66c116c8bea538
Misagh Moayyed <mm1844@gmail.com>
2018-04-27 00:47:57 +0430

    added tests

---
+++ support/cas-server-support-saml-idp-web/src/test/java/org/apereo/cas/support/saml/web/idp/audit/SamlResponseAuditResourceResolverTests.java
@@ -0,0 +1,64 @@
+package org.apereo.cas.support.saml.web.idp.audit;
+
+import org.apereo.cas.util.CollectionUtils;
+import org.aspectj.lang.JoinPoint;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.opensaml.saml.saml2.core.Issuer;
+import org.opensaml.saml.saml2.core.Response;
+import org.opensaml.soap.soap11.Body;
+import org.opensaml.soap.soap11.Envelope;
+import org.opensaml.soap.soap11.Fault;
+import org.opensaml.soap.soap11.FaultActor;
+import org.opensaml.soap.soap11.FaultString;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * This is {@link SamlResponseAuditResourceResolverTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.3.0
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = RefreshAutoConfiguration.class)
+public class SamlResponseAuditResourceResolverTests {
+    @Test
+    public void verifyAction() {
+        final SamlResponseAuditResourceResolver r = new SamlResponseAuditResourceResolver();
+        final Response response = mock(Response.class);
+        final Issuer issuer = mock(Issuer.class);
+        when(issuer.getValue()).thenReturn("https://idp.example.org");
+        when(response.getIssuer()).thenReturn(issuer);
+        when(response.getDestination()).thenReturn("https://sp.example.org");
+
+        String[] result = r.resolveFrom(mock(JoinPoint.class), response);
+        assertNotNull(result);
+        assertTrue(result.length > 0);
+        
+        final Envelope envelope = mock(Envelope.class);
+        final Body body = mock(Body.class);
+
+        when(body.getUnknownXMLObjects()).thenReturn(CollectionUtils.wrapList(response));
+        when(envelope.getBody()).thenReturn(body);
+        result = r.resolveFrom(mock(JoinPoint.class), envelope);
+        assertNotNull(result);
+        assertTrue(result.length > 0);
+
+        final Fault fault = mock(Fault.class);
+        final FaultActor actor = mock(FaultActor.class);
+        when(actor.getValue()).thenReturn("actor");
+        final FaultString msg = mock(FaultString.class);
+        when(msg.getValue()).thenReturn("message");
+        when(fault.getMessage()).thenReturn(msg);
+        when(fault.getActor()).thenReturn(actor);
+        when(body.getUnknownXMLObjects()).thenReturn(CollectionUtils.wrapList(fault));
+        result = r.resolveFrom(mock(JoinPoint.class), envelope);
+        assertNotNull(result);
+        assertTrue(result.length > 0);
+    }
+}
