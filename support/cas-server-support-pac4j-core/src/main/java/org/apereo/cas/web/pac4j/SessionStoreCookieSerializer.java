8d6685539d04396c0654c884534990c0bb4939a5
Simon Bear <simon.bear@csiro.au>
2018-04-27 04:30:31 +1000

    Changes to support OAuth1RequestToken deserialisation for SessionStoreCookie (#3278)

--- support/cas-server-support-pac4j-core/src/main/java/org/apereo/cas/web/pac4j/SessionStoreCookieSerializer.java
+++ support/cas-server-support-pac4j-core/src/main/java/org/apereo/cas/web/pac4j/SessionStoreCookieSerializer.java
@@ -1,6 +1,7 @@
 package org.apereo.cas.web.pac4j;
 
 import com.fasterxml.jackson.core.util.MinimalPrettyPrinter;
+import com.fasterxml.jackson.databind.Module;
 import org.apereo.cas.util.serialization.AbstractJacksonBackedStringSerializer;
 
 import java.util.Map;
@@ -11,15 +12,20 @@ import java.util.Map;
  * @author Misagh Moayyed
  * @since 5.3.0
  */
-public class SessionStoreCookieSerializer extends AbstractJacksonBackedStringSerializer<Map> {
+public class SessionStoreCookieSerializer extends AbstractJacksonBackedStringSerializer<Map<String, Object>> {
     private static final long serialVersionUID = -1152522695984638020L;
 
-    public SessionStoreCookieSerializer() {
+
+    public SessionStoreCookieSerializer(final Module... additionalModules) {
         super(new MinimalPrettyPrinter());
+        for (Module module: additionalModules) {
+            getObjectMapper().registerModule(module);
+        }
     }
 
+    @SuppressWarnings("unchecked")
     @Override
-    protected Class<Map> getTypeToSerialize() {
-        return Map.class;
+    protected Class<Map<String, Object>> getTypeToSerialize() {
+        return (Class<Map<String,Object>>)(Class<?>) Map.class;
     }
 }
