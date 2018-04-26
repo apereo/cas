e7962b509e453a5357a93845de4fd7ff9f9ff1f2
Misagh Moayyed <mm1844@gmail.com>
2018-04-27 00:06:58 +0430

    clean up ticket building logic

--- support/cas-server-support-token-tickets/src/main/java/org/apereo/cas/token/authentication/principal/TokenWebApplicationServiceResponseBuilder.java
+++ support/cas-server-support-token-tickets/src/main/java/org/apereo/cas/token/authentication/principal/TokenWebApplicationServiceResponseBuilder.java
@@ -9,6 +9,8 @@ import org.apereo.cas.services.RegisteredServiceAccessStrategyUtils;
 import org.apereo.cas.services.RegisteredServiceProperty;
 import org.apereo.cas.services.ServicesManager;
 import org.apereo.cas.token.TokenTicketBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
@@ -20,6 +22,7 @@ import java.util.Map;
  */
 public class TokenWebApplicationServiceResponseBuilder extends WebApplicationServiceResponseBuilder {
     private static final long serialVersionUID = -2863268279032438778L;
+    private static final Logger LOGGER = LoggerFactory.getLogger(TokenWebApplicationServiceResponseBuilder.class);
 
     private final ServicesManager servicesManager;
     private final TokenTicketBuilder tokenTicketBuilder;
@@ -35,9 +38,12 @@ public class TokenWebApplicationServiceResponseBuilder extends WebApplicationSer
                                                   final Map<String, String> parameters) {
         final RegisteredService registeredService = this.servicesManager.findServiceBy(service);
         RegisteredServiceAccessStrategyUtils.ensureServiceAccessIsAllowed(service, registeredService);
-        final boolean tokenAsResponse = RegisteredServiceProperty.RegisteredServiceProperties.TOKEN_AS_RESPONSE.isAssignedTo(registeredService);
+        final boolean tokenAsResponse = RegisteredServiceProperty.RegisteredServiceProperties.TOKEN_AS_SERVICE_TICKET.isAssignedTo(registeredService);
 
         if (!tokenAsResponse) {
+            LOGGER.debug("Registered service [{}] is not configured to issue JWTs for service tickets. "
+                    + "Make sure the service property [{}] is defined and set to true", registeredService,
+                RegisteredServiceProperty.RegisteredServiceProperties.TOKEN_AS_SERVICE_TICKET.getPropertyName());
             return super.buildInternal(service, parameters);
         }
 
