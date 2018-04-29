d8d2ba99dbe025697073c73ecd3e4fb222eee400
Misagh Moayyed <mmoayyed@unicon.net>
2018-04-29 12:02:45 +0430

    added tests

---
+++ core/cas-server-core-tickets/src/test/java/org/apereo/cas/ticket/registry/CachingTicketRegistryTests.java
@@ -0,0 +1,45 @@
+package org.apereo.cas.ticket.registry;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apereo.cas.CipherExecutor;
+import org.apereo.cas.logout.LogoutManager;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * Test case to test the {@code CachingTicketRegistry} based on test cases to test all
+ * Ticket Registries.
+ *
+ * @author Misagh Moayyed
+ * @since 5.3.0
+ */
+@RunWith(Parameterized.class)
+@Slf4j
+public class CachingTicketRegistryTests extends AbstractTicketRegistryTests {
+
+    public CachingTicketRegistryTests(final boolean useEncryption) {
+        super(useEncryption);
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object> getTestParameters() {
+        return Arrays.asList(false, true);
+    }
+
+    @Override
+    public TicketRegistry getNewTicketRegistry() {
+        return new CachingTicketRegistry(mock(LogoutManager.class));
+    }
+
+    @Test
+    public void verifyOtherConstructor() {
+        assertNotNull(new DefaultTicketRegistry(10, 10, 5, CipherExecutor.noOp()));
+    }
+}
