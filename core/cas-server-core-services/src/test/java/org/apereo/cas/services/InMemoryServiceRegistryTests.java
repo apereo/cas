9cab89d64f81a6a5e82f2bc9d365d4d81c68a55e
Timur Duehr <tduehr@gmail.com>
2018-05-04 18:12:45 -0500

    AbstractServiceRegistryTests and InMemoryServiceRegistryTests

--- core/cas-server-core-services/src/test/java/org/apereo/cas/services/InMemoryServiceRegistryTests.java
+++ core/cas-server-core-services/src/test/java/org/apereo/cas/services/InMemoryServiceRegistryTests.java
@@ -1,9 +1,6 @@
 package org.apereo.cas.services;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 /**
  * This is test cases for {@link InMemoryServiceRegistry}.
@@ -11,45 +8,13 @@ import static org.junit.Assert.*;
  * @since 4.1.0
  */
 @Slf4j
-public class InMemoryServiceRegistryTests {
+public class InMemoryServiceRegistryTests extends AbstractServiceRegistryTests {
 
     private static final String SERVICE_ID = "service";
 
-    @Test
-    public void verifySave() {
-        final InMemoryServiceRegistry reg = new InMemoryServiceRegistry();
-        final RegisteredService svc = RegisteredServiceTestUtils.getRegisteredService(SERVICE_ID);
-        assertEquals(reg.save(svc), svc);
+    @Override
+    public ServiceRegistry getNewServiceRegistry() {
+        return new InMemoryServiceRegistry();
     }
 
-    @Test
-    public void verifyLoadEmpty() {
-        final InMemoryServiceRegistry reg = new InMemoryServiceRegistry();
-        assertTrue(reg.load().isEmpty());
-    }
-
-    @Test
-     public void verifySaveAndLoad() {
-        final InMemoryServiceRegistry reg = new InMemoryServiceRegistry();
-        final RegisteredService svc = RegisteredServiceTestUtils.getRegisteredService(SERVICE_ID);
-        assertEquals(reg.save(svc), svc);
-        assertEquals(1, reg.load().size());
-    }
-
-    @Test
-    public void verifySaveAndFind() {
-        final InMemoryServiceRegistry reg = new InMemoryServiceRegistry();
-        final RegisteredService svc = RegisteredServiceTestUtils.getRegisteredService(SERVICE_ID);
-        assertEquals(reg.save(svc), svc);
-        assertEquals(reg.findServiceById(svc.getId()), svc);
-    }
-
-    @Test
-    public void verifySaveAndDelete() {
-        final InMemoryServiceRegistry reg = new InMemoryServiceRegistry();
-        final RegisteredService svc = RegisteredServiceTestUtils.getRegisteredService(SERVICE_ID);
-        assertEquals(reg.save(svc), svc);
-        assertTrue(reg.delete(svc));
-        assertTrue(reg.load().isEmpty());
-    }
 }
