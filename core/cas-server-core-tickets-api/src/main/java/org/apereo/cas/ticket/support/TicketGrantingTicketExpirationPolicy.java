1069c77ac8c0347b127ff95f8a012e0dafaed167
Misagh Moayyed <mm1844@gmail.com>
2018-05-05 18:08:45 +0430

    clean up tests

--- core/cas-server-core-tickets-api/src/main/java/org/apereo/cas/ticket/support/TicketGrantingTicketExpirationPolicy.java
+++ core/cas-server-core-tickets-api/src/main/java/org/apereo/cas/ticket/support/TicketGrantingTicketExpirationPolicy.java
@@ -5,14 +5,14 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apereo.cas.ticket.TicketState;
 import org.springframework.util.Assert;
-import javax.annotation.PostConstruct;
+
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
-import lombok.NoArgsConstructor;
 
 /**
  * Provides the Ticket Granting Ticket expiration policy.  Ticket Granting Tickets
@@ -54,16 +54,11 @@ public class TicketGrantingTicketExpirationPolicy extends AbstractCasExpirationP
         this.timeToKillInSeconds = timeToKill;
     }
 
-    /**
-     * After properties set.
-     */
-    @PostConstruct
-    public void afterPropertiesSet() {
-        Assert.isTrue(this.maxTimeToLiveInSeconds >= this.timeToKillInSeconds, "maxTimeToLiveInSeconds must be greater than or equal to timeToKillInSeconds.");
-    }
-
     @Override
     public boolean isExpired(final TicketState ticketState) {
+        Assert.isTrue(this.maxTimeToLiveInSeconds >= this.timeToKillInSeconds,
+            "maxTimeToLiveInSeconds must be greater than or equal to timeToKillInSeconds.");
+
         final ZonedDateTime currentSystemTime = getCurrentSystemTime();
         final ZonedDateTime creationTime = ticketState.getCreationTime();
         final ZonedDateTime lastTimeUsed = ticketState.getLastTimeUsed();
