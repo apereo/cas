23504f0ce0a7e63e7537a75e1957ae275090c640
Misagh Moayyed <mmoayyed@unicon.net>
2018-05-06 22:57:36 +0430

    added tests

---
+++ support/cas-server-support-aup-webflow/src/test/java/org/apereo/cas/web/flow/AcceptableUsagePolicyVerifyActionTests.java
@@ -0,0 +1,39 @@
+package org.apereo.cas.web.flow;
+
+import org.apereo.cas.authentication.CoreAuthenticationTestUtils;
+import org.apereo.cas.mock.MockTicketGrantingTicket;
+import org.apereo.cas.web.support.WebUtils;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.webflow.context.servlet.ServletExternalContext;
+import org.springframework.webflow.execution.Action;
+import org.springframework.webflow.test.MockRequestContext;
+
+import static org.junit.Assert.*;
+
+/**
+ * This is {@link AcceptableUsagePolicyVerifyActionTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.3.0
+ */
+public class AcceptableUsagePolicyVerifyActionTests extends BaseAcceptableUsagePolicyActionTests {
+
+    @Autowired
+    @Qualifier("acceptableUsagePolicyVerifyAction")
+    private Action acceptableUsagePolicyVerifyAction;
+
+    @Test
+    public void verifyAction() throws Exception {
+        final MockRequestContext context = new MockRequestContext();
+        final MockHttpServletRequest request = new MockHttpServletRequest();
+        context.setExternalContext(new ServletExternalContext(new MockServletContext(), request, new MockHttpServletResponse()));
+        WebUtils.putCredential(context, CoreAuthenticationTestUtils.getCredentialsWithSameUsernameAndPassword());
+        WebUtils.putTicketGrantingTicketInScopes(context, new MockTicketGrantingTicket("casuser"));
+        assertEquals(AcceptableUsagePolicyVerifyAction.EVENT_ID_MUST_ACCEPT, acceptableUsagePolicyVerifyAction.execute(context).getId());
+    }
+}
