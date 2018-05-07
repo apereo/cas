377865801ffc6812e831cdf7837aacc3d865fea5
Misagh Moayyed <mm1844@gmail.com>
2018-05-07 18:33:46 +0430

    clean up config

---
+++ core/cas-server-core-authentication-api/src/test/java/org/apereo/cas/authentication/GroovyAuthenticationPreProcessorTests.java
@@ -0,0 +1,32 @@
+package org.apereo.cas.authentication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * This is {@link GroovyAuthenticationPreProcessorTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.3.0
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = RefreshAutoConfiguration.class)
+public class GroovyAuthenticationPreProcessorTests {
+    @Test
+    public void verifyAction() {
+        final GroovyAuthenticationPreProcessor g = new GroovyAuthenticationPreProcessor(new ClassPathResource("GroovyPreProcessor.groovy"));
+        final AuthenticationTransaction transaction = mock(AuthenticationTransaction.class);
+        when(transaction.getPrimaryCredential()).thenReturn(Optional.of(CoreAuthenticationTestUtils.getCredentialsWithSameUsernameAndPassword()));
+        assertTrue(g.process(transaction));
+        assertTrue(g.supports(CoreAuthenticationTestUtils.getCredentialsWithSameUsernameAndPassword()));
+    }
+}
