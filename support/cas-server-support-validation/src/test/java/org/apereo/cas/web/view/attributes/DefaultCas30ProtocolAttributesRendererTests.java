5f563a09fe49f5c8daa1908f80a892e69465afea
Misagh Moayyed <mm1844@gmail.com>
2018-05-09 22:49:04 +0430

    added tests

---
+++ support/cas-server-support-validation/src/test/java/org/apereo/cas/web/view/attributes/DefaultCas30ProtocolAttributesRendererTests.java
@@ -0,0 +1,23 @@
+package org.apereo.cas.web.view.attributes;
+
+import org.apereo.cas.authentication.CoreAuthenticationTestUtils;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+/**
+ * This is {@link DefaultCas30ProtocolAttributesRendererTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.3.0
+ */
+public class DefaultCas30ProtocolAttributesRendererTests {
+    @Test
+    public void verifyAction() {
+        final DefaultCas30ProtocolAttributesRenderer r = new DefaultCas30ProtocolAttributesRenderer();
+        final Map results = CoreAuthenticationTestUtils.getAttributeRepository().getBackingMap();
+        assertFalse(r.render(results).isEmpty());
+    }
+}
