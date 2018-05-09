dacbf1d1d08de542ff8a399e1a149544e6cfa593
Misagh Moayyed <mm1844@gmail.com>
2018-05-09 22:45:58 +0430

    added tests

---
+++ support/cas-server-support-yubikey-core/src/test/java/org/apereo/cas/adaptors/yubikey/DenyAllYubiKeyAccountValidatorTests.java
@@ -0,0 +1,19 @@
+package org.apereo.cas.adaptors.yubikey;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * This is {@link DenyAllYubiKeyAccountValidatorTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.3.0
+ */
+public class DenyAllYubiKeyAccountValidatorTests {
+    @Test
+    public void verifyAction() {
+        final DenyAllYubiKeyAccountValidator v = new DenyAllYubiKeyAccountValidator();
+        assertFalse(v.isValid("anything", "anything"));
+    }
+}
