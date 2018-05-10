367d20fed720ec9014bc5a52b5ec5066527dcb20
Misagh Moayyed <mm1844@gmail.com>
2018-05-10 22:23:29 +0430

    Working on saml validation and service extraction

--- support/cas-server-support-saml/src/main/java/org/apereo/cas/config/authentication/support/SamlServiceFactoryConfiguration.java
+++ support/cas-server-support-saml/src/main/java/org/apereo/cas/config/authentication/support/SamlServiceFactoryConfiguration.java
@@ -7,7 +7,11 @@ import org.apereo.cas.authentication.principal.WebApplicationService;
 import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.support.saml.authentication.principal.SamlService;
 import org.apereo.cas.support.saml.authentication.principal.SamlServiceFactory;
+import org.apereo.cas.support.saml.util.Saml10ObjectBuilder;
 import org.apereo.cas.util.CollectionUtils;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,15 +28,17 @@ import java.util.Collection;
 @EnableConfigurationProperties(CasConfigurationProperties.class)
 @Slf4j
 public class SamlServiceFactoryConfiguration implements ServiceFactoryConfigurer {
+    @Autowired
+    @Qualifier("saml10ObjectBuilder")
+    private ObjectProvider<Saml10ObjectBuilder> saml10ObjectBuilder;
+
     @Override
     public Collection<ServiceFactory<? extends WebApplicationService>> buildServiceFactories() {
         return CollectionUtils.wrap(samlServiceFactory());
     }
 
-
     @Bean
     public ServiceFactory<SamlService> samlServiceFactory() {
-        return new SamlServiceFactory();
+        return new SamlServiceFactory(this.saml10ObjectBuilder.getIfAvailable());
     }
-
 }
