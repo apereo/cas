76debca24c15224cd9191bfcf51c42d1e464f6bf
Fabien Crespel <fabien@crespel.net>
2018-05-11 11:34:15 +0200

    Fix OIDC token revocation to better respect RFC 7009 (5.2.x backport) (#3306)

--- support/cas-server-support-oidc/src/main/java/org/apereo/cas/oidc/web/controllers/OidcRevocationEndpointController.java
+++ support/cas-server-support-oidc/src/main/java/org/apereo/cas/oidc/web/controllers/OidcRevocationEndpointController.java
@@ -1,14 +1,11 @@
 package org.apereo.cas.oidc.web.controllers;
 
-
-import org.apache.commons.lang3.StringUtils;
 import org.apereo.cas.authentication.principal.PrincipalFactory;
 import org.apereo.cas.authentication.principal.ServiceFactory;
 import org.apereo.cas.authentication.principal.WebApplicationService;
 import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.oidc.OidcConstants;
 import org.apereo.cas.services.ServicesManager;
-import org.apereo.cas.support.oauth.OAuth20Constants;
 import org.apereo.cas.support.oauth.profile.OAuth20ProfileScopeToAttributesFilter;
 import org.apereo.cas.support.oauth.services.OAuthRegisteredService;
 import org.apereo.cas.support.oauth.util.OAuth20Utils;
@@ -25,7 +22,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -60,24 +57,23 @@ public class OidcRevocationEndpointController extends BaseOAuth20Controller {
      * @param response the response
      * @return the jwk set
      */
-    @GetMapping(value = '/' + OidcConstants.BASE_OIDC_URL + '/' + OidcConstants.REVOCATION_URL)
+    @PostMapping(value = '/' + OidcConstants.BASE_OIDC_URL + '/' + OidcConstants.REVOCATION_URL)
     public ResponseEntity<String> handleRequestInternal(final HttpServletRequest request,
                                                         final HttpServletResponse response) {
         try {
             final CredentialsExtractor<UsernamePasswordCredentials> authExtractor = new BasicAuthExtractor(getClass().getSimpleName());
             final UsernamePasswordCredentials credentials = authExtractor.extract(Pac4jUtils.getPac4jJ2EContext(request, response));
             if (credentials == null) {
-                throw new IllegalArgumentException("No credentials are provided to verify introspection on the access token");
+                throw new IllegalArgumentException("No credentials are provided to verify revocation of the token");
             }
 
             final OAuthRegisteredService service = OAuth20Utils.getRegisteredOAuthService(this.servicesManager, credentials.getUsername());
             if (this.validator.checkServiceValid(service)
-                    && this.validator.checkParameterExist(request, OAuth20Constants.ACCESS_TOKEN)
+                    && this.validator.checkParameterExist(request, OidcConstants.TOKEN)
                     && this.validator.checkClientSecret(service, credentials.getPassword())) {
                 final String token = request.getParameter(OidcConstants.TOKEN);
-                if (StringUtils.isNotBlank(token)) {
-                    this.ticketRegistry.deleteTicket(token);
-                }
+                LOGGER.debug("Located token [{}] in the revocation request", token);
+                this.ticketRegistry.deleteTicket(token);
             }
         } catch (final Exception e) {
             LOGGER.error(e.getMessage(), e);
