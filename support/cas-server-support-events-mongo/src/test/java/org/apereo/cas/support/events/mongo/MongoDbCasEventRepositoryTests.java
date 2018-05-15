ffc7851ddd7ace84aca478ff6ed7e8ba4bb29fe5
Timur Duehr <tduehr@gmail.com>
2018-05-15 14:49:58 -0500

    mongo tests

--- support/cas-server-support-events-mongo/src/test/java/org/apereo/cas/support/events/mongo/MongoDbCasEventRepositoryTests.java
+++ support/cas-server-support-events-mongo/src/test/java/org/apereo/cas/support/events/mongo/MongoDbCasEventRepositoryTests.java
@@ -1,16 +1,21 @@
 package org.apereo.cas.support.events.mongo;
 
-import lombok.extern.slf4j.Slf4j;
+import org.apereo.cas.category.MongoDbCategory;
 import org.apereo.cas.config.MongoDbEventsConfiguration;
 import org.apereo.cas.support.events.AbstractCasEventRepositoryTests;
 import org.apereo.cas.support.events.CasEventRepository;
-import org.junit.runner.RunWith;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.experimental.categories.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.rules.SpringClassRule;
+import org.springframework.test.context.junit4.rules.SpringMethodRule;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Test cases for {@link MongoDbCasEventRepository}.
@@ -18,11 +23,17 @@ import org.springframework.test.context.junit4.SpringRunner;
  * @author Misagh Moayyed
  * @since 5.2.0
  */
-@RunWith(SpringRunner.class)
+@Category(MongoDbCategory.class)
 @SpringBootTest(classes = {MongoDbEventsConfiguration.class, RefreshAutoConfiguration.class})
 @TestPropertySource(locations = {"classpath:/mongoevents.properties"})
 @Slf4j
 public class MongoDbCasEventRepositoryTests extends AbstractCasEventRepositoryTests {
+
+    @ClassRule
+    public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
+
+    @Rule
+    public final SpringMethodRule springMethodRule = new SpringMethodRule();
 
     @Autowired
     @Qualifier("casEventRepository")
