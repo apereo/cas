7934adcc1ef394cec45e845d29829fe16731fd56
Misagh Moayyed <mm1844@gmail.com>
2018-05-17 21:22:33 +0430

    fix styling issues

--- core/cas-server-core-services/src/test/java/org/apereo/cas/services/AnonymousRegisteredServiceUsernameAttributeProviderTests.java
+++ core/cas-server-core-services/src/test/java/org/apereo/cas/services/AnonymousRegisteredServiceUsernameAttributeProviderTests.java
@@ -3,9 +3,11 @@ package org.apereo.cas.services;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
+import org.apereo.cas.authentication.CoreAuthenticationTestUtils;
 import org.apereo.cas.authentication.principal.Principal;
 import org.apereo.cas.authentication.principal.Service;
 import org.apereo.cas.authentication.principal.ShibbolethCompatiblePersistentIdGenerator;
+import org.apereo.cas.util.CollectionUtils;
 import org.junit.Test;
 
 import java.io.File;
@@ -28,8 +30,8 @@ public class AnonymousRegisteredServiceUsernameAttributeProviderTests {
     @Test
     public void verifyPrincipalResolution() {
         final AnonymousRegisteredServiceUsernameAttributeProvider provider = new AnonymousRegisteredServiceUsernameAttributeProvider(
-                new ShibbolethCompatiblePersistentIdGenerator(CASROX));
-        
+            new ShibbolethCompatiblePersistentIdGenerator(CASROX));
+
         final Service service = mock(Service.class);
         when(service.getId()).thenReturn("id");
         final Principal principal = mock(Principal.class);
@@ -41,9 +43,9 @@ public class AnonymousRegisteredServiceUsernameAttributeProviderTests {
     @Test
     public void verifyEquality() {
         final AnonymousRegisteredServiceUsernameAttributeProvider provider = new AnonymousRegisteredServiceUsernameAttributeProvider(
-                        new ShibbolethCompatiblePersistentIdGenerator(CASROX));
+            new ShibbolethCompatiblePersistentIdGenerator(CASROX));
         final AnonymousRegisteredServiceUsernameAttributeProvider provider2 = new AnonymousRegisteredServiceUsernameAttributeProvider(
-                        new ShibbolethCompatiblePersistentIdGenerator(CASROX));
+            new ShibbolethCompatiblePersistentIdGenerator(CASROX));
 
         assertEquals(provider, provider2);
     }
@@ -51,9 +53,24 @@ public class AnonymousRegisteredServiceUsernameAttributeProviderTests {
     @Test
     public void verifySerializeADefaultRegisteredServiceUsernameProviderToJson() throws IOException {
         final AnonymousRegisteredServiceUsernameAttributeProvider providerWritten = new AnonymousRegisteredServiceUsernameAttributeProvider(
-                        new ShibbolethCompatiblePersistentIdGenerator(CASROX));
+            new ShibbolethCompatiblePersistentIdGenerator(CASROX));
         MAPPER.writeValue(JSON_FILE, providerWritten);
         final RegisteredServiceUsernameAttributeProvider providerRead = MAPPER.readValue(JSON_FILE, AnonymousRegisteredServiceUsernameAttributeProvider.class);
         assertEquals(providerWritten, providerRead);
+    }
+
+    @Test
+    public void verifyGeneratedIdsMatch() {
+        final String salt = "nJ+G!VgGt=E2xCJp@Kb+qjEjE4R2db7NEW!9ofjMNas2Tq3h5h!nCJxc3Sr#kv=7JwU?#MN=7e+r!wpcMw5RF42G8J"
+            + "8tNkGp4g4rFZ#RnNECL@wZX5=yia+KPEwwq#CA9EM38=ZkjK2mzv6oczCVC!m8k!=6@!MW@xTMYH8eSV@7yc24Bz6NUstzbTWH3pnGojZm7pW8N"
+            + "wjLypvZKqhn7agai295kFBhMmpS\n9Jz9+jhVkJfFjA32GiTkZ5hvYiFG104xWnMbHk7TsGrfw%tvACAs=f3C";
+        final ShibbolethCompatiblePersistentIdGenerator gen = new ShibbolethCompatiblePersistentIdGenerator(salt);
+        gen.setAttribute("employeeId");
+        final AnonymousRegisteredServiceUsernameAttributeProvider provider = new AnonymousRegisteredServiceUsernameAttributeProvider(gen);
+        final String result = provider.resolveUsername(CoreAuthenticationTestUtils.getPrincipal("anyuser",
+            CollectionUtils.wrap("employeeId", "T911327")),
+            CoreAuthenticationTestUtils.getService("https://cas.example.org/app"),
+            CoreAuthenticationTestUtils.getRegisteredService());
+        assertEquals("ujWTRNKPPso8S+4geOvcOZtv778=", result);
     }
 }
