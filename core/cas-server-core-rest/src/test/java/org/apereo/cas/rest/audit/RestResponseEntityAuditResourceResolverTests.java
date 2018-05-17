65d4a1a803ffb16d88fc5f8ea9bad124eecbfd7b
Misagh Moayyed <mm1844@gmail.com>
2018-05-17 12:55:35 +0430

    added tests

---
+++ core/cas-server-core-rest/src/test/java/org/apereo/cas/rest/audit/RestResponseEntityAuditResourceResolverTests.java
@@ -0,0 +1,36 @@
+package org.apereo.cas.rest.audit;
+
+import org.apereo.cas.util.CollectionUtils;
+import org.apereo.cas.util.MockWebServer;
+import org.aspectj.lang.JoinPoint;
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * This is {@link RestResponseEntityAuditResourceResolverTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.3.0
+ */
+public class RestResponseEntityAuditResourceResolverTests {
+    @Test
+    public void verifyAction() {
+        final RestResponseEntityAuditResourceResolver r = new RestResponseEntityAuditResourceResolver(true);
+        try (MockWebServer webServer = new MockWebServer(9193)) {
+            webServer.start();
+            final MultiValueMap headers = new LinkedMultiValueMap();
+            headers.put("header", CollectionUtils.wrapList("value"));
+            headers.put("location", CollectionUtils.wrapList("someplace"));
+            final ResponseEntity entity = new ResponseEntity("The Response Body", headers, HttpStatus.OK);
+            assertTrue(r.resolveFrom(mock(JoinPoint.class), entity).length > 0);
+        } catch (final Exception e) {
+            throw new AssertionError(e.getMessage(), e);
+        }
+    }
+}
