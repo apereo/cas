4c8d6a7cc0f24f87f7218f1cc7a66462d5f19387
Misagh Moayyed <mm1844@gmail.com>
2018-05-19 21:51:55 +0430

    Working on scim and x509 tests

---
+++ support/cas-server-support-scim/src/test/java/org/apereo/cas/scim/v2/ScimV2PrincipalAttributeMapperTests.java
@@ -0,0 +1,42 @@
+package org.apereo.cas.scim.v2;
+
+import com.unboundid.scim2.common.types.Meta;
+import com.unboundid.scim2.common.types.Name;
+import com.unboundid.scim2.common.types.UserResource;
+import org.apereo.cas.authentication.CoreAuthenticationTestUtils;
+import org.junit.Test;
+
+import java.net.URI;
+import java.util.Calendar;
+
+/**
+ * This is {@link ScimV2PrincipalAttributeMapperTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.3.0
+ */
+public class ScimV2PrincipalAttributeMapperTests {
+    @Test
+    public void verifyAction() throws Exception {
+        final UserResource user = new UserResource();
+        user.setActive(true);
+        user.setDisplayName("CASUser");
+        user.setId("casuser");
+        final Name name = new Name();
+        name.setGivenName("casuser");
+        user.setName(name);
+        final Meta meta = new Meta();
+        meta.setResourceType("User");
+        meta.setCreated(Calendar.getInstance());
+        meta.setLocation(new URI("http://localhost:8218"));
+        user.setMeta(meta);
+
+        try {
+            final ScimV2PrincipalAttributeMapper mapper = new ScimV2PrincipalAttributeMapper();
+            mapper.map(user, CoreAuthenticationTestUtils.getPrincipal(),
+                CoreAuthenticationTestUtils.getCredentialsWithSameUsernameAndPassword());
+        } catch (final Exception e) {
+            throw new AssertionError(e.getMessage(), e);
+        }
+    }
+}
