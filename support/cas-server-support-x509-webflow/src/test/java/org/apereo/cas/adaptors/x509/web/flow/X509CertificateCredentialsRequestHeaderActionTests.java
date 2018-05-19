fb99a66d1ef89807dd5868812b77818605a8ba1a
Misagh Moayyed <mmoayyed@unicon.net>
2018-05-19 20:45:35 +0430

    Working on scim and x509 tests

--- support/cas-server-support-x509-webflow/src/test/java/org/apereo/cas/adaptors/x509/web/flow/X509CertificateCredentialsRequestHeaderActionTests.java
+++ support/cas-server-support-x509-webflow/src/test/java/org/apereo/cas/adaptors/x509/web/flow/X509CertificateCredentialsRequestHeaderActionTests.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apereo.cas.adaptors.x509.authentication.principal.AbstractX509CertificateTests;
 import org.apereo.cas.adaptors.x509.config.X509AuthenticationConfiguration;
 import org.apereo.cas.web.flow.CasWebflowConstants;
-import org.apereo.cas.web.flow.X509CertificateCredentialsNonInteractiveAction;
 import org.apereo.cas.web.flow.config.X509AuthenticationWebflowConfiguration;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,38 +17,30 @@ import org.springframework.webflow.context.servlet.ServletExternalContext;
 import org.springframework.webflow.execution.Action;
 import org.springframework.webflow.test.MockRequestContext;
 
-import java.security.cert.X509Certificate;
-
 import static org.junit.Assert.*;
 
-
 /**
- * @author Marvin S. Addison
- * @since 3.0.0
+ * This is {@link X509CertificateCredentialsRequestHeaderActionTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.3.0
  */
-@TestPropertySource(locations = {"classpath:/x509.properties"})
+@TestPropertySource(locations = {"classpath:/x509.properties"},
+    properties = "cas.authn.x509.extractCert=true")
 @Import(value = {X509AuthenticationWebflowConfiguration.class, X509AuthenticationConfiguration.class})
 @Slf4j
-public class X509CertificateCredentialsNonInteractiveActionTests extends AbstractX509CertificateTests {
+public class X509CertificateCredentialsRequestHeaderActionTests extends AbstractX509CertificateTests {
 
     @Autowired
     @Qualifier("x509Check")
     private Action action;
 
     @Test
-    public void verifyNoCredentialsResultsInError() throws Exception {
-        final MockRequestContext context = new MockRequestContext();
-        context.setExternalContext(new ServletExternalContext(
-            new MockServletContext(), new MockHttpServletRequest(), new MockHttpServletResponse()));
-        assertEquals(CasWebflowConstants.TRANSITION_ID_ERROR, this.action.execute(context).getId());
-    }
-
-    @Test
-    public void verifyCredentialsResultsInSuccess() throws Exception {
+    public void verifyCredentialsResultsInAuthnFailure() throws Exception {
         final MockRequestContext context = new MockRequestContext();
         final MockHttpServletRequest request = new MockHttpServletRequest();
-        request.setAttribute(X509CertificateCredentialsNonInteractiveAction.REQUEST_ATTRIBUTE_X509_CERTIFICATE, new X509Certificate[]{VALID_CERTIFICATE});
+        request.addHeader("ssl_client_cert", VALID_CERTIFICATE.getContent());
         context.setExternalContext(new ServletExternalContext(new MockServletContext(), request, new MockHttpServletResponse()));
-        assertEquals(CasWebflowConstants.TRANSITION_ID_SUCCESS, this.action.execute(context).getId());
+        assertEquals(CasWebflowConstants.TRANSITION_ID_AUTHENTICATION_FAILURE, this.action.execute(context).getId());
     }
 }
