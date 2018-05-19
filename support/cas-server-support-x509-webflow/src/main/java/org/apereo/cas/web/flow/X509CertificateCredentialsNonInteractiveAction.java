01354c8d812b5e5647b2bf0985d3a471be636281
Misagh Moayyed <mm1844@gmail.com>
2018-05-19 20:45:35 +0430

    Working on scim and x509 tests

--- support/cas-server-support-x509-webflow/src/main/java/org/apereo/cas/web/flow/X509CertificateCredentialsNonInteractiveAction.java
+++ support/cas-server-support-x509-webflow/src/main/java/org/apereo/cas/web/flow/X509CertificateCredentialsNonInteractiveAction.java
@@ -1,7 +1,5 @@
 package org.apereo.cas.web.flow;
 
-import java.security.cert.X509Certificate;
-
 import lombok.extern.slf4j.Slf4j;
 import org.apereo.cas.adaptors.x509.authentication.principal.X509CertificateCredential;
 import org.apereo.cas.authentication.Credential;
@@ -9,7 +7,12 @@ import org.apereo.cas.authentication.adaptive.AdaptiveAuthenticationPolicy;
 import org.apereo.cas.web.flow.actions.AbstractNonInteractiveCredentialsAction;
 import org.apereo.cas.web.flow.resolver.CasDelegatingWebflowEventResolver;
 import org.apereo.cas.web.flow.resolver.CasWebflowEventResolver;
+import org.apereo.cas.web.support.WebUtils;
 import org.springframework.webflow.execution.RequestContext;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.cert.X509Certificate;
+import java.util.Arrays;
 
 /**
  * Concrete implementation of AbstractNonInteractiveCredentialsAction that
@@ -22,7 +25,10 @@ import org.springframework.webflow.execution.RequestContext;
 @Slf4j
 public class X509CertificateCredentialsNonInteractiveAction extends AbstractNonInteractiveCredentialsAction {
 
-    private static final String CERTIFICATE_REQUEST_ATTRIBUTE = "javax.servlet.request.X509Certificate";
+    /**
+     * Attribute to indicate the x509 certificate.
+     */
+    public static final String REQUEST_ATTRIBUTE_X509_CERTIFICATE = "javax.servlet.request.X509Certificate";
 
     public X509CertificateCredentialsNonInteractiveAction(final CasDelegatingWebflowEventResolver initialAuthenticationAttemptWebflowEventResolver,
                                                           final CasWebflowEventResolver serviceTicketRequestWebflowEventResolver,
@@ -32,13 +38,14 @@ public class X509CertificateCredentialsNonInteractiveAction extends AbstractNonI
 
     @Override
     protected Credential constructCredentialsFromRequest(final RequestContext context) {
-        final X509Certificate[] certificates = (X509Certificate[]) context.getExternalContext().getRequestMap().get(CERTIFICATE_REQUEST_ATTRIBUTE);
+        final HttpServletRequest request = WebUtils.getHttpServletRequestFromExternalWebflowContext(context);
+        final X509Certificate[] certificates = (X509Certificate[]) request.getAttribute(REQUEST_ATTRIBUTE_X509_CERTIFICATE);
 
         if (certificates == null || certificates.length == 0) {
-            LOGGER.debug("Certificates not found in request attribute: {}", CERTIFICATE_REQUEST_ATTRIBUTE);
+            LOGGER.debug("Certificates not found in request attribute: {}", REQUEST_ATTRIBUTE_X509_CERTIFICATE);
             return null;
         }
-        LOGGER.debug("Certificate found in request.");
+        LOGGER.debug("[{}] Certificate(s) found in request: [{}]", certificates.length, Arrays.toString(certificates));
         return new X509CertificateCredential(certificates);
     }
 }
