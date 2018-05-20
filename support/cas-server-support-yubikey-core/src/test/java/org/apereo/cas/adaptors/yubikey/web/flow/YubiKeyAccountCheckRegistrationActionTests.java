a55aab12f3c54b6607897736166189bfd54344a3
Misagh Moayyed <mm1844@gmail.com>
2018-05-20 13:31:21 +0430

    Working on tests

---
+++ support/cas-server-support-yubikey-core/src/test/java/org/apereo/cas/adaptors/yubikey/web/flow/YubiKeyAccountCheckRegistrationActionTests.java
@@ -0,0 +1,10 @@
+package org.apereo.cas.adaptors.yubikey.web.flow;
+
+/**
+ * This is {@link YubiKeyAccountCheckRegistrationActionTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.3.0
+ */
+public class YubiKeyAccountCheckRegistrationActionTests {
+}
---
+++ support/cas-server-support-yubikey-core/src/test/java/org/apereo/cas/adaptors/yubikey/web/flow/YubiKeyAccountSaveRegistrationActionTests.java
@@ -0,0 +1,10 @@
+package org.apereo.cas.adaptors.yubikey.web.flow;
+
+/**
+ * This is {@link YubiKeyAccountSaveRegistrationActionTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.3.0
+ */
+public class YubiKeyAccountSaveRegistrationActionTests {
+}
