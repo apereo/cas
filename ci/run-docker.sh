0f5953fa87f69fb110fca1d9f43ef41b4ef67737
Misagh Moayyed <mm1844@gmail.com>
2018-05-22 00:19:04 +0430

    updated

--- ci/run-docker.sh
+++ ci/run-docker.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ "$MATRIX_JOB_TYPE" == "TEST" ]; then
-    set -o errexit
+#    set -o errexit
 
-    sudo apt update -y
+#    sudo apt update -y
 
-    echo "Upgrading Docker..."
-    sudo apt install --only-upgrade docker-ce -y
-    echo "Upgrade Done"
+#    echo "Upgrading Docker..."
+#    sudo apt install --only-upgrade docker-ce -y
+#    echo "Upgrade Done"
 
     docker info
 fi
