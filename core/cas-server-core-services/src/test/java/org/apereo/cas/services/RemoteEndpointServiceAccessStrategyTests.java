f789ef8b386f39aa380232175fbf58142bd8a691
Misagh Moayyed <mmoayyed@unicon.net>
2018-05-23 15:50:31 +0430

    cntd working on test cases

--- core/cas-server-core-services/src/test/java/org/apereo/cas/services/RemoteEndpointServiceAccessStrategyTests.java
+++ core/cas-server-core-services/src/test/java/org/apereo/cas/services/RemoteEndpointServiceAccessStrategyTests.java
@@ -3,10 +3,15 @@ package org.apereo.cas.services;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
+import org.apereo.cas.authentication.CoreAuthenticationTestUtils;
+import org.apereo.cas.util.MockWebServer;
 import org.junit.Test;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.MediaType;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 import static org.junit.Assert.*;
 
@@ -23,12 +28,22 @@ public class RemoteEndpointServiceAccessStrategyTests {
     @Test
     public void verifySerializeAX509CertificateCredentialToJson() throws IOException {
         final RemoteEndpointServiceAccessStrategy strategyWritten = new RemoteEndpointServiceAccessStrategy();
-
         MAPPER.writeValue(JSON_FILE, strategyWritten);
-
         final RegisteredServiceAccessStrategy credentialRead = MAPPER.readValue(JSON_FILE, RemoteEndpointServiceAccessStrategy.class);
-
         assertEquals(strategyWritten, credentialRead);
     }
 
+    @Test
+    public void verifyOperation() {
+        final RemoteEndpointServiceAccessStrategy strategy = new RemoteEndpointServiceAccessStrategy();
+        strategy.setEndpointUrl("http://localhost:8755");
+        strategy.setAcceptableResponseCodes("200,201");
+        try (MockWebServer webServer = new MockWebServer(8755,
+            new ByteArrayResource("OK".getBytes(StandardCharsets.UTF_8), "REST Output"), MediaType.APPLICATION_JSON_VALUE)) {
+            webServer.start();
+            assertTrue(strategy.doPrincipalAttributesAllowServiceAccess("casuser", CoreAuthenticationTestUtils.getAttributes()));
+        } catch (final Exception e) {
+            throw new AssertionError(e.getMessage(), e);
+        }
+    }
 }
