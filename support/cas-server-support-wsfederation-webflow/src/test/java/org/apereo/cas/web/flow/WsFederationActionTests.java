fa602b982be96c936b52cb513905546102a77e78
Misagh Moayyed <mm1844@gmail.com>
2018-05-27 11:24:03 -0700

    added tests

---
+++ support/cas-server-support-wsfederation-webflow/src/test/java/org/apereo/cas/web/flow/WsFederationActionTests.java
@@ -0,0 +1,16 @@
+package org.apereo.cas.web.flow;
+
+import org.junit.Test;
+
+/**
+ * This is {@link WsFederationActionTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.3.0
+ */
+public class WsFederationActionTests extends BaseWsFederationWebflowTests {
+    @Test
+    public void verifyOperation() {
+        
+    }
+}
