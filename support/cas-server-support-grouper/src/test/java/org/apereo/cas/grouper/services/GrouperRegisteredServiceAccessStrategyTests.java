1a753ce8d67677854b64c8e8020ab646540bd8d2
Misagh Moayyed <mm1844@gmail.com>
2018-05-27 14:41:35 -0700

    added tests

--- support/cas-server-support-grouper/src/test/java/org/apereo/cas/grouper/services/GrouperRegisteredServiceAccessStrategyTests.java
+++ support/cas-server-support-grouper/src/test/java/org/apereo/cas/grouper/services/GrouperRegisteredServiceAccessStrategyTests.java
@@ -1,14 +1,24 @@
 package org.apereo.cas.grouper.services;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.apereo.cas.services.AbstractRegisteredService;
+import org.apereo.cas.services.JsonServiceRegistry;
 import org.apereo.cas.services.RegisteredServiceTestUtils;
+import org.apereo.cas.services.replication.NoOpRegisteredServiceReplicationStrategy;
+import org.apereo.cas.services.resource.DefaultRegisteredServiceResourceNamingStrategy;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.core.io.ClassPathResource;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+
+import static org.mockito.Mockito.*;
 
 /**
  * The {@link GrouperRegisteredServiceAccessStrategyTests} provides
@@ -20,7 +30,33 @@ import java.util.Set;
 @Slf4j
 public class GrouperRegisteredServiceAccessStrategyTests {
 
-    
+    private static final ClassPathResource RESOURCE = new ClassPathResource("services");
+
+    @BeforeClass
+    public static void prepTests() throws Exception {
+        FileUtils.cleanDirectory(RESOURCE.getFile());
+    }
+
+    @Test
+    public void checkAccessStrategyJson() throws Exception {
+
+        final Map<String, Set<String>> attributes = new HashMap<>();
+        final Set<String> v1 = new HashSet<>();
+        v1.add("admin");
+        attributes.put("memberOf", v1);
+
+        final AbstractRegisteredService service = RegisteredServiceTestUtils.getRegisteredService("test");
+        final GrouperRegisteredServiceAccessStrategy grouper = new GrouperRegisteredServiceAccessStrategy();
+        grouper.setRequiredAttributes(attributes);
+        service.setAccessStrategy(grouper);
+        final JsonServiceRegistry dao = new JsonServiceRegistry(RESOURCE, false,
+            mock(ApplicationEventPublisher.class),
+            new NoOpRegisteredServiceReplicationStrategy(),
+            new DefaultRegisteredServiceResourceNamingStrategy());
+        dao.save(service);
+        dao.load();
+    }
+
     @Test
     public void checkGrouperAttributes() {
         final ClassPathResource resource = new ClassPathResource("grouper.client.properties");
