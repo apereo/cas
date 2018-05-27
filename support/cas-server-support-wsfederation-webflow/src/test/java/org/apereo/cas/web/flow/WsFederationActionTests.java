68abce258a5080f4785bbd7e655dcc894a4eaf3d
Misagh Moayyed <mm1844@gmail.com>
2018-05-27 12:12:25 -0700

    updated

--- support/cas-server-support-wsfederation-webflow/src/test/java/org/apereo/cas/web/flow/WsFederationActionTests.java
+++ support/cas-server-support-wsfederation-webflow/src/test/java/org/apereo/cas/web/flow/WsFederationActionTests.java
@@ -1,6 +1,15 @@
 package org.apereo.cas.web.flow;
 
+import org.apereo.cas.authentication.CoreAuthenticationTestUtils;
+import org.apereo.cas.web.support.WebUtils;
 import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.webflow.context.servlet.ServletExternalContext;
+import org.springframework.webflow.test.MockRequestContext;
+
+import static org.junit.Assert.*;
 
 /**
  * This is {@link WsFederationActionTests}.
@@ -10,7 +19,12 @@ import org.junit.Test;
  */
 public class WsFederationActionTests extends BaseWsFederationWebflowTests {
     @Test
-    public void verifyOperation() {
-        
+    public void verifyRequestOperation() throws Exception {
+        final MockRequestContext context = new MockRequestContext();
+        final MockHttpServletRequest request = new MockHttpServletRequest();
+        context.setExternalContext(new ServletExternalContext(new MockServletContext(), request, new MockHttpServletResponse()));
+        WebUtils.putService(context, CoreAuthenticationTestUtils.getWebApplicationService());
+        wsFederationAction.execute(context);
+        assertTrue(context.getFlowScope().contains(WsFederationRequestBuilder.PARAMETER_NAME_WSFED_CLIENTS));
     }
 }
