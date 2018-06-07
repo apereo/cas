c27c6b5f0d2e1b29568acce5cbc4effdf1b67aa9
Misagh Moayyed <mm1844@gmail.com>
2018-06-07 09:44:53 -0700

    updated

--- core/cas-server-core-util-api/src/main/java/org/apereo/cas/util/InetAddressUtils.java
+++ core/cas-server-core-util-api/src/main/java/org/apereo/cas/util/InetAddressUtils.java
@@ -28,7 +28,7 @@ public class InetAddressUtils {
             final URL url = new URL(urlAddr);
             return InetAddress.getByName(url.getHost());
         } catch (final Exception e) {
-            LOGGER.debug("Host name could not be determined automatically.", e);
+            LOGGER.trace("Host name could not be determined automatically.", e);
         }
         return null;
     }
