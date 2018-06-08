88f658ed4bca477621ee98c3c0e0e0793660fdae
NgSekLong <longsek@gmail.com>
2018-06-08 17:54:00 +0800

    [Not finished] Change OAuthCode generate method
    - Using Mock to generate OAuthCode cannot demonstrate the problem, 
    - Try to use `oAuthCodeFactory.create(holder.getService(), holder.getAuthentication(),  holder.getTicketGrantingTicket(), holder.getScopes());` instead
    
    - Change OAuth20TokenRequestValidator  to OAuth20AuthorizationCodeGrantTypeTokenRequestValidator

--- support/cas-server-support-oauth/src/test/java/org/apereo/cas/support/oauth/validator/token/OAuth20AuthorizationCodeGrantTypeTokenRequestValidatorTests.java
+++ support/cas-server-support-oauth/src/test/java/org/apereo/cas/support/oauth/validator/token/OAuth20AuthorizationCodeGrantTypeTokenRequestValidatorTests.java
@@ -1,25 +1,44 @@
 package org.apereo.cas.support.oauth.validator.token;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apereo.cas.CentralAuthenticationService;
+import org.apereo.cas.authentication.AuthenticationResult;
+import org.apereo.cas.authentication.DefaultAuthenticationResult;
 import org.apereo.cas.authentication.principal.Service;
+import org.apereo.cas.config.CasCoreConfiguration;
+import org.apereo.cas.config.CasOAuthConfiguration;
 import org.apereo.cas.services.RegisteredServiceAccessStrategyAuditableEnforcer;
 import org.apereo.cas.services.RegisteredServiceTestUtils;
 import org.apereo.cas.services.ServicesManager;
 import org.apereo.cas.support.oauth.OAuth20Constants;
 import org.apereo.cas.support.oauth.OAuth20GrantTypes;
 import org.apereo.cas.support.oauth.authenticator.Authenticators;
+import org.apereo.cas.support.oauth.authenticator.OAuth20CasAuthenticationBuilder;
 import org.apereo.cas.support.oauth.services.OAuthRegisteredService;
+import org.apereo.cas.support.oauth.util.OAuth20Utils;
+import org.apereo.cas.support.oauth.web.response.accesstoken.ext.AccessTokenRequestDataHolder;
+import org.apereo.cas.support.oauth.web.response.callback.OAuth20AuthorizationResponseBuilder;
 import org.apereo.cas.ticket.code.OAuthCode;
+import org.apereo.cas.ticket.code.OAuthCodeFactory;
 import org.apereo.cas.ticket.registry.TicketRegistry;
 import org.apereo.cas.util.CollectionUtils;
+import org.apereo.cas.web.flow.config.CasCoreWebflowConfiguration;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.pac4j.core.context.J2EContext;
 import org.pac4j.core.context.Pac4jConstants;
 import org.pac4j.core.profile.CommonProfile;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.servlet.http.HttpSession;
+import java.util.Set;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -30,10 +49,36 @@ import static org.mockito.Mockito.*;
  * @author Misagh Moayyed
  * @since 6.0.0
  */
+@SpringBootTest(classes = {
+        RefreshAutoConfiguration.class,
+        CasOAuthConfiguration.class,
+        CasCoreWebflowConfiguration.class,
+        CasCoreConfiguration.class,
+        })
+@RunWith(SpringRunner.class)
 public class OAuth20AuthorizationCodeGrantTypeTokenRequestValidatorTests {
     private OAuth20TokenRequestValidator validator;
 
+    @Autowired
+    private CentralAuthenticationService centralAuthenticationService;
+
+    @Autowired
+    private OAuth20CasAuthenticationBuilder oauthCasAuthenticationBuilder;
+
+
+    @Autowired
+    @Qualifier("defaultOAuthCodeFactory")
+    protected OAuthCodeFactory oAuthCodeFactory;
+
+
+    @Autowired
+    @Qualifier("oauthAuthorizationCodeResponseBuilder")
+    public OAuth20AuthorizationResponseBuilder oAuth20AuthorizationCodeAuthorizationResponseBuilder;
+
+
     private TicketRegistry ticketRegistry;
+
+
 
     @Before
     public void before() {
@@ -46,11 +91,26 @@ public class OAuth20AuthorizationCodeGrantTypeTokenRequestValidatorTests {
         registeredService.setClientSecret("secret");
         registeredService.setServiceId(service.getId());
 
-        final OAuthCode oauthCode = mock(OAuthCode.class);
-        when(oauthCode.getId()).thenReturn("OC-12345678");
-        when(oauthCode.isExpired()).thenReturn(false);
-        when(oauthCode.getAuthentication()).thenReturn(RegisteredServiceTestUtils.getAuthentication());
-        when(oauthCode.getService()).thenReturn(service);
+
+        final MockHttpServletRequest request = new MockHttpServletRequest();
+        final MockHttpServletResponse response = new MockHttpServletResponse();
+
+        final Service oauthCasAuthenticationBuilderService = this.oauthCasAuthenticationBuilder.buildService(registeredService,  new J2EContext(request, response), false);
+
+
+        J2EContext context= new J2EContext(request, response);
+
+        final String grantType = StringUtils.defaultIfEmpty(context.getRequestParameter(OAuth20Constants.GRANT_TYPE),
+                OAuth20GrantTypes.AUTHORIZATION_CODE.getType()).toUpperCase();
+        final Set<String> scopes = OAuth20Utils.parseRequestScopes(context);
+
+        AuthenticationResult authenticationResult = new DefaultAuthenticationResult(RegisteredServiceTestUtils.getAuthentication(), oauthCasAuthenticationBuilderService) ;
+
+        final AccessTokenRequestDataHolder holder = new AccessTokenRequestDataHolder(oauthCasAuthenticationBuilderService, RegisteredServiceTestUtils.getAuthentication(),
+                registeredService, centralAuthenticationService.createTicketGrantingTicket(authenticationResult), OAuth20GrantTypes.valueOf(grantType), scopes);
+        final OAuthCode oauthCode = oAuthCodeFactory.create(holder.getService(), holder.getAuthentication(),
+                holder.getTicketGrantingTicket(), holder.getScopes());
+
 
         this.ticketRegistry = mock(TicketRegistry.class);
         when(ticketRegistry.getTicket(anyString(), any())).thenReturn(oauthCode);
