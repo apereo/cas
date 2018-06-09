e8de766a1c6c56031a94e128abe21d1ffe7b2d67
Misagh Moayyed <mm1844@gmail.com>
2018-06-08 17:12:13 -0700

    updated

--- api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/support/geo/maxmind/MaxmindProperties.java
+++ api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/support/geo/maxmind/MaxmindProperties.java
@@ -1,11 +1,11 @@
 package org.apereo.cas.configuration.model.support.geo.maxmind;
 
-import org.apereo.cas.configuration.support.RequiresModule;
-import org.apereo.cas.configuration.support.RequiredProperty;
-import org.springframework.core.io.Resource;
-import java.io.Serializable;
 import lombok.Getter;
 import lombok.Setter;
+import org.apereo.cas.configuration.model.support.geo.BaseGeoLocationProperties;
+import org.apereo.cas.configuration.support.RequiredProperty;
+import org.apereo.cas.configuration.support.RequiresModule;
+import org.springframework.core.io.Resource;
 
 /**
  * This is {@link MaxmindProperties}.
@@ -16,7 +16,7 @@ import lombok.Setter;
 @RequiresModule(name = "cas-server-support-geolocation-maxmind")
 @Getter
 @Setter
-public class MaxmindProperties implements Serializable {
+public class MaxmindProperties extends BaseGeoLocationProperties {
 
     private static final long serialVersionUID = 7883029275219817797L;
 
