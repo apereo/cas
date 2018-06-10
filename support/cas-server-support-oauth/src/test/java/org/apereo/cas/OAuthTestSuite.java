b36edcae15850235afaafc48a8b9ed956c9b4fa3
Misagh Moayyed <mm1844@gmail.com>
2018-06-09 22:51:20 -0700

    work on travis-ci build

--- support/cas-server-support-oauth/src/test/java/org/apereo/cas/OAuthTestSuite.java
+++ support/cas-server-support-oauth/src/test/java/org/apereo/cas/OAuthTestSuite.java
@@ -1,11 +1,23 @@
 package org.apereo.cas;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apereo.cas.support.oauth.services.OAuthWebApplicationServiceTests;
+import org.apereo.cas.support.oauth.services.OAuth20WebApplicationServiceTests;
+import org.apereo.cas.support.oauth.validator.authorization.OAuth20AuthorizationCodeResponseTypeAuthorizationRequestValidatorTests;
+import org.apereo.cas.support.oauth.validator.authorization.OAuth20ClientCredentialsGrantTypeAuthorizationRequestValidatorTests;
+import org.apereo.cas.support.oauth.validator.authorization.OAuth20PasswordGrantTypeAuthorizationRequestValidatorTests;
+import org.apereo.cas.support.oauth.validator.authorization.OAuth20RefreshTokenGrantTypeAuthorizationRequestValidatorTests;
+import org.apereo.cas.support.oauth.validator.token.OAuth20AuthorizationCodeGrantTypeTokenRequestValidatorTests;
+import org.apereo.cas.support.oauth.validator.token.OAuth20ClientCredentialsGrantTypeTokenRequestValidatorTests;
+import org.apereo.cas.support.oauth.validator.token.OAuth20PasswordGrantTypeTokenRequestValidatorTests;
+import org.apereo.cas.support.oauth.validator.token.OAuth20RefreshTokenGrantTypeTokenRequestValidatorTests;
 import org.apereo.cas.support.oauth.web.OAuth20AccessTokenControllerMemcachedTests;
 import org.apereo.cas.support.oauth.web.OAuth20AccessTokenControllerTests;
 import org.apereo.cas.support.oauth.web.OAuth20AuthorizeControllerTests;
 import org.apereo.cas.support.oauth.web.OAuth20ProfileControllerTests;
+import org.apereo.cas.support.oauth.web.audit.AccessTokenGrantRequestAuditResourceResolverTests;
+import org.apereo.cas.support.oauth.web.audit.OAuth20UserProfileDataAuditResourceResolverTests;
+import org.apereo.cas.support.oauth.web.views.OAuth20DefaultUserProfileViewRendererFlatTests;
+import org.apereo.cas.support.oauth.web.views.OAuth20DefaultUserProfileViewRendererNestedTests;
 import org.apereo.cas.ticket.accesstoken.OAuthAccessTokenExpirationPolicyTests;
 import org.apereo.cas.ticket.accesstoken.OAuthAccessTokenSovereignExpirationPolicyTests;
 import org.apereo.cas.ticket.refreshtoken.OAuthRefreshTokenExpirationPolicyTests;
@@ -26,10 +38,22 @@ import org.junit.runners.Suite;
     OAuthAccessTokenExpirationPolicyTests.class,
     OAuthAccessTokenSovereignExpirationPolicyTests.class,
     OAuthRefreshTokenExpirationPolicyTests.class,
+    OAuth20AuthorizationCodeGrantTypeTokenRequestValidatorTests.class,
+    OAuth20PasswordGrantTypeTokenRequestValidatorTests.class,
+    OAuth20ClientCredentialsGrantTypeTokenRequestValidatorTests.class,
+    OAuth20RefreshTokenGrantTypeTokenRequestValidatorTests.class,
+    OAuth20AuthorizationCodeResponseTypeAuthorizationRequestValidatorTests.class,
+    OAuth20ClientCredentialsGrantTypeAuthorizationRequestValidatorTests.class,
+    OAuth20PasswordGrantTypeAuthorizationRequestValidatorTests.class,
+    OAuth20RefreshTokenGrantTypeAuthorizationRequestValidatorTests.class,
     OAuthRefreshTokenSovereignExpirationPolicyTests.class,
     OAuth20AccessTokenControllerMemcachedTests.class,
     OAuth20ProfileControllerTests.class,
-    OAuthWebApplicationServiceTests.class
+    OAuth20WebApplicationServiceTests.class,
+    OAuth20DefaultUserProfileViewRendererFlatTests.class,
+    OAuth20DefaultUserProfileViewRendererNestedTests.class,
+    AccessTokenGrantRequestAuditResourceResolverTests.class,
+    OAuth20UserProfileDataAuditResourceResolverTests.class
 })
 @Slf4j
 public class OAuthTestSuite {
