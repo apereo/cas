cc2fac511cb8d8d54e365286de183c8267c83970
Misagh Moayyed <mmoayyed@unicon.net>
2018-06-14 12:04:29 -0700

    fixes webflow handling of AUP to access the authn object and not the TGT

--- support/cas-server-support-aup-core/src/main/java/org/apereo/cas/aup/DefaultAcceptableUsagePolicyRepository.java
+++ support/cas-server-support-aup-core/src/main/java/org/apereo/cas/aup/DefaultAcceptableUsagePolicyRepository.java
@@ -2,6 +2,8 @@ package org.apereo.cas.aup;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.tuple.Pair;
+import org.apereo.cas.authentication.Authentication;
+import org.apereo.cas.authentication.AuthenticationException;
 import org.apereo.cas.authentication.Credential;
 import org.apereo.cas.authentication.principal.Principal;
 import org.apereo.cas.ticket.registry.TicketRegistrySupport;
@@ -31,7 +33,11 @@ public class DefaultAcceptableUsagePolicyRepository extends AbstractPrincipalAtt
     @Override
     public Pair<Boolean, Principal> verify(final RequestContext requestContext, final Credential credential) {
         final String key = credential.getId();
-        final Principal principal = WebUtils.getPrincipalFromRequestContext(requestContext, this.ticketRegistrySupport);
+        final Authentication authentication = WebUtils.getAuthentication(requestContext);
+        if (authentication == null) {
+            throw new AuthenticationException("No authentication could be found in the current context");
+        }
+        final Principal principal = authentication.getPrincipal();
         if (this.policyMap.containsKey(key)) {
             return Pair.of(this.policyMap.get(key), principal);
         }
