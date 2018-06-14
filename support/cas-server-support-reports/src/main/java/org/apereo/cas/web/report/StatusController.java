9d4afb6f9d282092a0266a26886e6c9573fef176
NgSekLong <longsek@gmail.com>
2018-06-14 16:07:41 +0800

    Update StatusController.java

--- support/cas-server-support-reports/src/main/java/org/apereo/cas/web/report/StatusController.java
+++ support/cas-server-support-reports/src/main/java/org/apereo/cas/web/report/StatusController.java
@@ -57,11 +57,7 @@ public class StatusController extends BaseCasMvcEndpoint {
             response.setStatus(HttpStatus.SERVICE_UNAVAILABLE.value());
         }
 
-        sb.append("Health: ").append(status.getCode()).append("\n\n\t");
-
-        String healthDetail = Joiner.on("\n\n\t").withKeyValueSeparator(": ").join(health.getDetails());
-        sb.append(healthDetail);        
-        
+        sb.append("Health: ").append(status.getCode());
         sb.append("\n\nHost:\t\t").append(
             StringUtils.isBlank(casProperties.getHost().getName())
                 ? InetAddressUtils.getCasServerHostName()
