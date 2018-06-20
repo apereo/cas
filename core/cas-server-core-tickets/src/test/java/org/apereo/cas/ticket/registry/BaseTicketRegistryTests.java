74866f6172fc53bb9837ccccde242ea480ace116
Misagh Moayyed <mm1844@gmail.com>
2018-06-20 06:52:09 -0700

    clean up

--- core/cas-server-core-tickets/src/test/java/org/apereo/cas/ticket/registry/BaseTicketRegistryTests.java
+++ core/cas-server-core-tickets/src/test/java/org/apereo/cas/ticket/registry/BaseTicketRegistryTests.java
@@ -196,7 +196,8 @@ public abstract class BaseTicketRegistryTests {
                     CoreAuthenticationTestUtils.getAuthentication(),
                     new NeverExpiresExpirationPolicy()));
             }
-            assertEquals(TICKETS_IN_REGISTRY, this.ticketRegistry.deleteAll());
+            final long actual = this.ticketRegistry.deleteAll();
+            assertEquals(TICKETS_IN_REGISTRY, actual);
         } catch (final Exception e) {
             throw new AssertionError(CAUGHT_AN_EXCEPTION_BUT_WAS_NOT_EXPECTED + e.getMessage(), e);
         }
