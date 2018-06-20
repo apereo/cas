8e5bbcba7921ea7343fbe853d415826d137eef96
Timur Duehr <tduehr@gmail.com>
2018-06-20 18:17:28 -0400

    event support

---
+++ support/cas-server-support-events-couchdb/src/main/java/org/apereo/cas/config/CouchDbEventsConfiguration.java
@@ -0,0 +1,59 @@
+package org.apereo.cas.config;
+
+import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.couchdb.core.CouchDbConnectorFactory;
+import org.apereo.cas.couchdb.events.EventCouchDbRepository;
+import org.apereo.cas.support.events.CasEventRepository;
+import org.apereo.cas.support.events.CouchDbCasEventRepository;
+
+import lombok.val;
+import org.ektorp.impl.ObjectMapperFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * This is {@link CouchDbEventsConfiguration}, defines certain beans via configuration
+ * while delegating some to Spring namespaces inside the context config file.
+ *
+ * @author Timur Duehr
+ * @since 6.0.0
+ */
+@Configuration("couchDbEventsConfiguration")
+@EnableConfigurationProperties(CasConfigurationProperties.class)
+public class CouchDbEventsConfiguration {
+
+    @Autowired
+    private CasConfigurationProperties casProperties;
+
+    @Autowired
+    @Qualifier("defaultObjectMapperFactory")
+    private ObjectMapperFactory objectMapperFactory;
+
+    @ConditionalOnMissingBean(name = "couchDbEventRepository")
+    @Bean
+    @RefreshScope
+    public EventCouchDbRepository couchDbEventRepository(@Qualifier("eventCouchDbFactory") final CouchDbConnectorFactory eventCouchDbFactory) {
+        val repository = new EventCouchDbRepository(eventCouchDbFactory.getCouchDbConnector(), casProperties.getEvents().getCouchDb().isCreateIfNotExists());
+        repository.initStandardDesignDocument();
+        return repository;
+    }
+
+    @ConditionalOnMissingBean(name = "eventCouchDbFactory")
+    @Bean
+    @RefreshScope
+    public CouchDbConnectorFactory eventCouchDbFactory() {
+        return new CouchDbConnectorFactory(casProperties.getEvents().getCouchDb(), objectMapperFactory);
+    }
+
+    @ConditionalOnMissingBean(name = "couchDbCasEventRepository")
+    @Bean
+    @RefreshScope
+    public CasEventRepository casEventRepository(@Qualifier("couchDbEventRepository") final EventCouchDbRepository eventCouchDbRepository) {
+        return new CouchDbCasEventRepository(eventCouchDbRepository, casProperties.getEvents().getCouchDb().isAsynchronous());
+    }
+}
