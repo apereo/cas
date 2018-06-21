ef0e8e44dcf6f719bfb5a2995b08c90e7ad9d5bc
Misagh Moayyed <mm1844@gmail.com>
2018-06-21 16:27:33 -0700

    Update init-travis-build.sh

--- ci/init-travis-build.sh
+++ ci/init-travis-build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 echo -e "Building branch: ${TRAVIS_BRANCH}"
+echo -e "Building pull request branch: ${TRAVIS_PULL_REQUEST_BRANCH}"
 echo -e "Build directory: ${TRAVIS_BUILD_DIR}"
 echo -e "Build id: ${TRAVIS_BUILD_ID}"
+echo -e "Build type: ${TRAVIS_EVENT_TYPE}"
 echo -e "Builder number: ${TRAVIS_BUILD_NUMBER}"
 echo -e "Job id: ${TRAVIS_JOB_ID}"
 echo -e "Job number: ${TRAVIS_JOB_NUMBER}"
@@ -10,7 +12,9 @@ echo -e "Repo slug: ${TRAVIS_REPO_SLUG}"
 echo -e "OS name: ${TRAVIS_OS_NAME}"
 echo -e "Pull Request: ${TRAVIS_PULL_REQUEST}"
 echo -e "Commit Message: ${TRAVIS_COMMIT_MESSAGE}"
-echo -e "Job Type: ${MATRIX_JOB_TYPE}"
+echo -e "User: ${USER}"
+echo -e "User HOME directory: ${HOME}"
+echo -e "JAVA_HOME: ${JAVA_HOME}"
 
 if [ "$TRAVIS_SECURE_ENV_VARS" == "false" ]
 then
