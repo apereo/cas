2eb0982d475fb3850306803ff517b52f72450928
Timur Duehr <tduehr@gmail.com>
2018-06-26 21:37:11 -0500

    u2f support

---
+++ support/cas-server-support-u2f-couchdb/src/main/java/org/apereo/cas/config/U2FCouchDbConfiguration.java
@@ -0,0 +1,94 @@
+package org.apereo.cas.config;
+
+import org.apereo.cas.CipherExecutor;
+import org.apereo.cas.adaptors.u2f.storage.U2FCouchDbDeviceRepository;
+import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.configuration.model.support.mfa.U2FMultifactorProperties;
+import org.apereo.cas.couchdb.U2FDeviceRegistrationCouchDbRepository;
+import org.apereo.cas.couchdb.core.CouchDbConnectorFactory;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
+import lombok.val;
+import org.apache.commons.lang3.StringUtils;
+import org.ektorp.CouchDbConnector;
+import org.ektorp.CouchDbInstance;
+import org.ektorp.impl.ObjectMapperFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.Serializable;
+
+/**
+ * This is {@link U2FCouchDbConfiguration}.
+ *
+ * @author Timur Duehr
+ * @since 6.0.0
+ */
+@Configuration("couchDbU2fConfiguration")
+@EnableConfigurationProperties(CasConfigurationProperties.class)
+public class U2FCouchDbConfiguration {
+    @Autowired
+    private CasConfigurationProperties casProperties;
+
+    @Autowired
+    @Qualifier("u2fRegistrationRecordCipherExecutor")
+    private CipherExecutor<Serializable, String> u2fRegistrationRecordCipherExecutor;
+
+    @Autowired
+    @Qualifier("u2fCouchDbFactory")
+    private CouchDbConnectorFactory u2fCouchDbFactory;
+
+    @Autowired
+    @Qualifier("defaultObjectMapperFactory")
+    private ObjectMapperFactory objectMapperFactory;
+    @RefreshScope
+    @Bean
+    public CouchDbInstance u2fCouchDbInstance() {
+        return u2fCouchDbFactory.createInstance();
+    }
+
+    @RefreshScope
+    @Bean
+    public CouchDbConnector u2fCouchDbConnector() {
+        return u2fCouchDbFactory.createConnector();
+    }
+
+    @Bean
+    @RefreshScope
+    public U2FDeviceRegistrationCouchDbRepository couchDbU2fDeviceRegistrationRepository() {
+        final U2FMultifactorProperties.CouchDb couchDb = casProperties.getAuthn().getMfa().getU2f().getCouchDb();
+        return new U2FDeviceRegistrationCouchDbRepository(u2fCouchDbFactory.create(),
+            couchDb.isCreateIfNotExists());
+    }
+
+    @Bean
+    @RefreshScope
+    public CouchDbConnectorFactory u2fCouchDbFactory() {
+        return new CouchDbConnectorFactory(casProperties.getAuthn().getMfa().getU2f().getCouchDb(), objectMapperFactory);
+    }
+
+    @Bean
+    @RefreshScope
+    public U2FCouchDbDeviceRepository u2fDeviceRepository() {
+        val u2f = casProperties.getAuthn().getMfa().getU2f();
+
+        val couchDb = u2f.getCouchDb();
+
+
+        final LoadingCache<String, String> requestStorage =
+            Caffeine.newBuilder()
+                .expireAfterWrite(u2f.getExpireRegistrations(), u2f.getExpireRegistrationsTimeUnit())
+                .build(key -> StringUtils.EMPTY);
+        val repo = new U2FCouchDbDeviceRepository(requestStorage, couchDbU2fDeviceRegistrationRepository(),
+            u2f.getExpireRegistrations(),
+            u2f.getExpireDevicesTimeUnit(),
+            couchDb.isAsyncronous());
+        repo.setCipherExecutor(this.u2fRegistrationRecordCipherExecutor);
+        return repo;
+    }
+}
