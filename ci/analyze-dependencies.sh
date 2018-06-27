946043c754aadb59fbc562b7a07db7f5cc6a2718
Misagh Moayyed <mmoayyed@unicon.net>
2018-06-27 10:57:39 -0700

    optimize CI build

--- ci/analyze-dependencies.sh
+++ ci/analyze-dependencies.sh
@@ -19,6 +19,9 @@ else
     runBuild=true
 fi
 
+if [ "$runBuild" = false ]; then
+    exit 0
+fi
 
 prepCommand="echo 'Running command...'; "
 gradle="./gradlew $@"
@@ -29,43 +32,38 @@ echo -e "***********************************************"
 echo -e "Gradle build started at `date`"
 echo -e "***********************************************"
 
-if [ "$runBuild" = true ]; then
-    echo "Running dependency analysis..."
+echo "Running dependency analysis..."
 
-    gradleBuild="$gradleBuild dependencyCheckAnalyze dependencyCheckUpdate -x javadoc -x check \
-        -DskipNpmLint=true -DskipGradleLint=true --parallel -DskipSass=true -DskipNpmLint=true \
-        -DskipNodeModulesCleanUp=true -DskipNpmCache=true -DskipNestedConfigMetadataGen=true "
+gradleBuild="$gradleBuild dependencyCheckAnalyze dependencyCheckUpdate -x javadoc -x check \
+    -DskipNpmLint=true -DskipGradleLint=true --parallel -DskipSass=true -DskipNpmLint=true \
+    -DskipNodeModulesCleanUp=true -DskipNpmCache=true -DskipNestedConfigMetadataGen=true "
 
-    if [[ "${TRAVIS_COMMIT_MESSAGE}" == *"[show streams]"* ]]; then
-        gradleBuild="$gradleBuild -DshowStandardStreams=true "
-    fi
+if [[ "${TRAVIS_COMMIT_MESSAGE}" == *"[show streams]"* ]]; then
+    gradleBuild="$gradleBuild -DshowStandardStreams=true "
 fi
 
-if [ -z "$gradleBuild" ]; then
-    echo "Gradle build will be ignored since no commands are specified to run."
+tasks="$gradle $gradleBuildOptions $gradleBuild"
+echo -e "***************************************************************************************"
+echo $prepCommand
+echo $tasks
+echo -e "***************************************************************************************"
+
+waitloop="while sleep 9m; do echo -e '\n=====[ Gradle build is still running ]====='; done &"
+eval $waitloop
+waitRetVal=$?
+
+eval $prepCommand
+eval $tasks
+retVal=$?
+
+echo -e "***************************************************************************************"
+echo -e "Gradle build finished at `date` with exit code $retVal"
+echo -e "***************************************************************************************"
+
+if [ $retVal == 0 ]; then
+    echo "Gradle build finished successfully."
 else
-    tasks="$gradle $gradleBuildOptions $gradleBuild"
-    echo -e "***************************************************************************************"
-    echo $prepCommand
-    echo $tasks
-    echo -e "***************************************************************************************"
-
-    waitloop="while sleep 9m; do echo -e '\n=====[ Gradle build is still running ]====='; done &"
-    eval $waitloop
-    waitRetVal=$?
-
-    eval $prepCommand
-    eval $tasks
-    retVal=$?
-
-    echo -e "***************************************************************************************"
-    echo -e "Gradle build finished at `date` with exit code $retVal"
-    echo -e "***************************************************************************************"
-
-    if [ $retVal == 0 ]; then
-        echo "Gradle build finished successfully."
-    else
-        echo "Gradle build did NOT finish successfully."
-        exit $retVal
-    fi
+    echo "Gradle build did NOT finish successfully."
+    exit $retVal
 fi
+
