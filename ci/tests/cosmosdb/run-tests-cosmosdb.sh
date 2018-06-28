eade025deae82266e1f3be1f0c0be541e05f066d
tduehr <tduehr@users.noreply.github.com>
2018-06-28 11:47:03 -0500

    fixup test environment variables (#3381)

--- ci/tests/cosmosdb/run-tests-cosmosdb.sh
+++ ci/tests/cosmosdb/run-tests-cosmosdb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 prepCommand="echo 'Running command...'; "
-gradle="./gradlew $@"
+gradle="MATRIX_SERVER=COSMOSDB ./gradlew $@"
 gradleBuild=""
 gradleBuildOptions="--stacktrace --build-cache --configure-on-demand --no-daemon "
 
@@ -9,7 +9,7 @@ echo -e "***********************************************"
 echo -e "Gradle build started at `date`"
 echo -e "***********************************************"
 
-gradleBuild="$gradleBuild testCosmosDb coveralls -DMATRIX_SERVER=COSMOSDB -x javadoc -x check \
+gradleBuild="$gradleBuild testCosmosDb coveralls -x javadoc -x check \
     -DskipNpmLint=true -DskipGradleLint=true -DskipSass=true -DskipNpmLint=true --parallel \
     -DskipNodeModulesCleanUp=true -DskipNpmCache=true -DskipNestedConfigMetadataGen=true "
 
