665f869da8bc5a83cb8b29a8897581c86ca02ea9
Misagh Moayyed <mmoayyed@unicon.net>
2018-07-08 10:33:47 +0430

    add properties for oidc discovery settings [force build]

--- support/cas-server-support-oidc/src/main/java/org/apereo/cas/oidc/discovery/OidcServerDiscoverySettingsFactory.java
+++ support/cas-server-support-oidc/src/main/java/org/apereo/cas/oidc/discovery/OidcServerDiscoverySettingsFactory.java
@@ -1,16 +1,10 @@
 package org.apereo.cas.oidc.discovery;
 
-import lombok.val;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.apereo.cas.configuration.CasConfigurationProperties;
-import org.apereo.cas.support.oauth.OAuth20GrantTypes;
-import org.apereo.cas.support.oauth.OAuth20ResponseTypes;
-import org.apereo.cas.util.CollectionUtils;
 import org.springframework.beans.factory.FactoryBean;
-
-import java.util.ArrayList;
 
 /**
  * This is {@link OidcServerDiscoverySettingsFactory}.
@@ -26,30 +20,15 @@ public class OidcServerDiscoverySettingsFactory implements FactoryBean<OidcServe
     @Override
     public OidcServerDiscoverySettings getObject() {
         val oidc = casProperties.getAuthn().getOidc();
-        val discoveryProperties =
-            new OidcServerDiscoverySettings(casProperties, oidc.getIssuer());
-
+        val discoveryProperties = new OidcServerDiscoverySettings(casProperties, oidc.getIssuer());
         discoveryProperties.setClaimsSupported(oidc.getClaims());
         discoveryProperties.setScopesSupported(oidc.getScopes());
-        discoveryProperties.setResponseTypesSupported(
-            CollectionUtils.wrapList(OAuth20ResponseTypes.CODE.getType(),
-                OAuth20ResponseTypes.TOKEN.getType(),
-                OAuth20ResponseTypes.IDTOKEN_TOKEN.getType()));
-
+        discoveryProperties.setResponseTypesSupported(oidc.getResponseTypesSupported());
         discoveryProperties.setSubjectTypesSupported(oidc.getSubjectTypes());
-        discoveryProperties.setClaimTypesSupported(CollectionUtils.wrap("normal"));
-
-        val authnMethods = new ArrayList<String>();
-        authnMethods.add("client_secret_basic");
-        discoveryProperties.setIntrospectionSupportedAuthenticationMethods(authnMethods);
-
-        discoveryProperties.setGrantTypesSupported(
-            CollectionUtils.wrapList(OAuth20GrantTypes.AUTHORIZATION_CODE.getType(),
-                OAuth20GrantTypes.PASSWORD.getType(),
-                OAuth20GrantTypes.CLIENT_CREDENTIALS.getType(),
-                OAuth20GrantTypes.REFRESH_TOKEN.getType()));
-
-        discoveryProperties.setIdTokenSigningAlgValuesSupported(CollectionUtils.wrapList("none", "RS256"));
+        discoveryProperties.setClaimTypesSupported(oidc.getClaimTypesSupported());
+        discoveryProperties.setIntrospectionSupportedAuthenticationMethods(oidc.getIntrospectionSupportedAuthenticationMethods());
+        discoveryProperties.setGrantTypesSupported(oidc.getGrantTypesSupported());
+        discoveryProperties.setIdTokenSigningAlgValuesSupported(oidc.getIdTokenSigningAlgValuesSupported());
         return discoveryProperties;
     }
 
