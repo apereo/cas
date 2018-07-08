b65f1a7fac2ce343086bb392d1c84d7b88ba75b5
Misagh Moayyed <mm1844@gmail.com>
2018-07-08 09:37:34 +0430

    oauth introspection [force build]

--- support/cas-server-support-oidc/src/main/java/org/apereo/cas/oidc/web/controllers/OidcIntrospectionEndpointController.java
+++ support/cas-server-support-oidc/src/main/java/org/apereo/cas/oidc/web/controllers/OidcIntrospectionEndpointController.java
@@ -1,35 +1,23 @@
 package org.apereo.cas.oidc.web.controllers;
 
-import lombok.val;
-
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
+import lombok.val;
 import org.apereo.cas.CentralAuthenticationService;
-import org.apereo.cas.audit.AuditableContext;
 import org.apereo.cas.audit.AuditableExecution;
-import org.apereo.cas.authentication.AuthenticationManager;
 import org.apereo.cas.authentication.principal.PrincipalFactory;
 import org.apereo.cas.authentication.principal.ServiceFactory;
 import org.apereo.cas.authentication.principal.WebApplicationService;
 import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.oidc.OidcConstants;
-import org.apereo.cas.oidc.introspection.OidcIntrospectionAccessTokenResponse;
 import org.apereo.cas.services.ServicesManager;
-import org.apereo.cas.support.oauth.OAuth20Constants;
 import org.apereo.cas.support.oauth.profile.OAuth20ProfileScopeToAttributesFilter;
 import org.apereo.cas.support.oauth.services.OAuthRegisteredService;
-import org.apereo.cas.support.oauth.util.OAuth20Utils;
-import org.apereo.cas.support.oauth.web.endpoints.BaseOAuth20Controller;
+import org.apereo.cas.support.oauth.web.endpoints.OAuth20IntrospectionEndpointController;
+import org.apereo.cas.support.oauth.web.response.introspection.OAuth20IntrospectionAccessTokenResponse;
 import org.apereo.cas.ticket.accesstoken.AccessToken;
 import org.apereo.cas.ticket.accesstoken.AccessTokenFactory;
 import org.apereo.cas.ticket.registry.TicketRegistry;
-import org.apereo.cas.util.CollectionUtils;
-import org.apereo.cas.util.HttpRequestUtils;
-import org.apereo.cas.util.Pac4jUtils;
 import org.apereo.cas.web.support.CookieRetrievingCookieGenerator;
-import org.pac4j.core.credentials.UsernamePasswordCredentials;
-import org.pac4j.core.credentials.extractor.BasicAuthExtractor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -37,7 +25,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.stream.Collectors;
 
 /**
  * This is {@link OidcIntrospectionEndpointController}.
@@ -46,11 +33,7 @@ import java.util.stream.Collectors;
  * @since 5.2.0
  */
 @Slf4j
-public class OidcIntrospectionEndpointController extends BaseOAuth20Controller {
-
-    private final CentralAuthenticationService centralAuthenticationService;
-    private final AuditableExecution registeredServiceAccessStrategyEnforcer;
-
+public class OidcIntrospectionEndpointController extends OAuth20IntrospectionEndpointController {
     public OidcIntrospectionEndpointController(final ServicesManager servicesManager,
                                                final TicketRegistry ticketRegistry,
                                                final AccessTokenFactory accessTokenFactory,
@@ -62,9 +45,8 @@ public class OidcIntrospectionEndpointController extends BaseOAuth20Controller {
                                                final CentralAuthenticationService centralAuthenticationService,
                                                final AuditableExecution registeredServiceAccessStrategyEnforcer) {
         super(servicesManager, ticketRegistry, accessTokenFactory, principalFactory,
-            webApplicationServiceServiceFactory, scopeToAttributesFilter, casProperties, cookieGenerator);
-        this.centralAuthenticationService = centralAuthenticationService;
-        this.registeredServiceAccessStrategyEnforcer = registeredServiceAccessStrategyEnforcer;
+            webApplicationServiceServiceFactory, scopeToAttributesFilter,
+            casProperties, cookieGenerator, centralAuthenticationService, registeredServiceAccessStrategyEnforcer);
     }
 
     /**
@@ -77,9 +59,10 @@ public class OidcIntrospectionEndpointController extends BaseOAuth20Controller {
     @GetMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
         produces = MediaType.APPLICATION_JSON_VALUE,
         value = {'/' + OidcConstants.BASE_OIDC_URL + '/' + OidcConstants.INTROSPECTION_URL})
-    public ResponseEntity<OidcIntrospectionAccessTokenResponse> handleRequest(final HttpServletRequest request,
-                                                                              final HttpServletResponse response) {
-        return handlePostRequest(request, response);
+    @Override
+    public ResponseEntity<OAuth20IntrospectionAccessTokenResponse> handleRequest(final HttpServletRequest request,
+                                                                                 final HttpServletResponse response) {
+        return handleRequest(request, response);
     }
 
     /**
@@ -92,78 +75,16 @@ public class OidcIntrospectionEndpointController extends BaseOAuth20Controller {
     @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
         produces = MediaType.APPLICATION_JSON_VALUE,
         value = {'/' + OidcConstants.BASE_OIDC_URL + '/' + OidcConstants.INTROSPECTION_URL})
-    public ResponseEntity<OidcIntrospectionAccessTokenResponse> handlePostRequest(final HttpServletRequest request,
-                                                                                  final HttpServletResponse response) {
-        try {
-            val authExtractor = new BasicAuthExtractor();
-            val credentials = authExtractor.extract(Pac4jUtils.getPac4jJ2EContext(request, response));
-            if (credentials == null) {
-                throw new IllegalArgumentException("No credentials are provided to verify introspection on the access token");
-            }
-
-            val service = OAuth20Utils.getRegisteredOAuthServiceByClientId(this.servicesManager, credentials.getUsername());
-            if (validateIntrospectionRequest(service, credentials, request)) {
-                val accessToken = StringUtils.defaultIfBlank(request.getParameter(OAuth20Constants.ACCESS_TOKEN),
-                    request.getParameter(OAuth20Constants.TOKEN));
-
-                LOGGER.debug("Located access token [{}] in the request", accessToken);
-                val ticket = this.centralAuthenticationService.getTicket(accessToken, AccessToken.class);
-                if (ticket != null) {
-                    return createIntrospectionResponse(service, ticket);
-                }
-            }
-        } catch (final Exception e) {
-            LOGGER.error(e.getMessage(), e);
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-        return new ResponseEntity<>(HttpStatus.OK);
+    @Override
+    public ResponseEntity<OAuth20IntrospectionAccessTokenResponse> handlePostRequest(final HttpServletRequest request,
+                                                                                     final HttpServletResponse response) {
+        return super.handlePostRequest(request, response);
     }
 
-    private boolean validateIntrospectionRequest(final OAuthRegisteredService registeredService,
-                                                 final UsernamePasswordCredentials credentials,
-                                                 final HttpServletRequest request) {
-        val tokenExists = HttpRequestUtils.doesParameterExist(request, OAuth20Constants.ACCESS_TOKEN)
-            || HttpRequestUtils.doesParameterExist(request, OAuth20Constants.TOKEN);
-
-
-        if (tokenExists && OAuth20Utils.checkClientSecret(registeredService, credentials.getPassword())) {
-            val service = webApplicationServiceServiceFactory.createService(registeredService.getServiceId());
-            val audit = AuditableContext.builder()
-                .service(service)
-                .registeredService(registeredService)
-                .build();
-            val accessResult = this.registeredServiceAccessStrategyEnforcer.execute(audit);
-            return !accessResult.isExecutionFailure();
-        }
-        return false;
-    }
-
-    private ResponseEntity<OidcIntrospectionAccessTokenResponse> createIntrospectionResponse(final OAuthRegisteredService service, final AccessToken ticket) {
-        val introspect = new OidcIntrospectionAccessTokenResponse();
-        introspect.setActive(true);
-        introspect.setClientId(service.getClientId());
-        val authentication = ticket.getAuthentication();
-        val subject = authentication.getPrincipal().getId();
-        introspect.setSub(subject);
-        introspect.setUniqueSecurityName(subject);
-        introspect.setExp(ticket.getExpirationPolicy().getTimeToLive());
-        introspect.setIat(ticket.getCreationTime().toInstant().getEpochSecond());
-
-        val methods = authentication.getAttributes().get(AuthenticationManager.AUTHENTICATION_METHOD_ATTRIBUTE);
-        val realmNames = CollectionUtils.toCollection(methods)
-            .stream()
-            .map(Object::toString)
-            .collect(Collectors.joining(","));
-
-        introspect.setRealmName(realmNames);
-        introspect.setTokenType(OAuth20Constants.TOKEN_TYPE_BEARER);
-
-        val grant = authentication.getAttributes()
-            .getOrDefault(OAuth20Constants.GRANT_TYPE, StringUtils.EMPTY).toString().toLowerCase();
-        introspect.setGrantType(grant);
-        introspect.setScope(OidcConstants.StandardScopes.OPENID.getScope());
-        introspect.setAud(service.getServiceId());
-        introspect.setIss(casProperties.getAuthn().getOidc().getIssuer());
-        return new ResponseEntity<>(introspect, HttpStatus.OK);
+    @Override
+    protected OAuth20IntrospectionAccessTokenResponse createIntrospectionResponse(final OAuthRegisteredService service, final AccessToken ticket) {
+        val r = super.createIntrospectionResponse(service, ticket);
+        r.setScope(OidcConstants.StandardScopes.OPENID.getScope());
+        return r;
     }
 }
