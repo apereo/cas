6a4e58ecc14a87dddda103493897893a912b5cd9
Misagh Moayyed <mm1844@gmail.com>
2018-07-15 14:03:28 +0430

    continue working on device code flow in oauth and error codes.

--- support/cas-server-support-oauth/src/main/java/org/apereo/cas/support/oauth/web/response/accesstoken/OAuth20AccessTokenResponseGenerator.java
+++ support/cas-server-support-oauth/src/main/java/org/apereo/cas/support/oauth/web/response/accesstoken/OAuth20AccessTokenResponseGenerator.java
@@ -1,13 +1,12 @@
 package org.apereo.cas.support.oauth.web.response.accesstoken;
 
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.apereo.cas.authentication.principal.Service;
 import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.configuration.support.Beans;
 import org.apereo.cas.support.oauth.OAuth20Constants;
 import org.apereo.cas.support.oauth.OAuth20ResponseTypes;
 import org.apereo.cas.support.oauth.services.OAuthRegisteredService;
@@ -15,11 +14,13 @@ import org.apereo.cas.ticket.accesstoken.AccessToken;
 import org.apereo.cas.ticket.refreshtoken.RefreshToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.http.MediaType;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * This is {@link OAuth20AccessTokenResponseGenerator}.
@@ -29,9 +30,7 @@ import java.io.IOException;
  */
 @Slf4j
 public class OAuth20AccessTokenResponseGenerator implements AccessTokenResponseGenerator {
-    private static final int DEVICE_REQUEST_INTERVAL = 15;
-
-    private static final JsonFactory JSON_FACTORY = new JsonFactory(new ObjectMapper().findAndRegisterModules());
+    private static final ObjectMapper MAPPER = new ObjectMapper().findAndRegisterModules();
 
     /**
      * The Resource loader.
@@ -39,76 +38,89 @@ public class OAuth20AccessTokenResponseGenerator implements AccessTokenResponseG
     @Autowired
     protected ResourceLoader resourceLoader;
 
-    /**
-     * CAS settings.
-     */
-    @Autowired
-    protected CasConfigurationProperties casProperties;
-
     @Override
     @SneakyThrows
-    public void generate(final HttpServletRequest request,
-                         final HttpServletResponse response,
-                         final OAuthRegisteredService registeredService,
-                         final Service service,
-                         final OAuth20TokenGeneratedResult result,
-                         final long accessTokenTimeout,
-                         final OAuth20ResponseTypes responseType,
-                         final CasConfigurationProperties casProperties) {
+    public ModelAndView generate(final HttpServletRequest request,
+                                 final HttpServletResponse response,
+                                 final OAuthRegisteredService registeredService,
+                                 final Service service,
+                                 final OAuth20TokenGeneratedResult result,
+                                 final long accessTokenTimeout,
+                                 final OAuth20ResponseTypes responseType,
+                                 final CasConfigurationProperties casProperties) {
 
         if (OAuth20ResponseTypes.DEVICE_CODE == responseType) {
-            generateResponseForDeviceToken(request, response, registeredService, service, result, casProperties);
-        } else {
-            generateResponseForAccessToken(request, response, registeredService, service, result, accessTokenTimeout, responseType);
+            return generateResponseForDeviceToken(request, response, registeredService, service, result, casProperties);
         }
-    }
 
-    @SneakyThrows
-    private void generateResponseForDeviceToken(final HttpServletRequest request,
-                                                final HttpServletResponse response,
-                                                final OAuthRegisteredService registeredService,
-                                                final Service service,
-                                                final OAuth20TokenGeneratedResult result,
-                                                final CasConfigurationProperties casProperties) {
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        try (val jsonGenerator = getResponseJsonGenerator(response)) {
-            jsonGenerator.writeStartObject();
-            val uri = casProperties.getServer().getPrefix()
-                .concat(OAuth20Constants.BASE_OAUTH20_URL)
-                .concat("/")
-                .concat(OAuth20Constants.DEVICE_AUTHZ_URL);
-            jsonGenerator.writeStringField(OAuth20Constants.DEVICE_VERIFICATION_URI, uri);
-            jsonGenerator.writeStringField(OAuth20Constants.DEVICE_USER_CODE, result.getUserCode().get());
-            jsonGenerator.writeStringField(OAuth20Constants.DEVICE_CODE, result.getDeviceCode().get());
-            jsonGenerator.writeNumberField(OAuth20Constants.DEVICE_INTERVAL, DEVICE_REQUEST_INTERVAL);
-            jsonGenerator.writeEndObject();
-        }
-    }
-
-    private void generateResponseForAccessToken(final HttpServletRequest request, final HttpServletResponse response,
-                                                final OAuthRegisteredService registeredService, final Service service,
-                                                final OAuth20TokenGeneratedResult result, final long timeout,
-                                                final OAuth20ResponseTypes responseType) throws Exception {
-        val accessToken = result.getAccessToken().get();
-        val refreshToken = result.getRefreshToken().get();
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        try (val jsonGenerator = getResponseJsonGenerator(response)) {
-            jsonGenerator.writeStartObject();
-            generateJsonInternal(request, response, jsonGenerator, accessToken,
-                refreshToken, timeout, service, registeredService, responseType);
-            jsonGenerator.writeEndObject();
-        }
+        return generateResponseForAccessToken(request, response, registeredService, service, result, accessTokenTimeout, responseType);
     }
 
     /**
-     * Gets response json generator.
+     * Generate response for device token model and view.
      *
-     * @param response the response
-     * @return the response json generator
-     * @throws IOException the io exception
+     * @param request           the request
+     * @param response          the response
+     * @param registeredService the registered service
+     * @param service           the service
+     * @param result            the result
+     * @param casProperties     the cas properties
+     * @return the model and view
      */
-    protected JsonGenerator getResponseJsonGenerator(final HttpServletResponse response) throws IOException {
-        return JSON_FACTORY.createGenerator(response.getWriter());
+    @SneakyThrows
+    protected ModelAndView generateResponseForDeviceToken(final HttpServletRequest request,
+                                                          final HttpServletResponse response,
+                                                          final OAuthRegisteredService registeredService,
+                                                          final Service service,
+                                                          final OAuth20TokenGeneratedResult result,
+                                                          final CasConfigurationProperties casProperties) {
+        val model = getDeviceTokenResponseModel(result, casProperties);
+        return new ModelAndView(new MappingJackson2JsonView(MAPPER), model);
+    }
+
+    /**
+     * Gets device token response model.
+     *
+     * @param result        the result
+     * @param casProperties the cas properties
+     * @return the device token response model
+     */
+    protected Map getDeviceTokenResponseModel(final OAuth20TokenGeneratedResult result, final CasConfigurationProperties casProperties) {
+        val model = new LinkedHashMap<>();
+        val uri = casProperties.getServer().getPrefix()
+            .concat(OAuth20Constants.BASE_OAUTH20_URL)
+            .concat("/")
+            .concat(OAuth20Constants.DEVICE_AUTHZ_URL);
+        model.put(OAuth20Constants.DEVICE_VERIFICATION_URI, uri);
+        model.put(OAuth20Constants.DEVICE_USER_CODE, result.getUserCode().get());
+        model.put(OAuth20Constants.DEVICE_CODE, result.getDeviceCode().get());
+
+        val deviceRefreshInterval = Beans.newDuration(casProperties.getAuthn().getOauth().getDeviceToken().getRefreshInterval()).getSeconds();
+        model.put(OAuth20Constants.DEVICE_INTERVAL, deviceRefreshInterval);
+        return model;
+    }
+
+    /**
+     * Generate response for access token model and view.
+     *
+     * @param request           the request
+     * @param response          the response
+     * @param registeredService the registered service
+     * @param service           the service
+     * @param result            the result
+     * @param timeout           the timeout
+     * @param responseType      the response type
+     * @return the model and view
+     * @throws Exception the exception
+     */
+    protected ModelAndView generateResponseForAccessToken(final HttpServletRequest request, final HttpServletResponse response,
+                                                          final OAuthRegisteredService registeredService, final Service service,
+                                                          final OAuth20TokenGeneratedResult result, final long timeout,
+                                                          final OAuth20ResponseTypes responseType) throws Exception {
+        val accessToken = result.getAccessToken().get();
+        val refreshToken = result.getRefreshToken().get();
+        val model = getAccessTokenResponseModel(request, response, accessToken, refreshToken, timeout, service, registeredService, responseType);
+        return new ModelAndView(new MappingJackson2JsonView(MAPPER), model);
     }
 
     /**
@@ -116,29 +128,31 @@ public class OAuth20AccessTokenResponseGenerator implements AccessTokenResponseG
      *
      * @param request           the request
      * @param response          the response
-     * @param jsonGenerator     the json generator
      * @param accessTokenId     the access token id
      * @param refreshTokenId    the refresh token id
      * @param timeout           the timeout
      * @param service           the service
      * @param registeredService the registered service
      * @param responseType      the response type
+     * @return the access token response model
      * @throws Exception the exception
      */
-    protected void generateJsonInternal(final HttpServletRequest request,
-                                        final HttpServletResponse response,
-                                        final JsonGenerator jsonGenerator,
-                                        final AccessToken accessTokenId,
-                                        final RefreshToken refreshTokenId,
-                                        final long timeout,
-                                        final Service service,
-                                        final OAuthRegisteredService registeredService,
-                                        final OAuth20ResponseTypes responseType) throws Exception {
-        jsonGenerator.writeStringField(OAuth20Constants.ACCESS_TOKEN, accessTokenId.getId());
-        jsonGenerator.writeStringField(OAuth20Constants.TOKEN_TYPE, OAuth20Constants.TOKEN_TYPE_BEARER);
-        jsonGenerator.writeNumberField(OAuth20Constants.EXPIRES_IN, timeout);
+    protected Map getAccessTokenResponseModel(final HttpServletRequest request,
+                                              final HttpServletResponse response,
+                                              final AccessToken accessTokenId,
+                                              final RefreshToken refreshTokenId,
+                                              final long timeout,
+                                              final Service service,
+                                              final OAuthRegisteredService registeredService,
+                                              final OAuth20ResponseTypes responseType) throws Exception {
+        val model = new LinkedHashMap<>();
+
+        model.put(OAuth20Constants.ACCESS_TOKEN, accessTokenId.getId());
+        model.put(OAuth20Constants.TOKEN_TYPE, OAuth20Constants.TOKEN_TYPE_BEARER);
+        model.put(OAuth20Constants.EXPIRES_IN, timeout);
         if (refreshTokenId != null) {
-            jsonGenerator.writeStringField(OAuth20Constants.REFRESH_TOKEN, refreshTokenId.getId());
+            model.put(OAuth20Constants.REFRESH_TOKEN, refreshTokenId.getId());
         }
+        return model;
     }
 }
