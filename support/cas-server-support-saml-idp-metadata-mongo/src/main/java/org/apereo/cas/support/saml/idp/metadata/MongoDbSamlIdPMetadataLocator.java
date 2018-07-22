1d870ad0f8d26c8bb432b1da98b14d68c42616b2
Misagh Moayyed <mm1844@gmail.com>
2018-07-22 09:32:49 +0430

    allow idp metadata support for mongo [force build]

--- support/cas-server-support-saml-idp-metadata-mongo/src/main/java/org/apereo/cas/support/saml/idp/metadata/MongoDbSamlIdPMetadataLocator.java
+++ support/cas-server-support-saml-idp-metadata-mongo/src/main/java/org/apereo/cas/support/saml/idp/metadata/MongoDbSamlIdPMetadataLocator.java
@@ -2,11 +2,18 @@ package org.apereo.cas.support.saml.idp.metadata;
 
 import org.apereo.cas.CipherExecutor;
 import org.apereo.cas.support.saml.idp.metadata.locator.SamlIdPMetadataLocator;
+import org.apereo.cas.support.saml.services.idp.metadata.SamlIdPMetadataDocument;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * This is {@link MongoDbSamlIdPMetadataLocator}.
@@ -19,40 +26,59 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 public class MongoDbSamlIdPMetadataLocator implements SamlIdPMetadataLocator {
     private final transient MongoTemplate mongoTemplate;
     private final String collectionName;
-    private final CipherExecutor metadataCipherExecutor;
+    private final CipherExecutor<String, String> metadataCipherExecutor;
+
+    private SamlIdPMetadataDocument metadataDocument;
 
     @Override
     public Resource getSigningCertificate() {
-        return null;
+        fetchMetadataDocument();
+        val cert = metadataDocument.getSigningCertificate();
+        return new InputStreamResource(new ByteArrayInputStream(cert.getBytes(StandardCharsets.UTF_8)));
     }
 
     @Override
     public Resource getSigningKey() {
-        return null;
+        fetchMetadataDocument();
+        val data = metadataDocument.getSigningKey();
+        val cert = metadataCipherExecutor.decode(data);
+        return new InputStreamResource(new ByteArrayInputStream(cert.getBytes(StandardCharsets.UTF_8)));
     }
 
     @Override
     public Resource getMetadata() {
-        return null;
+        fetchMetadataDocument();
+        val data = metadataDocument.getMetadata();
+        return new InputStreamResource(new ByteArrayInputStream(data.getBytes(StandardCharsets.UTF_8)));
     }
 
     @Override
     public Resource getEncryptionCertificate() {
-        return null;
+        fetchMetadataDocument();
+        val cert = metadataDocument.getEncryptionCertificate();
+        return new InputStreamResource(new ByteArrayInputStream(cert.getBytes(StandardCharsets.UTF_8)));
     }
 
     @Override
     public Resource getEncryptionKey() {
-        return null;
+        fetchMetadataDocument();
+        val data = metadataDocument.getEncryptionKey();
+        val cert = metadataCipherExecutor.decode(data);
+        return new InputStreamResource(new ByteArrayInputStream(cert.getBytes(StandardCharsets.UTF_8)));
     }
 
     @Override
     public void initialize() {
+        fetchMetadataDocument();
+    }
 
+    private void fetchMetadataDocument() {
+        metadataDocument = this.mongoTemplate.findOne(new Query(), SamlIdPMetadataDocument.class, this.collectionName);
     }
 
     @Override
     public boolean exists() {
-        return false;
+        fetchMetadataDocument();
+        return metadataDocument != null;
     }
 }
