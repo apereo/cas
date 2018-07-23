3b63dd3aaf287b6b198a01b09f5b50449129bfa4
Misagh Moayyed <mm1844@gmail.com>
2018-07-23 16:30:54 +0430

    working on simple mfa provided by cas

---
+++ support/cas-server-support-simple-mfa/src/main/java/org/apereo/cas/config/CasSimpleMultifactorAuthenticationConfiguration.java
@@ -0,0 +1,17 @@
+package org.apereo.cas.config;
+
+import org.apereo.cas.configuration.CasConfigurationProperties;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * This is {@link CasSimpleMultifactorAuthenticationConfiguration}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.0.0
+ */
+@Configuration("casSimpleMultifactorAuthenticationConfiguration")
+@EnableConfigurationProperties(CasConfigurationProperties.class)
+public class CasSimpleMultifactorAuthenticationConfiguration {
+}
