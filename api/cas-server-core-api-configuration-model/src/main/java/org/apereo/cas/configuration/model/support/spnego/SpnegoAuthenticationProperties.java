a23e74ac6fd3a4db843b17cb0ac40654c6de1988
Misagh Moayyed <mmoayyed@unicon.net>
2018-08-03 16:57:08 +0430

    reorganize spnego properties [force build]

---
+++ api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/support/spnego/SpnegoAuthenticationProperties.java
@@ -0,0 +1,66 @@
+package org.apereo.cas.configuration.model.support.spnego;
+
+import org.apereo.cas.configuration.support.RequiresModule;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+/**
+ * This is {@link SpnegoAuthenticationProperties}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.0.0
+ */
+@RequiresModule(name = "cas-server-support-spnego")
+@Getter
+@Setter
+public class SpnegoAuthenticationProperties implements Serializable {
+    private static final long serialVersionUID = 4513529663377430783L;
+    /**
+     * Jcifs Netbios cache policy.
+     */
+    private long cachePolicy = 600;
+
+    /**
+     * The Jcifs domain controller.
+     */
+    private String jcifsDomainController;
+
+    /**
+     * The Jcifs domain.
+     */
+    private String jcifsDomain;
+
+    /**
+     * The Jcifs password.
+     */
+    private String jcifsPassword;
+
+    /**
+     * The Jcifs service password.
+     */
+    private String jcifsServicePassword;
+
+    /**
+     * The Jcifs service principal.
+     */
+    private String jcifsServicePrincipal = "HTTP/cas.example.com@EXAMPLE.COM";
+
+    /**
+     * Spnego JCIFS timeout.
+     */
+    private String timeout = "PT5M";
+
+    /**
+     * The Jcifs netbios wins.
+     */
+    private String jcifsNetbiosWins;
+
+    /**
+     * The Jcifs username.
+     */
+    private String jcifsUsername;
+}
+
