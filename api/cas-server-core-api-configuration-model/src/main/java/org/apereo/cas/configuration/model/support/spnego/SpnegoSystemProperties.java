924a1aaaad134ce5c6673dd5af7cca40b75fa7c5
Misagh Moayyed <mm1844@gmail.com>
2018-08-03 16:57:08 +0430

    reorganize spnego properties [force build]

---
+++ api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/support/spnego/SpnegoSystemProperties.java
@@ -0,0 +1,50 @@
+package org.apereo.cas.configuration.model.support.spnego;
+
+import org.apereo.cas.configuration.support.RequiresModule;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+/**
+ * This is {@link SpnegoSystemProperties}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.0.0
+ */
+@RequiresModule(name = "cas-server-support-spnego")
+@Getter
+@Setter
+public class SpnegoSystemProperties implements Serializable {
+    private static final long serialVersionUID = -7213507143858237596L;
+    /**
+     * The Login conf.
+     */
+    private String loginConf;
+
+    /**
+     * The Kerberos conf.
+     */
+    private String kerberosConf;
+
+    /**
+     * The Kerberos kdc.
+     */
+    private String kerberosKdc = "172.10.1.10";
+
+    /**
+     * The Kerberos realm.
+     */
+    private String kerberosRealm = "EXAMPLE.COM";
+
+    /**
+     * The Kerberos debug.
+     */
+    private String kerberosDebug;
+
+    /**
+     * The Use subject creds only.
+     */
+    private boolean useSubjectCredsOnly;
+}
