cd12a7a7263ab988612bc82f16b1c629d0fe8153
Misagh Moayyed <mmoayyed@unicon.net>
2018-08-04 12:25:24 +0430

    fix checkstyle issue [force build]

--- support/cas-server-support-spnego-webflow/src/test/java/org/apereo/cas/web/flow/AbstractSpnegoTests.java
+++ support/cas-server-support-spnego-webflow/src/test/java/org/apereo/cas/web/flow/AbstractSpnegoTests.java
@@ -29,7 +29,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.webflow.execution.Action;
 
 /**
