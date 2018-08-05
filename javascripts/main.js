9c74f339613d3942880579384aa18f889b152230
Misagh Moayyed <mmoayyed@unicon.net>
2018-08-05 13:28:10 +0430

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -313,8 +313,8 @@ function generateDependencyLangFragments() {
       var gradleId = Math.floor((Math.random() * 10000) + 1);
       
       var tabs = "<ul class='nav nav-pills'> \
-  <li class='nav-item'><a class='nav-link active' data-toggle='tab' href='#maven" + mavenId + "'>Maven</a></li> \
-  <li class='nav-item'><a class='nav-link' data-toggle='tab' href='#gradle" + gradleId + "'>Gradle</a></li> \
+  <li class='nav-item'><a class='nav-link' data-toggle='tab' href='#maven" + mavenId + "'>Maven</a></li> \
+  <li class='nav-item'><a class='nav-link active' data-toggle='tab' href='#gradle" + gradleId + "'>Gradle</a></li> \
   <li role='presentation' class='nav-item dropdown'> \
       <a class='nav-link dropdown-toggle' data-toggle='dropdown' href='#' role='button' aria-haspopup='true' aria-expanded='false'>Resources<span class='caret'></span></a> \
       <div class='dropdown-menu'> \
