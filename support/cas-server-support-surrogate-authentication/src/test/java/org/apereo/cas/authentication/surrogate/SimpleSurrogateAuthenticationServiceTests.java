e35884ed22588821438c989c2760394725a4442a
tduehr <tduehr@users.noreply.github.com>
2018-08-10 01:09:06 -0500

    Clean surrogate (#3404)
    * clean up tests
    
    * clean up test resources

---
+++ support/cas-server-support-surrogate-authentication/src/test/java/org/apereo/cas/authentication/surrogate/SimpleSurrogateAuthenticationServiceTests.java
@@ -0,0 +1,22 @@
+package org.apereo.cas.authentication.surrogate;
+
+import org.apereo.cas.util.CollectionUtils;
+
+import lombok.Getter;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
+
+/**
+ * This is {@link SimpleSurrogateAuthenticationServiceTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.3.0
+ */
+@Getter
+@SpringBootTest(classes = {
+    RefreshAutoConfiguration.class
+})
+public class SimpleSurrogateAuthenticationServiceTests extends BaseSurrogateAuthenticationServiceTests {
+    private final SurrogateAuthenticationService service = new SimpleSurrogateAuthenticationService(
+            CollectionUtils.wrap("casuser", CollectionUtils.wrapList("banderson")), servicesManager);
+}
