2247507734f342ed35a2ddf5663f2ec3bd2b1803
Misagh Moayyed <mmoayyed@unicon.net>
2018-08-14 12:41:53 +0430

    cntd working on uma test cases with authz request

--- support/cas-server-support-oauth-api/src/main/java/org/apereo/cas/support/oauth/OAuth20ClaimTokenFormats.java
+++ support/cas-server-support-oauth-api/src/main/java/org/apereo/cas/support/oauth/OAuth20ClaimTokenFormats.java
@@ -11,7 +11,13 @@ import lombok.Getter;
 @Getter
 public enum OAuth20ClaimTokenFormats {
 
+    /**
+     * JWT token format.
+     */
     JWT("urn:ietf:params:oauth:token-type:jwt"),
+    /**
+     * IDToken token format.
+     */
     IDTOKEN("http://openid.net/specs/openid-connect-core-1_0.html#IDToken");
 
     private final String type;
