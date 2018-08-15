5f998b63b460f5bd37298291751d0680bcae8e8c
Misagh Moayyed <mmoayyed@unicon.net>
2018-08-15 16:43:10 +0430

    move mariadb tests into a separate CI job for parallelism

---
+++ ci/tests/mariadb/run-mariadb-server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# while sleep 9m; do echo -e '\n=====[ Gradle build is still running ]====='; done &
+
+echo "Running MariaDb docker image..."
+docker run --name mariadb -e MYSQL_ROOT_PASSWORD=mypass -d mariadb:10.1
+
+docker ps | grep "mariadb"
+retVal=$?
+if [ $retVal == 0 ]; then
+    echo "MariaDb docker image is running."
+else
+    echo "MariaDb docker image failed to start."
+    exit $retVal
+fi
