f37bfb5d2cf3b4fc375daf6728e46ccceeb5fe2f
Misagh Moayyed <mmoayyed@unicon.net>
2018-08-15 16:33:43 +0430

    move rest-based tests into a separate CI job for parallelism

--- api/cas-server-core-api-test-category/src/test/groovy/org/apereo/cas/category/RestfulApiCategory.groovy
+++ api/cas-server-core-api-test-category/src/test/groovy/org/apereo/cas/category/RestfulApiCategory.groovy
@@ -3,7 +3,7 @@ package org.apereo.cas.category
 /**
  * Category marking tests that need RESTful API and server.
  *
- * @author Misagh
+ * @author Misagh Moayyed
  * @since 6.0.0
  */
 interface RestfulApiCategory {
