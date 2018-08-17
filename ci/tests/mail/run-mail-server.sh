bf024e0aaa71c57fdcac08d9d84d58b99b419b68
Misagh Moayyed <mm1844@gmail.com>
2018-08-17 15:29:55 +0430

    update rel notes; ensure mail server download is successful for tests

--- ci/tests/mail/run-mail-server.sh
+++ ci/tests/mail/run-mail-server.sh
@@ -5,5 +5,10 @@
 echo "Downloading SMTP mail mock server..."
 wget -O MockMock.jar https://github.com/tweakers-dev/MockMock/blob/master/release/MockMock.jar?raw=true
 
+if [ ! -f MockMock.jar ]; then
+    echo "Mock mail server file MockMock.jar could not be downloaded!"
+    exit 1
+fi
+
 echo "Running SMTP mail mock server..."
 java -jar MockMock.jar -p 25000 &>/dev/null &
