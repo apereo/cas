b6902f7631e935f1e112e8205dc480f465b33476
--replace-all <PestytheRat@gmail.com>
2018-08-17 13:18:56 -0600

    Adding the ability to name the saml artifacts cache storage name and the saml attribute query cache

---
+++ api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/support/saml/idp/SamlIdPTicketProperties.java
@@ -0,0 +1,29 @@
+package org.apereo.cas.configuration.model.support.saml.idp;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+/**
+ * This is {@link SamlIdPTicketProperties.java}.
+ *
+ * @author Samuel Lyons
+ * @since 5.3.0
+ */
+@Getter
+@Setter
+public class SamlIdPTicketProperties implements Serializable {
+
+    private static final long serialVersionUID = 9837089259390742073L;
+
+    /**
+     * name that should be given to the saml artifact cache storage name.
+     */
+    private String samlArtifactsCacheStorageName = "samlArtifactsCache";
+
+    /**
+     * The name that should be given to the saml attribute query cache storage name.
+     */
+    private String samlAttributeQueryCacheStorageName = "samlAttributeQueryCache";
+}
