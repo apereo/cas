299756a2201f46994ba2aa9eb155c395d3b3cbe9
Misagh Moayyed <mm1844@gmail.com>
2018-08-17 19:25:49 +0430

    provide actuator endpoints for oauth token management

---
+++ support/cas-server-support-oauth/src/main/java/org/apereo/cas/support/oauth/web/mgmt/OAuth20TokenManagementEndpoint.java
@@ -0,0 +1,83 @@
+package org.apereo.cas.support.oauth.web.mgmt;
+
+import org.apereo.cas.ticket.Ticket;
+import org.apereo.cas.ticket.accesstoken.AccessToken;
+import org.apereo.cas.ticket.refreshtoken.RefreshToken;
+import org.apereo.cas.ticket.registry.TicketRegistry;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.springframework.boot.actuate.endpoint.annotation.DeleteOperation;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.annotation.Selector;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+/**
+ * This is {@link OAuth20TokenManagementEndpoint}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.0.0
+ */
+@Endpoint(id = "oauth-access-tokens", enableByDefault = false)
+@RequiredArgsConstructor
+@Slf4j
+public class OAuth20TokenManagementEndpoint {
+    /**
+     * The Ticket registry.
+     */
+    private final TicketRegistry ticketRegistry;
+
+    /**
+     * Gets access tokens.
+     *
+     * @return the access tokens
+     */
+    @ReadOperation
+    public Collection<Ticket> getTokens() {
+        return ticketRegistry.getTickets(ticket -> (ticket instanceof AccessToken || ticket instanceof RefreshToken) && !ticket.isExpired())
+            .sorted(Comparator.comparing(Ticket::getId))
+            .collect(Collectors.toList());
+    }
+
+
+    /**
+     * Gets access token.
+     *
+     * @param ticketId the ticket id
+     * @return the access token
+     */
+    @ReadOperation
+    public Ticket getToken(@Selector final String ticketId) {
+        var ticket = (Ticket) ticketRegistry.getTicket(ticketId, AccessToken.class);
+        if (ticket == null) {
+            ticket = ticketRegistry.getTicket(ticketId, RefreshToken.class);
+        }
+        if (ticket == null) {
+            LOGGER.debug("Ticket [{}] is not found", ticketId);
+            return null;
+        }
+        if (ticket.isExpired()) {
+            LOGGER.debug("Ticket [{}] is has expired", ticketId);
+            return null;
+        }
+        return ticket;
+    }
+
+    /**
+     * Delete access token.
+     *
+     * @param ticketId the ticket id
+     */
+    @DeleteOperation
+    public void deleteToken(@Selector final String ticketId) {
+        val ticket = getToken(ticketId);
+        if (ticket != null) {
+            ticketRegistry.deleteTicket(ticketId);
+        }
+    }
+}
