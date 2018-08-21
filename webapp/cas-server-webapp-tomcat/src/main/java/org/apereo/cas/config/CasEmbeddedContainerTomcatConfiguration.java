e8559266f5d0839c71e8e965fb91531d3883d9c9
Misagh Moayyed <mm1844@gmail.com>
2018-08-21 19:56:41 +0430

    ensure password mgmt can report back errors on validation

--- webapp/cas-server-webapp-tomcat/src/main/java/org/apereo/cas/config/CasEmbeddedContainerTomcatConfiguration.java
+++ webapp/cas-server-webapp-tomcat/src/main/java/org/apereo/cas/config/CasEmbeddedContainerTomcatConfiguration.java
@@ -5,7 +5,6 @@ import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.configuration.support.Beans;
 import org.apereo.cas.util.ResourceUtils;
 
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.apache.catalina.authenticator.BasicAuthenticator;
@@ -147,13 +146,16 @@ public class CasEmbeddedContainerTomcatConfiguration {
 
             final RewriteValve valve = new RewriteValve() {
                 @Override
-                @SneakyThrows
-                protected synchronized void startInternal() {
-                    super.startInternal();
-                    try (val is = res.getInputStream();
-                         val isr = new InputStreamReader(is, StandardCharsets.UTF_8);
-                         val buffer = new BufferedReader(isr)) {
-                        parse(buffer);
+                public synchronized void startInternal() {
+                    try {
+                        super.startInternal();
+                        try (val is = res.getInputStream();
+                             val isr = new InputStreamReader(is, StandardCharsets.UTF_8);
+                             val buffer = new BufferedReader(isr)) {
+                            parse(buffer);
+                        }
+                    } catch (final Exception e) {
+                        LOGGER.error(e.getMessage(), e);
                     }
                 }
             };
