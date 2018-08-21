7726b10fca5582ae5cebebf40cbacd376fdfcec2
--replace-all <PestytheRat@gmail.com>
2018-08-21 10:55:02 -0600

    added and fixed documentation

--- api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/support/saml/idp/SamlIdPTicketProperties.java
+++ api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/support/saml/idp/SamlIdPTicketProperties.java
@@ -1,16 +1,19 @@
 package org.apereo.cas.configuration.model.support.saml.idp;
 
+import org.apereo.cas.configuration.support.RequiresModule;
+
 import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
 
 /**
- * This is {@link SamlIdPTicketProperties.java}.
+ * This is {@link SamlIdPTicketProperties}.
  *
  * @author Samuel Lyons
  * @since 5.3.0
  */
+@RequiresModule(name = "cas-server-support-saml-idp")
 @Getter
 @Setter
 public class SamlIdPTicketProperties implements Serializable {
