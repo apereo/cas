7c0cd177c4336b7c28decbc441915a2c8a0ab892
Misagh Moayyed <mm1844@gmail.com>
2018-08-24 22:11:10 +0430

    add session management via jdbc; fix startup errors

---
+++ support/cas-server-support-jdbc-drivers/src/main/java/org/apereo/cas/config/ConditionalDataSourceConfiguration.java
@@ -0,0 +1,18 @@
+package org.apereo.cas.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * This is {@link ConditionalDataSourceConfiguration}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.0.0
+ */
+@ConditionalOnProperty(name = "spring.datasource.url")
+@Configuration("conditionalDataSourceConfiguration")
+@Import(DataSourceAutoConfiguration.class)
+public class ConditionalDataSourceConfiguration {
+}
