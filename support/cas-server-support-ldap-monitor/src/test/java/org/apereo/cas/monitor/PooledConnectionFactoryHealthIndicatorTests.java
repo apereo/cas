f6a97c92eadb76c8b95b149b2935afc8d458a2e0
Misagh Moayyed <mm1844@gmail.com>
2018-08-27 21:37:54 +0430

    clean up ldap tests

--- support/cas-server-support-ldap-monitor/src/test/java/org/apereo/cas/monitor/PooledConnectionFactoryHealthIndicatorTests.java
+++ support/cas-server-support-ldap-monitor/src/test/java/org/apereo/cas/monitor/PooledConnectionFactoryHealthIndicatorTests.java
@@ -1,11 +1,16 @@
 package org.apereo.cas.monitor;
 
+import org.apereo.cas.category.LdapCategory;
 import org.apereo.cas.config.CasCoreUtilConfiguration;
 import org.apereo.cas.monitor.config.LdapMonitorConfiguration;
+import org.apereo.cas.util.junit.ConditionalIgnore;
+import org.apereo.cas.util.junit.ConditionalIgnoreRule;
+import org.apereo.cas.util.junit.RunningContinuousIntegrationCondition;
 
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.actuate.health.HealthIndicator;
@@ -24,14 +29,24 @@ import static org.junit.Assert.*;
  * @author Marvin S. Addison
  * @since 4.0.0
  */
-@SpringBootTest(classes = {LdapMonitorConfiguration.class,
+@SpringBootTest(classes = {
+    LdapMonitorConfiguration.class,
     CasCoreUtilConfiguration.class,
-    RefreshAutoConfiguration.class})
-@TestPropertySource(locations = {"classpath:/ldapmonitor.properties"})
+    RefreshAutoConfiguration.class
+})
+@TestPropertySource(properties = {
+    "cas.monitor.ldap.ldapUrl=ldap://localhost:10389",
+    "cas.monitor.ldap.useSsl=false"
+})
+@Category(LdapCategory.class)
+@ConditionalIgnore(condition = RunningContinuousIntegrationCondition.class)
 public class PooledConnectionFactoryHealthIndicatorTests {
     @ClassRule
     public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
 
+    @Rule
+    public final ConditionalIgnoreRule conditionalIgnoreRule = new ConditionalIgnoreRule();
+    
     @Rule
     public final SpringMethodRule springMethodRule = new SpringMethodRule();
 
