73c2f33267055a854a1c2d4b00992f14ae664dda
Misagh Moayyed <mm1844@gmail.com>
2018-08-29 14:16:58 +0430

    fix logger placeholders

--- core/cas-server-core-authentication-api/src/main/java/org/apereo/cas/authentication/adaptive/DefaultAdaptiveAuthenticationPolicy.java
+++ core/cas-server-core-authentication-api/src/main/java/org/apereo/cas/authentication/adaptive/DefaultAdaptiveAuthenticationPolicy.java
@@ -84,7 +84,7 @@ public class DefaultAdaptiveAuthenticationPolicy implements AdaptiveAuthenticati
         if (ipResult.isRanked()) {
             val threshold = adaptiveAuthenticationProperties.getRisk().getThreshold();
             if (ipResult.getScore() >= threshold) {
-                LOGGER.warn("Client IP [{}] is rejected for authentication because intelligence score [{}] is higher than the configured risk threshold",
+                LOGGER.warn("Client IP [{}] is rejected for authentication because intelligence score [{}] is higher than the configured risk threshold [{}]",
                     clientIp, ipResult.getScore(), threshold);
                 return true;
             }
