447a6a06464e0e0530db6e330e0e9a799fa53b55
Misagh Moayyed <mm1844@gmail.com>
2018-09-04 14:14:41 +0430

    fix tests; upgrade junit

--- support/cas-server-support-actions/src/main/java/org/apereo/cas/web/flow/login/SetServiceUnauthorizedRedirectUrlAction.java
+++ support/cas-server-support-actions/src/main/java/org/apereo/cas/web/flow/login/SetServiceUnauthorizedRedirectUrlAction.java
@@ -1,6 +1,5 @@
 package org.apereo.cas.web.flow.login;
 
-import org.apereo.cas.services.RegisteredService;
 import org.apereo.cas.services.ServicesManager;
 import org.apereo.cas.web.support.WebUtils;
 
