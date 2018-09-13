f41ec13e38e0291ed73475546acf243485f75365
Misagh Moayyed <mm1844@gmail.com>
2018-09-13 09:27:29 +0200

    introduce princial attribute id per delegated client

--- support/cas-server-support-pac4j-core-clients/src/main/java/org/apereo/cas/support/pac4j/authentication/handler/support/ClientAuthenticationHandler.java
+++ support/cas-server-support-pac4j-core-clients/src/main/java/org/apereo/cas/support/pac4j/authentication/handler/support/ClientAuthenticationHandler.java
@@ -12,6 +12,7 @@ import org.apereo.cas.web.support.WebUtils;
 
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+import org.pac4j.core.client.BaseClient;
 import org.pac4j.core.client.Clients;
 import org.pac4j.core.exception.HttpAction;
 
@@ -51,17 +52,20 @@ public class ClientAuthenticationHandler extends AbstractPac4jAuthenticationHand
             val credentials = clientCredentials.getCredentials();
             LOGGER.debug("Client name: [{}]", clientCredentials.getClientName());
 
-            // get client
-            val client = this.clients.findClient(clientCredentials.getClientName());
+            val client = (BaseClient) this.clients.findClient(clientCredentials.getClientName());
             LOGGER.debug("Delegated client is: [{}]", client);
 
+            if (client == null) {
+                throw new IllegalArgumentException("Unable to determine client based on client name " + clientCredentials.getClientName());
+            }
+            
             val request = WebUtils.getHttpServletRequestFromExternalWebflowContext();
             val response = WebUtils.getHttpServletResponseFromExternalWebflowContext();
             val webContext = Pac4jUtils.getPac4jJ2EContext(request, response);
 
             val userProfile = client.getUserProfile(credentials, webContext);
             LOGGER.debug("Final user profile is: [{}]", userProfile);
-            return createResult(clientCredentials, userProfile);
+            return createResult(clientCredentials, userProfile, client);
         } catch (final HttpAction e) {
             throw new PreventedException(e);
         }
