da50d517205cf4e0c9cd9fe1f6881677b25245f2
tduehr <tduehr@users.noreply.github.com>
2018-09-15 10:00:12 -0500

    fix style (#3522)
    * fix style
    
    * fix ldap test

--- core/cas-server-core-authentication-mfa/src/main/java/org/apereo/cas/authentication/AbstractMultifactorAuthenticationProviderBypass.java
+++ core/cas-server-core-authentication-mfa/src/main/java/org/apereo/cas/authentication/AbstractMultifactorAuthenticationProviderBypass.java
@@ -1,9 +1,10 @@
 package org.apereo.cas.authentication;
 
-import lombok.extern.slf4j.Slf4j;
 import org.apereo.cas.authentication.principal.Principal;
 import org.apereo.cas.configuration.model.support.mfa.MultifactorAuthenticationProviderBypassProperties;
 import org.apereo.cas.services.MultifactorAuthenticationProvider;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * This is base for implementations of MultifactorAuthenticationProviderBypass.
