b07c526e2c91d5f77283e6a929c1431a177997f6
tduehr <tduehr@users.noreply.github.com>
2018-09-17 01:16:24 -0500

    Clean audit (#3464)
    * base tests
    
    * jdbc
    
    * mongo
    
    * rest
    
    * couchbase

--- support/cas-server-support-audit-mongo/src/test/java/org/apereo/cas/audit/MongoDbAuditTrailManagerTests.java
+++ support/cas-server-support-audit-mongo/src/test/java/org/apereo/cas/audit/MongoDbAuditTrailManagerTests.java
@@ -1,30 +1,21 @@
 package org.apereo.cas.audit;
 
+import org.apereo.cas.audit.spi.BaseAuditConfigurationTests;
 import org.apereo.cas.audit.spi.config.CasCoreAuditConfiguration;
 import org.apereo.cas.category.MongoDbCategory;
 import org.apereo.cas.config.CasCoreUtilConfiguration;
 import org.apereo.cas.config.CasCoreWebConfiguration;
 import org.apereo.cas.config.CasSupportMongoDbAuditConfiguration;
 import org.apereo.cas.config.support.CasWebApplicationServiceFactoryConfiguration;
-import org.apereo.cas.util.DateTimeUtils;
 
-import lombok.val;
-import org.apereo.inspektr.audit.AuditActionContext;
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.Test;
+import lombok.Getter;
+import org.apereo.inspektr.audit.AuditTrailManager;
 import org.junit.experimental.categories.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.rules.SpringClassRule;
-import org.springframework.test.context.junit4.rules.SpringMethodRule;
-
-import java.time.LocalDate;
-
-import static org.junit.Assert.*;
 
 /**
  * This is {@link MongoDbAuditTrailManagerTests}.
@@ -51,28 +42,10 @@ import static org.junit.Assert.*;
     "cas.audit.mongo.authenticationDatabaseName=admin"
 })
 @Category(MongoDbCategory.class)
-public class MongoDbAuditTrailManagerTests {
-
-    @ClassRule
-    public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
-
-    @Rule
-    public final SpringMethodRule springMethodRule = new SpringMethodRule();
+@Getter
+public class MongoDbAuditTrailManagerTests extends BaseAuditConfigurationTests {
 
     @Autowired
-    @Qualifier("auditTrailExecutionPlan")
-    private AuditTrailExecutionPlan auditTrailExecutionPlan;
-
-    @Test
-    public void verify() {
-        val twoDaysAgo = LocalDate.now().minusDays(2);
-        val since = DateTimeUtils.dateOf(twoDaysAgo);
-        val ctx = new AuditActionContext("casuser", "resource",
-            "action", "appcode", since, "clientIp",
-            "serverIp");
-        auditTrailExecutionPlan.record(ctx);
-
-        val results = auditTrailExecutionPlan.getAuditRecordsSince(twoDaysAgo);
-        assertFalse(results.isEmpty());
-    }
+    @Qualifier("mongoDbAuditTrailManager")
+    private AuditTrailManager auditTrailManager;
 }
