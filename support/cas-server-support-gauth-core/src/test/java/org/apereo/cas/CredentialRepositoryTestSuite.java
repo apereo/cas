cc5b4fe4564549a64bea622da5edc069e8b0e6f8
Timur Duehr <tduehr@gmail.com>
2018-09-19 15:44:48 -0500

    clean up gauth

---
+++ support/cas-server-support-gauth-core/src/test/java/org/apereo/cas/CredentialRepositoryTestSuite.java
@@ -0,0 +1,23 @@
+package org.apereo.cas;
+
+import org.apereo.cas.gauth.credential.InMemoryGoogleAuthenticatorTokenCredentialRepositoryTests;
+import org.apereo.cas.gauth.credential.JsonGoogleAuthenticatorTokenCredentialRepositoryTests;
+import org.apereo.cas.gauth.credential.RestGoogleAuthenticatorTokenCredentialRepositoryTests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * This is {@link CredentialRepositoryTestSuite}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.3.0
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    InMemoryGoogleAuthenticatorTokenCredentialRepositoryTests.class,
+    JsonGoogleAuthenticatorTokenCredentialRepositoryTests.class,
+    RestGoogleAuthenticatorTokenCredentialRepositoryTests.class
+})
+public class CredentialRepositoryTestSuite {
+}
---
+++ support/cas-server-support-gauth-jpa/src/test/java/org/apereo/cas/gauth/credential/JpaGoogleAuthenticatorTokenCredentialRepositoryTests.java
@@ -0,0 +1,103 @@
+package org.apereo.cas.gauth.credential;
+
+import org.apereo.cas.config.CasCoreAuthenticationConfiguration;
+import org.apereo.cas.config.CasCoreAuthenticationHandlersConfiguration;
+import org.apereo.cas.config.CasCoreAuthenticationMetadataConfiguration;
+import org.apereo.cas.config.CasCoreAuthenticationPolicyConfiguration;
+import org.apereo.cas.config.CasCoreAuthenticationPrincipalConfiguration;
+import org.apereo.cas.config.CasCoreAuthenticationServiceSelectionStrategyConfiguration;
+import org.apereo.cas.config.CasCoreAuthenticationSupportConfiguration;
+import org.apereo.cas.config.CasCoreConfiguration;
+import org.apereo.cas.config.CasCoreHttpConfiguration;
+import org.apereo.cas.config.CasCoreServicesAuthenticationConfiguration;
+import org.apereo.cas.config.CasCoreServicesConfiguration;
+import org.apereo.cas.config.CasCoreTicketCatalogConfiguration;
+import org.apereo.cas.config.CasCoreTicketsConfiguration;
+import org.apereo.cas.config.CasCoreUtilConfiguration;
+import org.apereo.cas.config.CasCoreWebConfiguration;
+import org.apereo.cas.config.CasPersonDirectoryConfiguration;
+import org.apereo.cas.config.GoogleAuthenticatorJpaConfiguration;
+import org.apereo.cas.config.support.CasWebApplicationServiceFactoryConfiguration;
+import org.apereo.cas.config.support.EnvironmentConversionServiceInitializer;
+import org.apereo.cas.config.support.authentication.GoogleAuthenticatorAuthenticationEventExecutionPlanConfiguration;
+import org.apereo.cas.logout.config.CasCoreLogoutConfiguration;
+import org.apereo.cas.otp.repository.credentials.OneTimeTokenCredentialRepository;
+import org.apereo.cas.util.SchedulingUtils;
+
+import lombok.Getter;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * Test cases for {@link JpaGoogleAuthenticatorTokenCredentialRepository}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.0.0
+ */
+@SpringBootTest(classes = {
+    GoogleAuthenticatorJpaConfiguration.class,
+    JpaGoogleAuthenticatorTokenCredentialRepositoryTests.JpaTestConfiguration.class,
+    GoogleAuthenticatorAuthenticationEventExecutionPlanConfiguration.class,
+    CasCoreTicketsConfiguration.class,
+    CasCoreTicketCatalogConfiguration.class,
+    CasCoreLogoutConfiguration.class,
+    CasCoreHttpConfiguration.class,
+    CasCoreAuthenticationConfiguration.class,
+    CasCoreServicesAuthenticationConfiguration.class,
+    CasCoreAuthenticationMetadataConfiguration.class,
+    CasCoreAuthenticationPolicyConfiguration.class,
+    CasCoreAuthenticationPrincipalConfiguration.class,
+    CasCoreAuthenticationHandlersConfiguration.class,
+    CasCoreAuthenticationSupportConfiguration.class,
+    CasPersonDirectoryConfiguration.class,
+    CasCoreServicesConfiguration.class,
+    CasWebApplicationServiceFactoryConfiguration.class,
+    CasCoreUtilConfiguration.class,
+    CasCoreConfiguration.class,
+    CasCoreAuthenticationServiceSelectionStrategyConfiguration.class,
+    AopAutoConfiguration.class,
+    RefreshAutoConfiguration.class,
+    CasCoreWebConfiguration.class},
+    properties = "cas.authn.mfa.gauth.crypto.enabled=false")
+@EnableTransactionManagement(proxyTargetClass = true)
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableScheduling
+@ContextConfiguration(initializers = EnvironmentConversionServiceInitializer.class)
+@Getter
+public class JpaGoogleAuthenticatorTokenCredentialRepositoryTests extends BaseOneTimeTokenCredentialRepositoryTests {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Autowired(required = false)
+    @Qualifier("googleAuthenticatorAccountRegistry")
+    private OneTimeTokenCredentialRepository registry;
+
+    @Before
+    public void cleanUp() {
+        this.getRegistry().deleteAll();
+    }
+
+    @TestConfiguration
+    public static class JpaTestConfiguration implements InitializingBean {
+        @Autowired
+        protected ApplicationContext applicationContext;
+
+        @Override
+        public void afterPropertiesSet() {
+            SchedulingUtils.prepScheduledAnnotationBeanPostProcessor(applicationContext);
+        }
+    }
+}
