b80299c9541c49c4afe889c319f86375b74d2197
tduehr <tduehr@users.noreply.github.com>
2018-09-20 22:59:02 -0500

    correct spelling (#3537)

--- core/cas-server-core-audit-api/src/main/java/org/apereo/cas/audit/spi/AbstractAuditTrailManager.java
+++ core/cas-server-core-audit-api/src/main/java/org/apereo/cas/audit/spi/AbstractAuditTrailManager.java
@@ -25,7 +25,7 @@ import java.util.concurrent.Executors;
 public abstract class AbstractAuditTrailManager implements AuditTrailManager {
 
     /**
-     * Save records asyncronously.
+     * Save records asynchronously.
      */
     protected boolean asynchronous;
 
