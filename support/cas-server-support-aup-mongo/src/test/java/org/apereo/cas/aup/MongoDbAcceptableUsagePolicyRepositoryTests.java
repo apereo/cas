266ea07b2db3e0d3d89e5815fdce936043a136d7
tduehr <tduehr@users.noreply.github.com>
2018-09-22 12:54:45 -0500

    Clean aup (#3469)
    * base
    
    * jdbc
    
    * ldap
    
    * mongo
    
    * default

--- support/cas-server-support-aup-mongo/src/test/java/org/apereo/cas/aup/MongoDbAcceptableUsagePolicyRepositoryTests.java
+++ support/cas-server-support-aup-mongo/src/test/java/org/apereo/cas/aup/MongoDbAcceptableUsagePolicyRepositoryTests.java
@@ -1,6 +1,5 @@
 package org.apereo.cas.aup;
 
-import org.apereo.cas.authentication.CoreAuthenticationTestUtils;
 import org.apereo.cas.category.MongoDbCategory;
 import org.apereo.cas.config.CasAcceptableUsagePolicyMongoDbConfiguration;
 import org.apereo.cas.config.CasAuthenticationEventExecutionPlanTestConfiguration;
@@ -19,33 +18,15 @@ import org.apereo.cas.config.CasPersonDirectoryTestConfiguration;
 import org.apereo.cas.config.CasRegisteredServicesTestConfiguration;
 import org.apereo.cas.config.support.CasWebApplicationServiceFactoryConfiguration;
 import org.apereo.cas.logout.config.CasCoreLogoutConfiguration;
-import org.apereo.cas.mock.MockTicketGrantingTicket;
-import org.apereo.cas.ticket.registry.TicketRegistry;
-import org.apereo.cas.util.CollectionUtils;
 import org.apereo.cas.util.junit.ConditionalIgnore;
 import org.apereo.cas.util.junit.ConditionalIgnoreRule;
 import org.apereo.cas.util.junit.RunningContinuousIntegrationCondition;
-import org.apereo.cas.web.support.WebUtils;
 
-import lombok.val;
-import org.junit.ClassRule;
 import org.junit.Rule;
-import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.rules.SpringClassRule;
-import org.springframework.test.context.junit4.rules.SpringMethodRule;
-import org.springframework.webflow.context.servlet.ServletExternalContext;
-import org.springframework.webflow.test.MockRequestContext;
-
-import static org.junit.Assert.*;
 
 /**
  * This is {@link MongoDbAcceptableUsagePolicyRepositoryTests}.
@@ -87,36 +68,7 @@ import static org.junit.Assert.*;
     "cas.acceptableUsagePolicy.aupAttributeName=accepted"
     }
 )
-public class MongoDbAcceptableUsagePolicyRepositoryTests {
-    @ClassRule
-    public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
-
-    @Rule
-    public final SpringMethodRule springMethodRule = new SpringMethodRule();
-
+public class MongoDbAcceptableUsagePolicyRepositoryTests extends BaseAcceptableUsagePolicyRepositoryTests {
     @Rule
     public final ConditionalIgnoreRule conditionalIgnoreRule = new ConditionalIgnoreRule();
-
-    @Autowired
-    @Qualifier("acceptableUsagePolicyRepository")
-    private AcceptableUsagePolicyRepository acceptableUsagePolicyRepository;
-
-    @Autowired
-    @Qualifier("ticketRegistry")
-    private TicketRegistry ticketRegistry;
-
-    @Test
-    public void verifyAction() {
-        val context = new MockRequestContext();
-        val request = new MockHttpServletRequest();
-        context.setExternalContext(new ServletExternalContext(new MockServletContext(), request, new MockHttpServletResponse()));
-
-        val c = CoreAuthenticationTestUtils.getCredentialsWithSameUsernameAndPassword("casaup");
-        val tgt = new MockTicketGrantingTicket("casaup", c, CollectionUtils.wrap("accepted", "false"));
-        ticketRegistry.addTicket(tgt);
-        WebUtils.putTicketGrantingTicketInScopes(context, tgt);
-
-        assertFalse(acceptableUsagePolicyRepository.verify(context, c).getLeft());
-        assertTrue(acceptableUsagePolicyRepository.submit(context, c));
-    }
 }
