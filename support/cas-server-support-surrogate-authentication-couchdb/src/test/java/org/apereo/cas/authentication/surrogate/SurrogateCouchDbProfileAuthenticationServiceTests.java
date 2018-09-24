0812aee3f59c53b9869cb7380c66c9bf94c3dbb8
tduehr <tduehr@users.noreply.github.com>
2018-09-24 02:37:44 -0500

    surrogate support (#3526)

---
+++ support/cas-server-support-surrogate-authentication-couchdb/src/test/java/org/apereo/cas/authentication/surrogate/SurrogateCouchDbProfileAuthenticationServiceTests.java
@@ -0,0 +1,118 @@
+package org.apereo.cas.authentication.surrogate;
+
+import org.apereo.cas.category.CouchDbCategory;
+import org.apereo.cas.config.CasCoreAuthenticationConfiguration;
+import org.apereo.cas.config.CasCoreAuthenticationHandlersConfiguration;
+import org.apereo.cas.config.CasCoreAuthenticationMetadataConfiguration;
+import org.apereo.cas.config.CasCoreAuthenticationPolicyConfiguration;
+import org.apereo.cas.config.CasCoreAuthenticationPrincipalConfiguration;
+import org.apereo.cas.config.CasCoreAuthenticationServiceSelectionStrategyConfiguration;
+import org.apereo.cas.config.CasCoreAuthenticationSupportConfiguration;
+import org.apereo.cas.config.CasCoreConfiguration;
+import org.apereo.cas.config.CasCoreHttpConfiguration;
+import org.apereo.cas.config.CasCoreServicesAuthenticationConfiguration;
+import org.apereo.cas.config.CasCoreServicesConfiguration;
+import org.apereo.cas.config.CasCoreTicketCatalogConfiguration;
+import org.apereo.cas.config.CasCoreTicketsConfiguration;
+import org.apereo.cas.config.CasCoreUtilConfiguration;
+import org.apereo.cas.config.CasCoreWebConfiguration;
+import org.apereo.cas.config.CasCouchDbCoreConfiguration;
+import org.apereo.cas.config.CasPersonDirectoryTestConfiguration;
+import org.apereo.cas.config.SurrogateAuthenticationAuditConfiguration;
+import org.apereo.cas.config.SurrogateAuthenticationConfiguration;
+import org.apereo.cas.config.SurrogateAuthenticationMetadataConfiguration;
+import org.apereo.cas.config.SurrogateCouchDbAuthenticationServiceConfiguration;
+import org.apereo.cas.config.support.CasWebApplicationServiceFactoryConfiguration;
+import org.apereo.cas.couchdb.core.CouchDbConnectorFactory;
+import org.apereo.cas.couchdb.core.CouchDbProfileDocument;
+import org.apereo.cas.couchdb.core.ProfileCouchDbRepository;
+import org.apereo.cas.logout.config.CasCoreLogoutConfiguration;
+import org.apereo.cas.services.web.config.CasThemesConfiguration;
+import org.apereo.cas.util.CollectionUtils;
+import org.apereo.cas.web.config.CasCookieConfiguration;
+import org.apereo.cas.web.flow.config.CasCoreWebflowConfiguration;
+import org.apereo.cas.web.flow.config.CasWebflowContextConfiguration;
+
+import lombok.Getter;
+import lombok.val;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.experimental.categories.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
+
+/**
+ * This is {@link SurrogateCouchDbProfileAuthenticationServiceTests}.
+ *
+ * @author Timur Duehr
+ * @since 6.0.0
+ */
+@Category(CouchDbCategory.class)
+@SpringBootTest(classes = {
+    RefreshAutoConfiguration.class,
+    CasCouchDbCoreConfiguration.class,
+    SurrogateCouchDbAuthenticationServiceConfiguration.class,
+    SurrogateAuthenticationConfiguration.class,
+    SurrogateAuthenticationAuditConfiguration.class,
+    SurrogateAuthenticationMetadataConfiguration.class,
+    CasCoreAuthenticationPrincipalConfiguration.class,
+    CasCoreAuthenticationPolicyConfiguration.class,
+    CasCoreAuthenticationMetadataConfiguration.class,
+    CasCoreAuthenticationSupportConfiguration.class,
+    CasCoreAuthenticationHandlersConfiguration.class,
+    CasWebApplicationServiceFactoryConfiguration.class,
+    CasCoreHttpConfiguration.class,
+    CasCoreUtilConfiguration.class,
+    CasCoreTicketCatalogConfiguration.class,
+    CasCoreTicketsConfiguration.class,
+    CasPersonDirectoryTestConfiguration.class,
+    CasCoreAuthenticationConfiguration.class,
+    CasCoreWebConfiguration.class,
+    CasWebApplicationServiceFactoryConfiguration.class,
+    CasCoreServicesAuthenticationConfiguration.class,
+    CasCoreServicesConfiguration.class,
+    CasCoreWebflowConfiguration.class,
+    CasWebflowContextConfiguration.class,
+    CasCoreConfiguration.class,
+    CasCoreLogoutConfiguration.class,
+    CasCookieConfiguration.class,
+    CasThemesConfiguration.class,
+    CasCoreAuthenticationServiceSelectionStrategyConfiguration.class
+    }, properties = {
+    "cas.authn.surrogate.couchDb.profileBased=true",
+    "cas.authn.surrogate.couchDb.surrogatePrincipalsAttribute=surrogateFor",
+    "org.ektorp.support.AutoUpdateViewOnChange=true"
+})
+@Getter
+public class SurrogateCouchDbProfileAuthenticationServiceTests extends BaseSurrogateAuthenticationServiceTests {
+
+    @Autowired
+    @Qualifier("surrogateAuthenticationService")
+    private SurrogateAuthenticationService service;
+
+    @Autowired
+    @Qualifier("surrogateCouchDbFactory")
+    private CouchDbConnectorFactory couchDbFactory;
+
+    @Autowired
+    @Qualifier("surrogateAuthorizationProfileCouchDbRepository")
+    private ProfileCouchDbRepository repository;
+
+    @Before
+    public void setUp() {
+        couchDbFactory.getCouchDbInstance().createDatabaseIfNotExists(couchDbFactory.getCouchDbConnector().getDatabaseName());
+        repository.initStandardDesignDocument();
+
+        val profile = new CouchDbProfileDocument();
+        profile.setUsername("casuser");
+        profile.setAttribute("surrogateFor", CollectionUtils.wrapList("banderson"));
+        repository.add(profile);
+    }
+
+    @After
+    public void tearDown() {
+        couchDbFactory.getCouchDbInstance().deleteDatabase(couchDbFactory.getCouchDbConnector().getDatabaseName());
+    }
+}
