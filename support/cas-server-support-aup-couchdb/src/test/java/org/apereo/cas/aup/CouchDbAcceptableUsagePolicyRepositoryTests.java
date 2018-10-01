76d32832985613358d10332184a3b947a160a67d
Misagh Moayyed <mm1844@gmail.com>
2018-10-01 18:51:56 +0330

    fix aup tests

--- support/cas-server-support-aup-couchdb/src/test/java/org/apereo/cas/aup/CouchDbAcceptableUsagePolicyRepositoryTests.java
+++ support/cas-server-support-aup-couchdb/src/test/java/org/apereo/cas/aup/CouchDbAcceptableUsagePolicyRepositoryTests.java
@@ -2,25 +2,11 @@ package org.apereo.cas.aup;
 
 import org.apereo.cas.category.CouchDbCategory;
 import org.apereo.cas.config.CasAcceptableUsagePolicyCouchDbConfiguration;
-import org.apereo.cas.config.CasAuthenticationEventExecutionPlanTestConfiguration;
-import org.apereo.cas.config.CasCoreAuthenticationPrincipalConfiguration;
-import org.apereo.cas.config.CasCoreAuthenticationServiceSelectionStrategyConfiguration;
-import org.apereo.cas.config.CasCoreConfiguration;
-import org.apereo.cas.config.CasCoreHttpConfiguration;
-import org.apereo.cas.config.CasCoreServicesConfiguration;
-import org.apereo.cas.config.CasCoreTicketCatalogConfiguration;
-import org.apereo.cas.config.CasCoreTicketIdGeneratorsConfiguration;
-import org.apereo.cas.config.CasCoreTicketsConfiguration;
-import org.apereo.cas.config.CasCoreUtilConfiguration;
-import org.apereo.cas.config.CasCoreWebConfiguration;
 import org.apereo.cas.config.CasCouchDbCoreConfiguration;
-import org.apereo.cas.config.CasDefaultServiceTicketIdGeneratorsConfiguration;
-import org.apereo.cas.config.CasPersonDirectoryTestConfiguration;
-import org.apereo.cas.config.CasRegisteredServicesTestConfiguration;
-import org.apereo.cas.config.support.CasWebApplicationServiceFactoryConfiguration;
 import org.apereo.cas.couchdb.core.CouchDbConnectorFactory;
 import org.apereo.cas.couchdb.core.ProfileCouchDbRepository;
-import org.apereo.cas.logout.config.CasCoreLogoutConfiguration;
+import org.apereo.cas.util.junit.ConditionalIgnore;
+import org.apereo.cas.util.junit.RunningContinuousIntegrationCondition;
 
 import lombok.Getter;
 import org.junit.After;
@@ -28,8 +14,8 @@ import org.junit.Before;
 import org.junit.experimental.categories.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
 
 /**
  * This is {@link CouchDbAcceptableUsagePolicyRepositoryTests}.
@@ -37,29 +23,10 @@ import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
  * @author Timur Duehr
  * @since 6.0.0
  */
-@SpringBootTest(classes = {
-    CasAcceptableUsagePolicyCouchDbConfiguration.class,
-    RefreshAutoConfiguration.class,
-    CasCoreConfiguration.class,
-    CasCouchDbCoreConfiguration.class,
-    CasCoreTicketsConfiguration.class,
-    CasCoreLogoutConfiguration.class,
-    CasCoreServicesConfiguration.class,
-    CasCoreTicketIdGeneratorsConfiguration.class,
-    CasCoreTicketCatalogConfiguration.class,
-    CasCoreAuthenticationServiceSelectionStrategyConfiguration.class,
-    CasCoreHttpConfiguration.class,
-    CasCoreWebConfiguration.class,
-    CasPersonDirectoryTestConfiguration.class,
-    CasCoreUtilConfiguration.class,
-    CasRegisteredServicesTestConfiguration.class,
-    CasWebApplicationServiceFactoryConfiguration.class,
-    CasAuthenticationEventExecutionPlanTestConfiguration.class,
-    CasDefaultServiceTicketIdGeneratorsConfiguration.class,
-    CasCoreAuthenticationPrincipalConfiguration.class},
-    properties = {"cas.acceptableUsagePolicy.couchDb.asynchronous=false"})
+@Import({CasCouchDbCoreConfiguration.class, CasAcceptableUsagePolicyCouchDbConfiguration.class})
+@TestPropertySource(properties = "cas.acceptableUsagePolicy.couchDb.asynchronous=false")
 @Category(CouchDbCategory.class)
-
+@ConditionalIgnore(condition = RunningContinuousIntegrationCondition.class)
 @Getter
 public class CouchDbAcceptableUsagePolicyRepositoryTests extends BaseAcceptableUsagePolicyRepositoryTests {
 
