06dfec407aa892c558af33458d27ff494014018e
Travis Schmidt <tsschmidt@ucdavis.edu>
2018-10-04 07:41:58 -0700

    Add mfa credential interface

--- support/cas-server-support-duo-core/src/main/java/org/apereo/cas/adaptors/duo/web/flow/action/DuoDirectAuthenticationAction.java
+++ support/cas-server-support-duo-core/src/main/java/org/apereo/cas/adaptors/duo/web/flow/action/DuoDirectAuthenticationAction.java
@@ -2,8 +2,9 @@ package org.apereo.cas.adaptors.duo.web.flow.action;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apereo.cas.adaptors.duo.authn.DuoDirectCredential;
+import org.apereo.cas.adaptors.duo.authn.DuoMultifactorAuthenticationProvider;
+import org.apereo.cas.web.flow.mfa.AbstractMultifactorAuthenticationAction;
 import org.apereo.cas.web.support.WebUtils;
-import org.springframework.webflow.action.AbstractAction;
 import org.springframework.webflow.execution.Event;
 import org.springframework.webflow.execution.RequestContext;
 
@@ -14,11 +15,11 @@ import org.springframework.webflow.execution.RequestContext;
  * @since 5.0.0
  */
 @Slf4j
-public class DuoDirectAuthenticationAction extends AbstractAction {
+public class DuoDirectAuthenticationAction extends AbstractMultifactorAuthenticationAction<DuoMultifactorAuthenticationProvider> {
     
     @Override
     protected Event doExecute(final RequestContext requestContext) {
-        final DuoDirectCredential c = new DuoDirectCredential(WebUtils.getAuthentication(requestContext));
+        final DuoDirectCredential c = new DuoDirectCredential(WebUtils.getAuthentication(requestContext), provider.createUniqueId());
         WebUtils.putCredential(requestContext, c);
         return success();
     }
