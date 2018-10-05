65bac997376bdf6f7121f9321c4bbf2a0cce21f9
Travis Schmidt <tsschmidt@ucdavis.edu>
2018-10-05 10:19:27 -0700

    Fix codacy issue

--- api/cas-server-core-api-mfa/src/main/java/org/apereo/cas/authentication/MultifactorAuthenticationCredential.java
+++ api/cas-server-core-api-mfa/src/main/java/org/apereo/cas/authentication/MultifactorAuthenticationCredential.java
@@ -23,6 +23,7 @@ public interface MultifactorAuthenticationCredential {
      * @param providerId - the id
      */
     default void setProviderId(final String providerId) {
+        // Sets the id of the provider
     }
 
 }
