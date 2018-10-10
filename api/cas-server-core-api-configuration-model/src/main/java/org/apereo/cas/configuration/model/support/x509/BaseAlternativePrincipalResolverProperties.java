e91bc4b327f198e7b83d3fea765640964463e25f
Hal Deadman <hdeadman@users.noreply.github.com>
2018-10-10 04:51:25 -0400

    add fallback principal parameter to upn and cnedipi resolvers (#3583)
    provides option to get principal from another attribute when upn is not
    present.
    break out x509 resolver properties to their own classes

---
+++ api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/support/x509/BaseAlternativePrincipalResolverProperties.java
@@ -0,0 +1,31 @@
+package org.apereo.cas.configuration.model.support.x509;
+
+import org.apereo.cas.configuration.support.RequiresModule;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+/**
+ * This is {@link BaseAlternativePrincipalResolverProperties}.
+ * @since 6.0.0
+ */
+@RequiresModule(name = "cas-server-support-x509-webflow")
+@Getter
+@Setter
+public abstract class BaseAlternativePrincipalResolverProperties implements Serializable {
+    /**
+     * Attribute name that will be used by X509 principal resolvers if the main attribute in the
+     * certificate is not present. This only applies to principal resolvers that are looking
+     * for attributes in the certificate that are not common to all certificates.
+     * (e.g. SUBJECT_ALT_NAME, CN_EDIPI)
+     * <p>
+     * This assumes you would rather get something like the subjectDn rather than null
+     * where null would allow falling through to another authentication mechanism.
+     * <p>
+     * Currently supported values are: subjectDn, sigAlgOid, subjectX500Principal.
+     */
+    private String alternatePrincipalAttribute;
+
+}
