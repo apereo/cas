3e10a986d71be09f372f68aff1ff7d4db4b36183
Misagh Moayyed <mm1844@gmail.com>
2018-10-11 14:04:10 +0330

    refactor logout api; fix tests

--- support/cas-server-support-actions/src/main/java/org/apereo/cas/web/flow/GatewayServicesManagementCheckAction.java
+++ support/cas-server-support-actions/src/main/java/org/apereo/cas/web/flow/GatewayServicesManagementCheckAction.java
@@ -25,22 +25,18 @@ public class GatewayServicesManagementCheckAction extends AbstractAction {
     @Override
     protected Event doExecute(final RequestContext context) {
         val service = WebUtils.getService(context);
-
         val registeredService = this.servicesManager.findServiceBy(service);
 
         if (registeredService == null) {
-            val msg = String.format("Service Management: Unauthorized Service Access. "
-                + "Service [%s] does not match entries in service registry.", service.getId());
+            val msg = String.format("Unauthorized Service Access: Service [%s] does not match entries in service registry.", service.getId());
             LOGGER.warn(msg);
             throw new UnauthorizedServiceException(UnauthorizedServiceException.CODE_UNAUTHZ_SERVICE, msg);
         }
 
         if (!registeredService.getAccessStrategy().isServiceAccessAllowed()) {
-            val msg = String.format("Service Management: Access to service [%s] "
-                + "is disabled by the service registry.", service.getId());
+            val msg = String.format("Access to service [%s] is disabled by the service registry.", service.getId());
             LOGGER.warn(msg);
-            WebUtils.putUnauthorizedRedirectUrlIntoFlowScope(context,
-                registeredService.getAccessStrategy().getUnauthorizedRedirectUrl());
+            WebUtils.putUnauthorizedRedirectUrlIntoFlowScope(context, registeredService.getAccessStrategy().getUnauthorizedRedirectUrl());
             throw new UnauthorizedServiceException(UnauthorizedServiceException.CODE_UNAUTHZ_SERVICE, msg);
         }
         return success();
