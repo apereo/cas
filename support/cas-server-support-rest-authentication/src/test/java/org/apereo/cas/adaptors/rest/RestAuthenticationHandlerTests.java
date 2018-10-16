ff000ae089e6bc759f6cf628617c065e27886583
Misagh Moayyed <mm1844@gmail.com>
2018-10-16 12:13:00 +0330

    Switch gradle tests to use JUnit Platform (#3588)
    * switch tests to use junit platform
    
    * fix tests
    
    * fix checkstyle
    
    * merge with master
    
    * fix checkstyle imports

--- support/cas-server-support-rest-authentication/src/test/java/org/apereo/cas/adaptors/rest/RestAuthenticationHandlerTests.java
+++ support/cas-server-support-rest-authentication/src/test/java/org/apereo/cas/adaptors/rest/RestAuthenticationHandlerTests.java
@@ -90,8 +90,11 @@ import static org.springframework.test.web.client.response.MockRestResponseCreat
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @Category(RestfulApiCategory.class)
 public class RestAuthenticationHandlerTests {
+
     @ClassRule
     public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
+
+    private static final ObjectMapper MAPPER = new ObjectMapper().findAndRegisterModules();
 
     @Rule
     public final SpringMethodRule springMethodRule = new SpringMethodRule();
@@ -120,14 +123,12 @@ public class RestAuthenticationHandlerTests {
     public void verifySuccess() throws Exception {
         val principalWritten = new DefaultPrincipalFactory().createPrincipal("casuser");
 
-        val mapper = new ObjectMapper().findAndRegisterModules();
         val writer = new StringWriter();
-        mapper.writeValue(writer, principalWritten);
+        MAPPER.writeValue(writer, principalWritten);
 
         server.andRespond(withSuccess(writer.toString(), MediaType.APPLICATION_JSON));
 
-        val res =
-            authenticationHandler.authenticate(CoreAuthenticationTestUtils.getCredentialsWithSameUsernameAndPassword());
+        val res = authenticationHandler.authenticate(CoreAuthenticationTestUtils.getCredentialsWithSameUsernameAndPassword());
         assertEquals("casuser", res.getPrincipal().getId());
     }
 
