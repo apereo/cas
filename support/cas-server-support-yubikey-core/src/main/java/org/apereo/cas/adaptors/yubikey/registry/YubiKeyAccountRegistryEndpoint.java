447dd1da3b5c13393f53c71e9a66acc1a3e4c399
Misagh Moayyed <mmoayyed@unicon.net>
2018-10-18 23:36:36 +0330

    backport surrogate swf-error mapping fix from 5.3.x

--- support/cas-server-support-yubikey-core/src/main/java/org/apereo/cas/adaptors/yubikey/registry/YubiKeyAccountRegistryEndpoint.java
+++ support/cas-server-support-yubikey-core/src/main/java/org/apereo/cas/adaptors/yubikey/registry/YubiKeyAccountRegistryEndpoint.java
@@ -35,10 +35,7 @@ public class YubiKeyAccountRegistryEndpoint {
     @ReadOperation
     public YubiKeyAccount get(@Selector final String username) {
         val result = registry.getAccount(username);
-        if (result.isPresent()) {
-            return result.get();
-        }
-        return null;
+        return result.orElse(null);
     }
 
     /**
