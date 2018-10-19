366d3305c2be46f5d21e2ad154db7dcc12a854d1
Misagh Moayyed <mm1844@gmail.com>
2018-10-19 17:15:02 +0330

    working on banner detection

--- webapp/cas-server-webapp-init/src/main/java/org/apereo/cas/CasEmbeddedContainerUtils.java
+++ webapp/cas-server-webapp-init/src/main/java/org/apereo/cas/CasEmbeddedContainerUtils.java
@@ -1,8 +1,17 @@
 package org.apereo.cas;
 
+import org.apereo.cas.util.spring.boot.AbstractCasBanner;
+import org.apereo.cas.util.spring.boot.DefaultCasBanner;
+
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+import org.reflections.util.FilterBuilder;
+import org.springframework.boot.Banner;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -31,5 +40,32 @@ public class CasEmbeddedContainerUtils {
         val properties = new HashMap<String, Object>();
         properties.put(EMBEDDED_CONTAINER_CONFIG_ACTIVE, embeddedContainerActive);
         return properties;
+    }
+
+    /**
+     * Gets cas banner instance.
+     *
+     * @return the cas banner instance
+     */
+    public static Banner getCasBannerInstance() {
+        val packageName = CasEmbeddedContainerUtils.class.getPackage().getName();
+        val reflections = new Reflections(new ConfigurationBuilder()
+                .filterInputsBy(new FilterBuilder().includePackage(packageName))
+                .setUrls(ClasspathHelper.forPackage(packageName))
+                .setScanners(new SubTypesScanner(true)));
+
+        val subTypes = reflections.getSubTypesOf(AbstractCasBanner.class);
+        subTypes.remove(DefaultCasBanner.class);
+
+        if (subTypes.isEmpty()) {
+            return new DefaultCasBanner();
+        }
+        try {
+            val clz = subTypes.iterator().next();
+            return clz.getDeclaredConstructor().newInstance();
+        } catch (final Exception e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+        return new DefaultCasBanner();
     }
 }
