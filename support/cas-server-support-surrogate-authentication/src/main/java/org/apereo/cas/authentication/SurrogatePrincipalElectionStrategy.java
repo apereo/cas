d66f21c368e7965bf15c0e9870115bdbc81d86a0
Kirill Gagarski <kirill.gagarski@gmail.com>
2018-10-19 17:09:28 +0300

    Disable merging impersonator and impersonee (#3575)

--- support/cas-server-support-surrogate-authentication/src/main/java/org/apereo/cas/authentication/SurrogatePrincipalElectionStrategy.java
+++ support/cas-server-support-surrogate-authentication/src/main/java/org/apereo/cas/authentication/SurrogatePrincipalElectionStrategy.java
@@ -1,9 +1,14 @@
 package org.apereo.cas.authentication;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apereo.cas.authentication.principal.Principal;
+import org.apereo.cas.authentication.principal.PrincipalFactory;
+import org.apereo.cas.authentication.principal.PrincipalFactoryUtils;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.Optional;
 
 /**
@@ -12,11 +17,40 @@ import java.util.Optional;
  * @author Misagh Moayyed
  * @since 5.3.0
  */
+@Slf4j
+@RequiredArgsConstructor
 public class SurrogatePrincipalElectionStrategy extends DefaultPrincipalElectionStrategy {
     private static final long serialVersionUID = -3112906686072339162L;
 
-    @SuppressFBWarnings("PRMC_POSSIBLY_REDUNDANT_METHOD_CALLS")
+    private final PrincipalFactory principalFactory;
+
+    public SurrogatePrincipalElectionStrategy() {
+        this(PrincipalFactoryUtils.newPrincipalFactory());
+    }
+
     @Override
+    public Principal nominate(final Collection<Authentication> authentications,
+                              final Map<String, Object> principalAttributes) {
+        final Principal principal = getPrincipalFromAuthentication(authentications);
+        final Principal finalPrincipal;
+
+        if (principal instanceof SurrogatePrincipal) {
+            finalPrincipal = this.principalFactory.createPrincipal(principal.getId(), principal.getAttributes());
+        } else {
+            finalPrincipal = this.principalFactory.createPrincipal(principal.getId(), principalAttributes);
+        }
+
+        LOGGER.debug("Nominated [{}] as the primary principal", finalPrincipal);
+        return finalPrincipal;
+    }
+
+    /**
+     * Gets principal from authentication.
+     *
+     * @param authentications the authentications
+     * @return the principal from authentication
+     */
+    @SuppressFBWarnings("PRMC_POSSIBLY_REDUNDANT_METHOD_CALLS")
     protected Principal getPrincipalFromAuthentication(final Collection<Authentication> authentications) {
         final Optional<SurrogatePrincipal> result = authentications
             .stream()
