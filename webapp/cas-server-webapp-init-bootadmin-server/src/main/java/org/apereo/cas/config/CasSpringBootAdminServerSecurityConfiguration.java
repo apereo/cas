6af4bd87a56ab8e0c2b23d029b5d38dd1f927354
Misagh Moayyed <mm1844@gmail.com>
2018-10-22 14:49:01 +0330

    add spring security to boot-admin

---
+++ webapp/cas-server-webapp-init-bootadmin-server/src/main/java/org/apereo/cas/config/CasSpringBootAdminServerSecurityConfiguration.java
@@ -0,0 +1,47 @@
+package org.apereo.cas.config;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+/**
+ * This is {@link CasSpringBootAdminServerSecurityConfiguration}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.0.0
+ */
+@Slf4j
+@Configuration("casSpringBootAdminServerSecurityConfiguration")
+public class CasSpringBootAdminServerSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private AdminServerProperties adminServerProperties;
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        val adminContextPath = adminServerProperties.getContextPath();
+
+        val successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+        successHandler.setDefaultTargetUrl(adminContextPath + "/");
+        http.authorizeRequests()
+            .antMatchers(adminContextPath + "/assets/**").permitAll()
+            .antMatchers(adminContextPath + "/login").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
+            .logout().logoutUrl(adminContextPath + "/logout").and()
+            .httpBasic().and()
+            .csrf()
+            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            .ignoringAntMatchers(
+                adminContextPath + "/instances",
+                adminContextPath + "/actuator/**"
+            );
+    }
+}
