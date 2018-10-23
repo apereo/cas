24c28eb6a0f23e76bae12d90efae0ab365d76d71
Misagh Moayyed <mm1844@gmail.com>
2018-10-24 01:20:04 +0330

    Fix issue with surrogates and saml2 response destination

--- support/cas-server-support-surrogate-authentication/src/main/java/org/apereo/cas/authentication/SurrogatePrincipalElectionStrategy.java
+++ support/cas-server-support-surrogate-authentication/src/main/java/org/apereo/cas/authentication/SurrogatePrincipalElectionStrategy.java
@@ -1,11 +1,7 @@
 package org.apereo.cas.authentication;
 
 import org.apereo.cas.authentication.principal.Principal;
-import org.apereo.cas.authentication.principal.PrincipalFactory;
-import org.apereo.cas.authentication.principal.PrincipalFactoryUtils;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Collection;
@@ -19,42 +15,13 @@ import java.util.Optional;
  * @since 5.3.0
  */
 @Slf4j
-@RequiredArgsConstructor
 public class SurrogatePrincipalElectionStrategy extends DefaultPrincipalElectionStrategy {
     private static final long serialVersionUID = -3112906686072339162L;
-
-    private final PrincipalFactory principalFactory;
-
-    public SurrogatePrincipalElectionStrategy() {
-        this(PrincipalFactoryUtils.newPrincipalFactory());
-    }
 
     @Override
     public Principal nominate(final Collection<Authentication> authentications,
                               final Map<String, Object> principalAttributes) {
-        final Principal principal = getPrincipalFromAuthentication(authentications);
-        final Principal finalPrincipal;
 
-        if (principal instanceof SurrogatePrincipal) {
-            final Principal surrogatePrincipal = ((SurrogatePrincipal) principal).getSurrogate();
-            LOGGER.debug("Principal [{}] indicates a surrogate", surrogatePrincipal);
-            finalPrincipal = this.principalFactory.createPrincipal(surrogatePrincipal.getId(), surrogatePrincipal.getAttributes());
-        } else {
-            finalPrincipal = this.principalFactory.createPrincipal(principal.getId(), principalAttributes);
-        }
-
-        LOGGER.debug("Nominated [{}] as the primary principal", finalPrincipal);
-        return finalPrincipal;
-    }
-
-    /**
-     * Gets principal from authentication.
-     *
-     * @param authentications the authentications
-     * @return the principal from authentication
-     */
-    @SuppressFBWarnings("PRMC_POSSIBLY_REDUNDANT_METHOD_CALLS")
-    protected Principal getPrincipalFromAuthentication(final Collection<Authentication> authentications) {
         final Optional<SurrogatePrincipal> result = authentications
             .stream()
             .map(Authentication::getPrincipal)
@@ -62,8 +29,11 @@ public class SurrogatePrincipalElectionStrategy extends DefaultPrincipalElection
             .map(SurrogatePrincipal.class::cast)
             .findFirst();
         if (result.isPresent()) {
-            return result.get().getSurrogate();
+            final Principal surrogatePrincipal = result.get().getSurrogate();
+            LOGGER.debug("Nominated [{}] as the surrogate principal", surrogatePrincipal);
+            return surrogatePrincipal;
         }
-        return super.getPrincipalFromAuthentication(authentications);
+        return super.nominate(authentications, principalAttributes);
     }
+
 }
