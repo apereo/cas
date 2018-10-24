cfc0f251f4fa8ac1a04b0f86885d5953f3f8f3c5
Misagh Moayyed <mmoayyed@unicon.net>
2018-10-24 23:30:59 +0330

    add ordering to service factories

--- core/cas-server-core-web/src/main/java/org/apereo/cas/config/CasCoreWebConfiguration.java
+++ core/cas-server-core-web/src/main/java/org/apereo/cas/config/CasCoreWebConfiguration.java
@@ -20,6 +20,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.HierarchicalMessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.AnnotationAwareOrderComparator;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
 
@@ -87,6 +88,7 @@ public class CasCoreWebConfiguration {
     public ArgumentExtractor argumentExtractor(final List<ServiceFactoryConfigurer> configurers) {
         val serviceFactoryList = new ArrayList<ServiceFactory<? extends WebApplicationService>>();
         configurers.forEach(c -> serviceFactoryList.addAll(c.buildServiceFactories()));
+        AnnotationAwareOrderComparator.sortIfNecessary(configurers);
         return new DefaultArgumentExtractor(serviceFactoryList);
     }
 
