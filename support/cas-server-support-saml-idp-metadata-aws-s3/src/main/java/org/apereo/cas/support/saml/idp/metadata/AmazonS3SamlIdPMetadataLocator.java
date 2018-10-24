1847fb6b18b868375fc6c0fca38d3a9f0f410b99
Misagh Moayyed <mm1844@gmail.com>
2018-10-24 12:36:05 +0330

    add support for amz s3 for idp metadata

---
+++ support/cas-server-support-saml-idp-metadata-aws-s3/src/main/java/org/apereo/cas/support/saml/idp/metadata/AmazonS3SamlIdPMetadataLocator.java
@@ -0,0 +1,65 @@
+package org.apereo.cas.support.saml.idp.metadata;
+
+import org.apereo.cas.CipherExecutor;
+import org.apereo.cas.support.saml.idp.metadata.locator.AbstractSamlIdPMetadataLocator;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.util.IOUtils;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+
+import java.math.BigInteger;
+
+/**
+ * This is {@link AmazonS3SamlIdPMetadataLocator}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.0.0
+ */
+@Slf4j
+public class AmazonS3SamlIdPMetadataLocator extends AbstractSamlIdPMetadataLocator {
+    private final transient AmazonS3 s3Client;
+
+    private final String bucketName;
+
+    public AmazonS3SamlIdPMetadataLocator(final CipherExecutor<String, String> metadataCipherExecutor, final String bucketName, final AmazonS3 s3Client) {
+        super(metadataCipherExecutor);
+        this.bucketName = bucketName;
+        this.s3Client = s3Client;
+    }
+
+    @Override
+    protected void fetchMetadataDocument() {
+        try {
+            LOGGER.debug("Locating S3 object(s) from bucket [{}]...", bucketName);
+            val result = s3Client.listObjectsV2(bucketName);
+            val objects = result.getObjectSummaries();
+            LOGGER.debug("Located [{}] S3 object(s) from bucket [{}]", objects.size(), bucketName);
+
+            if (objects.isEmpty()) {
+                throw new IllegalArgumentException("No objects found in bucket " + bucketName);
+            }
+            val obj = objects.get(0);
+            val objectKey = obj.getKey();
+            LOGGER.debug("Fetching object [{}] from bucket [{}]", objectKey, bucketName);
+            val object = s3Client.getObject(obj.getBucketName(), objectKey);
+            try (val is = object.getObjectContent()) {
+                metadataDocument.setId(BigInteger.valueOf(System.nanoTime()));
+
+                val objectMetadata = object.getObjectMetadata();
+                if (objectMetadata != null) {
+                    metadataDocument.setEncryptionCertificate(objectMetadata.getUserMetaDataOf("encryptionCertificate"));
+                    metadataDocument.setSigningCertificate(objectMetadata.getUserMetaDataOf("signingCertificate"));
+                    metadataDocument.setEncryptionKey(objectMetadata.getUserMetaDataOf("encryptionKey"));
+                    metadataDocument.setSigningKey(objectMetadata.getUserMetaDataOf("signingKey"));
+                }
+                metadataDocument.setMetadata(IOUtils.toString(is));
+            } catch (final Exception e) {
+                LOGGER.error(e.getMessage(), e);
+            }
+        } catch (final Exception e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+    }
+}
+
