9c15bd30c043ccef9d78758809638d0a6ddff1dd
Misagh Moayyed <mm1844@gmail.com>
2018-11-05 14:24:43 +0330

    more audit tests

---
+++ core/cas-server-core-audit-api/src/test/java/org/apereo/cas/audit/spi/resource/TicketAsFirstParameterResourceResolverTests.java
@@ -0,0 +1,25 @@
+package org.apereo.cas.audit.spi.resource;
+
+import lombok.val;
+import org.aspectj.lang.JoinPoint;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * This is {@link TicketAsFirstParameterResourceResolverTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.0.0
+ */
+public class TicketAsFirstParameterResourceResolverTests {
+    @Test
+    public void verifyOperation() {
+        val jp = mock(JoinPoint.class);
+        when(jp.getArgs()).thenReturn(new Object[]{"ST-123434"});
+        val resolver = new TicketAsFirstParameterResourceResolver();
+        val input = resolver.resolveFrom(jp, null);
+        assertTrue(input.length > 0);
+    }
+}
