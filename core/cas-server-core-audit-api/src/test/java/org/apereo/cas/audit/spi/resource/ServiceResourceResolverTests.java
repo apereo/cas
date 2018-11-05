acafef2844ad285624cdd7d44b3abeb0bf5ae725
Misagh Moayyed <mm1844@gmail.com>
2018-11-05 14:24:43 +0330

    more audit tests

---
+++ core/cas-server-core-audit-api/src/test/java/org/apereo/cas/audit/spi/resource/ServiceResourceResolverTests.java
@@ -0,0 +1,31 @@
+package org.apereo.cas.audit.spi.resource;
+
+import org.apereo.cas.authentication.CoreAuthenticationTestUtils;
+import org.apereo.cas.services.RegisteredServiceTestUtils;
+
+import lombok.val;
+import org.aspectj.lang.JoinPoint;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * This is {@link ServiceResourceResolverTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.0.0
+ */
+public class ServiceResourceResolverTests {
+    @Test
+    public void verifyOperation() {
+        val jp = mock(JoinPoint.class);
+        when(jp.getArgs()).thenReturn(new Object[]{"something", RegisteredServiceTestUtils.getService()});
+        val resolver = new ServiceResourceResolver();
+        var input = resolver.resolveFrom(jp, null);
+        assertTrue(input.length > 0);
+
+        input = resolver.resolveFrom(jp, new RuntimeException());
+        assertTrue(input.length > 0);
+    }
+}
