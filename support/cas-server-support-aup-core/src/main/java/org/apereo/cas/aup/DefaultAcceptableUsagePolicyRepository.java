df321ee53431864550f0fa4d966d5b249e27cb19
Hal Deadman <hdeadman@users.noreply.github.com>
2018-11-06 07:01:43 -0500

    add new scope of acceptance for default aup repository (#3632)
    * add new scope of acceptance for default aup repository
    
    once per authentication vs once per server uptime, current way still
    default
    
    * add new scope of acceptance for default aup repository
    
    once per authentication vs once per server uptime, current way still
    default

--- support/cas-server-support-aup-core/src/main/java/org/apereo/cas/aup/DefaultAcceptableUsagePolicyRepository.java
+++ support/cas-server-support-aup-core/src/main/java/org/apereo/cas/aup/DefaultAcceptableUsagePolicyRepository.java
@@ -3,6 +3,7 @@ package org.apereo.cas.aup;
 import org.apereo.cas.authentication.AuthenticationException;
 import org.apereo.cas.authentication.Credential;
 import org.apereo.cas.authentication.principal.Principal;
+import org.apereo.cas.configuration.model.support.aup.AcceptableUsagePolicyProperties;
 import org.apereo.cas.ticket.registry.TicketRegistrySupport;
 import org.apereo.cas.web.support.WebUtils;
 
@@ -23,30 +24,51 @@ public class DefaultAcceptableUsagePolicyRepository extends AbstractPrincipalAtt
 
     private static final long serialVersionUID = -3059445754626980894L;
 
+    private static final String AUP_ACCEPTED = "AUP_ACCEPTED";
+
+    private AcceptableUsagePolicyProperties.Scope scope;
+
     private final Map<String, Boolean> policyMap = new ConcurrentHashMap<>();
 
-    public DefaultAcceptableUsagePolicyRepository(final TicketRegistrySupport ticketRegistrySupport) {
+    public DefaultAcceptableUsagePolicyRepository(final TicketRegistrySupport ticketRegistrySupport,
+                                                  final AcceptableUsagePolicyProperties.Scope scope) {
         super(ticketRegistrySupport, null);
+        this.scope = scope;
     }
 
     @Override
     public Pair<Boolean, Principal> verify(final RequestContext requestContext, final Credential credential) {
-        val key = credential.getId();
+        val storageInfo = getKeyAndMap(requestContext, credential);
+        val key = storageInfo.getLeft();
+        val map = storageInfo.getRight();
         val authentication = WebUtils.getAuthentication(requestContext);
         if (authentication == null) {
             throw new AuthenticationException("No authentication could be found in the current context");
         }
         val principal = authentication.getPrincipal();
-        if (this.policyMap.containsKey(key)) {
-            return Pair.of(this.policyMap.get(key), principal);
+        if (map.containsKey(key)) {
+            return Pair.of((Boolean) map.get(key), principal);
         }
         return Pair.of(Boolean.FALSE, principal);
     }
 
     @Override
     public boolean submit(final RequestContext requestContext, final Credential credential) {
-        this.policyMap.put(credential.getId(), Boolean.TRUE);
-        return this.policyMap.containsKey(credential.getId());
+        Pair<String, Map> storageInfo = getKeyAndMap(requestContext, credential);
+        val key = storageInfo.getLeft();
+        val map = storageInfo.getRight();
+        map.put(key, Boolean.TRUE);
+        return map.containsKey(key);
     }
 
+    private Pair<String, Map> getKeyAndMap(final RequestContext requestContext, final Credential credential) {
+        switch (scope) {
+            case GLOBAL:
+                return Pair.of(credential.getId(), policyMap);
+            case AUTHENTICATION:
+                return Pair.of(AUP_ACCEPTED, requestContext.getFlowScope().asMap());
+            default:
+                throw new IllegalStateException("Unexpected scope");
+        }
+    }
 }
