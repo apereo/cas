e60fd46689ae69472549a8d95246edb7ea4441e3
Misagh Moayyed <mm1844@gmail.com>
2018-11-07 14:41:53 +0330

    minor code cleanup for needless parenthesis

--- support/cas-server-support-pac4j-webflow/src/main/java/org/apereo/cas/web/flow/DelegatedAuthenticationSAML2ClientLogoutAction.java
+++ support/cas-server-support-pac4j-webflow/src/main/java/org/apereo/cas/web/flow/DelegatedAuthenticationSAML2ClientLogoutAction.java
@@ -47,7 +47,7 @@ public class DelegatedAuthenticationSAML2ClientLogoutAction extends AbstractActi
             Client<?, ?> client;
             try {
                 val currentClientName = findCurrentClientName(context);
-                client = (currentClientName == null) ? null : clients.findClient(currentClientName);
+                client = currentClientName == null ? null : clients.findClient(currentClientName);
             } catch (final TechnicalException e) {
                 LOGGER.debug("No SAML2 client found: " + e.getMessage(), e);
                 client = null;
