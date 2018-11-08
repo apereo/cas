3eb5c092e06d1dd50977acedd81958bc8267fd40
Misagh Moayyed <mm1844@gmail.com>
2018-11-08 16:34:33 +0330

    fix tests with json and yaml registries

--- core/cas-server-core-util-api/src/main/java/org/apereo/cas/util/serialization/AbstractJacksonBackedStringSerializer.java
+++ core/cas-server-core-util-api/src/main/java/org/apereo/cas/util/serialization/AbstractJacksonBackedStringSerializer.java
@@ -76,25 +76,25 @@ public abstract class AbstractJacksonBackedStringSerializer<T> implements String
     @SneakyThrows
     public T from(final String json) {
         val jsonString = isJsonFormat() ? JsonValue.readHjson(json).toString() : json;
-        return readObjectFromJson(jsonString);
+        return readObjectFromString(jsonString);
     }
 
     @Override
     @SneakyThrows
     public T from(final File json) {
-        val jsonString = isJsonFormat()
+        val string = isJsonFormat()
             ? JsonValue.readHjson(FileUtils.readFileToString(json, StandardCharsets.UTF_8)).toString()
             : FileUtils.readFileToString(json, StandardCharsets.UTF_8);
-        return readObjectFromJson(jsonString);
+        return readObjectFromString(string);
     }
 
     @Override
     @SneakyThrows
     public T from(final Reader json) {
-        val jsonString = isJsonFormat()
+        val string = isJsonFormat()
             ? JsonValue.readHjson(json).toString()
-            : String.join("", IOUtils.readLines(json));
-        return readObjectFromJson(jsonString);
+            : String.join("\n", IOUtils.readLines(json));
+        return readObjectFromString(string);
     }
 
     @Override
@@ -106,7 +106,7 @@ public abstract class AbstractJacksonBackedStringSerializer<T> implements String
     @SneakyThrows
     public T from(final InputStream json) {
         val jsonString = readJsonFrom(json);
-        return readObjectFromJson(jsonString);
+        return readObjectFromString(jsonString);
     }
 
     /**
@@ -228,7 +228,7 @@ public abstract class AbstractJacksonBackedStringSerializer<T> implements String
      * @param jsonString the json string
      * @return the type
      */
-    protected T readObjectFromJson(final String jsonString) {
+    protected T readObjectFromString(final String jsonString) {
         try {
             LOGGER.trace("Attempting to consume [{}]", jsonString);
             return this.objectMapper.readValue(jsonString, getTypeToSerialize());
