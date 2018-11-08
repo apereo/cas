eccbcf1f4ca34f2bc755db0668b532fea1c455c9
Misagh Moayyed <mmoayyed@unicon.net>
2018-11-09 00:52:23 +0330

    minor formatting adjustments

--- support/cas-server-support-ws-idp/src/main/java/org/apereo/cas/ws/idp/authentication/WSFederationAuthenticationServiceSelectionStrategy.java
+++ support/cas-server-support-ws-idp/src/main/java/org/apereo/cas/ws/idp/authentication/WSFederationAuthenticationServiceSelectionStrategy.java
@@ -5,6 +5,8 @@ import org.apereo.cas.authentication.principal.Service;
 import org.apereo.cas.authentication.principal.ServiceFactory;
 import org.apereo.cas.ws.idp.WSFederationConstants;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.apache.http.NameValuePair;
@@ -20,10 +22,12 @@ import java.util.Optional;
  * @since 5.1.0
  */
 @Slf4j
+@Getter
+@Setter
 public class WSFederationAuthenticationServiceSelectionStrategy implements AuthenticationServiceSelectionStrategy {
     private static final long serialVersionUID = 8035218407906419228L;
 
-    private final int order = Ordered.HIGHEST_PRECEDENCE;
+    private int order = Ordered.HIGHEST_PRECEDENCE;
     private final transient ServiceFactory webApplicationServiceFactory;
 
     public WSFederationAuthenticationServiceSelectionStrategy(final ServiceFactory webApplicationServiceFactory) {
@@ -33,11 +37,10 @@ public class WSFederationAuthenticationServiceSelectionStrategy implements Authe
     private static Optional<NameValuePair> getRealmAsParameter(final Service service) {
         try {
             val builder = new URIBuilder(service.getId());
-            val param = builder.getQueryParams()
+            return builder.getQueryParams()
                 .stream()
                 .filter(p -> p.getName().equals(WSFederationConstants.WTREALM))
                 .findFirst();
-            return param;
         } catch (final Exception e) {
             LOGGER.error(e.getMessage(), e);
         }
@@ -51,11 +54,10 @@ public class WSFederationAuthenticationServiceSelectionStrategy implements Authe
             }
 
             val builder = new URIBuilder(service.getId());
-            val param = builder.getQueryParams()
+            return builder.getQueryParams()
                 .stream()
                 .filter(p -> p.getName().equals(WSFederationConstants.WREPLY))
                 .findFirst();
-            return param;
         } catch (final Exception e) {
             LOGGER.error(e.getMessage(), e);
         }
