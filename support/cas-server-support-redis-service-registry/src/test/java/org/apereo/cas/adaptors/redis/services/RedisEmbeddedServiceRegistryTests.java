1ccb73576fc7ce31d9253b6de22a09853405be90
Misagh Moayyed <mm1844@gmail.com>
2018-11-08 15:58:03 +0330

    add tests against redis-server non-embedded

---
+++ support/cas-server-support-redis-service-registry/src/test/java/org/apereo/cas/adaptors/redis/services/RedisEmbeddedServiceRegistryTests.java
@@ -0,0 +1,71 @@
+package org.apereo.cas.adaptors.redis.services;
+
+import org.apereo.cas.category.RedisCategory;
+import org.apereo.cas.config.RedisServiceRegistryConfiguration;
+import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.services.AbstractServiceRegistryTests;
+import org.apereo.cas.services.RegexRegisteredService;
+import org.apereo.cas.services.RegisteredService;
+import org.apereo.cas.services.ServiceRegistry;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.test.context.TestPropertySource;
+import redis.embedded.RedisServer;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * Unit test for {@link RedisServiceRegistry} class.
+ *
+ * @author Misagh Moayyed
+ * @since 4.0.0
+ */
+@RunWith(Parameterized.class)
+@SpringBootTest(classes = {RedisServiceRegistryConfiguration.class, RefreshAutoConfiguration.class})
+@EnableScheduling
+@TestPropertySource(properties = {"cas.serviceRegistry.redis.host=localhost", "cas.serviceRegistry.redis.port=6380"})
+@EnableConfigurationProperties(CasConfigurationProperties.class)
+@Category(RedisCategory.class)
+public class RedisEmbeddedServiceRegistryTests extends AbstractServiceRegistryTests {
+    private static RedisServer REDIS_SERVER;
+
+    @Autowired
+    @Qualifier("redisServiceRegistry")
+    private ServiceRegistry dao;
+
+    public RedisEmbeddedServiceRegistryTests(final Class<? extends RegisteredService> registeredServiceClass) {
+        super(registeredServiceClass);
+    }
+
+    @BeforeClass
+    public static void startRedis() throws Exception {
+        REDIS_SERVER = new RedisServer(6380);
+        REDIS_SERVER.start();
+    }
+
+    @AfterClass
+    public static void stopRedis() {
+        REDIS_SERVER.stop();
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object> getTestParameters() {
+        return Arrays.asList(RegexRegisteredService.class);
+    }
+
+    @Override
+    public ServiceRegistry getNewServiceRegistry() {
+        return this.dao;
+    }
+}
