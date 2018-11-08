13ee6082f1890af9ccad34e9eb60c5819c533d99
Misagh Moayyed <mm1844@gmail.com>
2018-11-08 16:34:33 +0330

    fix tests with json and yaml registries

--- core/cas-server-core-services-api/src/main/java/org/apereo/cas/services/util/RegisteredServiceYamlSerializer.java
+++ core/cas-server-core-services-api/src/main/java/org/apereo/cas/services/util/RegisteredServiceYamlSerializer.java
@@ -2,6 +2,7 @@ package org.apereo.cas.services.util;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.apache.commons.io.FileUtils;
 
@@ -14,6 +15,7 @@ import java.nio.charset.StandardCharsets;
  * @author Misagh Moayyed
  * @since 5.0.0
  */
+@Slf4j
 public class RegisteredServiceYamlSerializer extends DefaultRegisteredServiceJsonSerializer {
     private static final long serialVersionUID = -6026921045861422473L;
 
@@ -28,6 +30,7 @@ public class RegisteredServiceYamlSerializer extends DefaultRegisteredServiceJso
             val contents = FileUtils.readFileToString(file, StandardCharsets.UTF_8.name()).trim();
             return contents.startsWith("--- !<");
         } catch (final Exception e) {
+            LOGGER.trace(e.getMessage(), e);
             return false;
         }
     }
