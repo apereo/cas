69652caf454a3c63c1c34be54d88b86738c51c96
Misagh Moayyed <mm1844@gmail.com>
2018-11-11 20:24:43 +0330

    test cases for watchers

--- core/cas-server-core-services-registry/src/main/java/org/apereo/cas/services/resource/BaseResourceBasedRegisteredServiceWatcher.java
+++ core/cas-server-core-services-registry/src/main/java/org/apereo/cas/services/resource/BaseResourceBasedRegisteredServiceWatcher.java
@@ -20,7 +20,7 @@ public abstract class BaseResourceBasedRegisteredServiceWatcher implements Consu
     /**
      * Consumer to log warnings for duplicate service defns.
      */
-    public static final Consumer<RegisteredService> LOG_SERVICE_DUPLICATE =
+    static final Consumer<RegisteredService> LOG_SERVICE_DUPLICATE =
         service -> LOGGER.warn("Found a service definition [{}] with a duplicate id [{}]. "
             + "This will overwrite previous service definitions and is likely a configuration problem. "
             + "Make sure all services have a unique id and try again.", service.getServiceId(), service.getId());
---
+++ core/cas-server-core-services-registry/src/test/java/org/apereo/cas/services/resource/DeleteResourceBasedRegisteredServiceWatcherTests.java
@@ -0,0 +1,59 @@
+package org.apereo.cas.services.resource;
+
+import org.apereo.cas.services.util.DefaultRegisteredServiceJsonSerializer;
+import org.apereo.cas.support.events.service.CasRegisteredServiceDeletedEvent;
+import org.apereo.cas.support.events.service.CasRegisteredServicesLoadedEvent;
+
+import lombok.SneakyThrows;
+import lombok.val;
+import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.junit.Assert.*;
+
+/**
+ * This is {@link DeleteResourceBasedRegisteredServiceWatcherTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.0.0
+ */
+public class DeleteResourceBasedRegisteredServiceWatcherTests {
+
+    @SneakyThrows
+    @Test
+    public void verifyOperationNotFound() {
+        val result = new AtomicBoolean(false);
+        val watcher = new DeleteResourceBasedRegisteredServiceWatcher(new AbstractResourceBasedServiceRegistry(new ClassPathResource("services"),
+            Collections.singletonList(new DefaultRegisteredServiceJsonSerializer()), o -> result.set(o.getClass().equals(CasRegisteredServicesLoadedEvent.class))) {
+            @Override
+            protected String[] getExtensions() {
+                return new String[]{"json"};
+            }
+        });
+        watcher.accept(new File("removed.json"));
+        assertTrue(result.get());
+    }
+
+    @SneakyThrows
+    @Test
+    public void verifyOperationFoundDeleted() {
+        val result = new AtomicBoolean(false);
+        val registry = new AbstractResourceBasedServiceRegistry(new ClassPathResource("services"),
+            Collections.singletonList(new DefaultRegisteredServiceJsonSerializer()), o -> result.set(o.getClass().equals(CasRegisteredServiceDeletedEvent.class))) {
+            @Override
+            protected String[] getExtensions() {
+                return new String[]{"json"};
+            }
+        };
+        var results = registry.load();
+        assertFalse(results.isEmpty());
+        val watcher = new DeleteResourceBasedRegisteredServiceWatcher(registry);
+        watcher.accept(new File("Sample-1.json"));
+        assertTrue(result.get());
+        assertEquals(0, registry.size());
+    }
+}
