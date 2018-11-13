40ea57d39ef0c96269df91fc72e1421c56196d77
Misagh Moayyed <mm1844@gmail.com>
2018-11-14 00:20:03 +0330

    add support for soap authn; initial pass

---
+++ support/cas-server-support-soap-authentication/src/main/java/org/apereo/cas/config/SoapAuthenticationConfiguration.java
@@ -0,0 +1,99 @@
+package org.apereo.cas.config;
+
+import org.apereo.cas.authentication.AuthenticationEventExecutionPlanConfigurer;
+import org.apereo.cas.authentication.AuthenticationHandler;
+import org.apereo.cas.authentication.SoapAuthenticationClient;
+import org.apereo.cas.authentication.SoapAuthenticationHandler;
+import org.apereo.cas.authentication.principal.PrincipalFactory;
+import org.apereo.cas.authentication.principal.PrincipalFactoryUtils;
+import org.apereo.cas.authentication.principal.PrincipalNameTransformerUtils;
+import org.apereo.cas.authentication.principal.PrincipalResolver;
+import org.apereo.cas.authentication.support.password.PasswordEncoderUtils;
+import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.services.ServicesManager;
+
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+/**
+ * This is {@link SoapAuthenticationConfiguration}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.0.0
+ */
+@Configuration("soapAuthenticationConfiguration")
+@EnableConfigurationProperties(CasConfigurationProperties.class)
+@Slf4j
+public class SoapAuthenticationConfiguration {
+    @Autowired
+    private CasConfigurationProperties casProperties;
+
+    @Autowired
+    @Qualifier("servicesManager")
+    private ObjectProvider<ServicesManager> servicesManager;
+
+    @Autowired
+    @Qualifier("defaultPrincipalResolver")
+    private ObjectProvider<PrincipalResolver> defaultPrincipalResolver;
+
+    @ConditionalOnMissingBean(name = "soapAuthenticationPrincipalFactory")
+    @Bean
+    public PrincipalFactory soapAuthenticationPrincipalFactory() {
+        return PrincipalFactoryUtils.newPrincipalFactory();
+    }
+
+    @ConditionalOnMissingBean(name = "soapAuthenticationAuthenticationHandler")
+    @Bean
+    @RefreshScope
+    public AuthenticationHandler soapAuthenticationAuthenticationHandler() {
+        val soap = casProperties.getAuthn().getSoap();
+        val handler = new SoapAuthenticationHandler(soap.getName(),
+            servicesManager.getIfAvailable(),
+            soapAuthenticationPrincipalFactory(),
+            soap.getOrder(),
+            soapAuthenticationClient());
+        handler.setPrincipalNameTransformer(PrincipalNameTransformerUtils.newPrincipalNameTransformer(soap.getPrincipalTransformation()));
+        handler.setPasswordEncoder(PasswordEncoderUtils.newPasswordEncoder(soap.getPasswordEncoder()));
+        return handler;
+    }
+
+    @ConditionalOnMissingBean(name = "soapAuthenticationEventExecutionPlanConfigurer")
+    @Bean
+    public AuthenticationEventExecutionPlanConfigurer soapAuthenticationEventExecutionPlanConfigurer() {
+        return plan -> plan.registerAuthenticationHandlerWithPrincipalResolver(soapAuthenticationAuthenticationHandler(), defaultPrincipalResolver.getIfAvailable());
+    }
+
+    @ConditionalOnMissingBean(name = "soapAuthenticationMarshaller")
+    @Bean
+    public Jaxb2Marshaller soapAuthenticationMarshaller() {
+        val marshaller = new Jaxb2Marshaller();
+        marshaller.setContextPath("org.apereo.cas.authentication.soap");
+        return marshaller;
+    }
+
+    @ConditionalOnMissingBean(name = "soapAuthenticationClient")
+    @RefreshScope
+    @Bean
+    public SoapAuthenticationClient soapAuthenticationClient() {
+        val soap = casProperties.getAuthn().getSoap();
+        if (StringUtils.isBlank(soap.getUrl())) {
+            throw new BeanCreationException("No SOAP url is defined");
+        }
+        val client = new SoapAuthenticationClient();
+        client.setMarshaller(soapAuthenticationMarshaller());
+        client.setUnmarshaller(soapAuthenticationMarshaller());
+        client.setDefaultUri(soap.getUrl());
+        return client;
+    }
+}
