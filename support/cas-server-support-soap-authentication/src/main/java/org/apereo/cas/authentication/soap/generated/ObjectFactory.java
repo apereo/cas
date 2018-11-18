2142293a4ee58664e77844995dff8d05480a7e56
Misagh Moayyed <mmoayyed@unicon.net>
2018-11-18 13:48:56 +0330

    updated

--- support/cas-server-support-soap-authentication/src/main/java/org/apereo/cas/authentication/soap/generated/ObjectFactory.java
+++ support/cas-server-support-soap-authentication/src/main/java/org/apereo/cas/authentication/soap/generated/ObjectFactory.java
@@ -8,7 +8,7 @@ import javax.xml.bind.annotation.XmlRegistry;
  * This object contains factory methods for each 
  * Java content interface and Java element interface 
  * generated in the org.apereo.cas.authentication.soap.generated package. 
- * <p>An ObjectFactory allows you to programatically 
+ * <p>An ObjectFactory allows you to programmatically
  * construct new instances of the Java representation 
  * for XML content. The Java representation of XML 
  * content can consist of schema derived interfaces 
