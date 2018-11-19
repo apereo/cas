4fce9d228cccc2b8518cae1fc8b57a6796cae2ef
Misagh Moayyed <mmoayyed@unicon.net>
2018-11-19 14:03:06 +0330

    updated

---
+++ Procfile
@@ -0,0 +1,1 @@
+web:    java $JAVA_OPTS -jar target/casbot.jar --server.port=$PORT --server.ssl.enabled=false
--- src/main/java/org/apereo/cas/CasBotApplication.java
+++ src/main/java/org/apereo/cas/CasBotApplication.java
@@ -20,6 +20,7 @@ import org.apereo.cas.github.GitHubOperations;
 import org.apereo.cas.github.GitHubTemplate;
 import org.apereo.cas.github.RegexLinkParser;
 
+import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -39,6 +40,12 @@ public class CasBotApplication {
 
     @Bean
     public GitHubTemplate gitHubTemplate(final GitHubProperties gitHubProperties) {
+        if (gitHubProperties.getCredentials().getPassword() == null) {
+            throw new BeanCreationException("No credential password is specified");
+        }
+        if (gitHubProperties.getCredentials().getUsername() == null) {
+            throw new BeanCreationException("No credential username is specified");
+        }
         return new GitHubTemplate(gitHubProperties.getCredentials().getUsername(),
             gitHubProperties.getCredentials().getPassword(), new RegexLinkParser());
     }
