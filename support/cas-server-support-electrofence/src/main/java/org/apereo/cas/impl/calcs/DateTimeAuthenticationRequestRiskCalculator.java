a40a164fe62be65318dcdb0c6404803dc610aaca
Misagh Moayyed <mmoayyed@unicon.net>
2018-11-20 23:18:42 +0330

    working on cleaning up autowired fields

--- support/cas-server-support-electrofence/src/main/java/org/apereo/cas/impl/calcs/DateTimeAuthenticationRequestRiskCalculator.java
+++ support/cas-server-support-electrofence/src/main/java/org/apereo/cas/impl/calcs/DateTimeAuthenticationRequestRiskCalculator.java
@@ -1,6 +1,7 @@
 package org.apereo.cas.impl.calcs;
 
 import org.apereo.cas.authentication.Authentication;
+import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.services.RegisteredService;
 import org.apereo.cas.support.events.CasEventRepository;
 import org.apereo.cas.support.events.dao.CasEvent;
@@ -24,16 +25,15 @@ import java.util.Collection;
 @Slf4j
 public class DateTimeAuthenticationRequestRiskCalculator extends BaseAuthenticationRequestRiskCalculator {
 
-    private final int windowInHours;
-
-    public DateTimeAuthenticationRequestRiskCalculator(final CasEventRepository casEventRepository, final int windowInHours) {
-        super(casEventRepository);
-        this.windowInHours = windowInHours;
+    public DateTimeAuthenticationRequestRiskCalculator(final CasEventRepository casEventRepository,
+                                                       final CasConfigurationProperties casProperties) {
+        super(casEventRepository, casProperties);
     }
 
     @Override
     protected BigDecimal calculateScore(final HttpServletRequest request, final Authentication authentication,
                                         final RegisteredService service, final Collection<? extends CasEvent> events) {
+        val windowInHours = casProperties.getAuthn().getAdaptive().getRisk().getDateTime().getWindowInHours();
         val timestamp = ZonedDateTime.now(ZoneOffset.UTC);
         LOGGER.debug("Filtering authentication events for timestamp [{}]", timestamp);
 
@@ -51,10 +51,6 @@ public class DateTimeAuthenticationRequestRiskCalculator extends BaseAuthenticat
             .count();
 
         LOGGER.debug("Total authentication events found for [{}] in a [{}]h window: [{}]", timestamp, windowInHours, count);
-        if (count == events.size()) {
-            LOGGER.debug("Principal [{}] has always authenticated from [{}]", authentication.getPrincipal(), timestamp);
-            return LOWEST_RISK_SCORE;
-        }
-        return getFinalAveragedScore(count, events.size());
+        return calculateScoreBasedOnEventsCount(authentication, events, count);
     }
 }
