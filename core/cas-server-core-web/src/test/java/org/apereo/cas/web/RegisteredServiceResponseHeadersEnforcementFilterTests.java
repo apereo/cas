659605164337912cf4123c626ccdbdd157590adc
Misagh Moayyed <mm1844@gmail.com>
2018-11-20 17:58:38 +0330

    port response headers fixes forward from 5.3.x

--- core/cas-server-core-web/src/test/java/org/apereo/cas/web/RegisteredServiceResponseHeadersEnforcementFilterTests.java
+++ core/cas-server-core-web/src/test/java/org/apereo/cas/web/RegisteredServiceResponseHeadersEnforcementFilterTests.java
@@ -1,28 +1,30 @@
 package org.apereo.cas.web;
 
+import org.apereo.cas.CasProtocolConstants;
 import org.apereo.cas.authentication.DefaultAuthenticationServiceSelectionPlan;
 import org.apereo.cas.authentication.DefaultAuthenticationServiceSelectionStrategy;
-import org.apereo.cas.authentication.principal.Service;
-import org.apereo.cas.authentication.principal.WebApplicationService;
-import org.apereo.cas.services.RegisteredService;
+import org.apereo.cas.authentication.principal.WebApplicationServiceFactory;
+import org.apereo.cas.services.DefaultRegisteredServiceProperty;
+import org.apereo.cas.services.DefaultServicesManager;
+import org.apereo.cas.services.InMemoryServiceRegistry;
 import org.apereo.cas.services.RegisteredServiceProperty;
 import org.apereo.cas.services.RegisteredServiceProperty.RegisteredServiceProperties;
-import org.apereo.cas.services.ServicesManager;
+import org.apereo.cas.services.RegisteredServiceTestUtils;
 import org.apereo.cas.services.web.support.RegisteredServiceResponseHeadersEnforcementFilter;
-import org.apereo.cas.web.support.ArgumentExtractor;
+import org.apereo.cas.web.support.DefaultArgumentExtractor;
 
 import lombok.val;
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.mock.web.MockFilterChain;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -34,14 +36,15 @@ import static org.mockito.Mockito.*;
  * @since 5.3.0
  */
 @SpringBootTest(classes = RefreshAutoConfiguration.class)
-@RunWith(MockitoJUnitRunner.class)
 public class RegisteredServiceResponseHeadersEnforcementFilterTests {
 
     @Test
     public void verifyCacheControl() throws Exception {
         val filter = getFilterForProperty(RegisteredServiceProperties.HTTP_HEADER_ENABLE_CACHE_CONTROL);
         val response = new MockHttpServletResponse();
-        filter.doFilter(new MockHttpServletRequest(), response, new MockFilterChain());
+        val request = new MockHttpServletRequest();
+        request.addParameter(CasProtocolConstants.PARAMETER_SERVICE, "service-0");
+        filter.doFilter(request, response, new MockFilterChain());
         assertNotNull(response.getHeader("Cache-Control"));
     }
 
@@ -61,7 +64,9 @@ public class RegisteredServiceResponseHeadersEnforcementFilterTests {
         val filter = getFilterForProperty(RegisteredServiceProperties.HTTP_HEADER_ENABLE_STRICT_TRANSPORT_SECURITY);
         val response = new MockHttpServletResponse();
         val request = new MockHttpServletRequest();
+        request.addParameter(CasProtocolConstants.PARAMETER_SERVICE, "service-0");
         request.setSecure(true);
+        filter.doFilter(request, response, new MockFilterChain());
         filter.doFilter(request, response, new MockFilterChain());
         assertNotNull(response.getHeader("Strict-Transport-Security"));
     }
@@ -70,39 +75,62 @@ public class RegisteredServiceResponseHeadersEnforcementFilterTests {
     public void verifyXContentOptions() throws Exception {
         val filter = getFilterForProperty(RegisteredServiceProperties.HTTP_HEADER_ENABLE_XCONTENT_OPTIONS);
         val response = new MockHttpServletResponse();
-        filter.doFilter(new MockHttpServletRequest(), response, new MockFilterChain());
+        val request = new MockHttpServletRequest();
+        request.addParameter(CasProtocolConstants.PARAMETER_SERVICE, "service-0");
+        filter.doFilter(request, response, new MockFilterChain());
         assertNotNull(response.getHeader("X-Content-Type-Options"));
     }
 
     @Test
     public void verifyXframeOptions() throws Exception {
-        val filter = getFilterForProperty(RegisteredServiceProperties.HTTP_HEADER_ENABLE_XFRAME_OPTIONS);
-        val response = new MockHttpServletResponse();
-        filter.doFilter(new MockHttpServletRequest(), response, new MockFilterChain());
-        assertNotNull(response.getHeader("X-Frame-Options"));
+        val filter = getFilterForProperty(Pair.of(RegisteredServiceProperties.HTTP_HEADER_ENABLE_XFRAME_OPTIONS, "true"),
+            Pair.of(RegisteredServiceProperties.HTTP_HEADER_XFRAME_OPTIONS, "sameorigin"));
+
+        filter.setXFrameOptions("some-other-value");
+        filter.setEnableXFrameOptions(true);
+
+        var response = new MockHttpServletResponse();
+        val request = new MockHttpServletRequest();
+        request.setParameter(CasProtocolConstants.PARAMETER_SERVICE, "service-0");
+        filter.doFilter(request, response, new MockFilterChain());
+        assertEquals("sameorigin", response.getHeader("X-Frame-Options"));
+
+        response = new MockHttpServletResponse();
+        request.setParameter(CasProtocolConstants.PARAMETER_SERVICE, "service-something-else");
+        filter.doFilter(request, response, new MockFilterChain());
+        assertEquals("some-other-value", response.getHeader("X-Frame-Options"));
     }
 
     @Test
     public void verifyXssProtection() throws Exception {
         val filter = getFilterForProperty(RegisteredServiceProperties.HTTP_HEADER_ENABLE_XSS_PROTECTION);
         val response = new MockHttpServletResponse();
-        filter.doFilter(new MockHttpServletRequest(), response, new MockFilterChain());
+        val request = new MockHttpServletRequest();
+        request.addParameter(CasProtocolConstants.PARAMETER_SERVICE, "service-0");
+        filter.doFilter(request, response, new MockFilterChain());
         assertNotNull(response.getHeader("X-XSS-Protection"));
     }
 
     private static RegisteredServiceResponseHeadersEnforcementFilter getFilterForProperty(final RegisteredServiceProperties p) {
-        val servicesManager = mock(ServicesManager.class);
-        val argumentExtractor = mock(ArgumentExtractor.class);
-        val webApplicationService = mock(WebApplicationService.class);
-        when(argumentExtractor.extractService(any(HttpServletRequest.class))).thenReturn(webApplicationService);
+        return getFilterForProperty(Pair.of(p, "true"));
+    }
 
-        val registeredService = mock(RegisteredService.class);
-        val props = new LinkedHashMap<String, RegisteredServiceProperty>();
-        val prop = mock(RegisteredServiceProperty.class);
-        when(prop.getValue()).thenReturn(Boolean.TRUE.toString());
-        props.put(p.getPropertyName(), prop);
-        when(registeredService.getProperties()).thenReturn(props);
-        when(servicesManager.findServiceBy(any(Service.class))).thenReturn(registeredService);
+    private static RegisteredServiceResponseHeadersEnforcementFilter getFilterForProperty(final Pair<RegisteredServiceProperties, String>... properties) {
+        val servicesManager = new DefaultServicesManager(new InMemoryServiceRegistry(),
+            mock(ApplicationEventPublisher.class), new LinkedHashSet<>());
+        val argumentExtractor = new DefaultArgumentExtractor(new WebApplicationServiceFactory());
+
+        val service = RegisteredServiceTestUtils.getRegisteredService("service-0");
+        val props1 = new LinkedHashMap<String, RegisteredServiceProperty>();
+        for (var i = 0; i < properties.length; i++) {
+            val p = properties[i];
+            val prop1 = new DefaultRegisteredServiceProperty();
+            prop1.addValue(p.getValue());
+            props1.put(p.getKey().getPropertyName(), prop1);
+        }
+        service.setProperties(props1);
+        servicesManager.save(service);
+
         return new RegisteredServiceResponseHeadersEnforcementFilter(servicesManager, argumentExtractor,
             new DefaultAuthenticationServiceSelectionPlan(new DefaultAuthenticationServiceSelectionStrategy()));
     }
