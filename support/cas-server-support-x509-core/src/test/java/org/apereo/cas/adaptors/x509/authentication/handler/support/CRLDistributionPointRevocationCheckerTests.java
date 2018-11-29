d80224bc986acfaa77692c66cb047017e8bd02a8
Timur Duehr <tduehr@gmail.com>
2018-11-29 13:23:54 -0500

    refactor CRL parameterized tests

--- support/cas-server-support-x509-core/src/test/java/org/apereo/cas/adaptors/x509/authentication/handler/support/CRLDistributionPointRevocationCheckerTests.java
+++ support/cas-server-support-x509-core/src/test/java/org/apereo/cas/adaptors/x509/authentication/handler/support/CRLDistributionPointRevocationCheckerTests.java
@@ -3,7 +3,6 @@ package org.apereo.cas.adaptors.x509.authentication.handler.support;
 import org.apereo.cas.adaptors.x509.authentication.ExpiredCRLException;
 import org.apereo.cas.adaptors.x509.authentication.revocation.RevokedCertificateException;
 import org.apereo.cas.adaptors.x509.authentication.revocation.checker.CRLDistributionPointRevocationChecker;
-import org.apereo.cas.adaptors.x509.authentication.revocation.checker.RevocationChecker;
 import org.apereo.cas.adaptors.x509.authentication.revocation.policy.AllowRevocationPolicy;
 import org.apereo.cas.adaptors.x509.authentication.revocation.policy.ThresholdExpiredCRLRevocationPolicy;
 import org.apereo.cas.util.MockWebServer;
@@ -15,21 +14,23 @@ import net.sf.ehcache.CacheManager;
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.security.GeneralSecurityException;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.*;
 
 
 /**
@@ -38,49 +39,33 @@ import java.util.Collection;
  * @author Marvin S. Addison
  * @since 3.4.76
  */
-@RunWith(Parameterized.class)
 @Slf4j
-public class CRLDistributionPointRevocationCheckerTests extends AbstractCRLRevocationCheckerTests {
-
-
-    /**
-     * Instance under test.
-     */
-    private final CRLDistributionPointRevocationChecker checker;
+public class CRLDistributionPointRevocationCheckerTests extends BaseCRLRevocationCheckerTests {
 
     /**
      * Answers requests for CRLs made to localhost:8085.
      */
-    private final MockWebServer webServer;
+    private MockWebServer webServer;
 
-
-    /**
-     * Creates a new test instance with given parameters.
-     *
-     * @param checker   Revocation checker instance.
-     * @param certFiles File names of certificates to check.
-     * @param crlFile   File name of CRL file to serve out.
-     * @param expected  Expected result of check; null to indicate expected success.
-     */
-    public CRLDistributionPointRevocationCheckerTests(
+    @ParameterizedTest
+    @MethodSource("getTestParameters")
+    public void checkCertificate(
         final CRLDistributionPointRevocationChecker checker,
         final String[] certFiles,
         final String crlFile,
-        final GeneralSecurityException expected) throws Exception {
-
-        super(certFiles, expected);
+        final GeneralSecurityException expected) throws IOException, InterruptedException {
 
         val file = new File(System.getProperty("java.io.tmpdir"), "ca.crl");
-        if (file.exists()) {
-            file.delete();
-        }
         val out = new FileOutputStream(file);
         IOUtils.copy(new ClassPathResource(crlFile).getInputStream(), out);
 
-        this.checker = checker;
-
         this.webServer = new MockWebServer(8085, new FileSystemResource(file), "text/plain");
+
+        this.webServer.start();
         LOGGER.debug("Web server listening on port 8085 serving file [{}]", crlFile);
+        Thread.sleep(500);
+
+        super.checkCertificate(checker, certFiles, expected);
     }
 
     /**
@@ -88,10 +73,9 @@ public class CRLDistributionPointRevocationCheckerTests extends AbstractCRLRevoc
      *
      * @return Test parameter data.
      */
-    @Parameters
-    public static Collection<Object[]> getTestParameters() {
+    public static Stream<Arguments> getTestParameters() {
         CacheManager.getInstance().removeAllCaches();
-        val params = new ArrayList<Object[]>();
+        val params = new ArrayList<Arguments>();
         var cache = (Cache) null;
         val defaultPolicy = new ThresholdExpiredCRLRevocationPolicy(0);
         val zeroThresholdPolicy = new ThresholdExpiredCRLRevocationPolicy(0);
@@ -100,72 +84,71 @@ public class CRLDistributionPointRevocationCheckerTests extends AbstractCRLRevoc
         // Valid certificate on valid CRL data with encoded url
         cache = new Cache("crlCache-0", 100, false, false, 20, 10);
         CacheManager.getInstance().addCache(cache);
-        params.add(new Object[]{
+        params.add(arguments(
             new CRLDistributionPointRevocationChecker(cache, defaultPolicy, null),
             new String[]{"uservalid-encoded-crl.crt"},
             "test ca.crl",
-            null,
-        });
+            null
+        ));
 
         // Test case #1
         // Valid certificate on valid CRL data
         cache = new Cache("crlCache-1", 100, false, false, 20, 10);
         CacheManager.getInstance().addCache(cache);
-        params.add(new Object[]{
+        params.add(arguments(
             new CRLDistributionPointRevocationChecker(cache, defaultPolicy, null, true),
             new String[]{"user-valid-distcrl.crt"},
             "userCA-valid.crl",
-            null,
-        });
+            null
+        ));
 
 
         // Test case #2
         // Revoked certificate on valid CRL data
         cache = new Cache("crlCache-2", 100, false, false, 20, 10);
         CacheManager.getInstance().addCache(cache);
-        params.add(new Object[]{
+        params.add(arguments(
             new CRLDistributionPointRevocationChecker(cache, defaultPolicy, null),
             new String[]{"user-revoked-distcrl.crt"},
             "userCA-valid.crl",
-            new RevokedCertificateException(ZonedDateTime.now(ZoneOffset.UTC), new BigInteger("1")),
-        });
+            new RevokedCertificateException(ZonedDateTime.now(ZoneOffset.UTC), new BigInteger("1"))
+        ));
 
         // Test case #3
         // Valid certificate on expired CRL data
         cache = new Cache("crlCache-3", 100, false, false, 20, 10);
         CacheManager.getInstance().addCache(cache);
-        params.add(new Object[]{
+        params.add(arguments(
             new CRLDistributionPointRevocationChecker(cache, zeroThresholdPolicy, null),
             new String[]{"user-valid-distcrl.crt"},
             "userCA-expired.crl",
-            new ExpiredCRLException("test", ZonedDateTime.now(ZoneOffset.UTC)),
-        });
+            new ExpiredCRLException("test", ZonedDateTime.now(ZoneOffset.UTC))
+        ));
 
         // Test case #4
         // Valid certificate on expired CRL data with custom expiration
         // policy to always allow expired CRL data
         cache = new Cache("crlCache-4", 100, false, false, 20, 10);
         CacheManager.getInstance().addCache(cache);
-        params.add(new Object[]{
+        params.add(arguments(
             new CRLDistributionPointRevocationChecker(cache, crl -> {
             }, null),
             new String[]{"user-valid-distcrl.crt"},
             "userCA-expired.crl",
-            null,
-        });
+            null
+        ));
 
         // Test case #5
         // Valid certificate with no CRL distribution points defined but with
         // "AllowRevocationPolicy" set to allow unavailable CRL data
         cache = new Cache("crlCache-5", 100, false, false, 20, 10);
         CacheManager.getInstance().addCache(cache);
-        val checker5 =
-            new CRLDistributionPointRevocationChecker(cache, defaultPolicy, new AllowRevocationPolicy());
-        params.add(new Object[]{checker5,
+        params.add(arguments(
+            new CRLDistributionPointRevocationChecker(cache, defaultPolicy, new AllowRevocationPolicy()),
             new String[]{"user-valid.crt"},
             "userCA-expired.crl",
-            null,
-        });
+            null
+        ));
 
         // Test case #6
         // EJBCA test case
@@ -175,14 +158,14 @@ public class CRLDistributionPointRevocationCheckerTests extends AbstractCRLRevoc
         // Make sure we can convert given URI to valid URI and confirm it's revoked
         cache = new Cache("crlCache-6", 100, false, false, 20, 10);
         CacheManager.getInstance().addCache(cache);
-        params.add(new Object[]{
+        params.add(arguments(
             new CRLDistributionPointRevocationChecker(cache, defaultPolicy, null),
             new String[]{"user-revoked-distcrl2.crt"},
             "userCA-valid.crl",
-            new RevokedCertificateException(ZonedDateTime.now(ZoneOffset.UTC), new BigInteger("1")),
-        });
+            new RevokedCertificateException(ZonedDateTime.now(ZoneOffset.UTC), new BigInteger("1"))
+        ));
 
-        return params;
+        return params.stream();
     }
 
     @AfterAll
@@ -195,27 +178,11 @@ public class CRLDistributionPointRevocationCheckerTests extends AbstractCRLRevoc
 
     /**
      * Called once before every test.
-     *
-     * @throws Exception On setup errors.
-     */
-    @BeforeEach
-    public void initialize() throws Exception {
-        this.webServer.start();
-        Thread.sleep(500);
-    }
-
-    /**
-     * Called once before every test.
      */
     @AfterEach
     public void afterEachTest() {
         LOGGER.debug("Stopping web server...");
         this.webServer.stop();
         LOGGER.debug("Web server stopped [{}]", !this.webServer.isRunning());
-    }
-
-    @Override
-    protected RevocationChecker getChecker() {
-        return this.checker;
     }
 }
--- support/cas-server-support-x509-core/src/test/java/org/apereo/cas/adaptors/x509/authentication/handler/support/ThresholdExpiredCRLRevocationPolicyTests.java
+++ support/cas-server-support-x509-core/src/test/java/org/apereo/cas/adaptors/x509/authentication/handler/support/ThresholdExpiredCRLRevocationPolicyTests.java
@@ -7,20 +7,19 @@ import org.apereo.cas.util.DateTimeUtils;
 
 import lombok.RequiredArgsConstructor;
 import lombok.val;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import javax.security.auth.x500.X500Principal;
 import java.security.GeneralSecurityException;
 import java.security.cert.X509CRL;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.apereo.cas.util.junit.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.*;
 
 /**
  * Unit test for {@link ThresholdExpiredCRLRevocationPolicy} class.
@@ -28,90 +27,53 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author Marvin S. Addison
  * @since 3.4.7
  */
-@RunWith(Parameterized.class)
 @RequiredArgsConstructor
 public class ThresholdExpiredCRLRevocationPolicyTests {
-    /**
-     * Policy instance under test.
-     */
-    private final ThresholdExpiredCRLRevocationPolicy policy;
-
-    /**
-     * CRL to test.
-     */
-    private final X509CRL crl;
-
-    /**
-     * Expected result of check; null for success
-     */
-    private final GeneralSecurityException expected;
-
-
     /**
      * Gets the unit test parameters.
      *
      * @return Test parameter data.
      */
-    @Parameters
-    public static Collection<Object[]> getTestParameters() {
-        val params = new ArrayList<Object[]>();
-
+    public static Stream<Arguments> getTestParameters() {
         val now = ZonedDateTime.now(ZoneOffset.UTC);
         val twoHoursAgo = now.minusHours(2);
         val oneHourAgo = now.minusHours(1);
         val halfHourAgo = now.minusMinutes(30);
         val issuer = new X500Principal("CN=CAS");
 
-        // Test case #1
-        // Expect expired for zero leniency on CRL expiring 1ms ago
-        val zeroThreshold = new ThresholdExpiredCRLRevocationPolicy(0);
-        params.add(new Object[]{
-            zeroThreshold,
-            new MockX509CRL(issuer, DateTimeUtils.dateOf(oneHourAgo), DateTimeUtils.dateOf(now.minusSeconds(1))),
-            new ExpiredCRLException("CN=CAS", ZonedDateTime.now(ZoneOffset.UTC)),
-        });
+        return Stream.of(
+            // Test case #1
+            // Expect expired for zero leniency on CRL expiring 1ms ago
+            arguments(
+                new ThresholdExpiredCRLRevocationPolicy(0),
+                new MockX509CRL(issuer, DateTimeUtils.dateOf(oneHourAgo), DateTimeUtils.dateOf(now.minusSeconds(1))),
+                new ExpiredCRLException("CN=CAS", ZonedDateTime.now(ZoneOffset.UTC))
+            ),
 
-        // Test case #2
-        // Expect expired for 1h leniency on CRL expired 1 hour 1ms ago
-        val oneHourThreshold = new ThresholdExpiredCRLRevocationPolicy(3600);
-        params.add(new Object[]{
-            oneHourThreshold,
-            new MockX509CRL(issuer, DateTimeUtils.dateOf(twoHoursAgo), DateTimeUtils.dateOf(oneHourAgo.minusSeconds(1))),
-            new ExpiredCRLException("CN=CAS", ZonedDateTime.now(ZoneOffset.UTC)),
-        });
+            // Test case #2
+            // Expect expired for 1h leniency on CRL expired 1 hour 1ms ago
+            arguments(
+                new ThresholdExpiredCRLRevocationPolicy(3600),
+                new MockX509CRL(issuer, DateTimeUtils.dateOf(twoHoursAgo), DateTimeUtils.dateOf(oneHourAgo.minusSeconds(1))),
+                new ExpiredCRLException("CN=CAS", ZonedDateTime.now(ZoneOffset.UTC))
+            ),
 
-        // Test case #3
-        // Expect valid for 1h leniency on CRL expired 30m ago
-        params.add(new Object[]{
-            oneHourThreshold,
-            new MockX509CRL(issuer, DateTimeUtils.dateOf(twoHoursAgo), DateTimeUtils.dateOf(halfHourAgo)),
-            null,
-        });
-
-        return params;
+            // Test case #3
+            // Expect valid for 1h leniency on CRL expired 30m ago
+            arguments(
+                new ThresholdExpiredCRLRevocationPolicy(3600),
+                new MockX509CRL(issuer, DateTimeUtils.dateOf(twoHoursAgo), DateTimeUtils.dateOf(halfHourAgo)),
+                null
+            )
+        );
     }
 
     /**
      * Test method for {@link ThresholdExpiredCRLRevocationPolicy#apply(java.security.cert.X509CRL)}.
      */
-    @Test
-    public void verifyApply() {
-        try {
-            this.policy.apply(this.crl);
-            if (this.expected != null) {
-                fail("Expected exception of type " + this.expected.getClass());
-            }
-        } catch (final GeneralSecurityException e) {
-            if (this.expected == null) {
-                e.printStackTrace();
-                fail("Revocation check failed unexpectedly with exception: " + e);
-            } else {
-                val expectedClass = this.expected.getClass();
-                val actualClass = e.getClass();
-                assertTrue(
-                    expectedClass.isAssignableFrom(actualClass),
-                    String.format("Expected exception of type %s but got %s", expectedClass, actualClass));
-            }
-        }
+    @ParameterizedTest
+    @MethodSource("getTestParameters")
+    public void verifyApply(final ThresholdExpiredCRLRevocationPolicy policy, final X509CRL crl, final GeneralSecurityException expected) {
+        assertThrowsOrNot(expected, () -> policy.apply(crl));
     }
 }
