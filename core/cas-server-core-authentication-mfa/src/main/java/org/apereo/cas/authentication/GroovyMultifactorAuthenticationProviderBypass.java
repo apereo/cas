9f78ebbf5d0bbeac72398d989a47427db548fad7
Misagh Moayyed <mm1844@gmail.com>
2018-12-07 10:47:28 -0700

    let groovy mfa bypass be watchable

--- core/cas-server-core-authentication-mfa/src/main/java/org/apereo/cas/authentication/GroovyMultifactorAuthenticationProviderBypass.java
+++ core/cas-server-core-authentication-mfa/src/main/java/org/apereo/cas/authentication/GroovyMultifactorAuthenticationProviderBypass.java
@@ -1,11 +1,12 @@
 package org.apereo.cas.authentication;
 
-import lombok.extern.slf4j.Slf4j;
 import org.apereo.cas.authentication.principal.Principal;
 import org.apereo.cas.configuration.model.support.mfa.MultifactorAuthenticationProviderBypassProperties;
 import org.apereo.cas.services.MultifactorAuthenticationProvider;
 import org.apereo.cas.services.RegisteredService;
-import org.apereo.cas.util.scripting.ScriptingUtils;
+import org.apereo.cas.util.scripting.WatchableGroovyScriptResource;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,11 +20,11 @@ import javax.servlet.http.HttpServletRequest;
 @Slf4j
 public class GroovyMultifactorAuthenticationProviderBypass implements MultifactorAuthenticationProviderBypass {
     private static final long serialVersionUID = -4909072898415688377L;
-
-    private final transient Resource groovyScript;
+    private final WatchableGroovyScriptResource watchableScript;
 
     public GroovyMultifactorAuthenticationProviderBypass(final MultifactorAuthenticationProviderBypassProperties bypass) {
-        this.groovyScript = bypass.getGroovy().getLocation();
+        final Resource groovyScript = bypass.getGroovy().getLocation();
+        this.watchableScript = new WatchableGroovyScriptResource(groovyScript);
     }
 
     @Override
@@ -35,9 +36,9 @@ public class GroovyMultifactorAuthenticationProviderBypass implements Multifacto
             final Principal principal = authentication.getPrincipal();
             LOGGER.debug("Evaluating multifactor authentication bypass properties for principal [{}], "
                     + "service [{}] and provider [{}] via Groovy script [{}]",
-                principal.getId(), registeredService, provider, this.groovyScript);
-            return ScriptingUtils.executeGroovyScript(this.groovyScript,
-                new Object[]{authentication, principal, registeredService, provider, LOGGER, request}, Boolean.class);
+                principal.getId(), registeredService, provider, watchableScript.getResource());
+            final Object[] args = {authentication, principal, registeredService, provider, LOGGER, request};
+            return watchableScript.execute(args, Boolean.class);
         } catch (final Exception e) {
             LOGGER.error(e.getMessage(), e);
             return true;
