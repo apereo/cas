7bfa0196f589c1baaeddc7d847fa9cf7f66b7451
Misagh Moayyed <mm1844@gmail.com>
2018-12-11 15:35:57 -0700

    added log statements to wsfed ops

--- support/cas-server-support-ws-idp/src/main/java/org/apereo/cas/ws/idp/authentication/WSFederationAuthenticationServiceSelectionStrategy.java
+++ support/cas-server-support-ws-idp/src/main/java/org/apereo/cas/ws/idp/authentication/WSFederationAuthenticationServiceSelectionStrategy.java
@@ -5,6 +5,8 @@ import org.apereo.cas.authentication.principal.Service;
 import org.apereo.cas.authentication.principal.ServiceFactory;
 import org.apereo.cas.ws.idp.WSFederationConstants;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URIBuilder;
@@ -19,15 +21,13 @@ import java.util.Optional;
  * @since 5.1.0
  */
 @Slf4j
+@Getter
+@RequiredArgsConstructor
 public class WSFederationAuthenticationServiceSelectionStrategy implements AuthenticationServiceSelectionStrategy {
     private static final long serialVersionUID = 8035218407906419228L;
 
     private final int order = Ordered.HIGHEST_PRECEDENCE;
     private final transient ServiceFactory webApplicationServiceFactory;
-
-    public WSFederationAuthenticationServiceSelectionStrategy(final ServiceFactory webApplicationServiceFactory) {
-        this.webApplicationServiceFactory = webApplicationServiceFactory;
-    }
 
     @Override
     public Service resolveServiceFrom(final Service service) {
@@ -39,12 +39,28 @@ public class WSFederationAuthenticationServiceSelectionStrategy implements Authe
                 return this.webApplicationServiceFactory.createService(serviceReply);
             }
         }
+        LOGGER.debug("Resolved final service as [{}]", service);
         return service;
     }
 
     @Override
     public boolean supports(final Service service) {
-        return service != null && getRealmAsParameter(service).isPresent() && getReplyAsParameter(service).isPresent();
+        if (service == null) {
+            LOGGER.debug("Provided service is undefined");
+            return false;
+        }
+        LOGGER.debug("Evaluating service requested identified as [{}]", service.getId());
+        final Optional<NameValuePair> realmAsParameter = getRealmAsParameter(service);
+        if (!realmAsParameter.isPresent()) {
+            LOGGER.debug("Parameter [{}] is undefined in the request", WSFederationConstants.WTREALM);
+            return false;
+        }
+        final Optional<NameValuePair> replyAsParameter = getReplyAsParameter(service);
+        if (!replyAsParameter.isPresent()) {
+            LOGGER.debug("Parameter [{}] is undefined in the request", WSFederationConstants.WREPLY);
+            return false;
+        }
+        return true;
     }
 
     private static Optional<NameValuePair> getRealmAsParameter(final Service service) {
