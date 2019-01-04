a8897cda16ccb6b6d6258216adb4d9f4149e6bfe
Martin Böhmer <MartinBoehmer@users.noreply.github.com>
2019-01-04 01:49:08 +0100

    Made AUP over JDBC more flexible in terms of saving the user decision to the database.

--- support/cas-server-support-aup-jdbc/src/main/java/org/apereo/cas/aup/JdbcAcceptableUsagePolicyRepository.java
+++ support/cas-server-support-aup-jdbc/src/main/java/org/apereo/cas/aup/JdbcAcceptableUsagePolicyRepository.java
@@ -7,6 +7,11 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.webflow.execution.RequestContext;
 
 import javax.sql.DataSource;
+import lombok.NonNull;
+import org.apache.commons.lang3.StringUtils;
+import org.apereo.cas.authentication.principal.Principal;
+import org.apereo.cas.configuration.model.support.aup.AcceptableUsagePolicyProperties;
+import org.apereo.cas.web.support.WebUtils;
 
 /**
  * This is {@link JdbcAcceptableUsagePolicyRepository}.
@@ -23,23 +28,44 @@ public class JdbcAcceptableUsagePolicyRepository extends AbstractPrincipalAttrib
     private static final long serialVersionUID = 1600024683199961892L;
     
     private final transient JdbcTemplate jdbcTemplate;
-    private final String tableName;
+    private final AcceptableUsagePolicyProperties properties;
 
     public JdbcAcceptableUsagePolicyRepository(final TicketRegistrySupport ticketRegistrySupport,
                                                final String aupAttributeName,
                                                final DataSource dataSource,
-                                               final String tableName) {
+                                               final AcceptableUsagePolicyProperties properties) {
         super(ticketRegistrySupport, aupAttributeName);
         this.jdbcTemplate = new JdbcTemplate(dataSource);
-        this.tableName = tableName;
+        this.properties = properties;
     }
 
     @Override
     public boolean submit(final RequestContext requestContext, final Credential credential) {
         try {
-            final String sql = String.format("UPDATE %s SET %s=true WHERE username=?", this.tableName, this.aupAttributeName);
-            LOGGER.debug("Executing update query [{}]", sql);
-            return this.jdbcTemplate.update(sql, credential.getId()) > 0;
+            final AcceptableUsagePolicyProperties.Jdbc jdbc = properties.getJdbc();
+            String aupColumnName = properties.getAupAttributeName();
+            if (StringUtils.isNotBlank(jdbc.getAupColumn())) {
+                aupColumnName = jdbc.getAupColumn();
+            }
+            final String sql = String.format(jdbc.getSqlUpdateAUP(), jdbc.getTableName(), aupColumnName, jdbc.getPrincipalIdColumn());
+            String principalId = credential.getId();
+            if (StringUtils.isNotBlank(jdbc.getPrincipalIdAttribute())) {
+                @NonNull
+                final Principal principal = WebUtils.getAuthentication(requestContext).getPrincipal();
+                String pIdAttribName = jdbc.getPrincipalIdAttribute();
+                if (principal.getAttributes().containsKey(pIdAttribName)) {
+                    Object pIdAttribValue = principal.getAttributes().get(pIdAttribName);
+                    if (pIdAttribValue instanceof String) {
+                        principalId = pIdAttribValue.toString();
+                    } else {
+                        LOGGER.warn("Principal attribute [{}] was found, but its value [{}] is not a String", pIdAttribName, pIdAttribValue);
+                    }
+                } else {
+                    LOGGER.warn("Principal attribute [{}] cannot be found. Falling back to principal ID", pIdAttribName);
+                }
+            }
+            LOGGER.debug("Executing update query [{}] for principal [{}]", sql, principalId);
+            return this.jdbcTemplate.update(sql, principalId) > 0;
         } catch (final Exception e) {
             LOGGER.error(e.getMessage(), e);
         }
