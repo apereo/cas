69619591f7d0161ceb5cedaa31747c97e861bcb2
Misagh Moayyed <mm1844@gmail.com>
2019-01-15 12:34:50 -0700

    minor cleanup

--- support/cas-server-support-themes/src/main/java/org/apereo/cas/services/web/RequestHeaderThemeResolver.java
+++ support/cas-server-support-themes/src/main/java/org/apereo/cas/services/web/RequestHeaderThemeResolver.java
@@ -1,5 +1,7 @@
 package org.apereo.cas.services.web;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.servlet.theme.AbstractThemeResolver;
@@ -13,10 +15,14 @@ import javax.servlet.http.HttpServletResponse;
  * @author Misagh Moayyed
  * @since 5.2.0
  */
+@Slf4j
+@RequiredArgsConstructor
 public class RequestHeaderThemeResolver extends AbstractThemeResolver {
+    private final String themeHeaderName;
+
     @Override
     public String resolveThemeName(final HttpServletRequest request) {
-        val theme = request.getHeader("theme");
+        val theme = request.getHeader(this.themeHeaderName);
         return StringUtils.defaultIfBlank(theme, getDefaultThemeName());
     }
 
