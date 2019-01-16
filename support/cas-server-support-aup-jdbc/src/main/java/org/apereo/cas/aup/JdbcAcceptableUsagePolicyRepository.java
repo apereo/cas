23aafdff625ffb0b10c6314ffdb070de660589ce
Martin Böhmer <MartinBoehmer@users.noreply.github.com>
2019-01-16 12:07:55 +0100

    Configuration improvements for AUP JDBC (port forward to 6.0.x) (#3746)
    * Made AUP over JDBC more flexible in terms of saving the user decision to the database.
    
    * Added test for new AUP JDBC configuration. Refactored existing tests as needed.
    
    * Checkstyle improvements.
    
    * Added test suite for AUP JDBC.
    
    * Revised handling mutli-valued and empty attribute values.

--- support/cas-server-support-aup-jdbc/src/main/java/org/apereo/cas/aup/JdbcAcceptableUsagePolicyRepository.java
+++ support/cas-server-support-aup-jdbc/src/main/java/org/apereo/cas/aup/JdbcAcceptableUsagePolicyRepository.java
@@ -1,10 +1,15 @@
 package org.apereo.cas.aup;
 
 import org.apereo.cas.authentication.Credential;
+import org.apereo.cas.configuration.model.support.aup.AcceptableUsagePolicyProperties;
 import org.apereo.cas.ticket.registry.TicketRegistrySupport;
+import org.apereo.cas.util.CollectionUtils;
+import org.apereo.cas.web.support.WebUtils;
 
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.webflow.execution.RequestContext;
 
@@ -25,26 +30,66 @@ public class JdbcAcceptableUsagePolicyRepository extends AbstractPrincipalAttrib
     private static final long serialVersionUID = 1600024683199961892L;
 
     private final transient JdbcTemplate jdbcTemplate;
-    private final String tableName;
+    private final AcceptableUsagePolicyProperties properties;
 
     public JdbcAcceptableUsagePolicyRepository(final TicketRegistrySupport ticketRegistrySupport,
                                                final String aupAttributeName,
                                                final DataSource dataSource,
-                                               final String tableName) {
+                                               final AcceptableUsagePolicyProperties properties) {
         super(ticketRegistrySupport, aupAttributeName);
         this.jdbcTemplate = new JdbcTemplate(dataSource);
-        this.tableName = tableName;
+        this.properties = properties;
     }
 
     @Override
     public boolean submit(final RequestContext requestContext, final Credential credential) {
         try {
-            val sql = String.format("UPDATE %s SET %s=true WHERE username=?", this.tableName, this.aupAttributeName);
-            LOGGER.debug("Executing update query [{}]", sql);
-            return this.jdbcTemplate.update(sql, credential.getId()) > 0;
+            val jdbc = properties.getJdbc();
+            var aupColumnName = properties.getAupAttributeName();
+            if (StringUtils.isNotBlank(jdbc.getAupColumn())) {
+                aupColumnName = jdbc.getAupColumn();
+            }
+            val sql = String.format(jdbc.getSqlUpdateAUP(), jdbc.getTableName(), aupColumnName, jdbc.getPrincipalIdColumn());
+            val principalId = determinePrincipalId(requestContext, credential);
+            LOGGER.debug("Executing update query [{}] for principal [{}]", sql, principalId);
+            return this.jdbcTemplate.update(sql, principalId) > 0;
         } catch (final Exception e) {
             LOGGER.error(e.getMessage(), e);
         }
         return false;
+    }
+    
+    /**
+     * Extracts principal ID from a principal attribute or the provided credentials.
+     * 
+     * @param requestContext the context
+     * @param credential the credential
+     * @return the principal ID to update the AUP setting in the database for
+     */
+    protected String determinePrincipalId(final RequestContext requestContext, final Credential credential) {
+        if (StringUtils.isBlank(properties.getJdbc().getPrincipalIdAttribute())) {
+            return credential.getId();
+        }
+        @NonNull
+        val principal = WebUtils.getAuthentication(requestContext).getPrincipal();
+        val pIdAttribName = properties.getJdbc().getPrincipalIdAttribute();
+        if (!principal.getAttributes().containsKey(pIdAttribName)) {
+            throw new IllegalStateException("Principal attribute [" + pIdAttribName + "] cannot be found");
+        }
+        val pIdAttributeValue = principal.getAttributes().get(pIdAttribName);
+        val pIdAttributeValues = CollectionUtils.toCollection(pIdAttributeValue);
+        var principalId = "";
+        if (!pIdAttributeValues.isEmpty()) {
+            principalId = pIdAttributeValues.iterator().next().toString().trim();
+        }
+        if (pIdAttributeValues.size() > 1) {
+            LOGGER.warn("Principal attribute [{}] was found, but its value [{}] is multi-valued. "
+                    + "Proceeding with the first element [{}]", pIdAttribName, pIdAttributeValue, principalId);
+        }
+        if (principalId.isEmpty()) {
+            throw new IllegalStateException("Principal attribute [" + pIdAttribName + "] was found, but it is either empty"
+                    + " or multi-valued with an empty element");
+        }
+        return principalId;
     }
 }
