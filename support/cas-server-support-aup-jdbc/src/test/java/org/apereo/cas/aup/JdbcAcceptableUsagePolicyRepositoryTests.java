2797ba0c445650f52ff0974ec71b582044f1b536
Martin Böhmer <MartinBoehmer@users.noreply.github.com>
2019-01-16 12:08:17 +0100

    Configuration improvements for AUP JDBC (#3733)
    The database update of the AUP acceptance can now be configured in a very flexible way, if necessary. The defaults ensure backward compatibility of older production configurations.

--- support/cas-server-support-aup-jdbc/src/test/java/org/apereo/cas/aup/JdbcAcceptableUsagePolicyRepositoryTests.java
+++ support/cas-server-support-aup-jdbc/src/test/java/org/apereo/cas/aup/JdbcAcceptableUsagePolicyRepositoryTests.java
@@ -1,48 +1,14 @@
 package org.apereo.cas.aup;
 
-import org.apereo.cas.authentication.CoreAuthenticationTestUtils;
-import org.apereo.cas.authentication.Credential;
-import org.apereo.cas.config.CasAcceptableUsagePolicyJdbcConfiguration;
-import org.apereo.cas.config.CasAuthenticationEventExecutionPlanTestConfiguration;
-import org.apereo.cas.config.CasCoreAuthenticationPrincipalConfiguration;
-import org.apereo.cas.config.CasCoreAuthenticationServiceSelectionStrategyConfiguration;
-import org.apereo.cas.config.CasCoreConfiguration;
-import org.apereo.cas.config.CasCoreHttpConfiguration;
-import org.apereo.cas.config.CasCoreServicesConfiguration;
-import org.apereo.cas.config.CasCoreTicketCatalogConfiguration;
-import org.apereo.cas.config.CasCoreTicketIdGeneratorsConfiguration;
-import org.apereo.cas.config.CasCoreTicketsConfiguration;
-import org.apereo.cas.config.CasCoreUtilConfiguration;
-import org.apereo.cas.config.CasCoreWebConfiguration;
-import org.apereo.cas.config.CasDefaultServiceTicketIdGeneratorsConfiguration;
-import org.apereo.cas.config.CasPersonDirectoryTestConfiguration;
-import org.apereo.cas.config.CasRegisteredServicesTestConfiguration;
-import org.apereo.cas.config.support.CasWebApplicationServiceFactoryConfiguration;
-import org.apereo.cas.logout.config.CasCoreLogoutConfiguration;
 import org.apereo.cas.util.CollectionUtils;
-import org.apereo.cas.web.support.WebUtils;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.webflow.context.servlet.ServletExternalContext;
-import org.springframework.webflow.test.MockRequestContext;
-
-import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.Statement;
-import org.apereo.cas.authentication.Authentication;
-import org.apereo.cas.authentication.principal.Principal;
-
+import org.junit.After;
 import static org.junit.Assert.*;
+
 
 /**
  * This is {@link JdbcAcceptableUsagePolicyRepositoryTests}.
@@ -50,36 +16,8 @@ import static org.junit.Assert.*;
  * @author Misagh Moayyed
  * @since 5.3.0
  */
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = {
-    CasAcceptableUsagePolicyJdbcConfiguration.class,
-    RefreshAutoConfiguration.class,
-    CasCoreConfiguration.class,
-    CasCoreTicketsConfiguration.class,
-    CasCoreLogoutConfiguration.class,
-    CasCoreServicesConfiguration.class,
-    CasCoreTicketIdGeneratorsConfiguration.class,
-    CasCoreTicketCatalogConfiguration.class,
-    CasCoreAuthenticationServiceSelectionStrategyConfiguration.class,
-    CasCoreHttpConfiguration.class,
-    CasCoreWebConfiguration.class,
-    CasPersonDirectoryTestConfiguration.class,
-    CasCoreUtilConfiguration.class,
-    CasRegisteredServicesTestConfiguration.class,
-    CasWebApplicationServiceFactoryConfiguration.class,
-    CasAuthenticationEventExecutionPlanTestConfiguration.class,
-    CasDefaultServiceTicketIdGeneratorsConfiguration.class,
-    CasCoreAuthenticationPrincipalConfiguration.class
-})
 @TestPropertySource(locations = {"classpath:/jdbc-aup.properties"})
-public class JdbcAcceptableUsagePolicyRepositoryTests {
-    @Autowired
-    @Qualifier("acceptableUsagePolicyRepository")
-    private AcceptableUsagePolicyRepository acceptableUsagePolicyRepository;
-
-    @Autowired
-    @Qualifier("acceptableUsagePolicyDataSource")
-    private DataSource acceptableUsagePolicyDataSource;
+public class JdbcAcceptableUsagePolicyRepositoryTests extends BaseJdbcAcceptableUsagePolicyRepositoryTests {
 
     @Before
     public void setUp() throws Exception {
@@ -90,18 +28,25 @@ public class JdbcAcceptableUsagePolicyRepositoryTests {
         s.execute("INSERT INTO aup_table (id, username, accepted) values (100, 'casuser', false);");
         c.close();
     }
+    
+    @After
+    public void tearDown() throws Exception {
+        final Connection c = this.acceptableUsagePolicyDataSource.getConnection();
+        final Statement s = c.createStatement();
+        c.setAutoCommit(true);
+        s.execute("DROP TABLE aup_table;");
+        c.close();
+    }
 
     @Test
-    public void verifyAction() {
-        final MockRequestContext context = new MockRequestContext();
-        final MockHttpServletRequest request = new MockHttpServletRequest();
-        context.setExternalContext(new ServletExternalContext(new MockServletContext(), request, new MockHttpServletResponse()));
-        final Credential c = CoreAuthenticationTestUtils.getCredentialsWithSameUsernameAndPassword("casuser");
-        final Principal pricipal = CoreAuthenticationTestUtils.getPrincipal(c.getId(), CollectionUtils.wrap("accepted", "false"));
-        final Authentication auth = CoreAuthenticationTestUtils.getAuthentication(pricipal);
-        WebUtils.putAuthentication(auth, context);
-
-        assertFalse(acceptableUsagePolicyRepository.verify(context, c).getLeft());
-        assertTrue(acceptableUsagePolicyRepository.submit(context, c));
+    public void verifyActionWithDefaultConfig() {
+        verifyAction("casuser", CollectionUtils.wrap("accepted", "false"));
     }
+    
+    @Test
+    public void determinePrincipalIdWithDefaultConfig() {
+        final String principalId = determinePrincipalId("casuser", CollectionUtils.wrap("accepted", "false"));
+        assertEquals("casuser", principalId);
+    }
+    
 }
