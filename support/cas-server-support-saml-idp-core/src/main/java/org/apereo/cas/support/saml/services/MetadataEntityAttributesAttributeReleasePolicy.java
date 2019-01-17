c78c1e55e88f8fa250ede0b3d44c19856f24af16
Misagh Moayyed <mm1844@gmail.com>
2019-01-17 03:29:31 -0700

    - attr rel policy for saml2 refeds - improvs to metadata fetching over urls - endpoint to manage saml2 metadata cache - doc updates

--- support/cas-server-support-saml-idp-core/src/main/java/org/apereo/cas/support/saml/services/MetadataEntityAttributesAttributeReleasePolicy.java
+++ support/cas-server-support-saml-idp-core/src/main/java/org/apereo/cas/support/saml/services/MetadataEntityAttributesAttributeReleasePolicy.java
@@ -46,13 +46,16 @@ public class MetadataEntityAttributesAttributeReleasePolicy extends BaseSamlRegi
                                                                         final SamlRegisteredServiceCachingMetadataResolver resolver,
                                                                         final SamlRegisteredServiceServiceProviderMetadataFacade facade,
                                                                         final EntityDescriptor entityDescriptor) {
-        val attr = new EntityAttributesPredicate.Candidate(this.entityAttribute, this.entityAttributeFormat);
-        attr.setValues(this.entityAttributeValues);
-        LOGGER.debug("Loading entity attribute predicate filter for candidate [{}] with values [{}]", attr.getName(), attr.getValues());
+        val attr = new EntityAttributesPredicate.Candidate(getEntityAttribute(), getEntityAttributeFormat());
+        attr.setValues(getEntityAttributeValues());
+        LOGGER.trace("Loading entity attribute predicate filter for candidate [{}] with values [{}]", attr.getName(), attr.getValues());
         val predicate = new EntityAttributesPredicate(CollectionUtils.wrap(attr), true);
         if (predicate.apply(entityDescriptor)) {
+            LOGGER.debug("Authorizing release of allowed attributes [{}] for entity id [{}]", attributes, entityDescriptor.getEntityID());
             return authorizeReleaseOfAllowedAttributes(attributes);
         }
+        LOGGER.debug("Unable to authorize attribute release for entity attribute category [{}] and value(s) [{}] to entity id [{}]",
+            getEntityAttribute(), getEntityAttributeValues(), entityDescriptor.getEntityID());
         return new HashMap<>(0);
     }
 }
