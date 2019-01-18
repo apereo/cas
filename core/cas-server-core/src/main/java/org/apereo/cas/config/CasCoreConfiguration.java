8b50b11f99706fcedda4ab555a1f378c597cde11
Dmitriy Kopylenko <dima767@gmail.com>
2019-01-18 14:14:40 -0500

    Debug instrumentation (#3753)
    Debug logging instrumentation

--- core/cas-server-core/src/main/java/org/apereo/cas/config/CasCoreConfiguration.java
+++ core/cas-server-core/src/main/java/org/apereo/cas/config/CasCoreConfiguration.java
@@ -83,8 +83,10 @@ public class CasCoreConfiguration {
     @ConditionalOnMissingBean(name = "authenticationPolicyFactory")
     public ContextualAuthenticationPolicyFactory<ServiceContext> authenticationPolicyFactory() {
         if (casProperties.getAuthn().getPolicy().isRequiredHandlerAuthenticationPolicyEnabled()) {
+            LOGGER.debug("Applying configuration for Required Handler Authentication Policy");
             return new RequiredHandlerAuthenticationPolicyFactory();
         }
+        LOGGER.debug("Applying configuration for Accept Any Authentication Policy");
         return new AcceptAnyAuthenticationPolicyFactory();
     }
 
