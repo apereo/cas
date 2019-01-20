95f097d1a46a6b56e7f7dbf88ec4f5b304849c7b
Martin Böhmer <MartinBoehmer@users.noreply.github.com>
2019-01-20 22:40:22 +0100

    This fixes #3755

--- core/cas-server-core-services-api/src/main/java/org/apereo/cas/services/DefaultRegisteredServiceAccessStrategy.java
+++ core/cas-server-core-services-api/src/main/java/org/apereo/cas/services/DefaultRegisteredServiceAccessStrategy.java
@@ -280,17 +280,30 @@ public class DefaultRegisteredServiceAccessStrategy implements RegisteredService
      */
     protected boolean requiredAttributesFoundInMap(final Map<String, Object> principalAttributes, final Map<String, Set<String>> requiredAttributes) {
         final Set<String> difference = requiredAttributes.keySet().stream().filter(a -> principalAttributes.keySet().contains(a)).collect(Collectors.toSet());
+        LOGGER.debug("Difference of checking required attributes: [{}]", difference);
         if (this.requireAllAttributes && difference.size() < requiredAttributes.size()) {
             return false;
         }
-        return difference.stream().anyMatch(key -> {
-            final Set<String> values = requiredAttributes.get(key);
-            final Set<Object> availableValues = CollectionUtils.toCollection(principalAttributes.get(key));
-            final Pattern pattern = RegexUtils.concatenate(values, this.caseInsensitive);
-            if (pattern != RegexUtils.MATCH_NOTHING_PATTERN) {
-                return availableValues.stream().map(Object::toString).anyMatch(pattern.asPredicate());
-            }
-            return availableValues.stream().anyMatch(values::contains);
-        });
+        if (this.requireAllAttributes) {
+            return difference.stream().allMatch(key -> {
+                return requiredAttributeFound(key, principalAttributes, requiredAttributes);
+            });
+        } else {
+            return difference.stream().anyMatch(key -> {
+                return requiredAttributeFound(key, principalAttributes, requiredAttributes);
+            });
+        }
     }
+    
+    private boolean requiredAttributeFound(final String attributeName, final Map<String, Object> principalAttributes, final Map<String, Set<String>> requiredAttributes) {
+        final Set<String> values = requiredAttributes.get(attributeName);
+        final Set<Object> availableValues = CollectionUtils.toCollection(principalAttributes.get(attributeName));
+        final Pattern pattern = RegexUtils.concatenate(values, this.caseInsensitive);
+        LOGGER.debug("Checking [{}] against [{}] with pattern [{}] for attribute [{}]", values, availableValues, pattern, attributeName);
+        if (pattern != RegexUtils.MATCH_NOTHING_PATTERN) {
+            return availableValues.stream().map(Object::toString).anyMatch(pattern.asPredicate());
+        }
+        return availableValues.stream().anyMatch(values::contains);
+    }
+    
 }
