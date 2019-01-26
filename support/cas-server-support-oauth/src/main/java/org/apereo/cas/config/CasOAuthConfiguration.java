62a6ccfb41cfc8de2b7df86dc302dec8f4a93982
hdeadman <hal.deadman@gmail.com>
2019-01-26 18:17:32 -0500

    Fix for CAS not finding built-in services due to incorrect priority
    Change some built-in services from MAX_VALUE (low priority, last) to
    MIN_VALUE/HIGHEST_PRIORITY b/c CAS built-in services or internal services
    will not match non-CAS services (b/c they are
    ^protocol://domain/casprefix/.*)
    
    Use Ordered.HIGHEST_PRIORITY instead of Integer.MIN_VALUE which are same
    but HIGHEST_PRIORITY makes intent clear.

--- support/cas-server-support-oauth/src/main/java/org/apereo/cas/config/CasOAuthConfiguration.java
+++ support/cas-server-support-oauth/src/main/java/org/apereo/cas/config/CasOAuthConfiguration.java
@@ -118,6 +118,7 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -745,7 +746,7 @@ public class CasOAuthConfiguration implements AuditTrailRecordResolutionPlanConf
             public void configureServiceRegistry(final ServiceRegistryExecutionPlan plan) {
                 val service = new RegexRegisteredService();
                 service.setId(RandomUtils.getNativeInstance().nextLong());
-                service.setEvaluationOrder(Integer.MAX_VALUE);
+                service.setEvaluationOrder(Ordered.HIGHEST_PRECEDENCE);
                 service.setName(service.getClass().getSimpleName());
                 service.setDescription("OAuth Authentication Callback Request URL");
                 service.setServiceId(oauthCallbackService().getId());
