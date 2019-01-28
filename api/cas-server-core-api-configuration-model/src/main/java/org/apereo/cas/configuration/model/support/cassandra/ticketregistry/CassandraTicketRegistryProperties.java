8e10ae0ae60302e6a6daaf20e163b26900b17b6d
Knut-Sigurd Knuteson <knut.sigurd@openmarket.com>
2019-01-28 13:27:25 -0800

    fix ant checkstyle errors

--- api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/support/cassandra/ticketregistry/CassandraTicketRegistryProperties.java
+++ api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/support/cassandra/ticketregistry/CassandraTicketRegistryProperties.java
@@ -6,8 +6,14 @@ import org.apereo.cas.configuration.support.RequiresModule;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * This is {@link CassandraTicketRegistryProperties}.
+ *
+ * @since 6.1.0
+ */
 @RequiresModule(name = "cas-server-support-cassandra-ticket-registry")
 @Getter
 @Setter
 public class CassandraTicketRegistryProperties extends BaseCassandraProperties {
 }
+
