e3a8d4790f1aa544bac405a124b3e1cf0fdecc3e
tduehr <tduehr@users.noreply.github.com>
2019-01-31 03:55:36 -0600

    core/cas-server-core-audit junit5 (#3777)
    migrates the audit functionality

--- core/cas-server-core-audit-api/src/test/java/org/apereo/cas/audit/spi/CoreAuditTestsSuite.java
+++ core/cas-server-core-audit-api/src/test/java/org/apereo/cas/audit/spi/CoreAuditTestsSuite.java
@@ -4,8 +4,7 @@ import org.apereo.cas.audit.spi.principal.ChainingAuditPrincipalIdProviderTests;
 import org.apereo.cas.audit.spi.resource.ServiceResourceResolverTests;
 import org.apereo.cas.audit.spi.resource.TicketAsFirstParameterResourceResolverTests;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
+import org.junit.platform.suite.api.SelectClasses;
 
 /**
  * This is {@link CoreAuditTestsSuite}.
@@ -13,8 +12,7 @@ import org.junit.runners.Suite;
  * @author Misagh Moayyed
  * @since 6.0.0
  */
-@RunWith(Suite.class)
-@Suite.SuiteClasses({
+@SelectClasses({
     AuditActionContextJsonSerializerTests.class,
     ServiceResourceResolverTests.class,
     TicketAsFirstParameterResourceResolverTests.class,
