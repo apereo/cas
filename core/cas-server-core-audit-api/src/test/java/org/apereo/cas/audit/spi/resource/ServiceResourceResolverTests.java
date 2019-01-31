c9d03ee1be98d0d3d7ffb9ade6bad75d790ad536
tduehr <tduehr@users.noreply.github.com>
2019-01-31 03:55:36 -0600

    core/cas-server-core-audit junit5 (#3777)
    migrates the audit functionality

--- core/cas-server-core-audit-api/src/test/java/org/apereo/cas/audit/spi/resource/ServiceResourceResolverTests.java
+++ core/cas-server-core-audit-api/src/test/java/org/apereo/cas/audit/spi/resource/ServiceResourceResolverTests.java
@@ -4,9 +4,9 @@ import org.apereo.cas.services.RegisteredServiceTestUtils;
 
 import lombok.val;
 import org.aspectj.lang.JoinPoint;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 /**
