ace04fd9ad831330c6c57367f6aa383e00e79ddd
Timur Duehr <tduehr@gmail.com>
2019-01-31 18:37:37 -0600

    cas-server-core-services-registry

--- core/cas-server-core-services-registry/src/test/java/org/apereo/cas/services/ServiceRegistryInitializerTests.java
+++ core/cas-server-core-services-registry/src/test/java/org/apereo/cas/services/ServiceRegistryInitializerTests.java
@@ -1,7 +1,7 @@
 package org.apereo.cas.services;
 
 import lombok.val;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationEventPublisher;
 
 import java.util.Collection;
