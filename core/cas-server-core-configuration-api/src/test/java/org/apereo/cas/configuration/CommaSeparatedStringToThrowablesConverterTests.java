5ee3154cf1dd36206ec4b7fed8a59cfa0540e377
tduehr <tduehr@users.noreply.github.com>
2019-02-03 23:28:46 -0600

    migrate core/cas-server-core-configuration to junit5 (#3789)
    * cas-server-core-configuration
    
    * cas-server-core-configuration-api
    
    * cas-server-core-configuration-metadata-repository
    
    * fix test

--- core/cas-server-core-configuration-api/src/test/java/org/apereo/cas/configuration/CommaSeparatedStringToThrowablesConverterTests.java
+++ core/cas-server-core-configuration-api/src/test/java/org/apereo/cas/configuration/CommaSeparatedStringToThrowablesConverterTests.java
@@ -1,13 +1,9 @@
 package org.apereo.cas.configuration;
 
 import lombok.val;
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.Test;
-import org.springframework.test.context.junit4.rules.SpringClassRule;
-import org.springframework.test.context.junit4.rules.SpringMethodRule;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * This is {@link CommaSeparatedStringToThrowablesConverterTests}.
@@ -16,12 +12,6 @@ import static org.junit.Assert.*;
  * @since 5.3.0
  */
 public class CommaSeparatedStringToThrowablesConverterTests {
-    @ClassRule
-    public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
-
-    @Rule
-    public final SpringMethodRule springMethodRule = new SpringMethodRule();
-
     @Test
     public void verifyConverters() {
         val c = new CommaSeparatedStringToThrowablesConverter();
