24e465a920d1f0474e81cfdaf6a44841543132a8
Timur Duehr <tduehr@gmail.com>
2019-02-04 20:19:44 -0600

    support/cas-server-support-aws*

--- support/cas-server-support-aws-cognito-authentication/src/test/java/org/apereo/cas/authentication/AmazonCognitoAuthenticationAuthenticationHandlerTests.java
+++ support/cas-server-support-aws-cognito-authentication/src/test/java/org/apereo/cas/authentication/AmazonCognitoAuthenticationAuthenticationHandlerTests.java
@@ -7,8 +7,6 @@ import org.apereo.cas.config.CasRegisteredServicesTestConfiguration;
 import org.apereo.cas.configuration.CasConfigurationProperties;
 
 import lombok.val;
-import org.junit.ClassRule;
-import org.junit.Rule;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -17,8 +15,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
 import org.springframework.test.annotation.IfProfileValue;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.rules.SpringClassRule;
-import org.springframework.test.context.junit4.rules.SpringMethodRule;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * This is {@link AmazonCognitoAuthenticationAuthenticationHandlerTests}.
@@ -44,19 +42,13 @@ import org.springframework.test.context.junit4.rules.SpringMethodRule;
 @EnableConfigurationProperties(CasConfigurationProperties.class)
 @IfProfileValue(name = "cognitoEnabled", value = "true")
 public class AmazonCognitoAuthenticationAuthenticationHandlerTests {
-    @ClassRule
-    public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
-
-    @Rule
-    public final SpringMethodRule springMethodRule = new SpringMethodRule();
-
     @Autowired
     @Qualifier("amazonCognitoAuthenticationHandler")
     private AuthenticationHandler amazonCognitoAuthenticationHandler;
 
     @Test
-    public void verifyAction() throws Exception {
+    public void verifyAction() {
         val creds = CoreAuthenticationTestUtils.getCredentialsWithDifferentUsernameAndPassword("casuser", "Hell063!!");
-        amazonCognitoAuthenticationHandler.authenticate(creds);
+        assertDoesNotThrow(() -> amazonCognitoAuthenticationHandler.authenticate(creds));
     }
 }
