d50c83fc579f9b8c6f91f537af20edbc86954072
tduehr <tduehr@users.noreply.github.com>
2019-02-05 01:35:34 -0600

    core/cas-server-core-authentication migrations to junit5 WIP (#3779)
    * enable junit5
    
    * authn impl
    
    * cas-core-server-authentication-api
    
    * cas-server-core-authentication-attributes
    
    * core/cas-server-core-authentication-mfa*
    
    * core/cas-server-core-authentication-throttle
    
    * fix RestfulAuthenticationPolicyTests

--- core/cas-server-core-authentication-attributes/src/test/java/org/apereo/cas/services/ScriptedRegisteredServiceAttributeReleasePolicyTests.java
+++ core/cas-server-core-authentication-attributes/src/test/java/org/apereo/cas/services/ScriptedRegisteredServiceAttributeReleasePolicyTests.java
@@ -3,11 +3,11 @@ package org.apereo.cas.services;
 import org.apereo.cas.CoreAttributesTestUtils;
 
 import lombok.val;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * This is {@link ScriptedRegisteredServiceAttributeReleasePolicyTests}.
