45f825aa351a3a9096c928dd742f4def8555d0ed
tduehr <tduehr@users.noreply.github.com>
2019-02-06 05:01:27 -0600

    junit5 migrations: support/cas-server-support-consent WIP (#3805)
    * support/cas-server-support-consent-core/
    
    * support/cas-server-support-consent-couchdb/
    
    * support/cas-server-support-consent-jdbc/
    
    * support/cas-server-support-consent-ldap/
    
    * support/cas-server-support-consent-mongo/
    
    * support/cas-server-support-consent-rest/
    
    * support/cas-server-support-consent-webflow/

--- support/cas-server-support-consent-webflow/src/test/java/org/apereo/cas/web/flow/BaseConsentActionTests.java
+++ support/cas-server-support-consent-webflow/src/test/java/org/apereo/cas/web/flow/BaseConsentActionTests.java
@@ -15,14 +15,10 @@ import org.apereo.cas.config.support.CasWebApplicationServiceFactoryConfiguratio
 import org.apereo.cas.web.flow.config.CasCoreWebflowConfiguration;
 import org.apereo.cas.web.flow.config.CasWebflowContextConfiguration;
 
-import org.junit.ClassRule;
-import org.junit.Rule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
-import org.springframework.test.context.junit4.rules.SpringClassRule;
-import org.springframework.test.context.junit4.rules.SpringMethodRule;
 import org.springframework.webflow.execution.Action;
 
 /**
@@ -49,12 +45,6 @@ import org.springframework.webflow.execution.Action;
     CasCoreAuthenticationServiceSelectionStrategyConfiguration.class
 })
 public abstract class BaseConsentActionTests {
-    @ClassRule
-    public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
-
-    @Rule
-    public final SpringMethodRule springMethodRule = new SpringMethodRule();
-
     @Autowired
     @Qualifier("confirmConsentAction")
     protected Action confirmConsentAction;
