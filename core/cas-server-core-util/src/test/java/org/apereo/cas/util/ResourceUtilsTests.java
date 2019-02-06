e28747eead3c2ae04720b0baba6983c7fd6ea42e
tduehr <tduehr@users.noreply.github.com>
2019-02-06 09:16:50 -0600

    core/cas-server-core-util to junit5 WIP (#3790)
    * cas-server-core-util
    
    * cas-server-core-validation
    
    * fix style for deprecated classes

--- core/cas-server-core-util/src/test/java/org/apereo/cas/util/ResourceUtilsTests.java
+++ core/cas-server-core-util/src/test/java/org/apereo/cas/util/ResourceUtilsTests.java
@@ -1,16 +1,12 @@
 package org.apereo.cas.util;
 
 import lombok.val;
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.FileSystemResource;
-import org.springframework.test.context.junit4.rules.SpringClassRule;
-import org.springframework.test.context.junit4.rules.SpringMethodRule;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * This is {@link ResourceUtilsTests}.
@@ -19,12 +15,6 @@ import static org.junit.Assert.*;
  * @since 5.3.0
  */
 public class ResourceUtilsTests {
-    @ClassRule
-    public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
-
-    @Rule
-    public final SpringMethodRule springMethodRule = new SpringMethodRule();
-
     @Test
     public void verifyResourceExists() {
         assertFalse(ResourceUtils.doesResourceExist(new FileSystemResource("invalid.json")));
