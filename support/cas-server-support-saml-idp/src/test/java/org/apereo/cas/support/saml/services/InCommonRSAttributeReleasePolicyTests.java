13e84c067bc8d7ec5e7ddf8417eab52ea66e4774
Timur Duehr <tduehr@gmail.com>
2019-02-07 21:00:47 -0600

    support/cas-server-support-saml-idp

--- support/cas-server-support-saml-idp/src/test/java/org/apereo/cas/support/saml/services/InCommonRSAttributeReleasePolicyTests.java
+++ support/cas-server-support-saml-idp/src/test/java/org/apereo/cas/support/saml/services/InCommonRSAttributeReleasePolicyTests.java
@@ -1,7 +1,6 @@
 package org.apereo.cas.support.saml.services;
 
 import org.apereo.cas.authentication.CoreAuthenticationTestUtils;
-import org.apereo.cas.category.FileSystemCategory;
 import org.apereo.cas.support.saml.BaseSamlIdPConfigurationTests;
 import org.apereo.cas.support.saml.SamlIdPTestUtils;
 import org.apereo.cas.util.CollectionUtils;
@@ -9,15 +8,15 @@ import org.apereo.cas.util.CollectionUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.val;
 import org.apache.commons.io.FileUtils;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.experimental.categories.Category;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.springframework.core.io.FileSystemResource;
 
 import java.io.File;
 import java.io.IOException;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * This is {@link InCommonRSAttributeReleasePolicyTests}.
@@ -25,12 +24,12 @@ import static org.junit.Assert.*;
  * @author Misagh Moayyed
  * @since 6.1.0
  */
-@Category(FileSystemCategory.class)
+@Tag("FileSystem")
 public class InCommonRSAttributeReleasePolicyTests extends BaseSamlIdPConfigurationTests {
     private static final File JSON_FILE = new File(FileUtils.getTempDirectoryPath(), "InCommonRSAttributeReleasePolicyTests.json");
     private static final ObjectMapper MAPPER = new ObjectMapper().findAndRegisterModules();
     
-    @BeforeClass
+    @BeforeAll
     public static void beforeClass() {
         METADATA_DIRECTORY = new FileSystemResource(FileUtils.getTempDirectory());
     }
--- support/cas-server-support-saml-idp/src/test/java/org/apereo/cas/support/saml/web/idp/profile/builders/enc/SamlObjectSignatureValidatorTests.java
+++ support/cas-server-support-saml-idp/src/test/java/org/apereo/cas/support/saml/web/idp/profile/builders/enc/SamlObjectSignatureValidatorTests.java
@@ -1,6 +1,5 @@
 package org.apereo.cas.support.saml.web.idp.profile.builders.enc;
 
-import org.apereo.cas.category.FileSystemCategory;
 import org.apereo.cas.support.saml.BaseSamlIdPConfigurationTests;
 import org.apereo.cas.support.saml.InMemoryResourceMetadataResolver;
 import org.apereo.cas.support.saml.SamlIdPUtils;
@@ -10,9 +9,9 @@ import org.apereo.cas.support.saml.services.idp.metadata.SamlRegisteredServiceSe
 import lombok.val;
 import net.shibboleth.utilities.java.support.resolver.CriteriaSet;
 import org.apache.commons.io.FileUtils;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.experimental.categories.Category;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.opensaml.core.criterion.EntityIdCriterion;
 import org.opensaml.messaging.context.MessageContext;
 import org.opensaml.saml.common.SAMLObject;
@@ -36,14 +35,14 @@ import java.io.File;
  * @author Misagh Moayyed
  * @since 6.0.0
  */
-@Category(FileSystemCategory.class)
+@Tag("FileSystem")
 public class SamlObjectSignatureValidatorTests extends BaseSamlIdPConfigurationTests {
     private SAML2Configuration saml2ClientConfiguration;
     private SAML2MessageContext saml2MessageContext;
     private MessageContext<SAMLObject> samlContext;
     private SamlRegisteredServiceServiceProviderMetadataFacade adaptor;
 
-    @Before
+    @BeforeEach
     public void before() throws Exception {
         val idpMetadata = new File("src/test/resources/metadata/idp-metadata.xml").getCanonicalPath();
         val keystorePath = new File(FileUtils.getTempDirectory(), "keystore").getCanonicalPath();
