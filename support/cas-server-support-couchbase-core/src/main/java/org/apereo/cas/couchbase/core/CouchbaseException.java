b84e2c9cbeb8379fa2535a4d4df7846849458717
Timur Duehr <tduehr@gmail.com>
2019-02-12 15:49:41 -0600

    fix checkstyle

--- support/cas-server-support-couchbase-core/src/main/java/org/apereo/cas/couchbase/core/CouchbaseException.java
+++ support/cas-server-support-couchbase-core/src/main/java/org/apereo/cas/couchbase/core/CouchbaseException.java
@@ -1,7 +1,13 @@
 package org.apereo.cas.couchbase.core;
 
+/**
+ * This is {@link CouchbaseException}.
+ *
+ * @author Timur Duehr
+ * @since 6.1.0
+ */
 public class CouchbaseException extends RuntimeException {
-    public CouchbaseException(String message) {
+    public CouchbaseException(final String message) {
         super(message);
     }
 }
