4f5b02272bcfc7a1a4066ee8e1666850024a4ecb
Timur Duehr <tduehr@gmail.com>
2019-02-13 20:26:29 -0600

    core/cas-server-core-services-registry/

--- core/cas-server-core-services-registry/src/test/java/org/apereo/cas/services/ServiceRegistryInitializerTests.java
+++ core/cas-server-core-services-registry/src/test/java/org/apereo/cas/services/ServiceRegistryInitializerTests.java
@@ -4,10 +4,9 @@ import lombok.val;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationEventPublisher;
 
-import java.util.Collection;
 import java.util.Collections;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 /**
@@ -22,18 +21,18 @@ public class ServiceRegistryInitializerTests {
 
         val servicesManager = mock(ServicesManager.class);
         val jsonServiceRegistry = mock(ServiceRegistry.class);
-        when(jsonServiceRegistry.load()).thenReturn((Collection) Collections.singletonList(initialService));
+        when(jsonServiceRegistry.load()).thenReturn(Collections.singletonList(initialService));
 
         val serviceRegistry = new InMemoryServiceRegistry(mock(ApplicationEventPublisher.class));
         val serviceRegistryInitializer = new ServiceRegistryInitializer(jsonServiceRegistry, serviceRegistry, servicesManager);
         serviceRegistryInitializer.initServiceRegistryIfNecessary();
-        assertThat(serviceRegistry.size()).isEqualTo(1);
+        assertEquals(1, serviceRegistry.size());
 
         val initialService2 = newService();
-        when(jsonServiceRegistry.load()).thenReturn((Collection) Collections.singletonList(initialService2));
+        when(jsonServiceRegistry.load()).thenReturn(Collections.singletonList(initialService2));
 
         serviceRegistryInitializer.initServiceRegistryIfNecessary();
-        assertThat(serviceRegistry.size()).isEqualTo(1);
+        assertEquals(1, serviceRegistry.size());
     }
 
     private static RegisteredService newService() {
