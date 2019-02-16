29bb32e55ea3274d997732795558ae206bf1140d
Misagh Moayyed <mm1844@gmail.com>
2019-02-16 03:26:05 -0700

    Update CasInterruptConfiguration.java

--- support/cas-server-support-interrupt-core/src/main/java/org/apereo/cas/config/CasInterruptConfiguration.java
+++ support/cas-server-support-interrupt-core/src/main/java/org/apereo/cas/config/CasInterruptConfiguration.java
@@ -5,7 +5,6 @@ import org.apereo.cas.interrupt.DefaultInterruptInquiryExecutionPlan;
 import org.apereo.cas.interrupt.GroovyScriptInterruptInquirer;
 import org.apereo.cas.interrupt.InterruptInquiryExecutionPlan;
 import org.apereo.cas.interrupt.InterruptInquiryExecutionPlanConfigurer;
-import org.apereo.cas.interrupt.InterruptResponse;
 import org.apereo.cas.interrupt.JsonResourceInterruptInquirer;
 import org.apereo.cas.interrupt.RegexAttributeInterruptInquirer;
 import org.apereo.cas.interrupt.RestEndpointInterruptInquirer;
