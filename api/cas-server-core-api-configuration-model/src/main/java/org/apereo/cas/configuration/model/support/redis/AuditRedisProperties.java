4d8c8714b4f131ffe23b54542ea0c0ac7b2821a6
Misagh Moayyed <mm1844@gmail.com>
2019-02-21 15:22:22 -0700

    add support redis-backed audit logs

---
+++ api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/support/redis/AuditRedisProperties.java
@@ -0,0 +1,25 @@
+package org.apereo.cas.configuration.model.support.redis;
+
+import org.apereo.cas.configuration.support.RequiresModule;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Configuration properties for Redis.
+ *
+ * @author Misagh Moayyed
+ * @since 6.1.0
+ */
+@RequiresModule(name = "cas-server-support-audit-redis")
+@Getter
+@Setter
+public class AuditRedisProperties extends BaseRedisProperties {
+    private static final long serialVersionUID = -8112996050439638782L;
+
+    /**
+     * Execute the recording of audit records in async manner.
+     * This setting must almost always be set to true.
+     */
+    private boolean asynchronous = true;
+}
