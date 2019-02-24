0247ede119fc6660d5f484050c43b7273916f227
Misagh Moayyed <mmoayyed@unicon.net>
2019-02-24 02:15:44 -0700

    build support for custom fields; add blog and update docs

--- api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/core/web/view/ViewProperties.java
+++ api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/core/web/view/ViewProperties.java
@@ -9,7 +9,9 @@ import org.springframework.boot.context.properties.NestedConfigurationProperty;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * This is {@link ViewProperties}.
@@ -31,6 +33,15 @@ public class ViewProperties implements Serializable {
     private String defaultRedirectUrl;
 
     /**
+     * Additional custom fields that should be displayed
+     * on the login form and would be bound to the authentication
+     * credential during form-authentication
+     * to carry additional metadata and tags.
+     * Key is the name of the custom field.
+     */
+    private Map<String, CustomLoginField> customLoginFormFields = new LinkedHashMap<>();
+
+    /**
      * Comma separated paths to where CAS templates may be found.
      */
     private List<String> templatePrefixes = new ArrayList<>();
@@ -40,7 +51,6 @@ public class ViewProperties implements Serializable {
      */
     @NestedConfigurationProperty
     private Cas10ViewProperties cas1 = new Cas10ViewProperties();
-
 
     /**
      * CAS2 views and locations.
@@ -64,5 +74,26 @@ public class ViewProperties implements Serializable {
     @Setter
     public static class Rest extends RestEndpointProperties {
         private static final long serialVersionUID = -8102345678378393382L;
+    }
+
+    @RequiresModule(name = "cas-server-core-web", automated = true)
+    @Getter
+    @Setter
+    public static class CustomLoginField implements Serializable {
+        private static final long serialVersionUID = -7122345678378395582L;
+
+        /**
+         * The key for this field found in the message bundle
+         * used to present a label/text in CAS views.
+         */
+        private String messageBundleKey;
+        /**
+         * Whether this field is required to have a value.
+         */
+        private boolean required;
+        /**
+         * The id of the custom converter to use to convert bound property values.
+         */
+        private String converter;
     }
 }
