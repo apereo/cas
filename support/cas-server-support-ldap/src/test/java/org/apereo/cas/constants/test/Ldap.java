0dcee00a6a22a77e87fbd7f553084b1439ca7575
Timur Duehr <tduehr@gmail.com>
2019-02-26 08:53:34 -0600

    fixup! fixup! ldap and x509 properties

---
+++ support/cas-server-support-ldap/src/test/java/org/apereo/cas/constants/test/Ldap.java
@@ -0,0 +1,25 @@
+package org.apereo.cas.constants.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+
+/**
+ * This is {@link Ldap}. Constants for LDAP test classes.
+ *
+ * @author Timur Duehr
+ * @since 6.1.0
+ */
+public class Ldap {
+    @Autowired
+    private static Environment ENV;
+
+    public static String URL = ENV.getProperty("ldap.url");
+    public static String MANAGER_DN = ENV.getProperty("ldap.managerDn");
+    public static int PORT = Integer.parseInt(ENV.getProperty("ldap.port"));
+    public static String BIND_DN = ENV.getProperty("ldap.bindDn");
+    public static String PEOPLE_DN = ENV.getProperty("ldap.peopleDn");
+    public static String BIND_PASS = ENV.getProperty("ldap.bindPassword");
+    public static String BASE_DN = ENV.getProperty("ldap.baseDn");
+    public static String HOST = ENV.getProperty("ldap.host");
+
+}
