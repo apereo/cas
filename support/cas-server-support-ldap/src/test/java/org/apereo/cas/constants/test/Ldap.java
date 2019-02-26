89483dc2aa1c5d1924d2fb667a37010cf2654e33
Timur Duehr <tduehr@gmail.com>
2019-02-26 10:12:15 -0600

    fixup! fixup! fixup! ldap and x509 properties

--- support/cas-server-support-ldap/src/test/java/org/apereo/cas/constants/test/Ldap.java
+++ support/cas-server-support-ldap/src/test/java/org/apereo/cas/constants/test/Ldap.java
@@ -1,6 +1,9 @@
 package org.apereo.cas.constants.test;
 
+import org.apereo.cas.configuration.CasConfigurationProperties;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.core.env.Environment;
 
 /**
@@ -9,13 +12,14 @@ import org.springframework.core.env.Environment;
  * @author Timur Duehr
  * @since 6.1.0
  */
+@EnableConfigurationProperties(CasConfigurationProperties.class)
 public class Ldap {
     @Autowired
     private static Environment ENV;
 
     public static String URL = ENV.getProperty("ldap.url");
     public static String MANAGER_DN = ENV.getProperty("ldap.managerDn");
-    public static int PORT = Integer.parseInt(ENV.getProperty("ldap.port"));
+    public static int PORT = Integer.parseInt(ENV.getProperty("ldap.port", "0"));
     public static String BIND_DN = ENV.getProperty("ldap.bindDn");
     public static String PEOPLE_DN = ENV.getProperty("ldap.peopleDn");
     public static String BIND_PASS = ENV.getProperty("ldap.bindPassword");
