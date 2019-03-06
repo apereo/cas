f80226717b23ac0b824c5134fd847412347d99b8
Misagh Moayyed <mm1844@gmail.com>
2019-03-06 12:58:08 -0700

    additional tests for groovy inline + json serialization

--- core/cas-server-core-services/src/test/java/org/apereo/cas/services/GroovyRegisteredServiceUsernameProviderTests.java
+++ core/cas-server-core-services/src/test/java/org/apereo/cas/services/GroovyRegisteredServiceUsernameProviderTests.java
@@ -1,8 +1,15 @@
 package org.apereo.cas.services;
 
+import org.apereo.cas.util.CollectionUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.val;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,14 +21,36 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 @Tag("Groovy")
 public class GroovyRegisteredServiceUsernameProviderTests {
+    private static final File JSON_FILE = new File(FileUtils.getTempDirectoryPath(), "GroovyRegisteredServiceUsernameProviderTests.json");
+    private static final ObjectMapper MAPPER = new ObjectMapper().findAndRegisterModules();
 
     @Test
     public void verifyUsernameProvider() {
         val p = new GroovyRegisteredServiceUsernameProvider();
         p.setGroovyScript("file:src/test/resources/uid.groovy");
-        val id =
-            p.resolveUsername(RegisteredServiceTestUtils.getPrincipal(), RegisteredServiceTestUtils.getService(),
-                RegisteredServiceTestUtils.getRegisteredService());
+        val id = p.resolveUsername(RegisteredServiceTestUtils.getPrincipal(), RegisteredServiceTestUtils.getService(),
+            RegisteredServiceTestUtils.getRegisteredService());
         assertEquals("test", id);
+    }
+
+    @Test
+    public void verifyUsernameProviderInline() {
+        val p = new GroovyRegisteredServiceUsernameProvider();
+        p.setGroovyScript("groovy { return attributes['uid'] + '123456789' }");
+        var id = p.resolveUsername(RegisteredServiceTestUtils.getPrincipal("casuser",
+            CollectionUtils.wrap("uid", "CAS-System")), RegisteredServiceTestUtils.getService(),
+            RegisteredServiceTestUtils.getRegisteredService());
+        assertEquals("CAS-System123456789", id);
+    }
+
+    @Test
+    public void verifySerializationToJson() throws IOException {
+        val p = new GroovyRegisteredServiceUsernameProvider();
+        p.setGroovyScript("groovy { return 'something' }");
+        p.setEncryptUsername(true);
+        p.setCanonicalizationMode("NONE");
+        MAPPER.writeValue(JSON_FILE, p);
+        val repositoryRead = MAPPER.readValue(JSON_FILE, GroovyRegisteredServiceUsernameProvider.class);
+        assertEquals(p, repositoryRead);
     }
 }
