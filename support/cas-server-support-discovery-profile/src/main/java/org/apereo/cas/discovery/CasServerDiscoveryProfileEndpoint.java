eaa9f1cd68e104046a05a053397d05d2e2195b41
tsschmidt <tsschmidt@ucdavis.edu>
2019-03-07 10:16:00 -0800

    Remove unused imports

--- support/cas-server-support-discovery-profile/src/main/java/org/apereo/cas/discovery/CasServerDiscoveryProfileEndpoint.java
+++ support/cas-server-support-discovery-profile/src/main/java/org/apereo/cas/discovery/CasServerDiscoveryProfileEndpoint.java
@@ -9,8 +9,6 @@ import org.springframework.boot.actuate.endpoint.web.annotation.RestControllerEn
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 
