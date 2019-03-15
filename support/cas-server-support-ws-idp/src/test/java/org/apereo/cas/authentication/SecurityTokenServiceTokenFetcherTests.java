e1f061a415ed6c2a317cbbb18476ee26b94c5ad5
Timur Duehr <tduehr@gmail.com>
2019-03-14 19:14:45 -0500

    ws

--- support/cas-server-support-ws-idp/src/test/java/org/apereo/cas/authentication/SecurityTokenServiceTokenFetcherTests.java
+++ support/cas-server-support-ws-idp/src/test/java/org/apereo/cas/authentication/SecurityTokenServiceTokenFetcherTests.java
@@ -64,7 +64,21 @@ import static org.junit.jupiter.api.Assertions.*;
     CasDefaultServiceTicketIdGeneratorsConfiguration.class,
     CasCoreAuthenticationPrincipalConfiguration.class
 })
-@TestPropertySource(locations = "classpath:ws-idp.properties")
+@TestPropertySource(properties = {
+    "cas.authn.wsfedIdp.idp.realm=urn:org:apereo:cas:ws:idp:realm-CAS",
+    "cas.authn.wsfedIdp.idp.realmName=CAS",
+    "cas.authn.wsfedIdp.sts.signingKeystoreFile=classpath:ststrust.jks",
+    "cas.authn.wsfedIdp.sts.signingKeystorePassword=storepass",
+    "cas.authn.wsfedIdp.sts.encryptionKeystoreFile=classpath:stsencrypt.jks",
+    "cas.authn.wsfedIdp.sts.encryptionKeystorePassword=storepass",
+    "cas.authn.wsfedIdp.sts.subjectNameIdFormat=unspecified",
+    "cas.authn.wsfedIdp.sts.encryptTokens=true",
+    "cas.authn.wsfedIdp.sts.realm.keystoreFile=classpath:stsrealm_a.jks",
+    "cas.authn.wsfedIdp.sts.realm.keystorePassword=storepass",
+    "cas.authn.wsfedIdp.sts.realm.keystoreAlias=realma",
+    "cas.authn.wsfedIdp.sts.realm.keyPassword=realma",
+    "cas.authn.wsfedIdp.sts.realm.issuer=CAS"
+})
 public class SecurityTokenServiceTokenFetcherTests {
     @Autowired
     private CasConfigurationProperties casProperties;
