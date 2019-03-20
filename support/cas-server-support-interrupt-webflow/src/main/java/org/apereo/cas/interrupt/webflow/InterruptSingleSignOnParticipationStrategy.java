ba7cdfbf7601bce33577d811ff35873e78c6257e
Misagh Moayyed <mm1844@gmail.com>
2019-03-20 03:45:44 -0700

    refactor sso participation strategy with tests

--- support/cas-server-support-interrupt-webflow/src/main/java/org/apereo/cas/interrupt/webflow/InterruptSingleSignOnParticipationStrategy.java
+++ support/cas-server-support-interrupt-webflow/src/main/java/org/apereo/cas/interrupt/webflow/InterruptSingleSignOnParticipationStrategy.java
@@ -1,7 +1,6 @@
 package org.apereo.cas.interrupt.webflow;
 
-import org.apereo.cas.services.ServicesManager;
-import org.apereo.cas.web.flow.DefaultSingleSignOnParticipationStrategy;
+import org.apereo.cas.web.flow.SingleSignOnParticipationStrategy;
 
 import lombok.val;
 import org.springframework.webflow.execution.RequestContext;
@@ -12,18 +11,17 @@ import org.springframework.webflow.execution.RequestContext;
  * @author Misagh Moayyed
  * @since 5.2.0
  */
-public class InterruptSingleSignOnParticipationStrategy extends DefaultSingleSignOnParticipationStrategy {
-    public InterruptSingleSignOnParticipationStrategy(final ServicesManager servicesManager, final boolean createSsoOnRenewedAuthn,
-                                                      final boolean renewEnabled) {
-        super(servicesManager, createSsoOnRenewedAuthn, renewEnabled);
+public class InterruptSingleSignOnParticipationStrategy implements SingleSignOnParticipationStrategy {
+
+    @Override
+    public boolean supports(final RequestContext context) {
+        val response = InterruptUtils.getInterruptFrom(context);
+        return response != null;
     }
 
     @Override
     public boolean isParticipating(final RequestContext ctx) {
         val response = InterruptUtils.getInterruptFrom(ctx);
-        if (response != null && !response.isSsoEnabled()) {
-            return false;
-        }
-        return super.isParticipating(ctx);
+        return response.isSsoEnabled();
     }
 }
