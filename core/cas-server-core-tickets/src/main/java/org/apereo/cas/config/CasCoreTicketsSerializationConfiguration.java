5893a5318f68240541db6ccd42419063887ad9e9
Misagh Moayyed <mm1844@gmail.com>
2019-03-20 10:56:11 -0700

    refactor ticket->json serialization into plans for dynamic registration

--- core/cas-server-core-tickets/src/main/java/org/apereo/cas/config/CasCoreTicketsSerializationConfiguration.java
+++ core/cas-server-core-tickets/src/main/java/org/apereo/cas/config/CasCoreTicketsSerializationConfiguration.java
@@ -4,13 +4,20 @@ import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.ticket.serialization.DefaultTicketSerializationExecutionPlan;
 import org.apereo.cas.ticket.serialization.DefaultTicketStringSerializationManager;
 import org.apereo.cas.ticket.serialization.TicketSerializationExecutionPlan;
+import org.apereo.cas.ticket.serialization.TicketSerializationExecutionPlanConfigurer;
 import org.apereo.cas.ticket.serialization.TicketSerializationManager;
 
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.AnnotationAwareOrderComparator;
+
+import java.util.ArrayList;
 
 /**
  * This is {@link CasCoreTicketsSerializationConfiguration}.
@@ -23,10 +30,18 @@ import org.springframework.context.annotation.Configuration;
 @AutoConfigureAfter(CasCoreTicketsConfiguration.class)
 public class CasCoreTicketsSerializationConfiguration {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Bean
     @ConditionalOnMissingBean(name = "ticketSerializationExecutionPlan")
     public TicketSerializationExecutionPlan ticketSerializationExecutionPlan() {
-        return new DefaultTicketSerializationExecutionPlan();
+        val resolvers = applicationContext.getBeansOfType(TicketSerializationExecutionPlanConfigurer.class, false, true);
+        val providers = new ArrayList<TicketSerializationExecutionPlanConfigurer>(resolvers.values());
+        AnnotationAwareOrderComparator.sort(providers);
+        val plan = new DefaultTicketSerializationExecutionPlan();
+        providers.forEach(provider -> provider.configureTicketSerialization(plan));
+        return plan;
     }
 
     @Bean
