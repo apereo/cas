f97ceaf5397e6d01dfc82a3cd0b4f09407b516ba
Misagh Moayyed <mmoayyed@unicon.net>
2019-03-28 07:41:15 -0700

    reorganize naming strategy to handle pattern building

--- core/cas-server-core-services-registry/src/main/java/org/apereo/cas/services/resource/RegisteredServiceResourceNamingStrategy.java
+++ core/cas-server-core-services-registry/src/main/java/org/apereo/cas/services/resource/RegisteredServiceResourceNamingStrategy.java
@@ -1,6 +1,11 @@
 package org.apereo.cas.services.resource;
 
 import org.apereo.cas.services.RegisteredService;
+import org.apereo.cas.util.RegexUtils;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.regex.Pattern;
 
 /**
  * This is {@link RegisteredServiceResourceNamingStrategy}.  Interface to provide naming strategy
@@ -15,9 +20,23 @@ public interface RegisteredServiceResourceNamingStrategy {
     /**
      * Method will be called to provide a name for a resource to store a service.
      *
-     * @param service    - The Service to be saved.
+     * @param service   - The Service to be saved.
      * @param extension - The extension to be used.
      * @return - String representing a resource name.
      */
     String build(RegisteredService service, String extension);
+
+    /**
+     * Gets naming pattern.
+     *
+     * @param extensions the extensions
+     * @return the naming pattern
+     */
+    default Pattern buildNamingPattern(final String... extensions) {
+        var pattern = StringUtils.EMPTY;
+        if (extensions.length > 0) {
+            pattern = String.join("|", extensions);
+        }
+        return RegexUtils.createPattern("(\\w+)-(\\d+)\\.".concat(pattern));
+    }
 }
