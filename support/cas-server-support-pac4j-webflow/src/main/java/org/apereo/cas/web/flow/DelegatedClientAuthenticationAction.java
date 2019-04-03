13286711c86af80c016972789840de913429e83b
Jarkko Pätäri <jarkko.patari@gmail.com>
2019-04-03 13:06:51 +0300

    Pac4j: Fix SAML IdP initiated logout (#3871)
    Adds simple check that delegation handling may continue correctly for logout requests initiated by identity provider.
    
    Identified the problem when using SAML integration.

--- support/cas-server-support-pac4j-webflow/src/main/java/org/apereo/cas/web/flow/DelegatedClientAuthenticationAction.java
+++ support/cas-server-support-pac4j-webflow/src/main/java/org/apereo/cas/web/flow/DelegatedClientAuthenticationAction.java
@@ -179,7 +179,7 @@ public class DelegatedClientAuthenticationAction extends AbstractAuthenticationA
         val request = WebUtils.getHttpServletRequestFromExternalWebflowContext(context);
         val response = WebUtils.getHttpServletResponseFromExternalWebflowContext(context);
 
-        if (singleSignOnSessionExists(context)) {
+        if (!isLogoutRequest(request) && singleSignOnSessionExists(context)) {
             LOGGER.debug("An existing single sign-on session already exists. Skipping delegation and routing back to CAS authentication flow");
             return super.doExecute(context);
         }
@@ -241,6 +241,10 @@ public class DelegatedClientAuthenticationAction extends AbstractAuthenticationA
             LOGGER.info(e.getMessage(), e);
             throw new IllegalArgumentException("Delegated authentication has failed with client " + client.getName());
         }
+    }
+
+    private boolean isLogoutRequest(final HttpServletRequest request) {
+        return request.getParameter(SAML2ServiceProviderMetadataResolver.LOGOUT_ENDPOINT_PARAMETER) != null;
     }
 
     private boolean singleSignOnSessionExists(final RequestContext requestContext) {
@@ -448,8 +452,8 @@ public class DelegatedClientAuthenticationAction extends AbstractAuthenticationA
      * @return the service
      */
     protected Service restoreAuthenticationRequestInContext(final RequestContext requestContext, final J2EContext webContext, final String clientName) {
-        val logoutEndpoint = webContext.getRequestParameter(SAML2ServiceProviderMetadataResolver.LOGOUT_ENDPOINT_PARAMETER);
-        if (logoutEndpoint != null) {
+        val logoutEndpoint = isLogoutRequest(webContext.getRequest());
+        if (logoutEndpoint) {
             return null;
         } else {
             try {
