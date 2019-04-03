bdb9287eac1bd118bd3dc5253a763019583a56f3
Misagh Moayyed <mm1844@gmail.com>
2019-04-03 11:23:28 -0700

    attempt at fixing redis svc registry tests

--- support/cas-server-support-redis-service-registry/src/test/java/org/apereo/cas/adaptors/redis/services/BaseRedisSentinelServiceRegistryTests.java
+++ support/cas-server-support-redis-service-registry/src/test/java/org/apereo/cas/adaptors/redis/services/BaseRedisSentinelServiceRegistryTests.java
@@ -4,13 +4,16 @@ import org.apereo.cas.config.RedisServiceRegistryConfiguration;
 import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.services.AbstractServiceRegistryTests;
 import org.apereo.cas.services.ServiceRegistry;
+import org.apereo.cas.support.events.service.CasRegisteredServiceSavedEvent;
 
 import org.junit.jupiter.api.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -19,7 +22,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @author Julien Gribonvald
  * @since 6.1.0
  */
-@SpringBootTest(classes = {RedisServiceRegistryConfiguration.class, RefreshAutoConfiguration.class})
+@SpringBootTest(classes = {
+    RedisServiceRegistryConfiguration.class,
+    BaseRedisSentinelServiceRegistryTests.SentinelServerServiceRegistryTestConfiguration.class,
+    RefreshAutoConfiguration.class
+})
 @EnableScheduling
 @EnableConfigurationProperties(CasConfigurationProperties.class)
 @Tag("Redis")
@@ -31,5 +38,23 @@ public abstract class BaseRedisSentinelServiceRegistryTests extends AbstractServ
     @Override
     public ServiceRegistry getNewServiceRegistry() {
         return this.dao;
+    }
+
+
+    @TestConfiguration
+    public static class SentinelServerServiceRegistryTestConfiguration {
+        /**
+         * Introduce an artificial delay on save operations
+         * for tests to allow redis to catch up with the transaction
+         * and let CAS proceed with the next step and lookup.
+         *
+         * @param event the event
+         * @throws Exception the exception
+         */
+        @EventListener
+        public void onCasRegisteredServiceSaved(final CasRegisteredServiceSavedEvent event) throws Exception {
+            Thread.sleep(500);
+        }
+
     }
 }
