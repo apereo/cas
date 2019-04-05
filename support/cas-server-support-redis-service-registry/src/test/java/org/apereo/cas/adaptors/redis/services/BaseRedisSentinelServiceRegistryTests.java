f61b8978883e2a0a1e659dfa7d88b878fbd2f001
Misagh Moayyed <mm1844@gmail.com>
2019-04-05 11:14:02 -0700

    Update BaseRedisSentinelServiceRegistryTests.java

--- support/cas-server-support-redis-service-registry/src/test/java/org/apereo/cas/adaptors/redis/services/BaseRedisSentinelServiceRegistryTests.java
+++ support/cas-server-support-redis-service-registry/src/test/java/org/apereo/cas/adaptors/redis/services/BaseRedisSentinelServiceRegistryTests.java
@@ -53,7 +53,7 @@ public abstract class BaseRedisSentinelServiceRegistryTests extends AbstractServ
          */
         @EventListener
         public void onCasRegisteredServiceSaved(final CasRegisteredServiceSavedEvent event) throws Exception {
-            Thread.sleep(500);
+            Thread.sleep(5000);
         }
     }
 }
