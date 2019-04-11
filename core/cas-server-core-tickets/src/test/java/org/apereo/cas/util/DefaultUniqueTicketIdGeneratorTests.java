76ce27068808390a2599bc56fa7fbba75f3e4e98
Dmitriy Kopylenko <dima767@gmail.com>
2019-04-11 17:49:57 -0400

    Fix missing hyphen before suffix (#3885)

--- core/cas-server-core-tickets/src/test/java/org/apereo/cas/util/DefaultUniqueTicketIdGeneratorTests.java
+++ core/cas-server-core-tickets/src/test/java/org/apereo/cas/util/DefaultUniqueTicketIdGeneratorTests.java
@@ -23,15 +23,15 @@ public class DefaultUniqueTicketIdGeneratorTests {
         val suffix = "suffix";
         val generator = new DefaultUniqueTicketIdGenerator(10, suffix);
 
-        assertTrue(generator.getNewTicketId("test").endsWith(suffix));
+        assertTrue(generator.getNewTicketId("test").endsWith('-' + suffix));
     }
 
     @Test
     public void verifyNullSuffix() {
         val lengthWithoutSuffix = 23;
         val generator = new DefaultUniqueTicketIdGenerator(12, null);
-
         val ticketId = generator.getNewTicketId("test");
+
         assertEquals(lengthWithoutSuffix, ticketId.length());
     }
 }
