242830c89a04f23023354b505f64e75c58c0cca1
Misagh Moayyed <mmoayyed@unicon.net>
2019-04-24 02:07:23 -0700

    oidc tests

---
+++ webapp/cas-server-webapp-init/src/test/java/org/apereo/cas/AllCasWebApplicationTestsSuite.java
@@ -0,0 +1,13 @@
+package org.apereo.cas;
+
+import org.junit.platform.suite.api.SelectClasses;
+
+/**
+ * This is {@link AllCasWebApplicationTestsSuite}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.1.0
+ */
+@SelectClasses(CasEmbeddedContainerUtilsTests.class)
+public class AllCasWebApplicationTestsSuite {
+}
