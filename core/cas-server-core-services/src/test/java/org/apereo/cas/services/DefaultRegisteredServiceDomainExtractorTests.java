fd60d2aaecef382b8afa17a6fd27a8911aafbf10
Misagh Moayyed <mm1844@gmail.com>
2019-04-29 06:17:55 -0700

    refactor svc domain extraction plus tests

---
+++ core/cas-server-core-services/src/test/java/org/apereo/cas/services/DefaultRegisteredServiceDomainExtractorTests.java
@@ -0,0 +1,29 @@
+package org.apereo.cas.services;
+
+import org.apereo.cas.services.domain.DefaultRegisteredServiceDomainExtractor;
+import org.apereo.cas.services.domain.RegisteredServiceDomainExtractor;
+
+import lombok.val;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This is {@link DefaultRegisteredServiceDomainExtractorTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.1.0
+ */
+public class DefaultRegisteredServiceDomainExtractorTests {
+
+    @Test
+    public void verifyDomains() {
+        val ext = new DefaultRegisteredServiceDomainExtractor();
+        assertEquals("www.example.org", ext.extract("https://www.example.org"));
+        assertEquals("example.org", ext.extract("https://example.org"));
+        assertEquals("example.org", ext.extract("http://example.org"));
+        assertEquals(RegisteredServiceDomainExtractor.DOMAIN_DEFAULT, ext.extract("www.example.org"));
+        assertEquals("somewhere.example.org",
+            ext.extract("https://somewhere.example.org:1234/page/path"));
+    }
+}
