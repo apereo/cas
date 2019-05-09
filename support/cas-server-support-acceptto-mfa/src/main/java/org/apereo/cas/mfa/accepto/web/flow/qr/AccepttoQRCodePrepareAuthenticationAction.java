92e8812477e31a51e5ae7998c06bb96fda9bc909
Misagh Moayyed <mmoayyed@unicon.net>
2019-05-09 09:51:18 -0700

    build support for acceptto QR login

---
+++ support/cas-server-support-acceptto-mfa/src/main/java/org/apereo/cas/mfa/accepto/web/flow/qr/AccepttoQRCodePrepareAuthenticationAction.java
@@ -0,0 +1,27 @@
+package org.apereo.cas.mfa.accepto.web.flow.qr;
+
+import org.apereo.cas.services.ServicesManager;
+import org.apereo.cas.web.flow.login.InitializeLoginAction;
+
+import org.springframework.webflow.action.EventFactorySupport;
+import org.springframework.webflow.execution.Event;
+import org.springframework.webflow.execution.RequestContext;
+
+/**
+ * This is {@link AccepttoQRCodePrepareAuthenticationAction}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.1.0
+ */
+public class AccepttoQRCodePrepareAuthenticationAction extends InitializeLoginAction {
+    public AccepttoQRCodePrepareAuthenticationAction(final ServicesManager servicesManager) {
+        super(servicesManager);
+    }
+
+    @Override
+    public Event doExecute(final RequestContext requestContext) throws Exception {
+        return new EventFactorySupport()
+            .event(this, AccepttoQRCodeAuthenticationWebflowConfigurer.TRANSITION_ID_GENERATE_QR_CODE);
+    }
+}
+
