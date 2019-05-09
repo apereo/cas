603257c3d9babef9f1f7a1374046c0dd38abaf06
Hal Deadman <hal.deadman@gmail.com>
2019-05-08 20:17:26 -0400

    avoid creating class if no default constructor

--- support/cas-server-support-oidc-core-api/src/main/java/org/apereo/cas/oidc/profile/OidcProfileScopeToAttributesFilter.java
+++ support/cas-server-support-oidc-core-api/src/main/java/org/apereo/cas/oidc/profile/OidcProfileScopeToAttributesFilter.java
@@ -30,6 +30,7 @@ import org.reflections.scanners.SubTypesScanner;
 import org.reflections.util.ClasspathHelper;
 import org.reflections.util.ConfigurationBuilder;
 import org.reflections.util.FilterBuilder;
+import org.springframework.util.ClassUtils;
 
 import java.util.Collection;
 import java.util.LinkedHashMap;
@@ -76,12 +77,14 @@ public class OidcProfileScopeToAttributesFilter extends DefaultOAuth20ProfileSco
 
         val subTypes = reflections.getSubTypesOf(BaseOidcScopeAttributeReleasePolicy.class);
         subTypes.forEach(Unchecked.consumer(t -> {
-            val ex = t.getDeclaredConstructor().newInstance();
-            if (oidc.getScopes().contains(ex.getScopeType())) {
-                LOGGER.trace("Found OpenID Connect scope [{}] to filter attributes", ex.getScopeType());
-                filters.put(ex.getScopeType(), ex);
-            } else {
-                LOGGER.debug("OpenID Connect scope [{}] is not configured for use and will be ignored", ex.getScopeType());
+            if (ClassUtils.hasConstructor(t)) {
+                val ex = t.getDeclaredConstructor().newInstance();
+                if (oidc.getScopes().contains(ex.getScopeType())) {
+                    LOGGER.trace("Found standard OpenID Connect scope [{}] to filter attributes", ex.getScopeType());
+                    filters.put(ex.getScopeType(), ex);
+                } else {
+                    LOGGER.debug("OpenID Connect scope [{}] is not configured for use and will be ignored", ex.getScopeType());
+                }
             }
         }));
 
