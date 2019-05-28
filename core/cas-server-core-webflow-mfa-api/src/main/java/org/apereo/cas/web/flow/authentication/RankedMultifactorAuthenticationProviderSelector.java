4b607283a95e26b075e2559252faa50fb136f2dc
Misagh Moayyed <mm1844@gmail.com>
2019-05-28 06:53:01 -0700

    add more tests for mfa triggers

--- core/cas-server-core-webflow-mfa-api/src/main/java/org/apereo/cas/web/flow/authentication/RankedMultifactorAuthenticationProviderSelector.java
+++ core/cas-server-core-webflow-mfa-api/src/main/java/org/apereo/cas/web/flow/authentication/RankedMultifactorAuthenticationProviderSelector.java
@@ -24,7 +24,6 @@ import java.util.List;
 @Slf4j
 public class RankedMultifactorAuthenticationProviderSelector implements MultifactorAuthenticationProviderSelector {
 
-
     @Override
     public MultifactorAuthenticationProvider resolve(final Collection<MultifactorAuthenticationProvider> providers,
                                                      final RegisteredService service,
