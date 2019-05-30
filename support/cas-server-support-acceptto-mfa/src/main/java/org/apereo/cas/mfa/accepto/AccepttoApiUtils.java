b1cdf3be5545c122412356b92e93761fed04efdf
Misagh Moayyed <mmoayyed@unicon.net>
2019-05-30 11:04:13 -0700

    cntd with acceptto registration of devices and pairing

--- support/cas-server-support-acceptto-mfa/src/main/java/org/apereo/cas/mfa/accepto/AccepttoApiUtils.java
+++ support/cas-server-support-acceptto-mfa/src/main/java/org/apereo/cas/mfa/accepto/AccepttoApiUtils.java
@@ -2,11 +2,12 @@ package org.apereo.cas.mfa.accepto;
 
 import org.apereo.cas.authentication.Authentication;
 import org.apereo.cas.configuration.model.support.mfa.AccepttoMultifactorProperties;
+import org.apereo.cas.mfa.accepto.web.flow.AccepttoWebflowUtils;
 import org.apereo.cas.util.CollectionUtils;
 import org.apereo.cas.util.EncodingUtils;
 import org.apereo.cas.util.HttpUtils;
-import org.apereo.cas.util.crypto.PublicKeyFactoryBean;
 import org.apereo.cas.web.support.CookieUtils;
+import org.apereo.cas.web.support.WebUtils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.experimental.UtilityClass;
@@ -16,11 +17,13 @@ import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apereo.inspektr.common.web.ClientInfoHolder;
 import org.jose4j.jwt.JwtClaims;
 import org.jose4j.keys.AesKey;
+import org.springframework.webflow.execution.RequestContext;
 
-import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
+import java.security.PublicKey;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -98,58 +101,61 @@ public class AccepttoApiUtils {
     /**
      * Authenticate.
      *
-     * @param authentication the authentication
-     * @param acceptto       the acceptto
-     * @param request        the request
+     * @param authentication       the authentication
+     * @param acceptto             the acceptto
+     * @param requestContext       the request context
+     * @param apiResponsePublicKey the api response public key
      * @return the map
      */
     public static Map authenticate(final Authentication authentication,
                                    final AccepttoMultifactorProperties acceptto,
-                                   final HttpServletRequest request) {
+                                   final RequestContext requestContext,
+                                   final PublicKey apiResponsePublicKey) {
         val url = acceptto.getRegistrationApiUrl();
         val email = getUserEmailAttribute(authentication, acceptto);
         val sessionId = UUID.randomUUID().toString();
 
+        val request = WebUtils.getHttpServletRequestFromExternalWebflowContext(requestContext);
         LOGGER.debug("Principal email address determined from attribute [{}] is [{}]", acceptto.getEmailAttribute(), email);
         val parameters = CollectionUtils.<String, Object>wrap(
             "application_uid", acceptto.getApplicationId(),
             "type", "Login",
+            "ip_address", ClientInfoHolder.getClientInfo().getClientIpAddress(),
+            "remote_ip_address", request.getRemoteAddr(),
             "message", acceptto.getMessage(),
             "session_id", sessionId,
             "timeout", acceptto.getTimeout(),
             "email", email);
+
         CookieUtils.getCookieFromRequest("jwt", request)
             .ifPresent(cookie -> parameters.put("jwt", cookie.getValue()));
 
-        val claims = new JwtClaims();
-        claims.setClaim("uid", acceptto.getOrganizationId());
-        claims.setExpirationTimeMinutesInTheFuture(1);
-        val payload = claims.toJson();
-        LOGGER.trace("Authorization payload is [{}}", payload);
-        val signingKey = new AesKey(acceptto.getOrganizationSecret().getBytes(StandardCharsets.UTF_8));
-        val authzPayload = new String(EncodingUtils.signJwsHMACSha256(signingKey, payload.getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8);
-        LOGGER.trace("Signed authorization payload is [{}}", authzPayload);
+        AccepttoWebflowUtils.getEGuardianUserId(requestContext).ifPresent(value -> {
+            parameters.put("eguardian_user_id", value);
+        });
 
+        LOGGER.debug("Authentication API parameters are assembled as [{}]", parameters);
         HttpResponse response = null;
         try {
+            val authzPayload = buildAuthorizationHeaderPayloadForAuthentication(acceptto);
             val headers = CollectionUtils.<String, Object>wrap("Authorization", "Bearer " + authzPayload);
             response = HttpUtils.executePost(url, parameters, headers);
             if (response != null) {
                 val status = response.getStatusLine().getStatusCode();
                 LOGGER.debug("Authentication response status code is [{}]", status);
                 val results = MAPPER.readValue(response.getEntity().getContent(), Map.class);
+                LOGGER.trace("Received API response as [{}]", results);
+                if (!results.containsKey("content")) {
+                    throw new IllegalArgumentException("Unable to locate content in API response");
+                }
                 val content = results.get("content").toString();
+                LOGGER.trace("Loading public key from [{}] to validate response", acceptto.getRegistrationApiPublicKey().getLocation());
 
-                val factory = new PublicKeyFactoryBean();
-                factory.setResource(acceptto.getRegistrationApiPublicKey().getLocation());
-                factory.setSingleton(false);
-                factory.setAlgorithm("RSA");
-                val publicKey = factory.getObject();
-
-                val decoded = EncodingUtils.verifyJwsSignature(publicKey, content);
+                LOGGER.trace("Validating response signature for [{}]", content);
+                val decoded = EncodingUtils.verifyJwsSignature(apiResponsePublicKey, content);
                 if (decoded != null) {
                     val decodedResult = new String(decoded, StandardCharsets.UTF_8);
-                    LOGGER.debug("Received API response as [{}]", decodedResult);
+                    LOGGER.debug("Received final API response as [{}]", decodedResult);
                     return MAPPER.readValue(decodedResult, Map.class);
                 }
             }
@@ -159,6 +165,20 @@ public class AccepttoApiUtils {
             HttpUtils.close(response);
         }
         return new HashMap<>();
+    }
+
+    private static String buildAuthorizationHeaderPayloadForAuthentication(final AccepttoMultifactorProperties acceptto) {
+        val claims = new JwtClaims();
+        claims.setClaim("uid", acceptto.getOrganizationId());
+        claims.setExpirationTimeMinutesInTheFuture(1);
+        val payload = claims.toJson();
+        LOGGER.trace("Authorization payload is [{}}", payload);
+        val signingKey = new AesKey(acceptto.getOrganizationSecret().getBytes(StandardCharsets.UTF_8));
+        LOGGER.trace("Signing authorization payload...");
+        val signedBytes = EncodingUtils.signJwsHMACSha256(signingKey, payload.getBytes(StandardCharsets.UTF_8));
+        val authzPayload = new String(signedBytes, StandardCharsets.UTF_8);
+        LOGGER.trace("Signed authorization payload is [{}]", authzPayload);
+        return authzPayload;
     }
 
     /**
