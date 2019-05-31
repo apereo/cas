094944946929175746c2e0d49834fb9eaf6d0061
Misagh Moayyed <mm1844@gmail.com>
2019-05-31 13:40:23 -0700

    clean up hz instance config

--- support/cas-server-support-hazelcast-monitor/src/main/java/org/apereo/cas/monitor/config/HazelcastMonitorConfiguration.java
+++ support/cas-server-support-hazelcast-monitor/src/main/java/org/apereo/cas/monitor/config/HazelcastMonitorConfiguration.java
@@ -1,18 +1,17 @@
 package org.apereo.cas.monitor.config;
 
 import org.apereo.cas.configuration.CasConfigurationProperties;
-import org.apereo.cas.hz.HazelcastConfigurationFactory;
 import org.apereo.cas.monitor.HazelcastHealthIndicator;
 
-import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.actuate.autoconfigure.health.ConditionalOnEnabledHealthIndicator;
 import org.springframework.boot.actuate.health.HealthIndicator;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
@@ -32,19 +31,15 @@ public class HazelcastMonitorConfiguration implements DisposableBean {
     @Autowired
     private CasConfigurationProperties casProperties;
 
-    @ConditionalOnMissingBean(name = "casHealthIndicatorHazelcastInstance")
-    @Bean
-    public HazelcastInstance casHealthIndicatorHazelcastInstance() {
-        val hz = casProperties.getTicket().getRegistry().getHazelcast();
-        LOGGER.debug("Creating Hazelcast instance using properties [{}]", hz);
-        return Hazelcast.newHazelcastInstance(HazelcastConfigurationFactory.build(hz));
-    }
+    @Autowired
+    @Qualifier("casTicketRegistryHazelcastInstance")
+    private ObjectProvider<HazelcastInstance> casTicketRegistryHazelcastInstance;
 
     @Bean
     @RefreshScope
     @ConditionalOnEnabledHealthIndicator("hazelcastHealthIndicator")
     public HealthIndicator hazelcastHealthIndicator() {
-        val hazelcastInstance = casHealthIndicatorHazelcastInstance();
+        val hazelcastInstance = casTicketRegistryHazelcastInstance.getIfAvailable();
         val warn = casProperties.getMonitor().getWarn();
         return new HazelcastHealthIndicator(
             warn.getEvictionThreshold(),
@@ -55,9 +50,9 @@ public class HazelcastMonitorConfiguration implements DisposableBean {
 
     @Override
     public void destroy() {
-        val hz = casHealthIndicatorHazelcastInstance();
-        if (hz != null) {
-            hz.shutdown();
+        val hazelcastInstance = casTicketRegistryHazelcastInstance.getIfAvailable();
+        if (hazelcastInstance != null) {
+            hazelcastInstance.shutdown();
         }
     }
 }
