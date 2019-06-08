c8f9ee9ec4225c46da988377d33ed2be3c38cddd
Misagh Moayyed <mm1844@gmail.com>
2019-06-08 00:04:44 -0700

    working on test cases

---
+++ support/cas-server-support-reports/src/test/java/org/apereo/cas/web/report/CasReleaseAttributesReportEndpointTests.java
@@ -0,0 +1,36 @@
+package org.apereo.cas.web.report;
+
+import org.apereo.cas.services.RegisteredServiceTestUtils;
+
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This is {@link CasReleaseAttributesReportEndpointTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.1.0
+ */
+@TestPropertySource(properties = "management.endpoint.releaseAttributes.enabled=true")
+public class CasReleaseAttributesReportEndpointTests extends AbstractCasEndpointTests {
+    @Autowired
+    @Qualifier("releaseAttributesReportEndpoint")
+    private CasReleaseAttributesReportEndpoint endpoint;
+
+    @Test
+    public void verifyOperation() {
+        val service = RegisteredServiceTestUtils.getRegisteredService(UUID.randomUUID().toString());
+        this.servicesManager.save(service);
+        val response = endpoint.releasePrincipalAttributes("casuser", "casuser", service.getServiceId());
+        assertNotNull(response);
+        assertFalse(response.isEmpty());
+    }
+}
+
---
+++ support/cas-server-support-reports/src/test/java/org/apereo/cas/web/report/CasResolveAttributesReportEndpointTests.java
@@ -0,0 +1,30 @@
+package org.apereo.cas.web.report;
+
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This is {@link CasResolveAttributesReportEndpointTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.1.0
+ */
+@TestPropertySource(properties = "management.endpoint.resolveAttributes.enabled=true")
+public class CasResolveAttributesReportEndpointTests extends AbstractCasEndpointTests {
+    @Autowired
+    @Qualifier("resolveAttributesReportEndpoint")
+    private CasResolveAttributesReportEndpoint endpoint;
+
+    @Test
+    public void verifyOperation() {
+        val response = endpoint.resolvePrincipalAttributes("casuser");
+        assertNotNull(response);
+        assertFalse(response.isEmpty());
+    }
+}
+
