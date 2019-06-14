b9782e760a485f9ef7f7d8af79f8fdb8d0991fd3
Rich Renomeron <renomeron_r@omb.eop.gov>
2019-06-14 15:35:15 -0400

    Port MDUI bean def fixes to 5.3 branch (#4029)
    * Port MDUI bean def fixes to 5.3 branch
    
    Backport of commit 6d4b11ff615e20e2661a7dfd374433520956651a
    
    * Fix test classes to include config for beans missing by removing DependsOnBean annotation
    
    * Fix typo

--- support/cas-server-support-saml-mdui/src/main/java/org/apereo/cas/support/saml/mdui/config/SamlMetadataUIWebflowConfiguration.java
+++ support/cas-server-support-saml-mdui/src/main/java/org/apereo/cas/support/saml/mdui/config/SamlMetadataUIWebflowConfiguration.java
@@ -15,7 +15,6 @@ import org.apereo.cas.web.flow.CasWebflowExecutionPlan;
 import org.apereo.cas.web.flow.CasWebflowExecutionPlanConfigurer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
@@ -63,7 +62,6 @@ public class SamlMetadataUIWebflowConfiguration implements CasWebflowExecutionPl
     private MetadataResolverAdapter chainingSamlMetadataUIMetadataResolverAdapter;
 
     @ConditionalOnMissingBean(name = "samlMetadataUIWebConfigurer")
-    @ConditionalOnBean(name = "defaultWebflowConfigurer")
     @Bean
     @DependsOn("defaultWebflowConfigurer")
     public CasWebflowConfigurer samlMetadataUIWebConfigurer() {
