0a16b9d967f0ee4acf77681dcb948e2021182d73
Misagh Moayyed <mm1844@gmail.com>
2019-06-18 21:27:53 -0700

    Update SamlMetadataUIWebflowConfiguration.java

--- support/cas-server-support-saml-mdui/src/main/java/org/apereo/cas/support/saml/mdui/config/SamlMetadataUIWebflowConfiguration.java
+++ support/cas-server-support-saml-mdui/src/main/java/org/apereo/cas/support/saml/mdui/config/SamlMetadataUIWebflowConfiguration.java
@@ -17,7 +17,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
