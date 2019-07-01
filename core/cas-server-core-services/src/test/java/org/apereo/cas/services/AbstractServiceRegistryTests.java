e786df519ce83537433a754c8ce272bc5c5bdef2
Simon Bear <simon.bear@csiro.au>
2019-07-01 14:48:30 +1000

    Remove unused findServiceById(String) method

--- core/cas-server-core-services/src/test/java/org/apereo/cas/services/AbstractServiceRegistryTests.java
+++ core/cas-server-core-services/src/test/java/org/apereo/cas/services/AbstractServiceRegistryTests.java
@@ -113,7 +113,6 @@ public abstract class AbstractServiceRegistryTests {
     @Test
     public void verifyNonExistingService() {
         assertNull(this.serviceRegistry.findServiceById(9999991));
-        assertNull(this.serviceRegistry.findServiceById("9999991"));
     }
 
     @ParameterizedTest
