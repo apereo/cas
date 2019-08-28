8ddd98e5ad945ba49ffd95b92f1c4c5e43f93ca5
Hal Deadman <hal.deadman@gmail.com>
2019-08-28 10:37:53 -0400

    remove generate-idp-meta from test pending more testing with args

--- ci/tests/shell/cas-shell-script.sh
+++ ci/tests/shell/cas-shell-script.sh
@@ -2,5 +2,4 @@ help
 encrypt-value value SOMEVALUE password JASTYPTPW alg PBEWITHSHAAND256BITAES-CBC-BC provider BC
 decrypt-value value {cas-cipher}iARpnWTURDdiAhWdcHXxqJpncj4iRo3w9i2UT33stcs= password JASTYPTPW alg PBEWITHSHAAND256BITAES-CBC-BC provider BC
 jasypt-test-algorithms
-generate-idp-metadata metadataLocation ./ scope example.org hostName https://cas.example.org/cas entityId cas.example.org force
 exit
