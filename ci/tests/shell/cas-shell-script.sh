353415242f91318e18c2a2abee5ce1e90f5d0b88
hdeadman <hal.deadman@gmail.com>
2019-08-28 06:48:11 -0400

    squash! updated comment

--- ci/tests/shell/cas-shell-script.sh
+++ ci/tests/shell/cas-shell-script.sh
@@ -2,5 +2,5 @@ help
 encrypt-value value SOMEVALUE password JASTYPTPW alg PBEWITHSHAAND256BITAES-CBC-BC provider BC
 decrypt-value value {cas-cipher}iARpnWTURDdiAhWdcHXxqJpncj4iRo3w9i2UT33stcs= password JASTYPTPW alg PBEWITHSHAAND256BITAES-CBC-BC provider BC
 jasypt-test-algorithms
-generate-idp-metadata
+generate-idp-metadata metadataLocation ./ scope example.org force true hostName https://cas.example.org/cas entityId cas.example.org
 exit
