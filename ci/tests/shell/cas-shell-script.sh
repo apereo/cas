6d453d714f0c40e0081de0a863257e96a8037e7b
Hal Deadman <hdeadman@users.noreply.github.com>
2019-08-28 15:34:23 -0400

    fix shell so it exits, add test to make sure it starts and runs script (#4230)
    * fix shell so it exits, add test to make sure it starts and runs script

---
+++ ci/tests/shell/cas-shell-script.sh
@@ -0,0 +1,5 @@
+help
+encrypt-value value SOMEVALUE password JASTYPTPW alg PBEWITHSHAAND256BITAES-CBC-BC provider BC
+decrypt-value value {cas-cipher}iARpnWTURDdiAhWdcHXxqJpncj4iRo3w9i2UT33stcs= password JASTYPTPW alg PBEWITHSHAAND256BITAES-CBC-BC provider BC
+jasypt-test-algorithms
+exit
