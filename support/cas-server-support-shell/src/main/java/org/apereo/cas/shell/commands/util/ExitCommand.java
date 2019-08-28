021e1f8f432c97c7827a8c57046958acb74a57f0
hdeadman <hal.deadman@gmail.com>
2019-08-27 22:33:34 -0400

    suppress spotbugs warning

--- support/cas-server-support-shell/src/main/java/org/apereo/cas/shell/commands/util/ExitCommand.java
+++ support/cas-server-support-shell/src/main/java/org/apereo/cas/shell/commands/util/ExitCommand.java
@@ -1,5 +1,6 @@
 package org.apereo.cas.shell.commands.util;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.commands.Quit;
 
@@ -17,6 +18,7 @@ public class ExitCommand extends Quit {
     /**
      * Quit the shell when requested, regardless of running threads.
      */
+    @SuppressFBWarnings("DM_EXIT")
     public void quit() {
         super.quit();
         System.exit(0);
