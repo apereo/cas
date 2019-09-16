9e612d1afce23001dff7ea3729828a90260dc8ae
Misagh Moayyed <mm1844@gmail.com>
2019-09-16 14:22:23 +0430

    move test case to the right test class

--- support/cas-server-support-oidc/src/test/java/org/apereo/cas/oidc/claims/OidcEmailScopeAttributeReleasePolicyTests.java
+++ support/cas-server-support-oidc/src/test/java/org/apereo/cas/oidc/claims/OidcEmailScopeAttributeReleasePolicyTests.java
@@ -8,6 +8,7 @@ import org.apereo.cas.util.CollectionUtils;
 import lombok.val;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.TestPropertySource;
 
 import java.util.List;
 
@@ -20,6 +21,10 @@ import static org.junit.jupiter.api.Assertions.*;
  * @since 6.1.0
  */
 @Tag("OIDC")
+@TestPropertySource(properties = {
+    "cas.authn.oidc.claimsMap.email=mail",
+    "cas.authn.oidc.claimsMap.email_verified=mail_confirmed"
+})
 public class OidcEmailScopeAttributeReleasePolicyTests extends AbstractOidcTests {
     @Test
     public void verifyOperation() {
@@ -32,5 +37,29 @@ public class OidcEmailScopeAttributeReleasePolicyTests extends AbstractOidcTests
             CoreAuthenticationTestUtils.getService(),
             CoreAuthenticationTestUtils.getRegisteredService());
         assertTrue(policy.getAllowedAttributes().stream().allMatch(attrs::containsKey));
+    }
+
+    @Test
+    public void verifyClaimMapOperation() {
+        val policy = new OidcEmailScopeAttributeReleasePolicy();
+        assertEquals(OidcConstants.StandardScopes.EMAIL.getScope(), policy.getScopeType());
+        assertNotNull(policy.getAllowedAttributes());
+
+        val principal = CoreAuthenticationTestUtils.getPrincipal(CollectionUtils.wrap("mail", List.of("cas@example.org"),
+            "mail_confirmed", List.of("cas@example.org"), "phone", List.of("0000000000")));
+        val attrs = policy.getAttributes(principal,
+            CoreAuthenticationTestUtils.getService(),
+            CoreAuthenticationTestUtils.getRegisteredService());
+        assertEquals(List.of("cas@example.org"), attrs.get("email"));
+        assertEquals(List.of("cas@example.org"), attrs.get("email_verified"));
+        assertFalse(attrs.containsKey("phone"));
+
+        val serviceTicketPrincipal = CoreAuthenticationTestUtils.getPrincipal(attrs);
+        val releaseAttrs = policy.getAttributes(serviceTicketPrincipal,
+            CoreAuthenticationTestUtils.getService(),
+            CoreAuthenticationTestUtils.getRegisteredService());
+        assertEquals(List.of("cas@example.org"), releaseAttrs.get("email"));
+        assertEquals(List.of("cas@example.org"), releaseAttrs.get("email_verified"));
+        assertFalse(releaseAttrs.containsKey("phone"));
     }
 }
