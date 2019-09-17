cab829a54208e7f6b7112fa1fce9a6c137546154
vinarasimhanlatha <vinarasimhanlatha@ebay.com>
2019-09-17 12:30:02 +0200

    Added test to cover limited scopes

--- support/cas-server-support-oidc/src/test/java/org/apereo/cas/oidc/profile/OidcProfileScopeToAttributesFilterTests.java
+++ support/cas-server-support-oidc/src/test/java/org/apereo/cas/oidc/profile/OidcProfileScopeToAttributesFilterTests.java
@@ -67,6 +67,32 @@ public class OidcProfileScopeToAttributesFilterTests extends AbstractOidcTests {
     }
 
     @Test
+    public void verifyOperationFilterWithLimitedScopes() {
+        val service = getOidcRegisteredService();
+        val accessToken = mock(AccessToken.class);
+        when(accessToken.getTicketGrantingTicket()).thenReturn(new MockTicketGrantingTicket("casuser"));
+        when(accessToken.getScopes()).thenReturn(CollectionUtils.wrapSet(
+                OidcConstants.StandardScopes.OPENID.getScope(),
+                OidcConstants.StandardScopes.PHONE.getScope(),
+                OidcConstants.StandardScopes.PROFILE.getScope(),
+                OidcConstants.StandardScopes.ADDRESS.getScope(),
+                OidcConstants.StandardScopes.EMAIL.getScope()));
+
+        service.getScopes().clear();
+        service.getScopes().add(OidcConstants.StandardScopes.EMAIL.getScope());
+
+        val context = new JEEContext(new MockHttpServletRequest(), new MockHttpServletResponse());
+        val original = CoreAuthenticationTestUtils.getPrincipal(
+                CollectionUtils.wrap("email", "casuser@example.org", "address", "1234 Main Street",
+                        "phone", "123445677", "name", "CAS", "gender", "male"));
+        val principal = profileScopeToAttributesFilter.filter(CoreAuthenticationTestUtils.getService(),
+                original, service, context, accessToken);
+
+        assertTrue(principal.getAttributes().containsKey("email"));
+        assertEquals(1, principal.getAttributes().size());
+    }
+
+    @Test
     public void verifyOperationFilterWithOpenIdWithoutServiceScopes() {
         val service = getOidcRegisteredService();
         val accessToken = mock(AccessToken.class);
