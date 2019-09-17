7c27d290ffb384bad805101f838ca6fd6bd66583
vinarasimhanlatha <vinarasimhanlatha@ebay.com>
2019-09-17 13:36:17 +0200

    added back changes removed due to merge

--- support/cas-server-support-oauth/src/test/java/org/apereo/cas/support/oauth/web/AbstractOAuth20Tests.java
+++ support/cas-server-support-oauth/src/test/java/org/apereo/cas/support/oauth/web/AbstractOAuth20Tests.java
@@ -179,6 +179,8 @@ public abstract class AbstractOAuth20Tests {
     public static final String PASSWORD = "password";
     public static final String GOOD_USERNAME = "test";
     public static final String GOOD_PASSWORD = "test";
+    public static final String CODE_CHALLENGE = "myclientcode";
+    public static final String CODE_CHALLENGE_METHOD_PLAIN = "plain";
 
     public static final int DELTA = 2;
     public static final int TIMEOUT = 7200;
@@ -295,12 +297,17 @@ public abstract class AbstractOAuth20Tests {
     }
 
     protected OAuthCode addCode(final Principal principal, final OAuthRegisteredService registeredService) {
+        return addCodeWithChallenge(principal, registeredService, null, null);
+    }
+
+    protected OAuthCode addCodeWithChallenge(final Principal principal, final OAuthRegisteredService registeredService,
+                                             final String codeChallenge, final String codeChallengeMethod) {
         val authentication = getAuthentication(principal);
         val factory = new WebApplicationServiceFactory();
         val service = factory.createService(registeredService.getClientId());
         val code = oAuthCodeFactory.create(service, authentication,
-            new MockTicketGrantingTicket("casuser"), new ArrayList<>(),
-            null, null, CLIENT_ID, new HashMap<>());
+                new MockTicketGrantingTicket("casuser"), new ArrayList<>(),
+                codeChallenge, codeChallengeMethod, CLIENT_ID, new HashMap<>());
         this.ticketRegistry.addTicket(code);
         return code;
     }
