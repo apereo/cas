f30f1d492e4a9f69a2308831b6fa599f629a32f5
smaszno <smaszno@gmail.com>
2019-09-27 12:21:24 +0200

    OpenActivator logging

--- support/cas-server-support-ldap-core/src/main/java/org/apereo/cas/util/OpenActivator.java
+++ support/cas-server-support-ldap-core/src/main/java/org/apereo/cas/util/OpenActivator.java
@@ -2,13 +2,14 @@ package org.apereo.cas.util;
 
 import org.ldaptive.Connection;
 import org.ldaptive.pool.Activator;
+import lombok.extern.slf4j.Slf4j;
 /*
   We need to specify out own activator, based on ConnectActivator from ldaptive, 
   because ConnectActivator does not check whether the connection is already open, 
   which can cause an exception (when opening an open connection)
   Feel free to move this class somewhere else...
 */
-  
+  @Slf4j
     public class OpenActivator implements Activator<Connection> {
         
         public boolean activate(Connection c) {
