392e8e6dd24650880e04c4e167e451911bd62eac
Misagh Moayyed <mm1844@gmail.com>
2019-09-30 13:19:55 +0330

    add spring swf sesison mgmt back to CAS

---
+++ core/cas-server-core-webflow-api/src/test/java/org/springframework/webflow/test/CasMockViewFactoryCreator.java
@@ -0,0 +1,13 @@
+package org.springframework.webflow.test;
+
+/**
+ * This is {@link CasMockViewFactoryCreator}.
+ * 
+ * This class is required to be in this package
+ * to have access to {@link MockViewFactoryCreator} which is package-private itself.
+ * 
+ * @author Misagh Moayyed
+ * @since 6.1.0
+ */
+public class CasMockViewFactoryCreator extends MockViewFactoryCreator {
+}
