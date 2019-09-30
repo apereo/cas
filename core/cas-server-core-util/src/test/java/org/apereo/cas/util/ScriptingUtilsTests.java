2290ca8e8b626a163567310937fa6b17baeefd57
Sean Gottschalk <seangottschalk@gmail.com>
2019-09-30 14:12:23 -0700

    Fix issue where Groovy scripts on the classpath cannot be loaded

--- core/cas-server-core-util/src/test/java/org/apereo/cas/util/ScriptingUtilsTests.java
+++ core/cas-server-core-util/src/test/java/org/apereo/cas/util/ScriptingUtilsTests.java
@@ -6,6 +6,7 @@ import lombok.SneakyThrows;
 import lombok.val;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 
 import java.io.File;
@@ -22,17 +23,17 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ScriptingUtilsTests {
 
     @Test
-    public void verifyInlineGroovyScript() {
+    void verifyInlineGroovyScript() {
         assertTrue(ScriptingUtils.isInlineGroovyScript("groovy {return 0}"));
     }
 
     @Test
-    public void verifyExternalGroovyScript() {
+    void verifyExternalGroovyScript() {
         assertTrue(ScriptingUtils.isExternalGroovyScript("file:/tmp/sample.groovy"));
     }
 
     @Test
-    public void verifyGroovyScriptShellExecution() {
+    void verifyGroovyScriptShellExecution() {
         val script = ScriptingUtils.parseGroovyShellScript("return name");
         val result = ScriptingUtils.executeGroovyShellScript(script, CollectionUtils.wrap("name", "casuser"), String.class);
         assertEquals("casuser", result);
@@ -40,7 +41,7 @@ public class ScriptingUtilsTests {
 
     @Test
     @SneakyThrows
-    public void verifyGroovyResourceExecution() {
+    void verifyGroovyResourceFileExecution() {
         val file = File.createTempFile("test", ".groovy");
         FileUtils.write(file, "def process(String name) { return name }", StandardCharsets.UTF_8);
         val resource = new FileSystemResource(file);
@@ -50,14 +51,27 @@ public class ScriptingUtilsTests {
     }
 
     @Test
-    public void verifyGroovyResourceEngineExecution() {
+    @SneakyThrows
+    void verifyGroovyResourceClasspathExecution() {
+        try {
+            val resource = new ClassPathResource("ScriptingUtilsTestGroovyScript.groovy");
+
+            val result = ScriptingUtils.executeGroovyScript(resource, "process", String.class, "casuser");
+            assertEquals("casuser", result);
+        } catch (final Exception e) {
+            throw new AssertionError(e.getMessage(), e);
+        }
+    }
+
+    @Test
+    void verifyGroovyResourceEngineExecution() {
         val result = ScriptingUtils.executeGroovyScriptEngine("return name", CollectionUtils.wrap("name", "casuser"), String.class);
         assertEquals("casuser", result);
     }
 
     @Test
     @SneakyThrows
-    public void verifyResourceScriptEngineExecution() {
+    void verifyResourceScriptEngineExecution() {
         val file = File.createTempFile("test", ".groovy");
         FileUtils.write(file, "def run(String name) { return name }", StandardCharsets.UTF_8);
 
