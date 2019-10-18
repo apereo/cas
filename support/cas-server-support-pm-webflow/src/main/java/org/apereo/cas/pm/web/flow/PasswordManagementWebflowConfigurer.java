9751aadf63cae887b954e24fba7e76124c92afd9
Axel Stohn <axman000@gmail.com>
2019-10-18 03:11:28 -0700

    Change required to fix PM webflow functionality (#4357)
    * WIP - change required to fix PM functionality
    
    * Refactored to meet CAS admin request

--- support/cas-server-support-pm-webflow/src/main/java/org/apereo/cas/pm/web/flow/PasswordManagementWebflowConfigurer.java
+++ support/cas-server-support-pm-webflow/src/main/java/org/apereo/cas/pm/web/flow/PasswordManagementWebflowConfigurer.java
@@ -17,6 +17,7 @@ import org.springframework.webflow.engine.Flow;
 import org.springframework.webflow.engine.ViewState;
 import org.springframework.webflow.engine.builder.support.FlowBuilderServices;
 import org.springframework.webflow.execution.Action;
+import org.springframework.webflow.execution.RequestContext;
 
 /**
  * This is {@link PasswordManagementWebflowConfigurer}.
@@ -65,8 +66,7 @@ public class PasswordManagementWebflowConfigurer extends AbstractCasWebflowConfi
     }
 
     private void createAccountStatusViewStates(final Flow flow) {
-        val initialAction = getState(flow, CasWebflowConstants.ACTION_ID_INITIAL_FLOW_SETUP, ActionState.class);
-        initialAction.getActionList().add(requestContext -> {
+        flow.getStartActionList().add(requestContext -> {
             WebUtils.putPasswordManagementEnabled(requestContext, casProperties.getAuthn().getPm().isEnabled());
             return null;
         });
