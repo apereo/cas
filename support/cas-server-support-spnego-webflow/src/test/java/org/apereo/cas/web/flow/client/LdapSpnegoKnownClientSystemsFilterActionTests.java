161f6ce1536cdc9af19bc5f0e98a8ff8d4e55d16
Misagh Moayyed <mm1844@gmail.com>
2019-10-27 17:35:24 +0400

    fix spnego tests

--- support/cas-server-support-spnego-webflow/src/test/java/org/apereo/cas/web/flow/client/LdapSpnegoKnownClientSystemsFilterActionTests.java
+++ support/cas-server-support-spnego-webflow/src/test/java/org/apereo/cas/web/flow/client/LdapSpnegoKnownClientSystemsFilterActionTests.java
@@ -12,7 +12,24 @@ import org.springframework.test.context.TestPropertySource;
  * @author Misagh Moayyed
  * @since 4.1
  */
-@TestPropertySource(locations = {"classpath:/spnego.properties", "classpath:/spnego-ldap.properties"})
+@TestPropertySource(properties = {
+    "cas.authn.spnego.ldap.ldapUrl=ldap://localhost:1381",
+    "cas.authn.spnego.ldap.useSsl=false",
+    "cas.authn.spnego.ldap.baseDn=ou=people,dc=example,dc=org",
+    "cas.authn.spnego.ldap.searchFilter=host={host}",
+    "cas.authn.spnego.ldap.bindDn=cn=Directory Manager,dc=example,dc=org",
+    "cas.authn.spnego.ldap.bindCredential=Password",
+
+    "cas.authn.attributeRepository.stub.attributes.uid=uid",
+    "cas.authn.attributeRepository.stub.attributes.host=host",
+    "cas.authn.attributeRepository.stub.attributes.mail=mail",
+
+    "cas.authn.spnego.alternativeRemoteHostAttribute=",
+    "cas.authn.spnego.ipsToCheckPattern=.+",
+    "cas.authn.spnego.dnsTimeout=0",
+    "cas.authn.spnego.hostNameClientActionStrategy=ldapSpnegoClientAction",
+    "cas.authn.spnego.spnegoAttributeName=mail"
+})
 @DisabledIfContinuousIntegration
 public class LdapSpnegoKnownClientSystemsFilterActionTests extends BaseLdapSpnegoKnownClientSystemsFilterActionTests {
 
