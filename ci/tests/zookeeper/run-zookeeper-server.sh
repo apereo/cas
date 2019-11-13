cfd42499793f2c30de1129855a874bf2f64ce719
Misagh Moayyed <mm1844@gmail.com>
2019-11-13 19:53:16 +0400

    added zookeeper tests for cas config

--- ci/tests/zookeeper/run-zookeeper-server.sh
+++ ci/tests/zookeeper/run-zookeeper-server.sh
@@ -3,6 +3,6 @@
 echo "Running ZooKeeper docker image..."
 docker run --name zookeeper -p 2181:2181 -p 2888:2888 -p 3888:3888 -p 8080:8080 -d zookeeper:latest
 echo "Waiting for ZooKeeper image to prepare..."
-sleep 10
+sleep 5
 docker ps | grep "zookeeper"
 echo "ZooKeeper docker image is running."
