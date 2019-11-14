daa138e1cb2a1dd6a80d33bb77402dfeb8129587
Misagh Moayyed <mm1844@gmail.com>
2019-11-14 14:25:43 +0400

    fix compile issues

--- support/cas-server-support-pac4j-webflow/src/main/java/org/apereo/cas/web/flow/DelegatedAuthenticationClientLogoutAction.java
+++ support/cas-server-support-pac4j-webflow/src/main/java/org/apereo/cas/web/flow/DelegatedAuthenticationClientLogoutAction.java
@@ -2,11 +2,10 @@ package org.apereo.cas.web.flow;
 
 import org.apereo.cas.web.support.WebUtils;
 
-import java.util.Optional;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+import org.pac4j.core.client.Client;
 import org.pac4j.core.client.Clients;
 import org.pac4j.core.context.JEEContext;
 import org.pac4j.core.context.session.SessionStore;
@@ -17,6 +16,8 @@ import org.pac4j.core.profile.ProfileManager;
 import org.springframework.webflow.action.AbstractAction;
 import org.springframework.webflow.execution.Event;
 import org.springframework.webflow.execution.RequestContext;
+
+import java.util.Optional;
 
 /**
  * This is {@link DelegatedAuthenticationClientLogoutAction}.
@@ -35,33 +36,8 @@ import org.springframework.webflow.execution.RequestContext;
 @RequiredArgsConstructor
 public class DelegatedAuthenticationClientLogoutAction extends AbstractAction {
     private final Clients clients;
+
     private final SessionStore<JEEContext> sessionStore;
-
-    @Override
-    protected Event doExecute(final RequestContext requestContext) {
-        try {
-            val request = WebUtils.getHttpServletRequestFromExternalWebflowContext(requestContext);
-            val response = WebUtils.getHttpServletResponseFromExternalWebflowContext(requestContext);
-            val context = new JEEContext(request, response, this.sessionStore);
-
-            val currentProfile = findCurrentProfile(context);
-            val clientResult = currentProfile == null ? Optional.empty() : clients.findClient(currentProfile.getClientName());
-            if (clientResult.isPresent()) {
-                val client = clientResult.get();
-                LOGGER.debug("Located client [{}]", client);
-                val actionResult = client.getLogoutAction(context, currentProfile, null);
-                if (actionResult.isPresent()) {
-                    val action = (HttpAction) actionResult.get();
-                    new JEEHttpActionAdapter().adapt(action, context);
-                }
-            } else {
-                LOGGER.debug("The current client cannot be found at all, no logout action will be executed.");
-            }
-        } catch (final Exception e) {
-            LOGGER.warn(e.getMessage(), e);
-        }
-        return null;
-    }
 
     /**
      * Finds the current profile from the context.
@@ -73,6 +49,34 @@ public class DelegatedAuthenticationClientLogoutAction extends AbstractAction {
         val pm = new ProfileManager<CommonProfile>(webContext, webContext.getSessionStore());
         val profile = pm.get(true);
         return profile.orElse(null);
+    }
+
+    @Override
+    protected Event doExecute(final RequestContext requestContext) {
+        try {
+            val request = WebUtils.getHttpServletRequestFromExternalWebflowContext(requestContext);
+            val response = WebUtils.getHttpServletResponseFromExternalWebflowContext(requestContext);
+            val context = new JEEContext(request, response, this.sessionStore);
+
+            val currentProfile = findCurrentProfile(context);
+            val clientResult = currentProfile == null
+                ? Optional.<Client>empty()
+                : clients.findClient(currentProfile.getClientName());
+            if (clientResult.isPresent()) {
+                val client = clientResult.get();
+                LOGGER.debug("Located client [{}]", client);
+                val actionResult = client.getLogoutAction(context, currentProfile, null);
+                if (actionResult.isPresent()) {
+                    val action = (HttpAction) actionResult.get();
+                    new JEEHttpActionAdapter().adapt(action, context);
+                }
+            } else {
+                LOGGER.debug("The current client cannot be found and no logout action will be executed.");
+            }
+        } catch (final Exception e) {
+            LOGGER.warn(e.getMessage(), e);
+        }
+        return null;
     }
 
 }
