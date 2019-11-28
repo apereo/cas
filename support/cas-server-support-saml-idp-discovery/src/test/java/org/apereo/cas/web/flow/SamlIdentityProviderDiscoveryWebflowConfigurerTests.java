fe296a1876693669a0666f7188f9ba8569d09115
Misagh Moayyed <mm1844@gmail.com>
2019-11-28 13:05:41 +0400

    add tests

---
+++ support/cas-server-support-saml-idp-discovery/src/test/java/org/apereo/cas/web/flow/SamlIdentityProviderDiscoveryWebflowConfigurerTests.java
@@ -0,0 +1,46 @@
+package org.apereo.cas.web.flow;
+
+import org.apereo.cas.config.CoreSamlConfiguration;
+import org.apereo.cas.config.SamlIdentityProviderDiscoveryConfiguration;
+import org.apereo.cas.support.pac4j.config.Pac4jDelegatedAuthenticationConfiguration;
+import org.apereo.cas.support.pac4j.config.support.authentication.Pac4jAuthenticationEventExecutionPlanConfiguration;
+import org.apereo.cas.web.DelegatedClientIdentityProviderConfiguration;
+import org.apereo.cas.web.flow.config.DelegatedAuthenticationWebflowConfiguration;
+
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcAutoConfiguration;
+import org.springframework.context.annotation.Import;
+import org.springframework.webflow.engine.Flow;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This is {@link SamlIdentityProviderDiscoveryWebflowConfigurerTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.2.0
+ */
+@Import({
+    ThymeleafAutoConfiguration.class,
+    WebMvcAutoConfiguration.class,
+    MockMvcAutoConfiguration.class,
+    ErrorMvcAutoConfiguration.class,
+    CoreSamlConfiguration.class,
+    Pac4jAuthenticationEventExecutionPlanConfiguration.class,
+    Pac4jDelegatedAuthenticationConfiguration.class,
+    DelegatedAuthenticationWebflowConfiguration.class,
+    SamlIdentityProviderDiscoveryConfiguration.class,
+    BaseWebflowConfigurerTests.SharedTestConfiguration.class
+})
+public class SamlIdentityProviderDiscoveryWebflowConfigurerTests extends BaseWebflowConfigurerTests {
+    @Test
+    public void verifyOperation() {
+        assertFalse(casWebflowExecutionPlan.getWebflowConfigurers().isEmpty());
+        val flow = (Flow) this.loginFlowDefinitionRegistry.getFlowDefinition(CasWebflowConfigurer.FLOW_ID_LOGIN);
+        assertNotNull(flow);
+    }
+}
