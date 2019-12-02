c5fcdb309bc681a8906cf83a87c4d89aa380b861
Misagh Moayyed <mm1844@gmail.com>
2019-12-02 21:16:57 +0400

    add tests for trusted webflow mfa config

---
+++ support/cas-server-support-radius-mfa/src/test/java/org/apereo/cas/adaptors/radius/web/flow/BaseRadiusMultifactorAuthenticationTests.java
@@ -0,0 +1,81 @@
+package org.apereo.cas.adaptors.radius.web.flow;
+
+import org.apereo.cas.config.CasCoreAuthenticationConfiguration;
+import org.apereo.cas.config.CasCoreAuthenticationPrincipalConfiguration;
+import org.apereo.cas.config.CasCoreAuthenticationSupportConfiguration;
+import org.apereo.cas.config.CasCoreConfiguration;
+import org.apereo.cas.config.CasCoreHttpConfiguration;
+import org.apereo.cas.config.CasCoreMultifactorAuthenticationConfiguration;
+import org.apereo.cas.config.CasCoreServicesConfiguration;
+import org.apereo.cas.config.CasCoreTicketIdGeneratorsConfiguration;
+import org.apereo.cas.config.CasCoreTicketsConfiguration;
+import org.apereo.cas.config.CasCoreUtilConfiguration;
+import org.apereo.cas.config.CasCoreWebConfiguration;
+import org.apereo.cas.config.CasPersonDirectoryTestConfiguration;
+import org.apereo.cas.config.RadiusMultifactorConfiguration;
+import org.apereo.cas.config.support.CasWebApplicationServiceFactoryConfiguration;
+import org.apereo.cas.config.support.authentication.RadiusTokenAuthenticationComponentSerializationConfiguration;
+import org.apereo.cas.config.support.authentication.RadiusTokenAuthenticationEventExecutionPlanConfiguration;
+import org.apereo.cas.config.support.authentication.RadiusTokenAuthenticationMultifactorProviderBypassConfiguration;
+import org.apereo.cas.logout.config.CasCoreLogoutConfiguration;
+import org.apereo.cas.trusted.config.MultifactorAuthnTrustConfiguration;
+import org.apereo.cas.trusted.config.MultifactorAuthnTrustWebflowConfiguration;
+import org.apereo.cas.trusted.config.MultifactorAuthnTrustedDeviceFingerprintConfiguration;
+import org.apereo.cas.web.config.CasCookieConfiguration;
+import org.apereo.cas.web.flow.config.CasCoreWebflowConfiguration;
+import org.apereo.cas.web.flow.config.CasMultifactorAuthenticationWebflowConfiguration;
+import org.apereo.cas.web.flow.config.CasWebflowContextConfiguration;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration;
+import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
+import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * This is {@link BaseRadiusMultifactorAuthenticationTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.2.0
+ */
+public abstract class BaseRadiusMultifactorAuthenticationTests {
+    @ImportAutoConfiguration({
+        RefreshAutoConfiguration.class,
+        MailSenderAutoConfiguration.class,
+        AopAutoConfiguration.class
+    })
+    @SpringBootConfiguration
+    @Import({
+        CasCoreMultifactorAuthenticationConfiguration.class,
+        CasMultifactorAuthenticationWebflowConfiguration.class,
+        CasPersonDirectoryTestConfiguration.class,
+        CasCoreServicesConfiguration.class,
+        CasCoreAuthenticationConfiguration.class,
+        CasCoreAuthenticationSupportConfiguration.class,
+        CasCoreAuthenticationPrincipalConfiguration.class,
+        CasCoreTicketsConfiguration.class,
+        CasCoreTicketIdGeneratorsConfiguration.class,
+        CasWebApplicationServiceFactoryConfiguration.class,
+        CasCoreLogoutConfiguration.class,
+        CasCookieConfiguration.class,
+        CasCoreHttpConfiguration.class,
+        CasCoreConfiguration.class,
+        CasCoreWebConfiguration.class,
+        CasCoreWebflowConfiguration.class,
+        CasWebflowContextConfiguration.class,
+        CasCoreUtilConfiguration.class,
+
+        MultifactorAuthnTrustConfiguration.class,
+        MultifactorAuthnTrustedDeviceFingerprintConfiguration.class,
+        MultifactorAuthnTrustWebflowConfiguration.class,
+        RadiusMultifactorConfiguration.RadiusMultifactorTrustConfiguration.class,
+
+        RadiusMultifactorConfiguration.class,
+        RadiusTokenAuthenticationComponentSerializationConfiguration.class,
+        RadiusTokenAuthenticationEventExecutionPlanConfiguration.class,
+        RadiusTokenAuthenticationMultifactorProviderBypassConfiguration.class
+    })
+    public static class SharedTestConfiguration {
+    }
+}
---
+++ support/cas-server-support-radius-mfa/src/test/java/org/apereo/cas/adaptors/radius/web/flow/RadiusMultifactorWebflowConfigurerTests.java
@@ -0,0 +1,42 @@
+package org.apereo.cas.adaptors.radius.web.flow;
+
+import org.apereo.cas.web.flow.configurer.BaseMultifactorWebflowConfigurerTests;
+
+import org.junit.jupiter.api.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.webflow.definition.registry.FlowDefinitionRegistry;
+
+/**
+ * This is {@link RadiusMultifactorWebflowConfigurerTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.2.0
+ */
+@SpringBootTest(classes = BaseRadiusMultifactorAuthenticationTests.SharedTestConfiguration.class,
+    properties = {
+        "cas.authn.mfa.radius.trustedDeviceEnabled=true",
+        "cas.authn.mfa.trusted.deviceRegistrationEnabled=true",
+        "cas.authn.radius.client.sharedSecret=NoSecret",
+        "cas.authn.radius.client.inetAddress=localhost,localguest",
+        "cas.webflow.crypto.encryption.key=qLhvLuaobvfzMmbo9U_bYA",
+        "cas.webflow.crypto.signing.key=oZeAR5pEXsolruu4OQYsQKxf-FCvFzSsKlsVaKmfIl6pNzoPm6zPW94NRS1af7vT-0bb3DpPBeksvBXjloEsiA"
+    })
+@Tag("Radius")
+public class RadiusMultifactorWebflowConfigurerTests extends BaseMultifactorWebflowConfigurerTests {
+    @Autowired
+    @Qualifier("radiusFlowRegistry")
+    private FlowDefinitionRegistry radiusFlowRegistry;
+
+    @Override
+    protected FlowDefinitionRegistry getMultifactorFlowDefinitionRegistry() {
+        return this.radiusFlowRegistry;
+    }
+
+    @Override
+    protected String getMultifactorEventId() {
+        return RadiusMultifactorWebflowConfigurer.MFA_RADIUS_EVENT_ID;
+    }
+}
+
