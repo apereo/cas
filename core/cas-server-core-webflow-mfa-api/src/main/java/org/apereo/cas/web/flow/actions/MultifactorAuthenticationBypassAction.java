ed847f44dbeba1c4c4c64779d1ec662c173cbd85
Misagh Moayyed <mm1844@gmail.com>
2019-12-06 15:56:18 +0400

    working on test cases for mfa webflow

--- core/cas-server-core-webflow-mfa-api/src/main/java/org/apereo/cas/web/flow/actions/MultifactorAuthenticationBypassAction.java
+++ core/cas-server-core-webflow-mfa-api/src/main/java/org/apereo/cas/web/flow/actions/MultifactorAuthenticationBypassAction.java
@@ -5,7 +5,6 @@ import org.apereo.cas.web.support.WebUtils;
 
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.webflow.execution.Event;
 import org.springframework.webflow.execution.RequestContext;
@@ -32,28 +31,29 @@ public class MultifactorAuthenticationBypassAction extends AbstractMultifactorAu
 
         val bypass = provider.getBypassEvaluator();
 
+        val principal = authentication.getPrincipal();
         if (requestContext.getCurrentTransition().getId().equals(CasWebflowConstants.TRANSITION_ID_BYPASS)) {
             LOGGER.debug("Bypass triggered by MFA webflow for MFA for user [{}] for provider [{}]",
-                    authentication.getPrincipal().getId(), provider.getId());
+                    principal.getId(), provider.getId());
             bypass.rememberBypass(authentication, provider);
             LOGGER.debug("Authentication updated to remember bypass for user [{}] for provider [{}]",
-                    authentication.getPrincipal().getId(), provider.getId());
+                    principal.getId(), provider.getId());
             return yes();
         }
 
         if (bypass.shouldMultifactorAuthenticationProviderExecute(authentication, service, provider, request)) {
             LOGGER.debug("Bypass rules determined MFA should execute for user [{}] and provider [{}]",
-                    authentication.getPrincipal().getId(), provider.getId());
+                    principal.getId(), provider.getId());
             bypass.forgetBypass(authentication);
             LOGGER.debug("Authentication updated to forget any existing bypass for user [{}] for provider [{}]",
-                    authentication.getPrincipal().getId(), provider.getId());
+                    principal.getId(), provider.getId());
             return no();
         }
         LOGGER.debug("Bypass rules determined MFA should NOT execute for user [{}] for provider [{}]",
-                authentication.getPrincipal().getId(), provider.getId());
+                principal.getId(), provider.getId());
         bypass.rememberBypass(authentication, provider);
         LOGGER.debug("Authentication updated to remember bypass for user [{}] for provider [{}]",
-                authentication.getPrincipal().getId(), provider.getId());
+                principal.getId(), provider.getId());
         return yes();
     }
 }
