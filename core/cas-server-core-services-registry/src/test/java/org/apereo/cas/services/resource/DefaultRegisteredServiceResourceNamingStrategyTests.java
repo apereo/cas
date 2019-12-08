cfa1edb46e1ff2802e1f0869d5f793f83525dd06
Misagh Moayyed <mm1844@gmail.com>
2019-12-08 17:18:26 +0400

    support multipl ldaps for aup; add test for naming strategies

---
+++ core/cas-server-core-services-registry/src/test/java/org/apereo/cas/services/resource/DefaultRegisteredServiceResourceNamingStrategyTests.java
@@ -0,0 +1,30 @@
+package org.apereo.cas.services.resource;
+
+import org.apereo.cas.services.RegisteredService;
+
+import lombok.val;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * This is {@link DefaultRegisteredServiceResourceNamingStrategyTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.2.0
+ */
+public class DefaultRegisteredServiceResourceNamingStrategyTests {
+    @Test
+    public void verifyOperation() {
+        val service = mock(RegisteredService.class);
+        when(service.getId()).thenReturn(1000L);
+        when(service.getName()).thenReturn("Test");
+
+        val strategy = new DefaultRegisteredServiceResourceNamingStrategy();
+        assertNotNull(strategy.buildNamingPattern("json", "xml", "xyz"));
+        val result = strategy.build(service, "json");
+        assertNotNull(result);
+        assertEquals("Test-1000.json", result);
+    }
+}
