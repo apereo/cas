097ebfe29f60429653b0d843391395a921103700
Hayden Sartoris <hsartoris@bard.edu>
2019-12-19 15:36:00 -0500

    oh for goodness sake

--- support/cas-server-support-multiphase/src/main/java/org/apereo/cas/multiphase/config/MultiphaseUserAuthenticationConfiguration.java
+++ support/cas-server-support-multiphase/src/main/java/org/apereo/cas/multiphase/config/MultiphaseUserAuthenticationConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
