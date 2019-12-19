6c5ba4cb9af5b09b3d61d9ee4f384ebbf7e789ab
Hayden Sartoris <hsartoris@bard.edu>
2019-12-19 12:45:32 -0500

    bare configuration file

---
+++ support/cas-server-support-multiphase/src/main/java/org/apereo/cas/multiphase/config/MultiphaseUserAuthenticationConfiguration.java
@@ -0,0 +1,50 @@
+package org.apereo.cas.multiphase.config;
+
+import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.services.ServicesManager;
+import org.apereo.cas.web.flow.CasWebflowConfigurer;
+import org.apereo.cas.web.flow.CasWebflowExecutionPlanConfigurer;
+
+/**
+ * This is {@link MultiphaseUserAuthenticationConfiguration}.
+ *
+ * @author Hayden Sartoris
+ * @since 6.2.0
+ */
+
+@Configuration("multiphaseUserAuthenticationConfiguration")
+@EnableConfigurationProperties(CasConfigurationProperties.class)
+public class MultiphaseUserAuthenticationConfiguration {
+    
+    @Autowired
+    private CasConfigurationProperties casProperties;
+
+    @Autowired
+    @Qualifier("loginFlowRegistry")
+    private ObjectProvider<FlowDefinitionRegistry> loginFlowDefinitionRegistry;
+
+    @Autowired
+    @Qualifier("servicesManager")
+    private ObjectProvider<ServicesManager> servicesManager;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Autowired
+    private ObjectProvider<FlowBuilderServices> flowBuilderServices;
+
+    @ConditionalOnMissingBean(name = "multiphaseUserAuthenticationWebflowConfigurer")
+    @Bean
+    @DependsOn("defaultWebflowConfigurer")
+    public CasWebflowConfigurer multiphaseUserAuthenticationWebflowConfigurer() {
+        return new MultiphaseUserAuthenticationConfigurer(flowBuilderServices.getObject(),
+                loginFlowDefinitionRegistry.getObject(), applicationContext, casProperties);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(name = "multiphaseUserAuthenticationCasWebflowExecutionPlanConfigurer")
+    public CasWebflowExecutionPlanConfigurer multiphaseUserAuthenticationCasWebflowExecutionPlanConfigurer() {
+        return plan -> plan.registerWebflowConfigurer(multiphaseUserAuthenticationWebflowConfigurer());
+	}
+
+}
