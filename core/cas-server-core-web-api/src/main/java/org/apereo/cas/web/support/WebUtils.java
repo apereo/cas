27965d51da0bc923ac8f1b8d482fcd0ee0fd0d85
Hayden Sartoris <hsartoris@bard.edu>
2019-12-20 11:02:38 -0500

    some more logging

--- core/cas-server-core-web-api/src/main/java/org/apereo/cas/web/support/WebUtils.java
+++ core/cas-server-core-web-api/src/main/java/org/apereo/cas/web/support/WebUtils.java
@@ -928,6 +928,7 @@ public class WebUtils {
      * @param value          the value
      */
     public static void putMultiphaseAuthenticationEnabled(final RequestContext requestContext, final Boolean value) {
+        LOGGER.debug("putMultiphaseAuthenticationEnabled called with value [{}]", value);
         requestContext.getFlowScope().put("multiphaseEnabled", value);
     }
 
@@ -938,6 +939,7 @@ public class WebUtils {
      * @param value             the value
      */
     public static void putMultiphaseAuthenticationUsername(final RequestContext requestContext, final String username) {
+        LOGGER.debug("putMultiphaseAuthenticationUsername called with value [{}]", username);
         requestContext.getFlowScope().put("multiphaseUsername", username);
     }
 
@@ -948,6 +950,7 @@ public class WebUtils {
      * @param value          the value
      */
     public static boolean hasMultiphaseAuthenticationUsername(final RequestContext requestContext) {
+        LOGGER.debug("hasMultiphaseAuthenticationUsername called");
         return requestContext.getFlowScope().contains("multiphaseUsername");
     }
 
