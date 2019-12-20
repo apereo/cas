4fa1d03445cacb275f0127b2acb6fa2d37a6bd3b
Hayden Sartoris <hsartoris@bard.edu>
2019-12-20 11:04:03 -0500

    even more logging, just for fun

--- support/cas-server-support-multiphase/src/main/java/org/apereo/cas/web/flow/PrepareForMultiphaseAuthenticationAction.java
+++ support/cas-server-support-multiphase/src/main/java/org/apereo/cas/web/flow/PrepareForMultiphaseAuthenticationAction.java
@@ -4,6 +4,8 @@ import org.apereo.cas.services.ServicesManager;
 import org.apereo.cas.web.flow.login.InitializeLoginAction;
 import org.apereo.cas.web.support.WebUtils;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.webflow.action.EventFactorySupport;
 import org.springframework.webflow.execution.Event;
 import org.springframework.webflow.execution.RequestContext;
@@ -14,6 +16,7 @@ import org.springframework.webflow.execution.RequestContext;
  * @author Hayden Sartoris
  * @since 6.2.0
  */
+@Slf4j
 public class PrepareForMultiphaseAuthenticationAction extends InitializeLoginAction {
 	public PrepareForMultiphaseAuthenticationAction(final ServicesManager servicesManager) {
 		super(servicesManager);
@@ -21,6 +24,7 @@ public class PrepareForMultiphaseAuthenticationAction extends InitializeLoginAct
 
 	@Override
 	public Event doExecute(final RequestContext requestContext) throws Exception {
+        LOGGER.debug("doExecute on PrepareForMultiphaseAuthenticationAction called");
 		WebUtils.putMultiphaseAuthenticationEnabled(requestContext, Boolean.TRUE);
 		if (!WebUtils.hasMultiphaseAuthenticationUsername(requestContext)) {
 			return new EventFactorySupport().event(this, 
