7ff4c241a6e78468c041e4fa13e8d5dfe545a6a0
Hayden Sartoris <hsartoris@bard.edu>
2019-12-20 13:46:14 -0500

    added skeleton test suite; need to fill in test files

---
+++ support/cas-server-support-multiphase/src/test/java/org/apereo/cas/AllTestsSuite.java
@@ -0,0 +1,26 @@
+package org.apereo.cas;
+
+import org.apereo.cas.web.flow.PrepareForMultiphaseAuthenticationActionTests;
+import org.apereo.cas.web.flow.MultiphaseAuthenticationWebflowConfigurerTests;
+import org.apereo.cas.web.flow.StoreUserIdForAuthenticationActionTests;
+
+
+// TODO
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.runner.RunWithl
+
+/**
+ * This is {@link AllTestsSuite}
+ *
+ * @author Hayden Sartoris
+ * @since 6.2.0
+ */
+@SelectClasses({
+	PrepareForMultiphaseAuthenticationActionTests.class,
+	MultiphaseAuthenticationWebflowConfigurerTests.class,
+	StoreUserIdForAuthenticationActionTests.class
+})
+@RunWith(JUnitPlatform.class)
+public class AllTestsSuite {}
