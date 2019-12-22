3d76302c71b6836b6512ff819ef6dcc690da9398
Misagh Moayyed <mm1844@gmail.com>
2019-12-22 21:04:40 +0400

    fix failures

--- support/cas-server-support-jms-ticket-registry/src/main/java/org/apereo/cas/ticket/registry/JmsTicketRegistry.java
+++ support/cas-server-support-jms-ticket-registry/src/main/java/org/apereo/cas/ticket/registry/JmsTicketRegistry.java
@@ -8,6 +8,7 @@ import org.apereo.cas.ticket.registry.queue.DeleteTicketsMessageQueueCommand;
 import org.apereo.cas.ticket.registry.queue.UpdateTicketMessageQueueCommand;
 import org.apereo.cas.util.crypto.CipherExecutor;
 
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 
 /**
@@ -16,6 +17,7 @@ import lombok.val;
  * @author Misagh Moayyed
  * @since 5.2.0
  */
+@Slf4j
 public class JmsTicketRegistry extends DefaultTicketRegistry {
 
     private final JmsTicketRegistryPublisher ticketPublisher;
@@ -32,12 +34,14 @@ public class JmsTicketRegistry extends DefaultTicketRegistry {
     @Override
     public void addTicket(final Ticket ticket) {
         super.addTicket(ticket);
+        LOGGER.trace("Publishing add command for id [{}] and ticket [{}]", id, ticket.getId());
         ticketPublisher.publishMessageToQueue(new AddTicketMessageQueueCommand(id, ticket));
     }
 
     @Override
     public boolean deleteSingleTicket(final String ticketId) {
         val result = super.deleteSingleTicket(ticketId);
+        LOGGER.trace("Publishing delete command for id [{}] and ticket [{}]", id, ticketId);
         ticketPublisher.publishMessageToQueue(new DeleteTicketMessageQueueCommand(id, ticketId));
         return result;
     }
@@ -52,6 +56,7 @@ public class JmsTicketRegistry extends DefaultTicketRegistry {
     @Override
     public Ticket updateTicket(final Ticket ticket) {
         val result = super.updateTicket(ticket);
+        LOGGER.trace("Publishing update command for id [{}] and ticket [{}]", id, ticket.getId());
         ticketPublisher.publishMessageToQueue(new UpdateTicketMessageQueueCommand(id, ticket));
         return result;
     }
