6ab8c51c7a058b23d410e0bc690c6ea09f9f7774
Misagh Moayyed <mm1844@gmail.com>
2019-12-22 21:29:51 +0400

    add test for mssql server

---
+++ support/cas-server-support-yubikey-jpa/src/test/java/org/apereo/cas/adaptors/yubikey/dao/MicrosoftSQLServerJpaYubiKeyAccountRegistryTests.java
@@ -0,0 +1,27 @@
+package org.apereo.cas.adaptors.yubikey.dao;
+
+import org.apereo.cas.util.junit.EnabledIfContinuousIntegration;
+import org.apereo.cas.util.junit.EnabledIfPortOpen;
+
+import org.junit.jupiter.api.Tag;
+import org.springframework.test.context.TestPropertySource;
+
+/**
+ * This is {@link MicrosoftSQLServerJpaYubiKeyAccountRegistryTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.2.0
+ */
+@EnabledIfPortOpen(port = 1433)
+@EnabledIfContinuousIntegration
+@Tag("MsSqlServer")
+@TestPropertySource(properties = {
+    "cas.jdbc.showSql=true",
+    "cas.authn.mfa.yubikey.jpa.user=sa",
+    "cas.authn.mfa.yubikey.jpa.password=p@ssw0rd",
+    "cas.authn.mfa.yubikey.jpa.driverClass=com.microsoft.sqlserver.jdbc.SQLServerDriver",
+    "cas.authn.mfa.yubikey.jpa.url=jdbc:sqlserver://localhost:1433;databaseName=yubikey;useUnicode=true;characterEncoding=UTF-8",
+    "cas.authn.mfa.yubikey.jpa.dialect=org.hibernate.dialect.SQLServer2012Dialect"
+})
+public class MicrosoftSQLServerJpaYubiKeyAccountRegistryTests extends JpaYubiKeyAccountRegistryTests {
+}
