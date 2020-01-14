804aab59050b7953de72cd692df7124d2bacb4e3
Hayden Sartoris <hsartoris@bard.edu>
2020-01-14 09:46:39 -0500

    more checkstyle fixes

--- support/cas-server-support-gua/src/main/java/org/apereo/cas/web/flow/PrepareForGraphicalAuthenticationAction.java
+++ support/cas-server-support-gua/src/main/java/org/apereo/cas/web/flow/PrepareForGraphicalAuthenticationAction.java
@@ -3,7 +3,6 @@ package org.apereo.cas.web.flow;
 import org.apereo.cas.services.ServicesManager;
 import org.apereo.cas.web.support.WebUtils;
 
-import org.springframework.webflow.action.EventFactorySupport;
 import org.springframework.webflow.execution.Event;
 import org.springframework.webflow.execution.RequestContext;
 
