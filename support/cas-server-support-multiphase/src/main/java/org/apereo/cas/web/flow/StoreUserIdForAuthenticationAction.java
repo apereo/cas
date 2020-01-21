7fe33da270f0f72ab1d34e9d44af1396497981d1
Hayden Sartoris <hsartoris@bard.edu>
2020-01-21 16:08:15 -0500

    added audit config for Store Id action

--- support/cas-server-support-multiphase/src/main/java/org/apereo/cas/web/flow/StoreUserIdForAuthenticationAction.java
+++ support/cas-server-support-multiphase/src/main/java/org/apereo/cas/web/flow/StoreUserIdForAuthenticationAction.java
@@ -3,11 +3,13 @@ package org.apereo.cas.web.flow;
 import org.apereo.cas.authentication.credential.UsernamePasswordCredential;
 import org.apereo.cas.web.support.WebUtils;
 
-import lombok.RequiredArgsConstructor;
-import lombok.val;
+import org.apereo.inspektr.audit.annotation.Audit;
 import org.springframework.webflow.action.AbstractAction;
 import org.springframework.webflow.execution.Event;
 import org.springframework.webflow.execution.RequestContext;
+
+import lombok.RequiredArgsConstructor;
+import lombok.val;
 
 /**
  * This is {@link StoreUserIdForAuthenticationAction}.
@@ -17,7 +19,10 @@ import org.springframework.webflow.execution.RequestContext;
 @RequiredArgsConstructor
 public class StoreUserIdForAuthenticationAction extends AbstractAction {
     //private final MultiphaseUserEventResolver multiphaseUserEventResolver;
-
+    
+    @Audit(action = "AUTHENTICATION_EVENT",
+        actionResolverName = "AUTHENTICATION_EVENT_ACTION_RESOLVER",
+        resourceResolverName = "AUTHENTICATION_EVENT_RESOURCE_RESOLVER")
     @Override
     public Event doExecute(final RequestContext requestContext) {
         val username = requestContext.getRequestParameters().get("username");
