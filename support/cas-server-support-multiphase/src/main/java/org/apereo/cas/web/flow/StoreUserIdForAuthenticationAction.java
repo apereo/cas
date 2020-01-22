3d2be72c84c188f6d3cc5ded9b14642f6c94ba69
Hayden Sartoris <hsartoris@bard.edu>
2020-01-22 13:20:46 -0500

    checkstyle

--- support/cas-server-support-multiphase/src/main/java/org/apereo/cas/web/flow/StoreUserIdForAuthenticationAction.java
+++ support/cas-server-support-multiphase/src/main/java/org/apereo/cas/web/flow/StoreUserIdForAuthenticationAction.java
@@ -3,13 +3,13 @@ package org.apereo.cas.web.flow;
 import org.apereo.cas.authentication.credential.UsernamePasswordCredential;
 import org.apereo.cas.web.support.WebUtils;
 
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+
 import org.apereo.inspektr.audit.annotation.Audit;
 import org.springframework.webflow.action.AbstractAction;
 import org.springframework.webflow.execution.Event;
 import org.springframework.webflow.execution.RequestContext;
-
-import lombok.RequiredArgsConstructor;
-import lombok.val;
 
 /**
  * This is {@link StoreUserIdForAuthenticationAction}.
