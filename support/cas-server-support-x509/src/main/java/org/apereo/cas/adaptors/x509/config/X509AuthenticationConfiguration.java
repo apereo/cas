7781777905aff6b17f63a395df29c4c7ce895531
Hal Deadman <hdeadman@users.noreply.github.com>
2020-01-28 00:24:55 -0500

    Extract issuer dn from certificate as x509 attribute (#4655)
    * extract issuer dn as x509 attribute
    
    also allow specification of subject Dn format since default format is listed as "denigrated" in javadocs
    
    * Add table to document config values
    
    Co-authored-by: apereocas-bot <45107089+apereocas-bot@users.noreply.github.com>

--- support/cas-server-support-x509/src/main/java/org/apereo/cas/adaptors/x509/config/X509AuthenticationConfiguration.java
+++ support/cas-server-support-x509/src/main/java/org/apereo/cas/adaptors/x509/config/X509AuthenticationConfiguration.java
@@ -227,6 +227,7 @@ public class X509AuthenticationConfiguration {
     @ConditionalOnMissingBean(name = "x509SubjectDNPrincipalResolver")
     public PrincipalResolver x509SubjectDNPrincipalResolver() {
         val x509 = casProperties.getAuthn().getX509();
+        val subjectDn = x509.getSubjectDn();
         val personDirectory = casProperties.getPersonDirectory();
         val principal = x509.getPrincipal();
         val principalAttribute = StringUtils.defaultIfBlank(principal.getPrincipalAttribute(), personDirectory.getPrincipalAttribute());
@@ -237,7 +238,8 @@ public class X509AuthenticationConfiguration {
             principalAttribute,
             principal.isUseExistingPrincipalId() || personDirectory.isUseExistingPrincipalId(),
             principal.isAttributeResolutionEnabled(),
-            org.springframework.util.StringUtils.commaDelimitedListToSet(principal.getActiveAttributeRepositoryIds()));
+            org.springframework.util.StringUtils.commaDelimitedListToSet(principal.getActiveAttributeRepositoryIds()),
+            subjectDn.getSubjectDnFormat());
     }
 
     @Bean
