67207b77d837482ca16ec628c4eddefc83e7581e
Misagh Moayyed <mm1844@gmail.com>
2020-01-30 16:13:06 +0400

    fix checkstyle

--- support/cas-server-support-passwordless-jpa/src/test/java/org/apereo/cas/impl/token/JpaPasswordlessTokenRepositoryTests.java
+++ support/cas-server-support-passwordless-jpa/src/test/java/org/apereo/cas/impl/token/JpaPasswordlessTokenRepositoryTests.java
@@ -1,7 +1,6 @@
 package org.apereo.cas.impl.token;
 
 import org.apereo.cas.api.PasswordlessTokenRepository;
-import org.apereo.cas.api.PasswordlessUserAccountStore;
 import org.apereo.cas.config.JpaPasswordlessAuthenticationConfiguration;
 import org.apereo.cas.impl.BasePasswordlessUserAccountStoreTests;
 
