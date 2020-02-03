994d32f60a25bc929259875d36f3b784d7c6747d
Hayden Sartoris <hsartoris@bard.edu>
2020-02-03 13:57:08 -0500

    no longer extends MultiphaseAuth configurer since this flow now waits for multiphase to complete configuration. working on optional intermediary state

--- support/cas-server-support-gua-webflow/src/main/java/org/apereo/cas/web/flow/GraphicalUserAuthenticationWebflowConfigurer.java
+++ support/cas-server-support-gua-webflow/src/main/java/org/apereo/cas/web/flow/GraphicalUserAuthenticationWebflowConfigurer.java
@@ -1,11 +1,13 @@
 package org.apereo.cas.web.flow;
 
 import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.web.flow.configurer.AbstractCasWebflowConfigurer;
 
 import lombok.val;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.webflow.definition.registry.FlowDefinitionRegistry;
 import org.springframework.webflow.engine.ActionState;
+import org.springframework.webflow.engine.ViewState;
 import org.springframework.webflow.engine.Transition;
 import org.springframework.webflow.engine.builder.support.FlowBuilderServices;
 
@@ -15,7 +17,7 @@ import org.springframework.webflow.engine.builder.support.FlowBuilderServices;
  * @author Misagh Moayyed
  * @since 5.1.0
  */
-public class GraphicalUserAuthenticationWebflowConfigurer extends MultiphaseAuthenticationWebflowConfigurer {
+public class GraphicalUserAuthenticationWebflowConfigurer extends AbstractCasWebflowConfigurer {
 
     static final String STATE_ID_ACCEPT_GUA = "acceptUserGraphicsForAuthentication";
     static final String STATE_ID_GUA_DISPLAY_USER_GFX = "guaDisplayUserGraphics";
@@ -32,7 +34,6 @@ public class GraphicalUserAuthenticationWebflowConfigurer extends MultiphaseAuth
 
     @Override
     protected void doInitialize() {
-        super.doInitialize();
         val flow = getLoginFlow();
         if (flow != null) {
             val state = getState(flow, CasWebflowConstants.STATE_ID_MULTIPHASE_STORE_USERID, ActionState.class);
@@ -42,14 +43,27 @@ public class GraphicalUserAuthenticationWebflowConfigurer extends MultiphaseAuth
             createTransitionForState(state, CasWebflowConstants.TRANSITION_ID_SUCCESS,
                     STATE_ID_GUA_DISPLAY_USER_GFX, true);
 
+            val skipInterstitial = casProperties.getAuth().getGua().isGraphicOnMainLogin();
 
-            val viewStateGfx = createViewState(flow, STATE_ID_GUA_DISPLAY_USER_GFX, "casGuaDisplayUserGraphicsView");
+            var viewStateGfx;
+
+            if (casProperties.getAuth().getGua().isGraphicOnMainLogin()) {
+                viewStateGfx = getState(flow, targetStateId, ViewState.class);
+            } else {
+                viewStateGfx = createViewState(flow, STATE_ID_GUA_DISPLAY_USER_GFX, 
+                        "casGuaDisplayUserGraphicsView");
+                createTransitionForState(viewStateGfx, 
+                        CasWebflowConstants.TRANSITION_ID_SUBMIT, 
+                        CasWebflowConstants.STATE_ID_MULTIPHASE_STORE_USERID);
+            }
+
             viewStateGfx.getRenderActionList().add(createEvaluateAction(ACTION_ID_DISPLAY_USER_GRAPHICS_BEFORE_AUTHENTICATION));
-            createTransitionForState(viewStateGfx, CasWebflowConstants.TRANSITION_ID_SUBMIT, STATE_ID_ACCEPT_GUA);
 
+            /*
             val acceptState = createActionState(flow, STATE_ID_ACCEPT_GUA,
                 createEvaluateAction(ACTION_ID_ACCEPT_USER_GRAPHICS_FOR_AUTHENTICATION));
             createStateDefaultTransition(acceptState, targetStateId);
+            */
         }
     }
 }
