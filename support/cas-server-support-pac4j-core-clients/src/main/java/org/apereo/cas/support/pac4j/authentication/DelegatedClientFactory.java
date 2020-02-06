81a734fbfda65aa606b3225df3b4df24ac48a125
Fabio Martelli <fabio.martelli@gmail.com>
2020-02-06 08:43:16 +0100

    Remove cas.authn.pac4j.saml[0].signLogoutRequests (#4675)
    * remove cas.authn.pac4j.saml[0].signLogoutRequests
    
    * configuration property name into spring metadata
    
    * configuration property name into spring metadata
    
    Co-authored-by: apereocas-bot <45107089+apereocas-bot@users.noreply.github.com>

--- support/cas-server-support-pac4j-core-clients/src/main/java/org/apereo/cas/support/pac4j/authentication/DelegatedClientFactory.java
+++ support/cas-server-support-pac4j-core-clients/src/main/java/org/apereo/cas/support/pac4j/authentication/DelegatedClientFactory.java
@@ -417,7 +417,6 @@ public class DelegatedClientFactory {
                     cfg.setAttributeAsId(saml.getPrincipalIdAttribute());
                 }
                 cfg.setWantsAssertionsSigned(saml.isWantsAssertionsSigned());
-                cfg.setSpLogoutRequestSigned(saml.isSignLogoutRequests());
                 cfg.setUseNameQualifier(saml.isUseNameQualifier());
                 cfg.setAttributeConsumingServiceIndex(saml.getAttributeConsumingServiceIndex());
                 if (saml.getAssertionConsumerServiceIndex() >= 0) {
