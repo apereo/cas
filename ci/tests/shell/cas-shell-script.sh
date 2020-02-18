bba5bcc252d3130421bbf73449771f6b0aefc34d
Misagh Moayyed <mm1844@gmail.com>
2020-02-18 13:16:14 +0400

    validation ddl command via shell

--- ci/tests/shell/cas-shell-script.sh
+++ ci/tests/shell/cas-shell-script.sh
@@ -6,6 +6,7 @@ jasypt-test-algorithms
 generate-idp-metadata --metadataLocation "./" --subjectAltNames "cas.example.com,cas.example.io,cas.example.net" --force
 generate-key --key-size 256
 list-undocumented
+generate-ddl --dialect HSQL
 generate-jwt --subject casuser
 find --name cas.server.name
 generate-anonymous-user --username casuser --service example --salt ythr91%^1
