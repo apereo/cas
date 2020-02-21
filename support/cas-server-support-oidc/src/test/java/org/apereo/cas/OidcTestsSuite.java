e03adee8d4d94853c3799ecc296f454ff5960a13
Julien Huon <julien@huon.email>
2020-02-21 13:25:32 +0100

    Fix OIDC logout

--- support/cas-server-support-oidc/src/test/java/org/apereo/cas/OidcTestsSuite.java
+++ support/cas-server-support-oidc/src/test/java/org/apereo/cas/OidcTestsSuite.java
@@ -30,6 +30,7 @@ import org.apereo.cas.oidc.util.OidcAuthorizationRequestSupportTests;
 import org.apereo.cas.oidc.web.OidcAccessTokenResponseGeneratorTests;
 import org.apereo.cas.oidc.web.controllers.OidcIntrospectionEndpointControllerTests;
 import org.apereo.cas.oidc.web.controllers.OidcWellKnownEndpointControllerTests;
+import org.apereo.cas.oidc.web.controllers.logout.OidcLogoutEndpointControllerTests;
 import org.apereo.cas.oidc.web.flow.OidcAuthenticationContextWebflowEventResolverTests;
 import org.apereo.cas.oidc.web.flow.OidcRegisteredServiceUIActionTests;
 
@@ -66,6 +67,7 @@ import org.junit.runner.RunWith;
     OidcUserProfileViewRendererFlatTests.class,
     OidcAccessTokenResponseGeneratorTests.class,
     OidcIntrospectionEndpointControllerTests.class,
+    OidcLogoutEndpointControllerTests.class,
     OidcRestfulWebFingerUserInfoRepositoryTests.class,
     OidcAddressScopeAttributeReleasePolicyTests.class,
     OidcCustomScopeAttributeReleasePolicyTests.class,
