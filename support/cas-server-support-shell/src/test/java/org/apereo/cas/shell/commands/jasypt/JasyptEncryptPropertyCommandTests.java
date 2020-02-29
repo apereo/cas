ffdd2134588eab7667ab8f1f211ba89401db43eb
Misagh Moayyed <mm1844@gmail.com>
2020-02-29 22:50:05 +0330

    add tests

---
+++ support/cas-server-support-shell/src/test/java/org/apereo/cas/shell/commands/jasypt/JasyptEncryptPropertyCommandTests.java
@@ -0,0 +1,24 @@
+package org.apereo.cas.shell.commands.jasypt;
+
+import org.apereo.cas.shell.commands.BaseCasShellCommandTests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This is {@link JasyptEncryptPropertyCommandTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.2.0
+ */
+@EnableAutoConfiguration
+public class JasyptEncryptPropertyCommandTests extends BaseCasShellCommandTests {
+    @Test
+    public void verifyOperation() {
+        assertDoesNotThrow(() -> shell.evaluate(() -> "encrypt-value --value SOMEVALUE --password "
+            + "JASTYPTPW --alg PBEWITHSHAAND256BITAES-CBC-BC --provider BC"));
+    }
+}
+
