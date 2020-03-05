50e821e69896537cf033c3a6d3ad06979c135eeb
Misagh Moayyed <mm1844@gmail.com>
2020-03-05 18:02:38 +0330

    add tests

---
+++ webapp/cas-server-webapp-init-bootadmin-server/src/test/java/org/apereo/cas/CasSpringBootAdminServerServletInitializerTests.java
@@ -0,0 +1,30 @@
+package org.apereo.cas;
+
+import org.apereo.cas.util.MockServletContext;
+
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This is {@link CasSpringBootAdminServerServletInitializerTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.2.0
+ */
+public class CasSpringBootAdminServerServletInitializerTests {
+
+    @Test
+    public void verifyInitializer() {
+        val servletContext = new MockServletContext();
+        val servletInitializer = new CasSpringBootAdminServletInitializer();
+        assertDoesNotThrow(new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                servletInitializer.onStartup(servletContext);
+            }
+        });
+    }
+}
