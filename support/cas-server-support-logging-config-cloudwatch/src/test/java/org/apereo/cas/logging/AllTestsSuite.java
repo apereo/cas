3d5b155f1ca9d08ed54948fff2a77ec25a3443dd
Jj! <jj@unicon.net>
2020-03-10 16:55:28 -0500

    code style

--- support/cas-server-support-logging-config-cloudwatch/src/test/java/org/apereo/cas/logging/AllTestsSuite.java
+++ support/cas-server-support-logging-config-cloudwatch/src/test/java/org/apereo/cas/logging/AllTestsSuite.java
@@ -6,6 +6,8 @@ import org.junit.runner.RunWith;
 
 /**
  * Test suite for cloudwatch logs
+ *
+ * @since 6.2.0
  */
 @SelectClasses({CloudWatchAppenderSpecTests.class, CloudWatchAppenderTests.class})
 @RunWith(JUnitPlatform.class)
--- support/cas-server-support-logging-config-cloudwatch/src/test/java/org/apereo/cas/logging/CloudWatchAppenderSpecTests.java
+++ support/cas-server-support-logging-config-cloudwatch/src/test/java/org/apereo/cas/logging/CloudWatchAppenderSpecTests.java
@@ -10,8 +10,6 @@ import com.amazonaws.services.logs.model.DescribeLogStreamsResult;
 import com.amazonaws.services.logs.model.LogGroup;
 import com.amazonaws.services.logs.model.LogStream;
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.config.Configuration;
 import org.apache.logging.log4j.core.config.Configurator;
 import org.apache.logging.log4j.core.config.builder.api.ConfigurationBuilder;
 import org.apache.logging.log4j.core.config.builder.api.ConfigurationBuilderFactory;
@@ -28,13 +26,16 @@ import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+/**
+ * @since 6.2.0
+ */
 public class CloudWatchAppenderSpecTests {
     @Test
     @DisplayName("make sure that log4j plugin file is generated")
     public void fileGenerated() {
         ConfigurationBuilder<BuiltConfiguration> builder = ConfigurationBuilderFactory.newConfigurationBuilder();
         builder.add(builder.newAppender("cloudwatch", "CloudWatchAppender"));
-        Configuration configuration = builder.build();
+        var configuration = builder.build();
         Configurator.initialize(configuration);
         assertNotNull(configuration.getAppender("cloudwatch"));
     }
@@ -43,27 +44,30 @@ public class CloudWatchAppenderSpecTests {
     @MethodSource("generateTestCases")
     @DisplayName("making sure incoming parameters are set correctly")
     void specTest(final TestCase tC) {
-        AWSLogs mock = Mockito.mock(AWSLogs.class);
-        if (tC.logGroupExists) {
+        var mock = Mockito.mock(AWSLogs.class);
+        if (tC.logStreamExists) {
             Mockito.when(mock.describeLogStreams(Mockito.any(DescribeLogStreamsRequest.class))).thenReturn(createDescribeLogStreamsResult());
         }
         if (tC.logGroupExists) {
             Mockito.when(mock.describeLogGroups(Mockito.any(DescribeLogGroupsRequest.class))).thenReturn(createDescribeLogGroupsResult());
         }
 
-        // we do this because the lifecycle is a little different for this sort of programmatic configuration
-        CloudWatchAppender appender = new CloudWatchAppender("test", "test", "test", "30", null, tC.createIfNeeded, tC.createLogGroupIfNeeded, tC.createLogStreamIfNeeded, mock);
+        /*
+          we do this because the lifecycle is a little different for this sort of programmatic configuration
+          not allowing single line comments is lame
+         */
+        var appender = new CloudWatchAppender("test", "test", "test", "30", null, tC.createIfNeeded, tC.createLogGroupIfNeeded, tC.createLogStreamIfNeeded, mock);
         if (tC.throwsException) {
             Assertions.assertThrows(RuntimeException.class, appender::initialize);
         } else {
             appender.initialize();
 
             ConfigurationBuilder<BuiltConfiguration> builder = ConfigurationBuilderFactory.newConfigurationBuilder();
-            BuiltConfiguration configuration = builder.build();
+            var configuration = builder.build();
             configuration.addAppender(appender);
             Configurator.initialize(configuration);
 
-            Logger logger = LogManager.getLogger("test");
+            var logger = LogManager.getLogger("test");
             logger.info("here is a message");
 
             createLogGroup(mock, Objects.requireNonNullElse(tC.resultCreateLogGroupIfNeeded, Objects.requireNonNullElse(tC.createIfNeeded, true)));
@@ -73,8 +77,8 @@ public class CloudWatchAppenderSpecTests {
     }
 
     private static DescribeLogStreamsResult createDescribeLogStreamsResult() {
-        DescribeLogStreamsResult stream = new DescribeLogStreamsResult();
-        LogStream logStream = new LogStream();
+        var stream = new DescribeLogStreamsResult();
+        var logStream = new LogStream();
         logStream.setLogStreamName("test");
         logStream.setUploadSequenceToken("test");
         stream.getLogStreams().add(logStream);
@@ -83,8 +87,8 @@ public class CloudWatchAppenderSpecTests {
     }
 
     private static DescribeLogGroupsResult createDescribeLogGroupsResult() {
-        DescribeLogGroupsResult group = new DescribeLogGroupsResult();
-        LogGroup logGroup = new LogGroup();
+        var group = new DescribeLogGroupsResult();
+        var logGroup = new LogGroup();
         logGroup.setLogGroupName("test");
 
         return group;
@@ -107,62 +111,97 @@ public class CloudWatchAppenderSpecTests {
     }
 
     public static ArrayList<TestCase> generateTestCases() {
-        ArrayList<TestCase> Cases = new ArrayList<>();
+        ArrayList<TestCase> testCases = new ArrayList<>();
 
-        Cases.add(new TestCase(null, null, null, true, true));
-        Cases.add(new TestCase(null, null, false, false, false, true));
-        Cases.add(new TestCase(null, null, true, false, true));
-        Cases.add(new TestCase(null, false, null, false, false, true));
-        Cases.add(new TestCase(null, false, false, false, false, true)); // 5
-        Cases.add(new TestCase(null, false, true, false, true));
-        Cases.add(new TestCase(null, true, null, true, false, true));
-        Cases.add(new TestCase(null, true, false, true, false, true));
-        Cases.add(new TestCase(null, true, true, true, true));
-        Cases.add(new TestCase(false, null, null, false, false, true)); // 10
-        Cases.add(new TestCase(false, null, false, false, false, true));
-        Cases.add(new TestCase(false, null, true, false, true));
-        Cases.add(new TestCase(false, false, null, false, false, true));
-        Cases.add(new TestCase(false, false, false, false, false, true));
-        Cases.add(new TestCase(false, false, true, false, true)); // 15
-        Cases.add(new TestCase(false, true, null, true, false, true));
-        Cases.add(new TestCase(false, true, false, true, false, true));
-        Cases.add(new TestCase(false, true, true, true, true));
-        Cases.add(new TestCase(true, null, null, true, true));
-        Cases.add(new TestCase(true, null, false, false,false, true)); // 20
-        Cases.add(new TestCase(true, null, true, false,true));
-        Cases.add(new TestCase(true, false, null, false,false, true));
-        Cases.add(new TestCase(true, false, false, false,false, true));
-        Cases.add(new TestCase(true, false, true, false,true));
-        Cases.add(new TestCase(true, true, null, true,false, true)); // 25
-        Cases.add(new TestCase(true, true, false, true,false, true));
-        Cases.add(new TestCase(true, true, true, true,true));
-        Cases.add(new TestCase(true, null, null, true, true, false, false, false));
-        Cases.add(new TestCase(null, true, null, true, false, true, false, false));
-        Cases.add(new TestCase(null, false, true, false, true, false, false, false)); // 30
-        Cases.add(new TestCase(null, true, true, true, true, false, false, false));
+        testCases.add(new TestCase(null, null, null, true, true));
+        testCases.add(new TestCase(null, null, false, false, false, true));
+        testCases.add(new TestCase(null, null, true, false, true));
+        testCases.add(new TestCase(null, false, null, false, false, true));
+        testCases.add(new TestCase(null, false, false, false, false, true)); /* 5 */
+        testCases.add(new TestCase(null, false, true, false, true));
+        testCases.add(new TestCase(null, true, null, true, false, true));
+        testCases.add(new TestCase(null, true, false, true, false, true));
+        testCases.add(new TestCase(null, true, true, true, true));
+        testCases.add(new TestCase(false, null, null, false, false, true)); /* 10 */
+        testCases.add(new TestCase(false, null, false, false, false, true));
+        testCases.add(new TestCase(false, null, true, false, true));
+        testCases.add(new TestCase(false, false, null, false, false, true));
+        testCases.add(new TestCase(false, false, false, false, false, true));
+        testCases.add(new TestCase(false, false, true, false, true)); /* 15 */
+        testCases.add(new TestCase(false, true, null, true, false, true));
+        testCases.add(new TestCase(false, true, false, true, false, true));
+        testCases.add(new TestCase(false, true, true, true, true));
+        testCases.add(new TestCase(true, null, null, true, true));
+        testCases.add(new TestCase(true, null, false, false, false, true)); /* 20 */
+        testCases.add(new TestCase(true, null, true, false, true));
+        testCases.add(new TestCase(true, false, null, false, false, true));
+        testCases.add(new TestCase(true, false, false, false, false, true));
+        testCases.add(new TestCase(true, false, true, false, true));
+        testCases.add(new TestCase(true, true, null, true, false, true)); /* 25 */
+        testCases.add(new TestCase(true, true, false, true, false, true));
+        testCases.add(new TestCase(true, true, true, true, true));
+        testCases.add(new TestCase(true, null, null, true, true, false, false, false));
+        testCases.add(new TestCase(null, true, null, true, false, true, false, false));
+        testCases.add(new TestCase(null, false, true, false, true, false, false, false)); /* 30 */
+        testCases.add(new TestCase(null, true, true, true, true, false, false, false));
 
-        return Cases;
+        return testCases;
     }
 
-    public static class TestCase {
-        public final Boolean createIfNeeded;
-        public final Boolean createLogGroupIfNeeded;
-        public final Boolean createLogStreamIfNeeded;
-        public final Boolean resultCreateLogGroupIfNeeded;
-        public final Boolean resultCreateLogStreamIfNeeded;
-        public final Boolean logGroupExists;
-        public final Boolean logStreamExists;
-        public final Boolean throwsException;
+    private static class TestCase {
+        private final Boolean createIfNeeded;
+        private final Boolean createLogGroupIfNeeded;
+        private final Boolean createLogStreamIfNeeded;
+        private final Boolean resultCreateLogGroupIfNeeded;
+        private final Boolean resultCreateLogStreamIfNeeded;
+        private final Boolean logGroupExists;
+        private final Boolean logStreamExists;
+        private final Boolean throwsException;
 
-        public TestCase(final Boolean createIfNeeded, final Boolean createLogGroupIfNeeded, final Boolean createLogStreamIfNeeded, final Boolean resultCreateLogGroupIfNeeded, final Boolean resultCreateLogStreamIfNeeded) {
-            this(createIfNeeded, createLogGroupIfNeeded, createLogStreamIfNeeded, resultCreateLogGroupIfNeeded, resultCreateLogStreamIfNeeded, null, null, null);
+        TestCase(
+                final Boolean createIfNeeded,
+                final Boolean createLogGroupIfNeeded,
+                final Boolean createLogStreamIfNeeded,
+                final Boolean resultCreateLogGroupIfNeeded,
+                final Boolean resultCreateLogStreamIfNeeded) {
+            this(
+                    createIfNeeded,
+                    createLogGroupIfNeeded,
+                    createLogStreamIfNeeded,
+                    resultCreateLogGroupIfNeeded,
+                    resultCreateLogStreamIfNeeded,
+                    null,
+                    null,
+                    null);
         }
 
-        public TestCase(final Boolean createIfNeeded, final Boolean createLogGroupIfNeeded, final Boolean createLogStreamIfNeeded, final Boolean resultCreateLogGroupIfNeeded, final Boolean resultCreateLogStreamIfNeeded, final Boolean throwsException) {
-            this(createIfNeeded, createLogGroupIfNeeded, createLogStreamIfNeeded, resultCreateLogGroupIfNeeded, resultCreateLogStreamIfNeeded, throwsException, null, null);
+        TestCase(
+                final Boolean createIfNeeded,
+                final Boolean createLogGroupIfNeeded,
+                final Boolean createLogStreamIfNeeded,
+                final Boolean resultCreateLogGroupIfNeeded,
+                final Boolean resultCreateLogStreamIfNeeded,
+                final Boolean throwsException) {
+            this(
+                    createIfNeeded,
+                    createLogGroupIfNeeded,
+                    createLogStreamIfNeeded,
+                    resultCreateLogGroupIfNeeded,
+                    resultCreateLogStreamIfNeeded,
+                    throwsException,
+                    null,
+                    null);
         }
 
-        public TestCase(final Boolean createIfNeeded, final Boolean createLogGroupIfNeeded, final Boolean createLogStreamIfNeeded, final Boolean resultCreateLogGroupIfNeeded, final Boolean resultCreateLogStreamIfNeeded, final Boolean throwsException, final Boolean logGroupExists, final Boolean logStreamExists) {
+        TestCase(
+                final Boolean createIfNeeded,
+                final Boolean createLogGroupIfNeeded,
+                final Boolean createLogStreamIfNeeded,
+                final Boolean resultCreateLogGroupIfNeeded,
+                final Boolean resultCreateLogStreamIfNeeded,
+                final Boolean throwsException,
+                final Boolean logGroupExists,
+                final Boolean logStreamExists) {
             this.createIfNeeded = createIfNeeded;
             this.createLogGroupIfNeeded = createLogGroupIfNeeded;
             this.createLogStreamIfNeeded = createLogStreamIfNeeded;
