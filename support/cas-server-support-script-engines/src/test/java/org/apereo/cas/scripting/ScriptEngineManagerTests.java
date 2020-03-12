872f71e91dca878310c464946053c43cd3ef0039
Misagh Moayyed <mm1844@gmail.com>
2020-03-12 18:31:14 +0330

    wip: fix tests

--- support/cas-server-support-script-engines/src/test/java/org/apereo/cas/scripting/ScriptEngineManagerTests.java
+++ support/cas-server-support-script-engines/src/test/java/org/apereo/cas/scripting/ScriptEngineManagerTests.java
@@ -5,6 +5,7 @@ import org.apereo.cas.services.ScriptedRegisteredServiceAttributeReleasePolicy;
 import org.apereo.cas.util.scripting.ScriptingUtils;
 
 import lombok.val;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,11 +24,14 @@ import static org.junit.jupiter.api.Assertions.*;
  *
  * @author Misagh Moayyed
  * @since 6.1.0
+ * @deprecated 6.2
  */
 @SpringBootTest(classes = {
     RefreshAutoConfiguration.class,
     AopAutoConfiguration.class
 })
+@Tag("Groovy")
+@Deprecated
 public class ScriptEngineManagerTests {
     private static void runAttributeFilterInternallyFor(final String s) {
         val filter = new ScriptedRegisteredServiceAttributeReleasePolicy(s);
