2af0745e64460b8b8c828dee0e98c2633f90f465
Misagh Moayyed <mm1844@gmail.com>
2020-03-12 14:23:37 +0330

    fix issues with consentable attributes plus material design ui changes

--- support/cas-server-support-saml-idp/src/main/java/org/apereo/cas/config/SamlIdPWebflowConfiguration.java
+++ support/cas-server-support-saml-idp/src/main/java/org/apereo/cas/config/SamlIdPWebflowConfiguration.java
@@ -1,19 +1,26 @@
 package org.apereo.cas.config;
 
 import org.apereo.cas.authentication.AuthenticationServiceSelectionPlan;
+import org.apereo.cas.authentication.attribute.AttributeDefinitionStore;
 import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.consent.ConsentableAttributeBuilder;
 import org.apereo.cas.services.ServicesManager;
 import org.apereo.cas.support.saml.services.idp.metadata.cache.SamlRegisteredServiceCachingMetadataResolver;
 import org.apereo.cas.support.saml.web.flow.SamlIdPMetadataUIAction;
 import org.apereo.cas.support.saml.web.flow.SamlIdPMetadataUIWebflowConfigurer;
+import org.apereo.cas.support.saml.web.idp.profile.builders.attr.SamlIdPAttributeDefinition;
 import org.apereo.cas.web.flow.CasWebflowConfigurer;
 import org.apereo.cas.web.flow.CasWebflowExecutionPlanConfigurer;
 
+import lombok.val;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -57,6 +64,10 @@ public class SamlIdPWebflowConfiguration {
     @Qualifier("defaultSamlRegisteredServiceCachingMetadataResolver")
     private ObjectProvider<SamlRegisteredServiceCachingMetadataResolver> defaultSamlRegisteredServiceCachingMetadataResolver;
 
+    @Autowired
+    @Qualifier("attributeDefinitionStore")
+    private ObjectProvider<AttributeDefinitionStore> attributeDefinitionStore;
+
     @ConditionalOnMissingBean(name = "samlIdPMetadataUIWebConfigurer")
     @Bean
     @DependsOn("defaultWebflowConfigurer")
@@ -80,5 +91,33 @@ public class SamlIdPWebflowConfiguration {
     @ConditionalOnMissingBean(name = "samlIdPCasWebflowExecutionPlanConfigurer")
     public CasWebflowExecutionPlanConfigurer samlIdPCasWebflowExecutionPlanConfigurer() {
         return plan -> plan.registerWebflowConfigurer(samlIdPMetadataUIWebConfigurer());
+    }
+
+    @Configuration(value = "SamlIdPConsentWebflowConfiguration", proxyBeanMethods = false)
+    @ConditionalOnClass(value = ConsentableAttributeBuilder.class)
+    public class SamlIdPConsentWebflowConfiguration {
+
+        @Bean
+        @ConditionalOnMissingBean(name = "samlIdPConsentableAttributeBuilder")
+        @RefreshScope
+        public ConsentableAttributeBuilder samlIdPConsentableAttributeBuilder() {
+            return attribute -> {
+                val result = attributeDefinitionStore.getObject()
+                    .locateAttributeDefinition(defn -> {
+                        if (defn instanceof SamlIdPAttributeDefinition) {
+                            val samlAttr = SamlIdPAttributeDefinition.class.cast(defn);
+                            return samlAttr.getName().equalsIgnoreCase(attribute.getName())
+                                && StringUtils.isNotBlank(samlAttr.getFriendlyName());
+                        }
+                        return false;
+                    });
+
+                if (result.isPresent()) {
+                    val samlAttr = SamlIdPAttributeDefinition.class.cast(result.get());
+                    attribute.setFriendlyName(samlAttr.getFriendlyName());
+                }
+                return attribute;
+            };
+        }
     }
 }
