14c794693e2c4ecb5b1c14b1cb9f7471fb4c9620
Misagh Moayyed <mm1844@gmail.com>
2020-03-17 14:38:29 +0330

    added tests

---
+++ support/cas-server-support-throttle-bucket4j/src/test/java/org/apereo/cas/web/Bucket4jThrottledRequestExecutorTests.java
@@ -0,0 +1,40 @@
+package org.apereo.cas.web;
+
+import org.apereo.cas.config.CasBucket4jThrottlingConfiguration;
+import org.apereo.cas.throttle.ThrottledRequestExecutor;
+
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This is {@link Bucket4jThrottledRequestExecutorTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.2.0
+ */
+@SpringBootTest(classes = {
+    RefreshAutoConfiguration.class,
+    CasBucket4jThrottlingConfiguration.class
+})
+public class Bucket4jThrottledRequestExecutorTests {
+    @Autowired
+    @Qualifier("throttledRequestExecutor")
+    private ThrottledRequestExecutor throttledRequestExecutor;
+
+    @Test
+    public void verifyOperation() {
+        assertNotNull(throttledRequestExecutor);
+        val request = new MockHttpServletRequest();
+        val response = new MockHttpServletResponse();
+        assertFalse(this.throttledRequestExecutor.throttle(request, response));
+        assertTrue(response.containsHeader("X-Rate-Limit-Remaining"));
+    }
+}
