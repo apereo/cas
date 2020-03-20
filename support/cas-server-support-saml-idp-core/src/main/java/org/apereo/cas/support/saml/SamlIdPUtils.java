fed42fd4c216fec6fb9631e41120d0ffd7292644
Dmitriy Kopylenko <dima767@gmail.com>
2020-03-20 16:18:52 -0400

    checkstyle

--- support/cas-server-support-saml-idp-core/src/main/java/org/apereo/cas/support/saml/SamlIdPUtils.java
+++ support/cas-server-support-saml-idp-core/src/main/java/org/apereo/cas/support/saml/SamlIdPUtils.java
@@ -102,8 +102,8 @@ public class SamlIdPUtils {
                 if (authnRequest.isSigned()) {
                     endpoint = acsEndpointFromReq;
                 } else {
-                    if ((acsEndpointFromMetadata == null) ||
-                            !acsEndpointFromReq.getLocation().equals(adaptor.getAssertionConsumerService(binding).getLocation())) {
+                    if ((acsEndpointFromMetadata == null)
+                            || !acsEndpointFromReq.getLocation().equals(adaptor.getAssertionConsumerService(binding).getLocation())) {
                         throw new SamlException(String.format("Assertion consumer service from unsigned request [%s], does not match ACS from SP metadata [%s]",
                                 acsEndpointFromReq.getLocation(), adaptor.getAssertionConsumerService(binding).getLocation()));
                     }
