68298f4042f785260df9ba43ffc2082619d0beb6
Julien Huon <julien@huon.email>
2020-04-02 00:07:12 +0200

    Refresh token support for OAuth/OIDC public clients.
    * Add OAuth20RefreshToken Authenticator for OAuth/OIDC public clients 
    only.
    
    * Removed public clients support in the OAuth20ClientIDClientSecret 
    Authenticator.
    
    * Add tests for the new OAuth20RefreshToken Authenticator.
    
    * Add test for the existing OAuth20ClientIdClientSecret Authenticator.

--- support/cas-server-support-oauth/src/test/java/org/apereo/cas/support/oauth/authenticator/OAuth20ClientIdClientSecretAuthenticatorTests.java
+++ support/cas-server-support-oauth/src/test/java/org/apereo/cas/support/oauth/authenticator/OAuth20ClientIdClientSecretAuthenticatorTests.java
@@ -56,4 +56,21 @@ public class OAuth20ClientIdClientSecretAuthenticatorTests extends BaseOAuth20Au
         authenticator.validate(credentials, ctx);
         assertNull(credentials.getUserProfile());
     }
+
+    @Test
+    public void verifyAuthenticationWithGrantTypeRefreshTokenAndClientWithoutPassword() {
+        val refreshToken = getRefreshToken(serviceWithoutSecret);
+        ticketRegistry.addTicket(refreshToken);
+        
+        val credentials = new UsernamePasswordCredentials("serviceWithoutSecret", refreshToken.getId());
+        val request = new MockHttpServletRequest();
+        val ctx = new JEEContext(request, new MockHttpServletResponse());
+
+        request.addParameter(OAuth20Constants.GRANT_TYPE, OAuth20GrantTypes.REFRESH_TOKEN.name());
+        request.addParameter(OAuth20Constants.CLIENT_ID, "serviceWithoutSecret");
+        request.addParameter(OAuth20Constants.REFRESH_TOKEN, refreshToken.getId());
+
+        authenticator.validate(credentials, ctx);
+        assertNull(credentials.getUserProfile());
+    }
 }
