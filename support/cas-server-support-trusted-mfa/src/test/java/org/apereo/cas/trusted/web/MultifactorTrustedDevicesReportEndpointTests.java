bb9679a62b0683e9a25d027e94b65b94cf555465
Misagh Moayyed <mm1844@gmail.com>
2020-04-02 15:40:35 +0430

    add tests

--- support/cas-server-support-trusted-mfa/src/test/java/org/apereo/cas/trusted/web/MultifactorTrustedDevicesReportEndpointTests.java
+++ support/cas-server-support-trusted-mfa/src/test/java/org/apereo/cas/trusted/web/MultifactorTrustedDevicesReportEndpointTests.java
@@ -1,8 +1,11 @@
 package org.apereo.cas.trusted.web;
 
+import org.apereo.cas.trusted.authentication.api.MultifactorAuthenticationTrustRecord;
+import org.apereo.cas.trusted.authentication.api.MultifactorAuthenticationTrustStorage;
 import org.apereo.cas.trusted.config.MultifactorAuthnTrustConfiguration;
 import org.apereo.cas.web.report.AbstractCasEndpointTests;
 
+import lombok.val;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,9 +21,7 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author Misagh Moayyed
  * @since 6.2.0
  */
-@TestPropertySource(properties = {
-    "management.endpoint.multifactorTrustedDevices.enabled=true"
-})
+@TestPropertySource(properties = "management.endpoint.multifactorTrustedDevices.enabled=true")
 @Tag("MFA")
 @ImportAutoConfiguration(MultifactorAuthnTrustConfiguration.class)
 public class MultifactorTrustedDevicesReportEndpointTests extends AbstractCasEndpointTests {
@@ -29,8 +30,20 @@ public class MultifactorTrustedDevicesReportEndpointTests extends AbstractCasEnd
     @Qualifier("mfaTrustedDevicesReportEndpoint")
     private MultifactorTrustedDevicesReportEndpoint endpoint;
 
+    @Autowired
+    @Qualifier("mfaTrustEngine")
+    private MultifactorAuthenticationTrustStorage mfaTrustEngine;
+
     @Test
     public void verifyOperation() {
         assertNotNull(endpoint);
+        val record = mfaTrustEngine.set(MultifactorAuthenticationTrustRecord.newInstance("casuser",
+            "geography", "fingerprint"));
+        assertFalse(endpoint.devices().isEmpty());
+        assertFalse(endpoint.devicesForUser("casuser").isEmpty());
+
+        endpoint.revoke(record.getRecordKey());
+        assertTrue(endpoint.devices().isEmpty());
+        assertTrue(endpoint.devicesForUser("casuser").isEmpty());
     }
 }
