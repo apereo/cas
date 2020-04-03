6670c291b3732dc47d1ce2f54d0e9298c9f77019
Misagh Moayyed <mm1844@gmail.com>
2020-04-03 14:27:54 +0430

    add memcached tests

--- support/cas-server-support-memcached-monitor/src/test/java/org/apereo/cas/monitor/MemcachedHealthIndicatorTests.java
+++ support/cas-server-support-memcached-monitor/src/test/java/org/apereo/cas/monitor/MemcachedHealthIndicatorTests.java
@@ -2,8 +2,11 @@ package org.apereo.cas.monitor;
 
 import org.apereo.cas.config.CasCoreUtilSerializationConfiguration;
 import org.apereo.cas.monitor.config.MemcachedMonitorConfiguration;
+import org.apereo.cas.util.junit.EnabledIfPortOpen;
 
 import lombok.val;
+import net.spy.memcached.MemcachedClientIF;
+import org.apache.commons.pool2.ObjectPool;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +17,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
 import org.springframework.test.annotation.DirtiesContext;
 
+import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 /**
  * This is {@link MemcachedHealthIndicatorTests}.
@@ -34,6 +43,7 @@ import static org.junit.jupiter.api.Assertions.*;
 })
 @DirtiesContext
 @Tag("Memcached")
+@EnabledIfPortOpen(port = 11211)
 public class MemcachedHealthIndicatorTests {
     @Autowired
     @Qualifier("memcachedHealthIndicator")
@@ -43,5 +53,49 @@ public class MemcachedHealthIndicatorTests {
     public void verifyMonitorNotRunning() {
         val health = monitor.health();
         assertEquals(Status.OUT_OF_SERVICE, health.getStatus());
+    }
+
+    @Test
+    public void verifyUnavailableServers() throws Exception {
+        val memcached = mock(MemcachedClientIF.class);
+        when(memcached.getUnavailableServers()).thenReturn(List.of(new InetSocketAddress(1234)));
+        when(memcached.getAvailableServers()).thenReturn(List.of(new InetSocketAddress(11212)));
+        val client = mock(ObjectPool.class);
+        when(client.borrowObject()).thenReturn(memcached);
+        val indicator = new MemcachedHealthIndicator(client, 1, 1);
+        val health = indicator.health();
+        assertEquals(Status.DOWN, health.getStatus());
+    }
+
+    @Test
+    public void verifyMonitorError() throws Exception {
+        val memcached = mock(MemcachedClientIF.class);
+        when(memcached.getUnavailableServers()).thenThrow(new RuntimeException("error"));
+        when(memcached.getAvailableServers()).thenThrow(new RuntimeException("error"));
+        val client = mock(ObjectPool.class);
+        when(client.borrowObject()).thenReturn(memcached);
+        val indicator = new MemcachedHealthIndicator(client, 1, 1);
+        val health = indicator.health();
+        assertEquals(Status.DOWN, health.getStatus());
+    }
+
+    @Test
+    public void verifyMonitorSuccess() throws Exception {
+        val memcached = mock(MemcachedClientIF.class);
+        when(memcached.getUnavailableServers()).thenReturn(List.of());
+        val socket = new InetSocketAddress("localhost", 11212);
+        when(memcached.getAvailableServers()).thenReturn(List.of(socket));
+
+        val details = new HashMap<String, String>();
+        details.put("bytes", "1000");
+        details.put("limit_maxbytes", "500");
+        details.put("evictions", "10");
+
+        when(memcached.getStats()).thenReturn(Map.of(socket, details));
+        val client = mock(ObjectPool.class);
+        when(client.borrowObject()).thenReturn(memcached);
+        val indicator = new MemcachedHealthIndicator(client, 1, 1);
+        val health = indicator.health();
+        assertEquals(new Status("WARN"), health.getStatus());
     }
 }
