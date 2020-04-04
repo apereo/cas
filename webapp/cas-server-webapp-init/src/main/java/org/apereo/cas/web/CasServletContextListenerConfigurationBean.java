595eecb7b8635283a11a598dada2ec7b48614239
leeyc0 <leeyc0@users.noreply.github.com>
2020-04-05 02:07:48 +0800

    code style fixes
    Note: have to suppress FinalParameters and FinalLocalVariable
    checks because they are the methods from ServletContextListener

--- webapp/cas-server-webapp-init/src/main/java/org/apereo/cas/web/CasServletContextListenerConfigurationBean.java
+++ webapp/cas-server-webapp-init/src/main/java/org/apereo/cas/web/CasServletContextListenerConfigurationBean.java
@@ -1,19 +1,24 @@
 package org.apereo.cas.web;
 
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import lombok.extern.slf4j.Slf4j;
-import lombok.val;
-
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
-
-@Configuration
+/**
+ * This is {@link CasServletContextListenerConfigurationBean} that properly
+ * deregisters JDBC drivers.
+ *
+ * @author leeyc0
+ * @since 6.2.0
+ */
+@Configuration("casServletContextListenerConfigurationBean")
 @Slf4j
 public class CasServletContextListenerConfigurationBean {
     @Bean
@@ -31,26 +36,24 @@ public class CasServletContextListenerConfigurationBean {
 
         @Override
         public final void contextDestroyed(ServletContextEvent sce) {
-            // ... First close any background tasks which may be using the DB ...
-            // ... Then close any DB connection pools ...
+            /* ... First close any background tasks which may be using the DB ...
+               ... Then close any DB connection pools ...
 
-            // Now deregister JDBC drivers in this context's ClassLoader:
-            // Get the webapp's ClassLoader
+               Now deregister JDBC drivers in this context's ClassLoader:
+               Get the webapp's ClassLoader */
             val cl = Thread.currentThread().getContextClassLoader();
-            // Loop through all drivers
             val drivers = DriverManager.getDrivers();
             while (drivers.hasMoreElements()) {
                 val driver = drivers.nextElement();
                 if (driver.getClass().getClassLoader() == cl) {
-                    // This driver was registered by the webapp's ClassLoader, so deregister it:
+                    /* This driver was registered by the webapp's ClassLoader, so deregister it: */
                     try {
                         LOGGER.debug("Deregistering JDBC driver {}", driver);
                         DriverManager.deregisterDriver(driver);
-                    } catch (SQLException ex) {
+                    } catch (final SQLException ex) {
                         LOGGER.debug("Error deregistering JDBC driver {}", ex);
                     }
                 } else {
-                    // driver was not registered by the webapp's ClassLoader and may be in use elsewhere
                     LOGGER.debug("Not deregistering JDBC driver {} as it does not belong to this webapp's ClassLoader", driver);
                 }
             }
