49c171235f00db2ddbe7798ba386fb4e60f73d5c
Misagh Moayyed <misagh.moayyed@gmail.com>
2020-04-08 14:15:10 +0430

    Update CasLabels.java

--- src/main/java/org/apereo/cas/CasLabels.java
+++ src/main/java/org/apereo/cas/CasLabels.java
@@ -88,7 +88,7 @@ public enum CasLabels {
     LABEL_SEE_CONTRIBUTOR_GUIDELINES("See Contributor Guidelines"),
     LABEL_PROXY_AUTHENTICATION("Proxy Authentication", "pgt|proxy callback"),
     LABEL_RADIUS("RADIUS", "jradius"),
-    LABEL_REST_PROTOCOL("REST Protocol & Integration", "rest"),
+    LABEL_REST_PROTOCOL("REST Protocol & Integrations", "rest"),
     LABEL_REDIS("Redis", "redis|sentinel"),
     LABEL_REFACTORING_CODE_CLEANUP("Refactoring & Code CleanUp", "refactor:*\\s*|NPE|cleanup|CVE|spotbugs|typos|checkstyle|codacy|duplicate code"),
     LABEL_REMEMBER_ME("Remember-Me", "long-term|rememberme"),
