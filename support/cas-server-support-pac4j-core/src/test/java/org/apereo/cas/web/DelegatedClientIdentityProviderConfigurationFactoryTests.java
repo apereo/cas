212d5ae6dc6a6c629a80545390fd74ab1f5282df
Misagh Moayyed <mm1844@gmail.com>
2020-04-08 18:59:04 +0430

    fix tests

--- support/cas-server-support-pac4j-core/src/test/java/org/apereo/cas/web/DelegatedClientIdentityProviderConfigurationFactoryTests.java
+++ support/cas-server-support-pac4j-core/src/test/java/org/apereo/cas/web/DelegatedClientIdentityProviderConfigurationFactoryTests.java
@@ -1,22 +1,26 @@
 package org.apereo.cas.web;
 
 import org.apereo.cas.CasProtocolConstants;
-import org.apereo.cas.authentication.principal.SimpleWebApplicationServiceImpl;
+import org.apereo.cas.authentication.principal.ClientCustomPropertyConstants;
 import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.services.RegisteredServiceTestUtils;
+import org.apereo.cas.util.EncodingUtils;
 
 import lombok.val;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.pac4j.cas.client.CasClient;
 import org.pac4j.cas.config.CasConfiguration;
-import org.pac4j.core.context.WebContext;
+import org.pac4j.core.context.JEEContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
 
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.util.Optional;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 /**
  * This is {@link DelegatedClientIdentityProviderConfigurationFactoryTests}.
@@ -24,73 +28,70 @@ import static org.mockito.Mockito.when;
  * @author Adrian Gonzalez
  * @since 6.2.0
  */
+@SpringBootTest(classes = RefreshAutoConfiguration.class)
+@EnableConfigurationProperties(CasConfigurationProperties.class)
 public class DelegatedClientIdentityProviderConfigurationFactoryTests {
 
-    private CasConfigurationProperties config;
-    private WebContext webContextMock;
-    private CasClient client;
-    private SimpleWebApplicationServiceImpl service;
-
-    @BeforeEach
-    public void setUp() {
-        config = new CasConfigurationProperties();
-        webContextMock = mock(WebContext.class);
-        service = new SimpleWebApplicationServiceImpl();
-        client = new CasClient(new CasConfiguration());
-    }
+    @Autowired
+    private CasConfigurationProperties casProperties;
 
     @Test
     public void verifyRedirectUrl() {
-        val method = "some-method";
-        when(webContextMock.getRequestParameter(CasProtocolConstants.PARAMETER_METHOD)).thenReturn(Optional.of(method));
-        val locale = "some-locale";
-        when(webContextMock.getRequestParameter(config.getLocale().getParamName())).thenReturn(Optional.of(locale));
-        val theme = "some-theme";
-        when(webContextMock.getRequestParameter(config.getTheme().getParamName())).thenReturn(Optional.of(theme));
-        service.setSource("source");
-        service.setOriginalUrl("http://service.original.url.com");
-        val factory = newFactory();
+        val request = new MockHttpServletRequest();
+        val response = new MockHttpServletResponse();
 
-        Optional<DelegatedClientIdentityProviderConfiguration> actual = factory.resolve();
+        request.addParameter(CasProtocolConstants.PARAMETER_METHOD, "some-method");
+        request.addParameter(casProperties.getLocale().getParamName(), "some-locale");
+        request.addParameter(casProperties.getTheme().getParamName(), "some-theme");
+        val context = new JEEContext(request, response);
+
+        val service = RegisteredServiceTestUtils.getService("example");
+        service.setOriginalUrl("http://service.original.url.com");
+        val client = new CasClient(new CasConfiguration());
+        val factory = DelegatedClientIdentityProviderConfigurationFactory.builder()
+            .casProperties(casProperties)
+            .client(client)
+            .service(service)
+            .webContext(context)
+            .build();
+
+        val actual = factory.resolve();
 
         assertTrue(actual.isPresent());
         assertEquals(client.getName(), actual.get().getName());
         assertEquals("cas", actual.get().getType());
         val redirectUrl = actual.get().getRedirectUrl();
         assertNotNull(redirectUrl);
-        val invalidRedirectUrlMessage = "invalid " + redirectUrl;
-        assertTrue(redirectUrl.startsWith("clientredirect?"), invalidRedirectUrlMessage);
-        assertTrue(redirectUrl.contains("client_name=" + client.getName()), invalidRedirectUrlMessage);
-        assertTrue(redirectUrl.contains("method=" + method), invalidRedirectUrlMessage);
-        assertTrue(redirectUrl.contains("locale=" + locale), invalidRedirectUrlMessage);
-        assertTrue(redirectUrl.contains("theme=" + theme), invalidRedirectUrlMessage);
-        assertTrue(redirectUrl.contains("source=" + URLEncoder.encode(service.getOriginalUrl(), StandardCharsets.UTF_8)),
-                invalidRedirectUrlMessage);
+
+        assertTrue(redirectUrl.startsWith("clientredirect?"));
+        assertTrue(redirectUrl.contains("client_name=" + client.getName()));
+        assertTrue(redirectUrl.contains("method=some-method"));
+        assertTrue(redirectUrl.contains("locale=some-locale"));
+        assertTrue(redirectUrl.contains("theme=some-theme"));
+        assertTrue(redirectUrl.contains(EncodingUtils.urlEncode(service.getOriginalUrl())));
     }
 
-    /**
-     * check that the + character is encoded correctly
-     */
     @Test
     public void verifyRedirectUrlCorrectlyEncoded() {
-        service.setSource("source");
+        val request = new MockHttpServletRequest();
+        val response = new MockHttpServletResponse();
+        val context = new JEEContext(request, response);
+
+        val service = RegisteredServiceTestUtils.getService("example");
         service.setOriginalUrl("http://service.original.url.com?response_type=idtoken+token");
-        val factory = newFactory();
+        val client = new CasClient(new CasConfiguration());
+        client.setCustomProperties(Map.of(ClientCustomPropertyConstants.CLIENT_CUSTOM_PROPERTY_CSS_CLASS, "custom-class"));
+        val factory = DelegatedClientIdentityProviderConfigurationFactory.builder()
+            .casProperties(casProperties)
+            .client(client)
+            .service(service)
+            .webContext(context)
+            .build();
 
-        Optional<DelegatedClientIdentityProviderConfiguration> actual = factory.resolve();
-
+        val actual = factory.resolve();
         assertTrue(actual.isPresent());
         val redirectUrl = actual.get().getRedirectUrl();
         assertNotNull(redirectUrl);
-        assertTrue(redirectUrl.contains("source=" + URLEncoder.encode(service.getOriginalUrl(), StandardCharsets.UTF_8)));
-    }
-
-    private DelegatedClientIdentityProviderConfigurationFactory newFactory() {
-        return DelegatedClientIdentityProviderConfigurationFactory.builder()
-                    .casProperties(config)
-                    .client(client)
-                    .service(service)
-                    .webContext(webContextMock)
-                    .build();
+        assertTrue(redirectUrl.contains(EncodingUtils.urlEncode(service.getOriginalUrl())));
     }
 }
