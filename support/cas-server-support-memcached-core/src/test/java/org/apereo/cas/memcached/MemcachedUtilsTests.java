bc8dabd0b8454773fd7286a479e4e711579cc7b3
Misagh Moayyed <mm1844@gmail.com>
2020-04-18 01:02:34 +0430

    add tests

--- support/cas-server-support-memcached-core/src/test/java/org/apereo/cas/memcached/MemcachedUtilsTests.java
+++ support/cas-server-support-memcached-core/src/test/java/org/apereo/cas/memcached/MemcachedUtilsTests.java
@@ -2,7 +2,6 @@ package org.apereo.cas.memcached;
 
 import org.apereo.cas.configuration.model.support.memcached.BaseMemcachedProperties;
 
-import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -15,7 +14,6 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author Misagh Moayyed
  * @since 6.2.0
  */
-@Slf4j
 @Tag("Memcached")
 public class MemcachedUtilsTests {
 
