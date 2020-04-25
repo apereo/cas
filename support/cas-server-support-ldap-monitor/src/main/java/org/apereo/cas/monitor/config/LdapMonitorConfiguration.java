e9221ab232f92a32477c8c62e11b7dc25e202dd2
leeyc0 <leeyc0@users.noreply.github.com>
2020-04-25 15:54:21 +0800

    rewrite using prototype bean

--- support/cas-server-support-ldap-monitor/src/main/java/org/apereo/cas/monitor/config/LdapMonitorConfiguration.java
+++ support/cas-server-support-ldap-monitor/src/main/java/org/apereo/cas/monitor/config/LdapMonitorConfiguration.java
@@ -9,20 +9,22 @@ import org.apereo.cas.util.LdapUtils;
 import lombok.val;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
+import org.ldaptive.ConnectionValidator;
+import org.ldaptive.PooledConnectionFactory;
 import org.ldaptive.SearchConnectionValidator;
-import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.autoconfigure.health.ConditionalOnEnabledHealthIndicator;
 import org.springframework.boot.actuate.health.CompositeHealthContributor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ExecutorService;
 
 /**
  * This is {@link LdapMonitorConfiguration}.
@@ -38,18 +40,22 @@ public class LdapMonitorConfiguration {
     @Autowired
     private CasConfigurationProperties casProperties;
 
-    @Lazy
-    @Bean
-    public PooledLdapConnectionFactoryHealthIndicatorArrayList pooledLdapConnectionFactoryHealthIndicatorArrayListBean() {
-        return new PooledLdapConnectionFactoryHealthIndicatorArrayList();
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Bean(name = "ldapMonitorConfigurationPooledLdapConnectionFactoryHealthIndicator")
+    @Scope("prototype")
+    public PooledLdapConnectionFactoryHealthIndicator pooledLdapConnectionFactoryHealthIndicator(
+                                                      final Long maxWait,
+                                                      final PooledConnectionFactory factory,
+                                                      final ExecutorService executor,
+                                                      final ConnectionValidator validator) {
+        return new PooledLdapConnectionFactoryHealthIndicator(maxWait.longValue(), factory, executor, validator);
     }
 
     @Bean
-    @Autowired
     @ConditionalOnEnabledHealthIndicator("pooledLdapConnectionFactoryHealthIndicator")
-    public CompositeHealthContributor pooledLdapConnectionFactoryHealthIndicator(
-        final PooledLdapConnectionFactoryHealthIndicatorArrayList pooledLdapConnectionFactoryHealthIndicatorArrayListBean) {
-
+    public CompositeHealthContributor pooledLdapConnectionFactoryHealthIndicator() {
         val ldaps = casProperties.getMonitor().getLdap();
         val contributors = new LinkedHashMap<>(MAP_SIZE);
         ldaps.stream()
@@ -58,26 +64,15 @@ public class LdapMonitorConfiguration {
                 val executor = Beans.newThreadPoolExecutorFactoryBean(ldap.getPool());
                 executor.afterPropertiesSet();
                 val connectionFactory = LdapUtils.newLdaptivePooledConnectionFactory(ldap);
-                val healthIndicator = new PooledLdapConnectionFactoryHealthIndicator(Beans.newDuration(ldap.getMaxWait()).toMillis(),
-                    connectionFactory, executor.getObject(), new SearchConnectionValidator());
+                val healthIndicator = (PooledLdapConnectionFactoryHealthIndicator) this.applicationContext.getBean(
+                    "ldapMonitorConfigurationPooledLdapConnectionFactoryHealthIndicator",
+                    Long.valueOf(Beans.newDuration(ldap.getMaxWait()).toMillis()),
+                    connectionFactory, executor.getObject(),
+                    new SearchConnectionValidator());
                 val name = StringUtils.defaultIfBlank(ldap.getName(), UUID.randomUUID().toString());
-                pooledLdapConnectionFactoryHealthIndicatorArrayListBean.add(healthIndicator);
                 return Pair.of(name, healthIndicator);
             })
             .forEach(it -> contributors.put(it.getKey(), it.getValue()));
         return CompositeHealthContributor.fromMap((Map) contributors);
-    }
-
-    private static class PooledLdapConnectionFactoryHealthIndicatorArrayList implements DisposableBean {
-        private final ArrayList<PooledLdapConnectionFactoryHealthIndicator> list = new ArrayList<>();
-
-        public void add(final PooledLdapConnectionFactoryHealthIndicator healthIndicator) {
-            this.list.add(healthIndicator);
-        }
-
-        @Override
-        public void destroy() {
-            this.list.forEach(PooledLdapConnectionFactoryHealthIndicator::destroy);
-        }
     }
 }
