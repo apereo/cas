673953c7427b7b3d6920ed25172c1000419a5fc3
Misagh Moayyed <mm1844@gmail.com>
2020-05-01 14:37:14 +0430

    add tests

---
+++ support/cas-server-support-gua/src/test/java/org/apereo/cas/gua/impl/StaticUserGraphicalAuthenticationRepositoryTests.java
@@ -0,0 +1,28 @@
+package org.apereo.cas.gua.impl;
+
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This is {@link StaticUserGraphicalAuthenticationRepositoryTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.2.0
+ */
+public class StaticUserGraphicalAuthenticationRepositoryTests {
+
+    @Test
+    public void verifyImage() throws Exception {
+        val repo = new StaticUserGraphicalAuthenticationRepository(new ClassPathResource("image.jpg"));
+        assertFalse(repo.getGraphics("casuser").isEmpty());
+    }
+
+    @Test
+    public void verifyBadImage() throws Exception {
+        val repo = new StaticUserGraphicalAuthenticationRepository(new ClassPathResource("missing.jpg"));
+        assertTrue(repo.getGraphics("casuser").isEmpty());
+    }
+}
