d0f44f0de0448dcd0681d1de457b35c8bbde7df3
Misagh Moayyed <mm1844@gmail.com>
2020-05-02 21:51:01 +0430

    add tests

--- support/cas-server-support-saml-idp-metadata-rest/src/test/java/org/apereo/cas/support/saml/idp/metadata/RestfulSamlIdPMetadataCipherExecutorTests.java
+++ support/cas-server-support-saml-idp-metadata-rest/src/test/java/org/apereo/cas/support/saml/idp/metadata/RestfulSamlIdPMetadataCipherExecutorTests.java
@@ -1,6 +1,5 @@
 package org.apereo.cas.support.saml.idp.metadata;
 
-import org.apereo.cas.authentication.metadata.CacheCredentialsCipherExecutor;
 import org.apereo.cas.util.crypto.CipherExecutor;
 
 import lombok.val;
