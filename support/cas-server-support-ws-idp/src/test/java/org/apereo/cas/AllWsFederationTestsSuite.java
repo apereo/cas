60e07c46c985ec506efd6e7baf0a4d67a968ccff
Misagh Moayyed <mm1844@gmail.com>
2020-05-02 21:52:10 +0430

    add tests

--- support/cas-server-support-ws-idp/src/test/java/org/apereo/cas/AllWsFederationTestsSuite.java
+++ support/cas-server-support-ws-idp/src/test/java/org/apereo/cas/AllWsFederationTestsSuite.java
@@ -2,6 +2,7 @@ package org.apereo.cas;
 
 import org.apereo.cas.authentication.SecurityTokenServiceTokenFetcherTests;
 import org.apereo.cas.config.CoreWsSecuritySecurityTokenServiceConfigurationTests;
+import org.apereo.cas.ws.idp.metadata.WSFederationMetadataControllerTests;
 import org.apereo.cas.ws.idp.metadata.WSFederationMetadataWriterTests;
 import org.apereo.cas.ws.idp.web.WSFederationRequestTests;
 import org.apereo.cas.ws.idp.web.flow.WSFederationIdentityProviderWebflowConfigurerTests;
@@ -21,6 +22,7 @@ import org.junit.runner.RunWith;
     WSFederationIdentityProviderWebflowConfigurerTests.class,
     CoreWsSecuritySecurityTokenServiceConfigurationTests.class,
     WSFederationRequestTests.class,
+    WSFederationMetadataControllerTests.class,
     WSFederationMetadataWriterTests.class,
     WSFederationMetadataUIActionTests.class,
     SecurityTokenServiceTokenFetcherTests.class
