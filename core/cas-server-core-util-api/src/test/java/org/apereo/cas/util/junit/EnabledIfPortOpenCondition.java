13ca553d0e0517eebb59f1beb2c128a28aed2d3a
Hal Deadman <hdeadman@users.noreply.github.com>
2020-05-11 16:05:18 -0400

    Add a way to bypass unit test port available check (#4847)

--- core/cas-server-core-util-api/src/test/java/org/apereo/cas/util/junit/EnabledIfPortOpenCondition.java
+++ core/cas-server-core-util-api/src/test/java/org/apereo/cas/util/junit/EnabledIfPortOpenCondition.java
@@ -19,11 +19,16 @@ import java.util.Arrays;
  */
 public class EnabledIfPortOpenCondition implements ExecutionCondition {
     private static final ConditionEvaluationResult ENABLED_BY_DEFAULT = ConditionEvaluationResult.enabled("@EnabledIfPortOpen is not present");
+    private static final String IGNORE_PORT_CHECK = "IGNORE_PORT_CHECK";
 
     private static ConditionEvaluationResult enableIfOpen(final EnabledIfPortOpen annotation, final AnnotatedElement element) {
         val ports = annotation.port();
         if (ports.length == 0) {
             throw new IllegalArgumentException("At least one port must be defined");
+        }
+        if (ignorePortCheck()) {
+            return ConditionEvaluationResult.enabled(
+                    String.format("%s is enabled because %s environment variable is set", element, IGNORE_PORT_CHECK));
         }
         for (val port : ports) {
             if (port > 0 && SocketUtils.isTcpPortAvailable(port)) {
@@ -42,5 +47,14 @@ public class EnabledIfPortOpenCondition implements ExecutionCondition {
         return AnnotationSupport.findAnnotation(element, EnabledIfPortOpen.class)
             .map(annotation -> enableIfOpen(annotation, element))
             .orElse(ENABLED_BY_DEFAULT);
+    }
+
+    /**
+     * The available port check doesn't always work on Windows so this provides a bypass mechanism.
+     * @return true if IGNORE_PORT_CHECK environment variable equals true
+     */
+    private static boolean ignorePortCheck() {
+        val ignorePortCheck = System.getenv(IGNORE_PORT_CHECK);
+        return "true".equalsIgnoreCase(ignorePortCheck);
     }
 }
