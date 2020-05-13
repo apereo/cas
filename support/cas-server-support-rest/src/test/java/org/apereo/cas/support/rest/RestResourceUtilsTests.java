ef736679dddcf791d36da49bae0b1466119cb0a4
johnjcool <john.j.cool@googlemail.com>
2020-05-14 00:34:22 +0200

    added test

---
+++ support/cas-server-support-rest/src/test/java/org/apereo/cas/support/rest/RestResourceUtilsTests.java
@@ -0,0 +1,63 @@
+package org.apereo.cas.support.rest;
+
+import lombok.val;
+import org.apereo.cas.authentication.AuthenticationException;
+import org.apereo.cas.authentication.adaptive.UnauthorizedAuthenticationException;
+import org.apereo.cas.support.rest.resources.RestResourceUtils;
+import org.apereo.cas.util.CollectionUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.ResponseEntity;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+/**
+ * This is {@link RestResourceUtilsTests}.
+ *
+ * @author john.j.cool
+ * @since 6.1.6
+ */
+@ExtendWith(MockitoExtension.class)
+public class RestResourceUtilsTests {
+
+    @Mock
+    private ApplicationContext context;
+
+    @BeforeEach
+    public void before() {
+        MockitoAnnotations.initMocks(this);
+        doReturn("test").when(context)
+                .getMessage(any(), any(), any());
+    }
+
+    @Test
+    public void verifyNoFrameworkClassSendToClient() {
+        val request = mock(HttpServletRequest.class);
+        val msg = "test";
+        val map = CollectionUtils.<String, Throwable>wrap(
+                UnauthorizedAuthenticationException.class.getSimpleName(),
+                new UnauthorizedAuthenticationException(msg)
+        );
+
+        ResponseEntity<String> response = RestResourceUtils.createResponseEntityForAuthnFailure(
+                new AuthenticationException(msg, map, new HashMap<>(0)),
+                request,
+                context
+        );
+
+        assertTrue(response.getStatusCode().isError());
+        assertFalse(response.getBody().contains(UnauthorizedAuthenticationException.class.getSimpleName()));
+    }
+}
