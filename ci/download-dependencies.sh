5157e12519ab4890e1a25139576466017f51dd3e
Misagh Moayyed <mm1844@gmail.com>
2020-05-14 23:10:11 +0430

    ci

--- ci/download-dependencies.sh
+++ ci/download-dependencies.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
 source ./ci/functions.sh
 
-runBuild=false
-echo "Reviewing changes that might affect the Gradle build dependencies..."
-currentChangeSetAffectsDependencies
-retval=$?
-if [ "$retval" == 0 ]
-then
-    echo "Found changes that require the build to download dependencies."
-    runBuild=true
-else
-    echo "Changes do NOT affect project dependencies."
-    runBuild=false
-fi
-
-if [ "$runBuild" = false ]; then
-    exit 0
-fi
-
 gradle="./gradlew $@"
 gradleBuild=""
 gradleBuildOptions="--build-cache --configure-on-demand --no-daemon "
