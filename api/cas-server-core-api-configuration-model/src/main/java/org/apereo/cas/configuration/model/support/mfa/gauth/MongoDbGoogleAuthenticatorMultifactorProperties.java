e87a2c10fe4c4ddcec797e62748696f4ba06a02b
Misagh Moayyed <mm1844@gmail.com>
2020-05-15 10:49:51 +0430

    ci

--- api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/support/mfa/gauth/MongoDbGoogleAuthenticatorMultifactorProperties.java
+++ api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/support/mfa/gauth/MongoDbGoogleAuthenticatorMultifactorProperties.java
@@ -1,23 +1,12 @@
 package org.apereo.cas.configuration.model.support.mfa.gauth;
 
-import org.apereo.cas.configuration.model.core.util.EncryptionJwtSigningJwtCryptographyProperties;
-import org.apereo.cas.configuration.model.support.couchdb.BaseCouchDbProperties;
-import org.apereo.cas.configuration.model.support.jpa.AbstractJpaProperties;
-import org.apereo.cas.configuration.model.support.mfa.BaseMultifactorProviderProperties;
 import org.apereo.cas.configuration.model.support.mongo.SingleCollectionMongoDbProperties;
-import org.apereo.cas.configuration.model.support.quartz.ScheduledJobProperties;
-import org.apereo.cas.configuration.model.support.redis.BaseRedisProperties;
 import org.apereo.cas.configuration.support.RequiredProperty;
 import org.apereo.cas.configuration.support.RequiresModule;
-import org.apereo.cas.configuration.support.SpringResourceProperties;
-import org.apereo.cas.util.crypto.CipherExecutor;
 
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
-import org.springframework.boot.context.properties.NestedConfigurationProperty;
-
-import java.io.Serializable;
 
 /**
  * This is {@link MongoDbGoogleAuthenticatorMultifactorProperties}.
