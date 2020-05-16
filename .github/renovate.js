c2ace82b5286f0de8422f247bc835ac04a1edb3b
Misagh Moayyed <mm1844@gmail.com>
2020-05-16 13:15:43 +0430

    gh-17

--- .github/renovate.js
+++ .github/renovate.js
@@ -1,12 +1,14 @@
-{
-  "gitAuthor": "Renovate Bot <bot@renovateapp.com>",
-  "logLevel": "debug",
-  "onboarding": false,
-  "platform": "github",
-  "includeForks": true,
-  "repositories": [
-    "apereo/cas"
+module.exports = {
+  branchPrefix: 'renovate/',
+  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+  logLevel: 'debug',
+  onboarding: false,
+  platform: 'github',
+  includeForks: true,
+  repositories: [
+    'apereo/cas'
   ],
+  enabledManagers: ["gradle"],
   "gradle": {
     "enabled": true
   },
@@ -224,4 +226,4 @@
       ]
     }
   ]
-}
+};
