986f2b37af8cf4cf195ce2070e7daa1953192d49
leeyc0 <leeyc0@users.noreply.github.com>
2020-05-18 19:37:09 +0800

    Add unit tests for EncodingUtils

--- core/cas-server-core-util/src/test/java/org/apereo/cas/util/EncodingUtilsTests.java
+++ core/cas-server-core-util/src/test/java/org/apereo/cas/util/EncodingUtilsTests.java
@@ -9,6 +9,7 @@ import lombok.val;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.jose4j.keys.AesKey;
+import org.jose4j.keys.EcKeyUtil;
 import org.jose4j.keys.RsaKeyUtil;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -31,7 +32,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class EncodingUtilsTests {
 
     @SneakyThrows
-    private static PrivateKey getPrivateKey() {
+    private static PrivateKey getRSAPrivateKey() {
         val factory = new PrivateKeyFactoryBean();
         factory.setAlgorithm(RsaKeyUtil.RSA);
         factory.setLocation(new ClassPathResource("keys/RSA2048Private.key"));
@@ -40,8 +41,56 @@ public class EncodingUtilsTests {
     }
 
     @SneakyThrows
-    private static PublicKey getPublicKey() {
+    private static PublicKey getRSAPublicKey() {
         val factory = new PublicKeyFactoryBean(new ClassPathResource("keys/RSA2048Public.key"), RsaKeyUtil.RSA);
+        factory.setSingleton(false);
+        return factory.getObject();
+    }
+
+    @SneakyThrows
+    private static PrivateKey getP256PrivateKey() {
+        val factory = new PrivateKeyFactoryBean();
+        factory.setAlgorithm(EcKeyUtil.EC);
+        factory.setLocation(new ClassPathResource("keys/ECsecp256r1Private.key"));
+        factory.setSingleton(false);
+        return factory.getObject();
+    }
+
+    @SneakyThrows
+    private static PublicKey getP256PublicKey() {
+        val factory = new PublicKeyFactoryBean(new ClassPathResource("keys/ECsecp256r1Public.key"), EcKeyUtil.EC);
+        factory.setSingleton(false);
+        return factory.getObject();
+    }
+
+    @SneakyThrows
+    private static PrivateKey getP384PrivateKey() {
+        val factory = new PrivateKeyFactoryBean();
+        factory.setAlgorithm(EcKeyUtil.EC);
+        factory.setLocation(new ClassPathResource("keys/ECsecp384r1Private.key"));
+        factory.setSingleton(false);
+        return factory.getObject();
+    }
+
+    @SneakyThrows
+    private static PublicKey getP384PublicKey() {
+        val factory = new PublicKeyFactoryBean(new ClassPathResource("keys/ECsecp384r1Public.key"), EcKeyUtil.EC);
+        factory.setSingleton(false);
+        return factory.getObject();
+    }
+
+    @SneakyThrows
+    private static PrivateKey getP521PrivateKey() {
+        val factory = new PrivateKeyFactoryBean();
+        factory.setAlgorithm(EcKeyUtil.EC);
+        factory.setLocation(new ClassPathResource("keys/ECsecp521r1Private.key"));
+        factory.setSingleton(false);
+        return factory.getObject();
+    }
+
+    @SneakyThrows
+    private static PublicKey getP521PublicKey() {
+        val factory = new PublicKeyFactoryBean(new ClassPathResource("keys/ECsecp521r1Public.key"), EcKeyUtil.EC);
         factory.setSingleton(false);
         return factory.getObject();
     }
@@ -51,7 +100,7 @@ public class EncodingUtilsTests {
         val secret = EncodingUtils.generateJsonWebKey(512);
         val key = new AesKey(secret.getBytes(StandardCharsets.UTF_8));
         val value = "ThisValue";
-        val signed = EncodingUtils.signJwsHMACSha512(key, value.getBytes(StandardCharsets.UTF_8), Map.of());
+        val signed = EncodingUtils.SIGN_JWS_HMAC_SHA512.signJws(key, value.getBytes(StandardCharsets.UTF_8), Map.of());
         val jwt = EncodingUtils.verifyJwsSignature(key, signed);
         val result = new String(jwt, StandardCharsets.UTF_8);
         assertTrue(result.equals(value));
@@ -60,8 +109,35 @@ public class EncodingUtilsTests {
     @Test
     public void verifyRsaKeyForJwtSigning() {
         val value = "ThisValue";
-        val signed = EncodingUtils.signJwsRSASha512(getPrivateKey(), value.getBytes(StandardCharsets.UTF_8), Map.of());
-        val jwt = EncodingUtils.verifyJwsSignature(getPublicKey(), signed);
+        val signed = EncodingUtils.SIGN_JWS_RSA_SHA512.signJws(getRSAPrivateKey(), value.getBytes(StandardCharsets.UTF_8), Map.of());
+        val jwt = EncodingUtils.verifyJwsSignature(getRSAPublicKey(), signed);
+        val result = new String(jwt, StandardCharsets.UTF_8);
+        assertTrue(result.equals(value));
+    }
+
+    @Test
+    public void verifyP256KeyForJwtSigning() {
+        val value = "ThisValue";
+        val signed = EncodingUtils.SIGN_JWS_EC_P256.signJws(getP256PrivateKey(), value.getBytes(StandardCharsets.UTF_8), Map.of());
+        val jwt = EncodingUtils.verifyJwsSignature(getP256PublicKey(), signed);
+        val result = new String(jwt, StandardCharsets.UTF_8);
+        assertTrue(result.equals(value));
+    }
+
+    @Test
+    public void verifyP384KeyForJwtSigning() {
+        val value = "ThisValue";
+        val signed = EncodingUtils.SIGN_JWS_EC_P384.signJws(getP384PrivateKey(), value.getBytes(StandardCharsets.UTF_8), Map.of());
+        val jwt = EncodingUtils.verifyJwsSignature(getP384PublicKey(), signed);
+        val result = new String(jwt, StandardCharsets.UTF_8);
+        assertTrue(result.equals(value));
+    }
+
+    @Test
+    public void verifyP521KeyForJwtSigning() {
+        val value = "ThisValue";
+        val signed = EncodingUtils.SIGN_JWS_EC_P521.signJws(getP521PrivateKey(), value.getBytes(StandardCharsets.UTF_8), Map.of());
+        val jwt = EncodingUtils.verifyJwsSignature(getP521PublicKey(), signed);
         val result = new String(jwt, StandardCharsets.UTF_8);
         assertTrue(result.equals(value));
     }
@@ -82,8 +158,8 @@ public class EncodingUtilsTests {
     @Test
     public void verifyRsaKeyForJwtEncryption() {
         val value = "ThisValue";
-        val found = EncodingUtils.encryptValueAsJwtRsaOeap256Aes256Sha512(getPublicKey(), value);
-        val jwt = EncodingUtils.decryptJwtValue(getPrivateKey(), found);
+        val found = EncodingUtils.encryptValueAsJwtRsaOeap256Aes256Sha512(getRSAPublicKey(), value);
+        val jwt = EncodingUtils.decryptJwtValue(getRSAPrivateKey(), found);
         assertTrue(jwt.equals(value));
     }
 
