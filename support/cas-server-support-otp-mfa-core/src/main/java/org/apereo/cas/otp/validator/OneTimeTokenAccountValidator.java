4d4dd64ff55d54885108903e69670e655f3bcb4b
Hayden Sartoris <hsartoris@bard.edu>
2020-05-18 15:25:08 -0400

    completed refactor of parsing and validating to OneTimeTokenAccountValidator

--- support/cas-server-support-otp-mfa-core/src/main/java/org/apereo/cas/otp/validator/OneTimeTokenAccountValidator.java
+++ support/cas-server-support-otp-mfa-core/src/main/java/org/apereo/cas/otp/validator/OneTimeTokenAccountValidator.java
@@ -1,6 +1,8 @@
 package org.apereo.cas.otp.validator;
 
+import org.apereo.cas.authentication.Credential;
 import org.apereo.cas.authentication.OneTimeTokenAccount;
+import org.apereo.cas.authentication.credential.OneTimeTokenCredential;
 
 /**
  * This is {@link OneTimeTokenAccountValidator}.
@@ -11,11 +13,12 @@ import org.apereo.cas.authentication.OneTimeTokenAccount;
  * @author Hayden Sartoris
  * @since 6.2.0-RC5
  */
-@FunctionalInterface
 public interface OneTimeTokenAccountValidator {
 
     /**
      * Is the token valid for the given account?
+     *
+     * Assumes parsing logic has taken place.
      *
      * @param account the OTP account
      * @param token   the token to validate
@@ -23,4 +26,20 @@ public interface OneTimeTokenAccountValidator {
      */
     boolean isValid(OneTimeTokenAccount account, int token);
 
+    /**
+     * Parse presumed OTP from a Credential.
+     *
+     * @param credential the Credential presumed to contain the token
+     * @return the integer token
+     */
+    default int parseToken(Credential credential) throws IllegalArgumentException {
+        try {
+            OneTimeTokenCredential cred = (OneTimeTokenCredential) credential;
+            return Integer.parseInt(cred.getToken());
+        } catch (ClassCastException e) {
+            throw new IllegalArgumentException("Provided credential could not be cast to OneTimeTokenCredential", e);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Token could not be parsed: " + ((OneTimeTokenCredential) credential).getToken(), e);
+        }
+    }
 }
--- support/cas-server-support-otp-mfa-core/src/main/java/org/apereo/cas/otp/web/flow/OneTimeTokenAccountSaveRegistrationAction.java
+++ support/cas-server-support-otp-mfa-core/src/main/java/org/apereo/cas/otp/web/flow/OneTimeTokenAccountSaveRegistrationAction.java
@@ -4,14 +4,15 @@ import org.apereo.cas.authentication.OneTimeTokenAccount;
 import org.apereo.cas.authentication.credential.OneTimeTokenCredential;
 import org.apereo.cas.otp.repository.credentials.OneTimeTokenCredentialRepository;
 import org.apereo.cas.otp.validator.OneTimeTokenAccountValidator;
+import org.apereo.cas.web.flow.CasWebflowConstants;
 import org.apereo.cas.web.support.WebUtils;
 
-//import com.warrenstrange.googleauth.IGoogleAuthenticator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.webflow.action.AbstractAction;
+import org.springframework.webflow.action.EventFactorySupport;
 import org.springframework.webflow.execution.Event;
 import org.springframework.webflow.execution.RequestContext;
 
@@ -25,7 +26,6 @@ import org.springframework.webflow.execution.RequestContext;
 @RequiredArgsConstructor
 public class OneTimeTokenAccountSaveRegistrationAction extends AbstractAction {
     private final OneTimeTokenCredentialRepository repository;
-    //private final IGoogleAuthenticator googleAuthenticatorInstance;
     private final OneTimeTokenAccountValidator tokenValidator;
 
     @Override
@@ -34,29 +34,31 @@ public class OneTimeTokenAccountSaveRegistrationAction extends AbstractAction {
             .get(OneTimeTokenAccountCheckRegistrationAction.FLOW_SCOPE_ATTR_ACCOUNT, OneTimeTokenAccount.class);
         val uid = WebUtils.getAuthentication(requestContext).getPrincipal().getId();
         val credential = WebUtils.getCredential(requestContext, OneTimeTokenCredential.class);
+        val account = requestContext.getFlowScope().get("key", OneTimeTokenAccount.class);
 
-        if (!StringUtils.isNumeric(credential.getToken())) {
-            LOGGER.error("Non-numeric OTP token provided: [{}]", credential.getToken());
-            return error();
+        int token;
+        try {
+            token = tokenValidator.parseToken(credential);
+        } catch (IllegalArgumentException e) {
+            LOGGER.error("Unable to extract token from Credential [{}] for user [{}]",
+                    credential, uid);
+            return new EventFactorySupport().event(this, CasWebflowConstants.TRANSITION_ID_REGISTER);
         }
-
-        val token = Integer.parseInt(credential.getToken());
 
         LOGGER.debug("Attempting to validate OTP token [{}] with [{}]", token, account);
-        //val isCodeValid = this.googleAuthenticatorInstance.authorize(account.getSecretKey(), token);
-        val isCodeValid = this.tokenValidator.isValid(account, token);
+        val isCodeValid = tokenValidator.isValid(account, token);
 
-        if (!isCodeValid && account.getScratchCodes().contains(token)) {
-            LOGGER.warn("User [{}] attempted to use scratch code during OTP registration; this is likely a mistake.", uid);
-            return error();
-        }
         if (isCodeValid) {
             LOGGER.debug("Validated OTP token [{}] to register user [{}]", token, uid);
             repository.save(uid, account.getSecretKey(), account.getValidationCode(), account.getScratchCodes());
             return success();
         }
+        if (account.getScratchCodes().contains(token)) {
+            LOGGER.warn("User [{}] attempted to use scratch code during OTP registration; this is likely a mistake.", uid);
+            return new EventFactorySupport().event(this, CasWebflowConstants.TRANSITION_ID_REGISTER);
+        }
 
         LOGGER.warn("Failed to validate token [{}] to register user [{}]", token, uid);
-        return error();
+        return new EventFactorySupport().event(this, CasWebflowConstants.TRANSITION_ID_REGISTER);
     }
 }
