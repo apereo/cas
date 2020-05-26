971a638584b0bf0c3fc1fde1ca42a597f5803914
Misagh Moayyed <mm1844@gmail.com>
2020-05-26 15:49:58 +0430

    fix tests

--- support/cas-server-support-radius/src/test/java/org/apereo/cas/config/RadiusConfigurationTests.java
+++ support/cas-server-support-radius/src/test/java/org/apereo/cas/config/RadiusConfigurationTests.java
@@ -7,6 +7,7 @@ import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.configuration.model.support.radius.RadiusClientProperties;
 import org.apereo.cas.logout.config.CasCoreLogoutConfiguration;
 import org.apereo.cas.util.CollectionUtils;
+import org.apereo.cas.util.junit.EnabledIfPortOpen;
 import org.apereo.cas.web.config.CasCookieConfiguration;
 import org.apereo.cas.web.flow.config.CasCoreWebflowConfiguration;
 import org.apereo.cas.web.flow.config.CasMultifactorAuthenticationWebflowConfiguration;
