5d9add33b3dbbb6f9ba58b87a9edd0c283014f68
Misagh Moayyed <mm1844@gmail.com>
2020-05-30 00:27:06 +0430

    fix tests

--- testcas.sh
+++ testcas.sh
@@ -3,7 +3,7 @@
 clear
 
 printHelp() {
-    echo -e "\nUsage: ./testcas.sh --category [category1,category2,...] [--help] [--test TestClass] [--ignore-failures] [--no-wrapper] [--no-retry] [--debug] [--no-parallel] [--dry-run] [--info] \n"
+    echo -e "\nUsage: ./testcas.sh --category [category1,category2,...] [--help] [--test TestClass] [--ignore-failures] [--no-wrapper] [--no-retry] [--debug] [--no-parallel] [--dry-run] [--info] [--with-coverage] \n"
     echo -e "Available test categories are:\n"
     echo -e "simple,memcached,cassandra,groovy,kafka,ldap,rest,mfa,jdbc,mssql,oracle,radius,couchdb,\
 mariadb,files,postgres,dynamodb,couchbase,uma,saml,mail,aws,jms,hazelcast,jmx,ehcache,\
@@ -21,6 +21,10 @@ while (( "$#" )); do
     case "$1" in
     --no-parallel)
         parallel="--no-parallel "
+        shift
+        ;;
+    --with-coverage)
+        task+="jacocoRootReport "
         shift
         ;;
     --info)
