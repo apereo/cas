2378afc008a3b9002492d5010cfd5eec62b922e1
Misagh Moayyed <mm1844@gmail.com>
2020-05-29 14:59:56 +0430

    move test suite validation out of ci/build

--- ci/analyze-codebase.sh
+++ ci/analyze-codebase.sh
@@ -55,6 +55,14 @@ if [ $retVal != 0 ]; then
     exit $retVal
 fi
 
+echo -e "Checking for missing class in tests suites"
+groovy ./ci/groovy/CheckMissingClassInTestsSuite.groovy
+retVal=$?
+if [ $retVal != 0 ]; then
+    echo -e "Groovy analysis has found issues with test suites."
+    exit $retVal
+fi
+
 echo -e "Groovy analysis has successfully examined the codebase."
 echo -e "***************************************************************************************"
 echo -e "Build finished at `date`"
