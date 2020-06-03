72a90b492ed6c4f1b2ee9d714d754040aaf745f1
Misagh Moayyed <mm1844@gmail.com>
2020-06-03 18:56:52 +0430

    fix build

--- testcas.sh
+++ testcas.sh
@@ -15,7 +15,7 @@ parallel="--parallel "
 dryRun=""
 info=""
 gradleCmd="./gradlew"
-flags="--watch-fs --no-daemon --configure-on-demand --build-cache -x javadoc -x check -DskipNestedConfigMetadataGen=true -DshowStandardStreams=true "
+flags="--no-daemon --configure-on-demand --build-cache -x javadoc -x check -DskipNestedConfigMetadataGen=true -DshowStandardStreams=true "
 
 while (( "$#" )); do
     case "$1" in
