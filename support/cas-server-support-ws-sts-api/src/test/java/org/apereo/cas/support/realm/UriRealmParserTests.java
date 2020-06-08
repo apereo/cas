7c5f343bb4474bc1942d3e5eb6f0ad9a73a8753e
Misagh Moayyed <mm1844@gmail.com>
2020-06-08 20:08:37 +0430

    add tests

--- support/cas-server-support-ws-sts-api/src/test/java/org/apereo/cas/support/realm/UriRealmParserTests.java
+++ support/cas-server-support-ws-sts-api/src/test/java/org/apereo/cas/support/realm/UriRealmParserTests.java
@@ -3,21 +3,12 @@ package org.apereo.cas.support.realm;
 import lombok.val;
 import org.apache.cxf.sts.token.realm.RealmProperties;
 import org.apache.cxf.ws.security.sts.provider.STSException;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.AfterEach;
-
-import static org.apereo.cas.util.junit.Assertions.*;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.*;
-
-import static org.mockito.Mockito.*;
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Tag;
 
 import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * This is {@link UriRealmParserTests}.
@@ -29,7 +20,7 @@ import java.util.Map;
 public class UriRealmParserTests {
 
     @Test
-    public void verifySingleToken() {
+    public void verifyToken() {
         val properties = new RealmProperties();
         val parser = new UriRealmParser(Map.of("APEREO.ORG", properties));
         assertEquals("APEREO.ORG", parser.parseRealm(Map.of("org.apache.cxf.request.url", "https://apereo.org/cas")));
