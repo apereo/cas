c37bb1e5cff8880041992f24a2693db237276042
Misagh Moayyed <mm1844@gmail.com>
2020-06-09 16:04:45 +0430

    add tests

---
+++ support/cas-server-support-generic-remote-webflow/src/test/java/org/apereo/cas/web/flow/RemoteAddressNonInteractiveCredentialsActionTests.java
@@ -0,0 +1,47 @@
+package org.apereo.cas.web.flow;
+
+import org.apereo.cas.BaseRemoteAddressTests;
+
+import lombok.val;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.webflow.context.ExternalContextHolder;
+import org.springframework.webflow.context.servlet.ServletExternalContext;
+import org.springframework.webflow.execution.Action;
+import org.springframework.webflow.execution.RequestContextHolder;
+import org.springframework.webflow.test.MockRequestContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This is {@link RemoteAddressNonInteractiveCredentialsActionTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.3.0
+ */
+@SpringBootTest(classes = BaseRemoteAddressTests.SharedTestConfiguration.class,
+    properties = "cas.authn.remote-address.ip-address-range=192.168.1.0/255.255.255.0")
+@Tag("Simple")
+public class RemoteAddressNonInteractiveCredentialsActionTests {
+
+    @Autowired
+    @Qualifier("remoteAddressCheck")
+    private Action remoteAddressCheck;
+
+    @Test
+    public void verifyOperation() throws Exception {
+        val context = new MockRequestContext();
+        val request = new MockHttpServletRequest();
+        val response = new MockHttpServletResponse();
+        context.setExternalContext(new ServletExternalContext(new MockServletContext(), request, response));
+        RequestContextHolder.setRequestContext(context);
+        ExternalContextHolder.setExternalContext(context.getExternalContext());
+        assertNotNull(remoteAddressCheck.execute(context));
+    }
+}
