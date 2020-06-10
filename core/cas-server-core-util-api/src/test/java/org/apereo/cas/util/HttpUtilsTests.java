1311863b5994983ea2784c090e1889a3b654eba9
Misagh Moayyed <mm1844@gmail.com>
2020-06-10 20:36:57 +0430

    add tests

---
+++ core/cas-server-core-util-api/src/test/java/org/apereo/cas/util/HttpUtilsTests.java
@@ -0,0 +1,38 @@
+package org.apereo.cas.util;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This is {@link HttpUtilsTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.3.0
+ */
+@Tag("Utility")
+public class HttpUtilsTests {
+
+    @Test
+    public void verifyExec() {
+        assertNotNull(HttpUtils.getHttpClient());
+        assertNull(HttpUtils.execute("http://localhost:1234", "GET", Map.of()));
+        assertNull(HttpUtils.executeGet("http://localhost:1234", "user", "password", Map.of()));
+        assertNull(HttpUtils.executeDelete("http://localhost:1234", "user", "password", Map.of(), Map.of()));
+    }
+
+    @Test
+    public void verifyClose() {
+        assertDoesNotThrow(new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                HttpUtils.close(null);
+            }
+        });
+    }
+
+}
