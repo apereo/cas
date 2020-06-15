71c0470bb928642f2e82bf1f7c674f7f7996646e
Misagh Moayyed <mm1844@gmail.com>
2020-06-15 20:54:03 +0430

    add tests

--- core/cas-server-core-services/src/test/java/org/apereo/cas/services/util/RegisteredServiceYamlSerializerTests.java
+++ core/cas-server-core-services/src/test/java/org/apereo/cas/services/util/RegisteredServiceYamlSerializerTests.java
@@ -1,10 +1,15 @@
 package org.apereo.cas.services.util;
 
+import org.apereo.cas.services.RegexRegisteredService;
+
 import lombok.val;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,4 +27,13 @@ public class RegisteredServiceYamlSerializerTests {
         val zer = new RegisteredServiceYamlSerializer();
         assertFalse(zer.supports(new File("bad-file")));
     }
+
+    @Test
+    public void verifyWriter() {
+        val zer = new RegisteredServiceYamlSerializer();
+        val writer = new StringWriter();
+        zer.to(writer, new RegexRegisteredService());
+        assertNotNull(zer.from(new ByteArrayInputStream(writer.toString().getBytes(StandardCharsets.UTF_8))));
+    }
+
 }
