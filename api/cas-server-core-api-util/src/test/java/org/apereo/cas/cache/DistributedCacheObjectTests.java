46bd6a72419847f6684692eb0e21307c15364978
Misagh Moayyed <mm1844@gmail.com>
2020-06-17 12:32:13 +0430

    add tests

--- api/cas-server-core-api-util/src/test/java/org/apereo/cas/cache/DistributedCacheObjectTests.java
+++ api/cas-server-core-api-util/src/test/java/org/apereo/cas/cache/DistributedCacheObjectTests.java
@@ -19,10 +19,24 @@ public class DistributedCacheObjectTests {
     @Test
     public void verifyAction() {
         val o = new DistributedCacheObject<String>("objectValue");
+        assertNotNull(o.toString());
+        assertNotNull(o.getValue());
+        assertTrue(o.getTimestamp() > 0);
         assertTrue(o.getProperties().isEmpty());
         o.getProperties().put("key", "value");
         assertFalse(o.getProperties().isEmpty());
         assertNotNull(o.getProperty("key", String.class));
         assertTrue(o.containsProperty("key"));
+    }
+
+    @Test
+    public void verifyNullValue() {
+        val o = new DistributedCacheObject<String>("objectValue");
+        assertTrue(o.getProperties().isEmpty());
+        o.getProperties().put("key", null);
+        o.getProperties().put("key2", 12.54);
+        assertNull(o.getProperty("nothing", String.class));
+        assertNull(o.getProperty("key", String.class));
+        assertThrows(ClassCastException.class, () -> o.getProperty("key2", Long.class));
     }
 }
