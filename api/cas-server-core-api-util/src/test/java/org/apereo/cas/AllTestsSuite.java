bb891149ec76d7519fa0489637d5b3b711689283
Misagh Moayyed <mm1844@gmail.com>
2020-06-17 12:38:07 +0430

    add tests

--- api/cas-server-core-api-util/src/test/java/org/apereo/cas/AllTestsSuite.java
+++ api/cas-server-core-api-util/src/test/java/org/apereo/cas/AllTestsSuite.java
@@ -3,6 +3,7 @@ package org.apereo.cas;
 import org.apereo.cas.cache.DistributedCacheManagerTests;
 import org.apereo.cas.cache.DistributedCacheObjectTests;
 import org.apereo.cas.model.CapacityTests;
+import org.apereo.cas.util.serialization.StringSerializerTests;
 
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.SelectClasses;
@@ -16,6 +17,7 @@ import org.junit.runner.RunWith;
  */
 @SelectClasses({
     CapacityTests.class,
+    StringSerializerTests.class,
     DistributedCacheManagerTests.class,
     DistributedCacheObjectTests.class
 })
