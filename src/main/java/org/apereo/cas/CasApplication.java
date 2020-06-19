4cd8c669b5d34ea8aa2cbf73e71079668c653e75
Misagh Moayyed <mm1844@gmail.com>
2020-06-19 13:00:04 +0430

    updated

---
+++ src/main/java/org/apereo/cas/CasApplication.java
@@ -0,0 +1,13 @@
+package org.apereo.cas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasApplication.class, args);
+    }
+
+}
