acf9f742fcd9c933d9db60b8319713f4d1a3f61e
Misagh Moayyed <mm1844@gmail.com>
2020-07-02 14:53:46 +0430

    clean up simple test categories

--- core/cas-server-core-authentication/src/test/java/org/apereo/cas/authentication/DefaultAuthenticationEventExecutionPlanTests.java
+++ core/cas-server-core-authentication/src/test/java/org/apereo/cas/authentication/DefaultAuthenticationEventExecutionPlanTests.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author Misagh Moayyed
  * @since 6.3.0
  */
-@Tag("Simple")
+@Tag("Authentication")
 public class DefaultAuthenticationEventExecutionPlanTests {
     @Test
     public void verifyOperation() {
