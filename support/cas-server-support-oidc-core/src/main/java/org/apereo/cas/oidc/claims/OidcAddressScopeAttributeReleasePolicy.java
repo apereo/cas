f372180f6f3fef5a44826bf9f46c0e1236842d5d
Francesco Chicchiriccò <ilgrosso@apache.org>
2020-07-14 17:33:40 +0200

    Moving member visibility to public, for external refs

--- support/cas-server-support-oidc-core/src/main/java/org/apereo/cas/oidc/claims/OidcAddressScopeAttributeReleasePolicy.java
+++ support/cas-server-support-oidc-core/src/main/java/org/apereo/cas/oidc/claims/OidcAddressScopeAttributeReleasePolicy.java
@@ -5,8 +5,6 @@ import org.apereo.cas.oidc.OidcConstants;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * This is {@link OidcAddressScopeAttributeReleasePolicy}.
@@ -17,11 +15,11 @@ import java.util.stream.Stream;
 public class OidcAddressScopeAttributeReleasePolicy extends BaseOidcScopeAttributeReleasePolicy {
     private static final long serialVersionUID = 1532960981124784595L;
 
-    private List<String> allowedAttributes = Stream.of("address").collect(Collectors.toList());
+    public static final List<String> ALLOWED_ATTRIBUTES = List.of("address");
 
     public OidcAddressScopeAttributeReleasePolicy() {
         super(OidcConstants.StandardScopes.ADDRESS.getScope());
-        setAllowedAttributes(allowedAttributes);
+        setAllowedAttributes(ALLOWED_ATTRIBUTES);
     }
 
     @JsonIgnore
--- support/cas-server-support-oidc-core/src/main/java/org/apereo/cas/oidc/claims/OidcEmailScopeAttributeReleasePolicy.java
+++ support/cas-server-support-oidc-core/src/main/java/org/apereo/cas/oidc/claims/OidcEmailScopeAttributeReleasePolicy.java
@@ -5,8 +5,6 @@ import org.apereo.cas.oidc.OidcConstants;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * This is {@link OidcEmailScopeAttributeReleasePolicy}.
@@ -17,7 +15,7 @@ import java.util.stream.Stream;
 public class OidcEmailScopeAttributeReleasePolicy extends BaseOidcScopeAttributeReleasePolicy {
     private static final long serialVersionUID = 1532960981124784595L;
 
-    private static final List<String> ALLOWED_ATTRIBUTES = Stream.of("email", "email_verified").collect(Collectors.toList());
+    public static final List<String> ALLOWED_ATTRIBUTES = List.of("email", "email_verified");
 
     public OidcEmailScopeAttributeReleasePolicy() {
         super(OidcConstants.StandardScopes.EMAIL.getScope());
