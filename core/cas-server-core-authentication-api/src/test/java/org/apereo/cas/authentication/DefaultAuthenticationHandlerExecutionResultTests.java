62ef994cda7a6635753cfd60f5c2844ade0a6fbe
Misagh Moayyed <mm1844@gmail.com>
2020-07-16 21:41:07 +0430

    add tests

---
+++ core/cas-server-core-authentication-api/src/test/java/org/apereo/cas/authentication/DefaultAuthenticationHandlerExecutionResultTests.java
@@ -0,0 +1,33 @@
+package org.apereo.cas.authentication;
+
+import org.apereo.cas.DefaultMessageDescriptor;
+import org.apereo.cas.authentication.handler.support.SimpleTestUsernamePasswordAuthenticationHandler;
+import org.apereo.cas.authentication.metadata.BasicCredentialMetaData;
+import org.apereo.cas.util.CollectionUtils;
+
+import lombok.val;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This is {@link DefaultAuthenticationHandlerExecutionResultTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.3.0
+ */
+@Tag("Authentication")
+public class DefaultAuthenticationHandlerExecutionResultTests {
+
+    @Test
+    public void verifyOperation() {
+        val otp = CoreAuthenticationTestUtils.getCredentialsWithSameUsernameAndPassword();
+        val res = new DefaultAuthenticationHandlerExecutionResult(new SimpleTestUsernamePasswordAuthenticationHandler(),
+            new BasicCredentialMetaData(otp), CollectionUtils.wrapList(new DefaultMessageDescriptor("code1")));
+        assertFalse(res.getWarnings().isEmpty());
+        res.clearWarnings();
+        assertTrue(res.getWarnings().isEmpty());
+    }
+
+}
