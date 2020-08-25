b4a4094e95db64ee1d403296ed856c9cf9ac5c2a
Misagh Moayyed <mm1844@gmail.com>
2020-08-25 12:18:42 +0400

    adjustments

--- support/cas-server-support-thymeleaf/src/test/java/org/apereo/cas/services/web/ThemeBasedViewResolverTests.java
+++ support/cas-server-support-thymeleaf/src/test/java/org/apereo/cas/services/web/ThemeBasedViewResolverTests.java
@@ -1,23 +1,11 @@
 package org.apereo.cas.services.web;
 
-import org.apereo.cas.authentication.credential.BasicIdentifiableCredential;
-import org.apereo.cas.web.support.WebUtils;
-
 import lombok.val;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.mock.web.MockServletContext;
 import org.springframework.web.servlet.ThemeResolver;
-import org.springframework.webflow.context.ExternalContextHolder;
-import org.springframework.webflow.context.servlet.ServletExternalContext;
-import org.springframework.webflow.execution.RequestContextHolder;
-import org.springframework.webflow.test.MockRequestContext;
-import org.thymeleaf.context.WebEngineContext;
 
 import java.util.Locale;
-import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
