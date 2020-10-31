af5c694cb379b137fb1dd96bdb70550549ba6628
Julien Huon <julien@huon.email>
2020-10-31 20:58:43 +0100

    Add unit tests for the PM SSO Strategy.

---
+++ support/cas-server-support-pm-webflow/src/test/java/org/apereo/cas/pm/web/flow/PasswordManagementSingleSignOnParticipationStrategyTests.java
@@ -0,0 +1,34 @@
+package org.apereo.cas.pm.web.flow;
+
+import lombok.val;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.webflow.test.MockRequestContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This is {@link PasswordManagementSingleSignOnParticipationStrategyTests}.
+ *
+ * @author Julien Huon
+ * @since 6.3.0
+ */
+@Tag("WebflowConfig")
+public class PasswordManagementSingleSignOnParticipationStrategyTests {
+
+    @Test
+    public void verifyStrategyWithANonPmRequest() {
+        val s = new PasswordManagementSingleSignOnParticipationStrategy();
+        assertFalse(s.supports(new MockRequestContext()));
+    }
+
+    @Test
+    public void verifyStrategyWithAPmRequest() {
+        val s = new PasswordManagementSingleSignOnParticipationStrategy();
+        val ctx = new MockRequestContext();
+        ctx.putRequestParameter(PasswordManagementWebflowUtils.REQUEST_PARAMETER_NAME_PASSWORD_RESET_TOKEN, "resetToken");
+
+        assertTrue(s.supports(ctx));
+        assertFalse(s.isParticipating(ctx));
+    }
+}
