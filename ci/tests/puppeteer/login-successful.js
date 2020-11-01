9854b8f00e37b962162ca0aef3098e9768b312f0
Misagh Moayyed <mm1844@gmail.com>
2020-11-01 12:52:18 +0400

    clean up

--- ci/tests/puppeteer/login-successful.js
+++ ci/tests/puppeteer/login-successful.js
@@ -2,15 +2,25 @@ const puppeteer = require('puppeteer');
 const assert = require('assert');
 
 (async () => {
-    const browser = await puppeteer.launch();
+    
+    const browser = await puppeteer.launch({
+        ignoreHTTPSErrors: true
+    });
     const page = await browser.newPage();
-    await page.goto("http://localhost:8080/cas");
-    await page.screenshot({ path: 'login.png' });
+    await page.goto("https://localhost:8443/cas/login");
+    //await page.screenshot({ path: 'login.png' });
     await page.type('#username', "casuser");
     await page.type('#password', "Mellon");
     await page.keyboard.press('Enter');
     await page.waitForNavigation();
+
     const title = await page.title();
-    assert(title === "Log In Successful - CAS – Central Authentication Service")
+    console.log(title)
+    assert(title == "CAS - Central Authentication Service")
+
+    const header = await page.$eval('#content div h2', el => el.innerText)
+    console.log(header)
+    assert(header == "Log In Successful")
+
     await browser.close();
 })();
