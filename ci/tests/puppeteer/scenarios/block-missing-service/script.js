c6c52371742a59d1dc1da8b84df5c5d1ae277960
Misagh Moayyed <mm1844@gmail.com>
2020-11-02 15:36:29 +0400

    update

--- ci/tests/puppeteer/scenarios/block-missing-service/script.js
+++ ci/tests/puppeteer/scenarios/block-missing-service/script.js
@@ -5,21 +5,12 @@ const assert = require('assert');
     const browser = await puppeteer.launch({
         ignoreHTTPSErrors: true
     });
-    // const page = await browser.newPage();
-    // await page.goto("https://localhost:8443/cas/login");
-    // //await page.screenshot({ path: 'login.png' });
-    // await page.type('#username', "casuser");
-    // await page.type('#password', "Mellon");
-    // await page.keyboard.press('Enter');
-    // await page.waitForNavigation();
-    //
-    // const title = await page.title();
-    // console.log(title)
-    // assert(title == "CAS - Central Authentication Service")
-    //
-    // const header = await page.$eval('#content div h2', el => el.innerText)
-    // console.log(header)
-    // assert(header == "Log In Successful")
+    const page = await browser.newPage();
+    await page.goto("https://localhost:8443/cas/login");
+    
+    const header = await page.$eval('#content h2', el => el.innerText)
+    console.log(header)
+    assert(header == "Application Not Authorized to Use CAS")
 
     await browser.close();
 })();
