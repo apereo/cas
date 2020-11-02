a2415d8a7b1c6347548e598304d3118bb04002b1
Misagh Moayyed <mm1844@gmail.com>
2020-11-02 13:39:50 +0400

    update

--- ci/tests/puppeteer/run.sh
+++ ci/tests/puppeteer/run.sh
@@ -15,7 +15,8 @@ echo -e "\nBuilding CAS found in $PWD..."
 ./gradlew clean build --no-daemon -PcasModules="${dependencies}"
 
 echo -e "\nLaunching CAS..."
-java -jar build/libs/cas.war --server.ssl.key-store="${keystore}" &> /dev/null &
+#java -jar build/libs/cas.war --server.ssl.key-store="${keystore}" &> /dev/null &
+java -jar build/libs/cas.war --server.ssl.key-store="${keystore}" &
 pid=$!
 echo -e "\nWaiting for CAS under pid ${pid}"
 #until curl -k -L --output /dev/null --silent --fail https://localhost:8443/cas/login; do
