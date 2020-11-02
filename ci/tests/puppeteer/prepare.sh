69102cb92d877c9ec48aeb51daad23ce1e0a29fb
Misagh Moayyed <mm1844@gmail.com>
2020-11-02 11:29:35 +0400

    update

---
+++ ci/tests/puppeteer/prepare.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Installing jq"
+sudo apt-get install jq
+
+echo "Installing Puppeteer"
+npm i --prefix ./ci/tests/puppeteer puppeteer
+
+echo "Creating overlay work directory"
+rm -Rf ./ci/tests/puppeteer/overlay
+mkdir ./ci/tests/puppeteer/overlay
+cd ./ci/tests/puppeteer/overlay
+
+echo "Fetching CAS overlay"
+curl -k https://casinit.herokuapp.com/starter.tgz | tar -xzvf -
+
+dname="${dname:-CN=cas.example.org,OU=Example,OU=Org,C=US}"
+subjectAltName="${subjectAltName:-dns:example.org,dns:localhost,ip:127.0.0.1}"
+keystore="./thekeystore"
+echo -e "\nGenerating keystore ${keystore} for CAS with DN=${dname}, SAN=${subjectAltName} ..."
+[ -f "${keystore}" ] && rm "${keystore}"
+keytool -genkey -noprompt -alias cas -keyalg RSA -keypass changeit -storepass changeit \
+  -keystore "${keystore}" -dname "${dname}" -ext SAN="${subjectAltName}"
+[ -f "${keystore}" ] && echo "Creates ${keystore}"
---
+++ ci/tests/puppeteer/run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+dependencies="$1"
+scriptPath="$2"
+
+echo -e "*************************************"
+echo -e "CAS dependencies: ${dependencies}"
+echo -e "Script path: ${scriptPath}\n"
+echo -e "*************************************\n"
+
+cd ./ci/tests/puppeteer/overlay
+ls
+
+echo -e "\nBuilding CAS found in $PWD..."
+./gradlew clean build --no-daemon -PcasModules="${dependencies}"
+
+echo -e "\nLaunching CAS..."
+java -jar build/libs/cas.war --server.ssl.key-store="${keystore}" &> /dev/null &
+pid=$!
+echo -e "\nWaiting for CAS under pid ${pid}"
+until curl -k -L --output /dev/null --silent --fail https://localhost:8443/cas/login; do
+    printf '.'
+    sleep 1
+done
+echo -e "\n\nReady!"
+
+echo -e "*************************************"
+echo -e "Running ${scriptPath}\n"
+node ${scriptPath}
+echo -e "*************************************\n"
+
+echo -e "\nKilling process ${pid} ..."
+kill -9 $pid
+
