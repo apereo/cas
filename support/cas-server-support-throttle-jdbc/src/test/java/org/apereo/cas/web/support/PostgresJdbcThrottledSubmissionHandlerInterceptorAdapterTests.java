9a111bd44ec74956b75cbf9d6ec05c138d0d6e27
Misagh Moayyed <mm1844@gmail.com>
2020-11-09 10:35:21 +0400

    allow jdbc throttles to fetch records

--- support/cas-server-support-throttle-jdbc/src/test/java/org/apereo/cas/web/support/PostgresJdbcThrottledSubmissionHandlerInterceptorAdapterTests.java
+++ support/cas-server-support-throttle-jdbc/src/test/java/org/apereo/cas/web/support/PostgresJdbcThrottledSubmissionHandlerInterceptorAdapterTests.java
@@ -24,9 +24,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 }, properties = {
     "cas.jdbc.show-sql=true",
 
-    "cas.authn.throttle.usernameParameter=username",
+    "cas.authn.throttle.username-parameter=username",
     "cas.authn.throttle.failure.code=AUTHENTICATION_FAILED",
-    "cas.authn.throttle.usernameParameter=username",
+    "cas.authn.throttle.username-parameter=username",
     "cas.authn.throttle.failure.range-seconds=5",
 
     "cas.authn.throttle.jdbc.user=postgres",
