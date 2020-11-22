4ad50fde4f6fd6258c8c70f06e1b1cf33fd27850
Misagh <mm1844@gmail.com>
2020-11-22 15:24:57 +0400

    add tests

--- support/cas-server-support-oauth-uma/src/test/java/org/apereo/cas/uma/web/controllers/policy/UmaFindPolicyForResourceSetEndpointControllerTests.java
+++ support/cas-server-support-oauth-uma/src/test/java/org/apereo/cas/uma/web/controllers/policy/UmaFindPolicyForResourceSetEndpointControllerTests.java
@@ -6,6 +6,9 @@ import org.apereo.cas.uma.web.controllers.BaseUmaEndpointControllerTests;
 import lombok.val;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
 
 import java.util.Collection;
 import java.util.Map;
@@ -27,6 +30,7 @@ public class UmaFindPolicyForResourceSetEndpointControllerTests extends BaseUmaE
         var response = umaCreateResourceSetRegistrationEndpointController.registerResourceSet(body, results.getLeft(), results.getMiddle());
 
         var model = (Map) response.getBody();
+        assertNotNull(model);
         val resourceId = (long) model.get("resourceId");
 
         body = createUmaPolicyRegistrationRequest(getCurrentProfile(results.getLeft(), results.getMiddle())).toJson();
@@ -34,12 +38,57 @@ public class UmaFindPolicyForResourceSetEndpointControllerTests extends BaseUmaE
 
         response = umaFindPolicyForResourceSetEndpointController.getPoliciesForResourceSet(resourceId, results.getLeft(), results.getMiddle());
         model = (Map) response.getBody();
+        assertNotNull(model);
         val policyId = ((Collection<ResourceSetPolicy>) model.get("entity")).iterator().next().getId();
 
         response = umaFindPolicyForResourceSetEndpointController.getPolicyForResourceSet(resourceId,
             policyId, results.getLeft(), results.getMiddle());
         model = (Map) response.getBody();
+        assertNotNull(model);
         assertTrue(model.containsKey("code"));
         assertTrue(model.containsKey("entity"));
+    }
+
+    @Test
+    public void verifyMissingPolicyOperation() throws Exception {
+        val results = authenticateUmaRequestWithProtectionScope();
+        var body = createUmaResourceRegistrationRequest().toJson();
+        var response = umaCreateResourceSetRegistrationEndpointController.registerResourceSet(body, results.getLeft(), results.getMiddle());
+
+        var model = (Map) response.getBody();
+        assertNotNull(model);
+        val resourceId = (long) model.get("resourceId");
+
+        body = createUmaPolicyRegistrationRequest(getCurrentProfile(results.getLeft(), results.getMiddle())).toJson();
+        umaCreatePolicyForResourceSetEndpointController.createPolicyForResourceSet(resourceId, body, results.getLeft(), results.getMiddle());
+
+        response = umaFindPolicyForResourceSetEndpointController.getPoliciesForResourceSet(resourceId, results.getLeft(), results.getMiddle());
+        model = (Map) response.getBody();
+        assertNotNull(model);
+        response = umaFindPolicyForResourceSetEndpointController.getPolicyForResourceSet(resourceId,
+            123456, results.getLeft(), results.getMiddle());
+        model = (Map) response.getBody();
+        assertNotNull(model);
+        assertTrue(model.containsKey("code"));
+        assertFalse(model.containsKey("entity"));
+    }
+
+    @Test
+    public void verifyMissingOperation() throws Exception {
+        val results = authenticateUmaRequestWithProtectionScope();
+        var response = umaFindPolicyForResourceSetEndpointController.getPoliciesForResourceSet(10, results.getLeft(), results.getMiddle());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        response = umaFindPolicyForResourceSetEndpointController.getPolicyForResourceSet(10, 100, results.getLeft(), results.getMiddle());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    public void verifyNoAuthOperation() throws Exception {
+        var response = umaFindPolicyForResourceSetEndpointController.getPoliciesForResourceSet(10,
+            new MockHttpServletRequest(), new MockHttpServletResponse());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        response = umaFindPolicyForResourceSetEndpointController.getPolicyForResourceSet(10, 100,
+            new MockHttpServletRequest(), new MockHttpServletResponse());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
     }
 }
