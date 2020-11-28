0bcf8cc56b95bcca964a54c4583e456376332a0a
Hal Deadman <hal.deadman@gmail.com>
2020-11-27 19:49:02 -0500

    fix entrypoint.sh

--- app/src/main/resources/overlay/jib/entrypoint.sh
+++ app/src/main/resources/overlay/jib/entrypoint.sh
@@ -4,12 +4,14 @@ ENTRYPOINT_DEBUG=${ENTRYPOINT_DEBUG:-false}
 JVM_MEM_OPTS=${JVM_MEM_OPTS:--Xms512m -Xmx2048M}
 JVM_EXTRA_OPTS=${JVM_EXTRA_OPTS:--server -noverify -XX:+TieredCompilation -XX:TieredStopAtLevel=1}
 
-if [[ $ENTRYPOINT_DEBUG == "true" ]]; then
+if [ $ENTRYPOINT_DEBUG == "true" ]; then
   echo -e "\nChecking java..."
   java -version
 
-  echo -e "\nListing CAS configuration under /etc/cas..."
-  ls -R /etc/cas
+  if [ -d /etc/cas ] ; then
+    echo -e "\nListing CAS configuration under /etc/cas..."
+    ls -R /etc/cas
+  fi
 
   echo -e "\nJava args: ${JVM_MEM_OPTS} ${JVM_EXTRA_OPTS}"
 fi
