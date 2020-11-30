b63fa4706f9b7573b814c1f880dde2cfa8661905
Misagh <mm1844@gmail.com>
2020-11-30 10:05:47 +0400

    add tests

--- ci/tests/puppeteer/scenarios/renew-existing-sso/script.js
+++ ci/tests/puppeteer/scenarios/renew-existing-sso/script.js
@@ -1,23 +1,29 @@
 const puppeteer = require('puppeteer');
 const assert = require('assert');
+const url = require('url');
 
 (async () => {
     const browser = await puppeteer.launch({
-        ignoreHTTPSErrors: true
+        ignoreHTTPSErrors: true,
+        headless: false
     });
     const page = await browser.newPage();
+    
     await page.goto("https://localhost:8443/cas/login?service=https://example.com");
-
     await page.type('#username', "casuser");
     await page.type('#password', "Mellon");
     await page.keyboard.press('Enter');
     await page.waitForNavigation();
 
-    let gauth = await page.$('#mfa-gauth');
-    assert(await gauth.boundingBox() != null);
+    const tgc = (await page.cookies()).filter(value => value.name === "TGC")
+    assert(tgc != null);
 
-    let yb = await page.$('#mfa-yubikey');
-    assert(await yb.boundingBox() != null);
+    await page.goto("https://localhost:8443/cas/login?service=https://example.com&renew=true");
+
+    // await page.waitForTimeout(1000)
+
+    let span = await page.$('#existingSsoMsg');
+    assert(await span.boundingBox() != null);
 
     await browser.close();
 })();
