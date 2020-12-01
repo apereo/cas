ff5dd7fd46cce5fa9af9b3f257b0191c74cd65ad
Misagh <mm1844@gmail.com>
2020-12-01 21:19:32 +0400

    add tests

--- ci/tests/puppeteer/scenarios/mfa-provider-unavailable/script.js
+++ ci/tests/puppeteer/scenarios/mfa-provider-unavailable/script.js
@@ -8,28 +8,20 @@ const url = require('url');
         headless: true
     });
     const page = await browser.newPage();
-    
     await page.goto("https://localhost:8443/cas/login");
-
-    var pswd = await page.$('#password');
-    assert(pswd == null);
-
     await page.type('#username', "casuser");
+    await page.type('#password', "Mellon");
     await page.keyboard.press('Enter');
     await page.waitForNavigation();
-
-    // await page.waitForTimeout(10000)
-
-    const header = await page.$eval('#login h3', el => el.innerText)
-    console.log(header)
-    assert(header === "Provide Token")
-
-    const sub = await page.$eval('#login p', el => el.innerText)
-    console.log(sub)
-    assert(sub.startsWith("Please provide the security token sent to you"));
-
-    let span = await page.$('#token');
-    assert(await span.boundingBox() != null);
     
+    const header = await page.$eval('#content h2', el => el.innerText)
+    console.log(header)
+    assert(header === "MFA Provider Unavailable")
+
+    const sub = await page.$eval('#content p', el => el.innerText)
+    console.log(sub)
+    assert(sub.startsWith("CAS was unable to reach your configured MFA provider at this time."))
+
+
     await browser.close();
 })();
