1fd14cf3d51612ac3ab44b55d76a1d542b8d87aa
NgSekLong <longsek@gmail.com>
2020-12-04 02:12:08 +0800

    Fix Hazelcast Cluster shutdown when configuration is reloaded (#5002)

--- support/cas-server-support-hazelcast-ticket-registry/src/main/java/org/apereo/cas/config/HazelcastTicketRegistryConfiguration.java
+++ support/cas-server-support-hazelcast-ticket-registry/src/main/java/org/apereo/cas/config/HazelcastTicketRegistryConfiguration.java
@@ -19,7 +19,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -49,7 +48,6 @@ public class HazelcastTicketRegistryConfiguration {
     private ObjectProvider<TicketCatalog> ticketCatalog;
 
     @Bean
-    @RefreshScope
     public TicketRegistry ticketRegistry() {
         val hz = casProperties.getTicket().getRegistry().getHazelcast();
         val hazelcastInstance = casTicketRegistryHazelcastInstance();
