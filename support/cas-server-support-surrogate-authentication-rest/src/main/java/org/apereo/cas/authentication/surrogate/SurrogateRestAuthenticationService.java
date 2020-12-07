f9231e97299086a93939f67e1ee756bde3414222
Misagh <mm1844@gmail.com>
2020-12-07 11:05:03 +0400

    add tests

--- support/cas-server-support-surrogate-authentication-rest/src/main/java/org/apereo/cas/authentication/surrogate/SurrogateRestAuthenticationService.java
+++ support/cas-server-support-surrogate-authentication-rest/src/main/java/org/apereo/cas/authentication/surrogate/SurrogateRestAuthenticationService.java
@@ -2,7 +2,7 @@ package org.apereo.cas.authentication.surrogate;
 
 import org.apereo.cas.authentication.principal.Principal;
 import org.apereo.cas.authentication.principal.Service;
-import org.apereo.cas.configuration.model.support.surrogate.SurrogateAuthenticationProperties;
+import org.apereo.cas.configuration.model.support.surrogate.SurrogateRestfulAuthenticationProperties;
 import org.apereo.cas.services.ServicesManager;
 import org.apereo.cas.util.CollectionUtils;
 import org.apereo.cas.util.HttpUtils;
@@ -38,9 +38,9 @@ public class SurrogateRestAuthenticationService extends BaseSurrogateAuthenticat
         .configure(DeserializationFeature.READ_ENUMS_USING_TO_STRING, false)
         .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
-    private final SurrogateAuthenticationProperties.Rest properties;
+    private final SurrogateRestfulAuthenticationProperties properties;
 
-    public SurrogateRestAuthenticationService(final SurrogateAuthenticationProperties.Rest properties,
+    public SurrogateRestAuthenticationService(final SurrogateRestfulAuthenticationProperties properties,
                                               final ServicesManager servicesManager) {
         super(servicesManager);
         this.properties = properties;
