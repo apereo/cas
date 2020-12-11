8e3d4594037158d2a7b5629d3f173ccb2659e149
Jérôme LELEU <leleuj@gmail.com>
2020-12-11 16:22:18 +0100

    fix checktyle

--- api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/core/util/ClientCertificateProperties.java
+++ api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/core/util/ClientCertificateProperties.java
@@ -1,10 +1,11 @@
 package org.apereo.cas.configuration.model.core.util;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.apereo.cas.configuration.model.SpringResourceProperties;
 import org.apereo.cas.configuration.support.RequiredProperty;
 import org.apereo.cas.configuration.support.RequiresModule;
+
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
 
