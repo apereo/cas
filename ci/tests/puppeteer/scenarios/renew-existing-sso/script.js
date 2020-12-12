4d4f140a10617e950370d7bd05652eb2cbcd4e93
Misagh <mm1844@gmail.com>
2020-12-13 01:23:47 +0400

    fix tests

--- ci/tests/puppeteer/scenarios/renew-existing-sso/script.js
+++ ci/tests/puppeteer/scenarios/renew-existing-sso/script.js
@@ -15,11 +15,13 @@ const url = require('url');
     await page.keyboard.press('Enter');
     await page.waitForNavigation();
 
+    await page.goto("https://localhost:8443/cas/login");
+    await page.waitForTimeout(1000)
     const tgc = (await page.cookies()).filter(value => value.name === "TGC")
     assert(tgc.length !== 0);
 
     await page.goto("https://localhost:8443/cas/login?service=https://example.com&renew=true");
-    await page.waitForTimeout(2000)
+    await page.waitForTimeout(1000)
 
     let span = await page.$('#existingSsoMsg');
     assert(await span.boundingBox() != null);
