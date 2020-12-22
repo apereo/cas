b862307844534065575833c12abf43db99a74326
Misagh <mm1844@gmail.com>
2020-12-22 13:47:27 +0400

    fix build

--- support/cas-server-support-webauthn-rest/src/main/java/org/apereo/cas/config/RestfulWebAuthnConfiguration.java
+++ support/cas-server-support-webauthn-rest/src/main/java/org/apereo/cas/config/RestfulWebAuthnConfiguration.java
@@ -19,7 +19,7 @@ import org.springframework.context.annotation.Configuration;
  * @author Misagh Moayyed
  * @since 6.4.0
  */
-@Configuration("RestfulWebAuthnConfiguration")
+@Configuration(value = "RestfulWebAuthnConfiguration", proxyBeanMethods = false)
 @EnableConfigurationProperties(CasConfigurationProperties.class)
 public class RestfulWebAuthnConfiguration {
 
