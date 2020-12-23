7fcde8695c3da9db4e4a9fbd6ed8015fbcb122dc
Hal Deadman <hal.deadman@gmail.com>
2020-12-23 08:52:30 -0500

    remove MockWebServer usage from a test failing on windows

--- support/cas-server-support-u2f/src/test/java/org/apereo/cas/adaptors/u2f/storage/U2FRestResourceDeviceRepositoryTests.java
+++ support/cas-server-support-u2f/src/test/java/org/apereo/cas/adaptors/u2f/storage/U2FRestResourceDeviceRepositoryTests.java
@@ -2,25 +2,31 @@ package org.apereo.cas.adaptors.u2f.storage;
 
 import org.apereo.cas.config.U2FConfiguration;
 import org.apereo.cas.configuration.CasConfigurationProperties;
-import org.apereo.cas.util.MockWebServer;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 import lombok.SneakyThrows;
 import lombok.Synchronized;
 import lombok.val;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -32,46 +38,38 @@ import static org.junit.jupiter.api.Assertions.*;
  * This is {@link U2FRestResourceDeviceRepositoryTests}.
  *
  * @author Misagh Moayyed
+ * @author Hal Deadman
  * @since 5.3.0
  */
 @Tag("RestfulApi")
 @SpringBootTest(classes = {
-    U2FConfiguration.class,
-    AopAutoConfiguration.class,
-    RefreshAutoConfiguration.class
-}, properties = "cas.authn.mfa.u2f.rest.url=http://localhost:9190")
+        U2FRestResourceDeviceRepositoryTests.RestfulServiceRegistryTestConfiguration.class,
+        U2FConfiguration.class,
+        AopAutoConfiguration.class
+}, properties = {
+        "server.port=9190",
+        "cas.authn.mfa.u2f.rest.url=http://localhost:9190"
+}, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @Getter
 @EnableConfigurationProperties(CasConfigurationProperties.class)
+@EnableAutoConfiguration
 public class U2FRestResourceDeviceRepositoryTests extends AbstractU2FDeviceRepositoryTests {
-    private static final ObjectMapper MAPPER = new ObjectMapper()
-        .enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY)
-        .findAndRegisterModules();
 
-    private static MockWebServer WEB_SERVER;
-    
-    private static List<U2FDeviceRegistration> DEVICES = new ArrayList<>();
+    private static final ObjectMapper MAPPER = new ObjectMapper()
+            .enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY)
+            .findAndRegisterModules();
+
+    private static ArrayList<U2FDeviceRegistration> DEVICES = new ArrayList<>();
 
     @Autowired
     @Qualifier("u2fDeviceRepository")
     private U2FDeviceRepository deviceRepository;
-
-    @BeforeAll
-    public static void beforeClass() {
-        WEB_SERVER = new MockWebServer(9190);
-        WEB_SERVER.start();
-    }
 
     @Override
     @BeforeEach
     @Synchronized
     public void setUp() {
         DEVICES.clear();
-        configureResponse();
-    }
-
-    @AfterAll
-    public static void afterClass() {
-        WEB_SERVER.close();
     }
 
     @Test
@@ -85,24 +83,41 @@ public class U2FRestResourceDeviceRepositoryTests extends AbstractU2FDeviceRepos
     protected List<U2FDeviceRegistration> prepareDevices(final U2FDeviceRepository deviceRepository) {
         val devices = super.prepareDevices(deviceRepository);
         DEVICES.addAll(devices);
-        configureResponse();
         return devices;
     }
 
-    @SneakyThrows
-    @Synchronized
-    private static void configureResponse() {
-        val results = new HashMap<String, List<U2FDeviceRegistration>>();
-        results.put(BaseResourceU2FDeviceRepository.MAP_KEY_DEVICES, DEVICES);
-        WEB_SERVER.responseBody(MAPPER.writeValueAsString(results));
-    }
+    @TestConfiguration
+    @Lazy(false)
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS)
+    public static class RestfulServiceRegistryTestConfiguration {
 
-    @Override
-    @SneakyThrows
-    @Synchronized
-    protected void deleteDevice(final U2FDeviceRepository deviceRepository, final U2FDeviceRegistration device) {
-        DEVICES.removeIf(d -> d.getRecord().equals(device.getRecord()));
-        configureResponse();
-        super.deleteDevice(deviceRepository, device);
+        @RestController("deviceRepositoryController")
+        @RequestMapping("/")
+        public class DeviceRespositoryController {
+
+            @DeleteMapping
+            public void removeAll() {
+                DEVICES.clear();
+            }
+
+            @PostMapping
+            public void writeDevicesBackToResource(final ArrayList<U2FDeviceRegistration> devices) {
+                DEVICES.addAll(devices);
+            }
+
+            @DeleteMapping("/{id}")
+            public void deleteRegisteredDevice(@PathVariable(name = "id") final String id) {
+                DEVICES.removeIf(d -> String.valueOf(d.getId()).equals(id));
+            }
+
+            @SneakyThrows
+            @GetMapping
+            public String readDevicesFromResource() {
+                val results = new HashMap<String, ArrayList<U2FDeviceRegistration>>();
+                results.put(BaseResourceU2FDeviceRepository.MAP_KEY_DEVICES, DEVICES);
+                return MAPPER.writeValueAsString(results);
+            }
+        }
     }
 }
