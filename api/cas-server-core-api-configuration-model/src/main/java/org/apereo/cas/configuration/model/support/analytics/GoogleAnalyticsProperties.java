e48868e1276d503cf86b6c366725960d64199dec
Misagh <mm1844@gmail.com>
2021-01-03 21:27:57 +0400

    clean up props

--- api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/support/analytics/GoogleAnalyticsProperties.java
+++ api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/support/analytics/GoogleAnalyticsProperties.java
@@ -1,12 +1,12 @@
 package org.apereo.cas.configuration.model.support.analytics;
 
-import org.apereo.cas.configuration.model.support.cookie.CookieProperties;
 import org.apereo.cas.configuration.support.RequiresModule;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
 
 import java.io.Serializable;
 
@@ -34,33 +34,7 @@ public class GoogleAnalyticsProperties implements Serializable {
     /**
      * Cookie settings to be used with google analytics.
      */
-    private Cookie cookie = new Cookie();
+    @NestedConfigurationProperty
+    private GoogleAnalyticsCookieProperties cookie = new GoogleAnalyticsCookieProperties();
 
-    @RequiresModule(name = "cas-server-support-google-analytics", automated = true)
-    @Getter
-    @Setter
-    @Accessors(chain = true)
-    public static class Cookie extends CookieProperties {
-        private static final long serialVersionUID = -5432498833437602657L;
-
-        /**
-         * Attribute name to collect
-         * from the authentication event
-         * to serve as the cookie value.
-         */
-        private String attributeName;
-
-        /**
-         * A regular expression pattern
-         * that is tested against attribute values
-         * to only release and allow those that produce
-         * a successful match.
-         */
-        private String attributeValuePattern = ".+";
-
-        public Cookie() {
-            setName("CasGoogleAnalytics");
-            setPinToSession(false);
-        }
-    }
 }
