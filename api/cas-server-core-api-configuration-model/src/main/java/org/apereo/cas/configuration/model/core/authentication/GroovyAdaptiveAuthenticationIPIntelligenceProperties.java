8176c0faeb57486fff03d7247a0c69a3f21b9e75
Misagh <mm1844@gmail.com>
2021-01-03 21:27:57 +0400

    clean up props

---
+++ api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/core/authentication/GroovyAdaptiveAuthenticationIPIntelligenceProperties.java
@@ -0,0 +1,22 @@
+package org.apereo.cas.configuration.model.core.authentication;
+
+import org.apereo.cas.configuration.model.SpringResourceProperties;
+import org.apereo.cas.configuration.support.RequiresModule;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+/**
+ * This is {@link GroovyAdaptiveAuthenticationIPIntelligenceProperties}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.4.0
+ */
+@RequiresModule(name = "cas-server-core-authentication", automated = true)
+@Getter
+@Setter
+@Accessors(chain = true)
+public class GroovyAdaptiveAuthenticationIPIntelligenceProperties extends SpringResourceProperties {
+    private static final long serialVersionUID = 8079027843747126083L;
+}
