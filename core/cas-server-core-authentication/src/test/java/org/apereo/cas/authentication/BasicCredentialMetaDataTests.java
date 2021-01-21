12ae13f9f785d39a221bb7934328106377a534dc
Misagh <mm1844@gmail.com>
2021-01-21 13:16:42 +0400

    Clean up manual object-mapper refs

--- core/cas-server-core-authentication/src/test/java/org/apereo/cas/authentication/BasicCredentialMetaDataTests.java
+++ core/cas-server-core-authentication/src/test/java/org/apereo/cas/authentication/BasicCredentialMetaDataTests.java
@@ -2,6 +2,7 @@ package org.apereo.cas.authentication;
 
 import org.apereo.cas.authentication.credential.UsernamePasswordCredential;
 import org.apereo.cas.authentication.metadata.BasicCredentialMetaData;
+import org.apereo.cas.util.serialization.JacksonObjectMapperFactory;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.val;
