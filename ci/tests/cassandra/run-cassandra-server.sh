206357f48dbb7d3c2f27ab808cd5cd420a0eb3e7
Misagh <mm1844@gmail.com>
2021-02-05 17:47:22 +0400

    fix cassandra startup script

--- ci/tests/cassandra/run-cassandra-server.sh
+++ ci/tests/cassandra/run-cassandra-server.sh
@@ -16,17 +16,29 @@ else
 fi
 
 echo "Waiting for Cassandra server to come online..."
-sleep 40
+sleep 5
+cmd="curl --output /dev/null --silent -I http://localhost:9042"
+retVal=0
+while [ $retVal -ne 8 ]
+do
+  printf '.'
+  sleep 1
+  eval $cmd
+  retVal=$?
+done
 
 echo "Creating Cassandra keyspace: cas"
 docker exec cassandra cqlsh -e "CREATE KEYSPACE cas WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor' : 1}"
+sleep 2
 
 echo "Creating Cassandra users table"
 docker exec cassandra cqlsh -e "CREATE TABLE cas.users_table ( id UUID PRIMARY KEY, user_attr text, pwd_attr text )"
+sleep 2
 
 echo "Creating Cassandra services table"
 docker exec cassandra cqlsh -e "CREATE TABLE cas.casservices ( id bigint PRIMARY KEY, data text )"
+sleep 2
 
 echo "Creating Cassandra user record"
 docker exec cassandra cqlsh -e "INSERT INTO cas.users_table (id,user_attr,pwd_attr) VALUES (6ab09bec-e68e-48d9-a5f8-97e6fb4c9b47, 'casuser','Mellon') USING TTL 86400 AND TIMESTAMP 123456789;"
-
+sleep 2
