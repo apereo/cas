376a625788c37cf774d967a1be22b8637dad8413
Hal Deadman <hdeadman@users.noreply.github.com>
2021-02-11 00:53:24 -0500

    allow redis modules to be disabled via properties (#5044)

--- support/cas-server-support-throttle-redis/src/main/java/org/apereo/cas/config/CasRedisThrottlingConfiguration.java
+++ support/cas-server-support-throttle-redis/src/main/java/org/apereo/cas/config/CasRedisThrottlingConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
@@ -29,6 +30,7 @@ import org.springframework.data.redis.core.RedisTemplate;
  */
 @Configuration("casRedisThrottlingConfiguration")
 @EnableConfigurationProperties(CasConfigurationProperties.class)
+@ConditionalOnProperty(prefix = "cas.audit.redis", name = "enabled", havingValue = "true", matchIfMissing = true)
 public class CasRedisThrottlingConfiguration {
 
     @Autowired
