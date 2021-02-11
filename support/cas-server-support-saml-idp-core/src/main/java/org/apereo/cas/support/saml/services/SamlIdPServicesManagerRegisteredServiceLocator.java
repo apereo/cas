957d5bbad6e7e401bded7e0e329bf2285d48a3b7
Misagh <mm1844@gmail.com>
2021-02-11 11:39:59 +0400

    improve saml service detection in registry

--- support/cas-server-support-saml-idp-core/src/main/java/org/apereo/cas/support/saml/services/SamlIdPServicesManagerRegisteredServiceLocator.java
+++ support/cas-server-support-saml-idp-core/src/main/java/org/apereo/cas/support/saml/services/SamlIdPServicesManagerRegisteredServiceLocator.java
@@ -1,12 +1,18 @@
 package org.apereo.cas.support.saml.services;
 
+import org.apereo.cas.authentication.principal.Service;
 import org.apereo.cas.services.DefaultServicesManagerRegisteredServiceLocator;
+import org.apereo.cas.services.RegisteredService;
+import org.apereo.cas.support.saml.SamlIdPConstants;
 import org.apereo.cas.support.saml.SamlProtocolConstants;
 import org.apereo.cas.support.saml.services.idp.metadata.SamlRegisteredServiceServiceProviderMetadataFacade;
 import org.apereo.cas.support.saml.services.idp.metadata.cache.SamlRegisteredServiceCachingMetadataResolver;
 
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.core.Ordered;
+
+import java.util.Set;
 
 /**
  * This is {@link SamlIdPServicesManagerRegisteredServiceLocator}.
@@ -14,13 +20,17 @@ import org.springframework.core.Ordered;
  * @author Misagh Moayyed
  * @since 6.3.0
  */
+@Slf4j
 public class SamlIdPServicesManagerRegisteredServiceLocator extends DefaultServicesManagerRegisteredServiceLocator {
+    private static final Set<String> SAML_PROTOCOL_PARAMETERS = Set.of(
+        SamlProtocolConstants.PARAMETER_SAML_REQUEST,
+        SamlProtocolConstants.PARAMETER_ENTITY_ID,
+        SamlIdPConstants.PROVIDER_ID);
+
     public SamlIdPServicesManagerRegisteredServiceLocator(final SamlRegisteredServiceCachingMetadataResolver resolver) {
         setOrder(Ordered.HIGHEST_PRECEDENCE);
         setRegisteredServiceFilter((registeredService, service) -> {
-            val isSamlServiceProvider = registeredService.getClass().equals(SamlRegisteredService.class)
-                && service.getAttributes().containsKey(SamlProtocolConstants.PARAMETER_SAML_REQUEST);
-            
+            val isSamlServiceProvider = isSamlRegisteredService(registeredService, service);
             if (isSamlServiceProvider && registeredService.matches(service.getId())) {
                 val samlService = SamlRegisteredService.class.cast(registeredService);
                 val adaptor = SamlRegisteredServiceServiceProviderMetadataFacade.get(resolver, samlService, service.getId());
@@ -28,5 +38,24 @@ public class SamlIdPServicesManagerRegisteredServiceLocator extends DefaultServi
             }
             return false;
         });
+    }
+
+    /**
+     * Is saml registered service?
+     *
+     * @param registeredService the registered service
+     * @param service           the service
+     * @return the boolean
+     */
+    protected boolean isSamlRegisteredService(final RegisteredService registeredService, final Service service) {
+        if (registeredService instanceof SamlRegisteredService) {
+            val attributes = service.getAttributes();
+            LOGGER.trace("Reviewing service attributes [{}] for service [{}]", attributes, service.getId());
+
+            return SAML_PROTOCOL_PARAMETERS
+                .stream()
+                .anyMatch(attributes::containsKey);
+        }
+        return false;
     }
 }
