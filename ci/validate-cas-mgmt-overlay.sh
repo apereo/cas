6bcdf20503cbcfe50ff1234b9027bb7ec10fe081
Misagh <mm1844@gmail.com>
2021-02-14 17:45:33 +0400

    add cas mgmt overlay

--- ci/validate-cas-mgmt-overlay.sh
+++ ci/validate-cas-mgmt-overlay.sh
@@ -2,23 +2,17 @@
 
 java -jar app/build/libs/app.jar &
 pid=$!
-sleep 30
+sleep 15
+rm -Rf tmp &> /dev/null
 mkdir tmp
 cd tmp
 curl http://localhost:8080/starter.tgz -d type=cas-mgmt-overlay | tar -xzvf -
 kill -9 $pid
 
-echo "Running CAS Overlay with bootRun"
+echo "Building CAS Mgmt Overlay"
 ./gradlew clean build --no-daemon
-java -jar build/libs/cas-management.war -Dserver.ssl.enabled=false -Dserver.port=8092 &> /dev/null &
-pid=$!
-echo "Launched CAS with pid ${pid} using bootRun. Waiting for CAS server to come online..."
-until curl -k -L --output /dev/null --silent --fail http://localhost:8092/cas-management; do
-    echo -n '.'
-    sleep 5
-done
-echo -e "\n\nReady!"
-kill -9 $pid
+
+chmod +x *.sh
 
 echo "Build Container Image w/ Docker"
 ./docker-build.sh
