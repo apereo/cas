519b97a435a673f9a19836edb07822cd588ce9d4
Misagh <mm1844@gmail.com>
2021-02-16 10:26:25 +0400

    fix ci

--- ci/validate-cas-overlay.sh
+++ ci/validate-cas-overlay.sh
@@ -28,36 +28,36 @@ else
     exit 1
 fi
 
-#echo "Running CAS Overlay with bootRun"
-#./gradlew clean build bootRun -Dserver.ssl.enabled=false -Dserver.port=8090 &
-#pid=$!
-#sleep 15
-#echo "Launched CAS with pid ${pid} using bootRun. Waiting for CAS server to come online..."
-#until curl -k -L --output /dev/null --silent --fail http://localhost:8090/cas/login; do
-#    echo -n '.'
-#    sleep 5
-#done
-#echo -e "\n\nReady!"
-#kill -9 $pid
-#
-#echo "Building Docker image with Jib"
-#chmod -R 777 ./*.sh
-#./gradlew jibDockerBuild
-#
-#echo "Downloading Shell"
-#./gradlew downloadShell
-#
-#echo "Listing Views"
-#./gradlew listTemplateViews
-#
-#echo "Configuration Metadata Export"
-#./gradlew exportConfigMetadata
-#
-#echo "Exploding WAR"
-#./gradlew explodeWar
-#
-#echo "Build Container Image w/ Docker"
-#./docker-build.sh
-#
-#echo "Build Container Image w/ Docker Compose"
-#docker-compose build
+echo "Running CAS Overlay with bootRun"
+./gradlew clean build bootRun -Dserver.ssl.enabled=false -Dserver.port=8090 &
+pid=$!
+sleep 15
+echo "Launched CAS with pid ${pid} using bootRun. Waiting for CAS server to come online..."
+until curl -k -L --output /dev/null --silent --fail http://localhost:8090/cas/login; do
+   echo -n '.'
+   sleep 5
+done
+echo -e "\n\nReady!"
+kill -9 $pid
+
+echo "Building Docker image with Jib"
+chmod -R 777 ./*.sh
+./gradlew jibDockerBuild
+
+echo "Downloading Shell"
+./gradlew downloadShell
+
+echo "Listing Views"
+./gradlew listTemplateViews
+
+echo "Configuration Metadata Export"
+./gradlew exportConfigMetadata
+
+echo "Exploding WAR"
+./gradlew explodeWar
+
+echo "Build Container Image w/ Docker"
+./docker-build.sh
+
+echo "Build Container Image w/ Docker Compose"
+docker-compose build
