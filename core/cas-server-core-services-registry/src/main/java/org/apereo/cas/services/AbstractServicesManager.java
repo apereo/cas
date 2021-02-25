0040135ce04a5125549beebe40234a9efab5ce0b
Misagh <mm1844@gmail.com>
2021-02-25 20:55:23 +0400

    switch to assignable classes for svc mgr

--- core/cas-server-core-services-registry/src/main/java/org/apereo/cas/services/AbstractServicesManager.java
+++ core/cas-server-core-services-registry/src/main/java/org/apereo/cas/services/AbstractServicesManager.java
@@ -136,7 +136,7 @@ public abstract class AbstractServicesManager implements ServicesManager {
             return null;
         }
         val service = findServiceBy(requestedService);
-        if (service != null && service.getClass().equals(clazz)) {
+        if (service != null && clazz.isAssignableFrom(service.getClass())) {
             return (T) service;
         }
         return null;
@@ -151,7 +151,7 @@ public abstract class AbstractServicesManager implements ServicesManager {
     @Override
     public <T extends RegisteredService> T findServiceBy(final long id, final Class<T> clazz) {
         var service = getService(registeredService -> registeredService.getId() == id);
-        if (service != null && service.getClass().equals(clazz)) {
+        if (service != null && clazz.isAssignableFrom(service.getClass())) {
             return (T) service;
         }
         LOGGER.trace("The service with id [{}] and type [{}] is not found in the cache; trying to find it from [{}]",
@@ -189,7 +189,7 @@ public abstract class AbstractServicesManager implements ServicesManager {
             return null;
         }
         var service = getService(registeredService -> registeredService.getName().equals(name));
-        if (service != null && service.getClass().equals(clazz)) {
+        if (service != null && clazz.isAssignableFrom(service.getClass())) {
             return (T) service;
         }
         LOGGER.trace("The service with name [{}] and type [{}] is not found in the cache; trying to find it from [{}]",
