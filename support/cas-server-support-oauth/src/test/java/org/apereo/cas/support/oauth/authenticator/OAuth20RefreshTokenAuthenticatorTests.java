470fa9f4792103e376c67b2326ebb7f0198439f1
Misagh <misagh.moayyed@gmail.com>
2021-03-22 19:43:38 +0400

    fix tests

--- support/cas-server-support-oauth/src/test/java/org/apereo/cas/support/oauth/authenticator/OAuth20RefreshTokenAuthenticatorTests.java
+++ support/cas-server-support-oauth/src/test/java/org/apereo/cas/support/oauth/authenticator/OAuth20RefreshTokenAuthenticatorTests.java
@@ -9,13 +9,13 @@ import lombok.val;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junitpioneer.jupiter.RetryingTest;
 import org.pac4j.core.context.JEEContext;
 import org.pac4j.core.context.session.JEESessionStore;
 import org.pac4j.core.credentials.UsernamePasswordCredentials;
 import org.pac4j.core.exception.CredentialsException;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.retry.annotation.Retryable;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -38,7 +38,7 @@ public class OAuth20RefreshTokenAuthenticatorTests extends BaseOAuth20Authentica
     }
 
     @Test
-    @Retryable(value = Error.class)
+    @RetryingTest(3)
     public void verifyAuthentication() {
         val refreshToken = getRefreshToken(serviceWithoutSecret);
         ticketRegistry.addTicket(refreshToken);
