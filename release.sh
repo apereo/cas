a52ca0750857b02701ad10b302a72f3bd03d64de
Misagh <misagh.moayyed@gmail.com>
2021-03-23 14:37:12 +0400

    fix build after gradle 7

--- release.sh
+++ release.sh
@@ -8,12 +8,12 @@ TIMEOUT=640000
 function build {
     ./gradlew clean --parallel
     echo -e "\n${GREEN}Building CAS. Please be patient as this might take a while...${NORMAL}\n"
-    ./gradlew assemble -x test -x check -DpublishReleases=true -DsonatypeUsername="$1" -DsonatypePassword="$2"
+    ./gradlew assemble -x test -x check --no-watch-fs -DpublishReleases=true -DsonatypeUsername="$1" -DsonatypePassword="$2"
 }
 
 function publish {
     echo -e "\n${GREEN}Publishing CAS. Please be patient as this might take a while...${NORMAL}\n"
-    ./gradlew publish closeAndReleaseRepository -DpublishReleases=true -DsonatypeUsername="$1" -DsonatypePassword="$2" \
+    ./gradlew publish closeAndReleaseRepository --no-watch-fs -DpublishReleases=true -DsonatypeUsername="$1" -DsonatypePassword="$2" \
     -Dorg.gradle.internal.http.socketTimeout="${TIMEOUT}" -Dorg.gradle.internal.http.connectionTimeout="${TIMEOUT}"  \
     -Dorg.gradle.internal.publish.checksums.insecure=true
 }
