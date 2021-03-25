a6905fa648a9d90d924b8b0d0bf2167627cc9fb4
Misagh <misagh.moayyed@gmail.com>
2021-03-25 11:45:18 +0400

    allow ip ranges for cookie pinning

--- support/cas-server-support-duo-core/src/main/java/com/duosecurity/duoweb/DuoWebException.java
+++ support/cas-server-support-duo-core/src/main/java/com/duosecurity/duoweb/DuoWebException.java
@@ -2,7 +2,9 @@ package com.duosecurity.duoweb;
 
 public class DuoWebException extends Exception {
 
-  public DuoWebException(String message) {
+    private static final long serialVersionUID = -6943858564875112962L;
+
+    public DuoWebException(String message) {
     super(message);
   }
 }
