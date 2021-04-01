c5083ae07c1c31d6d01cd4e1394c5d239df11943
Misagh <misagh.moayyed@gmail.com>
2021-04-01 11:35:31 +0400

    add tests for password flows

--- core/cas-server-core-webflow-api/src/test/java/org/apereo/cas/web/flow/actions/ConsumerExecutionActionTests.java
+++ core/cas-server-core-webflow-api/src/test/java/org/apereo/cas/web/flow/actions/ConsumerExecutionActionTests.java
@@ -1,7 +1,5 @@
 package org.apereo.cas.web.flow.actions;
 
-import org.apereo.cas.web.flow.CasWebflowConstants;
-
 import lombok.val;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -10,19 +8,20 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.webflow.context.ExternalContextHolder;
 import org.springframework.webflow.context.servlet.ServletExternalContext;
+import org.springframework.webflow.execution.RequestContext;
 import org.springframework.webflow.execution.RequestContextHolder;
 import org.springframework.webflow.test.MockRequestContext;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * This is {@link StaticEventExecutionActionTests}.
+ * This is {@link ConsumerExecutionActionTests}.
  *
  * @author Misagh Moayyed
- * @since 6.3.0
+ * @since 6.4.0
  */
 @Tag("WebflowActions")
-public class StaticEventExecutionActionTests {
+public class ConsumerExecutionActionTests {
     @Test
     public void verifyOperation() throws Exception {
         val context = new MockRequestContext();
@@ -31,11 +30,9 @@ public class StaticEventExecutionActionTests {
         context.setExternalContext(new ServletExternalContext(new MockServletContext(), request, response));
         RequestContextHolder.setRequestContext(context);
         ExternalContextHolder.setExternalContext(context.getExternalContext());
-        val action = new StaticEventExecutionAction("custom");
-        assertEquals("custom", action.execute(context).getId());
 
-        assertEquals(CasWebflowConstants.TRANSITION_ID_SUCCESS,
-            StaticEventExecutionAction.SUCCESS.execute(context).getId());
+        val action = new ConsumerExecutionAction(RequestContext::getFlowScope);
+        action.setEventId("result");
+        assertNotNull(action.execute(context));
     }
-
 }
