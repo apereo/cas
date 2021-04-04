380e0ad4160c125453d6480a111602afe5e6838d
Misagh <misagh.moayyed@gmail.com>
2021-04-04 12:31:49 +0400

    force cookie tracking mode for all servlet initializers

--- webapp/cas-server-webapp-init-eureka-server/src/main/java/org/apereo/cas/CasEurekaServerServletInitializer.java
+++ webapp/cas-server-webapp-init-eureka-server/src/main/java/org/apereo/cas/CasEurekaServerServletInitializer.java
@@ -3,6 +3,11 @@ package org.apereo.cas;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.SessionTrackingMode;
+import java.util.Set;
+
 /**
  * This is {@link CasEurekaServerServletInitializer}.
  *
@@ -10,6 +15,11 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  * @since 5.1.0
  */
 public class CasEurekaServerServletInitializer extends SpringBootServletInitializer {
+    @Override
+    public void onStartup(final ServletContext servletContext) throws ServletException {
+        servletContext.setSessionTrackingModes(Set.of(SessionTrackingMode.COOKIE));
+        super.onStartup(servletContext);
+    }
 
     @Override
     public SpringApplicationBuilder configure(final SpringApplicationBuilder builder) {
