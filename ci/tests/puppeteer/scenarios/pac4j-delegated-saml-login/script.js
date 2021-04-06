8ab3874c7c55608480da568c7535eb0af32b3906
Misagh <misagh.moayyed@gmail.com>
2021-04-06 23:19:12 +0400

    add puppeteer test for interrupt+authn warnings

--- ci/tests/puppeteer/scenarios/pac4j-delegated-saml-login/script.js
+++ ci/tests/puppeteer/scenarios/pac4j-delegated-saml-login/script.js
@@ -57,7 +57,7 @@ const path = require('path');
     console.log(title)
     assert(title === "CAS - Central Authentication Service")
 
-    const header = await page.$eval('#content div h2', el => el.innerText)
+    const header = await page.$eval('#content div h2', el => el.innerText.trim())
     console.log(header)
     assert(header === "Log In Successful")
     
