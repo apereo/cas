c982ae7b4f3910cba7fb21ac63c32f60f2df422f
Wilber Saca <wismar25@hotmail.com>
2021-04-09 09:10:45 -0500

    Fix tests to use the concrete class.

--- support/cas-server-support-oauth/src/test/java/org/apereo/cas/support/oauth/util/OAuth20UtilsTests.java
+++ support/cas-server-support-oauth/src/test/java/org/apereo/cas/support/oauth/util/OAuth20UtilsTests.java
@@ -2,7 +2,6 @@ package org.apereo.cas.support.oauth.util;
 
 import org.apereo.cas.CasProtocolConstants;
 import org.apereo.cas.services.FullRegexRegisteredServiceMatchingStrategy;
-import org.apereo.cas.services.RegisteredService;
 import org.apereo.cas.services.RegisteredServiceTestUtils;
 import org.apereo.cas.services.ServicesManager;
 import org.apereo.cas.support.oauth.OAuth20Constants;
@@ -85,11 +84,11 @@ public class OAuth20UtilsTests {
 
     @Test
     public void verifyCheckCallbackValid() {
-        val registeredService = mock(RegisteredService.class);
-        when(registeredService.getServiceId()).thenReturn("http://test.org/.*");
-        when(registeredService.getMatchingStrategy()).thenReturn(null);
+        val registeredService = new OAuthRegisteredService();
+        registeredService.setServiceId("http://test.org/.*");
+        registeredService.setMatchingStrategy(null);
         assertFalse(OAuth20Utils.checkCallbackValid(registeredService, "http://test.org/cas"));
-        when(registeredService.getMatchingStrategy()).thenReturn(new FullRegexRegisteredServiceMatchingStrategy());
+        registeredService.setMatchingStrategy(new FullRegexRegisteredServiceMatchingStrategy());
         assertTrue(OAuth20Utils.checkCallbackValid(registeredService, "http://test.org/cas"));
         assertFalse(OAuth20Utils.checkCallbackValid(registeredService, "http://test2.org/cas"));
     }
