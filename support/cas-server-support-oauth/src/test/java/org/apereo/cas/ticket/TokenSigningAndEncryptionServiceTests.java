a30cd93a3e46c1f13f44cf2fb9c737f2f97d6025
Misagh <misagh.moayyed@gmail.com>
2021-04-09 11:26:55 +0400

    additional tests

--- support/cas-server-support-oauth/src/test/java/org/apereo/cas/ticket/TokenSigningAndEncryptionServiceTests.java
+++ support/cas-server-support-oauth/src/test/java/org/apereo/cas/ticket/TokenSigningAndEncryptionServiceTests.java
@@ -12,7 +12,6 @@ import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 /**
