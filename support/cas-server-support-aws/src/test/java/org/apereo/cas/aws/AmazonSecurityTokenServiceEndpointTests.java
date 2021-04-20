7450c88332feb598c8cddc7c63205134ddcf26b8
Misagh <misagh.moayyed@gmail.com>
2021-04-20 22:43:20 +0400

    fix mfa permissions for aws-sts endpoint

--- support/cas-server-support-aws/src/test/java/org/apereo/cas/aws/AmazonSecurityTokenServiceEndpointTests.java
+++ support/cas-server-support-aws/src/test/java/org/apereo/cas/aws/AmazonSecurityTokenServiceEndpointTests.java
@@ -1,14 +1,21 @@
 package org.apereo.cas.aws;
 
+import org.apereo.cas.authentication.mfa.TestMultifactorAuthenticationProvider;
 import org.apereo.cas.config.AmazonCoreConfiguration;
+import org.apereo.cas.config.CasAuthenticationEventExecutionPlanTestConfiguration;
+import org.apereo.cas.rest.config.CasCoreRestConfiguration;
+import org.apereo.cas.util.spring.ApplicationContextProvider;
 import org.apereo.cas.web.report.AbstractCasEndpointTests;
 
 import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.util.LinkedMultiValueMap;
@@ -27,15 +34,25 @@ import static org.junit.jupiter.api.Assertions.*;
     "cas.amazon-sts.endpoint=http://127.0.0.1:4566",
     "cas.amazon-sts.credential-access-key=test",
     "cas.amazon-sts.credential-secret-key=test",
-
+    "cas.authn.mfa.groovy-script.location=classpath:AmazonStsGroovyMfa.groovy",
     "management.endpoint.awsSts.enabled=true"
 })
 @Tag("AmazonWebServices")
-@Import(AmazonCoreConfiguration.class)
+@Import({AmazonCoreConfiguration.class, CasCoreRestConfiguration.class,
+    CasAuthenticationEventExecutionPlanTestConfiguration.class})
 public class AmazonSecurityTokenServiceEndpointTests extends AbstractCasEndpointTests {
     @Autowired
     @Qualifier("awsSecurityTokenServiceEndpoint")
     private AmazonSecurityTokenServiceEndpoint awsSecurityTokenServiceEndpoint;
+
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
+
+    @BeforeEach
+    public void beforeEach() {
+        ApplicationContextProvider.holdApplicationContext(applicationContext);
+        TestMultifactorAuthenticationProvider.registerProviderIntoApplicationContext(applicationContext);
+    }
 
     @Test
     public void verifyOperation() {
@@ -43,9 +60,42 @@ public class AmazonSecurityTokenServiceEndpointTests extends AbstractCasEndpoint
 
         val body = new LinkedMultiValueMap<String, String>();
         body.put("username", List.of("casuser"));
-        body.put("password", List.of("Mellon"));
+        body.put("password", List.of("resusac"));
 
         val credentials = awsSecurityTokenServiceEndpoint.fetchCredentials("PT15S", body, request);
         assertNotNull(credentials);
+    }
+
+    @Test
+    public void verifyContextValidationFails() {
+        val request = new MockHttpServletRequest();
+
+        val body = new LinkedMultiValueMap<String, String>();
+        body.put("username", List.of("test1234"));
+        body.put("password", List.of("4321tset"));
+
+        val credentials = awsSecurityTokenServiceEndpoint.fetchCredentials("PT15S", body, request);
+        assertNotNull(credentials);
+    }
+
+
+    @Test
+    public void verifyNoCredentials() {
+        val request = new MockHttpServletRequest();
+        val body = new LinkedMultiValueMap<String, String>();
+        val status = awsSecurityTokenServiceEndpoint.fetchCredentials("PT15S", body, request);
+        assertEquals(HttpStatus.BAD_REQUEST, status.getStatusCode());
+    }
+
+    @Test
+    public void verifyFailsAuthN() {
+        val request = new MockHttpServletRequest();
+
+        val body = new LinkedMultiValueMap<String, String>();
+        body.put("username", List.of("casuser"));
+        body.put("password", List.of("bad-password"));
+
+        val status = awsSecurityTokenServiceEndpoint.fetchCredentials("PT15S", body, request);
+        assertEquals(HttpStatus.UNAUTHORIZED, status.getStatusCode());
     }
 }
