1fba588323f6e1d0eaed72b18d8300dfa61252f9
Misagh <misagh.moayyed@gmail.com>
2021-04-26 22:58:36 +0400

    update rest+surrogate tests

---
+++ ci/tests/puppeteer/scenarios/rest-protocol-surrogate-login/script.js
@@ -0,0 +1,54 @@
+const assert = require('assert');
+const https = require('https');
+const axios = require('axios');
+
+(async () => {
+    const instance = axios.create({
+        httpsAgent: new https.Agent({
+            rejectUnauthorized: false
+        })
+    });
+
+    let params = new URLSearchParams()
+    params.append('username', 'user1+casuser')
+    params.append('password', 'Mellon')
+
+    instance
+        .post('https://localhost:8443/cas/v1/users', params, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        })
+        .then(res => {
+            console.log(res.data.authentication.attributes);
+            assert(res.data.authentication.attributes.surrogateUser != null);
+            assert(res.data.authentication.attributes.surrogateEnabled != null);
+            assert(res.data.authentication.attributes.surrogatePrincipal != null);
+        })
+        .catch(error => {
+            throw error;
+        })
+
+    params = new URLSearchParams()
+    params.append('username', 'casuser')
+    params.append('password', 'Mellon')
+
+    instance
+        .post('https://localhost:8443/cas/v1/users', params, {
+            headers: {
+                'Accept': 'application/json',
+                'X-Surrogate-Principal': 'user1',
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        })
+        .then(res => {
+            console.log(res.data.authentication.attributes);
+            assert(res.data.authentication.attributes.surrogateUser != null);
+            assert(res.data.authentication.attributes.surrogateEnabled != null);
+            assert(res.data.authentication.attributes.surrogatePrincipal != null);
+        })
+        .catch(error => {
+            throw error;
+        })
+})();
