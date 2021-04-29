6ddf95d0969b1da941eb252bf23072f665c1bc5e
Misagh <misagh.moayyed@gmail.com>
2021-04-29 10:40:55 +0400

    clean up schemas

--- support/cas-server-support-jpa-ticket-registry/src/main/java/org/apereo/cas/config/JpaTicketRegistryConfiguration.java
+++ support/cas-server-support-jpa-ticket-registry/src/main/java/org/apereo/cas/config/JpaTicketRegistryConfiguration.java
@@ -1,27 +1,23 @@
 package org.apereo.cas.config;
 
-import org.apereo.cas.CentralAuthenticationService;
 import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.configuration.model.support.jpa.JpaConfigurationContext;
 import org.apereo.cas.configuration.support.Beans;
 import org.apereo.cas.configuration.support.JpaBeans;
 import org.apereo.cas.jpa.JpaBeanFactory;
-import org.apereo.cas.ticket.AbstractTicket;
 import org.apereo.cas.ticket.TicketCatalog;
+import org.apereo.cas.ticket.registry.JpaTicketEntity;
 import org.apereo.cas.ticket.registry.JpaTicketRegistry;
 import org.apereo.cas.ticket.registry.TicketRegistry;
 import org.apereo.cas.ticket.registry.support.JpaLockingStrategy;
 import org.apereo.cas.ticket.registry.support.LockingStrategy;
+import org.apereo.cas.util.CollectionUtils;
 import org.apereo.cas.util.CoreTicketUtils;
 import org.apereo.cas.util.InetAddressUtils;
 import org.apereo.cas.util.spring.ApplicationContextProvider;
 
 import lombok.val;
 import org.apache.commons.lang3.StringUtils;
-import org.reflections.Reflections;
-import org.reflections.scanners.SubTypesScanner;
-import org.reflections.util.ClasspathHelper;
-import org.reflections.util.ConfigurationBuilder;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -32,7 +28,6 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -41,7 +36,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * This this {@link JpaTicketRegistryConfiguration}.
@@ -67,18 +61,9 @@ public class JpaTicketRegistryConfiguration {
 
     @Bean
     public Set<String> ticketPackagesToScan() {
-        val reflections =
-            new Reflections(new ConfigurationBuilder()
-                .setUrls(ClasspathHelper.forPackage(CentralAuthenticationService.NAMESPACE))
-                .setScanners(new SubTypesScanner(false)));
-        val subTypes = reflections.getSubTypesOf(AbstractTicket.class);
-        return subTypes
-            .stream()
-            .map(t -> t.getPackage().getName())
-            .collect(Collectors.<String>toSet());
+        return CollectionUtils.wrapSet(JpaTicketEntity.class.getPackage().getName());
     }
 
-    @Lazy
     @Bean
     public LocalContainerEntityManagerFactoryBean ticketEntityManagerFactory() {
         ApplicationContextProvider.holdApplicationContext(applicationContext);
