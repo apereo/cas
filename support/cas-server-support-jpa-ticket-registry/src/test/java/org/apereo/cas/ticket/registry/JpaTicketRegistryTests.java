71942b861bb6da3e7c57c898ff8e2fdaa860eebe
Misagh <misagh.moayyed@gmail.com>
2021-05-03 21:59:01 +0400

    fix build failures

--- support/cas-server-support-jpa-ticket-registry/src/test/java/org/apereo/cas/ticket/registry/JpaTicketRegistryTests.java
+++ support/cas-server-support-jpa-ticket-registry/src/test/java/org/apereo/cas/ticket/registry/JpaTicketRegistryTests.java
@@ -57,7 +57,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @Tag("JDBC")
 @Getter
 public class JpaTicketRegistryTests extends BaseTicketRegistryTests {
-    private static final int COUNT = 10_000;
+    private static final int COUNT = 1_000;
 
     @Autowired
     @Qualifier("defaultOAuthCodeFactory")
