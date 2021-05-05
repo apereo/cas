de61e70ec1b2f19fa97791b4f0c326a2536baa84
Misagh <misagh.moayyed@gmail.com>
2021-05-05 14:57:28 +0400

    re-enable tests; tests for authn metadata

--- api/cas-server-core-api-mfa/src/test/java/org/apereo/cas/authentication/MultifactorAuthenticationCredentialTests.java
+++ api/cas-server-core-api-mfa/src/test/java/org/apereo/cas/authentication/MultifactorAuthenticationCredentialTests.java
@@ -4,6 +4,8 @@ import lombok.val;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -18,6 +20,12 @@ public class MultifactorAuthenticationCredentialTests {
     @Test
     public void verifyOperation() {
         val input = new MultifactorAuthenticationCredential() {
+            private static final long serialVersionUID = -7854668847716061700L;
+
+            @Override
+            public String getId() {
+                return UUID.randomUUID().toString();
+            }
         };
         input.setProviderId("nothing");
         assertNull(input.getProviderId());
