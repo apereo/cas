5db0990919629207cd33b9516fb958c870a02abe
CAS <cas@apereo.org>
2021-05-13 15:39:10 +0400

    add tests for delegation with existing sso

--- core/cas-server-core-authentication-api/src/test/java/org/apereo/cas/authentication/DefaultAuthenticationBuilderTests.java
+++ core/cas-server-core-authentication-api/src/test/java/org/apereo/cas/authentication/DefaultAuthenticationBuilderTests.java
@@ -4,6 +4,9 @@ import org.apereo.cas.DefaultMessageDescriptor;
 import org.apereo.cas.authentication.credential.UsernamePasswordCredential;
 import org.apereo.cas.authentication.handler.support.SimpleTestUsernamePasswordAuthenticationHandler;
 import org.apereo.cas.authentication.metadata.BasicCredentialMetaData;
+import org.apereo.cas.authentication.principal.DefaultPrincipalElectionStrategy;
+import org.apereo.cas.configuration.model.core.authentication.PrincipalAttributesCoreProperties;
+import org.apereo.cas.util.CollectionUtils;
 
 import lombok.val;
 import org.junit.jupiter.api.Tag;
@@ -39,6 +42,26 @@ public class DefaultAuthenticationBuilderTests {
         builder.addFailure("Success", new FailedLoginException());
         assertFalse(builder.hasAttribute("invalid"));
         assertNotNull(builder.build());
+    }
+
+    @Test
+    public void verifyMergeOperation() {
+        val builder1 = new DefaultAuthenticationBuilder(CoreAuthenticationTestUtils.getPrincipal());
+        builder1.mergeAttribute("key", 12345);
+        builder1.mergeAttribute("key", CollectionUtils.wrapList(54321, 998877));
+        val authn1 = builder1.build();
+
+        val builder2 = new DefaultAuthenticationBuilder(CoreAuthenticationTestUtils.getPrincipal());
+        builder2.mergeAttribute("key", 112233);
+        builder2.mergeAttribute("key", CollectionUtils.wrapList(443322));
+        val authn2 = builder2.build();
+
+        val resultBuilder = new DefaultAuthenticationResultBuilderFactory().newBuilder();
+        val strategy = new DefaultPrincipalElectionStrategy();
+        strategy.setAttributeMerger(CoreAuthenticationUtils.getAttributeMerger(PrincipalAttributesCoreProperties.MergingStrategyTypes.MULTIVALUED));
+        val resultAuthn = resultBuilder.collect(authn1).collect(authn2).build(strategy);
+        assertNotNull(resultAuthn);
+        assertEquals(5, resultAuthn.getAuthentication().getAttributes().get("key").size());
     }
 
     @Test
