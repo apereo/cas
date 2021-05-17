4640600ec060c43302cd16ef03db3cfe67e6ebb6
CAS <cas@apereo.org>
2021-05-18 00:02:46 +0400

    reorg ui pages into directories

--- docs/cas-server-documentation-processor/src/main/java/org/apereo/cas/documentation/CasDocumentationApplication.java
+++ docs/cas-server-documentation-processor/src/main/java/org/apereo/cas/documentation/CasDocumentationApplication.java
@@ -45,6 +45,8 @@ public class CasDocumentationApplication {
             });
         var dataDirectory = args[0];
         var projectVersion = args[1];
+        var projectRootDirectory = args[2];
+
         var dataPath = new File(dataDirectory, projectVersion);
         if (dataPath.exists()) {
             FileUtils.deleteQuietly(dataPath);
@@ -59,6 +61,32 @@ public class CasDocumentationApplication {
 
         exportThirdPartyConfiguration(dataPath);
         exportRegisteredServiceProperties(dataPath);
+        exportTemplateViews(projectRootDirectory, dataPath);
+    }
+
+    private static void exportTemplateViews(final String projectRootDirectory, final File dataPath) {
+        var serviceProps = new File(dataPath, "userinterface-templates");
+        if (serviceProps.exists()) {
+            FileUtils.deleteQuietly(serviceProps);
+        }
+        serviceProps.mkdirs();
+        var uiFile = new File(serviceProps, "config.yml");
+        var properties = new ArrayList<Map<?, ?>>();
+
+        var root = new File(projectRootDirectory, "support/cas-server-support-thymeleaf");
+        var parent = new File(root, "src/main/resources/templates");
+
+        var files = FileUtils.listFiles(parent, new String[]{"html"}, true);
+        files
+            .stream()
+            .sorted()
+            .forEach(file -> {
+                var map = new LinkedHashMap<String, Object>();
+                val path = StringUtils.remove(file.getAbsolutePath(), root.getAbsolutePath());
+                map.put("name", path);
+                properties.add(map);
+            });
+        CasConfigurationMetadataCatalog.export(uiFile, properties);
     }
 
     private static void exportRegisteredServiceProperties(final File dataPath) {
