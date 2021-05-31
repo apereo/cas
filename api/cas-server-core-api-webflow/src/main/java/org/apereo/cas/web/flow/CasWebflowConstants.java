b6a91681451ed8a6f9cd7552fcef5af82282e9cd
Jérôme LELEU <leleuj@gmail.com>
2021-05-31 16:13:16 +0200

    Properly handle POST binding for SAML logout response

--- api/cas-server-core-api-webflow/src/main/java/org/apereo/cas/web/flow/CasWebflowConstants.java
+++ api/cas-server-core-api-webflow/src/main/java/org/apereo/cas/web/flow/CasWebflowConstants.java
@@ -162,6 +162,11 @@ public interface CasWebflowConstants {
     String TRANSITION_ID_REDIRECT = "redirect";
 
     /**
+     * Transition id 'redirect' .
+     */
+    String TRANSITION_ID_POST = "post";
+
+    /**
      * Transition id 'skip' .
      */
     String TRANSITION_ID_SKIP = "skip";
