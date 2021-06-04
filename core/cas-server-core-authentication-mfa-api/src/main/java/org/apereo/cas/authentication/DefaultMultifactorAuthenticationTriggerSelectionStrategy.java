7af684fd5ff144bf187dad102765786006a9bd5b
Misagh <mm1844@gmail.com>
2021-06-04 12:51:05 +0400

    add tests for mfa ignore execution

--- core/cas-server-core-authentication-mfa-api/src/main/java/org/apereo/cas/authentication/DefaultMultifactorAuthenticationTriggerSelectionStrategy.java
+++ core/cas-server-core-authentication-mfa-api/src/main/java/org/apereo/cas/authentication/DefaultMultifactorAuthenticationTriggerSelectionStrategy.java
@@ -24,45 +24,23 @@ import java.util.Optional;
 public class DefaultMultifactorAuthenticationTriggerSelectionStrategy implements MultifactorAuthenticationTriggerSelectionStrategy {
     private final Collection<MultifactorAuthenticationTrigger> multifactorAuthenticationTriggers;
 
-    /**
-     * Finalize trigger activation for.
-     *
-     * @param provider          the provider
-     * @param trigger           the trigger
-     * @param authentication    the authentication
-     * @param registeredService the registered service
-     * @param request           the request
-     * @param service           the service
-     * @return the optional
-     */
-    protected Optional<MultifactorAuthenticationProvider> finalizeTriggerActivationFor(final MultifactorAuthenticationProvider provider,
-                                                                                            final MultifactorAuthenticationTrigger trigger,
-                                                                                            final Authentication authentication,
-                                                                                            final RegisteredService registeredService,
-                                                                                            final HttpServletRequest request,
-                                                                                            final Service service) {
-        LOGGER.trace("Multifactor authentication is triggered via [{}]", trigger.getName());
-        if (registeredService.getMultifactorPolicy().isIgnoreExecution()) {
-            LOGGER.debug("Multifactor authentication policy for [{}] will ignore executions for trigger [{}]",
-                registeredService.getName(), trigger.getName());
-            return Optional.empty();
-        }
-        return Optional.of(provider);
-    }
-
     @Override
     public Optional<MultifactorAuthenticationProvider> resolve(final HttpServletRequest request,
                                                                final RegisteredService registeredService,
                                                                final Authentication authentication,
                                                                final Service service) {
+        if (registeredService != null && registeredService.getMultifactorPolicy().isIgnoreExecution()) {
+            LOGGER.debug("Multifactor authentication policy for [{}] will ignore trigger executions", registeredService.getName());
+            return Optional.empty();
+        }
+
         for (val trigger : multifactorAuthenticationTriggers) {
             if (!trigger.supports(request, registeredService, authentication, service)) {
                 continue;
             }
             val activated = trigger.isActivated(authentication, registeredService, request, service);
             if (activated.isPresent()) {
-                return finalizeTriggerActivationFor(activated.get(), trigger, authentication,
-                    registeredService, request, service);
+                return activated;
             }
         }
         return Optional.empty();
