6e61fe304c4044bf987ef9a1f63f77ba4ae359f5
Fabian Busse <fabian.busse@lgln.niedersachsen.de>
2021-06-16 11:29:32 +0200

    cleanup, assert no ticket is provided in oidc response

--- ci/tests/puppeteer/scenarios/oidc-authzcode-pac4j-delegated-login/script.js
+++ ci/tests/puppeteer/scenarios/oidc-authzcode-pac4j-delegated-login/script.js
@@ -4,14 +4,15 @@ const cas = require('../../cas.js');
 
 (async () => {
     const browser = await puppeteer.launch(cas.browserOptions());
-    const page = await browser.newPage();
-    const url = "https://localhost:8443/cas/oidc/authorize\
-                ?response_type=code\
-                &client_id=client\
-                &scope=openid%20MyCustomScope\
-                &redirect_uri=http://localhost:8080/oidclogin\
-                &nonce=3d3a7457f9ad3&state=1735fd6c43c14\
-                &claims=%7B%22userinfo%22%3A%20%7B%20%22name%22%3A%20%7B%22essential%22%3A%20true%7D%2C%22phone_number%22%3A%20%7B%22essential%22%3A%20true%7D%7D%7D";
+    const page = await cas.newPage(browser);
+
+    const url = "https://localhost:8443/cas/oidc/authorize?" +
+        "client_id=client&" +
+        "redirect_uri=https%3A%2F%2Foidcdebugger.com%2Fdebug&" +
+        "scope=openid%20email%20profile%20address%20phone&" +
+        "response_type=code&" +
+        "response_mode=form_post&" +
+        "nonce=vn4qulthnx";
     await page.goto(url);
 
     await cas.assertVisibility(page, '#loginProviders')
@@ -21,23 +22,27 @@ const cas = require('../../cas.js');
 
     await cas.loginWith(page, "casuser", "Mellon");
 
+    await page.waitForTimeout(1000)
+
+    let result = new URL(page.url());
+    console.log(result.searchParams.toString())
+
+    assert(result.searchParams.has("ticket") === false);
+    assert(result.searchParams.has("client_id"));
+    assert(result.searchParams.has("redirect_uri"));
+    assert(result.searchParams.has("scope"));
+
+    console.log("Allowing release of scopes and claims...")
+    await cas.click(page, "#allow")
+    await page.waitForNavigation();
     await page.waitForTimeout(2000)
 
-    await cas.assertVisibility(page, '#userInfoClaims')
+    let header = await cas.textContent(page, "h1.green-text");
+    assert(header === "Success!")
 
-    await cas.assertVisibility(page, '#scopes')
+    console.log(page.url());
+    assert(page.url().startsWith("https://oidcdebugger.com/debug"))
 
-    await cas.assertVisibility(page, '#MyCustomScope')
-
-    await cas.assertVisibility(page, '#openid')
-
-    await cas.assertVisibility(page, '#informationUrl')
-
-    await cas.assertVisibility(page, '#privacyUrl')
-
-    await cas.assertVisibility(page, '#name')
-
-    await cas.assertVisibility(page, '#phone_number')
-
+    await page.waitForTimeout(30000)
     await browser.close();
 })();
