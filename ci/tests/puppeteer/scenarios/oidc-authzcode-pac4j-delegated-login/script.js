07bcaf8cafe895ddf5d6736bdd9db679c3dad07c
Fabian Busse <fabian.busse@lgln.niedersachsen.de>
2021-06-16 10:24:43 +0200

    service definitions cleanup, url wrapped

--- ci/tests/puppeteer/scenarios/oidc-authzcode-pac4j-delegated-login/script.js
+++ ci/tests/puppeteer/scenarios/oidc-authzcode-pac4j-delegated-login/script.js
@@ -5,7 +5,13 @@ const cas = require('../../cas.js');
 (async () => {
     const browser = await puppeteer.launch(cas.browserOptions());
     const page = await browser.newPage();
-    const url = "https://localhost:8443/cas/oidc/authorize?response_type=code&client_id=client&scope=openid%20MyCustomScope&redirect_uri=http://localhost:8080/oidclogin&nonce=3d3a7457f9ad3&state=1735fd6c43c14&claims=%7B%22userinfo%22%3A%20%7B%20%22name%22%3A%20%7B%22essential%22%3A%20true%7D%2C%22phone_number%22%3A%20%7B%22essential%22%3A%20true%7D%7D%7D";
+    const url = "https://localhost:8443/cas/oidc/authorize\
+                ?response_type=code\
+                &client_id=client\
+                &scope=openid%20MyCustomScope\
+                &redirect_uri=http://localhost:8080/oidclogin\
+                &nonce=3d3a7457f9ad3&state=1735fd6c43c14\
+                &claims=%7B%22userinfo%22%3A%20%7B%20%22name%22%3A%20%7B%22essential%22%3A%20true%7D%2C%22phone_number%22%3A%20%7B%22essential%22%3A%20true%7D%7D%7D";
     await page.goto(url);
 
     await cas.assertVisibility(page, '#loginProviders')
