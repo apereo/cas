c0c8ce8454dc0dbdffccba53c01af7f5f93d4731
Misagh <mm1844@gmail.com>
2021-06-17 13:04:26 +0400

    test for saml2 idp initiated

--- ci/tests/puppeteer/scenarios/saml2-idp-login-unsolicited/script.js
+++ ci/tests/puppeteer/scenarios/saml2-idp-login-unsolicited/script.js
@@ -8,35 +8,42 @@ const cas = require('../../cas.js');
     const browser = await puppeteer.launch(cas.browserOptions());
     const page = await cas.newPage(browser);
     await page.goto("https://samltest.id/upload.php");
-    await page.waitForTimeout(1000)
+    // await page.waitForTimeout(1000)
 
     const fileElement = await page.$("input[type=file]");
     let metadata = path.join(__dirname, '/saml-md/idp-metadata.xml');
     console.log("Metadata file: " + metadata);
+
     await fileElement.uploadFile(metadata);
-    await page.waitForTimeout(1000)
+    // await page.waitForTimeout(1000)
 
     await cas.click(page, "input[name='submit']")
     await page.waitForNavigation();
-    await page.waitForTimeout(1000)
 
-    await page.goto("https://samltest.id/start-idp-test/");
-    await cas.type(page,'input[name=\'entityID\']', "https://cas.apereo.org/saml/idp");
     // await page.waitForTimeout(1000)
-    await cas.click(page, "input[type='submit']")
-    await page.waitForNavigation();
 
+    let entityId = "https://samltest.id/saml/sp";
+    let url = "https://localhost:8443/cas/idp/profile/SAML2/Unsolicited/SSO";
+    url += `?providerId=${entityId}`;
+    url += "&target=https%3A%2F%2Flocalhost%3A8443%2Fcas%2Flogin";
+
+    console.log("Navigating to " + url);
+    await page.goto(url);
     await page.waitForTimeout(1000)
-
     await cas.loginWith(page, "casuser", "Mellon");
-    await page.waitForTimeout(5500)
+    await page.waitForTimeout(2000)
+
+    const title = await page.title();
+    console.log(title);
+    assert(title === "CAS - Central Authentication Service")
+
+    const header = await cas.innerText(page, '#content div h2');
+    assert(header === "Log In Successful")
 
     let metadataDir = path.join(__dirname, '/saml-md');
     fs.rmdirSync(metadataDir, { recursive: true });
-
-    const header = await cas.textContent(page, "div.entry-content p");
-    assert(header.startsWith("Your browser has completed the full SAML 2.0 round-trip"));
     
     await browser.close();
 })();
+
 
