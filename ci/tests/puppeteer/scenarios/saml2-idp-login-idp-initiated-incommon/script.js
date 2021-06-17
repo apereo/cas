f83100a082ab8441058b1e4c94f05d7b75314dae
Misagh <mm1844@gmail.com>
2021-06-17 20:20:17 +0400

    fix tests

--- ci/tests/puppeteer/scenarios/saml2-idp-login-idp-initiated-incommon/script.js
+++ ci/tests/puppeteer/scenarios/saml2-idp-login-idp-initiated-incommon/script.js
@@ -1,7 +1,8 @@
 const puppeteer = require('puppeteer');
-const performance = require('perf_hooks');
+const performance = require('perf_hooks').performance;
 const fs = require('fs');
 const path = require('path');
+const assert = require('assert');
 const cas = require('../../cas.js');
 
 (async () => {
@@ -17,21 +18,27 @@ const cas = require('../../cas.js');
         "https://cole.uconline.edu/shibboleth-sp"
     ];
 
+    let count = 0;
     for (const entityId of entityIds) {
         let url = "https://localhost:8443/cas/idp/profile/SAML2/Unsolicited/SSO";
         url += `?providerId=${entityId}`;
         url += "&target=https%3A%2F%2Flocalhost%3A8443%2Fcas%2Flogin";
 
         console.log("Navigating to " + url);
-        let s = performance.now();
+        let s = await performance.now();
         await page.goto(url);
-        let e = performance.now();
-        let duration = e - s;
-        console.log("Request took " + duration + " ms.")
-        
+        let e = await performance.now();
+        let duration = (e - s) / 1000;
+        console.log("Request took " + duration + " seconds.")
+
+        if (count > 1 && duration > 8) {
+            throw "Request took longer than expected";
+        }
+    
         await page.waitForTimeout(1000);
         await cas.assertVisibility(page, '#username')
         await cas.assertVisibility(page, '#password')
+        count++;
     }
     let metadataDir = path.join(__dirname, '/saml-md');
     fs.rmdirSync(metadataDir, { recursive: true });
