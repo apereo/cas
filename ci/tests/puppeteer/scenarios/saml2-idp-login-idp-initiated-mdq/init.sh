e82b42287cea5452c20bfb27823445e84906defb
Misagh <mm1844@gmail.com>
2021-06-17 22:41:34 +0400

    fix tests

--- ci/tests/puppeteer/scenarios/saml2-idp-login-idp-initiated-mdq/init.sh
+++ ci/tests/puppeteer/scenarios/saml2-idp-login-idp-initiated-mdq/init.sh
@@ -3,5 +3,5 @@ echo -e "Removing previous SAML metadata directory"
 rm -Rf "${PWD}/ci/tests/puppeteer/scenarios/${SCENARIO}/saml-md"
 
 echo -e "Fetching InCommon MDQ signing certificate..."
-cd "${TMPDIR}" || exit
+cd /tmp && pwd
 wget http://md.incommon.org/certs/inc-md-cert-mdq.pem
