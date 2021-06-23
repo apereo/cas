1b83cef9097cbf7bba921dea4c40a872b6e73933
Hal Deadman <deadman_harold@bah.com>
2021-06-23 18:05:43 -0400

    wip just run puppeteer

--- ci/tests/puppeteer/scenarios/x509-login-ldap-attributes/create_cert.sh
+++ ci/tests/puppeteer/scenarios/x509-login-ldap-attributes/create_cert.sh
@@ -3,7 +3,13 @@ rm cert.pem
 rm key.pem
 rm truststore.jks
 rm truststore.p12
-# -addext 'basicConstraints=CA:FALSE'
-openssl req -x509 -nodes -newkey rsa:2048 -keyout key.pem -out cert.pem -days 3650 -subj "/C=US/ST=Virginia/L=Fairfax/O=Some Company, Inc./OU=IT/CN=CAS User" -addext 'subjectAltName=otherName:msUPN;UTF8:1234567890@college.edu,email:1234567890@college.edu'
+CONFIG="
+[req]
+distinguished_name=dn
+[ dn ]
+[ ext ]
+basicConstraints=CRITICAL,CA:FALSE
+"
+openssl req -x509 -config <(echo "$CONFIG") -nodes -newkey rsa:2048 -keyout key.pem -out cert.pem -days 3650 -subj "/C=US/ST=Virginia/L=Fairfax/O=Some Company, Inc./OU=IT/CN=CAS User" -addext 'basicConstraints=critical,CA:FALSE' -addext 'subjectAltName=otherName:msUPN;UTF8:1234567890@college.edu,email:1234567890@college.edu'
 keytool -importcert -keystore truststore.jks -storetype JKS -storepass changeit -alias casuser -file cert.pem -noprompt
 keytool -importcert -keystore truststore.p12 -storetype PKCS12 -storepass changeit -alias casuser -file cert.pem -noprompt
