fe77ac69bee7393e0921140338f3ab17d40ab580
Misagh <mm1844@gmail.com>
2021-06-25 23:49:06 +0400

    fix checkstyle

--- support/cas-server-support-ldap/src/main/java/org/apereo/cas/config/LdapAuthenticationConfiguration.java
+++ support/cas-server-support-ldap/src/main/java/org/apereo/cas/config/LdapAuthenticationConfiguration.java
@@ -2,43 +2,17 @@ package org.apereo.cas.config;
 
 import org.apereo.cas.authentication.AuthenticationEventExecutionPlanConfigurer;
 import org.apereo.cas.authentication.AuthenticationHandler;
-import org.apereo.cas.authentication.AuthenticationPasswordPolicyHandlingStrategy;
-import org.apereo.cas.authentication.CoreAuthenticationUtils;
-import org.apereo.cas.authentication.LdapAuthenticationHandler;
 import org.apereo.cas.authentication.principal.PrincipalFactory;
 import org.apereo.cas.authentication.principal.PrincipalFactoryUtils;
-import org.apereo.cas.authentication.principal.PrincipalNameTransformerUtils;
 import org.apereo.cas.authentication.principal.PrincipalResolver;
-import org.apereo.cas.authentication.support.DefaultLdapAccountStateHandler;
-import org.apereo.cas.authentication.support.OptionalWarningLdapAccountStateHandler;
-import org.apereo.cas.authentication.support.RejectResultCodeLdapPasswordPolicyHandlingStrategy;
-import org.apereo.cas.authentication.support.password.DefaultPasswordPolicyHandlingStrategy;
-import org.apereo.cas.authentication.support.password.GroovyPasswordPolicyHandlingStrategy;
-import org.apereo.cas.authentication.support.password.PasswordEncoderUtils;
-import org.apereo.cas.authentication.support.password.PasswordPolicyContext;
 import org.apereo.cas.configuration.CasConfigurationProperties;
-import org.apereo.cas.configuration.model.support.ldap.LdapAuthenticationProperties;
-import org.apereo.cas.configuration.model.support.ldap.LdapPasswordPolicyProperties;
 import org.apereo.cas.services.ServicesManager;
-import org.apereo.cas.util.CollectionUtils;
 import org.apereo.cas.util.LdapUtils;
-import org.apereo.cas.util.LoggingUtils;
 
-import com.google.common.collect.Multimap;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.apache.commons.lang3.StringUtils;
-import org.jooq.lambda.Unchecked;
-import org.ldaptive.auth.AuthenticationResponse;
-import org.ldaptive.auth.AuthenticationResponseHandler;
-import org.ldaptive.auth.Authenticator;
-import org.ldaptive.auth.ext.ActiveDirectoryAuthenticationResponseHandler;
-import org.ldaptive.auth.ext.EDirectoryAuthenticationResponseHandler;
-import org.ldaptive.auth.ext.FreeIPAAuthenticationResponseHandler;
-import org.ldaptive.auth.ext.PasswordExpirationAuthenticationResponseHandler;
-import org.ldaptive.auth.ext.PasswordPolicyAuthenticationResponseHandler;
-import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -50,12 +24,8 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.time.Period;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Set;
 
 /**
  * This is {@link LdapAuthenticationConfiguration} that attempts to create
