9df73b7d1f937ad134bc0adc6d42b63ec1ac195d
Hal Deadman <hal.deadman@gmail.com>
2021-06-28 23:23:03 -0400

    fix groovy

--- ci/tests/puppeteer/scenarios/x509-login-ldap-attributes/attributes.groovy
+++ ci/tests/puppeteer/scenarios/x509-login-ldap-attributes/attributes.groovy
@@ -1,6 +1,6 @@
 import java.util.*
 
-def Map<String, List<Object>> run(final Object... args) {
+Map<String, List<Object>> run(final Object... args) {
     def uid = args[0]
     def attributes = args[1]
     def logger = args[2]
