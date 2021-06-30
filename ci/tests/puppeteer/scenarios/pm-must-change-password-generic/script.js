1a1a3d12db698a8e3c6d9cf9c2df7910e4ddd480
Misagh <mm1844@gmail.com>
2021-06-30 22:04:40 +0400

    fix password-mgmt flows missing credential; add tests

--- ci/tests/puppeteer/scenarios/pm-must-change-password-generic/script.js
+++ ci/tests/puppeteer/scenarios/pm-must-change-password-generic/script.js
@@ -6,14 +6,19 @@ const cas = require('../../cas.js');
     const browser = await puppeteer.launch(cas.browserOptions());
     const page = await cas.newPage(browser);
     await page.goto("https://localhost:8443/cas/login");
-
     await cas.loginWith(page, "mustchangepswd", "mustchangepswd");
-
+    await page.waitForTimeout(2000)
     const header = await cas.innerText(page, '#pwdmain h3');
+
     assert(header === "Hello, mustchangepswd. You must change your password.")
 
-    let pwddesc = await cas.innerText(page, '#pwddesc');
-    assert(pwddesc === "Please change your password.")
-    
+    await cas.type(page,'#password', "Jv!e0mKD&dCNl^Q");
+    await cas.type(page,'#confirmedPassword', "Jv!e0mKD&dCNl^Q");
+    await page.keyboard.press('Enter');
+    await page.waitForNavigation();
+
+    const success = await cas.innerText(page, '#content h2');
+    assert(success === "Password Change Successful")
+
     await browser.close();
 })();
