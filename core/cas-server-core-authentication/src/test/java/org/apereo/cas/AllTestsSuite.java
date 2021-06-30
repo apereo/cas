08a8512acdff57b5ac689de9dbd5bae755e504f3
Hal Deadman <hal.deadman@gmail.com>
2021-06-29 23:00:04 -0400

    put test class back

--- core/cas-server-core-authentication/src/test/java/org/apereo/cas/AllTestsSuite.java
+++ core/cas-server-core-authentication/src/test/java/org/apereo/cas/AllTestsSuite.java
@@ -3,6 +3,7 @@ package org.apereo.cas;
 import org.apereo.cas.authentication.AcceptUsersAuthenticationHandlerTests;
 import org.apereo.cas.authentication.BasicCredentialMetaDataTests;
 import org.apereo.cas.authentication.CacheCredentialsMetaDataPopulatorTests;
+import org.apereo.cas.authentication.CoreAuthenticationUtilsTests;
 import org.apereo.cas.authentication.DefaultAuthenticationEventExecutionPlanTests;
 import org.apereo.cas.authentication.DefaultAuthenticationManagerTests;
 import org.apereo.cas.authentication.DefaultAuthenticationTests;
@@ -59,6 +60,7 @@ import org.junit.platform.suite.api.Suite;
     JaasAuthenticationHandlerSystemConfigurationTests.class,
     JaasAuthenticationHandlerTests.class,
     SimpleTestUsernamePasswordHandlerTests.class,
+    CoreAuthenticationUtilsTests.class,
     DefaultAttributeDefinitionTests.class,
     UsernamePasswordCredentialTests.class,
     DefaultPasswordEncoderTests.class,
