36fa0bedfb13c49e508dba49f5561545f955c494
Misagh <mm1844@gmail.com>
2021-07-06 21:33:11 +0400

    fix tests

--- support/cas-server-support-surrogate-authentication/src/test/java/org/apereo/cas/authentication/surrogate/BaseSurrogateAuthenticationServiceTests.java
+++ support/cas-server-support-surrogate-authentication/src/test/java/org/apereo/cas/authentication/surrogate/BaseSurrogateAuthenticationServiceTests.java
@@ -18,13 +18,17 @@ import org.apereo.cas.config.CasCoreTicketCatalogConfiguration;
 import org.apereo.cas.config.CasCoreTicketIdGeneratorsConfiguration;
 import org.apereo.cas.config.CasCoreTicketsConfiguration;
 import org.apereo.cas.config.CasCoreUtilConfiguration;
+import org.apereo.cas.config.CasCoreUtilSerializationConfiguration;
 import org.apereo.cas.config.CasCoreWebConfiguration;
 import org.apereo.cas.config.CasPersonDirectoryTestConfiguration;
 import org.apereo.cas.config.SurrogateAuthenticationAuditConfiguration;
 import org.apereo.cas.config.SurrogateAuthenticationConfiguration;
 import org.apereo.cas.config.SurrogateAuthenticationMetadataConfiguration;
+import org.apereo.cas.config.SurrogateAuthenticationRestConfiguration;
+import org.apereo.cas.config.SurrogateComponentSerializationConfiguration;
 import org.apereo.cas.config.support.CasWebApplicationServiceFactoryConfiguration;
 import org.apereo.cas.logout.config.CasCoreLogoutConfiguration;
+import org.apereo.cas.rest.config.CasCoreRestConfiguration;
 import org.apereo.cas.services.ServicesManager;
 import org.apereo.cas.services.web.config.CasThemesConfiguration;
 import org.apereo.cas.web.config.CasCookieConfiguration;
@@ -92,8 +96,11 @@ public abstract class BaseSurrogateAuthenticationServiceTests {
     @SpringBootConfiguration
     @Import({
         SurrogateAuthenticationConfiguration.class,
+        SurrogateComponentSerializationConfiguration.class,
         SurrogateAuthenticationAuditConfiguration.class,
         SurrogateAuthenticationMetadataConfiguration.class,
+        SurrogateAuthenticationRestConfiguration.class,
+        CasCoreRestConfiguration.class,
         CasCoreAuthenticationPrincipalConfiguration.class,
         CasCoreAuthenticationPolicyConfiguration.class,
         CasCoreAuthenticationMetadataConfiguration.class,
@@ -109,6 +116,7 @@ public abstract class BaseSurrogateAuthenticationServiceTests {
         CasCoreServicesConfiguration.class,
         CasCoreWebflowConfiguration.class,
         CasWebflowContextConfiguration.class,
+        CasCoreUtilSerializationConfiguration.class,
         CasCoreMultifactorAuthenticationConfiguration.class,
         CasMultifactorAuthenticationWebflowConfiguration.class,
         CasCoreConfiguration.class,
