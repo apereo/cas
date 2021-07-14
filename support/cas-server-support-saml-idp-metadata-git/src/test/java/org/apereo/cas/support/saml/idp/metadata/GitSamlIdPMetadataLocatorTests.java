638e5aa53b7156db84d8c73f7c505cf0ad4ca1ed
Misagh <mm1844@gmail.com>
2021-07-14 10:01:43 +0400

    remove dirties-context - wip

--- support/cas-server-support-saml-idp-metadata-git/src/test/java/org/apereo/cas/support/saml/idp/metadata/GitSamlIdPMetadataLocatorTests.java
+++ support/cas-server-support-saml-idp-metadata-git/src/test/java/org/apereo/cas/support/saml/idp/metadata/GitSamlIdPMetadataLocatorTests.java
@@ -13,7 +13,6 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 
 import java.io.File;
@@ -37,7 +36,6 @@ import static org.junit.jupiter.api.Assertions.*;
 })
 @Tag("FileSystem")
 @Slf4j
-@DirtiesContext
 public class GitSamlIdPMetadataLocatorTests extends BaseGitSamlMetadataTests {
 
     @BeforeAll
