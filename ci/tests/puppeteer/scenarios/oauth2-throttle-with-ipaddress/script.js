275143088620f05ea5c1648536c1c3db76aa2bbc
Misagh <mm1844@gmail.com>
2021-08-01 19:23:20 +0400

    add tests for oauth2 throttling

--- ci/tests/puppeteer/scenarios/oauth2-throttle-with-ipaddress/script.js
+++ ci/tests/puppeteer/scenarios/oauth2-throttle-with-ipaddress/script.js
@@ -6,7 +6,7 @@ async function throttleTokenEndpoint() {
     params += "client_secret=unknown&";
     params += "grant_type=client_credentials&";
     params += "scope=openid";
-    let url = 'https://localhost:8443/cas/oidc/token?' + params;
+    let url = 'https://localhost:8443/cas/oauth2.0/token?' + params;
 
     console.log("Log in attempt: #1 " + new Date().toISOString())
     await submitRequest(url, 401);
@@ -18,7 +18,7 @@ async function throttleTokenEndpoint() {
 }
 
 async function throttleUserInfoEndpoint() {
-    let url = 'https://localhost:8443/cas/oidc/profile?access_token=1234567890';
+    let url = 'https://localhost:8443/cas/oauth2.0/profile?access_token=1234567890';
     console.log("Log in attempt: #1 " + new Date().toISOString())
     await submitRequest(url, 401);
     console.log("Log in attempt: #2 " + new Date().toISOString())
