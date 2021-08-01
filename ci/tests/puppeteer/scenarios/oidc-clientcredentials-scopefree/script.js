c56ad49ce20b9ce407fc4cb088d50ebb887da656
Misagh <mm1844@gmail.com>
2021-08-01 14:09:49 +0400

    clean up use of axios

--- ci/tests/puppeteer/scenarios/oidc-clientcredentials-scopefree/script.js
+++ ci/tests/puppeteer/scenarios/oidc-clientcredentials-scopefree/script.js
@@ -1,14 +1,8 @@
-const https = require('https');
 const assert = require('assert');
-const axios = require('axios');
 const jwt = require('jsonwebtoken');
+const cas = require('../../cas.js');
 
 (async () => {
-    const instance = axios.create({
-        httpsAgent: new https.Agent({
-            rejectUnauthorized: false
-        })
-    });
 
     let params = "client_id=client&";
     params += "client_secret=secret&";
@@ -17,37 +11,33 @@ const jwt = require('jsonwebtoken');
 
     let url = 'https://localhost:8443/cas/oidc/token?' + params;
     console.log("Calling " + url);
-    
-    instance
-        .post(url, new URLSearchParams(), {
-            headers: {
-                'Content-Type': "application/json"
-            }
-        })
-        .then(res => {
-            console.log(res.data);
-            assert(res.data.access_token !== null);
 
-            console.log("Decoding JWT access token...");
-            let decoded = jwt.decode(res.data.access_token);
-            console.log(decoded);
-            
-            assert(res.data.id_token !== null);
-            assert(res.data.refresh_token !== null);
-            assert(res.data.token_type !== null);
-            assert(res.data.scope !== null);
+    await cas.doPost(url, "", {
+        'Content-Type': "application/json"
+    }, function (res) {
 
-            console.log("Decoding id token...")
-            decoded = jwt.decode(res.data.id_token);
-            console.log(decoded);
+        console.log(res.data);
+        assert(res.data.access_token !== null);
 
-            assert(decoded.sub !== null)
-            assert(decoded.cn !== null)
-            assert(decoded.name !== null)
-            assert(decoded["preferred_username"] !== null)
-            assert(decoded["given-name"] !== null)
-        })
-        .catch(error => {
-            throw 'Operation failed: ' + error;
-        })
+        console.log("Decoding JWT access token...");
+        let decoded = jwt.decode(res.data.access_token);
+        console.log(decoded);
+
+        assert(res.data.id_token !== null);
+        assert(res.data.refresh_token !== null);
+        assert(res.data.token_type !== null);
+        assert(res.data.scope !== null);
+
+        console.log("Decoding id token...")
+        decoded = jwt.decode(res.data.id_token);
+        console.log(decoded);
+
+        assert(decoded.sub !== null)
+        assert(decoded.cn !== null)
+        assert(decoded.name !== null)
+        assert(decoded["preferred_username"] !== null)
+        assert(decoded["given-name"] !== null)
+    }, function (error) {
+        throw 'Operation failed: ' + error;
+    });
 })();
