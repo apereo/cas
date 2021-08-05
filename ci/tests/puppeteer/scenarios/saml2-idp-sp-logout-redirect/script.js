037e40bd540fdea35ea923353ed2f840e6bf96e0
Misagh <mm1844@gmail.com>
2021-08-05 11:47:59 +0400

    fix tests

--- ci/tests/puppeteer/scenarios/saml2-idp-sp-logout-redirect/script.js
+++ ci/tests/puppeteer/scenarios/saml2-idp-sp-logout-redirect/script.js
@@ -6,15 +6,14 @@ const fs = require("fs");
 
 (async () => {
     const browser = await puppeteer.launch(cas.browserOptions());
-
-    const page = await cas.newPage(browser);
+    let page = await cas.newPage(browser);
     await page.goto("https://localhost:8443/cas/login");
-    await page.waitForTimeout(1000);
     await cas.loginWith(page, "casuser", "Mellon");
+    await page.close();
 
-    await page.waitForTimeout(2000);
-
-    await page.once('request', request => {
+    page = await cas.newPage(browser);
+    await page.setRequestInterception(true);
+    await page.on('request', request => {
         let data = {
             'method': 'POST',
             'postData': 'SAMLRequest=PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz48c2FtbDJwOkxvZ291dFJlcXVlc3QgeG1sbnM6c2FtbDJwPSJ1cm46b2FzaXM6bmFtZXM6dGM6U0FNTDoyLjA6cHJvdG9jb2wiIERlc3RpbmF0aW9uPSJodHRwczovL2xvY2FsaG9zdDo4NDQzL2Nhcy9pZHAvcHJvZmlsZS9TQU1MMi9QT1NUL1NMTyIgSUQ9IjEyMzQiIElzc3VlSW5zdGFudD0iMjAyMS0wNi0wMVQxMDowNDoyMi43NzJaIiBWZXJzaW9uPSIyLjAiPjxzYW1sMjpJc3N1ZXIgeG1sbnM6c2FtbDI9InVybjpvYXNpczpuYW1lczp0YzpTQU1MOjIuMDphc3NlcnRpb24iPmh0dHBzOi8vc2FtbHRlc3QuaWQvc2FtbC9zcDwvc2FtbDI6SXNzdWVyPjxzYW1sMjpOYW1lSUQgeG1sbnM6c2FtbDI9InVybjpvYXNpczpuYW1lczp0YzpTQU1MOjIuMDphc3NlcnRpb24iIEZvcm1hdD0idXJuOm9hc2lzOm5hbWVzOnRjOlNBTUw6Mi4wOm5hbWVpZC1mb3JtYXQ6dHJhbnNpZW50Ij5mYWtlbmFtZWlkPC9zYW1sMjpOYW1lSUQ%2BPHNhbWwycDpTZXNzaW9uSW5kZXg%2BZmFrZXNlc3Npb25pbmRleDwvc2FtbDJwOlNlc3Npb25JbmRleD48L3NhbWwycDpMb2dvdXRSZXF1ZXN0Pg%3D%3D',
@@ -23,16 +22,10 @@ const fs = require("fs");
                 'Content-Type': 'application/x-www-form-urlencoded'
             },
         };
-        page.setRequestInterception(true);
-        request.setRequestInterception(true)
         request.continue(data);
-        page.setRequestInterception(false);
     });
-
-    await page.setRequestInterception(true);
     await page.goto('https://localhost:8443/cas/idp/profile/SAML2/POST/SLO');
     await page.waitForTimeout(2000);
-
     assert(page.url().startsWith('https://samltest.id/Shibboleth.sso/SLO/Redirect?SAMLResponse='));
     let metadataDir = path.join(__dirname, '/saml-md');
     fs.rmdir(metadataDir, { recursive: true }, () => {});
