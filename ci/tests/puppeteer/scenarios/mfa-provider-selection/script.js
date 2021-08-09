6255a2243ea611d95d8c23d4399ed4e7373658b3
Misagh <mm1844@gmail.com>
2021-08-09 12:41:20 +0400

    fix tests

--- ci/tests/puppeteer/scenarios/mfa-provider-selection/script.js
+++ ci/tests/puppeteer/scenarios/mfa-provider-selection/script.js
@@ -6,12 +6,8 @@ const cas = require('../../cas.js');
     const browser = await puppeteer.launch(cas.browserOptions());
     const page = await cas.newPage(browser);
     await page.goto("https://localhost:8443/cas/login?service=https://example.com");
-
     await cas.loginWith(page, "casuser", "Mellon");
-
     await cas.assertVisibility(page, '#mfa-gauth')
-
     await cas.assertVisibility(page, '#mfa-yubikey')
-
     await browser.close();
 })();
