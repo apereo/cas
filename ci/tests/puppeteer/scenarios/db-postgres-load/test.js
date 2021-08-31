85b75892cb24d68a0871a8ba5c019b61ffc0c9fc
Hal Deadman <hal.deadman@gmail.com>
2021-08-31 14:09:33 -0400

    add postgres test and make postgres use text column for tgt

--- ci/tests/puppeteer/scenarios/db-postgres-load/test.js
+++ ci/tests/puppeteer/scenarios/db-postgres-load/test.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
 const cas = require('../../cas.js');
 const colors = require("colors");
+const assert = require("assert");
 
 (async () => {
     const browser = await puppeteer.launch(cas.browserOptions());
@@ -12,6 +13,16 @@ const colors = require("colors");
     await cas.assertTicketGrantingCookie(page);
     await cas.assertPageTitle(page, "CAS - Central Authentication Service");
     await cas.assertInnerText(page, '#content div h2', "Log In Successful");
+
+    await page.goto("https://localhost:8443/cas/logout");
+
+    let url = await page.url()
+    console.log(`Page url: ${url}`)
+    assert(url === "https://localhost:8443/cas/logout")
+
+    await page.waitForTimeout(1000)
+    await cas.assertNoTicketGrantingCookie(page);
+
     await browser.close();
     console.log(colors.green(`Login test complete.`));
 })();
