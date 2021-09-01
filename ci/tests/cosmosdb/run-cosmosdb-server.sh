0e2dbf3f40691046d689f41d6eda066666159991
Misagh <mm1844@gmail.com>
2021-09-01 17:00:11 +0400

    verify cosmos tests via docker

--- ci/tests/cosmosdb/run-cosmosdb-server.sh
+++ ci/tests/cosmosdb/run-cosmosdb-server.sh
@@ -6,7 +6,6 @@ echo "Trying to determine Docker image for platform $dockerPlatform"
 if [[ $dockerPlatform =~ "windows" ]]; then
   md %LOCALAPPDATA%\CosmosDBEmulator\bind-mount
   docker run --name cosmosdb --memory 2GB \
-    --mount "type=bind,source=%LOCALAPPDATA%\CosmosDBEmulator\bind-mount,destination=C:\CosmosDB.Emulator\bind-mount" \
     -d --tty -p 8081:8081 -p 8900:8900 -p 8901:8901 -p 8902:8902 -p 10250:10250 -p 10251:10251 \
     -p 10252:10252 -p 10253:10253 -p 10254:10254 -p 10255:10255 -p 10256:10256 -p 10350:10350 \
     mcr.microsoft.com/cosmosdb/windows/azure-cosmos-emulator
