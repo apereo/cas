803674987bae47d03d0ed05ccd294e1bc6abb31d
Misagh <mm1844@gmail.com>
2021-09-01 21:32:48 +0400

    verify cosmos tests via docker

--- ci/tests/cosmosdb/run-cosmosdb-server.sh
+++ ci/tests/cosmosdb/run-cosmosdb-server.sh
@@ -4,22 +4,23 @@ dockerPlatform=$(docker version --format '{{json .Server.Os}}')
 echo "Trying to determine Docker image for platform $dockerPlatform"
 
 if [[ $dockerPlatform =~ "windows" ]]; then
-  md %LOCALAPPDATA%\CosmosDBEmulator\bind-mount
-  docker run --name cosmosdb --memory 2GB \
-    -d --tty -p 8081:8081 -p 8900:8900 -p 8901:8901 -p 8902:8902 -p 10250:10250 -p 10251:10251 \
-    -p 10252:10252 -p 10253:10253 -p 10254:10254 -p 10255:10255 -p 10256:10256 -p 10350:10350 \
-    mcr.microsoft.com/cosmosdb/windows/azure-cosmos-emulator
-else
-  ipaddr="$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}' | head -n 1)"
-  echo "System IP address is $ipaddr"
-  docker pull mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator
-  docker run --rm -p 8081:8081 -p 10251:10251 -p 10252:10252 -p 10253:10253 -p 10254:10254 \
-    -m 4g --cpus=2.0 --name=cosmosdb \
-    -e AZURE_COSMOS_EMULATOR_PARTITION_COUNT=4 \
-    -e AZURE_COSMOS_EMULATOR_ENABLE_DATA_PERSISTENCE=true \
-    -e AZURE_COSMOS_EMULATOR_IP_ADDRESS_OVERRIDE="$ipaddr" \
-    -d mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator
+#  docker run --name cosmosdb --memory 2GB \
+#    -d --tty -p 8081:8081 -p 8900:8900 -p 8901:8901 -p 8902:8902 -p 10250:10250 -p 10251:10251 \
+#    -p 10252:10252 -p 10253:10253 -p 10254:10254 -p 10255:10255 -p 10256:10256 -p 10350:10350 \
+#    mcr.microsoft.com/cosmosdb/windows/azure-cosmos-emulator
+  echo "Windows platform is not supported for CosmosDb"
+  exit 0
 fi
+
+ipaddr="$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}' | head -n 1)"
+echo "System IP address is $ipaddr"
+docker pull mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator
+docker run --rm -p 8081:8081 -p 10251:10251 -p 10252:10252 -p 10253:10253 -p 10254:10254 \
+  -m 4g --cpus=2.0 --name=cosmosdb \
+  -e AZURE_COSMOS_EMULATOR_PARTITION_COUNT=4 \
+  -e AZURE_COSMOS_EMULATOR_ENABLE_DATA_PERSISTENCE=true \
+  -e AZURE_COSMOS_EMULATOR_IP_ADDRESS_OVERRIDE="$ipaddr" \
+  -d mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator
 
 echo "Waiting for CosmosDb to come online..."
 sleep 30
