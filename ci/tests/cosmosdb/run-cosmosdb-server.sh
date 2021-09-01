fc21e94cb1f8c05770a3869065ddc99c68028def
Misagh <mm1844@gmail.com>
2021-09-01 15:35:42 +0400

    fix tests

--- ci/tests/cosmosdb/run-cosmosdb-server.sh
+++ ci/tests/cosmosdb/run-cosmosdb-server.sh
@@ -4,8 +4,8 @@ ipaddr="$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}' | head
 echo "System IP address is $ipaddr"
 docker pull mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator
 docker run --rm -p 8081:8081 -p 10251:10251 -p 10252:10252 -p 10253:10253 -p 10254:10254 \
-  -m 3g --cpus=2.0 --name=cosmosdb \
-  -e AZURE_COSMOS_EMULATOR_PARTITION_COUNT=2 \
+  -m 4g --cpus=2.0 --name=cosmosdb \
+  -e AZURE_COSMOS_EMULATOR_PARTITION_COUNT=4 \
   -e AZURE_COSMOS_EMULATOR_ENABLE_DATA_PERSISTENCE=true \
   -e AZURE_COSMOS_EMULATOR_IP_ADDRESS_OVERRIDE="$ipaddr" \
   -d mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator
