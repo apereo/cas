4b92c15b9b62cee63c52331c8f6cdeddf482230a
Misagh <mm1844@gmail.com>
2021-09-07 12:05:43 +0400

    remove excluded auto-config classes. should not be necessary

--- webapp/cas-server-webapp-init/src/main/java/org/apereo/cas/web/CasWebApplication.java
+++ webapp/cas-server-webapp-init/src/main/java/org/apereo/cas/web/CasWebApplication.java
@@ -11,8 +11,6 @@ import lombok.val;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.groovy.template.GroovyTemplateAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -32,10 +30,7 @@ import java.time.Instant;
  * @since 5.0.0
  */
 @EnableDiscoveryClient
-@SpringBootApplication(exclude = {
-    GroovyTemplateAutoConfiguration.class,
-    DataSourceAutoConfiguration.class
-}, proxyBeanMethods = false)
+@SpringBootApplication(proxyBeanMethods = false)
 @EnableConfigurationProperties(CasConfigurationProperties.class)
 @EnableAsync
 @EnableAspectJAutoProxy(proxyTargetClass = true)
