2c222f7d32791948b4aa53b43665e814df74a6ed
Misagh <mm1844@gmail.com>
2021-09-15 11:51:23 +0400

    allow entity id from saml request for saml attr release policies

--- support/cas-server-support-saml-idp/src/test/java/org/apereo/cas/support/saml/services/GroovySamlRegisteredServiceAttributeReleasePolicyTests.java
+++ support/cas-server-support-saml-idp/src/test/java/org/apereo/cas/support/saml/services/GroovySamlRegisteredServiceAttributeReleasePolicyTests.java
@@ -5,7 +5,9 @@ import org.apereo.cas.services.RegisteredServiceTestUtils;
 import org.apereo.cas.support.saml.BaseSamlIdPConfigurationTests;
 import org.apereo.cas.support.saml.SamlIdPTestUtils;
 import org.apereo.cas.support.saml.SamlProtocolConstants;
+import org.apereo.cas.support.saml.SamlUtils;
 import org.apereo.cas.util.CollectionUtils;
+import org.apereo.cas.util.EncodingUtils;
 import org.apereo.cas.util.HttpRequestUtils;
 
 import lombok.val;
@@ -15,6 +17,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.TestPropertySource;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -80,4 +83,24 @@ public class GroovySamlRegisteredServiceAttributeReleasePolicyTests extends Base
         assertFalse(attributes.isEmpty());
     }
 
+    @Test
+    public void verifyScriptReleasesSamlAttributesWithSamlRequest() throws Exception {
+        val filter = new GroovySamlRegisteredServiceAttributeReleasePolicy();
+        filter.setGroovyScript("classpath:saml-groovy-attrs.groovy");
+        filter.setAllowedAttributes(CollectionUtils.wrapList("uid", "givenName", "displayName"));
+        val registeredService = SamlIdPTestUtils.getSamlRegisteredService();
+        registeredService.setAttributeReleasePolicy(filter);
+
+        val request = (MockHttpServletRequest) HttpRequestUtils.getHttpServletRequestFromRequestAttributes();
+        request.removeParameter(SamlProtocolConstants.PARAMETER_ENTITY_ID);
+
+        val authnRequest = SamlIdPTestUtils.getAuthnRequest(openSamlConfigBean, registeredService);
+        try (val writer = SamlUtils.transformSamlObject(openSamlConfigBean, authnRequest)) {
+            val samlRequest = EncodingUtils.encodeBase64(writer.toString().getBytes(StandardCharsets.UTF_8));
+            val service = RegisteredServiceTestUtils.getService();
+            service.getAttributes().put(SamlProtocolConstants.PARAMETER_SAML_REQUEST, List.of(samlRequest));
+            val attributes = filter.getAttributes(CoreAuthenticationTestUtils.getPrincipal(), service, registeredService);
+            assertFalse(attributes.isEmpty());
+        }
+    }
 }
