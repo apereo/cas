a37208c49b93e7bd68dc4f0431591bdd4b4b237c
Misagh <mm1844@gmail.com>
2021-09-23 20:38:34 +0400

    fix tests

--- support/cas-server-support-saml-idp-web/src/main/java/org/apereo/cas/support/saml/web/idp/profile/builders/nameid/SamlProfileSamlNameIdBuilder.java
+++ support/cas-server-support-saml-idp-web/src/main/java/org/apereo/cas/support/saml/web/idp/profile/builders/nameid/SamlProfileSamlNameIdBuilder.java
@@ -83,7 +83,7 @@ public class SamlProfileSamlNameIdBuilder extends AbstractSaml20ObjectBuilder im
     }
 
     private static String parseAndBuildRequiredNameIdFormat(final SamlRegisteredService service) {
-        val fmt = StringUtils.defaultString(service.getRequiredNameIdFormat()).trim();
+        val fmt = StringUtils.defaultString(service.getRequiredNameIdFormat(), NameIDType.UNSPECIFIED).trim();
         LOGGER.debug("Required NameID format assigned to service [{}] is [{}]", service.getName(), fmt);
 
         if (StringUtils.containsIgnoreCase(NameIDType.EMAIL, fmt)) {
