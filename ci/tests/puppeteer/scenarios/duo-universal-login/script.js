3f84e2a0dc2a03e0057870f89b23c9a07dc17918
Misagh <mm1844@gmail.com>
2021-10-22 17:18:36 +0400

    fix duo puppeteer tests

--- ci/tests/puppeteer/scenarios/duo-universal-login/script.js
+++ ci/tests/puppeteer/scenarios/duo-universal-login/script.js
@@ -5,9 +5,8 @@ const cas = require('../../cas.js');
     const browser = await puppeteer.launch(cas.browserOptions());
     const page = await cas.newPage(browser);
     await page.goto("https://localhost:8443/cas/login?authn_method=mfa-duo");
-    await cas.loginWith(page, "casuser", "Mellon");
-    await cas.loginDuoSecurityBypassCode(page, 'universal-prompt');
-    await page.waitForTimeout(3000)
+    await cas.loginWith(page, "duobypass", "Mellon");
+    await page.waitForTimeout(5000)
     await cas.screenshot(page);
     await cas.assertInnerText(page, '#content div h2', "Log In Successful");
     await cas.assertTicketGrantingCookie(page);
