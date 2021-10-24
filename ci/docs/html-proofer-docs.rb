f731682f84a4cc9594e94d574a4b12e12590544e
Misagh <mm1844@gmail.com>
2021-10-24 16:54:09 +0400

    fix doc validation

--- ci/docs/html-proofer-docs.rb
+++ ci/docs/html-proofer-docs.rb
@@ -2,32 +2,48 @@ require 'html-proofer'
 require 'find'
 require 'fileutils'
 
+system("clear") || system("cls")
+
+ROOT_DIRECTORY="cas"
+VERSION="development"
+
 options = {
   :typhoeus => { followlocation: true },
   :disable_external => false,
   :allow_hash_href => true,
-  :only_4xx => true,
+  # :only_4xx => true,
   :empty_alt_ignore => true,
-  :http_status_ignore => [401],
+  :http_status_ignore => [401,429,301,302],
   :parallel => { :in_processes => 3},
   :url_swap => {
     %r{^/cas/} => '/'
   }, 
   :url_ignore => [
-    %r{^/\d+.\d+.x},
-    %r{^/development/},
-    %r{Older-Versions},
+    %r{/*\d+.\d+.x/},
     %r{^#.+},
-    %r{^../images/}
+    %r{localhost},
+    %r{/cas/development/$},
+    %r{(images|javascripts|stylesheets)}
   ],
   :verbose => true
 }
 
-files = Dir.glob("/root/docs/**/*.html")
+Dir.mkdir(ROOT_DIRECTORY) unless File.exist?(ROOT_DIRECTORY)
+FileUtils.rm_r("#{ROOT_DIRECTORY}/#{VERSION}") if File.exist?("#{ROOT_DIRECTORY}/#{VERSION}")
+FileUtils.rm_r("#{ROOT_DIRECTORY}/developer") if File.exist?("#{ROOT_DIRECTORY}/developer")
+
+# Copy project documentation
+FileUtils.cp_r("/root/docs/#{VERSION}", "#{ROOT_DIRECTORY}")
+FileUtils.cp_r("/root/docs/developer", "#{ROOT_DIRECTORY}/developer")
+files = Dir.glob("/root/docs/*.html")
 for file in files 
-  dir = File.dirname(file)
-  if dir.end_with?("/docs") || file.include?("development")
-   puts "Checking file #{file}"
-   HTMLProofer.check_file(file, options).run
-  end
+  FileUtils.cp(file, ROOT_DIRECTORY)
 end
+
+# files = Dir.glob("#{ROOT_DIRECTORY}/**/*.html")
+# for file in files 
+#   puts file
+# end
+
+puts "Checking files..."
+HTMLProofer.check_directory("./#{ROOT_DIRECTORY}", options).run
