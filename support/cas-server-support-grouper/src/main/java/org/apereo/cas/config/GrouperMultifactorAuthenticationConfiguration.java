5ffab7317ed6ce4d4ebe1d7bdc73ca69f5eeab4e
Misagh <mm1844@gmail.com>
2021-10-28 15:31:17 +0400

    move grouper person directory dao to grouper support module

--- support/cas-server-support-grouper/src/main/java/org/apereo/cas/config/GrouperMultifactorAuthenticationConfiguration.java
+++ support/cas-server-support-grouper/src/main/java/org/apereo/cas/config/GrouperMultifactorAuthenticationConfiguration.java
@@ -44,13 +44,15 @@ public class GrouperMultifactorAuthenticationConfiguration {
     @Bean
     @RefreshScope(proxyMode = ScopedProxyMode.DEFAULT)
     @Autowired
-    public MultifactorAuthenticationTrigger grouperMultifactorAuthenticationTrigger(final CasConfigurationProperties casProperties,
-                                                                                    final ConfigurableApplicationContext applicationContext,
-                                                                                    @Qualifier("grouperFacade")
-                                                                                    final GrouperFacade grouperFacade,
-                                                                                    @Qualifier("multifactorAuthenticationProviderResolver")
-                                                                                    final MultifactorAuthenticationProviderResolver multifactorAuthenticationProviderResolver) {
-        return new GrouperMultifactorAuthenticationTrigger(casProperties, multifactorAuthenticationProviderResolver, grouperFacade, applicationContext);
+    public MultifactorAuthenticationTrigger grouperMultifactorAuthenticationTrigger(
+        final CasConfigurationProperties casProperties,
+        final ConfigurableApplicationContext applicationContext,
+        @Qualifier("grouperFacade")
+        final GrouperFacade grouperFacade,
+        @Qualifier("multifactorAuthenticationProviderResolver")
+        final MultifactorAuthenticationProviderResolver multifactorAuthenticationProviderResolver) {
+        return new GrouperMultifactorAuthenticationTrigger(casProperties,
+            multifactorAuthenticationProviderResolver, grouperFacade, applicationContext);
     }
 
     @Bean
@@ -62,7 +64,8 @@ public class GrouperMultifactorAuthenticationConfiguration {
         final MultifactorAuthenticationTrigger grouperMultifactorAuthenticationTrigger,
         @Qualifier("casWebflowConfigurationContext")
         final CasWebflowEventResolutionConfigurationContext casWebflowConfigurationContext) {
-        val r = new DefaultMultifactorAuthenticationProviderWebflowEventResolver(casWebflowConfigurationContext, grouperMultifactorAuthenticationTrigger);
+        val r = new DefaultMultifactorAuthenticationProviderWebflowEventResolver(
+            casWebflowConfigurationContext, grouperMultifactorAuthenticationTrigger);
         LOGGER.debug("Activating MFA event resolver based on Grouper groups...");
         initialAuthenticationAttemptWebflowEventResolver.addDelegate(r);
         return r;
