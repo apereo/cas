77837e969cabff764c8d9ae8bee822b6e59f078f
Misagh <mm1844@gmail.com>
2021-10-31 19:11:50 +0400

    fix checkstyle

--- core/cas-server-core/src/test/java/org/apereo/cas/config/CasRegisteredServicesTestConfiguration.java
+++ core/cas-server-core/src/test/java/org/apereo/cas/config/CasRegisteredServicesTestConfiguration.java
@@ -1,36 +1,14 @@
 package org.apereo.cas.config;
 
-import org.apereo.cas.TestOneTimePasswordAuthenticationHandler;
-import org.apereo.cas.authentication.AcceptUsersAuthenticationHandler;
 import org.apereo.cas.authentication.principal.RegisteredServicePrincipalAttributesRepository;
 import org.apereo.cas.authentication.principal.cache.CachingPrincipalAttributesRepository;
-import org.apereo.cas.services.AnonymousRegisteredServiceUsernameAttributeProvider;
-import org.apereo.cas.services.AnyAuthenticationHandlerRegisteredServiceAuthenticationPolicyCriteria;
-import org.apereo.cas.services.DefaultRegisteredServiceAccessStrategy;
-import org.apereo.cas.services.DefaultRegisteredServiceAuthenticationPolicy;
-import org.apereo.cas.services.DefaultRegisteredServiceDelegatedAuthenticationPolicy;
-import org.apereo.cas.services.DefaultRegisteredServiceProperty;
-import org.apereo.cas.services.DefaultRegisteredServiceUsernameProvider;
-import org.apereo.cas.services.PrincipalAttributeRegisteredServiceUsernameProvider;
-import org.apereo.cas.services.RefuseRegisteredServiceProxyPolicy;
-import org.apereo.cas.services.RegexMatchingRegisteredServiceProxyPolicy;
-import org.apereo.cas.services.RegisteredServiceProperty;
-import org.apereo.cas.services.RegisteredServicePublicKeyImpl;
 import org.apereo.cas.services.RegisteredServiceTestUtils;
-import org.apereo.cas.services.ReturnAllAttributeReleasePolicy;
-import org.apereo.cas.services.ReturnAllowedAttributeReleasePolicy;
-import org.apereo.cas.services.consent.DefaultRegisteredServiceConsentPolicy;
-import org.apereo.cas.util.CollectionUtils;
 
-import lombok.val;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
 
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 /**
