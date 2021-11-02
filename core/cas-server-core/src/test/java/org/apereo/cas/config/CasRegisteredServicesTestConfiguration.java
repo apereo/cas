b3b7a026fbf5e87935501cd3258eee654fb3eeca
Misagh <mm1844@gmail.com>
2021-11-02 09:56:25 +0400

    minor test adjustments

--- core/cas-server-core/src/test/java/org/apereo/cas/config/CasRegisteredServicesTestConfiguration.java
+++ core/cas-server-core/src/test/java/org/apereo/cas/config/CasRegisteredServicesTestConfiguration.java
@@ -4,9 +4,11 @@ import org.apereo.cas.authentication.principal.RegisteredServicePrincipalAttribu
 import org.apereo.cas.authentication.principal.cache.CachingPrincipalAttributesRepository;
 import org.apereo.cas.services.RegisteredServiceTestUtils;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 
 import java.util.List;
 
@@ -26,6 +28,7 @@ public class CasRegisteredServicesTestConfiguration {
 
     @ConditionalOnMissingBean(name = "inMemoryRegisteredServices")
     @Bean
+    @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public List inMemoryRegisteredServices() throws Exception {
         return RegisteredServiceTestUtils.getRegisteredServicesForTests();
     }
