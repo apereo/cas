2ae52910b846d20a1116aee55d16bcb1c205845b
Misagh <mm1844@gmail.com>
2021-11-05 12:21:10 +0400

    use the correct context factory for servlet initializer

--- webapp/cas-server-webapp-init/src/main/java/org/apereo/cas/web/CasWebApplicationServletInitializer.java
+++ webapp/cas-server-webapp-init/src/main/java/org/apereo/cas/web/CasWebApplicationServletInitializer.java
@@ -3,6 +3,9 @@ package org.apereo.cas.web;
 import org.apereo.cas.CasEmbeddedContainerUtils;
 import org.apereo.cas.util.spring.boot.AbstractCasSpringBootServletInitializer;
 
+import lombok.val;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 import java.util.List;
 
 /**
@@ -14,7 +17,16 @@ import java.util.List;
 public class CasWebApplicationServletInitializer extends AbstractCasSpringBootServletInitializer {
 
     public CasWebApplicationServletInitializer() {
-        super(List.of(CasWebApplication.class), CasEmbeddedContainerUtils.getCasBannerInstance(), CasEmbeddedContainerUtils.getApplicationStartup());
+        super(List.of(CasWebApplication.class),
+            CasEmbeddedContainerUtils.getCasBannerInstance(),
+            CasEmbeddedContainerUtils.getApplicationStartup());
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(final SpringApplicationBuilder givenBuilder) {
+        val builder = super.configure(givenBuilder);
+        builder.contextFactory(webApplicationType -> new CasWebApplicationContext());
+        return builder;
     }
 }
 
