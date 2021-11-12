eca627ac9386a50db0686d3c034f8189f744cc57
Hal Deadman <hal.deadman@gmail.com>
2021-11-12 09:50:50 -0500

    add javadoc

--- support/cas-server-support-x509-webflow/src/main/java/org/apereo/cas/web/flow/X509CertificateCredentialsNonInteractiveAction.java
+++ support/cas-server-support-x509-webflow/src/main/java/org/apereo/cas/web/flow/X509CertificateCredentialsNonInteractiveAction.java
@@ -30,7 +30,11 @@ public class X509CertificateCredentialsNonInteractiveAction extends AbstractNonI
      * Attribute to indicate the x509 certificate.
      */
     public static final String REQUEST_ATTRIBUTE_X509_CERTIFICATE = "javax.servlet.request.X509Certificate";
-    
+
+    /**
+     * Attribute that indicates an error has occurred. Used to break out of the flow and send user to a page
+     * with an error message.
+     */
     public static final String REQUEST_ATTRIBUTE_X509_ERROR = "X509CertificateAuthenticationError";
 
     /**
