634792111f20a7e974b949fbf82cbf448927a915
Misagh <mm1844@gmail.com>
2021-11-23 11:56:11 +0400

    refactor+cleanup

--- support/cas-server-support-trusted-mfa-redis/src/main/java/org/apereo/cas/trusted/authentication/storage/RedisMultifactorAuthenticationTrustStorage.java
+++ support/cas-server-support-trusted-mfa-redis/src/main/java/org/apereo/cas/trusted/authentication/storage/RedisMultifactorAuthenticationTrustStorage.java
@@ -16,6 +16,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -33,10 +34,11 @@ public class RedisMultifactorAuthenticationTrustStorage extends BaseMultifactorA
 
     private final RedisTemplate<String, List<MultifactorAuthenticationTrustRecord>> redisTemplate;
 
-    public RedisMultifactorAuthenticationTrustStorage(final TrustedDevicesMultifactorProperties properties,
-                                                      final CipherExecutor<Serializable, String> cipherExecutor,
-                                                      final RedisTemplate<String, List<MultifactorAuthenticationTrustRecord>> redisTemplate,
-                                                      final MultifactorAuthenticationTrustRecordKeyGenerator keyGenerationStrategy) {
+    public RedisMultifactorAuthenticationTrustStorage(
+        final TrustedDevicesMultifactorProperties properties,
+        final CipherExecutor<Serializable, String> cipherExecutor,
+        final RedisTemplate<String, List<MultifactorAuthenticationTrustRecord>> redisTemplate,
+        final MultifactorAuthenticationTrustRecordKeyGenerator keyGenerationStrategy) {
         super(properties, cipherExecutor, keyGenerationStrategy);
         this.redisTemplate = redisTemplate;
     }
@@ -81,17 +83,6 @@ public class RedisMultifactorAuthenticationTrustStorage extends BaseMultifactorA
                     redisTemplate.delete(Objects.requireNonNull(recordKeys));
                 });
         }
-    }
-
-    private Set<? extends MultifactorAuthenticationTrustRecord> getFromRedisKeys(final Set<String> keys) {
-        if (keys != null) {
-            return keys.stream()
-                .map(redisKey -> redisTemplate.boundValueOps(redisKey).get())
-                .filter(Objects::nonNull)
-                .flatMap(List::stream)
-                .collect(Collectors.toSet());
-        }
-        return new HashSet<>(0);
     }
 
     @Override
@@ -139,5 +130,15 @@ public class RedisMultifactorAuthenticationTrustStorage extends BaseMultifactorA
         results.add(record);
         redisTemplate.boundValueOps(redisKey).set(results);
         return record;
+    }
+
+    private Set<? extends MultifactorAuthenticationTrustRecord> getFromRedisKeys(final Set<String> keys) {
+        return Optional.ofNullable(keys)
+            .map(givenKeys -> givenKeys.stream()
+                .map(redisKey -> redisTemplate.boundValueOps(redisKey).get())
+                .filter(Objects::nonNull)
+                .flatMap(List::stream)
+                .collect(Collectors.toSet()))
+            .orElseGet(() -> new HashSet<>(0));
     }
 }
