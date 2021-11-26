e57e32c2cfe2b0fef65ffac3987d5dc5385a5493
Misagh <mm1844@gmail.com>
2021-11-26 18:58:52 +0400

    add test for config profiles and properties

---
+++ ci/tests/puppeteer/scenarios/configuration-properties-profiles/script.js
@@ -0,0 +1,28 @@
+const cas = require('../../cas.js');
+const assert = require("assert");
+
+(async () => {
+     await sendRequest("cas.custom.properties.all", "everything")
+     // await sendRequest("cas.custom.properties.direct", "directValue")
+     await sendRequest("cas.custom.properties.groovy1", "custom1")
+     await sendRequest("cas.custom.properties.groovy2", "custom2")
+     await sendRequest("cas.custom.properties.environment", "custom1")
+     await sendRequest("cas.custom.properties.profile", "custom2")
+     await sendRequest("cas.custom.properties.source", "yaml")
+})();
+
+async function sendRequest(key, value) {
+    await cas.logg(`Asking for property ${key}`)
+    await cas.doGet(`https://localhost:8443/cas/actuator/env/${key}`,
+        function(res) {
+            assert(res.status === 200)
+            let casSource = res.data.propertySources[2];
+            assert(casSource !== null);
+            assert(casSource.name === "bootstrapProperties-casCompositePropertySource")
+            cas.logg(`Comparing ${casSource.property.value} with ${value}`);
+            assert(casSource.property.value === value)
+        },
+        function(error) {
+            throw error;
+        })
+}
