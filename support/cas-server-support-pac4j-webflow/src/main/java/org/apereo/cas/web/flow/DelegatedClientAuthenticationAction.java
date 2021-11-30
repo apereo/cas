cbb167669d540a9b8efd06d2d9b9a1b37d8e51d9
Jérôme LELEU <leleuj@gmail.com>
2021-11-30 15:53:52 +0100

    Open some CAS classes

--- support/cas-server-support-pac4j-webflow/src/main/java/org/apereo/cas/web/flow/DelegatedClientAuthenticationAction.java
+++ support/cas-server-support-pac4j-webflow/src/main/java/org/apereo/cas/web/flow/DelegatedClientAuthenticationAction.java
@@ -52,7 +52,10 @@ import java.util.stream.Stream;
 @Slf4j
 @Getter
 public class DelegatedClientAuthenticationAction extends AbstractAuthenticationAction {
-    private final DelegatedClientAuthenticationConfigurationContext configContext;
+    /**
+     * The configuration context.
+     */
+    protected final DelegatedClientAuthenticationConfigurationContext configContext;
 
     private final DelegatedClientAuthenticationWebflowManager delegatedClientAuthenticationWebflowManager;
 
