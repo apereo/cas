1d4712dfa80ec0add5b906a7bc78d95647641e2b
Misagh <mm1844@gmail.com>
2021-12-22 09:42:31 +0400

    fix tests

--- testcas.sh
+++ testcas.sh
@@ -45,7 +45,7 @@ parallel="--parallel "
 dryRun=""
 info=""
 gradleCmd="./gradlew"
-flags="--no-daemon --configure-on-demand --build-cache -x javadoc -x check -DskipNestedConfigMetadataGen=true "
+flags="--no-daemon --configure-on-demand --build-cache -x javadoc -x check -DskipNestedConfigMetadataGen=true -Dverbose=true "
 coverageTask=""
 
 while (( "$#" )); do
