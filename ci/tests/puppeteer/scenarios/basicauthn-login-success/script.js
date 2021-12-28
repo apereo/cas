6858a4db008cd5428a59d374cad11038961ab2ba
Misagh <mm1844@gmail.com>
2021-12-28 14:00:33 +0400

    add puppeteer test case for basic authn

---
+++ ci/tests/puppeteer/scenarios/basicauthn-login-success/script.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer');
+const cas = require('../../cas.js');
+const assert = require('assert');
+
+(async () => {
+    let value = `casuser:Mellon`;
+    let buff = Buffer.alloc(value.length, value);
+    let authzHeader = `Basic ${buff.toString('base64')}`;
+    console.log(`Authorization header: ${authzHeader}`);
+    await cas.doGet("https://localhost:8443/cas/login?service=https://apereo.github.io",
+        function(res) {
+            assert(res.status === 200)
+            assert(res.request.host === "apereo.github.io")
+            assert(res.request.protocol === "https:")
+            assert(res.request.method === "GET")
+            assert(res.request.path.includes("/?ticket=ST-"));
+        },
+        function(error) {
+            throw error;
+        }, {
+           'Authorization': authzHeader
+        });
+})();
