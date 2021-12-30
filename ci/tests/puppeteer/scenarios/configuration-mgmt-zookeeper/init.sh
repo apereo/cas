b06c8ff2ce43d9787187c1be90360ee698bc9dec
Misagh <mm1844@gmail.com>
2021-12-30 20:37:26 +0400

    add puppeteer test case for zookeeper

---
+++ ci/tests/puppeteer/scenarios/configuration-mgmt-zookeeper/init.sh
@@ -0,0 +1,18 @@
+set -e
+echo Starting Apache ZooKeeper Server
+${PWD}/ci/tests/zookeeper/run-zookeeper-server.sh
+
+docker exec zookeeper zkCli.sh -server localhost:2181 create /config  &> /dev/null
+docker exec zookeeper zkCli.sh -server localhost:2181 create /config/cas &> /dev/null
+docker exec zookeeper zkCli.sh -server localhost:2181 create /config/cas/cas &> /dev/null
+docker exec zookeeper zkCli.sh -server localhost:2181 create /config/cas/cas/audit &> /dev/null
+docker exec zookeeper zkCli.sh -server localhost:2181 create /config/cas/cas/audit/engine &> /dev/null
+docker exec zookeeper zkCli.sh -server localhost:2181 create /config/cas/cas/audit/engine/enabled &> /dev/null
+docker exec zookeeper zkCli.sh -server localhost:2181 set /config/cas/cas/audit/engine/enabled "false" &> /dev/null
+
+docker exec zookeeper zkCli.sh -server localhost:2181 create /config/cas/cas/authn &> /dev/null
+docker exec zookeeper zkCli.sh -server localhost:2181 create /config/cas/cas/authn/accept &> /dev/null
+docker exec zookeeper zkCli.sh -server localhost:2181 create /config/cas/cas/authn/accept/users &> /dev/null
+docker exec zookeeper zkCli.sh -server localhost:2181 set /config/cas/cas/authn/accept/users "zookeeper::p@SSword" &> /dev/null
+
+echo -e "\n\nReady!"
