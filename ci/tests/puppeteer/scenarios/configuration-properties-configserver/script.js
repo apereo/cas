f147be2bc0079990ed772cd913e3644264299b71
Misagh <mm1844@gmail.com>
2022-01-05 11:12:34 +0400

    add puppeteer test case for spring cloud config server

--- ci/tests/puppeteer/scenarios/configuration-properties-configserver/script.js
+++ ci/tests/puppeteer/scenarios/configuration-properties-configserver/script.js
@@ -1,7 +1,15 @@
 const puppeteer = require('puppeteer');
 const cas = require('../../cas.js');
+const assert = require("assert");
 
 (async () => {
+
+    await cas.doGet("http://casuser:Mellon@localhost:8888/casconfigserver/cas/dev",
+        res => {
+            assert(res.status === 200)
+        }, err => {
+            throw err;
+        })
     const browser = await puppeteer.launch(cas.browserOptions());
     const page = await cas.newPage(browser);
     await page.goto("https://localhost:8443/cas/login?service=https://apereo.github.io");
