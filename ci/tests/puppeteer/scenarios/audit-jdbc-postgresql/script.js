59063748ae36d915eea99de2d70b5fb30da9cec1
Misagh <mm1844@gmail.com>
2022-01-05 13:33:20 +0400

    add puppeteer test case for jdbc audit with postgres

---
+++ ci/tests/puppeteer/scenarios/audit-jdbc-postgresql/script.js
@@ -0,0 +1,29 @@
+const puppeteer = require('puppeteer');
+const cas = require('../../cas.js');
+const assert = require("assert");
+
+(async () => {
+    const browser = await puppeteer.launch(cas.browserOptions());
+    const page = await cas.newPage(browser);
+    await page.goto("https://localhost:8443/cas/login");
+    await cas.loginWith(page, "casuser", "Mellon");
+    await cas.assertTicketGrantingCookie(page);
+    await cas.assertPageTitle(page, "CAS - Central Authentication Service Log In Successful");
+    await cas.assertInnerText(page, '#content div h2', "Log In Successful");
+    await browser.close();
+
+    await cas.doPost("https://localhost:8443/cas/actuator/auditLog", {}, {
+        'Content-Type': 'application/json'
+    }, res => {
+        assert(res.data.length === 4);
+        assert(res.data[0].principal !== null)
+        assert(res.data[0].actionPerformed !== null)
+        assert(res.data[0].applicationCode !== null)
+        assert(res.data[0].clientIpAddress !== null)
+        assert(res.data[0].serverIpAddress !== null)
+        assert(res.data[0].resourceOperatedUpon !== null)
+    }, error => {
+        throw(error);
+    })
+    
+})();
