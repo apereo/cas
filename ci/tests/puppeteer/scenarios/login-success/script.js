d9ff3c521acb8b8c36c078cdb25177ee37c7c7c5
Misagh <mm1844@gmail.com>
2022-01-14 11:57:30 +0330

    add puppeteer test case

--- ci/tests/puppeteer/scenarios/login-success/script.js
+++ ci/tests/puppeteer/scenarios/login-success/script.js
@@ -1,10 +1,19 @@
 const puppeteer = require('puppeteer');
 const cas = require('../../cas.js');
+const assert = require("assert");
 
 (async () => {
     const browser = await puppeteer.launch(cas.browserOptions());
     const page = await cas.newPage(browser);
     await page.goto("https://localhost:8443/cas/login");
+
+    await page.focus("#username");
+    await page.keyboard.press("Tab");
+    await page.focus("#password");
+    await page.keyboard.press("Tab");
+
+    await cas.assertVisibility(page, "#usernameValidationMessage");
+    await cas.assertVisibility(page, "#passwordValidationMessage");
 
     await cas.loginWith(page, "casuser", "Mellon");
 
