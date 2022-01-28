83ba214e3f5411514111b477e7618d9196968c5b
Misagh <mm1844@gmail.com>
2022-01-28 18:20:04 +0400

    fix build issues

--- support/cas-server-support-oidc/src/test/java/org/apereo/cas/oidc/ticket/OidcPushedAuthorizationRequestResponseBuilderTests.java
+++ support/cas-server-support-oidc/src/test/java/org/apereo/cas/oidc/ticket/OidcPushedAuthorizationRequestResponseBuilderTests.java
@@ -15,7 +15,6 @@ import org.junit.jupiter.api.Test;
 import org.pac4j.core.context.JEEContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
 import static org.junit.jupiter.api.Assertions.*;
