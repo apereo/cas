29b57409aa02ca99b4bcc7bae9355df712e069e0
Misagh <mm1844@gmail.com>
2022-02-25 14:56:26 +0400

    fix build issues

--- support/cas-server-support-saml-idp/src/main/java/org/apereo/cas/config/SamlIdPDelegatedAuthenticationConfiguration.java
+++ support/cas-server-support-saml-idp/src/main/java/org/apereo/cas/config/SamlIdPDelegatedAuthenticationConfiguration.java
@@ -12,8 +12,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ScopedProxyMode;
 
 /**
  * This is {@link SamlIdPDelegatedAuthenticationConfiguration}.
