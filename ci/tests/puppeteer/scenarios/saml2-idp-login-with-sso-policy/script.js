124a4be7dae94cf30eaec5c4c70cf7897a39f466
Misagh <mm1844@gmail.com>
2022-03-11 14:04:15 +0400

    improve saml2 idp tests with docker-based SP

--- ci/tests/puppeteer/scenarios/saml2-idp-login-with-sso-policy/script.js
+++ ci/tests/puppeteer/scenarios/saml2-idp-login-with-sso-policy/script.js
@@ -10,15 +10,9 @@ const cas = require('../../cas.js');
     await page.goto("https://localhost:8443/cas/login");
     await page.waitForTimeout(1000);
     await cas.loginWith(page, "casuser", "Mellon");
-
-    await cas.uploadSamlMetadata(page, path.join(__dirname, '/saml-md/idp-metadata.xml'));
-
-    await page.goto("https://samltest.id/start-idp-test/");
-    await cas.type(page,'input[name=\'entityID\']', "https://cas.apereo.org/saml/idp");
-    // await page.waitForTimeout(1000)
-    await cas.click(page, "input[type='submit']")
-    await page.waitForNavigation();
-    await page.waitForTimeout(3000)
+    
+    await page.goto("http://localhost:9443/simplesaml/module.php/core/authenticate.php?as=default-sp");
+    await page.waitForTimeout(2000)
     await cas.removeDirectory(path.join(__dirname, '/saml-md'));
     await cas.assertVisibility(page, '#username')
     await cas.assertVisibility(page, '#password')
