011b501b3ca536b225319f9f5a7a5bad75d563a0
Misagh <mm1844@gmail.com>
2022-03-11 13:58:30 +0400

    improve saml2 idp tests with docker-based SP

--- ci/tests/puppeteer/scenarios/saml2-idp-login-idp-initiated/script.js
+++ ci/tests/puppeteer/scenarios/saml2-idp-login-idp-initiated/script.js
@@ -5,18 +5,16 @@ const cas = require('../../cas.js');
 (async () => {
     const browser = await puppeteer.launch(cas.browserOptions());
     const page = await cas.newPage(browser);
-    await cas.uploadSamlMetadata(page, path.join(__dirname, '/saml-md/idp-metadata.xml'));
-    let entityId = "https://samltest.id/saml/sp";
+    
+    const entityId = "http://localhost:9443/simplesaml/module.php/saml/sp/metadata.php/default-sp";
     let url = "https://localhost:8443/cas/idp/profile/SAML2/Unsolicited/SSO";
     url += `?providerId=${entityId}`;
     url += "&target=https%3A%2F%2Flocalhost%3A8443%2Fcas%2Flogin";
-
     console.log(`Navigating to ${url}`);
     await page.goto(url);
     await page.waitForTimeout(1000)
     await cas.loginWith(page, "casuser", "Mellon");
-    await page.waitForTimeout(2000)
-
+    await page.waitForTimeout(1000)
     await cas.assertPageTitle(page, "CAS - Central Authentication Service Log In Successful");
     await cas.assertInnerText(page, '#content div h2', "Log In Successful");
     await cas.removeDirectory(path.join(__dirname, '/saml-md'));
