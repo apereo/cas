bb9d85350a3a2e04bef8961a591d48d3a73f7441
Misagh <mm1844@gmail.com>
2022-03-11 14:07:42 +0400

    improve saml2 idp tests with docker-based SP

--- ci/tests/puppeteer/scenarios/saml2-idp-login-idp-initiated-relaystate/script.js
+++ ci/tests/puppeteer/scenarios/saml2-idp-login-idp-initiated-relaystate/script.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cas = require('../../cas.js');
 
 async function unsolicited(page, target) {
-    const entityId = "https://samltest.id/saml/sp";
+    const entityId = "http://localhost:9443/simplesaml/module.php/saml/sp/metadata.php/default-sp";
 
     let url = "https://localhost:8443/cas/idp/profile/SAML2/Unsolicited/SSO";
     url += `?providerId=${entityId}`;
@@ -22,7 +22,6 @@ async function unsolicited(page, target) {
 (async () => {
     const browser = await puppeteer.launch(cas.browserOptions());
     const page = await browser.newPage();
-    await cas.uploadSamlMetadata(page, path.join(__dirname, '/saml-md/idp-metadata.xml'));
 
     await page.goto("https://localhost:8443/cas/login");
     await cas.loginWith(page, "casuser", "Mellon");
