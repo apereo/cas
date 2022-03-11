8406dc39930687ea1483497772d6935d3de46ddf
Misagh <mm1844@gmail.com>
2022-03-11 13:09:57 +0400

    improve support for saml2 idp tests with simplesamlphp

--- ci/tests/saml2/run-saml-server.sh
+++ ci/tests/saml2/run-saml-server.sh
@@ -19,9 +19,14 @@ else
 fi
 
 docker stop simplesamlphp-idp || true && docker rm simplesamlphp-idp || true
-echo -e "Running SAML2 IdP with entity id ${SP_ENTITY_ID} and ACS service ${SP_ACS_SERVICE} on port 9443"
+echo -e "Running SAML2 IdP with SP entity id ${SP_ENTITY_ID} and SP ACS service ${SP_ACS_SERVICE} on port 9443"
+
 docker run --name=simplesamlphp-idp -p 9443:8080 \
   -e SIMPLESAMLPHP_SP_ENTITY_ID="${SP_ENTITY_ID}" \
   -e SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE="${SP_ACS_SERVICE}" \
   -e SIMPLESAMLPHP_SP_SINGLE_LOGOUT_SERVICE="${SP_SLO_SERVICE}" \
+  -e IDP_ENCRYPTION_CERTIFICATE="${IDP_ENCRYPTION_CERTIFICATE}" \
+  -e IDP_SIGNING_CERTIFICATE="${IDP_SIGNING_CERTIFICATE}" \
+  -v $PWD/ci/tests/saml2/saml20-idp-remote.php:/var/www/simplesamlphp/metadata/saml20-idp-remote.php \
+  -v $PWD/ci/tests/saml2/authsources.php:/var/www/simplesamlphp/config/authsources.php \
   -d kenchan0130/simplesamlphp
