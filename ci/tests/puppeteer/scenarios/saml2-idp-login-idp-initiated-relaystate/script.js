e2e6e5d0ce46168c87214a13b41514fca5ddc60e
Misagh <mm1844@gmail.com>
2022-03-12 11:00:46 +0400

    fix tests

--- ci/tests/puppeteer/scenarios/saml2-idp-login-idp-initiated-relaystate/script.js
+++ ci/tests/puppeteer/scenarios/saml2-idp-login-idp-initiated-relaystate/script.js
@@ -24,6 +24,7 @@ async function unsolicited(page, target) {
     const page = await browser.newPage();
 
     await page.goto("https://localhost:8443/cas/login", {waitUntil: 'networkidle2'});
+    await page.waitForTimeout(1000)
     await cas.loginWith(page, "casuser", "Mellon");
 
     await unsolicited(page, "https://apereo.github.io");
--- ci/tests/puppeteer/scenarios/saml2-idp-redirect-binding-login/script.js
+++ ci/tests/puppeteer/scenarios/saml2-idp-redirect-binding-login/script.js
@@ -6,8 +6,8 @@ const cas = require('../../cas.js');
     const browser = await puppeteer.launch(cas.browserOptions());
     try {
         const page = await cas.newPage(browser);
-        await page.goto("http://localhost:9443/simplesaml/module.php/core/authenticate.php?as=default-sp");
-        await page.waitForTimeout(1000)
+        await page.goto("http://localhost:9443/simplesaml/module.php/core/authenticate.php?as=default-sp", {waitUntil: 'networkidle2'});
+        await page.waitForTimeout(2000)
         await cas.loginWith(page, "casuser", "Mellon");
 
         await page.waitForSelector('#table_with_attributes', {visible: true});
