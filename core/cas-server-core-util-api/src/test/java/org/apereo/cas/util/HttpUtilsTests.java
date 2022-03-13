52c243453979dbf15274cf723c4827081adc3059
hdeadman <hal.deadman@gmail.com>
2022-03-13 14:43:01 -0400

    address feedback

--- core/cas-server-core-util-api/src/test/java/org/apereo/cas/util/HttpUtilsTests.java
+++ core/cas-server-core-util-api/src/test/java/org/apereo/cas/util/HttpUtilsTests.java
@@ -4,7 +4,10 @@ import lombok.val;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.function.Executable;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.http.HttpMethod;
 
 import java.util.UUID;
@@ -19,6 +22,7 @@ import static org.mockito.Mockito.*;
  * @since 6.3.0
  */
 @Tag("Utility")
+@ExtendWith(OutputCaptureExtension.class)
 public class HttpUtilsTests {
 
     @Test
@@ -61,4 +65,15 @@ public class HttpUtilsTests {
         });
     }
 
+    @Test
+    public void verifyBadSSLLogging(final CapturedOutput capturedOutput) {
+        val exec = HttpUtils.HttpExecutionRequest.builder()
+                .method(HttpMethod.GET)
+                .url("https://untrusted-root.badssl.com/endpoint?secret=sensitiveinfo")
+                .build();
+        assertNull(HttpUtils.execute(exec));
+        val output = capturedOutput.getAll();
+        assertTrue(output.contains("https://untrusted-root.badssl.com/endpoint"));
+        assertFalse(output.contains("sensitiveinfo"));
+    }
 }
