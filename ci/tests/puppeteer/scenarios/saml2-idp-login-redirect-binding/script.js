06e5f69ac20b6481b738a26e7a2f422eee93edba
Misagh <mm1844@gmail.com>
2022-03-13 12:13:44 +0400

    fix tests

--- ci/tests/puppeteer/scenarios/saml2-idp-login-redirect-binding/script.js
+++ ci/tests/puppeteer/scenarios/saml2-idp-login-redirect-binding/script.js
@@ -6,10 +6,10 @@ const cas = require('../../cas.js');
     const browser = await puppeteer.launch(cas.browserOptions());
     try {
         const page = await cas.newPage(browser);
-        await page.goto("http://localhost:9443/simplesaml/module.php/core/authenticate.php?as=default-sp", {waitUntil: 'networkidle2'});
+        await page.goto("http://localhost:9443/simplesaml/module.php/core/authenticate.php?as=default-sp");
         await page.waitForTimeout(2000)
         await cas.loginWith(page, "casuser", "Mellon");
-
+        await page.waitForTimeout(3000)
         await page.waitForSelector('#table_with_attributes', {visible: true});
         await cas.assertInnerTextContains(page, "#content p", "status page of SimpleSAMLphp");
         await cas.assertVisibility(page, "#table_with_attributes");
--- ci/tests/puppeteer/scenarios/saml2-idp-login-with-sso/script.js
+++ ci/tests/puppeteer/scenarios/saml2-idp-login-with-sso/script.js
@@ -11,7 +11,8 @@ const cas = require('../../cas.js');
     await cas.loginWith(page, "casuser", "Mellon");
     
     await page.goto("http://localhost:9443/simplesaml/module.php/core/authenticate.php?as=default-sp");
-    await page.waitForTimeout(1000)
+    await page.waitForTimeout(3000)
+    await cas.screenshot(page);
     await page.waitForSelector('#table_with_attributes', {visible: true});
     await cas.assertInnerTextContains(page, "#content p", "status page of SimpleSAMLphp");
     await cas.assertVisibility(page, "#table_with_attributes");
