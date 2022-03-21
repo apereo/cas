2ee3d9cb70b9d41b9bfc8d8a0cbac4c91b2693bf
Misagh <mm1844@gmail.com>
2022-03-21 17:32:49 +0400

    allow customization of banner / include HZ version in banner

---
+++ support/cas-server-support-hazelcast-core/src/test/java/org/apereo/cas/AllTestsSuite.java
@@ -0,0 +1,20 @@
+package org.apereo.cas;
+
+import org.apereo.cas.hz.HazelcastBannerContributorTests;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+/**
+ * This is {@link AllTestsSuite}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.0.0-RC3
+ */
+@SelectClasses({
+    HazelcastBannerContributorTests.class,
+    HazelcastBannerContributorTests.class
+})
+@Suite
+public class AllTestsSuite {
+}
