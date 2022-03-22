7b47b650f864b615eaa60d18ead844fc42958387
Misagh <mm1844@gmail.com>
2022-03-22 17:10:57 +0400

    include datatables webjar; support features that are disabled by default

--- core/cas-server-core-util-api/src/main/java/org/apereo/cas/util/spring/boot/CasFeatureEnabledCondition.java
+++ core/cas-server-core-util-api/src/main/java/org/apereo/cas/util/spring/boot/CasFeatureEnabledCondition.java
@@ -4,6 +4,7 @@ import org.apereo.cas.configuration.support.CasFeatureModule;
 
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.autoconfigure.condition.ConditionOutcome;
 import org.springframework.boot.autoconfigure.condition.SpringBootCondition;
@@ -40,10 +41,18 @@ public class CasFeatureEnabledCondition extends SpringBootCondition {
         val attributes = metadata.getAnnotationAttributes(ConditionalOnFeature.class.getName());
         val name = attributes.get("feature").toString();
         val module = attributes.get("module").toString();
+        val enabledByDefault = BooleanUtils.toBoolean(attributes.get("enabledByDefault").toString());
 
         val feature = CasFeatureModule.FeatureCatalog.valueOf(name);
         val property = getPropertyName(feature, module);
         LOGGER.trace("Checking for feature module capability via [{}]", property);
+
+        if (!context.getEnvironment().containsProperty(property) && !enabledByDefault) {
+            val message = "CAS feature " + property + " is disabled by default and must be explicitly enabled.";
+            LOGGER.trace(message);
+            return ConditionOutcome.noMatch(message);
+        }
+
         val propertyValue = context.getEnvironment().getProperty(property);
         if (StringUtils.equalsIgnoreCase(propertyValue, "false")) {
             val message = "CAS feature " + property + " is set to false.";
