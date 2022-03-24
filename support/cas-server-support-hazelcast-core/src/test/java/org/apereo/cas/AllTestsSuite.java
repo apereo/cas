294861be187e093b84f8fc39ccc2db5b063f7ea7
Misagh <mm1844@gmail.com>
2022-03-24 19:18:24 +0400

    fix style issus + test annotations

--- support/cas-server-support-hazelcast-core/src/test/java/org/apereo/cas/AllTestsSuite.java
+++ support/cas-server-support-hazelcast-core/src/test/java/org/apereo/cas/AllTestsSuite.java
@@ -1,6 +1,7 @@
 package org.apereo.cas;
 
 import org.apereo.cas.hz.HazelcastBannerContributorTests;
+import org.apereo.cas.hz.HazelcastConfigurationFactoryTests;
 
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
@@ -12,7 +13,7 @@ import org.junit.platform.suite.api.Suite;
  * @since 6.0.0-RC3
  */
 @SelectClasses({
-    HazelcastBannerContributorTests.class,
+    HazelcastConfigurationFactoryTests.class,
     HazelcastBannerContributorTests.class
 })
 @Suite
