4838eef61dfa00f1bcce501d4bbbccf822615f2a
antoine777 <antoine.mollard777@gmail.com>
2022-03-26 18:19:37 +0100

    Exclude initToken and nextToken from SpnegoCredential JSON Serialization (#5432)

--- support/cas-server-support-spnego/src/test/java/org/apereo/cas/support/spnego/authentication/principal/SpnegoCredentialsTests.java
+++ support/cas-server-support-spnego/src/test/java/org/apereo/cas/support/spnego/authentication/principal/SpnegoCredentialsTests.java
@@ -1,12 +1,16 @@
 package org.apereo.cas.support.spnego.authentication.principal;
 
 import org.apereo.cas.authentication.principal.PrincipalFactoryUtils;
+import org.apereo.cas.util.serialization.JacksonObjectMapperFactory;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.val;
 import org.apache.commons.lang3.ArrayUtils;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -15,6 +19,8 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 @Tag("Spnego")
 public class SpnegoCredentialsTests {
+    private static final ObjectMapper MAPPER = JacksonObjectMapperFactory.builder()
+        .defaultTypingEnabled(false).build().toObjectMapper();
 
     @Test
     public void verifyToStringWithNoPrincipal() {
@@ -42,4 +48,21 @@ public class SpnegoCredentialsTests {
         val hash2 = credential.hashCode();
         assertNotEquals(hash1, hash2);
     }
+
+    @Test
+    public void verifyToStringWithToken() {
+        val credentials = new SpnegoCredential(new byte[16]);
+        credentials.setNextToken(new byte[16]);
+        assertThat(credentials.toString(), not(containsString("initToken")));
+        assertThat(credentials.toString(), not(containsString("nextToken")));
+    }
+
+    @Test
+    public void verifyJsonWithToken() throws Exception {
+        val credentials = new SpnegoCredential(new byte[16]);
+        credentials.setNextToken(new byte[16]);
+        assertThat(MAPPER.writeValueAsString(credentials), not(containsString("initToken")));
+        assertThat(MAPPER.writeValueAsString(credentials), not(containsString("nextToken")));
+    }
+
 }
