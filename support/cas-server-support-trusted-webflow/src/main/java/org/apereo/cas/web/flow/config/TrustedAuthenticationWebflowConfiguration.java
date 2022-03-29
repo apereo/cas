0d9dfc5a0cf6623780972cd181a893fd41f04c43
Misagh <mm1844@gmail.com>
2022-03-29 12:38:29 +0400

    fix build

--- support/cas-server-support-trusted-webflow/src/main/java/org/apereo/cas/web/flow/config/TrustedAuthenticationWebflowConfiguration.java
+++ support/cas-server-support-trusted-webflow/src/main/java/org/apereo/cas/web/flow/config/TrustedAuthenticationWebflowConfiguration.java
@@ -2,6 +2,7 @@ package org.apereo.cas.web.flow.config;
 
 import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.configuration.support.CasFeatureModule;
+import org.apereo.cas.util.spring.boot.ConditionalOnFeature;
 import org.apereo.cas.web.flow.CasWebflowConfigurer;
 import org.apereo.cas.web.flow.CasWebflowConstants;
 import org.apereo.cas.web.flow.CasWebflowExecutionPlanConfigurer;
