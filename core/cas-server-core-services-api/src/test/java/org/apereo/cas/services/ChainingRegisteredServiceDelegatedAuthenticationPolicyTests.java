48c6c5a611880a5233141543d718beb9fc4afb12
Misagh <mm1844@gmail.com>
2022-04-01 17:32:10 +0400

    allow delegation selection strategy per service

---
+++ core/cas-server-core-services-api/src/test/java/org/apereo/cas/services/ChainingRegisteredServiceDelegatedAuthenticationPolicyTests.java
@@ -0,0 +1,27 @@
+package org.apereo.cas.services;
+
+import lombok.val;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This is {@link ChainingRegisteredServiceDelegatedAuthenticationPolicyTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.6.0
+ */
+@Tag("Delegation")
+public class ChainingRegisteredServiceDelegatedAuthenticationPolicyTests {
+    @Test
+    public void verifySelectionStrategy() throws Exception {
+        val chain = new ChainingRegisteredServiceDelegatedAuthenticationPolicy();
+        val p1 = new DefaultRegisteredServiceDelegatedAuthenticationPolicy();
+        val p2 = new DefaultRegisteredServiceDelegatedAuthenticationPolicy();
+        p2.setSelectionStrategy("groovy { providers.first()}");
+        chain.addStrategy(p1);
+        chain.addStrategy(p2);
+        assertTrue(chain.getSelectionStrategy().startsWith("groovy {"));
+    }
+}
