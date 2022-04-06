ccf206e5635517af246375d84605583e122a52ae
Misagh <mm1844@gmail.com>
2022-04-06 16:26:21 +0400

    add oauth tests

---
+++ ci/tests/puppeteer/scenarios/oauth2-login-clientcredentials/script.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const cas = require('../../cas.js');
+
+(async () => {
+    let params = "client_id=client&";
+    params += "client_secret=secret&";
+    params += "scope=example&";
+    params += "grant_type=client_credentials&";
+    let url = `https://localhost:8443/cas/oauth2.0/token?${params}`;
+    await cas.doPost(url, params, {
+        'Content-Type': "application/json"
+    }, res => {
+        console.log(res.data);
+        assert(res.data.access_token !== null);
+        cas.decodeJwt(res.data.access_token, true).then(decoded => {
+            assert(decoded !== null)
+            assert(decoded.payload["sub"] === "client")
+            assert(decoded.payload.client_id === "client")
+            assert(decoded.payload.grant_type === "CLIENT_CREDENTIALS")
+        });
+
+    }, error => {
+        throw `Operation failed: ${error}`;
+    });
+})();
