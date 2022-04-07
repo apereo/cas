84865106a72cf1f061136cbae3b09b41185a3919
Misagh <mm1844@gmail.com>
2022-04-07 11:07:52 +0400

    tests for service tgt expiration policy

---
+++ ci/tests/puppeteer/scenarios/service-expiration-policy-tgt/script.js
@@ -0,0 +1,18 @@
+const puppeteer = require('puppeteer');
+const cas = require('../../cas.js');
+
+(async () => {
+    const browser = await puppeteer.launch(cas.browserOptions());
+    const page = await cas.newPage(browser);
+    await cas.goto(page, "https://localhost:8443/cas/login?service=https://apereo.github.io");
+    await cas.loginWith(page, "casuser", "Mellon");
+    await cas.goto(page, "https://localhost:8443/cas/login");
+    await cas.assertCookie(page);
+    await page.waitForTimeout(4000);
+    await cas.goto(page, "https://localhost:8443/cas/login");
+    await page.waitForTimeout(1000);
+    
+    await cas.assertVisibility(page, "#username");
+    await cas.assertVisibility(page, "#password");
+    await browser.close();
+})();
