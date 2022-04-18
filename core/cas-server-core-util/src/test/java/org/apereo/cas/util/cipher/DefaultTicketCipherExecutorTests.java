8e426a49337a0052fd2c6760c898c4220419a41c
Misagh <mm1844@gmail.com>
2022-04-18 15:04:03 +0400

    testing and small polish

--- core/cas-server-core-util/src/test/java/org/apereo/cas/util/cipher/DefaultTicketCipherExecutorTests.java
+++ core/cas-server-core-util/src/test/java/org/apereo/cas/util/cipher/DefaultTicketCipherExecutorTests.java
@@ -73,17 +73,15 @@ public class DefaultTicketCipherExecutorTests {
     }
 
     @Test
-    public void verifyCas63BackwardCompatibility() {
+    public void verifyCompatibilityWithCAS63() {
         val encodedByCas63 = "ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LkViVE0wX0ZzMnhfam9tTzNuMGtpcXU4ODhoZ1JZZlR5bUE5bkRHOGh3aWZCazVuND"
-            + "VtTXVmZy40ZUNYYTluRFl3NnJmeEtHU1Y0c2laajh0aG1Sc1BUd0ZDa3NjMXJRYW8tSGlGVGJ6V1lMcXF4Y080TjVxNENqR3IwM0VYZGNOc3V1cV9ISTRidGVsdw==";
+                             + "VtTXVmZy40ZUNYYTluRFl3NnJmeEtHU1Y0c2laajh0aG1Sc1BUd0ZDa3NjMXJRYW8tSGlGVGJ6V1lMcXF4Y080TjVxNENqR3IwM0VYZGNOc3V1cV9ISTRidGVsdw==";
         val encoded = EncodingUtils.decodeBase64(encodedByCas63);
-
         val signingKey = "RDt6YZHZIH7jUv3nBNIsUMp5Nbs5hblVhK9YYI44KyOXSP5nxpHXD67mH2_7DgklQAPBUcr7WNuOVoeqUFqL6A";
         val encryptionKey = "UdLCpnNxmFOviC3M-kDvvQ";
-        val cipherCas63 = new DefaultTicketCipherExecutor(encryptionKey, signingKey,
-                "AES", 512, 16, "cas63");
-
-        val decoded = cipherCas63.decode(encoded);
-        assertEquals("CAS_SERVER_VERSION_6.3.X", new String(decoded, StandardCharsets.UTF_8));
+        val cipher1 = new DefaultTicketCipherExecutor(encryptionKey, signingKey,
+            "AES", 512, 16, "cas63");
+        val decoded = new String(cipher1.decode(encoded), StandardCharsets.UTF_8);
+        assertEquals("CAS_SERVER_VERSION_6.3.X", decoded);
     }
 }
