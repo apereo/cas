5535ecac1ac4a0984b477d6fa7c1ed3a2b39c88c
Jérôme LELEU <leleuj@gmail.com>
2022-05-05 10:49:14 +0200

    Fix the consistency of the JWT attributes for the OIDC user info

--- support/cas-server-support-oidc-core-api/src/main/java/org/apereo/cas/oidc/profile/OidcUserProfileViewRenderer.java
+++ support/cas-server-support-oidc-core-api/src/main/java/org/apereo/cas/oidc/profile/OidcUserProfileViewRenderer.java
@@ -21,6 +21,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
@@ -56,7 +58,16 @@ public class OidcUserProfileViewRenderer extends OAuth20DefaultUserProfileViewRe
             if (signingAndEncryptionService.shouldSignToken(registeredService)
                 || signingAndEncryptionService.shouldEncryptToken(registeredService)) {
                 val claims = new JwtClaims();
-                userProfile.forEach(claims::setClaim);
+                userProfile.forEach((key, value) -> {
+                    if (OAuth20DefaultUserProfileViewRenderer.MODEL_ATTRIBUTE_ATTRIBUTES.equals(key)) {
+                        val attributes = (Map<String, Object>) value;
+                        val newAttributes = new HashMap<String, Object>();
+                        attributes.forEach((k, v) -> newAttributes.put(k, useSingleValueForSingletonList(v)));
+                        claims.setClaim(key, newAttributes);
+                    } else {
+                        claims.setClaim(key, useSingleValueForSingletonList(value));
+                    }
+                });
                 claims.setAudience(registeredService.getClientId());
                 claims.setIssuedAt(NumericDate.now());
                 claims.setIssuer(this.signingAndEncryptionService.getIssuer());
@@ -78,5 +89,12 @@ public class OidcUserProfileViewRenderer extends OAuth20DefaultUserProfileViewRe
             LoggingUtils.error(LOGGER, e);
         }
         return new ResponseEntity<>("Unable to produce user profile", HttpStatus.BAD_REQUEST);
+    }
+
+    private static Object useSingleValueForSingletonList(final Object value) {
+        if (value instanceof Collection && ((Collection) value).size() == 1) {
+            return ((Collection) value).iterator().next();
+        }
+        return value;
     }
 }
