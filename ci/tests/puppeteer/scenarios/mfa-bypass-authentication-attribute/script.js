ff34154e00e5e3a07da316848b00aaee96623a3b
Misagh <mm1844@gmail.com>
2022-05-15 11:37:33 +0400

    add mfa tests to bypass by authn attribute/handler

---
+++ ci/tests/puppeteer/scenarios/mfa-bypass-authentication-attribute/script.js
@@ -0,0 +1,17 @@
+const puppeteer = require('puppeteer');
+const cas = require('../../cas.js');
+
+(async () => {
+    const browser = await puppeteer.launch(cas.browserOptions());
+    const page = await cas.newPage(browser);
+    await cas.goto(page, "https://localhost:8443/cas/login?service=https://github.com");
+    await cas.loginWith(page, "casuser", "Mellon");
+
+    await page.waitForTimeout(1000)
+    await cas.assertTicketParameter(page)
+
+    await cas.goto(page, "https://localhost:8443/cas");
+    await cas.assertCookie(page);
+
+    await browser.close();
+})();
