37824f5098e3ce7253c794e53e000d3a731027ce
Misagh <mm1844@gmail.com>
2022-06-15 22:52:12 +0400

    allow gauth flow id to be configurable

--- ci/tests/puppeteer/scenarios/mfa-gauth-login/script.js
+++ ci/tests/puppeteer/scenarios/mfa-gauth-login/script.js
@@ -4,7 +4,7 @@ const cas = require('../../cas.js');
 (async () => {
     const browser = await puppeteer.launch(cas.browserOptions());
     const page = await cas.newPage(browser);
-    await cas.goto(page, "https://localhost:8443/cas/login?authn_method=mfa-gauth");
+    await cas.goto(page, "https://localhost:8443/cas/login?authn_method=GoogleAuth");
     await cas.loginWith(page, "casuser", "Mellon");
     await page.waitForTimeout(3000)
     await cas.screenshot(page)
