1cbe3421ecd63acb91c9644a3baa03bac45bd9d9
Misagh Moayyed <mm1844@gmail.com>
2022-07-22 22:37:33 +0400

    fix jacoco setup to remove duplicate class

--- testcas.sh
+++ testcas.sh
@@ -45,7 +45,7 @@ parallel="--parallel "
 dryRun=""
 info=""
 gradleCmd="./gradlew"
-flags="--no-daemon --configure-on-demand --build-cache -x javadoc -x check -DskipNestedConfigMetadataGen=true -Dverbose=true "
+flags="--stacktrace --no-daemon --configure-on-demand --build-cache -x javadoc -x check -DskipNestedConfigMetadataGen=true -Dverbose=true "
 coverageTask=""
 
 while (( "$#" )); do
