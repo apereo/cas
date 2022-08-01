37dd64de041b838ac2c82675a0ce2ff85f2a8c7b
Misagh Moayyed <mm1844@gmail.com>
2022-08-01 18:19:14 +0400

    add test case for groovy+impersonation

---
+++ ci/tests/puppeteer/scenarios/surrogate-login-groovy/impersonation.groovy
@@ -0,0 +1,28 @@
+import org.apereo.cas.authentication.principal.*
+
+def canAuthenticate(Object... args) {
+    def surrogate = args[0].toString()
+    def principal = args[1] as Principal
+    def service = args[2] as Service
+    def logger = args[3]
+
+    logger.info("Checking surrogate access {}", surrogate)
+    def accounts = getAccounts(principal.id, logger)
+    return accounts.contains(surrogate)
+}
+
+
+def getAccounts(Object... args) {
+    def user = args[0].toString()
+    def logger = args[1]
+
+    logger.info("Getting accounts for {}", user)
+    switch (user) {
+        case "casuser1":
+            return ["casuser2", "casuser3"]
+        case "casuser2":
+            return ["casuser3"]
+        default:
+            return []
+    }
+}
---
+++ ci/tests/puppeteer/scenarios/surrogate-login-groovy/script.js
@@ -0,0 +1,36 @@
+const puppeteer = require('puppeteer');
+const assert = require('assert');
+
+const cas = require('../../cas.js');
+
+(async () => {
+    const browser = await puppeteer.launch(cas.browserOptions());
+    const page = await cas.newPage(browser);
+    await impersonate(page, "casuser1");
+    await impersonate(page, "casuser2");
+
+    console.log("Checking empty list of authorized accounts for user...");
+    await cas.goto(page, "https://localhost:8443/cas/login?service=https://apereo.github.io");
+    await cas.loginWith(page, `+casuser3`, "Mellon");
+    await page.waitForTimeout(1000);
+    await cas.assertTicketParameter(page);
+    await cas.goto(page, `https://localhost:8443/cas/logout`);
+
+    console.log("Checking auto selection for unauthorized user...");
+    await cas.goto(page, "https://localhost:8443/cas/login?service=https://apereo.github.io");
+    await cas.loginWith(page, `anotheruser+casuser3`, "Mellon");
+    await page.waitForTimeout(1000);
+    await cas.assertInnerTextStartsWith(page, "#loginErrorsPanel p", "You are not authorized to impersonate");
+
+    await browser.close();
+})();
+
+async function impersonate(page, username) {
+    await cas.goto(page, "https://localhost:8443/cas/login?service=https://apereo.github.io");
+    await cas.loginWith(page, `+${username}`, "Mellon");
+    await page.waitForTimeout(1000);
+    await cas.assertTextContent(page, "#titlePanel h2", "Choose Account");
+    await cas.assertTextContentStartsWith(page, "#surrogateInfo", "You are provided with a list of accounts");
+    await cas.assertVisibility(page, '#surrogateTarget');
+    await cas.goto(page, `https://localhost:8443/cas/logout`);
+}
