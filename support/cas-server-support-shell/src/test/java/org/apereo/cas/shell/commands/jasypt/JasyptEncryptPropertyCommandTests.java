3ab9cf57f458cc4d30f0a5af06762410c16dbaaa
Misagh Moayyed <mm1844@gmail.com>
2022-08-17 18:33:48 +0400

    allow file encryption

--- support/cas-server-support-shell/src/test/java/org/apereo/cas/shell/commands/jasypt/JasyptEncryptPropertyCommandTests.java
+++ support/cas-server-support-shell/src/test/java/org/apereo/cas/shell/commands/jasypt/JasyptEncryptPropertyCommandTests.java
@@ -2,9 +2,15 @@ package org.apereo.cas.shell.commands.jasypt;
 
 import org.apereo.cas.shell.commands.BaseCasShellCommandTests;
 
+import lombok.val;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,13 +26,21 @@ public class JasyptEncryptPropertyCommandTests extends BaseCasShellCommandTests 
     @Test
     public void verifyOperation() {
         assertDoesNotThrow(() -> shell.evaluate(() -> "encrypt-value --value SOMEVALUE --password "
-            + "JASTYPTPW --alg PBEWITHSHAAND256BITAES-CBC-BC --provider BC"));
+                                                      + "JASTYPTPW --alg PBEWITHSHAAND256BITAES-CBC-BC --provider BC"));
+    }
+
+    @Test
+    public void verifyFileEncryption() throws Exception {
+        val file = File.createTempFile("file", ".txt");
+        FileUtils.write(file, UUID.randomUUID().toString(), StandardCharsets.UTF_8);
+        assertDoesNotThrow(() -> shell.evaluate(() -> "encrypt-value --file " + file.getAbsolutePath() + " --password "
+                                                      + "JASTYPTPW --alg PBEWITHSHAAND256BITAES-CBC-BC --provider BC"));
     }
 
     @Test
     public void verifyOperationWithInitVector() {
         assertDoesNotThrow(() -> shell.evaluate(() -> "encrypt-value --value SOMEVALUE --password "
-                + "JASTYPTPW --alg PBEWITHSHAAND256BITAES-CBC-BC --provider BC"));
+                                                      + "JASTYPTPW --alg PBEWITHSHAAND256BITAES-CBC-BC --provider BC"));
     }
 }
 
