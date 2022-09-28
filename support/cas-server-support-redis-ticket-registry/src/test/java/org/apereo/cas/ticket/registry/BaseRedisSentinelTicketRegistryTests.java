c7822f2dfbb5b5a12fc976421c24f1b199f08eb8
Misagh Moayyed <mm1844@gmail.com>
2022-09-28 11:48:12 +0400

    support better redis health metrics and stats

--- support/cas-server-support-redis-ticket-registry/src/test/java/org/apereo/cas/ticket/registry/BaseRedisSentinelTicketRegistryTests.java
+++ support/cas-server-support-redis-ticket-registry/src/test/java/org/apereo/cas/ticket/registry/BaseRedisSentinelTicketRegistryTests.java
@@ -1,5 +1,6 @@
 package org.apereo.cas.ticket.registry;
 
+import org.apereo.cas.config.RedisCoreConfiguration;
 import org.apereo.cas.config.RedisTicketRegistryConfiguration;
 import org.apereo.cas.redis.core.CasRedisTemplate;
 import org.apereo.cas.ticket.Ticket;
@@ -7,6 +8,7 @@ import org.apereo.cas.ticket.Ticket;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -17,7 +19,10 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * @author Julien Gribonvald
  * @since 6.1.0
  */
-@Import(RedisTicketRegistryConfiguration.class)
+@Import({
+    RedisCoreConfiguration.class,
+    RedisTicketRegistryConfiguration.class
+})
 @EnableTransactionManagement(proxyTargetClass = false)
 @EnableAspectJAutoProxy(proxyTargetClass = false)
 @Getter
@@ -25,6 +30,10 @@ public abstract class BaseRedisSentinelTicketRegistryTests extends BaseTicketReg
     @Autowired
     @Qualifier("ticketRedisTemplate")
     protected CasRedisTemplate<String, Ticket> ticketRedisTemplate;
+
+    @Autowired
+    @Qualifier("redisHealthIndicator")
+    protected HealthIndicator redisHealthIndicator;
 
     @Autowired
     @Qualifier(TicketRegistry.BEAN_NAME)
