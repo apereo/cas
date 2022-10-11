5afba1403b774ad0aabf1387924272228d3da4a4
Jérôme LELEU <leleuj@gmail.com>
2022-10-11 16:51:45 +0200

    Use the HttpClient to fetch SAML metadata

--- support/cas-server-support-saml-idp-metadata/src/main/java/org/apereo/cas/support/saml/services/idp/metadata/cache/resolver/UrlResourceMetadataResolver.java
+++ support/cas-server-support-saml-idp-metadata/src/main/java/org/apereo/cas/support/saml/services/idp/metadata/cache/resolver/UrlResourceMetadataResolver.java
@@ -18,6 +18,7 @@ import org.apereo.cas.util.HttpUtils;
 import org.apereo.cas.util.LoggingUtils;
 import org.apereo.cas.util.ResourceUtils;
 import org.apereo.cas.util.function.FunctionUtils;
+import org.apereo.cas.util.http.HttpClient;
 import org.apereo.cas.util.spring.SpringExpressionLanguageValueResolver;
 
 import lombok.extern.slf4j.Slf4j;
@@ -63,11 +64,15 @@ public class UrlResourceMetadataResolver extends BaseSamlRegisteredServiceMetada
 
     private static final String DIRNAME_METADATA_BACKUPS = "metadata-backups";
 
+    private final HttpClient httpClient;
+
     private final File metadataBackupDirectory;
 
-    public UrlResourceMetadataResolver(final SamlIdPProperties samlIdPProperties,
+    public UrlResourceMetadataResolver(final HttpClient httpClient,
+                                       final SamlIdPProperties samlIdPProperties,
                                        final OpenSamlConfigBean configBean) {
         super(samlIdPProperties, configBean);
+        this.httpClient = httpClient;
 
         val md = samlIdPProperties.getMetadata();
         val backupLocation = StringUtils.defaultIfBlank(md.getHttp().getMetadataBackupLocation(), md.getFileSystem().getLocation());
@@ -223,6 +228,7 @@ public class UrlResourceMetadataResolver extends BaseSamlRegisteredServiceMetada
             .method(HttpMethod.GET)
             .url(metadataLocation)
             .proxyUrl(service.getMetadataProxyLocation())
+            .httpClient(httpClient)
             .build();
         return HttpUtils.execute(exec);
     }
