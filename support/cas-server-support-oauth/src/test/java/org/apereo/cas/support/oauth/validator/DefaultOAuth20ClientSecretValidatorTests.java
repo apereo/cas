e600d61e217c1916a1199f18c6e09ecb06e505f0
Misagh Moayyed <mm1844@gmail.com>
2022-10-12 12:09:35 +0400

    fix build issues

--- support/cas-server-support-oauth/src/test/java/org/apereo/cas/support/oauth/validator/DefaultOAuth20ClientSecretValidatorTests.java
+++ support/cas-server-support-oauth/src/test/java/org/apereo/cas/support/oauth/validator/DefaultOAuth20ClientSecretValidatorTests.java
@@ -6,6 +6,7 @@ import org.apereo.cas.util.EncodingUtils;
 import org.apereo.cas.util.RandomUtils;
 
 import lombok.val;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -80,7 +81,7 @@ public class DefaultOAuth20ClientSecretValidatorTests extends AbstractOAuth20Tes
         registeredService.setClientSecret(secret);
         val result = oauth20ClientSecretValidator.validate(registeredService, null);
         assertFalse(result);
-        val result2 = oauth20ClientSecretValidator.validate(registeredService, String.EMPTY);
+        val result2 = oauth20ClientSecretValidator.validate(registeredService, StringUtils.EMPTY);
         assertFalse(result2);
     }
 }
