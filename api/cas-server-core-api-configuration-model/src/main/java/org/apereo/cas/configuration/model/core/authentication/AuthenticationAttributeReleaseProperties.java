23367aabf963bc67c5a9700124a4da66434dbc22
CAS in the cloud LELEU Jérôme <leleuj@gmail.com>
2022-10-17 15:16:19 +0200

    Use authn attribute release policy for resource password grant type (#5529)

--- api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/core/authentication/AuthenticationAttributeReleaseProperties.java
+++ api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/core/authentication/AuthenticationAttributeReleaseProperties.java
@@ -25,7 +25,8 @@ public class AuthenticationAttributeReleaseProperties implements Serializable {
 
     /**
      * Whether CAS authentication/protocol attributes
-     * should be released as part of ticket validation.
+     * should be released as part of ticket validation
+     * or for the resource owner password grant type.
      */
     private boolean enabled = true;
 
