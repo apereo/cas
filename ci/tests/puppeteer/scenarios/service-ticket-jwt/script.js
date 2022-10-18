01144b3fa7569ce0dc23d6e32faa2bdfa370a1cf
Misagh Moayyed <mm1844@gmail.com>
2022-10-18 16:51:53 +0400

    test case for jwt as service ticket

---
+++ ci/tests/puppeteer/scenarios/service-ticket-jwt/script.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer');
+const cas = require('../../cas.js');
+const path = require("path");
+const assert = require('assert');
+
+(async () => {
+    const browser = await puppeteer.launch(cas.browserOptions());
+    const page = await cas.newPage(browser);
+    const service = "https://httpbin.org/anything/1";
+    await cas.goto(page, `https://localhost:8443/cas/login?service=${service}`);
+    await cas.loginWith(page, "casuser", "Mellon");
+    await page.waitForTimeout(2000);
+    let ticket = await cas.assertTicketParameter(page);
+
+    const { payload, protectedHeader } = await cas.decryptJwt(ticket, path.join(__dirname, 'private.key'));
+    
+    assert(payload.iss === "https://localhost:8443/cas");
+    assert(payload.aud === "https://httpbin.org/anything/1");
+    assert(payload.credentialType === "UsernamePasswordCredential");
+    assert(payload.sub === "casuser");
+    assert(payload.jti.startsWith("ST-1-"));
+    
+    await browser.close();
+})();
