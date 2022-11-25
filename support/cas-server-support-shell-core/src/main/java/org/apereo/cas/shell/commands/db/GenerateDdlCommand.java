d6ace664c438f9670be6a8f5f39d155a01e0d469
Misagh Moayyed <mm1844@gmail.com>
2022-11-25 12:14:01 +0400

    fix build

--- support/cas-server-support-shell-core/src/main/java/org/apereo/cas/shell/commands/db/GenerateDdlCommand.java
+++ support/cas-server-support-shell-core/src/main/java/org/apereo/cas/shell/commands/db/GenerateDdlCommand.java
@@ -2,6 +2,8 @@ package org.apereo.cas.shell.commands.db;
 
 import org.apereo.cas.util.ReflectionUtils;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.MappedSuperclass;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.apache.commons.lang3.BooleanUtils;
@@ -40,8 +42,6 @@ import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.MappedSuperclass;
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Map;
@@ -124,35 +124,43 @@ public class GenerateDdlCommand {
     public String generate(
         @ShellOption(value = {"file", "--file"},
             help = "DDL file to contain to generated script",
-            defaultValue = "/etc/cas/config/cas-db-schema.sql") final String file,
+            defaultValue = "/etc/cas/config/cas-db-schema.sql")
+        final String file,
         @ShellOption(value = {"dialect", "--dialect"},
             help = "Database dialect class",
-            defaultValue = "HSQL") final String dialect,
+            defaultValue = "HSQL")
+        final String dialect,
         @ShellOption(value = {"url", "--url"},
             help = "JDBC database connection URL",
-            defaultValue = "jdbc:hsqldb:mem:cas") final String jdbcUrl,
+            defaultValue = "jdbc:hsqldb:mem:cas")
+        final String jdbcUrl,
         @ShellOption(value = {"delimiter", "--delimiter"},
             help = "Delimiter to use for separation of statements when generating SQL",
-            defaultValue = ";") final String delimiter,
+            defaultValue = ";")
+        final String delimiter,
         @ShellOption(value = {"pretty", "--pretty"},
             help = "Format DDL scripts and pretty-print the output",
-            defaultValue = "false") final Boolean pretty,
+            defaultValue = "false")
+        final Boolean pretty,
         @ShellOption(value = {"dropSchema", "--dropSchema"},
             help = "Generate DROP SQL statements in the DDL",
-            defaultValue = "false") final Boolean dropSchema,
+            defaultValue = "false")
+        final Boolean dropSchema,
         @ShellOption(value = {"createSchema", "--createSchema"},
             help = "Generate DROP SQL statements in the DDL",
-            defaultValue = "false") final Boolean createSchema,
+            defaultValue = "false")
+        final Boolean createSchema,
         @ShellOption(value = {"haltOnError", "--haltOnError"},
             help = "Halt if an error occurs during the generation process",
-            defaultValue = "false") final Boolean haltOnError) {
+            defaultValue = "false")
+        final Boolean haltOnError) {
 
         LOGGER.info("Requested database dialect type [{}]", dialect);
         val dialectName = DIALECTS_MAP.getOrDefault(dialect.trim(), dialect);
         LOGGER.info("Using database dialect class [{}]", dialectName);
         if (!dialectName.contains(".")) {
             LOGGER.warn("Dialect name must be a fully qualified class name. Supported dialects by default are [{}] "
-                + "or you may specify the dialect class directly", DIALECTS_MAP.keySet());
+                        + "or you may specify the dialect class directly", DIALECTS_MAP.keySet());
             return null;
         }
 
