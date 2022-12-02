7a9ba68b3c54617e34c6f291f64db8bda2a9a032
Jérôme LELEU <leleuj@gmail.com>
2022-12-02 11:00:37 +0100

    Demo surrogation scenarios for passwordless flow

---
+++ ci/tests/puppeteer/scenarios/passwordless-delegated-login-surrogation/delegate-idp-selector.groovy
@@ -0,0 +1,18 @@
+import org.apereo.cas.api.*
+
+def run(Object[] args) {
+    def passwordlessUser = args[0] as PasswordlessUserAccount
+    def clients = args[1] as Set
+    def httpServletRequest = args[2]
+    def logger = args[3]
+
+    logger.info("Available identity providers are {}", clients)
+    logger.info("Passwordless user account is {}", passwordlessUser.username)
+    if (passwordlessUser.username == "casuser-server") {
+        return clients.find { it.name == "CasClient-Server" }
+    }
+    if (passwordlessUser.username == "casuser-client") {
+        return clients.find { it.name == "CasClient-Client" }
+    }
+    return clients.find { it.name == "CasClient-None" }
+}
