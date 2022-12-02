45c292b213fa4055f1579eee6991ff55a4e92183
Misagh Moayyed <mm1844@gmail.com>
2022-12-02 10:41:01 +0400

    rework webflow decorators to support multiple

--- support/cas-server-support-actions/src/test/java/org/apereo/cas/web/flow/RenderLoginActionTests.java
+++ support/cas-server-support-actions/src/test/java/org/apereo/cas/web/flow/RenderLoginActionTests.java
@@ -1,29 +1,31 @@
 package org.apereo.cas.web.flow;
 
-import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.util.MockWebServer;
 import org.apereo.cas.util.serialization.JacksonObjectMapperFactory;
-import org.apereo.cas.web.flow.login.RenderLoginAction;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.val;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.webflow.context.ExternalContextHolder;
 import org.springframework.webflow.context.servlet.ServletExternalContext;
+import org.springframework.webflow.execution.Action;
 import org.springframework.webflow.execution.RequestContextHolder;
 import org.springframework.webflow.test.MockRequestContext;
 
 import java.util.Map;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.nio.charset.StandardCharsets.*;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.*;
 
 /**
  * This is {@link RenderLoginActionTests}.
@@ -32,58 +34,75 @@ import static org.springframework.http.HttpStatus.OK;
  * @since 6.2.0
  */
 @Tag("WebflowActions")
-public class RenderLoginActionTests extends AbstractWebflowActionsTests {
+public class RenderLoginActionTests {
     private static final ObjectMapper MAPPER = JacksonObjectMapperFactory.builder()
         .defaultTypingEnabled(true).build().toObjectMapper();
 
-    @Test
-    public void verifyNoRender() throws Exception {
-        val context = new MockRequestContext();
-        val request = new MockHttpServletRequest();
-        val response = new MockHttpServletResponse();
-        context.setExternalContext(new ServletExternalContext(new MockServletContext(), request, response));
-        RequestContextHolder.setRequestContext(context);
-        ExternalContextHolder.setExternalContext(context.getExternalContext());
+    @Nested
+    @SuppressWarnings("ClassCanBeStatic")
+    public class DefaultTests extends AbstractWebflowActionsTests {
+        @Autowired
+        @Qualifier(CasWebflowConstants.ACTION_ID_RENDER_LOGIN_FORM)
+        private Action renderLoginAction;
 
-        val properties = new CasConfigurationProperties();
-        val action = new RenderLoginAction(getServicesManager(), properties, applicationContext);
-        assertNull(action.execute(context));
-    }
-
-    @Test
-    public void verifyGroovyRender() throws Exception {
-        val context = new MockRequestContext();
-        val request = new MockHttpServletRequest();
-        val response = new MockHttpServletResponse();
-        context.setExternalContext(new ServletExternalContext(new MockServletContext(), request, response));
-        RequestContextHolder.setRequestContext(context);
-        ExternalContextHolder.setExternalContext(context.getExternalContext());
-
-        val properties = new CasConfigurationProperties();
-        properties.getWebflow().getLoginDecorator()
-            .getGroovy().setLocation(new ClassPathResource("GroovyLoginDecorator.groovy"));
-        val action = new RenderLoginAction(getServicesManager(), properties, applicationContext);
-        assertNull(action.execute(context));
-    }
-
-    @Test
-    public void verifyRestfulRender() throws Exception {
-        val context = new MockRequestContext();
-        val request = new MockHttpServletRequest();
-        val response = new MockHttpServletResponse();
-        context.setExternalContext(new ServletExternalContext(new MockServletContext(), request, response));
-        RequestContextHolder.setRequestContext(context);
-        ExternalContextHolder.setExternalContext(context.getExternalContext());
-
-        val entity = MAPPER.writeValueAsString(Map.of("key", "value"));
-        try (val webServer = new MockWebServer(1234,
-            new ByteArrayResource(entity.getBytes(UTF_8), "Output"), OK)) {
-            webServer.start();
-
-            val properties = new CasConfigurationProperties();
-            properties.getWebflow().getLoginDecorator().getRest().setUrl("http://localhost:1234");
-            val action = new RenderLoginAction(getServicesManager(), properties, applicationContext);
-            assertNull(action.execute(context));
+        @Test
+        public void verifyNoRender() throws Exception {
+            val context = new MockRequestContext();
+            val request = new MockHttpServletRequest();
+            val response = new MockHttpServletResponse();
+            context.setExternalContext(new ServletExternalContext(new MockServletContext(), request, response));
+            RequestContextHolder.setRequestContext(context);
+            ExternalContextHolder.setExternalContext(context.getExternalContext());
+            assertNull(renderLoginAction.execute(context));
         }
     }
+
+    @Nested
+    @SuppressWarnings("ClassCanBeStatic")
+    @TestPropertySource(properties = "cas.webflow.login-decorator.groovy.location=classpath:/GroovyLoginWebflowDecorator.groovy")
+    public class GroovyRendererTests extends AbstractWebflowActionsTests {
+        @Autowired
+        @Qualifier(CasWebflowConstants.ACTION_ID_RENDER_LOGIN_FORM)
+        private Action renderLoginAction;
+
+        @Test
+        public void verifyGroovyRender() throws Exception {
+            val context = new MockRequestContext();
+            val request = new MockHttpServletRequest();
+            val response = new MockHttpServletResponse();
+            context.setExternalContext(new ServletExternalContext(new MockServletContext(), request, response));
+            RequestContextHolder.setRequestContext(context);
+            ExternalContextHolder.setExternalContext(context.getExternalContext());
+            assertNull(renderLoginAction.execute(context));
+            assertNotNull(context.getFlowScope().get("decoration"));
+        }
+    }
+
+    @Nested
+    @SuppressWarnings("ClassCanBeStatic")
+    @TestPropertySource(properties = "cas.webflow.login-decorator.rest.url=http://localhost:1234")
+    public class RestfulRendererTests extends AbstractWebflowActionsTests {
+        @Autowired
+        @Qualifier(CasWebflowConstants.ACTION_ID_RENDER_LOGIN_FORM)
+        private Action renderLoginAction;
+
+        @Test
+        public void verifyRestfulRender() throws Exception {
+            val context = new MockRequestContext();
+            val request = new MockHttpServletRequest();
+            val response = new MockHttpServletResponse();
+            context.setExternalContext(new ServletExternalContext(new MockServletContext(), request, response));
+            RequestContextHolder.setRequestContext(context);
+            ExternalContextHolder.setExternalContext(context.getExternalContext());
+
+            val entity = MAPPER.writeValueAsString(Map.of("key", "value"));
+            try (val webServer = new MockWebServer(1234,
+                new ByteArrayResource(entity.getBytes(UTF_8), "Output"), OK)) {
+                webServer.start();
+                assertNull(renderLoginAction.execute(context));
+                assertNotNull(context.getFlowScope().get("decoration"));
+            }
+        }
+    }
+
 }
