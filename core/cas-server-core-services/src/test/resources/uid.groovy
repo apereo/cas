823113107a635537a72c033efb54eff86ef01262
Hal Deadman <hdeadman@users.noreply.github.com>
2022-12-08 12:01:04 -0500

    allow inotify watch to be turned off in GroovyRegisteredServiceUsernameProvider (#5563)

--- core/cas-server-core-services/src/test/resources/uid.groovy
+++ core/cas-server-core-services/src/test/resources/uid.groovy
@@ -1,3 +1,8 @@
-logger.info("Testing username attribute for attributes $attributes and service ${service.id}")
-return "test"
-
+def run(Object[] args) {
+    def attributes = args[0]
+    def id = args[1]
+    def service = args[2]
+    def logger = args[3]
+    logger.info("Testing username attribute for attributes $attributes and service ${service.id}")
+    return "fromscript"
+}
