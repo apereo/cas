f4bfaef541ef06e2f729ea0eb13097e7e018c12f
Hal Deadman <hdeadman@users.noreply.github.com>
2022-12-08 12:01:04 -0500

    allow inotify watch to be turned off in GroovyRegisteredServiceUsernameProvider (#5563)
    # Conflicts:
    #	core/cas-server-core-services-api/src/main/java/org/apereo/cas/services/GroovyRegisteredServiceUsernameProvider.java

--- core/cas-server-core-services-api/src/main/java/org/apereo/cas/services/GroovyRegisteredServiceUsernameProvider.java
+++ core/cas-server-core-services-api/src/main/java/org/apereo/cas/services/GroovyRegisteredServiceUsernameProvider.java
@@ -4,18 +4,10 @@ import org.apereo.cas.authentication.principal.Principal;
 import org.apereo.cas.authentication.principal.Service;
 import org.apereo.cas.configuration.support.ExpressionLanguageCapable;
 import org.apereo.cas.util.CollectionUtils;
-import org.apereo.cas.util.ResourceUtils;
-import org.apereo.cas.util.function.FunctionUtils;
 import org.apereo.cas.util.scripting.ExecutableCompiledGroovyScript;
-import org.apereo.cas.util.scripting.GroovyShellScript;
-import org.apereo.cas.util.scripting.ScriptingUtils;
-import org.apereo.cas.util.scripting.WatchableGroovyScriptResource;
-import org.apereo.cas.util.spring.SpringExpressionLanguageValueResolver;
-
+import org.apereo.cas.util.spring.ApplicationContextProvider;
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,10 +17,6 @@ import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.apache.commons.lang3.StringUtils;
 
-import jakarta.persistence.PostLoad;
-import jakarta.persistence.Transient;
-
-import java.io.Serial;
 
 /**
  * Resolves the username for the service to be the default principal id.
@@ -41,63 +29,54 @@ import java.io.Serial;
 @Setter
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
-@AllArgsConstructor
 @Accessors(chain = true)
 public class GroovyRegisteredServiceUsernameProvider extends BaseRegisteredServiceUsernameAttributeProvider {
 
-    @Serial
     private static final long serialVersionUID = 5823989148794052951L;
 
     @ExpressionLanguageCapable
     private String groovyScript;
-
-    @JsonIgnore
-    @Transient
-    @org.springframework.data.annotation.Transient
-    private transient ExecutableCompiledGroovyScript executableScript;
 
     @JsonCreator
     public GroovyRegisteredServiceUsernameProvider(@JsonProperty("groovyScript") final String script) {
         this.groovyScript = script;
     }
 
+    private static String fetchAttributeValue(final Principal principal,
+                                              final Service service,
+                                              final RegisteredService registeredService,
+                                              final String groovyScript) {
+
+        return ApplicationContextProvider.getScriptResourceCacheManager()
+            .map(cacheMgr -> {
+                val script = cacheMgr.resolveScriptableResource(groovyScript, registeredService.getServiceId(), groovyScript);
+                return fetchAttributeValueFromScript(script, principal, service);
+            })
+            .map(Object::toString)
+            .orElseThrow(() -> new RuntimeException("No groovy script cache manager is available to execute username provider"));
+    }
+
     @Override
     public String resolveUsernameInternal(final Principal principal, final Service service, final RegisteredService registeredService) {
         if (StringUtils.isNotBlank(this.groovyScript)) {
-            initializeWatchableScriptIfNeeded();
-            val result = getGroovyAttributeValue(principal, service);
+            val result = fetchAttributeValue(principal, service, registeredService, groovyScript);
             if (result != null) {
                 LOGGER.debug("Found username [{}] from script", result);
-                return result.toString();
+                return result;
             }
         }
         LOGGER.warn("Groovy script [{}] is not valid. CAS will switch to use the default principal identifier [{}]", this.groovyScript, principal.getId());
         return principal.getId();
     }
 
-    @PostLoad
-    private void initializeWatchableScriptIfNeeded() {
-        if (this.executableScript == null) {
-            val matcherInline = ScriptingUtils.getMatcherForInlineGroovyScript(groovyScript);
-            val matcherFile = ScriptingUtils.getMatcherForExternalGroovyScript(groovyScript);
-
-            if (matcherFile.find()) {
-                val script = SpringExpressionLanguageValueResolver.getInstance().resolve(matcherFile.group());
-                val resource = FunctionUtils.doUnchecked(() -> ResourceUtils.getRawResourceFrom(script));
-                this.executableScript = new WatchableGroovyScriptResource(resource);
-            } else if (matcherInline.find()) {
-                this.executableScript = new GroovyShellScript(matcherInline.group(1));
-            }
-        }
-    }
-
-    private Object getGroovyAttributeValue(final Principal principal, final Service service) {
+    private static Object fetchAttributeValueFromScript(final ExecutableCompiledGroovyScript script,
+                                                        final Principal principal, final Service service) {
         val args = CollectionUtils.<String, Object>wrap("attributes", principal.getAttributes(),
             "id", principal.getId(),
             "service", service,
             "logger", LOGGER);
-        executableScript.setBinding(args);
-        return executableScript.execute(args.values().toArray(), Object.class);
+        script.setBinding(args);
+        return script.execute(args.values().toArray(), Object.class);
     }
 
 }
