f1591ae43429dffb0834d8dd0d012a1a89d6ffd3
Misagh Moayyed <mm1844@gmail.com>
2022-12-10 15:30:38 +0100

    move session management enum into its own component

---
+++ api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/core/web/session/SessionStorageTypes.java
@@ -0,0 +1,27 @@
+package org.apereo.cas.configuration.model.core.web.session;
+
+/**
+ * This is {@link SessionStorageTypes}.
+ *
+ * @author Misagh Moayyed
+ * @since 7.0.0
+ */
+public enum SessionStorageTypes {
+    /**
+     * Authentication requests, and other session data collected as part of authentication protocol flows and requests
+     * are kept in the http servlet session that is local to the server.
+     */
+    HTTP,
+    /**
+     * Authentication requests, and other session data collected as part of authentication protocol flows and requests
+     * are kept in the client browser's session storage, signed and encrypted. All interactions
+     * require client-side read/write operations to restore the session from the browser.
+     */
+    BROWSER_SESSION_STORAGE,
+    /**
+     * Authentication requests, and other session data collected as part of authentication protocol flows and requests
+     * are tracked as CAS tickets in the registry and replicated across the entire cluster
+     * as tickets.
+     */
+    TICKET_REGISTRY
+}
