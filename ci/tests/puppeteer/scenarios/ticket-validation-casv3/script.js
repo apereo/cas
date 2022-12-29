764c6982b45fc16e07419af94d7e7beed434881a
Misagh Moayyed <mm1844@gmail.com>
2022-12-29 11:07:45 +0400

    add tests

--- ci/tests/puppeteer/scenarios/ticket-validation-casv3/script.js
+++ ci/tests/puppeteer/scenarios/ticket-validation-casv3/script.js
@@ -5,7 +5,7 @@ const cas = require('../../cas.js');
 (async () => {
     const browser = await puppeteer.launch(cas.browserOptions());
     const page = await cas.newPage(browser);
-    const service = "https://example.com";
+    const service = "https://apereo.github.io";
 
     await cas.goto(page, `https://localhost:8443/cas/login?service=${service}`);
     await cas.loginWith(page, "casuser", "Mellon");
@@ -14,7 +14,7 @@ const cas = require('../../cas.js');
     const body = await cas.doRequest(`https://localhost:8443/cas/p3/serviceValidate?service=${service}&ticket=${ticket}`);
     console.log(body);
     assert(body.includes('<cas:serviceResponse xmlns:cas=\'http://www.yale.edu/tp/cas\'>'));
-    assert(body.includes('<cas:user>casuser</cas:user>'));
+    assert(body.includes('<cas:user>CASUSER@EXAMPLE.ORG</cas:user>'));
     assert(body.includes('<cas:credentialType>UsernamePasswordCredential</cas:credentialType>'));
     assert(body.includes('<cas:isFromNewLogin>true</cas:isFromNewLogin>'));
     assert(body.includes('<cas:authenticationMethod>STATIC</cas:authenticationMethod>'));
