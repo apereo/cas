d620429b54b0a5bde12e77baa0c809535677aaec
Misagh Moayyed <mm1844@gmail.com>
2022-12-31 21:33:44 +0400

    add test cases

---
+++ ci/tests/puppeteer/scenarios/delegated-login-session-timeout-saml2-idp/script.js
@@ -0,0 +1,34 @@
+const puppeteer = require('puppeteer');
+const cas = require('../../cas.js');
+const path = require('path');
+const assert = require('assert');
+
+(async () => {
+    const browser = await puppeteer.launch(cas.browserOptions());
+    const page = await cas.newPage(browser);
+
+    await cas.goto(page, "https://localhost:8443/cas/login?locale=en");
+    await page.waitForTimeout(2000);
+
+    await cas.assertVisibility(page, '#loginProviders');
+    await cas.assertVisibility(page, 'li #SAML2Client');
+    
+    await cas.click(page, "li #SAML2Client");
+    await page.waitForNavigation();
+
+    await cas.loginWith(page, "user1", "password");
+    await page.waitForTimeout(3000);
+
+    let url = await page.url();
+    console.log(`Page URL: ${url}`);
+    await cas.screenshot(page);
+    await cas.assertCookie(page, false);
+    await cas.assertParameter(page, "client_name");
+    assert(url.includes("https://localhost:8443/cas/login"));
+    await cas.assertInnerText(page, "#content h2", "Application Not Authorized to Use CAS");
+    
+    await cas.removeDirectory(path.join(__dirname, '/saml-md'));
+    await browser.close();
+})();
+
+
