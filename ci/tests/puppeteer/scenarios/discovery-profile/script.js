21e204595f3598141fb44c20e423617e10d06eca
Misagh Moayyed <mm1844@gmail.com>
2023-01-06 15:00:43 +0400

    add test cases for electrofence with ip calc

--- ci/tests/puppeteer/scenarios/discovery-profile/script.js
+++ ci/tests/puppeteer/scenarios/discovery-profile/script.js
@@ -5,13 +5,13 @@ const cas = require('../../cas.js');
     await cas.doGet("https://localhost:8443/cas/actuator/discoveryProfile",
         res => {
             assert(res.status === 200);
-            // assert(res.data.profile.registeredServiceTypesSupported !== null);
-            // assert(res.data.profile.availableAuthenticationHandlers !== null);
-            // assert(res.data.profile.availableAttributes !== null);
-            // assert(res.data.profile.delegatedClientTypesSupported !== null);
-            // assert(res.data.profile.userDefinedScopes !== null);
-            // assert(res.data.profile.multifactorAuthenticationProviderTypesSupported !== null);
-            // assert(res.data.profile.ticketTypesSupported !== null);
+            assert(res.data.profile.registeredServiceTypesSupported !== null);
+            assert(res.data.profile.availableAuthenticationHandlers !== null);
+            assert(res.data.profile.availableAttributes !== null);
+            assert(res.data.profile.delegatedClientTypesSupported !== null);
+            assert(res.data.profile.userDefinedScopes !== null);
+            assert(res.data.profile.multifactorAuthenticationProviderTypesSupported !== null);
+            assert(res.data.profile.ticketTypesSupported !== null);
         },
         error => {
             throw error;
