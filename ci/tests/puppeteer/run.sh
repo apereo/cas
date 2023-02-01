a4902741abfdce4c4781e15f03ac53a76df4864e
Misagh Moayyed <mm1844@gmail.com>
2023-02-01 10:16:02 +0400

    troubleshoot hanging builds

--- ci/tests/puppeteer/run.sh
+++ ci/tests/puppeteer/run.sh
@@ -40,6 +40,41 @@ function printred() {
   printf "${RED}$1${ENDCOLOR}\n"
 }
 
+function progressbar() {
+    current="$1"
+    total="$2"
+
+    bar_size=60
+    bar_char_done="#"
+    bar_char_todo="-"
+    bar_percentage_scale=2
+    
+    # calculate the progress in percentage
+    percent=$(bc <<< "scale=$bar_percentage_scale; 100 * $current / $total" )
+    # The number of done and todo characters
+    done=$(bc <<< "scale=0; $bar_size * $percent / 100" )
+    todo=$(bc <<< "scale=0; $bar_size - $done" )
+
+    # build the done and todo sub-bars
+    done_sub_bar=$(printf "%${done}s" | tr " " "${bar_char_done}")
+    todo_sub_bar=$(printf "%${todo}s" | tr " " "${bar_char_todo}")
+
+    # output the bar
+    echo -ne "\rProgress: [${done_sub_bar}${todo_sub_bar}] ${percent}%"
+#    if [ $total -eq $current ]; then
+#        echo -e "\nDONE"
+#    fi
+}
+
+function sleepfor() {
+  tasks_in_total="$1"
+  for current_task in $(seq "$tasks_in_total")
+  do
+    sleep 1
+    progressbar "$current_task" "$tasks_in_total"
+  done
+}
+
 casVersion=($(cat "$PWD"/gradle.properties | grep "version" | cut -d= -f2))
 echo -n "Running Puppeteer tests for Apereo CAS Server: " && printcyan "${casVersion}"
 
@@ -346,7 +381,7 @@ if [[ "${REBUILD}" == "true" && "${RERUN}" != "true" ]]; then
     printcyan "Launching build in background to make observing slow builds easier..."
     $buildcmd > build.log 2>&1 &
     pid=$!
-    sleep 25
+    sleepfor 25
     printgreen "Current Java processes found for PID ${pid}"
     ps -ef | grep $pid | grep java
     if [[ $? -ne 0 ]]; then
@@ -370,7 +405,7 @@ if [[ "${REBUILD}" == "true" && "${RERUN}" != "true" ]]; then
           exit 3
        fi
        echo -n '.'
-       sleep 30
+       sleepfor 30
     done
     wait $pid
     if [ $? -ne 0 ]; then
@@ -487,7 +522,7 @@ if [[ "${RERUN}" != "true" ]]; then
          ${properties} &
       pid=$!
       printcyan "Waiting for CAS instance #${c} under process id ${pid}"
-      sleep 60
+      sleepfor 30
       casLogin="https://localhost:${serverPort}/cas/login"
       printcyan "Checking CAS server's status @ ${casLogin}"
       curl -k -L --output /dev/null --silent --fail $casLogin
