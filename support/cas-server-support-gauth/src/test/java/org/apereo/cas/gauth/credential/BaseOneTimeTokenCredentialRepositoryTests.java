7d20d302a91ac798301fd9187ae4e0024773cd1a
Misagh Moayyed <mm1844@gmail.com>
2023-02-10 00:04:57 +0400

    fix build

--- support/cas-server-support-gauth/src/test/java/org/apereo/cas/gauth/credential/BaseOneTimeTokenCredentialRepositoryTests.java
+++ support/cas-server-support-gauth/src/test/java/org/apereo/cas/gauth/credential/BaseOneTimeTokenCredentialRepositoryTests.java
@@ -17,6 +17,7 @@ import org.apereo.cas.config.CasCoreServicesConfiguration;
 import org.apereo.cas.config.CasCoreTicketCatalogConfiguration;
 import org.apereo.cas.config.CasCoreTicketIdGeneratorsConfiguration;
 import org.apereo.cas.config.CasCoreTicketsConfiguration;
+import org.apereo.cas.config.CasCoreTicketsSerializationConfiguration;
 import org.apereo.cas.config.CasCoreUtilConfiguration;
 import org.apereo.cas.config.CasCoreWebConfiguration;
 import org.apereo.cas.config.CasPersonDirectoryConfiguration;
@@ -243,6 +244,7 @@ public abstract class BaseOneTimeTokenCredentialRepositoryTests {
         GoogleAuthenticatorAuthenticationMultifactorProviderBypassConfiguration.class,
         CasCoreTicketsConfiguration.class,
         CasCoreTicketIdGeneratorsConfiguration.class,
+        CasCoreTicketsSerializationConfiguration.class,
         CasCoreTicketCatalogConfiguration.class,
         CasCoreLogoutConfiguration.class,
         CasCoreHttpConfiguration.class,
