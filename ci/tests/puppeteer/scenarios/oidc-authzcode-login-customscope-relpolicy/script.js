b170e34671e01bbeb1d15f9c49244c90f32cab55
Misagh Moayyed <mm1844@gmail.com>
2023-02-15 22:41:02 +0400

    initial test setup

---
+++ ci/tests/puppeteer/scenarios/oidc-authzcode-login-customscope-relpolicy/script.js
@@ -0,0 +1,67 @@
+const puppeteer = require('puppeteer');
+const cas = require('../../cas.js');
+const assert = require('assert');
+
+(async () => {
+    const browser = await puppeteer.launch(cas.browserOptions());
+    const page = await cas.newPage(browser);
+    const url = "https://localhost:8443/cas/oidc/oidcAuthorize?response_type=code"
+        + "&client_id=client&scope=openid%20profile%20MyCustomScope&"
+        + "redirect_uri=https://apereo.github.io";
+
+    await cas.goto(page, url);
+    await page.waitForTimeout(1000);
+    await cas.loginWith(page, "casuser", "Mellon");
+    await page.waitForTimeout(1000);
+    if (await cas.isVisible(page, "#allow")) {
+        await cas.click(page, "#allow");
+        await page.waitForNavigation();
+    }
+
+    let code = await cas.assertParameter(page, "code");
+    console.log(`Current code is ${code}`);
+    const accessTokenUrl = `https://localhost:8443/cas/oidc/token?grant_type=authorization_code`
+        + `&client_id=client&client_secret=secret&redirect_uri=https://apereo.github.io&code=${code}`;
+    await cas.goto(page, accessTokenUrl);
+    await page.waitForTimeout(1000);
+    let content = await cas.textContent(page, "body");
+    const payload = JSON.parse(content);
+    console.log(payload);
+    assert(payload.access_token != null);
+    assert(payload.token_type != null);
+    assert(payload.expires_in != null);
+    assert(payload.scope != null);
+
+    let decoded = await cas.decodeJwt(payload.id_token);
+    // assert(decoded.sub !== null);
+    // assert(decoded.client_id !== null);
+    // assert(decoded["preferred_username"] !== null);
+    //
+    // assert(decoded["identity-name"] === undefined);
+    // assert(decoded["common-name"] === undefined);
+    // assert(decoded["lastname"] === undefined);
+    //
+    // assert(decoded["cn"] !== null);
+    // assert(decoded["family_name"] !== null);
+    // assert(decoded["name"] !== null);
+
+
+    let profileUrl = `https://localhost:8443/cas/oidc/profile?access_token=${payload.access_token }`;
+    console.log(`Calling user profile ${profileUrl}`);
+
+    await cas.doPost(profileUrl, "", {
+        'Content-Type': "application/json"
+    }, res => {
+        // assert(decoded["common-name"] === undefined);
+        // assert(decoded["lastname"] === undefined);
+        //
+        // assert(res.data["cn"] != null);
+        // assert(res.data["name"] != null);
+        // assert(res.data["family_name"] != null);
+        // assert(res.data.sub != null)
+    }, error => {
+        throw `Operation failed: ${error}`;
+    });
+
+    await browser.close();
+})();
