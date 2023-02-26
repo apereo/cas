5f04723a4696f9210b13fc498d068a31c029a855
dmalia1 <dmalia1@Gmail.com>
2023-02-26 09:44:26 -0600

    refactor: CasConfigurationWatchService-- passed in ClientInfo.

--- core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/CasConfigurationWatchService.java
+++ core/cas-server-core-configuration/src/main/java/org/apereo/cas/configuration/CasConfigurationWatchService.java
@@ -11,6 +11,7 @@ import org.apereo.cas.util.io.PathWatcherService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+import org.apereo.inspektr.common.web.ClientInfoHolder;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ConfigurableApplicationContext;
 
@@ -26,11 +27,11 @@ import java.io.File;
 @Slf4j
 @RequiredArgsConstructor
 public class CasConfigurationWatchService implements Closeable, InitializingBean {
-    private final ComposableFunction<File, AbstractCasEvent> createConfigurationCreatedEvent = file -> new CasConfigurationCreatedEvent(this, file.toPath(), null);
+    private final ComposableFunction<File, AbstractCasEvent> createConfigurationCreatedEvent = file -> new CasConfigurationCreatedEvent(this, file.toPath(), ClientInfoHolder.getClientInfo());
 
-    private final ComposableFunction<File, AbstractCasEvent> createConfigurationModifiedEvent = file -> new CasConfigurationModifiedEvent(this, file.toPath(), null);
+    private final ComposableFunction<File, AbstractCasEvent> createConfigurationModifiedEvent = file -> new CasConfigurationModifiedEvent(this, file.toPath(), ClientInfoHolder.getClientInfo());
 
-    private final ComposableFunction<File, AbstractCasEvent> createConfigurationDeletedEvent = file -> new CasConfigurationDeletedEvent(this, file.toPath(), null);
+    private final ComposableFunction<File, AbstractCasEvent> createConfigurationDeletedEvent = file -> new CasConfigurationDeletedEvent(this, file.toPath(), ClientInfoHolder.getClientInfo());
 
     private final CasConfigurationPropertiesEnvironmentManager configurationPropertiesEnvironmentManager;
 
