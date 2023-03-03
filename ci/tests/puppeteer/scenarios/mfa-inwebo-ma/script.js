a1f867067ccf8a6565f3cff13e77ea3a55fee2f9
Misagh Moayyed <mm1844@gmail.com>
2023-03-03 17:03:19 +0400

    fix inwebo tests with MA

--- ci/tests/puppeteer/scenarios/mfa-inwebo-ma/script.js
+++ ci/tests/puppeteer/scenarios/mfa-inwebo-ma/script.js
@@ -9,19 +9,9 @@ const cas = require('../../cas.js');
     await cas.loginWith(page, "testcas", "password");
     await page.waitForTimeout(5000);
     await cas.screenshot(page);
-
-    const startBrowserForm = await page.$('#startBrowserForm');
-    assert(startBrowserForm != null);
-    const startPushForm = await page.$('#startPushForm');
-    assert(startPushForm != null);
-
     console.log("Asking for the PIN code");
-    await page.$eval('button[name=browser]', button => button.click());
-    await page.waitForTimeout(8000);
-
     await cas.assertVisibility(page, "#code");
     await cas.assertVisibility(page, "#pin");
     await cas.assertVisibility(page, "#enrollButton");
-
     await browser.close();
 })();
