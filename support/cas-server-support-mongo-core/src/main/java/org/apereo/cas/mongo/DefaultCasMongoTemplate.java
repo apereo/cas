76ec9c0bc39198f47187759df4a2d9e8013ac045
David Malia <althor@ksu.edu>
2023-03-15 16:03:33 -0500

    refactor: move try-with-resources to defaultCasMongoTemplate
    Moved this to the defaultCasMongoTemplate.
    The streaming is getting
    called from the MongoDbTicketRegistry as well,
    so reverted the change to
    MongoDbCasEventRepository, and
    then overrode the doStream method in the
    DefaulCasMongoTemplate class.

--- support/cas-server-support-mongo-core/src/main/java/org/apereo/cas/mongo/DefaultCasMongoTemplate.java
+++ support/cas-server-support-mongo-core/src/main/java/org/apereo/cas/mongo/DefaultCasMongoTemplate.java
@@ -1,8 +1,15 @@
 package org.apereo.cas.mongo;
 
+import lombok.val;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * This is {@link DefaultCasMongoTemplate}.
@@ -14,5 +21,13 @@ public class DefaultCasMongoTemplate extends MongoTemplate implements CasMongoOp
     public DefaultCasMongoTemplate(final MongoDatabaseFactory mongoDbFactory,
                                    final MappingMongoConverter mappingMongoConverter) {
         super(mongoDbFactory, mappingMongoConverter);
+    }
+
+
+    @Override
+    protected <T> Stream<T> doStream(final Query query, final Class<?> entityType, final String collectionName, final Class<T> returnType) {
+        try (val stream = super.doStream(query, entityType, collectionName, returnType)){
+            return stream.collect(Collectors.toList()).stream();
+        }
     }
 }
