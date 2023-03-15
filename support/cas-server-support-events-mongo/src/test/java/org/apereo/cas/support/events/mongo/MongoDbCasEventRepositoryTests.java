7dfa6374a72211f3fe1e617992383ece5937f5b6
David Malia <althor@ksu.edu>
2023-03-15 14:56:56 -0500

    test: Added failing test
    Added test to show that the mongdb cursors are not getting
    closed
    automatically. This can lead to issues where high traffic on
    login can cause the mongodb to
    run out of open cursors, as the open
    cursors hang around until mongodb closes them automatically.
    This is
    analogous to Oracle Database Connection cursors, where you can have too
    many open cursors on your connection, and one should close them when no
    longer needed.

--- support/cas-server-support-events-mongo/src/test/java/org/apereo/cas/support/events/mongo/MongoDbCasEventRepositoryTests.java
+++ support/cas-server-support-events-mongo/src/test/java/org/apereo/cas/support/events/mongo/MongoDbCasEventRepositoryTests.java
@@ -1,17 +1,33 @@
 package org.apereo.cas.support.events.mongo;
 
+
 import org.apereo.cas.config.CasCoreHttpConfiguration;
 import org.apereo.cas.config.MongoDbEventsConfiguration;
+import org.apereo.cas.mock.MockTicketGrantingTicket;
+import org.apereo.cas.support.events.AbstractCasEvent;
 import org.apereo.cas.support.events.AbstractCasEventRepositoryTests;
 import org.apereo.cas.support.events.CasEventRepository;
+import org.apereo.cas.support.events.dao.CasEvent;
+import org.apereo.cas.support.events.ticket.CasTicketGrantingTicketCreatedEvent;
+import org.apereo.cas.util.DateTimeUtils;
 import org.apereo.cas.util.junit.EnabledIfListeningOnPort;
 
 import lombok.Getter;
+import lombok.val;
+import org.bson.Document;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * Test cases for {@link MongoDbCasEventRepository}.
@@ -38,7 +54,59 @@ import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
 @EnabledIfListeningOnPort(port = 27017)
 public class MongoDbCasEventRepositoryTests extends AbstractCasEventRepositoryTests {
 
+    private static final int NUM_OF_EVENTS_LARGER_THAN_MIN_BATCH_SIZE = 103;
+
     @Autowired
     @Qualifier(CasEventRepository.BEAN_NAME)
     private CasEventRepository eventRepository;
+
+    @Autowired
+    @Qualifier("mongoEventsTemplate")
+    private MongoTemplate mongoTemplate;
+
+    private long currentCount;
+
+
+
+    @BeforeEach
+    public void setup() throws Exception {
+        currentCount = getOpenCursorCount();
+        val tgt = new MockTicketGrantingTicket("casuser");
+        val event = new CasTicketGrantingTicketCreatedEvent(this, tgt, null);
+        for (var x = 0; x < NUM_OF_EVENTS_LARGER_THAN_MIN_BATCH_SIZE; x++) {
+            val dto = prepareCasEvent(event);
+            dto.setCreationTime(event.getTicketGrantingTicket().getCreationTime().toString());
+            dto.putEventId(event.getTicketGrantingTicket().getId());
+            dto.setPrincipalId(event.getTicketGrantingTicket().getAuthentication().getPrincipal().getId());
+            eventRepository.save(dto);
+        }
+    }
+
+    @Test
+    public void makeSureCursorsGetClosed() throws Exception {
+        val stream = eventRepository.load();
+        assertNotNull(stream);
+        assertEquals(currentCount, getOpenCursorCount());
+
+    }
+
+    private CasEvent prepareCasEvent(final AbstractCasEvent event) {
+        val dto = new CasEvent();
+        dto.setType(event.getClass().getCanonicalName());
+        dto.putTimestamp(event.getTimestamp());
+        val dt = DateTimeUtils.zonedDateTimeOf(Instant.ofEpochMilli(event.getTimestamp()));
+        dto.setCreationTime(dt.toString());
+        return dto;
+    }
+
+    public long getOpenCursorCount() {
+        var cmd = new Document("serverStatus", 1);
+        cmd.append("metrics", true);
+        cmd.append("cursor", true);
+        var result = mongoTemplate.executeCommand(cmd);
+        var open = (Document) result.get("metrics", Document.class).get("cursor", Document.class).get("open", Document.class);
+        return open.getLong("total");
+    }
+
+
 }
