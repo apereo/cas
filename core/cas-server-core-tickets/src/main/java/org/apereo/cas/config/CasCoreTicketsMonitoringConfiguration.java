9b477f9825d558e9fed90a3bffc6534913d3b8d1
Misagh Moayyed <mm1844@gmail.com>
2023-03-17 10:01:47 +0400

    minor build fixes

--- core/cas-server-core-tickets/src/main/java/org/apereo/cas/config/CasCoreTicketsMonitoringConfiguration.java
+++ core/cas-server-core-tickets/src/main/java/org/apereo/cas/config/CasCoreTicketsMonitoringConfiguration.java
@@ -13,13 +13,14 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.jooq.lambda.Unchecked;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 
 /**
  * This is {@link CasCoreTicketsMonitoringConfiguration}.
@@ -35,6 +36,7 @@ import org.springframework.context.annotation.Bean;
 })
 @ConditionalOnBean(name = ExecutableObserver.BEAN_NAME)
 @AutoConfiguration
+@AutoConfigureOrder(Ordered.LOWEST_PRECEDENCE)
 public class CasCoreTicketsMonitoringConfiguration {
 
     @Bean
@@ -48,16 +50,11 @@ public class CasCoreTicketsMonitoringConfiguration {
     @SuppressWarnings("UnusedMethod")
     record TicketRegistryMonitoringAspect(ObjectProvider<ExecutableObserver> observerProvider) {
         @Around("allComponentsInTicketRegistryNamespace()")
-        public Object aroundTicketRegistryOperations(final ProceedingJoinPoint joinPoint) {
-            return observerProvider
-                .stream()
-                .map(Unchecked.function(observer -> {
-                    val taskName = joinPoint.getSignature().getDeclaringTypeName() + '.' + joinPoint.getSignature().getName();
-                    val task = new MonitorableTask(taskName);
-                    return observer.supply(task, () -> executeJoinpoint(joinPoint));
-                }))
-                .findFirst()
-                .orElseGet(() -> executeJoinpoint(joinPoint));
+        public Object aroundTicketRegistryOperations(final ProceedingJoinPoint joinPoint) throws Throwable {
+            val observer = observerProvider.getObject();
+            val taskName = joinPoint.getSignature().getDeclaringTypeName() + '.' + joinPoint.getSignature().getName();
+            val task = new MonitorableTask(taskName);
+            return observer.supply(task, () -> executeJoinpoint(joinPoint));
         }
 
         private static Object executeJoinpoint(final ProceedingJoinPoint joinPoint) {
