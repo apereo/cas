f97e5fccbed90e1e7e7d5f1eea9c3087c1f1cd6a
Misagh Moayyed <mm1844@gmail.com>
2023-04-01 22:00:13 +0400

    fix tests - wip

--- ci/tests/gcp/entrypoint.sh
+++ ci/tests/gcp/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-echo "Setting project id to ${GCP_PROJECT_ID}"
-gcloud config set project "${GCP_PROJECT_ID}"
+echo "Running $GCLOUD_COMPONENT"
 
-# Start the process in the background to allow subsequent services to start
-echo "Starting Firestore emulator on port ${FIRESTORE_PORT}"
-gcloud beta emulators firestore start --host-port="0.0.0.0:${FIRESTORE_PORT}"
-
-## The last command must not be sent to the background
-## to allow the Docker container to keep running
-#echo "Starting PubSub emulator on port ${PUBSUB_PORT}"
-#gcloud beta emulators pubsub start --host-port="0.0.0.0:${PUBSUB_PORT}"
+case "$GCLOUD_COMPONENT" in
+"pubsub")
+  echo "Starting PubSub emulator on port ${PUBSUB_PORT}"
+  gcloud beta emulators pubsub start --project="${GCP_PROJECT_ID}" --host-port="0.0.0.0:${PUBSUB_PORT}"
+  ;;
+"firestore")
+  echo "Starting Firestore emulator on port ${FIRESTORE_PORT}"
+  gcloud beta emulators firestore start --project="${GCP_PROJECT_ID}" --host-port="0.0.0.0:${FIRESTORE_PORT}"
+  ;;
+esac
