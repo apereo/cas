5d7d605076164db9eaf32137f70a84db90f9459b
Misagh Moayyed <mm1844@gmail.com>
2023-04-04 10:52:39 +0400

    allow oauth services to pass through OIDC endpoints; fix type checking

--- ci/tests/puppeteer/scenarios/oidc-authzcode-idtoken-issuer/script.js
+++ ci/tests/puppeteer/scenarios/oidc-authzcode-idtoken-issuer/script.js
@@ -2,13 +2,9 @@ const puppeteer = require('puppeteer');
 const cas = require('../../cas.js');
 const assert = require('assert');
 
-(async () => {
-    const browser = await puppeteer.launch(cas.browserOptions());
-    const page = await cas.newPage(browser);
-    const url = "https://localhost:8443/cas/oidc/authorize?response_type=code"
-        + "&client_id=client&scope=openid%20profile&"
-        + "redirect_uri=https://apereo.github.io";
-
+async function testService(page, clientId, oidc = true) {
+    console.log(`Testing application with client id ${clientId}`);
+    const url = `https://localhost:8443/cas/oidc/authorize?response_type=code&client_id=${clientId}&scope=openid%20profile&redirect_uri=https://apereo.github.io`;
     await cas.goto(page, url);
     await page.waitForTimeout(1000);
     await cas.loginWith(page, "casuser", "Mellon");
@@ -21,25 +17,40 @@ const assert = require('assert');
 
     let code = await cas.assertParameter(page, "code");
     console.log(`Current code is ${code}`);
-    const accessTokenUrl = `https://localhost:8443/cas/oidc/token?grant_type=authorization_code`
-        + `&client_id=client&client_secret=secret&redirect_uri=https://apereo.github.io&code=${code}`;
+    const accessTokenUrl = `https://localhost:8443/cas/oidc/token?grant_type=authorization_code&client_id=${clientId}&client_secret=secret&redirect_uri=https://apereo.github.io&code=${code}`;
     await cas.goto(page, accessTokenUrl);
     await page.waitForTimeout(1000);
     let content = await cas.textContent(page, "body");
     const payload = JSON.parse(content);
     console.log(payload);
     assert(payload.access_token != null);
-    assert(payload.id_token != null);
 
     console.log("Decoding access token...");
     let decodedAccessToken = await cas.decodeJwt(payload.access_token);
-    assert(decodedAccessToken.iss === "https://sso.example.org/cas/oidc");
 
-    console.log("Decoding ID token...");
-    let decodedIdToken = await cas.decodeJwt(payload.id_token);
-    assert(decodedIdToken.sub !== null);
-    assert(decodedIdToken.client_id !== null);
-    assert(decodedIdToken.iss === "https://sso.example.org/cas/oidc");
+    if (oidc) {
+        assert(decodedAccessToken.iss === "https://sso.example.org/cas/oidc");
+        console.log("Decoding ID token...");
+        assert(payload.id_token != null);
+        let decodedIdToken = await cas.decodeJwt(payload.id_token);
+        assert(decodedIdToken.sub !== null);
+        assert(decodedIdToken.client_id !== null);
+        assert(decodedIdToken.iss === "https://sso.example.org/cas/oidc");
+    } else {
+        assert(decodedAccessToken.grant_type === "authorization_code");
+        assert(decodedAccessToken.iss === "https://localhost:8443/cas/oidc");
+        assert(decodedAccessToken.client_id === "oauth-clientid");
+    }
+    
+    await cas.goto(page, `https://localhost:8443/cas/logout`);
+    await page.waitForTimeout(1000);
+    console.log("=========================================================")
+}
 
+(async () => {
+    const browser = await puppeteer.launch(cas.browserOptions());
+    const page = await cas.newPage(browser);
+    await testService(page, "client", true);
+    await testService(page, "oauth-clientid", false);
     await browser.close();
 })();
