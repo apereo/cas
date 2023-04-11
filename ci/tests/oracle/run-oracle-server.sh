f90f746654d2c0315135c601447d5676dbbaa2f2
Misagh Moayyed <mm1844@gmail.com>
2023-04-11 09:23:55 +0400

    update docker images

--- ci/tests/oracle/run-oracle-server.sh
+++ ci/tests/oracle/run-oracle-server.sh
@@ -4,7 +4,8 @@
 
 echo "Running Oracle docker image..."
 docker stop oracle-db || true
-docker run --rm -d -p 1521:1521 --name oracle-db --rm store/oracle/database-enterprise:12.2.0.1-slim
+#docker run --rm -d -p 1521:1521 --name oracle-db --rm store/oracle/database-enterprise:12.2.0.1-slim
+docker run --rm -d -p 1521:1521 --name oracle-db --rm gvenzl/oracle-free:23
 echo "Waiting for Oracle image to prepare..."
 sleep 90
 docker ps | grep "oracle-db"
