92bcb90ee1801e507fb62d5492c74955a72c6b51
Misagh Moayyed <mm1844@gmail.com>
2023-05-02 17:23:43 +0400

    fix doc generation process - wip

--- ci/docs/publish.sh
+++ ci/docs/publish.sh
@@ -46,9 +46,9 @@ publishDocs=true
 buildDocs=true
 serve=false
 clone=true
-buildFeatures=false
-shellCommands=false
-dependencyVersions=false
+buildFeatures=true
+shellCommands=true
+dependencyVersions=true
 
 while (("$#")); do
   case "$1" in
@@ -259,46 +259,46 @@ else
   rm -Rf "$PWD/gh-pages/_data"
 fi
 
-#if [[ $proofRead == "true" ]]; then
-#  printgreen "Looking for badly named include fragments..."
-#  ls "$PWD"/gh-pages/_includes/$branchVersion/*.md | grep -v '\-configuration.md$'
-#  docsVal=$?
-#  if [ $docsVal == 0 ]; then
-#    printred "Found include fragments whose name does not end in '-configuration.md'"
-#    exit 1
-#  fi
-#
-#  printgreen "Looking for unused include fragments..."
-#  res=0
-#  files=$(ls $PWD/gh-pages/_includes/$branchVersion/*.md)
-#  for f in $files; do
-#    fname=$(basename "$f")
-#    #  echo "Looking for $fname in $PWD/gh-pages/$branchVersion";
-#    grep -r $fname "$PWD/gh-pages/$branchVersion" --include \*.md >/dev/null 2>&1
-#    docsVal=$?
-#    if [ $docsVal == 1 ]; then
-#      grep -r $fname "$PWD/gh-pages/_includes/$branchVersion" --include \*.md >/dev/null 2>&1
-#      docsVal=$?
-#    fi
-#    if [ $docsVal == 1 ]; then
-#      grep "fragment:keep" $f >/dev/null 2>&1
-#      docsVal=$?
-#      if [ $docsVal == 1 ]; then
-#        printred "$f is unused."
-#        rm "docs/cas-server-documentation/_includes/$fname"
-#        res=1
-#      fi
-#    fi
-#  done
-#
-#  if [ $res == 1 ]; then
-#    printred "Found unused include fragments."
-#    exit 1
-#  fi
-#
-#else
-#  printgreen "Skipping validation of documentation links..."
-#fi
+if [[ $proofRead == "true" ]]; then
+  printgreen "Looking for badly named include fragments..."
+  ls "$PWD"/gh-pages/_includes/$branchVersion/*.md | grep -v '\-configuration.md$'
+  docsVal=$?
+  if [ $docsVal == 0 ]; then
+    printred "Found include fragments whose name does not end in '-configuration.md'"
+    exit 1
+  fi
+
+  printgreen "Looking for unused include fragments..."
+  res=0
+  files=$(ls $PWD/gh-pages/_includes/$branchVersion/*.md)
+  for f in $files; do
+    fname=$(basename "$f")
+    #  echo "Looking for $fname in $PWD/gh-pages/$branchVersion";
+    grep -r $fname "$PWD/gh-pages/$branchVersion" --include \*.md >/dev/null 2>&1
+    docsVal=$?
+    if [ $docsVal == 1 ]; then
+      grep -r $fname "$PWD/gh-pages/_includes/$branchVersion" --include \*.md >/dev/null 2>&1
+      docsVal=$?
+    fi
+    if [ $docsVal == 1 ]; then
+      grep "fragment:keep" $f >/dev/null 2>&1
+      docsVal=$?
+      if [ $docsVal == 1 ]; then
+        printred "$f is unused."
+        rm "docs/cas-server-documentation/_includes/$fname"
+        res=1
+      fi
+    fi
+  done
+
+  if [ $res == 1 ]; then
+    printred "Found unused include fragments."
+    exit 1
+  fi
+
+else
+  printgreen "Skipping validation of documentation links..."
+fi
 
 
 if [[ ${buildDocs} == "true" ]]; then
@@ -330,77 +330,77 @@ if [[ ${buildDocs} == "true" ]]; then
   popd
 fi
 
-#if [[ $proofRead == "true" ]]; then
-#  printgreen "Validating documentation links..."
-#  validateProjectDocumentation
-#  retVal=$?
-#  if [[ ${retVal} -eq 1 ]]; then
-#    printred "Failed to validate documentation.\n"
-#    exit ${retVal}
-#  fi
-#fi
+if [[ $proofRead == "true" ]]; then
+  printgreen "Validating documentation links..."
+  validateProjectDocumentation
+  retVal=$?
+  if [[ ${retVal} -eq 1 ]]; then
+    printred "Failed to validate documentation.\n"
+    exit ${retVal}
+  fi
+fi
 
 pushd .
 cd "$PWD/gh-pages"
 
-#if [[ $clone == "true" ]]; then
-#  rm -Rf .jekyll-cache .jekyll-metadata .sass-cache "$branchVersion/build"
-#  printgreen "\nConfiguring git repository settings...\n"
-#  rm -Rf .git
-#  git init
-#  git config init.defaultBranch master
-#  git remote add origin "${REPOSITORY_ADDR}"
-#  git config user.email "cas@apereo.org"
-#  git config user.name "CAS"
-#  git config core.fileMode false
-#
-#  printgreen "Checking out gh-pages branch..."
-#  git switch gh-pages 2>/dev/null || git switch -c gh-pages 2>/dev/null
-#  printgreen "Configuring tracking branches for repository...\n"
-#  git branch -u origin/gh-pages
-#
-#  rm -Rf "./$branchVersion"
-#  mv "_site/$branchVersion" .
-#  touch "$branchVersion/.nojekyll"
-#  rm -Rf _site
-#  rm -Rf _data
-#fi
+if [[ $clone == "true" ]]; then
+  rm -Rf .jekyll-cache .jekyll-metadata .sass-cache "$branchVersion/build"
+  printgreen "\nConfiguring git repository settings...\n"
+  rm -Rf .git
+  git init
+  git config init.defaultBranch master
+  git remote add origin "${REPOSITORY_ADDR}"
+  git config user.email "cas@apereo.org"
+  git config user.name "CAS"
+  git config core.fileMode false
 
-#if [[ "${publishDocs}" == "true" ]]; then
-#  printgreen "Adding changes to the git index...\n"
-#  git add --all -f 2>/dev/null
-#
-#  printgreen "Committing changes...\n"
-#  git commit -am "Published docs to [gh-pages] from $branchVersion." 2>/dev/null
-#  git status
-#
-#  printgreen "Pushing changes to remote repository...\n"
-#  if [ -z "$GH_PAGES_TOKEN" ] && [ "${GITHUB_REPOSITORY}" != "${REPOSITORY_NAME}" ]; then
-#    printyellow "\nNo GitHub token is defined to publish documentation. Skipping"
-#    popd
-#    if [[ $clone == "true" ]]; then
-#      rm -Rf "$PWD/gh-pages"
-#      exit 0
-#    fi
-#  fi
-#
-#  printgreen "Pushing upstream to origin/gh-pages...\n"
-#  git push -fq origin gh-pages
-#  retVal=$?
-#else
-#  printyellow "Skipping documentation push to remote repository...\n"
-#fi
+  printgreen "Checking out gh-pages branch..."
+  git switch gh-pages 2>/dev/null || git switch -c gh-pages 2>/dev/null
+  printgreen "Configuring tracking branches for repository...\n"
+  git branch -u origin/gh-pages
+
+  rm -Rf "./$branchVersion"
+  mv "_site/$branchVersion" .
+  touch "$branchVersion/.nojekyll"
+  rm -Rf _site
+  rm -Rf _data
+fi
+
+if [[ "${publishDocs}" == "true" ]]; then
+  printgreen "Adding changes to the git index...\n"
+  git add --all -f 2>/dev/null
+
+  printgreen "Committing changes...\n"
+  git commit -am "Published docs to [gh-pages] from $branchVersion." 2>/dev/null
+  git status
+
+  printgreen "Pushing changes to remote repository...\n"
+  if [ -z "$GH_PAGES_TOKEN" ] && [ "${GITHUB_REPOSITORY}" != "${REPOSITORY_NAME}" ]; then
+    printyellow "\nNo GitHub token is defined to publish documentation. Skipping"
+    popd
+    if [[ $clone == "true" ]]; then
+      rm -Rf "$PWD/gh-pages"
+      exit 0
+    fi
+  fi
+
+  printgreen "Pushing upstream to origin/gh-pages...\n"
+  git push -fq origin gh-pages
+  retVal=$?
+else
+  printyellow "Skipping documentation push to remote repository...\n"
+fi
 
 popd
 
-#if [[ $clone == "true" ]]; then
-#  rm -Rf "$PWD/gh-pages"
-#fi
-#
-#if [[ ${retVal} -eq 0 ]]; then
-#  printgreen "Done processing documentation to $branchVersion.\n"
-#  exit 0
-#else
-#  printred "Failed to process documentation.\n"
-#  exit ${retVal}
-#fi
+if [[ $clone == "true" ]]; then
+  rm -Rf "$PWD/gh-pages"
+fi
+
+if [[ ${retVal} -eq 0 ]]; then
+  printgreen "Done processing documentation to $branchVersion.\n"
+  exit 0
+else
+  printred "Failed to process documentation.\n"
+  exit ${retVal}
+fi
