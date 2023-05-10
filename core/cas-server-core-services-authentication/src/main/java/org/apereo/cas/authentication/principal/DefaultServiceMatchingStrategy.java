2336dab2ecb8d071e3bf37f1cf041ee0b8478cfc
Misagh Moayyed <mm1844@gmail.com>
2023-05-10 22:02:55 +0400

    allow matching strategy to remove fragment for service validations
    also encode the fragment on form submissions

--- core/cas-server-core-services-authentication/src/main/java/org/apereo/cas/authentication/principal/DefaultServiceMatchingStrategy.java
+++ core/cas-server-core-services-authentication/src/main/java/org/apereo/cas/authentication/principal/DefaultServiceMatchingStrategy.java
@@ -5,9 +5,11 @@ import org.apereo.cas.util.LoggingUtils;
 
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+import org.apache.commons.lang3.StringUtils;
 
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.util.regex.Pattern;
 
 /**
  * This is {@link DefaultServiceMatchingStrategy}.
@@ -17,17 +19,24 @@ import java.nio.charset.StandardCharsets;
  */
 @Slf4j
 public record DefaultServiceMatchingStrategy(ServicesManager servicesManager) implements ServiceMatchingStrategy {
+    private static final Pattern FRAGMENT_PATTERN = Pattern.compile("#.+");
+
     @Override
     public boolean matches(final Service service, final Service serviceToMatch) {
         try {
-            val thisUrl = URLDecoder.decode(service.getId(), StandardCharsets.UTF_8);
-            val serviceUrl = URLDecoder.decode(serviceToMatch.getId(), StandardCharsets.UTF_8);
-
-            LOGGER.trace("Decoded urls and comparing [{}] with [{}]", thisUrl, serviceUrl);
-            return thisUrl.equalsIgnoreCase(serviceUrl);
+            if (service != null && serviceToMatch != null) {
+                val thisUrl = removeFragmentFrom(URLDecoder.decode(service.getId(), StandardCharsets.UTF_8));
+                val serviceUrl = removeFragmentFrom(URLDecoder.decode(serviceToMatch.getId(), StandardCharsets.UTF_8));
+                LOGGER.debug("Decoded urls and comparing [{}] with [{}]", thisUrl, serviceUrl);
+                return thisUrl.equalsIgnoreCase(serviceUrl);
+            }
         } catch (final Exception e) {
             LoggingUtils.error(LOGGER, e);
         }
         return false;
+    }
+
+    private static String removeFragmentFrom(final String id) {
+        return FRAGMENT_PATTERN.matcher(id).replaceAll(StringUtils.EMPTY);
     }
 }
--- core/cas-server-core-services-authentication/src/test/java/org/apereo/cas/authentication/principal/DefaultServiceMatchingStrategyTests.java
+++ core/cas-server-core-services-authentication/src/test/java/org/apereo/cas/authentication/principal/DefaultServiceMatchingStrategyTests.java
@@ -3,6 +3,7 @@ package org.apereo.cas.authentication.principal;
 import org.apereo.cas.services.ServicesManager;
 
 import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -18,11 +19,57 @@ import static org.mockito.Mockito.*;
 @Tag("Authentication")
 public class DefaultServiceMatchingStrategyTests {
 
-    @Test
-    public void verifyOperation() {
+    private ServiceMatchingStrategy strategy;
+
+    @BeforeEach
+    public void setup() {
         val mgr = mock(ServicesManager.class);
-        val strategy = new DefaultServiceMatchingStrategy(mgr);
+        this.strategy = new DefaultServiceMatchingStrategy(mgr);
+    }
+
+    @Test
+    public void verifyServicesAsNull() {
         assertFalse(strategy.matches(null, null));
     }
 
+    @Test
+    public void verifyServicesMatch() {
+        val service = getService("https://www.google.org");
+        assertTrue(strategy.matches(service, service));
+    }
+
+    @Test
+    public void verifyServicesDifferById() {
+        val service1 = getService("https://www.google.org/");
+        val service2 = getService("https://www.google.org");
+        assertFalse(strategy.matches(service1, service2));
+    }
+
+    @Test
+    public void verifyServicesDifferByWWW() {
+        val service1 = getService("https://google.org");
+        val service2 = getService("https://www.google.org");
+        assertFalse(strategy.matches(service1, service2));
+    }
+
+    @Test
+    public void verifyServicesMatchByFragmentEncoded() {
+        val service1 = getService("https://google.org");
+        val service2 = getService("https://google.org%23/A/B/C");
+        assertTrue(strategy.matches(service1, service2));
+    }
+
+    @Test
+    public void verifyServicesMatchByFragmentDecoded() {
+        val service1 = getService("https://google.org");
+        val service2 = getService("https://google.org#/A/B/C");
+        assertTrue(strategy.matches(service1, service2));
+    }
+
+    private static Service getService(final String id) {
+        val service = mock(WebApplicationService.class);
+        when(service.getId()).thenReturn(id);
+        when(service.getOriginalUrl()).thenReturn(id);
+        return service;
+    }
 }
