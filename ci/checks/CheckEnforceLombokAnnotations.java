48d6474551b7842764e3324f92e74829e22c0475
Misagh Moayyed <mm1844@gmail.com>
2023-05-10 11:18:35 +0400

    webauthn: fixed for metadata attestation trust with fido metadata svc

--- ci/checks/CheckEnforceLombokAnnotations.java
+++ ci/checks/CheckEnforceLombokAnnotations.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Locale;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.regex.Pattern;
 
