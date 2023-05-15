c6bfe7d85e0d861ad2a2f915d556ffe028c831a6
Hal Deadman <hdeadman@users.noreply.github.com>
2023-05-15 02:06:07 -0400

    fix sonarqube puppeteer test (#5642)

--- ci/tests/puppeteer/scenarios/oidc-login-sonarqube/script.js
+++ ci/tests/puppeteer/scenarios/oidc-login-sonarqube/script.js
@@ -5,8 +5,8 @@ const cas = require('../../cas.js');
     const browser = await puppeteer.launch(cas.browserOptions());
     const page = await cas.newPage(browser);
 
-    await cas.goto(page, "https://host.k3d.internal");
-    await page.waitForTimeout(5000);
+    await cas.goto(page, "https://host.k3d.internal/sessions/new?return_to=%2F");
+    await page.waitForTimeout(10000);
     await cas.assertPageTitle(page, "SonarQube");
     await cas.assertInnerText(page, 'h1.login-title',"Log in to SonarQube");
     //await cas.sleep(5000);
