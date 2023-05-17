ce3de588db0d4f43f211437afb824d5c28da066e
Misagh Moayyed <mm1844@gmail.com>
2023-05-17 07:46:53 +0400

    pick up the correct current password when changing passwords

--- support/cas-server-support-pm-webflow/src/main/java/org/apereo/cas/pm/web/flow/actions/PasswordChangeAction.java
+++ support/cas-server-support-pm-webflow/src/main/java/org/apereo/cas/pm/web/flow/actions/PasswordChangeAction.java
@@ -60,7 +60,7 @@ public class PasswordChangeAction extends BaseCasWebflowAction {
         try {
             val bean = getPasswordChangeRequest(requestContext);
             Optional.ofNullable(WebUtils.getCredential(requestContext, UsernamePasswordCredential.class))
-                    .ifPresent(credential -> bean.setCurrentPassword(bean.getCurrentPassword()));
+                    .ifPresent(credential -> bean.setCurrentPassword(bean.getPassword()));
             
             LOGGER.debug("Attempting to validate the password change bean for username [{}]", bean.getUsername());
             if (StringUtils.isBlank(bean.getUsername()) || !passwordValidationService.isValid(bean)) {
