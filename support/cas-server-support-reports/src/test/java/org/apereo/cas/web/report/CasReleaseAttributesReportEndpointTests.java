6defa165532378b7c774212d990fe1a999522090
Misagh Moayyed <mm1844@gmail.com>
2023-05-18 08:16:51 +0400

    allow attribute-release actuator to work w/o password

--- support/cas-server-support-reports/src/test/java/org/apereo/cas/web/report/CasReleaseAttributesReportEndpointTests.java
+++ support/cas-server-support-reports/src/test/java/org/apereo/cas/web/report/CasReleaseAttributesReportEndpointTests.java
@@ -1,10 +1,14 @@
 package org.apereo.cas.web.report;
 
+import org.apereo.cas.services.RegisteredService;
 import org.apereo.cas.services.RegisteredServiceTestUtils;
 
 import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.TestPropertySource;
@@ -26,14 +30,22 @@ public class CasReleaseAttributesReportEndpointTests extends AbstractCasEndpoint
     @Qualifier("releaseAttributesReportEndpoint")
     private CasReleaseAttributesReportEndpoint endpoint;
 
-    @Test
-    public void verifyOperation() {
-        val service = RegisteredServiceTestUtils.getRegisteredService(UUID.randomUUID().toString());
-        servicesManager.save(service);
-        val response = endpoint.releasePrincipalAttributes("casuser", "casuser", service.getServiceId());
+    private RegisteredService registeredService;
+
+    @BeforeEach
+    public void setup() {
+        registeredService = RegisteredServiceTestUtils.getRegisteredService(UUID.randomUUID().toString());
+        servicesManager.save(registeredService);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = "casuser")
+    @NullAndEmptySource
+    public void verifyOperation(final String password) {
+        val response = endpoint.releasePrincipalAttributes("casuser", password, registeredService.getServiceId());
         assertNotNull(response);
         assertFalse(response.isEmpty());
-        assertNotNull(endpoint.releaseAttributes("casuser", "casuser", service.getServiceId()));
+        assertNotNull(endpoint.releaseAttributes("casuser", password, registeredService.getServiceId()));
     }
 }
 
