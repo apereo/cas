5ef25acb255315603bb1560a315e636f47fa51aa
Misagh Moayyed <mm1844@gmail.com>
2023-06-06 16:15:08 +0400

    clean up

--- core/cas-server-core-util-api/src/test/java/org/apereo/cas/util/logging/LogMessageSummarizerTests.java
+++ core/cas-server-core-util-api/src/test/java/org/apereo/cas/util/logging/LogMessageSummarizerTests.java
@@ -8,18 +8,26 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * This is {@link DisabledLogMessageSummarizerTests}.
+ * This is {@link LogMessageSummarizerTests}.
  *
  * @author Misagh Moayyed
  * @since 7.0.0
  */
 @Tag("Utility")
 @Slf4j
-public class DisabledLogMessageSummarizerTests {
+public class LogMessageSummarizerTests {
     @Test
     public void verifyOperation() throws Exception {
         val summarizer = new DisabledLogMessageSummarizer();
         assertFalse(summarizer.shouldSummarize(LOGGER));
         assertTrue(summarizer.summarizeStackTrace("Message", new IllegalArgumentException("Error")).isEmpty());
     }
+
+    @Test
+    public void verifyExceptionMessageIsNull() {
+        val defaultLogMessageSummarizer = new DefaultLogMessageSummarizer();
+        assertNotNull(defaultLogMessageSummarizer.summarizeStackTrace(null, new IllegalArgumentException()));
+    }
 }
+
+
