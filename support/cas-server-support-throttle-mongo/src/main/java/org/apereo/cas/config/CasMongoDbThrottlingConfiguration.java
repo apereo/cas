a1e077d878ff79246fcc2b51a58047eb3c05c1b4
Misagh Moayyed <mm1844@gmail.com>
2023-06-18 21:33:54 +0400

    fix tests

--- support/cas-server-support-throttle-mongo/src/main/java/org/apereo/cas/config/CasMongoDbThrottlingConfiguration.java
+++ support/cas-server-support-throttle-mongo/src/main/java/org/apereo/cas/config/CasMongoDbThrottlingConfiguration.java
@@ -3,19 +3,30 @@ package org.apereo.cas.config;
 import org.apereo.cas.authentication.CasSSLContext;
 import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.configuration.features.CasFeatureModule;
+import org.apereo.cas.mongo.BaseConverters;
 import org.apereo.cas.mongo.MongoDbConnectionFactory;
+import org.apereo.cas.util.DateTimeUtils;
+import org.apereo.cas.util.function.FunctionUtils;
+import org.apereo.cas.util.serialization.JacksonObjectMapperFactory;
 import org.apereo.cas.util.spring.boot.ConditionalOnFeatureEnabled;
 import org.apereo.cas.web.support.MongoDbThrottledSubmissionHandlerInterceptorAdapter;
 import org.apereo.cas.web.support.ThrottledSubmissionHandlerConfigurationContext;
 import org.apereo.cas.web.support.ThrottledSubmissionHandlerInterceptor;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.val;
+import org.apereo.inspektr.audit.AuditActionContext;
+import org.bson.Document;
+import org.bson.json.JsonMode;
+import org.bson.json.JsonWriterSettings;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ScopedProxyMode;
+import javax.annotation.Nonnull;
+import java.util.List;
 
 /**
  * This is {@link CasMongoDbThrottlingConfiguration}.
@@ -37,9 +48,23 @@ public class CasMongoDbThrottlingConfiguration {
         @Qualifier(CasSSLContext.BEAN_NAME)
         final CasSSLContext casSslContext) {
         val mongo = casProperties.getAudit().getMongo();
-        val factory = new MongoDbConnectionFactory(casSslContext.getSslContext());
+        val factory = new MongoDbConnectionFactory(List.of(new AuditActionContextConverter()), casSslContext.getSslContext());
         val mongoTemplate = factory.buildMongoTemplate(mongo);
         MongoDbConnectionFactory.createCollection(mongoTemplate, mongo.getCollection(), mongo.isDropCollection());
         return new MongoDbThrottledSubmissionHandlerInterceptorAdapter(authenticationThrottlingConfigurationContext, mongoTemplate, mongo.getCollection());
+    }
+
+    private static class AuditActionContextConverter extends BaseConverters.NullConverter<Document, AuditActionContext> {
+        private static final ObjectMapper MAPPER = JacksonObjectMapperFactory.builder()
+            .defaultTypingEnabled(false).build().toObjectMapper();
+
+        @Override
+        public AuditActionContext convert(@Nonnull final Document document) {
+            return FunctionUtils.doUnchecked(() ->
+                MAPPER.readValue(document.toJson(JsonWriterSettings.builder()
+                    .outputMode(JsonMode.RELAXED)
+                    .dateTimeConverter((value, writer) -> writer.writeString(DateTimeUtils.localDateTime(value).toString()))
+                    .build()), AuditActionContext.class));
+        }
     }
 }
