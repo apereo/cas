937ac82b44443e9bbb1f93668317abd409e25ef1
Misagh Moayyed <mm1844@gmail.com>
2023-06-20 23:14:50 +0400

    fix tests

--- ci/tests/puppeteer/configure-ci.sh
+++ ci/tests/puppeteer/configure-ci.sh
@@ -18,15 +18,16 @@ function printyellow() {
 SCENARIO_CONFIG_FILE="$PWD/ci/tests/puppeteer/scenarios/$1/script.json"
 echo "Checking scenario configuration file: ${SCENARIO_CONFIG_FILE}"
 
-NODE_VERSION_REQUIRED=$(cat "$SCENARIO_CONFIG_FILE" | jq -j '.requirements.nodejs')
+NODE_VERSION_REQUIRED=$(cat "$SCENARIO_CONFIG_FILE" | jq -j '.requirements.nodejs // empty')
 NATIVE_BUILD_ENABLED=$(cat "$SCENARIO_CONFIG_FILE" | jq -j '.requirements.graalvm.enabled // false')
 
 if [[ -z $NODE_VERSION_REQUIRED ]]; then
+  echo "Scenario configuration does not specify a Node.js version"
   NODE_VERSION_REQUIRED=${NODE_CURRENT}
 fi
 if [[ -z $NATIVE_BUILD_ENABLED ]]; then
   NATIVE_BUILD_ENABLED="false"
 fi
 
-printgreen "Nodejs version: ${NODE_VERSION_REQUIRED}"
+printgreen "Node.js version: ${NODE_VERSION_REQUIRED}"
 printgreen "Graal VM native build enabled: ${NATIVE_BUILD_ENABLED}"
