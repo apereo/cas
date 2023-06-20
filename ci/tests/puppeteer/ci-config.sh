cefa4ef8a43a2010b4c0d6c08fa569285bc368c4
Misagh Moayyed <mm1844@gmail.com>
2023-06-20 19:46:39 +0400

    fix tests

--- ci/tests/puppeteer/ci-config.sh
+++ ci/tests/puppeteer/ci-config.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
-echo "Current Nodejs version: ${NODE_CURRENT}"
-export NODE_REQUIRED=${NODE_CURRENT}
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+ENDCOLOR="\e[0m"
 
-CI_CONFIG="$PWD/ci/tests/puppeteer/scenarios/$1/ci.json"
-echo "Checking scenario CI configuration file: ${CI_CONFIG}"
+function printred() {
+  printf "${RED}$1${ENDCOLOR}\n"
+}
+function printgreen() {
+  printf "${GREEN}$1${ENDCOLOR}\n"
+}
+function printyellow() {
+  printf "${YELLOW}$1${ENDCOLOR}\n"
+}
 
-if [[ -f $CI_CONFIG ]]; then
-  echo "Found $CI_CONFIG file"
-  cat "$CI_CONFIG"
-  export NODE_REQUIRED=$(cat "$CI_CONFIG" | jq -j '."nodejs.version"')
+SCENARIO_CONFIG_FILE="$PWD/ci/tests/puppeteer/scenarios/$1/script.json"
+echo "Checking scenario configuration file: ${SCENARIO_CONFIG_FILE}"
+
+NODE_VERSION_REQUIRED=$(cat "$SCENARIO_CONFIG_FILE" | jq -j '.requirements.nodejs')
+NATIVE_BUILD_ENABLED=$(cat "$SCENARIO_CONFIG_FILE" | jq -j '.requirements.graalvm.enabled // false')
+
+if [[ -z $NODE_VERSION_REQUIRED ]]; then
+  NODE_VERSION_REQUIRED=${NODE_CURRENT}
 fi
-echo "Final Nodejs version: ${NODE_REQUIRED}"
+if [[ -z $NATIVE_BUILD_ENABLED ]]; then
+  NATIVE_BUILD_ENABLED="false"
+fi
+
+printgreen "Nodejs version: ${NODE_VERSION_REQUIRED}"
+printgreen "Graal VM native build enabled: ${NATIVE_BUILD_ENABLED}"
