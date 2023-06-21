27d4e5860a6d67ca8775c4c4c239f9f42f2ca0ed
Miguel <mmcamara@unav.es>
2023-06-21 13:04:37 +0200

    Parametrized run-saml-server.sh to allow running multiple instances.

--- ci/tests/saml2/run-saml-server.sh
+++ ci/tests/saml2/run-saml-server.sh
@@ -3,7 +3,7 @@
 if [[ -z "${SP_SLO_SERVICE}" ]]; then
   export SP_SLO_SERVICE="https://localhost:8443/cas/login?client_name=SAML2Client&logoutendpoint=true"
 else
-  echo -e "Found existing SLO service at ${SP_SLO_SERVICE}"
+  echo -e "Found existing SLO service at 8443"
 fi
 
 if [[ -z "${SP_ACS_SERVICE}" ]]; then
@@ -13,13 +13,15 @@ else
 fi
 
 if [[ -z "${SP_ENTITY_ID}" ]]; then
-  export SP_ENTITY_ID="cas:apereo:pac4j:saml"
+  export SP_ENTITY_ID="cas:apereo:pac4j:saml:${SP_HOST_PORT:-9443}"
 else
   echo -e "Found existing SP entity id at ${SP_ENTITY_ID}"
 fi
 
-docker stop simplesamlphp-idp || true && docker rm simplesamlphp-idp || true
-echo -e "Running SAML2 IdP with SP entity id ${SP_ENTITY_ID} and SP ACS service ${SP_ACS_SERVICE} on port 9443"
+export CONTAINER_ID=simplesamlphp-idp-${SP_HOST_PORT:-9443}
+
+docker stop ${CONTAINER_ID} || true && docker rm ${CONTAINER_ID} || true
+echo -e "Running SAML2 IdP with SP entity id ${SP_ENTITY_ID} and SP ACS service ${SP_ACS_SERVICE} on port ${SP_HOST_PORT:-9443}"
 
 echo "Creating private key and certificate for SP metadata"
 openssl req -newkey rsa:3072 -new -x509 -days 365 \
@@ -43,7 +45,7 @@ fi
 echo -e "Using IDP signing certificate:\n$IDP_SIGNING_CERTIFICATE"
 echo -e "Using IDP encryption certificate:\n$IDP_ENCRYPTION_CERTIFICATE"
 
-docker run --name=simplesamlphp-idp -p 9443:8080 \
+docker run --name=${CONTAINER_ID} -p ${SP_HOST_PORT:-9443}:8080 \
   -e SIMPLESAMLPHP_SP_ENTITY_ID="${SP_ENTITY_ID}" \
   -e SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE="${SP_ACS_SERVICE}" \
   -e SIMPLESAMLPHP_SP_SINGLE_LOGOUT_SERVICE="${SP_SLO_SERVICE}" \
