apereo/cas

commit 901d8895f99dd72d72973a951cd2d8876c6ac6ff

decouple service session tracking from tgt to allow for customizations

core/cas-server-core-tickets-api/src/main/java/org/apereo/cas/ticket/DefaultServiceTicketSessionTrackingPolicy.java
@@ -1,0 +1,65 @@
+package org.apereo.cas.ticket;
+
+import org.apereo.cas.authentication.principal.Service;
+import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.ticket.registry.TicketRegistry;
+
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.apache.commons.lang3.StringUtils;
+import org.jooq.lambda.Unchecked;
+
+import java.util.stream.Collectors;
+
+/**
+ * This is {@link DefaultServiceTicketSessionTrackingPolicy}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.6.0
+ */
+@RequiredArgsConstructor
+public class DefaultServiceTicketSessionTrackingPolicy implements ServiceTicketSessionTrackingPolicy {
+    private final CasConfigurationProperties casProperties;
+
+    private final TicketRegistry ticketRegistry;
+
+    @Override
+    public synchronized void track(final AuthenticatedServicesAwareTicketGrantingTicket ownerTicket,
+                                   final ServiceTicket serviceTicket) {
+        ownerTicket.update();
+        serviceTicket.getService().setPrincipal(ownerTicket.getRoot().getAuthentication().getPrincipal().getId());
+
+        val onlyTrackMostRecentSession = casProperties.getTicket().getTgt().getCore().isOnlyTrackMostRecentSession();
+        if (onlyTrackMostRecentSession) {
+            val path = normalizePath(serviceTicket.getService());
+            val toRemove = ownerTicket.getAuthenticatedServices()
+                .entrySet()
+                .stream()
+                .filter(entry -> {
+                    val normalizedExistingPath = normalizePath(entry.getValue());
+                    return path.equals(normalizedExistingPath);
+                })
+                .collect(Collectors.toList());
+
+            toRemove.forEach(Unchecked.consumer(entry -> {
+                ownerTicket.getAuthenticatedServices().remove(entry.getKey());
+                ticketRegistry.deleteTicket(entry.getKey());
+            }));
+        }
+        ownerTicket.getAuthenticatedServices().put(serviceTicket.getId(), serviceTicket.getService());
+    }
+
+    /**
+     * Normalize the path of a service by removing the query string and everything after a semi-colon.
+     *
+     * @param service the service to normalize
+     * @return the normalized path
+     */
+    private static String normalizePath(final Service service) {
+        var path = service.getId();
+        path = StringUtils.substringBefore(path, "?");
+        path = StringUtils.substringBefore(path, ";");
+        path = StringUtils.substringBefore(path, "#");
+        return path;
+    }
+}

.github/workflows/test-ubuntu.yml
@@ -11,7 +11,7 @@ env:
   GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
   JDK_CURRENT: 11
   RETRY_MINUTES: 180
-  CATEGORY_REGEX: ".*" # Use this to limit which tests run
+  CATEGORY_REGEX: ".*" # Limit which test categories run
 
 ##########################################################################
 

api/cas-server-core-api-ticket/src/main/java/org/apereo/cas/ticket/AuthenticatedServicesAwareTicketGrantingTicket.java
@@ -1,0 +1,16 @@
+package org.apereo.cas.ticket;
+
+import org.apereo.cas.authentication.principal.Service;
+
+import java.util.Map;
+
+/**
+ * This is {@link AuthenticatedServicesAwareTicketGrantingTicket}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.6.0
+ */
+public interface AuthenticatedServicesAwareTicketGrantingTicket extends TicketGrantingTicket {
+
+    Map<String, Service> getAuthenticatedServices();
+}

api/cas-server-core-api-ticket/src/main/java/org/apereo/cas/ticket/ServiceTicketSessionTrackingPolicy.java
@@ -1,0 +1,24 @@
+package org.apereo.cas.ticket;
+
+/**
+ * This is {@link ServiceTicketSessionTrackingPolicy}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.6.0
+ */
+@FunctionalInterface
+public interface ServiceTicketSessionTrackingPolicy {
+
+    String BEAN_NAME = "serviceTicketSessionTrackingPolicy";
+
+    /**
+     * Track application attempt and access.
+     * Typically, ticket-granting tickets keep track of applications
+     * and service tickets for which they are authorized to issue tickets.
+     *
+     * @param ownerTicket   the owner ticket
+     * @param serviceTicket the service ticket
+     */
+    void track(AuthenticatedServicesAwareTicketGrantingTicket ownerTicket,
+               ServiceTicket serviceTicket);
+}

api/cas-server-core-api-ticket/src/main/java/org/apereo/cas/ticket/TicketGrantingTicket.java
@@ -27,36 +27,27 @@ public interface TicketGrantingTicket extends TicketGrantingTicketAwareTicket {
     String PREFIX = "TGT";
 
     /**
-     * Update service and track session.
-     *
-     * @param id                         the id
-     * @param service                    the service
-     * @param onlyTrackMostRecentSession the only track most recent session
-     */
-    void trackService(String id, Service service, boolean onlyTrackMostRecentSession);
-
-    /**
      * Grant a ServiceTicket for a specific service.
      *
-     * @param id                         The unique identifier for this ticket.
-     * @param service                    The service for which we are granting a ticket
-     * @param expirationPolicy           the expiration policy.
-     * @param credentialProvided         current credential event for issuing this ticket. Could be null.
-     * @param onlyTrackMostRecentSession track the most recent session by keeping the latest service ticket
+     * @param id                 The unique identifier for this ticket.
+     * @param service            The service for which we are granting a ticket
+     * @param expirationPolicy   the expiration policy.
+     * @param credentialProvided current credential event for issuing this ticket. Could be null.
+     * @param trackingPolicy     the tracking policy
      * @return the service ticket granted to a specific service for the principal of the TicketGrantingTicket
      */
     ServiceTicket grantServiceTicket(String id,
                                      Service service,
                                      ExpirationPolicy expirationPolicy,
                                      boolean credentialProvided,
-                                     boolean onlyTrackMostRecentSession);
+                                     ServiceTicketSessionTrackingPolicy trackingPolicy);
 
     /**
      * Gets an immutable map of service ticket and services accessed by this ticket-granting ticket.
      *
      * @return an immutable map of service ticket and services accessed by this ticket-granting ticket.
      */
-    Map<String, Service> getServices();
+    Map<String, Service> getAuthenticatedServices();
 
     /**
      * Gets proxy granting tickets created by this TGT.

api/cas-server-core-api-ticket/src/main/java/org/apereo/cas/ticket/proxy/ProxyGrantingTicket.java
@@ -2,6 +2,7 @@ package org.apereo.cas.ticket.proxy;
 
 import org.apereo.cas.authentication.principal.Service;
 import org.apereo.cas.ticket.ExpirationPolicy;
+import org.apereo.cas.ticket.ServiceTicketSessionTrackingPolicy;
 import org.apereo.cas.ticket.TicketGrantingTicket;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
@@ -30,16 +31,15 @@ public interface ProxyGrantingTicket extends TicketGrantingTicket {
     /**
      * Grant a proxy ticket for a specific service.
      *
-     * @param id                         The unique identifier for this ticket.
-     * @param service                    The service for which we are granting a ticket
-     * @param expirationPolicy           the expiration policy.
-     * @param onlyTrackMostRecentSession track the most recent session by keeping the latest service ticket
-     * @return the service ticket granted to a specific service for the
-     * principal of the TicketGrantingTicket
+     * @param id               The unique identifier for this ticket.
+     * @param service          The service for which we are granting a ticket
+     * @param expirationPolicy the expiration policy.
+     * @param trackingPolicy   the tracking policy
+     * @return the service ticket granted to a specific service for the principal of the TicketGrantingTicket
      */
     ProxyTicket grantProxyTicket(String id, Service service,
                                  ExpirationPolicy expirationPolicy,
-                                 boolean onlyTrackMostRecentSession);
+                                 ServiceTicketSessionTrackingPolicy trackingPolicy);
 
 }
 

core/cas-server-core-logout-api/src/main/java/org/apereo/cas/logout/DefaultLogoutManager.java
@@ -57,7 +57,7 @@ public class DefaultLogoutManager implements LogoutManager {
     private List<SingleLogoutRequestContext> performLogoutForTicket(final SingleLogoutExecutionRequest context) {
         val ticketToBeLoggedOut = context.getTicketGrantingTicket();
         val streamServices = Stream.concat(
-            Stream.of(ticketToBeLoggedOut.getServices()),
+            Stream.of(ticketToBeLoggedOut.getAuthenticatedServices()),
             Stream.of(ticketToBeLoggedOut.getProxyGrantingTickets()));
         val logoutServices = streamServices
             .map(Map::entrySet)

core/cas-server-core-logout/src/test/java/org/apereo/cas/logout/DefaultLogoutManagerTests.java
@@ -97,7 +97,7 @@ public class DefaultLogoutManagerTests {
             new DefaultAuthenticationServiceSelectionPlan(new DefaultAuthenticationServiceSelectionStrategy()));
 
         this.simpleWebApplicationServiceImpl = getService(URL);
-        tgt.getServices().put(ID, this.simpleWebApplicationServiceImpl);
+        tgt.getAuthenticatedServices().put(ID, this.simpleWebApplicationServiceImpl);
 
         val plan = new DefaultLogoutExecutionPlan();
         plan.registerSingleLogoutServiceMessageHandler(singleLogoutServiceMessageHandler);

commit 1784de0df051ec27c817164b3e1f9cda181a5726

fix: tracking policy should not delete service tickets
This reverts a hard to spot / reproduce issue introduced by 901d8895f99:
If more STs/PTs are issued one right after another from the same parent ticket, validation of
the first ticket(s) can fail, because it might get deleted before
its validation call arrives.

In practice, this probably affects just use cases when STs/PTs are
issued via CAS REST API - i.e. applications which for whatever reason
don't keep a standard session with a called backend application.
Anyway, it seems safer to rather rely on the short-lived tickets' automatic expiration,
at least by default.

core/cas-server-core-tickets-api/src/main/java/org/apereo/cas/ticket/DefaultServiceTicketSessionTrackingPolicy.java
@@ -7,7 +7,6 @@ import org.apereo.cas.ticket.registry.TicketRegistry;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.apache.commons.lang3.StringUtils;
-import org.jooq.lambda.Unchecked;
 
 /**
  * This is {@link DefaultServiceTicketSessionTrackingPolicy}.
@@ -52,10 +51,9 @@ public class DefaultServiceTicketSessionTrackingPolicy implements ServiceTicketS
                     return path.equals(normalizedExistingPath);
                 }).toList();
 
-            toRemove.forEach(Unchecked.consumer(entry -> {
+            toRemove.forEach(entry -> {
                 ownerTicket.getServices().remove(entry.getKey());
-                ticketRegistry.deleteTicket(entry.getKey());
-            }));
+            });
         }
         ownerTicket.getServices().put(serviceTicket.getId(), serviceTicket.getService());
     }