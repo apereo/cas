3fea9303fd23c454d2b1a81c0ddb0d0cbb3b24c1
Ryan Mathis <rmathis@unicon.net>
2023-08-10 15:08:07 -0700

    Added views for services

--- support/cas-server-support-palantir/client/vite.config.js
+++ support/cas-server-support-palantir/client/vite.config.js
@@ -3,27 +3,36 @@ import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'https://localhost:8443',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      }
-    }
-  },
-  plugins: [react()],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/main.jsx'),
-      name: 'Palantir',
-      fileName: (format) => `palantir.${format}.js`
+export default defineConfig(({mode}) => ({
+    define: {
+        'process.env.NODE_ENV': JSON.stringify(mode),
     },
-    rollupOptions: {
-      external: [],
-      output: {}
+    target: 'esnext',
+    base: '/cas/palantir',
+    server: {
+        port: 3000,
+        proxy: {
+            '/api': {
+                target: 'https://localhost:8443',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            }
+        }
+    },
+    plugins: [
+        react(),
+    ],
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/main.jsx'),
+            name: 'Palantir',
+            fileName: (format) => `palantir.${format}.js`,
+            formats: [
+                'cjs',
+                'umd',
+                'iife',
+                'es'
+            ]
+        }
     }
-  }
-});
+  }));
