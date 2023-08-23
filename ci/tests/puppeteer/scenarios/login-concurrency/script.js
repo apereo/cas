c7bb352dafb150a9ae41ac7e00b2646201e1518d
Misagh Moayyed <mm1844@gmail.com>
2023-08-23 08:24:50 +0400

    add test case for concurrent logins with saml-validate

---
+++ ci/tests/puppeteer/scenarios/login-concurrency/script.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+const fs = require('fs');
+const {JSONPath} = require('jsonpath-plus');
+
+const startPuppeteerLoadTest = require('puppeteer-loadtest');
+let args = process.argv.slice(2);
+const config = JSON.parse(fs.readFileSync(args[0]));
+assert(config != null);
+
+const paramOptions = {
+    file: config.loadScript,
+    samplesRequested: 25,
+    concurrencyRequested: 10
+};
+const loadtest = async () => await startPuppeteerLoadTest(paramOptions);
+
+loadtest().then(results => {
+    console.log(JSON.stringify(results, null, 2));
+    const samples = JSONPath({path: '$..sample', json: results });
+    assert(samples.length === parseInt(paramOptions.samplesRequested))
+});
---
+++ ci/tests/puppeteer/scenarios/login-concurrency/test.js
@@ -0,0 +1,66 @@
+const puppeteer = require('puppeteer');
+const cas = require('../../cas.js');
+const https = require("https");
+const assert = require("assert");
+
+(async () => {
+    const browser = await puppeteer.launch(cas.browserOptions());
+    const service = "https://localhost:9859/anything/app";
+    let ticket = null;
+    try {
+        const page = await cas.newPage(browser);
+        const availableUsers = ["casuser", "casadmin"];
+        const userid = availableUsers[Math.floor(Math.random() * availableUsers.length)];
+        console.log(`Selected user: ${userid}`);
+        await cas.goto(page, `https://localhost:8443/cas/login?TARGET=${service}`);
+        await cas.loginWith(page, userid, "Mellon");
+        ticket = await cas.assertParameter(page, "SAMLart");
+    } finally {
+        await browser.close();
+    }
+
+    let request = `<?xml version="1.0" encoding="UTF-8"?>
+<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
+<SOAP-ENV:Header/>
+<SOAP-ENV:Body>
+<samlp:Request xmlns:samlp="urn:oasis:names:tc:SAML:1.0:protocol" MajorVersion="1"
+MinorVersion="1" RequestID="_192.168.16.51.1024506224022"
+IssueInstant="2023-03-19T17:03:44.022Z">
+<samlp:AssertionArtifact>${ticket}</samlp:AssertionArtifact>
+</samlp:Request>
+</SOAP-ENV:Body>
+</SOAP-ENV:Envelope>`;
+
+    let options = {
+        protocol: 'https:',
+        hostname: 'localhost',
+        port: 8443,
+        path: `/cas/samlValidate?TARGET=${service}&SAMLart=${ticket}`,
+        method: 'POST',
+        rejectUnauthorized: false,
+        headers: {
+            'Content-Length': request.length
+        }
+    };
+
+    const post = options =>
+        new Promise((resolve, reject) => {
+            let req = https
+                .request(options, res => {
+                    res.setEncoding('utf8');
+                    const body = [];
+                    res.on('data', chunk => body.push(chunk));
+                    res.on('end', () => resolve(body.join('')));
+                })
+                .on('error', reject);
+            req.write(request);
+        });
+    const body = await post(options);
+    console.log(body);
+
+    if (userid === "casuser") {
+        assert(`body.contains("<saml1:AttributeValue>USER-ACCOUNT</saml1:AttributeValue>")`);
+    } else {
+        assert(`body.contains("<saml1:AttributeValue>ADMINISTRATOR</saml1:AttributeValue>")`);
+    }
+})();
