2b7215ea58cb513d724b66f5aa6ab4160cd6f277
Misagh Moayyed <mm1844@gmail.com>
2023-08-24 10:18:24 +0400

    refactor script handling

--- core/cas-server-core-util-api/src/main/java/org/apereo/cas/util/scripting/GroovyShellScript.java
+++ core/cas-server-core-util-api/src/main/java/org/apereo/cas/util/scripting/GroovyShellScript.java
@@ -3,13 +3,11 @@ package org.apereo.cas.util.scripting;
 import groovy.lang.Binding;
 import groovy.lang.Script;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.apache.commons.lang3.builder.ToStringBuilder;
-
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * This is {@link GroovyShellScript}.
@@ -20,14 +18,10 @@ import java.util.concurrent.locks.ReentrantLock;
 @Getter
 @Slf4j
 public class GroovyShellScript implements ExecutableCompiledGroovyScript {
-    private final ReentrantLock executionLock = new ReentrantLock();
-
     private final Script groovyScript;
-
     private final String script;
 
-    @Setter
-    private Map<String, Object> binding = new HashMap<>();
+    private final ThreadLocal<Map<String, Object>> bindingThreadLocal = new ThreadLocal<>();
 
     public GroovyShellScript(final String script) {
         this.script = script;
@@ -45,19 +39,26 @@ public class GroovyShellScript implements ExecutableCompiledGroovyScript {
     }
 
     @Override
-    public <T> T execute(final Object[] args, final Class<T> clazz, final boolean failOnError) {
-        executionLock.lock();
+    public synchronized <T> T execute(final Object[] args, final Class<T> clazz, final boolean failOnError) {
         try {
             LOGGER.trace("Beginning to execute script [{}]", this);
+            val binding = bindingThreadLocal.get();
             if (groovyScript != null) {
                 if (binding != null && !binding.isEmpty()) {
+                    LOGGER.trace("Setting binding [{}]", binding);
                     groovyScript.setBinding(new Binding(binding));
                 }
-                return ScriptingUtils.executeGroovyShellScript(groovyScript, clazz);
+                LOGGER.trace("Current binding [{}]", groovyScript.getBinding());
+                val result = ScriptingUtils.executeGroovyShellScript(groovyScript, clazz);
+                LOGGER.debug("Groovy script [{}] returns result [{}]", this, result);
+                return result;
             }
             return null;
         } finally {
-            executionLock.unlock();
+            bindingThreadLocal.remove();
+            if (groovyScript != null) {
+                groovyScript.setBinding(new Binding(Map.of()));
+            }
             LOGGER.trace("Completed script execution [{}]", this);
         }
     }
@@ -68,10 +69,15 @@ public class GroovyShellScript implements ExecutableCompiledGroovyScript {
     }
 
     @Override
+    public void setBinding(final Map<String, Object> args) {
+        bindingThreadLocal.set(new HashMap<>(args));
+    }
+
+
+    @Override
     public String toString() {
         return new ToStringBuilder(this)
             .append("script", script)
-            .append("executionLock", executionLock)
             .toString();
     }
 }
