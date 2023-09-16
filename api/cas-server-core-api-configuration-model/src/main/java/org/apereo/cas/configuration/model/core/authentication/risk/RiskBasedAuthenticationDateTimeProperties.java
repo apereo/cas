db7df04c2491ff1c4bc8fa386dcdb79a38d92651
Misagh Moayyed <mm1844@gmail.com>
2023-09-16 09:37:41 +0400

    reorganize config properties for risk authn

---
+++ api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/core/authentication/risk/RiskBasedAuthenticationDateTimeProperties.java
@@ -0,0 +1,35 @@
+package org.apereo.cas.configuration.model.core.authentication.risk;
+
+import org.apereo.cas.configuration.support.RequiresModule;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * This is {@link RiskBasedAuthenticationDateTimeProperties}.
+ *
+ * @author Misagh Moayyed
+ * @since 7.0.0
+ */
+@Getter
+@Setter
+@Accessors(chain = true)
+@RequiresModule(name = "cas-server-support-electrofence")
+public class RiskBasedAuthenticationDateTimeProperties implements Serializable {
+    @Serial
+    private static final long serialVersionUID = -3776875583039922050L;
+
+    /**
+     * Enable date/time checking and criteria
+     * to calculate risky authentication attempts.
+     */
+    private boolean enabled;
+
+    /**
+     * The hourly window used before and after each authentication event
+     * in calculation to establish a pattern that can then be compared against the threshold.
+     */
+    private int windowInHours = 2;
+}
---
+++ api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/core/authentication/risk/RiskBasedAuthenticationProperties.java
@@ -0,0 +1,63 @@
+package org.apereo.cas.configuration.model.core.authentication.risk;
+
+import org.apereo.cas.configuration.support.RequiresModule;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * This is {@link RiskBasedAuthenticationProperties}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.1.0
+ */
+@RequiresModule(name = "cas-server-support-electrofence")
+@Getter
+@Setter
+@Accessors(chain = true)
+public class RiskBasedAuthenticationProperties implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 3826749727400569308L;
+
+    /**
+     * Handle risky authentication attempts via an IP criteria.
+     */
+    @NestedConfigurationProperty
+    private RiskBasedAuthenticationIpAddressProperties ip = new RiskBasedAuthenticationIpAddressProperties();
+
+    /**
+     * Handle risky authentication attempts via a user-agent criteria.
+     */
+    @NestedConfigurationProperty
+    private RiskBasedAuthenticationUserAgentProperties agent = new RiskBasedAuthenticationUserAgentProperties();
+
+    /**
+     * Handle risky authentication attempts via geolocation criteria.
+     */
+    @NestedConfigurationProperty
+    private RiskBasedAuthenticationGeoLocationProperties geoLocation = new RiskBasedAuthenticationGeoLocationProperties();
+
+    /**
+     * Handle risky authentication attempts via an date/time criteria.
+     */
+    @NestedConfigurationProperty
+    private RiskBasedAuthenticationDateTimeProperties dateTime = new RiskBasedAuthenticationDateTimeProperties();
+
+    /**
+     * Design how responses should be handled, in the event
+     * that an authentication event is deemed risky.
+     */
+    @NestedConfigurationProperty
+    private RiskBasedAuthenticationResponseProperties response = new RiskBasedAuthenticationResponseProperties();
+
+    /**
+     * Core configuration settings for assessing risky authentication attempts.
+     */
+    @NestedConfigurationProperty
+    private RiskBasedAuthenticationCoreProperties core = new RiskBasedAuthenticationCoreProperties();
+
+}
