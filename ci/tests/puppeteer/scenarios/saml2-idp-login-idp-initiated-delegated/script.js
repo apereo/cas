14dd7f1d5f5e375c16659589f1703cab37cd89dd
Misagh Moayyed <mm1844@gmail.com>
2023-09-25 20:57:48 +0400

    make sure saml2 address fields in confirmation data are IPs

--- ci/tests/puppeteer/scenarios/saml2-idp-login-idp-initiated-delegated/script.js
+++ ci/tests/puppeteer/scenarios/saml2-idp-login-idp-initiated-delegated/script.js
@@ -20,9 +20,11 @@ async function startFlow(context, clientName) {
     assert(content.form.SAMLResponse != null);
     let samlResponse = await cas.base64Decode(content.form.SAMLResponse);
     let parsedResult = await cas.parseXML(samlResponse);
+    console.dir(parsedResult, {depth: null, colors: true});
     assert(parsedResult["saml2p:Response"]["$"]["InResponseTo"] === undefined);
     const subjectConfirmation = parsedResult["saml2p:Response"]["saml2:Assertion"][0]["saml2:Subject"][0]["saml2:SubjectConfirmation"][0];
     assert(subjectConfirmation["saml2:SubjectConfirmationData"][0]["$"]["InResponseTo"] === undefined);
+    assert(subjectConfirmation["saml2:SubjectConfirmationData"][0]["$"]["Address"] === "127.0.0.1");
 
     await cas.gotoLogin(page, "https://apereo.github.io");
     await page.waitForTimeout(6000);
