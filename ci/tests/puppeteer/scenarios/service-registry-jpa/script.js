8dce4f35f512b6faed605a4b21e2166cc07cc897
Misagh Moayyed <mm1844@gmail.com>
2023-09-29 14:36:33 +0400

    add test case for jpa services - check reliability while db is down

---
+++ ci/tests/puppeteer/scenarios/service-registry-jpa/script.js
@@ -0,0 +1,84 @@
+const assert = require('assert');
+const cas = require('../../cas.js');
+const fs = require('fs');
+const path = require("path");
+const puppeteer = require("puppeteer");
+
+const TOTAL = 20;
+const BASE_URL = "https://localhost:8443/cas/actuator/registeredServices";
+
+async function fetchServices() {
+    await cas.log("Fetching services from CAS");
+    return await cas.doGet(BASE_URL,
+        res => {
+            assert(res.status === 200);
+            assert(res.data[1].length === TOTAL);
+            return res.data[1];
+        },
+        error => {
+            throw error;
+        }, {
+            'Content-Type': 'application/json'
+        });
+}
+
+async function verifyServices() {
+    await cas.log("Verifying services from CAS");
+    for (let i = 1; i <= TOTAL; i++) {
+        await cas.doGet(`${BASE_URL}/${i}`,
+            res => {
+            }, error => {
+                throw error;
+            }, { 'Content-Type': "application/json" });
+    }
+}
+
+
+async function importServices() {
+    let template = path.join(__dirname, 'registered-service.json');
+    let contents = fs.readFileSync(template, 'utf8');
+    for (let i = 1; i <= TOTAL; i++) {
+        let serviceBody = contents.replaceAll("${id}", String(i));
+        await cas.log(`Import registered service:\n${serviceBody}`);
+        await cas.doRequest(`${BASE_URL}/import`, "POST", {
+            'Accept': 'application/json',
+            'Content-Length': serviceBody.length,
+            'Content-Type': 'application/json'
+        }, 201, serviceBody);
+    }
+}
+
+(async () => {
+    let mysql = await cas.dockerContainer("mysql-server");
+
+    await importServices();
+    await fetchServices();
+
+    await cas.log("Pausing MySQL docker container");
+    await mysql.pause();
+
+    await verifyServices();
+
+    const browser = await puppeteer.launch(cas.browserOptions());
+    const page = await cas.newPage(browser);
+    const service = "https://apereo.github.io";
+    await cas.gotoLogin(page, service);
+    await cas.loginWith(page);
+    let ticket = await cas.assertTicketParameter(page);
+    let body = await cas.doRequest(`https://localhost:8443/cas/p3/serviceValidate?service=${service}&ticket=${ticket}&format=JSON`);
+    await cas.log(body);
+    let json = JSON.parse(body);
+    let authenticationSuccess = json.serviceResponse.authenticationSuccess;
+    assert(authenticationSuccess.user === "casuser");
+    await cas.gotoLogout(page);
+    await browser.close();
+
+    await cas.log("Unpausing MySQL docker container");
+    await mysql.unpause();
+    
+    await cas.sleep(2000);
+    await fetchServices();
+    await verifyServices();
+    await cas.logg("All CAS services are available");
+    await process.exit(0);
+})();
