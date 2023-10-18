7cac10ada4b4d79f862b561e84c1243bca9a8ca7
Misagh Moayyed <mm1844@gmail.com>
2023-10-18 21:02:30 +0400

    fix tests

--- support/cas-server-support-redis-ticket-registry/src/main/java/org/apereo/cas/ticket/registry/key/RedisKeyGeneratorFactory.java
+++ support/cas-server-support-redis-ticket-registry/src/main/java/org/apereo/cas/ticket/registry/key/RedisKeyGeneratorFactory.java
@@ -16,19 +16,42 @@ import java.util.stream.Stream;
 public class RedisKeyGeneratorFactory {
     private final Map<String, RedisKeyGenerator> keyGeneratorMap = new HashMap<>();
 
+    /**
+     * Gets redis key generator.
+     *
+     * @param key the key
+     * @return the redis key generator
+     */
     public Optional<RedisKeyGenerator> getRedisKeyGenerator(final String key) {
         return Optional.ofNullable(keyGeneratorMap.get(key));
     }
 
+    /**
+     * Register redis key generator.
+     *
+     * @param key       the key
+     * @param generator the generator
+     * @return the redis key generator factory
+     */
     public RedisKeyGeneratorFactory registerRedisKeyGenerator(final String key, final RedisKeyGenerator generator) {
         keyGeneratorMap.put(key, generator);
         return this;
     }
 
+    /**
+     * Gets redis key generators.
+     *
+     * @return the redis key generators
+     */
     public List<RedisKeyGenerator> getRedisKeyGenerators() {
         return new ArrayList<>(keyGeneratorMap.values());
     }
 
+    /**
+     * Register redis key generators.
+     *
+     * @param keyGenerators the key generators
+     */
     public void registerRedisKeyGenerators(final RedisKeyGenerator... keyGenerators) {
         Stream.of(keyGenerators).forEach(gen -> registerRedisKeyGenerator(gen.getType(), gen));
     }
