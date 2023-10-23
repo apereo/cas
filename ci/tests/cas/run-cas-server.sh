7d183ac67cb92d14d15f4ef635cde726d7eb177c
Misagh Moayyed <mm1844@gmail.com>
2023-10-23 19:51:36 +0400

    fix tests

--- ci/tests/cas/run-cas-server.sh
+++ ci/tests/cas/run-cas-server.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-if [[ -z "${CAS_KEYSTORE}" ]] ; then
-  keystore="$PWD"/ci/tests/cas/thekeystore.ignore
-  echo -e "Generating keystore for CAS Server at ${keystore}"
-  dname="${dname:-CN=localhost,OU=Example,OU=Org,C=US}"
-  subjectAltName="${subjectAltName:-dns:example.org,dns:localhost,ip:127.0.0.1}"
-  [ -f "${keystore}" ] && rm "${keystore}"
-  keytool -genkey -noprompt -alias cas -keyalg RSA \
-    -keypass changeit -storepass changeit \
-    -keystore "${keystore}" -dname "${dname}"
-  [ -f "${keystore}" ] && echo "Created ${keystore}"
-  export CAS_KEYSTORE="${keystore}"
-else
+if [[ -f "${CAS_KEYSTORE}" ]] ; then
   echo -e "Found existing CAS keystore at ${CAS_KEYSTORE}"
+else
+  keystore="$PWD"/ci/tests/cas/thekeystore.ignore
+    echo -e "Generating keystore for CAS Server at ${keystore}"
+    dname="${dname:-CN=localhost,OU=Example,OU=Org,C=US}"
+    subjectAltName="${subjectAltName:-dns:example.org,dns:localhost,ip:127.0.0.1}"
+    [ -f "${keystore}" ] && rm "${keystore}"
+    keytool -genkey -noprompt -alias cas -keyalg RSA \
+      -keypass changeit -storepass changeit \
+      -keystore "${keystore}" -dname "${dname}"
+    [ -f "${keystore}" ] && echo "Created ${keystore}"
+    export CAS_KEYSTORE="${keystore}"
 fi
 
 # We need to rename the TGC cookie so it does not conflict with
@@ -78,7 +78,7 @@ docker network create casserver-network
 docker run --rm -d \
   --mount type=bind,source="${CAS_KEYSTORE}",target=/etc/cas/thekeystore \
   -e SPRING_APPLICATION_JSON="${properties}" --network casserver-network \
-  -p 8797:8797 -p 8444:8443 --name casserver apereo/cas:6.6.13
+  -p 8797:8797 -p 8444:8443 --name casserver apereo/cas:6.6.12
 docker logs -f casserver &
 echo -e "Waiting for CAS..."
 until curl -k -L --output /dev/null --silent --fail https://localhost:8444/cas/login; do
