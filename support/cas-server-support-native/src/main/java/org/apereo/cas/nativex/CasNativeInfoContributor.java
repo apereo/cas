727d247710d78a01b8ecbe35402df5bddfd06384
Misagh Moayyed <mm1844@gmail.com>
2023-10-29 16:33:13 +0400

    test cleanup

--- support/cas-server-support-native/src/main/java/org/apereo/cas/nativex/CasNativeInfoContributor.java
+++ support/cas-server-support-native/src/main/java/org/apereo/cas/nativex/CasNativeInfoContributor.java
@@ -15,10 +15,12 @@ import java.util.HashMap;
  * @since 7.0.0
  */
 public class CasNativeInfoContributor implements InfoContributor {
+    private static final String GRAALVM_VERSION = Version.getCurrent().toString();
+
     @Override
     public void contribute(final Info.Builder builder) {
         val nativeImageInfo = new HashMap<>();
-        nativeImageInfo.put("graalVMVersion", Version.getCurrent().toString());
+        nativeImageInfo.put("graalVMVersion", GRAALVM_VERSION);
         nativeImageInfo.put("executable", ImageInfo.isExecutable());
         builder.withDetail("nativeImage", nativeImageInfo);
     }
