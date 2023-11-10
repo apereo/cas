eb1510e18dfb1c1291810aa80aa2690bb92e8141
Misagh Moayyed <mm1844@gmail.com>
2023-11-10 15:09:46 +0400

    minor polish

--- support/cas-server-support-wsfederation/src/test/java/org/apereo/cas/support/wsfederation/attributes/GroovyWsFederationAttributeMutatorTests.java
+++ support/cas-server-support-wsfederation/src/test/java/org/apereo/cas/support/wsfederation/attributes/GroovyWsFederationAttributeMutatorTests.java
@@ -1,6 +1,7 @@
 package org.apereo.cas.support.wsfederation.attributes;
 
 import org.apereo.cas.authentication.CoreAuthenticationTestUtils;
+import org.apereo.cas.util.scripting.WatchableGroovyScriptResource;
 
 import lombok.val;
 import org.junit.jupiter.api.Tag;
@@ -19,7 +20,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class GroovyWsFederationAttributeMutatorTests {
     @Test
     void verifyAction() throws Throwable {
-        val attributeMutator = new GroovyWsFederationAttributeMutator(new ClassPathResource("GroovyWsFedMutator.groovy"));
+        val location = new ClassPathResource("GroovyWsFedMutator.groovy");
+        val attributeMutator = new GroovyWsFederationAttributeMutator(new WatchableGroovyScriptResource(location));
         val results = attributeMutator.modifyAttributes(CoreAuthenticationTestUtils.getAttributes());
         assertEquals(1, results.size());
         assertTrue(results.containsKey("mail"));
