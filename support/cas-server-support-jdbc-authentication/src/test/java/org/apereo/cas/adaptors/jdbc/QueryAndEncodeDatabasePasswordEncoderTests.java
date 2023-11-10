b65ccc3e7bafdf36feb46455bb587ca768540406
Misagh Moayyed <mm1844@gmail.com>
2023-11-10 10:27:20 +0400

    extract query-and-encode db password encoder out

---
+++ support/cas-server-support-jdbc-authentication/src/test/java/org/apereo/cas/adaptors/jdbc/QueryAndEncodeDatabasePasswordEncoderTests.java
@@ -0,0 +1,54 @@
+package org.apereo.cas.adaptors.jdbc;
+
+import org.apereo.cas.configuration.model.support.jdbc.authn.QueryEncodeJdbcAuthenticationProperties;
+import lombok.val;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This is {@link QueryAndEncodeDatabasePasswordEncoderTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 7.0.0
+ */
+@Tag("JDBCAuthentication")
+public class QueryAndEncodeDatabasePasswordEncoderTests {
+    private static final String STATIC_SALT = "oU92&p4b&NIWjAbC";
+    private static final String DYNAMIC_SALT = "l!Ue%wj6TPMNf*lI";
+
+    @Test
+    void verifyStaticSaltOnly() throws Exception {
+        val properties = new QueryEncodeJdbcAuthenticationProperties()
+            .setAlgorithmName("SHA256")
+            .setStaticSalt(STATIC_SALT)
+            .setNumberOfIterations(1);
+        val encoder = new QueryAndEncodeDatabasePasswordEncoder(properties);
+        val encoded = encoder.encode("p@$$w0rd", Map.of());
+        assertEquals("4c32a1b1d6fa058a3e145c474bebc7dc30a34ad40df18202006e30d9313b3918", encoded);
+    }
+
+    @Test
+    void verifyStaticAndDynamicSalt() throws Exception {
+        val properties = new QueryEncodeJdbcAuthenticationProperties()
+            .setAlgorithmName("SHA512")
+            .setStaticSalt(STATIC_SALT)
+            .setNumberOfIterations(100)
+            .setSaltFieldName("dynamicSalt");
+         val encoder = new QueryAndEncodeDatabasePasswordEncoder(properties);
+         val encoded = encoder.encode("p@$$w0rd", Map.of("dynamicSalt", DYNAMIC_SALT));
+         assertEquals("51072005fd3ecfc1edc22fc1eff611487defde788301991a667119ae32cecc7da1af6afed420b61318aa11151b0f6ec357c1869dc75acccdb5170273fad4e957", encoded);
+    }
+
+    @Test
+    void verifyNoStaticSaltWithDynamicIterations() throws Exception {
+        val properties = new QueryEncodeJdbcAuthenticationProperties()
+            .setAlgorithmName("SHA256")
+            .setNumberOfIterationsFieldName("iterations")
+            .setSaltFieldName("dynamicSalt");
+        val encoder = new QueryAndEncodeDatabasePasswordEncoder(properties);
+        val encoded = encoder.encode("p@$$w0rd", Map.of("dynamicSalt", DYNAMIC_SALT, "iterations", 1000));
+        assertEquals("d14889d4eec75142f69f3620a629e6efa79ac92d11ef9214ee0dd4ddf7c0108c", encoded);
+    }
+}
--- support/cas-server-support-saml-idp/src/test/java/org/apereo/cas/support/saml/web/idp/profile/slo/SamlIdPSingleLogoutRedirectionStrategyRedirectBindingTests.java
+++ support/cas-server-support-saml-idp/src/test/java/org/apereo/cas/support/saml/web/idp/profile/slo/SamlIdPSingleLogoutRedirectionStrategyRedirectBindingTests.java
@@ -64,7 +64,6 @@ class SamlIdPSingleLogoutRedirectionStrategyRedirectBindingTests extends BaseSam
         val response = new MockHttpServletResponse();
         val logoutResponse = samlIdPSingleLogoutRedirectionStrategy.handle(request, response);
         assertNotNull(WebUtils.getLogoutRedirectUrl(request, String.class));
-        assertTrue(logoutResponse.getLogoutPostUrl().isEmpty());
-        assertTrue(logoutResponse.getLogoutPostData().isEmpty());
+        assertNull(logoutResponse);
     }
 }
