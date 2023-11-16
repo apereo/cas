f8c46250a5af94005f8a7c94da69be88a33274a4
Misagh Moayyed <mm1844@gmail.com>
2023-11-16 19:18:53 +0400

    display message before unauthorized direct URLs are used for access strategy

--- ci/tests/puppeteer/scenarios/service-access-strategy-unauthz-url/script.js
+++ ci/tests/puppeteer/scenarios/service-access-strategy-unauthz-url/script.js
@@ -3,15 +3,15 @@ const cas = require('../../cas.js');
 const assert = require("assert");
 
 (async () => {
-    const service = "https://apereo.github.io";
-    
     const browser = await puppeteer.launch(cas.browserOptions());
     const page = await cas.newPage(browser);
-    await cas.goto(page, `https://localhost:8443/cas/login?service=${service}`);
+    await cas.goto(page, `https://localhost:8443/cas/login?service=https://apereo.github.io`);
     await cas.loginWith(page);
     await page.waitForTimeout(1000);
     await cas.screenshot(page);
     await cas.logPage(page);
+    await cas.assertInnerText(page, "#content h2", "Application Not Authorized to Use CAS");
+    await page.waitForTimeout(4000);
     let url = await page.url();
     assert(url === "https://localhost:9859/anything/info");
     await browser.close();
