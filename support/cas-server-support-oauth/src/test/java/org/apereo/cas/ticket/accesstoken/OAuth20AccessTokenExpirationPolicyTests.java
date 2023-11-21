8c7bfa36527ba1ce28a69eb2530c5ced07aa23e8
Misagh Moayyed <mm1844@gmail.com>
2023-11-21 19:23:17 +0400

    fix tests

--- support/cas-server-support-oauth/src/test/java/org/apereo/cas/ticket/accesstoken/OAuth20AccessTokenExpirationPolicyTests.java
+++ support/cas-server-support-oauth/src/test/java/org/apereo/cas/ticket/accesstoken/OAuth20AccessTokenExpirationPolicyTests.java
@@ -20,7 +20,7 @@ import static org.mockito.Mockito.*;
  * @author Misagh Moayyed
  * @since 5.0.0
  */
-@TestPropertySource(properties = "cas.logout.remove-descendant-tickets=true")
+@TestPropertySource(properties = "cas.ticket.track-descendant-tickets=true")
 @Tag("OAuthToken")
 class OAuth20AccessTokenExpirationPolicyTests extends BaseOAuth20ExpirationPolicyTests {
     private static final File JSON_FILE = new File(FileUtils.getTempDirectoryPath(), "oAuthTokenExpirationPolicy.json");
