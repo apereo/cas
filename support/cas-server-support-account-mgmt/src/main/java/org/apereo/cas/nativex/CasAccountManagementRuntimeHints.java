9b35b84a9475783ae0e5194075d80055be2b5206
Misagh Moayyed <mm1844@gmail.com>
2023-11-25 00:34:10 +0400

    minor native image hints/fixes

---
+++ support/cas-server-support-account-mgmt/src/main/java/org/apereo/cas/nativex/CasAccountManagementRuntimeHints.java
@@ -0,0 +1,33 @@
+package org.apereo.cas.nativex;
+
+import org.apereo.cas.acct.provision.AccountRegistrationProvisionerConfigurer;
+import org.apereo.cas.util.nativex.CasRuntimeHintsRegistrar;
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * This is {@link CasAccountManagementRuntimeHints}.
+ *
+ * @author Misagh Moayyed
+ * @since 7.0.0
+ */
+public class CasAccountManagementRuntimeHints implements CasRuntimeHintsRegistrar {
+    @Override
+    public void registerHints(final RuntimeHints hints, final ClassLoader classLoader) {
+        registerReflectionHints(hints, List.of(
+            AccountRegistrationProvisionerConfigurer.class
+        ));
+    }
+
+    private static void registerReflectionHints(final RuntimeHints hints, final Collection entries) {
+        entries.forEach(el -> hints.reflection().registerType((Class) el,
+            MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
+            MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS,
+            MemberCategory.INVOKE_DECLARED_METHODS,
+            MemberCategory.INVOKE_PUBLIC_METHODS,
+            MemberCategory.DECLARED_FIELDS,
+            MemberCategory.PUBLIC_FIELDS));
+    }
+}
