aea1fe63556a3d646da57cae4f5807048dcd50a8
Misagh Moayyed <mm1844@gmail.com>
2023-11-27 13:05:18 +0400

    minor polish

--- api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/support/acme/AcmeProperties.java
+++ api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/support/acme/AcmeProperties.java
@@ -1,5 +1,6 @@
 package org.apereo.cas.configuration.model.support.acme;
 
+import org.apereo.cas.configuration.features.CasFeatureModule;
 import org.apereo.cas.configuration.model.SpringResourceProperties;
 import org.apereo.cas.configuration.support.DurationCapable;
 import org.apereo.cas.configuration.support.RequiredProperty;
@@ -27,7 +28,7 @@ import java.util.List;
 @Setter
 @Accessors(chain = true)
 @JsonFilter("AcmeProperties")
-public class AcmeProperties implements Serializable {
+public class AcmeProperties implements Serializable, CasFeatureModule {
     @Serial
     private static final long serialVersionUID = -561637865919944706L;
 
