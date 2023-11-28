a47856f6aebdba23c25c316d9c66a6ca719bf7de
Misagh Moayyed <mm1844@gmail.com>
2023-11-28 19:42:44 +0400

    minor polish

--- api/cas-server-core-api-authentication/src/main/java/org/apereo/cas/authentication/PrincipalElectionStrategyConflictResolver.java
+++ api/cas-server-core-api-authentication/src/main/java/org/apereo/cas/authentication/PrincipalElectionStrategyConflictResolver.java
@@ -1,9 +1,7 @@
 package org.apereo.cas.authentication;
 
 import org.apereo.cas.authentication.principal.Principal;
-
 import java.util.List;
-import java.util.Map;
 
 /**
  * This is {@link PrincipalElectionStrategyConflictResolver}
