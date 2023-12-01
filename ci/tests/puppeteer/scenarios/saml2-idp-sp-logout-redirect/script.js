cfbaa4d61a084c182a7c8ef0fb2cc7cf9222f946
Misagh Moayyed <mm1844@gmail.com>
2023-12-01 11:12:41 +0400

    fix tests

--- ci/tests/puppeteer/scenarios/saml2-idp-sp-logout-redirect/script.js
+++ ci/tests/puppeteer/scenarios/saml2-idp-sp-logout-redirect/script.js
@@ -9,10 +9,9 @@ const path = require("path");
     await cas.gotoLogin(page);
     await cas.loginWith(page);
     await page.close();
-
-    page = await cas.newPage(browser);
-    await page.setRequestInterception(true);
-    await page.on('request', request => {
+    let page2 = await cas.newPage(browser);
+    await page2.setRequestInterception(true);
+    await page2.on('request', request => {
         let data = {
             'method': 'POST',
             'postData': 'SAMLRequest=PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz48c2FtbDJwOkxvZ291dFJlcXVlc3QgeG1sbnM6c2FtbDJwPSJ1cm46b2FzaXM6bmFtZXM6dGM6U0FNTDoyLjA6cHJvdG9jb2wiIERlc3RpbmF0aW9uPSJodHRwczovL2xvY2FsaG9zdDo4NDQzL2Nhcy9pZHAvcHJvZmlsZS9TQU1MMi9QT1NUL1NMTyIgSUQ9IjEyMzQiIElzc3VlSW5zdGFudD0iMjAyMS0wNi0wMVQxMDowNDoyMi43NzJaIiBWZXJzaW9uPSIyLjAiPjxzYW1sMjpJc3N1ZXIgeG1sbnM6c2FtbDI9InVybjpvYXNpczpuYW1lczp0YzpTQU1MOjIuMDphc3NlcnRpb24iPmh0dHBzOi8vc2FtbHRlc3QuaWQvc2FtbC9zcDwvc2FtbDI6SXNzdWVyPjxzYW1sMjpOYW1lSUQgeG1sbnM6c2FtbDI9InVybjpvYXNpczpuYW1lczp0YzpTQU1MOjIuMDphc3NlcnRpb24iIEZvcm1hdD0idXJuOm9hc2lzOm5hbWVzOnRjOlNBTUw6Mi4wOm5hbWVpZC1mb3JtYXQ6dHJhbnNpZW50Ij5mYWtlbmFtZWlkPC9zYW1sMjpOYW1lSUQ%2BPHNhbWwycDpTZXNzaW9uSW5kZXg%2BZmFrZXNlc3Npb25pbmRleDwvc2FtbDJwOlNlc3Npb25JbmRleD48L3NhbWwycDpMb2dvdXRSZXF1ZXN0Pg%3D%3D',
@@ -23,8 +22,8 @@ const path = require("path");
         };
         request.continue(data);
     });
-    await cas.goto(page, 'https://localhost:8443/cas/idp/profile/SAML2/POST/SLO');
-    assert(await page.url().startsWith('https://samltest.id/Shibboleth.sso/SLO/Redirect?SAMLResponse='));
+    await cas.goto(page2, 'https://localhost:8443/cas/idp/profile/SAML2/POST/SLO');
+    assert(await page2.url().startsWith('https://samltest.id/Shibboleth.sso/SLO/Redirect?SAMLResponse='));
     await cas.removeDirectoryOrFile(path.join(__dirname, '/saml-md'));
     await browser.close();
 })();
