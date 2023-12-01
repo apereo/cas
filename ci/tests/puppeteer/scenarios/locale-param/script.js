832152429312c54810c121038389ef7810e560cc
Olivier Bado-Faustin <bado@unice.fr>
2023-12-01 18:23:46 +0100

    A11y html (#5884)
    * Add `th:lang="${#locale}"` to every `<html root tag, so screen readers will use proper language
    
    * Remove useless "role=main" on every `main` html tags (redundant)
    
    * Puppeteer Tests :
    * Add `exports.attrValue` function in puppeteer utils to get specified attribute value of a single element
    * Add  `exports.assertAttribute` function in puppeteer utils to test an attribute value of a single element
    * Add puppeter Assertion to test value of "lang" attribute when locale is specified
    
    * * Remove attrValue & assertAttribute functions from puppeteer utils
    * Remove JsDoc function comments from cas.js
    
    Puppeteer scenari modified and successfully passed:
    [x] locale-param
    [x] locale-selection
    [x] simple-mfa-login
    
    * * Remove every `th:lang="${#locale}"` occurences in child html templates, and put them instead in base layout template
    * Remove remaining jdoc comments in cas.js
    
    ---------
    
    Co-authored-by: Misagh Moayyed <mm1844@gmail.com>

--- ci/tests/puppeteer/scenarios/locale-param/script.js
+++ ci/tests/puppeteer/scenarios/locale-param/script.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const assert = require('assert');
 const cas = require('../../cas.js');
 
 (async () => {
@@ -7,15 +8,24 @@ const cas = require('../../cas.js');
 
     await cas.goto(page, "https://localhost:8443/cas/login?locale=de");
     await cas.assertInnerText(page, "#content #fm1 button[name=submitBtn]", "ANMELDEN");
+    // Assert that HTML root node has attribute `lang="de"`
+    let node = await page.$('html');
+    assert("de" === await node.evaluate(el => el.getAttribute("lang")));
 
     await cas.gotoLogin(page, "https://apereo.github.io");
     await cas.assertInnerText(page, "#content #fm1 button[name=submitBtn]", "SE CONNECTER");
+    // Assert that HTML root node has attribute `lang="fr"`
+    node = await page.$('html');
+    assert("fr" === await node.evaluate(el => el.getAttribute("lang")));
 
     await cas.gotoLogin(page);
     await cas.assertInnerText(page, "#content #fm1 button[name=submitBtn]", "SE CONNECTER");
 
     await cas.goto(page, "https://localhost:8443/cas/login?locale=es&service=https://apereo.github.io");
     await cas.assertInnerText(page, "#content #fm1 button[name=submitBtn]", "INICIAR SESIÓN");
-    
+    // Assert that HTML root node has attribute `lang="es"`
+    node = await page.$('html');
+    assert("es" === await node.evaluate(el => el.getAttribute("lang")));
+
     await browser.close();
 })();
--- ci/tests/puppeteer/scenarios/locale-selection/script.js
+++ ci/tests/puppeteer/scenarios/locale-selection/script.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const assert = require('assert');
 const cas = require('../../cas.js');
 
 (async () => {
@@ -9,6 +10,13 @@ const cas = require('../../cas.js');
     });
     await cas.gotoLogin(page);
     await page.waitForTimeout(1000);
+
+    // Assert that submitBtn is displayed in german language.
     await cas.assertInnerText(page, "#content #fm1 button[name=submitBtn]", "ANMELDEN");
+
+    // Assert that HTML root node has attribute `lang="de"`
+    const node = await page.$('html');
+    assert("de" === await node.evaluate(el => el.getAttribute("lang")));
+
     await browser.close();
 })();
