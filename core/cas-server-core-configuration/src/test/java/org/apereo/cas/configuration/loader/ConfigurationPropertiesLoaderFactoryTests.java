dda072be3b25d6348f448477ceb78bf1b2b2605e
Misagh Moayyed <mm1844@gmail.com>
2023-12-03 12:57:48 +0400

    rework groovy scripts for static compilation

---
+++ core/cas-server-core-configuration/src/test/java/org/apereo/cas/configuration/loader/ConfigurationPropertiesLoaderFactoryTests.java
@@ -0,0 +1,42 @@
+package org.apereo.cas.configuration.loader;
+
+import org.apereo.cas.config.CasCoreConfigurationWatchConfiguration;
+import org.apereo.cas.config.CasCoreEnvironmentBootstrapConfiguration;
+import org.apereo.cas.config.CasCoreStandaloneBootstrapConfiguration;
+import lombok.val;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.test.context.ActiveProfiles;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This is {@link ConfigurationPropertiesLoaderFactoryTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 7.0.0
+ */
+@SpringBootTest(classes = {
+    CasCoreEnvironmentBootstrapConfiguration.class,
+    CasCoreConfigurationWatchConfiguration.class,
+    CasCoreStandaloneBootstrapConfiguration.class
+})
+@ActiveProfiles("dev")
+@Tag("Groovy")
+public class ConfigurationPropertiesLoaderFactoryTests {
+    @Autowired
+    @Qualifier(ConfigurationPropertiesLoaderFactory.BEAN_NAME)
+    private ConfigurationPropertiesLoaderFactory configurationPropertiesLoaderFactory;
+
+    @Test
+    void verifyOperation() throws Exception {
+        val loader = configurationPropertiesLoaderFactory.getLoader(
+            new ClassPathResource("directory/cas.groovy"), "groovyConfig");
+        val source = loader.load();
+        assertEquals("test::dev", source.getProperty("cas.authn.accept.users"));
+        assertEquals("Static", source.getProperty("cas.authn.accept.name"));
+    }
+}
