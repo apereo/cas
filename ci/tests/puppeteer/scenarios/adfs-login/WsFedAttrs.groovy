804df1be9ef6b05db27c4745ba9f419cbf498b89
Misagh Moayyed <mm1844@gmail.com>
2023-12-13 18:08:53 +0400

    more native image runtime hints and fixes

--- ci/tests/puppeteer/scenarios/adfs-login/WsFedAttrs.groovy
+++ ci/tests/puppeteer/scenarios/adfs-login/WsFedAttrs.groovy
@@ -3,7 +3,7 @@ Map run(final Object... args) {
     def logger = (Logger) args[1]
     logger.info("Mutating attributes {}", attributes)
     def result = new LinkedHashMap(attributes)
-    result.put("email", ["casuser@example.org"])
-    result.put("upn", ["casuser@apereo.org"])
+    result.put("email", List.of("casuser@example.org"))
+    result.put("upn", List.of("casuser@apereo.org"))
     return result
 }
