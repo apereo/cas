c03ccffe0c46dfc3cac3efdf7e8796d537f9f9fb
Misagh Moayyed <mm1844@gmail.com>
2023-12-18 13:38:22 +0400

    fix tests/docs references

--- core/cas-server-core-authentication-mfa/src/main/java/org/apereo/cas/nativex/CasCoreMultifactorAuthenticationRuntimeHints.java
+++ core/cas-server-core-authentication-mfa/src/main/java/org/apereo/cas/nativex/CasCoreMultifactorAuthenticationRuntimeHints.java
@@ -1,6 +1,7 @@
 package org.apereo.cas.nativex;
 
 import org.apereo.cas.authentication.ChainingMultifactorAuthenticationProvider;
+import org.apereo.cas.authentication.MultifactorAuthenticationTrigger;
 import org.apereo.cas.util.nativex.CasRuntimeHintsRegistrar;
 
 import org.springframework.aot.hint.MemberCategory;
@@ -17,7 +18,7 @@ import java.util.List;
 public class CasCoreMultifactorAuthenticationRuntimeHints implements CasRuntimeHintsRegistrar {
     @Override
     public void registerHints(final RuntimeHints hints, final ClassLoader classLoader) {
-        registerProxyHints(hints, ChainingMultifactorAuthenticationProvider.class);
+        registerProxyHints(hints, MultifactorAuthenticationTrigger.class, ChainingMultifactorAuthenticationProvider.class);
         registerReflectionHints(hints, List.of(ChainingMultifactorAuthenticationProvider.class));
     }
 
