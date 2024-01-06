cdabb57b63f754a470c75f7df5fa499d9b0d72c8
Misagh Moayyed <mm1844@gmail.com>
2024-01-06 20:07:24 +0400

    support attribute release activation criteria

--- api/cas-server-core-api-services/src/main/java/org/apereo/cas/services/RegisteredServiceAttributeReleaseActivationCriteria.java
+++ api/cas-server-core-api-services/src/main/java/org/apereo/cas/services/RegisteredServiceAttributeReleaseActivationCriteria.java
@@ -1,6 +1,7 @@
 package org.apereo.cas.services;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import org.springframework.core.Ordered;
 import java.io.Serializable;
 
 /**
@@ -11,21 +12,26 @@ import java.io.Serializable;
  */
 @FunctionalInterface
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS)
-public interface RegisteredServiceAttributeReleaseCondition extends Serializable {
+public interface RegisteredServiceAttributeReleaseActivationCriteria extends Serializable, Ordered {
 
     /**
     * Is authorized to release authentication attributes.
     *
     * @return true/false
     */
-    boolean isSatisfied(RegisteredServiceAttributeReleasePolicyContext context);
+    boolean shouldActivate(RegisteredServiceAttributeReleasePolicyContext context);
 
+    @Override
+    default int getOrder() {
+        return 0;
+    }
+    
     /**
      * Always allow the condition to pass.
      *
      * @return the registered service attribute release condition
      */
-    static RegisteredServiceAttributeReleaseCondition always() {
+    static RegisteredServiceAttributeReleaseActivationCriteria always() {
         return context -> true;
     }
 }
