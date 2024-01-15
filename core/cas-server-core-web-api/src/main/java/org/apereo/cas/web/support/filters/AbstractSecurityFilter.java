da25f68115990fa6760cce08ff141501f9bc2f50
Misagh Moayyed <mm1844@gmail.com>
2024-01-15 16:20:00 +0400

    protect browser storage data from bad/stale data

--- core/cas-server-core-web-api/src/main/java/org/apereo/cas/web/support/filters/AbstractSecurityFilter.java
+++ core/cas-server-core-web-api/src/main/java/org/apereo/cas/web/support/filters/AbstractSecurityFilter.java
@@ -5,7 +5,11 @@ import org.apereo.cas.util.LoggingUtils;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.http.HttpStatus;
+import org.springframework.webflow.conversation.NoSuchConversationException;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 /**
@@ -19,15 +23,35 @@ import jakarta.servlet.http.HttpServletResponse;
 @Getter
 public abstract class AbstractSecurityFilter {
     protected static void throwException(final Throwable exception) {
-        throwException(exception, null);
+        throwException(exception, null, null);
     }
 
-    protected static void throwException(final Throwable exception, final HttpServletResponse servletResponse) {
+    protected static void throwException(final Throwable exception, final HttpServletResponse servletResponse,
+                                         final HttpServletRequest servletRequest) {
         LoggingUtils.error(LOGGER, exception);
-        if (exception instanceof UnauthorizedServiceException) {
-            servletResponse.setStatus(HttpStatus.FORBIDDEN.value());
+        if (servletResponse != null) {
+            if (exception instanceof UnauthorizedServiceException) {
+                servletResponse.setStatus(HttpStatus.FORBIDDEN.value());
+            }
+            val nsce = (RuntimeException) getRootCause(exception, NoSuchConversationException.class);
+            if (nsce != null) {
+                servletRequest.setAttribute(RequestDispatcher.ERROR_EXCEPTION, nsce);
+                throw nsce;
+            }
         }
         throw exception instanceof final RuntimeException re ? re : new RuntimeException(exception);
     }
 
+    private static Throwable getRootCause(final Throwable exception, final Class<?> cause) {
+        var rootCause = exception.getCause();
+        if (rootCause != null) {
+            do {
+                if (rootCause.getClass().equals(cause)) {
+                    return rootCause;
+                }
+                rootCause = rootCause.getCause();
+            } while (rootCause != null);
+        }
+        return null;
+    }
 }
