3d3a1a1f7127b35f625bfcf95f0820e4c373f4ad
Jérôme LELEU <leleuj@gmail.com>
2024-01-15 15:29:29 +0100

    Fix passwordless + reset password

--- support/cas-server-support-passwordless-webflow/src/main/java/org/apereo/cas/web/flow/PrepareForPasswordlessAuthenticationAction.java
+++ support/cas-server-support-passwordless-webflow/src/main/java/org/apereo/cas/web/flow/PrepareForPasswordlessAuthenticationAction.java
@@ -1,7 +1,10 @@
 package org.apereo.cas.web.flow;
 
 import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.pm.PasswordManagementService;
 
+import lombok.val;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.webflow.action.EventFactorySupport;
 import org.springframework.webflow.execution.Event;
 import org.springframework.webflow.execution.RequestContext;
@@ -20,6 +23,11 @@ public class PrepareForPasswordlessAuthenticationAction extends BasePasswordless
 
     @Override
     protected Event doExecuteInternal(final RequestContext requestContext) {
+        val pwdresetTicket = requestContext.getRequestParameters().get(PasswordManagementService.PARAMETER_PASSWORD_RESET_TOKEN);
+        if (StringUtils.isNotBlank(pwdresetTicket)) {
+            return success();
+        }
+
         PasswordlessWebflowUtils.putPasswordlessAuthenticationEnabled(requestContext, Boolean.TRUE);
         if (!PasswordlessWebflowUtils.hasPasswordlessAuthenticationAccount(requestContext) && isLoginFlowActive(requestContext)) {
             return new EventFactorySupport().event(this, CasWebflowConstants.TRANSITION_ID_PASSWORDLESS_GET_USERID);
