79c6abf265d9b75713ccd30f5ac1c777eb74915e
Misagh Moayyed <mm1844@gmail.com>
2024-01-20 15:32:13 +0400

    allow option to reverse match for principal attribute mfa triggers

--- core/cas-server-core-authentication-mfa-api/src/test/java/org/apereo/cas/authentication/mfa/trigger/BaseMultifactorAuthenticationTriggerTests.java
+++ core/cas-server-core-authentication-mfa-api/src/test/java/org/apereo/cas/authentication/mfa/trigger/BaseMultifactorAuthenticationTriggerTests.java
@@ -7,19 +7,18 @@ import org.apereo.cas.authentication.principal.Principal;
 import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.services.RegisteredService;
 import org.apereo.cas.util.http.HttpRequestUtils;
-import org.apereo.cas.util.spring.ApplicationContextProvider;
 import lombok.val;
 import org.apereo.inspektr.common.web.ClientInfo;
 import org.apereo.inspektr.common.web.ClientInfoHolder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.StaticApplicationContext;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import java.util.List;
@@ -51,14 +50,14 @@ public abstract class BaseMultifactorAuthenticationTriggerTests {
 
     protected TestMultifactorAuthenticationProvider multifactorAuthenticationProvider;
 
+    @Autowired
     protected ConfigurableApplicationContext applicationContext;
+
+    @Autowired
+    protected CasConfigurationProperties casProperties;
 
     @BeforeEach
     public void setup(final TestInfo info) {
-        this.applicationContext = new StaticApplicationContext();
-        applicationContext.refresh();
-        ApplicationContextProvider.holdApplicationContext(applicationContext);
-
         if (!info.getTags().contains("DisableProviderRegistration")) {
             this.multifactorAuthenticationProvider = TestMultifactorAuthenticationProvider.registerProviderIntoApplicationContext(applicationContext);
         }
