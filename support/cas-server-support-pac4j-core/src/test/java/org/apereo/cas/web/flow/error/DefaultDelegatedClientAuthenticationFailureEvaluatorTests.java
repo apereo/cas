eef2c7db7049fe82b4f17aeeb5ab38330b76d055
Jérôme LELEU <leleuj@gmail.com>
2024-01-24 14:38:59 +0100

    Fix authn delegation + throttling

---
+++ support/cas-server-support-pac4j-core/src/test/java/org/apereo/cas/web/flow/error/DefaultDelegatedClientAuthenticationFailureEvaluatorTests.java
@@ -0,0 +1,29 @@
+package org.apereo.cas.web.flow.error;
+
+import org.apereo.cas.web.flow.DelegatedClientAuthenticationConfigurationContext;
+
+import lombok.val;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * This is {@link DefaultDelegatedClientAuthenticationFailureEvaluatorTests}.
+ *
+ * @author Jerome LELEU
+ * @since 7.1.0
+ */
+@Tag("Delegation")
+public class DefaultDelegatedClientAuthenticationFailureEvaluatorTests {
+
+    @Test
+    public void verifyThrottling() {
+        val evaluator = new DefaultDelegatedClientAuthenticationFailureEvaluator(
+                mock(DelegatedClientAuthenticationConfigurationContext.class));
+        val optModelAndView = evaluator.evaluate(new MockHttpServletRequest(), 423);
+        assertEquals("error/423", optModelAndView.get().getViewName());
+    }
+}
