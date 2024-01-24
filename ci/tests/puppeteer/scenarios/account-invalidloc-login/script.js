82aeea9d2372e5ad86ebbbe729ad5ee63d4acee1
Olivier Bado-Faustin <bado@unice.fr>
2024-01-24 14:15:06 +0100

    I18n fixes
    Several I18n fixes, mostly adding missing french strings
    
    * Correct small english typos ("This is **the** your", "to login" -> "to log in",...)
    * Add about 200 missing french i18n strings
    * Add missing close tag (`/p`)
    * Remove unused strings (screen.welcome.welcome, cas.attrreleaseview, cas.mfa.azure.pagetitle...)

--- ci/tests/puppeteer/scenarios/account-invalidloc-login/script.js
+++ ci/tests/puppeteer/scenarios/account-invalidloc-login/script.js
@@ -6,6 +6,6 @@ const cas = require("../../cas.js");
     const page = await cas.newPage(browser);
     await cas.gotoLogin(page);
     await cas.loginWith(page, "invalidlocation", "invalidlocation");
-    await cas.assertInnerText(page, "#content h2", "You cannot login from this workstation.");
+    await cas.assertInnerText(page, "#content h2", "You cannot log in from this workstation.");
     await browser.close();
 })();
