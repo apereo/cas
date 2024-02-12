461a1c232eaeff15885db85a4aadfb882a4d1088
Misagh Moayyed <mm1844@gmail.com>
2024-02-12 23:48:47 +0400

    allow mdc logging parameters and headers to be customized

--- core/cas-server-core-logging/src/test/java/org/apereo/cas/logging/web/ThreadContextMDCServletFilterTests.java
+++ core/cas-server-core-logging/src/test/java/org/apereo/cas/logging/web/ThreadContextMDCServletFilterTests.java
@@ -1,21 +1,27 @@
 package org.apereo.cas.logging.web;
 
 import org.apereo.cas.authentication.CoreAuthenticationTestUtils;
+import org.apereo.cas.config.CasCoreCookieAutoConfiguration;
+import org.apereo.cas.config.CasCoreLoggingAutoConfiguration;
+import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.ticket.registry.TicketRegistrySupport;
-import org.apereo.cas.util.spring.DirectObjectProvider;
 import org.apereo.cas.web.cookie.CasCookieBuilder;
-
 import lombok.val;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.mock.web.MockFilterChain;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
-
 import jakarta.servlet.FilterConfig;
 import jakarta.servlet.http.HttpServletRequest;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -26,9 +32,22 @@ import static org.mockito.Mockito.*;
  * @since 5.3.0
  */
 @Tag("Web")
+@SpringBootTest(classes = {
+    RefreshAutoConfiguration.class,
+    CasCoreCookieAutoConfiguration.class,
+    CasCoreLoggingAutoConfiguration.class
+})
+@EnableConfigurationProperties(CasConfigurationProperties.class)
 class ThreadContextMDCServletFilterTests {
+
+    @Autowired
+    @Qualifier("threadContextMDCServletFilter")
+    private FilterRegistrationBean<ThreadContextMDCServletFilter> threadContextMDCServletFilter;
+
     @Test
     void verifyFilter() throws Throwable {
+        assertNotNull(threadContextMDCServletFilter);
+
         val request = new MockHttpServletRequest();
         request.setRequestURI("/cas/login");
         request.setRemoteAddr("1.2.3.4");
@@ -41,8 +60,11 @@ class ThreadContextMDCServletFilterTests {
         request.setQueryString("queryString");
         request.setMethod("method");
         request.setParameter("p1", "v1");
+        request.setParameter("password", "helloworld");
+        request.setParameter("confirmedPassword", "123456");
         request.setAttribute("a1", "v1");
         request.addHeader("h1", "v1");
+        request.addHeader("cookie", "TGC = 12345; SESSION=12345667");
 
         val response = new MockHttpServletResponse();
         val filterChain = new MockFilterChain();
@@ -51,11 +73,15 @@ class ThreadContextMDCServletFilterTests {
         val ticketSupport = mock(TicketRegistrySupport.class);
         when(cookieBuilder.retrieveCookieValue(any(HttpServletRequest.class))).thenReturn("TICKET");
         when(ticketSupport.getAuthenticatedPrincipalFrom(anyString())).thenReturn(CoreAuthenticationTestUtils.getPrincipal());
-        val filter = new ThreadContextMDCServletFilter(
-            new DirectObjectProvider<>(ticketSupport),
-            new DirectObjectProvider<>(cookieBuilder));
+        val filter = threadContextMDCServletFilter.getFilter();
         filter.init(mock(FilterConfig.class));
         filter.doFilter(request, response, filterChain);
         assertEquals(HttpStatus.OK.value(), response.getStatus());
+
+        val mdcElements = MDC.getCopyOfContextMap();
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        assertFalse(mdcElements.containsKey("password"));
+        assertFalse(mdcElements.containsKey("confirmedPassword"));
+        assertFalse(mdcElements.containsKey("cookie"));
     }
 }
