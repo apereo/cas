f4924db98f8eecf40a65557cbf5ce2038a0b0dd4
Misagh Moayyed <mm1844@gmail.com>
2024-02-13 12:25:09 +0400

    fix build

--- support/cas-server-support-oauth/src/test/java/org/apereo/cas/support/oauth/web/endpoints/OAuth20AccessTokenEndpointControllerTests.java
+++ support/cas-server-support-oauth/src/test/java/org/apereo/cas/support/oauth/web/endpoints/OAuth20AccessTokenEndpointControllerTests.java
@@ -852,7 +852,8 @@ class OAuth20AccessTokenEndpointControllerTests {
             val factory = new WebApplicationServiceFactory();
             val service = factory.createService(registeredService.getServiceId());
             val expiringRefreshTokenFactory = new OAuth20DefaultRefreshTokenFactory(
-                alwaysExpiresExpirationPolicyBuilder(), servicesManager, descendantTicketsTrackingPolicy, casProperties);
+                alwaysExpiresExpirationPolicyBuilder(), ticketRegistry,
+                servicesManager, descendantTicketsTrackingPolicy, casProperties);
             val refreshToken = expiringRefreshTokenFactory.create(service, authentication,
                 new MockTicketGrantingTicket("casuser"), new ArrayList<>(), registeredService.getClientId(), StringUtils.EMPTY, new HashMap<>(),
                 OAuth20ResponseTypes.CODE, OAuth20GrantTypes.AUTHORIZATION_CODE);
