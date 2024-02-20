2f0b3db56e0f941e27b085eaa53f5013bbd89dec
Misagh Moayyed <mm1844@gmail.com>
2024-02-20 21:23:05 +0400

    fix tests

--- ci/tests/puppeteer/scenarios/oidc-login-sonarqube/script.js
+++ ci/tests/puppeteer/scenarios/oidc-login-sonarqube/script.js
@@ -9,14 +9,8 @@ const cas = require("../../cas.js");
     await cas.waitForTimeout(page, 10000);
     await cas.assertPageTitle(page, "SonarQube");
     await cas.assertInnerText(page, "h1.login-title","Log in to SonarQube");
-    //await cas.sleep(5000);
     await cas.click(page,"a.identity-provider-link");
-    //await cas.sleep(5000);
     await cas.loginWith(page);
-    //await cas.sleep(5000);
-    // hit strapi endpoint that triggers CAS login to get JWT
-    //await cas.goto(page, "https://host.k3d.internal");
-    await cas.waitForTimeout(page, 3000);
     await cas.assertInnerTextContains(page, "ul.global-navbar-menu","Projects");
     await browser.close();
 })();
