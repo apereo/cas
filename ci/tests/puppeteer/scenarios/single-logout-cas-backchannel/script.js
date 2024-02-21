df48e02e860284de9be5d64ca95da0605fd1dd6e
Misagh Moayyed <mm1844@gmail.com>
2024-02-22 01:53:20 +0400

    fix tests

--- ci/tests/puppeteer/scenarios/single-logout-cas-backchannel/script.js
+++ ci/tests/puppeteer/scenarios/single-logout-cas-backchannel/script.js
@@ -15,7 +15,7 @@ const assert = require("assert");
     await cas.gotoLogout(page);
     await cas.goto(page, "https://localhost:8444/protected");
     await cas.logPage(page);
-    await cas.waitForElement(page, "#username");
+    await cas.waitForTimeout(page, 3000);
     url = await page.url();
     assert(url.startsWith("https://localhost:8443/cas/login?service="));
     await cas.shutdownCas("https://localhost:8444");
