441f5c66a0a851b32f2d4d663d9f8ddddcfa1e0d
Misagh Moayyed <mm1844@gmail.com>
2024-02-21 18:52:21 +0400

    support saml2 attribute release based on entity groups

--- support/cas-server-support-saml-idp-core/src/main/java/org/apereo/cas/support/saml/services/MetadataEntityGroupAttributeReleasePolicy.java
+++ support/cas-server-support-saml-idp-core/src/main/java/org/apereo/cas/support/saml/services/MetadataEntityGroupAttributeReleasePolicy.java
@@ -4,7 +4,6 @@ import org.apereo.cas.services.RegisteredServiceAttributeReleasePolicyContext;
 import org.apereo.cas.support.saml.services.idp.metadata.SamlRegisteredServiceMetadataAdaptor;
 import org.apereo.cas.support.saml.services.idp.metadata.cache.SamlRegisteredServiceCachingMetadataResolver;
 import org.apereo.cas.util.RegexUtils;
-
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,9 +11,9 @@ import lombok.ToString;
 import lombok.val;
 import org.opensaml.core.xml.ElementExtensibleXMLObject;
 import org.opensaml.saml.ext.saml2mdrpi.RegistrationInfo;
+import org.opensaml.saml.saml2.metadata.AffiliationDescriptor;
 import org.opensaml.saml.saml2.metadata.EntitiesDescriptor;
 import org.opensaml.saml.saml2.metadata.EntityDescriptor;
-
 import java.io.Serial;
 import java.util.HashMap;
 import java.util.List;
@@ -22,21 +21,21 @@ import java.util.Map;
 import java.util.Optional;
 
 /**
- * This is {@link MetadataRegistrationAuthorityAttributeReleasePolicy}.
+ * This is {@link MetadataEntityGroupAttributeReleasePolicy}.
  *
  * @author Misagh Moayyed
- * @since 5.2.0
+ * @since 7.1.0
  */
 @ToString(callSuper = true)
 @Getter
 @Setter
 @EqualsAndHashCode(callSuper = true)
-public class MetadataRegistrationAuthorityAttributeReleasePolicy extends BaseSamlRegisteredServiceAttributeReleasePolicy {
+public class MetadataEntityGroupAttributeReleasePolicy extends BaseSamlRegisteredServiceAttributeReleasePolicy {
 
     @Serial
-    private static final long serialVersionUID = -4273733307124962357L;
+    private static final long serialVersionUID = -5274638817124962357L;
 
-    private String registrationAuthority;
+    private String group;
 
     @Override
     protected Map<String, List<Object>> getAttributesForSamlRegisteredService(
@@ -45,14 +44,19 @@ public class MetadataRegistrationAuthorityAttributeReleasePolicy extends BaseSam
         final SamlRegisteredServiceMetadataAdaptor facade,
         final EntityDescriptor entityDescriptor,
         final RegisteredServiceAttributeReleasePolicyContext context) throws Throwable {
-        val extensions = Optional.ofNullable(facade.getExtensions())
-            .map(ElementExtensibleXMLObject::getUnknownXMLObjects).orElseGet(List::of);
 
-        val matched = extensions
-            .stream()
-            .filter(RegistrationInfo.class::isInstance)
-            .map(RegistrationInfo.class::cast)
-            .anyMatch(info -> RegexUtils.find(this.registrationAuthority, info.getRegistrationAuthority()));
-        return matched ? authorizeReleaseOfAllowedAttributes(context, attributes) : new HashMap<>();
+        val affiliationDescriptor = entityDescriptor.getAffiliationDescriptor();
+        if (affiliationDescriptor != null) {
+            if (RegexUtils.find(this.group, affiliationDescriptor.getID())
+                || RegexUtils.find(this.group, affiliationDescriptor.getOwnerID())) {
+                return authorizeReleaseOfAllowedAttributes(context, attributes);
+            }
+        }
+        if (entityDescriptor.getParent() instanceof final EntitiesDescriptor ed) {
+            if (RegexUtils.find(this.group, ed.getName())) {
+                return authorizeReleaseOfAllowedAttributes(context, attributes);
+            }
+        }
+        return new HashMap<>(0);
     }
 }
