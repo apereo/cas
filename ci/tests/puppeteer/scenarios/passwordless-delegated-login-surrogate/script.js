0b22e34d1e0d3db62fb43b4acb9efc6d4ab5664c
Misagh Moayyed <mm1844@gmail.com>
2024-02-25 18:01:11 +0400

    fix tests

--- ci/tests/puppeteer/scenarios/passwordless-delegated-login-surrogate/script.js
+++ ci/tests/puppeteer/scenarios/passwordless-delegated-login-surrogate/script.js
@@ -25,17 +25,16 @@ async function startAuthFlow(page, username) {
     await cas.screenshot(page);
     await cas.assertCookie(page);
     await cas.assertInnerTextStartsWith(page, "#content div p", "You, user3, have successfully logged in");
-
     await cas.click(page, "#auth-tab");
-    await cas.sleep(1000);
+    await cas.sleep(2000);
     await cas.screenshot(page);
     await cas.type(page, "#attribute-tab-1 input[type=search]", "surrogate");
-    await cas.sleep(1000);
+    await cas.sleep(2000);
     await cas.screenshot(page);
     await cas.assertInnerTextStartsWith(page, "#surrogateEnabled td code kbd", "[true]");
     await cas.assertInnerTextStartsWith(page, "#surrogatePrincipal td code kbd", "[casuser]");
     await cas.assertInnerTextStartsWith(page, "#surrogateUser td code kbd", "[user3]");
-    await cas.sleep(1000);
+    await cas.sleep(2000);
     await cas.screenshot(page);
 }
 
