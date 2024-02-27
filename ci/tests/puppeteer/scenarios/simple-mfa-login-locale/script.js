a8da453ff89eedf160871c6895b95c3ae401dee1
Misagh Moayyed <mm1844@gmail.com>
2024-02-27 15:43:10 +0400

    fix tests

--- ci/tests/puppeteer/scenarios/simple-mfa-login-locale/script.js
+++ ci/tests/puppeteer/scenarios/simple-mfa-login-locale/script.js
@@ -9,7 +9,7 @@ const cas = require("../../cas.js");
     const page = await cas.newPage(browser);
     await cas.gotoLoginWithAuthnMethod(page, undefined, "mfa-simple", "de");
     await cas.loginWith(page);
-    await cas.sleep(1000);
+    await cas.sleep(4000);
     await cas.assertVisibility(page, "#token");
 
     let code = await cas.extractFromEmail(browser);
@@ -20,9 +20,9 @@ const cas = require("../../cas.js");
     await page.bringToFront();
     await cas.type(page, "#token", code);
     await cas.submitForm(page, "#fm1");
-    await cas.sleep(1000);
+    await cas.sleep(3000);
     await cas.submitForm(page, "#registerform");
-    await cas.sleep(1000);
+    await cas.sleep(3000);
     await cas.assertInnerText(page, "#content div h2", "Anmeldung erfolgreich");
     await cas.assertCookie(page);
 
--- ci/tests/puppeteer/scenarios/stateless-ticket-registrysaml2-idp/script.js
+++ ci/tests/puppeteer/scenarios/stateless-ticket-registrysaml2-idp/script.js
@@ -9,7 +9,7 @@ async function normalAuthenticationFlow(context) {
     await cas.sleep(2000);
     await cas.screenshot(page);
     await cas.loginWith(page);
-    await cas.sleep(3000);
+    await cas.sleep(6000);
     await page.waitForSelector("#table_with_attributes", {visible: true});
     await cas.assertInnerTextContains(page, "#content p", "status page of SimpleSAMLphp");
     await cas.assertVisibility(page, "#table_with_attributes");
