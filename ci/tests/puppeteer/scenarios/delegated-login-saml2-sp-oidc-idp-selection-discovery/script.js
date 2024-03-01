ab1d65c9683c46230adada8dbcb8b20311edce9a
SavvasMisaghMoayyed <mmoayyed@unicon.net>
2024-03-01 21:09:45 +0400

    fix tests

--- ci/tests/puppeteer/scenarios/delegated-login-saml2-sp-oidc-idp-selection-discovery/script.js
+++ ci/tests/puppeteer/scenarios/delegated-login-saml2-sp-oidc-idp-selection-discovery/script.js
@@ -18,12 +18,12 @@ async function startWithCasSp(page) {
     await cas.gotoLogin(page, service);
     await cas.assertVisibility(page, "#selectProviderButton");
     await cas.submitForm(page, "#providerDiscoveryForm");
-    await cas.sleep(3000);
+    await cas.sleep(4000);
     await cas.type(page, "#username", "casuser@heroku.org");
     await cas.submitForm(page, "#discoverySelectionForm");
-    await cas.sleep(2000);
+    await cas.sleep(5000);
     await cas.loginWith(page);
-    await cas.sleep(4000);
+    await cas.sleep(5000);
     const ticket = await cas.assertTicketParameter(page);
     const body = await cas.doRequest(`https://localhost:8443/cas/p3/serviceValidate?service=${service}&ticket=${ticket}`);
     await cas.log(body);
@@ -34,24 +34,25 @@ async function startWithSamlSp(page) {
     await cas.gotoLogout(page);
 
     await cas.goto(page, "http://localhost:9443/simplesaml/module.php/core/authenticate.php?as=default-sp");
-    await cas.sleep(1000);
+    await cas.sleep(4000);
     
     await cas.assertVisibility(page, "#selectProviderButton");
     await cas.submitForm(page, "#providerDiscoveryForm");
-    await cas.sleep(3000);
+    await cas.sleep(5000);
     await cas.type(page, "#username", "casuser@example.org");
     await cas.submitForm(page, "#discoverySelectionForm");
-    await cas.sleep(2000);
+    await cas.sleep(4000);
     await cas.loginWith(page, "info@fawnoos.com", "QFkN&d^bf9vhS3KS49",
         "#okta-signin-username", "#okta-signin-password");
 
-    await page.waitForSelector("#table_with_attributes", {visible: true});
+    await cas.sleep(6000);
     await cas.assertInnerTextContains(page, "#content p", "status page of SimpleSAMLphp");
     await cas.assertVisibility(page, "#table_with_attributes");
     const authData = JSON.parse(await cas.innerHTML(page, "details pre"));
     await cas.log(authData);
-    
+
     await cas.gotoLogin(page);
+    await cas.sleep(2000);
     await cas.assertCookie(page);
     await cas.removeDirectoryOrFile(path.join(__dirname, "/saml-md"));
 }
