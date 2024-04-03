b39043461f02b3dec8fd6256ee07ab6916de7bb3
Misagh Moayyed <mm1844@gmail.com>
2024-04-03 19:55:01 +0400

    support OIDC CIBA authn requests - WIP

---
+++ support/cas-server-support-oidc-core-api/src/main/java/org/apereo/cas/oidc/ticket/OidcDefaultCibaRequest.java
@@ -0,0 +1,36 @@
+package org.apereo.cas.oidc.ticket;
+
+import org.apereo.cas.ticket.AbstractTicket;
+import org.apereo.cas.ticket.ExpirationPolicy;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import java.io.Serial;
+import java.util.Set;
+
+/**
+ * This is {@link OidcDefaultCibaRequest}.
+ *
+ * @author Misagh Moayyed
+ * @since 7.1.0
+ */
+@Getter
+public class OidcDefaultCibaRequest extends AbstractTicket implements OidcCibaRequest {
+    @Serial
+    private static final long serialVersionUID = 2209847487395823514L;
+
+    @JsonIgnore
+    private final String prefix = OidcCibaRequest.PREFIX;
+
+    private final Set<String> scopes;
+
+    private final String clientId;
+
+    public OidcDefaultCibaRequest(final String id,
+                                  final ExpirationPolicy expirationPolicy,
+                                  final Set<String> scopes,
+                                  final String clientId) {
+        super(id, expirationPolicy);
+        this.scopes = scopes;
+        this.clientId = clientId;
+    }
+}
