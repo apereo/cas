159fd010b3386e6bb470ca06136d8bb2602f18dd
CAS in the cloud LELEU Jérôme <leleuj@gmail.com>
2024-06-11 15:49:41 +0200

    Small refactoring for the WebAuthn support
    The idea of this PR is to allow customisations by replacing the current local Caffeine `Cache` by a clustered one (based on the ticket registry for example).
    
    No new test is needed.

--- support/cas-server-support-webauthn-core/src/main/java/com/yubico/core/DefaultSessionManager.java
+++ support/cas-server-support-webauthn-core/src/main/java/com/yubico/core/DefaultSessionManager.java
@@ -1,24 +1,17 @@
 package com.yubico.core;
 
-
 import com.github.benmanes.caffeine.cache.Cache;
-import com.github.benmanes.caffeine.cache.Caffeine;
 import com.yubico.webauthn.data.ByteArray;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 
+@RequiredArgsConstructor
 public class DefaultSessionManager implements SessionManager {
-    private final Cache<ByteArray, ByteArray> sessionIdsToUsers = newCache();
+    private final Cache<ByteArray, ByteArray> sessionIdsToUsers;
 
-    private final Cache<ByteArray, ByteArray> usersToSessionIds = newCache();
-
-    private static Cache<ByteArray, ByteArray> newCache() {
-        return Caffeine.newBuilder()
-            .maximumSize(100L)
-            .expireAfterAccess(5L, TimeUnit.MINUTES)
-            .build();
-    }
+    private final Cache<ByteArray, ByteArray> usersToSessionIds;
 
     @Override
     public ByteArray createSession(@NonNull final ByteArray userHandle) {
