e965a751acb4bcf843cd0cb39b8708b348f3ebbd
Misagh Moayyed <mm1844@gmail.com>
2024-07-03 16:51:47 +0400

    polish: move groovy support into its own module

--- support/cas-server-support-pm-webflow/src/test/java/org/apereo/cas/pm/web/flow/actions/BasePasswordManagementActionTests.java
+++ support/cas-server-support-pm-webflow/src/test/java/org/apereo/cas/pm/web/flow/actions/BasePasswordManagementActionTests.java
@@ -11,6 +11,7 @@ import org.apereo.cas.config.CasCoreLogoutAutoConfiguration;
 import org.apereo.cas.config.CasCoreMultifactorAuthenticationAutoConfiguration;
 import org.apereo.cas.config.CasCoreMultifactorAuthenticationWebflowAutoConfiguration;
 import org.apereo.cas.config.CasCoreNotificationsAutoConfiguration;
+import org.apereo.cas.config.CasCoreScriptingAutoConfiguration;
 import org.apereo.cas.config.CasCoreServicesAutoConfiguration;
 import org.apereo.cas.config.CasCoreTicketsAutoConfiguration;
 import org.apereo.cas.config.CasCoreUtilAutoConfiguration;
@@ -132,7 +133,9 @@ public abstract class BasePasswordManagementActionTests {
         CasCoreNotificationsAutoConfiguration.class,
         CasCoreMultifactorAuthenticationAutoConfiguration.class,
         CasCoreMultifactorAuthenticationWebflowAutoConfiguration.class,
-        CasCoreWebflowAutoConfiguration.class})
+        CasCoreWebflowAutoConfiguration.class,
+        CasCoreScriptingAutoConfiguration.class
+    })
     @SpringBootConfiguration(proxyBeanMethods = false)
     @Import(CasPersonDirectoryTestConfiguration.class)
     public static class SharedTestConfiguration {
