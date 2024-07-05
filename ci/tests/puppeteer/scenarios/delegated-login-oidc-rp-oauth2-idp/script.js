ae0663388221b0bbc035f7a116f7b798eb8e93cb
Misagh Moayyed <mm1844@gmail.com>
2024-07-05 17:35:05 +0400

    add puppeteer tests for oauth2 delegated authN

---
+++ ci/tests/puppeteer/scenarios/delegated-login-oidc-rp-oauth2-idp/script.js
@@ -0,0 +1,66 @@
+const assert = require("assert");
+const cas = require("../../cas.js");
+
+(async () => {
+    await cas.httpServer(__dirname, 5432, false);
+    let failure = false;
+    const browser = await cas.newBrowser(cas.browserOptions());
+    try {
+        const page = await cas.newPage(browser);
+
+        const redirectUri = "https://localhost:9859/anything/1";
+        const url = "https://localhost:8443/cas/oidc/oidcAuthorize?" +
+            `client_id=client&redirect_uri=${decodeURIComponent(redirectUri)}&` +
+            "scope=openid%20email%20profile%20address%20phone&response_type=code";
+
+        await cas.goto(page, url);
+
+        await cas.assertVisibility(page, "li #GOOGLE");
+        await cas.click(page, "li #GOOGLE");
+        await cas.waitForNavigation(page);
+
+        await cas.sleep(3000);
+        await cas.screenshot(page);
+
+        const password = process.env.GOOGLE_CASUSER_PASSWORD;
+        await cas.type(page, "input#identifierId", "apereocasuser@gmail.com");
+        await cas.pressEnter(page);
+        await cas.sleep(5000);
+        await cas.type(page, "input[type='password']", password, true);
+        await cas.pressEnter(page);
+        await cas.sleep(7000);
+        await cas.screenshot(page);
+        await cas.click(page, "button#submit_approve_access");
+        await cas.sleep(7000);
+
+        const result = new URL(page.url());
+        await cas.log(result.searchParams.toString());
+
+        assert(result.searchParams.has("ticket") === false);
+        assert(result.searchParams.has("client_id"));
+        assert(result.searchParams.has("redirect_uri"));
+        assert(result.searchParams.has("scope"));
+        await cas.sleep(2000);
+
+        await cas.log("Allowing release of scopes and claims...");
+        await cas.click(page, "#allow");
+        await cas.waitForNavigation(page);
+        await cas.sleep(2000);
+
+        await cas.logPage(page);
+        assert(await page.url().startsWith("https://localhost:9859/anything/1"));
+        await cas.sleep(2000);
+        const body = JSON.parse(await cas.innerText(page, "pre"));
+        assert(body.args.code !== null);
+
+        await cas.gotoLogout(page);
+    } catch (e) {
+        failure = true;
+        throw e;
+    } finally {
+        await browser.close();
+        if (!failure) {
+            await process.exit(0);
+        }
+    }
+})();
