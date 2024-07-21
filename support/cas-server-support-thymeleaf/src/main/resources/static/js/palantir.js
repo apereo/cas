a2370577ef45c90bbcf45c8dcd04bea7f285aa16
Misagh Moayyed <mm1844@gmail.com>
2024-07-21 18:49:09 +0400

    palantir: minor polish

--- support/cas-server-support-thymeleaf/src/main/resources/static/js/palantir.js
+++ support/cas-server-support-thymeleaf/src/main/resources/static/js/palantir.js
@@ -3,20 +3,28 @@
  */
 function fetchServices() {
     $.get(`${casServerPrefix}/actuator/registeredServices`, response => {
-
+        let serviceCountByType = [0,0,0,0,0];
         let applicationsTable = $("#applicationsTable").DataTable();
         applicationsTable.clear();
         for (const service of response[1]) {
             let icon = "mdi-web-box";
+
             const serviceClass = service["@class"];
             if (serviceClass.includes("CasRegisteredService")) {
                 icon = "mdi-alpha-c-box-outline";
+                serviceCountByType[0]++;
             } else if (serviceClass.includes("SamlRegisteredService")) {
                 icon = "mdi-alpha-s-box-outline";
+                serviceCountByType[1]++;
             } else if (serviceClass.includes("OAuthRegisteredService")) {
                 icon = "mdi-alpha-o-circle-outline";
+                serviceCountByType[2]++;
             } else if (serviceClass.includes("OidcRegisteredService")) {
                 icon = "mdi-alpha-o-box-outline";
+                serviceCountByType[3]++;
+            } else if (serviceClass.includes("WSFederationRegisteredService")) {
+                icon = "mdi-alpha-w-box-outline";
+                serviceCountByType[4]++;
             }
 
             let serviceDetails = `<span serviceId="${service.id}" title='${service.name}'>${service.name}</span>`;
@@ -47,6 +55,10 @@ function fetchServices() {
             });
         }
         applicationsTable.draw();
+        
+        servicesChart.data.datasets[0].data = serviceCountByType;
+        servicesChart.update();
+
         initializeServiceButtons();
     }).fail((xhr, status, error) => {
         console.error("Error fetching data:", error);
@@ -144,6 +156,14 @@ function initializeFooterButtons() {
 }
 
 /**
+ * Charts objects.
+ */
+let servicesChart = null;
+let memoryChart = null;
+let statisticsChart = null;
+let systemHealthChart = null;
+let jvmThreadsChart = null;
+/**
  * Initialization Functions
  */
 async function initializeAccessStrategyOperations() {
@@ -190,6 +210,46 @@ async function initializeAccessStrategyOperations() {
         });
         event.preventDefault();
     }, false);
+}
+
+function initializeJvmMetrics() {
+    function fetchJvmThreadMetric(metricName) {
+        return new Promise((resolve, reject) => {
+            $.get(`${casServerPrefix}/actuator/metrics/${metricName}`, response => {
+                resolve(response.measurements[0].value);
+            }).fail((xhr, status, error) => {
+                console.error("Error fetching data:", error);
+                reject(error);
+            });
+        });
+    }
+
+    async function fetchJvmMetrics() {
+        try {
+            const promises = [
+                "jvm.threads.daemon",
+                "jvm.threads.live",
+                "jvm.threads.peak",
+                "jvm.threads.started",
+                "jvm.threads.states"
+            ].map(metric => fetchJvmThreadMetric(metric));
+            const results = await Promise.all(promises);
+            const numbersArray = results.map(result => Number(result));
+            console.log("Fetched values:", numbersArray);
+            return numbersArray;
+        } catch (error) {
+            console.error("An error occurred:", error);
+        }
+    }
+
+    let tabs = new mdc.tabBar.MDCTabBar(document.querySelector("#dashboardTabBar"));
+    const index = tabs.foundation.adapter.getFocusedTabIndex();
+    if (index === 3) {
+        fetchJvmMetrics().then(payload => {
+            jvmThreadsChart.data.datasets[0].data = payload;
+            jvmThreadsChart.update();
+        });
+    }
 }
 
 async function initializeScheduledTasksOperations() {
@@ -247,6 +307,17 @@ async function initializeScheduledTasksOperations() {
     }).fail((xhr, status, error) => {
         console.error("Error fetching data:", error);
     });
+
+    let tabs = new mdc.tabBar.MDCTabBar(document.querySelector("#dashboardTabBar"));
+    tabs.listen("MDCTabBar:activated", ev => {
+        let index = ev.detail.index;
+        if (index === 3) {
+            initializeJvmMetrics();
+        }
+    });
+    setInterval(() => {
+        initializeJvmMetrics();
+    }, 4000);
 }
 
 async function initializeTicketsOperations() {
@@ -360,7 +431,7 @@ async function initializeTicketsOperations() {
 }
 
 async function initializeSystemOperations() {
-    function configureHealthChart(systemHealthChart) {
+    function configureHealthChart() {
         $.get(`${casServerPrefix}/actuator/health`, response => {
             console.log(response);
             const payload = {
@@ -392,7 +463,7 @@ async function initializeSystemOperations() {
         });
     }
 
-    function configureStatistics(statisticsChart) {
+    function configureStatistics() {
         $.get(`${casServerPrefix}/actuator/statistics`, response => {
             const expired = response.expiredTickets;
             const valid = response.validTickets;
@@ -409,7 +480,7 @@ async function initializeSystemOperations() {
         });
     }
 
-    function configureSystemData(memoryChart) {
+    function configureSystemData() {
         fetchSystemData(response => {
             const maximum = convertMemoryToGB(response.systemInfo["JVM Maximum Memory"]);
             const free = convertMemoryToGB(response.systemInfo["JVM Free Memory"]);
@@ -417,6 +488,24 @@ async function initializeSystemOperations() {
             console.log("Memory: ", maximum, total, free);
             memoryChart.data.datasets[0].data = [maximum, total, free];
             memoryChart.update();
+        });
+        $.get(`${casServerPrefix}/actuator/metrics/http.server.requests`, response => {
+            let count = response.measurements[0].value;
+            let totalTime = response.measurements[1].value.toFixed(2);
+            let maxTime = response.measurements[2].value.toFixed(2);
+            $("#httpRequestsCount").text(count);
+            $("#httpRequestsTotalTime").text(`${totalTime}s`);
+            $("#httpRequestsMaxTime").text(`${maxTime}s`);
+        }).fail((xhr, status, error) => {
+            console.error("Error fetching data:", error);
+        });
+        $.get(`${casServerPrefix}/actuator/metrics/http.server.requests.active`, response => {
+            let active = response.measurements[0].value;
+            let duration = response.measurements[1].value.toFixed(2);
+            $("#httpRequestsActive").text(active);
+            $("#httpRequestsDuration").text(`${duration}s`);
+        }).fail((xhr, status, error) => {
+            console.error("Error fetching data:", error);
         });
     }
 
@@ -447,109 +536,24 @@ async function initializeSystemOperations() {
         }
     });
 
-    const memoryChartCtx = document.getElementById("memoryChart").getContext("2d");
-    const memoryChart = new Chart(memoryChartCtx, {
-        type: "bar",
-        data: {
-            labels: ["Total Memory", "Free Memory", "Used Memory"],
-            datasets: [{
-                label: "Memory (GB)",
-                data: [0, 0, 0],
-                fill: true,
-                backgroundColor: ["rgba(54, 162, 235, 0.2)", "rgba(75, 192, 192, 0.2)", "rgba(255, 99, 132, 0.2)"],
-                borderColor: ["rgba(54, 162, 235, 1)", "rgba(75, 192, 192, 1)", "rgba(255, 99, 132, 1)"],
-                borderWidth: 2
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true,
-                    stacked: true
-                },
-                x: {
-                    stacked: true,
-                    grid: {
-                        offset: true
-                    }
-                }
-            }
-        }
-    });
-
-    const statisticsChartCtx = document.getElementById("statisticsChart").getContext("2d");
-    const statisticsChart = new Chart(statisticsChartCtx, {
-        type: "bar",
-        data: {
-            labels: ["Current Tickets", "Expired (Removed) Tickets"],
-            datasets: [{
-                label: "Ticket Registry",
-                data: [0, 0],
-                fill: true,
-                backgroundColor: ["rgba(75, 192, 192, 0.2)", "rgba(255, 99, 132, 0.2)"],
-                borderColor: ["rgba(75, 192, 192, 1)", "rgba(255, 99, 132, 1)"],
-                borderWidth: 2
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true,
-                    stacked: true,
-                    ticks: {
-                        stepSize: 1
-                    }
-                },
-                x: {
-                    stacked: true,
-                    grid: {
-                        offset: true
-                    }
-                }
-            }
-        }
-    });
-
-    const systemHealthCtx = document.getElementById("systemHealthChart").getContext("2d");
-    const systemHealthChart = new Chart(systemHealthCtx, {
-        type: "bar",
-        data: {
-            datasets: [{
-                label: "System Health",
-                fill: true,
-                borderWidth: 2
-            }]
-        },
-        options: {
-            indexAxis: "y",
-            scales: {
-                x: {
-                    ticks: {
-                        display: false
-                    }
-                }
-            }
-        }
-    });
-
     setInterval(() => {
         const index = tabs.foundation.adapter.getFocusedTabIndex();
         if (index === 1) {
-            configureSystemData(memoryChart);
-        }
-    }, 2000);
-
-    setInterval(() => {
-        const index = tabs.foundation.adapter.getFocusedTabIndex();
-        if (index === 1) {
-            configureStatistics(statisticsChart);
+            configureSystemData();
         }
     }, 5000);
 
     setInterval(() => {
         const index = tabs.foundation.adapter.getFocusedTabIndex();
         if (index === 1) {
-            configureHealthChart(systemHealthChart);
+            configureStatistics();
+        }
+    }, 5000);
+
+    setInterval(() => {
+        const index = tabs.foundation.adapter.getFocusedTabIndex();
+        if (index === 1) {
+            configureHealthChart();
         }
     }, 5000);
 
@@ -561,7 +565,7 @@ async function initializeSystemOperations() {
                             <div class="mdc-chip" role="row">
                                 <div class="mdc-chip__ripple"></div>
                                 <span role="gridcell">
-                                  <span class="mdc-chip__text">${element.trim()}</span>
+                                  <span class="mdc-chip__text">${element.trim().replace("CasFeatureModule.", "")}</span>
                                 </span>
                             </div>
                         `.trim();
@@ -569,9 +573,9 @@ async function initializeSystemOperations() {
         }
     });
 
-    configureSystemData(memoryChart);
-    configureStatistics(statisticsChart);
-    configureHealthChart(systemHealthChart);
+    configureSystemData();
+    configureStatistics();
+    configureHealthChart();
 }
 
 async function initializeServiceButtons() {
@@ -670,13 +674,167 @@ async function initializeServicesOperations() {
             $("#applicationsTable td").addClass("mdc-data-table__cell");
         }
     });
+    
     fetchServices();
     initializeFooterButtons();
+}
+
+async function initializeAllCharts() {
+    servicesChart = new Chart(document.getElementById("servicesChart").getContext("2d"), {
+        type: "pie",
+        data: {
+            labels: [
+                'CAS',
+                'SAML2',
+                'OAuth',
+                'OpenID Connect',
+                'Ws-Federation'
+            ],
+            datasets: [{
+                label: 'Registered Services',
+                data: [0,0,0,0,0],
+                backgroundColor: [
+                    'deepskyblue',
+                    'indianred',
+                    'mediumpurple',
+                    'limegreen',
+                    'slategrey'
+                ],
+                hoverOffset: 4
+            }]
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true
+                }
+            }
+        }
+    });
+    servicesChart.update();
+    
+    memoryChart = new Chart(document.getElementById("memoryChart").getContext("2d"), {
+        type: "bar",
+        data: {
+            labels: ["Total Memory", "Free Memory", "Used Memory"],
+            datasets: [{
+                label: "Memory (GB)",
+                data: [0, 0, 0],
+                fill: true,
+                backgroundColor: ["rgba(54, 162, 235, 0.2)", "rgba(75, 192, 192, 0.2)", "rgba(255, 99, 132, 0.2)"],
+                borderColor: ["rgba(54, 162, 235, 1)", "rgba(75, 192, 192, 1)", "rgba(255, 99, 132, 1)"],
+                borderWidth: 2
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    stacked: true
+                },
+                x: {
+                    stacked: true,
+                    grid: {
+                        offset: true
+                    }
+                }
+            }
+        }
+    });
+
+    statisticsChart = new Chart(document.getElementById("statisticsChart").getContext("2d"), {
+        type: "bar",
+        data: {
+            labels: ["Current Tickets", "Expired (Removed) Tickets"],
+            datasets: [{
+                label: "Ticket Registry",
+                data: [0, 0],
+                fill: true,
+                backgroundColor: ["rgba(75, 192, 192, 0.2)", "rgba(255, 99, 132, 0.2)"],
+                borderColor: ["rgba(75, 192, 192, 1)", "rgba(255, 99, 132, 1)"],
+                borderWidth: 2
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    stacked: true,
+                    ticks: {
+                        stepSize: 1
+                    }
+                },
+                x: {
+                    stacked: true,
+                    grid: {
+                        offset: true
+                    }
+                }
+            }
+        }
+    });
+
+    systemHealthChart = new Chart(document.getElementById("systemHealthChart").getContext("2d"), {
+        type: "bar",
+        data: {
+            datasets: [{
+                label: "System Health",
+                fill: true,
+                borderWidth: 2
+            }]
+        },
+        options: {
+            indexAxis: "y",
+            scales: {
+                x: {
+                    ticks: {
+                        display: false
+                    }
+                }
+            }
+        }
+    });
+
+    jvmThreadsChart = new Chart(document.getElementById("jvmThreadsChart").getContext("2d"), {
+        type: "bar",
+        data: {
+            labels: ["Daemon", "Live", "Peak", "Started", "States"],
+            datasets: [{
+                label: "JVM Threads",
+                data: [0, 0, 0, 0, 0],
+                fill: true,
+                borderWidth: 2
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    stacked: true,
+                    ticks: {
+                        stepSize: 1
+                    }
+                },
+                x: {
+                    stacked: true,
+                    grid: {
+                        offset: true
+                    }
+                }
+            }
+        }
+    });
+
 }
 
 async function initializePalantir() {
     try {
         await Promise.all([
+            initializeAllCharts(),
             initializeScheduledTasksOperations(),
             initializeServicesOperations(),
             initializeAccessStrategyOperations(),
