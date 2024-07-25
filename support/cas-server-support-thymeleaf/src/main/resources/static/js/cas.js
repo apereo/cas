7f77f3cfd2ab7b2359ed1cf878c1cab0642cd197
Misagh Moayyed <mm1844@gmail.com>
2024-07-25 14:26:37 +0400

    minor tweaks to dom content loading and events

--- support/cas-server-support-thymeleaf/src/main/resources/static/js/cas.js
+++ support/cas-server-support-thymeleaf/src/main/resources/static/js/cas.js
@@ -32,6 +32,24 @@ function getLastTwoWords(str) {
     return parts.slice(-2).join('.');
 }
 
+function formatDateYearMonthDayHourMinute(date) {
+    const d = new Date(date);
+    const year = d.getFullYear();
+    const month = (d.getMonth() + 1).toString().padStart(2, "0"); // Months are zero-based
+    const day = d.getDate().toString().padStart(2, "0");
+    const hours = d.getHours().toString().padStart(2, "0");
+    const minutes = d.getMinutes().toString().padStart(2, "0");
+    return `${year}-${month}-${day} ${hours}:${minutes}`;
+}
+
+function formatDateYearMonthDay(date) {
+    const d = new Date(date);
+    const year = d.getFullYear();
+    const month = (d.getMonth() + 1).toString().padStart(2, "0");
+    const day = d.getDate().toString().padStart(2, "0");
+    return `${year}-${month}-${day}`;
+}
+
 
 function camelcaseToTitleCase(str) {
     return str
