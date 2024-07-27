65e6bee0ef5ad04515ce34036c3e6229cbc1b474
Misagh Moayyed <mm1844@gmail.com>
2024-07-27 10:41:03 +0400

    use apache compress to process/extract zip resources

--- core/cas-server-core-events-api/src/main/java/org/apereo/cas/support/events/web/CasEventsReportEndpoint.java
+++ core/cas-server-core-events-api/src/main/java/org/apereo/cas/support/events/web/CasEventsReportEndpoint.java
@@ -7,6 +7,7 @@ import org.apereo.cas.web.BaseCasRestActuatorEndpoint;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.val;
+import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
@@ -22,7 +23,6 @@ import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Comparator;
 import java.util.stream.Collectors;
-import java.util.zip.ZipInputStream;
 
 /**
  * This is {@link CasEventsReportEndpoint}.
@@ -108,7 +108,7 @@ public class CasEventsReportEndpoint extends BaseCasRestActuatorEndpoint {
     private ResponseEntity<CasEvent> importEventsAsStream(final HttpServletRequest request) throws Throwable {
         val eventRepository = applicationContext.getBean(CasEventRepository.BEAN_NAME, CasEventRepository.class);
         try (val bais = new ByteArrayInputStream(IOUtils.toByteArray(request.getInputStream()));
-             val zipIn = new ZipInputStream(bais)) {
+             val zipIn = new ZipArchiveInputStream(bais)) {
             var entry = zipIn.getNextEntry();
             while (entry != null) {
                 if (!entry.isDirectory()) {
@@ -116,7 +116,6 @@ public class CasEventsReportEndpoint extends BaseCasRestActuatorEndpoint {
                     val casEvent = MAPPER.readValue(requestBody, CasEvent.class);
                     eventRepository.save(casEvent);
                 }
-                zipIn.closeEntry();
                 entry = zipIn.getNextEntry();
             }
         }
