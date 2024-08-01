3b5c689eadfc15aeddf9b32eca2fc2124e41b151
Misagh Moayyed <mm1844@gmail.com>
2024-08-01 11:20:42 +0400

    add consent support to palantir

--- support/cas-server-support-consent-core/src/main/java/org/apereo/cas/consent/AttributeConsentReportEndpoint.java
+++ support/cas-server-support-consent-core/src/main/java/org/apereo/cas/consent/AttributeConsentReportEndpoint.java
@@ -32,6 +32,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
@@ -61,6 +62,25 @@ public class AttributeConsentReportEndpoint extends BaseCasRestActuatorEndpoint 
     }
 
     /**
+     * Consent all decisions.
+     *
+     * @return the collection
+     */
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Get all consent decisions")
+    public Collection<Map<String, Object>> consentDecisions() {
+        val result = new HashSet<Map<String, Object>>();
+        val consentDecisions = consentRepository.getObject().findConsentDecisions();
+        consentDecisions.forEach(decision -> {
+            val map = new HashMap<String, Object>();
+            map.put("decision", decision);
+            map.put("attributes", consentEngine.getObject().resolveConsentableAttributesFrom(decision));
+            result.add(map);
+        });
+        return result;
+    }
+    
+    /**
      * Consent decisions collection.
      *
      * @param principal the principal
@@ -74,13 +94,13 @@ public class AttributeConsentReportEndpoint extends BaseCasRestActuatorEndpoint 
         final String principal) {
         val result = new HashSet<Map<String, Object>>();
         LOGGER.debug("Fetching consent decisions for principal [{}]", principal);
-        val consentDecisions = this.consentRepository.getObject().findConsentDecisions(principal);
+        val consentDecisions = consentRepository.getObject().findConsentDecisions(principal);
         LOGGER.debug("Resolved consent decisions for principal [{}]: [{}]", principal, consentDecisions);
 
         consentDecisions.forEach(d -> {
             val map = new HashMap<String, Object>();
             map.put("decision", d);
-            map.put("attributes", this.consentEngine.getObject().resolveConsentableAttributesFrom(d));
+            map.put("attributes", consentEngine.getObject().resolveConsentableAttributesFrom(d));
             result.add(map);
         });
         return result;
@@ -107,6 +127,7 @@ public class AttributeConsentReportEndpoint extends BaseCasRestActuatorEndpoint 
         val headers = new HttpHeaders();
         headers.setContentDisposition(ContentDisposition.attachment()
             .filename(Objects.requireNonNull(resource.getFilename())).build());
+        headers.put("Filename", List.of("consent.zip"));
         return new ResponseEntity<>(resource, headers, HttpStatus.OK);
     }
 
@@ -161,6 +182,6 @@ public class AttributeConsentReportEndpoint extends BaseCasRestActuatorEndpoint 
         @PathVariable
         final String principal) throws Throwable {
         LOGGER.debug("Deleting all consent decisions for principal [{}].", principal);
-        return this.consentRepository.getObject().deleteConsentDecisions(principal);
+        return consentRepository.getObject().deleteConsentDecisions(principal);
     }
 }
