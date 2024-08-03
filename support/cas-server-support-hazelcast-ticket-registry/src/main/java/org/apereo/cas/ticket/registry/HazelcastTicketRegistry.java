5070b7f93cda6eccf339645df3bb28eca5f4bfb2
Misagh Moayyed <mm1844@gmail.com>
2024-08-03 14:06:25 +0400

    update versions; minor polish in HZ registry to remove parallel streams

--- support/cas-server-support-hazelcast-ticket-registry/src/main/java/org/apereo/cas/ticket/registry/HazelcastTicketRegistry.java
+++ support/cas-server-support-hazelcast-ticket-registry/src/main/java/org/apereo/cas/ticket/registry/HazelcastTicketRegistry.java
@@ -265,11 +265,11 @@ public class HazelcastTicketRegistry extends AbstractTicketRegistry implements A
     public Stream<? extends Ticket> stream() {
         return ticketCatalog
             .findAll()
-            .parallelStream()
+            .stream()
             .map(metadata -> getTicketMapInstanceByMetadata(metadata).values())
             .flatMap(tickets -> {
                 var resultStream = tickets
-                    .parallelStream()
+                    .stream()
                     .map(HazelcastTicketDocument::getTicket);
                 if (properties.getPageSize() > 0) {
                     resultStream = resultStream.limit(properties.getPageSize());
