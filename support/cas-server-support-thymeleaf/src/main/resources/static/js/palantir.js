a467f08691b2aa56226b67a93a7097a40f83fdfe
Misagh Moayyed <mm1844@gmail.com>
2024-08-07 11:14:28 +0400

    allow palantir to remove throttled records individually

--- support/cas-server-support-thymeleaf/src/main/resources/static/js/palantir.js
+++ support/cas-server-support-thymeleaf/src/main/resources/static/js/palantir.js
@@ -2212,10 +2212,11 @@ async function initializeThrottlesOperations() {
         autoWidth: false,
         columnDefs: [
             {width: "15%", targets: 1},
-            {width: "15%", targets: 2},
+            {width: "10%", targets: 2},
             {width: "15%", targets: 3},
-            {width: "15%", targets: 4},
-            {width: "20%", targets: 5}
+            {width: "10%", targets: 4},
+            {width: "20%", targets: 5},
+            {width: "10%", targets: 6},
         ],
         drawCallback: settings => {
             $("#throttlesTable tr").addClass("mdc-data-table__row");
@@ -2227,23 +2228,60 @@ async function initializeThrottlesOperations() {
         throttlesTable.clear();
         $.get(actuatorEndpoints.throttles, response => {
             for (const record of response) {
+
+                let buttons = `
+                     <button type="button" name="removeThrottledAttempt" href="#" 
+                            data-key='${record.key}'
+                            class="mdc-button mdc-button--raised min-width-32x">
+                        <i class="mdi mdi-delete min-width-32x" aria-hidden="true"></i>
+                    </button>
+                `;
+                
                 throttlesTable.row.add({
                     0: `<code>${record.key}</code>`,
                     1: `<code>${record.id}</code>`,
                     2: `<code>${record.value}</code>`,
                     3: `<code>${record.username}</code>`,
                     4: `<code>${record.clientIpAddress}</code>`,
-                    5: `<code>${record.expiration}</code>`
+                    5: `<code>${record.expiration}</code>`,
+                    6: `${buttons}`,
                 });
             }
             throttlesTable.draw();
+            
+            $("button[name=removeThrottledAttempt]").off().on("click", function () {
+                const key = $(this).data("key");
+                swal({
+                    title: "Are you sure you want to delete this entry?",
+                    text: "Once deleted, you may not be able to recover this entry.",
+                    icon: "warning",
+                    buttons: true,
+                    dangerMode: true
+                })
+                    .then((willDelete) => {
+                        if (willDelete) {
+                            $.ajax({
+                                url: `${actuatorEndpoints.throttles}/${key}`,
+                                type: "DELETE",
+                                contentType: "application/x-www-form-urlencoded",
+                                success: (response, status, xhr) => {
+                                    let nearestTr = $(this).closest("tr");
+                                    throttlesTable.row(nearestTr).remove().draw();
+                                },
+                                error: (xhr, status, error) => {
+                                    console.error("Error fetching data:", error);
+                                    displayErrorInBanner(xhr);
+                                }
+                            });
+                        }
+                    });
+            });
         }).fail((xhr, status, error) => {
             console.error("Error fetching data:", error);
             displayErrorInBanner(xhr);
         });
     }
-
-
+    
     if (actuatorEndpoints.throttles) {
         fetchThrottledAttempts();
 
@@ -2277,7 +2315,7 @@ async function initializeThrottlesOperations() {
         $("button[name=clearThrottlesButton]").off().on("click", () => {
             swal({
                 title: "Are you sure you want to clear throttled entries?",
-                text: "Released entries will be removed from the authentication throttling store.",
+                text: "All entries will be removed from the authentication throttling store.",
                 icon: "warning",
                 buttons: true,
                 dangerMode: true
