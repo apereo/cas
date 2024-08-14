423cd4898a3cba2c02f88e68ea77b71fa700137e
Misagh Moayyed <mm1844@gmail.com>
2024-08-14 20:09:56 +0400

    fix id generation for services

--- core/cas-server-core-services/src/test/java/org/apereo/cas/services/InMemoryServiceRegistryTests.java
+++ core/cas-server-core-services/src/test/java/org/apereo/cas/services/InMemoryServiceRegistryTests.java
@@ -1,14 +1,16 @@
 package org.apereo.cas.services;
 
-import lombok.val;
+import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.springframework.context.support.StaticApplicationContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 import java.util.UUID;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -19,13 +21,16 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 @Tag("RegisteredService")
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(classes = RefreshAutoConfiguration.class)
+@EnableConfigurationProperties(CasConfigurationProperties.class)
 class InMemoryServiceRegistryTests extends AbstractServiceRegistryTests {
 
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
+    
     @Override
     public ServiceRegistry getNewServiceRegistry() {
-        val appCtx = new StaticApplicationContext();
-        appCtx.refresh();
-        return new InMemoryServiceRegistry(appCtx);
+        return new InMemoryServiceRegistry(applicationContext);
     }
 
     @Test
