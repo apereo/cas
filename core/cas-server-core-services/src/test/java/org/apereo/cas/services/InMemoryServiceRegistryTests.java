cd4b8f22dd04412a086ed200c2af577a5fc1576f
Misagh Moayyed <mm1844@gmail.com>
2024-08-15 10:00:12 +0400

    fix build issues / tests

--- core/cas-server-core-services/src/test/java/org/apereo/cas/services/InMemoryServiceRegistryTests.java
+++ core/cas-server-core-services/src/test/java/org/apereo/cas/services/InMemoryServiceRegistryTests.java
@@ -1,10 +1,12 @@
 package org.apereo.cas.services;
 
 import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.test.CasTestExtension;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,6 +25,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest(classes = RefreshAutoConfiguration.class)
 @EnableConfigurationProperties(CasConfigurationProperties.class)
+@ExtendWith(CasTestExtension.class)
 class InMemoryServiceRegistryTests extends AbstractServiceRegistryTests {
 
     @Autowired
