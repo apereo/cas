363e1435e192f0147263cfff6c94b03062fff82e
Misagh Moayyed <mm1844@gmail.com>
2024-09-05 15:56:27 +0400

    update tests

--- ci/tests/puppeteer/scenarios/undertow-login-success/script.js
+++ ci/tests/puppeteer/scenarios/undertow-login-success/script.js
@@ -1,4 +1,3 @@
-
 const cas = require("../../cas.js");
 
 (async () => {
@@ -6,6 +5,7 @@ const cas = require("../../cas.js");
     const page = await cas.newPage(browser);
     await cas.gotoLogin(page);
     await cas.loginWith(page);
+    await cas.sleep(3000);
     await cas.assertCookie(page);
     await cas.assertPageTitle(page, "CAS - Central Authentication Service Log In Successful");
     await cas.assertInnerText(page, "#content div h2", "Log In Successful");
