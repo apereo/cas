44f79f4e4539e78b3ad6d35a6d04ad42feb4be30
Misagh Moayyed <mm1844@gmail.com>
2024-09-06 21:41:20 +0400

    update tests

--- ci/tests/puppeteer/scenarios/thymeleaf-templates-external/script.js
+++ ci/tests/puppeteer/scenarios/thymeleaf-templates-external/script.js
@@ -1,12 +1,14 @@
-
 const cas = require("../../cas.js");
+const assert = require("assert");
 
 (async () => {
     const browser = await cas.newBrowser(cas.browserOptions());
     const page = await cas.newPage(browser);
     await cas.gotoLogin(page);
-
     await cas.assertVisibility(page, "#externalFooter");
-
+    const response = await cas.goto(page, "https://localhost:8443/cas/pages/index.txt");
+    await cas.sleep(1000);
+    await cas.log(`${response.status()} ${response.statusText()}`);
+    assert(response.ok());
     await browser.close();
 })();
