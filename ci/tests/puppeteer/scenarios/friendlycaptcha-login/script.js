25f78b56b0d2c4ef3e648b1fd05696af527a90c1
CAS in the cloud LELEU Jérôme <leleuj@gmail.com>
2024-09-06 12:24:48 +0200

    WIP Add Friendly Captcha support (#6119)
    Co-authored-by: Misagh Moayyed <mm1844@gmail.com>

---
+++ ci/tests/puppeteer/scenarios/friendlycaptcha-login/script.js
@@ -0,0 +1,12 @@
+
+const cas = require("../../cas.js");
+
+(async () => {
+    const browser = await cas.newBrowser(cas.browserOptions());
+    const page = await cas.newPage(browser);
+    await cas.gotoLogin(page);
+
+    await cas.assertVisibility(page, "#friendlyCaptchaSection");
+
+    await browser.close();
+})();
