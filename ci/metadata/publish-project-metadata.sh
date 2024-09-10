16f0af307f2a6033b1be1d8bf67790905e23f49f
Misagh Moayyed <mm1844@gmail.com>
2024-09-10 08:24:24 +0400

    use native mongodb tooling to publish module metadata [skip ci]

--- ci/metadata/publish-project-metadata.sh
+++ ci/metadata/publish-project-metadata.sh
@@ -1,28 +1,11 @@
 #!/bin/bash
 
-if [[ -z $CAS_PROJECT_DB_URL_ENDPOINT || -z $CAS_PROJECT_DB_API_KEY ]]; then
-  echo "Unable to execute without proper credentials"
-  exit 1
-fi
 
-# Use Httpie to bypass curl limitations on POST parameter size
-function execPostRequestWithHttpie() {
-  action="$1"
-  data="$2"
-  echo "$data" | http -qq POST "${CAS_PROJECT_DB_URL_ENDPOINT}/action/${action}" \
-                          Content-Type:application/json \
-                          api-key:"${CAS_PROJECT_DB_API_KEY}"
-  if [ $? -eq 1 ]; then
-    echo "Unable to publish data"
-    exit 1
-  fi
-  echo -e "Done"
-}
 
 function publishProjectModules() {
-  echo "Publishing CAS modules for CAS version ${casVersion}"
+  echo "Creating CAS module metadata for CAS version ${casVersion}"
   ./gradlew --no-configuration-cache --build-cache --configure-on-demand \
-    --no-daemon --parallel publishProjectModules -x test -x javadoc -x check
+    --no-daemon --parallel generateProjectModulesMetadata -x test -x javadoc -x check
   if [ $? -eq 1 ]; then
     echo "Unable to successfully generate metadata for project modules"
     exit 1
@@ -33,27 +16,25 @@ function publishProjectModules() {
   echo "CAS simple version number is: $versionNumbers"
   collectionName="casmodules$versionNumbers"
   echo "CAS module collection is $collectionName"
-
-  coords="\"collection\":\"$collectionName\",\"database\":\"apereocas\",\"dataSource\":\"cascluster\"";
-
-  echo "Removing previous records for ${casVersion} from ${coords}"
-  execPostRequestWithHttpie "deleteMany" "{${coords},\"filter\":{}}"
-
-  echo "Uploading module records for ${casVersion} to ${coords}"
-  input=$(cat build/modules.json)
-  execPostRequestWithHttpie "insertMany" "{${coords},\"documents\": ${input}}"
+  
+  echo "Removing previous records for ${casVersion}"
+  mongosh "${CAS_MODULE_METADATA_MONGDODB_URL}" --eval "db.$collectionName.deleteMany({})"
+  
+  echo "Uploading module records for ${casVersion} to $collectionName"
+  mongoimport --uri "$CAS_MODULE_METADATA_MONGODB_URL" \
+    --collection "$collectionName" --file build/modules.json --type json
   
   if [ $? -eq 1 ]; then
-    echo "Unable to upload records for CAS version ${casVersion}. Aborting..."
+    echo "Unable to upload module metadata for CAS version ${casVersion}. Aborting..."
     exit 1
   fi
 }
 
-echo "Running Gradle build to determine CAS version..."
-casVersion=$(./gradlew casVersion --no-configuration-cache --no-daemon -q)
-if [ $? -eq 1 ]; then
-  echo "Unable to determine CAS version. Aborting..."
+if [[ -z $CAS_MODULE_METADATA_MONGODB_URL ]]; then
+  echo "Unable to publish module metadata without a URL."
   exit 1
 fi
 
+echo "Checking CAS version..."
+casVersion=$(cat gradle.properties | grep version | awk -F"=" '{printf $2}')
 publishProjectModules
