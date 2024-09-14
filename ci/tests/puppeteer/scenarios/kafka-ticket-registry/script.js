7dec9dce8643c4309083d49815b7d1a95f17fff4
Misagh Moayyed <mm1844@gmail.com>
2024-09-14 13:46:59 +0400

    support apache kafka as ticket registry

---
+++ ci/tests/puppeteer/scenarios/kafka-ticket-registry/script.js
@@ -0,0 +1,37 @@
+
+const cas = require("../../cas.js");
+const assert = require("assert");
+
+(async () => {
+    const browser = await cas.newBrowser(cas.browserOptions());
+    try {
+        const page = await cas.newPage(browser);
+        const response = await cas.gotoLogin(page);
+        await cas.sleep(3000);
+        await cas.log(`${response.status()} ${response.statusText()}`);
+        assert(response.ok());
+
+        await cas.loginWith(page);
+        await cas.sleep(3000);
+        await cas.assertCookie(page);
+        await cas.assertPageTitle(page, "CAS - Central Authentication Service Log In Successful");
+        await cas.assertInnerText(page, "#content div h2", "Log In Successful");
+
+        await cas.sleep(1000);
+        await cas.goto(page, "https://localhost:8444/cas/login");
+        await cas.sleep(2000);
+        await cas.assertCookie(page);
+
+        await cas.gotoLogout(page);
+        await cas.logPage(page);
+        const url = await page.url();
+        assert(url === "https://localhost:8443/cas/logout");
+        await cas.sleep(1000);
+        await cas.assertCookie(page, false);
+        await cas.goto(page, "https://localhost:8444/cas/login");
+        await cas.sleep(1000);
+        await cas.assertCookie(page, false);
+    } finally {
+        await browser.close();
+    }
+})();
