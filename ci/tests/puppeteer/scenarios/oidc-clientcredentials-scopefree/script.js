cbef0a43c55d53f14451f09bc36e291fa5219318
Jérôme LELEU <leleuj@gmail.com>
2024-09-23 10:00:02 +0200

    Complement the oidc-clientcredentials-scopefree CI test

--- ci/tests/puppeteer/scenarios/oidc-clientcredentials-scopefree/script.js
+++ ci/tests/puppeteer/scenarios/oidc-clientcredentials-scopefree/script.js
@@ -6,10 +6,43 @@ const cas = require("../../cas.js");
     let params = "grant_type=client_credentials&";
     params += "scope=openid";
 
-    const url = `https://localhost:8443/cas/oidc/token?${params}`;
-    await cas.log(`Calling ${url}`);
+    const urlOidc = `https://localhost:8443/cas/oidc/token?${params}`;
+    await cas.log(`Calling ${urlOidc}`);
 
-    await cas.doPost(url, "", {
+    await cas.doPost(urlOidc, "", {
+        "Content-Type": "application/json",
+        "Authorization": `Basic ${btoa("client:secret")}`
+    }, async (res) => {
+
+        await cas.log(res.data);
+        assert(res.data.access_token !== null);
+
+        await cas.log("Decoding JWT ID token...");
+        const idTokenDecoded = await cas.decodeJwt(res.data.id_token);
+
+        await cas.log("Decoding JWT access token...");
+        const accessTokenDecoded = await cas.decodeJwt(res.data.access_token);
+
+        assert(res.data.id_token !== undefined);
+        assert(res.data.refresh_token !== undefined);
+        assert(res.data.token_type !== undefined);
+        assert(res.data.scope !== undefined);
+        assert(idTokenDecoded.sub !== undefined);
+        assert(idTokenDecoded.cn !== undefined);
+        assert(idTokenDecoded.name !== undefined);
+        assert(idTokenDecoded["preferred_username"] !== undefined);
+        assert(idTokenDecoded["given-name"] !== undefined);
+        assert(accessTokenDecoded.name !== undefined);
+        assert(accessTokenDecoded.cn !== undefined);
+        assert(accessTokenDecoded["given-name"] !== undefined);
+    }, (error) => {
+        throw `Operation failed: ${error}`;
+    });
+
+    const urlOAuth = `https://localhost:8443/cas/oauth2.0/accessToken?${params}`;
+    await cas.log(`Calling ${urlOAuth}`);
+
+    await cas.doPost(urlOAuth, "", {
         "Content-Type": "application/json",
         "Authorization": `Basic ${btoa("client:secret")}`
     }, async (res) => {
@@ -18,17 +51,15 @@ const cas = require("../../cas.js");
         assert(res.data.access_token !== null);
 
         await cas.log("Decoding JWT access token...");
-        const decoded = await cas.decodeJwt(res.data.id_token);
+        const accessTokenDecoded = await cas.decodeJwt(res.data.access_token);
 
-        assert(res.data.id_token !== null);
-        assert(res.data.refresh_token !== null);
-        assert(res.data.token_type !== null);
-        assert(res.data.scope !== null);
-        assert(decoded.sub !== null);
-        assert(decoded.cn !== null);
-        assert(decoded.name !== null);
-        assert(decoded["preferred_username"] !== null);
-        assert(decoded["given-name"] !== null);
+        assert(res.data.id_token === undefined);
+        assert(res.data.refresh_token !== undefined);
+        assert(res.data.token_type !== undefined);
+        assert(res.data.scope !== undefined);
+        assert(accessTokenDecoded.name !== undefined);
+        assert(accessTokenDecoded.cn !== undefined);
+        assert(accessTokenDecoded["given-name"] !== undefined);
     }, (error) => {
         throw `Operation failed: ${error}`;
     });
