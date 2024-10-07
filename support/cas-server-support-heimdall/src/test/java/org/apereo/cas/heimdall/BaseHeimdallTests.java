7aa8f27d0ba700b3bf8e926ddff08766dc5ac2af
Misagh Moayyed <mm1844@gmail.com>
2024-10-07 10:28:20 +0400

    feat: CAS to act as an authz server - wip

--- support/cas-server-support-heimdall/src/test/java/org/apereo/cas/heimdall/BaseHeimdallTests.java
+++ support/cas-server-support-heimdall/src/test/java/org/apereo/cas/heimdall/BaseHeimdallTests.java
@@ -7,6 +7,7 @@ import org.apereo.cas.config.CasCoreLogoutAutoConfiguration;
 import org.apereo.cas.config.CasCoreMultifactorAuthenticationAutoConfiguration;
 import org.apereo.cas.config.CasCoreMultifactorAuthenticationWebflowAutoConfiguration;
 import org.apereo.cas.config.CasCoreNotificationsAutoConfiguration;
+import org.apereo.cas.config.CasCoreScriptingAutoConfiguration;
 import org.apereo.cas.config.CasCoreServicesAutoConfiguration;
 import org.apereo.cas.config.CasCoreTicketsAutoConfiguration;
 import org.apereo.cas.config.CasCoreWebAutoConfiguration;
@@ -40,7 +41,8 @@ abstract class BaseHeimdallTests {
         CasCoreWebflowAutoConfiguration.class,
         CasCoreWebAutoConfiguration.class,
         CasCoreServicesAutoConfiguration.class,
-
+        CasCoreScriptingAutoConfiguration.class,
+        
         CasThymeleafAutoConfiguration.class,
         CasOAuth20WebflowAutoConfiguration.class,
         CasOidcAutoConfiguration.class,
