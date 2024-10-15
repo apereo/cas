d96cedc11a16c2d65c4e9ce3e6af2b0dc3816b35
Misagh Moayyed <mm1844@gmail.com>
2024-10-15 17:36:47 +0400

    dedicated pwm endpoint to kickstart the psw reset flow

--- support/cas-server-support-reports/src/test/java/org/apereo/cas/web/report/AbstractCasEndpointTests.java
+++ support/cas-server-support-reports/src/test/java/org/apereo/cas/web/report/AbstractCasEndpointTests.java
@@ -104,9 +104,11 @@ public abstract class AbstractCasEndpointTests {
         CasCoreValidationAutoConfiguration.class
     })
     @SpringBootConfiguration(proxyBeanMethods = false)
-    @Import({AbstractCasEndpointTests.AuditTestConfiguration.class,
+    @Import({
+        AbstractCasEndpointTests.AuditTestConfiguration.class,
         CasAuthenticationEventExecutionPlanTestConfiguration.class,
-        CasRegisteredServicesTestConfiguration.class})
+        CasRegisteredServicesTestConfiguration.class
+    })
     public static class SharedTestConfiguration {
     }
 }
