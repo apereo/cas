fdb3dfe3338674b531be28312b8f9adc473fa7cd
Jérôme LELEU <leleuj@gmail.com>
2024-10-28 09:31:08 +0100

    rfeactor the test

--- ci/tests/puppeteer/scenarios/login-double-session/script.js
+++ ci/tests/puppeteer/scenarios/login-double-session/script.js
@@ -5,21 +5,27 @@ const assert = require("assert");
 (async () => {
     const browser = await cas.newBrowser(cas.browserOptions());
     const page = await cas.newPage(browser);
+    // perform a login at the CAS server for the CAS app as casuser
     await cas.goto(page, "https://localhost:8444");
     await cas.sleep(1000);
     await cas.goto(page, "https://localhost:8444/protected");
     await cas.loginWith(page);
     await cas.sleep(1000);
     await cas.logPage(page);
-    const url = await page.url();
+    let url = await page.url();
     assert(url.startsWith("https://localhost:8444/protected"));
     await cas.assertInnerTextContains(page, "div.starter-template h2 span", "casuser");
     await cas.sleep(1000);
-    await cas.gotoLogin(page, "https://localhost:8444/protected", 8443, true);
+    // renew login at the CAS server as admin
+    await cas.gotoLogin(page, undefined, 8443, true);
     await cas.loginWith(page, "admin", "pwd");
     await cas.sleep(2000);
+    // call the CAS app to confirm the new session as admin
+    await cas.goto(page, "https://localhost:8444/protected");
+    await cas.sleep(1000);
+    url = await page.url();
+    assert(url.startsWith("https://localhost:8444/protected"));
     await cas.assertInnerTextContains(page, "div.starter-template h2 span", "admin");
     await cas.sleep(1000);
-    await cas.shutdownCas("https://localhost:8444");
     await browser.close();
 })();
