6d904540aa70363b301b8690814b57f043972fee
Misagh Moayyed <mm1844@gmail.com>
2024-10-30 14:40:24 +0400

    do not rely on profile mgmr to fetch tgt

--- support/cas-server-support-oauth/src/test/java/org/apereo/cas/support/oauth/web/OAuth20TicketGrantingTicketAwareSecurityLogicTests.java
+++ support/cas-server-support-oauth/src/test/java/org/apereo/cas/support/oauth/web/OAuth20TicketGrantingTicketAwareSecurityLogicTests.java
@@ -4,7 +4,7 @@ import org.apereo.cas.AbstractOAuth20Tests;
 import org.apereo.cas.authentication.principal.Principal;
 import org.apereo.cas.mock.MockTicketGrantingTicket;
 import org.apereo.cas.services.RegisteredServiceTestUtils;
-import org.apereo.cas.ticket.TicketGrantingTicket;
+import org.apereo.cas.util.MockRequestContext;
 import org.apereo.cas.validation.Assertion;
 import org.apereo.cas.web.cookie.CasCookieBuilder;
 import lombok.val;
@@ -15,6 +15,8 @@ import org.pac4j.core.profile.BasicUserProfile;
 import org.pac4j.core.profile.ProfileManager;
 import org.pac4j.jee.context.JEEContext;
 import org.pac4j.jee.context.session.JEESessionStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import java.util.List;
@@ -30,6 +32,9 @@ import static org.mockito.Mockito.*;
  */
 @Tag("OAuthToken")
 class OAuth20TicketGrantingTicketAwareSecurityLogicTests extends AbstractOAuth20Tests {
+    @Autowired
+    @Qualifier(CasCookieBuilder.BEAN_NAME_TICKET_GRANTING_COOKIE_BUILDER)
+    private CasCookieBuilder ticketGrantingTicketCookieGenerator;
 
     @Test
     void verifyStatelessOperation() throws Exception {
@@ -44,50 +49,25 @@ class OAuth20TicketGrantingTicketAwareSecurityLogicTests extends AbstractOAuth20
         val context = new JEEContext(request, response);
         val profileManager = new ProfileManager(context, new JEESessionStore());
         profileManager.save(true, profile, false);
-        val logic = new OAuth20TicketGrantingTicketAwareSecurityLogic(mock(CasCookieBuilder.class), ticketRegistry);
+        val logic = new OAuth20TicketGrantingTicketAwareSecurityLogic(ticketGrantingTicketCookieGenerator, ticketRegistry);
         assertFalse(logic.loadProfiles(new CallContext(context, new JEESessionStore()), profileManager, List.of()).isEmpty());
-        
+
     }
 
-    @Test
-    void verifyLoadWithBadTicketInSession() throws Throwable {
-        val request = new MockHttpServletRequest();
-        val response = new MockHttpServletResponse();
-        val context = new JEEContext(request, response);
-        val profileManager = new ProfileManager(context, new JEESessionStore());
-        val profile = new BasicUserProfile();
-        profile.addAttribute(TicketGrantingTicket.class.getName(), UUID.randomUUID().toString());
-        profileManager.save(true, profile, false);
-        val logic = new OAuth20TicketGrantingTicketAwareSecurityLogic(mock(CasCookieBuilder.class), ticketRegistry);
-        assertTrue(logic.loadProfiles(new CallContext(context, new JEESessionStore()), profileManager, List.of()).isEmpty());
-    }
 
     @Test
     void verifyLoadWithValidTicket() throws Throwable {
-        val request = new MockHttpServletRequest();
-        val response = new MockHttpServletResponse();
-        val context = new JEEContext(request, response);
-        val profileManager = new ProfileManager(context, new JEESessionStore());
-        profileManager.save(true, new BasicUserProfile(), false);
-
         val tgt = new MockTicketGrantingTicket(UUID.randomUUID().toString());
-        val profile = new BasicUserProfile();
-        profile.addAttribute(TicketGrantingTicket.class.getName(), tgt.getId());
         ticketRegistry.addTicket(tgt);
-        profileManager.save(true, profile, false);
-        val logic = new OAuth20TicketGrantingTicketAwareSecurityLogic(mock(CasCookieBuilder.class), ticketRegistry);
-        assertFalse(logic.loadProfiles(new CallContext(context, new JEESessionStore()), profileManager, List.of()).isEmpty());
-    }
-
-    @Test
-    void verifyLoadNoProfileWhenNoTgtAvailable() throws Throwable {
-        val request = new MockHttpServletRequest();
-        val response = new MockHttpServletResponse();
-
-        val context = new JEEContext(request, response);
+        val requestContext = MockRequestContext.create(applicationContext).withUserAgent("Chrome");
+        requestContext.setClientInfo();
+        val cookie = ticketGrantingTicketCookieGenerator.addCookie(requestContext.getHttpServletRequest(), requestContext.getHttpServletResponse(), tgt.getId());
+        assertNotNull(cookie);
+        requestContext.setRequestCookiesFromResponse();
+        val context = new JEEContext(requestContext.getHttpServletRequest(), requestContext.getHttpServletResponse());
         val profileManager = new ProfileManager(context, new JEESessionStore());
         profileManager.save(true, new BasicUserProfile(), false);
-        val logic = new OAuth20TicketGrantingTicketAwareSecurityLogic(mock(CasCookieBuilder.class), ticketRegistry);
-        assertTrue(logic.loadProfiles(new CallContext(context, new JEESessionStore()), profileManager, List.of()).isEmpty());
+        val logic = new OAuth20TicketGrantingTicketAwareSecurityLogic(ticketGrantingTicketCookieGenerator, ticketRegistry);
+        assertFalse(logic.loadProfiles(new CallContext(context, new JEESessionStore()), profileManager, List.of()).isEmpty());
     }
 }
