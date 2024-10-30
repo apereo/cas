cef0dd1fc1d83e090703ff24551f07d3f37066a2
Misagh Moayyed <mm1844@gmail.com>
2024-10-30 14:32:57 +0400

    remove undertow support for VTs. now offered by spring boot in 3.4 RC1

--- webapp/cas-server-webapp-undertow/src/main/java/org/apereo/cas/config/CasEmbeddedContainerUndertowAutoConfiguration.java
+++ webapp/cas-server-webapp-undertow/src/main/java/org/apereo/cas/config/CasEmbeddedContainerUndertowAutoConfiguration.java
@@ -3,17 +3,11 @@ package org.apereo.cas.config;
 import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.configuration.features.CasFeatureModule;
 import org.apereo.cas.util.spring.boot.ConditionalOnFeatureEnabled;
-
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnThreading;
-import org.springframework.boot.autoconfigure.thread.Threading;
 import org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.embedded.undertow.UndertowDeploymentInfoCustomizer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
-import java.util.concurrent.Executors;
 
 /**
  * This is {@link CasEmbeddedContainerUndertowAutoConfiguration}.
@@ -26,9 +20,4 @@ import java.util.concurrent.Executors;
 @ConditionalOnFeatureEnabled(feature = CasFeatureModule.FeatureCatalog.Undertow)
 @AutoConfiguration(before = ServletWebServerFactoryAutoConfiguration.class)
 public class CasEmbeddedContainerUndertowAutoConfiguration {
-    @Bean
-    @ConditionalOnThreading(Threading.VIRTUAL)
-    public UndertowDeploymentInfoCustomizer undertowDeploymentInfoCustomizer() {
-        return deploymentInfo -> deploymentInfo.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
-    }
 }
