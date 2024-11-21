apereo/cas

commit 53055665a13378da0908c31165524937ee24fbf7

favor service in the flow over service in http request

core/cas-server-core-services-authentication/src/main/java/org/apereo/cas/authentication/principal/AbstractWebApplicationService.java
@@ -1,14 +1,19 @@
 package org.apereo.cas.authentication.principal;
 
+import org.apereo.cas.util.function.FunctionUtils;
 import org.apereo.cas.util.jpa.MultivaluedMapToJsonAttributeConverter;
 import org.apereo.cas.validation.ValidationResponseType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.hc.core5.net.URIBuilder;
 import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
 import jakarta.persistence.DiscriminatorColumn;
@@ -75,5 +80,30 @@ public abstract class AbstractWebApplicationService implements WebApplicationSer
         this.id = id;
         this.originalUrl = originalUrl;
         this.artifactId = artifactId;
+    }
+
+    @Override
+    @CanIgnoreReturnValue
+    @JsonIgnore
+    public WebApplicationService setFragment(final String fragment) {
+        if (StringUtils.isNotBlank(fragment)) {
+            this.id = collectFragmentFor(this.id, fragment);
+            this.originalUrl = collectFragmentFor(this.originalUrl, fragment);
+        }
+        return this;
+    }
+
+    @Override
+    @JsonIgnore
+    public String getFragment() {
+        return FunctionUtils.doUnchecked(() -> new URIBuilder(this.id).getFragment());
+    }
+
+    private static String collectFragmentFor(final String id, final String fragment) {
+        return FunctionUtils.doUnchecked(() -> new URIBuilder(id)
+            .setFragment(fragment)
+            .build()
+            .toURL()
+            .toExternalForm());
     }
 }

core/cas-server-core-webflow-api/src/main/java/org/apereo/cas/web/flow/resolver/impl/DefaultCasDelegatingWebflowEventResolver.java
@@ -200,12 +200,13 @@ public class DefaultCasDelegatingWebflowEventResolver extends AbstractCasWebflow
     }
 
     protected Service locateServiceForRequest(final RequestContext context) throws Throwable {
-        val serviceFromFlow = WebUtils.getService(context);
         val strategies = getConfigurationContext().getAuthenticationRequestServiceSelectionStrategies();
+        val serviceFromFlow = WebUtils.getService(context);
+        val serviceFromRequest = WebUtils.getService(getConfigurationContext().getArgumentExtractors(), context);
         if (serviceFromFlow == null) {
-            val serviceFromRequest = WebUtils.getService(getConfigurationContext().getArgumentExtractors(), context);
             return strategies.resolveService(serviceFromRequest);
         }
+        serviceFromFlow.setFragment(serviceFromRequest.getFragment());
         return strategies.resolveService(serviceFromFlow);
     }
 }

core/cas-server-core-webflow-mfa/src/test/java/org/apereo/cas/web/flow/resolver/impl/DefaultCasDelegatingWebflowEventResolverTests.java
@@ -54,7 +54,7 @@ class DefaultCasDelegatingWebflowEventResolverTests {
         void verifyAuthFails() throws Throwable {
             val context = MockRequestContext.create(applicationContext);
 
-            val id = UUID.randomUUID().toString();
+            val id = "https://app.%s.org#helloworld".formatted(UUID.randomUUID().toString());
             WebUtils.putServiceIntoFlowScope(context, RegisteredServiceTestUtils.getService(id));
             context.setParameter(CasProtocolConstants.PARAMETER_SERVICE, id);
             val registeredService = RegisteredServiceTestUtils.getRegisteredService(id);
@@ -64,6 +64,10 @@ class DefaultCasDelegatingWebflowEventResolverTests {
             assertEquals(CasWebflowConstants.TRANSITION_ID_AUTHENTICATION_FAILURE, event.getId());
             assertTrue(event.getAttributes().contains(Credential.class.getName()));
             assertTrue(event.getAttributes().contains(WebApplicationService.class.getName()));
+            val service = (WebApplicationService) event.getAttributes().get(WebApplicationService.class.getName());
+            assertNotNull(service);
+            assertEquals(service.getId(), id);
+            assertEquals(service.getOriginalUrl(), id);
         }
 
         @Test

api/cas-server-core-api-authentication/src/main/java/org/apereo/cas/authentication/principal/WebApplicationService.java
@@ -1,6 +1,7 @@
 package org.apereo.cas.authentication.principal;
 
 import org.apereo.cas.validation.ValidationResponseType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 /**
  * Represents a service using CAS that comes from the web.
@@ -53,4 +54,20 @@ public interface WebApplicationService extends Service {
      * @param loggedOutAlready if the service is already logged out.
      */
     void setLoggedOutAlready(boolean loggedOutAlready);
+
+    /**
+     * Collect fragment in web application service.
+     *
+     * @param fragment the fragment
+     * @return the web application service
+     */
+    @JsonIgnore
+    WebApplicationService setFragment(String fragment);
+
+    /**
+     * Gets fragment.
+     *
+     * @return the fragment
+     */
+    String getFragment();
 }

ci/tests/puppeteer/scenarios/login-url-fragment/script.js
@@ -13,8 +13,9 @@ const assert = require("assert");
     await cas.assertTicketParameter(page);
     await cas.logPage(page);
     const url = await page.url();
+    await cas.assertTicketParameter(page);
     assert((url.match(/#/g) || []).length === 1);
-    const result = new URL(page.url());
+    const result = new URL(url);
     await cas.logg(`URL hash is ${result.hash}`);
     assert(result.hash.startsWith("#hello-world"));
     await browser.close();

support/cas-server-support-thymeleaf/src/main/resources/static/js/cas.js
@@ -165,6 +165,7 @@ function preserveAnchorTagOnForm() {
         } else {
             action += location.search + encodeURIComponent(location.hash);
         }
+        console.log(`Preserving URL fragment in form action: ${action}`);
         $("#fm1").attr("action", action);
 
     });

commit 759884591cd450d67ad1b6848946605143758b45

Handle URISyntaxException in setting of service fragment
Fixing a backwards incompatibility regression introduced in https://github.com/apereo/cas/commit/53055665a13378da0908c31165524937ee24fbf7 that was subsequently released as part of 7.1.1 patch release.

Given an example URL, where the service contains query string parameters with illegal characters, or any other means which generate an exception upon parsing into the UriBuilder object:

https://localhost:8443/cas/login?service=https%3A%2F%2Finit.cas.org%2F%3Fquery%3D%2FInvalid%20Format

Prior, CAS would process the request and log the following ERROR:

`ERROR [org.apereo.cas.authentication.principal.AbstractServiceFactory] - <Unable to extract query parameters from [https://init.cas.org/?query=/Invalid Format]: [java.net.URISyntaxException: Illegal character in query at index 36: https://init.cas.org/?query=/Invalid Format]>`

After the patch update, CAS is throwing a 500 Internal Server Error:

`Exception thrown executing org.apereo.cas.web.flow.actions.InitialAuthenticationAction@6b46ca59 in state 'realSubmit' of flow 'login' -- action execution attributes were 'map[[empty]]'`

This PR adds similar logic as the previously logged error defined [here](https://github.com/apereo/cas/blob/master/core/cas-server-core-services-authentication/src/main/java/org/apereo/cas/authentication/principal/AbstractServiceFactory.java#L95-L97).

A test has been included that verifies a service URL that throws URISyntaxException will be caught and processing will continue, thus fixing the regression.

core/cas-server-core-services-authentication/src/main/java/org/apereo/cas/authentication/principal/AbstractWebApplicationService.java
@@ -96,7 +96,7 @@ public abstract class AbstractWebApplicationService implements WebApplicationSer
     @Override
     @JsonIgnore
     public String getFragment() {
-        return FunctionUtils.doUnchecked(() -> new URIBuilder(this.id).getFragment());
+        return FunctionUtils.doAndHandle(() -> new URIBuilder(this.id).getFragment());
     }
 
     private static String collectFragmentFor(final String id, final String fragment) {

core/cas-server-core-webflow-api/src/main/java/org/apereo/cas/web/flow/resolver/impl/DefaultCasDelegatingWebflowEventResolver.java
@@ -207,7 +207,11 @@ public class DefaultCasDelegatingWebflowEventResolver extends AbstractCasWebflow
             return strategies.resolveService(serviceFromRequest);
         }
         if (serviceFromRequest != null) {
-            serviceFromFlow.setFragment(serviceFromRequest.getFragment());
+            val fragment = serviceFromRequest.getFragment();
+
+            if (fragment != null) {
+                serviceFromFlow.setFragment(fragment);
+            }
         }
         return strategies.resolveService(serviceFromFlow);
     }

core/cas-server-core-webflow-mfa/src/test/java/org/apereo/cas/web/flow/resolver/impl/DefaultCasDelegatingWebflowEventResolverTests.java
@@ -22,7 +22,10 @@ import org.springframework.binding.expression.support.LiteralExpression;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.webflow.context.servlet.ServletExternalContext;
 import org.springframework.webflow.engine.Transition;
 import org.springframework.webflow.engine.support.DefaultTargetStateResolver;
 import org.springframework.webflow.engine.support.DefaultTransitionCriteria;
@@ -92,6 +95,25 @@ class DefaultCasDelegatingWebflowEventResolverTests {
             WebUtils.putServiceIntoFlowScope(context, RegisteredServiceTestUtils.getService(id));
             val event = initialAuthenticationAttemptWebflowEventResolver.resolveSingle(context);
             assertEquals(CasWebflowConstants.TRANSITION_ID_AUTHENTICATION_FAILURE, event.getId());
+        }
+
+        @Test
+        void verifyCanCatchUriParsingException() throws Throwable {
+            val context = MockRequestContext.create(applicationContext);
+            val requestContext = new MockHttpServletRequest();
+            requestContext.setParameter("service", "https://init.cas.org/?q=Illegal Character");
+
+            context.setExternalContext(new ServletExternalContext(null, requestContext, new MockHttpServletResponse()));
+
+            val id = UUID.randomUUID().toString();
+            val registeredService = RegisteredServiceTestUtils.getRegisteredService(id);
+            registeredService.setAccessStrategy(new DefaultRegisteredServiceAccessStrategy().setEnabled(false));
+            servicesManager.save(registeredService);
+
+            WebUtils.putServiceIntoFlowScope(context, RegisteredServiceTestUtils.getService(id));
+            WebUtils.putAuthentication(RegisteredServiceTestUtils.getAuthentication(), context);
+            val event = initialAuthenticationAttemptWebflowEventResolver.resolveSingle(context);
+            assertEquals(CasWebflowConstants.TRANSITION_ID_ERROR, event.getId());
         }
     }
 