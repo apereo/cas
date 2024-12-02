apereo/cas

commit 999c8ce8af2b55ce0eebb9c624b5de291e5ff3f2

start cipher/metadata ops in background

core/cas-server-core-util-api/src/main/java/org/apereo/cas/util/cipher/BaseStringCipherExecutor.java
@@ -16,6 +16,7 @@ import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.apache.commons.lang3.StringUtils;
+import org.jooq.lambda.Unchecked;
 import org.jose4j.json.JsonUtil;
 import org.jose4j.jwe.KeyManagementAlgorithmIdentifiers;
 import org.jose4j.jwk.PublicJsonWebKey;
@@ -23,6 +24,7 @@ import java.io.Serializable;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.LinkedHashMap;
+import java.util.concurrent.Executors;
 
 /**
  * The {@link BaseStringCipherExecutor} is the default
@@ -131,16 +133,24 @@ public abstract class BaseStringCipherExecutor extends AbstractCipherExecutor<Se
 
     protected void initialize() {
         if (!initialized) {
-            if (this.encryptionEnabled) {
-                configureEncryptionParameters(secretKeyEncryption, contentEncryptionAlgorithmIdentifier);
-            } else {
-                LOGGER.debug("Encryption is not enabled for [{}]. The cipher [{}] will only attempt to produce signed objects",
-                    getName(), getClass().getSimpleName());
-            }
-            if (this.signingEnabled) {
-                configureSigningParameters(secretKeySigning);
-            } else {
-                LOGGER.info("Signing is not enabled for [{}]. The cipher [{}] will attempt to produce plain objects", getName(), getClass().getSimpleName());
+            try (val executor = Executors.newVirtualThreadPerTaskExecutor()) {
+                val signingCertTask = Unchecked.runnable(() -> {
+                    if (this.signingEnabled) {
+                        configureSigningParameters(secretKeySigning);
+                    } else {
+                        LOGGER.info("Signing is not enabled for [{}]. The cipher [{}] will attempt to produce plain objects", getName(), getClass().getSimpleName());
+                    }
+                });
+                val encryptionCertTask = Unchecked.runnable(() -> {
+                    if (this.encryptionEnabled) {
+                        configureEncryptionParameters(secretKeyEncryption, contentEncryptionAlgorithmIdentifier);
+                    } else {
+                        LOGGER.debug("Encryption is not enabled for [{}]. The cipher [{}] will only attempt to produce signed objects",
+                            getName(), getClass().getSimpleName());
+                    }
+                });
+                executor.execute(signingCertTask);
+                executor.execute(encryptionCertTask);
             }
             this.initialized = true;
         }

core/cas-server-core-util-api/src/main/java/org/apereo/cas/util/cipher/BaseBinaryCipherExecutor.java
@@ -15,6 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.jooq.lambda.Unchecked;
 import org.jose4j.jwk.JsonWebKey;
 import org.jose4j.jwk.OctJwkGenerator;
 
@@ -26,6 +27,7 @@ import javax.crypto.spec.SecretKeySpec;
 
 import java.nio.charset.StandardCharsets;
 import java.security.spec.AlgorithmParameterSpec;
+import java.util.concurrent.Executors;
 
 /**
  * This is {@link BaseBinaryCipherExecutor}.
@@ -72,8 +74,12 @@ public abstract class BaseBinaryCipherExecutor extends AbstractCipherExecutor<by
                                        final int signingKeySize, final int encryptionKeySize,
                                        final String cipherName) {
         this.cipherName = cipherName;
-        ensureSigningKeyExists(signingSecretKey, signingKeySize);
-        ensureEncryptionKeyExists(encryptionSecretKey, encryptionKeySize);
+        try (val executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            val signingCertTask = Unchecked.runnable(() -> ensureSigningKeyExists(signingSecretKey, signingKeySize));
+            val encryptionCertTask = Unchecked.runnable(() -> ensureEncryptionKeyExists(encryptionSecretKey, encryptionKeySize));
+            executor.execute(signingCertTask);
+            executor.execute(encryptionCertTask);
+        }
         this.encryptionKey = new SecretKeySpec(this.encryptionSecretKey, this.secretKeyAlgorithm);
         this.parameterSpec = buildParameterSpec(encryptionKeySize);
     }

support/cas-server-support-saml-idp-core/src/main/java/org/apereo/cas/support/saml/idp/metadata/generator/BaseSamlIdPMetadataGenerator.java
@@ -4,7 +4,6 @@ import org.apereo.cas.support.saml.SamlUtils;
 import org.apereo.cas.support.saml.services.SamlRegisteredService;
 import org.apereo.cas.support.saml.services.idp.metadata.SamlIdPMetadataDocument;
 import org.apereo.cas.util.spring.SpringExpressionLanguageValueResolver;
-
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -14,14 +13,15 @@ import lombok.val;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.velocity.VelocityContext;
+import org.jooq.lambda.Unchecked;
 import org.opensaml.saml.saml2.metadata.EntityDescriptor;
 import org.springframework.core.annotation.AnnotationAwareOrderComparator;
-
 import java.io.Serial;
 import java.io.Serializable;
 import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
+import java.util.concurrent.Executors;
 
 /**
  * A metadata generator based on a predefined template.
@@ -47,21 +47,30 @@ public abstract class BaseSamlIdPMetadataGenerator implements SamlIdPMetadataGen
             if (shouldGenerateMetadata(registeredService)) {
                 LOGGER.trace("Creating metadata artifacts for [{}]...", owner);
 
-                LOGGER.info("Creating self-signed certificate for signing...");
-                val signing = buildSelfSignedSigningCert(registeredService);
-
-                LOGGER.info("Creating self-signed certificate for encryption...");
-                val encryption = buildSelfSignedEncryptionCert(registeredService);
-
-                LOGGER.info("Creating SAML2 metadata for identity provider...");
-                val metadata = buildMetadataGeneratorParameters(signing, encryption, registeredService);
-
                 val doc = newSamlIdPMetadataDocument();
-                doc.setEncryptionCertificate(encryption.getKey());
-                doc.setEncryptionKey(encryption.getValue());
-                doc.setSigningCertificate(signing.getKey());
-                doc.setSigningKey(signing.getValue());
-                doc.setMetadata(metadata);
+                try (val executor = Executors.newVirtualThreadPerTaskExecutor()) {
+                    val signingCertTask = Unchecked.callable(() -> {
+                        LOGGER.info("Creating self-signed certificate for signing...");
+                        return buildSelfSignedSigningCert(registeredService);
+                    });
+                    val encryptionCertTask = Unchecked.callable(() -> {
+                        LOGGER.info("Creating self-signed certificate for encryption...");
+                        return buildSelfSignedEncryptionCert(registeredService);
+                    });
+
+                    val signingFuture = executor.submit(signingCertTask);
+                    val encryptionFuture = executor.submit(encryptionCertTask);
+                    val signing = signingFuture.get();
+                    val encryption = encryptionFuture.get();
+                    LOGGER.info("Creating SAML2 metadata for identity provider...");
+                    val metadata = buildMetadataGeneratorParameters(signing, encryption, registeredService);
+
+                    doc.setEncryptionCertificate(encryption.getKey());
+                    doc.setEncryptionKey(encryption.getValue());
+                    doc.setSigningCertificate(signing.getKey());
+                    doc.setSigningKey(signing.getValue());
+                    doc.setMetadata(metadata);
+                }
                 return finalizeMetadataDocument(doc, registeredService);
             }
             LOGGER.debug("Skipping metadata generation process for [{}]", owner);

commit 4b0f7ff46e2bf4c2d0a28eb9e2b798c72dfa78da

Deadlock Caused by Changes in Commit 999c8ce (05.09.2024)

core/cas-server-core-util-api/src/main/java/org/apereo/cas/util/cipher/BaseStringCipherExecutor.java
@@ -213,6 +213,8 @@ public abstract class BaseStringCipherExecutor extends AbstractCipherExecutor<Se
         configureSigningKey(signingKeyToUse);
     }
 
+// Deadlock detected
+        
     private void configureEncryptionParameters(final String secretKeyEncryption, final String contentEncryptionAlgorithmIdentifier) {
         var secretKeyToUse = secretKeyEncryption;
         if (StringUtils.isBlank(secretKeyToUse)) {