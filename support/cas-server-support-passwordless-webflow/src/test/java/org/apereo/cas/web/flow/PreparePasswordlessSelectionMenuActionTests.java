295dee47b4dc37118e10e1237a340645f26a6080
Misagh Moayyed <mm1844@gmail.com>
2024-12-13 12:30:21 +0400

    chore: update tests

--- support/cas-server-support-passwordless-webflow/src/test/java/org/apereo/cas/web/flow/PreparePasswordlessSelectionMenuActionTests.java
+++ support/cas-server-support-passwordless-webflow/src/test/java/org/apereo/cas/web/flow/PreparePasswordlessSelectionMenuActionTests.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
@@ -25,9 +26,9 @@ import static org.junit.jupiter.api.Assertions.*;
  * @since 7.2.0
  */
 @Tag("WebflowAuthenticationActions")
+@ImportAutoConfiguration(CasDelegatedAuthenticationCasAutoConfiguration.class)
 @Import({
     PreparePasswordlessSelectionMenuActionTests.MultifactorTestConfiguration.class,
-    CasDelegatedAuthenticationCasAutoConfiguration.class,
     BaseWebflowConfigurerTests.SharedTestConfiguration.class
 })
 @TestPropertySource(properties = {
