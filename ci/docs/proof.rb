4046afb1b953f23dda9e1bdb9c4a57db5612b571
Misagh Moayyed <mm1844@gmail.com>
2024-12-17 18:18:19 +0400

    doc updates: [skip ci]

--- ci/docs/proof.rb
+++ ci/docs/proof.rb
@@ -15,6 +15,8 @@ CURRENT_DIR=Dir.pwd
 SOURCE_DIRECTORY="#{CURRENT_DIR}/gh-pages/_site"
  
 
+Typhoeus::Config.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+
 options = {
   :typhoeus => {
     :followlocation => true,
