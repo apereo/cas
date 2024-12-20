5796a47686e34d29df759bb4cb6a58600882e62e
Misagh Moayyed <mm1844@gmail.com>
2024-12-20 20:54:44 +0400

    chore: update tests

--- ci/tests/puppeteer/scenarios/mfa-yubikey-login-groovy-per-service/script.js
+++ ci/tests/puppeteer/scenarios/mfa-yubikey-login-groovy-per-service/script.js
@@ -4,7 +4,7 @@ const cas = require("../../cas.js");
 (async () => {
     const browser = await cas.newBrowser(cas.browserOptions());
     const page = await cas.newPage(browser);
-    await cas.gotoLogin(page, "https://apereo.github.io");
+    await cas.gotoLogin(page, "https://localhost:9859/anything/cas");
     await cas.loginWith(page);
 
     await cas.sleep(1000);
@@ -13,6 +13,6 @@ const cas = require("../../cas.js");
     await cas.assertInvisibility(page, "button[name=register]");
     await cas.type(page, "#token", "12345678901234567890123456789012345");
     await cas.submitForm(page, "#yubiKeyForm", (response) => response.status() === 200);
-    await cas.assertTextContentStartsWith(page, "div .banner-danger span", "Credentials are rejected/invalid");
+    await cas.assertTextContentStartsWith(page, "div .banner-danger span", "Authentication attempt has failed");
     await browser.close();
 })();
