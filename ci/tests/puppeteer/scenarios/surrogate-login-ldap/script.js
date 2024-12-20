bfb6418941a39f360d32996248410dc32efcb1e3
Misagh Moayyed <mm1844@gmail.com>
2024-12-20 14:53:09 +0400

    chore: update tests

---
+++ ci/tests/puppeteer/scenarios/surrogate-login-ldap/script.js
@@ -0,0 +1,32 @@
+const cas = require("../../cas.js");
+const assert = require("assert");
+
+const SERVICE = "https://localhost:9859/anything/cas";
+
+async function impersonate(page, surrogate, user) {
+    await cas.gotoLogin(page, SERVICE);
+    await cas.loginWith(page, `${surrogate}+${user}`);
+    await cas.sleep(1000);
+    const ticket = await cas.assertTicketParameter(page);
+    const body = await cas.doRequest(`https://localhost:8443/cas/p3/serviceValidate?service=${SERVICE}&ticket=${ticket}&format=JSON`);
+    await cas.log(body);
+    const json = JSON.parse(body);
+    const authenticationSuccess = json.serviceResponse.authenticationSuccess;
+    assert(authenticationSuccess.user === surrogate);
+    assert(authenticationSuccess.attributes.cn[0] === surrogate);
+    assert(authenticationSuccess.attributes.uid[0] === surrogate);
+    assert(authenticationSuccess.attributes.mail[0] === `${surrogate}@example.org`);
+    assert(authenticationSuccess.attributes.surrogateUser[0] === surrogate);
+    assert(authenticationSuccess.attributes.surrogateEnabled[0] === true);
+    assert(authenticationSuccess.attributes.surrogatePrincipal[0] === user);
+    await cas.gotoLogout(page);
+}
+
+(async () => {
+    const browser = await cas.newBrowser(cas.browserOptions());
+    const page = await cas.newPage(browser);
+    await impersonate(page, "casperson1", "casuser");
+    await cas.separator();
+    await impersonate(page, "casperson2", "casuser");
+    await browser.close();
+})();
