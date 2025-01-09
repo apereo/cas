a26f25bd13e73fa1a7b183512eeae4c6fafc1d5a
Misagh Moayyed <mm1844@gmail.com>
2025-01-09 12:13:18 +0400

    chore: update tests [skip ci]

--- ci/tests/grouper/run-grouper-server.sh
+++ ci/tests/grouper/run-grouper-server.sh
@@ -9,22 +9,28 @@ docker compose -f $COMPOSE_FILE down >/dev/null 2>/dev/null || true
 docker compose -f $COMPOSE_FILE up -d
 docker compose -f $COMPOSE_FILE logs &
 echo "Waiting for Grouper server to come online..."
-sleep 120
+sleep 60
 docker ps | grep "grouper"
 retVal=$?
 if [ $retVal == 0 ]; then
   max_attempts=5
   attempt=1
-  until curl -k -L -u "GrouperSystem:@4HHXr6SS42@IHz2" --fail https://localhost:7443/grouper; do
+  until curl --connect-timeout 10 -k -L -u "GrouperSystem:@4HHXr6SS42@IHz2" --fail https://localhost:7443/grouper; do
     if [ $attempt -ge $max_attempts ]; then
       echo "Reached maximum attempts ($max_attempts). Exiting."
+      echo "====================================="
       docker ps | grep "grouper"
+      echo "====================================="
+      echo "Grouper core container logs:"
       docker logs grouper-core &
+      echo "====================================="
+      echo "Grouper WS container logs:"
       docker logs grouper-ws &
+      echo "====================================="
       exit 1
     fi
     echo -n '.'
-    sleep 2
+    sleep 5
     attempt=$((attempt + 1))
   done
   echo "Grouper docker container is now running"
