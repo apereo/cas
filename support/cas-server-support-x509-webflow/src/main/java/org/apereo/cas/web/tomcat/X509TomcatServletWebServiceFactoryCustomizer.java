c344f52754badcd1bcdc42022f56c666419f0eae
Misagh Moayyed <mm1844@gmail.com>
2025-01-28 08:34:17 +0400

    feat: allow tomcat x509 connector to set max form-post, request and header sizes

--- support/cas-server-support-x509-webflow/src/main/java/org/apereo/cas/web/tomcat/X509TomcatServletWebServiceFactoryCustomizer.java
+++ support/cas-server-support-x509-webflow/src/main/java/org/apereo/cas/web/tomcat/X509TomcatServletWebServiceFactoryCustomizer.java
@@ -1,11 +1,10 @@
 package org.apereo.cas.web.tomcat;
 
 import org.apereo.cas.configuration.CasConfigurationProperties;
-
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.apache.catalina.connector.Connector;
-import org.apache.coyote.http11.AbstractHttp11JsseProtocol;
+import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.apache.tomcat.util.net.SSLHostConfig;
 import org.apache.tomcat.util.net.SSLHostConfigCertificate;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
@@ -36,16 +35,28 @@ public class X509TomcatServletWebServiceFactoryCustomizer extends ServletWebServ
     public void customize(final ConfigurableServletWebServerFactory factory) {
         val webflow = casProperties.getAuthn().getX509().getWebflow();
         if (factory instanceof final TomcatServletWebServerFactory tomcat && webflow.getPort() > 0) {
-
             LOGGER.debug("Creating X509 configuration for the tomcat container...");
             val connector = new Connector("HTTP/1.1");
             connector.setPort(webflow.getPort());
             connector.setScheme("https");
             connector.setSecure(true);
             connector.setAllowTrace(true);
-            val protocol = (AbstractHttp11JsseProtocol) connector.getProtocolHandler();
-            protocol.setSSLEnabled(true);
+            
+            val maxPostSize = Long.valueOf(serverProperties.getTomcat().getMaxHttpFormPostSize().toBytes());
+            connector.setMaxPostSize(maxPostSize.intValue());
+            LOGGER.debug("Configured max post size for the tomcat connector on port [{}] to be [{}]", webflow.getPort(), maxPostSize);
 
+            val protocol = (AbstractHttp11Protocol) connector.getProtocolHandler();
+            protocol.setSSLEnabled(true);
+            
+            val maxHeaderSize = Long.valueOf(serverProperties.getMaxHttpRequestHeaderSize().toBytes());
+            protocol.setMaxHttpRequestHeaderSize(maxHeaderSize.intValue());
+            LOGGER.debug("Configured max request header size for the tomcat connector on port [{}] to be [{}]", webflow.getPort(), maxHeaderSize);
+            
+            val maxResponseHeader = Long.valueOf(serverProperties.getTomcat().getMaxHttpResponseHeaderSize().toBytes());
+            protocol.setMaxHttpResponseHeaderSize(maxResponseHeader.intValue());
+            LOGGER.debug("Configured max response header size for the tomcat connector on port [{}] to be [{}]", webflow.getPort(), maxResponseHeader);
+            
             val sslHostConfig = new SSLHostConfig();
             sslHostConfig.setSslProtocol("TLS");
             sslHostConfig.setHostName(protocol.getDefaultSSLHostConfigName());
