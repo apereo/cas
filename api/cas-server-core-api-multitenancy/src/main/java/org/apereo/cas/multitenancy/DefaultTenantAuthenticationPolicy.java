832ccebc0e69ff53ddacc275d74572995c0fc24d
Misagh Moayyed <mm1844@gmail.com>
2025-02-04 10:12:50 +0400

    feat: allow tenants to support authentication handlers

---
+++ api/cas-server-core-api-multitenancy/src/main/java/org/apereo/cas/multitenancy/DefaultTenantAuthenticationPolicy.java
@@ -0,0 +1,25 @@
+package org.apereo.cas.multitenancy;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.io.Serial;
+import java.util.List;
+
+/**
+ * This is {@link DefaultTenantAuthenticationPolicy}.
+ *
+ * @author Misagh Moayyed
+ * @since 7.2.0
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+public class DefaultTenantAuthenticationPolicy implements TenantAuthenticationPolicy {
+    @Serial
+    private static final long serialVersionUID = -9012299259747093234L;
+
+    private List<String> authenticationHandlers;
+}
---
+++ core/cas-server-core-services-registry/src/main/java/org/apereo/cas/services/BaseServicesManagerRegisteredServiceLocator.java
@@ -0,0 +1,70 @@
+package org.apereo.cas.services;
+
+import org.apereo.cas.authentication.principal.Service;
+import org.apereo.cas.services.query.BasicRegisteredServiceQueryIndex;
+import org.apereo.cas.services.query.RegisteredServiceQueryAttribute;
+import org.apereo.cas.services.query.RegisteredServiceQueryIndex;
+import org.apereo.cas.util.CollectionUtils;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.val;
+import org.apache.commons.lang3.tuple.Pair;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.BiPredicate;
+
+/**
+ * This is {@link BaseServicesManagerRegisteredServiceLocator}.
+ *
+ * @author Misagh Moayyed
+ * @since 6.3.0
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public abstract class BaseServicesManagerRegisteredServiceLocator implements ServicesManagerRegisteredServiceLocator {
+    
+    private int order = DEFAULT_ORDER;
+
+    private BiPredicate<RegisteredService, Service> registeredServiceFilter = (registeredService, service) -> {
+        val supportedType = supports(registeredService, service);
+        return supportedType && registeredService.matches(service.getId());
+    };
+
+    @Override
+    public RegisteredService locate(final Collection<? extends RegisteredService> candidates, final Service service) {
+        return candidates
+            .stream()
+            .filter(registeredService -> supports(registeredService, service))
+            .filter(registeredService -> registeredServiceFilter.test(registeredService, service))
+            .findFirst()
+            .orElse(null);
+    }
+
+    @Override
+    public boolean supports(final RegisteredService registeredService, final Service service) {
+        val serviceType = getRegisteredServiceIndexedType();
+        return serviceType.getValue().isAssignableFrom(registeredService.getClass())
+                && registeredService.getFriendlyName().equalsIgnoreCase(serviceType.getKey());
+    }
+
+    @Override
+    public List<RegisteredServiceQueryIndex> getRegisteredServiceIndexes() {
+        val registeredServiceIndexedType = getRegisteredServiceIndexedType().getValue();
+        return CollectionUtils.wrapArrayList(BasicRegisteredServiceQueryIndex.hashIndex(
+                new RegisteredServiceQueryAttribute(registeredServiceIndexedType, long.class, "id")),
+            BasicRegisteredServiceQueryIndex.hashIndex(
+                new RegisteredServiceQueryAttribute(registeredServiceIndexedType, String.class, "name")),
+            BasicRegisteredServiceQueryIndex.hashIndex(
+                new RegisteredServiceQueryAttribute(registeredServiceIndexedType, String.class, "serviceId")),
+            BasicRegisteredServiceQueryIndex.hashIndex(
+                new RegisteredServiceQueryAttribute(registeredServiceIndexedType, String.class, "friendlyName")),
+            BasicRegisteredServiceQueryIndex.hashIndex(
+                new RegisteredServiceQueryAttribute(registeredServiceIndexedType, String.class, "@class")));
+    }
+    
+    protected abstract Pair<String, Class<? extends RegisteredService>> getRegisteredServiceIndexedType();
+}
