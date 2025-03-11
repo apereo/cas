8bdff33fdd27cd4b3e5ecb153022e797bc38808d
Misagh Moayyed <mm1844@gmail.com>
2025-03-11 11:59:36 +0400

    fix: allow interrupt external redirects when flow is blocked

--- ci/tests/puppeteer/scenarios/interrupt-afterauthn-groovy/script.js
+++ ci/tests/puppeteer/scenarios/interrupt-afterauthn-groovy/script.js
@@ -1,11 +1,12 @@
 
 const cas = require("../../cas.js");
+const assert = require("assert");
 
 (async () => {
     const browser = await cas.newBrowser(cas.browserOptions());
+    
     const page = await cas.newPage(browser);
     await cas.gotoLogin(page);
-
     await cas.loginWith(page, "testuser", "testuser");
     await cas.assertTextContent(page, "#content h1", "Authentication Interrupt");
     await cas.sleep(1000);
@@ -16,5 +17,18 @@ const cas = require("../../cas.js");
     await cas.assertCookie(page);
     await cas.sleep(1000);
     await cas.assertInnerText(page, "#content div h2", "Log In Successful");
+    await cas.gotoLogout(page);
+
+    await cas.gotoLogin(page);
+    await cas.loginWith(page, "blockuser", "blockuser");
+    await cas.sleep(1000);
+    await cas.assertInvisibility(page, "#proceed");
+    await cas.click(page, "#link1");
+    await cas.sleep(2000);
+    await cas.logPage(page);
+    const url = await page.url();
+    assert(url.includes("https://apereo.github.io"));
+    await cas.gotoLogout(page);
+    
     await browser.close();
 })();
