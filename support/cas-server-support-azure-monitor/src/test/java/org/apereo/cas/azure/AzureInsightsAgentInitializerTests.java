c5eeb384d97b18c07c13e7c7d6715c9ebc3933c4
Misagh Moayyed <mm1844@gmail.com>
2025-03-13 19:08:36 +0400

    feat: support azure data explorer kusto for logging

--- support/cas-server-support-azure-monitor/src/test/java/org/apereo/cas/azure/AzureInsightsAgentInitializerTests.java
+++ support/cas-server-support-azure-monitor/src/test/java/org/apereo/cas/azure/AzureInsightsAgentInitializerTests.java
@@ -1,13 +1,12 @@
 package org.apereo.cas.azure;
 
-import lombok.val;
-import org.apache.commons.lang3.ArrayUtils;
 import org.apereo.cas.test.CasTestExtension;
 import org.apereo.cas.util.app.ApplicationEntrypointInitializer;
+import lombok.val;
+import org.apache.commons.lang3.ArrayUtils;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability;
 import java.util.ServiceLoader;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,7 +18,6 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 @Tag("Simple")
 @ExtendWith(CasTestExtension.class)
-@AutoConfigureObservability
 class AzureInsightsAgentInitializerTests {
     static {
         System.setProperty(AzureInsightsAgentInitializer.AZURE_MONITOR_AGENT_ENABLED, "yes");
