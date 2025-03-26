b09dfa76ff4fb8f54cdc78c7c99f0fde08827228
Misagh Moayyed <mm1844@gmail.com>
2025-03-26 09:47:39 +0400

    fix tests

--- ci/tests/kafka/run-kafka-server.sh
+++ ci/tests/kafka/run-kafka-server.sh
@@ -8,11 +8,12 @@ test -f $COMPOSE_FILE || COMPOSE_FILE=docker-compose.yml
 docker compose -f $COMPOSE_FILE up -d
 docker compose -f $COMPOSE_FILE logs &
 sleep 10
-docker ps
-COUNT_KAFKA=$(docker ps --format "{{.ID}}: {{.Names}}" | grep -c "kafka")
-if [[ ${COUNT_KAFKA} -eq 2 ]]; then
-    echo "Kafka docker containers are running."
+docker ps | grep "kafka-server"
+retVal=$?
+if [ $retVal == 0 ]; then
+    echo "Kafka docker container is running."
 else
-    echo "Kafka docker containers failed to start."
-    exit 1
+    echo "Kafka docker container failed to start."
+    exit $retVal
 fi
+
