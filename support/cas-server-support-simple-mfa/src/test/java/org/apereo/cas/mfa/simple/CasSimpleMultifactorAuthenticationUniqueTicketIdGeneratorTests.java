a68447a37b01cc3ff3873d7a08c09b2806c8713a
Misagh Moayyed <mm1844@gmail.com>
2025-04-14 09:48:24 +0400

    update tests

--- support/cas-server-support-simple-mfa/src/test/java/org/apereo/cas/mfa/simple/CasSimpleMultifactorAuthenticationUniqueTicketIdGeneratorTests.java
+++ support/cas-server-support-simple-mfa/src/test/java/org/apereo/cas/mfa/simple/CasSimpleMultifactorAuthenticationUniqueTicketIdGeneratorTests.java
@@ -1,11 +1,16 @@
 package org.apereo.cas.mfa.simple;
 
-import org.apereo.cas.mfa.simple.ticket.CasSimpleMultifactorAuthenticationUniqueTicketIdGenerator;
-
+import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.test.CasTestExtension;
+import org.apereo.cas.ticket.UniqueTicketIdGenerator;
 import lombok.val;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -14,11 +19,19 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author Misagh Moayyed
  * @since 6.1.0
  */
+@SpringBootTest(classes = BaseCasSimpleMultifactorAuthenticationTests.SharedTestConfiguration.class)
+@EnableConfigurationProperties(CasConfigurationProperties.class)
 @Tag("MFAProvider")
+@ExtendWith(CasTestExtension.class)
 class CasSimpleMultifactorAuthenticationUniqueTicketIdGeneratorTests {
+
+    @Autowired
+    @Qualifier("casSimpleMultifactorAuthenticationUniqueTicketIdGenerator")
+    private UniqueTicketIdGenerator casSimpleMultifactorAuthenticationUniqueTicketIdGenerator;
+
     @Test
-    void verifyOperation() {
-        val gen = new CasSimpleMultifactorAuthenticationUniqueTicketIdGenerator(6);
-        assertNotNull(gen.getNewTicketId("CAS"));
+    void verifyOperation() throws Throwable {
+        val token = casSimpleMultifactorAuthenticationUniqueTicketIdGenerator.getNewTicketId("CAS");
+        assertNotNull(token);
     }
 }
