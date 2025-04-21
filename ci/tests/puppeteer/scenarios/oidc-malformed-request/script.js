095497298828d974c99a1c7fbb8c658500966420
Misagh Moayyed <mm1844@gmail.com>
2025-04-21 13:43:53 +0400

    minor polish

--- ci/tests/puppeteer/scenarios/oidc-malformed-request/script.js
+++ ci/tests/puppeteer/scenarios/oidc-malformed-request/script.js
@@ -5,17 +5,20 @@ const assert = require("assert");
 (async () => {
     const browser = await cas.newBrowser(cas.browserOptions());
     const page = await cas.newPage(browser);
-    let response = await cas.gotoLogin(page, "https://upgrade.badssl.com");
+
+    const service = "https://localhost:9859/anything/unknown";
+
+    let response = await cas.gotoLogin(page, service);
     await cas.sleep(1000);
     await cas.log(`${response.status()} ${response.statusText()}`);
     assert(response.status() === 403);
 
-    response = await cas.gotoLogin(page, "https://upgrade.badssl.com&client_id=client");
+    response = await cas.gotoLogin(page, `${service}&client_id=client`);
     await cas.log(`${response.status()} ${response.statusText()}`);
     assert(response.status() === 403);
 
     await cas.log("Checking for unauthorized logout redirect...");
-    response = await cas.goto(page, "https://localhost:8443/cas/logout?client_id=client&service=https://upgrade.badssl.com");
+    response = await cas.goto(page, `https://localhost:8443/cas/logout?client_id=client&service=${service}`);
     await cas.sleep(1000);
     await cas.log(`${response.status()} ${response.statusText()}`);
     assert(response.status() === 403);
