9f36eb63a20a3c2db9ace11675e7574e4f68792f
Misagh Moayyed <mm1844@gmail.com>
2025-04-23 17:22:47 +0400

    update wsfed tests

--- ci/tests/puppeteer/scenarios/wsfed-idp-login/init.sh
+++ ci/tests/puppeteer/scenarios/wsfed-idp-login/init.sh
@@ -3,7 +3,7 @@ echo "Running Fediz sample client web application using keystore ${CAS_KEYSTORE}
 docker run --rm -d -p9876:9876 -p5008:5008 -p8976:8076 \
   -v "${CAS_KEYSTORE}":/etc/cas/thekeystore \
   -e SP_SSL_KEYSTORE_PATH="/etc/cas/thekeystore"\
-  --name "fediz" apereo/fediz-client-webapp
+  --name "fediz" apereo/fediz-client-webapp:2025
 docker logs -f fediz &
 sleep 15
 docker ps | grep "fediz"
