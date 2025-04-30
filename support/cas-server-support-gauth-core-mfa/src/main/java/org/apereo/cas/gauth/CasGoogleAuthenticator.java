e8a8c432dfea5cd54661b1971ac6e30dc5676d78
Misagh Moayyed <mm1844@gmail.com>
2025-04-30 10:53:21 +0400

    minor polish

--- support/cas-server-support-gauth-core-mfa/src/main/java/org/apereo/cas/gauth/CasGoogleAuthenticator.java
+++ support/cas-server-support-gauth-core-mfa/src/main/java/org/apereo/cas/gauth/CasGoogleAuthenticator.java
@@ -18,12 +18,14 @@ public interface CasGoogleAuthenticator {
     String BEAN_NAME = "googleAuthenticatorInstance";
 
     /**
+     * Wrapper for {@link IGoogleAuthenticator#createCredentials()}.
      * @return secret key
      * @see IGoogleAuthenticator#createCredentials()
      */
     GoogleAuthenticatorKey createCredentials();
 
     /**
+     * Wrapper for {@link IGoogleAuthenticator#createCredentials(String)}.
      * @param userName the user name.
      * @return secret key
      * @see IGoogleAuthenticator#createCredentials(String)
@@ -32,6 +34,7 @@ public interface CasGoogleAuthenticator {
 
 
     /**
+     * Wrapper for {@link IGoogleAuthenticator#authorize(String, int)}.
      * @param secret           the encoded secret key.
      * @param verificationCode the verification code.
      * @return {@code true} if the validation code is valid.
@@ -40,12 +43,14 @@ public interface CasGoogleAuthenticator {
     boolean authorize(String secret, int verificationCode);
 
     /**
+     * Wrapper for {@link IGoogleAuthenticator#getCredentialRepository()}.
      * @return the credential repository used by this instance.
      * @see IGoogleAuthenticator#getCredentialRepository()
      */
     ICredentialRepository getCredentialRepository();
 
     /**
+     * Sets the credential repository to use.
      * @param repository The credential repository to use, or {@code null} to
      *                   disable this feature.
      * @see IGoogleAuthenticator#setCredentialRepository(ICredentialRepository)
@@ -53,6 +58,7 @@ public interface CasGoogleAuthenticator {
     void setCredentialRepository(ICredentialRepository repository);
 
     /**
+     * Wrapper for {@link IGoogleAuthenticator#getTotpPassword(String)}.
      * @param secret the encoded secret key.
      * @return the current TOTP password.
      * @see IGoogleAuthenticator#getTotpPassword(String)
