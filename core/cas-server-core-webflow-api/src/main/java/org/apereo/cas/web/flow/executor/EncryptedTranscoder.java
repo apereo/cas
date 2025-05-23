e9c359e35dad0369b1f3058451933689b0a44e82
Misagh Moayyed <mm1844@gmail.com>
2025-05-23 11:41:04 +0400

    fix stye issues

--- core/cas-server-core-webflow-api/src/main/java/org/apereo/cas/web/flow/executor/EncryptedTranscoder.java
+++ core/cas-server-core-webflow-api/src/main/java/org/apereo/cas/web/flow/executor/EncryptedTranscoder.java
@@ -50,18 +50,19 @@ public class EncryptedTranscoder implements Transcoder {
         if (o == null) {
             return ArrayUtils.EMPTY_BYTE_ARRAY;
         }
-        try (val outBuffer = new ByteArrayOutputStream();
-             val out = this.compression
-                 ? new ObjectOutputStream(new GZIPOutputStream(outBuffer))
-                 : new ObjectOutputStream(outBuffer)) {
+        try (val outBuffer = new ByteArrayOutputStream()) {
+            try (val out = this.compression
+                ? new ObjectOutputStream(new GZIPOutputStream(outBuffer))
+                : new ObjectOutputStream(outBuffer)) {
 
-            writeObjectToOutputStream(o, out);
+                writeObjectToOutputStream(o, out);
+            } catch (final NotSerializableException e) {
+                LoggingUtils.warn(LOGGER, e);
+            }
             return encrypt(outBuffer);
-        } catch (final NotSerializableException e) {
-            LoggingUtils.warn(LOGGER, e);
-            return ArrayUtils.EMPTY_BYTE_ARRAY;
         }
     }
+
 
     @Override
     @SuppressWarnings("BanSerializableRead")
