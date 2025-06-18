9dc1f261d24d8997b99e2eed9ca8862d964889db
CAS in the cloud LELEU Jérôme <leleuj@gmail.com>
2025-06-18 17:34:26 +0200

    Improve WebAuthn test coverage
    This PR tests the WebAuthn QR code authentication.

---
+++ ci/tests/puppeteer/scenarios/mfa-webauthn-register-qr-login/script.js
@@ -0,0 +1,54 @@
+const cas = require("../../cas.js");
+
+(async () => {
+    const browser = await cas.newBrowser(cas.browserOptions());
+    const page = await cas.newPage(browser);
+
+    const virtualAuthenticator = await cas.createWebAuthnVirtualAuthenticator(page);
+
+    await cas.gotoLoginWithAuthnMethod(page, undefined, "mfa-webauthn");
+
+    await cas.loginWith(page);
+    await cas.sleep(1000);
+    await cas.assertTextContent(page, "#status", "Register Device");
+    await cas.assertVisibility(page, "#messages");
+    await cas.assertInvisibility(page, "#device-info");
+    await cas.assertInvisibility(page, "#device-icon");
+    await cas.assertInvisibility(page, "#device-name");
+    await cas.assertVisibility(page, "#credentialNickname");
+    await cas.assertVisibility(page, "#registerButton");
+    await cas.assertVisibility(page, "#residentKeysPanel");
+    await cas.assertVisibility(page, "#registerDiscoverableCredentialButton");
+
+    await page.click("#credentialNickname", {clickCount: 3});
+    await cas.pressBackspace(page);
+    await page.type("#credentialNickname", "mydevice");
+
+    await cas.click(page,"#registerButton");
+
+    await cas.sleep(5000);
+
+    await cas.click(page, "#qrCodeButton");
+
+    await cas.sleep(1000);
+
+    await cas.click(page, "#authnButton");
+
+    await cas.sleep(5000);
+
+    const pages = await browser.pages();
+    await pages[1].close();
+
+    await cas.assertCookie(page);
+    await cas.assertPageTitle(page, "CAS - Central Authentication Service Log In Successful");
+    await cas.assertInnerText(page, "#content div h2", "Log In Successful");
+    await cas.click(page, "#auth-tab");
+    await cas.sleep(1000);
+    await cas.assertInnerTextContains(page, "#attribute-tab-1 table#attributesTable tbody", "[WebAuthnAuthenticationHandler]");
+    await cas.assertInnerTextContains(page, "#attribute-tab-1 table#attributesTable tbody", "[mfa-webauthn]");
+    await cas.assertInnerTextContains(page, "#attribute-tab-1 table#attributesTable tbody", "[WebAuthnCredential]");
+
+    await cas.sleep(2000);
+    await cas.removeWebAuthnVirtualAuthenticator(virtualAuthenticator);
+    await browser.close();
+})();
