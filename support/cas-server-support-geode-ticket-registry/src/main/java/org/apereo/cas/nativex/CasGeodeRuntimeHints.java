7865fadba8ac49dbc3e1bfe44688a9f46ad5bd98
Misagh Moayyed <mm1844@gmail.com>
2025-06-18 12:10:53 +0400

    chore: allow cas logo to be displayed via theme

--- support/cas-server-support-geode-ticket-registry/src/main/java/org/apereo/cas/nativex/CasGeodeRuntimeHints.java
+++ support/cas-server-support-geode-ticket-registry/src/main/java/org/apereo/cas/nativex/CasGeodeRuntimeHints.java
@@ -2,7 +2,9 @@ package org.apereo.cas.nativex;
 
 import org.apereo.cas.ticket.registry.GeodeTicketDocument;
 import org.apereo.cas.util.nativex.CasRuntimeHintsRegistrar;
+import lombok.val;
 import org.apache.geode.internal.cache.InternalCacheBuilder;
+import org.apache.geode.internal.serialization.DataSerializableFixedID;
 import org.apache.geode.logging.internal.log4j.api.LogService;
 import org.springframework.aot.hint.RuntimeHints;
 
@@ -16,7 +18,13 @@ public class CasGeodeRuntimeHints implements CasRuntimeHintsRegistrar {
     @Override
     public void registerHints(final RuntimeHints hints, final ClassLoader classLoader) {
         registerReflectionHints(hints, LogService.class);
+
+        val subclasses = findSubclassesOf(DataSerializableFixedID.class);
+        registerReflectionHints(hints, subclasses);
+        registerSerializationHints(hints, subclasses);
+
         registerSerializationHints(hints, GeodeTicketDocument.class);
         registerSerializationHints(hints, InternalCacheBuilder.class);
+
     }
 }
