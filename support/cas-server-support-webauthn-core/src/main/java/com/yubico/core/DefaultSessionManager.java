35786755dbc78ece45136f3fd61d0ffaaf3fca9b
Jérôme LELEU <leleuj@gmail.com>
2025-06-24 09:00:33 +0200

    clean class

--- support/cas-server-support-webauthn-core/src/main/java/com/yubico/core/DefaultSessionManager.java
+++ support/cas-server-support-webauthn-core/src/main/java/com/yubico/core/DefaultSessionManager.java
@@ -3,13 +3,10 @@ package com.yubico.core;
 import com.yubico.webauthn.data.ByteArray;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import lombok.val;
 
 import java.util.Optional;
 
 @RequiredArgsConstructor
-@Slf4j
 public class DefaultSessionManager implements SessionManager {
     private final WebSessionWebAuthnCache<ByteArray> sessionIdsToUsers;
 
@@ -18,7 +15,6 @@ public class DefaultSessionManager implements SessionManager {
     @Override
     public ByteArray createSession(@NonNull final ByteArray userHandle) {
         var sessionId = usersToSessionIds.get(userHandle, __ -> SessionManager.generateRandom(32));
-        LOGGER.debug("createSession / [{}] -> [{}]", userHandle.getBase64(), sessionId.getBase64());
         sessionIdsToUsers.put(sessionId, userHandle);
         return sessionId;
     }
@@ -26,8 +22,6 @@ public class DefaultSessionManager implements SessionManager {
     @Override
     public Optional<ByteArray> getSession(
         @NonNull final ByteArray sessionId) {
-        val o = Optional.ofNullable(sessionIdsToUsers.getIfPresent(sessionId));
-        LOGGER.debug("getSession / [{}] -> [{}]", sessionId.getBase64(), o);
-        return o;
+        return Optional.ofNullable(sessionIdsToUsers.getIfPresent(sessionId));
     }
 }
