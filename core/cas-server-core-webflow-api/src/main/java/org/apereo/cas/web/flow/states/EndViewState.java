264d3d59ec32e7233176cd1a0ebe4220d4c834cf
Misagh Moayyed <mm1844@gmail.com>
2025-07-15 13:08:14 +0400

    fix: update tests

--- core/cas-server-core-webflow-api/src/main/java/org/apereo/cas/web/flow/states/EndViewState.java
+++ core/cas-server-core-webflow-api/src/main/java/org/apereo/cas/web/flow/states/EndViewState.java
@@ -3,11 +3,15 @@ package org.apereo.cas.web.flow.states;
 import org.apereo.cas.util.function.FunctionUtils;
 import lombok.Setter;
 import lombok.val;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.webflow.action.ExternalRedirectAction;
 import org.springframework.webflow.core.collection.LocalAttributeMap;
 import org.springframework.webflow.engine.Flow;
 import org.springframework.webflow.engine.RequestControlContext;
 import org.springframework.webflow.engine.ViewState;
+import org.springframework.webflow.execution.Action;
 import org.springframework.webflow.execution.FlowExecutionException;
+import org.springframework.webflow.execution.RequestContext;
 import org.springframework.webflow.execution.ViewFactory;
 
 /**
@@ -36,21 +40,41 @@ public class EndViewState extends ViewState {
     protected void doEnter(final RequestControlContext context) throws FlowExecutionException {
         try {
             super.doEnter(context);
-            if (forceRenderView) {
+            if (shouldRenderView(context)) {
                 val view = getViewFactory().getView(context);
-                if (view != null) {
-                    context.setCurrentView(view);
-                    context.viewRendering(view);
-                    getRenderActionList().execute(context);
-                    FunctionUtils.doAndHandle(__ -> view.render());
-                    context.getFlashScope().clear();
-                    context.getMessageContext().clearMessages();
-                    context.getExternalContext().recordResponseComplete();
-                    context.viewRendered(view);
-                }
+                context.setCurrentView(view);
+                context.viewRendering(view);
+                getRenderActionList().execute(context);
+                FunctionUtils.doAndHandle(__ -> view.render());
+                context.getFlashScope().clear();
+                context.getMessageContext().clearMessages();
+                context.getExternalContext().recordResponseComplete();
+                context.viewRendered(view);
             }
         } finally {
             context.endActiveFlowSession(this.getId(), new LocalAttributeMap<>());
         }
+    }
+
+    /**
+     * Decide if the view should be forcefully rendered.
+     * The view will be rendered if the view is not an
+     * {@link ExternalRedirectAction}, meaning we are not set
+     * to redirect to an external url after the state is done.
+     *
+     * @param context the context
+     * @return true/false
+     */
+    protected boolean shouldRenderView(final RequestContext context) {
+        if (forceRenderView) {
+            val field = ReflectionUtils.findField(context.getCurrentView().getClass(), "action");
+            if (field != null) {
+                field.trySetAccessible();
+                val action = (Action) ReflectionUtils.getField(field, context.getCurrentView());
+                return !(action instanceof ExternalRedirectAction);
+            }
+            return true;
+        }
+        return false;
     }
 }
