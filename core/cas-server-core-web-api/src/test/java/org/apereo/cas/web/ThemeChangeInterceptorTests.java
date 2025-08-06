123a7290d76386eb7b7249f0b41b450a70451a3c
Misagh Moayyed <mm1844@gmail.com>
2025-08-06 12:25:12 +0400

    fix: move theme interceptor out of spring namespace

--- core/cas-server-core-web-api/src/test/java/org/apereo/cas/web/ThemeChangeInterceptorTests.java
+++ core/cas-server-core-web-api/src/test/java/org/apereo/cas/web/ThemeChangeInterceptorTests.java
@@ -1,10 +1,12 @@
-package org.springframework.web.servlet.theme;
+package org.apereo.cas.web;
 
+import org.apereo.cas.web.support.ThemeChangeInterceptor;
 import lombok.val;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.web.servlet.theme.SessionThemeResolver;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -19,7 +21,7 @@ class ThemeChangeInterceptorTests {
     @Test
     void verifyOperation() throws Exception {
         val themeResolver = new SessionThemeResolver();
-        val interceptor = new ThemeChangeInterceptor(themeResolver, "theme");
+        val interceptor = new org.apereo.cas.web.support.ThemeChangeInterceptor(themeResolver, "theme");
         val request = new MockHttpServletRequest();
         request.setParameter(ThemeChangeInterceptor.DEFAULT_PARAM_NAME, "cas");
         assertTrue(interceptor.preHandle(request, new MockHttpServletResponse(), new Object()));
