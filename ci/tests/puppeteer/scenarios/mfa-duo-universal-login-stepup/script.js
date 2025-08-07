d1f0aa8c4f2b7136934585d83c1bfc6a644c720c
Misagh Moayyed <mm1844@gmail.com>
2025-08-07 18:56:12 +0400

    chore: polish test

--- ci/tests/puppeteer/scenarios/mfa-duo-universal-login-stepup/script.js
+++ ci/tests/puppeteer/scenarios/mfa-duo-universal-login-stepup/script.js
@@ -5,24 +5,27 @@ const cas = require("../../cas.js");
     const browser = await cas.newBrowser(cas.browserOptions({ args: [ "--accept-lang=de", "--lang=de"] }));
     const page = await cas.newPage(browser);
 
-    const stepUpUrl = "https://localhost:8443/cas/login?service=https://localhost:9859/anything/1&authn_method=mfa-duo";
+    const service = "https://localhost:9859/anything/1";
 
     await cas.log("Establish SSO session");
     await cas.gotoLogin(page);
     await cas.updateDuoSecurityUserStatus("duocode");
+    await cas.sleep(2000);
     await cas.loginWith(page, "duocode", "Mellon");
+    await cas.sleep(2000);
     await cas.assertCookie(page);
 
     await cas.log("Force SSO session to step up with Duo MFA");
-    await cas.goto(page, stepUpUrl);
+    await cas.gotoLoginWithAuthnMethod(page, service, "mfa-duo");
     await cas.sleep(4000);
 
     await cas.log("Abandon MFA and go back to CAS to check for SSO");
     await cas.gotoLogin(page);
+    await cas.sleep(1000);
     await cas.assertCookie(page);
 
     await cas.log("Repeat: force SSO session to step up with Duo MFA");
-    await cas.goto(page, stepUpUrl);
+    await cas.gotoLoginWithAuthnMethod(page, service, "mfa-duo");
     await cas.sleep(6000);
     await cas.loginDuoSecurityBypassCode(page, "duocode");
     await cas.sleep(6000);
