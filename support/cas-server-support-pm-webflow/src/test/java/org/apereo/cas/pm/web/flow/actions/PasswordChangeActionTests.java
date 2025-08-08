c9e9d323e88901040e0f9a077ed088c54673eb82
alberto bogi <35602311+alberto-bogi@users.noreply.github.com>
2025-08-08 18:14:19 +0200

    Input for current password in password management operation (#7333)
    Co-authored-by: alberto bogi <alberto.bogi@tirasa.net>
    Co-authored-by: Misagh Moayyed <mm1844@gmail.com>

--- support/cas-server-support-pm-webflow/src/test/java/org/apereo/cas/pm/web/flow/actions/PasswordChangeActionTests.java
+++ support/cas-server-support-pm-webflow/src/test/java/org/apereo/cas/pm/web/flow/actions/PasswordChangeActionTests.java
@@ -104,4 +104,21 @@ class PasswordChangeActionTests extends BasePasswordManagementActionTests {
         assertEquals(CasWebflowConstants.TRANSITION_ID_ERROR, passwordChangeAction.execute(context).getId());
     }
 
+    @Test
+    void verifyCurrentPasswordWrong() throws Throwable {
+        val context = MockRequestContext.create(applicationContext);
+
+        val credential = RegisteredServiceTestUtils.getCredentialsWithDifferentUsernameAndPassword("error-credential", "P@ssword");
+        WebUtils.putCredential(context, credential);
+
+        val changeReq = new PasswordChangeRequest();
+        changeReq.setUsername("bad-credential");
+        changeReq.setPassword("P@ssword".toCharArray());
+        changeReq.setConfirmedPassword("P@ssword".toCharArray());
+        changeReq.setCurrentPassword("B@dP@ssword".toCharArray());
+        context.getFlowScope().put(PasswordManagementWebflowConfigurer.FLOW_VAR_ID_PASSWORD, changeReq);
+        PasswordManagementWebflowUtils.putPasswordResetUsername(context, changeReq.getUsername());
+        assertEquals(CasWebflowConstants.TRANSITION_ID_ERROR, passwordChangeAction.execute(context).getId());
+    }
+
 }
