ff6ef86610387221af1e87da8093f75ee6d94411
Misagh Moayyed <mm1844@gmail.com>
2025-08-12 13:41:28 +0400

    chore: polish build when logback is included automatically

--- ci/tests/puppeteer/scenarios/logging-with-logback/script.js
+++ ci/tests/puppeteer/scenarios/logging-with-logback/script.js
@@ -6,9 +6,7 @@ const assert = require("assert");
     const browser = await cas.newBrowser(cas.browserOptions());
     const page = await cas.newPage(browser);
     await cas.gotoLogin(page);
-
     await cas.loginWith(page);
-
     await cas.assertCookie(page);
     await cas.assertPageTitle(page, "CAS - Central Authentication Service Log In Successful");
     await cas.assertInnerText(page, "#content div h2", "Log In Successful");
