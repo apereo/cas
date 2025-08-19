0623baf15c47463a9b4a68839cef9240de046079
Misagh Moayyed <mm1844@gmail.com>
2025-08-19 22:02:07 +0400

    chore: fix build issues

--- core/cas-server-core-audit-api/src/main/java/org/apereo/inspektr/audit/support/DelegatingAuditEventRepository.java
+++ core/cas-server-core-audit-api/src/main/java/org/apereo/inspektr/audit/support/DelegatingAuditEventRepository.java
@@ -1,13 +1,14 @@
 package org.apereo.inspektr.audit.support;
 
+import org.apereo.inspektr.common.web.ClientInfoHolder;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.apache.commons.lang3.StringUtils;
 import org.apereo.cas.support.events.CasEventRepository;
 import org.apereo.cas.support.events.dao.CasEvent;
 import org.apereo.cas.util.function.FunctionUtils;
 import org.apereo.cas.util.http.HttpRequestUtils;
-import lombok.RequiredArgsConstructor;
-import lombok.val;
-import org.apache.commons.lang3.StringUtils;
-import org.apereo.inspektr.common.web.ClientInfoHolder;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.actuate.audit.AuditEvent;
 import org.springframework.boot.actuate.audit.AuditEventRepository;
 import org.springframework.boot.actuate.audit.InMemoryAuditEventRepository;
@@ -15,7 +16,6 @@ import java.time.Instant;
 import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 
 /**
  * This is {@link DelegatingAuditEventRepository}.
@@ -25,54 +25,57 @@ import java.util.function.Function;
  */
 @RequiredArgsConstructor
 public class DelegatingAuditEventRepository implements AuditEventRepository {
-    private final CasEventRepository casEventRepository;
+    private final ObjectProvider<CasEventRepository> casEventRepository;
     private final AuditEventRepository auditEventRepository = new InMemoryAuditEventRepository();
 
     @Override
     public void add(final AuditEvent event) {
-        FunctionUtils.doUnchecked(__ -> {
-            val clientInfo = ClientInfoHolder.getClientInfo();
-            val casEvent = new CasEvent();
-            casEvent.setPrincipalId(event.getPrincipal());
-            casEvent.setType(event.getType());
-            casEvent.putTimestamp(event.getTimestamp().toEpochMilli());
-            casEvent.setCreationTime(event.getTimestamp());
+        casEventRepository.ifAvailable(repo ->
+            FunctionUtils.doUnchecked(__ -> {
+                val clientInfo = ClientInfoHolder.getClientInfo();
+                val casEvent = new CasEvent();
+                casEvent.setPrincipalId(event.getPrincipal());
+                casEvent.setType(event.getType());
+                casEvent.putTimestamp(event.getTimestamp().toEpochMilli());
+                casEvent.setCreationTime(event.getTimestamp());
 
-            if (clientInfo != null) {
-                casEvent.putClientIpAddress(clientInfo.getClientIpAddress());
-                casEvent.putServerIpAddress(clientInfo.getServerIpAddress());
-                casEvent.putAgent(clientInfo.getUserAgent());
-                val geoLocationRequest = HttpRequestUtils.getHttpServletRequestGeoLocation(clientInfo.getGeoLocation());
-                casEvent.putGeoLocation(geoLocationRequest);
-                casEvent.putTenant(clientInfo.getTenant());
-            }
-            casEventRepository.save(casEvent);
-            auditEventRepository.add(event);
-        });
+                if (clientInfo != null) {
+                    casEvent.putClientIpAddress(clientInfo.getClientIpAddress());
+                    casEvent.putServerIpAddress(clientInfo.getServerIpAddress());
+                    casEvent.putAgent(clientInfo.getUserAgent());
+                    val geoLocationRequest = HttpRequestUtils.getHttpServletRequestGeoLocation(clientInfo.getGeoLocation());
+                    casEvent.putGeoLocation(geoLocationRequest);
+                    casEvent.putTenant(clientInfo.getTenant());
+                }
+                repo.save(casEvent);
+            }));
+        auditEventRepository.add(event);
     }
 
     @Override
     public List<AuditEvent> find(final String principal, final Instant after, final String type) {
         val results = new ArrayList<AuditEvent>();
-        if (StringUtils.isNotBlank(principal) && after != null && StringUtils.isNotBlank(type)) {
-            results.addAll(casEventRepository.getEventsOfTypeForPrincipal(type, principal, after.atZone(ZoneOffset.UTC))
-                .map(DelegatingAuditEventRepository::auditEventMapper).toList());
-        } else if (StringUtils.isNotBlank(principal) && after != null) {
-            results.addAll(casEventRepository.getEventsForPrincipal(principal, after.atZone(ZoneOffset.UTC))
-                .map(DelegatingAuditEventRepository::auditEventMapper).toList());
-        } else if (StringUtils.isNotBlank(principal) && StringUtils.isNotBlank(type)) {
-            results.addAll(casEventRepository.getEventsOfTypeForPrincipal(type, principal)
-                .map(DelegatingAuditEventRepository::auditEventMapper).toList());
-        } else if (StringUtils.isNotBlank(type) && after != null) {
-            results.addAll(casEventRepository.getEventsOfType(type, after.atZone(ZoneOffset.UTC))
-                .map(DelegatingAuditEventRepository::auditEventMapper).toList());
-        } else if (StringUtils.isNotBlank(principal)) {
-            results.addAll(casEventRepository.getEventsForPrincipal(principal)
-                .map(DelegatingAuditEventRepository::auditEventMapper).toList());
-        } else if (StringUtils.isNotBlank(type)) {
-            results.addAll(casEventRepository.getEventsOfType(type)
-                .map(DelegatingAuditEventRepository::auditEventMapper).toList());
-        }
+        casEventRepository.ifAvailable(repo -> {
+            if (StringUtils.isNotBlank(principal) && after != null && StringUtils.isNotBlank(type)) {
+                results.addAll(repo.getEventsOfTypeForPrincipal(type, principal, after.atZone(ZoneOffset.UTC))
+                    .map(DelegatingAuditEventRepository::auditEventMapper).toList());
+            } else if (StringUtils.isNotBlank(principal) && after != null) {
+                results.addAll(repo.getEventsForPrincipal(principal, after.atZone(ZoneOffset.UTC))
+                    .map(DelegatingAuditEventRepository::auditEventMapper).toList());
+            } else if (StringUtils.isNotBlank(principal) && StringUtils.isNotBlank(type)) {
+                results.addAll(repo.getEventsOfTypeForPrincipal(type, principal)
+                    .map(DelegatingAuditEventRepository::auditEventMapper).toList());
+            } else if (StringUtils.isNotBlank(type) && after != null) {
+                results.addAll(repo.getEventsOfType(type, after.atZone(ZoneOffset.UTC))
+                    .map(DelegatingAuditEventRepository::auditEventMapper).toList());
+            } else if (StringUtils.isNotBlank(principal)) {
+                results.addAll(repo.getEventsForPrincipal(principal)
+                    .map(DelegatingAuditEventRepository::auditEventMapper).toList());
+            } else if (StringUtils.isNotBlank(type)) {
+                results.addAll(repo.getEventsOfType(type)
+                    .map(DelegatingAuditEventRepository::auditEventMapper).toList());
+            }
+        });
 
         if (results.isEmpty()) {
             results.addAll(auditEventRepository.find(principal, after, type));
