1e7f1de1e930ddd97d1ade3eb1f44897b74175e9
Misagh Moayyed <mm1844@gmail.com>
2025-09-08 07:37:47 -0700

    fix: allow broken ldap records to be skipped for webauthn

--- core/cas-server-core-authentication-api/src/main/java/org/apereo/cas/authentication/DefaultAuthenticationEventExecutionPlan.java
+++ core/cas-server-core-authentication-api/src/main/java/org/apereo/cas/authentication/DefaultAuthenticationEventExecutionPlan.java
@@ -24,7 +24,6 @@ import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
