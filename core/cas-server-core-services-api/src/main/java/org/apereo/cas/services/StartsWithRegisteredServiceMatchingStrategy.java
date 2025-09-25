719b36d2fb5aa2904e30127f08982fdd6e3bafa4
Misagh Moayyed <mm1844@gmail.com>
2025-09-25 00:44:44 -0700

    fix: force callback url to start with server prefix

---
+++ core/cas-server-core-services-api/src/main/java/org/apereo/cas/services/StartsWithRegisteredServiceMatchingStrategy.java
@@ -0,0 +1,38 @@
+package org.apereo.cas.services;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+import org.apache.commons.lang3.Strings;
+import java.io.Serial;
+
+/**
+ * This is {@link StartsWithRegisteredServiceMatchingStrategy}.
+ *
+ * @author Misagh Moayyed
+ * @since 7.1.0
+ */
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+@AllArgsConstructor
+@Accessors(chain = true)
+public class StartsWithRegisteredServiceMatchingStrategy implements RegisteredServiceMatchingStrategy {
+    @Serial
+    private static final long serialVersionUID = -2345895859210185565L;
+
+    private String expectedUrl;
+
+    @Override
+    public boolean matches(final RegisteredService registeredService, final String serviceId) {
+        return Strings.CI.startsWith(serviceId, expectedUrl);
+    }
+}
---
+++ core/cas-server-core-services/src/test/java/org/apereo/cas/services/StartsWithRegisteredServiceMatchingStrategyTests.java
@@ -0,0 +1,47 @@
+package org.apereo.cas.services;
+
+import org.apereo.cas.util.RandomUtils;
+import org.apereo.cas.util.serialization.JacksonObjectMapperFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.val;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import java.nio.file.Files;
+import java.util.Locale;
+import java.util.UUID;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * This is {@link StartsWithRegisteredServiceMatchingStrategyTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 7.3.0
+ */
+@Tag("RegisteredService")
+class StartsWithRegisteredServiceMatchingStrategyTests {
+    private static final ObjectMapper MAPPER = JacksonObjectMapperFactory.builder()
+        .defaultTypingEnabled(true).build().toObjectMapper();
+
+    @Test
+    void verifySerialization() throws Throwable {
+        val jsonFile = Files.createTempFile(RandomUtils.randomAlphabetic(8), ".json").toFile();
+        val service = RegisteredServiceTestUtils.getRegisteredService(UUID.randomUUID().toString());
+        val strategy = new StartsWithRegisteredServiceMatchingStrategy().setExpectedUrl("https://google.com");
+        service.setMatchingStrategy(strategy);
+        MAPPER.writeValue(jsonFile, service);
+        val read = MAPPER.readValue(jsonFile, RegisteredService.class);
+        assertEquals(read, service);
+    }
+
+    @Test
+    void verifyOperation() {
+        val service = RegisteredServiceTestUtils.getRegisteredService(RegisteredServiceTestUtils.CONST_TEST_URL);
+        val strategy = new StartsWithRegisteredServiceMatchingStrategy()
+            .setExpectedUrl(RegisteredServiceTestUtils.CONST_TEST_URL);
+        assertTrue(strategy.matches(service, RegisteredServiceTestUtils.CONST_TEST_URL));
+        assertTrue(strategy.matches(service, RegisteredServiceTestUtils.CONST_TEST_URL.toUpperCase(Locale.ENGLISH)));
+        assertFalse(strategy.matches(service, "https://.*"));
+    }
+}
