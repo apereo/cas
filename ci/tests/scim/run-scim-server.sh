eabf0eeae15b603e9b8de22e69f527cfad5b07ce
Misagh Moayyed <mm1844@gmail.com>
2025-10-17 14:25:03 -0700

    chore: working on test fixes - wip

--- ci/tests/scim/run-scim-server.sh
+++ ci/tests/scim/run-scim-server.sh
@@ -3,6 +3,7 @@ export DOCKER_IMAGE="personify/personify-scim-server:2.1.2.RELEASE"
 echo "Running SCIM docker container..."
 docker stop scim-server || true && docker rm scim-server || true
 docker run --rm --name="scim-server" -p 9666:8080 -d ${DOCKER_IMAGE}
+docker logs -f scim-server &
 echo "Waiting for SCIM container to prepare..."
 sleep 5
 docker ps | grep "scim-server"
