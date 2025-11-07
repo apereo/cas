1f697e687e2e003becf877fa4d39ab12b61694a1
Misagh Moayyed <mm1844@gmail.com>
2025-11-07 11:05:17 -0800

    fix: events endpoint to produce proper json

--- core/cas-server-core-events-api/src/main/java/org/apereo/cas/support/events/web/CasEventsReportEndpoint.java
+++ core/cas-server-core-events-api/src/main/java/org/apereo/cas/support/events/web/CasEventsReportEndpoint.java
@@ -87,7 +87,6 @@ public class CasEventsReportEndpoint extends BaseCasRestActuatorEndpoint {
         final HttpServletResponse response,
         @RequestParam(required = false, defaultValue = "1000")
         final int limit) throws Exception {
-        response.setContentType(MediaType.APPLICATION_NDJSON_VALUE);
         val emitter = new ResponseBodyEmitter();
 
         executor.submit(() ->
@@ -95,15 +94,15 @@ public class CasEventsReportEndpoint extends BaseCasRestActuatorEndpoint {
                 try (val stream = eventRepositoryProvider.getObject().load()
                     .sorted(Comparator.comparingLong(CasEvent::getTimestamp).reversed())
                     .limit(limit)) {
-                    emitter.send("[", MediaType.APPLICATION_JSON);
+                    emitter.send("[".getBytes(StandardCharsets.UTF_8), MediaType.APPLICATION_JSON);
                     val first = new AtomicBoolean(true);
                     stream.forEach(Unchecked.consumer(pojo -> {
                         if (!first.getAndSet(false)) {
-                            emitter.send(",");
+                            emitter.send(",".getBytes(StandardCharsets.UTF_8), MediaType.APPLICATION_JSON);
                         }
-                        emitter.send(MAPPER.writeValueAsString(pojo), MediaType.APPLICATION_JSON);
+                        emitter.send(MAPPER.writeValueAsBytes(pojo), MediaType.APPLICATION_JSON);
                     }));
-                    emitter.send("]", MediaType.APPLICATION_JSON);
+                    emitter.send("]".getBytes(StandardCharsets.UTF_8), MediaType.APPLICATION_JSON);
                     emitter.complete();
                 } catch (final Exception e) {
                     emitter.completeWithError(e);
