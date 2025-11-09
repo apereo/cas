62279ef5285afdeda23a18283583517ad8e7aa56
Misagh Moayyed <mm1844@gmail.com>
2025-11-09 15:13:24 -0800

    fix: separate surrogate credential extractor from default impl

--- support/cas-server-support-pac4j-core/src/main/java/org/apereo/cas/authentication/principal/DefaultDelegatedAuthenticationCredentialExtractor.java
+++ support/cas-server-support-pac4j-core/src/main/java/org/apereo/cas/authentication/principal/DefaultDelegatedAuthenticationCredentialExtractor.java
@@ -1,15 +1,10 @@
 package org.apereo.cas.authentication.principal;
 
-import org.apereo.cas.util.function.FunctionUtils;
 import org.apereo.cas.web.support.WebUtils;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.pac4j.core.client.BaseClient;
-import org.pac4j.core.context.CallContext;
 import org.pac4j.core.context.session.SessionStore;
-import org.pac4j.core.credentials.Credentials;
-import org.pac4j.jee.context.JEEContext;
 import org.springframework.webflow.execution.RequestContext;
 import java.util.Optional;
 
@@ -20,38 +15,21 @@ import java.util.Optional;
  * @since 7.0.0
  */
 @Slf4j
-@RequiredArgsConstructor
-public class DefaultDelegatedAuthenticationCredentialExtractor implements DelegatedAuthenticationCredentialExtractor {
-    private final SessionStore sessionStore;
+public class DefaultDelegatedAuthenticationCredentialExtractor extends BaseDelegatedAuthenticationCredentialExtractor {
+    public DefaultDelegatedAuthenticationCredentialExtractor(final SessionStore sessionStore) {
+        super(sessionStore);
+    }
 
     @Override
     public Optional<ClientCredential> extract(final BaseClient client, final RequestContext requestContext) {
         LOGGER.debug("Fetching credentials from delegated client [{}]", client);
         val credentials = getCredentialsFromDelegatedClient(requestContext, client);
-        if (credentials.isPresent()) {
-            val clientCredentialResult = buildClientCredential(client, requestContext, credentials.get());
-            clientCredentialResult.ifPresent(clientCredential -> WebUtils.putCredential(requestContext, clientCredential));
-            return clientCredentialResult;
-        }
-        return Optional.empty();
-    }
-
-    protected Optional<ClientCredential> buildClientCredential(final BaseClient client, final RequestContext requestContext, final Credentials credentials) {
-        LOGGER.info("Credentials are successfully authenticated using the delegated client [{}]", client.getName());
-        return Optional.of(new ClientCredential(credentials, client.getName()));
-    }
-
-    protected Optional<Credentials> getCredentialsFromDelegatedClient(final RequestContext requestContext, final BaseClient client) {
-        return FunctionUtils.doAndHandle(() -> {
-            val request = WebUtils.getHttpServletRequestFromExternalWebflowContext(requestContext);
-            val response = WebUtils.getHttpServletResponseFromExternalWebflowContext(requestContext);
-            val webContext = new JEEContext(request, response);
-            val callContext = new CallContext(webContext, this.sessionStore);
-            return client.getCredentials(callContext)
-                .map(clientCredentials -> client.validateCredentials(callContext, clientCredentials))
-                .filter(Optional::isPresent)
-                .map(Optional::get);
-        }, e -> Optional.<Credentials>empty()).get();
-
+        return credentials.flatMap(cred ->
+            buildClientCredential(client, requestContext, cred)
+                .map(clientCredential -> {
+                    WebUtils.putCredential(requestContext, clientCredential);
+                    return clientCredential;
+                })
+        );
     }
 }
