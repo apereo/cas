27552b9c1f204de8b65e4158f3d332c41b5ccbaf
Misagh Moayyed <mm1844@gmail.com>
2025-12-19 13:15:24 -0800

    fix: disable spring session mongodb; gradle metadata invalid

--- support/cas-server-support-session-mongo/src/main/java/org/apereo/cas/config/CasMongoSessionAutoConfiguration.java
+++ support/cas-server-support-session-mongo/src/main/java/org/apereo/cas/config/CasMongoSessionAutoConfiguration.java
@@ -7,7 +7,6 @@ import org.apereo.cas.util.spring.boot.ConditionalOnFeatureEnabled;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.data.mongodb.autoconfigure.MongoDataAutoConfiguration;
 import org.springframework.boot.mongodb.autoconfigure.MongoAutoConfiguration;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
@@ -16,8 +15,9 @@ import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.convert.MongoConverter;
-import org.springframework.session.data.mongo.JdkMongoSessionConverter;
-import org.springframework.session.data.mongo.config.annotation.web.http.EnableMongoHttpSession;
+
+import org.mongodb.spring.session.JdkMongoSessionConverter;
+
 import java.time.Duration;
 
 /**
