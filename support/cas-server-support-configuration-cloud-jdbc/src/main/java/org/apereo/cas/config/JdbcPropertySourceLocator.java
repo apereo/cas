e09aa39fbcf0019c858e5c9eb1bf4607572b393b
Misagh Moayyed <mm1844@gmail.com>
2025-12-25 16:11:47 -0800

    feat: support mutable property sources for jdbc

---
+++ support/cas-server-support-configuration-cloud-jdbc/src/main/java/org/apereo/cas/config/JdbcPropertySourceLocator.java
@@ -0,0 +1,27 @@
+package org.apereo.cas.config;
+
+import module java.base;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.springframework.cloud.bootstrap.config.PropertySourceLocator;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * This is {@link JdbcPropertySourceLocator}.
+ *
+ * @author Misagh Moayyed
+ * @since 8.0.0
+ */
+@RequiredArgsConstructor
+public class JdbcPropertySourceLocator implements PropertySourceLocator {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public PropertySource<?> locate(final Environment environment) {
+        val sourceName = JdbcPropertySource.class.getSimpleName();
+        return new JdbcPropertySource(sourceName, jdbcTemplate);
+    }
+
+}
