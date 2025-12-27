79bd4282407b0b8dc082722d5f2da7bcd7aefd56
Misagh Moayyed <mm1844@gmail.com>
2025-12-27 13:18:04 -0800

    feat: support mutable property sources for aws dynamodb

--- support/cas-server-support-dynamodb-core/src/main/java/org/apereo/cas/dynamodb/DynamoDbTableUtils.java
+++ support/cas-server-support-dynamodb-core/src/main/java/org/apereo/cas/dynamodb/DynamoDbTableUtils.java
@@ -8,7 +8,6 @@ import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.apache.commons.lang3.tuple.Pair;
-import software.amazon.awssdk.core.exception.SdkClientException;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.model.AttributeDefinition;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
@@ -46,9 +45,8 @@ public class DynamoDbTableUtils {
      * @param dynamo    the dynamo
      * @param tableName the table name
      * @return the table description
-     * @throws Exception the exception
      */
-    public static TableDescription waitUntilActive(final DynamoDbClient dynamo, final String tableName) throws Exception {
+    public static TableDescription waitUntilActive(final DynamoDbClient dynamo, final String tableName) {
         return waitUntilActive(dynamo, tableName, DEFAULT_WAIT_TIMEOUT, DEFAULT_WAIT_INTERVAL);
     }
 
@@ -60,10 +58,10 @@ public class DynamoDbTableUtils {
      * @param timeout   the timeout
      * @param interval  the interval
      * @return the table description
-     * @throws Exception the exception
      */
-    public static TableDescription waitUntilActive(final DynamoDbClient dynamo, final String tableName, final int timeout,
-                                       final int interval) throws Exception {
+    public static TableDescription waitUntilActive(final DynamoDbClient dynamo, final String tableName,
+                                                   final int timeout,
+                                                   final int interval) {
         val table = waitForTableDescription(dynamo, tableName, TableStatus.ACTIVE, timeout, interval);
 
         if (table == null || !table.tableStatusAsString().equals(TableStatus.ACTIVE.toString())) {
@@ -154,9 +152,9 @@ public class DynamoDbTableUtils {
         val billingMode = BillingMode.fromValue(dynamoDbProperties.getBillingMode().name());
         return billingMode == BillingMode.PROVISIONED
             ? ProvisionedThroughput.builder()
-                .readCapacityUnits(dynamoDbProperties.getReadCapacity())
-                .writeCapacityUnits(dynamoDbProperties.getWriteCapacity())
-                .build()
+            .readCapacityUnits(dynamoDbProperties.getReadCapacity())
+            .writeCapacityUnits(dynamoDbProperties.getWriteCapacity())
+            .build()
             : null;
     }
 
@@ -313,8 +311,7 @@ public class DynamoDbTableUtils {
                                                             final String tableName,
                                                             final TableStatus desiredStatus,
                                                             final int timeout,
-                                                            final int interval)
-        throws Exception {
+                                                            final int interval) {
         val startTime = System.currentTimeMillis();
         val endTime = startTime + timeout;
 
@@ -329,7 +326,7 @@ public class DynamoDbTableUtils {
             } catch (final ResourceNotFoundException rnfe) {
                 LOGGER.trace(rnfe.getMessage());
             }
-            Thread.sleep(interval);
+            FunctionUtils.doUnchecked(_ -> Thread.sleep(interval));
         }
         return table;
     }
@@ -380,20 +377,13 @@ public class DynamoDbTableUtils {
             .filter(Objects::nonNull);
     }
 
-    static class TableNeverTransitionedToStateException extends SdkClientException {
+    public static class TableNeverTransitionedToStateException extends RuntimeException {
 
         @Serial
         private static final long serialVersionUID = 8920567021104846647L;
 
-        /**
-         * Instantiates a new Table never transitioned to state exception.
-         *
-         * @param tableName     the table name
-         * @param desiredStatus the desired status
-         */
         TableNeverTransitionedToStateException(final String tableName, final TableStatus desiredStatus) {
-            super(SdkClientException.builder()
-                .message("Table " + tableName + " never transitioned to desired state of " + desiredStatus.toString()));
+            super("Table " + tableName + " never transitioned to desired state of " + desiredStatus.toString());
         }
 
     }
