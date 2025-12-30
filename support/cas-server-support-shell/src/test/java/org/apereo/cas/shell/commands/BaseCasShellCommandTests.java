24b7927f8b116fbb3c3dbeaf35ad0ef5197e0d0a
Misagh Moayyed <mm1844@gmail.com>
2025-12-30 15:44:02 -0800

    fix: update build after shell v4 release

--- support/cas-server-support-shell/src/test/java/org/apereo/cas/shell/commands/BaseCasShellCommandTests.java
+++ support/cas-server-support-shell/src/test/java/org/apereo/cas/shell/commands/BaseCasShellCommandTests.java
@@ -12,8 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.shell.boot.SpringShellAutoConfiguration;
 import org.springframework.shell.core.ShellRunner;
+import org.springframework.shell.core.autoconfigure.SpringShellAutoConfiguration;
 
 /**
  * This is {@link BaseCasShellCommandTests}.
