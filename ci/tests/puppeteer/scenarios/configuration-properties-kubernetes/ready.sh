9faeb354dcb7051e493011997815ee0163cfea65
Misagh Moayyed <mm1844@gmail.com>
2026-01-06 20:23:57 -0800

    chore: polish

--- ci/tests/puppeteer/scenarios/configuration-properties-kubernetes/ready.sh
+++ ci/tests/puppeteer/scenarios/configuration-properties-kubernetes/ready.sh
@@ -4,9 +4,10 @@
 
 export DOCKER_IMAGE="cas-${SCENARIO}:latest"
 
+mkdir -p ${SCENARIO_FOLDER}/k8config
 rm -rf ${SCENARIO_FOLDER}/k8config/thekeystore
 cp ${SCENARIO_FOLDER}/../../overlay/thekeystore ${SCENARIO_FOLDER}/k8config/thekeystore
-echo "Copied thekeystore to ${SCENARIO_FOLDER}/k8config/thekeystore"
+echo "Copied keystore to ${SCENARIO_FOLDER}/k8config/thekeystore"
 
 chmod +x "${PWD}/ci/tests/kubernetes/run-kubernetes-server.sh"
 ${PWD}/ci/tests/kubernetes/run-kubernetes-server.sh
