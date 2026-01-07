a544573a0aa647d92e8b890a22aa8b0ef07f130b
Misagh Moayyed <mm1844@gmail.com>
2026-01-07 15:54:09 -0800

    chore: use gum instead of progressbar in ci logs

--- ci/tests/puppeteer/run.sh
+++ ci/tests/puppeteer/run.sh
@@ -52,32 +52,6 @@ function printred() {
   printf "🔥  ${RED}$1${ENDCOLOR}\n"
 }
 
-function progressbar() {
-  current="$1"
-  total="$2"
-
-  bar_size=60
-  bar_char_done="#"
-  bar_char_todo="-"
-  bar_percentage_scale=2
-
-  # calculate the progress in percentage
-  percent=$(bc <<<"scale=$bar_percentage_scale; 100 * $current / $total")
-  # The number of done and todo characters
-  done=$(bc <<<"scale=0; $bar_size * $percent / 100")
-  todo=$(bc <<<"scale=0; $bar_size - $done")
-
-  # build the done and todo sub-bars
-  done_sub_bar=$(printf "%${done}s" | tr " " "${bar_char_done}")
-  todo_sub_bar=$(printf "%${todo}s" | tr " " "${bar_char_todo}")
-
-  # output the bar
-  echo -ne "\rProgress: [${done_sub_bar}${todo_sub_bar}] ${percent}%"
-  if [ $total -eq $current ]; then
-    echo -e "\n"
-  fi
-}
-
 function downloadAndRunExternalTomcat() {
   local casWebApp="$1"
   shift
@@ -188,11 +162,7 @@ function downloadAndRunExternalTomcat() {
 }
 
 function sleepfor() {
-  tasks_in_total="$1"
-  for current_task in $(seq "$tasks_in_total"); do
-    sleep 1
-    progressbar "$current_task" "$tasks_in_total"
-  done
+  gum spin --spinner dot --title "Running task..." -- sleep $1
 }
 
 function fetchCasVersion() {
