4eae6e68c3c37f42995fbe584d4d22f42b16f6e6
Misagh Moayyed <mm1844@gmail.com>
2026-01-10 15:10:27 -0800

    fix: fix dpop validation rule w/ max-age

--- support/cas-server-support-oidc-core-api/src/main/java/org/apereo/cas/oidc/authn/OidcDPoPAuthenticator.java
+++ support/cas-server-support-oidc-core-api/src/main/java/org/apereo/cas/oidc/authn/OidcDPoPAuthenticator.java
@@ -71,7 +71,7 @@ public class OidcDPoPAuthenticator implements Authenticator {
             .collect(Collectors.toSet());
         val seconds = Beans.newDuration(casProperties.getAuthn().getOidc().getCore().getSkew()).toSeconds();
         val endpointURI = new URI(webContext.getRequestURL());
-        val verifier = new DPoPTokenRequestVerifier(algorithms, endpointURI, seconds, 0, null);
+        val verifier = new DPoPTokenRequestVerifier(algorithms, endpointURI, seconds, seconds, null);
         val signedProof = getSignedProofOfPosessionJwt(dPopProof);
         val dPopIssuer = new DPoPIssuer(new ClientID(clientId));
         return verifier.verify(dPopIssuer, signedProof, null);
