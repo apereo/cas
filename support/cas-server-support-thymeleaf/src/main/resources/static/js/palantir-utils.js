dea34853d74933d7d7246ce696f73ee6ef2b2ab1
Misagh Moayyed <mm1844@gmail.com>
2026-01-15 22:29:33 -0800

    update tooltips for admin console

--- support/cas-server-support-thymeleaf/src/main/resources/static/js/palantir-utils.js
+++ support/cas-server-support-thymeleaf/src/main/resources/static/js/palantir-utils.js
@@ -129,3 +129,22 @@ function initializeDatePickers() {
         }
     });
 }
+
+function initializeTooltips() {
+    $( function() {
+        $( document ).tooltip({
+            position: {
+                my: "center bottom-20",
+                at: "center top",
+                using: function( position, feedback ) {
+                    $( this ).css( position );
+                    $( "<div>" )
+                        .addClass( "arrow" )
+                        .addClass( feedback.vertical )
+                        .addClass( feedback.horizontal )
+                        .appendTo( this );
+                }
+            }
+        });
+    });
+}
