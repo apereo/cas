623857881d3707fbe6067e1934d854585a7087f1
Misagh Moayyed <mm1844@gmail.com>
2026-01-18 10:08:18 -0800

    fix: allow groovy attribute resolution to use caches scripts

--- support/cas-server-support-person-directory-core/src/test/java/org/apereo/cas/persondir/GroovyPersonAttributeDaoTests.java
+++ support/cas-server-support-person-directory-core/src/test/java/org/apereo/cas/persondir/GroovyPersonAttributeDaoTests.java
@@ -1,11 +1,14 @@
 package org.apereo.cas.persondir;
 
 import module java.base;
+import org.apereo.cas.config.CasCoreScriptingAutoConfiguration;
+import org.apereo.cas.config.CasCoreUtilAutoConfiguration;
 import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.configuration.model.core.authentication.GroovyPrincipalAttributesProperties;
 import org.apereo.cas.persondir.groovy.GroovyPersonAttributeDao;
 import org.apereo.cas.persondir.groovy.InternalGroovyScriptDao;
 import org.apereo.cas.test.CasTestExtension;
+import org.apereo.cas.util.spring.boot.SpringBootTestAutoConfigurations;
 import lombok.val;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -13,7 +16,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,8 +27,12 @@ import static org.junit.jupiter.api.Assertions.*;
  * @since 5.3.0
  */
 @Tag("Groovy")
-@SpringBootTest(classes = RefreshAutoConfiguration.class)
+@SpringBootTest(classes = {
+    CasCoreUtilAutoConfiguration.class,
+    CasCoreScriptingAutoConfiguration.class
+})
 @ExtendWith(CasTestExtension.class)
+@SpringBootTestAutoConfigurations
 @EnableConfigurationProperties(CasConfigurationProperties.class)
 class GroovyPersonAttributeDaoTests {
     @Autowired
@@ -45,5 +51,14 @@ class GroovyPersonAttributeDaoTests {
 
         val results = dao.getPeople(queryAttributes);
         assertFalse(results.isEmpty());
+        val personAttributes = results.stream().findFirst().orElseThrow();
+        assertEquals("casuser", personAttributes.getName());
+        val attributes = personAttributes.getAttributes();
+        assertTrue(attributes.containsKey("givenName"));
+        assertTrue(attributes.containsKey("lastName"));
+        assertTrue(attributes.containsKey("name"));
+        assertTrue(attributes.containsKey("nickname"));
+        assertTrue(attributes.containsKey("username"));
+
     }
 }
