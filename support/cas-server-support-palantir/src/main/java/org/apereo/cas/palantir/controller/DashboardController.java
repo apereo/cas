a7cfda68db494661dad1c3eb9d2be51ac98f101f
Misagh Moayyed <mm1844@gmail.com>
2026-01-19 15:14:02 -0800

    fix: fix build

--- support/cas-server-support-palantir/src/main/java/org/apereo/cas/palantir/controller/DashboardController.java
+++ support/cas-server-support-palantir/src/main/java/org/apereo/cas/palantir/controller/DashboardController.java
@@ -34,7 +34,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
