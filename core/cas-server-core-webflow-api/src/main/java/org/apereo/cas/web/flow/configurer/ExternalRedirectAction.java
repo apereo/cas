bf449ab913cfab8c51df495c14a67d13d68da38b
Misagh Moayyed <mm1844@gmail.com>
2026-02-05 08:57:32 -0800

    control how external redirects with webflow execute

---
+++ core/cas-server-core-webflow-api/src/main/java/org/apereo/cas/web/flow/configurer/ExternalRedirectAction.java
@@ -0,0 +1,38 @@
+package org.apereo.cas.web.flow.configurer;
+
+import module java.base;
+import org.apereo.cas.web.support.WebUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.springframework.binding.expression.Expression;
+import org.springframework.webflow.action.AbstractAction;
+import org.springframework.webflow.execution.Event;
+import org.springframework.webflow.execution.RequestContext;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * This is {@link ExternalRedirectAction}.
+ *
+ * @author Misagh Moayyed
+ * @since 8.0.0
+ */
+@RequiredArgsConstructor
+public class ExternalRedirectAction extends AbstractAction {
+    private final Expression resourceUri;
+
+    @Override
+    protected Event doExecute(final RequestContext context) throws Exception {
+        val location = (String) this.resourceUri.getValue(context);
+        if (location.startsWith("servletRelative:") || location.startsWith("contextRelative:")
+            || location.startsWith("serverRelative:") || location.startsWith("http://")
+            || location.startsWith("https://")) {
+            context.getExternalContext().requestExternalRedirect(location);
+        } else {
+            val response = WebUtils.getHttpServletResponseFromExternalWebflowContext(context);
+            response.setStatus(HttpServletResponse.SC_FOUND);
+            response.setHeader("Location", location);
+            context.getExternalContext().recordResponseComplete();
+        }
+        return this.success();
+    }
+}
---
+++ core/cas-server-core-webflow-api/src/test/java/org/apereo/cas/web/flow/actions/ExternalRedirectActionTests.java
@@ -0,0 +1,42 @@
+package org.apereo.cas.web.flow.actions;
+
+import module java.base;
+import org.apereo.cas.util.MockRequestContext;
+import org.apereo.cas.web.flow.CasWebflowConstants;
+import org.apereo.cas.web.flow.configurer.ExternalRedirectAction;
+import lombok.val;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.binding.expression.support.LiteralExpression;
+import org.springframework.webflow.test.MockExternalContext;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This is {@link ExternalRedirectActionTests}.
+ *
+ * @author Misagh Moayyed
+ * @since 8.0.0
+ */
+@Tag("WebflowActions")
+class ExternalRedirectActionTests {
+    @Test
+    void verifyExternal() throws Exception {
+        val action = new ExternalRedirectAction(new LiteralExpression("https://apereo.github.io"));
+        val context = MockRequestContext.create();
+        val results = action.execute(context);
+        val external = (MockExternalContext) context.getExternalContext();
+        assertTrue(external.isResponseComplete());
+        assertEquals("https://apereo.github.io", external.getExternalRedirectUrl());
+        assertEquals(CasWebflowConstants.TRANSITION_ID_SUCCESS, results.getId());
+    }
+
+    @Test
+    void verifyDirect() throws Exception {
+        val action = new ExternalRedirectAction(new LiteralExpression("custom://apereo.github.io"));
+        val context = MockRequestContext.create();
+        val results = action.execute(context);
+        assertTrue(context.getExternalContext().isResponseComplete());
+        assertEquals("custom://apereo.github.io", context.getHttpServletResponse().getHeader("Location"));
+        assertEquals(CasWebflowConstants.TRANSITION_ID_SUCCESS, results.getId());
+    }
+}
