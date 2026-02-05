f7aa73cd1ee4de323f59f13096901c96336e22be
Misagh Moayyed <mm1844@gmail.com>
2026-02-04 19:38:48 -0800

    update tests

--- support/cas-server-support-oidc/src/test/java/org/apereo/cas/oidc/web/controllers/OidcIntrospectionEndpointControllerTests.java
+++ support/cas-server-support-oidc/src/test/java/org/apereo/cas/oidc/web/controllers/OidcIntrospectionEndpointControllerTests.java
@@ -3,10 +3,9 @@ package org.apereo.cas.oidc.web.controllers;
 import module java.base;
 import org.apereo.cas.oidc.AbstractOidcTests;
 import org.apereo.cas.oidc.OidcConstants;
-import org.apereo.cas.oidc.web.controllers.introspection.OidcIntrospectionEndpointController;
 import org.apereo.cas.support.oauth.OAuth20Constants;
-import org.apereo.cas.support.oauth.web.response.introspection.OAuth20IntrospectionAccessTokenResponse;
 import org.apereo.cas.util.EncodingUtils;
+import org.apereo.cas.util.serialization.JacksonObjectMapperFactory;
 import com.nimbusds.jwt.EncryptedJWT;
 import com.nimbusds.jwt.JWTParser;
 import com.nimbusds.jwt.PlainJWT;
@@ -15,14 +14,13 @@ import lombok.val;
 import org.jose4j.jws.AlgorithmIdentifiers;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.pac4j.core.context.HttpConstants;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
+import tools.jackson.databind.ObjectMapper;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * This is {@link OidcIntrospectionEndpointControllerTests}.
@@ -36,19 +34,14 @@ import static org.junit.jupiter.api.Assertions.*;
     "cas.authn.oidc.discovery.introspection-encrypted-response-alg-values-supported=RSA-OAEP-256"
 })
 class OidcIntrospectionEndpointControllerTests extends AbstractOidcTests {
-    @Autowired
-    @Qualifier("oidcIntrospectionEndpointController")
-    protected OidcIntrospectionEndpointController oidcIntrospectionEndpointController;
+
+    private static final ObjectMapper MAPPER = JacksonObjectMapperFactory.builder()
+        .defaultTypingEnabled(false).build().toObjectMapper();
 
     @Test
     void verifyOperationWithValidTicketAsJwtSignedEncrypted() throws Throwable {
-        val request = getHttpRequestForEndpoint(OidcConstants.INTROSPECTION_URL);
-        val response = new MockHttpServletResponse();
-        request.addHeader(HttpHeaders.ACCEPT, OAuth20Constants.INTROSPECTION_JWT_HEADER_CONTENT_TYPE);
-
         val accessToken = getAccessToken(UUID.randomUUID().toString());
-        val value = EncodingUtils.encodeBase64((accessToken.getClientId() + ":secret").getBytes(StandardCharsets.UTF_8));
-        request.addHeader(HttpConstants.AUTHORIZATION_HEADER, HttpConstants.BASIC_HEADER_PREFIX + value);
+        val credentials = EncodingUtils.encodeBase64((accessToken.getClientId() + ":secret").getBytes(StandardCharsets.UTF_8));
 
         val oidcRegisteredService = getOidcRegisteredService(accessToken.getClientId());
         oidcRegisteredService.setIntrospectionSignedResponseAlg(AlgorithmIdentifiers.RSA_USING_SHA256);
@@ -56,134 +49,157 @@ class OidcIntrospectionEndpointControllerTests extends AbstractOidcTests {
         oidcRegisteredService.setIntrospectionEncryptedResponseEncoding("A128CBC-HS256");
         servicesManager.save(oidcRegisteredService);
         ticketRegistry.addTicket(accessToken);
-        request.addParameter(OAuth20Constants.TOKEN, accessToken.getId());
-        val result = oidcIntrospectionEndpointController.handleRequest(request, response);
-        assertInstanceOf(EncryptedJWT.class, JWTParser.parse(result.getBody().toString()));
-        assertEquals(OAuth20Constants.INTROSPECTION_JWT_HEADER_CONTENT_TYPE, result.getHeaders().getContentType().toString());
+
+        val response = mockMvc.perform(post("/cas/" + OidcConstants.BASE_OIDC_URL + '/' + OidcConstants.INTROSPECTION_URL)
+                .with(withHttpRequestProcessor())
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .header(HttpHeaders.ACCEPT, OAuth20Constants.INTROSPECTION_JWT_HEADER_CONTENT_TYPE)
+                .header(HttpHeaders.AUTHORIZATION, "Basic " + credentials)
+                .param(OAuth20Constants.TOKEN, accessToken.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(OAuth20Constants.INTROSPECTION_JWT_HEADER_CONTENT_TYPE))
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+        assertInstanceOf(EncryptedJWT.class, JWTParser.parse(response));
     }
 
     @Test
     void verifyOperationWithValidTicketAsJwtSigned() throws Throwable {
-        val request = getHttpRequestForEndpoint(OidcConstants.INTROSPECTION_URL);
-        val response = new MockHttpServletResponse();
-        request.addHeader(HttpHeaders.ACCEPT, OAuth20Constants.INTROSPECTION_JWT_HEADER_CONTENT_TYPE);
-
         val accessToken = getAccessToken(UUID.randomUUID().toString());
-        val value = EncodingUtils.encodeBase64((accessToken.getClientId() + ":secret").getBytes(StandardCharsets.UTF_8));
-        request.addHeader(HttpConstants.AUTHORIZATION_HEADER, HttpConstants.BASIC_HEADER_PREFIX + value);
+        val credentials = EncodingUtils.encodeBase64((accessToken.getClientId() + ":secret").getBytes(StandardCharsets.UTF_8));
 
         val oidcRegisteredService = getOidcRegisteredService(accessToken.getClientId());
         oidcRegisteredService.setIntrospectionSignedResponseAlg(AlgorithmIdentifiers.RSA_USING_SHA512);
         servicesManager.save(oidcRegisteredService);
         ticketRegistry.addTicket(accessToken);
-        request.addParameter(OAuth20Constants.TOKEN, accessToken.getId());
-        val result = oidcIntrospectionEndpointController.handleRequest(request, response).getBody();
-        assertInstanceOf(SignedJWT.class, JWTParser.parse(result.toString()));
+
+        val response = mockMvc.perform(post("/cas/" + OidcConstants.BASE_OIDC_URL + '/' + OidcConstants.INTROSPECTION_URL)
+                .with(withHttpRequestProcessor())
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .header(HttpHeaders.ACCEPT, OAuth20Constants.INTROSPECTION_JWT_HEADER_CONTENT_TYPE)
+                .header(HttpHeaders.AUTHORIZATION, "Basic " + credentials)
+                .param(OAuth20Constants.TOKEN, accessToken.getId()))
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+        assertInstanceOf(SignedJWT.class, JWTParser.parse(response));
     }
 
     @Test
     void verifyOperationWithValidTicketAsJwtSignedWithNone() throws Throwable {
-        val request = getHttpRequestForEndpoint(OidcConstants.INTROSPECTION_URL);
-        val response = new MockHttpServletResponse();
-        request.addHeader(HttpHeaders.ACCEPT, OAuth20Constants.INTROSPECTION_JWT_HEADER_CONTENT_TYPE);
-
         val accessToken = getAccessToken(UUID.randomUUID().toString());
-        val value = EncodingUtils.encodeBase64((accessToken.getClientId() + ":secret").getBytes(StandardCharsets.UTF_8));
-        request.addHeader(HttpConstants.AUTHORIZATION_HEADER, HttpConstants.BASIC_HEADER_PREFIX + value);
+        val credentials = EncodingUtils.encodeBase64((accessToken.getClientId() + ":secret").getBytes(StandardCharsets.UTF_8));
 
         val oidcRegisteredService = getOidcRegisteredService(accessToken.getClientId());
         oidcRegisteredService.setIntrospectionSignedResponseAlg(AlgorithmIdentifiers.NONE);
         servicesManager.save(oidcRegisteredService);
         ticketRegistry.addTicket(accessToken);
-        request.addParameter(OAuth20Constants.TOKEN, accessToken.getId());
-        val result = oidcIntrospectionEndpointController.handleRequest(request, response);
-        assertTrue(result.getStatusCode().is4xxClientError());
+
+        mockMvc.perform(post("/cas/" + OidcConstants.BASE_OIDC_URL + '/' + OidcConstants.INTROSPECTION_URL)
+                .with(withHttpRequestProcessor())
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .header(HttpHeaders.ACCEPT, OAuth20Constants.INTROSPECTION_JWT_HEADER_CONTENT_TYPE)
+                .header(HttpHeaders.AUTHORIZATION, "Basic " + credentials)
+                .param(OAuth20Constants.TOKEN, accessToken.getId()))
+            .andExpect(status().is4xxClientError());
     }
 
     @Test
     void verifyOperationWithValidTicketAsJwtSignedWithNoneEncryption() throws Throwable {
-        val request = getHttpRequestForEndpoint(OidcConstants.INTROSPECTION_URL);
-        val response = new MockHttpServletResponse();
-        request.addHeader(HttpHeaders.ACCEPT, OAuth20Constants.INTROSPECTION_JWT_HEADER_CONTENT_TYPE);
-
         val accessToken = getAccessToken(UUID.randomUUID().toString());
-        val value = EncodingUtils.encodeBase64((accessToken.getClientId() + ":secret").getBytes(StandardCharsets.UTF_8));
-        request.addHeader(HttpConstants.AUTHORIZATION_HEADER, HttpConstants.BASIC_HEADER_PREFIX + value);
+        val credentials = EncodingUtils.encodeBase64((accessToken.getClientId() + ":secret").getBytes(StandardCharsets.UTF_8));
 
         val oidcRegisteredService = getOidcRegisteredService(accessToken.getClientId());
         oidcRegisteredService.setIntrospectionEncryptedResponseAlg(AlgorithmIdentifiers.NONE);
         servicesManager.save(oidcRegisteredService);
         ticketRegistry.addTicket(accessToken);
-        request.addParameter(OAuth20Constants.TOKEN, accessToken.getId());
-        val result = oidcIntrospectionEndpointController.handleRequest(request, response);
-        assertTrue(result.getStatusCode().is4xxClientError());
+
+        mockMvc.perform(post("/cas/" + OidcConstants.BASE_OIDC_URL + '/' + OidcConstants.INTROSPECTION_URL)
+                .with(withHttpRequestProcessor())
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .header(HttpHeaders.ACCEPT, OAuth20Constants.INTROSPECTION_JWT_HEADER_CONTENT_TYPE)
+                .header(HttpHeaders.AUTHORIZATION, "Basic " + credentials)
+                .param(OAuth20Constants.TOKEN, accessToken.getId()))
+            .andExpect(status().is4xxClientError());
     }
 
     @Test
     void verifyOperationWithValidTicketAsJwtPlain() throws Throwable {
-        val request = getHttpRequestForEndpoint(OidcConstants.INTROSPECTION_URL);
-        val response = new MockHttpServletResponse();
-        request.addHeader(HttpHeaders.ACCEPT, OAuth20Constants.INTROSPECTION_JWT_HEADER_CONTENT_TYPE);
-
         val accessToken = getAccessToken(UUID.randomUUID().toString());
-        val value = EncodingUtils.encodeBase64((accessToken.getClientId() + ":secret").getBytes(StandardCharsets.UTF_8));
-        request.addHeader(HttpConstants.AUTHORIZATION_HEADER, HttpConstants.BASIC_HEADER_PREFIX + value);
+        val credentials = EncodingUtils.encodeBase64((accessToken.getClientId() + ":secret").getBytes(StandardCharsets.UTF_8));
 
         val oidcRegisteredService = getOidcRegisteredService(accessToken.getClientId());
         oidcRegisteredService.setIntrospectionSignedResponseAlg(null);
         servicesManager.save(oidcRegisteredService);
         ticketRegistry.addTicket(accessToken);
-        request.addParameter(OAuth20Constants.TOKEN, accessToken.getId());
-        val result = oidcIntrospectionEndpointController.handleRequest(request, response);
-        assertInstanceOf(PlainJWT.class, JWTParser.parse(result.getBody().toString()));
-        assertEquals(OAuth20Constants.INTROSPECTION_JWT_HEADER_CONTENT_TYPE, result.getHeaders().getContentType().toString());
+
+        val response = mockMvc.perform(post("/cas/" + OidcConstants.BASE_OIDC_URL + '/' + OidcConstants.INTROSPECTION_URL)
+                .with(withHttpRequestProcessor())
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .header(HttpHeaders.ACCEPT, OAuth20Constants.INTROSPECTION_JWT_HEADER_CONTENT_TYPE)
+                .header(HttpHeaders.AUTHORIZATION, "Basic " + credentials)
+                .param(OAuth20Constants.TOKEN, accessToken.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(OAuth20Constants.INTROSPECTION_JWT_HEADER_CONTENT_TYPE))
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+        assertInstanceOf(PlainJWT.class, JWTParser.parse(response));
     }
 
     @Test
     void verifyOperationWithValidTicket() throws Throwable {
-        val request = getHttpRequestForEndpoint(OidcConstants.INTROSPECTION_URL);
-        val response = new MockHttpServletResponse();
-
-        val value = EncodingUtils.encodeBase64("clientid:secret".getBytes(StandardCharsets.UTF_8));
-        request.addHeader(HttpConstants.AUTHORIZATION_HEADER, HttpConstants.BASIC_HEADER_PREFIX + value);
+        val credentials = EncodingUtils.encodeBase64("clientid:secret".getBytes(StandardCharsets.UTF_8));
 
         val accessToken = getAccessToken();
         servicesManager.save(getOidcRegisteredService());
         ticketRegistry.addTicket(accessToken);
-        request.addParameter(OAuth20Constants.TOKEN, accessToken.getId());
-        val result = oidcIntrospectionEndpointController.handleRequest(request, response);
-        val body = (OAuth20IntrospectionAccessTokenResponse) result.getBody();
-        assertNotNull(body);
-        assertTrue(Instant.ofEpochSecond(body.getExp()).isAfter(Instant.ofEpochSecond(body.getIat())));
-        assertTrue(body.isActive());
-        assertEquals(accessToken.getScopes(), Set.of(body.getScope().split(" ")));
+
+        val response = mockMvc.perform(post("/cas/" + OidcConstants.BASE_OIDC_URL + '/' + OidcConstants.INTROSPECTION_URL)
+                .with(withHttpRequestProcessor())
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .header(HttpHeaders.AUTHORIZATION, "Basic " + credentials)
+                .param(OAuth20Constants.TOKEN, accessToken.getId()))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.active").value(true))
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+
+        val body = MAPPER.readValue(response, Map.class);
+        val exp = ((Number) body.get("exp")).longValue();
+        val iat = ((Number) body.get("iat")).longValue();
+        assertTrue(Instant.ofEpochSecond(exp).isAfter(Instant.ofEpochSecond(iat)));
+        assertEquals(accessToken.getScopes(), Set.of(body.get("scope").toString().split(" ")));
     }
 
     @Test
     void verifyBadEndpointRequest() throws Throwable {
-        val request = getHttpRequestForEndpoint("unknown/issuer");
-        request.setRequestURI("unknown/issuer");
-        val response = new MockHttpServletResponse();
-        val mv = oidcIntrospectionEndpointController.handleRequest(request, response);
-        assertEquals(HttpStatus.BAD_REQUEST, mv.getStatusCode());
+        mockMvc.perform(post("/cas/" + OidcConstants.BASE_OIDC_URL + '/' + OidcConstants.INTROSPECTION_URL)
+                .with(withHttpRequestProcessor())
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .with(request -> {
+                    request.setServerName("unknown.issuer.org");
+                    return request;
+                }))
+            .andExpect(status().isUnauthorized());
     }
 
     @Test
     void verifyOperationWithInvalidTicket() throws Throwable {
-        val request = getHttpRequestForEndpoint(OidcConstants.INTROSPECTION_URL);
-        val response = new MockHttpServletResponse();
-
-        val auth = "clientid:secret";
-        val value = EncodingUtils.encodeBase64(auth.getBytes(StandardCharsets.UTF_8));
-        request.addHeader(HttpConstants.AUTHORIZATION_HEADER, HttpConstants.BASIC_HEADER_PREFIX + value);
+        val credentials = EncodingUtils.encodeBase64("clientid:secret".getBytes(StandardCharsets.UTF_8));
 
         val accessToken = getAccessToken();
         servicesManager.save(getOidcRegisteredService());
-        request.addParameter(OAuth20Constants.TOKEN, accessToken.getId());
-        val result = oidcIntrospectionEndpointController.handleRequest(request, response);
-        assertEquals(HttpStatus.OK, result.getStatusCode());
-        val body = (OAuth20IntrospectionAccessTokenResponse) result.getBody();
-        assertNotNull(body);
-        assertFalse(body.isActive());
+
+        mockMvc.perform(post("/cas/" + OidcConstants.BASE_OIDC_URL + '/' + OidcConstants.INTROSPECTION_URL)
+                .with(withHttpRequestProcessor())
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .header(HttpHeaders.AUTHORIZATION, "Basic " + credentials)
+                .param(OAuth20Constants.TOKEN, accessToken.getId()))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.active").value(false));
     }
 }
