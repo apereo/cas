e03fc1105298ad6ab0d9537b754d43921e52d73d
Misagh Moayyed <mm1844@gmail.com>
2026-02-07 12:09:15 -0800

    small polish

--- core/cas-server-core-webflow-api/src/main/java/org/apereo/cas/web/flow/configurer/ExternalRedirectAction.java
+++ core/cas-server-core-webflow-api/src/main/java/org/apereo/cas/web/flow/configurer/ExternalRedirectAction.java
@@ -4,6 +4,7 @@ import module java.base;
 import org.apereo.cas.web.support.WebUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.binding.expression.Expression;
 import org.springframework.webflow.action.AbstractAction;
 import org.springframework.webflow.execution.Event;
@@ -23,15 +24,17 @@ public class ExternalRedirectAction extends AbstractAction {
     @Override
     protected Event doExecute(final RequestContext context) throws Exception {
         val location = (String) this.resourceUri.getValue(context);
-        if (location.startsWith("servletRelative:") || location.startsWith("contextRelative:")
-            || location.startsWith("serverRelative:") || location.startsWith("http://")
-            || location.startsWith("https://")) {
-            context.getExternalContext().requestExternalRedirect(location);
-        } else {
-            val response = WebUtils.getHttpServletResponseFromExternalWebflowContext(context);
-            response.setStatus(HttpServletResponse.SC_FOUND);
-            response.setHeader("Location", location);
-            context.getExternalContext().recordResponseComplete();
+        if (StringUtils.isNotBlank(location)) {
+            if (location.startsWith("servletRelative:") || location.startsWith("contextRelative:")
+                || location.startsWith("serverRelative:") || location.startsWith("http://")
+                || location.startsWith("https://")) {
+                context.getExternalContext().requestExternalRedirect(location);
+            } else {
+                val response = WebUtils.getHttpServletResponseFromExternalWebflowContext(context);
+                response.setStatus(HttpServletResponse.SC_FOUND);
+                response.setHeader("Location", location);
+                context.getExternalContext().recordResponseComplete();
+            }
         }
         return this.success();
     }
