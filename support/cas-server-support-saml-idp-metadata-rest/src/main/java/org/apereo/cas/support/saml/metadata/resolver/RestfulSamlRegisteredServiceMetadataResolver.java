58b4dcae363220b621d93dff465b9dbca86c1a28
Misagh Moayyed <mm1844@gmail.com>
2026-02-26 15:30:25 -0800

    feat: allow REST API saml2 sp metadata mgmr to support CRUD ops

--- support/cas-server-support-saml-idp-metadata-rest/src/main/java/org/apereo/cas/support/saml/metadata/resolver/RestfulSamlRegisteredServiceMetadataResolver.java
+++ support/cas-server-support-saml-idp-metadata-rest/src/main/java/org/apereo/cas/support/saml/metadata/resolver/RestfulSamlRegisteredServiceMetadataResolver.java
@@ -9,6 +9,7 @@ import org.apereo.cas.support.saml.OpenSamlConfigBean;
 import org.apereo.cas.support.saml.services.SamlRegisteredService;
 import org.apereo.cas.support.saml.services.idp.metadata.SamlMetadataDocument;
 import org.apereo.cas.support.saml.services.idp.metadata.cache.resolver.BaseSamlRegisteredServiceMetadataResolver;
+import org.apereo.cas.support.saml.services.idp.metadata.cache.resolver.SamlRegisteredServiceMetadataManager;
 import org.apereo.cas.util.CollectionUtils;
 import org.apereo.cas.util.LoggingUtils;
 import org.apereo.cas.util.http.HttpExecutionRequest;
@@ -21,13 +22,14 @@ import net.shibboleth.shared.resolver.CriteriaSet;
 import org.apache.commons.io.IOUtils;
 import org.apache.hc.core5.http.HttpEntityContainer;
 import org.apache.hc.core5.http.HttpResponse;
-import org.apache.hc.core5.http.HttpStatus;
 import org.apereo.inspektr.audit.annotation.Audit;
 import org.hjson.JsonValue;
 import org.opensaml.saml.metadata.resolver.MetadataResolver;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.ObjectMapper;
 
 /**
@@ -37,7 +39,8 @@ import tools.jackson.databind.ObjectMapper;
  * @since 5.3.0
  */
 @Slf4j
-public class RestfulSamlRegisteredServiceMetadataResolver extends BaseSamlRegisteredServiceMetadataResolver {
+public class RestfulSamlRegisteredServiceMetadataResolver extends BaseSamlRegisteredServiceMetadataResolver
+    implements SamlRegisteredServiceMetadataManager {
     private static final ObjectMapper MAPPER = JacksonObjectMapperFactory.builder()
         .defaultTypingEnabled(false).build().toObjectMapper();
 
@@ -67,7 +70,7 @@ public class RestfulSamlRegisteredServiceMetadataResolver extends BaseSamlRegist
                 .headers(headers)
                 .build();
             response = HttpUtils.execute(exec);
-            if (response != null && response.getCode() == HttpStatus.SC_OK) {
+            if (response != null && HttpStatus.resolve(response.getCode()).is2xxSuccessful()) {
                 try (val content = ((HttpEntityContainer) response).getEntity().getContent()) {
                     val result = IOUtils.toString(content, StandardCharsets.UTF_8);
                     val doc = MAPPER.readValue(JsonValue.readHjson(result).toString(), SamlMetadataDocument.class);
@@ -87,7 +90,7 @@ public class RestfulSamlRegisteredServiceMetadataResolver extends BaseSamlRegist
     public boolean supports(final SamlRegisteredService service) {
         try {
             val metadataLocation = service.getMetadataLocation();
-            return metadataLocation.trim().startsWith("rest://");
+            return metadataLocation.trim().startsWith(getSourceId());
         } catch (final Exception e) {
             LoggingUtils.error(LOGGER, e);
         }
@@ -110,7 +113,7 @@ public class RestfulSamlRegisteredServiceMetadataResolver extends BaseSamlRegist
     }
 
     @Override
-    public void saveOrUpdate(final SamlMetadataDocument document) {
+    public SamlMetadataDocument store(final SamlMetadataDocument document) {
         HttpResponse response = null;
         try {
             val rest = samlIdPProperties.getMetadata().getRest();
@@ -135,5 +138,152 @@ public class RestfulSamlRegisteredServiceMetadataResolver extends BaseSamlRegist
         } finally {
             HttpUtils.close(response);
         }
+        return document;
+    }
+
+    @Override
+    public List<SamlMetadataDocument> load() {
+        HttpResponse response = null;
+        try {
+            val rest = samlIdPProperties.getMetadata().getRest();
+            val headers = CollectionUtils.<String, String>wrap(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_XML_VALUE);
+            headers.putAll(rest.getHeaders());
+
+            val exec = HttpExecutionRequest.builder()
+                .basicAuthPassword(rest.getBasicAuthPassword())
+                .basicAuthUsername(rest.getBasicAuthUsername())
+                .method(HttpMethod.GET)
+                .url(rest.getUrl())
+                .maximumRetryAttempts(rest.getMaximumRetryAttempts())
+                .headers(headers)
+                .build();
+            response = HttpUtils.execute(exec);
+            if (response != null && HttpStatus.resolve(response.getCode()).is2xxSuccessful()) {
+                try (val content = ((HttpEntityContainer) response).getEntity().getContent()) {
+                    val result = IOUtils.toString(content, StandardCharsets.UTF_8);
+                    return MAPPER.readValue(JsonValue.readHjson(result).toString(), new TypeReference<>() {
+                    });
+                }
+            }
+        } catch (final Exception e) {
+            LoggingUtils.error(LOGGER, e);
+        } finally {
+            HttpUtils.close(response);
+        }
+        return List.of();
+    }
+
+    @Override
+    public void removeById(final long id) {
+        HttpResponse response = null;
+        try {
+            val rest = samlIdPProperties.getMetadata().getRest();
+            val headers = CollectionUtils.<String, String>wrap(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_XML_VALUE);
+            headers.putAll(rest.getHeaders());
+
+            val exec = HttpExecutionRequest.builder()
+                .basicAuthPassword(rest.getBasicAuthPassword())
+                .basicAuthUsername(rest.getBasicAuthUsername())
+                .method(HttpMethod.DELETE)
+                .url(rest.getUrl())
+                .parameters(CollectionUtils.wrap("id", String.valueOf(id)))
+                .maximumRetryAttempts(rest.getMaximumRetryAttempts())
+                .headers(headers)
+                .build();
+            response = HttpUtils.execute(exec);
+        } catch (final Exception e) {
+            LoggingUtils.error(LOGGER, e);
+        } finally {
+            HttpUtils.close(response);
+        }
+    }
+
+    @Override
+    public void removeByName(final String name) {
+        HttpResponse response = null;
+        try {
+            val rest = samlIdPProperties.getMetadata().getRest();
+            val headers = CollectionUtils.<String, String>wrap(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_XML_VALUE);
+            headers.putAll(rest.getHeaders());
+
+            val exec = HttpExecutionRequest.builder()
+                .basicAuthPassword(rest.getBasicAuthPassword())
+                .basicAuthUsername(rest.getBasicAuthUsername())
+                .method(HttpMethod.DELETE)
+                .url(rest.getUrl())
+                .parameters(CollectionUtils.wrap("name", name))
+                .maximumRetryAttempts(rest.getMaximumRetryAttempts())
+                .headers(headers)
+                .build();
+            response = HttpUtils.execute(exec);
+        } catch (final Exception e) {
+            LoggingUtils.error(LOGGER, e);
+        } finally {
+            HttpUtils.close(response);
+        }
+    }
+
+    @Override
+    public Optional<SamlMetadataDocument> findByName(final String name) {
+        HttpResponse response = null;
+        try {
+            val rest = samlIdPProperties.getMetadata().getRest();
+            val headers = CollectionUtils.<String, String>wrap(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_XML_VALUE);
+            headers.putAll(rest.getHeaders());
+
+            val exec = HttpExecutionRequest.builder()
+                .basicAuthPassword(rest.getBasicAuthPassword())
+                .basicAuthUsername(rest.getBasicAuthUsername())
+                .method(HttpMethod.GET)
+                .url(rest.getUrl())
+                .parameters(CollectionUtils.wrap("name", name))
+                .maximumRetryAttempts(rest.getMaximumRetryAttempts())
+                .headers(headers)
+                .build();
+            response = HttpUtils.execute(exec);
+            if (response != null && HttpStatus.resolve(response.getCode()).is2xxSuccessful()) {
+                try (val content = ((HttpEntityContainer) response).getEntity().getContent()) {
+                    val result = IOUtils.toString(content, StandardCharsets.UTF_8);
+                    return Optional.of(MAPPER.readValue(JsonValue.readHjson(result).toString(), SamlMetadataDocument.class));
+                }
+            }
+        } catch (final Exception e) {
+            LoggingUtils.error(LOGGER, e);
+        } finally {
+            HttpUtils.close(response);
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<SamlMetadataDocument> findById(final long id) {
+        HttpResponse response = null;
+        try {
+            val rest = samlIdPProperties.getMetadata().getRest();
+            val headers = CollectionUtils.<String, String>wrap(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_XML_VALUE);
+            headers.putAll(rest.getHeaders());
+
+            val exec = HttpExecutionRequest.builder()
+                .basicAuthPassword(rest.getBasicAuthPassword())
+                .basicAuthUsername(rest.getBasicAuthUsername())
+                .method(HttpMethod.GET)
+                .url(rest.getUrl())
+                .parameters(CollectionUtils.wrap("id", String.valueOf(id)))
+                .maximumRetryAttempts(rest.getMaximumRetryAttempts())
+                .headers(headers)
+                .build();
+            response = HttpUtils.execute(exec);
+            if (response != null && HttpStatus.resolve(response.getCode()).is2xxSuccessful()) {
+                try (val content = ((HttpEntityContainer) response).getEntity().getContent()) {
+                    val result = IOUtils.toString(content, StandardCharsets.UTF_8);
+                    return Optional.of(MAPPER.readValue(JsonValue.readHjson(result).toString(), SamlMetadataDocument.class));
+                }
+            }
+        } catch (final Exception e) {
+            LoggingUtils.error(LOGGER, e);
+        } finally {
+            HttpUtils.close(response);
+        }
+        return Optional.empty();
     }
 }
