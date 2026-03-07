2aec73acf12ef37b3ae65f353eebf98b821478ec
Misagh Moayyed <mm1844@gmail.com>
2026-03-07 11:47:16 -0800

    chore: minor polish

--- support/cas-server-support-oidc-core-api/src/main/java/org/apereo/cas/oidc/jwks/OidcJsonWebKeyStoreUtils.java
+++ support/cas-server-support-oidc-core-api/src/main/java/org/apereo/cas/oidc/jwks/OidcJsonWebKeyStoreUtils.java
@@ -46,9 +46,9 @@ public class OidcJsonWebKeyStoreUtils {
     /**
      * Gets json web key set.
      *
-     * @param oidcRegisteredService        the service
-     * @param resourceLoader the resource loader
-     * @param usage          the usage
+     * @param oidcRegisteredService the service
+     * @param resourceLoader        the resource loader
+     * @param usage                 the usage
      * @return the json web key set
      */
     public static Optional<JsonWebKeySet> getJsonWebKeySet(final OidcRegisteredService oidcRegisteredService,
@@ -56,8 +56,6 @@ public class OidcJsonWebKeyStoreUtils {
                                                            final Optional<OidcJsonWebKeyUsage> usage) {
         return FunctionUtils.doAndHandle(
                 () -> {
-                    val serviceJwks = SpringExpressionLanguageValueResolver.getInstance().resolve(oidcRegisteredService.getJwks());
-                    LOGGER.trace("Loading JSON web key from [{}]", serviceJwks);
                     val resource = getJsonWebKeySetResource(oidcRegisteredService, resourceLoader);
                     if (resource == null) {
                         LOGGER.debug("No JSON web keys or keystore resource could be found for [{}]", oidcRegisteredService.getServiceId());
