db1572b292e802e51f819799a9a8288db3602fab
Misagh Moayyed <mm1844@gmail.com>
2026-03-07 19:11:14 -0800

    feat: provide skeleton design for registering jwks for oidc clients

--- support/cas-server-support-oidc-core-api/src/main/java/org/apereo/cas/oidc/web/controllers/jwks/OidcJwksRegistrationEndpointController.java
+++ support/cas-server-support-oidc-core-api/src/main/java/org/apereo/cas/oidc/web/controllers/jwks/OidcJwksRegistrationEndpointController.java
@@ -8,19 +8,20 @@ import org.apereo.cas.oidc.jwks.register.ClientJwksRegistrationRequest;
 import org.apereo.cas.oidc.jwks.register.ClientJwksRegistrationResponse;
 import org.apereo.cas.oidc.jwks.register.ClientJwksRegistrationStore;
 import org.apereo.cas.oidc.web.controllers.BaseOidcController;
+import org.apereo.cas.ticket.accesstoken.OAuth20AccessToken;
+import org.apereo.cas.util.function.FunctionUtils;
 import com.nimbusds.jose.jwk.ECKey;
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.RSAKey;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.server.ResponseStatusException;
+import jakarta.servlet.http.HttpServletRequest;
 import java.security.Signature;
 
 /**
@@ -44,11 +45,15 @@ public class OidcJwksRegistrationEndpointController extends BaseOidcController {
         this.clientJwksRegistrationStore = clientJwksRegistrationStore;
     }
 
+    /**
+     * Handle registration.
+     *
+     * @param request the request
+     * @return the response entity
+     * @throws Throwable the throwable
+     */
     @PostMapping('/' + OidcConstants.BASE_OIDC_URL + '/' + OidcConstants.JWKS_URL + "/clients/register")
-    public ResponseEntity handleRegistration(
-        @RequestHeader(HttpHeaders.AUTHORIZATION) final String authorization,
-        @RequestBody final ClientJwksRegistrationRequest request) throws Throwable {
-
+    public ResponseEntity handleRegistration(@RequestBody final ClientJwksRegistrationRequest request) throws Throwable {
         val entry = clientJwksRegistrationNonceStore.find(request.nonceId());
 
         val jwk = JWK.parse(request.publicJwk());
@@ -66,9 +71,24 @@ public class OidcJwksRegistrationEndpointController extends BaseOidcController {
         return ResponseEntity.ok(new ClientJwksRegistrationResponse(jkt));
     }
 
+    /**
+     * Handle challenge.
+     *
+     * @param request the request
+     * @return the response entity
+     */
     @PostMapping('/' + OidcConstants.BASE_OIDC_URL + '/' + OidcConstants.JWKS_URL + "/clients/challenge")
-    public ResponseEntity handleChallenge(
-        @RequestHeader(HttpHeaders.AUTHORIZATION) final String authorization) {
+    public ResponseEntity handleChallenge(final HttpServletRequest request) {
+        val accessToken = getAccessTokenFromRequest(request).getValue();
+        val accessTokenTicket = FunctionUtils.doAndHandle(() -> {
+            val decodedToken = getConfigurationContext().getTicketRegistry().getTicket(accessToken, OAuth20AccessToken.class);
+            return decodedToken == null || decodedToken.isExpired() ? null : decodedToken;
+        });
+        if (accessTokenTicket == null
+            || accessTokenTicket.isExpired()
+            || !accessTokenTicket.getScopes().contains(OidcConstants.CLIENT_JWKS_REGISTRATION_SCOPE)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid access token");
+        }
         return ResponseEntity.ok(clientJwksRegistrationNonceStore.create());
     }
 
