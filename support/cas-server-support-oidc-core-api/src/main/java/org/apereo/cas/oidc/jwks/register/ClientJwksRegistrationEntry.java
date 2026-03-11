2f0d07a390f50f8118cb699a567709ee0dcbd750
Misagh Moayyed <mm1844@gmail.com>
2026-03-10 19:26:08 -0700

    feat: allow mongodb to manage oidc client jwks

--- support/cas-server-support-oidc-core-api/src/main/java/org/apereo/cas/oidc/jwks/register/ClientJwksRegistrationEntry.java
+++ support/cas-server-support-oidc-core-api/src/main/java/org/apereo/cas/oidc/jwks/register/ClientJwksRegistrationEntry.java
@@ -1,6 +1,7 @@
 package org.apereo.cas.oidc.jwks.register;
 
 import module java.base;
+import org.springframework.data.annotation.Id;
 
 /**
  * This is {@link ClientJwksRegistrationEntry}.
@@ -8,7 +9,8 @@ import module java.base;
  * @author Misagh Moayyed
  * @since 8.0.0
  */
-public record ClientJwksRegistrationEntry(String jkt, String jwk, Instant createdAt) implements Serializable {
+public record ClientJwksRegistrationEntry(
+    @Id String jkt, String jwk, Instant createdAt) implements Serializable {
     @Serial
     private static final long serialVersionUID = -3122269588579072890L;
 }
