d6948fef56b6d90caefcffab096033fce5423ea1
Misagh Moayyed <mm1844@gmail.com>
2026-03-12 11:16:14 -0700

    fix: handle skew-allownce overrides per app for saml2 conditions

--- support/cas-server-support-saml-idp-web/src/main/java/org/apereo/cas/support/saml/web/idp/profile/builders/conditions/SamlProfileSamlConditionsBuilder.java
+++ support/cas-server-support-saml-idp-web/src/main/java/org/apereo/cas/support/saml/web/idp/profile/builders/conditions/SamlProfileSamlConditionsBuilder.java
@@ -34,19 +34,12 @@ public class SamlProfileSamlConditionsBuilder extends AbstractSaml20ObjectBuilde
         return buildConditions(context);
     }
 
-    /**
-     * Build conditions.
-     *
-     * @param context the context
-     * @return the conditions
-     * @throws SamlException the saml exception
-     */
     protected Conditions buildConditions(final SamlProfileBuilderContext context) throws SamlException {
         val currentDateTime = ZonedDateTime.now(ZoneOffset.UTC);
         var skewAllowance = context.getRegisteredService().getSkewAllowance() != 0
             ? context.getRegisteredService().getSkewAllowance()
             : Beans.newDuration(casProperties.getAuthn().getSamlIdp().getResponse().getSkewAllowance()).toSeconds();
-        if (skewAllowance != 0) {
+        if (skewAllowance == 0) {
             skewAllowance = Beans.newDuration(casProperties.getSamlCore().getSkewAllowance()).toSeconds();
         }
 
