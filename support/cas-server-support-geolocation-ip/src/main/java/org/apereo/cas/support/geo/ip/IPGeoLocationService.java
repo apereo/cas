5817ac9250a4e828e4266decc7b584a72bb6736f
Misagh Moayyed <mm1844@gmail.com>
2026-03-16 09:19:12 -0700

    fix: remove compile issues after lib upgrade

--- support/cas-server-support-geolocation-ip/src/main/java/org/apereo/cas/support/geo/ip/IPGeoLocationService.java
+++ support/cas-server-support-geolocation-ip/src/main/java/org/apereo/cas/support/geo/ip/IPGeoLocationService.java
@@ -3,7 +3,9 @@ package org.apereo.cas.support.geo.ip;
 import module java.base;
 import org.apereo.cas.authentication.adaptive.geo.GeoLocationResponse;
 import org.apereo.cas.support.geo.AbstractGeoLocationService;
-import io.ipgeolocation.sdk.api.IPGeolocationAPI;
+import io.ipgeolocation.sdk.IpGeolocationClient;
+import io.ipgeolocation.sdk.Language;
+import io.ipgeolocation.sdk.LookupIpGeolocationRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
@@ -18,26 +20,26 @@ import org.apache.commons.lang3.StringUtils;
 @RequiredArgsConstructor
 @Slf4j
 public class IPGeoLocationService extends AbstractGeoLocationService {
-    private final IPGeolocationAPI api;
+    private final IpGeolocationClient api;
 
     @Override
-    public GeoLocationResponse locate(final InetAddress address) throws Throwable {
-        val geolocation = api.getIPGeolocation()
-            .lang("en")
-            .ip(address.getHostAddress())
-            .execute();
+    public GeoLocationResponse locate(final InetAddress address) {
+        val geolocation = api.lookupIpGeolocation(LookupIpGeolocationRequest.builder()
+            .ip(address.getHostAddress()).lang(Language.EN).build());
+
         LOGGER.debug("Geolocation results for [{}] are [{}]", address.getHostAddress(), geolocation);
-        if (geolocation != null && geolocation.getLocation() != null) {
+        if (geolocation.data().location() != null) {
             val location = new GeoLocationResponse();
-            val geoLocation = geolocation.getLocation();
+            val geoLocation = geolocation.data().location();
             return location
-                .setLatitude(Double.parseDouble(StringUtils.defaultIfBlank(geoLocation.getLatitude(), "0.0")))
-                .setLongitude(Double.parseDouble(StringUtils.defaultIfBlank(geoLocation.getLongitude(), "0.0")))
-                .addAddress(geoLocation.getCity())
-                .addAddress(geoLocation.getStateProv())
-                .addAddress(geoLocation.getCountryName())
-                .addAddress(geoLocation.getCountryCode3())
-                .addAddress(geoLocation.getZipcode());
+                .setLatitude(Double.parseDouble(StringUtils.defaultIfBlank(geoLocation.latitude(), "0.0")))
+                .setLongitude(Double.parseDouble(StringUtils.defaultIfBlank(geoLocation.longitude(), "0.0")))
+                .addAddress(geoLocation.city())
+                .addAddress(geoLocation.stateProv())
+                .addAddress(geoLocation.countryNameOfficial())
+                .addAddress(geoLocation.countryCode2())
+                .addAddress(geoLocation.countryCode3())
+                .addAddress(geoLocation.zipcode());
         }
         LOGGER.warn("Unable to determine geolocation results for [{}]", address.getHostAddress());
         return null;
