5d7684e2105293850f2fceec180546a062018975
Misagh Moayyed <mm1844@gmail.com>
2026-04-22 10:17:34 -0700

    chore: add test case for otel metrics and traces

---
+++ ci/tests/puppeteer/scenarios/metrics-tracing-otlp/script.js
@@ -0,0 +1,10 @@
+const cas = require("../../cas.js");
+
+(async () => {
+    const browser = await cas.newBrowser(cas.browserOptions());
+    const page = await cas.newPage(browser);
+    await cas.gotoLogin(page);
+    await cas.loginWith(page);
+    await cas.gotoLogout(page);
+    await cas.closeBrowser(browser);
+})();
