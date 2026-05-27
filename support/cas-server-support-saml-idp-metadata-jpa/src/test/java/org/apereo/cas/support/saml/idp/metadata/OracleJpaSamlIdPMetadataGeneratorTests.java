d86dc9d89a3939effe5ba44abade898bf08b8e93
Misagh Moayyed <mm1844@gmail.com>
2026-05-26 21:33:37 -0700

    chore: fix tests

--- support/cas-server-support-saml-idp-metadata-jpa/src/test/java/org/apereo/cas/support/saml/idp/metadata/OracleJpaSamlIdPMetadataGeneratorTests.java
+++ support/cas-server-support-saml-idp-metadata-jpa/src/test/java/org/apereo/cas/support/saml/idp/metadata/OracleJpaSamlIdPMetadataGeneratorTests.java
@@ -13,10 +13,10 @@ import org.springframework.test.context.TestPropertySource;
  */
 @TestPropertySource(properties = {
     "cas.jdbc.show-sql=false",
-    "cas.authn.saml-idp.metadata.jpa.user=system",
-    "cas.authn.saml-idp.metadata.jpa.password=Oradoc_db1",
+    "cas.authn.saml-idp.metadata.jpa.user=cas",
+    "cas.authn.saml-idp.metadata.jpa.password=cas",
     "cas.authn.saml-idp.metadata.jpa.driver-class=oracle.jdbc.driver.OracleDriver",
-    "cas.authn.saml-idp.metadata.jpa.url=jdbc:oracle:thin:@localhost:1521:FREE",
+    "cas.authn.saml-idp.metadata.jpa.url=jdbc:oracle:thin:@//localhost:1521/FREEPDB1",
     "cas.authn.saml-idp.metadata.jpa.dialect=org.hibernate.dialect.OracleDialect"
 })
 @EnabledIfListeningOnPort(port = 1521)
