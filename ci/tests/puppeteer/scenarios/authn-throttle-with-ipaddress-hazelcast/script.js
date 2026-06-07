0b311d6a91d6f77b2b7bcb86e58ef1dd59654acf
Misagh Moayyed <mm1844@gmail.com>
2026-06-07 08:41:42 -0700

    chore: fix build

--- ci/tests/puppeteer/scenarios/authn-throttle-with-ipaddress-hazelcast/script.js
+++ ci/tests/puppeteer/scenarios/authn-throttle-with-ipaddress-hazelcast/script.js
@@ -6,6 +6,7 @@ const cas = require("../../cas.js");
         "package": "org.apereo.cas.throttle",
         "level": "DEBUG"
     }]);
+    await cas.doDelete("https://localhost:8443/cas/actuator/throttles?clear=true");
     
     let browser = await cas.newBrowser(cas.browserOptions());
     let page = await cas.newPage(browser);
