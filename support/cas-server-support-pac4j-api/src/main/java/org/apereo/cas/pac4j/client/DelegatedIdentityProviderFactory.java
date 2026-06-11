58c44107f8d949107926b43d8eba8ae79ccde046
Misagh Moayyed <mm1844@gmail.com>
2026-06-10 20:00:35 -0700

    fix build

--- support/cas-server-support-pac4j-api/src/main/java/org/apereo/cas/pac4j/client/DelegatedIdentityProviderFactory.java
+++ support/cas-server-support-pac4j-api/src/main/java/org/apereo/cas/pac4j/client/DelegatedIdentityProviderFactory.java
@@ -16,6 +16,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * This is {@link DelegatedIdentityProviderFactory}.
