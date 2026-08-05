54e70a0786a58c457c0a87ba0caf93e0632348dd
Misagh Moayyed <mm1844@gmail.com>
2026-08-05 10:51:41 -0700

    fix tests

--- api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/support/oidc/federation/OidcFederationProperties.java
+++ api/cas-server-core-api-configuration-model/src/main/java/org/apereo/cas/configuration/model/support/oidc/federation/OidcFederationProperties.java
@@ -28,7 +28,7 @@ public class OidcFederationProperties implements Serializable {
      * The role of this server.
      */
     @RequiredProperty
-    private OidcFederationRole role;
+    private OidcFederationRole role = OidcFederationRole.OPENID_PROVIDER;
 
     /**
      * Organization name to include in the federation metadata.
